d0b6bce7185b3d79ce2f990c2f968be9a35071d0
Ryan Williams <ryan.blake.williams@gmail.com>
2013-01-22 17:33:28 -0500

    couple more feedback nits (sapling split of d8c0e1ada4c55b33dfc236defcb5ad3ab0907c01)

--- tests/python/twitter/pants/targets/test_util.py
+++ tests/python/twitter/pants/targets/test_util.py
@@ -1,9 +1,24 @@
+# ==================================================================================================
+# Copyright 2013 Foursquare Labs, Inc.
+# --------------------------------------------------------------------------------------------------
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this work except in compliance with the License.
+# You may obtain a copy of the License in the LICENSE file, or at:
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==================================================================================================
+
 __author__ = 'Ryan Williams'
 
 import unittest
-from twitter.pants.base import Target
 
-class MockPantsTarget(Target):
+class MockPantsTarget(object):
   def __init__(self, spec):
     self.foo = spec
 
@@ -35,7 +50,11 @@ class ResolveTest(unittest.TestCase):
   def testMixedList(self):
     self.assertEquals(
       resolve([MockPantsTarget("1"), "2", MockPantsTarget("3"), "4", "5"], clazz=MockPantsTarget),
-      [MockPantsTarget("1"), MockPantsTarget("2"), MockPantsTarget("3"), MockPantsTarget("4"), MockPantsTarget("5")])
+      [MockPantsTarget("1"),
+       MockPantsTarget("2"),
+       MockPantsTarget("3"),
+       MockPantsTarget("4"),
+       MockPantsTarget("5")])
 
   def testNonTarget(self):
     self.assertEquals(
