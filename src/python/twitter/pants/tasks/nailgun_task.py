eb4c09b47006ae966696b058b05da2a51bbfb3b8
Benjy <benjy@foursquare.com>
2013-02-05 14:54:18 -0800

    Fix forking bug.
    The old code caused a race condition in contexts, such as
    temporary_dir() etc.
    
    (sapling split of 8125c336f800945547aced3d6f065ae1dbf885ca)

--- src/python/twitter/pants/tasks/nailgun_task.py
+++ src/python/twitter/pants/tasks/nailgun_task.py
@@ -246,7 +246,9 @@ class NailgunTask(Task):
       with _safe_open(self._pidfile, 'w') as pidfile:
         pidfile.write('%d' % process.pid)
       log.debug('Spawned ng server @ %d' % process.pid)
-      sys.exit(0)
+      # Prevents finally blocks being executed, unlike sys.exit(). We don't want to execute finally
+      # blocks because we might, e.g., clean up tempfiles that the parent still needs.
+      os._exit(0)
 
 
 # Pick implementations for killall and _find. We don't use psutil, as it uses
