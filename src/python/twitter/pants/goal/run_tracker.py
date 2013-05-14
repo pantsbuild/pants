05856c8b8b93f7d4903af91faa351d5b0c191a93
Benjy <benjyw@gmail.com>
2013-05-14 11:37:35 -0400

    WorkUnit and RunTracker concepts for pants.
    WorkUnit represents a unit of work, for the purpose of tracking and timing.
    
    RunTracker keeps track of all the work in a single pants run.
    
    Auditors: markcc
    
    (sapling split of 5bb36168f8f0dc1e5c3cc20213f7af2d7d21ea78)

---
+++ src/python/twitter/pants/goal/run_tracker.py
@@ -0,0 +1,138 @@
+import os
+import sys
+import time
+
+from contextlib import contextmanager
+
+from twitter.pants.goal.artifact_cache_stats import ArtifactCacheStats
+from twitter.pants.base.run_info import RunInfo
+from twitter.pants.goal.aggregated_timings import AggregatedTimings
+from twitter.pants.goal.workunit import WorkUnit
+
+
+class RunTracker(object):
+  """Tracks and times the execution of a pants run.
+
+  Use like this:
+
+  run_tracker.start()
+  with run_tracker.new_workunit('compile'):
+    with run_tracker.new_workunit('java') as workunit1:
+      workunit1.report('Compiling java.')
+      ...
+      workunit1.report('Done compiling java.')
+    with run_tracker.new_workunit('scala') as workunit2:
+      ...
+  run_tracker.close()
+
+  """
+  def __init__(self, config):
+    self.run_timestamp = time.time()  # A double, so we get subsecond precision for ids.
+    cmd_line = ' '.join(['pants'] + sys.argv[1:])
+
+    # run_id is safe for use in paths.
+    millis = (self.run_timestamp * 1000) % 1000
+    run_id = 'pants_run_%s_%d' % \
+             (time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(self.run_timestamp)), millis)
+
+    self.info_dir = os.path.join(config.getdefault('info_dir'), run_id)
+    self.run_info = RunInfo(os.path.join(self.info_dir, 'info'))
+    self.run_info.add_basic_info(run_id, self.run_timestamp)
+    self.run_info.add_info('cmd_line', cmd_line)
+
+    # Create a 'latest' symlink, after we add_infos, so we're guaranteed that the file exists.
+    link_to_latest = os.path.join(os.path.dirname(self.info_dir), 'latest')
+    if os.path.exists(link_to_latest):
+      os.unlink(link_to_latest)
+    os.symlink(self.info_dir, link_to_latest)
+
+    # Time spent in a workunit, including its children.
+    self.cumulative_timings = AggregatedTimings(os.path.join(self.info_dir, 'cumulative_timings'))
+
+    # Time spent in a workunit, not including its children.
+    self.self_timings = AggregatedTimings(os.path.join(self.info_dir, 'self_timings'))
+
+    # Hit/miss stats for the artifact cache.
+    self.artifact_cache_stats = \
+      ArtifactCacheStats(os.path.join(self.info_dir, 'artifact_cache_stats'))
+
+    # We report to this Report.
+    self._report = None
+
+    # The workunit representing the entire pants run.
+    self.root_workunit = None
+
+    # The workunit we're currently executing.
+    # TODO: What does this mean when executing multiple workunits in parallel?
+    self._current_workunit = None
+
+    # Set later, after options are parsed.
+    # TODO: Get rid of this. We only need it in one place, so find some other solution for that.
+    self.options = None
+
+  def start(self, report):
+    """Start tracking this pants run."""
+    self._report = report
+    self._report.open()
+
+    self.root_workunit = WorkUnit(run_tracker=self, parent=None,
+                                  labels=[], name='all', cmd=None)
+    self.root_workunit.start()
+
+    self._report.start_workunit(self.root_workunit)
+    self._current_workunit = self.root_workunit
+
+  @contextmanager
+  def new_workunit(self, name, labels=list(), cmd=''):
+    """Creates a (hierarchical) subunit of work for the purpose of timing and reporting.
+
+    - name: A short name for this work. E.g., 'resolve', 'compile', 'scala', 'zinc'.
+    - labels: An optional iterable of labels. The reporters can use this to decide how to
+              display information about this work.
+    - cmd: An optional longer string representing this work.
+           E.g., the cmd line of a compiler invocation.
+
+    Use like this:
+
+    with context.new_workunit(name='compile', labels=[WorkUnit.GOAL]) as workunit:
+      <do scoped work here>
+      <set the outcome on workunit if necessary>
+
+    Note that the outcome will automatically be set to failure if an exception is raised
+    in a workunit, and to success otherwise, so usually you only need to set the
+    outcome explicitly if you want to set it to warning.
+    """
+    self._current_workunit = WorkUnit(run_tracker=self, parent=self._current_workunit,
+                                      name=name, labels=labels, cmd=cmd)
+    self._current_workunit.start()
+    try:
+      self._report.start_workunit(self._current_workunit)
+      yield self._current_workunit
+    except KeyboardInterrupt:
+      self._current_workunit.set_outcome(WorkUnit.ABORTED)
+      raise
+    except:
+      self._current_workunit.set_outcome(WorkUnit.FAILURE)
+      raise
+    else:
+      self._current_workunit.set_outcome(WorkUnit.SUCCESS)
+    finally:
+      self._report.end_workunit(self._current_workunit)
+      self._current_workunit.end()
+      self._current_workunit = self._current_workunit.parent
+
+  def report(self, *msg_elements):
+    """Log a message against the current workunit."""
+    self._report.message(self._current_workunit, *msg_elements)
+
+  def end(self):
+    """This pants run is over, so stop tracking it."""
+    while self._current_workunit:
+      self._report.end_workunit(self._current_workunit)
+      self._current_workunit.end()
+      self._current_workunit = self._current_workunit.parent
+    self._report.close()
+    try:
+      self.run_info.add_info('outcome', self.root_workunit.outcome_string())
+    except IOError:
+      pass  # If the goal is clean-all then the run info dir no longer exists...
---
+++ src/python/twitter/pants/goal/workunit.py
@@ -0,0 +1,183 @@
+import os
+import re
+import time
+import uuid
+
+from twitter.common.dirutil import safe_mkdir_for
+from twitter.common.rwbuf.read_write_buffer import FileBackedRWBuf
+
+
+class WorkUnit(object):
+  """A hierarchical unit of work, for the purpose of timing and reporting.
+
+  A WorkUnit can be subdivided into further WorkUnits. The WorkUnit concept is deliberately
+  decoupled from the phase/task hierarchy. This allows some flexibility in having, say,
+  sub-units inside a task. E.g., there might be one WorkUnit representing an entire pants run,
+  and that can be subdivided into WorkUnits for each phase. Each of those can be subdivided into
+  WorkUnits for each task, and a task can subdivide that into further work units, if finer-grained
+  timing and reporting is needed.
+  """
+
+  # The outcome of a workunit.
+  # It can only be set to a new value <= the old one.
+  ABORTED = 0
+  FAILURE = 1
+  WARNING = 2
+  SUCCESS = 3
+  UNKNOWN = 4
+
+  # Labels describing a workunit.  Reporting code can use this to decide how to display
+  # information about this workunit.
+  #
+  # Note that a workunit can have multiple labels where this makes sense, e.g., TOOL and NAILGUN.
+  SETUP = 0      # Parsing build files etc.
+  PHASE = 1      # Executing a phase.
+  GOAL = 2       # Executing a goal.
+  GROUP = 3      # Executing a group.
+  TOOL = 4       # Single invocations of a tool.
+  MULTITOOL = 5  # Multiple consecutive invocations of the same tool.
+  TEST = 6       # Running a test.
+  JVM = 7        # Running via the JVM.
+  NAILGUN = 8    # Running via nailgun.
+  REPL = 9
+
+  def __init__(self, run_tracker, parent, name, labels=(), cmd=''):
+    """
+    - run_tracker: The RunTracker that tracks this WorkUnit.
+    - parent: The containing workunit, if any. E.g., 'compile' might contain 'java', 'scala' etc.,
+              'scala' might contain 'compile', 'split' etc.
+    - name: A short name for this work. E.g., 'resolve', 'compile', 'scala', 'zinc'.
+    - labels: An optional iterable of labels. The reporters can use this to decide how to
+              display information about this work.
+    - cmd: An optional longer string representing this work.
+           E.g., the cmd line of a compiler invocation.
+    """
+    self._outcome = WorkUnit.UNKNOWN
+
+    self.run_tracker = run_tracker
+    self.parent = parent
+    self.children = []
+
+    if self.parent:
+      self.parent.children.append(self)
+
+    self.name = name
+    self.labels = set(labels)
+    self.cmd = cmd
+    self.id = uuid.uuid4()
+
+    # In seconds since the epoch. Doubles, to account for fractional seconds.
+    self.start_time = 0
+    self.end_time = 0
+
+    # A workunit may have multiple outputs, which we identify by a name.
+    # E.g., a tool invocation may have 'stdout', 'stderr', 'debug_log' etc.
+    self._outputs = {}  # name -> output buffer.
+
+  def has_label(self, label):
+    return label in self.labels
+
+  def start(self):
+    """Mark the time at which this workunit started."""
+    self.start_time = time.time()
+
+  def end(self):
+    """Mark the time at which this workunit ended."""
+    self.end_time = time.time()
+    for output in self._outputs.values():
+      output.close()
+    is_tool = self.has_label(WorkUnit.TOOL)
+    path = self.path()
+    self.run_tracker.cumulative_timings.add_timing(path, self.duration(), is_tool)
+    self.run_tracker.self_timings.add_timing(path, self._self_time(), is_tool)
+
+  def outcome(self):
+    """Returns the outcome of this workunit."""
+    return self._outcome
+
+  def set_outcome(self, outcome):
+    """Set the outcome of this work unit.
+
+    We can set the outcome on a work unit directly, but that outcome will also be affected by
+    those of its subunits. The right thing happens: The outcome of a work unit is the
+    worst outcome of any of its subunits and any outcome set on it directly."""
+    if outcome < self._outcome:
+      self._outcome = outcome
+      self.choose(0, 0, 0, 0, 0)  # Dummy call, to validate.
+      if self.parent: self.parent.set_outcome(self._outcome)
+
+  _valid_name_re = re.compile(r'\w+')
+
+  def output(self, name):
+    """Returns the output buffer for the specified output name (e.g., 'stdout')."""
+    m = WorkUnit._valid_name_re.match(name)
+    if not m or m.group(0) != name:
+      raise Exception('Invalid output name: %s' % name)
+    if name not in self._outputs:
+      path = os.path.join(self.run_tracker.info_dir, 'tool_outputs', '%s.%s' % (self.id, name))
+      safe_mkdir_for(path)
+      self._outputs[name] = FileBackedRWBuf(path)
+    return self._outputs[name]
+
+  def outputs(self):
+    """Returns the map of output name -> output buffer."""
+    return self._outputs
+
+  def choose(self, aborted_val, failure_val, warning_val, success_val, unknown_val):
+    """Returns one of the 5 arguments, depending on our outcome."""
+    if self._outcome not in range(0, 5):
+      raise Exception('Invalid outcome: %s' % self._outcome)
+    return (aborted_val, failure_val, warning_val, success_val, unknown_val)[self._outcome]
+
+  def outcome_string(self):
+    """Returns a human-readable string describing our outcome."""
+    return self.choose('ABORTED', 'FAILURE', 'WARNING', 'SUCCESS', 'UNKNOWN')
+
+  def duration(self):
+    """Returns the time (in fractional seconds) spent in this workunit and its children."""
+    return (self.end_time or time.time()) - self.start_time
+
+  def start_time_string(self):
+    """A convenient string representation of start_time."""
+    return time.strftime('%H:%M:%S', time.localtime(self.start_time))
+
+  def start_delta_string(self):
+    """A convenient string representation of how long after the run started we started."""
+    delta = int(self.start_time) - int(self.run_tracker.root_workunit.start_time)
+    return '%02d:%02d' % (delta / 60, delta % 60)
+
+  def ancestors(self):
+    """Returns a list consisting of this workunit and those enclosing it, up to the root."""
+    ret = []
+    workunit = self
+    while workunit is not None:
+      ret.append(workunit)
+      workunit = workunit.parent
+    return ret
+
+  def path(self):
+    """Returns a path string for this workunit, E.g., 'all:compile:jvm:scalac'."""
+    return ':'.join(reversed([w.name for w in self.ancestors()]))
+
+  def unaccounted_time(self):
+    """Returns non-leaf time spent in this workunit.
+
+    This assumes that all major work should be done in leaves.
+    TODO: Is this assumption valid?
+    """
+    return 0 if len(self.children) == 0 else self._self_time()
+
+  def to_dict(self):
+    """Useful for providing arguments to templates."""
+    ret = {}
+    for key in ['name', 'cmd', 'id', 'start_time', 'end_time',
+                'outcome', 'start_time_string', 'start_delta_string']:
+      val = getattr(self, key)
+      ret[key] = val() if hasattr(val, '__call__') else val
+    ret['parent'] = self.parent.to_dict() if self.parent else None
+    return ret
+
+  def _self_time(self):
+    """Returns the time spent in this workunit outside of any children."""
+    return self.duration() - sum([child.duration() for child in self.children])
+
