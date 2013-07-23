2e8688816e4a759ea3a005ae2d864d0829610c9f
Benjy <benjy@foursquare.com>
2013-07-22 17:17:21 -0700

    Get rid of idl-resolve and extract tasks.
    Code is still there, just unlinked them from the execution graph.
    
    (sapling split of b50d2700afe36fdcf924b249c257489c0975e7d3)

--- src/python/twitter/pants/commands/goal.py
+++ src/python/twitter/pants/commands/goal.py
@@ -466,23 +466,15 @@ class Goal(Command):
 
 # Install all default pants provided goals
 from twitter.pants import junit_tests
-from twitter.pants.targets import (
-  Benchmark,
-  JavaLibrary,
-  JvmBinary,
-  ScalaLibrary,
-  ScalaTests,
-  ScalacPlugin)
+from twitter.pants.targets import Benchmark, JvmBinary
 from twitter.pants.tasks.antlr_gen import AntlrGen
 from twitter.pants.tasks.benchmark_run import BenchmarkRun
 from twitter.pants.tasks.binary_create import BinaryCreate
 from twitter.pants.tasks.build_lint import BuildLint
 from twitter.pants.tasks.bundle_create import BundleCreate
 from twitter.pants.tasks.checkstyle import Checkstyle
-from twitter.pants.tasks.extract import Extract
 from twitter.pants.tasks.check_exclusives import CheckExclusives
 from twitter.pants.tasks.filedeps import FileDeps
-from twitter.pants.tasks.idl_resolve import IdlResolve
 from twitter.pants.tasks.ivy_resolve import IvyResolve
 from twitter.pants.tasks.jar_create import JarCreate
 from twitter.pants.tasks.java_compile import JavaCompile
@@ -686,27 +678,12 @@ goal(name='check-exclusives',
   action=CheckExclusives).install('check-exclusives').with_description(
   'Check exclusives declarations to verify that dependencies are consistent.')
 
-
-goal(
-  name='idl',
-  action=IdlResolve,
-).install('resolve-idl').with_description('Resolves idl jar dependencies.')
-
-goal(
-  name='extract',
-  action=Extract,
-).install('resolve-idl')
-
 # TODO(John Sirois): gen attempted as the sole Goal should gen for all known gen types but
 # recognize flags to narrow the gen set
-goal(name='thrift', action=ThriftGen,
-  dependencies=['resolve-idl']).install('gen').with_description('Generate code.')
-goal(name='scrooge', action=ScroogeGen,
-  dependencies=['resolve-idl']).install('gen')
-goal(name='protoc', action=ProtobufGen,
-  dependencies=['resolve-idl']).install('gen')
-goal(name='antlr', action=AntlrGen,
-  dependencies=['resolve-idl']).install('gen')
+goal(name='thrift', action=ThriftGen).install('gen').with_description('Generate code.')
+goal(name='scrooge', action=ScroogeGen).install('gen')
+goal(name='protoc', action=ProtobufGen).install('gen')
+goal(name='antlr', action=AntlrGen).install('gen')
 
 goal(
   name='checkstyle',
