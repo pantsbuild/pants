33162e728627628a0212f023e1a63f6ca04973c3
Peter Vlugter <pvlugter@gmail.com>
2013-09-06 10:11:47 +1200

    Bump version to 0.3.1-SNAPSHOT

--- project/ZincBuild.scala
+++ project/ZincBuild.scala
@@ -12,7 +12,7 @@ object ZincBuild extends Build {
 
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.typesafe.zinc",
-    version := "0.3.0-SNAPSHOT",
+    version := "0.3.1-SNAPSHOT",
     scalaVersion := "2.10.2",
     crossPaths := false
   )
