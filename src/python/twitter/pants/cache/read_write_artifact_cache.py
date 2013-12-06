ec79afeb0373e0155840bef4dcb84020bcc46542
Benjy <benjy@foursquare.com>
2013-12-06 12:39:28 -0800

    Fix bug in read_write_artifact_cache.
    Auditors: pl
    
    (sapling split of e57fc7a7e9ce640620e5db1c1c34d04b3b2a60fd)

--- src/python/twitter/pants/cache/read_write_artifact_cache.py
+++ src/python/twitter/pants/cache/read_write_artifact_cache.py
@@ -9,10 +9,24 @@ class ReadWriteArtifactCache(ArtifactCache):
   """
   def __init__(self, read_artifact_cache, write_artifact_cache):
     """Either cache can be None, in which case we don't read from/write to it."""
-    artifact_root = read_artifact_cache.artifact_root
-    if write_artifact_cache.artifact_root != artifact_root:
-      raise ValueError('Read and write artifact caches must have the same artifact root.')
-    ArtifactCache.__init__(self, read_artifact_cache.log, artifact_root)
+    artifact_roots = []
+    logs = []
+    def get_root_and_log(cache):
+      if cache is not None:
+        artifact_roots.append(cache.artifact_root)
+        logs.append(cache.log)
+    get_root_and_log(read_artifact_cache)
+    get_root_and_log(write_artifact_cache)
+    if len(artifact_roots) == 0:
+      # Parent will never be accessed, so this is OK. In fact, it's a good way to ensure it.
+      artifact_root = None
+      log = None
+    else:
+      artifact_root = artifact_roots[0]
+      log = logs[0]
+      if len(artifact_roots) > 1 and artifact_roots[1] != artifact_root:
+        raise ValueError('Read and write artifact caches must have the same artifact root.')
+    ArtifactCache.__init__(self, log, artifact_root)
     self._read_artifact_cache = read_artifact_cache
     self._write_artifact_cache = write_artifact_cache
 
