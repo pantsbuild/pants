3a22d214aab5f34906b4578993e09d123d84efea
Ted Dziuba <tdziuba@ebay.com>
2014-01-02 20:17:10 -0800

    - Fixed a bug where the Pants build process would fail to install distribute binaries into the virtualenv. - Removed (presumably) internal twitter servers from the setup script.
    (sapling split of f30f08b2fe3873601585222b41f8096c8f383d09)

--- build-support/python/setup.sh
+++ build-support/python/setup.sh
@@ -5,6 +5,7 @@ BOOTSTRAP_BIN=$BASE_DIR/.python/bin
 BOOTSTRAP_ENVIRONMENT=$BASE_DIR/.python/bootstrap
 CACHE=$BASE_DIR/.pants.d/.pip.cache
 PY=$(which python)
+VIRTUALENV_VERSION=1.9.1
 
 mkdir -p $BOOTSTRAP_BIN
 mkdir -p $BOOTSTRAP_ENVIRONMENT
@@ -30,21 +31,17 @@ fi
 PYTHON=$BOOTSTRAP_BIN/bootstrap
 
 pushd $CACHE >& /dev/null
-  if ! test -f virtualenv-1.7.1.2.tar.gz; then
+  VIRTUALENV_TARBALL=virtualenv-$VIRTUALENV_VERSION.tar.gz
+  if ! test -f $VIRTUALENV_TARBALL; then
     echo 'Installing virtualenv' 1>&2
-    for url in \
-      https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.7.1.2.tar.gz \
-      https://svn.twitter.biz/science-binaries/home/third_party/python/virtualenv-1.7.1.2.tar.gz; do
-      if curl --connect-timeout 10 -O $url; then
-        break
-      fi
-    done
+    curl --connect-timeout 10 -O \
+        https://pypi.python.org/packages/source/v/virtualenv/$VIRTUALENV_TARBALL
   fi
-  gzip -cd virtualenv-1.7.1.2.tar.gz | tar -xf - >& /dev/null
+  gzip -cd $VIRTUALENV_TARBALL | tar -xf - >& /dev/null
 popd >& /dev/null
 
 function virtualenv() {
-  $PYTHON $CACHE/virtualenv-1.7.1.2/virtualenv.py "$@"
+  $PYTHON $CACHE/virtualenv-$VIRTUALENV_VERSION/virtualenv.py "$@"
 }
 
 if virtualenv -p $PY --distribute $BOOTSTRAP_ENVIRONMENT; then
@@ -55,7 +52,6 @@ if virtualenv -p $PY --distribute $BOOTSTRAP_ENVIRONMENT; then
   for pkg in distribute pystache; do
     pip install \
       --download-cache=$CACHE \
-      -f https://svn.twitter.biz/science-binaries/home/third_party/python \
       -f https://pypi.python.org/simple \
       -U --no-index $pkg
   done
