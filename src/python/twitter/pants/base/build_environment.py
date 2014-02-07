d51239a38433477ce297f5a3cf380c9ce80b5439
John Sirois <jsirois@twitter.com>
2014-02-07 12:12:51 -0700

    Sync missed get_scm bootstrapping logic.
    (sapling split of 1bddf57d17ff94d2c51008588c8f44121ff8ec59)

--- src/python/twitter/pants/base/build_environment.py
+++ src/python/twitter/pants/base/build_environment.py
@@ -16,7 +16,10 @@
 
 from __future__ import print_function
 
+import os
 import sys
+
+from twitter.common import log
 
 from twitter.pants.version import VERSION as _VERSION
 
@@ -52,6 +55,19 @@ _SCM = None
 
 def get_scm():
   """Returns the pants Scm if any."""
+  # TODO(John Sirois): Extract a module/class to carry the bootstrap logic.
+  global _SCM
+  if not _SCM:
+    # We know about git, so attempt an auto-configure
+    git_dir = os.path.join(get_buildroot(), '.git')
+    if os.path.isdir(git_dir):
+      from twitter.pants.scm.git import Git
+      git = Git(worktree=get_buildroot())
+      try:
+        log.info('Detected git repository on branch %s' % git.branch_name)
+        set_scm(git)
+      except git.LocalException:
+        pass
   return _SCM
 
 
