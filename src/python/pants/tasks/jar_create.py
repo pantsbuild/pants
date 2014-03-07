1d2566b0a89c9a7970d8131827170e7bb9ad82ba
Tejal Desai <tdesai@twitter.com>
2014-03-07 09:42:15 -0800

    Remove the Target classes extra logic
    Conflicts:
    	src/python/pants/tasks/jar_create.py

--- src/python/pants/tasks/jar_create.py
+++ src/python/pants/tasks/jar_create.py
@@ -15,10 +15,10 @@ from pants.base.build_environment import get_buildroot
 from pants.fs.fs import safe_filename
 from pants.java.jar import Manifest, open_jar
 from pants.targets.jvm_binary import JvmBinary
+from pants.targets.scala_library import ScalaLibrary
 from pants.tasks import Task, TaskError
 from pants.tasks.javadoc_gen import javadoc
 from pants.tasks.scaladoc_gen import scaladoc
-
 
 
 DEFAULT_CONFS = ['default']
