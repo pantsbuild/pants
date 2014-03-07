91d1b7288756e137dd9cbf3d924a7ebded01c76f
Tejal Desai <tdesai@twitter.com>
2014-03-07 09:22:39 -0800

    Review comment set 2
    Conflicts:
    	src/python/pants/targets/scala_library.py
    	tests/python/pants_test/tasks/test_jar_create.py

--- src/python/pants/tasks/jvm_compile/scala/scala_compile.py
+++ src/python/pants/tasks/jvm_compile/scala/scala_compile.py
@@ -37,12 +37,6 @@ class ScalaCompile(JvmCompile):
                                  color=color,
                                  jvm_tool_bootstrapper=self._jvm_tool_bootstrapper)
 
-    # If we are compiling scala libraries with circular deps on java libraries we need to
-    # make sure those cycle deps are present.
-    for scala_target in self.context.targets(lambda t: isinstance(t, ScalaLibrary)):
-      for java_target in scala_target.java_sources:
-        self.context.add_target(java_target)
-
   def create_analysis_tools(self):
     return AnalysisTools(self.context, ZincAnalysisParser(self._classes_dir), ZincAnalysis)
 
