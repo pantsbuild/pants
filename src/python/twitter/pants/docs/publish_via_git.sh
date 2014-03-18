32ed9d539b1da2102f48ebdd9fda379f75c752c9
Larry Hosken <lhosken@twitter.com>
2014-03-18 10:25:27 -0700

    comment: publishing the docs? use ssh url, it's way smoother
    (sapling split of b1bae13be2287f7fed80650cef324a140253a18e)

--- src/python/twitter/pants/docs/publish_via_git.sh
+++ src/python/twitter/pants/docs/publish_via_git.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Usage:
-#   sh publish_via_git.sh https://github.com/pantsbuild/pantsbuild.github.io.git
+#   sh publish_via_git.sh git@github.com:pantsbuild/pantsbuild.github.io.git
 #
 #   or, to publish to a subdir under there:
 #
-#   sh publish_via_git.sh https://github.com/pantsbuild/pantsbuild.github.io.git subdir
+#   sh publish_via_git.sh git@github.com:pantsbuild/pantsbuild.github.io.git subdir
 #
 # Assuming you've already generated web content in _build/html/ ,
 # "publish" that content to a git repo. This is meant to work with
