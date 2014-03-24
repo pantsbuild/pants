cf85b2c0bcb9e8148791498d7b7847bc4f7cbffc
John Sirois <jsirois@twitter.com>
2014-03-24 10:35:08 -0600

    Revert "Move code from command/__init__.py to a properly-named file."
    This reverts commit 6b024d7c9735f43eb80389be293183ac77a10e68.
    
    (sapling split of a5a05432b74567241326b6757db4c039e1cc8c29)

--- src/python/twitter/pants/commands/goal.py
+++ src/python/twitter/pants/commands/goal.py
@@ -45,7 +45,7 @@ from twitter.pants.base.rcfile import RcFile
 from twitter.pants.base.run_info import RunInfo
 from twitter.pants.base.target import Target, TargetDefinitionException
 from twitter.pants.base.workunit import WorkUnit
-from twitter.pants.commands.command import Command
+from twitter.pants.commands import Command
 from twitter.pants.engine import Engine, GroupEngine
 from twitter.pants.goal import Context, GoalError, Phase
 from twitter.pants.goal import Goal as goal, Group as group
