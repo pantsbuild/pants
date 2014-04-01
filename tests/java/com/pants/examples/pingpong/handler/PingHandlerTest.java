341cf2fcdfd62af8b94ce871b34c5af34a0f1ecc
Benjy <benjy@foursquare.com>
2014-04-01 17:32:46 +0000

    More BUILD file fixes for external twitter-commons java deps.

--- tests/java/com/pants/examples/pingpong/handler/PingHandlerTest.java
+++ tests/java/com/pants/examples/pingpong/handler/PingHandlerTest.java
@@ -1,26 +1,13 @@
-// =================================================================================================
-// Copyright 2013 Twitter, Inc.
-// -------------------------------------------------------------------------------------------------
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this work except in compliance with the License.
-// You may obtain a copy of the License in the LICENSE file, or at:
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// =================================================================================================
+// Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-package com.twitter.common.examples.pingpong.handler;
+package com.pants.examples.pingpong.handler;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import com.twitter.common.base.Closure;
-import com.twitter.common.testing.EasyMockTest;
+import com.twitter.common.testing.easymock.EasyMockTest;
 
 public class PingHandlerTest extends EasyMockTest {
 
