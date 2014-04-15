7aeea886593b5630ae9b63b442a413e37a89471a
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2014-04-16 00:09:54 +0200

    Upgrade to Scala 2.10.4.

--- project/ZincBuild.scala
+++ project/ZincBuild.scala
@@ -13,7 +13,7 @@ object ZincBuild extends Build {
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.typesafe.zinc",
     version := "0.3.1-SNAPSHOT",
-    scalaVersion := "2.10.3",
+    scalaVersion := "2.10.4",
     crossPaths := false
   )
 
