fc1de6002f4b8348002fc3ee59f6bbea2f2245e7
Larry Hosken <lhosken@twitter.com>
2014-04-18 10:10:37 -0700

    sample: Java Hello World += log4j

--- src/java/com/pants/examples/hello/main/HelloMain.java
+++ src/java/com/pants/examples/hello/main/HelloMain.java
@@ -3,11 +3,17 @@
 
 package com.pants.examples.hello.main;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import com.pants.examples.hello.greet.Greeting;
 
 public class HelloMain {
+  static Logger log = Logger.getLogger("hello.main");
+
   public static void main(String[] args) {
-    System.out.println(Greeting.greet("world"));
+    PropertyConfigurator.configure("log4j.properties");
+    log.info(Greeting.greet("world"));
   }
   private HelloMain() {
     // not called. placates checkstyle
