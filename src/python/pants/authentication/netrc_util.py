73a6190ec7fed9366e46fb1a1270b8941310e837
Tejal Desai <tdesai@twitter.com>
2014-04-30 21:10:32 -0700

    Issue 90: Add Add authentication files to pants bin target Add tests for netrc
    commit a0fce480bdc12343982e652fcd820f7f0c3a8141
    Author: Tejal Desai <tdesai@twitter.com>
    Date:   Wed Apr 30 20:27:35 2014 -0700
    
        Fix typo
    
    commit 1b9f27304b06d846935bb18c297238b24e318f2d
    Author: Tejal Desai <tdesai@twitter.com>
    Date:   Wed Apr 30 11:53:19 2014 -0700
    
        Style check
    
    commit af6fd235938ada3ba87eb96979945894ecda797d
    Author: Tejal Desai <tdesai@twitter.com>
    Date:   Wed Apr 30 11:51:53 2014 -0700
    
        Added style check
    
    commit d5e59f42a06bbdc21e6656bd84eece642e91494b
    Author: Tejal Desai <tdesai@twitter.com>
    Date:   Wed Apr 30 00:49:46 2014 -0700
    
        Added tests and code review comments
    
    commit 7616a0bd97b984d442e2acb697c6d50ac48e410e
    Author: Tejal Desai <tdesai@twitter.com>
    Date:   Tue Apr 29 13:40:08 2014 -0700
    
        Add authentication files to pants bin target

--- src/python/pants/authentication/netrc_util.py
+++ src/python/pants/authentication/netrc_util.py
@@ -8,10 +8,16 @@ import os
 
 from netrc import netrc as NetrcDb, NetrcParseError
 
-from pants.tasks.task_error import TaskError
-
 
 class Netrc(object):
+
+  class NetrcError(Exception):
+    """
+      Raised to indicate Netrc errors
+    """
+
+    def __init__(self, *args, **kwargs):
+      super(Netrc.NetrcError, self).__init__(*args, **kwargs)
 
   def __init__(self):
     self._login = collections.defaultdict(lambda: None)
@@ -29,7 +35,7 @@ class Netrc(object):
     if not self._login and not self._password:
       db = os.path.expanduser('~/.netrc')
       if not os.path.exists(db):
-        raise TaskError('A ~/.netrc file is required to authenticate')
+        raise self.NetrcError('A ~/.netrc file is required to authenticate')
       try:
         db = NetrcDb(db)
         for host, value in db.hosts.items():
@@ -39,6 +45,6 @@ class Netrc(object):
             self._login[host] = login
             self._password[host] = password
         if len(self._login) == 0:
-          raise TaskError('Found no usable authentication blocks in ~/.netrc')
+          raise self.NetrcError('Found no usable authentication blocks in ~/.netrc')
       except NetrcParseError as e:
-        raise TaskError('Problem parsing ~/.netrc: %s' % e)
+        raise self.NetrcError('Problem parsing ~/.netrc: %s' % e)
---
+++ tests/python/pants_test/authentication/test_netrc_util.py
@@ -0,0 +1,56 @@
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+from contextlib import contextmanager
+import re
+
+import pytest
+from mock import MagicMock, mock_open, patch
+
+from pants.authentication.netrc_util import Netrc
+
+
+@patch('os.path')
+class TestNetrcUtil(object):
+
+  class MockOsPath(MagicMock):
+    def __init__(self):
+      self.expanduser.return_value = '~/.netrc'
+      self.exists.return_value = True
+
+  def test_netrc_success(self, MockOsPath):
+    with patch('pants.authentication.netrc_util.NetrcDb') as mock_netrc:
+      instance = mock_netrc.return_value
+      instance.hosts = {'host': ('user', 'user', 'passw0rd')}
+      instance.authenticators.return_value = ('user', 'user', 'passw0rd')
+      netrc = Netrc()
+      netrc._ensure_loaded()
+
+  def test_netrc_file_missing_error(self, MockOsPath):
+    MockOsPath.exists.return_value = False
+    netrc = Netrc()
+    with pytest.raises(netrc.NetrcError) as exc:
+      netrc._ensure_loaded()
+    assert exc.value.message == 'A ~/.netrc file is required to authenticate'
+
+  def test_netrc_parse_error(self, MockOsPath):
+    with self.netrc('machine test') as netrc:
+      with pytest.raises(netrc.NetrcError) as exc:
+        netrc._ensure_loaded()
+      assert re.search(r'Problem parsing', exc.value.message)
+
+  def test_netrc_no_usable_blocks(self, MockOsPath):
+    with self.netrc('') as netrc:
+      with pytest.raises(netrc.NetrcError) as exc:
+        netrc._ensure_loaded()
+      assert exc.value.message == 'Found no usable authentication blocks in ~/.netrc'
+
+  @contextmanager
+  def netrc(self, netrc_contents):
+    m = mock_open(read_data=netrc_contents)
+    with patch('__builtin__.open', m):
+      netrc = Netrc()
+      yield netrc
