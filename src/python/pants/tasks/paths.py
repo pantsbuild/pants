d38bb1d8d105511b88c22602f8922ef40f5b46d0
Patrick Lawson <pl@foursquare.com>
2014-05-19 09:40:56 -0400

    Fix path/paths goals for refactor
    Testing Done:
    CI passes.
    
    Reviewed at https://rbcommons.com/s/twitter/r/371/

--- src/python/pants/tasks/paths.py
+++ src/python/pants/tasks/paths.py
@@ -23,32 +23,7 @@ class PathFinder(ConsoleTask):
     self.target_roots = context.target_roots
 
   @classmethod
-  def _coerce_to_targets(cls, from_str, to_str):
-    if isinstance(from_str, Compatibility.string):
-      if not isinstance(to_str, Compatibility.string):
-        raise TaskError('Finding paths from string %s to non-string %s' % (from_str, str(to_str)))
-
-      from_address = Address.parse(get_buildroot(), from_str)
-      to_address = Address.parse(get_buildroot(), to_str)
-
-      from_target = Target.get(from_address)
-      to_target = Target.get(to_address)
-
-      if not from_target:
-        raise TaskError('Target %s doesn\'t exist' % from_address.reference())
-      if not to_target:
-        raise TaskError('Target %s doesn\'t exist' % to_address.reference())
-
-      return from_target, to_target
-
-    elif isinstance(to_str, Compatibility.string):
-      raise TaskError('Finding paths from string %s to non-string %s' % (to_str, str(from_str)))
-    return from_str, to_str
-
-  @classmethod
   def _find_paths(cls, from_target, to_target, log):
-    from_target, to_target = cls._coerce_to_targets(from_target, to_target)
-
     log.debug('Looking for all paths from %s to %s' % (from_target.address.reference(), to_target.address.reference()))
 
     paths = cls._find_paths_rec(from_target, to_target)
@@ -65,13 +40,11 @@ class PathFinder(ConsoleTask):
 
     if from_target not in cls.all_paths or to_target not in cls.all_paths[from_target]:
       paths = []
-      if hasattr(from_target, 'dependency_addresses'):
-        for address in from_target.dependency_addresses:
-          dep = Target.get(address)
-          for path in cls._find_paths_rec(dep, to_target):
-            new_path = copy.copy(path)
-            new_path.insert(0, from_target)
-            paths.append(new_path)
+      for dep in from_target.dependencies:
+        for path in cls._find_paths_rec(dep, to_target):
+          new_path = copy.copy(path)
+          new_path.insert(0, from_target)
+          paths.append(new_path)
 
       cls.all_paths[from_target][to_target] = paths
 
@@ -81,14 +54,14 @@ class PathFinder(ConsoleTask):
 
   @classmethod
   def _find_path(cls, from_target, to_target, log):
-    from_target, to_target = cls._coerce_to_targets(from_target, to_target)
-
-    log.debug('Looking for path from %s to %s' % (from_target.address.reference(), to_target.address.reference()))
+    log.debug('Looking for path from %s to %s' % (from_target.address.reference(),
+                                                  to_target.address.reference()))
 
     queue = [([from_target], 0)]
     while True:
       if not queue:
-        print('no path found from %s to %s!' % (from_target.address.reference(), to_target.address.reference()))
+        print('no path found from %s to %s!' % (from_target.address.reference(),
+                                                to_target.address.reference()))
         break
 
       path, indent = queue.pop(0)
@@ -105,10 +78,8 @@ class PathFinder(ConsoleTask):
           print('%s' % target.address.reference())
         break
 
-      if hasattr(next_target, 'dependency_addresses'):
-        for address in next_target.dependency_addresses:
-          dep = Target.get(address)
-          queue.append((path + [dep], indent + 1))
+      for dep in next_target.dependencies:
+        queue.append((path + [dep], indent + 1))
 
 
 class Path(PathFinder):
