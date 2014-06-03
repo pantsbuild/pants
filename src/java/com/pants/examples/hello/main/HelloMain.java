d665eafc5c771ae82758b0bb205361892fc40547
larry hosken (Twitter) <mister@lahosken.san-francisco.ca.us>
2014-06-03 11:47:31 -0700

    sample: java hello world. use resource. change bundled-file example.
    We want to demonstrate resources. We want to demonstrate file-bundles. The 'hello world' example was using a bundled file for something you'd more-properly use a resource for. So don't do that.
    
    Instead, load two "greetees". Load one from a bundled file, one from resource.
    
    Testing Done:
    $ ./build-support/bin/ci.sh
    
    $ ./pants goal bundle src/java/com/pants/examples/hello/main
    $ cd dist/main-bundle/
    $ java -jar main-bin.jar
    
    $ ./pants goal run src/scala/com/pants/example/hello/exe
    
    Bugs closed: https://github.com/pantsbuild/pants/issues/110
    
    Reviewed at https://rbcommons.com/s/twitter/r/350/

--- src/java/com/pants/examples/hello/main/HelloMain.java
+++ src/java/com/pants/examples/hello/main/HelloMain.java
@@ -3,17 +3,44 @@
 
 package com.pants.examples.hello.main;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
 
 import com.pants.examples.hello.greet.Greeting;
 
 public class HelloMain {
-  static Logger log = Logger.getLogger("hello.main");
 
-  public static void main(String[] args) {
-    PropertyConfigurator.configure("log4j.properties");
-    log.info(Greeting.greet("world"));
+  public static void main(String[] args) throws FileNotFoundException {
+    // Target of greeting is config'd in greetee.txt file, so read that:
+    String greetee = "default world";
+    Scanner scanner = null;
+    try {
+	scanner = new Scanner(new File("greetee.txt")).useDelimiter("\\Z");
+	greetee = scanner.next();
+    } finally {
+	if (scanner != null) {
+	    scanner.close();
+	}
+    }
+
+    System.out.println(Greeting.greet(greetee));
+
+    // Target of other greeting is config'd in resource, so read that:
+    greetee = "default world";
+    try {
+	InputStream is = HelloMain.class.getClassLoader()
+	    .getResourceAsStream("com/pants/example/hello/world.txt");
+	scanner = new Scanner(is).useDelimiter("\\Z");
+	greetee = scanner.next();
+    } finally {
+	if (scanner != null) {
+	    scanner.close();
+	}
+    }
+
+    System.out.println(Greeting.greet(greetee));
   }
   private HelloMain() {
     // not called. placates checkstyle
