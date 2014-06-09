68b5d43928fee87d857089e29b987ccdeb71bed2
Tejal Desai <tejal29@gmail.com>
2014-06-09 14:21:29 -0700

    Use temporary directory for publish local
    Testing Done:
    Locally.
    
    CI.
    https://travis-ci.org/pantsbuild/pants/builds/27161708
    
    Bugs closed: 195
    
    Reviewed at https://rbcommons.com/s/twitter/r/469/

--- tests/python/pants_test/tasks/test_jar_publish.py
+++ tests/python/pants_test/tasks/test_jar_publish.py
@@ -7,6 +7,7 @@ from __future__ import (nested_scopes, generators, division, absolute_import, wi
 from mock import Mock
 import pytest
 
+from twitter.common.contextutil import temporary_dir
 from pants.backend.jvm.tasks.jar_publish import JarPublish
 from pants.base.exceptions import TaskError
 
@@ -17,12 +18,13 @@ from pants_test.tasks.test_base import prepare_task
 class JarPublishTest(BaseTest):
 
   def test_smoke_publish(self):
-    task = prepare_task(JarPublish,
-                        args=['--test-local=/tmp'],
+    with temporary_dir() as publish_dir:
+      task = prepare_task(JarPublish,
+                        args=['--test-local=%s' % publish_dir],
                         build_graph=self.build_graph,
                         build_file_parser=self.build_file_parser)
-    task.scm = Mock()
-    task.execute()
+      task.scm = Mock()
+      task.execute()
 
   def test_publish_local_only(self):
     with pytest.raises(TaskError) as exc:
