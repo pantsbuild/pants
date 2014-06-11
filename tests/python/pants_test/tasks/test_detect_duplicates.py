ff446224fd6185c055712ea73f12471a8af4092a
Eric Ayers <zundel@squareup.com>
2014-06-11 15:50:49 -0400

    Regression in handling an external dep containing a unicode character classname
      (Accidentally left out changed from feedback in last merge)

--- tests/python/pants_test/tasks/test_detect_duplicates.py
+++ tests/python/pants_test/tasks/test_detect_duplicates.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-#  Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
