bfc1e683499e16b3d229567f4063305a8d7f890f
Eric Ayers <zundel@squareup.com>
2014-06-12 07:39:07 -0400

    Removing .pants.d during clean.sh makes it easier for me to get past the Untranslatable... exception

--- build-support/python/clean.sh
+++ build-support/python/clean.sh
@@ -5,4 +5,5 @@ rm -rf ${HOME}/.pex
 rm -rf ${PANTS_BASE}/build-support/pants_deps.venv
 rm -rf ${PANTS_BASE}/build-support/pants_dev_deps.venv
 rm -f  ${PANTS_BASE}/pants.pex
+rm -rf ${PANTS_BASE}/.pants.d
 find ${PANTS_BASE} -name '*.pyc' | xargs rm -f
