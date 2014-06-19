dccd515fb684421f4122df6ff425bfd07359606f
larry hosken (Twitter) <mister@lahosken.san-francisco.ca.us>
2014-06-19 10:19:20 -0700

    reenable this integration test. don't assume goal test retcode is 0 on success
    I previously commented out this test, which was breaking in Travis-CI.
    
    It assumed the exit code was 0 on success.
    Empirical evidence suggests that's not always true for 'goal test'
    in Travis-CI within a PantsRunIntegrationTest.
    (Yeah, that's weird: https://github.com/pantsbuild/pants/issues/254)
    So invoke './pants goal compile' instead; that still tests the scala-java stuff.
    
    While I was in there, printed more info in the assert.
    
    Testing Done:
    on my laptop: ./pants goal test tests/python/pants_test/targets:integration
    on Travis-CI: https://travis-ci.org/pantsbuild/pants/builds/27914227
    
    Bugs closed: 250
    
    Reviewed at https://rbcommons.com/s/twitter/r/569/

--- tests/python/pants_test/targets/test_scala_library_integration.py
+++ tests/python/pants_test/targets/test_scala_library_integration.py
@@ -10,5 +10,10 @@ from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 class TestScalaLibraryIntegrationTest(PantsRunIntegrationTest):
 
   def test_bundle(self):
-        pants_run = self.run_pants(['goal', 'test', 'src/scala/com/pants/testproject/javasources/BUILD:javasources'])
-        self.assertEquals(pants_run.returncode, self.PANTS_SUCCESS_CODE)
+        pants_run = self.run_pants(['goal', 'compile', 'src/scala/com/pants/testproject/javasources/BUILD:javasources'])
+        self.assertEquals(pants_run.returncode, self.PANTS_SUCCESS_CODE,
+                          "goal compile expected success, got {0}\n"
+                          "got stderr:\n{1}\n"
+                          "got stdout:\n{2}\n".format(pants_run.returncode,
+                                                      pants_run.stderr_data,
+                                                      pants_run.stdout_data))
