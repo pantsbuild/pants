6327d946a47aa4cd8f57da2a156f2f77e4edb58f
Mateor <mateorod9@gmail.com>
2014-06-28 19:55:12 -0500

    Update docsstrings of android_target and android_binary
    Fixing mistake found by @lahosken and adding the same changes
    to the android_target docstring as well.

--- src/python/pants/backend/android/targets/android_binary.py
+++ src/python/pants/backend/android/targets/android_binary.py
@@ -21,18 +21,18 @@ class AndroidBinary(AndroidTarget):
                excludes=None,
                **kwargs):
     """
-   :param string name: The name of this target, which combined with this
+    :param string name: The name of this target, which combined with this
       build file defines the :doc:`target address <target_addresses>`.
-    :param sources: A list of filenames representing the source code.
-    :type sources: ``Fileset`` or list of strings. Paths are relative to the
+    :param sources: Source code files to compile. Paths are relative to the
       BUILD file's directory.
+    :type sources: ``Fileset`` or list of strings.
     :param excludes: List of :ref:`exclude <bdict_exclude>`\s
       to filter this target's transitive dependencies against.
     :param package: Package name of app, e.g. 'com.pants.examples.hello'
     :type package: string
     :param resources: name of directory containing the android resources. Set as 'res' by default.
     :param build_tools_version: API for the Build Tools (separate from SDK version).
-      Default to latest available
+      Defaults to the latest full release.
     :param target_sdk_version: Version of the Android SDK the android target is built for
     :param min_sdk_version:  Earliest supported SDK by the android target
     :param release_type: Which keystore is used to sign target: 'debug' or 'release'.
