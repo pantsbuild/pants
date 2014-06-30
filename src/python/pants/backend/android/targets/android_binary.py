973d8a2501f0bbba2003ff440a9eab1d41c63cf0
Mateo Rodriguez <mateorod9@gmail.com>
2014-06-30 09:10:07 -0700

    Update docstring of AndroidBinary target
    tested at  https://travis-ci.org/pantsbuild/pants/builds/28688902
    
    reviewed at https://rbcommons.com/s/twitter/r/610/

--- src/python/pants/backend/android/targets/android_binary.py
+++ src/python/pants/backend/android/targets/android_binary.py
@@ -5,9 +5,11 @@
 from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
                         print_function, unicode_literals)
 
+from pants.base.build_manual import manual
 from pants.backend.android.targets.android_target import AndroidTarget
 
 
+@manual.builddict(tags=['android'])
 class AndroidBinary(AndroidTarget):
   """Produces an Android binary."""
 
@@ -19,22 +21,23 @@ class AndroidBinary(AndroidTarget):
                excludes=None,
                **kwargs):
     """
-   :param string name: The name of this target, which combined with this
-      build file defines the target :class:`pants.base.address.Address`.
-    :param sources: A list of filenames representing the source code.
-    :type sources: list of strings
-    :param excludes: One or more :class:`pants.targets.exclude.Exclude` instances
+    :param string name: The name of this target, which combined with this
+      build file defines the :doc:`target address <target_addresses>`.
+    :param sources: Source code files to compile. Paths are relative to the
+      BUILD file's directory.
+    :type sources: ``Fileset`` or list of strings.
+    :param excludes: List of :ref:`exclude <bdict_exclude>`\s
       to filter this target's transitive dependencies against.
     :param package: Package name of app, e.g. 'com.pants.examples.hello'
     :type package: string
     :param resources: name of directory containing the android resources. Set as 'res' by default.
     :param build_tools_version: API for the Build Tools (separate from SDK version).
-      Default to latest available
+      Defaults to the latest full release.
     :param target_sdk_version: Version of the Android SDK the android target is built for
     :param min_sdk_version:  Earliest supported SDK by the android target
     :param release_type: Which keystore is used to sign target: 'debug' or 'release'.
       Set as 'debug' by default.
     """
 
-    # TODO: Add some Compatibility error checks.
+    # TODO (mateor): Add some Compatibility error checks.
     super(AndroidBinary, self).__init__(name=name, sources=sources, **kwargs)
