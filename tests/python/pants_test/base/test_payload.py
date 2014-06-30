fc8fb80d3d250db0bfcb5a750f3095e2e09a508b
John Sirois <jsirois@twitter.com>
2014-06-30 16:26:02 -0600

    Rebase against master and port code to new api.

--- tests/python/pants_test/base/test_payload.py
+++ tests/python/pants_test/base/test_payload.py
@@ -9,20 +9,21 @@ import pytest
 
 from pants.backend.core.wrapped_globs import Globs
 from pants.backend.jvm.targets.java_library import JavaLibrary
+from pants.base.build_file_aliases import BuildFileAliases
 from pants_test.base_test import BaseTest
 
 
 class PayloadTest(BaseTest):
   @property
   def alias_groups(self):
-    return {
-      'target_aliases': {
+    return BuildFileAliases.create(
+      targets={
         'java_library': JavaLibrary,
       },
-      'applicative_path_relative_utils': {
+      macros={
         'globs': Globs,
       },
-    }
+    )
 
   def setUp(self):
     super(PayloadTest, self).setUp()
