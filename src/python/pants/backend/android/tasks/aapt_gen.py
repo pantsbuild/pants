98747f0cbb203d66866b06bd45fe6b44aecbe88a
Mateor <mateorod9@gmail.com>
2014-07-13 00:41:03 -0500

    Update docsstrings and remove debug info from aapt_gen
    Considering renaming this class as well.

--- src/python/pants/backend/android/tasks/aapt_gen.py
+++ src/python/pants/backend/android/tasks/aapt_gen.py
@@ -21,11 +21,10 @@ from pants.base.exceptions import TaskError
 
 class AaptGen(AndroidTask, CodeGen):
   """
-  CodeGen for Android app building with the Android Asset Packaging Tool.
-  There may be an aapt superclass or mixin, as aapt binary has future packaging functions
-  besides codegen.
+  Handles the processing of resources for Android targets with the
+  Android Asset Packaging Tool (aapt).
 
-  aapt supports 6 major commands: {dump, list, add, remove, crunch, package}
+  The aapt tool supports 6 major commands: [dump, list, add, remove, crunch, package]
   For right now, pants is only supporting 'package'. More to come as we support Release builds
   (crunch, at minimum).
 
@@ -37,23 +36,22 @@ class AaptGen(AndroidTask, CodeGen):
     super(AaptGen, cls).setup_parser(option_group, args, mkflag)
 
     option_group.add_option(mkflag("target-sdk"), dest="target_sdk",
-                            help="[%default] Specifies the target Android SDK. "
-                                 "Overrides AndroidManifest.xml.")
+                            help="[%default] Specifies the target Android SDK used to compile "
+                                 "resources. Overrides AndroidManifest.xml.")
 
     option_group.add_option(mkflag("build-tools-version"), dest="build_tools_version",
-                            help="[%default] Specifies the Android build-tools version with which "
-                                 "to compile the target.")
+                            help="[%default] Specifies the Android build-tools version used "
+                                 "to compile resources.")
 
   def __init__(self, context, workdir):
-    #define the params needed in the BUILD file {name, sources, dependencies, etc.}
     super(AaptGen, self).__init__(context, workdir)
     lang = 'java'
     self.gen_langs=set()
     self.gen_langs.add(lang)
+    # self._dist is an AndroidDistribution inherited from AndroidTask
     self.dist = self._dist
     self.forced_target_sdk = context.options.target_sdk
     self.forced_build_tools_version = context.options.build_tools_version
-
 
   def is_gentarget(self, target):
     return isinstance(target, AndroidResources)
@@ -65,21 +63,13 @@ class AaptGen(AndroidTask, CodeGen):
     return lang in self.gen_langs
 
   def genlang(self, lang, targets):
-    """aapt must override and generate code in :lang for the given targets.
-
-    May return a list of pairs (target, files) where files is a list of files
-    to be cached against the target.
-    """
-    print ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-    # somewhere in an aapt class we will need to handle "crunch" command for release builds.
     for target in targets:
       if lang != 'java':
         raise TaskError('Unrecognized android gen lang: %s' % lang)
       output_dir = self._aapt_out(target)
       safe_mkdir(output_dir)
-      args = []
 
+      args = []
       if self.forced_build_tools_version:
         args.append(self.aapt_tool(self.forced_build_tools_version))
       else:
@@ -87,75 +77,47 @@ class AaptGen(AndroidTask, CodeGen):
 
       args.extend(['package', '-m', '-J', output_dir, '-M', target.manifest,
                '-S', target.resource_dir, '-I'])
-      print ("self.forced_target_sdk is %s" % self.forced_target_sdk)
       if self.forced_target_sdk:
         args.append(self.android_jar_tool(self.forced_target_sdk))
       else:
         args.append(self.android_jar_tool(target.target_sdk))
-      print ("output_dir is %s" % output_dir)
-      #manifest = os.path.join(target.manifest)
+
       # BUILD files in the resource folder chokes aapt. This is a defensive measure.
-      ignored_assets='!.svn:!.git:!.ds_store:!*.scc:.*:<dir>_*:!CVS:!thumbs.db:!picasa.ini:!*~:BUILD*'
+      ignored_assets='!.svn:!.git:!.ds_store:!*.scc:.*:<dir>_*:!CVS:' \
+                     '!thumbs.db:!picasa.ini:!*~:BUILD*'
       args.extend(['--ignore-assets', ignored_assets])
-      print ("args are %s" % args)
       log.debug('Executing: %s' % ' '.join(args))
       process = subprocess.Popen(args)
       result = process.wait()
-      # TODO(mateor) refine/check this error catch
       if result != 0:
-        raise TaskError('Android %s ... exited non-zero (%i)' % (self.aapt_tool(target), result))
-
+        raise TaskError('Android aapt exited non-zero ({code})'.format(code=result))
 
   def createtarget(self, lang, gentarget, dependees):
-    """from: CodeGen: aapt class must override and create a synthetic target.
-    The target must contain the sources generated for the given gentarget.
-    """
-    print ("aapt createtarget")
-    #This method creates the new target to replace the acted upon resources in the target graph
-    # create the path and sources
     aapt_gen_file = os.path.join(self._aapt_out(gentarget), self.package_path(gentarget.package))
-    print ("AAPT_GEN_FILE: %s" % aapt_gen_file)
-    #Use the address to create a syntheticTarget address
     address = SyntheticAddress(spec_path=aapt_gen_file, target_name = gentarget.id)
-    # create new JavaLibraryTarget
-    print (gentarget.id, gentarget)
     tgt = self.context.add_new_target(address,
                                       JavaLibrary,
                                       derived_from=gentarget,
-                                      #TODO:are sources full path? Address seem to be
                                       sources=['R.java'],
-                                      #provides=gentarget.provides,
                                       dependencies=[])
-                                      # excludes=gentarget.excludes)
-    # update (or inject?) deps
-    #print (dependees)
-    print ("TARGET_ADDDRESS: %s" % tgt.address)
+
     for dependee in dependees:
       dependee.inject_dependency(tgt.address)
     return tgt
 
   def package_path(self, package):
-    #TODO test this
-    #needs to convert the package name (com.foo.bar) into a package path (com/foo/bar)
     return package.replace('.', os.sep)
-
 
   def _aapt_out(self, target):
     return os.path.join(target.address.spec_path, 'bin')
 
-  # resolve the tools on a per-target basis
   def aapt_tool(self, build_tools_version):
-    """This fetches the appropriate aapt tool.
-    The build_tools_version argument is a string(e.g. "19.1.")."""
+    """Fetches the appropriate aapt tool.The build_tools_version argument
+    is a string (e.g. "19.1.")."""
     aapt = self.dist.aapt_tool(build_tools_version)
-    print ("aapt_tool: %s" % aapt)
     return aapt
 
   def android_jar_tool(self, target_sdk):
-    """This fetches the appropriate android.jar. The target_sdk argument is a string (e.g. "18")."""
+    """Fetches the appropriate android.jar. The target_sdk argument is a string (e.g. "18")."""
     android_jar = self.dist.android_jar_tool(target_sdk)
-    print ("android_jar_tool: %s" % android_jar)
     return android_jar
-  #todo (mateor): debate merits of AaptClassMixin class
-
-  # .aar library files output R.txt with a flag of '--output-text-symbols
