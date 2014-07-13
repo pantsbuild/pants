b85543221e49cc76e1f351e2ca9091dd95b6a09a
John Sirois <john.sirois@gmail.com>
2014-07-13 10:27:59 -0700

    Improve backend plugin ergonomics.
    This adds support for omitting a backend entrypoint where
    no backend plugin is provided and also improves import
    errors by wrapping and giving the full module path that was
    not found.
    
    Test coverage is also added.
    
    Testing Done:
    $ PANTS_DEV=1 ./pants goal test tests/python/pants_test/base:all
    
    Bugs closed: 666
    
    Reviewed at https://rbcommons.com/s/twitter/r/666/

--- src/python/pants/base/exceptions.py
+++ src/python/pants/base/exceptions.py
@@ -10,12 +10,10 @@ from pants.base.build_manual import manual
 
 @manual.builddict()
 class TaskError(Exception):
-  """
-    Raised to indicate a task has failed.
+  """Indicates a task has failed."""
 
-    :param int exit_code: an optional exit code (1, by default)
-  """
   def __init__(self, *args, **kwargs):
+    """:param int exit_code: an optional exit code (1, by default)"""
     self._exit_code = kwargs.pop('exit_code', 1)
     super(TaskError, self).__init__(*args, **kwargs)
 
@@ -26,7 +24,21 @@ class TaskError(Exception):
 
 @manual.builddict()
 class TargetDefinitionException(Exception):
-  """Thrown on errors in target definitions."""
+  """Indicates an invalid target definition."""
 
   def __init__(self, target, msg):
-    super(Exception, self).__init__('Error with %s: %s' % (target, msg))
+    """
+    :param target: the target in question
+    :param string msg: a description of the target misconfiguration
+    """
+    super(Exception, self).__init__('Invalid target %s: %s' % (target, msg))
+
+
+@manual.builddict()
+class BuildConfigurationError(Exception):
+  """Indicates an error in a pants installation's configuration."""
+
+
+@manual.builddict()
+class BackendConfigurationError(BuildConfigurationError):
+  """Indicates a plugin backend with a missing or malformed register module."""
