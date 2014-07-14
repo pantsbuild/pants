888285efd6ef3f315461a4e098df29f55bb6d812
Mateor <mateorod9@gmail.com>
2014-07-14 16:21:26 -0500

    Finalize AndroidDistribution refactor
    All __init__ action has been redistributed to too retrieval.
    Now paths with no SDK can be set as Distributions and won't be
    revealed for the baseless traitor it is until we ask for a tool.
    
    This keeps pants from instantiating objects that request a valid
    distribution during bootstrap. If there is no SDK in the user's
    path they won't get the exceptions until they try and execute an
    Android task.
    
    Conflicts:
    	src/python/pants/backend/android/distribution/android_distribution.py

--- src/python/pants/backend/android/distribution/android_distribution.py
+++ src/python/pants/backend/android/distribution/android_distribution.py
@@ -32,12 +32,12 @@ class AndroidDistribution(object):
     key = (path)
     dist = cls._CACHED_SDK.get(key)
     if not dist:
-      dist = cls.locate(path)
+      dist = cls.set_sdk_path(path)
     cls._CACHED_SDK[key] = dist
     return dist
 
   @classmethod
-  def locate(cls, path):
+  def set_sdk_path(cls, path):
     def sdk_path(sdk_env_var):
       sdk = os.environ.get(sdk_env_var)
       return os.path.abspath(sdk) if sdk else None
@@ -75,14 +75,14 @@ class AndroidDistribution(object):
 
   def _validate_executable(self, tool):
     if not self._is_executable(tool):
-      raise self.Error('Failed to locate the %s executable. It does not appear to be an'
-                       ' installed portion of this %s' % (tool, 'Android SDK'))
+      raise self.Error('There is no {0!r} installed. The Android SDK may need to be updated'
+                       .format(tool))
     return tool
 
   def _validate_file(self, tool):
     if not os.path.isfile(tool):
-      raise self.Error('Failed to locate the %s file. It does not appear to be an'
-                       ' installed portion of this %s' % (tool, 'Android SDK'))
+      raise self.Error('There is no {0!r} installed.The Android SDK may need to be updated'
+                       .format(tool))
     return tool
 
   @staticmethod
