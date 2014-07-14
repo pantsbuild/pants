ad52e9055246fc111735f5aba56d589b459a0aa6
Mateor <mateorod9@gmail.com>
2014-07-14 15:26:38 -0500

    Allow passing of custom SDK location at CLI task invocation
    The AndroidDistribution class now caches by path, which will
    almost always be None, unless the user specifies on CLI.
    
    Conflicts:
    	src/python/pants/backend/android/distribution/android_distribution.py

--- src/python/pants/backend/android/distribution/android_distribution.py
+++ src/python/pants/backend/android/distribution/android_distribution.py
@@ -12,16 +12,13 @@ from twitter.common import log
 
 class AndroidDistribution(object):
   """
-  This class looks for Android SDks installed on the machine's path. It then verifies that that
-  SDK has the needed build-tools and API levels installed. There is caching of most lookups.
+  This class looks for Android SDks installed on the machine's path. The SDK path will not
+  be verified until an Android task requests a tool.
   """
 
-  # As of now, missing API or build-tools only raises an exception. The SDK could be updated from
+  # As of now, missing tools simply raises an exception. The SDK could be updated from
   # within this class, it just depends what state we want to leave the host machine afterwards.
   # There is probably a discussion to be had about bootstrapping as well.
-
-  # Good portions of this are inspired by pants.java.distribution. The two distributions could
-  # perhaps benefit from a refactor that makes some of the validation process common.
 
   class Error(Exception):
     """Indicates an invalid android distribution."""
@@ -29,39 +26,38 @@ class AndroidDistribution(object):
   _CACHED_SDK = {}
 
   @classmethod
-  def cached(cls, target_sdk=None, build_tools_version=None):
+  def cached(cls, path=None):
     # Tuple just used for quick lookup. This method will generally be passed no params, and will
-    # always cache the key(None, None). But it stops us from crawling the user path each invocation.
-    key = (target_sdk, build_tools_version)
+    # always cache the key(None). But a user can pass a specific sdk on the CLI through AndroidTask.
+    key = (path)
     dist = cls._CACHED_SDK.get(key)
     if not dist:
-      dist = cls.locate()
+      dist = cls.locate(path)
     cls._CACHED_SDK[key] = dist
     return dist
 
-
   @classmethod
-  def locate(cls, target_sdk=None, build_tools_version=None):
+  def locate(cls, path):
     def sdk_path(sdk_env_var):
       sdk = os.environ.get(sdk_env_var)
       return os.path.abspath(sdk) if sdk else None
 
-    def search_path():
+    def search_path(path):
+      # Check path if one is passed at instantiation, then check environmental variables
+      if path:
+        yield os.path.abspath(path)
       yield sdk_path('ANDROID_HOME')
       yield sdk_path('ANDROID_SDK_HOME')
       yield sdk_path('ANDROID_SDK')
 
-    for path in filter(None, search_path()):
+    for path in filter(None, search_path(path)):
       dist = cls(path)
       return dist
     dist = cls(None)
     return dist
 
   def __init__(self, sdk_path=None, target_sdk=None, build_tools_version=None):
-    """Creates an Android distribution wrapping the given sdk_path."""
-
-    if not os.path.isdir(sdk_path):
-      raise ValueError('The specified android sdk path is invalid: %s' % sdk_path)
+    """Creates an Android distribution and caches tools for quick retrieval."""
     self._sdk_path = sdk_path
     self._validated_tools = set()
 
