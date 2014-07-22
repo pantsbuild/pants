pantsbuild/pants

commit c7ec3120c725902d77732fb613c9860fc45833e7

Add fork java option

src/main/scala/com/typesafe/zinc/Setup.scala
@@ -19,6 +19,7 @@ case class Setup(
   sbtInterface: File,
   compilerInterfaceSrc: File,
   javaHome: Option[File],
+  forkJava: Boolean,
   cacheDir: File)
 
 /**
@@ -60,7 +61,7 @@ object Setup {
   def apply(settings: Settings): Setup = {
     val scalaJars = selectScalaJars(settings.scala)
     val (sbtInterface, compilerInterfaceSrc) = selectSbtJars(settings.sbt)
-    setup(scalaJars.compiler, scalaJars.library, scalaJars.extra, sbtInterface, compilerInterfaceSrc, settings.javaHome)
+    setup(scalaJars.compiler, scalaJars.library, scalaJars.extra, sbtInterface, compilerInterfaceSrc, settings.javaHome, settings.forkJava)
   }
 
   /**
@@ -72,7 +73,8 @@ object Setup {
     scalaExtra: Seq[File],
     sbtInterface: File,
     compilerInterfaceSrc: File,
-    javaHomeDir: Option[File]): Setup =
+    javaHomeDir: Option[File],
+    forkJava: Boolean): Setup =
   {
     val normalise: File => File = { _.getCanonicalFile }
     val compilerJar          = normalise(scalaCompiler)
@@ -82,7 +84,7 @@ object Setup {
     val compilerInterfaceJar = normalise(compilerInterfaceSrc)
     val javaHome             = javaHomeDir map normalise
     val cacheDir             = zincCacheDir
-    Setup(compilerJar, libraryJar, extraJars, sbtInterfaceJar, compilerInterfaceJar, javaHome, cacheDir)
+    Setup(compilerJar, libraryJar, extraJars, sbtInterfaceJar, compilerInterfaceJar, javaHome, forkJava, cacheDir)
   }
 
   /**
@@ -94,14 +96,16 @@ object Setup {
     scalaExtra: JList[File],
     sbtInterface: File,
     compilerInterfaceSrc: File,
-    javaHome: File): Setup =
+    javaHome: File,
+    forkJava: Boolean): Setup =
   setup(
     scalaCompiler,
     scalaLibrary,
     scalaExtra.asScala,
     sbtInterface,
     compilerInterfaceSrc,
-    Option(javaHome)
+    Option(javaHome),
+    forkJava
   )
 
   /**
@@ -110,7 +114,8 @@ object Setup {
   def create(
     scalaLocation: ScalaLocation,
     sbtJars: SbtJars,
-    javaHome: File): Setup =
+    javaHome: File,
+    forkJava: Boolean): Setup =
   {
     val scalaJars = selectScalaJars(scalaLocation)
     val (sbtInterface, compilerInterfaceSrc) = selectSbtJars(sbtJars)
@@ -120,7 +125,8 @@ object Setup {
       scalaJars.extra,
       sbtInterface,
       compilerInterfaceSrc,
-      Option(javaHome)
+      Option(javaHome),
+      forkJava
     )
   }
 
@@ -284,6 +290,7 @@ object Setup {
       "sbt interface"              -> sbtInterface,
       "compiler interface sources" -> compilerInterfaceSrc,
       "java home"                  -> javaHome,
+      "fork java"                  -> forkJava,
       "cache directory"            -> cacheDir)
     Util.show(("Setup", values), output)
   }

src/main/scala/com/typesafe/zinc/Compiler.scala
@@ -50,9 +50,27 @@ object Compiler {
   def create(setup: Setup, log: Logger): Compiler = {
     val instance     = scalaInstance(setup)
     val interfaceJar = compilerInterface(setup, instance, log)
-    val scalac       = IC.newScalaCompiler(instance, interfaceJar, ClasspathOptions.boot, log)
-    val javac        = AggressiveCompile.directOrFork(instance, ClasspathOptions.javac(false), setup.javaHome)
+    val scalac       = newScalaCompiler(instance, interfaceJar, log)
+    val javac        = newJavaCompiler(instance, setup.javaHome, setup.forkJava)
     new Compiler(scalac, javac)
+  }
+
+  /**
+   * Create a new scala compiler.
+   */
+  def newScalaCompiler(instance: ScalaInstance, interfaceJar: File, log: Logger): AnalyzingCompiler = {
+    IC.newScalaCompiler(instance, interfaceJar, ClasspathOptions.boot, log)
+  }
+
+  /**
+   * Create a new java compiler.
+   */
+  def newJavaCompiler(instance: ScalaInstance, javaHome: Option[File], fork: Boolean): JavaCompiler = {
+    val options = ClasspathOptions.javac(false)
+    if (fork || javaHome.isDefined)
+      sbt.compiler.JavaCompiler.fork(options, instance)(AggressiveCompile.forkJavac(javaHome))
+    else
+      sbt.compiler.JavaCompiler.directOrFork(options, instance)(AggressiveCompile.forkJavac(None))
   }
 
   /**

src/main/scala/com/typesafe/zinc/Settings.scala
@@ -27,6 +27,7 @@ case class Settings(
   scala: ScalaLocation       = ScalaLocation(),
   scalacOptions: Seq[String] = Seq.empty,
   javaHome: Option[File]     = None,
+  forkJava: Boolean          = false,
   javaOnly: Boolean          = false,
   javacOptions: Seq[String]  = Seq.empty,
   compileOrder: CompileOrder = CompileOrder.Mixed,
@@ -148,7 +149,8 @@ object Settings {
     prefix(    "-S", "<scalac-option>",        "Pass option to scalac",                      (s: Settings, o: String) => s.copy(scalacOptions = s.scalacOptions :+ o)),
 
     header("Java options:"),
-    file(      "-java-home", "directory",      "Java home directory (to select javac)",      (s: Settings, f: File) => s.copy(javaHome = Some(f))),
+    file(      "-java-home", "directory",      "Select javac home directory (and fork)",     (s: Settings, f: File) => s.copy(javaHome = Some(f))),
+    boolean(   "-fork-java",                   "Run java compiler in separate process",      (s: Settings) => s.copy(forkJava = true)),
     string(    "-compile-order", "order",      "Compile order for Scala and Java sources",   (s: Settings, o: String) => s.copy(compileOrder = compileOrder(o))),
     boolean(   "-java-only",                   "Don't add scala library to classpath",       (s: Settings) => s.copy(javaOnly = true)),
     prefix(    "-C", "<javac-option>",         "Pass option to javac",                       (s: Settings, o: String) => s.copy(javacOptions = s.javacOptions :+ o)),

commit 25154a1324efb650a578c05250af70ea2b7c6de0

Fix binary compatibility of Setup.create methods.
In minor releases we guarantee binary compatibility but
c7ec3120 slipped through the cracks. This change fixes the breakage
introduced in c7ec3120.

Fixes #44.

src/main/scala/com/typesafe/zinc/Setup.scala
@@ -108,6 +108,17 @@ object Setup {
     forkJava
   )
 
+  @deprecated("Use variant that takes forkJava parameter instead.", "0.3.6")
+  def create(
+    scalaCompiler: File,
+    scalaLibrary: File,
+    scalaExtra: JList[File],
+    sbtInterface: File,
+    compilerInterfaceSrc: File,
+    javaHome: File): Setup =
+      create(scalaCompiler, scalaLibrary, scalaExtra, sbtInterface,
+        compilerInterfaceSrc, javaHome, false)
+
   /**
    * Java API for creating Setup with ScalaLocation and SbtJars.
    */
@@ -129,6 +140,12 @@ object Setup {
       forkJava
     )
   }
+
+  @deprecated("Use variant that takes forkJava parameter instead.", "0.3.6")
+  def create(
+    scalaLocation: ScalaLocation,
+    sbtJars: SbtJars,
+    javaHome: File): Setup = create(scalaLocation, sbtJars, javaHome, false)
 
   /**
    * Select the scala jars.