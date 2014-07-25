8dbdcf96ffadc0b03c77cfc7ee4cf140d128cfde
Benjy <benjy@foursquare.com>
2014-07-25 23:39:36 +0000

    Restore namespace package to src/python/pants/__init__.py.
    It turns out to be needed after all.
    
    This reverts the salient part of commit 583eed04142a0aaa9afd525c178b834373f5ef22.

---
+++ src/python/pants/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
+                        print_function, unicode_literals)
+
+
+__import__('pkg_resources').declare_namespace(__name__)
