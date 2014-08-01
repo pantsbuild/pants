pantsbuild/pants

commit 6f7467363a5de9272ecc2f3dd4cd371d6378627f

Add support for nameHashing and recompileOnMacroDef options.
Add support for two new incremental compiler options introduced in
sbt 0.13.2:

  * `nameHashing` which enables new, experimental incremental compilation
    algorithm
  * `recompileOnMacroDef` which controls heuristic related to how
    macro definitions and their dependencies are handled

Note that since `recompileOnMacroDef` is turned on by default we introduced
`recompileOnMacroDefDisabled` command line option which changes the
default value to be off.

src/main/scala/com/typesafe/zinc/Settings.scala
@@ -114,7 +114,9 @@ case class IncOptions(
   apiDiffContextSize: Int        = DefaultIncOptions.apiDiffContextSize,
   apiDumpDirectory: Option[File] = DefaultIncOptions.apiDumpDirectory,
   transactional: Boolean         = false,
-  backup: Option[File]           = None
+  backup: Option[File]           = None,
+  recompileOnMacroDef: Boolean   = DefaultIncOptions.recompileOnMacroDef,
+  nameHashing: Boolean           = DefaultIncOptions.nameHashing
 ) {
   def options: sbt.inc.IncOptions = {
     sbt.inc.IncOptions(
@@ -124,7 +126,9 @@ case class IncOptions(
       apiDebug,
       apiDiffContextSize,
       apiDumpDirectory,
-      classfileManager
+      classfileManager,
+      recompileOnMacroDef,
+      nameHashing
     )
   }
 
@@ -205,6 +209,10 @@ object Settings {
     int(       "-api-diff-context-size", "n",  "Diff context size (in lines) for API debug", (s: Settings, i: Int) => s.copy(incOptions = s.incOptions.copy(apiDiffContextSize = i))),
     boolean(   "-transactional",               "Restore previous class files on failure",    (s: Settings) => s.copy(incOptions = s.incOptions.copy(transactional = true))),
     file(      "-backup", "directory",         "Backup location (if transactional)",         (s: Settings, f: File) => s.copy(incOptions = s.incOptions.copy(backup = Some(f)))),
+    boolean(   "-recompileOnMacroDefDisabled", "Disable recompilation of all dependencies of a macro def",
+      (s: Settings) => s.copy(incOptions = s.incOptions.copy(recompileOnMacroDef = false))),
+    boolean(   "-nameHashing",                 "Enable improved (experimental) incremental compilation algorithm",
+      (s: Settings) => s.copy(incOptions = s.incOptions.copy(nameHashing = true))),
 
     header("Analysis options:"),
     file(      "-analysis-cache", "file",      "Cache file for compile analysis",            (s: Settings, f: File) => s.copy(analysis = s.analysis.copy(cache = Some(f)))),

src/main/scala/com/typesafe/zinc/Inputs.scala
@@ -212,7 +212,9 @@ object Inputs {
       "api dump"               -> incOptions.apiDumpDirectory,
       "api diff context size"  -> incOptions.apiDiffContextSize,
       "transactional"          -> incOptions.transactional,
-      "backup directory"       -> incOptions.backup
+      "backup directory"       -> incOptions.backup,
+      "recompile on macro def" -> incOptions.recompileOnMacroDef,
+      "name hashing"           -> incOptions.nameHashing
     )
 
     val values = Seq(

commit 8142326fd194fd0c199616f4df73a10c4959a198

Restore binary compatibility of IncOptions constructor.
The 6f7467363a5de9272ecc2f3dd4cd371d6378627f introduced support for new
fields in IncOptions class but broke binary compatibility. Let's add
overloaded (and deprecated) constructor which fixes binary compatibility
of constructor.

NOTE: The IncOptions in zinc is a case class so restoring full binary
compatibility (for example, of copy method) is not feasible.

src/main/scala/com/typesafe/zinc/Settings.scala
@@ -118,6 +118,21 @@ case class IncOptions(
   recompileOnMacroDef: Boolean   = DefaultIncOptions.recompileOnMacroDef,
   nameHashing: Boolean           = DefaultIncOptions.nameHashing
 ) {
+  @deprecated("Use the primary constructor instead.", "0.3.5.2")
+  def this(
+    transitiveStep: Int,
+    recompileAllFraction: Double,
+    relationsDebug: Boolean,
+    apiDebug: Boolean,
+    apiDiffContextSize: Int,
+    apiDumpDirectory: Option[File],
+    transactional: Boolean,
+    backup: Option[File]
+  ) = {
+    this(transitiveStep, recompileAllFraction, relationsDebug, apiDebug, apiDiffContextSize,
+      apiDumpDirectory, transactional, backup, DefaultIncOptions.recompileOnMacroDef,
+      DefaultIncOptions.nameHashing)
+  }
   def options: sbt.inc.IncOptions = {
     sbt.inc.IncOptions(
       transitiveStep,