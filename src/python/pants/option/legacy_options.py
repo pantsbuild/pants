2656f9d81a88eed745422cac5f0db911c7e33ca5
Benjy <benjy@foursquare.com>
2014-08-01 18:09:45 +0000

    Convert options registered in the new style into old-style options.
    Temporary code needed only during migration to the new options system.
    
    Note: No __init__.py or BUILD files here. They are in another commit.

---
+++ src/python/pants/option/legacy_options.py
@@ -0,0 +1,108 @@
+# coding=utf-8
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
+                        print_function, unicode_literals)
+
+import copy
+from optparse import OptionGroup
+
+
+class LegacyOptionsError(Exception):
+  pass
+
+
+class LegacyOptions(object):
+  """Registers new-style options onto the old-style OptionParser.
+
+  The scope of a new style option is converted to a prefix on the old-style one, e.g.,
+  --foo in scope compile.scala becomes --compile-scala-foo.
+
+  This is a temporary class needed only during migration to the new options system.
+  It allows us to convert registration code to the new style while still supporting
+  the old flags at runtime.
+  """
+
+  # optparse can only support options of these types. During migration we may only
+  # use these types. After migration is complete and this class is deleted, we can use
+  # other types in exclusively new-style options.
+  OPTPARSE_TYPES = set([str, int, long, float, complex])
+
+  # optparse only supports these actions.
+  OPTPARSE_ACTIONS = set(['store', 'store_const', 'store_true', 'store_false',
+                          'append', 'append_const', 'count'])
+
+  def __init__(self, scope, optparser):
+    """This object registers on behalf of the given scope, into the given optparser."""
+    self._scope_prefix = scope.replace('.', '-')
+    self._optparser = optparser
+    self._option_group = self._get_option_group(scope, optparser)
+
+  def register(self, args, kwargs, legacy_dest=None, legacy_args=None):
+    """Register the option, using argparse params."""
+    if legacy_dest:
+      optparse_kwargs = copy.copy(kwargs)
+
+      # The args/kwargs are argparse-style, whereas we need optparse-style, so
+      # we perform necessary adjustments here.
+
+      optparse_args = []
+      for arg in legacy_args or args:
+        if arg.startswith('--no-'):
+          optparse_args.append('--no-%s-%s' % (self._scope_prefix, arg[5:]))
+          optparse_kwargs.pop('help', None)
+        elif arg.startswith('--'):
+          optparse_args.append('--%s-%s' % (self._scope_prefix, arg[2:]))
+        elif arg.startswith('-'):
+          if self._scope_prefix == '':
+            optparse_args.append(arg)
+          else:
+            raise LegacyOptionsError('Short legacy options only allowed at global scope.')
+
+      optparse_kwargs['dest'] = legacy_dest
+
+      # The 'type' kwarg is a function in argparse but a string in optparse.
+      typ = optparse_kwargs.pop('type', None)
+      if typ:
+        if typ in LegacyOptions.OPTPARSE_TYPES:
+          optparse_kwargs['type'] = typ.__name__
+        else:
+          raise LegacyOptionsError('Invalid optparse type: %s' % typ)
+
+      # 'choice' is a string subtype in optparse.
+      choices = optparse_kwargs.get('choices', None)
+      if choices and (typ is None or typ == str):
+        optparse_kwargs['type'] = 'choice'
+      else:  # optparse doesn't support non-string choices, so ignore the choices restriction.
+        optparse_kwargs.pop('choices', None)
+
+      action = optparse_kwargs.get('action', 'store')
+      if action not in LegacyOptions.OPTPARSE_ACTIONS:
+        raise LegacyOptionsError('Invalid optparse action: %s' % action)
+
+      options_container = self._option_group or self._optparser
+      options_container.add_option(*optparse_args, **optparse_kwargs)
+
+  def format_help(self):
+    """Return a help message for the old-style options.
+
+    Note that this message will reflect all options registered in the
+    relevant group, whether by this object or via the old registration code.
+    """
+    self._optparser.formatter.store_option_strings(self._optparser)
+    if self._option_group and self._option_group.option_list:
+      return self._option_group.format_help(self._optparser.formatter)
+    else:
+      return ''
+
+  def _get_option_group(self, scope, optparser):
+    if not scope:
+      return None
+    # See if we have an existing option group (from options registered the old way).
+    for option_group in optparser.option_groups:
+      if option_group.title == scope:
+        return option_group
+    option_group = OptionGroup(optparser, scope)
+    optparser.add_option_group(option_group)
+    return option_group
---
+++ tests/python/pants_test/option/test_legacy_options.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
+                        print_function, unicode_literals)
+
+import optparse
+import pytest
+import shlex
+import unittest
+
+from pants.option.legacy_options import LegacyOptions
+
+
+class LegacyOptionsTest(unittest.TestCase):
+  def test_registration(self):
+    optparser = optparse.OptionParser()
+    legacy_options = LegacyOptions('compile.java', optparser)
+    legacy_options.register('--foo', { 'action': 'store_true' }, 'compile_java_foo')
+    legacy_options.register('--bar', { 'type': long }, 'compile_java_bar')
+    legacy_options.register('--baz', { 'choices': ['xx', 'yy', 'zz'] }, 'compile_java_baz')
+    legacy_options.register('--qux', { 'type': int, 'choices': [1, 2, 3], 'action': 'append' },
+                            'compile_java_qux')
+    legacy_options.register('--corge', { 'type': int, 'default': 55 } ,
+                            'compile_java_corge')
+
+    args = shlex.split(str('--compile-java-foo --compile-java-bar=33 --compile-java-baz=xx '
+                           '--compile-java-qux=1 --compile-java-qux=4'))
+    opts, _ = optparser.parse_args(args)
+    self.assertTrue(opts.compile_java_foo)
+    self.assertEquals(opts.compile_java_bar, 33)
+    self.assertEquals(opts.compile_java_baz, 'xx')
+    self.assertEquals(opts.compile_java_qux, [1, 4])  # Choices not enforced for non-string types.
+    self.assertEquals(opts.compile_java_corge, 55)  # Defaults preserved.
+
+    with pytest.raises(SystemExit):
+      args = shlex.split(str('--compile-java-baz=ww'))  # Choices enforced for string types.
+      opts, _ = optparser.parse_args(args)
