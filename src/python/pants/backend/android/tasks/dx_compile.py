39f86eda7027b48646b984e80a27b7594ab9d08d
mateor <mateorod9@gmail.com>
2014-08-06 01:12:00 -0500

    Add setup_parser to dx_compile
    Allows CLI setting of VM options passed to the runjava, and
    build-tools-versions to specify a particular dx.jar to use
    to create the dex.

--- src/python/pants/backend/android/tasks/dx_compile.py
+++ src/python/pants/backend/android/tasks/dx_compile.py
@@ -15,19 +15,26 @@ from pants.base.workunit import WorkUnit
 from pants.util.dirutil import safe_mkdir
 
 
-class DxCompile(NailgunTask, AndroidTask):
+class DxCompile(AndroidTask, NailgunTask):
   """
   Compile java classes into dex files, Dalvik executables.
   """
 
-  _CONFIG_SECTION = 'dx-tool'
-# name of output file. "Output name must end with one of: .dex .jar .zip .apk or be a directory."
+  # name of output file. "Output name must end with one of: .dex .jar .zip .apk or be a directory."
   classes_dex = 'classes.dex'
+  _CONFIG_SECTION = 'dx-tool'
 
-  # @classmethod
-  # def setup_parser(cls, option_group, args, mkflag):
-  #   # VM options go here dx -J<options>
-  #   pass
+  @classmethod
+  def setup_parser(cls, option_group, args, mkflag):
+    super(DxCompile, cls).setup_parser(option_group, args, mkflag)
+
+    option_group.add_option(mkflag("build-tools-version"), dest="build_tools_version",
+                            help="[%default] Specifies the Android build-tools version used "
+                                 "to create the dex file.")
+
+    option_group.add_option(mkflag("VM-options"), dest="vm_options",
+                            help="[%default] Pass VM-specific options to the virtual machine "
+                                 "that runs dx.")
 
   @classmethod
   def is_dextarget(cls, target):
@@ -41,6 +48,9 @@ class DxCompile(NailgunTask, AndroidTask):
   def __init__(self, context, workdir):
     super(DxCompile, self).__init__(context, workdir)
     self._android_dist = self.android_sdk
+    self._forced_build_tools_version = context.options.build_tools_version
+    self._forced_vm_options = context.options.vm_options
+
     config_section = self.config_section
     self.setup_artifact_cache_from_config(config_section=config_section)
 
@@ -62,15 +72,20 @@ class DxCompile(NailgunTask, AndroidTask):
     #            See comment on self.classes_dex for restrictions.
     args.extend(['--dex', '--no-strict', '--output=' + dex_file])
 
-    # classes is a list of class files to be included in the created file.
+    # classes is a list of class files to be included in the created dex file.
     args.extend(classes)
     return args
 
   def _compile_dex(self, args, build_tools_version):
-    classpath = [self.dx_jar_tool(build_tools_version)]
-    java_main = 'com.android.dx.command.Main'
-    return self.runjava(classpath=classpath, main=java_main, args=args, workunit_name='dx')
+    if self._forced_build_tools_version:
+      classpath = [self.dx_jar_tool(self._forced_build_tools_version)]
+    else:
+      classpath = [self.dx_jar_tool(build_tools_version)]
 
+    jvm_options = self._forced_vm_options if self._forced_vm_options else None
+    java_main = 'com.android.dx.command.Main'
+    return self.runjava(classpath=classpath, jvm_options=jvm_options, main=java_main,
+                        args=args, workunit_name='dx')
 
   def execute(self):
     with self.context.new_workunit(name='dx-compile', labels=[WorkUnit.MULTITOOL]):
