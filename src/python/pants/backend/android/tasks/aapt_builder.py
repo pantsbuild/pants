d5765caa44965683bd432da561c798f5753be9c8
mateor <mateorod9@gmail.com>
2014-08-08 15:30:39 -0500

    Further cleanup of aapt_builder
    Still there are some hacks in this.

--- src/python/pants/backend/android/tasks/aapt_builder.py
+++ src/python/pants/backend/android/tasks/aapt_builder.py
@@ -11,7 +11,6 @@ import subprocess
 from twitter.common import log
 
 from pants.backend.android.targets.android_binary import AndroidBinary
-from pants.backend.android.targets.android_target import AndroidTarget
 from pants.backend.android.tasks.aapt_task import AaptTask
 from pants.base.build_environment import get_buildroot
 from pants.base.exceptions import TaskError
@@ -29,10 +28,6 @@ class AaptBuilder(AaptTask):
   def is_app(target):
     return isinstance(target, AndroidBinary)
 
-  @staticmethod
-  def is_android(target):
-    return isinstance(target, AndroidTarget)
-
   def __init__(self, context, workdir):
     super(AaptBuilder, self).__init__(context, workdir)
 
@@ -48,18 +43,17 @@ class AaptBuilder(AaptTask):
     else:
       args.append(self.aapt_tool(target.build_tools_version))
 
-    #TODO (MATEOR) update for this subclass
     # Glossary of used aapt flags. Aapt handles a ton of action, this will continue to expand.
     #   : 'package' is the main aapt operation (see class docstring for more info).
-    #   : '-m' is to "make" a package directory under location '-J'.
-    #   : '-J' Points to the output directory.
     #   : '-M' is the AndroidManifest.xml of the project.
     #   : '-S' points to the resource_dir to "spider" down while collecting resources.
-    #   : '-I' packages to add to base "include" set, here it is the android.jar of the target-sdk.
+    #   : '-I' packages to add to base "include" set, here the android.jar of the target-sdk.
+    #   : '--ignored-assets' patterns for the aapt to skip. This is the default w/ 'BUILD*' added.
+    #   : '-F' The name and location of the .apk file to output
+    #   : additional positional arguments are treated as input directories to gather files from.
 
-    args.extend(['package', '-v', '-f', '-M', target.manifest,
+    args.extend(['package', '-M', target.manifest,
                  '-S'])
-
     args.extend(resource_dir)
     if self._forced_target_sdk:
       args.extend(['-I', self.android_jar_tool(self._forced_target_sdk)])
@@ -71,13 +65,9 @@ class AaptBuilder(AaptTask):
     else:
       args.extend(['--ignore-assets', self.IGNORED_ASSETS])
 
-    # extend -F bin/*apk $INPUT_DIRS
     args.extend(['-F', os.path.join(self.workdir, target.name + '.apk')])
     args.extend(inputs)
     log.debug('Executing: {0}'.format(args))
-    print (args)
-    for arg in args:
-      print("arg {0} is: {1}".format(arg, type(arg)))
     return args
 
   def execute(self):
@@ -86,27 +76,27 @@ class AaptBuilder(AaptTask):
       targets = self.context.targets(self.is_app)
       #TODO (MATEOR) invalidation machinery
       for target in targets:
+        # to hold the Android target's resource_dir (e.g. 'res') and dir containing classes.dex
         input_dirs = []
+        # to hold the basedir containing an R.java
         resource_dir = []
         mapping = self.context.products.get('dex')
         for basedir in mapping.get(target):
           input_dirs.append(basedir)
 
+        def gather_resources(target):
+          """Check target deps for R.java and get its basedir and that target's resource_dir"""
+          android_codegen = self.context.products.get('android-gen')
 
-        def add_r_java(target):
-          new_resources = self.context.products.get('android-gen')
-          if new_resources.get(target) is not None:
+          # null check is needed as new_resources can contain null values. Further pushing needed.
+          if android_codegen.get(target) is not None:
             resource_dir.append(os.path.join(get_buildroot(), target.resource_dir))
-            print(resource_dir)
-            print("Donald DUUUCKCKCKCKCKCKCK")
-            for basedir in new_resources.get(target):
+            for basedir in android_codegen.get(target):
               input_dirs.append(os.path.join(basedir, self.package_path(target.package)))
 
-        target.walk(add_r_java)
-        print(type(resource_dir))
+        target.walk(gather_resources)
+
         process = subprocess.Popen(self.render_args(target, resource_dir, input_dirs))
         result = process.wait()
         if result != 0:
           raise TaskError('Android aapt tool exited non-zero ({code})'.format(code=result))
-
-        [u'/Users/mateor/development/android-sdk-macosx/build-tools/19.1.0/aapt', u'package', u'-v', u'-f', u'-M', u'src/android/example/AndroidManifest.xml', u'-S', None, u'-I', u'/Users/mateor/development/android-sdk-macosx/platforms/android-19/android.jar', u'--ignore-assets', '!.svn:!.git:!.ds_store:!*.scc:.*:<dir>_*:!CVS:!thumbs.db:!picasa.ini:!*~:BUILD*', u'-F', u'/Users/mateor/development/pants/.pants.d/bundle/apk', u'/Users/mateor/development/pants/.pants.d/dex/dex/src.android.example.hello', u'/Users/mateor/development/pants/.pants.d/gen/aapt/com/pants/examples/hello']
