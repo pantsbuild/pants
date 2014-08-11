df381c8482e6b631964453982428f480352dadfd
Mateo Rodriguez <mateorod9@gmail.com>
2014-08-11 13:58:14 -0600

    Add DxCompile task to android backend
    This task gathers the class files of a target and compiles them into a classes.dex.
    Dex file is a Dalvik exeecutable file. Once the classes have been dexed, then the app
    is ready to be packaged into an .apk.
    
    The classes.dex that is created is output to
    pants.d/dex/dex/target.id/classes.dex. The target.id is
    simply the package name. This is done b/c every Android target that
    is dexed will output a file named "classes.dex". Putting them in
    dirs that are named after the target.id means that they will
    not clobber each other unless the package names correspond
    (which would cause other problems as well).
    
    CLI options allow configuring of VM options and build-tools versions.
    
    This is just the task itself, I will write the test class tomorrow. I wanted to give people
    a chance to comment while I worked on them, in case large changes are needed.
    If you prefer to wait for the test class, that should be up tomorrow.
    
    Testing Done:
    CI.sh passes at 67089e7
    Full integration tests pass on local machine.
    
    Travis now passes [0].
    
    [0] https://travis-ci.org/pantsbuild/pants/jobs/31952336
    
    Reviewed at https://rbcommons.com/s/twitter/r/840/

---
+++ tests/python/pants_test/android/tasks/test_dx_compile_integration.py
@@ -0,0 +1,54 @@
+# coding=utf-8
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
+                        print_function, unicode_literals)
+
+import os
+import pytest
+
+from pants_test.pants_run_integration_test import PantsRunIntegrationTest
+
+
+class DxCompileIntegrationTest(PantsRunIntegrationTest):
+  """Integration test for DxCompile
+
+  The Android SDK is modular, finding an SDK on the PATH is no guarantee that there is
+  a dx.jar anywhere on disk. In this test we look for a set of default tools that will get the
+  job done or the test is skipped. The TARGET_SDK version must match the targetSDK value in the
+  AndroidManifest.xml of the target while the BUILD_TOOLS version is arbitrary.
+  """
+  BUILD_TOOLS = '19.1.0'
+  TARGET_SDK = '19'
+  ANDROID_SDK_LOCATION = 'ANDROID_HOME'
+  DEX_FILE = 'classes.dex'
+
+  @classmethod
+  def requirements(cls):
+    sdk_home = os.environ.get('ANDROID_HOME')
+    android_sdk = os.path.abspath(sdk_home) if sdk_home else None
+    if android_sdk:
+      if os.path.isfile(os.path.join(android_sdk, 'build-tools', cls.BUILD_TOOLS, 'lib',
+                                     'dx.jar')):
+        if os.path.isfile(os.path.join(android_sdk, 'platforms', 'android-' + cls.TARGET_SDK,
+                                       'android.jar')):
+          return True
+    return False
+
+  @pytest.mark.skipif('not DxCompileIntegrationTest.requirements()',
+                      reason='Dx integration test requires Android build-tools {0!r}, SDK {1!r}'
+                             ' and ANDROID_HOME set in path.'.format(BUILD_TOOLS, TARGET_SDK))
+  def test_dx_compile(self):
+    self.dx_test('src/android/example:hello')
+
+  def dx_test(self, target):
+      pants_run = self.run_pants(['goal', 'dex', target])
+      self.assertEquals(pants_run.returncode, self.PANTS_SUCCESS_CODE,
+                        "goal publish expected success, got {0}\n"
+                        "got stderr:\n{1}\n"
+                        "got stdout:\n{2}\n".format(pants_run.returncode,
+                                                    pants_run.stderr_data,
+                                                    pants_run.stdout_data))
+
+  #TODO(mateor): if in the future DxCompile outputs to dist then we can verify the artifact here.
