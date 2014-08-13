7b012e3eb9d8b4d334f1856de97c0712a68bf081
mateor <mateorod9@gmail.com>
2014-08-13 15:44:23 -0500

    Add jarsigner integration test
    Should all pass CI and then going up for a quick review

--- tests/python/pants_test/android/tasks/test_jarsigner_integration.py
+++ tests/python/pants_test/android/tasks/test_jarsigner_integration.py
@@ -9,34 +9,36 @@ import os
 import pytest
 
 from pants_test.android.android_integration_test import AndroidIntegrationTest
+from pants_test.tasks.test_base import is_exe
 
 
-class AaptBuilderIntegrationTest(AndroidIntegrationTest):
-  """Integration test for AaptBuilder, which builds an unsigned .apk
-
+class JarsignerIntegrationTest(AndroidIntegrationTest):
+  """Integration test for JarsignerTask
 
   The Android SDK is modular, finding an SDK on the PATH is no guarantee that there is
-  an aapt binary anywhere on disk. The TOOLS are the ones required by the target in the
-  'test_aapt_bundle' method. If you add a target, you may need to expand the TOOLS list
-  and perhaps define new BUILD_TOOLS or TARGET_SDK class variables."""
-
+  a dx.jar anywhere on disk. The TOOLS are the ones required by the target in 'test_dx_compile'
+  method. If you add a target, you may need to expand the TOOLS list and perhaps define new
+  BUILD_TOOLS or TARGET_SDK class variables.
+  """
   TOOLS = [
     os.path.join('build-tools', AndroidIntegrationTest.BUILD_TOOLS, 'aapt'),
     os.path.join('build-tools', AndroidIntegrationTest.BUILD_TOOLS, 'lib', 'dx.jar'),
     os.path.join('platforms', 'android-' + AndroidIntegrationTest.TARGET_SDK, 'android.jar')
   ]
 
-  tools = AndroidIntegrationTest.requirements(TOOLS)
+  JAVA = is_exe('java')
 
-  @pytest.mark.skipif('not AaptBuilderIntegrationTest.tools',
-                      reason='Android integration test requires tools {0!r} '
+  tools = AndroidIntegrationTest.requirements(TOOLS) and JAVA
+
+  @pytest.mark.skipif('not JarsignerIntegrationTest.tools',
+                      reason='Jarsigner integration test requires the JDK, Android tools {0!r} '
                              'and ANDROID_HOME set in path.'.format(TOOLS))
 
-  def test_aapt_bundle(self):
-    self.bundle_test('src/android/example:hello')
+  def test_jarsigner(self):
+    self.jarsigner_test('src/android/example:hello')
 
-  def bundle_test(self, target):
-    pants_run = self.run_pants(['goal', 'bundle', target])
+  def jarsigner_test(self, target):
+    pants_run = self.run_pants(['goal', 'sign', target])
     self.assertEquals(pants_run.returncode, self.PANTS_SUCCESS_CODE,
                       "goal publish expected success, got {0}\n"
                       "got stderr:\n{1}\n"
