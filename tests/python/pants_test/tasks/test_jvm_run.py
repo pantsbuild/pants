33158815ba79af85fd92acf761fd0128af493175
John Sirois <john.sirois@gmail.com>
2014-08-18 12:17:52 -0600

    Fixup JvmRun  only-write-cmd-line flag to accept relative paths.
    Introduce expand_path fs utility and use it on JvmRun.
    Add tests for this new util and JvmRun's cmdline-only mode.
    
    Testing Done:
    $ PANTS_DEV=1 ./pants goal test tests/python/pants_test/tasks/:jvm_run tests/python/pants_test/fs --test-pytest-options="-v -k'test_jvm_run or test_expand_path'"
    ...
    11:10:49 00:00   [test]
    11:10:49 00:00     [pytest]
    11:10:49 00:00       [run]
                         ============== test session starts ===============
                         platform linux2 -- Python 2.6.9 -- py-1.4.23 -- pytest-2.6.1 -- /usr/bin/python2.6
                         plugins: cov, timeout
                         collected 12 items
    
                         tests/python/pants_test/fs/test_expand_path.py::ExpandPathTest::test_absolute PASSED
                         tests/python/pants_test/fs/test_expand_path.py::ExpandPathTest::test_dot_relative PASSED
                         tests/python/pants_test/fs/test_expand_path.py::ExpandPathTest::test_env_var_expansion PASSED
                         tests/python/pants_test/fs/test_expand_path.py::ExpandPathTest::test_pure_relative PASSED
                         tests/python/pants_test/fs/test_expand_path.py::ExpandPathTest::test_user_expansion PASSED
                         tests/python/pants_test/tasks/test_jvm_run.py::JvmRunTest::test_cmdline_only PASSED
    
                          6 tests deselected by '-ktest_jvm_run or test_expand_path'
                         ===== 6 passed, 6 deselected in 0.19 seconds =====
    
    11:10:50 00:01     [junit]
    11:10:50 00:01     [specs]
                   SUCCESS
    
    Bugs closed: 494, 495
    
    Reviewed at https://rbcommons.com/s/twitter/r/908/

---
+++ tests/python/pants_test/tasks/test_jvm_run.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
+                        print_function, unicode_literals)
+
+import os
+
+from pants.backend.core.tasks.check_exclusives import ExclusivesMapping
+from pants.backend.jvm.targets.jvm_binary import JvmBinary
+from pants.backend.jvm.tasks.jvm_run import JvmRun
+from pants.engine.round_manager import RoundManager
+from pants.util.contextutil import pushd, temporary_dir
+from pants_test.base_test import BaseTest
+from pants_test.tasks.test_base import prepare_task
+
+
+class JvmRunTest(BaseTest):
+  def test_cmdline_only(self):
+    jvm_binary = self.make_target('src/java/com/pants:binary', JvmBinary, main="com.pants.Binary")
+    jvm_run = prepare_task(JvmRun, args=['--test-only-write-cmd-line=a'], targets=[jvm_binary])
+
+    round_manager = RoundManager(jvm_run.context)
+    jvm_run.prepare(round_manager)
+
+    self.prepare_exclusives(jvm_run.context, classpath=['bob', 'fred'])
+
+    with temporary_dir() as pwd:
+      with pushd(pwd):
+        cmdline_file = os.path.join(pwd, 'a')
+        self.assertFalse(os.path.exists(cmdline_file))
+        jvm_run.execute()
+        self.assertTrue(os.path.exists(cmdline_file))
+        with open(cmdline_file) as fp:
+          contents = fp.read()
+          self.assertTrue('java ', contents.endswith('java -cp bob:fred com.pants.Binary'))
+
+  def prepare_exclusives(self, context, key=None, classpath=None):
+    # TODO(John Sirois): Push this prep up into a test helper - its too much detail to replicate
+    # in tasks needing a classpath.
+    exclusives_mapping = ExclusivesMapping(context)
+    exclusives_mapping.set_base_classpath_for_group(
+        key or '<none>', [('default', entry) for entry in classpath or ['none']])
+    exclusives_mapping._populate_target_maps(context.targets())
+    context.products.safe_create_data('exclusives_groups', lambda: exclusives_mapping)
