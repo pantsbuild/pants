f9b1009b40b31d4dc7e71b558e0056488e904175
Jin Feng <jinfeng@outlook.com>
2014-08-21 07:37:34 -0400

    Make ./pants goal targets to find all the targets aliases/types automatically instead of being hardcoded.
    Currently in TargetsHelp, we hardcoded the list of target aliases to target types mapping. This RB changes it to make the discovery dynamically, by looking at all the registered aliases in build_configuration and then intersect them with all the installed target types from SourceRoot.
    
    Testing Done:
    passed: PANTS_DEV=1 ./pants goal test tests::
    
    Bugs closed: 333
    
    Reviewed at https://rbcommons.com/s/twitter/r/796/

--- tests/python/pants_test/tasks/test_targets_help.py
+++ tests/python/pants_test/tasks/test_targets_help.py
@@ -8,6 +8,7 @@ from __future__ import (nested_scopes, generators, division, absolute_import, wi
 import os.path
 
 from pants.base.build_environment import get_buildroot
+from pants.base.build_file_aliases import BuildFileAliases
 from pants.base.source_root import SourceRoot
 from pants.base.target import Target
 from pants.backend.core.tasks.targets_help import TargetsHelp
@@ -16,9 +17,16 @@ from pants_test.tasks.test_base import ConsoleTaskTest
 
 class TargetsHelpTest(ConsoleTaskTest):
 
+  MY_TARGET_ALIAS = 'my_target'
+
   @classmethod
   def task_type(cls):
     return TargetsHelp
+
+  @property
+  def alias_groups(self):
+    return BuildFileAliases.create(
+      targets={TargetsHelpTest.MY_TARGET_ALIAS: TargetsHelpTest.MyTarget})
 
   def setUp(self):
     super(TargetsHelpTest, self).setUp()
@@ -27,16 +35,22 @@ class TargetsHelpTest(ConsoleTaskTest):
   def test_list_installed_targets(self):
     self.assert_console_output(
       TargetsHelp.INSTALLED_TARGETS_HEADER,
-      '  %s: %s' % ('my_target'.rjust(TargetsHelp.MAX_ALIAS_LEN),
+      '  %s: %s' % (TargetsHelpTest.MY_TARGET_ALIAS.rjust(len(TargetsHelpTest.MY_TARGET_ALIAS)),
                     TargetsHelpTest.MyTarget.__doc__.split('\n')[0]))
 
   def test_get_details(self):
     self.assert_console_output(
       TargetsHelp.DETAILS_HEADER.substitute(
-        name='my_target', desc=TargetsHelpTest.MyTarget.__doc__),
+        name=TargetsHelpTest.MY_TARGET_ALIAS, desc=TargetsHelpTest.MyTarget.__doc__),
       '  name: The name of this target.',
       '   foo: Another argument.  (default: None)',
-      args=['--test-details=my_target'])
+      args=['--test-details={0}'.format(TargetsHelpTest.MY_TARGET_ALIAS)])
+
+  def test_invalid_target_alias_cli_option_cause_raise(self):
+    self.assert_console_raises(
+      ValueError,
+      args=['--test-details=invalid'])
+
 
   class MyTarget(Target):
     """One-line description of the target."""
