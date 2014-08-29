7480ac7ff785f568c04e818c09354aed49395410
Larry Hosken <lhosken@twitter.com>
2014-08-29 14:55:05 -0700

    progress

--- src/python/pants/docs/docsitegen.py
+++ src/python/pants/docs/docsitegen.py
@@ -12,3 +12,18 @@ Suggested use:
   ./build-support/bin/publish_docs.sh # invokes docsitegen.py
 """
 
+import os
+import sys
+import yaml
+
+def load_config(yaml_path):
+  config = yaml.load(file(yaml_path).read().decode('utf8'))
+  # do some sanity-testing on the config:
+  assert(config['tree'][0]['page'] == 'index')
+  return config
+
+def main():
+  config = load_config(sys.argv[1])
+
+if __name__ == "__main__":
+  sys.exit(main())
