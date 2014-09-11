ba650cc07292465e7c48077b4c915f31c164549a
Eric Ayers <zundel@squareup.com>
2014-09-11 17:11:07 -0400

    Changes the behavior of goal idea to create a subdirectory named for the project name.
    IntelliJ will fail to load the first time if you change the name and try to write the project into
    the same directory as an existing project.
    
    Also, enhanced the integration check to examine the contents of the project file when launching "hello"
    
    Testing Done:
    Manually confirmed the error "Cannot load module file '/Users/zundel/Development/java/squarepants/idea/foo4568.iml':
    File /Users/zundel/Development/java/squarepants/idea/foo4568.iml does not exist
    Would you like to remove module 'foo4568' from the project?"
    
    does not occur any more when you change the project name.
    
    Bugs closed: 564, 571
    
    Reviewed at https://rbcommons.com/s/twitter/r/1019/

--- src/python/pants/backend/jvm/tasks/ide_gen.py
+++ src/python/pants/backend/jvm/tasks/ide_gen.py
@@ -142,10 +142,15 @@ class IdeGen(JvmBinaryTask, JvmToolTaskMixin):
     else:
       self.java_jdk = '1.%d' % self.java_language_level
 
-    self.gen_project_workdir = os.path.abspath(
-      self.context.options.ide_gen_project_dir or
-      os.path.join(self.workdir, self.__class__.__name__, self.project_name)
-    )
+    # Always tack on the project name to the work dir so each project gets its own linked jars,
+    # etc. See https://github.com/pantsbuild/pants/issues/564
+    if self.context.options.ide_gen_project_dir:
+      self.gen_project_workdir = os.path.abspath(
+        os.path.join(self.context.options.ide_gen_project_dir, self.project_name))
+    else:
+      self.gen_project_workdir = os.path.abspath(
+        os.path.join(self.workdir, self.__class__.__name__, self.project_name))
+
     self.cwd = (
       os.path.abspath(self.context.options.ide_gen_project_cwd) if
       self.context.options.ide_gen_project_cwd else self.gen_project_workdir
--- tests/python/pants_test/tasks/test_ensime_integration.py
+++ tests/python/pants_test/tasks/test_ensime_integration.py
@@ -13,10 +13,9 @@ from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 class EnsimeIntegrationTest(PantsRunIntegrationTest):
 
-  def _ensime_test(self, specs, project_dir=None):
+  def _ensime_test(self, specs, project_dir = os.path.join('.pants.d', 'tmp-ensime', 'project'),
+      project_name='project'):
     """Helper method that tests ensime generation on the input spec list."""
-    if project_dir is None:
-      project_dir = os.path.join('.pants.d', 'tmp-ensime', 'project')
 
     if not os.path.exists(project_dir):
       os.makedirs(project_dir)
@@ -33,9 +32,10 @@ class EnsimeIntegrationTest(PantsRunIntegrationTest):
       # TODO: Actually validate the contents of the project files, rather than just
       # checking if they exist.
       expected_files = ('.ensime',)
-      self.assertTrue(os.path.exists(path),
-          'Failed to find project_dir at {dir}.'.format(dir=path))
-      self.assertTrue(all(os.path.exists(os.path.join(path, name))
+      workdir = os.path.join(path, project_name)
+      self.assertTrue(os.path.exists(workdir),
+          'Failed to find project_dir at {dir}.'.format(dir=workdir))
+      self.assertTrue(all(os.path.exists(os.path.join(workdir, name))
           for name in expected_files), 'Failed to find one of the ensime project files at {dir}'.format(dir=path))
 
   # Testing Ensime integration on a sample project
