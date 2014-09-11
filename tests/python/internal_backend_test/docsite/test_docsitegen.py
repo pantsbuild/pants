aa0bf4b7775ef96411fbd859e7755654a0b86f6c
Larry Hosken <lhosken@twitter.com>
2014-09-11 16:14:10 -0700

    why did I think I needed to register the backend just to test the code?

--- tests/python/internal_backend_test/docsite/test_docsitegen.py
+++ tests/python/internal_backend_test/docsite/test_docsitegen.py
@@ -2,7 +2,7 @@
 # Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import internal_backend.docsite.tasks.docsitegen
+from internal_backend.docsite.tasks import docsitegen
 
 import unittest
 
