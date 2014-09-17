684152a056b82096119b942ad250cc3e2368d678
Larry Hosken <lhosken@twitter.com>
2014-09-17 14:36:40 -0700

    wip

--- build-support/bin/publish_docs_new.sh
+++ build-support/bin/publish_docs_new.sh
@@ -52,7 +52,7 @@ function do_open() {
 }
 
 # generate some markdown as fodder for prototype doc site generator
-${HERE}/../../pants goal markdown --markdown-fragment :: || \
+${HERE}/../../pants goal markdown --markdown-fragment src:: || \
   die "Failed to generate HTML from markdown'."
 
 # invoke doc site generator
