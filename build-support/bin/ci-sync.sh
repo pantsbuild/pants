caa16289345f3cd61b5cb048deafe6b3d21c2b4d
John Sirois <john.sirois@gmail.com>
2014-09-22 13:24:20 -0600

    Fixup ci-sync.sh to cover committer ident.

--- build-support/bin/ci-sync.sh
+++ build-support/bin/ci-sync.sh
@@ -12,8 +12,9 @@
 # See .travis.osx.yml at the root of this repo for more information.
 
 cp .travis.osx.yml .travis.yml && \
-GIT_AUTHOR_NAME="${GH_USER}" GIT_AUTHOR_EMAIL=${GH_EMAIL} \
-  git commit -am "Prepare pants OSX mirror for CI." && \
-git config credential.helper "store --file=.git/credentials" && \
+git config --local user.email "${GH_EMAIL}" && \
+git config --local user.name "${GH_USER}" && \
+git commit -am "Prepare pants OSX mirror for CI." && \
+git config --local credential.helper "store --file=.git/credentials" && \
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials && \
 git push -f https://github.com/pantsbuild/pants-for-travis-osx-ci.git HEAD:master
