dac7377a58da07bf7b90fd87509342d80624803f
Larry Hosken <lhosken@twitter.com>
2014-09-22 16:39:01 -0700

    add a test. inspired by low coverage number.

--- tests/python/internal_backend_test/sitegen/test_sitegen.py
+++ tests/python/internal_backend_test/sitegen/test_sitegen.py
@@ -13,14 +13,16 @@ CONFIG_JSON = '''
 {
   "sources": {
     "index": "fake0/README.html",
-    "subdir/page1": "fake1/p1.html"
+    "subdir/page1": "fake1/p1.html",
+    "subdir/page2": "fake1/p2.html"
   },
   "extras": {
   },
   "tree": [
     { "page": "index",
       "children": [
-        { "page": "subdir/page1" }
+        { "page": "subdir/page1" },
+        { "page": "subdir/page2" }
       ]
     }
   ],
@@ -41,6 +43,15 @@ P1_HTML = '''
 <h1>Page 1</h1>
 '''
 
+P2_HTML = '''
+<head>
+  <title>Page 2: Electric Boogaloo</title>
+</head>
+<body>
+<h1>Page 2</h1>
+</body>
+'''
+
 TEMPLATE_MUSTACHE = '''
 {{{body_html}}}
 '''
@@ -51,13 +62,46 @@ class AllTheThingsTestCase(unittest.TestCase):
     self.config = json.loads(CONFIG_JSON)
     self.orig_soups = {
       'index': bs4.BeautifulSoup(INDEX_HTML),
-      'subdir/page1': bs4.BeautifulSoup(P1_HTML)
+      'subdir/page1': bs4.BeautifulSoup(P1_HTML),
+      'subdir/page2': bs4.BeautifulSoup(P2_HTML),
     }
     self.precomputed = sitegen.precompute(self.config, self.orig_soups)
 
   def test_fixup_internal_links(self):
     soups = self.orig_soups.copy()
     sitegen.fixup_internal_links(self.config, soups)
+    html = sitegen.render_html('index',
+                               self.config,
+                               soups,
+                               self.precomputed,
+                               TEMPLATE_MUSTACHE)
+    self.assertTrue('subdir/page1.html' in html,
+                    'p1.html link did not get fixed up to page1.html')
+
+  def test_find_title(self):
+    soups = self.orig_soups.copy()
+    html = sitegen.render_html('subdir/page2',
+                               self.config,
+                               soups,
+                               self.precomputed,
+                               '{{title}}')
+    self.assertEqual(html, 'Page 2: Electric Boogaloo',
+                     '''Didn't find correct title''')
+
+  def test_fixup_internal_links(self):
+    soups = self.orig_soups.copy()
+    sitegen.fixup_internal_links(self.config, soups)
+    html = sitegen.render_html('index',
+                               self.config,
+                               soups,
+                               self.precomputed,
+                               TEMPLATE_MUSTACHE)
+    self.assertTrue('subdir/page1.html' in html,
+                    'p1.html link did not get fixed up to page1.html')
+
+  def test_transform_fixes_up_internal_links(self):
+    soups = self.orig_soups.copy()
+    sitegen.transform_soups(self.config, soups, self.precomputed)
     html = sitegen.render_html('index',
                                self.config,
                                soups,
