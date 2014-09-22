eb65ab848dcb10b05237232adbd44d5f3950e772
John Sirois <john.sirois@gmail.com>
2014-09-22 12:15:28 -0600

    Sync the OSX CI eagerly and add diagnostics.
    Move the OSX CI sync up-front to leverage parallelism
    instead of waiting on the whole local build matric to complete.
    
    Also add diagnostics that record why the sync is skipped if
    skipped in addition to making skipping & syncing decisions more
    robust and explicit.

--- build-support/bin/ci-sync.sh
+++ build-support/bin/ci-sync.sh
@@ -11,9 +11,46 @@
 #
 # See .travis.osx.yml at the root of this repo for more information.
 
+# Only sync pantsbuild/pants-for-travis-osx-ci on pushes to pantsbuild/pants master
+# and only do this for 1 CI job in the build matrix.
+
+# The secured GH_* env vars will only be set for the pantsbuild/pants repo and
+# not for its forks, see:
+#  http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
+if [[ -z ${GH_TOKEN} ]]
+then
+  echo "Not syncing OSX CI for clone of $(git config --get remote.origin.url)."
+  exit 0
+fi
+
+# See this env var reference:
+#  http://docs.travis-ci.com/user/ci-environment/#Environment-variables
+if [[ "${TRAVIS_BRANCH}" != "master" ]]
+then
+  echo "Not syncing OSX CI for branch ${TRAVIS_BRANCH}."
+  exit 0
+fi
+
+# Iff there is an associated pull request, we'll have an env var like:
+#   TRAVIS_PULL_REQUEST=123
+if (( ${TRAVIS_PULL_REQUEST:-0} > 0 ))
+then
+  echo "Not syncing OSX CI for pull request ${TRAVIS_PULL_REQUEST}."
+  exit 0
+fi
+
+if [[ "${TRAVIS_BUILD_NUMBER}" != "${TRAVIS_JOB_NUMBER}" && \
+      "${TRAVIS_JOB_NUMBER/${TRAVIS_BUILD_NUMBER}./}" != "1" ]]
+then
+  echo "Not syncing OSX CI for auxillary CI job ${TRAVIS_JOB_NUMBER}."
+  exit 0
+fi
+
+echo "Syncing OSX CI to $(git rev-parse HEAD) for CI build ${TRAVIS_BUILD_NUMBER}." && \
 cp .travis.osx.yml .travis.yml && \
 GIT_AUTHOR_NAME="${GH_USER}" GIT_AUTHOR_EMAIL=${GH_EMAIL} \
   git commit -am "Prepare pants OSX mirror for CI." && \
 git config credential.helper "store --file=.git/credentials" && \
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials && \
 git push -f https://github.com/pantsbuild/pants-for-travis-osx-ci.git HEAD:master
+
