81929fa2cb656dd98500d38256dc9524b4e8cbab
Tejal Desai <tdesai@twitter.com>
2014-10-01 22:25:15 -0700

    Add tests

--- tests/python/pants_test/tasks/test_depmap_integration.py
+++ tests/python/pants_test/tasks/test_depmap_integration.py
@@ -5,11 +5,13 @@
 from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
                         print_function, unicode_literals)
 
+import json
 import os
-import re
 
 from pants.util.contextutil import temporary_dir
 
+from pants.ivy.bootstrapper import Bootstrapper
+from pants.base.build_environment import get_buildroot
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 
@@ -26,16 +28,29 @@ class DepmapIntegrationTest(PantsRunIntegrationTest):
   def test_depmap_with_resolve(self):
     with temporary_dir(root_dir=self.workdir_root()) as workdir:
       depmap_out_file = '{workdir}/depmap_out.txt'.format(workdir=workdir)
+      test_target = 'examples/tests/java/com/pants/examples/usethrift:usethrift'
       pants_run = self.run_pants_with_workdir(
-        ['goal', 'resolve', 'depmap', 'testprojects/src/java/com/pants/testproject/unicode/main',
-         '--depmap-project-info',
+        ['goal', 'resolve', 'depmap', test_target, '--depmap-project-info',
          '--depmap-output-file={out_file}'.format(out_file=depmap_out_file)], workdir)
       self._assert_run_success(pants_run)
       self.assertTrue(os.path.exists(depmap_out_file),
-                      msg='Couldn't find depmap output file in {out_file}'
-                      .format(out_file=depmap_out_file))
-
-    #TODO:(tdesai) Test the file contents.
+                      msg='Could not find depmap output file in {out_file}'
+                           .format(out_file=depmap_out_file))
+      with open(depmap_out_file) as json_file:
+        json_data = json.load(json_file)
+        targets = json_data['targets']
+        libraries = json_data['libraries']
+        # check single code gen module is listed in the target
+        codegen_target = os.path.join(os.path.relpath(workdir,get_buildroot()),
+                                      'gen/thrift/combined/gen-java:gen-java')
+        self.assertTrue(codegen_target in targets)
+        # check if transitively pulled in jar exists as dependency
+        self.assertTrue('org.hamcrest:hamcrest-core:1.3' in targets[test_target]['libraries'])
+        #check correct library path.
+        ivy_cache_dir = Bootstrapper.instance().ivy_cache_dir
+        self.assertEquals(libraries['commons-lang:commons-lang:2.5'],
+                          [os.path.join(ivy_cache_dir,
+                                        'commons-lang/commons-lang/jars/commons-lang-2.5.jar')])
 
   def test_depmap_without_resolve(self):
     with temporary_dir(root_dir=self.workdir_root()) as workdir:
@@ -46,5 +61,5 @@ class DepmapIntegrationTest(PantsRunIntegrationTest):
          '--depmap-output-file={out_file}'.format(out_file=depmap_out_file)], workdir)
       self._assert_run_success(pants_run)
       self.assertTrue(os.path.exists(depmap_out_file),
-                      msg='Couldn't find depmap output file {out_file}'
-                      .format(out_file=depmap_out_file))
+                      msg='Could not find depmap output file {out_file}'
+                           .format(out_file=depmap_out_file))
