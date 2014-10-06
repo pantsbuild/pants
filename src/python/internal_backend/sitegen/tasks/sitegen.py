e971ef5444d8d8c08b4de52795946ab0d715a214
larry hosken (Twitter) <mister@lahosken.san-francisco.ca.us>
2014-10-06 15:08:32 -0700

    sitegen task, create site with better navigation.
    Still not everything we want from a basic site generator,
    but some steps in the right direction:
    
    A site-wide table of contents, an outline in which the "nodes" are pages.
    
    For each page,
    + page-wide table of contents, a list of headings.
    + "breadcrumbs" up top, that show how/if this page fits into outline
    
    Testing Done:
    added some tests. mostly "normal" cases, didn't try to dream up corner cases.
    
    added some non-ASCII text to some test data, inspired by recent Unicode-mangling stories on the mailing list.
    
    started https://travis-ci.org/pantsbuild/pants/builds/36613009
    
    Reviewed at https://rbcommons.com/s/twitter/r/1086/

--- src/python/internal_backend/sitegen/tasks/sitegen.py
+++ src/python/internal_backend/sitegen/tasks/sitegen.py
@@ -5,19 +5,21 @@
 from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
                         print_function, unicode_literals)
 
-'''Static Site Generator for the Pants Build documentation site.
+"""Static Site Generator for the Pants Build documentation site.
 
 Suggested use:
   cd pants
   ./build-support/bin/publish_docs.sh  # invokes sitegen.py
-'''
+"""
 
+import collections
 import json
 import os
+import re
 import shutil
-import pystache
 
 import bs4
+import pystache
 
 from pants.backend.core.tasks.task import Task
 from pants.base.exceptions import TaskError
@@ -48,7 +50,7 @@ class SiteGen(Task):
 
 
 def load_config(json_path):
-  '''Load config info from a .json file and return it'''
+  """Load config info from a .json file and return it"""
   with open(json_path) as json_file:
     config = json.loads(json_file.read().decode('utf8'))
   # sanity-test the config:
@@ -57,7 +59,7 @@ def load_config(json_path):
 
 
 def load_soups(config):
-  '''Generate BeautifulSoup AST for each page listed in config'''
+  """Generate BeautifulSoup AST for each page listed in config"""
   soups = {}
   for page, path in config['sources'].items():
     with open(path, 'rb') as orig_file:
@@ -66,27 +68,27 @@ def load_soups(config):
 
 
 class Precomputed(object):
-  '''Info we compute (and preserve) before we mutate things.'''
+  """Info we compute (and preserve) before we mutate things."""
 
   def __init__(self, page):
-    '''
+    """
     :param page: dictionary of per-page precomputed info
-    '''
+    """
     self.page = page
 
 
 class PrecomputedPageInfo(object):
-  '''Info we compute (and preserve) for each page before we mutate things.'''
+  """Info we compute (and preserve) for each page before we mutate things."""
 
   def __init__(self, title):
-    '''
+    """
     :param title: Page title
-    '''
+    """
     self.title = title
 
 
 def precompute(config, soups):
-  '''Return info we want to compute (and preserve) before we mutate things.'''
+  """Return info we want to compute (and preserve) before we mutate things."""
   page = {}
   for p, soup in soups.items():
     title = get_title(soup) or p
@@ -95,11 +97,11 @@ def precompute(config, soups):
 
 
 def fixup_internal_links(config, soups):
-  '''Find href="..." links that link to pages in our docset; fix them up.
+  """Find href="..." links that link to pages in our docset; fix them up.
 
   We don't preserve relative paths between files as we copy-transform them
   from source to dest. So adjust the paths to work with new locations.
-  '''
+  """
   # Pages can come from different dirs; they can go to different dirs.
   # Thus, there's some relative-path-computing here.
   reverse_directory = {}
@@ -119,17 +121,128 @@ def fixup_internal_links(config, soups):
       tag['href'] = tag['href'].replace(old_rel_path, new_rel_path, 1)
 
 
+_heading_re = re.compile('^h[1-6]$')  # match heading tag names h1,h2,h3,...
+
+
+def ensure_headings_linkable(soups):
+  """foreach soup, foreach h1,h2,etc, if no id=... or name=..., give it one.
+
+  Enables tables of contents.
+  """
+  for soup in soups.values():
+    # To avoid re-assigning an existing id, note 'em down.
+    # Case-insensitve because distinguishing links #Foo and #foo would be weird.
+    existing_ids = set([])
+    for tag in soup.find_all(True):
+      existing_ids.add((tag.get('id') or '').lower())
+      existing_ids.add((tag.get('name') or '').lower())
+    count = 100
+    for tag in soup.find_all(_heading_re):
+      if not (tag.has_attr('id') or tag.has_attr('name')):
+        snippet = ''.join([c for c in tag.text if c.isalpha()])[:20]
+        while True:
+          count += 1
+          candidate_id = 'heading_{0}_{1}'.format(snippet, count).lower()
+          if not candidate_id in existing_ids:
+            existing_ids.add(candidate_id)
+            tag['id'] = candidate_id
+            break
+
+
 def transform_soups(config, soups, precomputed):
-  '''Mutate our soups to be better when we write them out later.'''
+  """Mutate our soups to be better when we write them out later."""
   fixup_internal_links(config, soups)
-  # TODO: more to come here
+  ensure_headings_linkable(soups)
+  # TODO: yet more to come here
 
 
 def get_title(soup):
-  '''Given a soup, pick out a title'''
+  """Given a soup, pick out a title"""
   if soup.title: return soup.title.string
   if soup.h1: return soup.h1.string
   return ''
+
+
+def generate_site_toc(config, precomputed, here):
+  site_toc = []
+  def recurse(tree, depth_so_far):
+    for node in tree:
+      if 'page' in node and node['page'] != 'index':
+        dst = node['page']
+        if dst == here:
+          link = here + '.html'
+        else:
+          link = os.path.relpath(dst + '.html', os.path.dirname(here))
+        site_toc.append(dict(depth=depth_so_far,
+                             link=link,
+                             text=precomputed.page[dst].title,
+                             here=(dst == here)))
+      if 'children' in node:
+        recurse(node['children'], depth_so_far + 1)
+  if 'tree' in config:
+    recurse(config['tree'], 0)
+  return site_toc
+
+
+def generate_breadcrumbs(config, precomputed, here):
+  """return template data for breadcrumbs"""
+  breadcrumb_pages = []
+  def recurse(tree, pages_so_far):
+    for node in tree:
+      if 'page' in node:
+        pages_so_far = pages_so_far + [node['page']]
+      if 'page' in node and node['page'] == here:
+        return pages_so_far
+      if 'children' in node:
+        r = recurse(node['children'], pages_so_far)
+        if r:
+          return r
+    return None
+
+  if 'tree' in config:
+    r = recurse(config['tree'], [])
+    if r:
+      breadcrumb_pages = r
+  breadcrumbs_template_data = []
+  for page in breadcrumb_pages:
+    breadcrumbs_template_data.append(dict(
+        link=os.path.relpath(page + '.html', os.path.dirname(here)),
+        text=precomputed.page[page].title))
+  return breadcrumbs_template_data
+
+
+def hdepth(tag):
+  """Compute an h tag's "outline depth".
+
+  E.g., h1 at top level is 1, h1 in a section is 2, h2 at top level is 2.
+  """
+  if not _heading_re.search(tag.name):
+    raise TaskError('Can\'t compute heading depth of non-heading {0}'.format(tag))
+  depth = int(tag.name[1], 10)  # get the 2 from 'h2'
+  cursor = tag
+  while cursor:
+    if cursor.name == 'section':
+      depth += 1
+    cursor = cursor.parent
+  return depth
+
+
+def generate_page_toc(soup):
+  """Return page-level (~list of headings) TOC template data for soup"""
+  found_depth_counts = collections.defaultdict(int)
+  for tag in soup.find_all(_heading_re):
+    if (tag.get('id') or tag.get('name')):
+      found_depth_counts[hdepth(tag)] += 1
+  depth_list = [i for i in range(100) if 1 < found_depth_counts[i]]
+  depth_list = depth_list[:4]
+  toc = []
+  for tag in soup.find_all(_heading_re):
+    depth = hdepth(tag)
+    if depth in depth_list:
+      toc.append(dict(depth=depth_list.index(depth) + 1,
+                      link=tag.get('id') or tag.get('name'),
+                      text=tag.text))
+  return toc
 
 
 def render_html(dst, config, soups, precomputed, template):
@@ -138,11 +251,16 @@ def render_html(dst, config, soups, precomputed, template):
   title = precomputed.page[dst].title
   topdots = ('../' * dst.count('/'))
   if soup.body:
-    body_html = soup.body.prettify()
+    body_html = '{0}'.format(soup.body)
   else:
-    body_html = soup.prettify()
+    body_html = '{0}'.format(soup)
+  page_toc = generate_page_toc(soup)
   html = renderer.render(template,
                          body_html=body_html,
+                         breadcrumbs=generate_breadcrumbs(config, precomputed, dst),
+                         site_toc=generate_site_toc(config, precomputed, dst),
+                         has_page_toc=bool(page_toc),
+                         page_toc=page_toc,
                          title=title,
                          topdots=topdots)
   return html
@@ -161,7 +279,7 @@ def write_en_pages(config, soups, precomputed, template):
 
 
 def copy_extras(config):
-  '''copy over "extra" files named in config json: stylesheets, logos, ...'''
+  """copy over "extra" files named in config json: stylesheets, logos, ..."""
   outdir = config['outdir']
   for dst, src in config['extras'].items():
     dst_path = os.path.join(outdir, dst)
@@ -172,7 +290,7 @@ def copy_extras(config):
 
 
 def load_template(config):
-  '''Return text of template file specified in config'''
+  """Return text of template file specified in config"""
   with open(config['template'], 'rb') as template_file:
     template = template_file.read().decode('utf-8')
   return template
