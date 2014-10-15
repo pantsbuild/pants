7df721222b91950481c2ca120cbef55d8798a245
Eric Ayers <zundel@squareup.com>
2014-10-15 19:58:27 -0400

    When resolving a tool dep, report back the pants.ini section with a reference that is failing. Added a convenience method to pull a dependency list from pants.ini and   report the section/key info along to register_jvm_tool()
    Testing Done:
    ci is baking.
    
    Manual Testing:
    
    1) renamed target 3rdparty:protobuf-2.4.1 to 3rdparty:protobuf-2.4.2 and ran example protobuf compile.
    
    ``
    $ ./pants goal compile examples/src/java/com/pants/examples/protobuf/distance
      ...
    Exception message: protobuf-2.4.1 was not found in BUILD file /Users/zundel/Src/Pants/3rdparty/BUILD. Perhaps you meant one of:
      :checkstyle
      ...
      :util-core
      referenced from [protobuf-gen] key: javadeps in pants.ini
    ```
    
    2) In BUILD.tools, I renamed '//:jar-tool to //:jar-tool2
    
    ```
    $ ./pants goal bundle examples/src/java/com/pants/examples/hello/main
      ...
                         Failed to resolve target for bootstrap tool: //:jar-tool. You probably need to add this dep to your tools BUILD file(s), usually located in the root of the build.
                         See pants.ini section: [jar-tool] key: bootstrap-tools
                         Error: jar-tool was not found in BUILD file /Users/zundel/Src/Pants/BUILD. Perhaps you meant one of:
                           BUILD:readme
                           ...
                           BUILD.tools:zinc
    
    ```
    
    Bugs closed: 676
    
    Reviewed at https://rbcommons.com/s/twitter/r/1162/

--- src/python/pants/backend/codegen/tasks/apache_thrift_gen.py
+++ src/python/pants/backend/codegen/tasks/apache_thrift_gen.py
@@ -20,6 +20,7 @@ from pants.backend.codegen.tasks.code_gen import CodeGen
 from pants.backend.jvm.targets.java_library import JavaLibrary
 from pants.backend.python.targets.python_library import PythonLibrary
 from pants.base.address import SyntheticAddress
+from pants.base.address_lookup_error import AddressLookupError
 from pants.base.build_environment import get_buildroot
 from pants.base.exceptions import TaskError
 from pants.base.target import Target
@@ -48,6 +49,12 @@ def _copytree(from_base, to_base):
 
 
 class ApacheThriftGen(CodeGen):
+
+
+  class DepLookupError(AddressLookupError):
+    """Thrown when a dependency can't be found"""
+    pass
+
   GenInfo = namedtuple('GenInfo', ['gen', 'deps'])
   ThriftSession = namedtuple('ThriftSession', ['outdir', 'cmd', 'process'])
 
@@ -92,8 +99,16 @@ class ApacheThriftGen(CodeGen):
       dependencies = OrderedSet()
       deps[category] = dependencies
       for depspec in depspecs:
-        dependencies.update(self.context.resolve(depspec))
+        try:
+          dependencies.update(self.context.resolve(depspec))
+        except AddressLookupError as e:
+          raise self.DepLookupError("{message}\n  referenced from [{section}] key: {key}"
+                                    "in pants.ini" .format(message=e, section='thrift-gen',
+                                                           key="gen->deps->{category}"
+                                                           .format(category=category)))
     return self.GenInfo(gen, deps)
+
+
 
   _gen_java = None
   @property
