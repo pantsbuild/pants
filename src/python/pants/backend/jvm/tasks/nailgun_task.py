7793a328b5fc354b314fac4722f7dcdf958dad9c
Benjy Weinberger <benjyw@gmail.com>
2014-10-24 00:16:36 +0000

    Migrate NailgunKillall and Provides to new options.
    Also fix a few bugs in Provides that cropped up while testing.
    
    Testing Done:
    Tested various flag combos.
    
    CI baking.
    
    Reviewed at https://rbcommons.com/s/twitter/r/1212/

--- src/python/pants/backend/jvm/tasks/nailgun_task.py
+++ src/python/pants/backend/jvm/tasks/nailgun_task.py
@@ -108,12 +108,11 @@ class NailgunTask(NailgunTaskBase, Task):
 class NailgunKillall(Task):
   """A task to manually kill nailguns."""
   @classmethod
-  def setup_parser(cls, option_group, args, mkflag):
-    super(NailgunKillall, cls).setup_parser(option_group, args, mkflag)
-    option_group.add_option(mkflag("everywhere"), dest="ng_killall_everywhere",
-                            default=False, action="store_true",
-                            help="[%default] Kill all nailguns servers launched by pants for "
-                                 "all workspaces on the system.")
+  def register_options(cls, register):
+    super(NailgunKillall, cls).register_options(register)
+    register('--everywhere', default=False, action='store_true',
+             legacy='ng_killall_everywhere',
+             help='Kill all nailguns servers launched by pants for all workspaces on the system.')
 
   def execute(self):
-    NailgunTaskBase.killall(everywhere=self.context.options.ng_killall_everywhere)
+    NailgunTaskBase.killall(everywhere=self.get_options().everywhere)
