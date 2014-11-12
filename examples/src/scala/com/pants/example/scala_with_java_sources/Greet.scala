bdcfc4adce080584ad39fb352fcda8f52f439ba3
Tejal Desai <tdesai@twitter.com>
2014-11-12 14:29:41 -0800

    Add java_sources to the depmap-info

---
+++ examples/src/scala/com/pants/example/scala_with_java_sources/Greet.scala
@@ -0,0 +1,13 @@
+// Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package com.pants.example.scala_with_java_sources;
+
+import com.pants.examples.hello.java_sources.GreetInterface
+
+
+object GreetEverybody {
+  def greet(everybody: String): String = {
+    return everybody
+  }
+}
