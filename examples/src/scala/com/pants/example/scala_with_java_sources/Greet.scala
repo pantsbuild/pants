b1f3301c48f3483cd95e90b775b1fee06d8cb82b
Tejal Desai <tdesai@twitter.com>
2014-11-12 16:46:09 -0800

    Add java scala dependency

--- examples/src/scala/com/pants/example/scala_with_java_sources/Greet.scala
+++ examples/src/scala/com/pants/example/scala_with_java_sources/Greet.scala
@@ -1,13 +1,14 @@
 // Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-package com.pants.example.scala_with_java_sources;
+package com.pants.examples.scala_with_java_sources;
 
-import com.pants.examples.hello.java_sources.GreetInterface
+import com.pants.examples.java_sources.Greet
 
 
 object GreetEverybody {
-  def greet(everybody: String): String = {
-    return everybody
+
+  def greetAll(everybody: Array[String]): Array[String] = {
+    everybody.map(x => Greet.greet(x))
   }
 }
