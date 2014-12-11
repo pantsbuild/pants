89590b5ce14bb415ff7fb4830458954676ef71b3
Larry Hosken <lhosken@twitter.com>
2014-12-10 16:42:15 -0800

    error in error message is errorful

--- src/python/pants/backend/python/tasks/python_setup.py
+++ src/python/pants/backend/python/tasks/python_setup.py
@@ -27,7 +27,7 @@ from pants.backend.python.tasks.python_task import PythonTask
 from pants.backend.python.thrift_builder import PythonThriftBuilder
 from pants.base.build_environment import get_buildroot
 from pants.base.config import Config
-from pants.base.exceptions import TargetDefinitionException
+from pants.base.exceptions import TargetDefinitionException, TaskError
 from pants.util.dirutil import safe_rmtree, safe_walk
 
 
@@ -404,7 +404,7 @@ class PythonSetup(PythonTask):
   def execute(self):
     targets = filter(PythonSetup.has_provides, self.context.target_roots)
     if not targets:
-      self.error('Target(s) must provide an artifact.')
+      raise TaskError('setup-py target(s) must provide an artifact.')
 
     setup_targets = OrderedSet()
     if self._recursive:
