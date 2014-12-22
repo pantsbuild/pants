8906cd9d0a858ce93a5bdd70a7aca0233ba6612f
David Turner <dturner@twopensource.com>
2014-12-22 15:08:02 -0500

    Make zinc relative its -classpath argument too.  (Again, because mesos creates long-ass paths).
    Testing Done:
    Ran new tests.  Also https://travis-ci.org/pantsbuild/pants/builds/44629688
    
    Reviewed at https://rbcommons.com/s/twitter/r/1528/

---
+++ tests/python/pants_test/backend/jvm/tasks/jvm_compile/test_zinc_utils.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
+                        print_function, unicode_literals)
+
+from mock import patch
+import os
+
+from pants.backend.jvm.tasks.jvm_compile.scala.zinc_utils import ZincUtils
+from pants_test.base_test import BaseTest
+
+
+class TestZincUtils(BaseTest):
+
+  def test_get_compile_args(self):
+    classpath = [os.path.join(self.build_root, 'foo.jar'),
+                 '/outside-build-root/bar.jar']
+    sources = ['X.scala']
+
+    args = ZincUtils._get_compile_args([], classpath, sources, 'bogus output dir',
+                                        'bogus analysis file', [])
+    classpath_found = False
+    classpath_correct = False
+    for arg in args:
+      if classpath_found:
+        self.assertEquals('foo.jar:/outside-build-root/bar.jar', arg)
+        classpath_correct = True
+        break
+      if arg == '-classpath':
+        classpath_found = True
+    self.assertTrue(classpath_correct)
