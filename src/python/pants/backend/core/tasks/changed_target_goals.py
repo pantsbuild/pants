3f27cecfdcd7d013393496d335cacbc4e7363eb1
David Taylor <tinystatemachine@gmail.com>
2014-12-26 15:15:13 +0000

    Compile/Test "changed" targets
    I think pretty much everyone has implmented some variation on this in wrappers, but with  some refactoring recently in WhatChanged and elsehwere, actual implementation of this as a proper task is
    pretty trivial now -- the comments are longer then the code.
    
    SCM changed target calculation is provided by a mixin (the same as is used by WhatChanged).
    The actual "compile" or "test" is just done by the existing goal -- this just asks round manager to schedule that.
    
    Thus "compile-changed" doesn't subclass a compile task, or even have any "compile" related functionality, but rather just asks the round manager to run "compile" for it after it replaces the target roots by consulting WhatChanged at the begining of its `prepare` method.
    
    I'm not super crazy about doing the root replacement in `prepare` -- ideally I'd prefer a dedicated method on Task that can optionally return alternative target roots. Tasks that want to find their own targets, like those in this change, would override that, then the round engine could ensure at-most-one target returned non-None and do the swap. However that currently isn't possible (discussed further here: https://docs.google.com/a/foursquare.com/document/d/1HM_vR8h5GR3JD8_yXXxoGWIieAiR5Bipdf6gwf51W3Y/edit#)
    
    Thanks to https://rbcommons.com/s/twitter/r/1543/, the approach used here should be safe though -- that change should at least ensure that if one of these tasks were mis-scheduled, it would crash and fail tests quickly, rather than potentially harming correctness more sublety later.

---
+++ src/python/pants/backend/core/tasks/changed_target_goals.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
+                        print_function, unicode_literals)
+
+from pants.backend.core.tasks.noop import (NoopExecTask, NoopCompile, NoopTest)
+from pants.backend.core.tasks.what_changed import ChangedFileTaskMixin
+
+
+class ChangedTargetTask(ChangedFileTaskMixin, NoopExecTask):
+  """A base class for tasks which find changed targets to act on.
+
+  Frequently other tasks already exist that actually do the desired work eg "compile" or "test".
+
+  A subclass of ChangedTargetTask can be used to create a new goal that runs an existing tasks
+  on what the SCM indicates are changed target rather than what the user specifies.
+
+  This is done by schduling that goal or task, unaltered, and running it as usual, with the
+  exception that context.target_roots have been first set to the SCM-derived "changed" targets.
+
+  This is achieved by this task first finding changed targets, then calling context.replace_targets,
+  before then asking the round manager to schedule the desired task or goal with a standard
+  require_data call. This is done in the prepare() method of this task, after which the run proceeds
+  as usual, though with different target roots, until finally getting to this task's execute, which
+  is a noop.
+
+  For asking the round manager to schedule a particular goal, it can be helpful to have some known
+  product_type in that goal. See noop.py and NoopExecTask for noop tasks that can easily be
+  installed in a goal to and provide some known product type.
+  """
+  @classmethod
+  def register_options(cls, register):
+    super(ChangedTargetTask, cls).register_options(register)
+    cls.register_change_file_options(register)
+
+  def prepare(self, round_manager):
+    super(ChangedTargetTask, self).prepare(round_manager)
+    changed = self._changed_targets()
+    self.context.replace_targets([self.context.build_graph.get_target(addr) for addr in changed])
+    readable = ''.join(sorted('\n\t* {}'.format(addr.reference()) for addr in changed))
+    self.context.log.info('Operating on changed {} target(s): {}'.format(len(changed), readable))
+
+
+class CompileChanged(ChangedTargetTask):
+  """Find and compile changed targets."""
+  def prepare(self, round_manager):
+    super(CompileChanged, self).prepare(round_manager)  # Replaces target roots
+    round_manager.require_data(NoopCompile.product_types()[0])
+
+
+class TestChanged(ChangedTargetTask):
+  """Find and test changed targets."""
+  def prepare(self, round_manager):
+    super(TestChanged, self).prepare(round_manager) # Replaces target roots
+    round_manager.require_data(NoopTest.product_types()[0])
