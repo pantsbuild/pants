e026e951bb62a5f91e47a7f612535fe8ce736647
John Sirois <john.sirois@gmail.com>
2015-01-16 10:42:46 -0600

    Kill leftover imports handling from early efforts.
    PythonLibrary targets have never had jar_dependencies so
    this was likely a broken code-path since inception in
    https://rbcommons.com/s/twitter/r/592/

--- src/python/pants/backend/codegen/tasks/protobuf_gen.py
+++ src/python/pants/backend/codegen/tasks/protobuf_gen.py
@@ -242,7 +242,6 @@ class ProtobufGen(CodeGen):
                                       derived_from=target,
                                       sources=genfiles,
                                       dependencies=self.pythondeps)
-    tgt.jar_dependencies.update(target.imports)
     for dependee in dependees:
       dependee.inject_dependency(tgt.address)
     return tgt
