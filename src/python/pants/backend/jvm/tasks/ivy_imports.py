9679a8d89b0078362bc1b7461043e0ee18c709a2
Benjy <benjyw@gmail.com>
2015-01-18 19:50:10 -0800

    Fixes to get CI to pass.

--- src/python/pants/backend/jvm/tasks/ivy_imports.py
+++ src/python/pants/backend/jvm/tasks/ivy_imports.py
@@ -5,11 +5,11 @@
 from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
                         print_function, unicode_literals)
 
-from pants.backend.jvm.ivy_utils import IvyUtils
+from pants.backend.jvm.tasks.ivy_task_mixin import IvyTaskMixin
 from pants.backend.jvm.tasks.nailgun_task import NailgunTask
 
 
-class IvyImports(NailgunTask):
+class IvyImports(IvyTaskMixin, NailgunTask):
   """Resolves a jar of .proto files for each target in the context which has imports (ie, for each
   JavaProtobufLibrary target).
   """
@@ -39,7 +39,6 @@ class IvyImports(NailgunTask):
 
     resolve_for = self.context.targets(lambda t: t.has_label('has_imports'))
     if resolve_for:
-      ivy_utils = IvyUtils(self.context.config, self.context.log)
       imports_map = self.context.products.get('ivy_imports')
       executor = self.create_java_executor()
       for target in resolve_for:
@@ -47,6 +46,6 @@ class IvyImports(NailgunTask):
         self.context.log.info('Mapping import jars for {target}: \n  {jars}'.format(
             target=nice_target_name(target),
             jars='\n  '.join(self._str_jar(s) for s in jars)))
-        ivy_utils.mapjars(imports_map, target, executor,
-                          workunit_factory=self.context.new_workunit,
-                          jars=jars)
+        self.mapjars(imports_map, target, executor,
+                     workunit_factory=self.context.new_workunit,
+                     jars=jars)
