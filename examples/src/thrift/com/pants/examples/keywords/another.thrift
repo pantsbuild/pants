553972156b9c650016fbdff4f2c65884dd0023bb
David Turner <dturner@twitter.com>
2015-01-23 21:13:11 -0500

    handle thrift inclusion for python in apache_thrift_gen

--- examples/src/thrift/com/pants/examples/keywords/another.thrift
+++ examples/src/thrift/com/pants/examples/keywords/another.thrift
@@ -4,12 +4,10 @@
 namespace java com.pants.examples.keywords.thriftjava
 namespace py com.pants.examples.keywords
 
-include "another.thrift"
-
 /**
  * Structure with field names consisting of python keywords.
  */
-struct Keywords {
+struct Another {
   1: optional string from;
   2: required i64 None;
 
