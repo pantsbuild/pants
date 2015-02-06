57c864d47300a3a1ab051ad03e2c2418a621c0f5
John Sirois <john.sirois@gmail.com>
2015-02-06 03:01:32 -0700

    (Un)Fix header import sorting for now.

--- tests/python/pants_test/base/test_deprecated.py
+++ tests/python/pants_test/base/test_deprecated.py
@@ -2,8 +2,8 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
-                        unicode_literals, with_statement)
+from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
+                        print_function, unicode_literals)
 
 import warnings
 from contextlib import contextmanager
