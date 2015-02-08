31629b15ca397ec00f965bd771ca0964f3a0f591
Eric Ayers <zundel@squareup.com>
2015-02-07 21:02:48 -0500

    Adding long form of help arguments to the help output
    Testing Done:
    As discussed at the Pants summit, this change modifies the existing help output to print the long form of the option.
    
    Previously, the help output printed only the short form (scoped) flags:
    
    ```
    run.jvm
      -h, --help              show this help message and exit
      --jvm-options <option>...
                              Run the jvm with these extra jvm options. (default:
                              ['-Xmx1g', '-XX:MaxPermSize=256m'])
      --args <arg>...         Run the jvm with these extra program args. (default:
                              [])
      --[no-]debug            Run the jvm under a debugger. (default: None)
      ...
    
    ```
    
    The updated output is as follows:
    
    ```
    run.jvm
      --run-jvm-help
      -h, --help              show this help message and exit
      --run-jvm-jvm-options
      --jvm-options <option>...
                              Run the jvm with these extra jvm options. (default:
                              ['-Xmx1g', '-XX:MaxPermSize=256m'])
      --run-jvm-args
      --args <arg>...         Run the jvm with these extra program args. (default:
                              [])
      --[no-]run-jvm-debug
      --[no-]debug            Run the jvm under a debugger. (default: None)
      ...
    ```
    
    Bugs closed: 1055
    
    Reviewed at https://rbcommons.com/s/twitter/r/1732/

--- src/python/pants/option/help_formatter.py
+++ src/python/pants/option/help_formatter.py
@@ -21,7 +21,31 @@ class PantsHelpFormatter(argparse.ArgumentDefaultsHelpFormatter):
     pass
 
   def start_section(self, heading):
+    self._pants_heading = heading
     pass
 
   def end_section(self):
     pass
+
+  def _pants_arg_long_format_helper(self, action):
+    if self._pants_heading:
+      heading = self._pants_heading.replace('.', '-')
+      for option_string in action.option_strings:
+        if option_string[:2] == '--':
+          if option_string[2:7] == '[no-]':
+            invert_flag = '[no-]'
+            option_flag = option_string[7:]
+          else:
+            invert_flag = ''
+            option_flag = option_string[2:]
+          return '--{invert_flag}{heading}-{option_flag}\n'.format(
+            invert_flag=invert_flag, heading=heading, option_flag=option_flag)
+
+  def add_argument(self, action):
+    """
+    Add the fully-qualified flag to the help output.
+    """
+    # NB: This uses _add_item() which is a private implementation detail of the
+    # ArgParse.HelpFormatter class.  It is subject to change in future releases of argparse.
+    self._add_item(self._pants_arg_long_format_helper, [action])
+    super(PantsHelpFormatter, self).add_argument(action)
