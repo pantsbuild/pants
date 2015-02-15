f341501d1578f347ac9a170116bb8d9ab08a6148
John Sirois <john.sirois@gmail.com>
2015-02-14 23:18:57 -0700

    check_header.sh and isort.sh fixes.

--- tests/python/pants_test/base/test_deprecated.py
+++ tests/python/pants_test/base/test_deprecated.py
@@ -2,16 +2,16 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
-                        print_function, unicode_literals)
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
 
 import warnings
 from contextlib import contextmanager
 
 import pytest
 
-from pants.base.deprecated import (deprecated, BadDecoratorNestingError, BadRemovalVersionError,
-                                   MissingRemovalVersionError, PastRemovalVersionError)
+from pants.base.deprecated import (BadDecoratorNestingError, BadRemovalVersionError,
+                                   MissingRemovalVersionError, PastRemovalVersionError, deprecated)
 from pants.version import VERSION
 
 
