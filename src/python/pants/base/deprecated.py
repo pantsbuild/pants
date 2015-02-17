1f69da2d3c423949b73c575d588cd24e753da3aa
John Sirois <john.sirois@gmail.com>
2015-02-17 11:55:05 -0700

    Introduce a @deprecated decorator.
    Deprecations will be an ongoing part of moving pants forward. This
    decorator standardizes denotation of deprecated functions by requiring a
    future version of removal be noted and by routing and displaying these
    warnings in a consistent way.
    
    Ported AbstractTarget.is_* deprecations to this new system as an
    example.
    
    Testing Done:
    Look and feel (note 1 warning instead of N and extra detail of file, line
    no and line snippet):
    ```console
    $ pants.dev compile examples/src/java/::
    ...
    02:56:25 00:00   [compile]
    02:56:25 00:00     [compile]
    02:56:25 00:00     [jvm]
    02:56:25 00:00       [jvm-compilers] WARN] /home/jsirois/dev-jsirois-pants3/src/python/pants/backend/jvm/tasks/jvm_compile/java/java_compile.py:151: DeprecationWarning:
    pants.base.target.is_apt is deprecated and will be removed in version 0.0.30:
    Do not use this method, use an isinstance check on AnnotationProcessor.
      if target.is_apt and target.processors:
    
    ...
    ```
    
    And then the new test:
    ```console
    $ PANTS_PY_COVERAGE=modules:pants.base.deprecated PANTS_DEV=1 ./pants test tests/python/pants_test/base:deprecated
    ...
    02:48:31 00:00   [test]
    02:48:31 00:00     [run_prep_command]
    02:48:31 00:00     [test]
    02:48:31 00:00     [pytest]
    02:48:31 00:00       [run]
                         ============== test session starts ===============
                         platform linux2 -- Python 2.7.8 -- py-1.4.26 -- pytest-2.6.4
                         plugins: timeout
                         collected 9 items
    
                         tests/python/pants_test/base/test_deprecated.py .........
    
                         ============ 9 passed in 0.13 seconds ============
                         Name                               Stmts   Miss Branch BrMiss  Cover
                         --------------------------------------------------------------------
                         src/python/pants/base/deprecated      32      0      8      0   100%
    
    02:48:32 00:01     [junit]
    02:48:32 00:01     [specs]
                   SUCCESS
    ```
    
    CI went green here:
      https://travis-ci.org/pantsbuild/pants/builds/50864471
    
    Bugs closed: 1048
    
    Reviewed at https://rbcommons.com/s/twitter/r/1725/

---
+++ src/python/pants/base/deprecated.py
@@ -0,0 +1,96 @@
+# coding=utf-8
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+import inspect
+import warnings
+from functools import wraps
+
+import six
+
+from pants.base.revision import Revision
+from pants.version import VERSION
+
+
+_PANTS_SEMVER = Revision.semver(VERSION)
+
+
+class DeprecationApplicationError(Exception):
+  """The base exception type thrown for any form of @deprecation application error."""
+
+
+class MissingRemovalVersionError(DeprecationApplicationError):
+  """Indicates the required removal_version was not supplied."""
+
+
+class BadRemovalVersionError(DeprecationApplicationError):
+  """Indicates the supplied removal_version was not a valid semver string."""
+
+
+class PastRemovalVersionError(DeprecationApplicationError):
+  """Indicates the supplied removal_version is not in the future.
+
+  All deprecations must give at least until the next release for users to adapt.
+  """
+
+
+class BadDecoratorNestingError(DeprecationApplicationError):
+  """Indicates the @deprecated decorator was innermost in a sequence of layered decorators."""
+
+
+def deprecated(removal_version, hint_message=None):
+  """Marks a function or method as deprecated.
+
+  A removal version must be supplied and it must be greater than the current 'pantsbuild.pants'
+  version.
+
+  When choosing a removal version there is a natural tension between the code-base, which benefits
+  from short deprecation cycles, and the user-base which may prefer to deal with deprecations less
+  frequently.  As a rule of thumb, if the hint message can fully convey corrective action
+  succinctly and you judge the impact to be on the small side (effects custom tasks as opposed to
+  effecting BUILD files), lean towards the next release version as the removal version; otherwise,
+  consider initiating a discussion to win consensus on a reasonable removal version.
+
+  :param str removal_version: The pantsbuild.pants version which will remove the deprecated
+                              function.
+  :param str hint_message: An optional hint pointing to alternatives to the deprecation.
+  :raises DeprecationApplicationError if the @deprecation is applied improperly.
+  """
+  if removal_version is None:
+    raise MissingRemovalVersionError('A removal_version must be specified for this deprecation.')
+
+  if not isinstance(removal_version, six.string_types):
+    raise BadRemovalVersionError('The removal_version must be a semver version string.')
+
+  try:
+    removal_semver = Revision.semver(removal_version)
+  except Revision.BadRevision as e:
+    raise BadRemovalVersionError('The given removal version {} is not a valid semver: '
+                                 '{}'.format(removal_version, e))
+
+  if removal_semver <= _PANTS_SEMVER:
+    raise PastRemovalVersionError('The removal version must be greater than the current pants '
+                                  'version of {} - given {}'.format(VERSION, removal_version))
+
+  def decorator(func):
+    if not inspect.isfunction(func):
+      raise BadDecoratorNestingError('The @deprecated decorator must be applied innermost of all '
+                                     'decorators.')
+
+    warning_message = ('\n{module}.{func_name} is deprecated and will be removed in version '
+                       '{removal_version}').format(module=func.__module__,
+                                                   func_name=func.__name__,
+                                                   removal_version=removal_version)
+
+    if hint_message:
+      warning_message += (':\n' + hint_message)
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+      warnings.warn(warning_message, DeprecationWarning, stacklevel=2)
+      return func(*args, **kwargs)
+    return wrapper
+  return decorator
---
+++ tests/python/pants_test/base/test_deprecated.py
@@ -0,0 +1,124 @@
+# coding=utf-8
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+import warnings
+from contextlib import contextmanager
+
+import pytest
+
+from pants.base.deprecated import (BadDecoratorNestingError, BadRemovalVersionError,
+                                   MissingRemovalVersionError, PastRemovalVersionError, deprecated)
+from pants.version import VERSION
+
+
+FUTURE_VERSION = '9999.9.9'
+
+
+@contextmanager
+def _test_deprecation():
+  with warnings.catch_warnings(record=True) as seen_warnings:
+    def assert_deprecation_warning():
+      assert len(seen_warnings) == 1
+      warning = seen_warnings[0]
+      assert isinstance(warning.message, DeprecationWarning)
+      return warning.message
+
+    warnings.simplefilter("always")
+    assert len(seen_warnings) == 0
+    yield assert_deprecation_warning
+    assert_deprecation_warning()
+
+
+def test_deprecated_function():
+  expected_return = 'deprecated_function'
+
+  @deprecated(FUTURE_VERSION)
+  def deprecated_function():
+    return expected_return
+
+  with _test_deprecation():
+    assert expected_return == deprecated_function()
+
+
+def test_deprecated_method():
+  expected_return = 'deprecated_method'
+
+  class Test(object):
+    @deprecated(FUTURE_VERSION)
+    def deprecated_method(self):
+      return expected_return
+
+  with _test_deprecation():
+    assert expected_return == Test().deprecated_method()
+
+
+def test_deprecated_property():
+  expected_return = 'deprecated_property'
+
+  class Test(object):
+    @property
+    @deprecated(FUTURE_VERSION)
+    def deprecated_property(self):
+      return expected_return
+
+  with _test_deprecation():
+    assert expected_return == Test().deprecated_property
+
+
+def test_deprecation_hint():
+  hint_message = 'Find the foos, fast!'
+  expected_return = 'deprecated_function'
+
+  @deprecated(FUTURE_VERSION, hint_message=hint_message)
+  def deprecated_function():
+    return expected_return
+
+  with _test_deprecation() as extract_deprecation_warning:
+    assert expected_return == deprecated_function()
+    assert hint_message in str(extract_deprecation_warning())
+
+
+def test_removal_version_required():
+  with pytest.raises(MissingRemovalVersionError):
+    @deprecated(None)
+    def test_func():
+      pass
+
+
+def test_removal_version_bad():
+  with pytest.raises(BadRemovalVersionError):
+    @deprecated(1.0)
+    def test_func():
+      pass
+
+  with pytest.raises(BadRemovalVersionError):
+    @deprecated('1.a.0')
+    def test_func():
+      pass
+
+
+def test_removal_version_same():
+  with pytest.raises(PastRemovalVersionError):
+    @deprecated(VERSION)
+    def test_func():
+      pass
+
+
+def test_removal_version_too_small():
+  with pytest.raises(PastRemovalVersionError):
+    @deprecated('0.0.27')
+    def test_func():
+      pass
+
+
+def test_bad_decorator_nesting():
+  with pytest.raises(BadDecoratorNestingError):
+    class Test(object):
+      @deprecated(FUTURE_VERSION)
+      @property
+      def test_prop(self):
+        pass
