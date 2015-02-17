3a5e79cfc7b737cd966b01b1dfeff5a7172d10a3
mateor <mateorod9@gmail.com>
2015-02-17 17:50:36 -0500

    Fix header issue.

--- tests/python/pants_test/android/test_android_manifest_parser.py
+++ tests/python/pants_test/android/test_android_manifest_parser.py
@@ -5,7 +5,7 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-from pants.backend.android.android_manifest_parser import AndroidManifestParser, AndroidManifest
+from pants.backend.android.android_manifest_parser import AndroidManifest, AndroidManifestParser
 from pants_test.util.test_xml_parser import TestXmlBase
 
 
