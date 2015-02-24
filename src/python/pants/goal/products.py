6977363641b60520468fee18460b45a92bd269f1
Tejal Desai <tdesai@twitter.com>
2015-02-24 12:22:33 -0800

    More tests and refactoring cases

--- src/python/pants/goal/products.py
+++ src/python/pants/goal/products.py
@@ -78,7 +78,7 @@ class RootedProducts(object):
 
 
 class MultipleRootedProducts(object):
-  """A product consisting of multiple roots, with associated file products."""
+  """A product consisting of multiple roots, with associated products."""
   def __init__(self):
     self._rooted_products_by_root = {}
 
