b944b3861bd6a2ed5bdef66b43f9848d97cdd376
Eric Ayers <zundel@squareup.com>
2015-02-25 10:55:27 -0500

    Ivy Imports now has a cache
    Add an invalidation check to Ivy Imports
    
    ```
     git diff master --stat
     examples/src/protobuf/com/pants/examples/imports/BUILD            |   7 ----
     src/python/pants/backend/codegen/tasks/protobuf_gen.py            |   7 ++++
     src/python/pants/backend/jvm/targets/BUILD                        |   1 +
     src/python/pants/backend/jvm/targets/import_jars_mixin.py         |  34 +++++++++++++++++-
     src/python/pants/backend/jvm/targets/unpacked_jars.py             |   3 --
     src/python/pants/backend/jvm/tasks/ivy_imports.py                 |  84 +++++++++++++++++++++++++++++++++++++-------
     src/python/pants/backend/jvm/tasks/ivy_task_mixin.py              |  29 ++++++++++++++-
     src/python/pants/backend/jvm/tasks/unpack_jars.py                 |   2 +-
     tests/python/pants_test/backend/__init__.py                       |   0
     tests/python/pants_test/backend/jvm/__init__.py                   |   0
     tests/python/pants_test/backend/jvm/targets/test_unpacked_jars.py |  25 +++++++++++++
     tests/python/pants_test/backend/jvm/tasks/BUILD                   |  15 ++++++++
     tests/python/pants_test/backend/jvm/tasks/__init__.py             |   0
     tests/python/pants_test/backend/jvm/tasks/jvm_compile/__init__.py |   0
     tests/python/pants_test/backend/jvm/tasks/test_ivy_imports.py     | 123 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     tests/python/pants_test/backend/jvm/tasks/test_unpack_jars.py     |  24 ++++++++-----
     16 files changed, 319 insertions(+), 35 deletions(-)
    ```
    
    Testing Done:
    Added a new unit test for ivy-imports.  Also ran this on targets in our own repo.
    
    Bugs closed: 1097, 1103
    
    Reviewed at https://rbcommons.com/s/twitter/r/1785/

--- src/python/pants/backend/jvm/targets/import_jars_mixin.py
+++ src/python/pants/backend/jvm/targets/import_jars_mixin.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
@@ -8,10 +8,17 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import six
 
 from pants.backend.jvm.targets.jar_library import JarLibrary
+from pants.base.address_lookup_error import AddressLookupError
 
 
 class ImportJarsMixin(object):
   """A Target Mixin to be used when a target declares JarLibraries to be imported."""
+
+  class UnresolvedImportError(AddressLookupError):
+    """Raised when an imported JarLibrary cannot be resolved."""
+
+  class ExpectedJarLibraryError(AddressLookupError):
+    """Raised when a target is referenced by a jar import that is not a JarLibrary."""
 
   @property
   def imported_jar_library_specs(self):
@@ -37,6 +44,31 @@ class ImportJarsMixin(object):
                                                             self.imported_jar_library_specs,
                                                             self._build_graph)
     return self._imported_jars
+
+  _imported_jar_libraries = None
+  @property
+  def imported_jar_libraries(self):
+    """:returns: target instances for specs referenced by imported_jar_library_specs.
+    :rtype: list of JarLibrary
+    """
+    if self._imported_jar_libraries is None:
+      libs = []
+      if self.imported_jar_library_specs:
+        for spec in self.imported_jar_library_specs:
+          resolved_target = self._build_graph.get_target_from_spec(spec,
+                                                                     relative_to=self.address.spec_path)
+          if not resolved_target:
+            raise self.UnresolvedImportError(
+              'Could not find JarLibrary target {spec} referenced from {relative_to}'
+              .format(spec=spec, relative_to=self.address.spec))
+          if not isinstance(resolved_target, JarLibrary):
+            raise self.ExpectedJarLibraryError(
+              'Expected JarLibrary got {target_type} for jar imports in {spec} referenced from {relative_to}'
+              .format(target_type=type(resolved_target), spec=spec,
+                      relative_to=self.address.spec))
+          libs.append(resolved_target)
+      self._imported_jar_libraries = libs
+    return self._imported_jar_libraries
 
   @property
   def traversable_specs(self):
--- tests/python/pants_test/backend/jvm/targets/test_unpacked_jars.py
+++ tests/python/pants_test/backend/jvm/targets/test_unpacked_jars.py
@@ -9,6 +9,7 @@ from textwrap import dedent
 
 from pants.backend.core.register import build_file_aliases as register_core
 from pants.backend.jvm.register import build_file_aliases as register_jvm
+from pants.backend.jvm.targets.import_jars_mixin import ImportJarsMixin
 from pants.backend.jvm.targets.jar_dependency import JarDependency
 from pants.backend.jvm.targets.unpacked_jars import UnpackedJars
 from pants.base.address import BuildFileAddress
@@ -34,6 +35,7 @@ class UnpackedJarsTest(BaseTest):
     unpacked_jars(name='foo',
       libraries=[':import_jars'],
     )
+
     jar_library(name='import_jars',
       jars=[
         jar(org='foo', name='bar', rev='123'),
@@ -50,3 +52,26 @@ class UnpackedJarsTest(BaseTest):
     self.assertEquals(1, len(target.imported_jars))
     import_jar_dep = target.imported_jars[0]
     self.assertIsInstance(import_jar_dep, JarDependency)
+
+
+  def test_bad_libraries_ref(self):
+    build_file = self.add_to_build_file('BUILD', dedent('''
+    unpacked_jars(name='foo',
+      libraries=[':wrong-type'],
+    )
+
+    unpacked_jars(name='wrong-type',
+      libraries=[':right-type'],
+    )
+
+    jar_library(name='right-type',
+      jars=[
+        jar(org='foo', name='bar', rev='123'),
+      ],
+    )
+   '''))
+    address = BuildFileAddress(build_file, 'foo')
+    self.build_graph.inject_address_closure(address)
+    target = self.build_graph.get_target(address)
+    with self.assertRaises(ImportJarsMixin.ExpectedJarLibraryError):
+      target.imported_jar_libraries
