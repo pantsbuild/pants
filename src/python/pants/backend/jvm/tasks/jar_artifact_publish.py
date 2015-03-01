a0418405002cc703c6556a787f885eb65f44f973
Tejal Desai <tdesai@twitter.com>
2015-03-01 14:11:26 -0800

    Fix imports

--- src/python/pants/backend/jvm/tasks/jar_artifact_publish.py
+++ src/python/pants/backend/jvm/tasks/jar_artifact_publish.py
@@ -2,10 +2,9 @@
 # Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
-                        print_function, unicode_literals)
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
 
-from collections import defaultdict
 import functools
 import getpass
 import hashlib
@@ -15,6 +14,7 @@ import pkgutil
 import shutil
 import sys
 import traceback
+from collections import defaultdict
 
 from twitter.common.collections import OrderedDict, OrderedSet
 from twitter.common.config import Properties
@@ -880,4 +880,3 @@ class JarArtifactPublish(JarTask, ScmPublish):
   @property
   def classifier(self):
     raise NotImplementedError('Subclasses must define classifier for the generated jar.')
-
