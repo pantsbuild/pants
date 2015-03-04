be0fac418979cd3e693f5b7fbbd4a25a03d222d9
John Sirois <john.sirois@gmail.com>
2015-03-03 22:50:58 -0700

    Fixup Context to return all synthetic targets in-play.
    Previously just synthetic targets related directly to the target
    roots were returned and this relied on synthetic targets derived from
    interior nodes being discovered in some other way.
    
    This also moves test_context.py to its proper home in pants_test/goal.

--- tests/python/pants_test/goal/test_context.py
+++ tests/python/pants_test/goal/test_context.py
@@ -5,6 +5,8 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+from pants.base.address import SyntheticAddress
+from pants.base.target import Target
 from pants_test.base_test import BaseTest
 
 
@@ -21,8 +23,8 @@ class ContextTest(BaseTest):
     d = self.make_target('d', dependencies=[c, a])
     e = self.make_target('e', dependencies=[d])
     context = self.context(target_roots=[a, b, c, d, e])
-    dependees = context.dependents(lambda t: t in set([e, c]))
-    self.assertEquals(set([c]), dependees.pop(d))
+    dependees = context.dependents(lambda t: t in {e, c})
+    self.assertEquals({c}, dependees.pop(d))
     self.assertEquals(0, len(dependees))
 
   def test_targets_order(self):
@@ -53,3 +55,21 @@ class ContextTest(BaseTest):
     self.assertEquals([a], context.targets())
     context._replace_targets([c])
     self.assertEquals([c, b, a], context.targets())
+
+  def test_targets_synthetic(self):
+    a = self.make_target('a')
+    b = self.make_target('b', dependencies=[a])
+    c = self.make_target('c', dependencies=[b])
+    d = self.make_target('d', dependencies=[c, a])
+    context = self.context(target_roots=[c])
+    self.assertEquals([c, b, a], context.targets())
+
+    syn_b = context.add_new_target(SyntheticAddress.parse('syn_b'), Target, derived_from=b)
+    context.add_new_target(SyntheticAddress.parse('syn_d'), Target, derived_from=d)
+    # We expect syn_b to be included now since it has been synthesized during this run from an
+    # in-play target.
+    self.assertEquals([c, b, a, syn_b], context.targets())
+
+    # And verify the predicate operates over both normal and synthetic targets.
+    self.assertEquals([syn_b], context.targets(lambda t: t.derived_from != t))
+    self.assertEquals([c, b, a], context.targets(lambda t: t.derived_from == t))
