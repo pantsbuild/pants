50734abbf4271d6d420456ccdcb588e4ece189fc
John Sirois <john.sirois@gmail.com>
2015-03-06 23:16:59 -0700

    Support local pre-commit checks.
    This change adds a setup script that will install or re-install a
    pre-commit hook to run pants lints.  The pre-commit script is also
    runnable directly to support a centralized stable script developers can
    run to execute all lints independent of performing a commit.

---
+++ build-support/bin/pre-commit.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# NB: pre-commit runs in the context of GIT_WORK_TREE, ie: pwd == REPO_ROOT
+
+source build-support/common.sh
+
+echo "Checking packages" && ./build-support/bin/check_packages.sh || exit 1
+echo "Checking imports" && ./build-support/bin/isort.sh || \
+  die "To fix import sort order, run \`build-support/bin/isort.sh -f\`"
+echo "Checking headers" && ./build-support/bin/check_header.sh || exit 1
+echo "Success"
