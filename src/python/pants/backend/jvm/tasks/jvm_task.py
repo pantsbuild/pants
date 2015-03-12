1057b914cd23e1b805ed81db0f32179e2c495340
Benjy <benjy@foursquare.com>
2015-03-12 22:29:05 +0000

    Remove the now-superfluous "parallel resource directories" hack.
    Also fix some missing deps for that file.

--- src/python/pants/backend/jvm/tasks/jvm_task.py
+++ src/python/pants/backend/jvm/tasks/jvm_task.py
@@ -5,11 +5,8 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-import os
-
 from pants.backend.core.tasks.task import Task
 from pants.backend.jvm.jvm_debug_config import JvmDebugConfig
-from pants.base.build_environment import get_buildroot
 from pants.util.strutil import safe_shlex_split
 
 
@@ -53,24 +50,7 @@ class JvmTask(Task):
 
   def classpath(self, targets, cp=None, confs=None):
     classpath = list(cp) if cp else []
-
     compile_classpaths = self.context.products.get_data('compile_classpath')
     compile_classpath = compile_classpaths.get_for_targets(targets)
     classpath.extend(path for conf, path in compile_classpath if not confs or conf in confs)
-
-    def add_resource_paths(predicate):
-      bases = set()
-      for target in self.context.targets():
-        if predicate(target):
-          if target.target_base not in bases:
-            sibling_resources_base = os.path.join(os.path.dirname(target.target_base), 'resources')
-            classpath.append(os.path.join(get_buildroot(), sibling_resources_base))
-            bases.add(target.target_base)
-
-    if self.context.config.getbool('jvm', 'parallel_src_paths', default=False):
-      add_resource_paths(lambda t: t.is_jvm and not t.is_test)
-
-    if self.context.config.getbool('jvm', 'parallel_test_paths', default=False):
-      add_resource_paths(lambda t: t.is_jvm and t.is_test)
-
     return classpath
