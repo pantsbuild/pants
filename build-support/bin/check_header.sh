fa4455012c18aa26dd115902fb02aa307052e696
Eric Ayers <zundel@squareup.com>
2015-03-13 05:16:30 -0700

    Added a python helper for check_header.sh in git pre-commit script.
    On my box this speeds up the header check from 5 seconds to a little over 1 second.
    
    ```
     build-support/bin/check_header.sh        | 46 ++--------------------------------------------
     build-support/bin/check_header_helper.py | 70 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     2 files changed, 72 insertions(+), 44 deletions(-)
    ```
    
    Testing Done:
    ```
    ~/Src/Pants zundel/cheader * git commit -a
    Checking packages
    Checking imports
    Checking headers
    ERROR: All .py files other than __init__.py should start with the following header:
    
    # coding=utf-8
    # Copyright YYYY Pants project contributors (see CONTRIBUTORS.md).
    # Licensed under the Apache License, Version 2.0 (see LICENSE).
    
    from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                            unicode_literals, with_statement)
    
    ---
    The following 1 file(s) do not conform:
      src/grody.py
    ```
    
    Bugs closed: 1250
    
    Reviewed at https://rbcommons.com/s/twitter/r/1910/

--- build-support/bin/check_header.sh
+++ build-support/bin/check_header.sh
@@ -1,50 +1,8 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-IFS=$'\n\t'
+set -e
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 cd ${REPO_ROOT}
 
-expected_header=$(cat <<EOF
-# coding=utf-8
-# Copyright YYYY Pants project contributors (see CONTRIBUTORS.md).
-# Licensed under the Apache License, Version 2.0 (see LICENSE).
-
-from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
-                        unicode_literals, with_statement)
-
-EOF
-)
-
-py_files=$(find src tests pants-plugins examples contrib -name '*.py' -not -name __init__.py)
-known_bad_headers=$(cat <<EOF
-tests/python/pants_test/tasks/false.py
-tests/python/pants_test/tasks/true.py
-EOF
-)
-
-bad_files=""
-for file in $py_files
-do
-  if echo "$known_bad_headers" | grep -Fx "$file" >> /dev/null
-  then
-    #known bad file, skip
-    continue
-  fi
-  header=$(head -7 "$file"|sed -e 's/20[0-9][0-9]/YYYY/' -e 's/	/        /g')
-  if [[ "$header" != "$expected_header" ]]
-  then
-    bad_files="${bad_files}"$'\n'"${file}"
-  fi
-done
-
-if [[ "x$bad_files" != "x" ]]
-then
-  echo "ERROR: All .py files other than __init__.py should start with the following header"
-  echo "$expected_header"
-  echo "---"
-  echo "The following files don't:"
-  echo "$bad_files"
-  exit 1
-fi
+build-support/bin/check_header_helper.py src tests pants-plugins examples contrib
