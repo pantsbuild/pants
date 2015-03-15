c2c41c117b418d65ae6e92ad85062c5f013d427b
Peter Seibel <pseibel@twitter.com>
2015-03-15 10:50:44 -0700

    Removing unneeded import.

--- src/python/pants/base/abbreviate_target_ids.py
+++ src/python/pants/base/abbreviate_target_ids.py
@@ -5,7 +5,7 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-from collections import Counter, defaultdict
+from collections import Counter
 from itertools import combinations
 
 
