1f6980170ef5a0d8c620fb59a86ab1d2259af0df
Nick Howard (Twitter) <nhoward@twopensource.com>
2015-03-19 17:00:22 -0600

    change set([]) / OrderedSet([]) to set() / OrderedSet()
    Clean up a few empty set constructions.
    
    Testing Done:
    ran affected tests; ci baking https://travis-ci.org/pantsbuild/pants/builds/55071411
    
    Bugs closed: 1289
    
    Reviewed at https://rbcommons.com/s/twitter/r/1947/

--- tests/python/pants_test/python/test_setup_py.py
+++ tests/python/pants_test/python/test_setup_py.py
@@ -54,7 +54,7 @@ class TestSetupPy(TaskTestBase):
     self.assertEqual(SetupPy.minified_dependencies(target_map['baz']), OrderedSet())
     self.assertEqual(SetupPy.install_requires(target_map['foo']), set(['bar==0.0.0']))
     self.assertEqual(SetupPy.install_requires(target_map['bar']), set(['baz==0.0.0']))
-    self.assertEqual(SetupPy.install_requires(target_map['baz']), set([]))
+    self.assertEqual(SetupPy.install_requires(target_map['baz']), set())
 
   @contextmanager
   def run_execute(self, target, recursive=False):
