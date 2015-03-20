5614580243207f922b7807474e9e2664679aa2c8
Tejal Desai <tejal29@gmail.com>
2015-03-20 09:42:18 -0700

    Make JavaProtobufLibrary not exportable to fix publish.
    PR: https://github.com/pantsbuild/pants/pull/1290
    
    Currently, publishing a java_library with protobuf deps is borken.
    After discussing this on pants-devel, chagning JavaProtobufLibrary to normal JVMTarget.
    
    Discussion here:
    https://groups.google.com/forum/#!topic/pants-devel/gl5Bqm9Iv_U
    
    Testing Done:
    https://travis-ci.org/pantsbuild/pants/builds/55099936
    
    Reviewed at https://rbcommons.com/s/twitter/r/1952/
    
    Conflicts:
    	src/python/pants/backend/codegen/targets/java_protobuf_library.py

--- src/python/pants/backend/codegen/targets/java_protobuf_library.py
+++ src/python/pants/backend/codegen/targets/java_protobuf_library.py
@@ -8,15 +8,16 @@ from __future__ import (nested_scopes, generators, division, absolute_import, wi
 import logging
 import six
 
-from pants.backend.jvm.targets.exportable_jvm_library import ExportableJvmLibrary
 
+from pants.backend.jvm.targets.import_jars_mixin import ImportJarsMixin
+from pants.backend.jvm.targets.jvm_target import JvmTarget
 from pants.base.payload import Payload
 from pants.base.payload_field import PrimitiveField
 
 logger = logging.getLogger(__name__)
 
 
-class JavaProtobufLibrary(ExportableJvmLibrary):
+class JavaProtobufLibrary(ImportJarsMixin, JvmTarget):
   """Generates a stub Java library from protobuf IDL files."""
 
   def __init__(self, payload=None, buildflags=None, imports=None, **kwargs):
