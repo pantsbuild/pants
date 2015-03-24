b6f4a4b3d4384d2538ed22b9582e671ba574611f
John Sirois <john.sirois@gmail.com>
2015-03-23 21:41:24 -0600

    Add a controlled repro case for a Twitter-like pants.pex.

---
+++ test-pants-pex.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-12.0.7.tar.gz && \
+tar -xzf virtualenv-12.0.7.tar.gz && \
+cd virtualenv-12.0.7 && \
+python2.7 virtualenv.py ../pex && \
+cd .. && \
+source ./pex/bin/activate && \
+pip install pex && \
+pex \
+  --no-wheel \
+  --python=python2.7 \
+  -r pantsbuild.pants==0.0.31 \
+  -r pantsbuild.pants.contrib.scrooge==0.0.31 \
+  -r twitter.common.pants==0.8.2 \
+  -r "beautifulsoup4>=4.3.2,<4.4" \
+  -e pants.bin.pants_exe:main \
+  -o pants.pex && \
+./pants.pex && \
+./pants.pex goals && \
+./pants.pex list && \
+./pants.pex compile compile testprojects/src/thrift/com/pants/::
+
