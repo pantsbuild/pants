683bb44013869916fc90f39f7bd4282c1b287c56
John Sirois <john.sirois@gmail.com>
2015-03-25 17:57:22 -0600

    Rid pantsbuild.pants of an un-needed antlr dep.
    The dep is only needed by 1 test that attempts to use the generated code
    to parse via the antlr runtime.  This change lifts the dep from the
    requirements.txt file used to bootstrap pants into a stand-alone
    python_requirement_library that the test can depend on without forcing
    each pants bootstrap to fetch this dep.
    
    Since 3rdparty/python/BUILD was used for an include in the docs, doc gen
    broke leading to fixes for CI doc gen errors.
    
    Testing Done:
    Ran `./build-support/bin/ci.sh` green locally.
    
    CI went green here:
      https://travis-ci.org/pantsbuild/pants/builds/55880777
    
    Bugs closed: 1328
    
    Reviewed at https://rbcommons.com/s/twitter/r/1989/

--- build-support/bin/release.sh
+++ build-support/bin/release.sh
@@ -39,8 +39,7 @@ PKG_PANTS_TESTINFRA=(
 )
 function pkg_pants_testinfra_install_test() {
   PIP_ARGS="$@"
-  pip install ${PIP_ARGS} pantsbuild.pants.testinfra==$(local_version) \
-    --allow-external antlr-python-runtime --allow-unverified antlr-python-runtime && \
+  pip install ${PIP_ARGS} pantsbuild.pants.testinfra==$(local_version) && \
   python -c "import pants_test"
 }
 
@@ -139,9 +138,6 @@ function install_and_test_packages() {
   PIP_ARGS=(
     "$@"
     --quiet
-
-    # Needed for now by pantsbuild.pants which any install requires.
-    --allow-external antlr-python-runtime --allow-unverified antlr-python-runtime
   )
 
   for PACKAGE in "${RELEASE_PACKAGES[@]}"
