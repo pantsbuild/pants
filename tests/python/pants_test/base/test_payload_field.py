6273e070f153ec9d7c362afcbef8604bf1cd7077
Eric Ayers <zundel@squareup.com>
2015-04-05 22:27:09 -0400

    Migrating more data to payload fields in jvm_app and jvm_binary targets
    - All fields custom to jvm_binary and jvm_app should now be reflected in the payload.
    - Added more tests to test_jvm_binary and test_jvm_app
    - Made tests for exceptions more explicit in test_jvm_app to make sure errors
      of type TargetDefinintionExeption have an address specified
    - New custom PayloadField added for JarRules
    
    Testing Done:
    Added unit tests. CI passed at: https://travis-ci.org/pantsbuild/pants/builds/56576452
    
    Bugs closed: 1349
    
    Reviewed at https://rbcommons.com/s/twitter/r/2011/

--- tests/python/pants_test/base/test_payload_field.py
+++ tests/python/pants_test/base/test_payload_field.py
@@ -5,11 +5,14 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+from hashlib import sha1
+
 from pants.backend.jvm.targets.exclude import Exclude
 from pants.backend.jvm.targets.jar_dependency import IvyArtifact, JarDependency
 from pants.backend.python.python_requirement import PythonRequirement
-from pants.base.payload_field import (ExcludesField, JarsField, PrimitiveField,
-                                      PythonRequirementsField, SourcesField)
+from pants.base.payload_field import (ExcludesField, FingerprintedField, FingerprintedMixin,
+                                      JarsField, PrimitiveField, PythonRequirementsField,
+                                      SourcesField)
 from pants_test.base_test import BaseTest
 
 
@@ -257,3 +260,32 @@ class PayloadTest(BaseTest):
           ).fingerprint()
 
     self.assertNotEqual(fp1, fp2)
+
+  def test_fingerprinted_field(self):
+    class TestValue(FingerprintedMixin):
+      def __init__(self, test_value):
+        self.test_value = test_value
+
+      def fingerprint(self):
+        hasher = sha1()
+        hasher.update(self.test_value)
+        return hasher.hexdigest()
+
+    field1 = TestValue('field1')
+    field1_same = TestValue('field1')
+    field2 = TestValue('field2')
+    self.assertEquals(field1.fingerprint(), field1_same.fingerprint())
+    self.assertNotEquals(field1.fingerprint(), field2.fingerprint())
+
+    fingerprinted_field1 = FingerprintedField(field1)
+    fingerprinted_field1_same = FingerprintedField(field1_same)
+    fingerprinted_field2 = FingerprintedField(field2)
+    self.assertEquals(fingerprinted_field1.fingerprint(), fingerprinted_field1_same.fingerprint())
+    self.assertNotEquals(fingerprinted_field1.fingerprint(), fingerprinted_field2.fingerprint())
+
+  def test_unimplemented_fingerprinted_field(self):
+    class TestUnimplementedValue(FingerprintedMixin):
+      pass
+
+    with self.assertRaises(NotImplementedError):
+      FingerprintedField(TestUnimplementedValue()).fingerprint()
