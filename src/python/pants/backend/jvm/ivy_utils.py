a1ce39bfa437ab4537fcb6af498a66539aac5f5a
David Turner <dturner@twopensource.com>
2015-04-13 22:54:53 -0400

    Classpath isolation: make ivy resolution locally accurate.
    In the global compilation strategy, the 'compile_classpath' product
    contains a list of artifacts for each target.  This list need not be
    accurate per-target so long as the total set of artifacts required for
    the build is included somewhere in the product.  In ivy resolution, a
    target T might request library L, version 1.0.  If some other target U
    in the build brings in L 2.0, Ivy might not resolve L 1.0's
    dependencies.  So, prior to this patch, the compile_classpath for T
    would show L, but not L's dependencies.
    
    In the isolated compilation strategy, compile_classpath must be
    accurate per-target, since targets will be built independently.  So
    if T were missing L's dependencies, T might not be buildable.
    
    This patch solves this problem by ignoring version numbers when
    retrieving dependencies, so that the resolved version can replace
    the requested version in all cases.
    
    Note that ivy resolution is still not 100% isolated; if T is built in
    the same compile as U, it will be built against L 2.0, while if it is
    built alone, it will be built against L 1.0.
    
    Testing Done:
    CI green: https://travis-ci.org/pantsbuild/pants/builds/58371122
    
    Reviewed at https://rbcommons.com/s/twitter/r/2064/

--- src/python/pants/backend/jvm/ivy_utils.py
+++ src/python/pants/backend/jvm/ivy_utils.py
@@ -27,13 +27,37 @@ from pants.ivy.bootstrapper import Bootstrapper
 from pants.util.dirutil import safe_mkdir, safe_open
 
 
-IvyModuleRef = namedtuple('IvyModuleRef', ['org', 'name', 'rev'])
 IvyArtifact = namedtuple('IvyArtifact', ['path', 'classifier'])
 IvyModule = namedtuple('IvyModule', ['ref', 'artifacts', 'callers'])
 
 
 logger = logging.getLogger(__name__)
 
+
+class IvyModuleRef(object):
+  def __init__(self, org, name, rev):
+    self.org = org
+    self.name = name
+    self.rev = rev
+
+  def __eq__(self, other):
+    return self.org == other.org and self.name == other.name and self.rev == other.rev
+
+  def __hash__(self):
+    return hash((self.org, self.name, self.rev))
+
+  @property
+  def unversioned(self):
+    """This returns an identifier for an IvyModuleRef without version information.
+
+       It's useful because ivy might return information about a
+       different version of a dependency than the one we request, and we
+       want to ensure that all requesters of any version of that
+       dependency are able to learn about it.
+    """
+
+    # latest.integration is ivy magic meaning "just get the latest version"
+    return IvyModuleRef(name=self.name, org=self.org, rev='latest.integration')
 
 class IvyInfo(object):
   def __init__(self):
@@ -45,11 +69,12 @@ class IvyInfo(object):
 
   def add_module(self, module):
     self.modules_by_ref[module.ref] = module
+    if not module.artifacts:
+      # Module was evicted, so do not record information about it
+      return
     for caller in module.callers:
-      self._deps_by_caller[caller].add(module.ref)
-    # Strip the version from the ref before recording artifacts.
-    unversioned_ref = IvyModuleRef(module.ref.org, module.ref.name, "")
-    self._artifacts_by_ref[unversioned_ref].update(module.artifacts)
+      self._deps_by_caller[caller.unversioned].add(module.ref)
+    self._artifacts_by_ref[module.ref.unversioned].update(module.artifacts)
 
   def traverse_dependency_graph(self, ref, collector, memo=None, visited=None):
     """Traverses module graph, starting with ref, collecting values for each ref into the sets
@@ -79,7 +104,7 @@ class IvyInfo(object):
     visited.add(ref)
 
     acc = collector(ref)
-    for dep in self._deps_by_caller.get(ref, ()):
+    for dep in self._deps_by_caller.get(ref.unversioned, ()):
       acc.update(self.traverse_dependency_graph(dep, collector, memo, visited))
     memo[ref] = acc
     return acc
@@ -104,9 +129,8 @@ class IvyInfo(object):
       valid_classifiers = jar.artifact_classifiers
       artifacts_for_jar = []
       for module_ref in self.traverse_dependency_graph(jar_module_ref, create_collection, memo):
-        unversioned_ref = IvyModuleRef(module_ref.org, module_ref.name, "")
         artifacts_for_jar.extend(
-          artifact for artifact in self._artifacts_by_ref[unversioned_ref]
+          artifact for artifact in self._artifacts_by_ref[module_ref.unversioned]
           if artifact.classifier in valid_classifiers
         )
 
