145354983194ea91c2140e40e7f15d98ec650445
David Turner <dturner@twitter.com>
2015-04-17 18:12:40 -0400

    fix git test on travis

--- tests/python/pants_test/backend/project_info/tasks/test_export_integration.py
+++ tests/python/pants_test/backend/project_info/tasks/test_export_integration.py
@@ -66,6 +66,8 @@ class ExportIntegrationTest(ConsoleTaskTestBase):
 
     try:
       with pushd(self.build_root):
+        subprocess.check_call(['git', 'config', 'user.email', 'you@example.com'])
+        subprocess.check_call(['git', 'config', 'user.name', 'Your Name'])
         subprocess.check_call(['git', 'init'])
         subprocess.check_call(['git', 'add', '.'])
         subprocess.check_call(['git', 'commit', '-m' 'initial commit'])
