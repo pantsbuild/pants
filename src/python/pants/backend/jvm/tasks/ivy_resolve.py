3f0ee0371d5117ddcd899e4272f5cf8eaa8492f4
Eric Ayers <zundel@squareup.com>
2015-04-28 10:39:59 -0400

    Added support for the ivy cache being under a symlink'ed dir
    Addresses issue from discussion in https://groups.google.com/forum/#!topic/pants-devel/dhnRICX-I3g
    
    Testing Done:
    CI running at: https://travis-ci.org/pantsbuild/pants/builds/60273133
    
    Besides adding the unit test, used the repro case from https://rbcommons.com/s/twitter/r/2085/ to compile a target after moving the cache to a symlink:
    
    ```
    mkdir ~/realcachedir
    ln -s ~/realcachedir ~/symlinkcachedir
    ```
    
    In `pants.ini` added:
    
    ```
    [ivy]
    cache_dir: /Users/Zundel/symlinkcachedir
    ```
    
    Then compiled something that required ivy resolution.
    
    ```
    ./pants compile examples/src/java/org/pantsbuild/example/antlr3
    ```
    
    Bugs closed: 1458
    
    Reviewed at https://rbcommons.com/s/twitter/r/2129/

--- src/python/pants/backend/jvm/tasks/ivy_resolve.py
+++ src/python/pants/backend/jvm/tasks/ivy_resolve.py
@@ -146,7 +146,11 @@ class IvyResolve(IvyTaskMixin, NailgunTask):
         # Add the artifacts from each dependency module.
         artifact_paths = []
         for artifact in ivy_info.get_artifacts_for_jar_library(target, memo=ivy_jar_memo):
-          artifact_paths.append(symlink_map[artifact.path])
+          if artifact.path in symlink_map:
+            key = artifact.path
+          else:
+            key = os.path.realpath(artifact.path)
+          artifact_paths.append(symlink_map[key])
         compile_classpath.add_for_target(target, [(conf, entry) for entry in artifact_paths])
 
     if self._report:
