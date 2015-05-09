14dfd3b71ba46c448a4070a259725fede54ebc7f
John Sirois <john.sirois@gmail.com>
2015-05-08 20:03:56 -0600

    Fixup ArtifactTest.

--- tests/python/pants_test/jvm/test_artifact.py
+++ tests/python/pants_test/jvm/test_artifact.py
@@ -5,7 +5,7 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-from pants.backend.jvm.artifact import Artifact
+from pants.backend.jvm.artifact import Artifact, PublicationMetadata
 from pants.backend.jvm.repository import Repository
 from pants_test.base_test import BaseTest
 
@@ -16,16 +16,22 @@ class ArtifactTest(BaseTest):
     repo = Repository(name="myRepo",
                       url="myUrl",
                       push_db_basedir="myPushDb")
-    Artifact(org="testOrg", name="testName", repo=repo, description="Test")
+
+    class TestPublicationMetadata(PublicationMetadata):
+      def _compute_fingerprint(self):
+        return None
+    metadata = TestPublicationMetadata()
+
+    Artifact(org="testOrg", name="testName", repo=repo, publication_metadata=metadata)
 
     with self.assertRaises(ValueError):
-      Artifact(org=1, name="testName", repo=repo, description="Test")
+      Artifact(org=1, name="testName", repo=repo, publication_metadata=metadata)
 
     with self.assertRaises(ValueError):
-      Artifact(org="testOrg", name=1, repo=repo, description="Test")
+      Artifact(org="testOrg", name=1, repo=repo, publication_metadata=metadata)
 
     with self.assertRaises(ValueError):
-      Artifact(org="testOrg", name="testName", repo=1, description="Test")
+      Artifact(org="testOrg", name="testName", repo=1, publication_metadata=metadata)
 
     with self.assertRaises(ValueError):
-      Artifact(org="testOrg", name="testName", repo=repo, description=1)
+      Artifact(org="testOrg", name="testName", repo=repo, publication_metadata=1)
