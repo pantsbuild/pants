653d78626e00c3323031b8b2dfc30a0cb997223c
Tom Howland <thowland@twopensource.com>
2015-05-14 12:23:17 -0700

    sort classifiers
    This bug sneaked by because scaladoc isn't enabled in Travis and consequently
    the test that would have caught it wasn't run.
    
    See https://github.com/pantsbuild/pants/issues/1545
    
    Testing Done:
    Added a unit test that will actually run in Travis:
    
    Travis passes: https://travis-ci.org/pantsbuild/pants/builds/62568880
    
    Reviewed at https://rbcommons.com/s/twitter/r/2218/

--- src/python/pants/backend/jvm/tasks/jar_publish.py
+++ src/python/pants/backend/jvm/tasks/jar_publish.py
@@ -241,7 +241,10 @@ class PomWriter(DependencyWriter):
     if classifier:
       s.add(classifier)
     s.update([i.classifier for i in artifacts if i.classifier])
-    return map(lambda x: TemplateData(classifier=x), s)
+
+    # We sort this for testing: having a test fail because the order changed isn't helpful.
+
+    return map(lambda x: TemplateData(classifier=x), sorted(s))
 
   def internaldep(self, jar_dependency, target, configurations=None, classifier=None):
     template_data = self.jardep(jar_dependency, classifier=classifier)
