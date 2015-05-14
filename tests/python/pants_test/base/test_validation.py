3283ef9c93a85d2c34cc5fdfad04b3ad25869acb
Simeon Franklin <sfranklin@twitter.com>
2015-05-14 13:39:55 -0700

    checkstyle improvements to the test for validation

--- tests/python/pants_test/base/test_validation.py
+++ tests/python/pants_test/base/test_validation.py
@@ -5,15 +5,12 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-import os
-import unittest
-from contextlib import contextmanager
-
 import pytest
 
 from pants.base import validation
 
 assert_list = validation.assert_list
+
 
 class FunctionCounter(object):
   def __init__(self, func):
@@ -23,18 +20,20 @@ class FunctionCounter(object):
   def __call__(self, *args, **kwargs):
     self.counter += 1
     return self.func(*args, **kwargs)
-    
+
 
 def test_valid_inputs():
   # list of strings gives list of strings
   assert assert_list(["file1.txt", "file2.txt"]) == ["file1.txt", "file2.txt"]
   assert assert_list(None) == []  # None is ok by default
 
+
 def test_invalid_inputs():
   with pytest.raises(ValueError):
-    assert_list({"file2.txt": True}) # Can't pass a dict by default
+    assert_list({"file2.txt": True})  # Can't pass a dict by default
   with pytest.raises(ValueError):
-     assert_list([["file2.txt"], "file2.txt"]) # Can't a list of non-string values
+    assert_list([["file2.txt"], "file2.txt"])  # Can't a list of non-string values
+
 
 def test_lazyness(monkeypatch):
   mock = FunctionCounter(isinstance)
@@ -44,5 +43,3 @@ def test_lazyness(monkeypatch):
   mock.counter = 0
   assert_list(set(), allowable=(list, set))
   assert mock.counter == 2  # isinstance was called twice
-  
-  
