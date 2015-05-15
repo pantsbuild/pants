7a613c8b087016cf35ba05fea26571c80b42e1c3
Simeon Franklin <sfranklin@twitter.com>
2015-05-15 16:00:56 -0700

    No need for laziness test

--- tests/python/pants_test/base/test_validation.py
+++ tests/python/pants_test/base/test_validation.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
@@ -7,20 +7,7 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 
 import pytest
 
-from pants.base import validation
-
-
-assert_list = validation.assert_list
-
-
-class FunctionCounter(object):
-  def __init__(self, func):
-    self.func = func
-    self.counter = 0
-
-  def __call__(self, *args, **kwargs):
-    self.counter += 1
-    return self.func(*args, **kwargs)
+from pants.base.validation import assert_list
 
 
 def test_valid_inputs():
@@ -33,14 +20,4 @@ def test_invalid_inputs():
   with pytest.raises(ValueError):
     assert_list({"file2.txt": True})  # Can't pass a dict by default
   with pytest.raises(ValueError):
-    assert_list([["file2.txt"], "file2.txt"])  # Can't a list of non-string values
-
-
-def test_lazyness(monkeypatch):
-  mock = FunctionCounter(isinstance)
-  monkeypatch.setattr(validation, 'isinstance', mock, raising=False)  # Create new module level var
-  assert_list([], allowable=(list, set))
-  assert mock.counter == 1  # isinstance was only called once despite allowable having two types
-  mock.counter = 0
-  assert_list(set(), allowable=(list, set))
-  assert mock.counter == 2  # isinstance was called twice
+    assert_list([["file2.txt"], "file2.txt"])  # All values in list must be stringy values
