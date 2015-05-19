458108b8e34e773dc605f22515d8b9e255279a80
John Sirois <john.sirois@gmail.com>
2015-05-19 00:08:28 -0600

    Find java distributions in well known locations.
    Both OSX and linux have well-known tools and conventions for discovering
    java distributions. This change adds support for scanning the
    `/usr/lib/jvm` dir on linux and using the `/usr/libexec/java_home` tool
    on OSX along with test coverage for both.
    
    In the course of adding new tests, old tests are refactored and, in
    particular, the monolithic locate test is broken apart into tightly
    scoped tests.

--- src/python/pants/java/distribution/distribution.py
+++ src/python/pants/java/distribution/distribution.py
@@ -8,7 +8,9 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import logging
 import os
 import pkgutil
+import plistlib
 import subprocess
+from collections import namedtuple
 from contextlib import contextmanager
 
 from six import string_types
@@ -58,6 +60,71 @@ class Distribution(object):
       cls._CACHE[key] = dist
     return dist
 
+  class _Location(namedtuple('Location', ['home_path', 'bin_path'])):
+    """Represents the location of a java distribution."""
+    @classmethod
+    def from_home(cls, home):
+      """Creates a location given the JAVA_HOME directory.
+
+      :param string home: The path of the JAVA_HOME directory.
+      :returns: The java distribution location.
+      """
+      return cls(home_path=home, bin_path=None)
+
+    @classmethod
+    def from_bin(cls, bin_path):
+      """Creates a location given the `java` executable parent directory.
+
+      :param string bin_path: The parent path of the `java` executable.
+      :returns: The java distribution location.
+      """
+      return cls(home_path=None, bin_path=bin_path)
+
+  # The `/usr/lib/jvm` dir is a common target of packages built for redhat and debian as well as
+  # other more exotic distributions.
+  _JAVA_DIST_DIR = '/usr/lib/jvm'
+
+  @classmethod
+  def _linux_java_homes(cls):
+    if os.path.isdir(cls._JAVA_DIST_DIR):
+      for path in os.listdir(cls._JAVA_DIST_DIR):
+        home = os.path.join(cls._JAVA_DIST_DIR, path)
+        if os.path.isdir(home):
+          yield cls._Location.from_home(home)
+
+  _OSX_JAVA_HOME_EXE = '/usr/libexec/java_home'
+
+  @classmethod
+  def _osx_java_homes(cls):
+    # OSX will have a java_home tool that can be used to locate a unix-compatible java home dir.
+    #
+    # See:
+    #   https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man1/java_home.1.html
+    #
+    # The `--xml` output looks like so:
+    # <?xml version="1.0" encoding="UTF-8"?>
+    # <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+    #                        "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    # <plist version="1.0">
+    #   <array>
+    #     <dict>
+    #       ...
+    #       <key>JVMHomePath</key>
+    #       <string>/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home</string>
+    #       ...
+    #     </dict>
+    #     ...
+    #   </array>
+    # </plist>
+    if os.path.exists(cls._OSX_JAVA_HOME_EXE):
+      try:
+        plist = subprocess.check_output([cls._OSX_JAVA_HOME_EXE, '--failfast', '--xml'])
+        for distribution in plistlib.readPlistFromString(plist):
+          home = distribution['JVMHomePath']
+          yield cls._Location.from_home(home)
+      except subprocess.CalledProcessError:
+        pass
+
   @classmethod
   def locate(cls, minimum_version=None, maximum_version=None, jdk=False):
     """Finds a java distribution that meets any given constraints and returns it.
@@ -65,22 +132,34 @@ class Distribution(object):
     First looks in JDK_HOME and JAVA_HOME if defined falling back to a search on the PATH.
     Raises Distribution.Error if no suitable java distribution could be found.
     """
-    def home_bin_path(home_env_var):
+    def env_home(home_env_var):
       home = os.environ.get(home_env_var)
-      return os.path.join(home, 'bin') if home else None
+      return cls._Location.from_home(home) if home else None
 
     def search_path():
-      yield home_bin_path('JDK_HOME')
-      yield home_bin_path('JAVA_HOME')
-      path = os.environ.get('PATH')
-      if path:
-        for p in path.strip().split(os.pathsep):
-          yield p
+      yield env_home('JDK_HOME')
+      yield env_home('JAVA_HOME')
 
-    for path in filter(None, search_path()):
+      for location in cls._linux_java_homes():
+        yield location
+
+      # OSX has non-standard on-disk layouts for jdks that can lead to trouble unless a special
+      # tool is used to locate the standard-layout dir.
+      for location in cls._osx_java_homes():
+        yield location
+
+      search_path = os.environ.get('PATH')
+      if search_path:
+        for bin_path in search_path.strip().split(os.pathsep):
+          yield cls._Location.from_bin(bin_path)
+
+    for location in filter(None, search_path()):
       try:
-        dist = cls(bin_path=path, minimum_version=minimum_version,
-                   maximum_version=maximum_version, jdk=jdk)
+        dist = cls(home_path=location.home_path,
+                   bin_path=location.bin_path,
+                   minimum_version=minimum_version,
+                   maximum_version=maximum_version,
+                   jdk=jdk)
         dist.validate()
         logger.debug('Located {} for constraints: minimum_version {}, maximum_version {}, jdk {}'
                      .format(dist, minimum_version, maximum_version, jdk))
@@ -97,9 +176,12 @@ class Distribution(object):
     #  http://www.oracle.com/technetwork/java/javase/versioning-naming-139433.html
     # These version strings comply with semver except that the traditional pre-release semver
     # slot (the 4th) can be delimited by an _ in the case of update releases of the jdk.
-    # We accommodate that difference here.
+    # We accommodate that difference here using lenient parsing.
+    # We also accommodate specification versions, which just have major and minor
+    # components; eg: `1.8`.  These are useful when specifying constraints a distribution must
+    # satisfy; eg to pick any 1.8 java distribution: '1.8' <= version <= '1.8.99'
     if isinstance(version, string_types):
-      version = Revision.semver(version.replace('_', '-'))
+      version = Revision.lenient(version)
     if version and not isinstance(version, Revision):
       raise ValueError('{} must be a string or a Revision object, given: {}'.format(name, version))
     return version
@@ -108,25 +190,32 @@ class Distribution(object):
   def _is_executable(path):
     return os.path.isfile(path) and os.access(path, os.X_OK)
 
-  def __init__(self, bin_path='/usr/bin', minimum_version=None, maximum_version=None, jdk=False):
+  def __init__(self, home_path=None, bin_path=None, minimum_version=None, maximum_version=None,
+               jdk=False):
     """Creates a distribution wrapping the given bin_path.
 
-    :param string bin_path: the path to the java distributions bin dir
+    :param string home_path: the path to the java distribution's home dir
+    :param string bin_path: the path to the java distribution's bin dir
     :param minimum_version: a modified semantic version string or else a Revision object
     :param maximum_version: a modified semantic version string or else a Revision object
     :param bool jdk: ``True`` to require the distribution be a JDK vs a JRE
     """
+    if home_path and not os.path.isdir(home_path):
+      raise ValueError('The specified java home path is invalid: {}'.format(home_path))
+    if bin_path and not os.path.isdir(bin_path):
+      raise ValueError('The specified binary path is invalid: {}'.format(bin_path))
+    if home_path and bin_path and not bin_path.startswith(home_path):
+      raise ValueError('The specified binary path {} is not inside java home {}'
+                       .format(bin_path, home_path))
 
-    if not os.path.isdir(bin_path):
-      raise ValueError('The specified distribution path is invalid: {}'.format(bin_path))
-    self._bin_path = bin_path
+    self._home = home_path
+    self._bin_path = bin_path or (os.path.join(home_path, 'bin') if home_path else '/usr/bin')
 
     self._minimum_version = self._parse_java_version("minimum_version", minimum_version)
     self._maximum_version = self._parse_java_version("maximum_version", maximum_version)
 
     self._jdk = jdk
 
-    self._home = None
     self._is_jdk = False
     self._system_properties = None
     self._version = None
