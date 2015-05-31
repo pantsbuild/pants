253180ecebfd047aeb51c417c5d672df725dba8f
mateor <mateorod9@gmail.com>
2015-05-31 18:38:16 -0500

    Add pants header to assorted python files
    Add header to all eligible python files. These files are not usually
    subject to the header check script. I only added the copyright to
    the check_header_helper.py script - it uses python 2.7 explicitly
    so adding the futures was unhelpful.

--- build-support/bin/check_header_helper.py
+++ build-support/bin/check_header_helper.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2.7
-#
-# Helper for check_headers.sh to check .py files in the repo to see if they start with the
+# coding=utf-8
+# Copyright YYYY Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+#  Helper for check_headers.sh to check .py files in the repo to see if they start with the
 # appropriate headers.
 #
 # usage: check_header_helper.py dir1 [ dir2 [ ... ] ]
--- migrations/0.0.24/src/python/publish_migration.py
+++ migrations/0.0.24/src/python/publish_migration.py
@@ -3,6 +3,10 @@
 # Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+
 import os
 import re
 import sys
