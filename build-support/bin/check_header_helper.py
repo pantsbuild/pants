7c489cab5e329df5185a5f2289c01094fafa4884
mateor <mateorod9@gmail.com>
2015-05-31 18:45:01 -0500

    Adjust the year on check_header_helper.py

--- build-support/bin/check_header_helper.py
+++ build-support/bin/check_header_helper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 # coding=utf-8
-# Copyright YYYY Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 #  Helper for check_headers.sh to check .py files in the repo to see if they start with the
