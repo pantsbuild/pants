c1dd79297d8b99a8ad0a3ec77f2741eabf1c41f4
Andy Reitz <ajr9@po.cwru.edu>
2015-06-08 14:27:34 -0700

    rm superfluous if

--- src/python/pants/backend/jvm/tasks/junit_run.py
+++ src/python/pants/backend/jvm/tasks/junit_run.py
@@ -759,12 +759,11 @@ class JUnitRun(JvmTask, JvmToolTaskMixin):
   def execute(self):
     if not self.get_options().skip:
       targets = self.context.targets()
-      if targets:
-        # TODO: move this check to an optional phase in goal_runner, so
-        # that missing sources can be detected early.
-        for target in targets:
-          if isinstance(target, junit_tests) and not  target.payload.sources.source_paths:
-            msg = 'JavaTests target {} must include a non-empty set of sources.'.format(target.address.spec)
-            raise TargetDefinitionException(target, msg)
+      # TODO: move this check to an optional phase in goal_runner, so
+      # that missing sources can be detected early.
+      for target in targets:
+        if isinstance(target, junit_tests) and not  target.payload.sources.source_paths:
+          msg = 'JavaTests target {} must include a non-empty set of sources.'.format(target.address.spec)
+          raise TargetDefinitionException(target, msg)
 
-        self._runner.execute(targets)
+      self._runner.execute(targets)
