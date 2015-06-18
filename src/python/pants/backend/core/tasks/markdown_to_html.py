aa3d71f151ae6a2d8d6c0610422c64c0225881d9
John Sirois <john.sirois@gmail.com>
2015-06-18 17:39:42 -0600

    Bad rst now fails the MarkdownToHtml task.
    There is an option to turn the task failure into a logged warning, but
    the underlying failures encountered by docutils while rendering rst to
    html are now detected.
    
    Tests are moved to their proper home and new coverage added for the rst
    rendering.

--- src/python/pants/backend/core/tasks/markdown_to_html.py
+++ src/python/pants/backend/core/tasks/markdown_to_html.py
@@ -8,6 +8,7 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import codecs
 import os
 import re
+import sys
 
 import markdown
 from docutils.core import publish_parts
@@ -26,6 +27,7 @@ from pants.base.address import SyntheticAddress
 from pants.base.build_environment import get_buildroot
 from pants.base.exceptions import TaskError
 from pants.base.generator import Generator
+from pants.base.workunit import WorkUnit
 from pants.util.dirutil import safe_mkdir, safe_open
 
 
@@ -36,6 +38,7 @@ def emit_codehighlight_css(path, style):
 
 
 WIKILINKS_PATTERN = r'\[\[([^\]]+)\]\]'
+
 
 class WikilinksPattern(markdown.inlinepatterns.Pattern):
   def __init__(self, build_url, markdown_instance=None):
@@ -57,6 +60,7 @@ class WikilinksExtension(markdown.Extension):
 
   def extendMarkdown(self, md, md_globals):
     md.inlinePatterns['wikilinks'] = WikilinksPattern(self.build_url, md)
+
 
 # !inc[start-at=void main&end-before=private HelloMain](HelloMain.java)
 INCLUDE_PATTERN = r'!inc(\[(?P<params>[^]]*)\])?\((?P<path>[^' + '\n' + r']*)\)'
@@ -125,10 +129,10 @@ def choose_include_text(s, params, source_path):
 
 
 class IncludeExcerptPattern(markdown.inlinepatterns.Pattern):
-  def __init__(self, md=None, source_path=None):
-    '''
-    :param source_path: path to source .md file.
-    '''
+  def __init__(self, source_path=None):
+    """
+    :param string source_path: Path to source `.md` file.
+    """
     markdown.inlinepatterns.Pattern.__init__(self, INCLUDE_PATTERN)
     self.source_path = source_path
 
@@ -180,24 +184,44 @@ class IncludeExcerptExtension(markdown.Extension):
 
 
 def page_to_html_path(page):
-  "Given a page target, return partial path for an output .html"
+  """Given a page target, return partial path for an output `.html`."""
   source_path = page.sources_relative_to_buildroot()[0]
   return os.path.splitext(source_path)[0] + ".html"
 
 
-def rst_to_html(in_rst):
-  """Returns HTML rendering of an RST fragment.
+def rst_to_html(in_rst, stderr):
+  """Renders HTML from an RST fragment.
 
-  :param in_rst: rst-formatted string
+  :param string in_rst: An rst formatted string.
+  :param stderr: An open stream to use for docutils stderr output.
+  :returns: A tuple of (html rendered rst, return code)
   """
   if not in_rst:
-    return ''
-  pp = publish_parts(in_rst, writer_name='html')
+    return '', 0
+
+  # Unfortunately, docutils is really setup for command line use.
+  # We're forced to patch the bits of sys its hardcoded to use so that we can call it in-process
+  # and still reliably determine errors.
+  # TODO(John Sirois): Move to a subprocess execution model utilizing a docutil chroot/pex.
+  orig_sys_exit = sys.exit
+  orig_sys_stderr = sys.stderr
+  returncodes = []
+  try:
+    sys.exit = returncodes.append
+    sys.stderr = stderr
+    pp = publish_parts(in_rst,
+                       writer_name='html',
+                       settings_overrides=dict(exit_status_level=1),
+                       enable_exit_status=True)
+  finally:
+    sys.exit = orig_sys_exit
+    sys.stderr = orig_sys_stderr
+
   return_value = ''
   if 'title' in pp and pp['title']:
     return_value += '<title>{0}</title>\n<p style="font: 200% bold">{0}</p>\n'.format(pp['title'])
   return_value += pp['body'].strip()
-  return return_value
+  return return_value, returncodes.pop() if returncodes else 0
 
 
 class MarkdownToHtml(Task):
@@ -210,6 +234,8 @@ class MarkdownToHtml(Task):
              help='Open the generated documents in a browser.')
     register('--fragment', action='store_true',
              help='Generate a fragment of html to embed in a page.')
+    register('--ignore-failure', default=False, action='store_true',
+             help='Do not consider rendering errors to be build errors.')
 
   @classmethod
   def product_types(cls):
@@ -247,60 +273,58 @@ class MarkdownToHtml(Task):
         if not page.dependencies and page not in interior_nodes:
           roots.add(page)
 
-    plaingenmap = self.context.products.get('markdown_html')
-    wikigenmap = self.context.products.get('wiki_html')
-    show = []
-    for page in self.context.targets(is_page):
-      def process_page(key, outdir, url_builder, config, genmap, fragment=False):
-        if page.format == 'rst':
-          html_path = self.process_rst(
-            os.path.join(outdir, page_to_html_path(page)),
-            os.path.join(page.payload.sources.rel_path, page.source),
-            self.fragment or fragment,
-          )
-        else:
-          html_path = self.process_md(
-            os.path.join(outdir, page_to_html_path(page)),
-            os.path.join(page.payload.sources.rel_path, page.source),
-            self.fragment or fragment,
-            url_builder,
-            config,
-            css=css
-          )
-        self.context.log.info('Processed {} to {}'.format(page.source, html_path))
-        relpath = os.path.relpath(html_path, outdir)
-        genmap.add(key, outdir, [relpath])
-        return html_path
+    with self.context.new_workunit(name='render', labels=[WorkUnit.MULTITOOL]):
+      plaingenmap = self.context.products.get('markdown_html')
+      wikigenmap = self.context.products.get('wiki_html')
+      show = []
+      for page in self.context.targets(is_page):
+        def process_page(key, outdir, url_builder, genmap, fragment=False):
+          if page.format == 'rst':
+            with self.context.new_workunit(name='rst') as workunit:
+              html_path = self.process_rst(
+                workunit,
+                page,
+                os.path.join(outdir, page_to_html_path(page)),
+                os.path.join(page.payload.sources.rel_path, page.source),
+                self.fragment or fragment,
+              )
+          else:
+            with self.context.new_workunit(name='md'):
+              html_path = self.process_md(
+                os.path.join(outdir, page_to_html_path(page)),
+                os.path.join(page.payload.sources.rel_path, page.source),
+                self.fragment or fragment,
+                url_builder,
+                css=css,
+              )
+          self.context.log.info('Processed {} to {}'.format(page.source, html_path))
+          relpath = os.path.relpath(html_path, outdir)
+          genmap.add(key, outdir, [relpath])
+          return html_path
 
-      def url_builder(linked_page, config=None):
-        dest = page_to_html_path(linked_page)
-        src_dir = os.path.dirname(page_to_html_path(page))
-        return linked_page.name, os.path.relpath(dest, src_dir)
+        def url_builder(linked_page):
+          dest = page_to_html_path(linked_page)
+          src_dir = os.path.dirname(page_to_html_path(page))
+          return linked_page.name, os.path.relpath(dest, src_dir)
 
-      page_path = os.path.join(outdir, 'html')
-      html = process_page(page, page_path, url_builder, lambda p: None, plaingenmap)
-      if css and not self.fragment:
-        plaingenmap.add(page, self.workdir, list(css_path))
-      if self.open and page in roots:
-        show.append(html)
+        page_path = os.path.join(outdir, 'html')
+        html = process_page(page, page_path, url_builder, plaingenmap)
+        if css and not self.fragment:
+          plaingenmap.add(page, self.workdir, list(css_path))
+        if self.open and page in roots:
+          show.append(html)
 
-      if page.provides:
-        for wiki in page.provides:
-          def get_config(page):
-            # Take the first provided WikiArtifact. If a page is published to multiple places, it's
-            # undefined what the "proper" one is to link to. So we just take whatever is "first".
-            for wiki_artifact in page.payload.provides:
-              return wiki_artifact.config
-          basedir = os.path.join(self.workdir, str(hash(wiki)))
-          process_page((wiki, page), basedir, wiki.wiki.url_builder, get_config,
-                         wikigenmap, fragment=True)
+        if page.provides:
+          for wiki in page.provides:
+            basedir = os.path.join(self.workdir, str(hash(wiki)))
+            process_page((wiki, page), basedir, wiki.wiki.url_builder, wikigenmap, fragment=True)
 
     if show:
       binary_util.ui_open(*show)
 
   PANTS_LINK = re.compile(r'''pants\(['"]([^)]+)['"]\)(#.*)?''')
 
-  def process_md(self, output_path, source, fragmented, url_builder, get_config, css=None):
+  def process_md(self, output_path, source, fragmented, url_builder, css=None):
     def parse_url(spec):
       match = self.PANTS_LINK.match(spec)
       if match:
@@ -310,7 +334,7 @@ class MarkdownToHtml(Task):
         if not page:
           raise TaskError('Invalid markdown link to pants target: "{}". '.format(match.group(1)) +
                           'Is your page missing a dependency on this target?')
-        alias, url = url_builder(page, config=get_config(page))
+        alias, url = url_builder(page)
         return alias, url + anchor
       else:
         return spec, spec
@@ -342,8 +366,7 @@ class MarkdownToHtml(Task):
         if fragmented:
           style_css = (HtmlFormatter(style=self.code_style)).get_style_defs('.codehilite')
           template = resource_string(__name__,
-                                     os.path.join(self._templates_dir,
-                                                  'fragment.mustache'))
+                                     os.path.join(self._templates_dir, 'fragment.mustache'))
           generator = Generator(template, style_css=style_css, md_html=md_html)
           generator.write(output)
         else:
@@ -353,22 +376,22 @@ class MarkdownToHtml(Task):
           generator.write(output)
         return output.name
 
-  def process_rst(self, output_path, source, fragmented):
-    safe_mkdir(os.path.dirname(output_path))
-    with codecs.open(output_path, 'w', 'utf-8') as output:
-      source_path = os.path.join(get_buildroot(), source)
-      with codecs.open(source_path, 'r', 'utf-8') as source_stream:
-        rst_html = rst_to_html(source_stream.read())
-        if fragmented:
-          template = resource_string(__name__,
-                                     os.path.join(self._templates_dir,
-                                                  'fragment.mustache'))
-          generator = Generator(template,
-                                md_html=rst_html)
-          generator.write(output)
+  def process_rst(self, workunit, page, output_path, source, fragmented):
+    source_path = os.path.join(get_buildroot(), source)
+    with codecs.open(source_path, 'r', 'utf-8') as source_stream:
+      rst_html, returncode = rst_to_html(source_stream.read(), stderr=workunit.output('stderr'))
+      if returncode != 0:
+        message = '{} rendered with errors.'.format(source_path)
+        if self.get_options().ignore_failure:
+          self.context.log.warn(message)
         else:
-          template = resource_string(__name__, os.path.join(self._templates_dir, 'page.mustache'))
-          generator = Generator(template,
-                                md_html=rst_html)
-          generator.write(output)
+          raise TaskError(message, exit_code=returncode, failed_targets=[page])
+
+      template_path = os.path.join(self._templates_dir,
+                                   'fragment.mustache' if fragmented else 'page.mustache')
+      template = resource_string(__name__, template_path)
+      generator = Generator(template, md_html=rst_html)
+      safe_mkdir(os.path.dirname(output_path))
+      with codecs.open(output_path, 'w', 'utf-8') as output:
+        generator.write(output)
         return output.name
