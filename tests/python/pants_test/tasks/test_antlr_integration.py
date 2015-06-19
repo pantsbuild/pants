bcd7a26dbf9097ea30ec769d01a8fd88c4ddb3ff
Qicheng Ma <qma@twitter.com>
2015-06-18 17:28:11 -0700

    add import

--- tests/python/pants_test/tasks/test_antlr_integration.py
+++ tests/python/pants_test/tasks/test_antlr_integration.py
@@ -5,6 +5,8 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import time
+
 from pants.util.contextutil import temporary_dir
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
