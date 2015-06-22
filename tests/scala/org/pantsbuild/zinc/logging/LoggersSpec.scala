36c14c79a5fc40e7f3496e256a483c59262d8e64
stuhood <stuhood@gmail.com>
2015-06-22 09:35:06 -0700

    WIP: Add noop test; will expand.

---
+++ tests/scala/org/pantsbuild/zinc/logging/LoggersSpec.scala
@@ -0,0 +1,15 @@
+// Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package org.pantsbuild.zinc.logging
+
+import org.junit.runner.RunWith
+import org.specs._
+
+@RunWith(classOf[runner.JUnitSuiteRunner])
+class LoggersSpec extends Specification with runner.JUnit {
+  "Loggers" should {
+    "log stuff" in {
+    }
+  }
+}
