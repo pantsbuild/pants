0737f23773940ee10ad5166de491f9ae7d19381a
Cody Gibb <codyhgibb@gmail.com>
2015-06-25 19:02:25 -0700

    Enabled caching for scalastyle
    - Enabled caching for scalastyle
    - Added a basic integration test for scalastyle
    
    Testing Done:
    Integration tests verifying that results cache on duplicate runs.
    
    CI: https://github.com/pantsbuild/pants/pull/1736
    
    Reviewed at https://rbcommons.com/s/twitter/r/2416/

--- src/python/pants/backend/jvm/tasks/scalastyle.py
+++ src/python/pants/backend/jvm/tasks/scalastyle.py
@@ -10,9 +10,11 @@ import re
 
 from pants.backend.jvm.tasks.jvm_tool_task_mixin import JvmToolTaskMixin
 from pants.backend.jvm.tasks.nailgun_task import NailgunTask
+from pants.base.cache_manager import VersionedTargetSet
 from pants.base.exceptions import TaskError
 from pants.base.target import Target
 from pants.process.xargs import Xargs
+from pants.util.dirutil import touch
 
 
 # TODO: Move somewhere more general?
@@ -94,6 +96,15 @@ class Scalastyle(NailgunTask):
 
     return scala_sources
 
+  def __init__(self, *args, **kwargs):
+    super(Scalastyle, self).__init__(*args, **kwargs)
+
+    self._results_dir = os.path.join(self.workdir, 'results')
+
+  def _create_result_file(self, target):
+    result_file = os.path.join(self._results_dir, target.id)
+    touch(result_file)
+    return result_file
 
   def execute(self):
     if self.get_options().skip:
@@ -105,28 +116,39 @@ class Scalastyle(NailgunTask):
     if not targets:
       return
 
-    scalastyle_config = self.validate_scalastyle_config()
-    scalastyle_excluder = self.create_file_excluder()
+    with self.invalidated(targets) as invalidation_check:
+      invalid_targets = []
+      for vt in invalidation_check.invalid_vts:
+        invalid_targets.extend(vt.targets)
 
-    self.context.log.debug('Non synthetic scala targets to be checked:')
-    for target in targets:
-      self.context.log.debug('  {address_spec}'.format(address_spec=target.address.spec))
+      scalastyle_config = self.validate_scalastyle_config()
+      scalastyle_excluder = self.create_file_excluder()
 
-    scala_sources = self.get_non_excluded_scala_sources(scalastyle_excluder, targets)
-    self.context.log.debug('Non excluded scala sources to be checked:')
-    for source in scala_sources:
-      self.context.log.debug('  {source}'.format(source=source))
+      self.context.log.debug('Non synthetic scala targets to be checked:')
+      for target in invalid_targets:
+        self.context.log.debug('  {address_spec}'.format(address_spec=target.address.spec))
 
-    if scala_sources:
-      def call(srcs):
-        cp = self.tool_classpath('scalastyle')
-        return self.runjava(classpath=cp,
-                            main=self._MAIN,
-                            args=['-c', scalastyle_config] + srcs)
-      result = Xargs(call).execute(scala_sources)
-      if result != 0:
-        raise TaskError('java {entry} ... exited non-zero ({exit_code})'.format(
-          entry=Scalastyle._MAIN, exit_code=result))
+      scala_sources = self.get_non_excluded_scala_sources(scalastyle_excluder, invalid_targets)
+      self.context.log.debug('Non excluded scala sources to be checked:')
+      for source in scala_sources:
+        self.context.log.debug('  {source}'.format(source=source))
+
+      if scala_sources:
+        def call(srcs):
+          cp = self.tool_classpath('scalastyle')
+          return self.runjava(classpath=cp,
+                              main=self._MAIN,
+                              args=['-c', scalastyle_config] + srcs)
+
+        result = Xargs(call).execute(scala_sources)
+        if result != 0:
+          raise TaskError('java {entry} ... exited non-zero ({exit_code})'.format(
+            entry=Scalastyle._MAIN, exit_code=result))
+
+        if self.artifact_cache_writes_enabled():
+          result_files = lambda vt: map(lambda t: self._create_result_file(t), vt.targets)
+          pairs = [(vt, result_files(vt)) for vt in invalidation_check.invalid_vts]
+          self.update_artifact_cache(pairs)
 
   def validate_scalastyle_config(self):
     scalastyle_config = self.get_options().config
---
+++ tests/python/pants_test/tasks/test_scalastyle_integration.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+from contextlib import contextmanager
+
+from pants.util.contextutil import temporary_dir
+from pants_test.pants_run_integration_test import PantsRunIntegrationTest
+
+
+class ScalastyleIntegrationTest(PantsRunIntegrationTest):
+  def test_scalastyle_cached(self):
+    with temporary_dir(root_dir=self.workdir_root()) as cache:
+      with temporary_dir(root_dir=self.workdir_root()) as workdir:
+        scalastyle_args = [
+          'clean-all',
+          'compile.scalastyle',
+          "--cache-write-to=['{}']".format(cache),
+          "--cache-read-from=['{}']".format(cache),
+          'examples/tests/scala/org/pantsbuild/example/hello/welcome',
+          '-ldebug'
+        ]
+
+        pants_run = self.run_pants_with_workdir(scalastyle_args, workdir)
+        self.assert_success(pants_run)
+        self.assertIn('abc_Scalastyle_compile_scalastyle will write to local artifact cache',
+            pants_run.stdout_data)
+
+        pants_run = self.run_pants_with_workdir(scalastyle_args, workdir)
+        self.assert_success(pants_run)
+        self.assertIn('abc_Scalastyle_compile_scalastyle will read from local artifact cache',
+            pants_run.stdout_data)
+        # make sure we are *only* reading from the cache and not also writing,
+        # implying there was as a cache hit
+        self.assertNotIn('abc_Scalastyle_compile_scalastyle will write to local artifact cache',
+            pants_run.stdout_data)
