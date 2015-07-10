0acdf8d8ab49a0a6bdf5084a99e0c1bca0231cf6
Eric Ayers <zundel@squareup.com>
2015-07-10 07:33:39 -0400

    Add an option --main to the run.jvm task to override the specification of 'main' on a jvm_binary() target
    Testing Done:
    CI green at https://travis-ci.org/pantsbuild/pants/builds/70208866
    
    Tested manually and added a new unit test.
    
    Bugs closed: 1775
    
    Reviewed at https://rbcommons.com/s/twitter/r/2442/

--- tests/python/pants_test/backend/jvm/tasks/test_jvm_run.py
+++ tests/python/pants_test/backend/jvm/tasks/test_jvm_run.py
@@ -6,6 +6,7 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 import os
+from contextlib import contextmanager
 
 from pants.backend.jvm.targets.jvm_binary import JvmBinary
 from pants.backend.jvm.tasks.jvm_run import JvmRun
@@ -20,16 +21,19 @@ class JvmRunTest(TaskTestBase):
 
   def setUp(self):
     super(JvmRunTest, self).setUp()
-    self.set_options(only_write_cmd_line='a')
 
-  def test_cmdline_only(self):
+  @contextmanager
+  def setup_cmdline_run(self, **options):
+    """Run the JvmRun task in command line only mode  with the specified extra options.
+    :returns: the command line string
+    """
+    self.set_options(only_write_cmd_line='a', **options)
     jvm_binary = self.make_target('src/java/org/pantsbuild:binary', JvmBinary,
                                   main='org.pantsbuild.Binary')
     context = self.context(target_roots=[jvm_binary])
     jvm_run =  self.create_task(context, 'unused')
-    classpath = [os.path.join(self.build_root, c) for c in ['bob', 'fred']]
-    self.populate_compile_classpath(context=jvm_run.context, classpath=classpath)
-
+    self._cmdline_classpath = [os.path.join(self.build_root, c) for c in ['bob', 'fred']]
+    self.populate_compile_classpath(context=jvm_run.context, classpath=self._cmdline_classpath)
     with temporary_dir() as pwd:
       with pushd(pwd):
         cmdline_file = os.path.join(pwd, 'a')
@@ -38,6 +42,16 @@ class JvmRunTest(TaskTestBase):
         self.assertTrue(os.path.exists(cmdline_file))
         with open(cmdline_file) as fp:
           contents = fp.read()
-          expected_suffix = 'java -cp {} org.pantsbuild.Binary'.format(
-            os.path.pathsep.join(classpath))
-          self.assertEquals(expected_suffix, contents[-len(expected_suffix):])
+          yield contents
+
+  def test_cmdline_only(self):
+    with self.setup_cmdline_run() as cmdline:
+      expected_suffix = 'java -cp {} org.pantsbuild.Binary'.format(
+        os.path.pathsep.join(self._cmdline_classpath))
+      self.assertEquals(expected_suffix, cmdline[-len(expected_suffix):])
+
+  def test_opt_main(self):
+    with self.setup_cmdline_run(main='org.pantsbuild.OptMain') as cmdline:
+      expected_suffix = 'java -cp {} org.pantsbuild.OptMain'.format(
+        os.path.pathsep.join(self._cmdline_classpath))
+      self.assertEquals(expected_suffix, cmdline[-len(expected_suffix):])
