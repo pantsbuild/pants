a3a9072ab222de7b253afd3421f6f5e11bb5f770
Kris Wilson <kwilson@twitter.com>
2015-07-16 10:34:30 -0700

    Add __future__ import required by CI.

--- src/python/pants/pantsd/process_manager.py
+++ src/python/pants/pantsd/process_manager.py
@@ -2,6 +2,9 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
 from contextlib import contextmanager
 import logging
 import os
