1410201e709bde29436567bc815766cec3abefb2
Tejal Desai <tdesai@twitter.com>
2015-07-29 16:42:28 -0700

    Fix style

--- contrib/npm_module/src/python/pants/contrib/npm_module/targets/gen_resources.py
+++ contrib/npm_module/src/python/pants/contrib/npm_module/targets/gen_resources.py
@@ -1,3 +1,10 @@
+# coding=utf-8
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
 from pants.base.exceptions import TargetDefinitionException
 from pants.base.payload import Payload
 from pants.base.payload_field import SourcesField
--- contrib/npm_module/src/python/pants/contrib/npm_module/tasks/resource_preprocessor.py
+++ contrib/npm_module/src/python/pants/contrib/npm_module/tasks/resource_preprocessor.py
@@ -1,3 +1,10 @@
+# coding=utf-8
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
 import os
 from abc import abstractmethod, abstractproperty
 
