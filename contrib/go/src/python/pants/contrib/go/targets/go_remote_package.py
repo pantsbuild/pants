c806b5396d66e8970cb785581fab5de026ddc6a7
Cody Gibb <cgibb@twitter.com>
2015-07-30 14:37:40 -0700

    Add documentation for zip_url

--- contrib/go/src/python/pants/contrib/go/targets/go_remote_package.py
+++ contrib/go/src/python/pants/contrib/go/targets/go_remote_package.py
@@ -14,14 +14,20 @@ from pants.contrib.go.targets.go_target import GoTarget
 class GoRemotePackage(GoTarget):
 
   def __init__(self, rev='', zip_url='', **kwargs):
-    """:param rev string: Identifies which version of the remote package to download.
-                          This could be a commit SHA (git), node id (hg), etc.
-    :param zip_url string: Any URL from which a zipfile can be downloaded containing
-                           the source code of the remote package. Can be a template
-                           string using variables {rev} (see :param rev:) and {id},
-                           which is the global import identifier of the package, which
-                           is specified by the path to the BUILD file relative to
-                           all 3rd party Go packages.
+    """
+    :param rev string: Identifies which version of the remote package to download.
+                       This could be a commit SHA (git), node id (hg), etc.
+    :param zip_url string:
+      - Any URL from which a zipfile can be downloaded containing the source code of the
+        remote package.
+      - Can be a template string using variables {rev} (see :param rev:) and {id}, which
+        is the global import identifier of the package, which is specified by the path to
+        the BUILD file relative to all 3rd party Go packages (see GoPlatform).
+          Example: "https://{id}/archive/{rev}.zip"
+      - The zip file is expected to have zipped the package directory itself, and NOT the
+        direct contents of the package.
+          Expected: `zip -r mylib.zip mylib/`
+               Not: `zip -r mylib.zip mylib/*`
     """
     payload = Payload()
     payload.add_fields({
