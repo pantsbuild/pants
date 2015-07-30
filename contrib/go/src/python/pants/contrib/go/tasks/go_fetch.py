4e80d6dbd8f4568732c82aeed97b1fa1bacdcdbe
Cody Gibb <cgibb@twitter.com>
2015-07-29 18:16:05 -0700

    Fixes product adding bug in GoFetch
    - Valid targets weren't adding their products

--- contrib/go/src/python/pants/contrib/go/tasks/go_fetch.py
+++ contrib/go/src/python/pants/contrib/go/tasks/go_fetch.py
@@ -31,16 +31,20 @@ class GoFetch(GoTask):
 
   def execute(self):
     self.context.products.safe_create_data('go_remote_pkg_source', lambda: defaultdict(str))
+
     with self.invalidated(self.context.targets(self.is_go_remote_pkg)) as invalidation_check:
-      for vt in invalidation_check.invalid_vts:
+      for vt in invalidation_check.all_vts:
         import_id = self.global_import_id(vt.target)
-        rev = vt.target.payload.get_field_value('rev')
-        zip_url = vt.target.payload.get_field_value('zip_url').format(id=import_id, rev=rev)
-        if not zip_url:
-          raise TaskError('No zip url specified for go_remote_package {id}'
-                          .format(id=import_id))
         dest_dir = os.path.join(vt.results_dir, import_id)
-        self._download_zip(zip_url, dest_dir)
+
+        if not vt.valid:
+          rev = vt.target.payload.get_field_value('rev')
+          zip_url = vt.target.payload.get_field_value('zip_url').format(id=import_id, rev=rev)
+          if not zip_url:
+            raise TaskError('No zip url specified for go_remote_package {id}'
+                            .format(id=import_id))
+          self._download_zip(zip_url, dest_dir)
+
         self.context.products.get_data('go_remote_pkg_source')[vt.target] = dest_dir
 
   def _download_zip(self, zip_url, dest_dir):
