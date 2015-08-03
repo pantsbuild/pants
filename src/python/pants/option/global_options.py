aa901a758c16a9e1b4f8fc3418f21daf72df8872
Eric Ayers <zundel@squareup.com>
2015-08-03 17:16:29 -0400

    Add the ability to turn off acquiring the global lock

--- src/python/pants/option/global_options.py
+++ src/python/pants/option/global_options.py
@@ -122,3 +122,6 @@ class GlobalOptionsRegistrar(Optionable):
     register('--build-file-rev',
              help='Read BUILD files from this scm rev instead of from the working tree.  This is '
              'useful for implementing pants-aware sparse checkouts.')
+    register('--lock', action='store_true', default=True,
+             help='Use a global lock to exclude other versions of pants from running during critical'
+                  'operations.')
