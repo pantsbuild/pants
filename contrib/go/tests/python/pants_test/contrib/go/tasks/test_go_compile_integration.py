cab33d83f28e5640bbd439bb9cbf901fa00e8834
Cody Gibb <cgibb@twitter.com>
2015-08-10 17:26:19 -0700

    Skip integration tests if Go not installed

--- contrib/go/tests/python/pants_test/contrib/go/tasks/test_go_compile_integration.py
+++ contrib/go/tests/python/pants_test/contrib/go/tasks/test_go_compile_integration.py
@@ -7,7 +7,9 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 
 import os
 
+import pytest
 from pants.util.contextutil import temporary_dir
+from pants_test.contrib.go.tasks.go_tool import GoTool
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 from pants.contrib.go.tasks.go_task import GoTask
@@ -15,6 +17,7 @@ from pants.contrib.go.tasks.go_task import GoTask
 
 class GoCompileIntegrationTest(PantsRunIntegrationTest):
 
+  @pytest.mark.skipif('not GoTool.go_installed()', reason='requires `go` command.')
   def test_go_compile_simple(self):
     with temporary_dir(root_dir=self.workdir_root()) as workdir:
       args = ['compile',
@@ -23,11 +26,7 @@ class GoCompileIntegrationTest(PantsRunIntegrationTest):
       self.assert_success(pants_run)
       # TODO(cgibb): Is it appropriate to be calling a GoTask static method from
       # an integration test?
-      try:
-        goos_goarch = GoTask.lookup_goos_goarch()
-      except OSError:
-        # Go isn't installed -- just stop the test here.
-        return
+      goos_goarch = GoTask.lookup_goos_goarch()
       expected_files = set('contrib.go.examples.src.go.{libname}.{libname}/'
                            'pkg/{goos_goarch}/contrib/go/examples/src/go/{libname}.a'
                            .format(libname=libname, goos_goarch=goos_goarch)
