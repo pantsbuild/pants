9281f87fd19c6e5db4ca18697ddbd9e1d3d34daa
Cody Gibb <cgibb@twitter.com>
2015-08-12 12:27:14 -0700

    Change zipfile server from IPv4 to IPv6

--- contrib/go/tests/python/pants_test/contrib/go/tasks/fetch_utils.py
+++ contrib/go/tests/python/pants_test/contrib/go/tasks/fetch_utils.py
@@ -7,6 +7,7 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 
 import multiprocessing
 import os
+import socket
 import time
 from BaseHTTPServer import HTTPServer
 from contextlib import contextmanager
@@ -15,12 +16,14 @@ from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 @contextmanager
 def zipfile_server():
-  addr = ('localhost', 3000)
+  addr = ('[::1]', 3000)
   def run_server():
     os.chdir('contrib/go/examples/test_remote_zips/zipfiles/')
     handler_cls = SimpleHTTPRequestHandler
     handler_cls.protocol_version = 'HTTP/1.1'
-    server = HTTPServer(addr, handler_cls)
+    server_cls = HTTPServer
+    server_cls.address_family = socket.AF_INET6
+    server = server_cls(addr, handler_cls)
     server.serve_forever()
   p = multiprocessing.Process(target=run_server)
   p.start()
