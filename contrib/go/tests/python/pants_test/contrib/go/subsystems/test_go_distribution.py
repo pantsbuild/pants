b995ac74b038d5d4ca5d26ea9ba22506d6118727
John Sirois <john.sirois@gmail.com>
2015-08-12 00:26:10 -0700

    Improve subsystem_util.
    Add a context manager that supports fully bootstrapping a Subsystem
    instance.  In the process factor up the bootstrapping code for re-use by
    BaseTest.
    
    Use the subsystem_instance to address some TODOs and simplify tests.

--- contrib/go/tests/python/pants_test/contrib/go/subsystems/test_go_distribution.py
+++ contrib/go/tests/python/pants_test/contrib/go/subsystems/test_go_distribution.py
@@ -6,46 +6,42 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 import os
-import subprocess
 import unittest
+from contextlib import contextmanager
 
-from pants.base.build_environment import get_pants_cachedir
-from pants.binaries.binary_util import BinaryUtil
+from pants_test.subsystem.subsystem_util import subsystem_instance
 
 from pants.contrib.go.subsystems.go_distribution import GoDistribution
 
 
 class GoDistributionTest(unittest.TestCase):
-  def setUp(self):
-    # TODO(John Sirois): Learn create_subsystem how to handle subsystem dependencies and do:
-    #   `create_subsystem(GoDistribution.Factory)`
-    # Instead of manually replicating default subsystem options here.
-    binary_util = BinaryUtil(baseurls=['https://dl.bintray.com/pantsbuild/bin/build-support'],
-                             timeout_secs=30,
-                             bootstrapdir=get_pants_cachedir())
-    self.go_distribution = GoDistribution(binary_util, 'bin/go', '1.4.2')
+
+  @contextmanager
+  def distribution(self):
+    with subsystem_instance(GoDistribution.Factory) as factory:
+      yield factory.create()
 
   def test_bootstrap(self):
-    go_cmd = self.go_distribution.create_go_cmd(cmd='env', args=['GOROOT'])
-    process = go_cmd.spawn(stdout=subprocess.PIPE)
-    stdout, _ = process.communicate()
-
-    self.assertEqual(0, process.returncode)
-    self.assertEqual(self.go_distribution.goroot, stdout.strip())
+    with self.distribution() as go_distribution:
+      go_cmd = go_distribution.create_go_cmd(cmd='env', args=['GOROOT'])
+      output = go_cmd.check_output()
+      self.assertEqual(go_distribution.goroot, output.strip())
 
   def test_go_command_no_gopath(self):
-    go_cmd = self.go_distribution.create_go_cmd(cmd='env', args=['GOROOT'])
+    with self.distribution() as go_distribution:
+      go_cmd = go_distribution.create_go_cmd(cmd='env', args=['GOROOT'])
 
-    self.assertEqual({'GOROOT': self.go_distribution.goroot}, go_cmd.env)
-    self.assertEqual('go', os.path.basename(go_cmd.cmdline[0]))
-    self.assertEqual(['env', 'GOROOT'], go_cmd.cmdline[1:])
-    self.assertRegexpMatches(str(go_cmd), r'^GOROOT=[^ ]+ .*/go env GOROOT$')
+      self.assertEqual({'GOROOT': go_distribution.goroot}, go_cmd.env)
+      self.assertEqual('go', os.path.basename(go_cmd.cmdline[0]))
+      self.assertEqual(['env', 'GOROOT'], go_cmd.cmdline[1:])
+      self.assertRegexpMatches(str(go_cmd), r'^GOROOT=[^ ]+ .*/go env GOROOT$')
 
   def test_go_command_gopath(self):
-    go_cmd = self.go_distribution.create_go_cmd(cmd='env', gopath='/tmp/fred', args=['GOROOT'])
+    with self.distribution() as go_distribution:
+      go_cmd = go_distribution.create_go_cmd(cmd='env', gopath='/tmp/fred', args=['GOROOT'])
 
-    self.assertEqual({'GOROOT': self.go_distribution.goroot,
-                      'GOPATH': '/tmp/fred'}, go_cmd.env)
-    self.assertEqual('go', os.path.basename(go_cmd.cmdline[0]))
-    self.assertEqual(['env', 'GOROOT'], go_cmd.cmdline[1:])
-    self.assertRegexpMatches(str(go_cmd), r'^GOROOT=[^ ]+ GOPATH=/tmp/fred .*/go env GOROOT$')
+      self.assertEqual({'GOROOT': go_distribution.goroot,
+                        'GOPATH': '/tmp/fred'}, go_cmd.env)
+      self.assertEqual('go', os.path.basename(go_cmd.cmdline[0]))
+      self.assertEqual(['env', 'GOROOT'], go_cmd.cmdline[1:])
+      self.assertRegexpMatches(str(go_cmd), r'^GOROOT=[^ ]+ GOPATH=/tmp/fred .*/go env GOROOT$')
