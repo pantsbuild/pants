43df0378febb1fc8366243d321e01ef833c3265f
Garrett Malmquist <garrett.malmquist@gmail.com>
2015-08-18 10:16:17 -0400

    Speeding up jvm-platform-validate step.
    Using self.invalidated mechanism, targets are only re-evaluated if
    they (1) they failed previously, (2) their dependencies have changed,
    or (3) their platform -target version has changed.
    
    Additionally, pruned down the list of dependencies that must be
    considered for each target. Instead of evaluating all transitive
    JvmTarget dependencies, only consider the "direct" JvmTarget
    dependencies. "Direct" is in quotes because I'm defining a JvmTarget
    'A' as "directly" depending on a JvmTarget 'B' iff there exists a
    path in the dependency graph from 'A' to 'B', and there are no
    internal vertices in the path that are JvmTargets.
    
    This behavior is documented in the jvm_dependency_map property.
    
    This addresses https://github.com/pantsbuild/pants/issues/1972. It'd probably be good for someone with access to test this change against the repos which were previously experiencing slow-down from jvm-platform-validate, to make sure this patch actually solves the problem.
    
    Testing Done:
    Manual testing implies caching is working as intended. Added a couple unit tests to test_jvm_platform_analysis.py. Everything passes locally.
    
    CI went green: https://travis-ci.org/pantsbuild/pants/builds/75650551
    CI went green: https://travis-ci.org/pantsbuild/pants/builds/75940854
    CI went green: https://travis-ci.org/pantsbuild/pants/builds/76001873
    
    Bugs closed: 1981
    
    Reviewed at https://rbcommons.com/s/twitter/r/2626/

---
+++ tests/python/pants_test/backend/jvm/tasks/test_jvm_platform_analysis_integration.py
@@ -0,0 +1,143 @@
+# coding=utf-8
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+import os
+from contextlib import contextmanager
+from textwrap import dedent
+
+from pants.util.contextutil import temporary_dir
+from pants_test.pants_run_integration_test import PantsRunIntegrationTest
+
+
+class JvmPlatformAnalysisIntegrationTest(PantsRunIntegrationTest):
+  """Make sure jvm-platform-analysis runs properly, especially with respect to caching behavior."""
+
+  class JavaSandbox(object):
+    """Testing sandbox for making temporary java_library targets."""
+
+    def __init__(self, test, workdir, javadir):
+      self.javadir = javadir
+      self.workdir = workdir
+      self.test = test
+      if not os.path.exists(self.workdir):
+        os.makedirs(self.workdir)
+
+    @property
+    def build_file_path(self):
+      return os.path.join(self.javadir, 'BUILD')
+
+    def write_build_file(self, contents):
+      with open(self.build_file_path, 'w') as f:
+        f.write(contents)
+
+    def spec(self, name):
+      return '{}:{}'.format(self.javadir, name)
+
+    def clean_all(self):
+      return self.test.run_pants(['clean-all'])
+
+    def jvm_platform_validate(self, *targets):
+      return self.test.run_pants_with_workdir(['jvm-platform-validate', '--check=fatal']
+                                              + map(self.spec, targets),
+                                              workdir=self.workdir)
+
+  @contextmanager
+  def setup_sandbox(self):
+    with temporary_dir('.') as tempdir:
+      workdir = os.path.abspath(tempdir)
+      javadir = os.path.join(tempdir, 'src', 'java')
+      os.makedirs(javadir)
+      with open(os.path.join(workdir, 'BUILD'), 'w') as f:
+        f.write(dedent('''
+          source_root('src/java', java_library)
+        '''))
+      yield self.JavaSandbox(self, os.path.join(workdir, '.pants.d'), javadir)
+
+  @property
+  def _good_one_two(self):
+    return dedent('''
+      java_library(name='one',
+        platform='1.7',
+      )
+
+      java_library(name='two',
+        platform='1.8',
+      )
+    ''')
+
+  @property
+  def _bad_one_two(self):
+    return dedent('''
+      java_library(name='one',
+        platform='1.7',
+        dependencies=[':two'],
+      )
+
+      java_library(name='two',
+        platform='1.8',
+      )
+    ''')
+
+  def test_good_targets_works_fresh(self):
+    with self.setup_sandbox() as sandbox:
+      sandbox.write_build_file(self._good_one_two)
+      self.assert_success(sandbox.clean_all())
+      self.assert_success(sandbox.jvm_platform_validate('one', 'two'))
+
+  def test_bad_targets_fails_fresh(self):
+    with self.setup_sandbox() as sandbox:
+      sandbox.write_build_file(self._bad_one_two)
+      self.assert_success(sandbox.clean_all())
+      self.assert_failure(sandbox.jvm_platform_validate('one', 'two'))
+
+  def test_good_then_bad(self):
+    with self.setup_sandbox() as sandbox:
+      sandbox.write_build_file(self._good_one_two)
+      self.assert_success(sandbox.clean_all())
+      self.assert_success(sandbox.jvm_platform_validate('one', 'two'))
+      sandbox.write_build_file(self._bad_one_two)
+      self.assert_failure(sandbox.jvm_platform_validate('one', 'two'))
+
+  def test_bad_then_good(self):
+    with self.setup_sandbox() as sandbox:
+      sandbox.write_build_file(self._bad_one_two)
+      self.assert_success(sandbox.clean_all())
+      self.assert_failure(sandbox.jvm_platform_validate('one', 'two'))
+      sandbox.write_build_file(self._good_one_two)
+      self.assert_success(sandbox.jvm_platform_validate('one', 'two'))
+
+  def test_bad_then_good(self):
+    with self.setup_sandbox() as sandbox:
+      sandbox.write_build_file(self._bad_one_two)
+      self.assert_success(sandbox.clean_all())
+      self.assert_failure(sandbox.jvm_platform_validate('one', 'two'))
+      sandbox.write_build_file(self._good_one_two)
+      self.assert_success(sandbox.jvm_platform_validate('one', 'two'))
+
+  def test_good_caching(self):
+    # Make sure targets are cached after a good run.
+    with self.setup_sandbox() as sandbox:
+      sandbox.write_build_file(self._good_one_two)
+      self.assert_success(sandbox.clean_all())
+      first_run = sandbox.jvm_platform_validate('one', 'two')
+      self.assert_success(first_run)
+      self.assertIn('Invalidated 2 targets', first_run.stdout_data)
+      second_run = sandbox.jvm_platform_validate('one', 'two')
+      self.assert_success(second_run)
+      self.assertNotIn('Invalidated 2 targets', second_run.stdout_data)
+
+  def test_bad_caching(self):
+    # Make sure targets aren't cached after a bad run.
+    with self.setup_sandbox() as sandbox:
+      sandbox.write_build_file(self._bad_one_two)
+      self.assert_success(sandbox.clean_all())
+      first_run = sandbox.jvm_platform_validate('one', 'two')
+      self.assert_failure(first_run)
+      self.assertIn('Invalidated 2 targets', first_run.stdout_data)
+      second_run = sandbox.jvm_platform_validate('one', 'two')
+      self.assert_failure(second_run)
+      self.assertIn('Invalidated 2 targets', second_run.stdout_data)
