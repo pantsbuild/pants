c93bab32fa8c3f6fc72c83e6d87917b29b4e017f
Patrick Lawson <pl@foursquare.com>
2015-08-31 19:39:18 -0400

    Switch JVM missing dep detection to use compile_classpath [IMPROVEMENT]
    This kills the use of `ivy_jar_products` and `ivy_resolve_symlink_map` in
    favor of `compile_classpath`.
    
    Testing Done:
    Foursquare internal CIs are showing correct missing dep detection behavior.
    
    CI is baking: https://travis-ci.org/pantsbuild/pants/builds/78136942
    
    Bugs closed: 2094
    
    Reviewed at https://rbcommons.com/s/twitter/r/2729/

--- src/python/pants/backend/jvm/tasks/jvm_dependency_check.py
+++ src/python/pants/backend/jvm/tasks/jvm_dependency_check.py
@@ -30,10 +30,9 @@ class JvmDependencyCheck(Task):
   def prepare(cls, options, round_manager):
     super(JvmDependencyCheck, cls).prepare(options, round_manager)
     if not options.skip:
-      round_manager.require_data('classes_by_target')
-      round_manager.require_data('ivy_jar_products')
-      round_manager.require_data('ivy_resolve_symlink_map')
       round_manager.require_data('actual_source_deps')
+      round_manager.require_data('classes_by_target')
+      round_manager.require_data('compile_classpath')
 
   @classmethod
   def register_options(cls, register):
@@ -106,9 +105,6 @@ class JvmDependencyCheck(Task):
     """
     targets_by_file = defaultdict(OrderedSet)
 
-    # Multiple JarLibrary targets can provide the same (org, name).
-    jarlibs_by_id = defaultdict(set)
-
     # Compute src -> target.
     self.context.log.debug('Mapping sources...')
     buildroot = get_buildroot()
@@ -117,9 +113,6 @@ class JvmDependencyCheck(Task):
       if isinstance(target, JvmTarget):
         for src in target.sources_relative_to_buildroot():
           targets_by_file[os.path.join(buildroot, src)].add(target)
-      elif isinstance(target, JarLibrary):
-        for jardep in target.jar_dependencies:
-          jarlibs_by_id[(jardep.org, jardep.name)].add(target)
       # TODO(Tejal Desai): pantsbuild/pants/65: Remove java_sources attribute for ScalaLibrary
       if isinstance(target, ScalaLibrary):
         for java_source in target.java_sources:
@@ -137,37 +130,10 @@ class JvmDependencyCheck(Task):
 
     # Compute jar -> target.
     self.context.log.debug('Mapping jars...')
-    with IvyTaskMixin.symlink_map_lock:
-      m = self.context.products.get_data('ivy_resolve_symlink_map')
-      all_symlinks_map = m.copy() if m is not None else {}
-      # We make a copy, so it's safe to use outside the lock.
-
-    def register_transitive_jars_for_ref(ivyinfo, ref):
-      deps_by_ref_memo = {}
-
-      def get_transitive_jars_by_ref(ref1):
-        def create_collection(current_ref):
-          return {ivyinfo.modules_by_ref[current_ref].artifact}
-        return ivyinfo.traverse_dependency_graph(ref1, create_collection, memo=deps_by_ref_memo)
-
-      target_key = (ref.org, ref.name)
-      if target_key in jarlibs_by_id:
-        # These targets provide all the jars in ref, and all the jars ref transitively depends on.
-        jarlib_targets = jarlibs_by_id[target_key]
-
-        for jar_path in get_transitive_jars_by_ref(ref):
-          # Register that each jarlib_target provides jar (via all its symlinks).
-          symlink = all_symlinks_map.get(os.path.realpath(jar_path), None)
-          if symlink:
-            for jarlib_target in jarlib_targets:
-              targets_by_file[symlink].add(jarlib_target)
-
-    ivy_products = self.context.products.get_data('ivy_jar_products')
-    if ivy_products:
-      for ivyinfos in ivy_products.values():
-        for ivyinfo in ivyinfos:
-          for ref in ivyinfo.modules_by_ref:
-            register_transitive_jars_for_ref(ivyinfo, ref)
+    compile_classpath = self.context.products.get_data('compile_classpath')
+    for jar_lib in self.context.targets(lambda t: isinstance(t, JarLibrary)):
+      for _, artifact_path in compile_classpath.get_for_target(jar_lib, transitive=False):
+        targets_by_file[artifact_path].add(jar_lib)
 
     return targets_by_file
 
