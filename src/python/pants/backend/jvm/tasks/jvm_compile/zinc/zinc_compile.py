70f256be0d616e4e5aee6d4898aa1d9bb3d4322b
Stu Hood <stuhood@gmail.com>
2015-09-14 10:03:33 -0700

    Bump to zinc 1.0.10 by default

--- src/python/pants/backend/jvm/tasks/jvm_compile/zinc/zinc_compile.py
+++ src/python/pants/backend/jvm/tasks/jvm_compile/zinc/zinc_compile.py
@@ -82,7 +82,7 @@ class ZincCompile(JvmCompile):
     cls.register_jvm_tool(register,
                           'zinc',
                           classpath=[
-                            JarDependency('org.pantsbuild', 'zinc', '1.0.8')
+                            JarDependency('org.pantsbuild', 'zinc', '1.0.10')
                           ],
                           main=cls._ZINC_MAIN,
                           custom_rules=[
