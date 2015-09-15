775600e948c88f6ab2f6d5b4c57b83c55da8fb61
John Sirois <john.sirois@gmail.com>
2015-09-14 23:46:56 -0600

    Fix resource handling for the python backend.
    Previously resource handling was broken in a few ways:
    1. Resources targets in the same build file could not be addressed
       via `PythonTarget.resources` because relative_to was not passed to
       the build_graph.
    2. Since resource targets were not modelled as dependencies, they
       could not be found via the `PythonTarget.resources` property since
       they never were loaded into the build graph.
    3. SetupPy only honored resources set via the `PythonTarget`
       `resources` constructor parameter.
    
    This change fixes resources to be modelled as dependencies like they
    are for `JvmTarget`, solving 2 above, and fixes resources target
    resolution to use `relative_to` solving 1 above.  `SetupPy` now reads
    resources from the fixed `PythonTarget.resources` property and gathers
    resource sources like it does for all other dependencies fixing 3
    above.
    
    All existing python targets are updated to use `target_resources`
    instead of `resources` as dogfood and a unit test is added to ensure
    both modes of expressing resources work.
    
    Testing Done:
    CI went green here:
      https://travis-ci.org/pantsbuild/pants/builds/80315273
    
    Bugs closed: 309, 1841, 2191
    
    Reviewed at https://rbcommons.com/s/twitter/r/2817/

--- tests/python/pants_test/backend/jvm/tasks/jvm_compile/test_resource_mapping.py
+++ tests/python/pants_test/backend/jvm/tasks/jvm_compile/test_resource_mapping.py
@@ -5,6 +5,8 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import os
+
 from pants.backend.jvm.tasks.jvm_compile.resource_mapping import ResourceMapping
 from pants_test.base_test import BaseTest
 
@@ -12,35 +14,34 @@ from pants_test.base_test import BaseTest
 class ResourceMappingTest(BaseTest):
 
   def test_resource_mapping_ok(self):
-    rel_dir = 'tests/python/pants_test/backend/jvm/tasks/jvm_compile/test-data/resource_mapping'
-    resource_mapping = ResourceMapping(rel_dir)
+    resource_mapping = self._create_mapping('test-data/resource_mapping')
 
     self.assertEquals(2, len(resource_mapping.mappings))
 
   def test_resource_mapping_short(self):
-    rel_dir = 'tests/python/pants_test/backend/jvm/tasks/jvm_compile/test-data/resource_mapping-broken-short'
-    resource_mapping = ResourceMapping(rel_dir)
+    resource_mapping = self._create_mapping('test-data/resource_mapping-broken-short')
 
     with self.assertRaises(ResourceMapping.TruncatedFileException):
       resource_mapping.mappings
 
   def test_resource_mapping_long(self):
-    rel_dir = 'tests/python/pants_test/backend/jvm/tasks/jvm_compile/test-data/resource_mapping-broken-long'
-    resource_mapping = ResourceMapping(rel_dir)
+    resource_mapping = self._create_mapping('test-data/resource_mapping-broken-long')
 
     with self.assertRaises(ResourceMapping.TooLongFileException):
       resource_mapping.mappings
 
   def test_resource_mapping_mangled(self):
-    rel_dir = 'tests/python/pants_test/backend/jvm/tasks/jvm_compile/test-data/resource_mapping-broken-mangled'
-    resource_mapping = ResourceMapping(rel_dir)
+    resource_mapping = self._create_mapping('test-data/resource_mapping-broken-mangled')
 
     with self.assertRaises(ResourceMapping.UnparseableLineException):
       resource_mapping.mappings
 
   def test_resource_mapping_noitems(self):
-    rel_dir = 'tests/python/pants_test/backend/jvm/tasks/jvm_compile/test-data/resource_mapping-broken-missing-items'
-    resource_mapping = ResourceMapping(rel_dir)
+    resource_mapping = self._create_mapping('test-data/resource_mapping-broken-missing-items')
 
     with self.assertRaises(ResourceMapping.MissingItemsLineException):
       resource_mapping.mappings
+
+  def _create_mapping(self, rel_path):
+    path = os.path.join('tests/python/pants_test/backend/jvm/tasks/jvm_compile', rel_path)
+    return ResourceMapping(path)
