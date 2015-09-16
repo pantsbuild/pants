5992baa489882c21218955b20a5638548711d9b9
Stu Hood <stuhood@gmail.com>
2015-09-16 13:26:59 -0700

    Lazily load zinc analysis
    Rather than eagerly loading all upstream analysis, we now lazily load it when it is requested, and allow it to be unloaded when necessary. It looks like sbt was always designed to be used this way, but zinc was eagerly building a map to fill the `getAnalysis` function that the sbt compiler desires.
    
    - Lazily load zinc analysis upon usage, and allow it to be freed by never holding strong references to the entire Analysis object.
    - Remove the zinc `-force-clean` flag, since it is unused from pants' perspective, and supporting it required additional Analysis apis.
    - Move `Cache` class to a `cache` package.
    
    Testing Done:
    cold compiles of large internal projects, and:
    
        ./pants test tests/python/pants_test/backend/jvm/tasks/jvm_compile/::
    
    ...with a locally published version.
    
    Reviewed at https://rbcommons.com/s/twitter/r/2827/

--- src/scala/org/pantsbuild/zinc/Compiler.scala
+++ src/scala/org/pantsbuild/zinc/Compiler.scala
@@ -10,7 +10,6 @@ import sbt.compiler.javac
 import sbt.{
   ClasspathOptions,
   CompileOptions,
-  CompileSetup,
   Logger,
   LoggerReporter,
   ScalaInstance
@@ -22,16 +21,12 @@ import sbt.compiler.{
   IC,
   MixedAnalyzingCompiler
 }
-import sbt.inc.{
-  Analysis,
-  AnalysisStore,
-  FileBasedStore,
-  ZincPrivateAnalysis
-}
+import sbt.inc.ZincPrivateAnalysis
 import sbt.Path._
 import xsbti.compile.{ JavaCompiler, GlobalsCache }
 
-import org.pantsbuild.zinc.Cache.Implicits
+import org.pantsbuild.zinc.cache.Cache
+import org.pantsbuild.zinc.cache.Cache.Implicits
 
 object Compiler {
   val CompilerInterfaceId = "compiler-interface"
@@ -40,18 +35,12 @@ object Compiler {
   /**
    * Static cache for zinc compilers.
    */
-  val compilerCache = Cache[Setup, Compiler](Setup.Defaults.compilerCacheLimit)
+  private val compilerCache = Cache[Setup, Compiler](Setup.Defaults.compilerCacheLimit)
 
   /**
    * Static cache for resident scala compilers.
    */
-  val residentCache: GlobalsCache = createResidentCache(Setup.Defaults.residentCacheLimit)
-
-  /**
-   * Static cache for compile analyses.  Values must be Options because in get() we don't yet know if, on
-   * a cache miss, the underlying file will yield a valid Analysis.
-   */
-  val analysisCache = Cache[FileFPrint, Option[(Analysis, CompileSetup)]](Setup.Defaults.analysisCacheLimit)
+  private val residentCache: GlobalsCache = createResidentCache(Setup.Defaults.residentCacheLimit)
 
   /**
    * Get or create a zinc compiler based on compiler setup.
@@ -114,43 +103,6 @@ object Compiler {
   }
 
   /**
-   * Create an analysis store backed by analysisCache.
-   *
-   * TODO: for all but the "output" analysis, the synchronization is overkill; everything upstream is immutable
-   */
-  def cachedAnalysisStore(cacheFile: File): AnalysisStore = {
-    val fileStore = AnalysisStore.cached(FileBasedStore(cacheFile))
-
-    val fprintStore = new AnalysisStore {
-      def set(analysis: Analysis, setup: CompileSetup) {
-        fileStore.set(analysis, setup)
-        FileFPrint.fprint(cacheFile) foreach { analysisCache.put(_, Some((analysis, setup))) }
-      }
-      def get(): Option[(Analysis, CompileSetup)] = {
-        FileFPrint.fprint(cacheFile) flatMap { fprint =>
-          analysisCache.getOrElseUpdate(fprint) {
-            fileStore.get
-          }
-        }
-      }
-    }
-
-    AnalysisStore.sync(AnalysisStore.cached(fprintStore))
-  }
-
-  /**
-   * Analysis for the given file if it is already cached.
-   */
-  def analysisOptionFor(cacheFile: File): Option[Analysis] =
-    cachedAnalysisStore(cacheFile).get map (_._1)
-
-  /**
-   * Check whether an analysis is empty.
-   */
-  def analysisIsEmpty(cacheFile: File): Boolean =
-    analysisOptionFor(cacheFile).isEmpty
-
-  /**
    * Create the scala instance for the compiler. Includes creating the classloader.
    */
   def scalaInstance(setup: Setup): ScalaInstance = {
@@ -199,13 +151,9 @@ class Compiler(scalac: AnalyzingCompiler, javac: JavaCompiler, setup: Setup) {
    */
   def compile(inputs: Inputs, cwd: Option[File], reporter: xsbti.Reporter, progress: xsbti.compile.CompileProgress)(log: Logger): Unit = {
     import inputs._
-    if (forceClean && Compiler.analysisIsEmpty(cacheFile)) {
-      Util.cleanAllClasses(classesDirectory)
-    }
 
     // load the existing analysis
-    // TODO: differentiate output analysis from input analysis
-    val targetAnalysisStore = Compiler.cachedAnalysisStore(cacheFile)
+    val targetAnalysisStore = AnalysisMap.cachedStore(cacheFile)
     val (previousAnalysis, previousSetup) =
       targetAnalysisStore.get().map {
         case (a, s) => (a, Some(s))
@@ -226,8 +174,8 @@ class Compiler(scalac: AnalyzingCompiler, javac: JavaCompiler, setup: Setup) {
         javacOptions,
         previousAnalysis,
         previousSetup,
-        analysisMap = analysisMap.get,
-        definesClass,
+        analysisMap = analysisMap.getAnalysis _,
+        definesClass = analysisMap.definesClass _,
         reporter,
         compileOrder,
         skip = false,
