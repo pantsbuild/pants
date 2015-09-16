d2ec126df6eda309c27881537745528698188efe
Nick Howard <nhoward@twitter.com>
2015-09-16 14:30:16 -0600

    clean up some lingering WIP in jvm_compile

--- src/python/pants/backend/jvm/tasks/jvm_compile/jvm_compile.py
+++ src/python/pants/backend/jvm/tasks/jvm_compile/jvm_compile.py
@@ -268,12 +268,7 @@ class JvmCompile(NailgunTaskBase, GroupMember):
 
         # Register products for all the valid targets.
         # We register as we go, so dependency checking code can use this data.
-        #valid_targets = list(set(relevant_targets) - set(invalid_targets))
-        valid_vts = [vt for vt in invalidation_check.all_vts if vt.valid]
-        valid_targets_2 = [vt.target for vt in valid_vts]
-        valid_targets = [vt.target for vt in valid_vts]
-        #assert valid_targets == valid_targets_2
-
+        valid_targets = list(set(relevant_targets) - set(invalid_targets))
         valid_compile_contexts = [self._strategy.compile_context(t) for t in valid_targets]
         self._register_vts(valid_compile_contexts)
 
@@ -334,10 +329,9 @@ class JvmCompile(NailgunTaskBase, GroupMember):
 
   def check_artifact_cache(self, vts):
     post_process_cached_vts = lambda cvts: self._strategy.post_process_cached_vts(cvts)
-    cachehitcallback=None#self._strategy.make_cache_hit_callback(vts)
-    cachehitcallback=self._strategy.create_cache_hit_callback(vts)
+    cache_hit_callback=self._strategy.create_cache_hit_callback(vts)
     return self.do_check_artifact_cache(vts, post_process_cached_vts=post_process_cached_vts,
-                                        cache_hit_callback=cachehitcallback)
+                                        cache_hit_callback=cache_hit_callback)
 
   def _create_empty_products(self):
     make_products = lambda: defaultdict(MultipleRootedProducts)
