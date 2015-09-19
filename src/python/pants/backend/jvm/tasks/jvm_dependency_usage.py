1c2fee9115328b712ff99360b413faad6b26f990
Stu Hood <stuhood@gmail.com>
2015-09-19 10:42:32 -0700

    Normalize dependency edges, and record self edges.

--- src/python/pants/backend/jvm/tasks/jvm_dependency_usage.py
+++ src/python/pants/backend/jvm/tasks/jvm_dependency_usage.py
@@ -15,6 +15,7 @@ from pants.backend.jvm.targets.jar_library import JarLibrary
 from pants.backend.jvm.tasks.jvm_compile.jvm_compile_isolated_strategy import create_size_estimators
 from pants.backend.jvm.tasks.jvm_dependency_analyzer import JvmDependencyAnalyzer
 from pants.base.build_environment import get_buildroot
+from pants.util.dirutil import fast_relpath
 from pants.util.fileutil import create_size_estimators
 
 
@@ -52,6 +53,12 @@ class JvmDependencyUsage(JvmDependencyAnalyzer):
     register('--output-file', type=str,
              help='Output dependency usage graph as JSON to the specified file.')
 
+  @classmethod
+  def prepare(cls, options, round_manager):
+    super(JvmDependencyUsage, cls).prepare(options, round_manager)
+    if not options.skip:
+      round_manager.require_data('classes_by_source')
+
   def execute(self):
     if self.get_options().skip:
       return
@@ -87,6 +94,23 @@ class JvmDependencyUsage(JvmDependencyAnalyzer):
     else:
       return True
 
+  def _normalize_product_dep(self, buildroot, classes_by_source, dep):
+    """Normalizes the given product dep from the given dep into a set of classfiles.
+
+    Product deps arrive as sources, jars, and classfiles: this method normalizes them to classfiles.
+
+    TODO: This normalization should happen in the super class.
+    """
+    if dep.endswith(".jar"):
+      # TODO: post sbt/zinc jar output patch, binary deps will be reported directly as classfiles
+      return set()
+    elif dep.endswith(".class"):
+      return set([dep])
+    else:
+      # assume a source file and convert to classfiles
+      rel_src = fast_relpath(dep, buildroot)
+      return set(p for _, paths in classes_by_source[rel_src].rel_paths() for p in paths)
+
   def create_dep_usage_graph(self, targets, buildroot):
     """Creates a graph of concrete targets, with their sum of products and dependencies.
 
@@ -94,6 +118,7 @@ class JvmDependencyUsage(JvmDependencyAnalyzer):
     """
 
     # Initialize all Nodes.
+    classes_by_source = self.context.products.get_data('classes_by_source')
     classes_by_target = self.context.products.get_data('classes_by_target')
     product_deps_by_src = self.context.products.get_data('product_deps_by_src')
     nodes = dict()
@@ -115,16 +140,18 @@ class JvmDependencyUsage(JvmDependencyAnalyzer):
         if self._select(derived_from):
           node.add_edge(Edge(True, set()), derived_from)
 
-      # Record the used products and undeclared Edges for this target.
+      # Record the used products and undeclared Edges for this target. Note that some of
+      # these may be self edges, which are considered later.
       target_product_deps_by_src = product_deps_by_src.get(target, dict())
       for src in target.sources_relative_to_buildroot():
         for product_dep in target_product_deps_by_src.get(os.path.join(buildroot, src), []):
           for dep_tgt in self.targets_by_file.get(product_dep, []):
             derived_from = dep_tgt.concrete_derived_from
-            if derived_from == concrete_target or not self._select(derived_from):
+            if not self._select(derived_from):
               continue
             is_declared = self._is_declared_dep(target, dep_tgt)
-            node.add_edge(Edge(is_declared, set([product_dep])), derived_from)
+            normalized_deps = self._normalize_product_dep(buildroot, classes_by_source, product_dep)
+            node.add_edge(Edge(is_declared, normalized_deps), derived_from)
 
     # Prune any Nodes with 0 products.
     for concrete_target, node in nodes.copy().items():
@@ -181,15 +208,23 @@ class DependencyUsageGraph(object):
       self._trans_job_size_cache[target] = self._job_size(target) + dep_sum
     return self._trans_job_size_cache[target]
 
+  def _edge_type(self, target, edge, dep):
+    if target == dep:
+      return 'self'
+    elif edge.is_declared:
+      return 'declared'
+    else:
+      return 'undeclared'
+
   def to_json(self):
     res_dict = {}
     def gen_dep_edge(node, edge, dep_tgt):
       dep_tgt_products_total = max(self._nodes[dep_tgt].products_total if dep_tgt in self._nodes else 1, 1)
       return {
         'target': dep_tgt.address.spec,
-        'is_declared': edge.is_declared,
+        'dependency_type': self._edge_type(node.concrete_target, edge, dep_tgt),
         'products_used': len(edge.products_used),
-        'products_used_ratio': 1.0 * len(edge.products_used) / dep_tgt_products_total
+        'products_used_ratio': len(edge.products_used) / dep_tgt_products_total,
       }
     for node in self._nodes.values():
       res_dict[node.concrete_target.address.spec] = {
