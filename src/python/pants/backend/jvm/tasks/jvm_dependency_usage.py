bd3e753e8cefcfa7c9b703ab116e5d858b942639
Stu Hood <stuhood@gmail.com>
2015-09-19 14:48:33 -0700

    Fix unit tests.

--- src/python/pants/backend/jvm/tasks/jvm_dependency_usage.py
+++ src/python/pants/backend/jvm/tasks/jvm_dependency_usage.py
@@ -179,6 +179,7 @@ class Node(object):
 
 class Edge(object):
   """Record a set of used products, and a boolean indicating that a depedency edge was declared."""
+
   def __init__(self, is_declared=False, products_used=None):
     self.products_used = products_used or set()
     self.is_declared = is_declared
--- tests/python/pants_test/backend/jvm/tasks/test_jvm_dependency_usage.py
+++ tests/python/pants_test/backend/jvm/tasks/test_jvm_dependency_usage.py
@@ -7,9 +7,8 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 
 from collections import defaultdict
 
+from pants.backend.jvm.targets.java_library import JavaLibrary
 from pants.backend.jvm.tasks.jvm_dependency_usage import JvmDependencyUsage
-from pants.base.payload import Payload
-from pants.base.payload_field import SourcesField
 from pants.goal.products import MultipleRootedProducts
 from pants_test.tasks.task_test_base import TaskTestBase
 
@@ -20,30 +19,23 @@ class TestJvmDependencyUsage(TaskTestBase):
   def task_type(cls):
     return JvmDependencyUsage
 
-  def _make_payload_from_sources(self, sources):
-    p = Payload()
-    p.add_field('sources', SourcesField('', sources))
-    p.freeze()
-    return p
-
-  def _setup_products(self, context):
+  def _setup(self):
+    context = self.context()
     classes_by_target = context.products.get_data('classes_by_target',
                                                   lambda: defaultdict(MultipleRootedProducts))
     product_deps_by_src = context.products.get_data('product_deps_by_src', dict)
-    return classes_by_target, product_deps_by_src
+    return context, classes_by_target, product_deps_by_src
+
+  def make_java_target(self, *args, **kwargs):
+    assert 'target_type' not in kwargs
+    return self.make_target(target_type=JavaLibrary, *args, **kwargs)
 
   def test_simple_dep_usage_graph(self):
-    t1 = self.make_target(spec=':t1',
-                          payload=self._make_payload_from_sources(['a.java', 'b.java']))
-    t2 = self.make_target(spec=':t2',
-                          payload=self._make_payload_from_sources(['c.java']),
-                          dependencies=[t1])
-    t3 = self.make_target(spec=':t3',
-                          payload=self._make_payload_from_sources(['d.java', 'e.java']),
-                          dependencies=[t1])
+    t1 = self.make_java_target(spec=':t1', sources=['a.java', 'b.java'])
+    t2 = self.make_java_target(spec=':t2', sources=['c.java'], dependencies=[t1])
+    t3 = self.make_java_target(spec=':t3', sources=['d.java', 'e.java'], dependencies=[t1])
     self.set_options(size_estimator='filecount')
-    context = self.context()
-    classes_by_target, product_deps_by_src = self._setup_products(context)
+    context, classes_by_target, product_deps_by_src = self._setup()
     classes_by_target[t1].add_rel_paths('', ['a.class', 'b.class'])
     classes_by_target[t2].add_rel_paths('', ['c.class'])
     classes_by_target[t3].add_rel_paths('', ['d.class', 'e.class'])
@@ -55,37 +47,32 @@ class TestJvmDependencyUsage(TaskTestBase):
     dep_usage = self.create_task(context)
     graph = dep_usage.create_dep_usage_graph([t1, t2, t3], '')
 
-    self.assertEqual(graph[t1].total_products, 2)
-    self.assertEqual(graph[t2].total_products, 1)
-    self.assertEqual(graph[t3].total_products, 2)
+    self.assertEqual(graph._nodes[t1].products_total, 2)
+    self.assertEqual(graph._nodes[t2].products_total, 1)
+    self.assertEqual(graph._nodes[t3].products_total, 2)
 
-    self.assertEqual(graph[t1].usage_by_dep, {})
-    self.assertEqual(graph[t2].usage_by_dep[graph[t1]], 1)
-    self.assertEqual(graph[t3].usage_by_dep[graph[t1]], 2)
-
-    usage_stats_by_node = graph._aggregate_product_usage_stats()
-    self.assertEqual(usage_stats_by_node[graph[t1]], (1, 2, 3))
-    self.assertEqual(usage_stats_by_node[graph[t2]], (0, 0, 0))
-    self.assertEqual(usage_stats_by_node[graph[t3]], (0, 0, 0))
+    self.assertEqual(graph._nodes[t1].dep_edges, {})
+    self.assertEqual(len(graph._nodes[t2].dep_edges[t1].products_used), 1)
+    self.assertEqual(len(graph._nodes[t3].dep_edges[t1].products_used), 2)
 
     self.assertEqual(graph._trans_job_size(t1), 2)
     self.assertEqual(graph._trans_job_size(t2), 3)
     self.assertEqual(graph._trans_job_size(t3), 4)
 
   def test_dep_usage_graph_with_synthetic_targets(self):
-    t1 = self.make_target(spec=':t1', payload=self._make_payload_from_sources(['t1.thrift']))
-    t1_x = self.make_target(spec=':t1.x', derived_from=t1)
-    t1_y = self.make_target(spec=':t1.y', derived_from=t1)
-    t1_z = self.make_target(spec=':t1.z', derived_from=t1)
-    t2 = self.make_target(spec=':t2',
-                          payload=self._make_payload_from_sources(['a.java', 'b.java']),
-                          dependencies=[t1, t1_x, t1_y, t1_z])
+    t1 = self.make_java_target(spec=':t1', sources=['t1.thrift'])
+    t1_x = self.make_java_target(spec=':t1.x', derived_from=t1)
+    t1_y = self.make_java_target(spec=':t1.y', derived_from=t1)
+    t1_z = self.make_java_target(spec=':t1.z', derived_from=t1)
+    t2 = self.make_java_target(spec=':t2',
+                               sources=['a.java', 'b.java'],
+                               dependencies=[t1, t1_x, t1_y, t1_z])
     self.set_options(size_estimator='nosize')
-    context = self.context()
-    classes_by_target, product_deps_by_src = self._setup_products(context)
+    context, classes_by_target, product_deps_by_src = self._setup()
     classes_by_target[t1_x].add_rel_paths('', ['x1.class'])
     classes_by_target[t1_y].add_rel_paths('', ['y1.class'])
     classes_by_target[t1_z].add_rel_paths('', ['z1.class', 'z2.class', 'z3.class'])
+    classes_by_target[t2].add_rel_paths('', ['a.class', 'b.class'])
     product_deps_by_src[t1] = {}
     product_deps_by_src[t1_x] = {}
     product_deps_by_src[t1_y] = {}
@@ -95,5 +82,5 @@ class TestJvmDependencyUsage(TaskTestBase):
     dep_usage = self.create_task(context)
     graph = dep_usage.create_dep_usage_graph([t1, t1_x, t1_y, t1_z, t2], '')
 
-    self.assertEqual(graph[t1].total_products, 5)
-    self.assertEqual(graph[t2].usage_by_dep[graph[t1]], 3)
+    self.assertEqual(graph._nodes[t1].products_total, 5)
+    self.assertEqual(len(graph._nodes[t2].dep_edges[t1].products_used), 3)
