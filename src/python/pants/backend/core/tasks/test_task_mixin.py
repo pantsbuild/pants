bdec5c3d1082f85d0bb129b370774e9db7e0f2b0
Sameer Brenn <sbrenn@twitter.com>
2015-10-02 10:06:48 -0600

    couple nits

--- src/python/pants/backend/core/tasks/test_task_mixin.py
+++ src/python/pants/backend/core/tasks/test_task_mixin.py
@@ -1,11 +1,11 @@
 # coding=utf-8
-# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-from abc import abstractmethod, abstractproperty
+from abc import abstractmethod
 
 
 class TestTaskMixin(object):
