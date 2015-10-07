57e6117de120b06c2922caffa65d456580d63f5c
Sameer Brenn <sbrenn@twitter.com>
2015-10-06 20:20:37 -0600

    catch the ugly case where the thread we care about isn't the main thread.

--- src/python/pants/util/timeout.py
+++ src/python/pants/util/timeout.py
@@ -15,18 +15,19 @@ class TimeoutReached(Exception):
 
 
 class Timeout(object):
-  """Timeout generator
+  """
+  Timeout generator. If seconds is None or 0, then the there is no timeout.
 
+    try:
       with Timeout(seconds):
-        try:
-          <do stuff>
-        except KeyboardInterrupt:
-          <handle timeout>
-
-      If seconds is None or 0, then the there is no timeout
+        <do stuff>
+    except TimeoutReached:
+      <handle timeout>
   """
 
   def __init__(self, seconds):
+    self._triggered = False
+
     if seconds:
       self._timer = threading.Timer(seconds, self._handle_timeout)
     else:
@@ -34,6 +35,7 @@ class Timeout(object):
 
   def _handle_timeout(self):
     sys.stderr.flush()  # Python 3 stderr is likely buffered.
+    self._triggered = True
     thread.interrupt_main()  # raises KeyboardInterrupt
 
   def __enter__(self):
@@ -41,7 +43,15 @@ class Timeout(object):
       self._timer.start()
 
   def __exit__(self, type_, value, traceback):
-    if type_ is not None and issubclass(type_, KeyboardInterrupt):
+    """
+    If triggered, raise TimeoutReached.
+
+    Rather than converting a KeyboardInterrupt to TimeoutReached here, we just check self._triggered,
+    which helps us in the case where the thread we are trying to timeout isn't the main thread. Of
+    course in that case the executing doesn't get interrupted at the appropriate time, but at least
+    the exception still gets raised.
+    """
+    if self._triggered:
       raise TimeoutReached
     elif self._timer is not None:
       self._timer.cancel()
