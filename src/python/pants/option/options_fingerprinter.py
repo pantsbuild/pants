a07f2a587892f453cef502b93ba26c26bba1827c
Stu Hood <stuhood@gmail.com>
2015-10-09 14:15:25 -0700

    Blargh. isort.

--- src/python/pants/option/options_fingerprinter.py
+++ src/python/pants/option/options_fingerprinter.py
@@ -8,7 +8,7 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import json
 from hashlib import sha1
 
-from pants.option.custom_types import file_option, target_option, target_list_option
+from pants.option.custom_types import file_option, target_list_option, target_option
 
 
 def stable_json_dumps(obj):
