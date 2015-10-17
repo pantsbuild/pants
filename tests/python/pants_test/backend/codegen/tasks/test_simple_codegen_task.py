103f729e2289dc3cea0241a2fb1729c24410b5de
Stu Hood <stuhood@gmail.com>
2015-10-16 17:45:13 -0700

    Collapse simple_codegen_task tests

--- tests/python/pants_test/backend/codegen/tasks/test_simple_codegen_task.py
+++ tests/python/pants_test/backend/codegen/tasks/test_simple_codegen_task.py
@@ -15,6 +15,7 @@ from pants.backend.jvm.targets.java_library import JavaLibrary
 from pants.backend.jvm.targets.jvm_target import JvmTarget
 from pants.base.exceptions import TaskError
 from pants.build_graph.build_file_aliases import BuildFileAliases
+from pants.util.dirutil import safe_mkdtemp
 from pants_test.tasks.task_test_base import TaskTestBase
 
 
@@ -30,11 +31,10 @@ class SimpleCodegenTaskTest(TaskTestBase):
       'dummy_library': SimpleCodegenTaskTest.DummyLibrary
     }))
 
-  def _create_dummy_task(self, target_roots=None, forced_codegen_strategy=None,
-                         hard_strategy_force=False, **options):
+  def _create_dummy_task(self, target_roots=None, **options):
     self.set_options(**options)
     task = self.create_task(self.context(target_roots=target_roots))
-    task.setup_for_testing(self, target_roots or [], forced_codegen_strategy, hard_strategy_force)
+    task.setup_for_testing(self, target_roots or [])
     return task
 
   def _create_dummy_library_targets(self, target_specs):
@@ -46,41 +46,6 @@ class SimpleCodegenTaskTest(TaskTestBase):
           )
         '''.format(name=spec_name)))
     return set([self.target(spec) for spec in target_specs])
-
-  def test_codegen_strategy(self):
-    self.set_options(strategy='isolated')
-    task = self.create_task(self.context())
-    self.assertEqual('isolated', task.codegen_strategy.name())
-
-  def test_codegen_workdir_suffix(self):
-    targets = self._create_dummy_library_targets([
-      'project/src/main/foogen/foo-lib:foo-target-a',
-      'project/src/main/foogen/foo-lib:foo-target-b',
-      'project/src/main/foogen/foo-bar:foo-target-a',
-      'project/src/main/genfoo/foo-bar:foo-target-a',
-    ])
-
-    task = self.create_task(self.context())
-
-    def get_suffix(target, strategy):
-      return task._codegen_strategy_for_name(strategy).codegen_workdir_suffix(target)
-
-    for target in targets:
-      self.assertTrue('isolated' in get_suffix(target, 'isolated'))
-
-    isolated_dirs = set(get_suffix(target, 'isolated') for target in targets)
-
-    self.assertEqual(len(targets), len(isolated_dirs),
-                     'There should be exactly one directory suffix per unique target!')
-
-  def test_codegen_workdir_suffix_stability(self):
-    specs = ['project/src/main/foogen/foo-lib:foo-target-a']
-    for target in self._create_dummy_library_targets(specs):
-      strategy = SimpleCodegenTask.IsolatedCodegenStrategy(None)
-      self.assertEqual(strategy.codegen_workdir_suffix(target),
-                        strategy.codegen_workdir_suffix(target),
-                        'Codegen workdir suffix should be stable given the same target!\n'
-                        '  target: {}'.format(target.address.spec))
 
   def _test_execute_strategy(self, strategy, expected_execution_count):
     dummy_suffixes = ['a', 'b', 'c']
@@ -112,9 +77,6 @@ class SimpleCodegenTaskTest(TaskTestBase):
     self._test_execute_strategy('isolated', 3)
 
   def test_execute_fail(self):
-    # Ensure whichever strategy is selected, it actually call execute_codegen to trigger our
-    # DummyTask `should_fail` logic.  The isolated strategy, for example, short circuits that call
-    # if there are no targets.
     dummy = self.make_target(spec='dummy', target_type=self.DummyLibrary)
     task = self._create_dummy_task(target_roots=[dummy])
     task.should_fail = True
@@ -155,12 +117,13 @@ class SimpleCodegenTaskTest(TaskTestBase):
     targets = self._get_duplication_test_targets()
     parent, good, bad = targets
     task = self._create_dummy_task(target_roots=targets, strategy='isolated', allow_dups=False)
+    target_workdirs = {t: safe_mkdtemp() for t in targets}
     for target in targets:
-      task.execute_codegen([target])
+      task.execute_codegen(target, target_workdirs[target])
 
     task = self._create_dummy_task(target_roots=targets, strategy='isolated', allow_dups=False)
-    with self.assertRaises(SimpleCodegenTask.IsolatedCodegenStrategy.DuplicateSourceError) as cm:
-      task.codegen_strategy.find_sources(bad)
+    with self.assertRaises(SimpleCodegenTask.DuplicateSourceError) as cm:
+      task.find_sources(bad, target_workdirs)
     should_contain = ['org/pantsbuild/example/ParentClass']
     should_not_contain = ['org/pantsbuild/example/ChildClass']
     message = str(cm.exception)
@@ -171,22 +134,15 @@ class SimpleCodegenTaskTest(TaskTestBase):
 
     # Should error same as above. Just to make sure the flag exists when the codegen strategy
     # is forced.
-    task = self._create_dummy_task(target_roots=targets, strategy='isolated', allow_dups=False,
-                                   forced_codegen_strategy='isolated')
-    with self.assertRaises(SimpleCodegenTask.IsolatedCodegenStrategy.DuplicateSourceError):
-      task.codegen_strategy.find_sources(bad)
+    task = self._create_dummy_task(target_roots=targets, strategy='isolated', allow_dups=False)
+    with self.assertRaises(SimpleCodegenTask.DuplicateSourceError):
+      task.find_sources(bad, target_workdirs)
 
     task = self._create_dummy_task(target_roots=targets, strategy='isolated', allow_dups=True)
-    task.codegen_strategy.find_sources(bad)  # Should not raise error, only warning.
+    task.find_sources(bad, target_workdirs)  # Should not raise error, only warning.
 
     task = self._create_dummy_task(target_roots=targets, strategy='isolated', allow_dups=False)
-    task.codegen_strategy.find_sources(good)  # Should be completely fine.
-
-  def test_unsupported_strategy_error(self):
-    task = self._create_dummy_task(target_roots=[], forced_codegen_strategy='potato',
-                                   hard_strategy_force=True)
-    with self.assertRaises(SimpleCodegenTask.UnsupportedStrategyError):
-      task.codegen_strategy
+    task.find_sources(good, target_workdirs)  # Should be completely fine.
 
   class DummyLibrary(JvmTarget):
     """Library of .dummy files, which are just text files which generate empty java files.
@@ -213,8 +169,6 @@ class SimpleCodegenTaskTest(TaskTestBase):
     the methods in this class perform some validation to ensure that they are being called correctly
     by SimpleCodegenTask.
     """
-    _forced_codegen_strategy = None
-    _hard_forced_codegen_strategy = None
 
     def __init__(self, *vargs, **kwargs):
       super(SimpleCodegenTaskTest.DummyGen, self).__init__(*vargs, **kwargs)
@@ -224,66 +178,37 @@ class SimpleCodegenTaskTest(TaskTestBase):
       self.should_fail = False
       self.execution_counts = 0
 
-    def setup_for_testing(self, test_case, all_targets, forced_codegen_strategy=None,
-                          hard_strategy_force=False):
+    def setup_for_testing(self, test_case, all_targets):
       """Gets this dummy generator class ready for testing.
 
       :param TaskTestBase test_case: the 'parent' test-case using this task. Used for asserts, etc.
       :param set all_targets: the set of all valid code-gen targets for this task, for validating
         the correctness of the chosen strategy.
-      :param str forced_codegen_strategy: the name of the forced codegen strategy ('isolated' or
-        'global') is this task should force a particular strategy, or None if no strategy should be
-        forced.
-      :param bool hard_strategy_force: if true, absolutely forces the codegen strategy to be the
-        of the given type, against all reason, even if it's unimplemented.
       """
       self._test_case = test_case
       self._all_targets = all_targets
       cls = type(self)
-      cls._forced_codegen_strategy = forced_codegen_strategy
-      cls._hard_forced_codegen_strategy = forced_codegen_strategy if hard_strategy_force else None
-
-    @classmethod
-    def forced_codegen_strategy(cls):
-      if cls._hard_forced_codegen_strategy is None:
-        return super(SimpleCodegenTaskTest.DummyGen, cls).forced_codegen_strategy()
-      return cls._hard_forced_codegen_strategy
-
-    @classmethod
-    def supported_strategy_types(cls):
-      if cls._forced_codegen_strategy is None or cls._hard_forced_codegen_strategy:
-        return [cls.IsolatedCodegenStrategy]
-      elif cls._forced_codegen_strategy == 'isolated':
-        return [cls.IsolatedCodegenStrategy]
-      raise ValueError('Unrecognized _forced_codegen_strategy for test ({}).'
-                       .format(cls._forced_codegen_strategy))
 
     def is_gentarget(self, target):
       return isinstance(target, SimpleCodegenTaskTest.DummyLibrary)
 
-    def execute_codegen(self, invalid_targets):
+    def execute_codegen(self, target, target_workdir):
       self.execution_counts += 1
       if self.should_fail: raise TaskError('Failed to generate target(s)')
-      if self.codegen_strategy.name() == 'isolated':
-        self._test_case.assertEqual(1, len(invalid_targets),
-                                    'Codegen should execute individually in isolated mode.')
-      else:
-        raise ValueError('Unknown codegen strategy "{}".'.format(self.codegen_strategy.name()))
 
-      for target in invalid_targets:
-        for path in self._dummy_sources_to_generate(target):
-          class_name = os.path.basename(path).split('.')[0]
-          package_name = os.path.relpath(os.path.dirname(path),
-                                         self.codegen_workdir(target)).replace(os.path.sep, '.')
-          if not os.path.exists(os.path.join(self._test_case.build_root, os.path.basename(path))):
-            self._test_case.create_dir(os.path.basename(path))
-          self._test_case.create_file(path)
-          with open(path, 'w') as f:
-            f.write('package {0};\n\n'.format(package_name))
-            f.write('public class {0} '.format(class_name))
-            f.write('{\n\\\\ ... nothing ... \n}\n')
+      for path in self._dummy_sources_to_generate(target, target_workdir):
+        class_name = os.path.basename(path).split('.')[0]
+        package_name = os.path.relpath(os.path.dirname(path),
+                                       target_workdir).replace(os.path.sep, '.')
+        if not os.path.exists(os.path.join(self._test_case.build_root, os.path.basename(path))):
+          self._test_case.create_dir(os.path.basename(path))
+        self._test_case.create_file(path)
+        with open(path, 'w') as f:
+          f.write('package {0};\n\n'.format(package_name))
+          f.write('public class {0} '.format(class_name))
+          f.write('{\n\\\\ ... nothing ... \n}\n')
 
-    def _dummy_sources_to_generate(self, target):
+    def _dummy_sources_to_generate(self, target, target_workdir):
       for source in target.sources_relative_to_buildroot():
         source = os.path.join(self._test_case.build_root, source)
         with open(source, 'r') as f:
@@ -291,15 +216,7 @@ class SimpleCodegenTaskTest(TaskTestBase):
             line = line.strip()
             if line:
               package_name, class_name = line.split(' ')
-              yield os.path.join(self.codegen_workdir(target),
-                                 os.path.join(*package_name.split('.')),
-                                 class_name)
-
-    def _find_sources_generated_by_target(self, target):
-      self._test_case.assertEqual('isolated', self.codegen_strategy.name(),
-                            '_find_sources_generated_by_target should only be called for '
-                            'strategy=isolated.')
-      return super(SimpleCodegenTaskTest.DummyGen, self)._find_sources_generated_by_target(target)
+              yield os.path.join(target_workdir, os.path.join(*package_name.split('.')), class_name)
 
     def synthetic_target_type(self, target):
       return JavaLibrary
