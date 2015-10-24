f76ad6cf229dbdae262a92f06fb13c9a0e76073f
Stu Hood <stuhood@gmail.com>
2015-10-24 10:11:32 -0700

    Undo the unnecessary complication from yesterday.

--- tests/python/pants_test/backend/core/tasks/test_task.py
+++ tests/python/pants_test/backend/core/tasks/test_task.py
@@ -8,8 +8,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import os
 import shutil
 
-import pytest
-
 from pants.backend.core.tasks.task import Task
 from pants.base.build_environment import get_buildroot
 from pants.base.payload import Payload
@@ -38,20 +36,20 @@ class DummyTask(Task):
     return True
 
   def execute(self):
-    with self.invalidated(self.context.targets(), invalidate_dependents=True) as invalidation:
-      for vt in invalidation.invalid_vts:
-        with open(os.path.join(get_buildroot(), vt.target.source), 'r') as infile:
-          outfile_name = os.path.join(vt.results_dir, os.path.basename(vt.target.source))
-          with open(outfile_name, 'a') as outfile:
-            outfile.write(infile.read())
-        vt.update()
-      return tuple(sorted(invalidation.invalid_vts, key=lambda v: v.target.address.spec))
+    with self.invalidated(self.context.targets()) as invalidation:
+      assert len(invalidation.invalid_vts) == 1
+      vt = invalidation.invalid_vts[0]
+      with open(os.path.join(get_buildroot(), vt.target.source), 'r') as infile:
+        outfile_name = os.path.join(vt.results_dir, os.path.basename(vt.target.source))
+        with open(outfile_name, 'a') as outfile:
+          outfile.write(infile.read())
+      vt.update()
+      return vt
 
 
 class TaskTest(TaskTestBase):
 
-  _filename = 'downstream'
-  _dep_filename = 'upstream'
+  _filename = 'f'
 
   @classmethod
   def task_type(cls):
@@ -62,20 +60,14 @@ class TaskTest(TaskTestBase):
       self.assertEquals(f.read(), content)
 
   def _fixture(self, incremental):
-    """One downstream target, and one upstream target."""
-    dep = self.make_target(':a', target_type=DummyLibrary, source=self._dep_filename)
-    self._create_clean_file(dep, 'upstream')
-    target = self.make_target(':b',
-                              target_type=DummyLibrary,
-                              source=self._filename,
-                              dependencies=[dep])
+    target = self.make_target(':t', target_type=DummyLibrary, source=self._filename)
     context = self.context(target_roots=[target])
     task = self.create_task(context)
     task._incremental = incremental
-    return target, dep, task
+    return target, task
 
   def _create_clean_file(self, target, content):
-    self.create_file(target.source, content)
+    self.create_file(self._filename, content)
     target.mark_invalidation_hash_dirty()
 
   def test_incremental(self):
@@ -83,21 +75,21 @@ class TaskTest(TaskTestBase):
 
     one = '1\n'
     two = '2\n'
-    target, _, task = self._fixture(incremental=True)
+    target, task = self._fixture(incremental=True)
 
     # Clean.
     self._create_clean_file(target, one)
-    _, vtA = task.execute()
+    vtA = task.execute()
     self.assertContent(vtA, one)
 
     # Cloned from vtA.
     self._create_clean_file(target, two)
-    vtB, = task.execute()
+    vtB = task.execute()
     self.assertContent(vtB, one + two)
 
     # Incremental atop existing directory for vtA.
     self._create_clean_file(target, one)
-    vtC, = task.execute()
+    vtC = task.execute()
     self.assertContent(vtC, one + one)
 
     # Confirm that there were two results dirs, and that the second was cloned.
@@ -107,48 +99,19 @@ class TaskTest(TaskTestBase):
     self.assertNotEqual(vtA.results_dir, vtB.results_dir)
     self.assertEqual(vtA.results_dir, vtC.results_dir)
 
-  # See https://github.com/pantsbuild/pants/issues/2446
-  @pytest.mark.xfail
-  def test_incremental_transitive_invalidation(self):
-    """Run twice, with an upstream fingerprint change.
-
-    TODO: It's possible that the current behaviour is buggy/unexpected: the vt.cache_key
-    should probably contain the transitive fingerprint, rather than the intransitive fingerprint.
-    """
-
-    one = '1\n'
-    two = '2\n'
-    target, dep, task = self._fixture(incremental=True)
-
-    # Run once.
-    self._create_clean_file(target, one)
-    _, vtA = task.execute()
-    self.assertContent(vtA, one)
-
-    # Then again with the upstream dep invalidated.
-    self._create_clean_file(dep, 'irrelevant!')
-    target.mark_invalidation_hash_dirty()
-    _, vtB = task.execute()
-    self.assertContent(vtB, one + one)
-
-    # Confirm that there is only one resuls dir.
-    self.assertContent(vtA, one + one)
-    self.assertContent(vtB, one + one)
-    self.assertEqual(vtA.results_dir, vtB.results_dir)
-
   def test_non_incremental(self):
     """Non-incremental should be completely unassociated."""
 
     one = '1\n'
     two = '2\n'
-    target, _, task = self._fixture(incremental=False)
+    target, task = self._fixture(incremental=False)
 
     # Run twice.
     self._create_clean_file(target, one)
-    _, vtA = task.execute()
+    vtA = task.execute()
     self.assertContent(vtA, one)
     self._create_clean_file(target, two)
-    vtB, = task.execute()
+    vtB = task.execute()
 
     # Confirm two unassociated directories.
     self.assertContent(vtA, one)
