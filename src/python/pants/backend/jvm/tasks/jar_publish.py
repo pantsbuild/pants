452ad6a8fa5707db6bda4fc11ba17ebff5132481
Timur Abishev <abishev.timur@gmail.com>
2015-10-28 12:00:34 -0700

    Fix jar_publish to work with files outside build root in case of generated files

--- src/python/pants/backend/jvm/tasks/jar_publish.py
+++ src/python/pants/backend/jvm/tasks/jar_publish.py
@@ -871,8 +871,10 @@ class JarPublish(ScmPublishMixin, JarTask):
     return sha.hexdigest()
 
   def changelog(self, target, sha):
+    # todo: temporary fix should be discussed, filtering synthetic files
+    files = [fi for fi in target.sources_relative_to_buildroot() if not fi.startswith(os.pardir)]
     return ensure_text(self.scm.changelog(from_commit=sha,
-                                          files=target.sources_relative_to_buildroot()))
+                                          files=files))
 
   def fetch_ivysettings(self, ivy):
     if self.get_options().ivy_settings:
