ae763d45c8afb75de45d1361fa4d8ae47ed2c9ec
Timur Abishev <abishev.timur@gmail.com>
2015-10-29 17:27:48 -0700

    Remove directory checking from clean task

--- src/python/pants/backend/core/tasks/clean.py
+++ src/python/pants/backend/core/tasks/clean.py
@@ -8,25 +8,14 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import os
 
 from pants.backend.core.tasks.task import Task
-from pants.base.build_environment import PANTS_WORKDIR_SUFFIX
-from pants.base.exceptions import TaskError
 from pants.util.dirutil import safe_rmtree
-
-
-def _cautious_get_working_directory(task):
-  """Check that working directory ends with right suffix before make destructive actions"""
-  workdir = task.get_options().pants_workdir
-  if not workdir.endswith(PANTS_WORKDIR_SUFFIX):
-    raise TaskError('DANGER: Attempting to use working directory {}, which is not ends with \'.pants.d\'!'
-                    .format(workdir))
-  return workdir
 
 
 class Invalidator(Task):
   """Invalidate the entire build."""
 
   def execute(self):
-    build_invalidator_dir = os.path.join(_cautious_get_working_directory(self), 'build_invalidator')
+    build_invalidator_dir = os.path.join(self.get_options().pants_workdir, 'build_invalidator')
     safe_rmtree(build_invalidator_dir)
 
 
@@ -34,4 +23,4 @@ class Cleaner(Task):
   """Clean all current build products."""
 
   def execute(self):
-    safe_rmtree(_cautious_get_working_directory(self))
+    safe_rmtree(self.get_options().pants_workdir)
