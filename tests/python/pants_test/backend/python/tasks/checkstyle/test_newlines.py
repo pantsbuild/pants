a0f70d66c82e820d8f38cd9aaa0288ad65bd33ad
Benjy <benjy@foursquare.com>
2015-10-30 01:40:23 +0000

    Refactor the python checkstyle plugin system.
    Now instead of the plugins knowing about their corresponding
    subsystems, the subsystems know about (and create) the plugins.
    
    This allows us to only import (and therefore parse and interpret)
    code we're actually going to use.  Performance profiles show that
    almost a second of startup time is spent just loading backends,
    and checkstyle was a good chunk of that.
    
    If we like how this works, we can contemplate something similar
    for backend code in general. Right now we load huge amounts of code
    even if we never invoke it.
    
    This change also creates some useful unittest infrastructure for
    testing these checkstyle plugins. As a result it switches all
    the relevant tests to use our idiomatic style (self.assertTrue
    instead of assert keyword, and so on).

--- tests/python/pants_test/backend/python/tasks/checkstyle/test_newlines.py
+++ tests/python/pants_test/backend/python/tasks/checkstyle/test_newlines.py
@@ -5,103 +5,83 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-from pants.backend.python.tasks.checkstyle.common import Nit, PythonFile
 from pants.backend.python.tasks.checkstyle.newlines import Newlines
+from pants_test.backend.python.tasks.checkstyle.plugin_test_base import CheckstylePluginTestBase
 
 
-TOPLEVEL = """
-def foo():
-  pass%s
-%s
-  pass
-"""
+class NewlinesTest(CheckstylePluginTestBase):
+  plugin_type = Newlines
 
-
-def test_newlines():
-  for toplevel_def in ('def bar():', 'class Bar(object):'):
-    for num_newlines in (0, 1, 3, 4):
-      newlines = Newlines(PythonFile.from_statement(TOPLEVEL % ('\n' * num_newlines, toplevel_def)))
-      nits = list(newlines.nits())
-      assert len(nits) == 1
-      assert nits[0].code == 'T302'
-      assert nits[0].severity == Nit.ERROR
-    newlines = Newlines(PythonFile.from_statement(TOPLEVEL % ('\n\n', toplevel_def)))
-    assert len(list(newlines.nits())) == 0
-
-
-GOOD_CLASS_DEF_1 = """
-class Foo(object):
-  def __init__(self):
+  TOPLEVEL = """
+  def foo():
+    pass{}
+  {}
     pass
+  """
 
-  def bar(self):
-    pass
-"""
+  def test_newlines(self):
+    for toplevel_def in ('def bar():', 'class Bar(object):'):
+      for num_newlines in (0, 1, 3, 4):
+        statement = self.TOPLEVEL.format('\n' * num_newlines, toplevel_def)
+        self.assertNit(statement, 'T302')
+      statement = self.TOPLEVEL.format('\n\n', toplevel_def)
+      self.assertNoNits(statement)
 
-GOOD_CLASS_DEF_2 = """
-class Foo(object):
-  def __init__(self):
-    pass
+  GOOD_CLASS_DEF_1 = """
+  class Foo(object):
+    def __init__(self):
+      pass
 
-  # this should be fine
-  def bar(self):
-    pass
-"""
+    def bar(self):
+      pass
+  """
 
+  GOOD_CLASS_DEF_2 = """
+  class Foo(object):
+    def __init__(self):
+      pass
 
-GOOD_CLASS_DEF_3 = """
-class Foo(object):
-  class Error(Exception): pass
-  class SomethingError(Error): pass
+    # this should be fine
+    def bar(self):
+      pass
+  """
 
-  def __init__(self):
-    pass
+  GOOD_CLASS_DEF_3 = """
+  class Foo(object):
+    class Error(Exception): pass
+    class SomethingError(Error): pass
 
-  def bar(self):
-    pass
-"""
+    def __init__(self):
+      pass
 
+    def bar(self):
+      pass
+  """
 
-BAD_CLASS_DEF_1 = """
-class Foo(object):
-  class Error(Exception): pass
-  class SomethingError(Error): pass
-  def __init__(self):
-    pass
+  BAD_CLASS_DEF_1 = """
+  class Foo(object):
+    class Error(Exception): pass
+    class SomethingError(Error): pass
+    def __init__(self):
+      pass
 
-  def bar(self):
-    pass
-"""
+    def bar(self):
+      pass
+  """
 
-BAD_CLASS_DEF_2 = """
-class Foo(object):
-  class Error(Exception): pass
-  class SomethingError(Error): pass
+  BAD_CLASS_DEF_2 = """
+  class Foo(object):
+    class Error(Exception): pass
+    class SomethingError(Error): pass
 
-  def __init__(self):
-    pass
-  def bar(self):
-    pass
-"""
+    def __init__(self):
+      pass
+    def bar(self):
+      pass
+  """
 
-
-def test_classdefs():
-  newlines = Newlines(PythonFile.from_statement(GOOD_CLASS_DEF_1))
-  assert len(list(newlines.nits())) == 0
-
-  newlines = Newlines(PythonFile.from_statement(GOOD_CLASS_DEF_2))
-  assert len(list(newlines.nits())) == 0
-
-  newlines = Newlines(PythonFile.from_statement(BAD_CLASS_DEF_1))
-  nits = list(newlines.nits())
-  assert len(nits) == 1
-  assert nits[0].code == 'T301'
-  assert nits[0]._line_number == 4
-  assert nits[0].severity == Nit.ERROR
-
-  newlines = Newlines(PythonFile.from_statement(BAD_CLASS_DEF_2))
-  nits = list(newlines.nits())
-  assert len(nits) == 1
-  assert nits[0].code == 'T301'
-  assert nits[0]._line_number == 7
-  assert nits[0].severity == Nit.ERROR
+  def test_classdefs(self):
+    self.assertNoNits(self.GOOD_CLASS_DEF_1)
+    self.assertNoNits(self.GOOD_CLASS_DEF_2)
+    self.assertNit(self.BAD_CLASS_DEF_1, 'T301', expected_line_number=4)
+    self.assertNit(self.BAD_CLASS_DEF_2, 'T301', expected_line_number=7)
