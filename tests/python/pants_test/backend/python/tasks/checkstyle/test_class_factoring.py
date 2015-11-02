8802c8c07af07a5fe81ba3f463ebdf1162a62e1b
Benjy Weinberger <benjyw@gmail.com>
2015-11-02 18:23:14 +0000

    Refactor the python checkstyle plugin system.
    Now instead of the plugins knowing about their corresponding
    subsystems, the subsystems know about (and create) the plugins.
    
    This allows us to only import (and therefore parse and interpret)
    code we're actually going to use.  Performance profiles show that
    almost a second of startup time is spent just loading backends,
    and checkstyle was a good chunk of that.
    
    If we like how this works, we can contemplate something similar
    for backend code in general. Right now we load huge amounts of code
    even if we never invoke it.
    
    This change also creates some useful unittest infrastructure for
    testing these checkstyle plugins. As a result it switches all
    the relevant tests to use our idiomatic style (self.assertTrue
    instead of assert keyword, and so on).
    
    Testing Done:
    CI passes: https://travis-ci.org/pantsbuild/pants/builds/88461495
    
    Reviewed at https://rbcommons.com/s/twitter/r/3061/

--- tests/python/pants_test/backend/python/tasks/checkstyle/test_class_factoring.py
+++ tests/python/pants_test/backend/python/tasks/checkstyle/test_class_factoring.py
@@ -6,21 +6,21 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 from pants.backend.python.tasks.checkstyle.class_factoring import ClassFactoring
-from pants.backend.python.tasks.checkstyle.common import Nit, PythonFile
+from pants.backend.python.tasks.checkstyle.common import Nit
+from pants_test.backend.python.tasks.checkstyle.plugin_test_base import CheckstylePluginTestBase
 
 
-BAD_CLASS = PythonFile.from_statement("""
+BAD_CLASS = """
 class Distiller(object):
   CONSTANT = "foo"
 
   def foo(self, value):
     return os.path.join(Distiller.CONSTANT, value)
-""")
+"""
 
 
-def test_class_factoring():
-  plugin = ClassFactoring(BAD_CLASS)
-  nits = list(plugin.nits())
-  assert len(nits) == 1
-  assert nits[0].code == 'T800'
-  assert nits[0].severity == Nit.WARNING
+class ClassFactoringTest(CheckstylePluginTestBase):
+  plugin_type = ClassFactoring
+
+  def test_class_factoring(self):
+    self.assertNit(BAD_CLASS, 'T800', Nit.WARNING)
