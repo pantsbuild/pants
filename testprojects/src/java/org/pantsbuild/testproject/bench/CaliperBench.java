3f307a3e314e156525a3e07946c19fbe29fba5ca
Nick Howard <nhoward@twitter.com>
2015-11-03 14:27:51 -0700

    adjust ws in bench class

--- testprojects/src/java/org/pantsbuild/testproject/bench/CaliperBench.java
+++ testprojects/src/java/org/pantsbuild/testproject/bench/CaliperBench.java
@@ -8,9 +8,10 @@ import java.util.Set;
 
 public class CaliperBench extends SimpleBenchmark {
 
-  public static final int ADDS_PER_REP = 1000;
+  static final int ADDS_PER_REP = 1000;
 
-  Set<Integer> set = new HashSet<Integer>();
+  final Set<Integer> set = new HashSet<Integer>();
+
   public void timeHashSetAdd(int reps) {
     for (int repNo = 0; repNo < reps; repNo++) {
       for (int i = 0; i < ADDS_PER_REP; i++) {
