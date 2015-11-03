f5320f47aaac0acb047337ad0e7b42ccea6cdaee
Nick Howard <nhoward@twitter.com>
2015-11-03 14:25:26 -0700

    rm unused field

--- testprojects/src/java/org/pantsbuild/testproject/bench/CaliperBench.java
+++ testprojects/src/java/org/pantsbuild/testproject/bench/CaliperBench.java
@@ -7,8 +7,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class CaliperBench extends SimpleBenchmark {
+
   public static final int ADDS_PER_REP = 1000;
-  int x = 0;
+
   Set<Integer> set = new HashSet<Integer>();
   public void timeHashSetAdd(int reps) {
     for (int repNo = 0; repNo < reps; repNo++) {
