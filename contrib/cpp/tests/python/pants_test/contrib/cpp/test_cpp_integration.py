76d6b8c926c021fd3c49b25f47bd0dad510a3653
John Sirois <john.sirois@gmail.com>
2015-11-06 13:47:33 -0700

    Remove pytest helpers where unittest will do.
    This is a global cleanup.  Some instances of pytest helper use are left
    but they either are in tests that use test functions or where there is
    no equivalent functionality in unittest(2).

--- contrib/cpp/tests/python/pants_test/contrib/cpp/test_cpp_integration.py
+++ contrib/cpp/tests/python/pants_test/contrib/cpp/test_cpp_integration.py
@@ -5,11 +5,19 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-import pytest
-from pants.util.contextutil import temporary_dir
+from unittest import skipUnless
+
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 from pants.contrib.cpp.toolchain.cpp_toolchain import CppToolchain
+
+
+def have_compiler():
+  try:
+    CppToolchain().compiler
+    return True
+  except CppToolchain.Error:
+    return False
 
 
 class CppIntegrationTest(PantsRunIntegrationTest):
@@ -22,46 +30,34 @@ class CppIntegrationTest(PantsRunIntegrationTest):
   TEST_LIBRARY_TARGET = 'contrib/cpp/examples/src/cpp/example/hello'
   TEST_RUN_TARGET = TEST_SIMPLE_BINARY_TARGET
 
-  @classmethod
-  def has_compiler(cls):
-    try:
-      CppToolchain().compiler
-      return True
-    except CppToolchain.Error:
-      return False
+  skipUnlessHaveCompiler = skipUnless(have_compiler(),
+                                      reason='cpp integration tests require compiler')
 
-  @pytest.mark.skipif('not CppIntegrationTest.has_compiler()',
-                      reason='cpp integration tests require compiler')
+  @skipUnlessHaveCompiler
   def test_cpp_library(self):
     self._binary_test(self.TEST_LIBRARY_TARGET)
 
-  @pytest.mark.skipif('not CppIntegrationTest.has_compiler()',
-                      reason='cpp integration tests require compiler')
+  @skipUnlessHaveCompiler
   def test_cpp_library_compile(self):
     self._compile_test(self.TEST_LIBRARY_TARGET)
 
-  @pytest.mark.skipif('not CppIntegrationTest.has_compiler()',
-                      reason='cpp integration tests require compiler')
+  @skipUnlessHaveCompiler
   def test_cpp_binary(self):
     self._binary_test(self.TEST_SIMPLE_BINARY_TARGET)
 
-  @pytest.mark.skipif('not CppIntegrationTest.has_compiler()',
-                      reason='cpp integration tests require compiler')
+  @skipUnlessHaveCompiler
   def test_cpp_binary_compile(self):
     self._compile_test(self.TEST_SIMPLE_BINARY_TARGET)
 
-  @pytest.mark.skipif('not CppIntegrationTest.has_compiler()',
-                      reason='cpp integration tests require compiler')
+  @skipUnlessHaveCompiler
   def test_cpp_binary_with_library(self):
     self._binary_test(self.TEST_BINARY_WITH_LIBRARY_TARGET)
 
-  @pytest.mark.skipif('not CppIntegrationTest.has_compiler()',
-                      reason='cpp integration tests require compiler')
+  @skipUnlessHaveCompiler
   def test_cpp_binary_with_library_compile(self):
     self._compile_test(self.TEST_BINARY_WITH_LIBRARY_TARGET)
 
-  @pytest.mark.skipif('not CppIntegrationTest.has_compiler()',
-                      reason='cpp integration tests require compiler')
+  @skipUnlessHaveCompiler
   def test_cpp_run(self):
     pants_run = self.run_pants(['run', self.TEST_RUN_TARGET])
     self.assert_success(pants_run)
