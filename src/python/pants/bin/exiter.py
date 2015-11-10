dbea04908a09ef4dfa7438b111405293c96ed38c
Kris Wilson <kwilson@twitter.com>
2015-11-10 13:42:01 -0800

    Address John/Stu's feedback.

--- src/python/pants/bin/exiter.py
+++ src/python/pants/bin/exiter.py
@@ -10,32 +10,66 @@ import traceback
 
 
 class Exiter(object):
-  def __init__(self):
+  """A class that provides standard runtime exit and global exception handling behavior.
+
+  The expected method call order of this class is as follows:
+
+   1) Call Exiter.set_except_hook() to set sys.excepthook to the internal exception hook. This
+      should happen as early as possible to ensure any/all exceptions are handled by the hook.
+   2) Call Exiter.apply_options() to set traceback printing behavior via an Options object.
+   3) Perform other operations as normal.
+   4) Call Exiter.exit(), Exiter.exit_and_fail() or exiter_inst() when you wish to exit the runtime.
+  """
+
+  def __init__(self, exiter=sys.exit, formatter=traceback.format_tb, print_backtraces=True):
+    """
+    :param func exiter: A function to be called to conduct the final exit of the runtime. (Optional)
+    :param func formatter: A function to be called to format any encountered tracebacks. (Optional)
+    :param bool print_backtraces: Whether or not to print backtraces by default. Can be
+                                  overridden by Exiter.apply_options(). (Optional)
+    """
     # Since we have some exit paths that run via the sys.excepthook,
     # symbols we use can become garbage collected before we use them; ie:
     # we can find `sys` and `traceback` are `None`.  As a result we capture
     # all symbols we need here to ensure we function in excepthook context.
     # See: http://stackoverflow.com/questions/2572172/referencing-other-modules-in-atexit
-    self._exit = sys.exit
-    self._format_tb = traceback.format_tb
-    self._should_print_backtrace = True
+    self._exit = exiter
+    self._format_tb = formatter
+    self._should_print_backtrace = print_backtraces
 
   def __call__(self, *args, **kwargs):
     """Map class calls to self.exit() to support sys.exit() fungibility."""
     return self.exit(*args, **kwargs)
 
   def apply_options(self, options):
+    """Applies global configuration options to internal behavior.
+
+    :param Options options: An instance of an Options object to fetch global options from.
+    """
     self._should_print_backtrace = options.for_global_scope().print_exception_stacktrace
 
   def exit(self, result=0, msg=None, out=sys.stderr):
+    """Exits the runtime.
+
+    :param result: The exit status. Typically a 0 indicating success or a 1 indicating failure, but
+                   can be a string as well. (Optional)
+    :param msg: A string message to print to stderr or another custom file desciptor before exiting.
+                (Optional)
+    :param out: The file descriptor to emit `msg` to. (Optional)
+    """
     if msg:
       print(msg, file=out)
     self._exit(result)
 
   def exit_and_fail(self, msg=None):
+    """Exits the runtime with an exit code of 1, indicating failure.
+
+    :param str msg: A string message to print to stderr before exiting. (Optional)
+    """
     self.exit(result=1, msg=msg)
 
-  def unhandled_exception_hook(self, exception_class, exception, tb):
+  def _unhandled_exception_hook(self, exception_class, exception, tb):
+    """Default sys.excepthook implementation for unhandled exceptions, used by set_except_hook()."""
     msg = ''
     if self._should_print_backtrace:
       msg = '\nException caught: ({})\n{}'.format(type(exception), ''.join(self._format_tb(tb)))
@@ -47,5 +81,5 @@ class Exiter(object):
     self.exit_and_fail(msg)
 
   def set_except_hook(self):
-    # Call the registration of the unhandled exception hook as early as possible in the code.
-    sys.excepthook = self.unhandled_exception_hook
+    """Sets the global exception hook."""
+    sys.excepthook = self._unhandled_exception_hook
