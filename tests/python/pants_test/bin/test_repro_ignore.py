d3d833468af6dd8005c3a5456c3718ef54d244f0
Matt Olsen <matt@digital-wanderlust.com>
2015-11-13 13:22:23 -0800

    switch to using unittest.TestCase

--- tests/python/pants_test/bin/test_repro_ignore.py
+++ tests/python/pants_test/bin/test_repro_ignore.py
@@ -6,56 +6,54 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 import os
-import shutil
+import unittest
 from functools import partial
 
-from pants.backend.core.tasks.clean import Cleaner
+from pants.base.build_root import BuildRoot
 from pants.bin.repro import Reproducer
 from pants.fs.archive import TGZ
-from pants.util.contextutil import open_tar, pushd, temporary_dir
+from pants.util.contextutil import pushd, temporary_dir
 from pants_test.bin.repro_mixin import ReproMixin
-from pants_test.tasks.task_test_base import TaskTestBase
+from pants_test.subsystem.subsystem_util import subsystem_instance
 
 
-class ReproOptionsTest(TaskTestBase, ReproMixin):
-
-  @classmethod
-  def task_type(cls):
-    """ Dummy task to invoke options to be fetched from subsystem """
-    return Cleaner
+class ReproOptionsTest(unittest.TestCase, ReproMixin):
 
   def test_ignore_dir(self):
     """Verify that passing --repro-ignore option ignores the directory"""
 
     # Buildroot is is based on your cwd so we need to step into a fresh
     # directory for repro to look at.
-    with pushd(self.build_root):
-      with temporary_dir() as capture_dir:
+    root_instance = BuildRoot()
+    with temporary_dir() as build_root:
+      with root_instance.temporary(build_root):
+        with pushd(build_root):
+          with temporary_dir() as capture_dir:
+            add_file = partial(self.add_file, build_root)
+            add_file('pants.ini', '')
+            add_file('.git/foo', 'foo')
+            add_file('dist/bar', 'bar')
+            add_file('foo/bar', 'baz')
+            add_file('src/test1', 'test1')
+            add_file('src/test2', 'test1')
 
-        add_file = partial(self.add_file, self.build_root)
-        add_file('pants.ini', '')
-        add_file('.git/foo', 'foo')
-        add_file('dist/bar', 'bar')
-        add_file('foo/bar', 'baz')
-        add_file('src/test1', 'test1')
-        add_file('src/test2', 'test1')
+            repro_file = os.path.join(capture_dir, 'repro.tar.gz')
+            options = {
+              'repro':dict(
+                capture=repro_file,
+                ignore=['src'],
+            )}
+            with subsystem_instance(Reproducer, **options) as repro_sub:
+              # self.create_task(self.context())
+              repro = repro_sub.create_repro()  # This is normally called in pants_exe
+              repro.capture(run_info_dict={})
 
-        repro_file = os.path.join(capture_dir, 'repro.tar.gz')
-        self.set_options_for_scope(Reproducer.options_scope,
-                                   capture=repro_file,
-                                   ignore=['src'],
-                                   pants_distdir='dist')
+              extract_loc = os.path.join(capture_dir, 'extract')
+              TGZ.extract(repro_file, extract_loc)
 
-        self.create_task(self.context())
-        repro = Reproducer.global_instance().create_repro()
-        repro.capture(run_info_dict={})
+              assert_file = partial(self.assert_file, extract_loc)
+              assert_file('foo/bar', 'baz')
 
-        extract_loc = os.path.join(capture_dir, 'extract')
-        TGZ.extract(repro_file, extract_loc)
-
-        assert_file = partial(self.assert_file, extract_loc)
-        assert_file('foo/bar', 'baz')
-
-        assert_not_exists = partial(self.assert_not_exists, extract_loc)
-        assert_not_exists('.git')
-        assert_not_exists('src')
+              assert_not_exists = partial(self.assert_not_exists, extract_loc)
+              assert_not_exists('.git')
+              assert_not_exists('src')
