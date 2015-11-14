06877bf5586dad6e992025a207102edd16e1600a
Tansy Arron <tansy.arron@gmail.com>
2015-11-13 20:46:03 -0500

    ran isort

--- src/python/pants/backend/core/tasks/sorttargets.py
+++ src/python/pants/backend/core/tasks/sorttargets.py
@@ -7,7 +7,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 
 from collections import defaultdict
 
-
 from pants.backend.core.tasks.console_task import ConsoleTask
 from pants.build_graph.build_graph import sort_targets
 
