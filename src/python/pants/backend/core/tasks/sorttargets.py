6a35cd62c84025df964918349941e02d014acac6
Tansy Arron <tansy.arron@gmail.com>
2015-11-13 20:43:05 -0500

    Remove use of twitter.common.util.topological_sort in SortTargets

--- src/python/pants/backend/core/tasks/sorttargets.py
+++ src/python/pants/backend/core/tasks/sorttargets.py
@@ -7,45 +7,23 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 
 from collections import defaultdict
 
-from twitter.common.util import topological_sort
 
 from pants.backend.core.tasks.console_task import ConsoleTask
-from pants.build_graph.target import Target
+from pants.build_graph.build_graph import sort_targets
 
 
 class SortTargets(ConsoleTask):
-  @staticmethod
-  def _is_target(item):
-    return isinstance(item, Target)
-
   @classmethod
   def register_options(cls, register):
     super(SortTargets, cls).register_options(register)
     register('--reverse', action='store_true', default=False,
              help='Sort least-dependent to most-dependent.')
 
-  def __init__(self, *args, **kwargs):
-    super(SortTargets, self).__init__(*args, **kwargs)
-    self._reverse = self.get_options().reverse
-
   def console_output(self, targets):
-    depmap = defaultdict(set)
-
-    def map_deps(target):
-      deps = depmap[target.address.spec]
-      for dep in target.dependencies:
-        deps.add(dep.address.spec)
-
-    for root in self.context.target_roots:
-      root.walk(map_deps)
-
-    tsorted = []
-    for group in topological_sort(depmap):
-      tsorted.extend(group)
-    if self._reverse:
-      tsorted = reversed(tsorted)
-
-    roots = set(root.address.spec for root in self.context.target_roots)
-    for address in tsorted:
-      if address in roots:
-        yield address
+    sorted_targets = sort_targets(targets)
+    # sort_targets already returns targets in reverse topologically sorted order.
+    if not self.get_options().reverse:
+      sorted_targets = reversed(sorted_targets)
+    for target in sorted_targets:
+      if target in self.context.target_roots:
+        yield target.address.reference()
--- tests/python/pants_test/tasks/test_sorttargets.py
+++ tests/python/pants_test/tasks/test_sorttargets.py
@@ -49,10 +49,11 @@ class SortTargetsTest(BaseSortTargetsTest):
 
   def test_sort(self):
     targets = [self.target('common/a'), self.target('common/c'), self.target('common/b')]
-    self.assertEqual(['common/a:a', 'common/b:b', 'common/c:c'],
+    print("HERE!!", type(targets[0]))
+    self.assertEqual(['common/a', 'common/b', 'common/c'],
                      list(self.execute_console_task(targets=targets)))
 
   def test_sort_reverse(self):
     targets = [self.target('common/c'), self.target('common/a'), self.target('common/b')]
-    self.assertEqual(['common/c:c', 'common/b:b', 'common/a:a'],
+    self.assertEqual(['common/c', 'common/b', 'common/a'],
                      list(self.execute_console_task(targets=targets, options={'reverse': True})))
