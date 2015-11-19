e38636fe4d7242760d1c911251e61f15196e3e33
Stu Hood <stuhood@gmail.com>
2015-11-19 11:59:19 -0800

    Add a deprecation version

--- src/python/pants/option/global_options.py
+++ src/python/pants/option/global_options.py
@@ -48,7 +48,7 @@ class GlobalOptionsRegistrar(Optionable):
     # after -l and -q in help output, which is conveniently contextual.
     register('--colors', '--color', action='store_true', default=True, recursive=True,
              help='Set whether log messages are displayed in color. NB: The singular `color` form '
-                  'is deprecated: prefer `colors`.')
+                  'is deprecated until 0.0.63: prefer `colors`.')
 
     # Pants code uses this only to verify that we are of the requested version. However
     # setup scripts, runner scripts, IDE plugins, etc., may grep this out of pants.ini
