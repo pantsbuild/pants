2a6254e0b7557dd16d165924ada2ba15aaf38cc5
Stu Hood <stuhood@gmail.com>
2015-11-19 11:42:33 -0800

    Add back the --[no-]color flag as deprecated.

--- src/python/pants/option/global_options.py
+++ src/python/pants/option/global_options.py
@@ -46,8 +46,9 @@ class GlobalOptionsRegistrar(Optionable):
              help='Squelches most console output.')
     # Not really needed in bootstrap options, but putting it here means it displays right
     # after -l and -q in help output, which is conveniently contextual.
-    register('--colors', action='store_true', default=True, recursive=True,
-             help='Set whether log messages are displayed in color.')
+    register('--colors', '--color', action='store_true', default=True, recursive=True,
+             help='Set whether log messages are displayed in color. NB: The singular `color` form '
+                  'is deprecated: prefer `colors`.')
 
     # Pants code uses this only to verify that we are of the requested version. However
     # setup scripts, runner scripts, IDE plugins, etc., may grep this out of pants.ini
