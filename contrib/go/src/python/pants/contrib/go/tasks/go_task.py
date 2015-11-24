de7285bfc7600d702dc023cdc189157b3fb654ab
John Sirois <john.sirois@gmail.com>
2015-11-24 11:56:11 -0700

    Fixup comment typo.

--- contrib/go/src/python/pants/contrib/go/tasks/go_task.py
+++ contrib/go/src/python/pants/contrib/go/tasks/go_task.py
@@ -129,7 +129,7 @@ class ImportOracle(object):
     """
     go_cmd = self._go_dist.create_go_cmd('list', args=['-json', pkg], gopath=gopath)
     with self._workunit_factory(pkg, cmd=str(go_cmd), labels=[WorkUnitLabel.TOOL]) as workunit:
-      # TODO(John Sirois): It would be nice to be able to tee the stdout to the workunit to we have
+      # TODO(John Sirois): It would be nice to be able to tee the stdout to the workunit so we have
       # a capture of the json available for inspection in the server console.
       process = go_cmd.spawn(stdout=subprocess.PIPE, stderr=workunit.output('stderr'))
       out, _ = process.communicate()
