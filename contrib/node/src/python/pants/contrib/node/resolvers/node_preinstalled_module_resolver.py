94c48c82912788b19325a9cd4415009375ef399b
Chris Pesto <cpesto@twitter.com>
2015-12-02 14:00:26 -0800

    Changing url to dependencies_archive_url

--- contrib/node/src/python/pants/contrib/node/resolvers/node_preinstalled_module_resolver.py
+++ contrib/node/src/python/pants/contrib/node/resolvers/node_preinstalled_module_resolver.py
@@ -29,32 +29,41 @@ class NodePreinstalledModuleResolver(NodeResolverBase):
 
     temp_dir = safe_mkdtemp()
 
-    archive_file_name = urllib_parse.urlsplit(target.url).path.split('/')[-1]
+    archive_file_name = urllib_parse.urlsplit(target.dependencies_archive_url).path.split('/')[-1]
     if not archive_file_name:
       raise TaskError('Could not determine archive file name for {target} from {url}'
-                      .format(target=target.address.reference(), url=target.url))
+                      .format(target=target.address.reference(),
+                              url=target.dependencies_archive_url))
 
     download_path = os.path.join(temp_dir, archive_file_name)
 
-    logger.info('Downloading archive {archive_file_name} from {url} to {path}'
-                .format(archive_file_name=archive_file_name, url=target.url, path=download_path))
+    logger.info('Downloading archive {archive_file_name} from {dependencies_archive_url} to {path}'
+                .format(archive_file_name=archive_file_name,
+                        dependencies_archive_url=target.dependencies_archive_url,
+                        path=download_path))
 
     try:
-      with closing(urllib_request.urlopen(target.url)) as opened_archive_url:
+      with closing(urllib_request.urlopen(target.dependencies_archive_url)) as opened_archive_url:
         with safe_open(download_path, 'wb') as downloaded_archive:
           downloaded_archive.write(opened_archive_url.read())
-    except (IOError, urllib_error.HTTPError, urllib_error.URLError, ValueError) as e:
-      raise TaskError('Failed to fetch preinstalled node_modules for {target} from {url}: {error}'
-                      .format(target=target.address.reference(), url=target.url, error=e))
+    except (IOError, urllib_error.HTTPError, urllib_error.URLError, ValueError) as error:
+      raise TaskError('Failed to fetch preinstalled node_modules for {target} from '
+                      '{dependencies_archive_url}: {error}'
+                      .format(target=target.address.reference(),
+                              url=target.dependencies_archive_url, error=error))
 
-    logger.info('Fetched archive {archive_file_name} from {url} to {path}'
-                .format(archive_file_name=archive_file_name, url=target.url, path=download_path))
+    logger.info('Fetched archive {archive_file_name} from {dependencies_archive_url} to {path}'
+                .format(archive_file_name=archive_file_name,
+                        dependencies_archive_url=target.dependencies_archive_url,
+                        path=download_path))
 
     archiver_for_path(archive_file_name).extract(download_path, temp_dir)
 
     extracted_node_modules = os.path.join(temp_dir, 'node_modules')
     if not os.path.isdir(extracted_node_modules):
-      raise TaskError('Did not find an extracted node_modules directory for {target} inside {url}'
-                      .format(target=target.address.reference(), url=target.url))
+      raise TaskError('Did not find an extracted node_modules directory for {target} '
+                      'inside {dependencies_archive_url}'
+                      .format(target=target.address.reference(),
+                              dependencies_archive_url=target.dependencies_archive_url))
 
     os.rename(extracted_node_modules, os.path.join(results_dir, 'node_modules'))
--- contrib/node/src/python/pants/contrib/node/targets/node_preinstalled_module.py
+++ contrib/node/src/python/pants/contrib/node/targets/node_preinstalled_module.py
@@ -12,22 +12,24 @@ from pants.contrib.node.targets.node_module import NodeModule
 
 
 class NodePreinstalledModule(NodeModule):
-  """A Node module with local sources which resolves its deps by downloading a preinstalled node_modules directory."""
+  """A NodeModule which resolves deps by downloading an archived node_modules directory."""
 
-  def __init__(self, url=None, sources=None, address=None, payload=None, **kwargs):
+  def __init__(self, dependencies_archive_url=None, sources=None,
+               address=None, payload=None, **kwargs):
     """
     :param string url: The location of a tar.gz file containing containing a node_modules directory.
     """
     payload = payload or Payload()
     payload.add_fields({
-      'url': PrimitiveField(url),
+      'dependencies_archive_url': PrimitiveField(dependencies_archive_url),
     })
-    super(NodePreinstalledModule, self).__init__(sources=sources, address=address, payload=payload, **kwargs)
+    super(NodePreinstalledModule, self).__init__(sources=sources, address=address,
+                                                 payload=payload, **kwargs)
 
   @property
-  def url(self):
+  def dependencies_archive_url(self):
     """Where to download the archive containing the node_modules directory.
 
     :rtype: string
     """
-    return self.payload.url
+    return self.payload.dependencies_archive_url
