4cc61f53c3d98ada2d98fe063757ef321ab27539
Chris Pesto <cpesto@twitter.com>
2015-12-02 14:21:28 -0800

    Switching to temporary_dir context

--- contrib/node/src/python/pants/contrib/node/resolvers/node_preinstalled_module_resolver.py
+++ contrib/node/src/python/pants/contrib/node/resolvers/node_preinstalled_module_resolver.py
@@ -14,7 +14,8 @@ import six.moves.urllib.parse as urllib_parse
 import six.moves.urllib.request as urllib_request
 from pants.base.exceptions import TaskError
 from pants.fs.archive import archiver_for_path
-from pants.util.dirutil import safe_mkdtemp, safe_open
+from pants.util.contextutil import temporary_dir
+from pants.util.dirutil import safe_open
 
 from pants.contrib.node.resolvers.node_resolver_base import NodeResolverBase
 
@@ -27,43 +28,43 @@ class NodePreinstalledModuleResolver(NodeResolverBase):
   def resolve_target(self, node_task, target, results_dir, node_paths):
     self._copy_sources(target, results_dir)
 
-    temp_dir = safe_mkdtemp()
+    with temporary_dir() as temp_dir:
+      archive_file_name = urllib_parse.urlsplit(target.dependencies_archive_url).path.split('/')[-1]
+      if not archive_file_name:
+        raise TaskError('Could not determine archive file name for {target} from {url}'
+                        .format(target=target.address.reference(),
+                                url=target.dependencies_archive_url))
 
-    archive_file_name = urllib_parse.urlsplit(target.dependencies_archive_url).path.split('/')[-1]
-    if not archive_file_name:
-      raise TaskError('Could not determine archive file name for {target} from {url}'
-                      .format(target=target.address.reference(),
-                              url=target.dependencies_archive_url))
+      download_path = os.path.join(temp_dir, archive_file_name)
 
-    download_path = os.path.join(temp_dir, archive_file_name)
+      logger.info('Downloading archive {archive_file_name} from '
+                  '{dependencies_archive_url} to {path}'
+                  .format(archive_file_name=archive_file_name,
+                          dependencies_archive_url=target.dependencies_archive_url,
+                          path=download_path))
 
-    logger.info('Downloading archive {archive_file_name} from {dependencies_archive_url} to {path}'
-                .format(archive_file_name=archive_file_name,
-                        dependencies_archive_url=target.dependencies_archive_url,
-                        path=download_path))
+      try:
+        with closing(urllib_request.urlopen(target.dependencies_archive_url)) as opened_archive_url:
+          with safe_open(download_path, 'wb') as downloaded_archive:
+            downloaded_archive.write(opened_archive_url.read())
+      except (IOError, urllib_error.HTTPError, urllib_error.URLError, ValueError) as error:
+        raise TaskError('Failed to fetch preinstalled node_modules for {target} from '
+                        '{dependencies_archive_url}: {error}'
+                        .format(target=target.address.reference(),
+                                url=target.dependencies_archive_url, error=error))
 
-    try:
-      with closing(urllib_request.urlopen(target.dependencies_archive_url)) as opened_archive_url:
-        with safe_open(download_path, 'wb') as downloaded_archive:
-          downloaded_archive.write(opened_archive_url.read())
-    except (IOError, urllib_error.HTTPError, urllib_error.URLError, ValueError) as error:
-      raise TaskError('Failed to fetch preinstalled node_modules for {target} from '
-                      '{dependencies_archive_url}: {error}'
-                      .format(target=target.address.reference(),
-                              url=target.dependencies_archive_url, error=error))
+      logger.info('Fetched archive {archive_file_name} from {dependencies_archive_url} to {path}'
+                  .format(archive_file_name=archive_file_name,
+                          dependencies_archive_url=target.dependencies_archive_url,
+                          path=download_path))
 
-    logger.info('Fetched archive {archive_file_name} from {dependencies_archive_url} to {path}'
-                .format(archive_file_name=archive_file_name,
-                        dependencies_archive_url=target.dependencies_archive_url,
-                        path=download_path))
+      archiver_for_path(archive_file_name).extract(download_path, temp_dir)
 
-    archiver_for_path(archive_file_name).extract(download_path, temp_dir)
+      extracted_node_modules = os.path.join(temp_dir, 'node_modules')
+      if not os.path.isdir(extracted_node_modules):
+        raise TaskError('Did not find an extracted node_modules directory for {target} '
+                        'inside {dependencies_archive_url}'
+                        .format(target=target.address.reference(),
+                                dependencies_archive_url=target.dependencies_archive_url))
 
-    extracted_node_modules = os.path.join(temp_dir, 'node_modules')
-    if not os.path.isdir(extracted_node_modules):
-      raise TaskError('Did not find an extracted node_modules directory for {target} '
-                      'inside {dependencies_archive_url}'
-                      .format(target=target.address.reference(),
-                              dependencies_archive_url=target.dependencies_archive_url))
-
-    os.rename(extracted_node_modules, os.path.join(results_dir, 'node_modules'))
+      os.rename(extracted_node_modules, os.path.join(results_dir, 'node_modules'))
