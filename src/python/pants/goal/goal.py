6c41d0b55f03ae9104419a88094d74945b34bc09
Benjy <benjy@foursquare.com>
2015-12-07 20:08:06 +0000

    Explicitly set the synthetic task subclass's __module__.
    This is so that it has the same stable_name() is the class
    it's synthesized from, which is useful in tests.
    
    Note that this change will invalidate all artifact caches,
    as the stable_name is used to generate cache paths.

--- src/python/pants/goal/goal.py
+++ src/python/pants/goal/goal.py
@@ -124,6 +124,7 @@ class _Goal(object):
                                       options_scope.replace('.', '_').replace('-', '_'))
     task_type = type(subclass_name, (superclass,), {
       '__doc__': superclass.__doc__,
+      '__module__': superclass.__module__,
       'options_scope': options_scope
     })
 
