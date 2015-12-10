2d64f950f061f08be27761e88aa126ef4eae7ed6
Timur Abishev <abishev.timur@gmail.com>
2015-12-10 15:15:44 +0000

    using new pants runner for synthetic jars

--- src/python/pants/backend/jvm/tasks/scala_repl.py
+++ src/python/pants/backend/jvm/tasks/scala_repl.py
@@ -5,8 +5,6 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-import os
-
 from pants.backend.jvm.targets.jar_dependency import JarDependency
 from pants.backend.jvm.targets.jar_library import JarLibrary
 from pants.backend.jvm.targets.jvm_target import JvmTarget
@@ -14,7 +12,6 @@ from pants.backend.jvm.tasks.jvm_task import JvmTask
 from pants.backend.jvm.tasks.jvm_tool_task_mixin import JvmToolTaskMixin
 from pants.java.distribution.distribution import DistributionLocator
 from pants.task.repl_task_mixin import ReplTaskMixin
-from pants.util.contextutil import temporary_file
 
 
 class ScalaRepl(JvmToolTaskMixin, ReplTaskMixin, JvmTask):
@@ -39,12 +36,10 @@ class ScalaRepl(JvmToolTaskMixin, ReplTaskMixin, JvmTask):
     return isinstance(target, (JarLibrary, JvmTarget))
 
   def setup_repl_session(self, targets):
-    repl_classpath = self.tool_classpath('scala-repl')
-    return self.tool_classpath('pants-runner'), self.classpath(targets, classpath_prefix=repl_classpath)
+    return self.tool_classpath('pants-runner') + self.tool_classpath('scala-repl') +\
+           self.classpath(targets)
 
-  def launch_repl(self, session_setup):
-    runner_classpath, repl_classpath = session_setup
-
+  def launch_repl(self, classpath):
     # The scala repl requires -Dscala.usejavacp=true since Scala 2.8 when launching in the way
     # we do here (not passing -classpath as a program arg to scala.tools.nsc.MainGenericRunner).
     jvm_options = self.jvm_options
@@ -53,19 +48,10 @@ class ScalaRepl(JvmToolTaskMixin, ReplTaskMixin, JvmTask):
 
     # NOTE: We execute with no workunit, as capturing REPL output makes it very sluggish.
     #
-    # NOTE: Using runner instead of synthetic jar here because the classLoader used by REPL
-    # does not load Class-Path from manifest, so we need another approach for long classpath
-    ScalaRepl.execute_java_with_runner(runner_classpath, repl_classpath,
-                                       self.get_options().main, jvm_options, self.args)
-
-  @staticmethod
-  def execute_java_with_runner(runner_classpath, classpath, main, jvm_options, args):
-    with temporary_file() as classpath_file:
-      classpath_file.write(os.pathsep.join(classpath))
-      classpath_file.close()
-
-      DistributionLocator.cached().execute_java(classpath=runner_classpath,
-                                                main=ScalaRepl._RUNNER_MAIN,
-                                                jvm_options=jvm_options,
-                                                args=[classpath_file.name, main] + args,
-                                                create_synthetic_jar=False)
+    # NOTE: Disable creating synthetic jar here because the classLoader used by REPL
+    # does not load Class-Path from manifest.
+    DistributionLocator.cached().execute_java(classpath=classpath,
+                                              main=ScalaRepl._RUNNER_MAIN,
+                                              jvm_options=jvm_options,
+                                              args=[self.get_options().main] + self.args,
+                                              create_synthetic_jar=True)
