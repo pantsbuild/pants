30c9a208f354fc2869d684d1d272f7372d402faf
Timur Abishev <abishev.timur@gmail.com>
2015-12-10 16:34:23 +0000

    New version of pants runner to use with synthetic jar.

--- src/java/org/pantsbuild/tools/runner/PantsRunner.java
+++ src/java/org/pantsbuild/tools/runner/PantsRunner.java
@@ -8,31 +8,60 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.nio.file.Files;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 /**
- * Helper class for running java code against a classpath read from a file.
- * Useful for very long classpaths that exceed the shell's command length.
+ * Helper class for running java code using synthetic jar.
+ * In some cases when code deal with classloaders, running this code
+ * using synthetic jar can fail. Running this cases using this Runner
+ * most likely fix all problems with classloaders.
  */
 public class PantsRunner {
-  public static void main(String[] args) throws ReflectiveOperationException, IOException {
-    if (args.length < 2) {
-      System.out.println("Usage: java -cp classpath_to_pants_runner " +
+  public static void main(String[] args) throws Exception {
+    if (args.length == 0) {
+      System.out.println("Usage: java -cp synthetic_jar " +
           "org.pantsbuild.tools.runner.PantsRunner " +
-          "classpath_file main_class args.\n" +
-          "Classpath file should consist of the classpath entries. " +
-          "Classpath entries should be specified in a single line, " +
-          "each entry being separated by the system's path delimiter.");
+          "main_class args.\n" +
+          "Synthetic jar should contain manifest file " +
+          "with properly declared Class-Path property.");
       System.exit(1);
     }
-    String classpath = new String(Files.readAllBytes(new File(args[0]).toPath())).trim();
+    String classpath = readClasspath();
     updateClassPathProperty(classpath);
-    updateClassLoader(classpath);
-    String[] mainArgs = new String[args.length - 2];
-    System.arraycopy(args, 2, mainArgs, 0, args.length - 2);
-    runMainMethod(args[1], mainArgs);
+    updateClassLoader(getClassLoader(), classpath);
+    String[] mainArgs = new String[args.length - 1];
+    System.arraycopy(args, 1, mainArgs, 0, args.length - 1);
+    runMainMethod(args[0], mainArgs);
+  }
+
+  private static String readClasspath() throws IOException, URISyntaxException {
+    URL[] urls = getClassLoader().getURLs();
+    if (urls.length != 1 || !urls[0].getProtocol().equals("file") ||
+        !urls[0].toString().endsWith(".jar")) {
+      throw new IllegalArgumentException("Should be exactly one jar file in the classpath.");
+    }
+    JarFile jar = new JarFile(new File(urls[0].toURI()));
+    Manifest manifest = jar.getManifest();
+    if (manifest == null) {
+      throw new IllegalArgumentException("Supplied jar file doesn't contains " +
+          "META-INF/MANIFEST.MF file.");
+    }
+    String classpath = manifest.getMainAttributes().getValue("Class-Path");
+    if (classpath == null) {
+      throw new IllegalArgumentException("Supplied jar file's manifest " +
+          "doesn't contains Class-Path section.");
+    }
+    return classpath.replace(" ", File.pathSeparator);
+  }
+
+  private static URLClassLoader getClassLoader() {
+    // Using context classloader here as it should be application one on the startup and
+    // because it's simple to mock context class loader in tests.
+    return (URLClassLoader) Thread.currentThread().getContextClassLoader();
   }
 
   private static void updateClassPathProperty(String classpath) {
@@ -40,9 +69,8 @@ public class PantsRunner {
         System.getProperty("java.class.path") + File.pathSeparator + classpath);
   }
 
-  private static void updateClassLoader(String classpath)
+  private static void updateClassLoader(URLClassLoader classLoader, String classpath)
       throws ReflectiveOperationException, MalformedURLException {
-    URLClassLoader classLoader = (URLClassLoader) PantsRunner.class.getClassLoader();
     Method addUrl = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
     addUrl.setAccessible(true);
     for (String entry : classpath.split(File.pathSeparator)) {
@@ -52,7 +80,8 @@ public class PantsRunner {
 
   private static void runMainMethod(String mainClass, String[] args)
       throws ReflectiveOperationException {
-    Method main = Class.forName(mainClass).getDeclaredMethod("main", String[].class);
+    Method main = Class.forName(mainClass, true, getClassLoader()).
+        getDeclaredMethod("main", String[].class);
     if (!Modifier.isStatic(main.getModifiers())) {
       throw new IllegalArgumentException("Method 'main' for " + mainClass + " is not static.");
     }
