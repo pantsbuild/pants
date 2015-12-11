15da59125f5e2daa273896675f350b08430b5596
Timur Abishev <abishev.timur@gmail.com>
2015-12-11 00:09:15 +0000

    check urls on existance in MainClass

--- testprojects/src/java/org/pantsbuild/testproject/runner/MainClass.java
+++ testprojects/src/java/org/pantsbuild/testproject/runner/MainClass.java
@@ -3,16 +3,19 @@
 
 package org.pantsbuild.testproject.runner;
 
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Arrays;
 
 public class MainClass {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println(DependentClass.getDependentClassMessage());
     System.out.println("Args: " + Arrays.toString(args));
     for (URL url : ((URLClassLoader) MainClass.class.getClassLoader()).getURLs()) {
       System.out.println("URL: " + url.toString().substring(url.toString().lastIndexOf("/") + 1));
+      // all urls should be openable
+      url.openStream().close();
     }
   }
 }
