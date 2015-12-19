e86819c094f38e07136cd56a17487b88d3e48708
Eric Ayers <zundel@squareup.com>
2015-12-19 14:46:29 -0500

    fixup docs for how to run unit tests
    Now that we've moved some tests that muck with the python interpreter out of the way, this works:
    
    ```
    ./pants test tests/python/pants_test:: --tag=-integration
    ```
    
    Testing Done:
    CI running at https://travis-ci.org/pantsbuild/pants/builds/97865125
    
    Staged at https://pantsbuild.github.io/staging/zundel/howto_develop.html#running-tests
    
    Bugs closed: 2748
    
    Reviewed at https://rbcommons.com/s/twitter/r/3276/

--- build-support/bin/unit-test.sh
+++ build-support/bin/unit-test.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-
-# We use some subshell pipelines to collect target lists, make sure target collection failing
-# fails the build.
-set -o pipefail
-
-./pants test $(./pants filter tests/python/pants_test:: --tag=-integration --filter-type=python_tests)
+set -e
+./pants test tests/python/pants_test:: --tag=-integration
