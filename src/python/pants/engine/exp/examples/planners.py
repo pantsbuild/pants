2a02e2052345b10de4a5c090e49009eed954aa3f
Stu Hood <stuhood@gmail.com>
2015-12-30 14:05:37 -0800

    Nuke Sources product.

--- src/python/pants/engine/exp/examples/planners.py
+++ src/python/pants/engine/exp/examples/planners.py
@@ -17,7 +17,6 @@ from pants.engine.exp.configuration import Configuration
 from pants.engine.exp.graph import Graph
 from pants.engine.exp.mapper import AddressMapper
 from pants.engine.exp.parsers import parse_json
-from pants.engine.exp.products import Sources
 from pants.engine.exp.scheduler import LocalScheduler, Plan, Planners, Subject, Task, TaskPlanner
 from pants.engine.exp.targets import Sources, Target
 from pants.util.memo import memoized, memoized_property
--- src/python/pants/engine/exp/products.py
+++ src/python/pants/engine/exp/products.py
@@ -35,28 +35,3 @@ def lift_native_product(subject, product_type):
   TODO: This is a placeholder to demonstrate the concept of lifting products off of targets
   and into the product namespace.
   """
-
-
-class Sources(object):
-  @classmethod
-  @memoized
-  def of(cls, ext):
-    type_name = b'Sources({!r})'.format(ext)
-
-    class_dict = {'ext': ext,
-                  # We need custom serialization for the dynamic class type.
-                  '__reduce__': lambda self: (_create_sources, ext)}
-
-    ext_type = type(type_name, (cls,), class_dict)
-
-    # Expose the custom class type at the module level to be pickle compatible.
-    setattr(sys.modules[cls.__module__], type_name, ext_type)
-
-    return ext_type
-
-  @classmethod
-  def ext(cls):
-    raise NotImplementedError()
-
-  def __repr__(self):
-    return 'Sources(ext={!r})'.format(self.ext)
