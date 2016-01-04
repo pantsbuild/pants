7ba009dba780c01b2c32528db380b15a04079e0b
Eric Ayers <zundel@squareup.com>
2016-01-04 15:41:59 -0500

    Adding sanity check for locale setting
    See https://github.com/pantsbuild/pants/issues/2465

--- src/python/pants/bin/pants_exe.py
+++ src/python/pants/bin/pants_exe.py
@@ -5,6 +5,7 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import locale
 import warnings
 
 
@@ -16,6 +17,23 @@ warnings.simplefilter('default', DeprecationWarning)
 
 from pants.bin.pants_runner import PantsRunner  # isort:skip
 from pants.bin.exiter import Exiter  # isort:skip
+
+
+class InvalidLocaleError(Exception):
+  """Raised when a valid locale can't be found."""
+
+# Sanity check for locale, See https://github.com/pantsbuild/pants/issues/2465.
+# This check is done early to give good feedback to user on how to fix the problem. Other
+# libraries called by Pants may fail with more obscure errors.
+try:
+  locale.getlocale()[1] or locale.getdefaultlocale()[1]
+except Exception as e:
+  raise InvalidLocaleError(
+      "{}: {}\n"
+      "  Could not get a valid locale. Check LC_* and LANG environment settings.\n"
+      "  Example for US English:\n"
+      "    LC_ALL=en_US.UTF-8\n"
+      "    LANG=en_US.UTF-8".format(type(e).__name__, e))
 
 
 def main():
