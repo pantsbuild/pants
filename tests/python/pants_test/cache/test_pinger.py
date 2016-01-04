8d23ce804b726a6d784fd19cb1648af0fd4224f3
Tansy Arron <tansy.arron@gmail.com>
2016-01-04 16:04:38 -0500

    Update pinger timeout in test_pinger_timeout_config and test_global_pinger_memo

--- tests/python/pants_test/cache/test_pinger.py
+++ tests/python/pants_test/cache/test_pinger.py
@@ -42,14 +42,14 @@ class TestPinger(BaseTest):
     self.assertEqual(ping_results[self.timeout_netloc], Pinger.UNREACHABLE)
 
   def test_pinger_timeout_config(self):
-    test = Pinger(timeout=self.slow_seconds - .01, tries=2)
+    test = Pinger(timeout=self.slow_seconds - .02, tries=2)
     netlocs = [self.fast_netloc, self.slow_netloc]
     ping_results = dict(test.pings(netlocs))
     self.assertLess(ping_results[self.fast_netloc], 1)
     self.assertEqual(ping_results[self.slow_netloc], Pinger.UNREACHABLE)
 
   def test_global_pinger_memo(self):
-    fast_pinger = Pinger(timeout=self.slow_seconds - .01, tries=2)
+    fast_pinger = Pinger(timeout=self.slow_seconds - .02, tries=2)
     slow_pinger = Pinger(timeout=self.timeout_seconds, tries=2)
     self.assertEqual(fast_pinger.pings([self.slow_netloc])[0][1], Pinger.UNREACHABLE)
     self.assertNotEqual(slow_pinger.pings([self.slow_netloc])[0][1], Pinger.UNREACHABLE)
