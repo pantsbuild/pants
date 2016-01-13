f5a7f09231f4558383eea89fe8f39e0bb1600e48
Kris Wilson <kwilson@twopensource.com>
2016-01-13 15:46:21 -0800

    Improve test.pytest failure when coverage is enabled.

--- tests/python/pants_test/backend/python/tasks/test_pytest_run_integration.py
+++ tests/python/pants_test/backend/python/tasks/test_pytest_run_integration.py
@@ -23,14 +23,15 @@ class PytestRunIntegrationTest(PantsRunIntegrationTest):
     start = time.time()
     pants_run = self.run_pants(['clean-all',
                                 'test.pytest',
+                                '--test-pytest-coverage=1',
                                 '--test-pytest-options="-k sleep_long"',
                                 '--timeout-default=1',
                                 'testprojects/tests/python/pants/timeout:sleeping_target'])
     end = time.time()
     self.assert_failure(pants_run)
 
-    # Ensure that the failure took less than 120 seconds to run.
-    self.assertLess(end - start, 120)
+    # Ensure that the failure took less than 5 seconds to run.
+    self.assertLess(end - start, 5)
 
     # Ensure that the timeout message triggered.
     self.assertIn("FAILURE: Timeout of 1 seconds reached", pants_run.stdout_data)
