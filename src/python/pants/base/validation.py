2463f3ad4f62c14a6cbf40e9eb8103e1bc67de64
Nick Howard (Twitter) <nhoward@twopensource.com>
2016-01-20 09:34:17 -0800

    Make more glob usages lazy; Pass FilesetWithSpec through source field validation, Make BundleProps.filemap lazy
    One of the things that consumes a lot of time while parsing BUILD build files is loading the values of globs eagerly. Two of the places this happens are during sources field validation and generating the filemap for jvm app bundles.
    
    This changes SourcesField so it allows FilesetWithSpec, ie globs, to be passed through without being converted into a list. This allows globs to remain unfulfilled until use rather than eagerly doing directory scanning on target initialization.
    
    It also changes jvm_app's bundle so that its filemap is also lazily created rather than eagerly.
    
    Testing Done:
    Added a test case for passthrough, ran ci.
    
    Travis passed https://travis-ci.org/pantsbuild/pants/builds/103508419
    
    Bugs closed: 2794
    
    Reviewed at https://rbcommons.com/s/twitter/r/3344/

--- src/python/pants/base/validation.py
+++ src/python/pants/base/validation.py
@@ -11,8 +11,7 @@ from twitter.common.dirutil.fileset import Fileset
 
 
 def assert_list(obj, expected_type=string_types, can_be_none=True, default=(), key_arg=None,
-    allowable=(list, Fileset, OrderedSet, set, tuple), allowable_add=(),
-    raise_type=ValueError):
+    allowable=(list, Fileset, OrderedSet, set, tuple), raise_type=ValueError):
   """
   This function is used to ensure that parameters set by users in BUILD files are of acceptable types.
   :param obj           : the object that may be a list. It will pass if it is of type in allowable.
@@ -21,8 +20,6 @@ def assert_list(obj, expected_type=string_types, can_be_none=True, default=(), k
   :param default       : this is the default to return if can_be_none is True and obj is None.
   :param key_arg       : this is the name of the key to which obj belongs to
   :param allowable     : the acceptable types for obj. We do not want to allow any iterable (eg string).
-  :param allowable_add : additional acceptable types; pass this to add to the default allowable types,
-                         rather than to replace them.
   :param raise_type    : the error to throw if the type is not correct.
   """
   def get_key_msg(key=None):
@@ -31,7 +28,7 @@ def assert_list(obj, expected_type=string_types, can_be_none=True, default=(), k
     else:
       return "In key '{}': ".format(key)
 
-  allowable = tuple(allowable) + tuple(allowable_add)
+  allowable = tuple(allowable)
 
   key_msg = get_key_msg(key_arg)
   val = obj
--- tests/python/pants_test/base/test_validation.py
+++ tests/python/pants_test/base/test_validation.py
@@ -37,13 +37,4 @@ class ParseValidation(unittest.TestCase):
     with self.assertRaisesRegexp(ValueError, "In key 'artifacts':"):
       assert_list([["file3.txt"]], key_arg='artifacts')  # All values most be strings
     with self.assertRaisesRegexp(ValueError, "In key 'jars':"):
-      assert_list(None, can_be_none=False, key_arg='jars')  # The default is ok as None only when can_be_noe is true
-
-  def test_additional_allowables(self):
-    def generator():
-      for x in range(11):
-        yield str(x)
-    assert_list(generator(), allowable_add=(GeneratorType,))
-    assert_list(['1', '2'], allowable_add=(GeneratorType,))
-    with self.assertRaises(ValueError):
-      assert_list(generator(), allowable_add=[])
+      assert_list(None, can_be_none=False, key_arg='jars')  # The default is ok as None only when can_be_none is true
