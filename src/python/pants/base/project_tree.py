faed58603378618d405540e3bbf4b9b8eaf1bf35
Timur Abishev <abishev.timur@gmail.com>
2016-01-20 20:18:19 -0800

    polish project tree comment

--- src/python/pants/base/project_tree.py
+++ src/python/pants/base/project_tree.py
@@ -18,8 +18,8 @@ logger = logging.getLogger(__name__)
 
 
 class ProjectTree(AbstractClass):
-  """A class used to abstract filesystem behaviour needed to load build files.
-  Currently regular filesystem or SCM are supported.
+  """A class to represent project tree which used to load build files from.
+  Have two implementations: based on file system and based on SCM.
   """
 
   def __init__(self, build_root):
