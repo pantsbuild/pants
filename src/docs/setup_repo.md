226a5be11c741b8a0676c477aa8880d2d8b585f6
Matt Olsen <matt@digital-wanderlust.com>
2016-01-22 18:14:05 -0800

    Remove the need to specify scalastyle in BUILD.tools
    Remove the need to spcify scalastyle in BUILD.tools
    
    * refactor tool registration and add scalastyle
    * add option for suffix_version override
    * remove style from build.tools file
    * fix unit tests broken when style was refactored out of BUILD.tools
    * fix unit tests that now need to initialize scalastyle since it is not in build.tools.
    * update docs regarding scala and build tools
    
    Testing Done:
    CI green: https://travis-ci.org/pantsbuild/pants/builds/103709277
    CI green: https://travis-ci.org/pantsbuild/pants/builds/103719462
    CI green: https://travis-ci.org/pantsbuild/pants/builds/104167207
    CI green: https://travis-ci.org/pantsbuild/pants/builds/104210120
    
    Bugs closed: 2534, 2797, 2821
    
    Reviewed at https://rbcommons.com/s/twitter/r/3355/

--- src/docs/setup_repo.md
+++ src/docs/setup_repo.md
@@ -52,12 +52,50 @@ Configure Pants' own Runtime Dependencies
 -----------------------------------------
 
 Pants calls out to other tools. E.g., it optionally uses `scalastyle` to check scala source code.
-Most tools come pre-configured by Pants. A few do require more setup though and these rely on
-special targets in your workspace to specify versions of the tools to fetch. These targets all live
-in the `BUILD.tools` file by convention. For example, when Pants fetches `scalastyle`, it looks in
-`BUILD.tools` for that target:
+Most tools come pre-configured by Pants. If you use scala a few tools are configured based on the
+major scala version provided with the --version flag.  You can specify custom tool versions by
+passing "custom" to --scala-platform-version. If a custom version of scala is desired some of these
+tools require more setup though and these rely on special targets in your workspace to specify
+versions of the tools to fetch .These targets all live in the `BUILD.tools` file by convention.
 
-!inc[start-at=scalastyle&end-before=scrooge-gen](../../BUILD.tools)
+For example, the following targets are used to provide custom scala tools:
+
+`//:scalastyle`: Scala style checking.
+`//:scalac`: Scala compiler.
+`//:scala-repl`: Scala REPL environment.
+`//:scala-library`: Scala runtime environment.
+
+Pants looks in `BUILD.tools` for that target.  Below is an example of BUILD.tools that overrides
+the default minor version for 2.10.  For these changes to take effect you would need to set
+--scala-platform-version to 'custom'.
+
+    jar_library(name = 'scalac',
+                jars = [
+                  jar(org = 'org.scala-lang', name = 'scala-compiler', rev = 2.10.3),
+                ])
+
+    jar_library(name = 'scala-library',
+                jars = [
+                  jar(org = 'org.scala-lang', name = 'scala-library', rev = 2.10.3),
+                ])
+
+    jar_library(name = 'scala-repl',
+                jars = [
+                  jar(org = 'org.scala-lang', name = 'jline', rev = 2.10.3, intransitive = True),
+                ],
+                dependencies = [
+                  ':scala-compiler',
+                  ':scala-library',
+                ])
+
+    jar_library(name = 'scalastyle',
+                jars = [
+                  scala_jar(org='org.scalastyle', name='scalastyle', rev='0.3.2')
+                ])
+
+Additional tools can be defined as follows in BUILD.tools:
+
+!inc[start-at=scala-js-library&end-before=scrooge-gen](../../BUILD.tools)
 
 When setting up your Pants repo, you may want to copy this file over from a working Pants repo and
 perhaps change some version numbers to fit your situation.
--- tests/python/pants_test/backend/jvm/tasks/test_scalastyle.py
+++ tests/python/pants_test/backend/jvm/tasks/test_scalastyle.py
@@ -70,17 +70,20 @@ class ScalastyleTest(NailgunTaskTestBase):
     self.context()  # We don't need the context, but this ensures subsystem option registration.
 
   def test_initialize_config_no_config_settings(self):
-    with self.assertRaises(Scalastyle.UnspecifiedConfig):
-      self._create_scalastyle_task(scalastyle_config=None).validate_scalastyle_config()
+    with self.scala_platform_setup():
+      with self.assertRaises(Scalastyle.UnspecifiedConfig):
+        self._create_scalastyle_task(scalastyle_config=None).validate_scalastyle_config()
 
   def test_initialize_config_config_setting_exist_but_invalid(self):
-    with self.assertRaises(Scalastyle.MissingConfig):
-      self._create_scalastyle_task(
-        scalastyle_config='file_does_not_exist.xml').validate_scalastyle_config()
+    with self.scala_platform_setup():
+      with self.assertRaises(Scalastyle.MissingConfig):
+        self._create_scalastyle_task(
+          scalastyle_config='file_does_not_exist.xml').validate_scalastyle_config()
 
   def test_excludes_setting_exists_but_invalid(self):
-    with self.assertRaises(TaskError):
-      FileExcluder('file_does_not_exist.txt', logger)
+    with self.scala_platform_setup():
+      with self.assertRaises(TaskError):
+        FileExcluder('file_does_not_exist.txt', logger)
 
   def test_excludes_parsed_loaded_correctly(self):
     excludes_text = dedent('''
@@ -98,6 +101,14 @@ class ScalastyleTest(NailgunTaskTestBase):
   @contextmanager
   def scala_platform_setup(self):
     with subsystem_instance(ScalaPlatform):
+      self.make_target(':scalastyle',
+                       JarLibrary,
+                       jars=[JarDependency('org.scalastyle', 'scalastyle_2.10', '0.3.2')]
+      )
+      self.make_target(':scalastyle_211',
+                       JarLibrary,
+                       jars=[JarDependency('org.scalastyle', 'scalastyle_2.11', '0.8.0')]
+      )
       self.make_target(':scala-compiler',
                        JarLibrary,
                        jars=[JarDependency('org.scala-lang', 'scala-compiler', '2.10.5')])
@@ -139,6 +150,9 @@ class ScalastyleTest(NailgunTaskTestBase):
       self.set_options_for_scope(ScalaPlatform.options_scope, scala_2_10_repl=':scala-repl')
       self.set_options_for_scope(ScalaPlatform.options_scope, scala_2_11_repl=':scala-repl_211')
       self.set_options_for_scope(ScalaPlatform.options_scope, scala_repl=':scala-repl')
+
+      self.set_options_for_scope(ScalaPlatform.options_scope, scalastyle_2_10=':scalastyle')
+      self.set_options_for_scope(ScalaPlatform.options_scope, scalastyle_2_11=':scalastyle_211')
       yield
 
   def test_get_non_synthetic_scala_targets(self):
