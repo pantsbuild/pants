f62c846885a62a06f925f647e57c4496c56c36ec
wisechengyi <wisechengyi@gmail.com>
2016-01-28 15:46:19 -0800

    no logging

--- src/python/pants/backend/project_info/tasks/export.py
+++ src/python/pants/backend/project_info/tasks/export.py
@@ -6,7 +6,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 import json
-import logging
 import os
 from collections import defaultdict
 
@@ -35,9 +34,6 @@ from pants.option.errors import OptionsError
 from pants.option.ranked_value import RankedValue
 from pants.task.console_task import ConsoleTask
 from pants.util.memo import memoized_property
-
-
-logger = logging.getLogger(__name__)
 
 
 # Changing the behavior of this task may affect the IntelliJ Pants plugin.
@@ -341,8 +337,6 @@ class ExportTask(IvyTaskMixin, PythonTask):
     if pants_target_type in self.target_aliases_map:
       return self.target_aliases_map.get(pants_target_type)
     else:
-      logger.warn('Unregistered target type {target_type}'
-                      .format(target_type=pants_target_type))
       return "{}.{}".format(pants_target_type.__module__, pants_target_type.__name__)
 
   @staticmethod
