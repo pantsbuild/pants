5051bef6db526fb21968eea0dc73fa7ad1e227f4
Timur Abishev <abishev.timur@gmail.com>
2016-02-04 16:02:15 +0000

    add test project for synthetic jar testing

---
+++ testprojects/tests/java/org/pantsbuild/testproject/syntheticjar/Util.java
@@ -0,0 +1,18 @@
+// Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package org.pantsbuild.testproject.syntheticjar.util;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+
+public class Util {
+  public static void failIfSyntheticJar() {
+    URL[] urls = ((URLClassLoader) (Thread.currentThread().getContextClassLoader())).getURLs();
+    if (urls.length == 1) {
+      throw new IllegalStateException("Synthetic jar run is detected, classpath: " +
+          Arrays.toString(urls));
+    }
+  }
+}
