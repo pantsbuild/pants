5f4ba0791a42d82ba283b3b7e9dac8355282ca7f
Stu Hood <stuhood@gmail.com>
2016-02-16 14:03:46 -0800

    Fixup spec parsing tests to only test parsing.

--- tests/python/pants_test/base/test_cmd_line_spec_parser.py
+++ tests/python/pants_test/base/test_cmd_line_spec_parser.py
@@ -8,7 +8,20 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import os
 
 from pants.base.cmd_line_spec_parser import CmdLineSpecParser
+from pants.base.specs import DescendantAddresses, SiblingAddresses, SingleAddress
 from pants_test.base_test import BaseTest
+
+
+def single(directory, name=None):
+  return SingleAddress(directory, name)
+
+
+def desc(directory):
+  return DescendantAddresses(directory)
+
+
+def sib(directory):
+  return SiblingAddresses(directory)
 
 
 class CmdLineSpecParserTest(BaseTest):
@@ -18,70 +31,66 @@ class CmdLineSpecParserTest(BaseTest):
     self._spec_parser = CmdLineSpecParser(self.build_root)
 
   def test_normal(self):
-    self.assert_parsed(':root', expected=[':root'])
-    self.assert_parsed('//:root', expected=[':root'])
+    self.assert_parsed(':root', single('', 'root'))
+    self.assert_parsed('//:root', single('', 'root'))
 
-    self.assert_parsed('a', expected=['a'])
-    self.assert_parsed('a:a', expected=['a'])
+    self.assert_parsed('a', single('a'))
+    self.assert_parsed('a:a', single('a', 'a'))
 
-    self.assert_parsed('a/b', expected=['a/b'])
-    self.assert_parsed('a/b:b', expected=['a/b'])
-    self.assert_parsed('a/b:c', expected=['a/b:c'])
+    self.assert_parsed('a/b', single('a/b'))
+    self.assert_parsed('a/b:b', single('a/b', 'b'))
+    self.assert_parsed('a/b:c', single('a/b', 'c'))
 
   def test_sibling(self):
-    self.assert_parsed(':', expected=[':root'])
-    self.assert_parsed('//:', expected=[':root'])
+    self.assert_parsed(':', sib(''))
+    self.assert_parsed('//:', sib(''))
 
-    self.assert_parsed('a:', expected=['a', 'a:b'])
-    self.assert_parsed('//a:', expected=['a', 'a:b'])
+    self.assert_parsed('a:', sib('a'))
+    self.assert_parsed('//a:', sib('a'))
 
-    self.assert_parsed('a/b:', expected=['a/b', 'a/b:c'])
-    self.assert_parsed('//a/b:', expected=['a/b', 'a/b:c'])
+    self.assert_parsed('a/b:', sib('a/b'))
+    self.assert_parsed('//a/b:', sib('a/b'))
 
   def test_sibling_or_descendents(self):
-    self.assert_parsed('::', expected=[':root', 'a', 'a:b', 'a/b', 'a/b:c'])
-    self.assert_parsed('//::', expected=[':root', 'a', 'a:b', 'a/b', 'a/b:c'])
+    self.assert_parsed('::', desc(''))
+    self.assert_parsed('//::', desc(''))
 
-    self.assert_parsed('a::', expected=['a', 'a:b', 'a/b', 'a/b:c'])
-    self.assert_parsed('//a::', expected=['a', 'a:b', 'a/b', 'a/b:c'])
+    self.assert_parsed('a::', desc('a'))
+    self.assert_parsed('//a::', desc('a'))
 
-    self.assert_parsed('a/b::', expected=['a/b', 'a/b:c'])
-    self.assert_parsed('//a/b::', expected=['a/b', 'a/b:c'])
+    self.assert_parsed('a/b::', desc('a/b'))
+    self.assert_parsed('//a/b::', desc('a/b'))
 
   def test_absolute(self):
-    self.assert_parsed(os.path.join(self.build_root, 'a'), expected=['a'])
-    self.assert_parsed(os.path.join(self.build_root, 'a:a'), expected=['a'])
-    self.assert_parsed(os.path.join(self.build_root, 'a:'), expected=['a', 'a:b'])
-    self.assert_parsed(os.path.join(self.build_root, 'a::'),
-                        expected=['a', 'a:b', 'a/b', 'a/b:c'])
+    self.assert_parsed(os.path.join(self.build_root, 'a'), single('a'))
+    self.assert_parsed(os.path.join(self.build_root, 'a:a'), single('a', 'a'))
+    self.assert_parsed(os.path.join(self.build_root, 'a:'), sib('a'))
+    self.assert_parsed(os.path.join(self.build_root, 'a::'), desc('a'))
 
+    with self.assertRaises(CmdLineSpecParser.BadSpecError):
+      self.assert_parsed('/not/the/buildroot/a', sib('a'))
+
+  def test_absolute_double_slashed(self):
+    # By adding a double slash, we are insisting that this absolute path is actually
+    # relative to the buildroot. Thus, it should parse correctly.
     double_absolute = '/' + os.path.join(self.build_root, 'a')
     self.assertEquals('//', double_absolute[:2],
                       'A sanity check we have a leading-// absolute spec')
-    with self.assertRaises(self.spec_parser.BadSpecError):
-      self.spec_parser.parse_addresses(double_absolute).next()
-
-    with self.assertRaises(self.spec_parser.BadSpecError):
-      self.spec_parser.parse_addresses('/not/the/buildroot/a').next()
+    self.assert_parsed(double_absolute, single(double_absolute[2:]))
 
   def test_cmd_line_affordances(self):
-    self.assert_parsed('./:root', expected=[':root'])
-    self.assert_parsed('//./:root', expected=[':root'])
-    self.assert_parsed('//./a/../:root', expected=[':root'])
-    self.assert_parsed(os.path.join(self.build_root, './a/../:root'),
-                       expected=[':root'])
+    self.assert_parsed('./:root', single('', 'root'))
+    self.assert_parsed('//./:root', single('', 'root'))
+    self.assert_parsed('//./a/../:root', single('', 'root'))
+    self.assert_parsed(os.path.join(self.build_root, './a/../:root'), single('', 'root'))
 
-    self.assert_parsed('a/', expected=['a'])
-    self.assert_parsed('./a/', expected=['a'])
-    self.assert_parsed(os.path.join(self.build_root, './a/'), expected=['a'])
+    self.assert_parsed('a/', single('a'))
+    self.assert_parsed('./a/', single('a'))
+    self.assert_parsed(os.path.join(self.build_root, './a/'), single('a'))
 
-    self.assert_parsed('a/b/:b', expected=['a/b'])
-    self.assert_parsed('./a/b/:b', expected=['a/b'])
-    self.assert_parsed(os.path.join(self.build_root, './a/b/:b'), expected=['a/b'])
-
-  def test_cmd_line_spec_list(self):
-    self.assert_parsed('a', 'a/b', expected=['a', 'a/b'])
-    self.assert_parsed('::', expected=[':root', 'a', 'a:b', 'a/b', 'a/b:c'])
+    self.assert_parsed('a/b/:b', single('a/b', 'b'))
+    self.assert_parsed('./a/b/:b', single('a/b', 'b'))
+    self.assert_parsed(os.path.join(self.build_root, './a/b/:b'), single('a/b', 'b'))
 
   def assert_parsed(self, spec_str, expected_spec):
     self.assertEqual(self._spec_parser.parse_spec(spec_str), expected_spec)
