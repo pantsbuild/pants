7aa4218063684ff0ab3323d35b555382eea0a444
Stu Hood <stuhood@gmail.com>
2016-02-16 11:24:33 -0800

    WIP: Clean up CmdLineSpecParserTest

--- tests/python/pants_test/base/test_cmd_line_spec_parser.py
+++ tests/python/pants_test/base/test_cmd_line_spec_parser.py
@@ -13,42 +13,46 @@ from pants_test.base_test import BaseTest
 
 class CmdLineSpecParserTest(BaseTest):
 
+  def setUp(self):
+    super(CmdLineSpecParserTest, self).setUp()
+    self._spec_parser = CmdLineSpecParser(self.build_root)
+
   def test_normal(self):
-    self.assert_scanned([':root'], expected=[':root'])
-    self.assert_scanned(['//:root'], expected=[':root'])
+    self.assert_parsed(':root', expected=[':root'])
+    self.assert_parsed('//:root', expected=[':root'])
 
-    self.assert_scanned(['a'], expected=['a'])
-    self.assert_scanned(['a:a'], expected=['a'])
+    self.assert_parsed('a', expected=['a'])
+    self.assert_parsed('a:a', expected=['a'])
 
-    self.assert_scanned(['a/b'], expected=['a/b'])
-    self.assert_scanned(['a/b:b'], expected=['a/b'])
-    self.assert_scanned(['a/b:c'], expected=['a/b:c'])
+    self.assert_parsed('a/b', expected=['a/b'])
+    self.assert_parsed('a/b:b', expected=['a/b'])
+    self.assert_parsed('a/b:c', expected=['a/b:c'])
 
   def test_sibling(self):
-    self.assert_scanned([':'], expected=[':root'])
-    self.assert_scanned(['//:'], expected=[':root'])
+    self.assert_parsed(':', expected=[':root'])
+    self.assert_parsed('//:', expected=[':root'])
 
-    self.assert_scanned(['a:'], expected=['a', 'a:b'])
-    self.assert_scanned(['//a:'], expected=['a', 'a:b'])
+    self.assert_parsed('a:', expected=['a', 'a:b'])
+    self.assert_parsed('//a:', expected=['a', 'a:b'])
 
-    self.assert_scanned(['a/b:'], expected=['a/b', 'a/b:c'])
-    self.assert_scanned(['//a/b:'], expected=['a/b', 'a/b:c'])
+    self.assert_parsed('a/b:', expected=['a/b', 'a/b:c'])
+    self.assert_parsed('//a/b:', expected=['a/b', 'a/b:c'])
 
   def test_sibling_or_descendents(self):
-    self.assert_scanned(['::'], expected=[':root', 'a', 'a:b', 'a/b', 'a/b:c'])
-    self.assert_scanned(['//::'], expected=[':root', 'a', 'a:b', 'a/b', 'a/b:c'])
+    self.assert_parsed('::', expected=[':root', 'a', 'a:b', 'a/b', 'a/b:c'])
+    self.assert_parsed('//::', expected=[':root', 'a', 'a:b', 'a/b', 'a/b:c'])
 
-    self.assert_scanned(['a::'], expected=['a', 'a:b', 'a/b', 'a/b:c'])
-    self.assert_scanned(['//a::'], expected=['a', 'a:b', 'a/b', 'a/b:c'])
+    self.assert_parsed('a::', expected=['a', 'a:b', 'a/b', 'a/b:c'])
+    self.assert_parsed('//a::', expected=['a', 'a:b', 'a/b', 'a/b:c'])
 
-    self.assert_scanned(['a/b::'], expected=['a/b', 'a/b:c'])
-    self.assert_scanned(['//a/b::'], expected=['a/b', 'a/b:c'])
+    self.assert_parsed('a/b::', expected=['a/b', 'a/b:c'])
+    self.assert_parsed('//a/b::', expected=['a/b', 'a/b:c'])
 
   def test_absolute(self):
-    self.assert_scanned([os.path.join(self.build_root, 'a')], expected=['a'])
-    self.assert_scanned([os.path.join(self.build_root, 'a:a')], expected=['a'])
-    self.assert_scanned([os.path.join(self.build_root, 'a:')], expected=['a', 'a:b'])
-    self.assert_scanned([os.path.join(self.build_root, 'a::')],
+    self.assert_parsed(os.path.join(self.build_root, 'a'), expected=['a'])
+    self.assert_parsed(os.path.join(self.build_root, 'a:a'), expected=['a'])
+    self.assert_parsed(os.path.join(self.build_root, 'a:'), expected=['a', 'a:b'])
+    self.assert_parsed(os.path.join(self.build_root, 'a::'),
                         expected=['a', 'a:b', 'a/b', 'a/b:c'])
 
     double_absolute = '/' + os.path.join(self.build_root, 'a')
@@ -61,55 +65,23 @@ class CmdLineSpecParserTest(BaseTest):
       self.spec_parser.parse_addresses('/not/the/buildroot/a').next()
 
   def test_cmd_line_affordances(self):
-    self.assert_scanned(['./:root'], expected=[':root'])
-    self.assert_scanned(['//./:root'], expected=[':root'])
-    self.assert_scanned(['//./a/../:root'], expected=[':root'])
-    self.assert_scanned([os.path.join(self.build_root, './a/../:root')],
+    self.assert_parsed('./:root', expected=[':root'])
+    self.assert_parsed('//./:root', expected=[':root'])
+    self.assert_parsed('//./a/../:root', expected=[':root'])
+    self.assert_parsed(os.path.join(self.build_root, './a/../:root'),
                        expected=[':root'])
 
-    self.assert_scanned(['a/'], expected=['a'])
-    self.assert_scanned(['./a/'], expected=['a'])
-    self.assert_scanned([os.path.join(self.build_root, './a/')], expected=['a'])
+    self.assert_parsed('a/', expected=['a'])
+    self.assert_parsed('./a/', expected=['a'])
+    self.assert_parsed(os.path.join(self.build_root, './a/'), expected=['a'])
 
-    self.assert_scanned(['a/b/:b'], expected=['a/b'])
-    self.assert_scanned(['./a/b/:b'], expected=['a/b'])
-    self.assert_scanned([os.path.join(self.build_root, './a/b/:b')], expected=['a/b'])
+    self.assert_parsed('a/b/:b', expected=['a/b'])
+    self.assert_parsed('./a/b/:b', expected=['a/b'])
+    self.assert_parsed(os.path.join(self.build_root, './a/b/:b'), expected=['a/b'])
 
   def test_cmd_line_spec_list(self):
-    self.assert_scanned(['a', 'a/b'], expected=['a', 'a/b'])
-    self.assert_scanned(['::'], expected=[':root', 'a', 'a:b', 'a/b', 'a/b:c'])
+    self.assert_parsed('a', 'a/b', expected=['a', 'a/b'])
+    self.assert_parsed('::', expected=[':root', 'a', 'a:b', 'a/b', 'a/b:c'])
 
-  def test_does_not_exist(self):
-    with self.assertRaises(self.spec_parser.BadSpecError):
-      list(self.spec_parser.parse_addresses('c'))
-
-    with self.assertRaises(self.spec_parser.BadSpecError):
-      list(self.spec_parser.parse_addresses('c:'))
-
-    with self.assertRaises(self.spec_parser.BadSpecError):
-      list(self.spec_parser.parse_addresses('c::'))
-
-  def test_build_ignore_patterns(self):
-    expected_specs = [':root', 'a', 'a:b', 'a/b', 'a/b:c']
-
-    # This bogus BUILD file gets in the way of parsing.
-    self.add_to_build_file('some/dir', 'COMPLETELY BOGUS BUILDFILE)\n')
-    with self.assertRaises(CmdLineSpecParser.BadSpecError):
-      self.assert_scanned(['::'], expected=expected_specs)
-
-    address_mapper_with_ignore = BuildFileAddressMapper(self.build_file_parser, self.project_tree,
-                                                        build_ignore_patterns=['some'])
-    self.spec_parser = CmdLineSpecParser(self.build_root, address_mapper_with_ignore)
-    self.assert_scanned(['::'], expected=expected_specs)
-
-  def test_exclude_target_regexps(self):
-    expected_specs = [':root', 'a', 'a:b', 'a/b', 'a/b:c']
-
-    # This bogus BUILD file gets in the way of parsing.
-    self.add_to_build_file('some/dir', 'COMPLETELY BOGUS BUILDFILE)\n')
-    with self.assertRaises(CmdLineSpecParser.BadSpecError):
-      self.assert_scanned(['::'], expected=expected_specs)
-
-    self.spec_parser = CmdLineSpecParser(self.build_root, self.address_mapper,
-                                         exclude_target_regexps=[r'.*some/dir.*'])
-    self.assert_scanned(['::'], expected=expected_specs)
+  def assert_parsed(self, spec_str, expected_spec):
+    self.assertEqual(self._spec_parser.parse_spec(spec_str), expected_spec)
