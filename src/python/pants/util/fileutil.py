d4482469b9aa937ee93eefe7add0f6e33f7e1574
Emily Caveness <emilycaveness@gmail.com>
2016-02-18 19:01:02 -0500

    Allow for random build ordering
    This change added a "random" choice to the --size-estimator option in JvmCompile, which is used to order job execution.  This change also added help to clarify the purpose of the "random" choice.
    
    Testing Done:
    CI passes here: https://travis-ci.org/pantsbuild/pants/builds/110182065
    
    Bugs closed: 2904, 2922
    
    Reviewed at https://rbcommons.com/s/twitter/r/3462/

--- src/python/pants/util/fileutil.py
+++ src/python/pants/util/fileutil.py
@@ -6,6 +6,7 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 import os
+import random
 import shutil
 
 from pants.util.contextutil import temporary_file
@@ -20,6 +21,13 @@ def atomic_copy(src, dst):
 
 
 def create_size_estimators():
+  """Create a dict of name to a function that returns an estimated size for a given target.
+
+  The estimated size is used to build the largest targets first (subject to dependency constraints).
+  Choose 'random' to choose random sizes for each target, which may be useful for distributed
+  builds.
+  :returns: Dict of a name to a function that returns an estimated size.
+  """
   def line_count(filename):
     with open(filename, 'rb') as fh:
       return sum(1 for line in fh)
@@ -28,4 +36,5 @@ def create_size_estimators():
     'filecount': lambda srcs: len(srcs),
     'filesize': lambda srcs: sum(os.path.getsize(src) for src in srcs),
     'nosize': lambda srcs: 0,
+    'random': lambda srcs: random.randint(0, 10000),
   }
