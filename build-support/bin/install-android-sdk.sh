414a32e14acaef0b9705aacf0779188336e3b203
Mateo Rodriguez <mateo@foursquare.com>
2016-03-04 23:09:09 +0000

    Switch to wget
    cause why not

--- build-support/bin/install-android-sdk.sh
+++ build-support/bin/install-android-sdk.sh
@@ -1,20 +1,21 @@
 # Install the Android SDK for the Pants Android contrib module.
-
+set -xf
 # ANDROID_HOME set in travis.yaml.
 # CURRENT_ANDROID_SDK_BUNDLE seeded in travis.yaml per OS.
 
 
 # This ANDROID_HOME is a hack to work for linux and osx - will be set perm in per-os yaml.
-
+# SDK_INSTALL_LOCATION="$HOME/opt/android-sdk-install"
+# ANDROID_HOME="$SDK_INSTALL_LOCATION/android-sdk-linux"
 mkdir -p "$SDK_INSTALL_LOCATION"
 
 ANDROID_SDK_URL="http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz"
 SDK_FILE=$(basename $ANDROID_SDK_URL)
 
-SDK_ARCHIVE_LOCATION = "$SDK_INSTALL_LOCATION/$SDK_FILE"
+echo "Downloading $ANDROID_SDK_URL..."
+SDK_ARCHIVE_LOCATION="$SDK_INSTALL_LOCATION"/"$SDK_FILE"
 
-curl "$ANDROID_SDK_URL" -O "$SDK_ARCHIVE_LOCATION"
-
+wget "$ANDROID_SDK_URL" -O "$SDK_ARCHIVE_LOCATION"
 tar -C "$SDK_INSTALL_LOCATION" -xf "$SDK_ARCHIVE_LOCATION"
 
 # Add SDKs as needed.
