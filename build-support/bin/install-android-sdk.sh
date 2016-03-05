34848a9debf166d15c2ff8aeccffea63aebd524b
Mateo Rodriguez <mateo@foursquare.com>
2016-03-05 06:43:34 +0000

    Make the keystore directory
    I am pretty sure the SDK makes this-
    unsure why this is needed here but oh well

--- build-support/bin/install-android-sdk.sh
+++ build-support/bin/install-android-sdk.sh
@@ -10,6 +10,9 @@ set -xf
 ANDROID_SDK_URL="http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz"
 SDK_FILE=$(basename $ANDROID_SDK_URL)
 
+# The debug.keystore has a well-known definition and location.
+KEYSTORE_LOCATION=$HOME/.android
+
 # Add SDKs as needed.
 declare -a SDK_MODULES=(platform-tools \
                        android-19 \
@@ -23,6 +26,7 @@ declare -a SDK_MODULES=(platform-tools \
 
 
 mkdir -p "$SDK_INSTALL_LOCATION"
+mkdir -p "$KEYSTORE_LOCATION"
 
 echo "Downloading $ANDROID_SDK_URL..."
 SDK_ARCHIVE_LOCATION="$SDK_INSTALL_LOCATION"/"$SDK_FILE"
@@ -33,8 +37,8 @@ tar -C "$SDK_INSTALL_LOCATION" -xf "$SDK_ARCHIVE_LOCATION"
 function join { local IFS="$1"; shift; echo "$*"; }
 MODULE_LIST=$(join , SDK_MODULES)
 
-echo "y" | "$ANDROID_HOME"/tools/android update sdk -u --all --filter $MODULE_LIST
+echo "y" | "$ANDROID_HOME"/tools/android update sdk -u --all --filter "$MODULE_LIST"
 
 # Generate well known debug.keystore
-keytool -genkey -v -keystore ~/.android/debug.keystore -alias androiddebugkey -storepass android  \
+keytool -genkey -v -keystore "$KEYSTORE_LOCATION"/debug.keystore -alias androiddebugkey -storepass android  \
         -keypass android -keyalg RSA -keysize 2048 -validity 10000 -dname "CN=Android Debug,O=Android,C=US"
