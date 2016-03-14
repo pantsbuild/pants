978cc6033db5eaa367d86a85e17ab81393bc3d28
Chris Heisterkamp <cheister@gmail.com>
2016-03-14 16:50:18 -0400

    add ignore option to pyflakes check
    Testing Done:
    Add ignore option to pyflakes style check that is similar to the ignore option for the PEP8 style check
    
    Using the option in pants.ini looks like
    
    ```
    [pycheck-pyflakes]
    ignore: [
        'F401'  # module imported but unused
      ]
    ```
    
    Travis CI: https://travis-ci.org/pantsbuild/pants/builds/115593092
    
    Bugs closed: 3041
    
    Reviewed at https://rbcommons.com/s/twitter/r/3569/

--- contrib/python/src/python/pants/contrib/python/checks/tasks/checkstyle/pep8_subsystem.py
+++ contrib/python/src/python/pants/contrib/python/checks/tasks/checkstyle/pep8_subsystem.py
@@ -13,23 +13,24 @@ from pants.contrib.python.checks.tasks.checkstyle.plugin_subsystem_base import P
 class PEP8Subsystem(PluginSubsystemBase):
   options_scope = 'pycheck-pep8'
 
+  # Code reference is here: http://pep8.readthedocs.org/en/latest/intro.html#error-codes
   DEFAULT_IGNORE_CODES = (
     # continuation_line_indentation
-    'E121',
-    'E124',
-    'E125',
-    'E127',
-    'E128',
+    'E121', # continuation line under-indented for hanging indent
+    'E124', # closing bracket does not match visual indentation
+    'E125', # continuation line with same indent as next logical line
+    'E127', # continuation line over-indented for visual indent
+    'E128', # continuation line under-indented for visual indent
 
     # imports_on_separate_lines
-    'E401',
+    'E401', # multiple imports on one line
 
     # indentation
-    'E111',
+    'E111', # indentation is not a multiple of four
 
     # trailing_whitespace
-    'W291',
-    'W293',
+    'W291', # trailing whitespace
+    'W293', # blank line contains whitespace
 
     # multiple statements
     # A common (acceptable) exception pattern at Twitter is:
@@ -39,10 +40,10 @@ class PEP8Subsystem(PluginSubsystemBase):
     #     class HerpError(Error): pass
     # We disable the pep8.py checking for these and instead have a more lenient filter
     # in the whitespace checker.
-    'E701',
-    'E301',
-    'E302',
-    'W292'
+    'E701', # multiple statements on one line (colon)
+    'E301', # expected 1 blank line, found 0
+    'E302', # expected 2 blank lines, found 0
+    'W292', # no newline at end of file
   )
 
   @classmethod
