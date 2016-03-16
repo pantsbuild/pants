e9e931498a84548536f4edaabfcfddc90bbfe3c9
Josh Soref <jsoref@users.noreply.github.com>
2016-03-16 19:13:11 +0000

    spelling: random

--- contrib/scrooge/tests/python/pants_test/contrib/scrooge/tasks/test_thrift_util.py
+++ contrib/scrooge/tests/python/pants_test/contrib/scrooge/tasks/test_thrift_util.py
@@ -29,7 +29,7 @@ class ThriftUtilTest(unittest.TestCase):
         include "sub/a_included.thrift" //Todo comment
         include "b_included.thrift"
         include "c_included.thrift" #jibberish
-        include "d_included.thrift" some ramdon
+        include "d_included.thrift" some random
       ''')
 
       a_included = self.write(os.path.join(a, 'sub', 'a_included.thrift'), '# noop')
