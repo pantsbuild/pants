b806ee0859c41f82d27290b0d8ada70b315de747
David Turner <dturner@twitter.com>
2016-03-25 14:54:37 -0400

    use server processor in example

--- contrib/go/testprojects/src/go/usethrift/example.go
+++ contrib/go/testprojects/src/go/usethrift/example.go
@@ -2,7 +2,25 @@ package usethrift
 
 import "thrifttest/duck"
 
+type EchoServerImpl struct {
+	//nothing
+}
+
+func (s EchoServerImpl) Ping() (err error) {
+	return nil
+}
+
+func (s EchoServerImpl) Echo(input string) (r string, err error) {
+	return input, nil
+}
+
 func whatevs() string {
 	d := duck.NewDuck()
+	s := &EchoServerImpl{}
+	p := duck.NewEchoServerProcessor(s)
+	_, ok := p.GetProcessorFunction("ping")
+	if !ok {
+		panic("no ping func")
+	}
 	return d.GetQuack()
 }
