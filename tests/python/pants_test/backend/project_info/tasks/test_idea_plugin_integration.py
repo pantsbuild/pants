2e74ceeffa1bf83c107fbaec2740a13607833655
wisechengyi <wisechengyi@gmail.com>
2016-04-12 18:08:10 -0700

    test fix

--- tests/python/pants_test/backend/project_info/tasks/test_idea_plugin_integration.py
+++ tests/python/pants_test/backend/project_info/tasks/test_idea_plugin_integration.py
@@ -8,7 +8,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import os
 from xml.dom import minidom
 
-from pants.backend.project_info.tasks.plugin_gen import PROJECT_OUTPUT_MESSAGE
 from pants.util.contextutil import temporary_file
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
