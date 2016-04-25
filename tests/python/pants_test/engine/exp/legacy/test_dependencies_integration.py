a2e13025d2fc9c4fc172c222066438887c377ecc
Stu Hood <stuhood@gmail.com>
2016-04-25 12:03:32 -0700

    [engine] Support for synthetic target injection
    In order to ship build graph caching before shipping support for codegen and other synthetic target injection tasks, we needed to add support to `legacy/graph.py` for injecting synthetic targets.
    
    - Move support for injecting synthetic targets back to the base class, see it is accomplished the same way by either implementation
    - Add support for `traversable_(dependency_)?specs`
    - In the `legacy:dependencies` impl, dump the transitive closure of targets rather than the visited addresses (the two are different in the presence of `traversable_(dependency_)?specs`)
    - Confirmed in the `legacy:dependencies` integration test that we get the synthetic scala target.
    
    Testing Done:
    https://travis-ci.org/pantsbuild/pants/builds/125159365
    
    Bugs closed: 3226, 3243
    
    Reviewed at https://rbcommons.com/s/twitter/r/3738/

--- tests/python/pants_test/engine/exp/legacy/test_dependencies_integration.py
+++ tests/python/pants_test/engine/exp/legacy/test_dependencies_integration.py
@@ -11,23 +11,30 @@ from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 
 class DependenciesIntegrationTest(PantsRunIntegrationTest, unittest.TestCase):
-  def do_deps(self, success, *args):
-    args = ['run', 'src/python/pants/engine/exp/legacy:dependencies', '--'] + list(args)
+  def assert_deps(self, success, spec, *expected_deps):
+    args = ['-q', 'run', 'src/python/pants/engine/exp/legacy:dependencies', '--'] + [spec]
     pants_run = self.run_pants(args)
     if success:
       self.assert_success(pants_run)
+      stdout_lines = pants_run.stdout_data.split('\n')
+      for dep in expected_deps:
+        self.assertIn(dep, stdout_lines)
     else:
       self.assert_failure(pants_run)
     return pants_run
 
-  def test_single(self):
-    self.do_deps(True, '3rdparty:guava')
+  def test_synthetic(self):
+    self.assert_deps(
+        True,
+        'examples/src/scala/org/pantsbuild/example/hello/welcome',
+        '//:scala_library_synthetic'
+      )
 
   def test_missing(self):
-    self.do_deps(False, '3rdparty:wait_seriously_there_is_a_library_named_that')
+    self.assert_deps(False, '3rdparty:wait_seriously_there_is_a_library_named_that')
 
   def test_siblings(self):
-    self.do_deps(True, '3rdparty:')
+    self.assert_deps(True, '3rdparty:')
 
   def test_descendants(self):
-    self.do_deps(True, '3rdparty::')
+    self.assert_deps(True, '3rdparty::')
