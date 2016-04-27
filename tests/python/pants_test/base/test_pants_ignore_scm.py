e63d4aed345e9b29081e0361d30ea6b9d8fa24c8
Yujie Chen <yujieproject@gmail.com>
2016-04-27 11:02:14 -0700

    Add support for pants-ignore to ProjectTree
    This review adds built-in support to src/python/pants/base/project_tree.py for an option --ignore-patterns. This will be a new option, independent from the existing --ignore-patterns option.
    
    The idea behind the split is that there are patterns that should never ever be matched by pants (similar to the things that are utterly ignored by git: those specified by .gitignore files) and should thus be pushed down into ProjectTree, which we hope will eventually be responsible for all filesystem access in pants. Additionally, there is a separate, higher-level set of ignore patterns that only applies to BUILD file parsing. The current --ignore-patterns option represents the latter high level option, as it is only (and should only) be applied to BUILD file parsing.
    
    Changes made:
    1. A new option has been added in global_options.py
    2. ProjectTree takes an additional init argument which is a list of ignore patterns (default to be [])
    3. All public methods of FileSystemProjectTree and ScmProjectTree have been made private and renemed to ..._raw since they don't have ignore logic.
    4. ProjectTree base class now has the implementation of common APIs for FileSystem and SCM project tree, with pants ignore logic. When querying ignored path, it will be shown as non-existent. When accessing ignored path, an exception will be thrown.
    5. ProjectTree created in legacy package is defaulted to ignore dotfiles.
    6. add 2 new test bases - ProjectTreeTestBase and PantsIgnoreTestBase (derived from ProjectTreeTestBase)
    7. 2 new test files to test pants ignore, 1 for file system project tree, 1 for scm project tree.
    8. remove "API public" doc string that was accodentally added in build_file_test_base.py.
    
    Something to notice:
    1. For pants ignore to work, there should be no "." in relative path to build_root.
    
    Testing Done:
    Latest CI:
    https://travis-ci.org/pantsbuild/pants/builds/126098504
    
    Bugs closed: 2956, 3232
    
    Reviewed at https://rbcommons.com/s/twitter/r/3698/

---
+++ tests/python/pants_test/base/test_pants_ignore_scm.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+# Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+import subprocess
+import unittest
+
+from pants.base.scm_project_tree import ScmProjectTree
+from pants.scm.git import Git
+from pants_test.base.pants_ignore_test_base import PantsIgnoreTestBase
+
+
+class ScmPantsIgnoreTest(unittest.TestCase, PantsIgnoreTestBase):
+  """
+  Common test cases are defined in PantsIgnoreTestBase.
+  Special test cases can be defined here.
+  """
+
+  def mk_project_tree(self, build_root, ignore_patterns=[]):
+    return ScmProjectTree(build_root, Git(worktree=build_root), 'HEAD', ignore_patterns)
+
+  def setUp(self):
+    super(ScmPantsIgnoreTest, self).setUp()
+    self.prepare()
+    subprocess.check_call(['git', 'init'])
+    subprocess.check_call(['git', 'config', 'user.email', 'you@example.com'])
+    subprocess.check_call(['git', 'config', 'user.name', 'Your Name'])
+    subprocess.check_call(['git', 'add', '.'])
+    subprocess.check_call(['git', 'commit', '-m' 'initial commit'])
+
+  def tearDown(self):
+    super(ScmPantsIgnoreTest, self).tearDown()
+    self.cleanup()
