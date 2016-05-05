bc6f9ebdf95a67d62c4f3d3c704c7fd415d19526
Kris Wilson <kwilson@twopensource.com>
2016-05-05 10:47:31 -0700

    Move watchman startup to its own test for better e2e timing.

--- src/python/pants/pantsd/service/pailgun_service.py
+++ src/python/pants/pantsd/service/pailgun_service.py
@@ -53,18 +53,21 @@ class PailgunService(PantsService):
       build_graph = None
 
       self._logger.debug('execution commandline: %s', arguments)
-      # N.B. This parses sys.argv by way of OptionsInitializer/OptionsBootstrapper prior to
-      # the main pants run to derive spec_roots for caching in the underlying scheduler.
-      spec_roots = self._parse_commandline_to_spec_roots(args=arguments)
-      self._logger.debug('parsed spec_roots: %s', spec_roots)
-      try:
-        self._logger.debug('requesting BuildGraph from %s', self._scheduler_service)
-        # N.B. This call is made in the pre-fork daemon context for reach and reuse of the resident
-        # scheduler for BuildGraph construction.
-        build_graph = self._scheduler_service.get_build_graph(spec_roots)
-      except Exception:
-        self._logger.warning('encountered exception during SchedulerService.get_build_graph():\n%s',
-                             traceback.format_exc())
+      if self._scheduler_service:
+        # N.B. This parses sys.argv by way of OptionsInitializer/OptionsBootstrapper prior to
+        # the main pants run to derive spec_roots for caching in the underlying scheduler.
+        spec_roots = self._parse_commandline_to_spec_roots(args=arguments)
+        self._logger.debug('parsed spec_roots: %s', spec_roots)
+        try:
+          self._logger.debug('requesting BuildGraph from %s', self._scheduler_service)
+          # N.B. This call is made in the pre-fork daemon context for reach and reuse of the
+          # resident scheduler for BuildGraph construction.
+          build_graph = self._scheduler_service.get_build_graph(spec_roots)
+        except Exception:
+          self._logger.warning(
+            'encountered exception during SchedulerService.get_build_graph():\n%s',
+            traceback.format_exc()
+          )
       return self._runner_class(sock, exiter, arguments, environment, build_graph)
 
     return PailgunServer(self._bind_addr, runner_factory)
