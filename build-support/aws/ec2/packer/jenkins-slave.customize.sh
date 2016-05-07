41eec04c22f14679bb4d10c6a74f2238a400d46c
John Sirois <john.sirois@gmail.com>
2016-05-07 13:55:28 -0600

    Expose JENKINS_LABELS to slaves.
    This also prints out the labels in print_node_info.sh.

--- build-support/aws/ec2/packer/jenkins-slave.customize.sh
+++ build-support/aws/ec2/packer/jenkins-slave.customize.sh
@@ -5,8 +5,6 @@
 set -o errexit
 set -o pipefail
 
-export DEBIAN_FRONTEND=noninteractive
-
 # Add some helpful info to the MOTD, cull some Ubuntu cruft.
 # ===
 cat << MOTD_SCRIPT | sudo tee /etc/update-motd.d/05-pantsbuild
@@ -58,64 +56,83 @@ sudo ln -s /mnt/xvdb/jenkins /jenkins
 # Install a small service that attempts to connect to the Jenkins master via JNLP.
 # ===
 sudo -u jenkins mkdir -p /home/jenkins/bin
-cat << JNLP_SERVICE | sudo -u jenkins tee /home/jenkins/bin/jenkins-slave-connect.sh
+cat << JNLP_SERVICE | sudo -u jenkins tee /home/jenkins/bin/jenkins-slave-connect
 #!/usr/bin/python2.7
 # Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-# Based on http://$JENKINS_URL/plugin/ec2/AMI-Scripts/ubuntu-init.py
+# Based on http://[JENKINS_URL]/plugin/ec2/AMI-Scripts/ubuntu-init.py
 
 from __future__ import print_function
 
 import base64
-import httplib
 import os
 import re
+import shutil
 import string
 import sys
 
-if len(sys.argv) != 2:
-  print('Usage: {} WORKDIR'.format(sys.argv[0]), file=sys.stderr)
+import xml.etree.ElementTree as ET
+
+import requests
+
+
+def die(message, *args, **kwargs):
+  print(message.format(*args, **kwargs), file=sys.stderr)
   sys.exit(1)
+
+
+if len(sys.argv) != 2:
+  die('Usage: {} WORKDIR', sys.argv[0])
 workdir = sys.argv[1]
 
 # This IP/HTTP endpoint is EC2 magic avaialable on every EC2 node and its used by
 # the Jenkins EC2 plugin.  See here for more details:
 #   http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html
-conn = httplib.HTTPConnection('169.254.169.254')
-conn.request('GET', '/latest/user-data')
-response = conn.getresponse()
-userdata = response.read()
-
-args = string.split(userdata, '&')
+response = requests.get('http://169.254.169.254/latest/user-data')
+if response.status_code != requests.codes.ok:
+  die('Failed to retrieve /latest/user-data: {}', response.status_code)
+args = string.split(response.text, '&')
 
 # The Jenkins Amazon EC2 plugin arranges JENKINS_URL and SLAVE_NAME automatically
 # and we configure 'User Data' in the plugin which populates USER_DATA.
 required_keys = ('JENKINS_URL', 'SLAVE_NAME', 'USER_DATA')
 params = {'workdir': workdir}
+username, password = None, None
 for arg in args:
   key, value = arg.split('=', 1)
   if key == 'JENKINS_URL':
-    value = re.sub(r'/$', '', value)
-  elif key == 'SLAVE_NAME':
-    slaveName = value
+    params[key] = re.sub(r'/$', '', value)
   elif key == 'USER_DATA':
     value = base64.b64decode(value)
+    username, password = value.split(':', 1)
   params[key] = value
 
 missing_keys = sorted(k for k in required_keys if k not in params)
 if missing_keys:
-  print('The following required EC2 /latest/user-data keys are missing:\n\t{}'
-        .format('\n\t'.join(missing_keys)), file=sys.stderr)
-  sys.exit(1)
+  die('The following required EC2 /latest/user-data keys are missing:\n\t{}',
+      '\n\t'.join(missing_keys))
 
-os.system('wget {JENKINS_URL}/jnlpJars/slave.jar -O {workdir}/slave.jar'.format(**params))
+response = requests.get('{JENKINS_URL}/jnlpJars/slave.jar'.format(**params), stream=True)
+if response.status_code != requests.codes.ok:
+  die('Failed to fetch slave.jar: {}', response.status_code)
+with open('{workdir}/slave.jar'.format(**params), 'wb') as fp:
+  response.raw.decode_content = True
+  shutil.copyfileobj(response.raw, fp)
+
+response = requests.get('{JENKINS_URL}/computer/{SLAVE_NAME}/config.xml'.format(**params),
+                        auth=(username, password))
+# This is optional data for Jenkinsfile nodes, so only process it if available.
+if response.status_code == requests.codes.ok:
+  labels = ' '.join(l.text for l in ET.fromstring(response.text).findall('label'))
+  os.putenv('JENKINS_LABELS', labels)
+
 os.system('java -jar {workdir}/slave.jar -noReconnect'
           ' -jnlpUrl {JENKINS_URL}/computer/{SLAVE_NAME}/slave-agent.jnlp'
           ' -jnlpCredentials {USER_DATA}'
           .format(**params))
 JNLP_SERVICE
-sudo -u jenkins chmod +x /home/jenkins/bin/jenkins-slave-connect.sh
+sudo -u jenkins chmod +x /home/jenkins/bin/jenkins-slave-connect
 
 cat << UNIT_FILE | sudo tee /etc/systemd/system/jenkins-slave-connect.service
 # Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
@@ -147,7 +164,7 @@ ExecStartPre=/bin/chown jenkins:jenkins /mnt/xvdb/jenkins
 
 # This runs as jenkins.
 User=jenkins
-ExecStart=/home/jenkins/bin/jenkins-slave-connect.sh /mnt/xvdb/jenkins
+ExecStart=/home/jenkins/bin/jenkins-slave-connect /mnt/xvdb/jenkins
 
 # Ideally we'd use on-failure but the jenkins.jar exits 0 for error conditions.
 # Since a Jenkins Slave should always be JNLP connected to the master to be useful,
