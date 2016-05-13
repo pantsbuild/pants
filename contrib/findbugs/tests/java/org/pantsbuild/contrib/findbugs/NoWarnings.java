ee49e2ba1a7021bf282655820966428609603721
Chris Heisterkamp <cheister@squareup.com>
2016-05-13 12:38:45 -0700

    Add FindBugs plugin to contrib

---
+++ contrib/findbugs/tests/java/org/pantsbuild/contrib/findbugs/NoWarnings.java
@@ -0,0 +1,10 @@
+// Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package org.pantsbuild.contrib.findbugs;
+
+public class NoWarnings {
+  public static void main(String[] args) {
+    System.out.println("No FindBugs warnings");
+  }
+}
