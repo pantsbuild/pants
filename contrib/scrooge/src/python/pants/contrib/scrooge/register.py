9c2733224774832b127705acf974d304eb2058b4
Wolfram Arnold <warnold@twitter.com>
2016-05-16 14:41:09 -0700

    Remove all the lua bits again... We'll add those internally (per review). Point java_thrift_library to new github issue

--- contrib/scrooge/src/python/pants/contrib/scrooge/register.py
+++ contrib/scrooge/src/python/pants/contrib/scrooge/register.py
@@ -5,24 +5,12 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-from pants.build_graph.build_file_aliases import BuildFileAliases
 from pants.goal.task_registrar import TaskRegistrar as task
 
-from pants.contrib.scrooge.targets.lua_library import LuaLibrary
-from pants.contrib.scrooge.tasks.lua_to_resources import LuaToResources
 from pants.contrib.scrooge.tasks.scrooge_gen import ScroogeGen
 from pants.contrib.scrooge.tasks.thrift_linter import ThriftLinter
-
-
-def build_file_aliases():
-  return BuildFileAliases(
-    targets={
-      'lua_library': LuaLibrary,
-    }
-  )
 
 
 def register_goals():
   task(name='thrift-linter', action=ThriftLinter).install()
   task(name='scrooge', action=ScroogeGen).install('gen')
-  task(name='lua-to-resources', action=LuaToResources).install('resources')
