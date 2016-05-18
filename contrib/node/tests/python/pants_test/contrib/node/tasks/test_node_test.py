b25945c1d7c6c82769392a946a004285cfd87048
Chris Pesto <cpesto@twitter.com>
2016-05-18 13:06:24 -0700

    Test

--- contrib/node/tests/python/pants_test/contrib/node/tasks/test_node_test.py
+++ contrib/node/tests/python/pants_test/contrib/node/tasks/test_node_test.py
@@ -35,39 +35,73 @@ class NodeTestTest(TaskTestBase):
     super(NodeTestTest, self).tearDown()
     NodeResolve._clear_resolvers()
 
-  def test_timeout(self):
-    self.create_file('src/node/too_long/package.json', contents=dedent("""
+  def _create_node_module_target(self):
+    self.create_file('src/node/test_node_test/package.json', contents=dedent("""
       {
-          "name": "pantsbuild.pants.test.too_long",
+          "name": "pantsbuild.pants.test.test_node_test",
           "version": "0.0.0",
           "scripts": {
             "test": "echo 0"
           }
       }
     """))
-    too_long_target = self.make_target(spec='src/node/too_long',
-                                       target_type=NodeModule,
-                                       sources=['package.json'])
+    return self.make_target(spec='src/node/test_node_test',
+                            target_type=NodeModule,
+                            sources=['package.json'])
 
-    too_long_target_test = self.make_target(spec='src/node/too_long:test',
-                                            target_type=NodeTestTarget,
-                                            dependencies=[too_long_target],
-                                            timeout = 5)
-
-    context = self.context(target_roots=[too_long_target_test])
+  def _resolve_node_module_and_create_tests_task(self, node_module_target, test_targets):
+    context = self.context(target_roots=test_targets)
 
     # Fake resolving so self.context.products.get_data(NodePaths) is populated for NodeTestTask.
-    too_long_target_root = os.path.join(self.build_root, too_long_target.address.spec_path)
+    node_module_target_root = os.path.join(self.build_root, node_module_target.address.spec_path)
     node_paths = context.products.get_data(NodePaths, init_func=NodePaths)
-    node_paths.resolved(too_long_target, too_long_target_root)
+    node_paths.resolved(node_module_target, node_module_target_root)
 
-    task = self.create_task(context)
+    return self.create_task(context)
+
+  def test_timeout(self):
+    target = self._create_node_module_target()
+
+    test_target = self.make_target(spec='src/node/test_node_test:test',
+                                   target_type=NodeTestTarget,
+                                   dependencies=[target],
+                                   timeout=5)
+
+    task = self._resolve_node_module_and_create_tests_task(target, [test_target])
 
     with patch('pants.task.testrunner_task_mixin.Timeout') as mock_timeout:
       mock_timeout().__exit__.side_effect = TimeoutReached(5)
 
       with self.assertRaises(TestFailedTaskError):
         task.execute()
+
+      # Ensures that Timeout is instantiated with a 5 second timeout.
+      args, kwargs = mock_timeout.call_args
+      self.assertEqual(args, (5,))
+
+  def test_timeout_multiple_targets(self):
+    target = self._create_node_module_target()
+
+    test_target1 = self.make_target(spec='src/node/test_node_test:test1',
+                                    target_type=NodeTestTarget,
+                                    dependencies=[target],
+                                    timeout=5)
+    test_target2 = self.make_target(spec='src/node/test_node_test:test2',
+                                    target_type=NodeTestTarget,
+                                    dependencies=[target],
+                                    timeout=5)
+
+    task = self._resolve_node_module_and_create_tests_task(target, [test_target1, test_target2])
+
+    with patch('pants.task.testrunner_task_mixin.Timeout') as mock_timeout:
+      mock_timeout().__exit__.side_effect = TimeoutReached(5)
+
+      with self.assertRaises(TestFailedTaskError) as cm:
+        task.execute()
+
+      # Verify that only the first target is in the failed_targets list, not all test targets.
+      self.assertEqual(len(cm.exception.failed_targets), 1)
+      self.assertEqual(cm.exception.failed_targets[0].address.spec, 'src/node/test_node_test:test1')
 
       # Ensures that Timeout is instantiated with a 5 second timeout.
       args, kwargs = mock_timeout.call_args
