28a3865db6b38db363619b86924e86d7ac70fead
Kris Wilson <kwilson@twopensource.com>
2016-05-25 15:44:56 -0700

    Add assertion that pantsd is not running during engine integration test.

--- tests/python/pants_test/engine/legacy/test_pants_engine_integration.py
+++ tests/python/pants_test/engine/legacy/test_pants_engine_integration.py
@@ -14,3 +14,4 @@ class PantsEngineIntegrationTest(PantsRunIntegrationTest):
     self.assert_success(pants_run)
     self.assertRegexpMatches(pants_run.stderr_data, 'build_graph is: .*LegacyBuildGraph')
     self.assertRegexpMatches(pants_run.stderr_data, 'ran \d+ scheduling iterations in')
+    self.assertNotRegexpMatches(pants_run.stderr_data, 'pantsd is running at pid \d+')
