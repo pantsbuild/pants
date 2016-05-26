0a0658dc89d9f61943c47fbb9eb921ae110d918e
Yujie Chen <yujieproject@gmail.com>
2016-05-26 15:26:21 -0400

    add test case

--- tests/python/pants_test/backend/python/tasks/test_python_binary_create.py
+++ tests/python/pants_test/backend/python/tasks/test_python_binary_create.py
@@ -5,10 +5,11 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import os
 from textwrap import dedent
 
 from pants.backend.python.tasks.python_binary_create import PythonBinaryCreate
-#from pants.util.contextutil import temporary_file_path
+from pants.base.run_info import RunInfo
 from pants_test.backend.python.tasks.python_task_test_base import PythonTaskTestBase
 
 
@@ -31,5 +32,23 @@ class PythonBinaryCreateTest(PythonTaskTestBase):
     self.binary = self.create_python_binary('src/python/bin', 'bin', 'lib.lib:main',
                                             dependencies=['//src/python/lib'])
 
-  def test_foo(self):
-    self.assertTrue(True)
+    self.task_context = self.context(target_roots=[self.binary])
+
+    self.run_info_dir = os.path.join(self.pants_workdir, self.options_scope, 'test/info')
+    self.task_context.run_tracker.run_info = RunInfo(self.run_info_dir)
+    self.test_task = self.create_task(self.task_context)
+    self.dist_root = os.path.join(self.build_root, 'dist')
+
+  def _check_products(self, bin_name):
+    pex_name = '{}.pex'.format(bin_name)
+    products = self.task_context.products.get('deployable_archive')
+    self.assertIsNotNone(products)
+    product_data = products.get(self.binary)
+    self.assertEquals({self.dist_root: [pex_name]},
+                      product_data)
+
+    self.assertTrue(os.path.exists(os.path.join(self.dist_root, pex_name)))
+
+  def test_deployable_archive_products(self):
+    self.test_task.execute()
+    self._check_products('bin')
