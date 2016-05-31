9e2b901e081e018299098bef9f258b8557f2e8bd
Stu Hood <stuhood@gmail.com>
2016-05-31 16:36:21 -0700

    Bump to 1.0.1

--- src/python/pants/version.py
+++ src/python/pants/version.py
@@ -8,6 +8,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 from pants.base.revision import Revision
 
 
-VERSION = '1.0.1-rc0'
+VERSION = '1.0.1'
 
 PANTS_SEMVER = Revision.semver(VERSION)
