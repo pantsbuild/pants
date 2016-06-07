52a18409fd936e2391770c934fa775be3d101e20
Kris Wilson <kwilson@twopensource.com>
2016-06-07 14:11:48 -0700

    Initial implementation.

--- src/python/pants/pantsd/util.py
+++ src/python/pants/pantsd/util.py
@@ -5,6 +5,7 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+from pants.bin.options_initializer import OptionsInitializer
 from pants.goal.goal import Goal
 from pants.goal.run_tracker import RunTracker
 
@@ -16,3 +17,6 @@ def clean_global_runtime_state():
 
   # Reset Goals and Tasks.
   Goal.clear()
+
+  # Reset backend/plugins state.
+  OptionsInitializer.reset()
