a05984373681f1b5975d048d8d54d20475b96c20
Ebube Chuba <echuba@twitter.com>
2016-06-16 13:48:52 -0700

    cleaned style issues

--- src/python/pants/core_tasks/clean.py
+++ src/python/pants/core_tasks/clean.py
@@ -10,13 +10,14 @@ import subprocess
 
 from pants.task.task import Task
 from pants.util.contextutil import temporary_dir
-from pants.util.dirutil import safe_concurrent_rename, safe_mkdir, safe_rmtree
+from pants.util.dirutil import safe_concurrent_rename, safe_mkdir
 
 
 class Clean(Task):
   """Delete all build products, creating a clean workspace.
   Speeds up run time of clean-all by moving current .pants.d to a temp folder
   and deleting it in a subprocess"""
+
   def execute(self):
     # although cleanup is set to False, temp dir is still deleted in subprocess
     with temporary_dir(cleanup=False) as tmpdir:
