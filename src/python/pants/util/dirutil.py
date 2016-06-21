98fc644784480ccca8aac99aa8dbdd5c00dc835b
Matt Olsen <digwanderlust@gmail.com>
2016-06-21 16:57:49 -0700

    fix typos

--- src/python/pants/util/dirutil.py
+++ src/python/pants/util/dirutil.py
@@ -16,18 +16,19 @@ import uuid
 from collections import defaultdict
 from contextlib import contextmanager
 
+from pants.base.build_root import BuildRoot
 from pants.util.strutil import ensure_text
 
 
 def atomic_copy_tree(src, dst):
   """Copy the directory src to dst, overwriting dst atomically."""
-  path = tempfile.mkdtemp(dir=root_dir, suffix=suffix)
+  path = tempfile.mkdtemp()
   try:
-    shutil.copytree(src, tmp_dst.name, symlinks=True)
-    os.chmod(tmp_dst.name, os.stat(src).st_mode)
-    os.rename(tmp_dst.name, dst)
+    shutil.copytree(src, path, symlinks=True)
+    os.chmod(path, os.stat(src).st_mode)
+    os.rename(path, dst)
   finally:
-      shutil.rmtree(path, ignore_errors=True)
+    shutil.rmtree(path, ignore_errors=True)
 
 
 def fast_relpath(path, start):
