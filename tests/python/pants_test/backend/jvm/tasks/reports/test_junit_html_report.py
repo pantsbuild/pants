534eb576d1948474f55f8dae13ca2d8349611c48
Eric Ayers <zundel@squareup.com>
2016-07-07 06:58:15 -0400

    Fix problems with unicode in junit XML output when writing to HTML report
    When there is unicode in the XML output, using --test-junit-html-report would result in an exception similar to:
    
    ```
    File "/Users/zundel/Src/Pants/src/python/pants/backend/jvm/tasks/junit_run.py", line 568, in _execute
        _do_report(exception=None)
      File "/Users/zundel/Src/Pants/src/python/pants/backend/jvm/tasks/junit_run.py", line 562, in _do_report
        html_file_path = JUnitHtmlReport().report(self.workdir, os.path.join(self.workdir, 'reports'))
      File "/Users/zundel/Src/Pants/src/python/pants/backend/jvm/tasks/reports/junit_html_report.py", line 104, in report
        fp.write(self.generate_html(testsuites))
    
    Exception message: 'ascii' codec can't encode character u'\xe5' in position 3508: ordinal not in range(128)
    ```
    
    This change converts the encoding to binary before writing it to the file.
    
    Testing Done:
    Added a unit test that reproduces the problem.
    
    CI green at https://travis-ci.org/pantsbuild/pants/builds/143000429
    
    Bugs closed: 3627, 3628
    
    Reviewed at https://rbcommons.com/s/twitter/r/4051/

--- tests/python/pants_test/backend/jvm/tasks/reports/test_junit_html_report.py
+++ tests/python/pants_test/backend/jvm/tasks/reports/test_junit_html_report.py
@@ -8,6 +8,8 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import os
 
 from pants.backend.jvm.tasks.reports.junit_html_report import JUnitHtmlReport
+from pants.util.contextutil import temporary_dir
+from pants.util.strutil import ensure_text
 from pants_test.base_test import BaseTest
 
 
@@ -15,7 +17,7 @@ class TestJUnitHtmlReport(BaseTest):
 
   def xml_file_path(self, file):
     return os.path.join(self.real_build_root,
-                        'tests/python/pants_test/backend/jvm/tasks/reports/junt_html_report_resources',
+                        'tests/python/pants_test/backend/jvm/tasks/reports/junit_html_report_resources',
                         file)
 
   def test_passing(self):
@@ -78,8 +80,27 @@ class TestJUnitHtmlReport(BaseTest):
     self.assertEqual(1, len(testsuites))
     self.assertEqual(0, len(testsuites[0].testcases))
 
+  def test_unicode(self):
+    testsuites = JUnitHtmlReport().parse_xml_file(self.xml_file_path('TEST-org.pantsbuild.UnicodeCharsTest.xml'))
+    self.assertEqual(1, len(testsuites))
+    self.assertEqual(2, testsuites[0].tests)
+    self.assertEqual(2, len(testsuites[0].testcases))
+    self.assertEquals(u'org.pantsbuild.PåssingTest', testsuites[0].name)
+    self.assertEquals(u'testTwö', testsuites[0].testcases[1].name)
+    self.assertIn(u'org.pantsbuild.PåssingTest.testTwö', testsuites[0].testcases[1].error['message'])
+
   def test_all(self):
     test_dir = os.path.join(self.real_build_root,
-                 'tests/python/pants_test/backend/jvm/tasks/reports/junt_html_report_resources')
+                 'tests/python/pants_test/backend/jvm/tasks/reports/junit_html_report_resources')
     testsuites = JUnitHtmlReport().parse_xml_files(test_dir)
-    self.assertEqual(6, len(testsuites))
+    self.assertEqual(7, len(testsuites))
+
+    with temporary_dir() as output_dir:
+      output_file = os.path.join(output_dir, 'junit-report.html')
+      JUnitHtmlReport().report(test_dir, output_dir)
+      self.assertTrue(os.path.exists(output_file))
+      with open(output_file) as html_file:
+        html_data = ensure_text(html_file.read())
+        self.assertIn(u'</span>&nbsp;org.pantsbuild.PåssingTest', html_data)
+        self.assertIn(u'</span>&nbsp;testTwö</td>', html_data)
+        self.assertIn(u'at org.pantsbuild.PåssingTest.testTwö(ErrorTest.java:29)', html_data)
