pantsbuild/pants

commit cb78b0ef7bc464fde4dfde2fb5aaecc18900785c

Implicitly provide source products.
The ide project gen tasks require the 'java', 'scala' products.
The only reason this works is that the scrooge task happens to
provide those products.  But if we try to deregister the codegen
backend in some repo that doesn't need it, we get a MissingProductError
from the round engine, and a quite unintuitive error message.

This change makes every source root implicitly provide a product
with the same name as that root's language(s).  This makes logical
sense, and also solves the problem above.

While testing this, I noticed an error in how we iterate over all
source roots: We assumed that all fixed source roots come from
the appropriate options. But in fact they might also be added
programmatically.  So this change also fixes that, and adds
appropriate testing.

Testing Done:
CI passed on travis: https://travis-ci.org/pantsbuild/pants/builds/142920098

Jenkins seemed to be having issues.

Reviewed at https://rbcommons.com/s/twitter/r/4053/

src/python/pants/engine/round_manager.py
@@ -36,6 +36,7 @@ class RoundManager(object):
     self._dependencies = set()
     self._context = context
     self._producer_infos_by_product_type = None
+    self._known_langs = set()
 
   def require(self, product_type):
     """Schedules the tasks that produce product_type to be executed before the requesting task.
@@ -63,8 +64,13 @@ class RoundManager(object):
   def _get_producer_infos_by_product_type(self, product_type):
     if self._producer_infos_by_product_type is None:
       self._producer_infos_by_product_type = self._index_products()
+      self._known_langs = set(self._context.source_roots.all_langs())
 
     producer_infos = self._producer_infos_by_product_type[product_type]
-    if not producer_infos:
+    # Products named for languages ('java', 'scala', 'python' etc.) are special: they represent
+    # source files in those languages, and are implicitly provided by a relevant source root.
+    # They may happen to also be explicitly provided by tasks (e.g., codegen(, but we don't want
+    # to fail with a MissingProductError if not.
+    if not producer_infos and product_type not in self._known_langs:
       raise self.MissingProductError("No producers registered for '{0}'".format(product_type))
     return producer_infos

src/python/pants/source/source_root.py
@@ -110,12 +110,10 @@ class SourceRoots(object):
                             ['pants_workdir', 'pants_supportdir', 'pants_distdir']})
 
     fixed_roots = set()
-    for fixed in self._options.source_roots, self._options.test_roots:
-      if fixed:
-        for root, langs in fixed.items():
-          if os.path.exists(os.path.join(buildroot, root)):
-            yield self._source_root_factory.create(root, langs)
-        fixed_roots.update(fixed.keys())
+    for root, langs in self._trie.fixed():
+      if os.path.exists(os.path.join(buildroot, root)):
+        yield self._source_root_factory.create(root, langs)
+      fixed_roots.add(root)
 
     for dirpath, dirnames, _ in os.walk(buildroot, topdown=True):
       relpath = os.path.relpath(dirpath, buildroot)
@@ -127,6 +125,11 @@ class SourceRoots(object):
           if not any(fixed_root.startswith(relpath) for fixed_root in fixed_roots):
             yield match  # Found a source root not a prefix of any fixed roots.
           del dirnames[:]  # Don't continue to walk into it.
+
+  def all_langs(self):
+    for root in self.all_roots():
+      for lang in root.langs:
+        yield lang
 
 
 class SourceRootConfig(Subsystem):
@@ -306,6 +309,17 @@ class SourceRootTrie(object):
       self.children[key] = child
       return child
 
+    def subpatterns(self):
+      if self.children:
+        for key, child in self.children.items():
+          for sp, langs in child.subpatterns():
+            if sp:
+              yield os.path.join(key, sp), langs
+            else:
+              yield key, langs
+      else:
+        yield '', self.langs
+
   def __init__(self, source_root_factory):
     self._source_root_factory = source_root_factory
     self._root = SourceRootTrie.Node()
@@ -314,9 +328,16 @@ class SourceRootTrie(object):
     """Add a pattern to the trie."""
     self._do_add_pattern(pattern, tuple())
 
-  def add_fixed(self, path, langs=None):
+  def add_fixed(self, path, langs):
     """Add a fixed source root to the trie."""
     self._do_add_pattern(os.path.join('^', path), tuple(langs))
+
+  def fixed(self):
+    """Returns a list of just the fixed source roots in the trie."""
+    for key, child in self._root.children.items():
+      if key == '^':
+        return list(child.subpatterns())
+    return []
 
   def _do_add_pattern(self, pattern, langs):
     keys = pattern.split(os.path.sep)

tests/python/pants_test/engine/test_round_engine.py
@@ -142,10 +142,24 @@ class RoundEngineTest(EngineTestBase, BaseTest):
       self.engine.attempt(self._context, self.as_goals('goal1'))
 
   def test_missing_product(self):
-    task =  self.install_task('task1', goal='goal1', required_data=['1'])
+    task = self.install_task('task1', goal='goal1', required_data=['1'])
     self.create_context(for_task_types=task)
     with self.assertRaises(self.engine.MissingProductError):
       self.engine.attempt(self._context, self.as_goals('goal1'))
+
+  def test_implicit_language_products(self):
+    task1 = self.install_task('task1', goal='goal1', required_data=['java'])
+    task2 = self.install_task('task2', goal='goal2', required_data=['scala'])
+    self.create_context(for_task_types=(task1 + task2))
+    self.create_dir('fixed/java')
+    self._context.source_roots.add_source_root('fixed/java', ['java'])
+
+    # Shouldn't raise, as we have a java source root to provide implicit java products.
+    self.engine.attempt(self._context, self.as_goals('goal1'))
+
+    # Should raise, as we do not have a scala source root, or any other source of scala products.
+    with self.assertRaises(self.engine.MissingProductError):
+      self.engine.attempt(self._context, self.as_goals('goal2'))
 
   def test_goal_cycle_direct(self):
     task1 = self.install_task('task1', goal='goal1', required_data=['2'], product_types=['1'])

tests/python/pants_test/source/test_source_root.py
@@ -79,6 +79,33 @@ class SourceRootTest(BaseTest):
                       trie.find('mysrc/scalastuff/org/pantsbuild/foo/Foo.scala'))
     self.assertIsNone(trie.find('my/project/mysrc/scalastuff/org/pantsbuild/foo/Foo.scala'))
 
+  def test_trie_traversal(self):
+    trie = SourceRootTrie(SourceRootFactory({}))
+    trie.add_pattern('foo1/bar1/baz1')
+    trie.add_pattern('foo1/bar1/baz2/qux')
+    trie.add_pattern('foo1/bar2/baz1')
+    trie.add_pattern('foo1/bar2/baz2')
+    trie.add_pattern('foo2/bar1')
+    trie.add_fixed('fixed1/bar1', ['lang1'])
+    trie.add_fixed('fixed2/bar2', ['lang2'])
+
+    # Test raw traversal.
+    self.assertEquals([('baz1', ()), ('baz2/qux', ())],
+                      list(trie._root.children['foo1'].children['bar1'].subpatterns()))
+
+    self.assertEquals([('bar1/baz1', ()), ('bar1/baz2/qux', ()),
+                       ('bar2/baz1', ()), ('bar2/baz2', ())],
+                      list(trie._root.children['foo1'].subpatterns()))
+
+    self.assertEquals([('foo1/bar1/baz1', ()), ('foo1/bar1/baz2/qux', ()),
+                       ('foo1/bar2/baz1', ()), ('foo1/bar2/baz2', ()), ('foo2/bar1', ()),
+                       ('^/fixed1/bar1', ('lang1',)), ('^/fixed2/bar2', ('lang2',))],
+                      list(trie._root.subpatterns()))
+
+    # Test the fixed() method.
+    self.assertEquals([('fixed1/bar1', ('lang1',)), ('fixed2/bar2', ('lang2',))],
+                      trie.fixed())
+
   def test_all_roots(self):
     self.create_dir('contrib/go/examples/3rdparty/go')
     self.create_dir('contrib/go/examples/src/go/src')
@@ -87,6 +114,7 @@ class SourceRootTest(BaseTest):
     self.create_dir('src/example/java')
     self.create_dir('src/example/python')
     self.create_dir('my/project/src/java')
+    self.create_dir('fixed/root/jvm')
     self.create_dir('not/a/srcroot/java')
 
     options = {
@@ -105,11 +133,16 @@ class SourceRootTest(BaseTest):
     options.update(self.options[''])  # We need inherited values for pants_workdir etc.
 
     source_roots = create_subsystem(SourceRootConfig, **options).get_source_roots()
+    # Ensure that we see any manually added roots.
+    source_roots.add_source_root('fixed/root/jvm', ('java', 'scala'))
+    source_roots.all_roots()
+
     self.assertEquals({SourceRoot('contrib/go/examples/3rdparty/go', ('go_remote',)),
                        SourceRoot('contrib/go/examples/src/go/src', ('go',)),
                        SourceRoot('src/java', ('java',)),
                        SourceRoot('src/python', ('python',)),
                        SourceRoot('src/example/java', ('java',)),
                        SourceRoot('src/example/python', ('python',)),
-                       SourceRoot('my/project/src/java', ('java',))},
+                       SourceRoot('my/project/src/java', ('java',)),
+                       SourceRoot('fixed/root/jvm', ('java','scala'))},
                       set(source_roots.all_roots()))

commit dbc708f164ecc2d9c614f2386a28f0f445b202b6

Support for optional product requirements.
That is, you can now request products for the purpose of scheduling
in the round manager, but it won't be an error if nothing produces them.

This is then used in ide_gen.py, to request source products ('java',
'python', etc.) This provides an alternative solution to the problem
of failing on missing products when no codegen produces those source
products.  The previous solution (cb78b0ef7bc) was to have source roots
implicitly provide those products.  However this caused performance
issues because we have to scan the filesystem for source roots. This
solution does not have that problem.

Testing Done:
CI passes: https://travis-ci.org/pantsbuild/pants/builds/144046650

I verified that this fixes the CI performance issues introduced by cb78b0ef7bc.

Reviewed at https://rbcommons.com/s/twitter/r/4071/

src/python/pants/backend/project_info/tasks/ide_gen.py
@@ -106,12 +106,16 @@ class IdeGen(IvyTaskMixin, NailgunTask):
   @classmethod
   def prepare(cls, options, round_manager):
     super(IdeGen, cls).prepare(options, round_manager)
+    # We optional_product() instead of require() because there's no guarantee
+    # that anything produces these products (typically only codegen produces them,
+    # because checked-in sources aren't considered products, at least in the old engine).
+    # See https://github.com/pantsbuild/pants/issues/3560.
     if options.python:
-      round_manager.require('python')
+      round_manager.optional_product('python')
     if options.java:
-      round_manager.require('java')
+      round_manager.optional_product('java')
     if options.scala:
-      round_manager.require('scala')
+      round_manager.optional_product('scala')
 
   class Error(TaskError):
     """IdeGen Error."""

src/python/pants/engine/round_manager.py
@@ -34,25 +34,53 @@ class RoundManager(object):
 
   def __init__(self, context):
     self._dependencies = set()
+    self._optional_dependencies = set()
     self._context = context
     self._producer_infos_by_product_type = None
-    self._known_langs = set()
 
   def require(self, product_type):
     """Schedules the tasks that produce product_type to be executed before the requesting task.
+
+    There must be at least one task that produces the required product type, or the
+    dependencies will not be satisfied.
 
     :API: public
     """
     self._dependencies.add(product_type)
     self._context.products.require(product_type)
 
+  def optional_product(self, product_type):
+    """Schedules tasks, if any, that produce product_type to be executed before the requesting task.
+
+    There need not be any tasks that produce the required product type.  All this method
+    guarantees is that if there are any then they will be executed before the requesting task.
+
+    :API: public
+    """
+    self._optional_dependencies.add(product_type)
+    self.require(product_type)
+
   def require_data(self, product_type):
     """Schedules the tasks that produce product_type to be executed before the requesting task.
+
+    There must be at least one task that produces the required product type, or the
+    dependencies will not be satisfied.
 
     :API: public
     """
     self._dependencies.add(product_type)
     self._context.products.require_data(product_type)
+
+  def optional_data(self, product_type):
+    """Schedules tasks, if any, that produce product_type to be executed before the requesting task.
+
+    There need not be any tasks that produce the required product type.  All this method
+    guarantees is that if there are any then they will be executed before the requesting task.
+
+    :API: public
+    """
+    self._optional_dependencies.add(product_type)
+    self.require_data(product_type)
 
   def get_dependencies(self):
     """Returns the set of data dependencies as producer infos corresponding to data requirements."""
@@ -64,13 +92,8 @@ class RoundManager(object):
   def _get_producer_infos_by_product_type(self, product_type):
     if self._producer_infos_by_product_type is None:
       self._producer_infos_by_product_type = self._index_products()
-      self._known_langs = set(self._context.source_roots.all_langs())
 
     producer_infos = self._producer_infos_by_product_type[product_type]
-    # Products named for languages ('java', 'scala', 'python' etc.) are special: they represent
-    # source files in those languages, and are implicitly provided by a relevant source root.
-    # They may happen to also be explicitly provided by tasks (e.g., codegen(, but we don't want
-    # to fail with a MissingProductError if not.
-    if not producer_infos and product_type not in self._known_langs:
+    if not producer_infos and product_type not in self._optional_dependencies:
       raise self.MissingProductError("No producers registered for '{0}'".format(product_type))
     return producer_infos

src/python/pants/source/source_root.py
@@ -126,11 +126,6 @@ class SourceRoots(object):
             yield match  # Found a source root not a prefix of any fixed roots.
           del dirnames[:]  # Don't continue to walk into it.
 
-  def all_langs(self):
-    for root in self.all_roots():
-      for lang in root.langs:
-        yield lang
-
 
 class SourceRootConfig(Subsystem):
   """Configuration for roots of source trees.

tests/python/pants_test/engine/test_round_engine.py
@@ -42,7 +42,8 @@ class RoundEngineTest(EngineTestBase, BaseTest):
   def construct_action(self, tag):
     return 'construct', tag, self._context
 
-  def record(self, tag, product_types=None, required_data=None, alternate_target_roots=None):
+  def record(self, tag, product_types=None, required_data=None, optional_data=None,
+             alternate_target_roots=None):
 
     class RecordingTask(Task):
       options_scope = tag
@@ -58,8 +59,10 @@ class RoundEngineTest(EngineTestBase, BaseTest):
 
       @classmethod
       def prepare(cls, options, round_manager):
-        for requirement in (required_data or ()):
-          round_manager.require_data(requirement)
+        for product in (required_data or ()):
+          round_manager.require_data(product)
+        for product in (optional_data or ()):
+          round_manager.optional_data(product)
         self.actions.append(self.prepare_action(tag))
 
       def __init__(me, *args, **kwargs):
@@ -72,12 +75,13 @@ class RoundEngineTest(EngineTestBase, BaseTest):
     return RecordingTask
 
   def install_task(self, name, product_types=None, goal=None, required_data=None,
-                   alternate_target_roots=None):
+                   optional_data=None, alternate_target_roots=None):
     """Install a task to goal and return all installed tasks of the goal.
 
     This is needed to initialize tasks' context.
     """
-    task_type = self.record(name, product_types, required_data, alternate_target_roots)
+    task_type = self.record(name, product_types, required_data, optional_data,
+                            alternate_target_roots)
     return super(RoundEngineTest,
                  self).install_task(name=name, action=task_type, goal=goal).task_types()
 
@@ -147,19 +151,11 @@ class RoundEngineTest(EngineTestBase, BaseTest):
     with self.assertRaises(self.engine.MissingProductError):
       self.engine.attempt(self._context, self.as_goals('goal1'))
 
-  def test_implicit_language_products(self):
-    task1 = self.install_task('task1', goal='goal1', required_data=['java'])
-    task2 = self.install_task('task2', goal='goal2', required_data=['scala'])
-    self.create_context(for_task_types=(task1 + task2))
-    self.create_dir('fixed/java')
-    self._context.source_roots.add_source_root('fixed/java', ['java'])
-
-    # Shouldn't raise, as we have a java source root to provide implicit java products.
+  def test_missing_optional_product(self):
+    task = self.install_task('task1', goal='goal1', optional_data=['1'])
+    self.create_context(for_task_types=task)
+    # Shouldn't raise, as the missing product is optional.
     self.engine.attempt(self._context, self.as_goals('goal1'))
-
-    # Should raise, as we do not have a scala source root, or any other source of scala products.
-    with self.assertRaises(self.engine.MissingProductError):
-      self.engine.attempt(self._context, self.as_goals('goal2'))
 
   def test_goal_cycle_direct(self):
     task1 = self.install_task('task1', goal='goal1', required_data=['2'], product_types=['1'])