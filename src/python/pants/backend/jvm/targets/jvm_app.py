bea6349be3be65dfc850ef03ef1accda87d5768b
Yujie Chen <yujieproject@gmail.com>
2016-07-14 11:59:06 -0700

    Convert BundleAdaptor to BundleProps during JvmApp target creation
    In v2 engine, TargetAdaptor serves as an intermediate state of a target and is eventually converted to its real target type, with one exception: BundleAdaptor.
    BundleAdaptor never gets converted to its "traditional" counterpart (which is BundleProps).
    BundleAdaptor structure and old BundleProps structure are not compatible, the hashing logic is based on BundleProps, thus exception will be thrown when applying the same logic onto BundleAdaptor
    
    This review converts BundleAdaptor to BundleProps during the creation of JvmApp target.
    
    Testing Done:
    ci green:
    https://travis-ci.org/pantsbuild/pants/builds/144788292
    
    Bugs closed: 3641
    
    Reviewed at https://rbcommons.com/s/twitter/r/4057/

--- src/python/pants/backend/jvm/targets/jvm_app.py
+++ src/python/pants/backend/jvm/targets/jvm_app.py
@@ -87,6 +87,13 @@ class BundleProps(namedtuple('_BundleProps', ['rel_path', 'mapper', 'fileset']))
     # Leave out fileset from hash calculation since it may not be hashable.
     return hash((self.rel_path, self.mapper))
 
+  @classmethod
+  def create_bundle_props(cls, file_set):
+    if not isinstance(file_set, FilesetWithSpec):
+      raise TypeError('The file_set must be a FilesetWithSpec, given {}.'.format(file_set))
+    mapper = RelativeToMapper(os.path.join(get_buildroot(), file_set.rel_root))
+    return cls(file_set.rel_root, mapper, file_set.files)
+
 
 class Bundle(object):
   """A set of files to include in an application bundle.
