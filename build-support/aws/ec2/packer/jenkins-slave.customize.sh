9a66a33708dfe23a89e321a60110f36a210960ed
Patrick Lawson <pl@foursquare.com>
2016-07-20 14:34:52 -0500

    Upstream Jenkins on-demand node manager

--- build-support/aws/ec2/packer/jenkins-slave.customize.sh
+++ build-support/aws/ec2/packer/jenkins-slave.customize.sh
@@ -28,24 +28,6 @@ cat << JENKINS_CLOUD_INIT | sudo tee /etc/cloud/cloud.cfg.d/99_jenkins_cloud_ini
 # Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-# This is a fix for the r3.xlarge instances we use as described here:
-#   https://forums.aws.amazon.com/thread.jspa?threadID=156342
-#
-# These instances come with unformatted local SSDs with TRIM support as outlined here:
-#   http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/InstanceStorage.html
-#
-# Although the fstab has an entry that mounts the local SSD epehemeral device to /mnt
-# this fails since the device is unformatted, so we format it here on 1st boot.
-#
-# Formatting advice taken from:
-#   http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ssd-instance-store.html
-bootcmd:
-- [ cloud-init-per, once, format_instance_storage, mkfs.ext4, '-E', nodiscard, /dev/xvdb ]
-- [ cloud-init-per, once, make_instance_storage_mount_point, mkdir, '-p', /mnt/xvdb ]
-
-mounts:
-- [ xvdb, /mnt/xvdb, ext4, 'defaults,nofail,discard', 0, 2 ]
-
 users:
 - default
 - name: jenkins
@@ -53,9 +35,9 @@ users:
   - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCWdCCh5LYbwKgFaEO2s1Hr+ONn55R+ViwDtCcLYGCdXMGFOD/0CEPYAPW2D2Cn29r1bxKryIGyA4RsAR5Op12AI9GwTXJF9nynjkeV+gKj2EMnqHzw53r1Rc5j8HMSTRXtjoRapjQ/BzNMHOkTVxwqdqnNmKlvRcFCpMCUA7Xz12q/4bhAHJ/TVnfZIDYBxNbvDfg8wKp0OHwnt2URbZJjKHll95wTJuiu72YoydiHEiCE2+Ja8l7/V54V7S8zoaJrxMDJiHJe2Yfx658D/olr83pGISytBXbhlxFFeVKnmj1l0sUpiUJSxlu2rq0tZdUJr/CYLae4r6mNKIoxxXB1
 
 runcmd:
-  - mkdir -p /mnt/xvdb/jenkins
-  - chown jenkins:jenkins /mnt/xvdb/jenkins
-  - rm -f /jenkins
-  - ln -s /mnt/xvdb/jenkins /jenkins
+  - echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-ff6daab6.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs4 defaults" >> /etc/fstab
+  - mkdir /mnt/efs
+  - mount -a
+  - mkdir -p /jenkins
+  - chown jenkins:jenkins /jenkins
 JENKINS_CLOUD_INIT
-
