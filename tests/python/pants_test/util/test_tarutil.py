e2c63af215e52168ede175fb643ea077a71935ff
Yujie Chen <yujieproject@gmail.com>
2016-07-25 17:08:37 -0400

    add a test case

--- tests/python/pants_test/util/test_tarutil.py
+++ tests/python/pants_test/util/test_tarutil.py
@@ -5,25 +5,49 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import os
 import tarfile
 import unittest
 
+from pants.util.dirutil import safe_delete, safe_mkdtemp, safe_rmtree, touch
 from pants.util.tarutil import TarFile
 
 
 class TarutilTest(unittest.TestCase):
   def setUp(self):
-    import pdb;pdb.set_trace()
+    self.basedir = safe_mkdtemp()
+
+    self.file_list = ['a', 'b', 'c']
+    self.file_tar = os.path.join(self.basedir, 'test.tar')
+
+    tar = TarFile.open(self.file_tar, mode='w')
+    for f in self.file_list:
+      full_path = os.path.join(self.basedir, f)
+      touch(full_path)
+      tar.add(full_path, f)
+      safe_delete(full_path)
+
+    tar.close()
 
   def tearDown(self):
-    tarfile.__author__
-    TarFile.__doc__
+    safe_rmtree(self.basedir)
 
-  def test_getattr(self):
-    pass
+  def inject_corruption(self):
+    with open(self.file_tar, 'r+w') as fp:
+      content = fp.read()
+      fp.seek(0)
+      fp.write(content[:512+148] + 'aaaaaaaa' + content[512+156:])
 
-  def test_recv(self):
-    pass
+  def extract_tar(self, path, **kwargs):
+    tar = TarFile.open(self.file_tar, mode='r', **kwargs)
+    tar.extractall(path=self.basedir)
+    tar.close()
 
-  def test_recv_max_larger_than_buf(self):
-    pass
+  def test_invalid_header_errorlevel_0(self):
+    self.inject_corruption()
+    self.assertIsNone(self.extract_tar(self.file_tar, errorlevel=0))
+
+  def test_invalid_header_errorlevel_1(self):
+    self.inject_corruption()
+    with self.assertRaises(tarfile.ReadError):
+      self.extract_tar(self.file_tar, errorlevel=1)
