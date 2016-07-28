3ee89fe3124f1feb45118127a02130ec872811af
wisechengyi <wisechengyi@gmail.com>
2016-07-28 00:32:34 -0700

    eh

--- build-support/build-python.sh
+++ build-support/build-python.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -o xtrace
+$PYTHON_DIR=$HOME/python27-ucs4/python
+if [ -f $PYTHON_DIR ];
+then
+  exit 0
+fi
 
 wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
 tar xvf Python-2.7.10.tgz
 cd Python-2.7.10
-./configure --enable-unicode=ucs4 --prefix=/home/travis/bin
+./configure --enable-unicode=ucs4 --prefix=$PYTHON_DIR
 make
 make install
  
-which python2.7
+alias python2.7=$PYTHON_DIR/python
