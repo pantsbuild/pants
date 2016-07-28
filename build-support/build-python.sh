65c34e5c1fc7a8dbd1d06c29ebdfab4aaab1db8e
wisechengyi <wisechengyi@gmail.com>
2016-07-27 23:48:27 -0700

    ugh

--- build-support/build-python.sh
+++ build-support/build-python.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
-tar xvf Python-2.7.10
-cd Python-2.7.10.tgz
+tar xvf Python-2.7.10.tgz
+cd Python-2.7.10
 ./configure --enable-unicode=ucs4 --prefix=/home/travis/bin
 make
 make install
