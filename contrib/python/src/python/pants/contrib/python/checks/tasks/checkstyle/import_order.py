4038e212d67b5df3b4c0ee9b437891b3c7370054
wisechengyi <wisechengyi@gmail.com>
2016-08-02 15:49:35 -0700

    sep out, no endswith

--- contrib/python/src/python/pants/contrib/python/checks/tasks/checkstyle/import_order.py
+++ contrib/python/src/python/pants/contrib/python/checks/tasks/checkstyle/import_order.py
@@ -64,21 +64,6 @@ class ImportOrder(CheckstylePlugin):
 
   @classmethod
   def classify_import(cls, node, name):
-    def is_module_on_std_lib_path(module):
-      """
-      Sometimes .py is a symlinked to the python interpreter, but .pyc
-      is generated next to it. Hence this function checks for both.
-      :param module: a module
-      :return: True is module is on interpreter's stdlib path.
-      """
-      module_file_real_path = os.path.realpath(module.__file__)
-      if module_file_real_path.startswith(cls.STANDARD_LIB_PATH):
-        return True
-      elif module_file_real_path.endswith('.pyc'):
-        py_file_real_path = os.path.realpath(os.path.splitext(module_file_real_path)[0] + '.py')
-        return py_file_real_path.startswith(cls.STANDARD_LIB_PATH)
-      return False
-
     if name == '' or (isinstance(node, ast.ImportFrom) and node.level > 0):
       return ImportType.PACKAGE
     if name.startswith('twitter.'):
@@ -89,7 +74,7 @@ class ImportOrder(CheckstylePlugin):
       module = __import__(name)
     except ImportError:
       return ImportType.THIRD_PARTY
-    if not hasattr(module, '__file__') or is_module_on_std_lib_path(module):
+    if not hasattr(module, '__file__') or cls.is_module_on_std_lib_path(module):
       return ImportType.STDLIB
     # Assume anything we can't classify is third-party
     return ImportType.THIRD_PARTY
@@ -98,6 +83,22 @@ class ImportOrder(CheckstylePlugin):
   def classify_import_node(cls, node):
     return set(cls.classify_import(node, module_name)
                for module_name in cls.extract_import_modules(node))
+
+  @classmethod
+  def is_module_on_std_lib_path(cls, module):
+    """
+    Sometimes .py is a symlinked to the python interpreter, but .pyc
+    is generated next to it. Hence this function checks for both.
+    :param module: a module
+    :return: True is module is on interpreter's stdlib path.
+    """
+    module_file_real_path = os.path.realpath(module.__file__)
+    if module_file_real_path.startswith(cls.STANDARD_LIB_PATH):
+      return True
+    elif os.path.splitext(module_file_real_path.endswith)[1] == '.pyc':
+      py_file_real_path = os.path.realpath(os.path.splitext(module_file_real_path)[0] + '.py')
+      return py_file_real_path.startswith(cls.STANDARD_LIB_PATH)
+    return False
 
   def import_errors(self, node):
     errors = []
