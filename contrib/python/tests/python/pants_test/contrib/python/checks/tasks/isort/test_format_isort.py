978d985d777235cf9caec50b38241d748ebfd26a
wisechengyi <wisechengyi@gmail.com>
2016-08-02 22:38:11 -0700

    import fix

--- contrib/python/tests/python/pants_test/contrib/python/checks/tasks/isort/test_format_isort.py
+++ contrib/python/tests/python/pants_test/contrib/python/checks/tasks/isort/test_format_isort.py
@@ -5,6 +5,8 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import subprocess
+
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 
