0ddbcfb4062da8b27393831b8ce435a7e859eabc
wisechengyi <wisechengyi@gmail.com>
2016-08-02 17:30:08 -0700

    looks good

--- contrib/python/src/python/pants/contrib/python/checks/tasks/isort/python_isort.py
+++ contrib/python/src/python/pants/contrib/python/checks/tasks/isort/python_isort.py
@@ -5,25 +5,45 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import logging
+
 import isort
+from pants.backend.python.tasks.python_task import PythonTask
 
-from pants.contrib.python.checks.tasks.checkstyle.checker import PythonCheckStyleTask
+from pants.option.custom_types import file_option
 
 
-class IsortPythonTask(PythonCheckStyleTask):
+class IsortPythonTask(PythonTask):
   """Add additonal steps to OSS PythonCheckStyleTask."""
+
+  _PYTHON_SOURCE_EXTENSION = '.py'
+
+  def __init__(self, *args, **kwargs):
+    super(IsortPythonTask, self).__init__(*args, **kwargs)
+    self.options = self.get_options()
 
   @classmethod
   def register_options(cls, register):
     super(IsortPythonTask, cls).register_options(register)
-    # register('--enable-import-sorting', fingerprint=True, default=False, type=bool,
-    #          help='sort python imports automatically.')
-    # register('--prompt', default=False, type=bool,
-    #          help='If enabled, skip this style checker.')
+    register('--config-file', fingerprint=True, type=file_option, default=None,
+             help='Specify isort config file.')
 
   def execute(self):
-    """Run Checkstyle on all found source files."""
-    # if self.options.skip:
-    #   return
+    """Run isort on all found source files."""
+
+    if self.options.config_file is None:
+      logging.error("Please specify config file with --config-file")
+      exit(1)
+
     for source in self.calculate_sources(self.context.targets()):
-      isort.SortImports(file_path=source, settings_path='./.isort.cfg')
+      isort.SortImports(file_path=source, settings_path=self.options.config_file)
+
+  def calculate_sources(self, targets):
+    """Generate a set of source files from the given targets."""
+    sources = set()
+    for target in targets:
+      sources.update(
+        source for source in target.sources_relative_to_buildroot()
+        if source.endswith(self._PYTHON_SOURCE_EXTENSION)
+      )
+    return sources
