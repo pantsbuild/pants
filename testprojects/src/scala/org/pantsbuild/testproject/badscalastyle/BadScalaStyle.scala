667847a1a4334f7befafb654eb36250feff38ec0
kmccaffrey <caitiem20@gmail.com>
2016-08-04 17:45:01 -0700

    scalafmt

---
+++ testprojects/src/scala/org/pantsbuild/testproject/badscalastyle/BadScalaStyle.scala
@@ -0,0 +1,11 @@
+// Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package org.pantsbuild.testproject.badscalastyle
+
+/**
+  * These comments are formmated incorrectly
+  * and the parameter list is too long for one line
+  */
+case class ScalaStyle(one: String, two: String, three: String, four: String, five: String, six: String, seven:String, eight:String, nine: String)
+  
