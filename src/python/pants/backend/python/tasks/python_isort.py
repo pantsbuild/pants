966e30f632bfb4ff8ee0d18f0b1ab59b68c82487
wisechengyi <wisechengyi@gmail.com>
2016-08-04 18:00:05 -0700

    almost good

--- src/python/pants/backend/python/tasks/python_isort.py
+++ src/python/pants/backend/python/tasks/python_isort.py
@@ -5,10 +5,12 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import logging
 import os
+import subprocess
 
-import isort
 from pants.backend.python.tasks.python_task import PythonTask
+from pants.binaries.binary_util import BinaryUtil
 from pants.option.custom_types import file_option
 
 
@@ -34,8 +36,31 @@ class IsortPythonTask(PythonTask):
     if self.options.skip:
       return
 
+    logging.info("hello")
+    isort_script = BinaryUtil.Factory.create().select_script('scripts/isort', '4.2.5', 'isort.pex')
+    # See http://cloc.sourceforge.net/#options for cloc cmd-line options.
+
+
+
+    #
     for source in self._calculate_sources(self.context.targets()):
-      isort.SortImports(file_path=source, settings_path=self.options.config_file)
+      cmd = [isort_script, source
+             # '--skip-uniqueness',
+             # '--ignored={}'.format(ignored_file),
+             # '--list-file={}'.format(list_file),
+             # '--report-file={}'.format(report_file)
+             ]
+      subprocess.check_call(cmd)
+
+        # with self.context.new_workunit(name='cloc',
+        #                                labels=[WorkUnitLabel.TOOL],
+        #                                cmd=' '.join(cmd)) as workunit:
+        #   result = subprocess.call(cmd,
+        #                            stdout=workunit.output('stdout'),
+        #                            stderr=workunit.output('stderr'))
+
+      # if result != 0:
+      #   raise TaskError('{} ... exited non-zero ({}).'.format(' '.join(cmd), result))
 
   def _calculate_sources(self, targets):
     """Generate a set of source files from the given targets."""
@@ -46,3 +71,7 @@ class IsortPythonTask(PythonTask):
         if os.path.splitext(source)[1] == self._PYTHON_SOURCE_EXTENSION
       )
     return sources
+
+# if __name__ == '__main__':
+#   sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+#   sys.exit(main())
