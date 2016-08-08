a94e2edfbf958b0bf243e1803b2cf04780ce2e45
wisechengyi <wisechengyi@gmail.com>
2016-08-07 22:02:35 -0700

    remove unused

--- tests/python/pants_test/backend/python/tasks/test_python_isort.py
+++ tests/python/pants_test/backend/python/tasks/test_python_isort.py
@@ -6,8 +6,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 import os
-import subprocess
-import sys
 from textwrap import dedent
 
 from pytest import fail
