911c6520da5a7fb7336aeb8721a35fc0ce937957
Nick Howard <nhoward@twitter.com>
2016-08-16 10:33:54 -0600

    reduce iterability of objects
    need to check tests after this

--- src/python/pants/util/objects.py
+++ src/python/pants/util/objects.py
@@ -9,14 +9,26 @@ from collections import namedtuple
 
 
 def datatype(*args, **kwargs):
-  """A wrapper for `namedtuple` that accounts for the type of the object in equality."""
+  """A wrapper for `namedtuple` that accounts for the type of the object in equality.
+
+  :param is_iterable: If passed as True, then the resulting type will be iterable. False by default.
+  """
+  is_iterable = kwargs.pop('is_iterable', False)
   class DataType(namedtuple(*args, **kwargs)):
     def __eq__(self, other):
       if self is other:
         return True
+
       # Compare types and fields.
-      return type(other) == type(self) and super(DataType, self).__eq__(other)
+      if type(self) != type(other):
+        return False
+      # Explicitly return super.__eq__'s value in case super returns NotImplemented
+      return super(DataType, self).__eq__(other)
 
     def __ne__(self, other):
       return not (self == other)
+
+    if not is_iterable:
+      def __iter__(self):
+        raise TypeError("'{}' object is not iterable".format(type(self).__name__))
   return DataType
---
+++ tests/python/pants_test/util/test_objects.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+# Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+import unittest
+
+from pants.util.objects import datatype
+
+
+class AbsClass(object):
+  pass
+
+
+class ReturnsNotImplemented(object):
+  def __eq__(self, other):
+    return NotImplemented
+
+
+class DatatypeTest(unittest.TestCase):
+
+  def test_eq_with_not_implemented_super(self):
+    class DatatypeSuperNotImpl(datatype('Foo', ['val']), ReturnsNotImplemented, tuple):
+      pass
+
+    self.assertNotEqual(DatatypeSuperNotImpl(1), DatatypeSuperNotImpl(1))
+
+  def test_repr(self):
+    bar = datatype('Bar', ['val', 'zal'])
+    self.assertEqual('Bar(val=1, zal=1)',repr(bar(1, 1)))
+
+    class Foo(datatype('F', ['val']), AbsClass):
+      pass
+
+    # Maybe this should be 'Foo(val=1)'?
+    self.assertEqual('F(val=1)', repr(Foo(1)))
+
+  def test_not_iterable_by_default(self):
+    bar = datatype('Bar', ['val'])
+    with self.assertRaises(TypeError):
+      for x in bar(1):
+        pass
+
+  def test_iterable_when_is_iterable(self):
+    bar = datatype('Bar', ['val'], is_iterable=True)
+    self.assertEqual([1], [x for x in bar(1)])
