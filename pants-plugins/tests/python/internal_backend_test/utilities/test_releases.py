69fcf06e54c7ead0b5851624823d1c7f66a56ce7
Matt Olsen <matt@digital-wanderlust.com>
2016-09-01 10:21:50 -0700

    Update pants versioning to use python's packaging.version
    Change pants versioning to use python's packaging.version rather than semver.  Python versioning doesn't
    follow the semver spec. This change allows us to more closely follow the python spec.  Specifically
    python prefers versions of the form 1.2.0dev6 rather than 1.2.0-dev6.  Semver does not allow for 1.2.0dev6.
    
    This change is a follow up to changes to our naming conventions.
    
    Testing Done:
    https://travis-ci.org/pantsbuild/pants/builds/156597437
    
    Ran ./build-support/bin/release.sh -n with both '1.2.0-dev6' and '1.2.0dev6'
    
    Bugs closed: 3382, 3827
    
    Reviewed at https://rbcommons.com/s/twitter/r/4200/

--- pants-plugins/tests/python/internal_backend_test/utilities/test_releases.py
+++ pants-plugins/tests/python/internal_backend_test/utilities/test_releases.py
@@ -7,21 +7,24 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 
 import unittest
 
+from packaging.version import Version
+
 from internal_backend.utilities.register import PantsReleases
-from pants.base.revision import Revision
 
 
 def _branch_name(revision_str):
-  return PantsReleases._branch_name(Revision.semver(revision_str))
+  return PantsReleases._branch_name(Version(revision_str))
 
 
 class ReleasesTest(unittest.TestCase):
 
   def test_branch_name_master(self):
     self.assertEquals('master', _branch_name('1.1.0-dev1'))
+    self.assertEquals('master', _branch_name('1.1.0dev1'))
 
   def test_branch_name_stable(self):
     self.assertEquals('1.1.x', _branch_name('1.1.0-rc1'))
+    self.assertEquals('1.1.x', _branch_name('1.1.0rc1'))
     self.assertEquals('2.1.x', _branch_name('2.1.0'))
 
   def test_branch_name_unknown_suffix(self):
