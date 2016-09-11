c1b881a89dc5d16045b6a43971991cde924f7ce2
Benjy <benjyw@gmail.com>
2016-09-11 13:28:30 -0700

    Remove an unneeded import.

--- src/python/pants/build_graph/build_configuration.py
+++ src/python/pants/build_graph/build_configuration.py
@@ -5,7 +5,6 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-import inspect
 import logging
 from collections import Iterable, namedtuple
 
