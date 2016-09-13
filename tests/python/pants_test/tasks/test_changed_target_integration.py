2f520bab6e7dad26281c6875d88d638b0904ea85
John Sirois <john.sirois@gmail.com>
2016-09-13 12:10:56 -0600

    Restore ChangedTargetGoalsIntegrationTest.
    All of its tests were marked xfail but are now re-enabled with changes
    to ensure the targets and git repo under test are isolated from the
    host pants repo.
    
    In order to facilitate integration tests using an ephemeral git repo,
    a change is made to `build-support/pants_venv` to not rely on git to
    find the host pants repo root directory.
    
    Testing Done:
    Locally green: `./pants test-changed`.
    
    CI went green here:
      https://travis-ci.org/pantsbuild/pants/builds/159416022
    
    Bugs closed: 3859
    
    Reviewed at https://rbcommons.com/s/twitter/r/4227/

--- tests/python/pants_test/tasks/test_changed_target_integration.py
+++ tests/python/pants_test/tasks/test_changed_target_integration.py
@@ -6,56 +6,123 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 import os
-from unittest import expectedFailure
+from contextlib import contextmanager
+from textwrap import dedent
 
+from pants.base.build_environment import get_buildroot
+from pants.util.contextutil import temporary_dir
+from pants.util.dirutil import safe_open
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
+from pants_test.testutils.git_util import initialize_repo
 
 
 class ChangedTargetGoalsIntegrationTest(PantsRunIntegrationTest):
-  def ref_for_greet_change(self):
-    # Unfortunately, as in being an integration test, it is difficult to mock the SCM used.
-    # Thus this will use the pants commit log, so we need a commit that changes greet example.
-    # Any comment/whitespace/etc change is enough though, as long as we know the SHA.
-    return '14cc5bc23561918dc7134427bfcb268506fcbcaa'
+  @contextmanager
+  def known_commits(self):
+    with temporary_dir(root_dir=get_buildroot()) as worktree:
+      with safe_open(os.path.join(worktree, 'README'), 'w') as fp:
+        fp.write('Just a test tree.')
 
-  def greet_classfile(self, workdir, filename):
-    path = 'compile/jvm/java/classes/org/pantsbuild/example/hello/greet'.split('/')
-    return os.path.join(workdir, *(path + [filename]))
+      with initialize_repo(worktree=worktree, gitdir=os.path.join(worktree, '.git')) as git:
+        src_file = os.path.join(worktree, 'src/java/org/pantsbuild/Class.java')
+        with safe_open(src_file, 'w') as fp:
+          fp.write(dedent("""
+          package org.pantsbuild;
 
-  @expectedFailure
+          class Class {
+            static final int MEANING_OF_LIFE = 42;
+          }
+          """))
+
+        src_build_file = os.path.join(worktree, 'src/java/org/pantsbuild/BUILD')
+        with safe_open(src_build_file, 'w') as fp:
+          fp.write("java_library(name='pantsbuild', sources=['Class.java'])")
+
+        git.add(src_file, src_build_file)
+        git.commit('Introduce Class.')
+
+        test_file = os.path.join(worktree, 'tests/java/org/pantsbuild/ClassTest.java')
+        with safe_open(test_file, 'w') as fp:
+          fp.write(dedent("""
+          package org.pantsbuild;
+
+          import org.junit.Assert;
+          import org.junit.Test;
+
+          public class ClassTest {
+            @Test public void test() {
+              Assert.assertEquals(42, Class.MEANING_OF_LIFE);
+            }
+          }
+          """))
+
+        test_build_file = os.path.join(worktree, 'tests/java/org/pantsbuild/BUILD')
+        with safe_open(test_build_file, 'w') as fp:
+          fp.write(dedent("""
+          jar_library(name='junit', jars=[jar('junit', 'junit', '4.12')])
+
+          junit_tests(
+            name='pantsbuild',
+            sources=['ClassTest.java'],
+            dependencies=[
+              ':junit',
+              '{}'
+            ]
+          )
+          """).format(os.path.relpath(os.path.dirname(src_build_file), get_buildroot())))
+
+        git.add(test_file, test_build_file)
+        git.commit('Introduce ClassTest.')
+
+        yield
+
+  _PACKAGE_PATH_PREFIX = os.sep + os.path.join('classes', 'org', 'pantsbuild')
+
+  def find_classfile(self, workdir, filename):
+    for root, dirs, files in os.walk(os.path.join(workdir, 'compile', 'zinc')):
+      for f in files:
+        candidate = os.path.join(root, f)
+        if candidate.endswith(os.path.join(self._PACKAGE_PATH_PREFIX, filename)):
+          return candidate
+
   def test_compile_changed(self):
-    cmd = ['compile-changed', '--diffspec={}'.format(self.ref_for_greet_change())]
+    with self.known_commits():
+      # Just look for changes in the 1st commit (addition of Class.java).
+      cmd = ['compile-changed', '--diffspec=HEAD~2..HEAD~1']
 
-    with self.temporary_workdir() as workdir:
-      # Nothing exists.
-      self.assertFalse(os.path.exists(self.greet_classfile(workdir, 'Greeting.class')))
-      self.assertFalse(os.path.exists(self.greet_classfile(workdir, 'GreetingTest.class')))
+      with self.temporary_workdir() as workdir:
+        # Nothing exists.
+        self.assertIsNone(self.find_classfile(workdir, 'Class.class'))
+        self.assertIsNone(self.find_classfile(workdir, 'ClassTest.class'))
 
-      run = self.run_pants_with_workdir(cmd, workdir)
-      self.assert_success(run)
+        run = self.run_pants_with_workdir(cmd, workdir)
+        self.assert_success(run)
 
-      # The directly changed target's produced classfile exists.
-      self.assertTrue(os.path.exists(self.greet_classfile(workdir, 'Greeting.class')))
-      self.assertFalse(os.path.exists(self.greet_classfile(workdir, 'GreetingTest.class')))
+        # The directly changed target's produced classfile exists.
+        self.assertIsNotNone(self.find_classfile(workdir, 'Class.class'))
+        self.assertIsNone(self.find_classfile(workdir, 'ClassTest.class'))
 
-    with self.temporary_workdir() as workdir:
-      # Nothing exists.
-      self.assertFalse(os.path.exists(self.greet_classfile(workdir, 'Greeting.class')))
-      self.assertFalse(os.path.exists(self.greet_classfile(workdir, 'GreetingTest.class')))
+      with self.temporary_workdir() as workdir:
+        # Nothing exists.
+        self.assertIsNone(self.find_classfile(workdir, 'Class.class'))
+        self.assertIsNone(self.find_classfile(workdir, 'ClassTest.class'))
 
-      run = self.run_pants_with_workdir(cmd + ['--include-dependees=direct'], workdir)
-      self.assert_success(run)
+        run = self.run_pants_with_workdir(cmd + ['--include-dependees=direct'], workdir)
+        self.assert_success(run)
 
-      # The changed target's and its direct dependees' (eg its tests) classfiles exist.
-      self.assertTrue(os.path.exists(self.greet_classfile(workdir, 'Greeting.class')))
-      self.assertTrue(os.path.exists(self.greet_classfile(workdir, 'GreetingTest.class')))
+        # The changed target's and its direct dependees' (eg its tests) classfiles exist.
+        # NB: This highlights a quirk of test-changed (really ChangeCalculator): it uses a
+        # potentially historical diff to calculate changed files but it always uses BUILD files
+        # from HEAD to determine dependees.  As such, in this case, although neither ClassTest.java
+        # nor its BUILD file existed in HEAD~2...HEAD~1 - they do now and so are seen as dependees
+        # of Class.java.
+        self.assertIsNotNone(self.find_classfile(workdir, 'Class.class'))
+        self.assertIsNotNone(self.find_classfile(workdir, 'ClassTest.class'))
 
-  @expectedFailure
   def test_test_changed(self):
-    with self.temporary_workdir() as workdir:
-      cmd = ['test-changed', '--diffspec={}'.format(self.ref_for_greet_change())]
-      junit_out = os.path.join(workdir, 'test', 'junit',
-        'org.pantsbuild.example.hello.greet.GreetingTest.out.txt')
+    with self.known_commits(), self.temporary_workdir() as workdir:
+      cmd = ['test-changed', '--diffspec=HEAD~2..HEAD~1']
+      junit_out = os.path.join(workdir, 'test', 'junit', 'org.pantsbuild.ClassTest.out.txt')
 
       self.assertFalse(os.path.exists(junit_out))
 
