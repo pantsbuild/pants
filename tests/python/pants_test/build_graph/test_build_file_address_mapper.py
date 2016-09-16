1fb9e19665efbffd55b0af1c713b2389d104438c
Yujie Chen <yujieproject@gmail.com>
2016-09-16 10:59:20 -0400

    Make LegacyAddressMapper v2 engine backed
    When testing v2 engine in OSS pants master, I found that LegacyAddressMapper is missing a scan_build_files method which is used in v1 BuildFileAddressMapper, thus causing some test failures. In addition, LegacyAddressMapper internally relies on LegacyBuildGraph to do things. This dependency is unnecessary.
    
    This change adds the missing method to LegacyAddressMapper, and implements all methods of LAM using v2 engine directly.
    
    A summary of this review:
    1. Implement "scan_build_files" on LegacyAddressMapper. As part of the effort, add a new custom_execution_request method in LocalScheduler class.
    2. Modify all other methods of LegacyAddressMapper to use v2 engine directly, and remove the dependency on LegacyBuildGraph.
    3. Remove "resolve_spec" method in AddressMapper base class, replace with a new method called "check_valid_spec", which will check if input spec is valid or not. This suffices the only use case of resolve_spec in master without returning an unused TargetAddressable.
    4. Add a new "resolve_address" method in BuildGraph, which will replace the usage of "address_mapper.resolve" in master. This is done to unify the behavior between v1 and v2 engine, since in v2 engine, only LegacyBuildGraph has instantiated target objects (ProductGraph only has TargetAdaptor).
    5. Modify test cases for LegacyAddressMapper
    6. Add some missing dependencies in several BUILD files.
    7. Fix a bug in go_buildgen()
    
    Testing Done:
    ci green:
    https://travis-ci.org/pantsbuild/pants/builds/160217903
    
    Bugs closed: 3871, 3875
    
    Reviewed at https://rbcommons.com/s/twitter/r/4239/

--- tests/python/pants_test/build_graph/test_build_file_address_mapper.py
+++ tests/python/pants_test/build_graph/test_build_file_address_mapper.py
@@ -31,17 +31,14 @@ class BuildFileAddressMapperTest(BaseTest):
     self.assertEqual(address.target_name, addressable.addressed_name)
     self.assertEqual(addressable.addressed_type, Target)
 
-  def test_resolve_spec(self):
+  def test_check_valid_spec(self):
     self.add_to_build_file('BUILD', dedent("""
       target(name='foozle')
       target(name='baz')
       """))
 
-    with self.assertRaises(AddressLookupError):
-      self.address_mapper.resolve_spec('//:bad_spec')
-
-    dependencies_addressable = self.address_mapper.resolve_spec('//:foozle')
-    self.assertEqual(dependencies_addressable.addressed_type, Target)
+    self.assertFalse(self.address_mapper.check_valid_spec('//:bad_spec'))
+    self.assertTrue(self.address_mapper.check_valid_spec('//:foozle'))
 
   def test_scan_addresses(self):
     root_build_file = self.add_to_build_file('BUILD', 'target(name="foo")')
@@ -101,14 +98,13 @@ class BuildFileAddressMapperTest(BaseTest):
                                  '\s+:foo2 \(from BUILD.2\)$'):
       self.address_mapper.resolve(address)
 
-  def test_raises_address_invalid_address_error(self):
-    with self.assertRaises(BuildFileAddressMapper.InvalidAddressError):
-      self.address_mapper.resolve_spec("../foo")
-
   def test_raises_empty_build_file_error(self):
+    # Create a BUILD that does not define any addresses
     self.add_to_build_file('BUILD', 'pass')
+
+    address = Address.parse('//:foo')
     with self.assertRaises(BuildFileAddressMapper.EmptyBuildFileError):
-      self.address_mapper.resolve_spec('//:foo')
+      self.address_mapper.resolve(address)
 
   def test_address_lookup_error_hierarchy(self):
     self.assertIsInstance(BuildFileAddressMapper.AddressNotInBuildFile(), AddressLookupError)
