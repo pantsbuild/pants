73aa361a68ecef0ed23b334f79088d4b15745918
Kris Wilson <kwilson@twopensource.com>
2016-09-27 13:36:14 -0700

    Undo deprecation.

--- src/python/pants/backend/graph_info/tasks/listtargets.py
+++ src/python/pants/backend/graph_info/tasks/listtargets.py
@@ -5,7 +5,6 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-from pants.base.deprecated import deprecated_conditional
 from pants.base.exceptions import TaskError
 from pants.task.console_task import ConsoleTask
 
@@ -78,14 +77,10 @@ class ListTargets(ConsoleTask):
         yield result
 
   def _target_roots(self):
-    if not self.context.target_roots:
-      if self._enable_v2_engine:
-        raise TaskError('No targets specified. To list all targets use: ./pants list ::')
-      else:
-        deprecated_conditional(lambda: not self.context.target_roots,
-                               '1.4.0',
-                               '`./pants list` (no explicit targets)',
-                               '\nSpecify at least one explicit target instead. To list '
-                               'all targets use: ./pants list ::\n')
-        return self.context.scan().targets(predicate=lambda target: not target.is_synthetic)
+    if not self.context.target_roots and not self._enable_v2_engine:
+      # For the v1 path, continue the behavior of `./pants list` implies `./pants list ::`.
+      return self.context.scan().targets(predicate=lambda target: not target.is_synthetic)
+
+    # For the v2 path, `./pants list` is a functional no-op. This matches the v2 mode behavior of
+    # `./pants --changed-parent=HEAD list` (w/ no changes) returning an empty result.
     return self.context.target_roots
