db09610d448b9b743b7cf9b1dc35a5d6751b3e96
Stu Hood <stuhood@gmail.com>
2016-09-28 17:48:42 -0700

    Remove commented line

--- src/python/pants/engine/legacy/graph.py
+++ src/python/pants/engine/legacy/graph.py
@@ -55,7 +55,6 @@ class LegacyBuildGraph(BuildGraph):
       the symbol table installed in the scheduler (TODO: see comment in `_instantiate_target`).
     """
     self._scheduler = scheduler
-    self._graph = None # TODO scheduler.product_graph
     self._target_types = self._get_target_types(symbol_table_cls)
     self._engine = engine
     super(LegacyBuildGraph, self).__init__()
