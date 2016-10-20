5e91e78ec5e77dd3c546dd494109828d0439fc32
Stu Hood <stuhood@gmail.com>
2016-10-19 22:10:29 -0700

    Bump version for 1.2.0rc3

--- src/python/pants/version.py
+++ src/python/pants/version.py
@@ -8,6 +8,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 from packaging.version import Version
 
 
-VERSION = '1.2.0rc2'
+VERSION = '1.2.0rc3'
 
 PANTS_SEMVER = Version(VERSION)
