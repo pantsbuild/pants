ba4269f3b1cfc0a414efde2aad8337f1251550b2
Yujie Chen <yujieproject@gmail.com>
2016-10-31 14:49:46 -0400

    support mutually_exclusive_group paramater in option registration
    The idea originates from option renaming process. To rename an option, we have to write a custom logic to prevent both options from being given and route value from "dest" of old option to "dest" of new option. With a "mutually_exclusive_group" argument in register(), old option can easily set it to be the new option. This patch implements the general "mutually exclusive" logic for options.
    
    The way mutually_exclusive_group works is, in register() method, a user can specify "mutually_exclusive_group=XXX", then "XXX" will be the dest for this option. "XXX" can be the name of another option, or a totally new name. When more than 1 option with dest being XXX given through flags, env var or config file, pants will throw an exception.
    
    Testing Done:
    https://travis-ci.org/pantsbuild/pants/builds/171997616
    
    Bugs closed: 3881, 4002
    
    Reviewed at https://rbcommons.com/s/twitter/r/4336/

--- src/python/pants/bin/goal_runner.py
+++ src/python/pants/bin/goal_runner.py
@@ -23,7 +23,6 @@ from pants.goal.goal import Goal
 from pants.goal.run_tracker import RunTracker
 from pants.help.help_printer import HelpPrinter
 from pants.java.nailgun_executor import NailgunProcessGroup
-from pants.option.ranked_value import RankedValue
 from pants.pantsd.subsystem.pants_daemon_launcher import PantsDaemonLauncher
 from pants.reporting.reporting import Reporting
 from pants.scm.subsystems.changed import Changed
@@ -66,27 +65,6 @@ class GoalRunnerFactory(object):
     self._fail_fast = self._global_options.fail_fast
     self._explain = self._global_options.explain
     self._kill_nailguns = self._global_options.kill_nailguns
-
-    self._handle_ignore_patterns()
-
-  # TODO: Remove this once we have better support of option renaming in option.parser
-  def _handle_ignore_patterns(self):
-    ignore_patterns_explicit = not self._global_options.is_default('ignore_patterns')
-    build_ignore_explicit = not self._global_options.is_default('build_ignore')
-    if ignore_patterns_explicit and build_ignore_explicit:
-      class MutualExclusiveOptionError(Exception):
-        """Raised when both of exclusive options are given."""
-
-      raise MutualExclusiveOptionError(
-        "Can't use both --ignore-patterns and --build-ignore, should use --build-ignore only.")
-
-    # If --ignore-patterns is specified, we copy it to --build-ignore,
-    # since the backend uses build_ignore.
-    if ignore_patterns_explicit:
-      self._global_options.build_ignore = RankedValue(
-        self._global_options.get_rank('ignore_patterns'),
-        self._global_options.ignore_patterns
-      )
 
   def _handle_help(self, help_request):
     """Handle requests for `help` information."""
