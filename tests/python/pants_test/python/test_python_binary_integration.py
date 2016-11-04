1335cbffb92b3b06352d0630f1c720288727771e
Kris Wilson <kwilson@twopensource.com>
2016-11-03 18:21:40 -0700

    Refine.

--- tests/python/pants_test/python/test_python_binary_integration.py
+++ tests/python/pants_test/python/test_python_binary_integration.py
@@ -5,15 +5,20 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import functools
 import os
 from contextlib import contextmanager
 
+from pants.util.contextutil import temporary_dir
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 from pex.pex_info import PexInfo
 
 
-TARGET_TMPL = '''
+TEST_PROJECT = 'testprojects/src/python/cache_fields'
+TEST_BUILD = os.path.join(TEST_PROJECT, 'BUILD')
+TEST_PEX = 'dist/cache_fields.pex'
+ZIPSAFE_TARGET_TMPL = '''
 python_binary(
   source='main.py',
   zip_safe={}
@@ -22,12 +27,6 @@ python_binary(
 
 
 class PythonBinaryIntegrationTest(PantsRunIntegrationTest):
-  TEST_PROJECT = 'testprojects/src/python/cache_fields'
-  TEST_BUILD = os.path.join(TEST_PROJECT, 'BUILD')
-  TEST_PEX = 'dist/cache_fields.pex'
-  ZIP_SAFE_TRUE_TARGET = TARGET_TMPL.format('True')
-  ZIP_SAFE_FALSE_TARGET = TARGET_TMPL.format('False')
-
   def assert_pex_attribute(self, pex, attr, value):
     self.assertTrue(os.path.exists(pex))
     pex_info = PexInfo.from_pex(pex)
@@ -35,7 +34,22 @@ class PythonBinaryIntegrationTest(PantsRunIntegrationTest):
 
   @staticmethod
   @contextmanager
+  def caching_config():
+    """Creates a temporary directory and returns a pants configuration for passing to pants_run."""
+    with temporary_dir() as tmp_dir:
+      yield {
+        'cache': {
+          'read': True,
+          'write': True,
+          'read_from': [tmp_dir],
+          'write_to': [tmp_dir]
+        }
+      }
+
+  @staticmethod
+  @contextmanager
   def mutated_file(filename, content):
+    """Temporarily mutates a file to simulate user edits."""
     with open(filename, 'rb') as fh:
       old_content = fh.read()
 
@@ -49,18 +63,23 @@ class PythonBinaryIntegrationTest(PantsRunIntegrationTest):
         fh.write(old_content)
 
   def test_zipsafe_caching(self):
-    # Create a pex from a simple python_binary target and assert it has zip_safe=True (default).
-    self.assert_success(self.run_pants(command=['binary', self.TEST_PROJECT]))
-    self.assert_pex_attribute(self.TEST_PEX, 'zip_safe', True)
+    with self.caching_config() as pants_ini_config:
+      build = functools.partial(
+        self.run_pants,
+        command=['binary', TEST_PROJECT],
+        config=pants_ini_config
+      )
 
-    # Mutate the target to set zip_safe=False and create/check the resulting pex.
-    with self.mutated_file(self.TEST_BUILD, self.ZIP_SAFE_FALSE_TARGET):
-      self.assert_success(self.run_pants(command=['binary', self.TEST_PROJECT]))
-      self.assertTrue(os.path.exists(self.TEST_PEX))
-      self.assert_pex_attribute(self.TEST_PEX, 'zip_safe', False)
+      # Create a pex from a simple python_binary target and assert it has zip_safe=True (default).
+      self.assert_success(build())
+      self.assert_pex_attribute(TEST_PEX, 'zip_safe', True)
 
-    # Mutate the target to set zip_safe=True and create/check the resulting pex.
-    with self.mutated_file(self.TEST_BUILD, self.ZIP_SAFE_TRUE_TARGET):
-      self.assert_success(self.run_pants(command=['binary', self.TEST_PROJECT]))
-      self.assertTrue(os.path.exists(self.TEST_PEX))
-      self.assert_pex_attribute(self.TEST_PEX, 'zip_safe', True)
+      # Simulate a user edit by adding zip_safe=False to the target and check the resulting pex.
+      with self.mutated_file(TEST_BUILD, ZIPSAFE_TARGET_TMPL.format('False')):
+        self.assert_success(build())
+        self.assert_pex_attribute(TEST_PEX, 'zip_safe', False)
+
+      # Simulate a user edit by adding zip_safe=True to the target and check the resulting pex.
+      with self.mutated_file(TEST_BUILD, ZIPSAFE_TARGET_TMPL.format('True')):
+        self.assert_success(build())
+        self.assert_pex_attribute(TEST_PEX, 'zip_safe', True)
