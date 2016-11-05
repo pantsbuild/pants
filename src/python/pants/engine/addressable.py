dd7c1955a654e39011848182a8dd45fbb40d79fc
Nick Howard <nhoward@twitter.com>
2016-11-04 18:16:26 -0600

    [engine] Skip re-creating copy of address if no variants
    The current parse_variants constructs a new address regardless of whether there were variants or not within the address. This adds a check that returns early if there was no at sign in the
    target_name, ie that the split target_name is equal to the original.

--- src/python/pants/engine/addressable.py
+++ src/python/pants/engine/addressable.py
@@ -436,7 +436,9 @@ def _extract_variants(address, variants_str):
 
 
 def parse_variants(address):
-  target_name, _, variants_str = address.target_name.partition('@')
+  target_name, at_sign, variants_str = address.target_name.partition('@')
+  if not at_sign:
+    return address, None
   variants = _extract_variants(address, variants_str) if variants_str else None
   normalized_address = Address(spec_path=address.spec_path, target_name=target_name)
   return normalized_address, variants
