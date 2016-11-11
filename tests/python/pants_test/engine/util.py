8b4178271d4abb585d726ca42677e9d0ac7143f4
John Sirois <john.sirois@gmail.com>
2016-11-11 11:49:08 -0800

    Kill unused import.

--- tests/python/pants_test/engine/util.py
+++ tests/python/pants_test/engine/util.py
@@ -5,8 +5,6 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-import os
-
 from pants.engine.subsystem.native import Native
 from pants_test.subsystem.subsystem_util import init_subsystem
 
