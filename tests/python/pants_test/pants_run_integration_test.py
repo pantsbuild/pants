f3cd193814b85f452867e4362aabb6a2be0eb1b5
wisechengyi <wisechengyi@gmail.com>
2016-11-15 13:22:44 -0800

    copy dir

--- tests/python/pants_test/pants_run_integration_test.py
+++ tests/python/pants_test/pants_run_integration_test.py
@@ -341,11 +341,15 @@ class PantsRunIntegrationTest(unittest.TestCase):
     # N.B. BUILD.tools needs to be copied vs symlinked to avoid a symlink prefix check error.
     files_to_copy = ('BUILD.tools',)
     files_to_link = ('pants', 'pants.ini', 'pants.travis-ci.ini', '.pants.d',
-                     'build-support', '3rdparty', 'pants-plugins', 'src', 'contrib')
+                     'build-support', 'pants-plugins', 'src')
+    dirs_to_copy = ('contrib', '3rdparty')
 
     with self.temporary_workdir() as tmp_dir:
       for filename in files_to_copy:
         shutil.copy(os.path.join(get_buildroot(), filename), os.path.join(tmp_dir, filename))
+
+      for dirname in dirs_to_copy:
+        shutil.copytree(os.path.join(get_buildroot(), dirname), os.path.join(tmp_dir, dirname))
 
       for filename in files_to_link:
         os.symlink(os.path.join(get_buildroot(), filename), os.path.join(tmp_dir, filename))
