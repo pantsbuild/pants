c3e3031ca152176779947c758d18900012120937
Peiyu Wang <wangpeiyu@gmail.com>
2016-11-18 11:07:17 -0800

    test_classmap_integration

---
+++ tests/python/pants_test/backend/jvm/tasks/test_classmap.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+# Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+#from pants.backend.jvm.tasks.classmap import ClassmapTask
+#from pants_test.pants_run_integration_test import PantsRunIntegrationTest
+from pants_test.tasks.task_test_base import ConsoleTaskTestBase
+
+
+#import os
+#from textwrap import dedent
+
+
+
+class ClassmapTaskTest(ConsoleTaskTestBase):
+  pass
