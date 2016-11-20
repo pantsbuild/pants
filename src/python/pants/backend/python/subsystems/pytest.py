cef686683ef339e18fcc4e5ead880bb256ff06e6
Benjy <benjyw@gmail.com>
2016-11-20 13:21:40 -0800

    Address code review comments.

--- src/python/pants/backend/python/subsystems/pytest.py
+++ src/python/pants/backend/python/subsystems/pytest.py
@@ -14,23 +14,27 @@ class PyTest(Subsystem):
   @classmethod
   def register_options(cls, register):
     super(PyTest, cls).register_options(register)
-    register('--pytest-requirements', default='pytest>=2.6,<2.7',
+    register('--requirements', advanced=True, default='pytest>=2.6,<2.7',
              help='Requirements string for the pytest library.')
     # NB, pytest-timeout 1.0.0 introduces a conflicting pytest>=2.8.0 requirement, see:
     #   https://github.com/pantsbuild/pants/issues/2566
-    register('--pytest-timeout-requirements', default='pytest-timeout<1.0.0',
+    register('--timeout-requirements', advanced=True, default='pytest-timeout<1.0.0',
              help='Requirements string for the pytest-timeout library.')
-    register('--pytest-cov-requirements', default='pytest-cov>=1.8,<1.9',
+    register('--cov-requirements', advanced=True, default='pytest-cov>=1.8,<1.9',
              help='Requirements string for the pytest-cov library.')
-    register('--unittest2-requirements', default='unittest2>=0.6.0,<=1.9.0',
+    register('--unittest2-requirements', advanced=True, default='unittest2>=0.6.0,<=1.9.0',
              help='Requirements string for the unittest2 library, which some python versions '
                   'may need.')
 
   def get_requirement_strings(self):
+    """Returns a tuple of requirements-style strings for pytest and related libraries.
+
+    Make sure the requirements are satisfied in any environment used for running tests.
+    """
     opts = self.get_options()
     return (
-      opts.pytest_requirements,
-      opts.pytest_timeout_requirements,
-      opts.pytest_cov_requirements,
+      opts.requirements,
+      opts.timeout_requirements,
+      opts.cov_requirements,
       opts.unittest2_requirements,
     )
