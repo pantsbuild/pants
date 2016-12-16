f77cea922ef87c1f7f3c1bb875325ada3d90878d
Yujie Chen <yujieproject@gmail.com>
2016-12-16 17:49:40 -0500

    tests

--- tests/python/pants_test/invalidation/test_strict_deps_invalidation_integration.py
+++ tests/python/pants_test/invalidation/test_strict_deps_invalidation_integration.py
@@ -8,6 +8,7 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import os
 import shutil
 
+from pants.base.build_environment import get_buildroot
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 
@@ -15,14 +16,26 @@ class RunTrackerIntegrationTest(PantsRunIntegrationTest):
 
   TEST_SRC = 'testprojects/tests/java/org/pantsbuild/testproject/strictdeps'
 
-  def test_strict_deps_false(self):
+  def copy_src_and_run_test(self, target_name, match_str):
     with self.temporary_sourcedir() as tmp_src:
-      src_dir = os.path.join(tmp_src, os.path.basename(self.TEST_SRC))
+      src_dir = os.path.relpath(os.path.join(tmp_src, os.path.basename(self.TEST_SRC)), get_buildroot())
       shutil.copytree(self.TEST_SRC, src_dir)
       with self.temporary_workdir() as workdir:
-        cmd = ['compile', '{}:A2'.format(src_dir)]
+        cmd = ['compile', '{}:{}'.format(src_dir, target_name)]
         pants_run = self.run_pants_with_workdir(command=cmd, workdir=workdir)
         self.assert_success(pants_run)
 
+        with open(os.path.join(src_dir, 'C.java'), 'ab') as fh:
+          fh.write('\n')
+
+        pants_run = self.run_pants_with_workdir(command=cmd, workdir=workdir)
+        self.assert_success(pants_run)
+        self.assertEqual(
+          '{}:{}'.format(src_dir, target_name) in pants_run.stdout_data,
+          match_str)
+
+  def test_strict_deps_false(self):
+    self.copy_src_and_run_test('A2', True)
+
   def test_strict_deps_true(self):
-    pass
+    self.copy_src_and_run_test('A1', False)
