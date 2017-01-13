6f21c6cbd6dada8eb8594026fb45fecf95bca75b
Peiyu Wang <wangpeiyu@gmail.com>
2017-01-12 23:06:03 -0800

    adding debug

--- build-support/bin/release.sh
+++ build-support/bin/release.sh
@@ -2,6 +2,7 @@
 # Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+set -x
 ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && cd "$(git rev-parse --show-toplevel)" && pwd)
 source ${ROOT}/build-support/common.sh
 
