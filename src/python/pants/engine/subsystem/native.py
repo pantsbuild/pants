c8dfeadbf86dfcf666f5341af97cb6ec894c44d0
Peiyu Wang <wangpeiyu@gmail.com>
2017-02-05 13:43:59 -0800

    Derive object id used in the native context from object's content (#4233)
    ### Problem
    
    In the native context, object uses an integer identifier which is derived by checking hash existence. In case of user overriding hash function, this can be buggy because object change may not be reflected and may end up retrieving stale object.
    
    #4199 illustrates the problem in more detail. Note `Struct` as of today is not used as a subject, neither does any of the subject objects overrides their hash function, but this is still a potential risk.
    
    ### Solution
    
    Switch to use the pickled content digest, still keep the integer as the external facing identifier to live in the native context, for the purpose of hiding the implementation detail. As in this review, it's one to one: digest to integer id.
    
    ### Result
    
    Performance impact estimate: negative `~40%`

--- src/python/pants/engine/subsystem/native.py
+++ src/python/pants/engine/subsystem/native.py
@@ -13,6 +13,7 @@ import six
 from cffi import FFI
 
 from pants.binaries.binary_util import BinaryUtil
+from pants.engine.storage import Storage
 from pants.option.custom_types import dir_option
 from pants.subsystem.subsystem import Subsystem
 from pants.util.memo import memoized_property
@@ -321,14 +322,45 @@ class RunnableComplete(datatype('RunnableComplete', ['value', 'is_throw'])):
   """Corresponds to the native object of the same name."""
 
 
-class ExternContext(object):
-  """A wrapper around python objects used in static extern functions in this module.
+class ObjectIdMap(object):
+  """In the native context, assign and memoize python objects an unique unsigned-integer Id.
 
-  In the native context, python objects are identified by an unsigned-integer Id which is
-  assigned and memoized here. Note that this is independent-from and much-lighter-than
-  the Digest computed when an object is stored via storage.py (which is generally only necessary
-  for multi-processing or cache lookups).
+  Underlying, the id is uniquely derived from object's digest instead of using its hash code
+  because the content may change and object's hash function could be overridden. In order not
+  to return the stale object we trade performance for correctness.
+
+  In the future, we could improve performance by only computing digests for mutable objects.
+  For this reason referring an implementation-independent id instead of the digest in the native
+  context is more flexible.
   """
+
+  def __init__(self):
+    # Objects indexed by their keys, i.e, content digests
+    self._objects = Storage.create(in_memory=True)
+    # Memoized object Ids.
+    self._id_to_key = dict()
+    self._key_to_id = dict()
+    self._next_id = 0
+
+  def put(self, obj):
+    key = self._objects.put(obj)
+    new_id = self._next_id
+    oid = self._key_to_id.setdefault(key, new_id)
+    if oid is not new_id:
+      # Object already existed.
+      return oid
+
+    # Object is new/unique.
+    self._id_to_key[oid] = key
+    self._next_id += 1
+    return oid
+
+  def get(self, oid):
+    return self._objects.get(self._id_to_key[oid])
+
+
+class ExternContext(object):
+  """A wrapper around python objects used in static extern functions in this module."""
 
   def __init__(self):
     # A handle to this object to ensure that the native wrapper survives at least as
@@ -340,9 +372,7 @@ class ExternContext(object):
     self._lock = threading.RLock()
 
     # Memoized object Ids.
-    self._id_generator = 0
-    self._id_to_obj = dict()
-    self._obj_to_id = dict()
+    self._object_id_map = ObjectIdMap()
 
     # An anonymous Id for Values that keep *Buffers alive.
     self.anon_id = TypeId(self.to_id(int))
@@ -376,19 +406,10 @@ class ExternContext(object):
   def put(self, obj):
     with self._lock:
       # If we encounter an existing id, return it.
-      new_id = self._id_generator
-      _id = self._obj_to_id.setdefault(obj, new_id)
-      if _id is not new_id:
-        # Object already existed.
-        return _id
-
-      # Object is new/unique.
-      self._id_to_obj[_id] = obj
-      self._id_generator += 1
-      return _id
+      return self._object_id_map.put(obj)
 
   def get(self, id_):
-    return self._id_to_obj[id_]
+    return self._object_id_map.get(id_)
 
   def to_id(self, typ):
     return self.put(typ)
