9b372d5fc96423363d4742f36e32581c820fbd0c
Benjy Weinberger <benjyw@gmail.com>
2017-02-09 18:10:53 -0800

    New python repl task. (#4219)
    Uses the new python pipeline.
    
    This involved a little refactoring: The logic for resolving
    requirements is now in a base class extended by the ResolveRequirements
    task.  The PythonRepl task also extends it, so it can optionally
    resolve iPython, if it needs to.

--- src/python/pants/backend/python/register.py
+++ src/python/pants/backend/python/register.py
@@ -14,6 +14,7 @@ from pants.backend.python.targets.python_library import PythonLibrary
 from pants.backend.python.targets.python_requirement_library import PythonRequirementLibrary
 from pants.backend.python.targets.python_tests import PythonTests
 from pants.backend.python.tasks2.gather_sources import GatherSources
+from pants.backend.python.tasks2.python_repl import PythonRepl as PythonRepl2
 from pants.backend.python.tasks2.python_run import PythonRun as PythonRun2
 from pants.backend.python.tasks2.resolve_requirements import ResolveRequirements
 from pants.backend.python.tasks2.select_interpreter import SelectInterpreter
@@ -61,3 +62,4 @@ def register_goals():
   task(name='requirements', action=ResolveRequirements).install('pyprep')
   task(name='sources', action=GatherSources).install('pyprep')
   task(name='py', action=PythonRun2).install('run2')
+  task(name='py', action=PythonRepl2).install('repl2')
