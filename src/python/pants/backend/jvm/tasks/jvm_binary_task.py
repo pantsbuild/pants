258a661edacdcf3d3851315724012141f1ae5eeb
Benjy Weinberger <benjyw@gmail.com>
2017-02-10 21:31:47 -0800

    Get rid of resources=[] stanzas in our JVMTargets. (#4247)
    Resources targets can, and should, just be regular dependencies.
    We should probably deprecate resources=[], there's no good reason for it to exist.
    
    Note that nothing changes for PythonTargets, where resources= are filesets.

--- src/python/pants/backend/jvm/tasks/jvm_binary_task.py
+++ src/python/pants/backend/jvm/tasks/jvm_binary_task.py
@@ -79,7 +79,7 @@ class JvmBinaryTask(JarBuilderTask):
 
     :param binary: The jvm_binary target to operate on.
     :param path: Write the output jar here, overwriting an existing file, if any.
-    :param string manifest_classpath: If set output jar will set as its manifest's
+    :param iterable manifest_classpath: If set output jar will set as its manifest's
       classpath, otherwise output jar will simply include class files.
     """
     # TODO(benjy): There's actually nothing here that requires 'binary' to be a jvm_binary.
