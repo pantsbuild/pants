93828b4fa30dcaa0b0c805e4b20d1638527fbbaa
Roger Jiang <rjiang@twitter.com>
2017-02-16 13:41:50 -0800

    Fix resource location string

--- contrib/node/examples/src/java/org/pantsbuild/testproject/jsresources/JsResourcesMain.java
+++ contrib/node/examples/src/java/org/pantsbuild/testproject/jsresources/JsResourcesMain.java
@@ -3,14 +3,23 @@ package org.pantsbuild.testproject.jsresources;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import java.io.IOException;
+import java.net.URL;
 
 public class JsResourcesMain {
-  private static String RESOURCE_PATH = "web-component-button/dist/Button.js";
+  private static String RESOURCE_PATH = "web-component-button-processed/Button.js";
+  private static String ALT_RESOURCE_PATH = (
+    "web-component-button-processed-with-dependency-artifacts/Button.js");
 
   public static void main(String[] args) throws IOException {
     // Introduce a 3rdparty library so we can test if Manifest's Class-Path entry is
     // set properly for both internal and external dependencies.
-    String content = Resources.toString(Resources.getResource(RESOURCE_PATH), Charsets.UTF_8);
+    URL resourceUrl;
+    try {
+      resourceUrl = Resources.getResource(RESOURCE_PATH);
+    } catch (IllegalArgumentException e) {
+      resourceUrl = Resources.getResource(ALT_RESOURCE_PATH);
+    }
+    String content = Resources.toString(resourceUrl, Charsets.UTF_8);
     // Ensure resource is loaded properly.
     System.out.println(content);
   }
