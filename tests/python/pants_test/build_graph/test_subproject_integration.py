0e2d2bb5a3963880dd224b5d3718df7304588192
Brandon Milton <brandon.milton21@gmail.com>
2017-02-27 11:55:38 -0800

    Make entire subproject integration test created on-demand

--- tests/python/pants_test/build_graph/test_subproject_integration.py
+++ tests/python/pants_test/build_graph/test_subproject_integration.py
@@ -5,10 +5,63 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, 
                         print_function, unicode_literals, with_statement)
 
+from contextlib import contextmanager
+from pants.util.dirutil import safe_file_dump, safe_rmtree
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 SUBPROJ_SPEC = 'testprojects/src/python/subproject_test/'
 SUBPROJ_ROOT = 'testprojects/src/python/subproject_test/subproject'
+
+TEST_BUILD = 'testprojects/src/python/subproject_test/BUILD'
+SUBPROJ_BUILD = 'testprojects/src/python/subproject_test/subproject/src/python/BUILD'
+HELPERSHELPERS_BUILD = 'testprojects/src/python/subproject_test/subproject/src/python/helpershelpers/BUILD'
+
+SUBPROJECT_TEST_CONTENTS = """
+python_library (
+  dependencies = ['//testprojects/src/python/subproject_test/subproject/src/python:helpers'],
+)
+"""
+
+SUBPROJECT_TEST_SUBPROJECT_CONTENTS = """
+python_library (
+  name = 'helpers',
+  dependencies = ['//src/python/helpershelpers'],
+)
+"""
+
+SUBPROJECT_TEST_SUBPROJECT_HELPERSHELPERS_CONTENTS = """
+python_library (
+  name = 'helpershelpers',
+)
+"""
+
+"""
+Test layout
+-----------
+
+testprojects/
+  src/
+    python/
+      subproject_test/
+        BUILD
+        subproject/
+          src/
+            python/
+              BUILD/
+              helpershelpers/
+                BUILD/
+"""
+
+
+@contextmanager
+def test_layout():
+  try:
+    safe_file_dump(TEST_BUILD, SUBPROJECT_TEST_CONTENTS)
+    safe_file_dump(SUBPROJ_BUILD, SUBPROJECT_TEST_SUBPROJECT_CONTENTS)
+    safe_file_dump(HELPERSHELPERS_BUILD, SUBPROJECT_TEST_SUBPROJECT_HELPERSHELPERS_CONTENTS)
+    yield
+  finally:
+    safe_rmtree(SUBPROJ_SPEC)
 
 
 class SubprojectIntegrationTest(PantsRunIntegrationTest):
@@ -19,9 +72,10 @@ class SubprojectIntegrationTest(PantsRunIntegrationTest):
     on a subproject which relies on its own internal library, a failure
     occurs without the --subproject-roots option
     """
-    pants_args = ['dependencies', SUBPROJ_SPEC]
-    pants_run = self.run_pants(pants_args)
-    self.assert_failure(pants_run)
+    with test_layout():
+      pants_args = ['dependencies', SUBPROJ_SPEC]
+      pants_run = self.run_pants(pants_args)
+      self.assert_failure(pants_run)
 
   def test_subproject_with_flag(self):
     """
@@ -29,7 +83,8 @@ class SubprojectIntegrationTest(PantsRunIntegrationTest):
     a subproject which relies on its own internal library, all things
     go well when that subproject is declared as a subproject
     """
-    pants_args = ['--subproject-roots={}'.format(SUBPROJ_ROOT), 
-                  'dependencies', SUBPROJ_SPEC]
-    pants_run = self.run_pants(pants_args)
-    self.assert_success(pants_run)
+    with test_layout():
+      pants_args = ['--subproject-roots={}'.format(SUBPROJ_ROOT), 
+                    'dependencies', SUBPROJ_SPEC]
+      pants_run = self.run_pants(pants_args)
+      self.assert_success(pants_run)
