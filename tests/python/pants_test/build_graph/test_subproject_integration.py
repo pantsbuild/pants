52cb38548bc1f508ec1ea9b5ceb2c18ee5fa3146
Brandon Milton <brandon.milton21@gmail.com>
2017-02-27 11:57:32 -0800

    Add proper header to subproject integration test

--- tests/python/pants_test/build_graph/test_subproject_integration.py
+++ tests/python/pants_test/build_graph/test_subproject_integration.py
@@ -1,9 +1,9 @@
 # coding=utf-8
-# Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2017 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import (absolute_import, division, generators, nested_scopes, 
-                        print_function, unicode_literals, with_statement)
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
 
 from contextlib import contextmanager
 from pants.util.dirutil import safe_file_dump, safe_rmtree
