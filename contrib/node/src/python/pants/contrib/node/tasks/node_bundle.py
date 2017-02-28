670a678a19f0c398da106e08c4faab135100d5d6
Roger <rogerj.2010@gmail.com>
2017-02-28 08:46:22 -0800

    Include Javascript files in JVM binary (#4264)
    ### Problem
    node_module is intended to be used as target to manipulate node.js modules.  A common practice is to have a JVM based backend server and node based frontend. The FE codes will likely have a transpile step to generate final Javascript codes to be included in the JAR artifact.  Currently, there is no support for use node_module as dependencies for jvm targets.  
    Corresponding to issue #4238 
    
    ### Solution
    
    - Add a node_build task to support running a single build script. 
    - Add three new parameters for node_module target:
      - build_script: build script name as defined in package.json.  All files that are needed
          for the build script must be included in sources.  The script should output build results
          in the directory specified by output_dir.  If build_script is not supplied, the node
          installation results will be considered as output. The output can be archived or included as
          resources for JVM target.
       - output_dir: relative path to assets generated by build script. The path will be
          preserved in the created JAR if the target is used as a JVM target dependency.
       - dev_dependency: boolean value.  Default is False. If a node_module is used as parts
          of devDependencies and thus should not be included in the final bundle or JVM binaries, set
          this value to True.
    - Add build output into runtime_classpath product under %target_name%.
    - Change node_bundle to use node_build output instead of NodePaths output.
      - NodePaths are input product for node_build.
    
    ### Result
    contrib/node/examples/src/java/org/pantsbuild/testproject/jsresources has two examples:
    - jsresources target will generate a JAR file that contains a web-component-button-process/Button.js that is the js file transpiled with webpack.
    - jsresources-with-dependency-artifacts will generate a JAR that contains an additional folder 'web-dependency-test' that is un-transform (include all sources and node_modules folder).

--- contrib/node/src/python/pants/contrib/node/tasks/node_bundle.py
+++ contrib/node/src/python/pants/contrib/node/tasks/node_bundle.py
@@ -11,7 +11,6 @@ from pants.base.build_environment import get_buildroot
 from pants.fs import archive
 from pants.util import dirutil
 
-from pants.contrib.node.tasks.node_paths import NodePaths
 from pants.contrib.node.tasks.node_task import NodeTask
 
 
@@ -25,31 +24,34 @@ class NodeBundle(NodeTask):
   @classmethod
   def prepare(cls, options, round_manager):
     super(NodeBundle, cls).prepare(options, round_manager)
-    round_manager.require_data(NodePaths)
+    round_manager.require_data('bundleable_js')
 
   def __init__(self, *args, **kwargs):
     super(NodeBundle, self).__init__(*args, **kwargs)
     self._outdir = self.get_options().pants_distdir
 
   def execute(self):
-    node_paths = self.context.products.get_data(NodePaths)
+    bundleable_js = self.context.products.get_data('bundleable_js')
     bundle_archive_product = self.context.products.get('deployable_archives')
     dirutil.safe_mkdir(self._outdir)  # Make sure dist dir is present.
 
     for target in self.context.target_roots:
       if self.is_node_bundle(target):
         archiver = archive.archiver(target.payload.archive)
-        # build_dir is a symlink.  Since dereference option for tar is set to False, we need to
-        # dereference manually to archive the linked build dir.
-        build_dir = os.path.realpath(node_paths.node_path(target.node_module))
-        self.context.log.debug('archiving {}'.format(build_dir))
-        archivepath = archiver.create(
-          build_dir,
-          self._outdir,
-          target.package_name,
-          None,
-          dereference=False
-        )
-        bundle_archive_product.add(
-          target, os.path.dirname(archivepath)).append(os.path.basename(archivepath))
-        self.context.log.info('created {}'.format(os.path.relpath(archivepath, get_buildroot())))
+        for _, abs_paths in bundleable_js[target.node_module].abs_paths():
+          for abs_path in abs_paths:
+            # build_dir is a symlink.  Since dereference option for tar is set to False, we need to
+            # dereference manually to archive the linked build dir.
+            build_dir = os.path.realpath(abs_path)
+            self.context.log.debug('archiving {}'.format(build_dir))
+            archivepath = archiver.create(
+              build_dir,
+              self._outdir,
+              target.package_name,
+              prefix=None,
+              dereference=False
+            )
+            bundle_archive_product.add(
+              target, os.path.dirname(archivepath)).append(os.path.basename(archivepath))
+            self.context.log.info(
+              'created {}'.format(os.path.relpath(archivepath, get_buildroot())))
