c9fe84a0f4dd87d82ce71aa316edc5cae38cc173
Benjy Weinberger <benjyw@gmail.com>
2017-03-02 14:01:56 -0800

    Fix lint issues.

--- src/python/pants/backend/python/tasks2/pytest_run.py
+++ src/python/pants/backend/python/tasks2/pytest_run.py
@@ -6,7 +6,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 import itertools
-import logging
 import os
 import re
 import shutil
