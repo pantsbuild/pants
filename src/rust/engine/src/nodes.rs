3f492bed9c05c1f4079e1a49b0dc14cb88d22ff3
Ity Kaul <ity@twitter.com>
2017-03-13 17:49:10 -0700

    pass ref instance instead of reference

--- src/rust/engine/src/nodes.rs
+++ src/rust/engine/src/nodes.rs
@@ -279,7 +279,7 @@ impl VFS<Failure> for Context {
 pub trait Node: Into<NodeKey> {
   type Output: Clone + fmt::Debug + Into<NodeResult> + TryFrom<NodeResult> + Send + 'static;
 
-  fn run(&self, context: Context) -> NodeFuture<Self::Output>;
+  fn run(self, context: Context) -> NodeFuture<Self::Output>;
 }
 
 /**
@@ -478,7 +478,7 @@ impl Select {
 impl Node for Select {
   type Output = Value;
 
-  fn run(&self, context: Context) -> NodeFuture<Value> {
+  fn run(self, context: Context) -> NodeFuture<Value> {
     // TODO add back support for variants https://github.com/pantsbuild/pants/issues/4020
 
     // If there is a variant_key, see whether it has been configured; if not, no match.
@@ -537,7 +537,7 @@ pub struct SelectLiteral {
 impl Node for SelectLiteral {
   type Output = Value;
 
-  fn run(&self, context: Context) -> NodeFuture<Value> {
+  fn run(self, context: Context) -> NodeFuture<Value> {
     context.ok(externs::val_for(&self.selector.subject))
   }
 }
@@ -618,7 +618,7 @@ impl SelectDependencies {
 impl Node for SelectDependencies {
   type Output = Value;
 
-  fn run(&self, context: Context) -> NodeFuture<Value> {
+  fn run(self, context: Context) -> NodeFuture<Value> {
     let node = self.clone();
 
     context
@@ -673,7 +673,7 @@ pub struct SelectProjection {
 impl Node for SelectProjection {
   type Output = Value;
 
-  fn run(&self, context: Context) -> NodeFuture<Value> {
+  fn run(self, context: Context) -> NodeFuture<Value> {
     let node = self.clone();
 
     context
@@ -734,7 +734,7 @@ pub struct LinkDest(PathBuf);
 impl Node for ReadLink {
   type Output = LinkDest;
 
-  fn run(&self, context: Context) -> NodeFuture<LinkDest> {
+  fn run(self, context: Context) -> NodeFuture<LinkDest> {
     let link = self.0.clone();
     context.core.vfs.read_link(&self.0)
       .map(|dest_path| LinkDest(dest_path))
@@ -765,7 +765,7 @@ pub struct Stat(PathBuf);
 impl Node for Stat {
   type Output = ();
 
-  fn run(&self, _: Context) -> NodeFuture<()> {
+  fn run(self, _: Context) -> NodeFuture<()> {
     future::ok(()).boxed()
   }
 }
@@ -789,7 +789,7 @@ pub struct DirectoryListing(Vec<fs::Stat>);
 impl Node for Scandir {
   type Output = DirectoryListing;
 
-  fn run(&self, context: Context) -> NodeFuture<DirectoryListing> {
+  fn run(self, context: Context) -> NodeFuture<DirectoryListing> {
     let dir = self.0.clone();
     context.core.vfs.scandir(&self.0)
       .then(move |listing_res| match listing_res {
@@ -859,7 +859,7 @@ impl Snapshot {
 impl Node for Snapshot {
   type Output = fs::Snapshot;
 
-  fn run(&self, context: Context) -> NodeFuture<fs::Snapshot> {
+  fn run(self, context: Context) -> NodeFuture<fs::Snapshot> {
     // Compute and parse PathGlobs for the subject.
     context
       .get(
@@ -934,7 +934,7 @@ impl Task {
 impl Node for Task {
   type Output = Value;
 
-  fn run(&self, context: Context) -> NodeFuture<Value> {
+  fn run(self, context: Context) -> NodeFuture<Value> {
     let deps =
       future::join_all(
         self.task.clause.iter()
@@ -1045,17 +1045,17 @@ impl NodeKey {
 impl Node for NodeKey {
   type Output = NodeResult;
 
-  fn run(&self, context: Context) -> NodeFuture<NodeResult> {
+  fn run(self, context: Context) -> NodeFuture<NodeResult> {
     match self {
-      &NodeKey::ReadLink(ref n) => n.run(context).map(|v| v.into()).boxed(),
-      &NodeKey::Stat(ref n) => n.run(context).map(|v| v.into()).boxed(),
-      &NodeKey::Scandir(ref n) => n.run(context).map(|v| v.into()).boxed(),
-      &NodeKey::Select(ref n) => n.run(context).map(|v| v.into()).boxed(),
-      &NodeKey::SelectDependencies(ref n) => n.run(context).map(|v| v.into()).boxed(),
-      &NodeKey::SelectLiteral(ref n) => n.run(context).map(|v| v.into()).boxed(),
-      &NodeKey::SelectProjection(ref n) => n.run(context).map(|v| v.into()).boxed(),
-      &NodeKey::Snapshot(ref n) => n.run(context).map(|v| v.into()).boxed(),
-      &NodeKey::Task(ref n) => n.run(context).map(|v| v.into()).boxed(),
+      NodeKey::ReadLink(n) => n.run(context).map(|v| v.into()).boxed(),
+      NodeKey::Stat(n) => n.run(context).map(|v| v.into()).boxed(),
+      NodeKey::Scandir(n) => n.run(context).map(|v| v.into()).boxed(),
+      NodeKey::Select(n) => n.run(context).map(|v| v.into()).boxed(),
+      NodeKey::SelectDependencies(n) => n.run(context).map(|v| v.into()).boxed(),
+      NodeKey::SelectLiteral(n) => n.run(context).map(|v| v.into()).boxed(),
+      NodeKey::SelectProjection(n) => n.run(context).map(|v| v.into()).boxed(),
+      NodeKey::Snapshot(n) => n.run(context).map(|v| v.into()).boxed(),
+      NodeKey::Task(n) => n.run(context).map(|v| v.into()).boxed(),
     }
   }
 }
