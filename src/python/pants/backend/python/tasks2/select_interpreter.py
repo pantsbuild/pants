f65bc3d8e3445afc996db3fea8c6d2577aefec3b
Benjy Weinberger <benjyw@gmail.com>
2017-03-17 13:30:04 +0200

    Refactor the new SelectInterpreter and GatherSources tasks. (#4337)
    Basically just breaks up the big execute() methods
    into several helpers.
    
    This is for two reasons:
    
    - Makes it easier to test invalidation (see below).
    - Will enable selecting interpreters and gathering sources
      separately for each target and its deps (for use by the
      PythonEval task) side-by-side with the current functionality
      of selecting a single interpreter and creating a single
      source PEX for the entire closure. This will be implemented
      in a future change.
    
    Note that testing invalidation isn't an idle idea - I noticed
    a bug in the custom fingerprint strategy that was mixing in
    the target payload for no good reason.  This change fixes
    that bug and adds a test for it.

--- src/python/pants/backend/python/tasks2/select_interpreter.py
+++ src/python/pants/backend/python/tasks2/select_interpreter.py
@@ -18,6 +18,7 @@ from pants.invalidation.cache_manager import VersionedTargetSet
 from pants.python.python_repos import PythonRepos
 from pants.task.task import Task
 from pants.util.dirutil import safe_mkdir_for
+from pants.util.memo import memoized_method
 
 
 class PythonInterpreterFingerprintStrategy(DefaultFingerprintHashingMixin, FingerprintStrategy):
@@ -31,7 +32,6 @@ class PythonInterpreterFingerprintStrategy(DefaultFingerprintHashingMixin, Finge
     if not hash_elements_for_target:
       return None
     hasher = hashlib.sha1()
-    hasher.update(python_target.payload.fingerprint())
     for element in hash_elements_for_target:
       hasher.update(element)
     return hasher.hexdigest()
@@ -60,36 +60,48 @@ class SelectInterpreter(Task):
             invalidation_check.all_vts).cache_key.hash
       else:
         target_set_id = 'no_targets'
-      interpreter_path_file = os.path.join(self.workdir, target_set_id, 'interpreter.path')
+      interpreter_path_file = self._interpreter_path_file(target_set_id)
       if not os.path.exists(interpreter_path_file):
-        interpreter_cache = PythonInterpreterCache(PythonSetup.global_instance(),
-                                                   PythonRepos.global_instance(),
-                                                   logger=self.context.log.debug)
-
-        # Cache setup's requirement fetching can hang if run concurrently by another pants proc.
-        self.context.acquire_lock()
-        try:
-          interpreter_cache.setup()
-        finally:
-          self.context.release_lock()
-
-        interpreter = interpreter_cache.select_interpreter_for_targets(python_tgts)
-        safe_mkdir_for(interpreter_path_file)
-        with open(interpreter_path_file, 'w') as outfile:
-          outfile.write(b'{}\t{}\n'.format(interpreter.binary, str(interpreter.identity)))
-          for dist, location in interpreter.extras.items():
-            dist_name, dist_version = dist
-            outfile.write(b'{}\t{}\t{}\n'.format(dist_name, dist_version, location))
+        self._create_interpreter_path_file(interpreter_path_file, python_tgts)
 
     if not interpreter:
-      with open(interpreter_path_file, 'r') as infile:
-        lines = infile.readlines()
-        binary, identity = lines[0].strip().split('\t')
-        extras = {}
-        for line in lines[1:]:
-          dist_name, dist_version, location = line.strip().split('\t')
-          extras[(dist_name, dist_version)] = location
-
-      interpreter = PythonInterpreter(binary, PythonIdentity.from_path(identity), extras)
+      interpreter = self._get_interpreter(interpreter_path_file)
 
     self.context.products.get_data(PythonInterpreter, lambda: interpreter)
+
+  @memoized_method
+  def _interpreter_cache(self):
+    interpreter_cache = PythonInterpreterCache(PythonSetup.global_instance(),
+                                               PythonRepos.global_instance(),
+                                               logger=self.context.log.debug)
+    # Cache setup's requirement fetching can hang if run concurrently by another pants proc.
+    self.context.acquire_lock()
+    try:
+      interpreter_cache.setup()
+    finally:
+      self.context.release_lock()
+    return interpreter_cache
+
+  def _create_interpreter_path_file(self, interpreter_path_file, targets):
+    interpreter_cache = self._interpreter_cache()
+    interpreter = interpreter_cache.select_interpreter_for_targets(targets)
+    safe_mkdir_for(interpreter_path_file)
+    with open(interpreter_path_file, 'w') as outfile:
+      outfile.write(b'{}\t{}\n'.format(interpreter.binary, str(interpreter.identity)))
+      for dist, location in interpreter.extras.items():
+        dist_name, dist_version = dist
+        outfile.write(b'{}\t{}\t{}\n'.format(dist_name, dist_version, location))
+
+  def _interpreter_path_file(self, target_set_id):
+    return os.path.join(self.workdir, target_set_id, 'interpreter.path')
+
+  @staticmethod
+  def _get_interpreter(interpreter_path_file):
+    with open(interpreter_path_file, 'r') as infile:
+      lines = infile.readlines()
+      binary, identity = lines[0].strip().split('\t')
+      extras = {}
+      for line in lines[1:]:
+        dist_name, dist_version, location = line.strip().split('\t')
+        extras[(dist_name, dist_version)] = location
+    return PythonInterpreter(binary, PythonIdentity.from_path(identity), extras)
