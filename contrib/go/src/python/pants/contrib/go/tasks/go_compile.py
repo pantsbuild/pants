e79d2bd59eb761aaa50ae9d85d3f9aecdea899c8
Benjy Weinberger <benjyw@gmail.com>
2017-03-23 09:59:49 -0700

    Speed up typical go builds. (#4362)
    Resolving remote Go libraries involves injecting targets representing
    the remote sources into the build graph, so they can be compiled locally.
    This must happen for every target, valid or not, in every build.
    
    Previously, however, this involved two time-consuming operations
    per item in the transitive closure of all remote deps:
    
    - A network call to fetch the go meta tag, to figure out the
      remote library root from the import path (at least for remote libraries
      fetched by cloning).
    
    - Shelling out to `go list` to get a library's imports.
    
    This change caches both results in workdir files, for reuse in
    subsequent pants runs.  This restores some of the snappiness of
    Go when run in Pants.
    
    Also tweaks a few other go concerns, such as more expressive logging.

--- contrib/go/src/python/pants/contrib/go/tasks/go_compile.py
+++ contrib/go/src/python/pants/contrib/go/tasks/go_compile.py
@@ -56,9 +56,11 @@ class GoCompile(GoWorkspaceTask):
 
   def _go_install(self, target, gopath):
     args = self.get_options().build_flags.split() + [target.import_path]
-    result, go_cmd = self.go_dist.execute_go_cmd('install', gopath=gopath, args=args,
-                                                 workunit_factory=self.context.new_workunit,
-                                                 workunit_labels=[WorkUnitLabel.COMPILER])
+    result, go_cmd = self.go_dist.execute_go_cmd(
+      'install', gopath=gopath, args=args,
+      workunit_factory=self.context.new_workunit,
+      workunit_name='install {}'.format(target.address.spec),
+      workunit_labels=[WorkUnitLabel.COMPILER])
     if result != 0:
       raise TaskError('{} failed with exit code {}'.format(go_cmd, result))
 
