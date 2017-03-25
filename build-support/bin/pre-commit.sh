d80e7fb9c5bc041c989dfb73831d45a30240cc27
Kris Wilson <kwilson@twopensource.com>
2017-03-24 22:23:37 -0700

    TravisCI tuning.

--- build-support/bin/pre-commit.sh
+++ build-support/bin/pre-commit.sh
@@ -10,10 +10,8 @@ then
 fi
 
 # N.B. This check needs to happen first, before any inadvertent bootstrapping can take place.
-echo "Checking native_engine_version" && ./build-support/bin/check_native_engine_version.sh || exit 1
-echo "Checking packages" && ./build-support/bin/check_packages.sh || exit 1
-echo "Checking imports" && ./build-support/bin/isort.sh || \
-  die "To fix import sort order, run \`build-support/bin/isort.sh -f\`"
-echo "Checking headers" && ./build-support/bin/check_header.sh || exit 1
-echo "Success"
-
+echo -n "* Checking native_engine_version: " && ./build-support/bin/check_native_engine_version.sh || exit 1
+echo "* Checking packages" && ./build-support/bin/check_packages.sh || exit 1
+echo "* Checking headers" && ./build-support/bin/check_header.sh || exit 1
+echo "* Checking imports" && ./build-support/bin/isort.sh || \
+  die "To fix import sort order, run \`./build-support/bin/isort.sh -f\`"
