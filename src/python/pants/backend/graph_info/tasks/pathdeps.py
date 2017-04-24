b0be0313320a25368c850e4eb96c24f30f190f1a
Stu Hood <stuhood@gmail.com>
2017-04-24 15:18:17 -0700

    Deprecate and fix pathdeps.

--- src/python/pants/backend/graph_info/tasks/pathdeps.py
+++ src/python/pants/backend/graph_info/tasks/pathdeps.py
@@ -7,6 +7,8 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 
 import os
 
+from pants.base.build_environment import get_buildroot
+from pants.base.deprecated import deprecated_conditional
 from pants.task.console_task import ConsoleTask
 
 
@@ -14,6 +16,10 @@ class PathDeps(ConsoleTask):
   """List all paths containing BUILD files the target depends on."""
 
   def console_output(self, targets):
+    deprecated_conditional(lambda: True, '1.5.0.dev0',
+                           'The `pathdeps` goal is deprecated. Please use `filedeps` instead.')
     def is_safe(t):
       return hasattr(t, 'address') and hasattr(t.address, 'rel_path')
-    return set(os.path.dirname(t.address.rel_path) for t in targets if is_safe(t))
+    buildroot = get_buildroot()
+    return set(os.path.normpath(os.path.join(buildroot, os.path.dirname(t.address.rel_path)))
+               for t in targets if is_safe(t))
