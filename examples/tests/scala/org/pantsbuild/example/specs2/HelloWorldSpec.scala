2afcf02ceb81ed4844f5cc440de1e2dfb5382ea3
wisechengyi <wisechengyi@gmail.com>
2017-04-25 14:25:40 -0700

    more files

---
+++ examples/tests/scala/org/pantsbuild/example/specs2/HelloWorldSpec.scala
@@ -0,0 +1,22 @@
+package org.pantsbuild.example.specs2
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.specs2.mutable._
+
+//@RunWith(classOf[JUnitRunner])
+class HelloWorldSpec extends Specification {
+  "This is a specification for the 'Hello world' string".txt
+
+  "The 'Hello world' string should" >> {
+    "contain 11 characters" >> {
+      "Hello world" must haveSize(11)
+    }
+    "start with 'Hello'" >> {
+      "Hello world" must startWith("Hello")
+    }
+    "end with 'world'" >> {
+      "Hello world" must endWith("world")
+    }
+  }
+}
