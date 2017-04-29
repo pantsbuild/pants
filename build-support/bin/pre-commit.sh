90fdfb95cc98cfb1b0df6f0fd836db8643ca6582
Stu Hood <stuhood@gmail.com>
2017-04-29 12:24:08 -0700

    Fix tag builds after the more-complete `isort` edit.

--- build-support/bin/pre-commit.sh
+++ build-support/bin/pre-commit.sh
@@ -13,5 +13,13 @@ fi
 echo -n "* Checking native_engine_version: " && ./build-support/bin/check_native_engine_version.sh || exit 1
 echo "* Checking packages" && ./build-support/bin/check_packages.sh || exit 1
 echo "* Checking headers" && ./build-support/bin/check_header.sh || exit 1
-echo "* Checking imports" && ./build-support/bin/isort.sh || \
-  die "To fix import sort order, run \`./build-support/bin/isort.sh -f\`"
+
+$(git rev-parse --verify master > /dev/null 2>&1)
+if [[ $? -eq 0 ]]; then
+  echo "* Checking imports" && ./build-support/bin/isort.sh || \
+    die "To fix import sort order, run \`./build-support/bin/isort.sh -f\`"
+else
+  # When travis builds a tag, it does so in a shallow clone without master fetched, which
+  # fails in pants changed.
+  echo "* Skipping import check in partial working copy."
+fi
