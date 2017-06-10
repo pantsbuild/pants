f8044069871443a53d002df54ace4902d1d32f18
Stu Hood <stuhood@gmail.com>
2017-06-09 18:28:17 -0700

    Working as expected. Huzzah.

--- contrib/scalafix/src/python/pants/contrib/scalafix/tasks/scalafix.py
+++ contrib/scalafix/src/python/pants/contrib/scalafix/tasks/scalafix.py
@@ -25,6 +25,8 @@ class ScalaFix(NailgunTask):
   _SCALA_SOURCE_EXTENSION = '.scala'
   _SCALAHOST_NAME = 'scalahost-nsc'
 
+  cache_target_dirs = True
+
   @classmethod
   def register_options(cls, register):
     super(ScalaFix, cls).register_options(register)
@@ -35,7 +37,7 @@ class ScalaFix(NailgunTask):
     cls.register_jvm_tool(register,
                           'scalafix',
                           classpath=[
-                            JarDependency(org='ch.epfl.scala', name='scalafix-cli_2.11.11', rev='0.4.0'),
+                            JarDependency(org='ch.epfl.scala', name='scalafix-cli_2.11.11', rev='0.4.2'),
                           ])
 
   @classmethod
@@ -43,53 +45,40 @@ class ScalaFix(NailgunTask):
     return super(ScalaFix, cls).subsystem_dependencies() + (ScalaPlatform,)
 
   @classmethod
-  def implementation_version(cls):
-    return super(ScalaFix, cls).implementation_version() + [('ScalaFix', 1)]
-
-  @classmethod
   def prepare(cls, options, round_manager):
     super(ScalaFix, cls).prepare(options, round_manager)
     # Require that compilation has completed.
     round_manager.require_data('runtime_classpath')
-
-  @property
-  def cache_target_dirs(self):
-    return True
 
   def _is_fixed(self, target):
     return target.has_sources(self._SCALA_SOURCE_EXTENSION) and (not target.is_synthetic)
 
   def execute(self):
     classpaths = self.context.products.get_data('runtime_classpath')
-    with self.invalidated(
-        self.context.targets(self._is_fixed),
-        invalidate_dependents=True) as invalidation_check:
+    # NB: This task uses only the literal classpath of each target, so does not need
+    # `invalidate_dependents=True`.
+    with self.invalidated(self.context.targets(self._is_fixed)) as invalidation_check:
       for vt in invalidation_check.all_vts:
         if not vt.valid:
           self.context.log.debug('Fixing {}...'.format(vt.target.address.spec))
-          self._run(vt.target, vt.results_dir, classpaths)
+          classpath = [e for _, e in classpaths.get_for_target(vt.target)]
+          self._run(vt.target, vt.results_dir, classpath)
         else:
           self.context.log.debug('Already fixed {}'.format(vt.target.address.spec))
         # Target outputs are valid: link them to dist.
-        self._link_to_dist(vt)
+        self._finalize_to_dest(vt.target, vt.results_dir)
 
-  def _link_to_dist(self, vt):
-    dest_dir = os.path.join(self.get_options().pants_distdir, 'scalafix', vt.target.id)
-    safe_mkdir_for(dest_dir, clean=True)
-    relative_symlink(vt.current_results_dir, dest_dir)
+  def _finalize_to_dest(self, target, results_dir):
+    for rel_source in target.sources_relative_to_buildroot():
+      src = os.path.join(results_dir, rel_source)
+      dst = os.path.join(get_buildroot(), rel_source)
+      shutil.copy(src, dst)
 
-  def _run(self, target, results_dir, classpaths):
-    # We operate on copies of the files, so we execute in place.
+  def _run(self, target, results_dir, classpath):
+    # We operate on copies of the files, so we always execute in place.
     args = ['--in-place']
-    # FIXME: This argument is not used currently, meaning that files are implicitly
-    # fixed in the buildroot itself. Once this issue is fixed, we should copy back
-    # out of the results_dir to the final location on success.
-    #  see https://github.com/scalacenter/scalafix/issues/176
     args.append('--sourceroot={}'.format(results_dir))
-    # NB: Only the classpath of the target itself is necessary:
-    #  see https://github.com/scalacenter/scalafix/issues/177
-    args.append('--classpath={}'.format(
-      ':'.join(jar for _, jar in classpaths.get_for_targets([target]))))
+    args.append('--classpath={}'.format(':'.join(classpath)))
     if self.get_options().config:
       args.append('--config={}'.format(self.get_options().config))
     if self.get_options().rewrites:
@@ -98,11 +87,11 @@ class ScalaFix(NailgunTask):
       args.append('--verbose')
 
     # Clone all sources to relative names in their destination directory.
-    for source in target.sources_relative_to_buildroot():
-      abs_path = os.path.join(get_buildroot(), source)
-      dest_file = os.path.join(results_dir, source)
-      safe_mkdir_for(dest_file)
-      shutil.copy(abs_path, dest_file)
+    for rel_source in target.sources_relative_to_buildroot():
+      src = os.path.join(get_buildroot(), rel_source)
+      dst = os.path.join(results_dir, rel_source)
+      safe_mkdir_for(dst)
+      shutil.copy(src, dst)
 
     # Execute.
     result = self.runjava(classpath=self.tool_classpath('scalafix'),
