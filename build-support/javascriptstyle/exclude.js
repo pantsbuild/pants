6159fca742a412c44d96feed798ab93fb5364fc4
Ny Saechao <nsaechao@twitter.com>
2017-08-02 17:09:31 -0700

    Add missing exclude.js file for blacklisting js file when running lint/fmt

---
+++ build-support/javascriptstyle/exclude.js
@@ -0,0 +1,12 @@
+module.exports = {
+  files: [
+    "contrib/node/examples/3rdparty",
+    "contrib/node/examples/src/node/preinstalled-project",
+    "contrib/node/examples/src/node/server-project",
+    "contrib/node/examples/src/node/web-build-tool",
+    "contrib/node/examples/src/node/web-component-button",
+    "contrib/node/examples/src/node/web-dependency-test",
+    "contrib/node/examples/src/node/web-project",
+    "contrib/node/tests"
+  ]
+};
