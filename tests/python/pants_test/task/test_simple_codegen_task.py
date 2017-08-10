24c25bcb75753edd03578fb1c1f059d7d7a52a40
Dorothy Ordogh <dordogh@twitter.com>
2017-08-10 14:22:00 -0700

    add test that compares generated target hashes of the same target with different sources

--- tests/python/pants_test/task/test_simple_codegen_task.py
+++ tests/python/pants_test/task/test_simple_codegen_task.py
@@ -268,3 +268,37 @@ class SimpleCodegenTaskTest(TaskTestBase):
     task = self._create_dummy_task(target_roots=targets, strategy='isolated')
     task.execute()
     self.assertEqual('copythis', task.codegen_targets()[0].copied)
+
+  def test_invalidation_of_generated_sources(self):
+    self.create_file('src/thrift/com/foo/one.thrift', 'initial state')
+
+    t1 = self.make_target(spec='src/thrift/com/foo:one',
+                          target_type=DummyLibrary,
+                          sources=['one.thrift'])
+
+    task1 = self._create_dummy_task(target_roots=t1, strategy='isolated')
+    task1.execute()
+
+    gen_targets = [self.build_graph.get_target(syn_addr)
+                   for syn_addr in self.build_graph.synthetic_addresses]
+    syn_targets_for_t1 = [target for target in gen_targets if target.derived_from == t1]
+
+    t1_hash = syn_targets_for_t1[0].invalidation_hash()
+
+    self.reset_build_graph()
+
+    self.create_file('src/thrift/com/foo/one.thrift', 'changed state')
+
+    t2 = self.make_target(spec='src/thrift/com/foo:one',
+                          target_type=DummyLibrary,
+                          sources=['one.thrift'])
+
+    task2 = self._create_dummy_task(target_roots=t2, strategy='isolated')
+    task2.execute()
+
+    gen_targets = [self.build_graph.get_target(syn_addr)
+                   for syn_addr in self.build_graph.synthetic_addresses]
+    syn_targets_for_t2 = [target for target in gen_targets if target.derived_from == t2]
+
+    t2_hash = syn_targets_for_t2[0].invalidation_hash()
+    self.assertNotEqual(t1_hash, t2_hash)
