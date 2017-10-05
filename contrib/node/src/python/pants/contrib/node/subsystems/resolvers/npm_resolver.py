33399dc132674eb9fc0f0d0247057293e0b1fc85
Roger Jiang <rjiang@twitter.com>
2017-10-05 10:52:33 -0700

    WIP

--- contrib/node/src/python/pants/contrib/node/subsystems/resolvers/npm_resolver.py
+++ contrib/node/src/python/pants/contrib/node/subsystems/resolvers/npm_resolver.py
@@ -43,7 +43,7 @@ class NpmResolver(Subsystem, NodeResolverBase):
             'including node_remote_module and other node dependencies. However, this is '
             'not fully supported.')
           self._emit_package_descriptor(node_task, target, results_dir, node_paths)
-        node_task.log.info('\nNpmResolver: target (%s)', target)
+        node_task.context.log.info('\nNpmResolver: target (%s)', target)
         result, npm_install = node_task.execute_npm(['install'],
                                                     workunit_name=target.address.reference(),
                                                     workunit_labels=[WorkUnitLabel.COMPILER])
@@ -94,12 +94,12 @@ class NpmResolver(Subsystem, NodeResolverBase):
     dependenciesToRemove = [
       'dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies'
     ]
-    node_task.context.log.debug(
+    node_task.context.log.info(
       'Removing {} from package.json for {}'.format(dependenciesToRemove, package['name']))
     for dependencyType in dependenciesToRemove:
       package.pop(dependencyType, None)
 
-    node_task.context.log.debug(
+    node_task.context.log.info(
       'Adding {} to package.json for {}'.format(dependencies, package['name']))
     package['dependencies'] = dependencies
 
