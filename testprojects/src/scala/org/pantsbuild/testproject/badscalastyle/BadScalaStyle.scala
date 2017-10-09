fe5f6d8b41ff6b7711ac946e8f9ddfbe7d0facb3
Stu Hood <stuhood@gmail.com>
2017-10-09 16:34:57 -0700

    WIP: Bump to latest version of the compiler plugin... seems like it might still be exploding on a bad match...?

--- testprojects/src/scala/org/pantsbuild/testproject/badscalastyle/BadScalaStyle.scala
+++ testprojects/src/scala/org/pantsbuild/testproject/badscalastyle/BadScalaStyle.scala
@@ -4,26 +4,15 @@
 package org.pantsbuild.testproject.badscalastyle
 
 /**
-  * These comments are formmated incorrectly
-  * and the parameter list is too long for one line
-  */
-case class ScalaStyle(one: String,
-                      two: String,
-                      three: String,
-                      four: String,
-                      five: String,
-                      six: String,
-                      seven: String,
-                      eight: String,
-                      nine: String)
+ * These comments are formmated incorrectly
+ * and the parameter list is too long for one line
+ */
+case class ScalaStyle(one: String,two: String,three: String,four: String,
+    five: String,six: String,seven: String,eight: String,  nine: String)
 
-class Person(name: String,
-             age: Int,
-             astrologicalSign: String,
-             shoeSize: Int,
-             favoriteColor: java.awt.Color) {
-  def getAge: Int = { return age }
-  def sum(longvariablename: List[String]): Int = {
-    longvariablename.map(_.toInt).foldLeft(0)(_ + _)
-  }
+class Person(name: String,age: Int,astrologicalSign: String,
+    shoeSize: Int,
+    favoriteColor: java.awt.Color) {
+  def getAge:Int={return age}
+  def sum(longvariablename: List[String]): Int = {longvariablename.map(_.toInt).foldLeft(0)(_ + _)}
 }
