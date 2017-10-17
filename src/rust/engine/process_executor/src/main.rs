396e319aacafb658654c8c8d00f905628dcbc845
Daniel Wagner-Hall <dwagnerhall@twitter.com>
2017-10-17 12:48:01 +0100

    Use clap for arg parsing in process_executor

--- src/rust/engine/process_executor/src/main.rs
+++ src/rust/engine/process_executor/src/main.rs
@@ -1,8 +1,11 @@
+extern crate clap;
 extern crate process_execution;
 
-use std::env;
+use clap::{App, AppSettings, Arg};
 use std::process::exit;
 use std::collections::BTreeMap;
+
+use std::iter::Iterator;
 
 /// A binary which takes args of format:
 ///  process_executor --env=FOO=bar --env=SOME=value -- /path/to/binary --flag --otherflag
@@ -11,43 +14,52 @@ use std::collections::BTreeMap;
 ///
 /// It does not perform $PATH lookup or shell expansion.
 fn main() {
-  let (argv, env, server) = {
-    let mut flags: Vec<String> = env::args().skip(1).collect();
-    let splitter = flags.iter().position(|x| x == "--");
-    let command = {
-      if let Some(index) = splitter {
-        flags.split_off(index + 1)
-      } else {
-        flags.drain(..).collect()
-      }
-    };
-    if flags.is_empty() && command.first().unwrap_or(&"".to_string()).starts_with("--") {
-      panic!("Must specify -- between flags and command to run");
-    }
-    flags.pop(); // --
+  let args = App::new("process_executor")
+    .arg(
+      Arg::with_name("server")
+        .long("server")
+        .takes_value(true)
+        .help(
+          "The host:port of the gRPC server to connect to. Forces remote execution. \
+If unspecified, local execution will be performed.",
+        ),
+    )
+    .arg(
+      Arg::with_name("env")
+        .long("env")
+        .takes_value(true)
+        .multiple(true)
+        .help(
+          "Environment variables with which the process should be run.",
+        ),
+    )
+    .setting(AppSettings::TrailingVarArg)
+    .arg(Arg::with_name("argv").multiple(true).last(true).required(
+      true,
+    ))
+    .get_matches();
 
-    let mut env = BTreeMap::new();
-    let mut server: Option<String> = None;
-
-    for flag in flags {
-      if flag.starts_with("--env=") {
-        let mut parts = flag["--env=".len()..].splitn(2, "=");
-        env.insert(
-          parts.next().unwrap().to_string(),
-          parts.next().unwrap_or_default().to_string(),
-        );
-      } else if flag.starts_with("--server=") {
-        server = Some(flag["--server=".len()..].to_string());
-      } else {
-        panic!("Didn't know how to interpret flag {}", flag);
-      }
-    }
-    (command, env, server)
-  };
+  let argv: Vec<String> = args
+    .values_of("argv")
+    .unwrap()
+    .map(|v| v.to_string())
+    .collect();
+  let env: BTreeMap<String, String> = args
+    .values_of("env")
+    .unwrap()
+    .map(|v| {
+      let mut parts = v.splitn(2, "=");
+      (
+        parts.next().unwrap().to_string(),
+        parts.next().unwrap_or_default().to_string(),
+      )
+    })
+    .collect();
+  let server = args.value_of("server");
 
   let request = process_execution::ExecuteProcessRequest { argv, env };
   let result = match server {
-    Some(addr) => process_execution::remote::run_command_remote(&addr, request).unwrap(),
+    Some(addr) => process_execution::remote::run_command_remote(addr, request).unwrap(),
     None => process_execution::local::run_command_locally(request).unwrap(),
   };
   print!("{}", String::from_utf8(result.stdout).unwrap());
