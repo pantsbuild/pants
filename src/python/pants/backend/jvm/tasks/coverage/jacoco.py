6cb16efe20d234bf17b0f079c17ff3631adff9f7
jtrobec <jtrobec@twitter.com>
2017-10-18 10:40:26 -0700

    Moving some re-computed values to private properties. Adding comments for clarity.

--- src/python/pants/backend/jvm/tasks/coverage/jacoco.py
+++ src/python/pants/backend/jvm/tasks/coverage/jacoco.py
@@ -34,12 +34,12 @@ class Jacoco(CoverageEngine):
                           JarDependency(
                             org='org.jacoco',
                             name='org.jacoco.agent',
-                            rev='0.7.9',
+                            rev='0.7.10-SNAPSHOT',
                             classifier='runtime',
                             intransitive=True)
                         ])
 
-      # We need to inject the jacoco agent at test runtime
+      # We'll use the jacoco-cli to generate reports
       cls.register_jvm_tool(register,
                         'jacoco-cli',
                         classpath=[
@@ -80,11 +80,13 @@ class Jacoco(CoverageEngine):
     options = settings.options
     self._context = settings.context
     self._targets = targets
+    self._coverage_targets = {t for t in targets if Jacoco.is_coverage_target(t)}
     self._agent_path = agent_path
     self._cli_path = cli_path
     self._execute_java = functools.partial(execute_java_for_targets, targets)
     self._coverage_force = options.coverage_force
     self._coverage_datafile = os.path.join(settings.coverage_dir, 'jacoco.exec')
+    self._coverage_report_dir = os.path.join(settings.coverage_dir, 'reports')
 
   def instrument(self):
     # jacoco does runtime instrumentation, so this only does clean-up of existing run
@@ -112,15 +114,14 @@ class Jacoco(CoverageEngine):
     if execution_failed_exception:
       self._context.log.warn('Test failed: {0}'.format(execution_failed_exception))
       if self._coverage_force:
-        self._context.log.warn('Generating report even though tests failed.')
+        self._context.log.warn('Generating report even though tests failed, because the coverage-force flag is set.')
       else:
         return
 
-    report_dir = os.path.join(self._settings.coverage_dir, 'reports')
-    safe_mkdir(report_dir, clean=True)
+    safe_mkdir(self._coverage_report_dir, clean=True)
     for report_format in ['xml', 'csv', 'html']:
-      target_path = os.path.join(report_dir, report_format)
-      args = ['report', self._coverage_datafile] + self.get_target_classpaths() + self.get_source_roots() + [
+      target_path = os.path.join(self._coverage_report_dir, report_format)
+      args = ['report', self._coverage_datafile] + self._get_target_classpaths() + self._get_source_roots() + [
         '--{report_format}={target_path}'.format(report_format=report_format,
                                                  target_path=target_path)
       ]
@@ -135,23 +136,26 @@ class Jacoco(CoverageEngine):
         raise TaskError("java {0} ... exited non-zero ({1})"
                         " 'failed to report'".format(main, result))
 
-  def get_target_classpaths(self):
+  def _get_target_classpaths(self):
     runtime_classpath = self._context.products.get_data('runtime_classpath')
 
     target_paths = []
-    for target in self._targets:
-      if Jacoco.is_coverage_target(target):
-        paths = runtime_classpath.get_for_target(target)
-        for (name, path) in paths:
-          target_paths.append(path)
+    for target in self._coverage_targets:
+      paths = runtime_classpath.get_for_target(target)
+      for (name, path) in paths:
+        target_paths.append(path)
 
-    return self.make_multiple_arg('--classfiles', target_paths)
+    return self._make_multiple_arg('--classfiles', target_paths)
 
-  def get_source_roots(self):
-    source_roots = {t.target_base for t in self._targets if Jacoco.is_coverage_target(t)}
-    return self.make_multiple_arg('--sourcefiles', source_roots)
+  def _get_source_roots(self):
+    source_roots = {t.target_base for t in self._coverage_targets}
+    return self._make_multiple_arg('--sourcefiles', source_roots)
 
-  def make_multiple_arg(self, arg_name, arg_list):
+  def _make_multiple_arg(self, arg_name, arg_list):
+    """Jacoco cli allows the specification of multiple values for certain args by repeating the argument
+    with a new value. E.g. --classfiles a.class --classfiles b.class, etc. This method creates a list of
+    strings interleaved with the arg name to satisfy that format.
+    """
     unique_args = list(set(arg_list))
 
     args = [(arg_name, f) for f in unique_args]
