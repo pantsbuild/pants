2b34fa36bafce68be6d919bbb6ee256ad530f761
Lionel Vital <lvital@twitter.com>
2017-11-17 12:23:26 -0800

    Update superhello.py
    fix import

--- examples/src/python/example/python_distribution/hello/superhello/superhello.py
+++ examples/src/python/example/python_distribution/hello/superhello/superhello.py
@@ -5,7 +5,7 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-from superhello import super_greet
+from super_greet import super_greet
 
 
 def superhello(greetee):
