d9dfcddb50fed370acaca8935ce168386578cffc
cheister <cheister@gmail.com>
2017-12-05 13:11:05 -0800

    Fix showing test output that happens after the tests are finished (#5165)

---
+++ tests/java/org/pantsbuild/tools/junit/lib/LogOutputInTeardownTest.java
@@ -0,0 +1,21 @@
+// Copyright 2017 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+package org.pantsbuild.tools.junit.lib;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LogOutputInTeardownTest {
+  @AfterClass
+  public static void tearDown() {
+    System.out.println("Output in tearDown");
+  }
+
+  @Test
+  public void test() {
+    Assert.assertTrue(true);
+  }
+}
