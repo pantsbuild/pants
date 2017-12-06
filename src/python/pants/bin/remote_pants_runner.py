38c2aa9a942ef6eae4fdda31a200c0d8be23359b
Kris Wilson <kwlzn@users.noreply.github.com>
2017-12-06 09:23:41 -0800

    [pantsd] Improve SIGQUIT handling in the thin client. (#5177)
    Fixes #5176

--- src/python/pants/bin/remote_pants_runner.py
+++ src/python/pants/bin/remote_pants_runner.py
@@ -55,17 +55,25 @@ class RemotePantsRunner(object):
     return PantsDaemon.Factory.create(bootstrap_options=self._bootstrap_options)
 
   @contextmanager
-  def _trapped_control_c(self, client):
-    """A contextmanager that overrides the SIGINT (control-c) handler and handles it remotely."""
+  def _trapped_signals(self, client):
+    """A contextmanager that overrides the SIGINT (control-c) and SIGQUIT (control-\) handlers
+    and handles them remotely."""
     def handle_control_c(signum, frame):
       client.send_control_c()
 
     existing_sigint_handler = signal.signal(signal.SIGINT, handle_control_c)
-    signal.siginterrupt(signal.SIGINT, False)  # Retry interrupted system calls.
+    # N.B. SIGQUIT will abruptly kill the pantsd-runner, which will shut down the other end
+    # of the Pailgun connection - so we send a gentler SIGINT here instead.
+    existing_sigquit_handler = signal.signal(signal.SIGQUIT, handle_control_c)
+
+    # Retry interrupted system calls.
+    signal.siginterrupt(signal.SIGINT, False)
+    signal.siginterrupt(signal.SIGQUIT, False)
     try:
       yield
     finally:
       signal.signal(signal.SIGINT, existing_sigint_handler)
+      signal.signal(signal.SIGQUIT, existing_sigquit_handler)
 
   def _setup_logging(self):
     """Sets up basic stdio logging for the thin client."""
@@ -94,7 +102,7 @@ class RemotePantsRunner(object):
                            err=self._stderr,
                            exit_on_broken_pipe=True)
 
-    with self._trapped_control_c(client):
+    with self._trapped_signals(client):
       # Execute the command on the pailgun.
       result = client.execute(self.PANTS_COMMAND, *self._args, **modified_env)
 
