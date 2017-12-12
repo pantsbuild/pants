71fd8c0e8cd7808c873ea99afe5e6b2fc36313f1
Chris Livingston <clivingston@twitter.com>
2017-12-12 10:39:49 -0800

    Remove unused imports

--- tests/python/pants_test/backend/python/tasks2/test_python_run_integration.py
+++ tests/python/pants_test/backend/python/tasks2/test_python_run_integration.py
@@ -11,7 +11,6 @@ from pex.pex_bootstrapper import get_pex_info
 
 from pants.base.build_environment import get_buildroot
 from pants.util.contextutil import temporary_dir
-from pants.util.process_handler import subprocess
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 from pants_test.testutils.pexrc_util import (ensure_python_interpreter,
                                              setup_pexrc_with_pex_python_path)
