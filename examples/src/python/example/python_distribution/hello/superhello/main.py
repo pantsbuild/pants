177c379d25001b8a17b8a2eb04d404683061b883
Lionel Vital <lvital@twitter.com>
2017-12-15 12:32:06 -0800

    Whitespace lint fixes and unused import

--- examples/src/python/example/python_distribution/hello/superhello/main.py
+++ examples/src/python/example/python_distribution/hello/superhello/main.py
@@ -5,8 +5,6 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-import sys
-
 # hello_package is a python module within the superhello python_distribution
 from hello_package import hello
 
--- examples/tests/python/example/python_distribution/hello/test_superhello/test_superhello.py
+++ examples/tests/python/example/python_distribution/hello/test_superhello/test_superhello.py
@@ -8,10 +8,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 # hello_package is a python module within the superhello python_distribution.
 from hello_package import hello
 
-
 # Example of writing a test that depends on a python_dist target.
-
-
-
 def test_superhello():
-	assert hello.hello() == "Super hello"
+  assert hello.hello() == "Super hello"
