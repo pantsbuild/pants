1fa0f9b6e1931b349b578056ad6641a32bb9aeb8
Yi Cheng <wisechengyi@gmail.com>
2017-12-18 21:11:16 -0800

    --amend

--- src/python/pants/backend/jvm/tasks/jvm_compile/jvm_classpath_publisher.py
+++ src/python/pants/backend/jvm/tasks/jvm_compile/jvm_classpath_publisher.py
@@ -33,7 +33,6 @@ class RuntimeClasspathPublisher(Task):
   def execute(self):
     basedir = os.path.join(self.get_options().pants_distdir, self._output_folder)
     runtime_classpath = self.context.products.get_data('runtime_classpath')
-
     targets = self.context.targets()
     if self.get_options().manifest_jar_only:
       classpath = ClasspathUtil.classpath(targets, runtime_classpath)
