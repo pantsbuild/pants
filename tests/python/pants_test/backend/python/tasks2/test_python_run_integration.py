fe5be4b6e91758325e64a71513038db2ecdcb2f4
Stu Hood <stuhood@gmail.com>
2017-12-26 15:02:00 -0800

    Add coverage tests for other pex/subprocess users.

--- tests/python/pants_test/backend/python/tasks2/test_python_run_integration.py
+++ tests/python/pants_test/backend/python/tasks2/test_python_run_integration.py
@@ -11,16 +11,18 @@ import sys
 from pex.pex_bootstrapper import get_pex_info
 
 from pants.util.contextutil import temporary_dir
-from pants_test.pants_run_integration_test import PantsRunIntegrationTest
+from pants_test.pants_run_integration_test import PantsRunIntegrationTest, ensure_daemon
 from pants_test.testutils.pexrc_util import setup_pexrc_with_pex_python_path
 
 
 class PythonRunIntegrationTest(PantsRunIntegrationTest):
   testproject = 'testprojects/src/python/interpreter_selection'
 
+  @ensure_daemon
   def test_run_3(self):
     self._maybe_run_version('3')
 
+  @ensure_daemon
   def test_run_27(self):
     self._maybe_run_version('2.7')
 
