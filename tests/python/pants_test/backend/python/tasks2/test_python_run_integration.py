22d55b7b3c4f322dbadbe50812322e55cc405e4e
Stu Hood <stuhood@gmail.com>
2017-12-27 12:30:28 -0800

    Clean up python skips to account for one that was missed.

--- tests/python/pants_test/backend/python/tasks2/test_python_run_integration.py
+++ tests/python/pants_test/backend/python/tasks2/test_python_run_integration.py
@@ -27,6 +27,9 @@ class PythonRunIntegrationTest(PantsRunIntegrationTest):
     self._maybe_run_version('2.7')
 
   def test_run_27_and_then_3(self):
+    if self.skip_if_no_python('2.7') or self.skip_if_no_python('3'):
+      return
+
     with temporary_dir() as interpreters_cache:
       pants_ini_config = {'python-setup': {'interpreter_cache_dir': interpreters_cache}}
       pants_run_27 = self.run_pants(
@@ -66,54 +69,50 @@ class PythonRunIntegrationTest(PantsRunIntegrationTest):
   def test_pants_run_interpreter_selection_with_pexrc(self):
     py27 = '2.7'
     py3 = '3'
-    if self.has_python_version(py27) and self.has_python_version(py3):
-      print('Found both python {} and python {}. Running test.'.format(py27, py3))
-      py27_path, py3_path = self.python_interpreter_path(py27), self.python_interpreter_path(py3)
-      with setup_pexrc_with_pex_python_path(os.path.join(os.path.dirname(sys.argv[0]), '.pexrc'), [py27_path, py3_path]):
-        with temporary_dir() as interpreters_cache:
-          pants_ini_config = {'python-setup': {'interpreter_cache_dir': interpreters_cache}}
-          pants_run_27 = self.run_pants(
-            command=['run', '{}:main_py2'.format(os.path.join(self.testproject, 'python_3_selection_testing'))],
-            config=pants_ini_config
-          )
-          self.assert_success(pants_run_27)
-          # Interpreter selection for Python 2 is problematic in CI due to multiple virtualenvs in play.
-          if not os.getenv('CI'):
-            self.assertIn(py27_path.split(py27)[0], pants_run_27.stdout_data)
-          pants_run_3 = self.run_pants(
-            command=['run', '{}:main_py3'.format(os.path.join(self.testproject, 'python_3_selection_testing'))],
-            config=pants_ini_config
-          )
-          self.assert_success(pants_run_3)
-          # Protection for when the sys.executable path underlies a symlink pointing to 'python' without '3'
-          # at the end of the basename.
-          self.assertIn(py3_path.split(py3)[0], pants_run_3.stdout_data)
-    else:
-      print('Could not find both python {} and python {} on system. Skipping.'.format(py27, py3))
-      self.skipTest('Missing neccesary Python interpreters on system.')
+    if self.skip_if_no_python(py27) or self.skip_if_no_python(py3):
+      return
+
+    py27_path, py3_path = self.python_interpreter_path(py27), self.python_interpreter_path(py3)
+    with setup_pexrc_with_pex_python_path(os.path.join(os.path.dirname(sys.argv[0]), '.pexrc'), [py27_path, py3_path]):
+      with temporary_dir() as interpreters_cache:
+        pants_ini_config = {'python-setup': {'interpreter_cache_dir': interpreters_cache}}
+        pants_run_27 = self.run_pants(
+          command=['run', '{}:main_py2'.format(os.path.join(self.testproject, 'python_3_selection_testing'))],
+          config=pants_ini_config
+        )
+        self.assert_success(pants_run_27)
+        # Interpreter selection for Python 2 is problematic in CI due to multiple virtualenvs in play.
+        if not os.getenv('CI'):
+          self.assertIn(py27_path.split(py27)[0], pants_run_27.stdout_data)
+        pants_run_3 = self.run_pants(
+          command=['run', '{}:main_py3'.format(os.path.join(self.testproject, 'python_3_selection_testing'))],
+          config=pants_ini_config
+        )
+        self.assert_success(pants_run_3)
+        # Protection for when the sys.executable path underlies a symlink pointing to 'python' without '3'
+        # at the end of the basename.
+        self.assertIn(py3_path.split(py3)[0], pants_run_3.stdout_data)
 
   def test_pants_binary_interpreter_selection_with_pexrc(self):
     py27 = '2.7'
     py3 = '3'
-    if self.has_python_version(py27) and self.has_python_version(py3):
-      print('Found both python {} and python {}. Running test.'.format(py27, py3))
-      py27_path, py3_path = self.python_interpreter_path(py27), self.python_interpreter_path(py3)
-      with setup_pexrc_with_pex_python_path(os.path.join(os.path.dirname(sys.argv[0]), '.pexrc')  , [py27_path, py3_path]):
-        with temporary_dir() as interpreters_cache:
-          pants_ini_config = {'python-setup': {'interpreter_cache_dir': interpreters_cache}}
-          pants_run_27 = self.run_pants(
-            command=['binary', '{}:main_py2'.format(os.path.join(self.testproject, 'python_3_selection_testing'))],
-            config=pants_ini_config
-          )
-          self.assert_success(pants_run_27)
-          pants_run_3 = self.run_pants(
-            command=['binary', '{}:main_py3'.format(os.path.join(self.testproject, 'python_3_selection_testing'))],
-            config=pants_ini_config
-          )
-          self.assert_success(pants_run_3)
-    else:
-      print('Could not find both python {} and python {} on system. Skipping.'.format(py27, py3))
-      self.skipTest('Missing neccesary Python interpreters on system.')
+    if self.skip_if_no_python(py27) or self.skip_if_no_python(py3):
+      return
+
+    py27_path, py3_path = self.python_interpreter_path(py27), self.python_interpreter_path(py3)
+    with setup_pexrc_with_pex_python_path(os.path.join(os.path.dirname(sys.argv[0]), '.pexrc')  , [py27_path, py3_path]):
+      with temporary_dir() as interpreters_cache:
+        pants_ini_config = {'python-setup': {'interpreter_cache_dir': interpreters_cache}}
+        pants_run_27 = self.run_pants(
+          command=['binary', '{}:main_py2'.format(os.path.join(self.testproject, 'python_3_selection_testing'))],
+          config=pants_ini_config
+        )
+        self.assert_success(pants_run_27)
+        pants_run_3 = self.run_pants(
+          command=['binary', '{}:main_py3'.format(os.path.join(self.testproject, 'python_3_selection_testing'))],
+          config=pants_ini_config
+        )
+        self.assert_success(pants_run_3)
 
     # Ensure proper interpreter constraints were passed to built pexes.
     py2_pex = os.path.join(os.getcwd(), 'dist', 'main_py2.pex')
@@ -127,17 +126,23 @@ class PythonRunIntegrationTest(PantsRunIntegrationTest):
     os.remove(py2_pex)
     os.remove(py3_pex)
 
+  def skip_if_no_python(self, version):
+    if not self.has_python_version(version):
+      msg = 'No python {} found. Skipping.'.format(version)
+      print(msg)
+      self.skipTest(msg)
+      return True
+    return False
+
   def _maybe_run_version(self, version):
-    if self.has_python_version(version):
-      print('Found python {}. Testing running on it.'.format(version))
-      echo = self._run_echo_version(version)
-      v = echo.split('.')  # E.g., 2.7.13.
-      self.assertTrue(len(v) > 2, 'Not a valid version string: {}'.format(v))
-      expected_components = version.split('.')
-      self.assertEquals(expected_components, v[:len(expected_components,)])
-    else:
-      print('No python {} found. Skipping.'.format(version))
-      self.skipTest('No python {} on system'.format(version))
+    if self.skip_if_no_python(version):
+      return
+
+    echo = self._run_echo_version(version)
+    v = echo.split('.')  # E.g., 2.7.13.
+    self.assertTrue(len(v) > 2, 'Not a valid version string: {}'.format(v))
+    expected_components = version.split('.')
+    self.assertEquals(expected_components, v[:len(expected_components,)])
 
   def _run_echo_version(self, version):
     binary_name = 'echo_interpreter_version_{}'.format(version)
