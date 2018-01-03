6d3425e61ac9a3910de56e5f6397e72e1b12113c
John Sirois <john.sirois@gmail.com>
2018-01-03 10:42:13 -0800

    Support merging of junit xml in reports. (#5257)
    Currently test execution is arranged such that reports over-write based
    on name; so no merging is needed (or possible), but information can be
    truncated. A forthcoming change will re-arrange output paths for
    individual batches eliminating report truncation and allowing for
    merging.
    
    Part of the work for #5256.

--- src/python/pants/backend/jvm/tasks/reports/junit_html_report.py
+++ src/python/pants/backend/jvm/tasks/reports/junit_html_report.py
@@ -5,7 +5,10 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-import glob
+import collections
+import fnmatch
+import itertools
+import logging
 import os
 import xml.etree.ElementTree as ET
 from abc import abstractmethod
@@ -13,16 +16,87 @@ from functools import total_ordering
 
 from pants.base.mustache import MustacheRenderer
 from pants.util import desktop
-from pants.util.dirutil import safe_mkdir_for
+from pants.util.dirutil import safe_mkdir_for, safe_walk
+from pants.util.memo import memoized_property
 from pants.util.meta import AbstractClass
+from pants.util.objects import datatype
 from pants.util.strutil import ensure_binary
+
+
+_LOGGER = logging.getLogger(__name__)
 
 
 @total_ordering
 class ReportTestSuite(object):
   """Data object for a JUnit test suite"""
 
-  def __init__(self, name, tests, errors, failures, skipped, time, testcases):
+  class MergeError(Exception):
+    def __init__(self, suites, test_cases):
+      error_message = ('Refusing to merge duplicate test cases in suite {!r} from files {}:'
+                       '\n    {}').format(suites[0].name,
+                                          ', '.join(s.file for s in suites),
+                                          '\n    '.join(map(str, test_cases)))
+      super(ReportTestSuite.MergeError, self).__init__(error_message)
+
+  @classmethod
+  def merged(cls, report_test_suites, error_on_conflict=True, logger=None):
+    """Merges any like-named test suites into one test suite encompasing all the suite's test cases.
+
+    :param report_test_suites: A sequence of test suites to merge results from.
+    :type report_test_suites: :class:`collections.Iterable` of :class:`ReportTestSuite`
+    :param bool error_on_conflict: `True` to raise when two or more test cases in a given test suite
+                                   have the same name; otherwise the conflict is logged and the 1st
+                                   encountered duplicate is used.
+    :param logger: An optional logger to use for logging merge conflicts.
+    :type logger: :class:`logging.Logger`
+    :raises: :class:`ReportTestSuite.MergeError` if configured to do so on merge errors.
+    :yields: One test suite per unique test suite name in `report_test_suites` with the results of
+             all like-named test suites merged.
+    :rtype: iter of :class:`ReportTestSuite`
+    """
+
+    logger = logger or _LOGGER
+
+    suites_by_name = collections.defaultdict(list)
+    for report_test_suite in report_test_suites:
+      suites_by_name[report_test_suite.name].append(report_test_suite)
+
+    for suite_name, suites in suites_by_name.items():
+      cases_by_name = collections.defaultdict(list)
+      for case in itertools.chain.from_iterable(s.testcases for s in suites):
+        cases_by_name[case.name].append(case)
+
+      test_cases = []
+      tests, errors, failures, skipped, time = 0, 0, 0, 0, 0
+      for cases in cases_by_name.values():
+        if len(cases) > 1:
+          if error_on_conflict:
+            raise cls.MergeError(suites, cases)
+          else:
+            logger.warning('Found duplicate test case results in suite {!r} from files: {}, '
+                           'using first result:\n -> {}'.format(suite_name,
+                                                                ', '.join(s.file for s in suites),
+                                                                '\n    '.join(map(str, cases))))
+        case = iter(cases).next()
+        tests += 1
+        time += case.time
+        if case.error:
+          errors += 1
+        elif case.failure:
+          failures += 1
+        elif case.skipped:
+          skipped += 1
+        test_cases.append(case)
+
+      yield cls(name=suite_name,
+                tests=tests,
+                errors=errors,
+                failures=failures,
+                skipped=skipped,
+                time=time,
+                testcases=test_cases)
+
+  def __init__(self, name, tests, errors, failures, skipped, time, testcases, file=None):
     self.name = name
     self.tests = int(tests)
     self.errors = int(errors)
@@ -30,6 +104,7 @@ class ReportTestSuite(object):
     self.skipped = int(skipped)
     self.time = float(time)
     self.testcases = testcases
+    self.file = file
 
   def __lt__(self, other):
     if (self.errors, self.failures) > (other.errors, other.failures):
@@ -58,7 +133,12 @@ class ReportTestSuite(object):
     return icon_class
 
   def as_dict(self):
-    d = self.__dict__
+    d = dict(name=self.name,
+             tests=self.tests,
+             errors=self.errors,
+             failures=self.failures,
+             skipped=self.skipped,
+             time=self.time)
     d['success'] = ReportTestSuite.success_rate(self.tests, self.errors, self.failures,
                                                 self.skipped)
     d['icon_class'] = ReportTestSuite.icon_class(self.tests, self.errors, self.failures,
@@ -67,16 +147,13 @@ class ReportTestSuite(object):
     return d
 
 
-class ReportTestCase(object):
+class ReportTestCase(datatype('ReportTestCase', ['name', 'time', 'failure', 'error', 'skipped'])):
   """Data object for a JUnit test case"""
 
-  def __init__(self, name, time, failure, error, skipped):
-    self.name = name
-    self.time = float(time)
-    self.failure = failure
-    self.error = error
-    self.skipped = skipped
+  def __new__(cls, name, time, failure=None, error=None, skipped=False):
+    return super(ReportTestCase, cls).__new__(cls, name, float(time), failure, error, skipped)
 
+  @memoized_property
   def icon_class(self):
     icon_class = 'test-passed'
     if self.skipped:
@@ -88,15 +165,13 @@ class ReportTestCase(object):
     return icon_class
 
   def as_dict(self):
-    d = {
-      'name': self.name,
-      'time': self.time,
-      'icon_class': self.icon_class()
-    }
+    d = dict(name=self.name,
+             time=self.time,
+             icon_class=self.icon_class)
     if self.error:
-      d['message'] = self.error['message']
+      d['message'] = self.error
     elif self.failure:
-      d['message'] = self.failure['message']
+      d['message'] = self.failure
     return d
 
 
@@ -126,17 +201,21 @@ class JUnitHtmlReport(JUnitHtmlReportInterface):
   """Generates an HTML report from JUnit TEST-*.xml files"""
 
   @classmethod
-  def create(cls, xml_dir, logger):
-    return cls(xml_dir=xml_dir, report_dir=os.path.join(xml_dir, 'reports'), logger=logger)
+  def create(cls, xml_dir, logger=None, error_on_conflict=True):
+    return cls(xml_dir=xml_dir,
+               report_dir=os.path.join(xml_dir, 'reports'),
+               logger=logger,
+               error_on_conflict=error_on_conflict)
 
-  def __init__(self, xml_dir, report_dir, logger):
+  def __init__(self, xml_dir, report_dir, logger=None, error_on_conflict=True):
     self._xml_dir = xml_dir
     self._report_file_path = os.path.join(report_dir, 'junit-report.html')
-    self._logger = logger
+    self._logger = logger or _LOGGER
+    self._error_on_conflict = error_on_conflict
 
   def report(self):
     self._logger.debug('Generating JUnit HTML report...')
-    testsuites = self._parse_xml_files(self._xml_dir)
+    testsuites = self._parse_xml_files()
     safe_mkdir_for(self._report_file_path)
     with open(self._report_file_path, 'wb') as fp:
       fp.write(ensure_binary(self._generate_html(testsuites)))
@@ -146,13 +225,16 @@ class JUnitHtmlReport(JUnitHtmlReportInterface):
   def maybe_open_report(self):
     desktop.ui_open(self._report_file_path)
 
-  @classmethod
-  def _parse_xml_files(cls, xml_dir):
+  def _parse_xml_files(self):
     testsuites = []
-    for xml_file in glob.glob(os.path.join(xml_dir, 'TEST-*.xml')):
-      testsuites += cls._parse_xml_file(xml_file)
-    testsuites.sort()
-    return testsuites
+    for root, dirs, files in safe_walk(self._xml_dir, topdown=True):
+      dirs.sort()  # Ensures a consistent gathering order.
+      for xml_file in sorted(fnmatch.filter(files, 'TEST-*.xml')):
+        testsuites += self._parse_xml_file(os.path.join(root, xml_file))
+    merged_suites = ReportTestSuite.merged(testsuites,
+                                           logger=self._logger,
+                                           error_on_conflict=self._error_on_conflict)
+    return sorted(merged_suites)
 
   @staticmethod
   def _parse_xml_file(xml_file):
@@ -163,16 +245,10 @@ class JUnitHtmlReport(JUnitHtmlReportInterface):
     for testcase in root.iter('testcase'):
       failure = None
       for f in testcase.iter('failure'):
-        failure = {
-          'type': f.attrib['type'],
-          'message': f.text
-        }
+        failure = f.text
       error = None
       for e in testcase.iter('error'):
-        error = {
-          'type': e.attrib['type'],
-          'message': e.text
-        }
+        error = e.text
       skipped = False
       for _s in testcase.iter('skipped'):
         skipped = True
@@ -193,7 +269,8 @@ class JUnitHtmlReport(JUnitHtmlReportInterface):
         testsuite.attrib['failures'],
         testsuite.attrib.get('skipped', 0),
         testsuite.attrib['time'],
-        testcases
+        testcases,
+        file=xml_file,
       ))
     return testsuites
 
