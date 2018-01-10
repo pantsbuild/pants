4fd15677a449e3a133a93fa9ee85ec80ed27e0b8
Chris Livingston <clivingston@twitter.com>
2018-01-10 14:44:23 -0800

    Cleanup integration test and move superhello test project to examples/tests due to test breakage when placed in testprojects

--- tests/python/pants_test/backend/python/tasks2/test_python_distribution_integration.py
+++ tests/python/pants_test/backend/python/tasks2/test_python_distribution_integration.py
@@ -17,12 +17,12 @@ class PythonDistributionIntegrationTest(PantsRunIntegrationTest):
   # whl by setup.py) and an associated test to be consumed by the pants goals tested below.
   superhello_project = 'examples/src/python/example/python_distribution/hello/superhello'
   superhello_tests = 'examples/tests/python/example/python_distribution/hello/test_superhello'
-  superhello_testprojects_project = 'testprojects/src/python/python_targets/python_distribution/superhello'
+  superhello_testproject = 'examples/tests/python/example/python_distribution/hello/superhello_testproject'
 
   def test_pants_binary(self):
     command=['binary', '{}:main'.format(self.superhello_project)]
-    pants_run_27 = self.run_pants(command=command)
-    self.assert_success(pants_run_27)
+    pants_run = self.run_pants(command=command)
+    self.assert_success(pants_run)
     # Check that the pex was built.
     pex = os.path.join(get_buildroot(), 'dist', 'main.pex')
     self.assertTrue(os.path.isfile(pex))
@@ -34,32 +34,32 @@ class PythonDistributionIntegrationTest(PantsRunIntegrationTest):
 
   def test_pants_run(self):
     command=['run', '{}:main'.format(self.superhello_project)]
-    pants_run_27 = self.run_pants(command=command)
-    self.assert_success(pants_run_27)
+    pants_run = self.run_pants(command=command)
+    self.assert_success(pants_run)
     # Check that text was properly printed to stdout.
-    self.assertIn('Super hello', pants_run_27.stdout_data)
+    self.assertIn('Super hello', pants_run.stdout_data)
 
   def test_pants_test(self):
     command=['test', '{}:superhello'.format(self.superhello_tests)]
-    pants_run_27 = self.run_pants(command=command)
-    self.assert_success(pants_run_27)
+    pants_run = self.run_pants(command=command)
+    self.assert_success(pants_run)
 
   def test_with_conflicting_deps(self):
     command=['run', '{}:main_with_conflicting_dep'.format(self.superhello_project)]
-    pants_run_27 = self.run_pants(command=command)
-    self.assert_failure(pants_run_27)
-    self.assertIn('Exception message: Could not satisfy all requirements', pants_run_27.stderr_data)
+    pants_run = self.run_pants(command=command)
+    self.assert_failure(pants_run)
+    self.assertIn('Exception message: Could not satisfy all requirements', pants_run.stderr_data)
     command=['binary', '{}:main_with_conflicting_dep'.format(self.superhello_project)]
-    pants_run_27 = self.run_pants(command=command)
-    self.assert_failure(pants_run_27)
-    self.assertIn('Exception message: Could not satisfy all requirements', pants_run_27.stderr_data)
+    pants_run = self.run_pants(command=command)
+    self.assert_failure(pants_run)
+    self.assertIn('Exception message: Could not satisfy all requirements', pants_run.stderr_data)
 
   def test_pants_binary_with_two_targets(self):
     # Test that targets with unique python_dist dependencies only build with their specific
     # listed python_dist dependencies (i.e. that built dist products are filtered properly).
-    command=['binary', '{}:main'.format(self.superhello_project), '{}:bin_with_python_dist'.format(self.superhello_testprojects_project)]
-    pants_run_27 = self.run_pants(command=command)
-    self.assert_success(pants_run_27)
+    command=['binary', '{}:main'.format(self.superhello_project), '{}:bin_with_python_dist'.format(self.superhello_testproject)]
+    pants_run = self.run_pants(command=command)
+    self.assert_success(pants_run)
     # Check that the pex was built.
     pex = os.path.join(get_buildroot(), 'dist', 'main.pex')
     self.assertTrue(os.path.isfile(pex))
