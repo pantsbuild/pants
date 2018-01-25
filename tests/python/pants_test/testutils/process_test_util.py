a3ca901c51ae9b8d4bcbf1ffe09935ec0a036281
Kris Wilson <kwilson@twopensource.com>
2018-01-24 18:51:58 -0800

    [pantsd] Repair pantsd integration tests for execution via pantsd.

--- tests/python/pants_test/testutils/process_test_util.py
+++ tests/python/pants_test/testutils/process_test_util.py
@@ -5,6 +5,8 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+from contextlib import contextmanager
+
 import psutil
 
 
@@ -12,14 +14,30 @@ class ProcessStillRunning(AssertionError):
   """Raised when a process shouldn't be running but is."""
 
 
-def assert_no_process_exists_by_command(name):
-  """Asserts that no process exists for a given command with a helpful error."""
+def _safe_iter_matching_processes(name):
   for proc in psutil.process_iter():
     try:
-      cmdline = proc.cmdline()
-      if name in ''.join(cmdline):
-        raise ProcessStillRunning(
-          'a {} process was detected at PID {} (cmdline={})'.format(name, proc.pid, cmdline)
-        )
+      if name in ''.join(proc.cmdline()):
+        yield proc
     except (psutil.NoSuchProcess, psutil.AccessDenied):
       pass
+
+
+def _matching_process_set(name):
+  return {p for p in _safe_iter_matching_processes(name)}
+
+
+@contextmanager
+def no_lingering_process_by_command(name):
+  """Asserts that no process exists for a given command with a helpful error, excluding
+  existing processes outside of the scope of the contextmanager."""
+  existing_processes = _matching_process_set(name)
+  yield
+  delta_processes = existing_processes.difference(_matching_process_set(name))
+  if delta_processes:
+    pids = [p.pid for p in delta_processes]
+    cmdlines = [''.join(p.cmdline()) for p in delta_processes]
+    raise ProcessStillRunning(
+      '{} {} processes were detected at PIDS {} (cmdlines={})'
+      .format(len(delta_processes), name, pids, cmdlines)
+    )
