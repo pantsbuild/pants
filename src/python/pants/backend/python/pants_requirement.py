395bc121b219b186cafad0f31ee98af8095d69f5
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-02-12 09:36:22 -0800

    remove unused requirement

--- src/python/pants/backend/python/pants_requirement.py
+++ src/python/pants/backend/python/pants_requirement.py
@@ -8,7 +8,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import os
 
 from pants.backend.python.python_requirement import PythonRequirement
-from pants.backend.python.targets.python_requirement_library import PythonRequirementLibrary
 from pants.base.build_environment import pants_version
 
 
