f396ce18a33a058bfa45480f87e013aa6c497c96
Stu Hood <stuhood@gmail.com>
2018-02-14 16:23:06 -0800

    Remove unused imports

--- tests/python/pants_test/backend/jvm/tasks/test_checkstyle_integration.py
+++ tests/python/pants_test/backend/jvm/tasks/test_checkstyle_integration.py
@@ -7,8 +7,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 
 import json
 import os
-import shutil
-from contextlib import contextmanager
 from textwrap import dedent
 
 from pants.base.build_environment import get_buildroot
