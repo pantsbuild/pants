bfe5a2e36a2688059e51fcc4c439317302afdf5b
Travis Crawford <tcrawford@slack-corp.com>
2018-02-26 10:16:41 -0800

    Thrifty support for pants
    Similar to how Wire generates Android-optimized Java code from
    protobufs, [Microsoft Thrifty](https://github.com/Microsoft/thrifty)
    generates Android-optimized Java code from thrift IDL files. Here we add
    a new `java_thrifty_library` target type to integrate Thrifty with
    Pants.

---
+++ contrib/thrifty/src/python/pants/contrib/thrifty/register.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+# Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+from pants.build_graph.build_file_aliases import BuildFileAliases
+from pants.goal.task_registrar import TaskRegistrar as task
+
+from pants.contrib.thrifty.java_thrifty_gen import JavaThriftyGen
+from pants.contrib.thrifty.java_thrifty_library import JavaThriftyLibrary
+
+
+def build_file_aliases():
+  return BuildFileAliases(
+    targets={
+      'java_thrifty_library': JavaThriftyLibrary,
+    }
+  )
+
+
+def register_goals():
+  task(name='thrifty', action=JavaThriftyGen).install('gen')
