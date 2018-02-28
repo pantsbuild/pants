f8d636753fcc4d0da144399ff30186ff615edbc9
Dorothy Ordogh <dordogh@twitter.com>
2018-02-28 10:43:25 -0800

    remove tempfile import

--- tests/python/pants_test/backend/jvm/tasks/test_coursier_resolve.py
+++ tests/python/pants_test/backend/jvm/tasks/test_coursier_resolve.py
@@ -6,7 +6,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 import os
-import tempfile
 from contextlib import contextmanager
 
 from mock import MagicMock
