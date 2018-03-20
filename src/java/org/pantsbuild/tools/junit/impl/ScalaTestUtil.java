be60f4848a5335b9326b19c5367dfa3c629d0793
Ian Tabolt <iantabolt@foursquare.com>
2018-03-20 14:52:11 -0400

    Also load junit runner class in static init

--- src/java/org/pantsbuild/tools/junit/impl/ScalaTestUtil.java
+++ src/java/org/pantsbuild/tools/junit/impl/ScalaTestUtil.java
@@ -5,31 +5,28 @@ import org.junit.runner.Runner;
 public final class ScalaTestUtil {
   private ScalaTestUtil() {}
 
-  /**
-   * org.scalatest.Suite.class, loaded with runtime reflection to avoid the
-   * scalatest dependency.
-   */
-  private static Class<?> suiteClass;
+   // Scalatest classes loaded once with runtime reflection to avoid the extra
+   // dependency.
+  private static Class<?> suiteClass = null;
+  private static Class<?> junitRunnerClass = null;
   static {
     try {
       suiteClass = Class.forName("org.scalatest.Suite");
+      junitRunnerClass = Class.forName("org.scalatest.junit.JUnitRunner");
     } catch (ClassNotFoundException e) {
       // No scalatest tests on classpath
-      suiteClass = null;
     }
   }
 
   /**
-   * Returns a scalatest junit runner using reflection in the classloader of the test.
+   * Returns a scalatest junit runner using reflection.
    * @param clazz the test class
    *
    * @return a new scala test junit runner
    */
   public static Runner getJUnitRunner(Class<?> clazz) {
     try {
-      Class<?> junitRunnerClass = Class.forName("org.scalatest.junit.JUnitRunner",
-          true, clazz.getClassLoader());
-      return (Runner)junitRunnerClass.getConstructor(Class.class).newInstance(clazz);
+      return (Runner) junitRunnerClass.getConstructor(Class.class).newInstance(clazz);
     } catch (Exception e) {
       // isScalaTest should fail if scala test isn't available so this is probably ok.
       throw new RuntimeException(e);
@@ -38,7 +35,7 @@ public final class ScalaTestUtil {
 
   /**
    * Checks if the passed in test clazz has an ancestor that is the scala test suite
-   * object (looked up in the test classes class loader).
+   * trait.
    * @param clazz the test class
    *
    * @return true if the test class is a scalatest test, false if not.
