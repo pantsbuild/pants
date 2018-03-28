pantsbuild/pants

commit c981c392690f73bdc18e0d8184ee41920e6a273f

Further --changed optimization (#5579)
### Problem

A few more performance issues were discovered in `./pants --changed=.. list` due to the removal of the v1 engine:

* In order to calculate `direct` or `transitive` dependents, `ChangeCalculator` needs to compute the dependents graph. But it was doing this using `HydratedTargets` (ie, with sources globs and other fields expanded).
* After the `ChangeCalculator` was used to compute the new set of `TargetRoots`, we were converting the matched `Address` objects back into `Spec`s, triggering #4533.
* When locating candidate owning targets for some sources, `SourceMapper` was using `HydratedTargets`, which are (implicitly/unnecessarily) transitive.
* When matching source files against candidate targets, `SourceMapper` was using un-batched regex-based spec matching calls.

### Solution

* Add and switch to computing `HydratedStructs` in `ChangeCalculator`, which do not require expanding sources globs.
* Rename `HydratedTargets` to `TransitiveHydratedTargets`, and add a non-transitive rule to compute `HydratedTargets`. Use this in `SourceMapper`.
* Preserve `Address` objects in `ChangedTargetRoots`, which allows for a single deduped `TransitiveHydratedTarget` walk to warm and populate the graph.
* Add and used batched forms of the `filespec` matching methods to avoid re-parsing/compiling regexes. These matches should be ported to rust at some point. 

### Result

Transitive runs that touch more of the graph take time closer to the `./pants list ::` time, as expected.
 
Before:
```
$ time ./pants --changed-diffspec=22ca0604b1c6ce8de019214b821e922aac66b026^..22ca0604b1c6ce8de019214b821e922aac66b026 --changed-include-dependees=transitive list | wc -l

     562

real	0m15.945s
user	0m15.180s
sys	0m1.731s
```
After:
```
$ time ./pants --changed-diffspec=22ca0604b1c6ce8de019214b821e922aac66b026^..22ca0604b1c6ce8de019214b821e922aac66b026 --changed-include-dependees=transitive list | wc -l

src/python/pants/engine/build_files.py
@@ -318,6 +318,12 @@ def _recursive_dirname(f):
   yield ''
 
 
+# TODO: This is a bit of a lie: `Struct` is effectively abstract, so this collection
+# will contain subclasses of `Struct` for the symbol table types. These APIs need more
+# polish before we make them public: see #4535 in particular.
+HydratedStructs = Collection.of(Struct)
+
+
 BuildFilesCollection = Collection.of(BuildFiles)
 
 
@@ -343,6 +349,14 @@ def create_graph_rules(address_mapper, symbol_table):
       hydrate_struct
     ),
     resolve_unhydrated_struct,
+    TaskRule(
+      HydratedStructs,
+      [SelectDependencies(symbol_table_constraint,
+                          BuildFileAddresses,
+                          field_types=(Address,),
+                          field='addresses')],
+      HydratedStructs
+    ),
     # BUILD file parsing.
     parse_address_family,
     build_files,
@@ -355,6 +369,7 @@ def create_graph_rules(address_mapper, symbol_table):
     # Root rules representing parameters that might be provided via root subjects.
     RootRule(Address),
     RootRule(BuildFileAddress),
+    RootRule(BuildFileAddresses),
     RootRule(AscendantAddresses),
     RootRule(DescendantAddresses),
     RootRule(SiblingAddresses),

src/python/pants/scm/change_calculator.py
@@ -13,7 +13,8 @@ from collections import defaultdict
 from pants.base.build_environment import get_scm
 from pants.base.specs import DescendantAddresses
 from pants.build_graph.address import Address
-from pants.engine.legacy.graph import HydratedTargets, target_types_from_symbol_table
+from pants.engine.build_files import HydratedStructs
+from pants.engine.legacy.graph import target_types_from_symbol_table
 from pants.engine.legacy.source_mapper import EngineSourceMapper
 from pants.goal.workspace import ScmWorkspace
 from pants.util.meta import AbstractClass
@@ -22,8 +23,8 @@ from pants.util.meta import AbstractClass
 logger = logging.getLogger(__name__)
 
 
-class _HydratedTargetDependentGraph(object):
-  """A graph for walking dependent addresses of HydratedTarget objects.
+class _DependentGraph(object):
+  """A graph for walking dependent addresses of TargetAdaptor objects.
 
   This avoids/imitates constructing a v1 BuildGraph object, because that codepath results
   in many references held in mutable global state (ie, memory leaks).
@@ -41,26 +42,27 @@ class _HydratedTargetDependentGraph(object):
   """
 
   @classmethod
-  def from_iterable(cls, target_types, iterable):
-    """Create a new HydratedTargetDependentGraph from an iterable of HydratedTarget instances."""
+  def from_iterable(cls, target_types, adaptor_iter):
+    """Create a new DependentGraph from an iterable of TargetAdaptor subclasses."""
     inst = cls(target_types)
-    for hydrated_target in iterable:
-      inst.inject_target(hydrated_target)
+    for target_adaptor in adaptor_iter:
+      inst.inject_target(target_adaptor)
     return inst
 
   def __init__(self, target_types):
     self._dependent_address_map = defaultdict(set)
     self._target_types = target_types
 
-  def inject_target(self, hydrated_target):
+  def inject_target(self, target_adaptor):
     """Inject a target, respecting all sources of dependencies."""
-    target_cls = self._target_types[hydrated_target.adaptor.type_alias]
+    target_cls = self._target_types[target_adaptor.type_alias]
 
-    declared_deps = hydrated_target.dependencies
-    implicit_deps = (Address.parse(s) for s in target_cls.compute_dependency_specs(kwargs=hydrated_target.adaptor.kwargs()))
+    declared_deps = target_adaptor.dependencies
+    implicit_deps = (Address.parse(s)
+                     for s in target_cls.compute_dependency_specs(kwargs=target_adaptor.kwargs()))
 
     for dep in itertools.chain(declared_deps, implicit_deps):
-      self._dependent_address_map[dep].add(hydrated_target.adaptor.address)
+      self._dependent_address_map[dep].add(target_adaptor.address)
 
   def dependents_of_addresses(self, addresses):
     """Given an iterable of addresses, yield all of those addresses dependents."""
@@ -142,12 +144,15 @@ class EngineChangeCalculator(ChangeCalculator):
     if changed_request.include_dependees not in ('direct', 'transitive'):
       return
 
-    # For dependee finding, we need to parse all build files.
-    product_iter = (t
-                    for targets in self._scheduler.product_request(HydratedTargets, [DescendantAddresses('')])
+    # For dependee finding, we need to parse all build files to collect all structs. But we
+    # don't need to fully hydrate targets (ie, expand their source globs), and so we use
+    # the `HydratedStructs` product. See #4535 for more info.
+    adaptor_iter = (t
+                    for targets in self._scheduler.product_request(HydratedStructs,
+                                                                   [DescendantAddresses('')])
                     for t in targets.dependencies)
-    graph = _HydratedTargetDependentGraph.from_iterable(target_types_from_symbol_table(self._symbol_table),
-                                                        product_iter)
+    graph = _DependentGraph.from_iterable(target_types_from_symbol_table(self._symbol_table),
+                                          adaptor_iter)
 
     if changed_request.include_dependees == 'direct':
       for address in graph.dependents_of_addresses(changed_addresses):

tests/python/pants_test/engine/test_rules.py
@@ -303,8 +303,8 @@ class RuleGraphMakerTest(unittest.TestCase):
       else:
         pass
 
-    self.assertEquals(36, len(all_rules))
-    self.assertEquals(66, len(root_rule_lines)) # 2 lines per entry
+    self.assertEquals(41, len(all_rules))
+    self.assertEquals(78, len(root_rule_lines)) # 2 lines per entry
 
   def test_smallest_full_test_multiple_root_subject_types(self):
     rules = [

src/python/pants/base/BUILD
@@ -160,6 +160,14 @@ python_library(
 )
 
 python_library(
+  name = 'target_roots',
+  sources = ['target_roots.py'],
+  dependencies = [
+    'src/python/pants/util:objects',
+  ],
+)
+
+python_library(
   name = 'worker_pool',
   sources = ['worker_pool.py'],
   dependencies = [

src/python/pants/base/target_roots.py
@@ -1,0 +1,28 @@
+# coding=utf-8
+# Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
+                        unicode_literals, with_statement)
+
+from pants.util.objects import datatype
+
+
+class InvalidSpecConstraint(Exception):
+  """Raised when invalid constraints are given via target specs and arguments like --changed*."""
+
+
+class TargetRoots(object):
+  """Determines the target roots for a given pants run."""
+
+
+class ChangedTargetRoots(datatype('ChangedTargetRoots', ['addresses']), TargetRoots):
+  """Target roots that have been altered by `--changed` functionality.
+
+  Contains a list of `Address`es rather than `Spec`s, because all inputs have already been
+  resolved, and are known to exist.
+  """
+
+
+class LiteralTargetRoots(datatype('LiteralTargetRoots', ['specs']), TargetRoots):
+  """User defined target roots, as pants.base.specs.Spec objects."""

src/python/pants/bin/engine_initializer.py
@@ -10,11 +10,13 @@ from collections import namedtuple
 
 from pants.base.build_environment import get_buildroot, get_scm
 from pants.base.file_system_project_tree import FileSystemProjectTree
-from pants.engine.build_files import create_graph_rules
+from pants.base.target_roots import ChangedTargetRoots, LiteralTargetRoots
+from pants.engine.build_files import BuildFileAddresses, create_graph_rules
 from pants.engine.fs import create_fs_rules
 from pants.engine.isolated_process import create_process_rules
 from pants.engine.legacy.address_mapper import LegacyAddressMapper
-from pants.engine.legacy.graph import HydratedTargets, LegacyBuildGraph, create_legacy_graph_tasks
+from pants.engine.legacy.graph import (LegacyBuildGraph, TransitiveHydratedTargets,
+                                       create_legacy_graph_tasks)
 from pants.engine.legacy.parser import LegacyPythonCallbacksParser
 from pants.engine.legacy.structs import (GoTargetAdaptor, JavaLibraryAdaptor, JunitTestsAdaptor,
                                          JvmAppAdaptor, PythonLibraryAdaptor, PythonTargetAdaptor,
@@ -74,12 +76,18 @@ class LegacyGraphHelper(namedtuple('LegacyGraphHelper', ['scheduler', 'symbol_ta
   """A container for the components necessary to construct a legacy BuildGraph facade."""
 
   def warm_product_graph(self, target_roots):
-    """Warm the scheduler's `ProductGraph` with `HydratedTargets` products.
+    """Warm the scheduler's `ProductGraph` with `TransitiveHydratedTargets` products.
 
     :param TargetRoots target_roots: The targets root of the request.
     """
     logger.debug('warming target_roots for: %r', target_roots)
-    request = self.scheduler.execution_request([HydratedTargets], target_roots.as_specs())
+    if type(target_roots) is ChangedTargetRoots:
+      subjects = [BuildFileAddresses(target_roots.addresses)]
+    elif type(target_roots) is LiteralTargetRoots:
+      subjects = target_roots.specs
+    else:
+      raise ValueError('Unexpected TargetRoots type: `{}`.'.format(target_roots))
+    request = self.scheduler.execution_request([TransitiveHydratedTargets], subjects)
     result = self.scheduler.execute(request)
     if result.error:
       raise result.error
@@ -95,7 +103,7 @@ class LegacyGraphHelper(namedtuple('LegacyGraphHelper', ['scheduler', 'symbol_ta
     graph = LegacyBuildGraph.create(self.scheduler, self.symbol_table)
     logger.debug('build_graph is: %s', graph)
     # Ensure the entire generator is unrolled.
-    for _ in graph.inject_specs_closure(target_roots.as_specs()):
+    for _ in graph.inject_roots_closure(target_roots):
       pass
 
     address_mapper = LegacyAddressMapper(self.scheduler, build_root or get_buildroot())

src/python/pants/bin/goal_runner.py
@@ -21,7 +21,7 @@ from pants.goal.goal import Goal
 from pants.goal.run_tracker import RunTracker
 from pants.help.help_printer import HelpPrinter
 from pants.init.subprocess import Subprocess
-from pants.init.target_roots import TargetRoots
+from pants.init.target_roots_calculator import TargetRootsCalculator
 from pants.java.nailgun_executor import NailgunProcessGroup
 from pants.option.ranked_value import RankedValue
 from pants.reporting.reporting import Reporting
@@ -113,7 +113,7 @@ class GoalRunnerFactory(object):
         include_trace_on_error=self._options.for_global_scope().print_exception_stacktrace
       )
 
-    target_roots = target_roots or TargetRoots.create(
+    target_roots = target_roots or TargetRootsCalculator.create(
       options=self._options,
       build_root=self._root_dir,
       change_calculator=graph_helper.change_calculator
@@ -133,21 +133,21 @@ class GoalRunnerFactory(object):
     goals = [Goal.by_name(goal) for goal in requested_goals]
     return goals
 
-  def _specs_to_targets(self, specs):
-    """Populate the BuildGraph and target list from a set of input specs."""
+  def _roots_to_targets(self, target_roots):
+    """Populate the BuildGraph and target list from a set of input TargetRoots."""
     with self._run_tracker.new_workunit(name='parse', labels=[WorkUnitLabel.SETUP]):
       def filter_for_tag(tag):
         return lambda target: tag in map(str, target.tags)
 
       tag_filter = wrap_filters(create_filters(self._tag, filter_for_tag))
 
-      def generate_targets(specs):
-        for address in self._build_graph.inject_specs_closure(specs, self._fail_fast):
+      def generate_targets():
+        for address in self._build_graph.inject_roots_closure(target_roots, self._fail_fast):
           target = self._build_graph.get_target(address)
           if tag_filter(target):
             yield target
 
-    return list(generate_targets(specs))
+      return list(generate_targets())
 
   def _should_be_quiet(self, goals):
     if self._explain:
@@ -174,7 +174,7 @@ class GoalRunnerFactory(object):
       goals = self._determine_goals(self._requested_goals)
       is_quiet = self._should_be_quiet(goals)
 
-      literal_target_roots = self._specs_to_targets(target_roots.as_specs())
+      target_root_instances = self._roots_to_targets(target_roots)
 
       # Now that we've parsed the bootstrap BUILD files, and know about the SCM system.
       self._run_tracker.run_info.add_scm_info()
@@ -186,7 +186,7 @@ class GoalRunnerFactory(object):
 
       context = Context(options=self._options,
                         run_tracker=self._run_tracker,
-                        target_roots=literal_target_roots,
+                        target_roots=target_root_instances,
                         requested_goals=self._requested_goals,
                         build_graph=self._build_graph,
                         build_file_parser=self._build_file_parser,

src/python/pants/build_graph/BUILD
@@ -17,6 +17,7 @@ python_library(
     'src/python/pants/base:payload',
     'src/python/pants/base:payload_field',
     'src/python/pants/base:specs',
+    'src/python/pants/base:target_roots',
     'src/python/pants/base:validation',
     'src/python/pants/option',
     'src/python/pants/source',

commit 739d74bd89896ac1f5e1541b5056aba8be503427

Fail for deleted-but-depended-on targets in changed (#5636)
### Problem

#5579 broke detection of deleted targets.

### Solution

As described in #382 (mega classic!), it might be possible to more deeply integrate change detection into the v2 engine itself to compute a delta on the graph. But for now we defer a deeper solution and simply ensure that we fail for deleted targets by transitively expanding targets. Adds a test to cover the behaviour.

### Result

Due to fully hydrating targets, this represents a linear performance regression from #5579: the runtime of `--changed-include-dependees=transitive` for a small set of roots used to be slightly lower than the runtime for `./pants list ::`, because the operation that occurred "on the entire graph" was computing `HydratedStructs`, rather than computing `TransitiveHydratedTargets`.

The impact for exactly that step is constant, and fairly high:
```
# before:
  DEBUG] Root Select(Collection(dependencies=(DescendantAddresses(directory=u''),)), =Collection.of(Struct)) completed.
  DEBUG] computed 1 nodes in 1.709688 seconds. there are 8858 total nodes.

# after:
  DEBUG] Root Select(Collection(dependencies=(DescendantAddresses(directory=u''),)), =TransitiveHydratedTargets) completed.
  DEBUG] computed 1 nodes in 2.989497 seconds. there are 15916 total nodes.
```
... but the impact on overall runtime is dependent on the count of targets that are transitively affected, because for all affected targets, we're going to need to compute transitive roots anyway. So for the example change from #5579 which affects 567 targets:
```
time ./pants --changed-diffspec=22ca0604b1c6ce8de019214b821e922aac66b026^..22ca0604b1c6ce8de019214b821e922aac66b026 --changed-include-dependees=transitive list | wc -l
# before:
  real	0m4.877s
  user	0m4.081s
  sys	0m1.068s

# after
  real	0m5.294s
  user	0m4.487s
  sys	0m1.142s
```

src/python/pants/engine/build_files.py
@@ -345,12 +345,6 @@ def _recursive_dirname(f):
   yield ''
 
 
-# TODO: This is a bit of a lie: `Struct` is effectively abstract, so this collection
-# will contain subclasses of `Struct` for the symbol table types. These APIs need more
-# polish before we make them public: see #4535 in particular.
-HydratedStructs = Collection.of(Struct)
-
-
 BuildFilesCollection = Collection.of(BuildFiles)
 
 
@@ -376,14 +370,6 @@ def create_graph_rules(address_mapper, symbol_table):
       hydrate_struct
     ),
     resolve_unhydrated_struct,
-    TaskRule(
-      HydratedStructs,
-      [SelectDependencies(symbol_table_constraint,
-                          BuildFileAddresses,
-                          field_types=(Address,),
-                          field='addresses')],
-      HydratedStructs
-    ),
     # BUILD file parsing.
     parse_address_family,
     build_files,

src/python/pants/scm/change_calculator.py
@@ -13,8 +13,8 @@ from collections import defaultdict
 from pants.base.build_environment import get_scm
 from pants.base.specs import DescendantAddresses
 from pants.build_graph.address import Address
-from pants.engine.build_files import HydratedStructs, Specs
-from pants.engine.legacy.graph import target_types_from_symbol_table
+from pants.engine.build_files import Specs
+from pants.engine.legacy.graph import TransitiveHydratedTargets, target_types_from_symbol_table
 from pants.engine.legacy.source_mapper import EngineSourceMapper
 from pants.goal.workspace import ScmWorkspace
 from pants.util.meta import AbstractClass
@@ -144,14 +144,15 @@ class EngineChangeCalculator(ChangeCalculator):
     if changed_request.include_dependees not in ('direct', 'transitive'):
       return
 
-    # For dependee finding, we need to parse all build files to collect all structs. But we
-    # don't need to fully hydrate targets (ie, expand their source globs), and so we use
-    # the `HydratedStructs` product. See #4535 for more info.
+    # TODO: For dependee finding, we technically only need to parse all build files to collect target
+    # dependencies. But in order to fully validate the graph and account for the fact that deleted
+    # targets do not show up as changed roots, we use the `TransitiveHydratedTargets` product.
+    #   see https://github.com/pantsbuild/pants/issues/382
     specs = (DescendantAddresses(''),)
-    adaptor_iter = (t
-                    for targets in self._scheduler.product_request(HydratedStructs,
+    adaptor_iter = (t.adaptor
+                    for targets in self._scheduler.product_request(TransitiveHydratedTargets,
                                                                    [Specs(specs)])
-                    for t in targets.dependencies)
+                    for t in targets.roots)
     graph = _DependentGraph.from_iterable(target_types_from_symbol_table(self._symbol_table),
                                           adaptor_iter)
 

tests/python/pants_test/engine/legacy/test_changed_integration.py
@@ -332,6 +332,13 @@ class ChangedIntegrationTest(PantsRunIntegrationTest, TestGenerator):
       self.assert_success(pants_run)
       self.assertEqual(pants_run.stdout_data.strip(), 'src/python/sources:text')
 
+  def test_changed_with_deleted_target_transitive(self):
+    with create_isolated_git_repo() as worktree:
+      safe_delete(os.path.join(worktree, 'src/resources/org/pantsbuild/resourceonly/BUILD'))
+      pants_run = self.run_pants(['list', '--changed-parent=HEAD', '--changed-include-dependees=transitive'])
+      self.assert_failure(pants_run)
+      self.assertIn('src/resources/org/pantsbuild/resourceonly', pants_run.stderr_data)
+
   def test_changed_in_directory_without_build_file(self):
     with create_isolated_git_repo() as worktree:
       create_file_in(worktree, 'new-project/README.txt', 'This is important.')

tests/python/pants_test/engine/test_rules.py
@@ -303,8 +303,8 @@ class RuleGraphMakerTest(unittest.TestCase):
       else:
         pass
 
-    self.assertEquals(20, len(all_rules))
-    self.assertEquals(36, len(root_rule_lines)) # 2 lines per entry
+    self.assertTrue(10 < len(all_rules))
+    self.assertTrue(30 < len(root_rule_lines)) # 2 lines per entry
 
   def test_smallest_full_test_multiple_root_subject_types(self):
     rules = [