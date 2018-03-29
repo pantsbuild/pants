53a67e8437812219b131e17af774952211d28496
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-03-29 13:03:05 -0700

    test C and C++ in python_dist testing

--- examples/src/python/example/python_distribution/hello/fasthello/setup.py
+++ examples/src/python/example/python_distribution/hello/fasthello/setup.py
@@ -9,11 +9,12 @@ from setuptools import setup, find_packages
 from distutils.core import Extension
 
 
-c_module = Extension(str('super_greet'), sources=[str('super_greet.c')])
+c_module = Extension(str('c_greet'), sources=[str('c_greet.c')])
+cpp_module = Extension(str('cpp_greet'), sources=[str('cpp_greet.cpp')])
 
 setup(
   name='fasthello',
   version='1.0.0',
-  ext_modules=[c_module],
+  ext_modules=[c_module, cpp_module],
   packages=find_packages(),
 )
