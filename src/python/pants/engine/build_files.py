220413e6e01398def394696c8823d210d288a56f
Stu Hood <stuhood@gmail.com>
2018-04-09 14:18:01 -0700

    Revert the fix that landed in 79d9d357. See #5683 for more info.

--- src/python/pants/engine/build_files.py
+++ src/python/pants/engine/build_files.py
@@ -235,11 +235,12 @@ def addresses_from_address_families(address_mapper, specs):
     matched = False
     for af in address_families:
       for a in af.addressables.keys():
+        if a in included:
+          continue
         if not exclude_address(a) and (predicate is None or predicate(a)):
           matched = True
-          if a not in included:
-            addresses.append(a)
-            included.add(a)
+          addresses.append(a)
+          included.add(a)
     return matched
 
   for spec in specs.dependencies:
