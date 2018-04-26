aa9ba6712f1d3b3439ecd8dc1e45ad49cc8b2b72
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-04-26 00:58:07 -0700

    remove lint overrides

--- contrib/python/src/python/pants/contrib/python/checks/tasks/checkstyle/new_style_classes.py
+++ contrib/python/src/python/pants/contrib/python/checks/tasks/checkstyle/new_style_classes.py
@@ -13,31 +13,7 @@ from pants.contrib.python.checks.tasks.checkstyle.common import CheckstylePlugin
 class NewStyleClasses(CheckstylePlugin):
   """Enforce the use of new-style classes."""
 
-  def __init__(self, options, python_file):
-    super(NewStyleClasses, self).__init__(options, python_file)
-
-    special_decorators = options.special_decorators
-    if not isinstance(special_decorators, list):
-      raise TypeError(
-        "NewStyleClasses special decorators must be a list (was: {!r})."
-        .format(special_decorators))
-    invalid_decorators = []
-    for decorator in special_decorators:
-      if not isinstance(decorator, str):
-        invalid_decorators.append(
-          "decorator name was not a string: {!r}"
-          .format(decorator))
-    if invalid_decorators:
-      raise TypeError("NewStyleClasses special decorators were invalid:\n{}"
-                      .format('\n'.join(invalid_decorators)))
-
-    self._special_decorators = frozenset(special_decorators)
-
   def nits(self):
     for class_def in self.iter_ast_types(ast.ClassDef):
       if not class_def.bases:
-        decorator_ids = frozenset(
-          call.func.id for call in class_def.decorator_list)
-        if not decorator_ids.intersection(self._special_decorators):
-          yield self.error(
-            'T606', 'Classes must be new-style classes.', class_def)
+        yield self.error('T606', 'Classes must be new-style classes.', class_def)
