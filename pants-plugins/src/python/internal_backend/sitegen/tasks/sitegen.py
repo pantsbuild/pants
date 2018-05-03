6a1ac80462820b9746fed311ff868f02e615683b
Marilyn Cruz <marcruz385@gmail.com>
2018-05-03 01:36:59 -0700

    Nav menu change with drop-downs (#5750)

--- pants-plugins/src/python/internal_backend/sitegen/tasks/sitegen.py
+++ pants-plugins/src/python/internal_backend/sitegen/tasks/sitegen.py
@@ -266,22 +266,36 @@ def generate_site_toc(config, precomputed, here):
 
   def recurse(tree, depth_so_far):
     for node in tree:
+      if 'collapsible_heading' in node and 'pages' in node:
+        heading = node['collapsible_heading']
+        pages = node['pages']
+        links = []
+        collapse_open = False
+        for cur_page in pages:
+          html_filename = '{}.html'.format(cur_page)
+          page_is_here = cur_page == here
+          if page_is_here:
+            link = html_filename
+            collapse_open = True
+          else:
+            link = os.path.relpath(html_filename, os.path.dirname(here))
+          links.append(dict(link=link, text=precomputed.page[cur_page].title, here=page_is_here))
+        site_toc.append(dict(depth=depth_so_far, links=links, dropdown=True, heading=heading, id=heading.replace(' ', '-'), open=collapse_open))
       if 'heading' in node:
         heading = node['heading']
-        site_toc.append(dict(depth=depth_so_far,
-                             link=None,
-                             text=heading,
-                             here=False))
-      if 'page' in node and node['page'] != 'index':
-        dst = node['page']
-        if dst == here:
-          link = here + '.html'
-        else:
-          link = os.path.relpath(dst + '.html', os.path.dirname(here))
-        site_toc.append(dict(depth=depth_so_far,
-                             link=link,
-                             text=precomputed.page[dst].title,
-                             here=(dst == here)))
+        site_toc.append(dict(depth=depth_so_far, links=None, dropdown=False, heading=heading, id=heading.replace(' ', '-')))
+      if 'pages' in node and not 'collapsible_heading' in node:
+        pages = node['pages']
+        links = []
+        for cur_page in pages:
+          html_filename = '{}.html'.format(cur_page)
+          page_is_here = cur_page == here
+          if page_is_here:
+            link = html_filename
+          else:
+            link = os.path.relpath(html_filename, os.path.dirname(here))
+          links.append(dict(link=link, text=precomputed.page[cur_page].title, here=page_is_here))
+        site_toc.append(dict(depth=depth_so_far, links=links, dropdown=False, heading=None, id=heading.replace(' ', '-')))
       if 'children' in node:
         recurse(node['children'], depth_so_far + 1)
   if 'tree' in config:
