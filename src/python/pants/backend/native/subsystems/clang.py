289618fbea33e4017d774dde1febb04f7739677b
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-05-03 22:39:09 -0700

    demonstrate platform-specific conditionals in clang.py

--- src/python/pants/backend/native/subsystems/clang.py
+++ src/python/pants/backend/native/subsystems/clang.py
@@ -8,17 +8,27 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import os
 
 from pants.binaries.binary_tool import ExecutablePathProvider, NativeTool
+from pants.util.osutil import get_normalized_os_name
 
 
 class Clang(NativeTool, ExecutablePathProvider):
   options_scope = 'clang'
   default_version = '6.0.0'
-  archive_type = 'tgz'
+
+  @property
+  def archive_type(self):
+    if get_normalized_os_name() == 'darwin':
+      return 'txz'
+    else:
+      return 'tgz'
 
   _OSX_URL_FMT = 'https://releases.llvm.org/{version}/clang+llvm-{version}-x86_64-apple-darwin.tar.xz'
 
-  # def urls(self):
-
+  def urls(self):
+    if get_normalized_os_name() == 'darwin':
+      return [self._OSX_URL_FMT.format(self.version())]
+    else:
+      return None
 
   def path_entries(self):
     return [os.path.join(self.select(), 'bin')]
