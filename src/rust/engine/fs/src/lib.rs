3f837d0ef323a0e6e82665b8fddf45476e9fb608
Daniel Wagner-Hall <dwagnerhall@twitter.com>
2018-05-04 18:08:01 +0100

    Extract OneOffStoreFileByDigest
    There are currently a few implementations of this for standalone
    utilities or tests.
    
    I'm about to need another one for local process execution, which is real
    production code.

--- src/rust/engine/fs/src/lib.rs
+++ src/rust/engine/fs/src/lib.rs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 mod snapshot;
-pub use snapshot::{Snapshot, StoreFileByDigest, EMPTY_DIGEST};
+pub use snapshot::{OneOffStoreFileByDigest, Snapshot, StoreFileByDigest, EMPTY_DIGEST};
 mod store;
 pub use store::Store;
 mod pool;
