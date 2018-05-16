7e3264d1306667e0f2ea1d3f578eb59bb7b74ed9
Daniel Wagner-Hall <dwagnerhall@twitter.com>
2018-05-16 14:16:05 +0100

    cloc uses v2 process execution

--- src/python/pants/backend/graph_info/tasks/cloc.py
+++ src/python/pants/backend/graph_info/tasks/cloc.py
@@ -8,12 +8,12 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import os
 
 from pants.backend.graph_info.subsystems.cloc_binary import ClocBinary
-from pants.base.build_environment import get_buildroot
 from pants.base.exceptions import TaskError
 from pants.base.workunit import WorkUnitLabel
+from pants.engine.fs import FilesContent, PathGlobs, PathGlobsAndRoot, Snapshot
+from pants.engine.isolated_process import ExecuteProcessRequest, ExecuteProcessResult
 from pants.task.console_task import ConsoleTask
 from pants.util.contextutil import temporary_dir
-from pants.util.process_handler import subprocess
 
 
 class CountLinesOfCode(ConsoleTask):
@@ -32,59 +32,75 @@ class CountLinesOfCode(ConsoleTask):
     register('--ignored', type=bool, fingerprint=True,
              help='Show information about files ignored by cloc.')
 
-  def _get_cloc_script(self):
-    return ClocBinary.global_instance().select(self.context)
-
   def console_output(self, targets):
     if not self.get_options().transitive:
       targets = self.context.target_roots
 
-    buildroot = get_buildroot()
+    # TODO: Work out a nice library-like utility for writing an argfile, as this will be common.
     with temporary_dir() as tmpdir:
-      # Write the paths of all files we want cloc to process to the so-called 'list file'.
-      # TODO: 1) list_file, report_file and ignored_file should be relative files within the
-      # execution "chroot", 2) list_file should be part of an input files Snapshot, and
-      # 3) report_file and ignored_file should be part of an output files Snapshot, when we have
-      # that capability.
-      list_file = os.path.join(tmpdir, 'list_file')
+      list_file = os.path.join(tmpdir, 'input_files_list')
+      input_files = set()
       with open(list_file, 'w') as list_file_out:
         for target in targets:
           for source in target.sources_relative_to_buildroot():
-            list_file_out.write(os.path.join(buildroot, source))
+            input_files.add(source)
+            list_file_out.write(source)
             list_file_out.write(b'\n')
+      list_file_snapshot = self.context._scheduler.capture_snapshots((
+        PathGlobsAndRoot(
+          PathGlobs(('input_files_list',), ()),
+          tmpdir,
+        ),
+      ))[0]
 
-      report_file = os.path.join(tmpdir, 'report_file')
-      ignored_file = os.path.join(tmpdir, 'ignored')
+    cloc_path, cloc_snapshot = ClocBinary.global_instance().hackily_snapshot(self.context)
 
-      # TODO: Look at how to make BinaryUtil support Snapshots - such as adding an instrinsic to do
-      # network fetch directly into a Snapshot.
-      # See http://cloc.sourceforge.net/#options for cloc cmd-line options.
-      cmd = (
-        self._get_cloc_script(),
-        '--skip-uniqueness',
-        '--ignored={}'.format(ignored_file),
-        '--list-file={}'.format(list_file),
-        '--report-file={}'.format(report_file)
-      )
-      with self.context.new_workunit(
-        name='cloc',
-        labels=[WorkUnitLabel.TOOL],
-        cmd=' '.join(cmd)) as workunit:
-        exit_code = subprocess.call(
-          cmd,
-          stdout=workunit.output('stdout'),
-          stderr=workunit.output('stderr')
-        )
+    # TODO: This should use an input file snapshot which should be provided on the Target object,
+    # rather than hackily re-snapshotting each of the input files.
+    # See https://github.com/pantsbuild/pants/issues/5762
+    input_pathglobs = PathGlobs(tuple(input_files), ())
+    input_snapshot = self.context._scheduler.product_request(Snapshot, [input_pathglobs])[0]
 
-        if exit_code != 0:
-          raise TaskError('{} ... exited non-zero ({}).'.format(' '.join(cmd), exit_code))
+    directory_digest = self.context._scheduler.merge_directories((
+      cloc_snapshot.directory_digest,
+      input_snapshot.directory_digest,
+      list_file_snapshot.directory_digest,
+    ))
 
-      with open(report_file, 'r') as report_file_in:
-        for line in report_file_in.read().split('\n'):
-          yield line
+    cmd = (
+      cloc_path,
+      '--skip-uniqueness',
+      '--ignored=ignored',
+      '--list-file=input_files_list',
+      '--report-file=report',
+    )
 
-      if self.get_options().ignored:
-        yield 'Ignored the following files:'
-        with open(ignored_file, 'r') as ignored_file_in:
-          for line in ignored_file_in.read().split('\n'):
-            yield line
+    # The cloc script reaches into $PATH to look up perl. Let's assume it's in /usr/bin.
+    env = ('PATH', '/usr/bin')
+    req = ExecuteProcessRequest(cmd, env, directory_digest, ('ignored', 'report'))
+    with self.context.new_workunit(
+      name='cloc',
+      labels=[WorkUnitLabel.TOOL],
+      cmd=' '.join(cmd)
+    ):
+      # TODO: Work out how to nicely push stdout/stderr into the workunit, probably via a helper
+      # function on context.
+      exec_result = self.context._scheduler.product_request(ExecuteProcessResult, [req])[0]
+
+    # TODO: Remove this check when https://github.com/pantsbuild/pants/issues/5719 is resolved.
+    if exec_result.exit_code != 0:
+      raise TaskError('{} ... exited non-zero ({}).'.format(' '.join(cmd), exec_result.exit_code))
+
+    files_content_tuple = self.context._scheduler.product_request(
+      FilesContent,
+      [exec_result.output_directory_digest]
+    )[0].dependencies
+
+    files_content = {fc.path: fc.content for fc in files_content_tuple}
+    for line in files_content['report'].split('\n'):
+      yield line
+
+    if self.get_options().ignored:
+      yield 'Ignored the following files:'
+      for line in files_content['ignored'].split('\n'):
+        yield line
