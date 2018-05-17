0ee4f70b1797e8e5cce5353f16e4d50f9a553561
Travis Crawford <tcrawford@slack-corp.com>
2018-05-16 17:07:30 -0700

    Add go_protobuf_library
    Following the per-language `*_protobuf_library` pattern, here we add `go_protobuf_library` support and as simple example showing how to use it.

--- src/python/pants/task/simple_codegen_task.py
+++ src/python/pants/task/simple_codegen_task.py
@@ -197,7 +197,7 @@ class SimpleCodegenTask(Task):
     return True
 
   def _get_synthetic_address(self, target, target_workdir):
-    synthetic_name = target.id
+    synthetic_name = target.name
     sources_rel_path = os.path.relpath(target_workdir, get_buildroot())
     synthetic_address = Address(sources_rel_path, synthetic_name)
     return synthetic_address
