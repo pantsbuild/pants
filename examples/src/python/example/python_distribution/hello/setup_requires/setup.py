8fa164d8d13a89b3804ae97547dc14dece534734
Chris Livingston <clivingston@twitter.com>
2018-05-22 15:49:33 -0700

    Fix lint

--- examples/src/python/example/python_distribution/hello/setup_requires/setup.py
+++ examples/src/python/example/python_distribution/hello/setup_requires/setup.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
