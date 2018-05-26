a2e505fb12e3b01cbbfdfaae2eb081b09942b535
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-05-26 12:24:04 -0700

    pass lint and rearrange file

--- tests/python/pants_test/engine/legacy/test_graph_integration.py
+++ tests/python/pants_test/engine/legacy/test_graph_integration.py
@@ -8,7 +8,6 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import unittest
 
 from pants.build_graph.address_lookup_error import AddressLookupError
-from pants.option.errors import ParseError
 from pants.option.scope import GLOBAL_SCOPE_CONFIG_SECTION
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
@@ -28,23 +27,6 @@ class GraphIntegrationTest(PantsRunIntegrationTest):
     ]),
     'some-missing-some-not': ("globs('*.txt', '*.rs')", ['*.rs']),
     'overlapping-globs': ("globs('sources.txt', '*.txt')", ['*.txt']),
-  }
-
-  _ERR_TARGETS = {
-    'testprojects/src/python/sources:some-missing-some-not': [
-      "globs('*.txt', '*.rs')",
-      "Snapshot(PathGlobs(include=(u\'testprojects/src/python/sources/*.txt\', u\'testprojects/src/python/sources/*.rs\'), exclude=(), glob_match_error_behavior<=GlobMatchErrorBehavior>=GlobMatchErrorBehavior(failure_behavior=error)))",
-      "Globs did not match. Excludes were: []. Unmatched globs were: [\"testprojects/src/python/sources/*.rs\"].",
-    ],
-    'testprojects/src/java/org/pantsbuild/testproject/bundle:missing-bundle-fileset': [
-      "['a/b/file1.txt']",
-      "RGlobs('*.aaaa', '*.bbbb')",
-      "Globs('*.aaaa')",
-      "ZGlobs('**/*.abab')",
-      "['file1.aaaa', 'file2.aaaa']",
-      "Snapshot(PathGlobs(include=(u\'testprojects/src/java/org/pantsbuild/testproject/bundle/*.aaaa\',), exclude=(), glob_match_error_behavior<=GlobMatchErrorBehavior>=GlobMatchErrorBehavior(failure_behavior=error)))",
-      "Globs did not match. Excludes were: []. Unmatched globs were: [\"testprojects/src/java/org/pantsbuild/testproject/bundle/*.aaaa\"].",
-    ]
   }
 
   _WARN_FMT = "WARN] In target {base}:{name} with {desc}={glob}: glob pattern '{as_zsh_glob}' did not match any files."
@@ -69,6 +51,23 @@ class GraphIntegrationTest(PantsRunIntegrationTest):
         as_zsh_glob=as_zsh_glob)
       self.assertIn(warning_msg, pants_run.stderr_data)
 
+  _ERR_TARGETS = {
+    'testprojects/src/python/sources:some-missing-some-not': [
+      "globs('*.txt', '*.rs')",
+      "Snapshot(PathGlobs(include=(u\'testprojects/src/python/sources/*.txt\', u\'testprojects/src/python/sources/*.rs\'), exclude=(), glob_match_error_behavior<=GlobMatchErrorBehavior>=GlobMatchErrorBehavior(failure_behavior=error)))",
+      "Globs did not match. Excludes were: []. Unmatched globs were: [\"testprojects/src/python/sources/*.rs\"].",
+    ],
+    'testprojects/src/java/org/pantsbuild/testproject/bundle:missing-bundle-fileset': [
+      "['a/b/file1.txt']",
+      "RGlobs('*.aaaa', '*.bbbb')",
+      "Globs('*.aaaa')",
+      "ZGlobs('**/*.abab')",
+      "['file1.aaaa', 'file2.aaaa']",
+      "Snapshot(PathGlobs(include=(u\'testprojects/src/java/org/pantsbuild/testproject/bundle/*.aaaa\',), exclude=(), glob_match_error_behavior<=GlobMatchErrorBehavior>=GlobMatchErrorBehavior(failure_behavior=error)))",
+      "Globs did not match. Excludes were: []. Unmatched globs were: [\"testprojects/src/java/org/pantsbuild/testproject/bundle/*.aaaa\"].",
+    ]
+  }
+
   def _list_target_check_error(self, target_name):
     expected_excerpts = self._ERR_TARGETS[target_name]
 
@@ -78,6 +77,8 @@ class GraphIntegrationTest(PantsRunIntegrationTest):
       },
     })
     self.assert_failure(pants_run)
+
+    self.assertIn(AddressLookupError.__name__, pants_run.stderr_data)
 
     for excerpt in expected_excerpts:
       self.assertIn(excerpt, pants_run.stderr_data)
