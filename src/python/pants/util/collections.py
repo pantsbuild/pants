e808b35d71af329e34b434e5b1639dcc875e5af5
Daniel Wagner-Hall <dwagnerhall@twitter.com>
2018-06-01 15:54:40 +0100

    Integration test for daemon environment scrubbing
    This shows that #5898 works, which itself fixed #5854

--- src/python/pants/util/collections.py
+++ src/python/pants/util/collections.py
@@ -9,3 +9,12 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 def combined_dict(*dicts):
   """Combine one or more dicts into a new, unified dict (dicts to the right take precedence)."""
   return {k: v for d in dicts for k, v in d.items()}
+
+def recursively_update(dict, dict2):
+  """dict.update but which merges child dicts (dict2 takes precedence where there's conflict)."""
+  for k, v in dict2.iteritems():
+    if k in dict:
+      if isinstance(v, type(dict)):
+        recursively_update(dict[k], v)
+        continue
+    dict[k] = v
