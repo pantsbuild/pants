5d51e14f3ec4c00e593045168fe736ff03fb5926
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-06-04 12:09:26 -0700

    remove unused import

--- tests/python/pants_test/util/test_meta.py
+++ tests/python/pants_test/util/test_meta.py
@@ -5,7 +5,6 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-import unittest
 from abc import abstractmethod, abstractproperty
 
 from pants.util.meta import AbstractClass, Singleton, classproperty
