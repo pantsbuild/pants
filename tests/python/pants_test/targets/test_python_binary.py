92ac82de52a0b23a55ca079504ca719a73440d39
Daniel Wagner-Hall <dwagnerhall@twitter.com>
2018-06-05 13:34:00 +0100

    TestPythonBinary uses proper sources

--- tests/python/pants_test/targets/test_python_binary.py
+++ tests/python/pants_test/targets/test_python_binary.py
@@ -7,10 +7,11 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 
 from pants.backend.python.targets.python_binary import PythonBinary
 from pants.base.exceptions import TargetDefinitionException
+from pants_test.engine.sources_test_base import SourcesTestBase
 from pants_test.test_base import TestBase
 
 
-class TestPythonBinary(TestBase):
+class TestPythonBinary(SourcesTestBase, TestBase):
   def setUp(self):
     super(TestPythonBinary, self).setUp()
     # Force creation of SourceRootConfig global instance. PythonBinary uses source roots
@@ -27,45 +28,65 @@ class TestPythonBinary(TestBase):
                             entry_point='blork').entry_point == 'blork'
 
   def test_python_binary_with_source_no_entry_point(self):
+    self.create_file('blork.py')
+    blork_sources = self.sources_for(['blork.py'])
+
     assert self.make_target(spec=':binary1',
                             target_type=PythonBinary,
-                            source='blork.py').entry_point == 'blork'
+                            sources=blork_sources).entry_point == 'blork'
+
+    self.create_dir('bin')
+    self.create_file('bin/blork.py')
+    bin_blork_sources = self.sources_for(['bin/blork.py'])
+
     assert self.make_target(spec=':binary2',
                             target_type=PythonBinary,
-                            source='bin/blork.py').entry_point == 'bin.blork'
+                            sources=bin_blork_sources).entry_point == 'bin.blork'
 
   def test_python_binary_with_entry_point_and_source(self):
+    self.create_file('blork.py')
+    blork_sources = self.sources_for(['blork.py'])
+
     assert 'blork' == self.make_target(spec=':binary1',
                                        target_type=PythonBinary,
                                        entry_point='blork',
-                                       source='blork.py').entry_point
+                                       sources=blork_sources).entry_point
     assert 'blork:main' == self.make_target(spec=':binary2',
                                             target_type=PythonBinary,
                                             entry_point='blork:main',
-                                            source='blork.py').entry_point
+                                            sources=blork_sources).entry_point
+
+    self.create_dir('bin')
+    self.create_file('bin/blork.py')
+    bin_blork_sources = self.sources_for(['bin/blork.py'])
+
     assert 'bin.blork:main' == self.make_target(spec=':binary3',
                                                 target_type=PythonBinary,
                                                 entry_point='bin.blork:main',
-                                                source='bin/blork.py').entry_point
+                                                sources=bin_blork_sources).entry_point
 
   def test_python_binary_with_entry_point_and_source_mismatch(self):
+    self.create_file('hork.py')
+
+    hork_sources = self.sources_for(['hork.py'])
+
     with self.assertRaises(TargetDefinitionException):
       self.make_target(spec=':binary1',
                        target_type=PythonBinary,
                        entry_point='blork',
-                       source='hork.py')
+                       sources=hork_sources)
     with self.assertRaises(TargetDefinitionException):
       self.make_target(spec=':binary2',
                        target_type=PythonBinary,
                        entry_point='blork:main',
-                       source='hork.py')
+                       sources=hork_sources)
     with self.assertRaises(TargetDefinitionException):
       self.make_target(spec=':binary3',
                        target_type=PythonBinary,
-                       entry_point='bin.blork',
-                       source='blork.py')
+                       entry_point='bin.hork',
+                       sources=hork_sources)
     with self.assertRaises(TargetDefinitionException):
       self.make_target(spec=':binary4',
                        target_type=PythonBinary,
-                       entry_point='bin.blork',
-                       source='bin.py')
+                       entry_point='hork.blork',
+                       sources=hork_sources)
