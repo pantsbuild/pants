ceb025e1d1c13ed460d8cb760812870f713d26c4
Dorothy Ordogh <dordogh@twitter.com>
2018-06-12 10:30:35 -0700

    tada

--- src/python/pants/backend/jvm/tasks/jvm_compile/jvm_compile.py
+++ src/python/pants/backend/jvm/tasks/jvm_compile/jvm_compile.py
@@ -483,7 +483,6 @@ class JvmCompile(NailgunTaskBase):
           self._record_compile_classpath(classpath, vts.targets, ctx.classes_dir)
 
         try:
-          print("OHHHHHHLALALALALALALALLAALALALLA")
           self.compile(
             ctx,
             self._args,
