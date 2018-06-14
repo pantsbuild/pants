0b3ec6b2d4c46f6fae63319b96af379d6e7ad770
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-06-14 12:43:18 -0700

    add libc resolution for crti.o through gcc -print-search-dirs

--- src/python/pants/backend/native/subsystems/platform_specific/linux/libc.py
+++ src/python/pants/backend/native/subsystems/platform_specific/linux/libc.py
@@ -5,25 +5,29 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
+import logging
 import os
 import re
-from hashlib import sha1
 
-from pants.backend.native.subsystems.platform_specific.linux.binutils import Binutils
+from twitter.common.collections import OrderedSet
+
 from pants.base.hash_utils import hash_file
 from pants.subsystem.subsystem import Subsystem
-from pants.util.dirutil import is_executable, split_basename_and_dirname
+from pants.util.dirutil import is_readable_dir
 from pants.util.memo import memoized_classproperty, memoized_property
 from pants.util.objects import datatype
 from pants.util.process_handler import subprocess
 
 
-class HostLibc(datatype(['libc_dir', 'fingerprint'])): pass
+logger = logging.getLogger(__name__)
+
+
+class HostLibcDev(datatype(['crti_object', 'fingerprint'])): pass
 
 
 # TODO: think about how this could share any structure with XCodeCLITools, which is very similar in
 # spirit.
-class Libc(Subsystem):
+class LibcDev(Subsystem):
   """Subsystem to detect and provide the host's installed version of a libc "dev" package.
 
   This subsystem exists to give a useful error message if the package isn't
@@ -32,75 +36,68 @@ class Libc(Subsystem):
 
   options_scope = 'libc'
 
-  class HostLibcResolutionError(Exception): pass
+  class HostLibcDevResolutionError(Exception): pass
 
   @classmethod
-  def subsystem_dependencies(cls):
-    return super(Libc, cls).subsystem_dependencies() + (Binutils.scoped(cls),)
+  def register_options(cls, register):
+    super(LibcDev, cls).register_options(register)
 
-  @memoized_property
-  def _binutils(self):
-    return Binutils.scoped_instance(self)
-
-  @memoized_property
-  def _ld_path(self):
-    return os.path.join(self._binutils.select(), 'bin', 'ld')
-
-  # NB: libc.so.6 is used on linux x86_64, see https://en.wikipedia.org/wiki/GNU_C_Library. The
-  # containing directory should be provided in the LD_LIBRARY_PATH when invoking a linker.
-  _LIBC_SO_NAME = 'libc.so.6'
+    # FIXME(now): make something in custom_types.py for "a path to an existing executable file
+    # (absolute or relative to buildroot), or a filename that will be resolved against the PATH in
+    # some subprocess"
+    register('--host-compiler', type=str, default='gcc', advanced=True,
+             help='The host compiler to invoke with -print-search-dirs to find the host libc.')
 
   @memoized_classproperty
-  def _libc_so_ldd_output_regex(cls):
-    return re.compile('^\s+{soname} => (.*?{soname}) '.format(soname=re.escape(cls._LIBC_SO_NAME)),
-                      flags=re.MULTILINE)
+  def _search_dirs_libraries_regex(cls):
+    return re.compile('^libraries: =(.*)$', flags=re.MULTILINE)
 
   @classmethod
-  def _parse_libc_ldd_output(cls, ldd_out):
-    libc_line = cls._libc_so_ldd_output_regex.search(ldd_out)
+  def _parse_search_dirs_output_libraries(cls, cmd_output):
+    libs_line = cls._search_dirs_libraries_regex.search(cmd_output)
 
-    if not libc_line:
-      raise cls.HostLibcResolutionError("???")
+    if not libs_line:
+      raise cls.HostLibcDevResolutionError("???/libs_line: {}".format(libs_line))
 
-    return libc_line.group(1)
+    return libs_line.group(1).split(':')
 
   # NB: this file is required to create executables. gcc can find it if the containing directory is
   # within the LIBRARY_PATH environment variable.
-  _EXECUTABLE_MAIN_REQUIRED_OBJECT_FILE = 'crti.o'
-
-  @classmethod
-  def _required_files(cls):
-    return [cls._LIBC_SO_NAME, cls._EXECUTABLE_MAIN_REQUIRED_OBJECT_FILE]
+  _LIBC_INIT_OBJECT_FILE = 'crti.o'
 
   @memoized_property
   def host_libc(self):
+    """???"""
+    compiler_exe = self.get_options().host_compiler
+    cmd = [compiler_exe, '-print-search-dirs']
     try:
-      # TODO: why does this work?
-      ldd_out = subprocess.check_output(['ldd', self._ld_path])
+      compiler_output = subprocess.check_output(cmd)
     except OSError as e:
-      raise self.HostLibcResolutionError("???: {}".format(e))
+      raise self.HostLibcDevResolutionError("???: {}".format(e))
 
-    libc_so_path = self._parse_libc_ldd_output(ldd_out)
+    compiler_search_libraries = self._parse_search_dirs_output_libraries(compiler_output)
 
-    if not is_executable(libc_so_path):
-      raise self.HostLibcResolutionError("???")
+    real_lib_dirs = OrderedSet()
 
-    libc_dir, libc_so_name = split_basename_and_dirname(libc_so_path)
+    for lib_dir_path in compiler_search_libraries:
+      # Could use a `seen_dir_paths` set if we want to avoid pinging the fs for duplicate entries.
+      if is_readable_dir(lib_dir_path):
+        # FIXME(now): raise if path is not absolute
+        real_lib_dirs.add(os.path.realpath(lib_dir_path))
+      else:
+        logger.debug("non-existent or non-accessible program directory at {} while locating libc."
+                     .format(lib_dir_path))
 
-    # TODO: this check may be unnecessary.
-    if libc_so_name != self._LIBC_SO_NAME:
-      raise self.HostLibcResolutionError("???")
+    libc_crti_object_file = None
+    for libc_dir_candidate in real_lib_dirs:
+      maybe_libc_crti = os.path.join(libc_dir_candidate, self._LIBC_INIT_OBJECT_FILE)
+      if os.path.isfile(maybe_libc_crti):
+        libc_crti_object_file = maybe_libc_crti
+        break
 
-    hasher = sha1()
+    if not libc_crti_object_file:
+      raise self.HostLibcDevResolutionError(
+        "Could not locate {fname} in library search dirs {dirs} parsed from the output of {cmd}."
+        .format(fname=self._LIBC_INIT_OBJECT_FILE, dirs=real_lib_dirs, cmd=cmd))
 
-    # FIXME: we should be taking a hash of the file contents as well! we can share this with
-    # XCodeCLITools!
-    for fname in self._required_files():
-      libc_file_path = os.path.join(libc_dir, fname)
-      if not os.path.isfile(libc_file_path):
-        raise self.HostLibcResolutionError(
-          "Could not locate required file {} in libc dir {}."
-          .format(fname, libc_dir))
-      hash_file(libc_file_path, digest=hasher)
-
-    return HostLibc(libc_dir, hasher.hexdigest())
+    return HostLibcDev(libc_crti_object_file, hash_file(libc_crti_object_file))
