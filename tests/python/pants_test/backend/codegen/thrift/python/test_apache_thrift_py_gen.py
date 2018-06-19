efdd46e1359b865ca3896af5d963b970208fe653
John Sirois <john.sirois@gmail.com>
2018-06-19 03:14:22 -0600

    Robustify test_namespace_effective PYTHONPATH. (#5976)
    The real problem is noted, but this quick fix should bolster against
    interpreter cache interpreters pointing off to python binaries that
    have no setuptools in their associated site-packages.
    
    Fixes #5972

--- tests/python/pants_test/backend/codegen/thrift/python/test_apache_thrift_py_gen.py
+++ tests/python/pants_test/backend/codegen/thrift/python/test_apache_thrift_py_gen.py
@@ -149,7 +149,13 @@ class ApacheThriftPyGenTest(TaskTestBase):
     interpreter_cache = PythonInterpreterCache(python_setup, python_repos)
     interpreter = interpreter_cache.select_interpreter_for_targets(targets)
 
-    pythonpath = [os.path.join(get_buildroot(), t.target_base) for t in targets]
+    # We need setuptools to import namespace packages (via pkg_resources), so we prime the
+    # PYTHONPATH with interpreter extras, which Pants always populates with setuptools and wheel.
+    # TODO(John Sirois): We really should be emitting setuptools in a
+    # `synthetic_target_extra_dependencies` override in `ApacheThriftPyGen`:
+    #   https://github.com/pantsbuild/pants/issues/5975
+    pythonpath = interpreter.extras.values()
+    pythonpath.extend(os.path.join(get_buildroot(), t.target_base) for t in targets)
     for dist in resolve(['thrift=={}'.format(self.get_thrift_version(apache_thrift_gen))],
                         interpreter=interpreter,
                         context=python_repos.get_network_context(),
