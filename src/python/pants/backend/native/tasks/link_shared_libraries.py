41067f84d8555c3024beee74e553c725d3cdd712
Chris Livingston <clivingston@twitter.com>
2018-06-22 14:37:47 -0700

    Add workaround for linux machines

--- src/python/pants/backend/native/tasks/link_shared_libraries.py
+++ src/python/pants/backend/native/tasks/link_shared_libraries.py
@@ -137,7 +137,7 @@ class LinkSharedLibraries(NativeTask):
 
   _SHARED_CMDLINE_ARGS = {
     'darwin': lambda: ['-mmacosx-version-min=10.11', '-Wl,-dylib'],
-    'linux': lambda: ['-shared'],
+    'linux': lambda: ['-Wl,-shared'],
   }
 
   def _get_shared_lib_cmdline_args(self):
