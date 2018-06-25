460f9ad79e928c11f6d9148c8d3acab018e370eb
Daniel Wagner-Hall <dwagnerhall@twitter.com>
2018-06-25 18:51:41 +0100

    Upgrade to v2 of bazel protobuf
    Key differences:
     * Execute is a streaming RPC - we only support a single response per
       stream.
     * Action is now a separate proto addressed by digest, rather than
       inlined.
     * Inline output files are no longer supported.
     * Output files are now specified on Command not Action.

--- src/rust/engine/testutil/mock/src/cas.rs
+++ src/rust/engine/testutil/mock/src/cas.rs
@@ -392,16 +392,12 @@ impl bazel_protos::remote_execution_grpc::ContentAddressableStorage for StubCASR
       None,
     ));
   }
-
   fn get_tree(
     &self,
     _ctx: grpcio::RpcContext,
     _req: bazel_protos::remote_execution::GetTreeRequest,
-    sink: grpcio::UnarySink<bazel_protos::remote_execution::GetTreeResponse>,
+    _sink: grpcio::ServerStreamingSink<bazel_protos::remote_execution::GetTreeResponse>,
   ) {
-    sink.fail(grpcio::RpcStatus::new(
-      grpcio::RpcStatusCode::Unimplemented,
-      None,
-    ));
+    unimplemented!()
   }
 }
