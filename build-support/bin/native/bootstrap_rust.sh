10ee0bb83e63e529df4f6c9d6fe701845c0c65a6
John Sirois <john.sirois@gmail.com>
2018-06-29 09:36:06 -0600

    Ensure correct toolchain per clone.
    Previously, if the Rust toolchain was changed, re-bootstrapping would
    proceed correctly in the first clone encountering the change but it
    would fail in other clones. Ensure the correct toolchain is set when
    building the symlink farm (but not needing to bootstrap the toolchain)
    to cover this case.

--- build-support/bin/native/bootstrap_rust.sh
+++ build-support/bin/native/bootstrap_rust.sh
@@ -23,6 +23,13 @@ function cargo_bin() {
   "${RUSTUP}" which cargo
 }
 
+function set_rust_toolchain() {
+    (
+      cd "${REPO_ROOT}"
+      "${RUSTUP}" override set "${RUST_TOOLCHAIN}" >&2
+    )
+}
+
 function ensure_native_build_prerequisites() {
   # Control a pants-specific rust toolchain.
   if [[ ! -x "${RUSTUP}" ]]
@@ -41,10 +48,7 @@ function ensure_native_build_prerequisites() {
   local -r cargo_versioned="cargo-${RUST_TOOLCHAIN}-${cargo_components_fp}"
   if [[ ! -x "${rust_toolchain_root}/${cargo_versioned}" ]]
   then
-    (
-      cd "${REPO_ROOT}"
-      "${RUSTUP}" override set "${RUST_TOOLCHAIN}" >&2
-    )
+    set_rust_toolchain
     "${RUSTUP}" component add ${RUST_COMPONENTS[@]} >&2
 
     ln -fs "$(cargo_bin)" "${rust_toolchain_root}/${cargo_versioned}"
@@ -52,6 +56,7 @@ function ensure_native_build_prerequisites() {
 
   local -r symlink_farm_root="${REPO_ROOT}/build-support/bin/native"
   if [[ ! -x "${symlink_farm_root}/.${cargo_versioned}" ]]; then
+    set_rust_toolchain
     (
       cd "${symlink_farm_root}"
 
