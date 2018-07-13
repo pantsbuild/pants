2c69db0935e3ea7cbd90dc7063e87ba04da51d5b
John Sirois <john.sirois@gmail.com>
2018-07-13 03:53:26 -0600

    Hack around PEX not forwarding custom interpreters to PEXEnvironment.

--- src/python/pants/backend/python/tasks/wrapped_pex.py
+++ src/python/pants/backend/python/tasks/wrapped_pex.py
@@ -4,8 +4,12 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import logging
 from builtins import object
 from copy import copy
+
+
+logger = logging.getLogger(__name__)
 
 
 class WrappedPEX(object):
@@ -15,46 +19,53 @@ class WrappedPEX(object):
   """
 
   _PEX_PATH_ENV_VAR_NAME = 'PEX_PATH'
+  _PEX_PYTHON_PATH_ENV_VAR_NAME = 'PEX_PYTHON_PATH'
 
-  # TODO(benjy): I think we can get rid of the interpreter argument.
-  # In all cases it appears to be set to pex.interpreter.
-  def __init__(self, pex, interpreter, extra_pex_paths=None):
+  def __init__(self, pex, extra_pex_paths=None):
     """
     :param pex: The main pex we wrap.
-    :param interpreter: The interpreter the main pex will run on.
     :param extra_pex_paths: Other pexes, to "merge" in via the PEX_PATH mechanism.
     """
     self._pex = pex
-    self._interpreter = interpreter
     self._extra_pex_paths = extra_pex_paths
 
   @property
   def interpreter(self):
-    return self._interpreter
+    return self._pex._interpreter
 
   def path(self):
     return self._pex.path()
 
   def cmdline(self, args=()):
     cmdline = ' '.join(self._pex.cmdline(args))
+
+    def render_env_var(key, value):
+      return '{key}={value}'.format(key=key, value=value)
+
+    env_vars = [(self._PEX_PYTHON_PATH_ENV_VAR_NAME, self._pex._interpreter.binary)]
+
     pex_path = self._pex_path()
     if pex_path:
-      return '{env_var_name}={pex_path} {cmdline}'.format(env_var_name=self._PEX_PATH_ENV_VAR_NAME,
-                                                          pex_path=pex_path,
-                                                          cmdline=cmdline)
-    else:
-      return cmdline
+      env_vars.append((self._PEX_PATH_ENV_VAR_NAME, pex_path))
+
+    return '{execution_control_env_vars} {cmdline}'.format(
+      execution_control_env_vars=' '.join(render_env_var(k, v) for k, v in env_vars),
+      cmdline=cmdline
+    )
 
   def run(self, *args, **kwargs):
+    env = copy(kwargs.pop('env', {}))
+
+    # Hack around bug in PEX where custom interpreters are not forwarded to PEXEnvironments.
+    # TODO(John Sirois): Remove when XXX is fixed.
+    env[self._PEX_PYTHON_PATH_ENV_VAR_NAME] = self._pex._interpreter.binary
+
     pex_path = self._pex_path()
     if pex_path:
-      kwargs_copy = copy(kwargs)
-      env = copy(kwargs_copy.get('env')) if 'env' in kwargs_copy else {}
-      env[self._PEX_PATH_ENV_VAR_NAME] = self._pex_path()
-      kwargs_copy['env'] = env
-      return self._pex.run(*args, **kwargs_copy)
-    else:
-      return self._pex.run(*args, **kwargs)
+      env[self._PEX_PATH_ENV_VAR_NAME] = pex_path
+
+    logger.debug('Executing WrappedPEX using: {}'.format(self.cmdline(args=tuple(*args))))
+    return self._pex.run(*args, env=env, **kwargs)
 
   def _pex_path(self):
     if self._extra_pex_paths:
