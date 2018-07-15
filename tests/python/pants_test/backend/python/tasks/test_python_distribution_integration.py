5bbc660a5901484ad72aecb3abd458d9492c8f85
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-07-14 21:49:02 -0700

    remove unused methods

--- tests/python/pants_test/backend/python/tasks/test_python_distribution_integration.py
+++ tests/python/pants_test/backend/python/tasks/test_python_distribution_integration.py
@@ -8,7 +8,6 @@ import functools
 import glob
 import os
 import re
-from contextlib import contextmanager
 
 from pants.backend.native.config.environment import Platform
 from pants.base.build_environment import get_buildroot
@@ -24,20 +23,6 @@ class PythonDistributionIntegrationTest(PantsRunIntegrationTest):
   fasthello_tests = 'examples/tests/python/example/python_distribution/hello/test_fasthello'
   fasthello_install_requires_dir = 'testprojects/src/python/python_distribution/fasthello_with_install_requires'
   hello_setup_requires = 'examples/src/python/example/python_distribution/hello/setup_requires'
-
-  @staticmethod
-  @contextmanager
-  def caching_config():
-    """Creates a temporary directory and returns a pants configuration for passing to pants_run."""
-    with temporary_dir() as tmp_dir:
-      yield {
-        'cache': {
-          'read': True,
-          'write': True,
-          'read_from': [tmp_dir],
-          'write_to': [tmp_dir]
-        }
-      }
 
   def _assert_native_greeting(self, output):
     self.assertIn('Hello from C!', output)
@@ -72,20 +57,15 @@ class PythonDistributionIntegrationTest(PantsRunIntegrationTest):
       # Check that text was properly printed to stdout.
       self._assert_native_greeting(pants_run.stdout_data)
 
-  @staticmethod
-  def rewrite_hello_c_source(orig_content):
-    return re.sub('"Hello from C!"', '"Hello from C?"', orig_content)
-
   def test_invalidation(self):
     fasthello_run = '{}:main_with_no_conflict'.format(self.fasthello_install_requires_dir)
 
-    with self.caching_config() as config, self.mock_buildroot(
+    with self.mock_buildroot(
         dirs_to_copy=[self.fasthello_install_requires_dir]) as buildroot, buildroot.pushd():
       run_target = functools.partial(
         self.run_pants_with_workdir,
         command=['run', fasthello_run],
         workdir=os.path.join(buildroot.dir, '.pants.d'),
-        config=config,
         build_root=buildroot.dir,
       )
 
