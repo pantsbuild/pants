c62ae87f90113080a96ba9b87ebdb17976024c98
Eric Arellano <ericarellano@me.com>
2018-07-16 16:33:43 -0400

    Port goal package to Python 3 (#6138)
    Part of https://github.com/pantsbuild/pants/issues/6062

--- src/python/pants/goal/artifact_cache_stats.py
+++ src/python/pants/goal/artifact_cache_stats.py
@@ -5,6 +5,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os
+from builtins import object, str, zip
 from collections import defaultdict, namedtuple
 
 from pants.cache.artifact_cache import UnreadableArtifact
