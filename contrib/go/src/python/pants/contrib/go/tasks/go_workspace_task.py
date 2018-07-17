68bbe9135f884e7ee24cb07bdc2d18cd62b0a7f5
John Barry <john.barry@gmail.com>
2018-07-17 18:59:03 -0500

    Port go to py3 (#6139)
    ### Problem
    
    Upgrading go to be compat w/py3
    
    ### Solution
    
    Very few, script handled 100% but I did revert a couple of excess lists.

--- contrib/go/src/python/pants/contrib/go/tasks/go_workspace_task.py
+++ contrib/go/src/python/pants/contrib/go/tasks/go_workspace_task.py
@@ -5,6 +5,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os
+from builtins import zip
 from itertools import chain
 
 from pants.base.build_environment import get_buildroot
