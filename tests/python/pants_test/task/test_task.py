ab33a98da80670e11fbd7525709f35c5d5499f23
Eric Arellano <earellano@foursquare.com>
2018-07-19 18:47:20 -0400

    Port task tests

--- tests/python/pants_test/task/test_task.py
+++ tests/python/pants_test/task/test_task.py
@@ -5,6 +5,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os
+from builtins import str
 
 from pants.base.build_environment import get_buildroot
 from pants.base.exceptions import TaskError
