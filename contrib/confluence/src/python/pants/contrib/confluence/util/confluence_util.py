f31e54b250451cd27f0ca97c1199c25e701ad022
John Barry <john.barry@gmail.com>
2018-07-21 18:38:21 -0500

    futurize confluence (#6115)
    use future idiom. fix urllib
    
    favor xmlrpc.client in import try as it's 2.7 and 3 compat

--- contrib/confluence/src/python/pants/contrib/confluence/util/confluence_util.py
+++ contrib/confluence/src/python/pants/contrib/confluence/util/confluence_util.py
@@ -7,8 +7,10 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import getpass
 import logging
 import mimetypes
-import urllib
+from builtins import object, str
 from os.path import basename
+
+from future.moves.urllib.parse import quote_plus
 
 
 log = logging.getLogger(__name__)
@@ -20,9 +22,9 @@ log = logging.getLogger(__name__)
 """Code to ease publishing text to Confluence wikis."""
 
 try:
-  from xmlrpclib import ServerProxy, Error as XMLRPCError, Binary
-except ImportError:
   from xmlrpc.client import ServerProxy, Error as XMLRPCError, Binary
+except ImportError:
+  from xmlrpclib import ServerProxy, Error as XMLRPCError, Binary
 
 mimetypes.init()
 
@@ -76,7 +78,7 @@ class Confluence(object):
   def get_url(server_url, wiki_space, page_title):
     """ return the url for a confluence page in a given space and with a given
     title. """
-    return '%s/display/%s/%s' % (server_url, wiki_space, urllib.quote_plus(page_title))
+    return '%s/display/%s/%s' % (server_url, wiki_space, quote_plus(page_title))
 
   def logout(self):
     """Terminates the session and connection to the server.
