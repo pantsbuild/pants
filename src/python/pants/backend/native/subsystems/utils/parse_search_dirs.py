900a47673f65cd8f40f8c9e616ad2a6ea3398d87
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-07-23 14:39:26 -0700

    refactor ParseSearchDirs

--- src/python/pants/backend/native/subsystems/utils/parse_search_dirs.py
+++ src/python/pants/backend/native/subsystems/utils/parse_search_dirs.py
@@ -37,24 +37,30 @@ class ParseSearchDirs(Subsystem):
   def _search_dirs_libraries_regex(cls):
     return re.compile('^libraries: =(.*)$', flags=re.MULTILINE)
 
-  def _parse_libraries_from_compiler_search_dirs(self, compiler_exe, env):
-    # This argument is supported by at least gcc and clang.
-    cmd = [compiler_exe, '-print-search-dirs']
-
+  def _invoke_compiler_exe(self, cmd, env):
     try:
       # Get stderr interspersed in the error message too -- this should not affect output parsing.
       compiler_output = subprocess.check_output(cmd, env=env, stderr=subprocess.STDOUT)
     except OSError as e:
       # We use `safe_shlex_join` here to pretty-print the command.
       raise self.ParseSearchDirsError(
-        "Invocation of '{}' with argv '{}' failed."
-        .format(compiler_exe, safe_shlex_join(cmd)),
+        "Process invocation with argv '{}' and environment {!r} failed."
+        .format(safe_shlex_join(cmd), env),
         e)
     except subprocess.CalledProcessError as e:
       raise self.ParseSearchDirsError(
-        "Invocation of '{}' with argv '{}' exited with non-zero code {}. output:\n{}"
-        .format(compiler_exe, safe_shlex_join(cmd), e.returncode, e.output),
+        "Process invocation with argv '{}' and environment {!r} exited with non-zero code {}. "
+        "output:\n{}"
+        .format(safe_shlex_join(cmd), env, e.returncode, e.output),
         e)
+
+    return compiler_output
+
+  def _parse_libraries_from_compiler_search_dirs(self, compiler_exe, env):
+    # This argument is supported by at least gcc and clang.
+    cmd = [compiler_exe, '-print-search-dirs']
+
+    compiler_output = self._invoke_compiler_exe(cmd, env)
 
     libs_line = self._search_dirs_libraries_regex.search(compiler_output)
     if not libs_line:
@@ -63,18 +69,20 @@ class ParseSearchDirs(Subsystem):
         .format(safe_shlex_join(cmd), compiler_output))
     return libs_line.group(1).split(':')
 
+  def _filter_existing_dirs(self, dir_candidates, compiler_exe):
+    real_dirs = OrderedSet()
+
+    for maybe_existing_dir in dir_candidates:
+      # Could use a `seen_dir_paths` set if we want to avoid pinging the fs for duplicate entries.
+      if is_readable_dir(maybe_existing_dir):
+        real_dirs.add(os.path.realpath(maybe_existing_dir))
+      else:
+        logger.debug("non-existent or non-accessible directory at {} while "
+                     "parsing directories from {}"
+                     .format(maybe_existing_dir, compiler_exe))
+
+    return list(real_dirs)
+
   def get_compiler_library_dirs(self, compiler_exe, env=None):
     all_dir_candidates = self._parse_libraries_from_compiler_search_dirs(compiler_exe, env=env)
-
-    real_lib_dirs = OrderedSet()
-
-    for lib_dir_path in all_dir_candidates:
-      # Could use a `seen_dir_paths` set if we want to avoid pinging the fs for duplicate entries.
-      if is_readable_dir(lib_dir_path):
-        real_lib_dirs.add(os.path.realpath(lib_dir_path))
-      else:
-        logger.debug("non-existent or non-accessible program directory at {} while "
-                     "parsing libraries from {}"
-                     .format(lib_dir_path, compiler_exe))
-
-    return list(real_lib_dirs)
+    return self._filter_existing_dirs(all_dir_candidates, compiler_exe)
