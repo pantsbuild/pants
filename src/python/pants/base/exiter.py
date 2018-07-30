d8be11a74e64885b3a383f8a2460aa4dc4b4fa9d
Eric Arellano <earellano@foursquare.com>
2018-07-30 13:28:07 -0400

    Fix exiter expecting unicode

--- src/python/pants/base/exiter.py
+++ src/python/pants/base/exiter.py
@@ -11,9 +11,7 @@ import os
 import signal
 import sys
 import traceback
-from builtins import object
-
-import six
+from builtins import object, str
 
 from pants.util.dirutil import safe_open
 
@@ -72,7 +70,7 @@ class Exiter(object):
     :param out: The file descriptor to emit `msg` to. (Optional)
     """
     if msg:
-      print(msg.encode('utf-8'), file=out or sys.stderr)
+      print(msg, file=out or sys.stderr)
     self._exit(result)
 
   def exit_and_fail(self, msg=None):
@@ -91,7 +89,7 @@ class Exiter(object):
     def format_msg(print_backtrace=True):
       msg = 'Exception caught: ({})\n'.format(type(exc))
       msg += '{}\n'.format(''.join(self._format_tb(tb))) if print_backtrace else '\n'
-      msg += 'Exception message: {}\n'.format(six.text_type(exc) if exc else 'none')
+      msg += 'Exception message: {}\n'.format(str(exc) if exc else 'none')
       msg += '\n' if add_newline else ''
       return msg
 
@@ -107,7 +105,7 @@ class Exiter(object):
           exception_log.write('timestamp: {}\n'.format(datetime.datetime.now().isoformat()))
           exception_log.write('args: {}\n'.format(sys.argv))
           exception_log.write('pid: {}\n'.format(os.getpid()))
-          exception_log.write(msg.encode('utf-8'))
+          exception_log.write(msg)
           exception_log.write('\n')
       except Exception as e:
         # This is all error recovery logic so we catch all exceptions from the logic above because
