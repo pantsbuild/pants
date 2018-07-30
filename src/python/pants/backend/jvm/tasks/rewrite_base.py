75415d45c748c015de667f4d38eb0c7f65c542f3
rkstap <robert@stapenhurst.co.uk>
2018-07-31 00:10:43 +0100

    Add --output-dir flag to ScalaFmt task (#6134)

--- src/python/pants/backend/jvm/tasks/rewrite_base.py
+++ src/python/pants/backend/jvm/tasks/rewrite_base.py
@@ -5,12 +5,16 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os
+import shutil
 from abc import abstractmethod, abstractproperty
 
 from pants.backend.jvm.tasks.nailgun_task import NailgunTask
 from pants.base.build_environment import get_buildroot
+from pants.base.deprecated import deprecated_conditional
 from pants.base.exceptions import TaskError
+from pants.option.custom_types import dir_option
 from pants.process.xargs import Xargs
+from pants.util.dirutil import fast_relpath, safe_mkdir_for_all
 from pants.util.memo import memoized_property
 from pants.util.meta import AbstractClass
 
@@ -25,6 +29,20 @@ class RewriteBase(NailgunTask, AbstractClass):
              default=cls.target_types(),
              advanced=True, type=list,
              help='The target types to apply formatting to.')
+    try:
+      sideeffecting = cls.sideeffecting
+    except AttributeError:
+      deprecated_conditional(
+        lambda: True,
+        '1.12.0.dev0',
+        "RewriteBase's sideeffecting property should be a class property, not an instance property "
+        "but class {} didn't have a class property.".format(cls.__name__),
+      )
+      sideeffecting = False
+    if sideeffecting:
+      register('--output-dir', advanced=True, type=dir_option, fingerprint=True,
+               help='Path to output directory. Any updated files will be written here. '
+               'If not specified, files will be modified in-place.')
 
   @classmethod
   def target_types(cls):
@@ -65,14 +83,27 @@ class RewriteBase(NailgunTask, AbstractClass):
     if not target_sources:
       return
 
-    result = Xargs(self._invoke_tool_in_place).execute(target_sources)
+    result = Xargs(self._invoke_tool).execute(target_sources)
     if result != 0:
       raise TaskError('{} is improperly implemented: a failed process '
                       'should raise an exception earlier.'.format(type(self).__name__))
 
-  def _invoke_tool_in_place(self, target_sources):
-    # Invoke in place.
-    result = self.invoke_tool(get_buildroot(), target_sources)
+  def _invoke_tool(self, target_sources):
+    buildroot = get_buildroot()
+    toolroot = buildroot
+    if self.sideeffecting and self.get_options().output_dir:
+      toolroot = self.get_options().output_dir
+      new_sources = [
+        (target, os.path.join(toolroot, fast_relpath(source, buildroot)))
+        for target, source in target_sources
+      ]
+      old_file_paths = [source for _, source in target_sources]
+      new_file_paths = [source for _, source in new_sources]
+      safe_mkdir_for_all(new_file_paths)
+      for old, new in zip(old_file_paths, new_file_paths):
+        shutil.copyfile(old, new)
+      target_sources = new_sources
+    result = self.invoke_tool(toolroot, target_sources)
     self.process_result(result)
     return result
 
