ad7a628cee44b98fc37e403effe9c6c2020ee1bd
Eric Arellano <earellano@foursquare.com>
2018-08-01 19:09:07 -0400

    Add open backport to invalidation

--- tests/python/pants_test/invalidation/test_strict_deps_invalidation_integration.py
+++ tests/python/pants_test/invalidation/test_strict_deps_invalidation_integration.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os
 import shutil
+from builtins import open
 
 from pants.base.build_environment import get_buildroot
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
