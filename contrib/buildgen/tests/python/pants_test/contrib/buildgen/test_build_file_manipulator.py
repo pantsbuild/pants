47f1ed93830487ad956b053dc7396ec56017b2d8
Eric Arellano <earellano@foursquare.com>
2018-08-02 18:01:24 -0400

    Add open backport to contrib buildgen

--- contrib/buildgen/tests/python/pants_test/contrib/buildgen/test_build_file_manipulator.py
+++ contrib/buildgen/tests/python/pants_test/contrib/buildgen/test_build_file_manipulator.py
@@ -4,6 +4,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from builtins import open
 from textwrap import dedent
 
 from pants.build_graph.address import Address
