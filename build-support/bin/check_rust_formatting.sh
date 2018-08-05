3ea2a07452e94e4cd63bfb0f690df290ae222953
Daniel Wagner-Hall <dawagner@gmail.com>
2018-08-05 08:06:25 -0700

    Update rustfmt to new flags

--- build-support/bin/check_rust_formatting.sh
+++ build-support/bin/check_rust_formatting.sh
@@ -15,7 +15,7 @@ function usage() {
   fi
 }
 
-write_mode=check
+check="true"
 
 while getopts "hf" opt; do
   case ${opt} in
@@ -24,7 +24,7 @@ while getopts "hf" opt; do
       exit 0
       ;;
     f)
-      write_mode=overwrite
+      check="false"
       ;;
     *)
       usage "Unrecognized arguments."
@@ -38,6 +38,9 @@ NATIVE_ROOT="${REPO_ROOT}/src/rust/engine"
 cmd=(
   ${REPO_ROOT}/build-support/bin/native/cargo fmt --all --
 )
+if [[ "${check}" == "true" ]]; then
+  cmd=("${cmd[@]}" "--check")
+fi
 
 bad_files=(
   $(
@@ -48,7 +51,7 @@ bad_files=(
     echo >&2 "Ensuring generated code is present for downstream formatting checks..."
     ${REPO_ROOT}/build-support/bin/native/cargo check -p bazel_protos
 
-    ${cmd[*]} --write-mode=${write_mode} | \
+    ${cmd[@]} | \
       awk '$0 ~ /^Diff in/ {print $3}' | \
       sort -u
     exit ${PIPESTATUS[0]}
@@ -57,7 +60,7 @@ bad_files=(
 exit_code=$?
 
 if [[ ${exit_code} -ne 0 ]]; then
-  if [[ "${write_mode}" == "check" ]]; then
+  if [[ "${check}" == "true" ]]; then
     echo >&2 "The following rust files were incorrectly formatted, run \`$0 -f\` to reformat them:"
     for bad_file in ${bad_files[*]}; do
       echo >&2 ${bad_file}
@@ -65,10 +68,10 @@ if [[ ${exit_code} -ne 0 ]]; then
   else
     cat << EOF >&2
 An error occurred while checking the formatting of rust files.
-Try running \`(cd "${NATIVE_ROOT}" && ${cmd[*]} --write-mode=${write_mode})\` to investigate.
+Try running \`(cd "${NATIVE_ROOT}" && ${cmd[@]})\` to investigate.
 Its error is:
 EOF
-    cd "${NATIVE_ROOT}" && ${cmd[*]} --write-mode=${write_mode} >/dev/null
+    cd "${NATIVE_ROOT}" && ${cmd[@]} >/dev/null
   fi
   exit 1
 fi
