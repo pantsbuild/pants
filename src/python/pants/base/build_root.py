051ecb21bf28cc98a49b571e23c2ea8f9249d9da
Daniel Wagner-Hall <dwagnerhall@twitter.com>
2018-08-07 21:09:27 -0700

    get_buildroot() always returns a string

--- src/python/pants/base/build_root.py
+++ src/python/pants/base/build_root.py
@@ -7,6 +7,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 from contextlib import contextmanager
 
+from future.utils import PY2
+
 from pants.util.meta import Singleton
 
 
@@ -53,6 +55,8 @@ class BuildRoot(Singleton):
         self._root_dir = override_buildroot
       else:
         self._root_dir = os.path.realpath(self.find_buildroot())
+      if PY2:
+        self._root_dir = self._root_dir.decode('utf-8')
     return self._root_dir
 
   @path.setter
