8dd77166dfdcc8b12d9fb8e67d375bc66bdecd31
Jameson Haller <jhaller@twitter.com>
2018-08-10 16:11:36 -0700

    Update default Node.js and Yarnpkg to currently used versions

--- contrib/node/src/python/pants/contrib/node/subsystems/yarnpkg_distribution.py
+++ contrib/node/src/python/pants/contrib/node/subsystems/yarnpkg_distribution.py
@@ -17,5 +17,5 @@ class YarnpkgDistribution(NativeTool):
 
   options_scope = 'yarnpkg-distribution'
   name = 'yarnpkg'
-  default_version = 'v0.19.1'
+  default_version = 'v1.6.0'
   archive_type = 'tgz'
