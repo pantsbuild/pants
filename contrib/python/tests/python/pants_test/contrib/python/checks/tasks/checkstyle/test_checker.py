8b78fbe807055ec7c1653af292cb9d92491b6ca1
Eric Arellano <earellano@foursquare.com>
2018-08-12 16:18:30 -0700

    Replace test_checker's example of print statements to bad class name.
    Python 3 raises a syntax error upon print statements, so the tests don't work as expected. Instead, bad class variable names works consistently across both Py2 and Py3.

--- contrib/python/tests/python/pants_test/contrib/python/checks/tasks/checkstyle/test_checker.py
+++ contrib/python/tests/python/pants_test/contrib/python/checks/tasks/checkstyle/test_checker.py
@@ -12,8 +12,8 @@ from pants.base.exceptions import TaskError
 from pants_test.backend.python.tasks.python_task_test_base import PythonTaskTestBase
 
 from pants.contrib.python.checks.tasks.checkstyle.checker import PythonCheckStyleTask
-from pants.contrib.python.checks.tasks.checkstyle.print_statements_subsystem import \
-  PrintStatementsSubsystem
+from pants.contrib.python.checks.tasks.checkstyle.variable_names_subsystem import \
+  VariableNamesSubsystem
 
 
 class PythonCheckStyleTaskTest(PythonTaskTestBase):
@@ -24,7 +24,7 @@ class PythonCheckStyleTaskTest(PythonTaskTestBase):
   def setUp(self):
     super(PythonCheckStyleTaskTest, self).setUp()
     PythonCheckStyleTask.clear_plugins()
-    PythonCheckStyleTask.register_plugin('print-statements', PrintStatementsSubsystem)
+    PythonCheckStyleTask.register_plugin('variable-names', VariableNamesSubsystem)
 
   def tearDown(self):
     super(PythonCheckStyleTaskTest, self).tearDown()
@@ -36,7 +36,8 @@ class PythonCheckStyleTaskTest(PythonTaskTestBase):
 
   def test_pass(self):
     self.create_file('a/python/pass.py', contents=dedent("""
-                       print('Print is a function')
+                       class UpperCase: 
+                         pass
                      """))
     target = self.make_target('a/python:pass', PythonLibrary, sources=['pass.py'])
     context = self.context(target_roots=[target])
@@ -45,7 +46,8 @@ class PythonCheckStyleTaskTest(PythonTaskTestBase):
 
   def test_failure(self):
     self.create_file('a/python/fail.py', contents=dedent("""
-                         print 'Print should not be used as a statement'
+                        class lower_case:
+                          pass
                        """))
     target = self.make_target('a/python:fail', PythonLibrary, sources=['fail.py'])
     context = self.context(target_roots=[target])
@@ -56,20 +58,21 @@ class PythonCheckStyleTaskTest(PythonTaskTestBase):
 
   def test_suppressed_file_passes(self):
     self.create_file('a/python/fail.py', contents=dedent("""
-                         print 'Print should not be used as a statement'
+                        class lower_case:
+                          pass
                        """))
     suppression_file = self.create_file('suppress.txt', contents=dedent("""
-    a/python/fail\.py::print-statements"""))
+    a/python/fail\.py::variable-names"""))
     target = self.make_target('a/python:fail', PythonLibrary, sources=['fail.py'])
     self.set_options(suppress=suppression_file)
     context = self.context(target_roots=[target], )
     task = self.create_task(context)
-
     self.assertEqual(0, task.execute())
 
   def test_failure_fail_false(self):
     self.create_file('a/python/fail.py', contents=dedent("""
-                       print 'Print should not be used as a statement'
+                        class lower_case:
+                          pass
                      """))
     target = self.make_target('a/python:fail', PythonLibrary, sources=['fail.py'])
     self.set_options(fail=False)
@@ -90,7 +93,8 @@ class PythonCheckStyleTaskTest(PythonTaskTestBase):
 
   def test_failure_print_nit(self):
     self.create_file('a/python/fail.py', contents=dedent("""
-                         print 'Print should not be used as a statement'
+                        class lower_case:
+                          pass
                        """))
     target = self.make_target('a/python:fail', PythonLibrary, sources=['fail.py'])
     context = self.context(target_roots=[target])
@@ -100,8 +104,8 @@ class PythonCheckStyleTaskTest(PythonTaskTestBase):
 
     self.assertEqual(1, len(nits))
     self.assertEqual(
-      """T607:ERROR   a/python/fail.py:002 Print used as a statement.\n"""
-      """     |print 'Print should not be used as a statement'""",
+      """T000:ERROR   a/python/fail.py:002 Classes must be UpperCamelCased\n"""
+      """     |class lower_case:""",
       str(nits[0]))
 
   def test_syntax_error_nit(self):
@@ -121,21 +125,3 @@ class PythonCheckStyleTaskTest(PythonTaskTestBase):
                      """     |invalid python\n"""
                      """     |""",
                      str(nits[0]))
-
-  def test_multiline_nit_printed_only_once(self):
-    self.create_file('a/python/fail.py', contents=dedent("""
-                         print ('Multi'
-                                'line') + 'expression'
-                       """))
-    target = self.make_target('a/python:fail', PythonLibrary, sources=['fail.py'])
-    context = self.context(target_roots=[target])
-    task = self.create_task(context)
-
-    nits = list(task.get_nits('a/python/fail.py'))
-
-    self.assertEqual(1, len(nits))
-    self.assertEqual(
-      """T607:ERROR   a/python/fail.py:002-003 Print used as a statement.\n"""
-      """     |print ('Multi'\n"""
-      """     |       'line') + 'expression'""",
-      str(nits[0]))
