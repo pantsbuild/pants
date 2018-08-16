66081a23d02f02937ec2681456ae1e596dc28c4e
Ny Saechao <nsaechao@twitter.com>
2018-08-15 19:16:46 -0700

    Add additional test cases for python_binary build option

--- tests/python/pants_test/python/test_python_binary_integration.py
+++ tests/python/pants_test/python/test_python_binary_integration.py
@@ -8,6 +8,7 @@ import functools
 import os
 from contextlib import contextmanager
 
+from parameterized import parameterized
 from pex.pex_info import PexInfo
 
 from pants.util.contextutil import temporary_dir
@@ -67,7 +68,14 @@ class PythonBinaryIntegrationTest(PantsRunIntegrationTest):
       self.assert_success(build())
       self.assert_pex_attribute(test_pex, 'zip_safe', True)
 
-  def test_allow_sdist_builds_fails(self):
+  @parameterized.expand([
+    (True, True, True),
+    (True, False, False),
+    (False, False, False),
+    # If allow_sdist_builds is false, resolve will fail before attempting to build binary
+    (False, True, False)
+  ])
+  def test_allow_sdist_builds(self, allow_sdist_builds, allow_build, expect_success):
     test_project = 'testprojects/src/python/sdist_builds'
     test_build = os.path.join(test_project, 'BUILD')
     test_src = os.path.join(test_project, 'main.py')
@@ -75,13 +83,12 @@ class PythonBinaryIntegrationTest(PantsRunIntegrationTest):
     with self.caching_config() as config, self.mock_buildroot() as buildroot, buildroot.pushd():
       build = functools.partial(
         self.run_pants_with_workdir,
-        # TODO(6352): Remove clean-all when python_binary cache invalidation respects python_setup options
-        command=['clean-all', 'binary', '--python-setup-allow-sdist-builds=False',
-                 '--python-setup-resolver-cache-dir="%s"' % os.path.join(buildroot.dir, 'python_cache/requirements'),
+        command=['clean-all', 'binary', '--python-setup-allow-sdist-builds=%s' % allow_sdist_builds,
+                 '--python-setup-resolver-cache-dir="%s"' % os.path.join(buildroot.new_buildroot, 'python_cache/requirements'),
                  test_project],
-        workdir=os.path.join(buildroot.dir, '.pants.d'),
+        workdir=os.path.join(buildroot.new_buildroot, '.pants.d'),
         config=config,
-        build_root=buildroot.dir
+        build_root=buildroot.new_buildroot
       )
 
       buildroot.write_file(test_src, '')
@@ -92,6 +99,7 @@ class PythonBinaryIntegrationTest(PantsRunIntegrationTest):
         '''
 python_binary(
   sources='main.py',
+  build={allow_build},
   dependencies=[':pyhelloworld3'],
 )
 
@@ -100,40 +108,8 @@ python_requirement_library(
     requirements = [
         python_requirement("pyhelloworld3==1.0.0"),
     ],
-)''')
-      self.assert_failure(build())
-
-  def test_allow_sdist_builds_succeeds(self):
-    test_project = 'testprojects/src/python/sdist_builds'
-    test_build = os.path.join(test_project, 'BUILD')
-    test_src = os.path.join(test_project, 'main.py')
-
-    with self.caching_config() as config, self.mock_buildroot() as buildroot, buildroot.pushd():
-      build = functools.partial(
-        self.run_pants_with_workdir,
-        command=['binary', '--python-setup-allow-sdist-builds=True',
-                 '--python-setup-resolver-cache-dir="%s"' % os.path.join(buildroot.dir, 'python_cache/requirements'),
-                 test_project],
-        workdir=os.path.join(buildroot.dir, '.pants.d'),
-        config=config,
-        build_root=buildroot.dir
-      )
-
-      buildroot.write_file(test_src, '')
-
-      # Create a pex from a simple python_binary target and assert it has zip_safe=True (default).
-      buildroot.write_file(
-        test_build,
-        '''
-python_binary(
-  sources='main.py',
-  dependencies=[':pyhelloworld3'],
-)
-
-python_requirement_library(
-    name = "pyhelloworld3",
-    requirements = [
-        python_requirement("pyhelloworld3==1.0.0"),
-    ],
-)''')
-      self.assert_success(build())
+)'''.format(allow_build=allow_build))
+      if expect_success:
+        self.assert_success(build())
+      else:
+        self.assert_failure(build())
