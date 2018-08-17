e21b71b7e8f4134ed91c52186992cf0838c736cd
John Sirois <john.sirois@gmail.com>
2018-08-17 08:59:40 -0600

    Add an environment marker to `pants_requirement`.
    This properly restricts the dependency to environments pants can
    actually run in.
    
    Fixes #6358

--- tests/python/pants_test/backend/python/test_pants_requirement.py
+++ tests/python/pants_test/backend/python/test_pants_requirement.py
@@ -19,9 +19,23 @@ class PantsRequirementTest(TestBase):
 
   def assert_pants_requirement(self, python_requirement_library):
     self.assertIsInstance(python_requirement_library, PythonRequirementLibrary)
-    pants_requirement = PythonRequirement('pantsbuild.pants=={}'.format(pants_version()))
-    self.assertEqual([pants_requirement.requirement],
-                     list(pr.requirement for pr in python_requirement_library.payload.requirements))
+    expected = PythonRequirement('pantsbuild.pants=={}'.format(pants_version()))
+
+    def key(python_requirement):
+      return (python_requirement.requirement.key,
+              python_requirement.requirement.specs,
+              python_requirement.requirement.extras)
+
+    self.assertEqual([key(expected)],
+                     [key(pr) for pr in python_requirement_library.payload.requirements])
+
+    req = list(python_requirement_library.payload.requirements)[0]
+    self.assertIsNotNone(req.requirement.marker)
+    self.assertTrue(req.requirement.marker.evaluate(),
+                    'pants_requirement() should always work in the current test environment')
+    self.assertFalse(req.requirement.marker.evaluate({'python_version': '3.5'}))
+    self.assertFalse(req.requirement.marker.evaluate({'python_version': '2.6'}))
+    self.assertTrue(req.requirement.marker.evaluate({'python_version': '2.7'}))
 
   def test_default_name(self):
     self.add_to_build_file('3rdparty/python/pants', 'pants_requirement()')
