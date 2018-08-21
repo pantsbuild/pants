f07c063abb46c185e6c965e3339a185c40580202
John Sirois <john.sirois@gmail.com>
2018-08-20 20:07:00 -0600

    Stabilize test case sorting in suites. (#6371)
    Fixes #6370

--- src/python/pants/backend/jvm/tasks/reports/junit_html_report.py
+++ src/python/pants/backend/jvm/tasks/reports/junit_html_report.py
@@ -101,7 +101,7 @@ class ReportTestSuite(object):
     self.failures = int(failures)
     self.skipped = int(skipped)
     self.time = float(time)
-    self.testcases = testcases
+    self.testcases = sorted(testcases)  # Ensures a consistent reporting order.
     self.file = file
 
   def __lt__(self, other):
@@ -236,7 +236,7 @@ class JUnitHtmlReport(JUnitHtmlReportInterface):
     root = ET.parse(xml_file).getroot()
 
     testcases = []
-    for testcase in sorted(root.iter('testcase'), key=lambda tc: tc.attrib['name']):
+    for testcase in root.iter('testcase'):
       failure = None
       for f in testcase.iter('failure'):
         failure = f.text
