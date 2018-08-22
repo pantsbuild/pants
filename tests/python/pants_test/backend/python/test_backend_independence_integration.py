cba7fa74a63a43e0291e67fa7266d2b4f6ec1e30
Benjy Weinberger <benjyw@gmail.com>
2018-08-22 11:37:20 -0700

    Fix lint issues.

--- tests/python/pants_test/backend/python/test_backend_independence_integration.py
+++ tests/python/pants_test/backend/python/test_backend_independence_integration.py
@@ -9,6 +9,7 @@ from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 class BackendIndependenceTest(PantsRunIntegrationTest):
   """Verifies that this backend works with no other backends present."""
+
   @classmethod
   def hermetic(cls):
     return True
