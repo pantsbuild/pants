c0a64d092242ac0b15b138726d654a9ef5b8ab8c
Chris Livingston <clivingston45@gmail.com>
2018-08-27 21:05:07 -0700

    Override interpreter constraints if global option is passed down (#6387)
    I commandeered @alanbato's PR at https://github.com/pantsbuild/pants/pull/6250
    
    ## Problem
    The original issue is documented at #6081, an attempt to solve this was #6234, but we've decided to take another path instead.
    
    ## Solution
    Instead of relying on interpreter constraints when building a pex in the repl task, we're checking if the --python-setup-interpreter-constraints is flagged. If so, it overrides the interpreter filters at interpreter selection time.
    
    ## Result
    Users should now be able to further filter the python interpreter to use even if the task does not declare an option to do so (like in the repl task)

--- src/python/pants/backend/python/interpreter_cache.py
+++ src/python/pants/backend/python/interpreter_cache.py
@@ -77,6 +77,7 @@ class PythonInterpreterCache(object):
     """Pick an interpreter compatible with all the specified targets."""
     tgts_by_compatibilities = defaultdict(list)
     filters = set()
+
     for target in targets:
       if isinstance(target, PythonTarget):
         c = self._python_setup.compatibility_or_constraints(target)
@@ -86,7 +87,7 @@ class PythonInterpreterCache(object):
     allowed_interpreters = set(self.setup(filters=filters))
 
     # Constrain allowed_interpreters based on each target's compatibility requirements.
-    for compatibility in tgts_by_compatibilities.keys():
+    for compatibility in tgts_by_compatibilities:
       compatible_with_target = set(self._matching(allowed_interpreters, compatibility))
       allowed_interpreters &= compatible_with_target
 
