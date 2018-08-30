bca8a911780c6798f0fa1eb306cdfb41f5505133
Ny Saechao <nsaechao@twitter.com>
2018-08-30 10:57:57 -0700

    nop to re-run travis ci

--- contrib/node/tests/python/pants_test/contrib/node/tasks/test_node_install_integration.py
+++ contrib/node/tests/python/pants_test/contrib/node/tasks/test_node_install_integration.py
@@ -19,6 +19,7 @@ class NodeInstallIntegrationTest(PantsRunIntegrationTest):
 
   def test_node_install_preinstalled_node_module_project(self):
     command = ['node-install',
+               # Use same install parameters as resolve.node in .pants.d, I.E. don't generate a new yarn.lock file
                '--npm-resolver-force-option-override=True',
                'contrib/node/examples/src/node/preinstalled-project:unit']
     pants_run = self.run_pants(command=command)
