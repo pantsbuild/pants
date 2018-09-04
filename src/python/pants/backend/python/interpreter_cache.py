232513b6b9bd2b56ce60193018e55bce399ed899
Mathieu <mathieu.c.sabourin@gmail.com>
2018-09-04 16:38:06 -0400

    [fix] Pass full path to isdir rather than just basename.

--- src/python/pants/backend/python/interpreter_cache.py
+++ src/python/pants/backend/python/interpreter_cache.py
@@ -122,8 +122,8 @@ class PythonInterpreterCache(object):
   def _setup_cached(self, filters):
     """Find all currently-cached interpreters."""
     for interpreter_dir in os.listdir(self._cache_dir):
-      if os.path.isdir(interpreter_dir):
-        path = os.path.join(self._cache_dir, interpreter_dir)
+      path = os.path.join(self._cache_dir, interpreter_dir)
+      if os.path.isdir(path):
         pi = self._interpreter_from_path(path, filters)
         if pi:
           self._logger('Detected interpreter {}: {}'.format(pi.binary, str(pi.identity)))
