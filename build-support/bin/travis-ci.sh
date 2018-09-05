b4675fe244d89174f6b0d46b91d99059c9818deb
John Sirois <john.sirois@gmail.com>
2018-09-05 17:00:25 -0600

    Document the travis-ci.sh script.

--- build-support/bin/travis-ci.sh
+++ build-support/bin/travis-ci.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+# TravisCI-specific environment fixups can live in this script which forwards to the generic ci
+# script post fixups.
 
 set -euo pipefail
 
@@ -11,6 +16,10 @@ function pyenv_path {
   done
 }
 
+# TravisCI uses pyenv to provide some interpreters pre-installed for images. Unfortunately it places
+# `~/.pyenv/shims` on the PATH and these shims are broken (see:
+# https://github.com/travis-ci/travis-ci/issues/8363). We work around this by placing
+# `~/.pyenv/versions/<version>/bin` dirs on the PATH ahead of the broken shims.
 if which pyenv &>/dev/null; then
   PYENV_PYTHONS="$(pyenv_path)"
   PYENV_PATH="$(echo ${PYENV_PYTHONS} | tr ' ' ':')"
@@ -19,4 +28,5 @@ if which pyenv &>/dev/null; then
 
   export PATH="${PYENV_PATH}:${PATH}"
 fi
+
 exec ./build-support/bin/ci.sh "$@"
