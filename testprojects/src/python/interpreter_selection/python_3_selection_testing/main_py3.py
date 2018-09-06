be8515edbdf1b24247c787b6f1df9fcde11fbe1c
John Sirois <john.sirois@gmail.com>
2018-09-06 07:27:22 -0600

    Fixup tests involving pexrc. (#6446)
    Previously these tests weren't reliably reading a pexrc and had fragile
    workarounds and paper-overs as a result. pexrc_util now reliably injects
    a pexrc with a temporary HOME.
    
    Also cleanup the affected tests - use @skipIf, respect 100 cols, ensure
    path comparisons with judicious application of `os.realpath`.

--- testprojects/src/python/interpreter_selection/python_3_selection_testing/main_py3.py
+++ testprojects/src/python/interpreter_selection/python_3_selection_testing/main_py3.py
@@ -2,6 +2,7 @@
 # Copyright 2017 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import os
 import sys
 
 from interpreter_selection.python_3_selection_testing.lib_py3 import say_something
@@ -12,7 +13,7 @@ from interpreter_selection.python_3_selection_testing.lib_py3 import say_somethi
 
 def main():
   v = sys.version_info
-  print(sys.executable)
+  print(os.path.realpath(sys.executable))
   print('%d.%d.%d' % v[0:3])
   return say_something()
 
