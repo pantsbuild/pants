b1dd7c8efbfeb9d2162193150f9e7219f8c2fb4a
John Sirois <john.sirois@gmail.com>
2018-10-11 16:46:33 -0400

    Introduce factory_dict.
    This generates values from keys when no entry is present, a feature
    you might guess collections.defaultdict would support by passing keys
    through to default_factory functions.
    
    Update products to use this.
    PR #6618 will also pull it in or else it will be used in a follow-up.

--- tests/python/pants_test/util/test_collections.py
+++ tests/python/pants_test/util/test_collections.py
@@ -7,7 +7,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import unittest
 from builtins import str
 
-from pants.util.collections import assert_single_element, combined_dict, recursively_update
+from pants.util.collections import (assert_single_element, combined_dict, factory_dict,
+                                    recursively_update)
 
 
 class TestCollections(unittest.TestCase):
@@ -20,6 +21,21 @@ class TestCollections(unittest.TestCase):
       ),
       {'a': 1, 'b': 2, 'c': 3}
     )
+
+  def test_factory_dict(self):
+    cubes = factory_dict(lambda x: x ** 3, ((x, x ** 2) for x in range(3)), three=42)
+    self.assertEqual(0, cubes[0])
+    self.assertEqual(1, cubes[1])
+    self.assertEqual(4, cubes[2])
+
+    self.assertEqual(27, cubes[3])
+
+    self.assertIsNone(cubes.get(4))
+    self.assertEqual(64, cubes[4])
+
+    self.assertEqual(42, cubes['three'])
+
+    self.assertEqual('jake', cubes.get(5, 'jake'))
 
   def test_recursively_update(self):
     d = {'a': 1, 'b': {'c': 2, 'o': 'z'}, 'z': {'y': 0}}
