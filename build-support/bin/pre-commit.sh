1fa424d1e6194599a900b85870017c67f81d7cf4
Chris Livingston <clivingston@twitter.com>
2018-10-15 13:48:13 -0700

    Do not quiet pants lint pre-commit hook

--- build-support/bin/pre-commit.sh
+++ build-support/bin/pre-commit.sh
@@ -19,7 +19,7 @@ $(git rev-parse --verify master > /dev/null 2>&1)
 if [[ $? -eq 0 ]]; then
   echo "* Checking imports" && ./build-support/bin/isort.sh || \
     die "To fix import sort order, run \`\"$(pwd)/build-support/bin/isort.sh\" -f\`"
-  echo "* Checking lint" && ./pants -q --exclude-target-regexp='testprojects/.*' --changed-parent=master lint || exit 1
+  echo "* Checking lint" && ./pants --exclude-target-regexp='testprojects/.*' --changed-parent=master lint || exit 1
 else
   # When travis builds a tag, it does so in a shallow clone without master fetched, which
   # fails in pants changed.
