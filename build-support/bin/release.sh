db576599b17aaee7d96ee62c80444f168333eedb
Yi Cheng <wisechengyi@gmail.com>
2018-10-22 17:05:12 -0700

    convert bash to python in release.sh

--- build-support/bin/release.sh
+++ build-support/bin/release.sh
@@ -440,25 +440,6 @@ function list_prebuilt_wheels() {
     --deploy-pants-wheels-path=${DEPLOY_PANTS_WHEELS_PATH} \
     --deploy-3rdparty-wheels-path=${DEPLOY_3RDPARTY_WHEELS_PATH}
   popd
-#   # List prebuilt wheels as tab-separated tuples of filename and URL-encoded name.
-#   wheel_listing="$(mktemp -t pants.wheels.XXXXX)"
-#   trap "rm -f ${wheel_listing}" RETURN
-
-#   for wheels_path in "${DEPLOY_PANTS_WHEELS_PATH}" "${DEPLOY_3RDPARTY_WHEELS_PATH}"; do
-#     curl -sSL "${BINARY_BASE_URL}/?prefix=${wheels_path}" > "${wheel_listing}"
-#     "${PY}" << EOF
-# from __future__ import print_function
-# import sys
-# import urllib
-# import xml.etree.ElementTree as ET
-# root = ET.parse("${wheel_listing}")
-# ns = {'s3': 'http://s3.amazonaws.com/doc/2006-03-01/'}
-# for key in root.findall('s3:Contents/s3:Key', ns):
-#   # Because filenames may contain characters that have different meanings
-#   # in URLs (namely '+'), # print the key both as url-encoded and as a file path.
-#   print('{}\t{}'.format(key.text, urllib.quote_plus(key.text)))
-# EOF
-#  done
 }
 
 function fetch_prebuilt_wheels() {
@@ -493,8 +474,6 @@ function fetch_and_check_prebuilt_wheels() {
 
   banner "Checking prebuilt wheels for ${PANTS_UNSTABLE_VERSION}"
   fetch_prebuilt_wheels "${check_dir}"
-
-  exit 1
 
   local missing=()
   for PACKAGE in "${RELEASE_PACKAGES[@]}"
--- src/python/pants/releases/release.py
+++ src/python/pants/releases/release.py
@@ -44,41 +44,23 @@ class Releaser(object):
       resp = requests.get(url, allow_redirects=True, auth=None)
       if resp.status_code != 200:
         raise requests.exceptions.HTTPError("HTTP GET {} failed. You might want to remove invalid entries in ~/.netrc (e.g. `default`)".format(url))
-
       wheel_listing = resp.content
-      # wheel_listing = subprocess.check_output(['curl', url])
 
       root = ET.fromstring(wheel_listing)
       ns = {'s3': 'http://s3.amazonaws.com/doc/2006-03-01/'}
-
-
       for key in root.findall('s3:Contents/s3:Key', ns):
         # Because filenames may contain characters that have different meanings
         # in URLs (namely '+'), # print the key both as url-encoded and as a file path.
         keys.append('{}\t{}'.format(key.text, urllib.quote_plus(key.text)))
 
-    return keys
-      # with temporary_file() as file_path:
-      #   fetcher = Fetcher(os.getcwd())
-      #   checksummer = fetcher.ChecksumListener(digest=hashlib.sha1())
-      #   try:
-      #     logger.info('\nDownloading {}'.format(url))
-      #     fetcher.download(url,
-      #                      listener=fetcher.ProgressListener().wrap(checksummer),
-      #                      path_or_fd=file_path,
-      #                      timeout_secs=5)
-      #     logger.info('sha1: {}'.format(checksummer.checksum))
-      #   except fetcher.Error as e:
-      #     raise fetcher.Error('Failed to download: {}'.format(e))
-
-
+    return '\n'.join(keys)
 
   def fetch_prebuilt_wheels(self, binary_base_url, pants_unstable_version):
     pass
 
-
   def fetch_and_check_prebuilt_wheels(self, deploy_dir):
-    print(deploy_dir)
+    pass
+
 
 if __name__ == '__main__':
   fire.Fire(Releaser)
