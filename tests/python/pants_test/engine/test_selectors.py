c86a0c8b5dd9b37ca082fc68b16e472c9dc58a1c
Dorothy Ordogh <dordogh@twitter.com>
2018-11-05 18:55:05 -0800

    Add test to assert that type validaiton works for Get

--- tests/python/pants_test/engine/test_selectors.py
+++ tests/python/pants_test/engine/test_selectors.py
@@ -7,10 +7,18 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import unittest
 from builtins import object
 
-from pants.engine.selectors import Select
+from pants.engine.selectors import Get, Select
 
 
 class AClass(object):
+  pass
+
+
+class BClass(object):
+  pass
+
+
+class SubBClass(BClass):
   pass
 
 
@@ -21,3 +29,10 @@ class SelectorsTest(unittest.TestCase):
 
   def assert_repr(self, expected, selector):
     self.assertEqual(expected, repr(selector))
+
+
+class GetTest(unittest.TestCase):
+  def test_get(self):
+    sub_b = SubBClass()
+    with self.assertRaises(TypeError):
+      Get(AClass, BClass, sub_b)
