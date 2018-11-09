d02e9822fdb87f74d653940dc6b31c81bc3d87d9
Eric Arellano <ericarellano@me.com>
2018-11-09 15:01:15 -0700

    Grammar fix

--- build-support/bin/check_banned_imports.sh
+++ build-support/bin/check_banned_imports.sh
@@ -5,7 +5,7 @@ RUST_FILES="$(find src/rust/engine -name '*.rs')"
 
 bad_files="$(echo ${PYTHON_FILES} | xargs grep -l "^import subprocess")"
 if [ -n "${bad_files}" ]; then
-    echo >&2 "Found forbidden imports. Instead of \`import subprocess\` you should \`from pants.util.process_handler import subprocess\`. Bad files:"
+    echo >&2 "Found forbidden imports. Instead of \`import subprocess\` you should use \`from pants.util.process_handler import subprocess\`. Bad files:"
     echo >&2 "${bad_files}"
     exit 1
 fi
@@ -13,7 +13,7 @@ fi
 # bad_files="$(echo ${PYTHON_FILES} | grep -v "src/python/pants/util/collections_backport.py" | xargs grep -l "^import collections\|^from collections import")"
 bad_files="$(echo ${PYTHON_FILES} | xargs grep -l "^import collections\|^from collections import")"
 if [ -n "${bad_files}" ]; then
-    echo >&2 "Found forbidden imports. Instead of \`import collections\` you should \`from pants.util import collections_backport\`. Bad files:"
+    echo >&2 "Found forbidden imports. Instead of \`import collections\` you should use \`from pants.util import collections_backport\`. Bad files:"
     echo >&2 "${bad_files}"
     exit 1
 fi
