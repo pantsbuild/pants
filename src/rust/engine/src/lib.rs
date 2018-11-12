4fd0d1509247526e183bd211ab811648939dbeb2
Daniel Wagner-Hall <dawagner@gmail.com>
2018-11-12 20:44:23 +0000

    Switch from enum_primitive to num_enum (#6756)
    It's cleaner.

--- src/rust/engine/src/lib.rs
+++ src/rust/engine/src/lib.rs
@@ -49,7 +49,6 @@ mod types;
 
 extern crate boxfuture;
 extern crate bytes;
-extern crate enum_primitive;
 extern crate fnv;
 extern crate fs;
 extern crate futures;
@@ -58,6 +57,7 @@ extern crate hashing;
 extern crate itertools;
 extern crate lazy_static;
 extern crate log;
+extern crate num_enum;
 extern crate parking_lot;
 extern crate process_execution;
 extern crate reqwest;
