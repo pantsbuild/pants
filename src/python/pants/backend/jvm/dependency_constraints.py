bd3cd5f2a14aa46a3e13f3ec6b0660decddcaa89
Borja Lorente <bescobar@twitter.com>
2018-11-13 17:14:49 -0800

    Integration tests and a heavy refactor

--- src/python/pants/backend/jvm/dependency_constraints.py
+++ src/python/pants/backend/jvm/dependency_constraints.py
@@ -33,12 +33,7 @@ class DependencyConstraints(PayloadField):
     return combine_hashes([c.fingerprint() for c in self._constraints])
 
   def check_all(self, target, task_context):
-    # TODO Execution is currently n^2, which is not very relevant because this is behind a flag.
-
-    # TODO Eventually, the next three lines should be moved out of here,
-    #      and the `classes_relevant_to_target` parameter removed from `check_target`.
-    #      Ideally we would have the same interface for all the subclasses of DependencyConstraint,
-    #      which I think should be check_target(target, task_context)
+    # TODO Execution is currently n^2.
     relevant_targets = DependencyConstraints.relevant_targets(target)
     for constraint in self._constraints:
       constraint.check_target(target, task_context, relevant_targets)
@@ -47,7 +42,7 @@ class DependencyConstraints(PayloadField):
   def relevant_targets(target):
     """
     Modify this method when the criteria changes
-    (i.e. the target itself should be included in the checks).
+    (e.g. the target itself should be included in the checks).
     """
     return target.dependencies
 
@@ -59,14 +54,46 @@ class BannedDependencyException(Exception):
 class Constraint(PayloadField):
   """Representation of a constraint on the target's dependencies."""
 
-  @abstractmethod
   def check_target(self, target, task_context, relevant_targets):
     """
     Check whether a given target complies with this constraint.
+
+    Note that there are constraints that apply to targets, and constraints that apply to classes.
+    Therefore, this flow is left general in purpose to accomodate for both requirements.
     :param target: Target to check.
     :param task_context: Context of the task, to extract from it any relevant information.
     :param relevant_targets: The set of targets that we have to check.
     :return:
+    """
+    items = self.get_collection_to_constrain(task_context, relevant_targets)
+    bad_elements = [item for item in items if self.predicate(item)]
+    if bad_elements:
+      raise BannedDependencyException(self.get_error_message(target, bad_elements))
+
+  def get_collection_to_constrain(self, context, relevant_targets):
+    """
+    Method to specify what the constraint applies to.
+    It will usually be targets, but there are some that apply to classes.
+    Overriding this method allows the user to express that.
+    """
+    return relevant_targets
+
+  @abstractmethod
+  def get_error_message(self, target, bad_elements):
+    """Error message to show when a constraint bans an element.
+    :param target Target that was examined.
+    :param bad_elements Elements that were banned by the constraint.
+                        These may be classes, targets or something else.
+    :return A string with the error message.
+    """
+    pass
+
+  @abstractmethod
+  def predicate(self, item):
+    """
+    A function to determine if an item is banned by this constraint.
+    :param item an intem of the type returned by get_collection_to_constrain
+    :return True if the item is banned, False otherwise.
     """
     pass
 
@@ -76,21 +103,26 @@ class JvmPackageConstraint(Constraint):
   def __init__(self, name):
     self.banned_package_name = name
 
-  def _get_classes_in_classpath(self, context, relevant_targets):
+  @staticmethod
+  def _get_classes_in_classpath(context, relevant_targets):
     classpath = context.products.get_data("runtime_classpath")
     analyzer = JvmDependencyAnalyzer(get_buildroot(), classpath)
     return analyzer.classes_for_targets(relevant_targets)
 
-  def check_target(self, target, context, relevant_targets):
-    relevant_classes = self._get_classes_in_classpath(context, relevant_targets)
-    banned_classes = [c for c in relevant_classes if c.startswith(self.banned_package_name)]
-    if banned_classes:
-      raise BannedDependencyException(
-        'Target {} uses rule "{}" to ban classes ({})'.format(
-          target.target_base,
-          self.banned_package_name,
-          ", ".join(banned_classes)
-        ))
+  def get_collection_to_constrain(self, context, relevant_targets):
+    # TODO we actually ignore this, bit since this is a method override, python guides said that we should call it.
+    super(JvmPackageConstraint, self).get_collection_to_constrain(context, relevant_targets)
+    return self._get_classes_in_classpath(context, relevant_targets)
+
+  def get_error_message(self, target, banned_classes):
+    return 'Target {} uses rule "{}" to ban classes ({})'.format(
+      target.target_base,
+      self.banned_package_name,
+      ", ".join(banned_classes)
+    )
+
+  def predicate(self, item):
+    return item.startswith(self.banned_package_name)
 
   def _compute_fingerprint(self):
     return stable_json_hash(self.banned_package_name)
@@ -102,22 +134,19 @@ class Tag(Constraint):
   def __init__(self, banned_tag_name):
     self.banned_tag_name = banned_tag_name
 
-  def _has_bad_tag(self, target):
-    return self.banned_tag_name in target.tags
-
-  def check_target(self, target, task_context, relevant_targets):
-    forbidden_targets = [t for t in relevant_targets if self._has_bad_tag(t)]
-    if forbidden_targets:
-      raise BannedDependencyException(
-        'Target {} has baned tag "{}", but these targets have it ({})'.format(
-          target.target_base,
-          self.banned_tag_name,
-          ", ".join([t.target_base for t in forbidden_targets])
-        ))
-
   def _compute_fingerprint(self):
     return stable_json_hash(self.banned_tag_name)
 
+
+  def get_error_message(self, target, forbidden_targets):
+    return 'Target {} has baned tag "{}", but these targets have it ({})'.format(
+      target.target_base,
+      self.banned_tag_name,
+      ", ".join([t.target_base for t in forbidden_targets])
+    )
+
+  def predicate(self, target):
+    return self.banned_tag_name in target.tags
 
 class TestDependencies(Constraint):
   """Check that this target does not depend on test targets"""
@@ -125,17 +154,14 @@ class TestDependencies(Constraint):
   def __init__(self):
     self.name = "test_dependencies"
 
-  def _is_test_dependency(self, dependency):
-    return isinstance(dependency, JUnitTests)
-
-  def check_target(self, target, task_context, relevant_targets):
-    forbidden_targets = [t for t in relevant_targets if self._is_test_dependency(t)]
-    if forbidden_targets:
-      raise BannedDependencyException(
-        'Target {} has test dependencies on targets ({})'.format(
-          target.target_base,
-          ", ".join([t.target_base for t in forbidden_targets])
-        ))
-
   def _compute_fingerprint(self):
     return stable_json_hash(self.name)
+
+  def get_error_message(self, target, forbidden_targets):
+    return 'Target {} has test dependencies on targets ({})'.format(
+      target.target_base,
+      ", ".join([t.target_base for t in forbidden_targets])
+    )
+
+  def predicate(self, dependency):
+    return isinstance(dependency, JUnitTests)
---
+++ tests/python/pants_test/backend/jvm/tasks/test_check_banned_deps_integration.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from pants_test.pants_run_integration_test import PantsRunIntegrationTest
+
+
+class CheckBannedDepsTest(PantsRunIntegrationTest):
+
+  TESTPROJECT_PATH = 'testprojects/src/scala/org/pantsbuild/testproject/banned_deps_test'
+
+  def _in_testproject(self, target):
+    return '{}:{}'.format(self.TESTPROJECT_PATH, target)
+
+  def test_package_constraints(self):
+    pants_run = self.do_command(
+      '--no-compile-check-banned-deps-skip',
+      'compile',
+      self._in_testproject('ban_packages'),
+      success=False)
+    self.assertIn(
+      'Exception message: Target testprojects/src/scala uses rule "scopt" to ban classes (',
+      pants_run.stderr_data.strip())
+
+  def test_test_dependency_constraints(self):
+    pants_run = self.do_command(
+      '--no-compile-check-banned-deps-skip',
+      'compile',
+      self._in_testproject('ban_testdeps'),
+      success=False)
+    self.assertIn(
+      'Target testprojects/src/scala has test dependencies on targets (testprojects/src/scala)',
+      pants_run.stderr_data.strip())
+
+  def test_tag_constraints(self):
+    pants_run = self.do_command(
+      '--no-compile-check-banned-deps-skip',
+      'compile',
+      self._in_testproject('ban_tags'),
+      success=False)
+    self.assertIn(
+      'Target testprojects/src/scala has baned tag "deprecated", but these targets have it (testprojects/src/scala)',
+      pants_run.stderr_data.strip())
