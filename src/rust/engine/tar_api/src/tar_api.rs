3dacfcdea41c55a250e8899255d803aec1ccc334
Yi Cheng <wisechengyi@gmail.com>
2018-11-15 15:01:31 -0800

    fix typo

--- src/rust/engine/tar_api/src/tar_api.rs
+++ src/rust/engine/tar_api/src/tar_api.rs
@@ -24,6 +24,36 @@ pub mod tar_tests {
   use std::path::PathBuf;
   use tempfile;
   use testutil::make_file;
+  use std::fs::File;
+  use flate2::write::GzEncoder;
+  use flate2::Compression;
+
+//  #[test]
+//  fn decompress_normal_tar_file() {
+//    // prepare a file containing 'hello world'
+//    let tmp_dir = tempfile::TempDir::new().unwrap();
+//    let path = PathBuf::from("marmosets");
+//    let content = "hello world".as_bytes().to_vec();
+//    let full_path = std::fs::canonicalize(tmp_dir.path()).unwrap().join(&path);
+//    make_file(
+//      &full_path,
+//      &content,
+//      0o600,
+//    );
+//
+//    // compress that file into a/b/c/hello.txt in the tar
+//    let tar_gz_path = std::fs::canonicalize(tmp_dir.path()).unwrap().join(&"x.tgz");
+//    let tar_gz_handle = File::create(tar_gz_path).unwrap();
+//    let enc = GzEncoder::new(tar_gz_handle, Compression::default());
+//    let mut tar = tar::Builder::new(enc);
+//    tar.append_file("a/b/c/hello.txt", &mut File::open("test_data/hello.txt").unwrap());
+//    tar.finish();
+//
+//    let tmp_dest_dir = tempfile::TempDir::new().unwrap();
+//    let x = tar_gz_path.as_path().to_owned();
+//    let result = decompress_tgz(&x, &tmp_dest_dir.path());
+//    assert!(result.is_err())
+//  }
 
   #[test]
   fn decompress_invalid_tar_file_path() {
@@ -34,14 +64,15 @@ pub mod tar_tests {
   #[test]
   fn decompress_invalid_tar_content() {
     let dir = tempfile::TempDir::new().unwrap();
-    let path = PathBuf::from("marmosets");
+    let tar_filename = PathBuf::from("marmosets");
     let content = "definitely not a valid tar".as_bytes().to_vec();
+    let tar_path = std::fs::canonicalize(dir.path()).unwrap().join(&tar_filename);
     make_file(
-      &std::fs::canonicalize(dir.path()).unwrap().join(&path),
+      &tar_path,
       &content,
       0o600,
     );
-    let result = decompress_tgz(&path, &PathBuf::from("a_dir"));
+    let result = decompress_tgz(&tar_path, &PathBuf::from("a_dir"));
     assert!(result.is_err())
   }
 }
