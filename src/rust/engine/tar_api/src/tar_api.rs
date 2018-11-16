8a6738677913cd383d308cfd0519d8dc3092a865
Yi Cheng <wisechengyi@gmail.com>
2018-11-15 16:01:26 -0800

    one more test

--- src/rust/engine/tar_api/src/tar_api.rs
+++ src/rust/engine/tar_api/src/tar_api.rs
@@ -21,39 +21,58 @@ pub fn decompress_tgz(tar_path: &Path, output_dir: &Path) -> Result<(), std::io:
 #[cfg(test)]
 pub mod tar_tests {
   use super::decompress_tgz;
-  use std::path::PathBuf;
-  use tempfile;
-  use testutil::make_file;
-  use std::fs::File;
   use flate2::write::GzEncoder;
   use flate2::Compression;
+  use std::fs::File;
+  use std::path::{Path, PathBuf};
+  use tempfile::TempDir;
+  use testutil::file::contents;
+  use testutil::make_file;
 
-//  #[test]
-//  fn decompress_normal_tar_file() {
-//    // prepare a file containing 'hello world'
-//    let tmp_dir = tempfile::TempDir::new().unwrap();
-//    let path = PathBuf::from("marmosets");
-//    let content = "hello world".as_bytes().to_vec();
-//    let full_path = std::fs::canonicalize(tmp_dir.path()).unwrap().join(&path);
-//    make_file(
-//      &full_path,
-//      &content,
-//      0o600,
-//    );
-//
-//    // compress that file into a/b/c/hello.txt in the tar
-//    let tar_gz_path = std::fs::canonicalize(tmp_dir.path()).unwrap().join(&"x.tgz");
-//    let tar_gz_handle = File::create(tar_gz_path).unwrap();
-//    let enc = GzEncoder::new(tar_gz_handle, Compression::default());
-//    let mut tar = tar::Builder::new(enc);
-//    tar.append_file("a/b/c/hello.txt", &mut File::open("test_data/hello.txt").unwrap());
-//    tar.finish();
-//
-//    let tmp_dest_dir = tempfile::TempDir::new().unwrap();
-//    let x = tar_gz_path.as_path().to_owned();
-//    let result = decompress_tgz(&x, &tmp_dest_dir.path());
-//    assert!(result.is_err())
-//  }
+  #[test]
+  fn decompress_normal_tar_file() {
+    // prepare a file containing 'hello world'
+    let tmp_dir = TempDir::new().unwrap();
+    let content = "hello world".as_bytes().to_vec();
+    let txt_full_path = std::fs::canonicalize(tmp_dir.path())
+      .unwrap()
+      .join(&PathBuf::from("hello.txt"));
+    make_file(&txt_full_path, &content, 0o600);
+
+    let path_in_tar = "a/b/c/d.txt";
+    let tgz_path = std::fs::canonicalize(tmp_dir.path())
+      .unwrap()
+      .join(&"simple.tgz");
+
+    let compress_result = compress(&txt_full_path, &path_in_tar, &tgz_path);
+    assert!(compress_result.is_ok());
+
+    // uncompress the tgz then make sure the content is good.
+    let tmp_dest_dir = tempfile::TempDir::new().unwrap();
+    let untar_result = decompress_tgz(&tgz_path.as_path(), &tmp_dest_dir.path());
+    assert!(untar_result.is_ok());
+    let expected_txt_path = std::fs::canonicalize(tmp_dest_dir.path())
+      .unwrap()
+      .join(&path_in_tar);
+    assert!(expected_txt_path.exists());
+    assert_eq!(content, contents(&expected_txt_path))
+  }
+
+  fn compress(
+    txt_full_path: &Path,
+    path_in_tar: &str,
+    tar_full_path: &Path,
+  ) -> Result<(), std::io::Error> {
+    // compress that file into a/b/c/hello.txt in the tar
+    let enc = GzEncoder::new(
+      File::create(tar_full_path.clone()).unwrap(),
+      Compression::default(),
+    );
+    let mut tar = tar::Builder::new(enc);
+    tar.append_file(path_in_tar, &mut File::open(txt_full_path.clone()).unwrap())?;
+    tar.into_inner()?;
+    Ok(())
+  }
 
   #[test]
   fn decompress_invalid_tar_file_path() {
@@ -66,12 +85,10 @@ pub mod tar_tests {
     let dir = tempfile::TempDir::new().unwrap();
     let tar_filename = PathBuf::from("marmosets");
     let content = "definitely not a valid tar".as_bytes().to_vec();
-    let tar_path = std::fs::canonicalize(dir.path()).unwrap().join(&tar_filename);
-    make_file(
-      &tar_path,
-      &content,
-      0o600,
-    );
+    let tar_path = std::fs::canonicalize(dir.path())
+      .unwrap()
+      .join(&tar_filename);
+    make_file(&tar_path, &content, 0o600);
     let result = decompress_tgz(&tar_path, &PathBuf::from("a_dir"));
     assert!(result.is_err())
   }
