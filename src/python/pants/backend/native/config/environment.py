d6fc56e122f1ceef199eb0682bcaf0e8a4e0914f
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-12-03 00:33:18 -0800

    fill out some docstrings

--- src/python/pants/backend/native/config/environment.py
+++ src/python/pants/backend/native/config/environment.py
@@ -31,6 +31,7 @@ class ExtensibleAlgebraic(AbstractClass):
   @abstractmethod
   def copy(self, **kwargs):
     """Analogous to a `datatype()`'s `copy()` method."""
+    raise NotImplementedError('copy() must be implemented by subclasses of ExtensibleAlgebraic!')
 
   def _single_list_field_operation(self, field_name, list_value, prepend=True):
     cur_value = getattr(self, field_name)
@@ -44,17 +45,20 @@ class ExtensibleAlgebraic(AbstractClass):
     return self.copy(**arg_dict)
 
   def prepend_field(self, field_name, list_value):
+    """Return a copy of this object with `list_value` prepended to the field named `field_name`."""
     return self._single_list_field_operation(field_name, list_value, prepend=True)
 
   def append_field(self, field_name, list_value):
+    """Return a copy of this object with `list_value` appended to the field named `field_name`."""
     return self._single_list_field_operation(field_name, list_value, prepend=False)
 
   def sequence(self, other, exclude_list_fields=None):
-    """
-    - the return type is the type of this object (or just whatever .copy() does!)
-      - but the `other` can be any `ExtensibleAlgebraic`
-    - for this particular operation: "add" all of the list_fields together, for the fields common to
-      both types
+    """Return a copy of this object which combines all the fields common to both `self` and `other`.
+
+    List fields will be concatenated.
+
+    The return type of this method is the type of `self`, but the `other` can be any
+    `ExtensibleAlgebraic` instance.
     """
     exclude_list_fields = exclude_list_fields or []
     overwrite_kwargs = {}
@@ -94,6 +98,7 @@ class Executable(ExtensibleAlgebraic):
 
     :rtype: str
     """
+    raise NotImplementedError('path_entries is a list field of Executable!')
 
   # TODO: rename this to 'runtime_library_dirs'!
   @abstractproperty
@@ -104,10 +109,12 @@ class Executable(ExtensibleAlgebraic):
     for libraries that are needed at link time.
     :rtype: list of str
     """
+    raise NotImplementedError('library_dirs is a list field of Executable!')
 
   @abstractproperty
   def exe_filename(self):
     """The "entry point" -- which file to invoke when PATH is set to `path_entries()`."""
+    raise NotImplementedError('exe_filename is a scalar field of Executable!')
 
   @abstractproperty
   def extra_args(self):
@@ -117,6 +124,7 @@ class Executable(ExtensibleAlgebraic):
 
     :rtype: list of str
     """
+    raise NotImplementedError('extra_args is a list field of Executable!')
 
   _platform = Platform.create()
 
@@ -149,7 +157,7 @@ class LinkerMixin(Executable):
 
   @classproperty
   def list_fields(cls):
-    return super(LinkerMixin, cls).list_fields + ['linking_library_dirs']
+    return super(LinkerMixin, cls).list_fields + ['linking_library_dirs', 'extra_object_files']
 
   @abstractproperty
   def linking_library_dirs(self):
@@ -157,6 +165,7 @@ class LinkerMixin(Executable):
 
     :rtype: list of str
     """
+    raise NotImplementedError('linking_library_dirs is a list field of LinkerMixin!')
 
   @abstractproperty
   def extra_object_files(self):
@@ -166,6 +175,7 @@ class LinkerMixin(Executable):
 
     :rtype: list of str
     """
+    raise NotImplementedError('include_dirs is a list field of CompilerMixin!')
 
   @property
   def as_invocation_environment_dict(self):
@@ -205,6 +215,7 @@ class CompilerMixin(Executable):
 
     :rtype: list of str
     """
+    raise NotImplementedError('extra_object_files is a list field of LinkerMixin!')
 
   @property
   def as_invocation_environment_dict(self):
