84b152c773d00a74c9c5473a7f877cf2ac7d8b15
Stu Hood <stuhood@twitter.com>
2018-12-07 14:03:47 -0800

    Flush the console after all @console_rules have completed (#6878)
    ### Problem
    
    I noticed some flakiness while using `--no-v1 --v2 list` under `pantsd`, and diffing the output of a successful and unsuccessful run indicated that it was an issue with truncation.
    
    ### Solution
    
    Add and use `Console.flush()` after all `@console_rules` have completed.
    
    ### Result
    
    I'm fairly sure that this fixes #6782: unmarking it here.

--- tests/python/pants_test/rules/test_test_integration.py
+++ tests/python/pants_test/rules/test_test_integration.py
@@ -4,8 +4,6 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import unittest
-
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 
@@ -92,7 +90,6 @@ testprojects/tests/python/pants/dummies:failing_target                          
 """,
     )
 
-  @unittest.skip('Flaky test: https://github.com/pantsbuild/pants/issues/6782')
   def test_mixed_python_tests(self):
     args = [
       '--no-v1',
