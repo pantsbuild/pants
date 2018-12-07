236dc253e020ce05a00a6fcaa290358d1f1161f0
Benjy Weinberger <benjyw@gmail.com>
2018-12-07 15:25:39 -0800

    Lint.

--- contrib/awslambda/python/src/python/pants/contrib/awslambda/python/examples/hello_handler.py
+++ contrib/awslambda/python/src/python/pants/contrib/awslambda/python/examples/hello_handler.py
@@ -2,6 +2,8 @@
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import pycountry
 
 from pants.contrib.awslambda.python.examples.hello_lib import say_hello
--- contrib/awslambda/python/src/python/pants/contrib/awslambda/python/examples/hello_lib.py
+++ contrib/awslambda/python/src/python/pants/contrib/awslambda/python/examples/hello_lib.py
@@ -2,6 +2,8 @@
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 
 def say_hello(s):
   print('Hello {}!'.format(s))
