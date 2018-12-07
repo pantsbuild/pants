a9bc3e323580cf795e560f50257d27f0c3db11f3
Benjy Weinberger <benjyw@gmail.com>
2018-12-07 15:15:05 -0800

    Add a comment.

--- contrib/awslambda/python/tests/python/pants_test/contrib/awslambda/python/test_python_awslambda_integration.py
+++ contrib/awslambda/python/tests/python/pants_test/contrib/awslambda/python/test_python_awslambda_integration.py
@@ -32,6 +32,10 @@ class PythonAWSLambdaIntegrationTest(PantsRunIntegrationTest):
       ]
       pants_run = self.run_pants(command=command, config=config)
       self.assert_success(pants_run)
+
+      # Now run the lambda via the wrapper handler injected by lambdex (note that this
+      # is distinct from the pex's entry point - a handler must be a function with two arguments,
+      # whereas the pex entry point is a module).
       awslambda = os.path.join(distdir, 'hello-lambda.pex')
       output = subprocess.check_output(env={'PEX_INTERPRETER': '1'}, args=[
         '{} -c "from lambdex_handler import handler; handler(None, None)"'.format(awslambda)
