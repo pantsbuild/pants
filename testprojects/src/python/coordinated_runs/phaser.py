808724e8259fb5a2519b85c05564f46d2e7f9e6d
John Sirois <john.sirois@gmail.com>
2018-12-12 10:22:05 -0800

    Ensure pants is running in excpetions sink tests.
    Previously, pants was run asynchronously with no way to guaranty it was
    up and initialized the way we expected it to be before testing its
    signal handling. Introduce a phaser testproject target that allows the
    integration test to ensure the pants run state is as expected.
    
    Fixes #6787

---
+++ testprojects/src/python/coordinated_runs/phaser.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import time
+
+
+arrive_file, await_file = sys.argv[1:3]
+
+# Signal we've arrived.
+with open(arrive_file, 'w') as fp:
+  fp.close()
+
+# Await a graceful join.
+while not os.path.isfile(await_file):
+  time.sleep(0.1)
