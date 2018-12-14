287626443c7a1928c5c41b9c01da60ec3404db76
Eric Arellano <ericarellano@me.com>
2018-12-14 08:13:18 -0700

    Remove remaining unnecessary __future__ imports (#6925)
    ### Problem
    `from __future__ import generators, nested_scopes, with_statement` are all enabled by default since Python 2.6+, so are redundant. 
    
    https://github.com/pantsbuild/pants/pull/6096 removed the majority of uses, but didn't remove from `testprojects/` and `migrations/`.
    
    ### Commands to generate change
    * `grep -rli 'from\ __future__\ import' testprojects migrations | xargs perl -pi -e 's/.*from __future__ import.*/from __future__ import absolute_import, division, print_function, unicode_literals/'`
    * `grep -rli 'from\ __future__\ import' testprojects migrations | xargs perl -ni -e 'print unless /.*with_statement.*/'`

--- testprojects/pants-plugins/src/python/test_pants_plugin/register.py
+++ testprojects/pants-plugins/src/python/test_pants_plugin/register.py
@@ -2,8 +2,7 @@
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
-                        unicode_literals, with_statement)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from test_pants_plugin.pants_infra_tests import PantsInfraTests
 from test_pants_plugin.subsystems.pants_test_infra import PantsTestInfra
--- testprojects/src/python/interpreter_selection/python_3_selection_testing/lib_py2.py
+++ testprojects/src/python/interpreter_selection/python_3_selection_testing/lib_py2.py
@@ -2,8 +2,7 @@
 # Copyright 2017 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
-                        unicode_literals, with_statement)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 
 # A simple example to include in a python 2 binary target
