e6137eb0cd1253240108625a5a0111798599895f
Eric Arellano <ericarellano@me.com>
2018-12-16 11:21:50 -0700

    Make generated C library names unique
    Fixes the error `FAILURE: The name 'asdf-cpp' was used for two shared libraries` when python_distribution/ctypes and python_distribution/ctypes_with_extra_compiler_flags are run at the same time.

--- testprojects/src/python/python_distribution/ctypes_with_extra_compiler_flags/setup.py
+++ testprojects/src/python/python_distribution/ctypes_with_extra_compiler_flags/setup.py
@@ -11,5 +11,5 @@ setup(
   name='ctypes_test',
   version='0.0.1',
   packages=find_packages(),
-  data_files=[('', ['libasdf-cpp.so'])],
+  data_files=[('', ['libasdf-cpp_ctypes-with-extra-compiler-flags.so'])],
 )
