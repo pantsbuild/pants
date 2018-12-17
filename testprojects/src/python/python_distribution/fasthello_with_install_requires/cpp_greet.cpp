b134e1975dec8aa56c80fed6a8437ea73f3aae30
Eric Arellano <ericarellano@me.com>
2018-12-17 06:59:11 -0700

    Add Python 3 support to C and C++ module initialization (#6930)
    ## Problem
    Python 3 makes changes to the C API, including module initialization. For example, Python 3 no longer supports `Py_InitModule`.
    
    However, we want to maintain support for Python 2 and do not want to have to create separate files for Python 2 and Python 3.
    
    ## Solution
    C preprocessor conditionals allow us to support both Python 2 and Python 3 in the same files.
    
    This PR's module initialization code results from a combination of the Python 3 C extension [porting guide](http://python3porting.com/cextensions.html#module-initialization), Python 3's C extension tutorial showing [how to initialize a module](https://docs.python.org/3/extending/extending.html#the-module-s-method-table-and-initialization-function), and the API docs [reference for the `PyModuleDef` object](https://docs.python.org/3/c-api/module.html#initializing-c-modules).

--- testprojects/src/python/python_distribution/fasthello_with_install_requires/cpp_greet.cpp
+++ testprojects/src/python/python_distribution/fasthello_with_install_requires/cpp_greet.cpp
@@ -9,6 +9,26 @@ static PyMethodDef Methods[] = {
   {NULL, NULL, 0, NULL}
 };
 
-PyMODINIT_FUNC initcpp_greet(void) {
-  (void) Py_InitModule("cpp_greet", Methods);
-}
+#if PY_MAJOR_VERSION >= 3
+  static struct PyModuleDef moduledef = {
+    PyModuleDef_HEAD_INIT,
+    "cpp_greet", /* m_name */
+    NULL,        /* m_doc */
+    -1,          /* m_size */
+    Methods,     /* m_methods */
+    NULL,        /* m_slots */
+    NULL,        /* m_traverse */
+    NULL,        /* m_clear */
+    NULL         /* m_free */
+  };
+#endif
+
+#if PY_MAJOR_VERSION >= 3
+  PyMODINIT_FUNC PyInit_cpp_greet(void) {
+    return PyModule_Create(&moduledef);
+  }
+#else
+  PyMODINIT_FUNC initcpp_greet(void) {
+    (void) Py_InitModule("cpp_greet", Methods);
+  }
+#endif
