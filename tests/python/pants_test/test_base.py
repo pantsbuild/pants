40102c2d7f1bf98c8ce2967a5b05d82ef2283ae4
Ekaterina Tyurina <etyurina@twitter.com>
2018-12-18 11:24:23 +0000

    Fix format

--- tests/python/pants_test/test_base.py
+++ tests/python/pants_test/test_base.py
@@ -36,8 +36,8 @@ from pants.source.source_root import SourceRootConfig
 from pants.subsystem.subsystem import Subsystem
 from pants.task.goal_options_mixin import GoalOptionsMixin
 from pants.util.contextutil import temporary_dir
-from pants.util.dirutil import (recursive_dirname, relative_symlink, safe_mkdir, safe_mkdtemp,
-  safe_open, safe_rmtree, safe_file_dump)
+from pants.util.dirutil import (recursive_dirname, relative_symlink, safe_file_dump, safe_mkdir,
+                                safe_mkdtemp, safe_open, safe_rmtree)
 from pants.util.memo import memoized_method
 from pants_test.base.context_utils import create_context_from_options
 from pants_test.engine.util import init_native
