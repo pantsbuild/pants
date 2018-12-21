d0cfbb8d5cc3d732c987f9f4aa3b1c1355061966
Daniel Wagner-Hall <dwagnerhall@twitter.com>
2018-12-21 11:02:15 +0000

    Markdown writer errors are written properly
    Before this change, if we encounter errors rendering markdown, docutils
    will try to write unicode errors to the RWBuf, which will fail because
    the RWBuf expects binary.

---
+++ tests/python/pants_test/util/test_rwbuf.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+# Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import unittest
+
+from pants.util.contextutil import temporary_file_path
+from pants.util.rwbuf import FileBackedRWBuf, StringWriter
+
+
+class StringWriterTest(unittest.TestCase):
+
+  def test_writes_string(self):
+    with temporary_file_path() as p:
+      fb = FileBackedRWBuf(p)
+      try:
+        sw = StringWriter(fb)
+        sw.write(u"\u2764 Curious Zelda")
+      finally:
+        fb.close()
+      with open(p, 'rb') as f:
+        contents = f.read()
+        self.assertEquals(contents, b'\xe2\x9d\xa4 Curious Zelda')
+
+  def test_rejects_binary(self):
+    with temporary_file_path() as p:
+      fb = FileBackedRWBuf(p)
+      sw = StringWriter(fb)
+      try:
+        with self.assertRaises(ValueError):
+          sw.write(b"Curious Zelda")
+      finally:
+        fb.close()
+
+  def test_can_write_binary_to_buffer(self):
+    with temporary_file_path() as p:
+      fb = FileBackedRWBuf(p)
+      try:
+        sw = StringWriter(fb)
+        sw.write(u"\u2764")
+        sw.buffer.write(b" Curious")
+        sw.write(u" Zelda")
+      finally:
+        fb.close()
+      with open(p, 'rb') as f:
+        contents = f.read()
+        self.assertEquals(contents, b'\xe2\x9d\xa4 Curious Zelda')
