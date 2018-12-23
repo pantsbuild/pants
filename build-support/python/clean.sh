0ef79c384f3e57b86f0e4574929bce84e8f47879
Eric Arellano <ericarellano@me.com>
2018-12-23 10:42:05 -0700

    Squash merge py3

--- build-support/python/clean.sh
+++ build-support/python/clean.sh
@@ -3,5 +3,6 @@
 PANTS_BASE=$(dirname $0)/../..
 rm -rf ${HOME}/.pex
 rm -rf ${PANTS_BASE}/build-support/pants_dev_deps.venv
+rm -rf ${PANTS_BASE}/build-support/pants_dev_deps.py{2,3}.venv
 rm -rf ${PANTS_BASE}/.pants.d
 find ${PANTS_BASE} -name '*.pyc' | xargs rm -f
