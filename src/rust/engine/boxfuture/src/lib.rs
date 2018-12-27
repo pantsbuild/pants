8f5b8a871c1a9d2a8e7548b770e2333b4266d535
Daniel Wagner-Hall <dawagner@gmail.com>
2018-12-27 17:25:34 +0000

    Error on unused must_use

--- src/rust/engine/boxfuture/src/lib.rs
+++ src/rust/engine/boxfuture/src/lib.rs
@@ -2,6 +2,7 @@
 // https://github.com/alexcrichton/futures-rs/issues/228 has background for its removal.
 // This avoids needing to call Box::new() around every future that we produce.
 
+#![deny(unused_must_use)]
 // Enable all clippy lints except for many of the pedantic ones. It's a shame this needs to be copied and pasted across crates, but there doesn't appear to be a way to include inner attributes from a common source.
 #![deny(
   clippy::all,
