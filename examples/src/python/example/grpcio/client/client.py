a8fca0bea3571c0bd21721f41740d54637741d54
Marcin Podolski <marcin.podolski@xain.io>
2018-12-27 10:05:55 +0100

    formatting issues

--- examples/src/python/example/grpcio/client/client.py
+++ examples/src/python/example/grpcio/client/client.py
@@ -7,12 +7,11 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import grpc
 from org.pantsbuild.example.service import service_pb2, service_pb2_grpc
 
-
 if __name__ == '__main__':
-    print('hello world from grpcio client!')
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = service_pb2_grpc.ExampleServiceStub(channel)
-        hello_response = stub.Hello(service_pb2.HelloRequest(action='hello'))
-        print(hello_response)
-        bye_response = stub.Hello(service_pb2.HelloRequest(action='bye'))
-        print(bye_response)
+  print('hello world from grpcio client!')
+  with grpc.insecure_channel('localhost:50051') as channel:
+    stub = service_pb2_grpc.ExampleServiceStub(channel)
+    hello_response = stub.Hello(service_pb2.HelloRequest(action='hello'))
+    print(hello_response)
+    bye_response = stub.Hello(service_pb2.HelloRequest(action='bye'))
+    print(bye_response)
