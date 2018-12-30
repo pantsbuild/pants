6c86ae741e79030a3f8f43101df53a996906ab6d
Benjy Weinberger <benjyw@gmail.com>
2018-12-30 15:57:51 +0200

    WIP

--- build-support/travis/generate_travis_yml.py
+++ build-support/travis/generate_travis_yml.py
@@ -7,9 +7,14 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import pkg_resources
 import pystache
 
+num_integration_shards = 7
+
 
 def generate_travis_yml():
   template = pkg_resources.resource_string(__name__, 'travis.yml.mustache').decode('utf-8')
-  context = {'integration_shards': range(0, 7)}
+  context = {
+    'integration_shards': range(0, num_integration_shards),
+    'integration_shards_length': num_integration_shards,
+  }
   print(pystache.render(template, context))
 
