81016eb6a9cd6c1f9ce09bb9b6b6500a3a16fabb
Benjy Weinberger <benjyw@gmail.com>
2019-01-02 16:10:21 -0800

    Whoops.

--- build-support/bin/get_ci_bootstrapped_pants_pex.sh
+++ build-support/bin/get_ci_bootstrapped_pants_pex.sh
@@ -23,4 +23,4 @@ chmod 755 ./bootstrapped.pants.pex
 # Pants code executing under test expects native_engine.so to be present as a resource
 # in the source tree. Normally it'll be there because we created it there during bootstrapping.
 # So here we have to manually extract it there from the bootstrapped pex.
-unzip -j pants.pex pants/engine/native_engine.so -d src/python/pants/engine/
+unzip -j bootstrapped.pants.pex pants/engine/native_engine.so -d src/python/pants/engine/
