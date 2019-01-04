78df78164180f1423d6dcbd9f8e5a2961bc248a8
Benjy Weinberger <benjyw@gmail.com>
2019-01-03 17:35:39 -0800

    A preliminary bootstrapping CI stage, to prevent repeated work in each CI shard. (#7012)
    A single job in the bootstrap phase builds a pants.pex and stores it in S3. The jobs in the test stage then retrieve that pants.pex and use it instead of bootstrapping their own. 
    
    This change also splits our CI test stage into more shards, and gives each shard a unique `CACHE_NAME` env var, so that they don't attempt to read/write the same cache (which is inadvisable, according to Travis).  To make this easier and DRYer, we switch from a directly-authored .travis.yml to generating one from a mustache template.  We add a pre-commit check to ensure that the .travis.yml has been correctly generated.
    
    Note that to facilitate pull requests, this change requires a public S3 bucket (`ci-public.pantsbuild.org') with global write permissions. This is obviously a little risky. We mitigate abuse risks thus:
    
    - The bucket has versioning turned on, so that we can detect if anyone overwrites an existing `pants.pex`. 
    - The bucket expires all versions of an object after 1 day.
    
    Abuse is still possible, but this seems like a calculated risk worth taking, since the benefits of faster CI are significant.

--- tests/python/pants_test/backend/python/pants_requirement_integration_test_base.py
+++ tests/python/pants_test/backend/python/pants_requirement_integration_test_base.py
@@ -7,10 +7,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 import shutil
 import uuid
-from builtins import open
 from contextlib import contextmanager
 
-from pants.base.build_environment import get_buildroot
 from pants.util.contextutil import environment_as, temporary_dir
 from pants.util.dirutil import safe_walk
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
@@ -23,21 +21,16 @@ class PantsRequirementIntegrationTestBase(PantsRunIntegrationTest):
     # on a globally unique local version identifier to ensure we can never collide with past,
     # present or future stable or unstable releases.
     unstable_version = '0.0.0+{}'.format(uuid.uuid4().hex)
-    version_dir = os.path.join(get_buildroot(), 'src/python/pants')
 
-    with self.file_renamed(version_dir, 'VERSION', 'VERSION.orig'):
-      with open(os.path.join(version_dir, 'VERSION'), 'w') as fp:
-        fp.write(unstable_version)
-
+    # Notes must be configured for all pants versions so we fake that out ephemerally here.
+    # In pants-plugins/src/python/internal_backend/utilities/register.py see
+    # PantsReleases.notes_for_version.
+    with environment_as(_PANTS_VERSION_OVERRIDE=unstable_version,
+                        PANTS_PANTS_RELEASES_BRANCH_NOTES="{'0.0.x': 'pants.ini'}"):
       pants_run = self.run_pants(['--version'])
       self.assert_success(pants_run)
       self.assertEqual(unstable_version, pants_run.stdout_data.strip())
-
-      # Notes must be configured for all pants versions so we fake that out ephemerally here.
-      # In pants-plugins/src/python/internal_backend/utilities/register.py see
-      # PantsReleases.notes_for_version.
-      with environment_as(PANTS_PANTS_RELEASES_BRANCH_NOTES="{'0.0.x': 'pants.ini'}"):
-        yield
+      yield
 
   def _iter_wheels(self, path):
     for root, _, files in safe_walk(path):
