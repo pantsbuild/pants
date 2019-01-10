58d51fb36192cb99301838c331073d8227fdfb57
Stu Hood <stuhood@gmail.com>
2019-01-09 18:50:48 -0800

    Render the symbolic path during an attempt to traverse outside the buildroot.

--- src/rust/engine/fs/src/lib.rs
+++ src/rust/engine/fs/src/lib.rs
@@ -380,9 +380,11 @@ impl PathGlob {
       let mut canonical_dir_parent = canonical_dir;
       let mut symbolic_path_parent = symbolic_path;
       if !canonical_dir_parent.0.pop() {
+        let mut symbolic_path = symbolic_path_parent;
+        symbolic_path.extend(parts.iter().map(|p| p.as_str()));
         return Err(format!(
-          "Globs may not traverse outside the root: {:?}",
-          parts
+          "Globs may not traverse outside of the buildroot: {:?}",
+          symbolic_path,
         ));
       }
       symbolic_path_parent.push(Path::new(*PARENT_DIR));
