0d43d594c74a7d27ca5bd952f8cf2cb9fcde6a1d
Eric Arellano <ericarellano@me.com>
2019-01-12 23:16:53 -0700

    Redistribute blacklist shards to favor blacklist

--- build-support/travis/generate_travis_yml.py
+++ build-support/travis/generate_travis_yml.py
@@ -8,8 +8,8 @@ import pkg_resources
 import pystache
 
 
-num_py3_integration_shards = 16
-num_py2_blacklist_integration_shards = 4
+num_py3_integration_shards = 13
+num_py2_blacklist_integration_shards = 7
 num_cron_integration_shards = 20
 
 
