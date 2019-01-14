6c2f3f1d5311383a4d134c9451a921fb91746b78
Eric Arellano <ericarellano@me.com>
2019-01-14 10:56:11 -0700

    Properly render \n in exceptions with Py3 (#7073)
    ### Problem
    When ran with Py3, stacktraces would not properly render new lines and would instead print them as literals. This is because the stacktrace was being printed as a byte string (with a `b''` prefix).
    
    See https://travis-ci.org/pantsbuild/pants/jobs/478884824#L2636, for example.
    
    ### Solution
    Don't use the `print` function when trying to print bytes, as it does not work as expected when printing bytes. In a Py3 REPL, `print(b"test", file=sys.stderr.buffer)` will raise a `TypeError`, even though it looks like it should work.
    
    Also use `sys.stderr.buffer` with Py3 to use a bytes interface.
    
    ### Result
    Stack traces now readable and printed as intended. See https://travis-ci.org/pantsbuild/pants/jobs/479174634#L2340, which compares to the above example. (Both were run using a Py3 pex).

--- src/python/pants/base/exception_sink.py
+++ src/python/pants/base/exception_sink.py
@@ -14,6 +14,7 @@ import traceback
 from builtins import object, str
 
 import setproctitle
+from future.utils import PY3
 
 from pants.base.build_environment import get_buildroot
 from pants.base.exiter import Exiter
@@ -142,6 +143,9 @@ class ExceptionSink(object):
     # NB: mutate process-global state!
     sys.excepthook = cls._log_unhandled_exception_and_exit
 
+  # TODO(7080): audit all usages of this function and add Py3 support, e.g. use `sys.stderr.buffer` instead
+  # of sys.stdout. However, we shouldn't need to proactively enforce that the stream is BytesIO, because Py3
+  # should already check when we try to use this.
   @classmethod
   def reset_interactive_output_stream(cls, interactive_output_stream):
     """
@@ -381,4 +385,4 @@ ExceptionSink.reset_log_location(os.path.join(get_buildroot(), '.pants.d'))
 # Sets except hook.
 ExceptionSink.reset_exiter(Exiter(exiter=sys.exit))
 # Sets a SIGUSR2 handler.
-ExceptionSink.reset_interactive_output_stream(sys.stderr)
+ExceptionSink.reset_interactive_output_stream(sys.stderr.buffer if PY3 else sys.stderr)
