503f4f3af858216161ea05d9a3092d4cdbb31ed6
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-01-16 20:26:24 -0800

    add a decorator to make tests that fail with increased worker count

--- tests/python/pants_test/backend/jvm/tasks/jvm_compile/rsc/test_rsc_compile_integration.py
+++ tests/python/pants_test/backend/jvm/tasks/jvm_compile/rsc/test_rsc_compile_integration.py
@@ -5,34 +5,34 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os
+from functools import wraps
 
 from pants.util.contextutil import temporary_dir
 from pants_test.backend.jvm.tasks.jvm_compile.base_compile_integration_test import BaseCompileIT
 
 
+def _execution_strategies(strategies, workers_range=[1]):
+  def wrapper(func):
+    @wraps(func)
+    def wrapper_self(*args, **kwargs):
+      for worker_count in workers_range:
+        for strategy in strategies:
+          func(*args, execution_strategy=strategy, worker_count=worker_count, **kwargs)
+    return wrapper_self
+  return wrapper
+
+
 class RscCompileIntegration(BaseCompileIT):
-  def test_basic_binary_subprocess(self):
+  @_execution_strategies(['nailgun', 'subprocess'])
+  def test_basic_binary_nonhermetic(self, execution_strategy, worker_count):
     with temporary_dir() as cache_dir:
       config = {
         'cache.compile.rsc': {'write_to': [cache_dir]},
         'jvm-platform': {'compiler': 'rsc'},
-        'compile.rsc': {'execution_strategy': 'subprocess'},
-      }
-
-      pants_run = self.run_pants(
-        ['compile',
-         'testprojects/src/scala/org/pantsbuild/testproject/mutual:bin',
-         ],
-        config)
-      self.assert_success(pants_run)
-
-  # TODO: determine if this is sufficient testing for the nailgun execution strategy.
-  def test_basic_binary_nailgun(self):
-    with temporary_dir() as cache_dir:
-      config = {
-        'cache.compile.rsc': {'write_to': [cache_dir]},
-        'jvm-platform': {'compiler': 'rsc'},
-        'compile.rsc': {'execution_strategy': 'nailgun'},
+        'compile.rsc': {
+          'execution_strategy': execution_strategy,
+          'worker_count': worker_count,
+        },
       }
 
       pants_run = self.run_pants(
@@ -75,12 +75,16 @@ class RscCompileIntegration(BaseCompileIT):
           'compile/rsc/current/.scala-library-synthetic/current/rsc/index/scala-library-synthetics.jar')
         self.assertTrue(os.path.exists(path))
 
-  def test_executing_multi_target_binary(self):
+  @_execution_strategies(['nailgun', 'subprocess'], [2])
+  def test_executing_multi_target_binary_nonhermetic(self, execution_strategy, worker_count):
     with temporary_dir() as cache_dir:
       config = {
         'cache.compile.rsc': {'write_to': [cache_dir]},
         'jvm-platform': {'compiler': 'rsc'},
-        'compile.rsc': {'execution_strategy': 'subprocess'}
+        'compile.rsc': {
+          'execution_strategy': execution_strategy,
+          'worker_count': worker_count,
+        }
       }
       with self.temporary_workdir() as workdir:
         pants_run = self.run_pants_with_workdir(
@@ -113,14 +117,16 @@ class RscCompileIntegration(BaseCompileIT):
         self.assert_success(pants_run)
         self.assertIn('Hello, Resource World!', pants_run.stdout_data)
 
-  def test_java_with_transitive_exported_scala_dep(self):
+  @_execution_strategies(['nailgun', 'subprocess'], [2])
+  def test_java_with_transitive_exported_scala_dep_nonhermetic(self, execution_strategy, worker_count):
     with temporary_dir() as cache_dir:
       config = {
         'cache.compile.rsc': {'write_to': [cache_dir]},
         'jvm-platform': {'compiler': 'rsc'},
         'compile.rsc': {
-          'execution_strategy': 'subprocess',
-          'worker_count': 1}
+          'execution_strategy': execution_strategy,
+          'worker_count': worker_count,
+        },
       }
       with self.temporary_workdir() as workdir:
         pants_run = self.run_pants_with_workdir(
