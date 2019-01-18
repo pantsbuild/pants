6893a87464ed8a06cad2921a4ca74a894f461cd2
Marcin Podolski <marcin.podolski@xain.io>
2019-01-18 10:22:13 +0100

    another fix test

--- src/python/pants/backend/codegen/grpcio/python/grpcio_prep.py
+++ src/python/pants/backend/codegen/grpcio/python/grpcio_prep.py
@@ -5,6 +5,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from pants.backend.codegen.grpcio.python.grpcio import Grpcio
+from pants.backend.codegen.grpcio.python.python_grpcio_library import PythonGrpcioLibrary
 from pants.backend.python.tasks.python_tool_prep_base import PythonToolInstance, PythonToolPrepBase
 
 
@@ -15,3 +16,10 @@ class GrpcioInstance(PythonToolInstance):
 class GrpcioPrep(PythonToolPrepBase):
   tool_subsystem_cls = Grpcio
   tool_instance_cls = GrpcioInstance
+
+  def execute(self):
+    targets = self.get_targets(lambda target: isinstance(target, PythonGrpcioLibrary))
+    if not targets:
+      return 0
+
+    super(GrpcioPrep, self).execute()
