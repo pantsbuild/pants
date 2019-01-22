165325d72cece59fd523a7139e0eb7e7b0361301
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-01-22 12:09:13 -0800

    remove testing for native code in a python_dist()

--- tests/python/pants_test/backend/python/tasks/test_build_local_python_distributions.py
+++ tests/python/pants_test/backend/python/tasks/test_build_local_python_distributions.py
@@ -10,8 +10,7 @@ from textwrap import dedent
 from twitter.common.collections import OrderedDict
 
 from pants.backend.python.targets.python_distribution import PythonDistribution
-from pants_test.backend.python.tasks.python_task_test_base import (check_wheel_platform_matches_host,
-                                                                   name_and_platform)
+from pants_test.backend.python.tasks.python_task_test_base import name_and_platform
 from pants_test.backend.python.tasks.util.build_local_dists_test_base import \
   BuildLocalPythonDistributionsTestBase
 
@@ -39,61 +38,6 @@ class TestBuildLocalDistsNativeSources(BuildLocalPythonDistributionsTestBase):
       }
     }),
 
-    ('src/python/plat_specific_dist:plat_specific_dist', {
-      'key': 'platform_specific',
-      'target_type': PythonDistribution,
-      'sources': ['__init__.py', 'setup.py', 'native_source.c'],
-      'filemap': {
-        'src/python/plat_specific_dist/__init__.py': '',
-        'src/python/plat_specific_dist/setup.py': dedent("""
-        from distutils.core import Extension
-        from setuptools import setup, find_packages
-        setup(
-          name='platform_specific_dist',
-          version='0.0.0',
-          packages=find_packages(),
-          extensions=[Extension('native_source', sources=['native_source.c'])]
-        )
-      """),
-        'src/python/plat_specific_dist/native_source.c': dedent("""
-        #include <Python.h>
-
-        static PyObject * native_source(PyObject *self, PyObject *args) {
-          return Py_BuildValue("s", "Hello from C!");
-        }
-
-        static PyMethodDef Methods[] = {
-          {"native_source", native_source, METH_VARARGS, ""},
-          {NULL, NULL, 0, NULL}
-        };
-
-        #if PY_MAJOR_VERSION >= 3
-          static struct PyModuleDef moduledef = {
-            PyModuleDef_HEAD_INIT,
-            "native_source", /* m_name */
-            NULL,            /* m_doc */
-            -1,              /* m_size */
-            Methods,         /* m_methods */
-            NULL,            /* m_slots */
-            NULL,            /* m_traverse */
-            NULL,            /* m_clear */
-            NULL             /* m_free */
-          };
-        #endif
-
-        #if PY_MAJOR_VERSION >= 3
-          PyMODINIT_FUNC PyInit_native_source(void) {
-            return PyModule_Create(&moduledef);
-          }
-        #else
-          PyMODINIT_FUNC initnative_source(void) {
-            (void) Py_InitModule("native_source", Methods);
-          }
-        #endif
-      """),
-      }
-    }),
-
   ])
 
   def test_python_create_universal_distribution(self):
@@ -107,15 +51,3 @@ class TestBuildLocalDistsNativeSources(BuildLocalPythonDistributionsTestBase):
     local_wheel = self._retrieve_single_product_at_target_base(local_wheel_products, universal_dist)
     _, _, wheel_platform = name_and_platform(local_wheel)
     self.assertEqual('any', wheel_platform)
-
-  def test_python_create_platform_specific_distribution(self):
-    platform_specific_dist = self.target_dict['platform_specific']
-    context, synthetic_target, snapshot_version = self._create_distribution_synthetic_target(
-      platform_specific_dist)
-    self.assertEqual(['platform_specific_dist==0.0.0+{}'.format(snapshot_version)],
-                      [str(x.requirement) for x in synthetic_target.requirements.value])
-
-    local_wheel_products = context.products.get('local_wheels')
-    local_wheel = self._retrieve_single_product_at_target_base(
-      local_wheel_products, platform_specific_dist)
-    self.assertTrue(check_wheel_platform_matches_host(local_wheel))
--- tests/python/pants_test/backend/python/tasks/test_python_distribution_integration.py
+++ tests/python/pants_test/backend/python/tasks/test_python_distribution_integration.py
@@ -18,79 +18,79 @@ from pants_test.testutils.py2_compat import assertRegex
 
 
 class PythonDistributionIntegrationTest(PantsRunIntegrationTest):
-  # The paths to both a project containing a simple C extension (to be packaged into a
-  # whl by setup.py) and an associated test to be consumed by the pants goals tested below.
-  fasthello_project = 'examples/src/python/example/python_distribution/hello/fasthello'
-  fasthello_tests = 'examples/tests/python/example/python_distribution/hello/test_fasthello'
-  fasthello_install_requires_dir = 'testprojects/src/python/python_distribution/fasthello_with_install_requires'
+  hello_install_requires_dir = 'testprojects/src/python/python_distribution/hello_with_install_requires'
   hello_setup_requires = 'examples/src/python/example/python_distribution/hello/setup_requires'
+  py_dist_test = 'examples/tests/python/example_test/python_distribution'
 
-  def _assert_native_greeting(self, output):
-    self.assertIn('Hello from C!', output)
-    self.assertIn('Hello from C++!', output)
+  def _assert_greeting(self, output):
+    self.assertEquals('hello!', output)
 
   def test_pants_binary(self):
     with temporary_dir() as tmp_dir:
       pex = os.path.join(tmp_dir, 'main.pex')
       command=[
-        '--pants-distdir={}'.format(tmp_dir), 'binary', '{}:main'.format(self.fasthello_project)]
+        '--pants-distdir={}'.format(tmp_dir), 'binary',
+        '{}:main_with_no_conflict'.format(self.hello_install_requires_dir),
+      ]
       pants_run = self.run_pants(command=command)
       self.assert_success(pants_run)
       # Check that the pex was built.
       self.assertTrue(os.path.isfile(pex))
       # Check that the pex runs.
       output = subprocess.check_output(pex).decode('utf-8')
-      self._assert_native_greeting(output)
+      self._assert_greeting(output)
       # Check that we have exactly one wheel output.
       single_wheel_output = assert_single_element(glob.glob(os.path.join(tmp_dir, '*.whl')))
       assertRegex(self, os.path.basename(single_wheel_output),
-                               r'\A{}'.format(re.escape('fasthello-1.0.0+')))
+                               r'\A{}'.format(re.escape('hello_with_install_requires-1.0.0+')))
 
   def test_pants_run(self):
     with temporary_dir() as tmp_dir:
       command=[
         '--pants-distdir={}'.format(tmp_dir),
         'run',
-        '{}:main'.format(self.fasthello_project)]
+        '{}:main_with_no_conflict'.format(self.hello_install_requires_dir)]
       pants_run = self.run_pants(command=command)
       self.assert_success(pants_run)
       # Check that text was properly printed to stdout.
-      self._assert_native_greeting(pants_run.stdout_data)
+      self._assert_greeting(pants_run.stdout_data)
 
-  def test_invalidation(self):
+  def test_pydist_invalidation(self):
     """Test that the current version of a python_dist() is resolved after modifying its sources."""
-    fasthello_run = '{}:main_with_no_conflict'.format(self.fasthello_install_requires_dir)
+    hello_run = '{}:main_with_no_conflict'.format(self.hello_install_requires_dir)
 
     with self.mock_buildroot(
-        dirs_to_copy=[self.fasthello_install_requires_dir]) as buildroot, buildroot.pushd():
+        dirs_to_copy=[self.hello_install_requires_dir]) as buildroot, buildroot.pushd():
       run_target = lambda: self.run_pants_with_workdir(
-        command=['run', fasthello_run],
+        command=['--quiet', 'run', hello_run],
         workdir=os.path.join(buildroot.new_buildroot, '.pants.d'),
         build_root=buildroot.new_buildroot,
       )
 
       unmodified_pants_run = run_target()
       self.assert_success(unmodified_pants_run)
-      self.assertIn('Hello from C!\n', unmodified_pants_run.stdout_data)
+      self.assertEquals('hello!', unmodified_pants_run.stdout_data)
 
       # Modify one of the source files for this target so that the output is different.
-      c_source_file = os.path.join(self.fasthello_install_requires_dir, 'c_greet.c')
-      with open(c_source_file, 'r') as f:
+      py_source_file = os.path.join(
+        self.hello_install_requires_dir, 'hello_package/hello.py')
+      with open(py_source_file, 'r') as f:
         orig_contents = f.read()
-      modified_contents = re.sub('"Hello from C!"', '"Hello from C?"', orig_contents)
-      with open(c_source_file, 'w') as f:
+      modified_contents = re.sub('!', '?', orig_contents)
+      with open(py_source_file, 'w') as f:
         f.write(modified_contents)
 
       modified_pants_run = run_target()
       self.assert_success(modified_pants_run)
-      self.assertIn('Hello from C?\n', modified_pants_run.stdout_data)
+      self.assertEquals('hello?', modified_pants_run.stdout_data)
 
   def test_pants_test(self):
     with temporary_dir() as tmp_dir:
       command=[
         '--pants-distdir={}'.format(tmp_dir),
         'test',
-        '{}:fasthello'.format(self.fasthello_tests)]
+        self.py_dist_test,
+      ]
       pants_run = self.run_pants(command=command)
       self.assert_success(pants_run)
       # Make sure that there is no wheel output when 'binary' goal is not invoked.
@@ -102,23 +102,23 @@ class PythonDistributionIntegrationTest(PantsRunIntegrationTest):
       command=[
         '--pants-distdir={}'.format(tmp_dir),
         'run',
-        '{}:main_with_no_conflict'.format(self.fasthello_install_requires_dir)]
+        '{}:main_with_no_conflict'.format(self.hello_install_requires_dir)]
       pants_run = self.run_pants(command=command)
       self.assert_success(pants_run)
       self.assertIn('United States', pants_run.stdout_data)
-      command=['binary', '{}:main_with_no_conflict'.format(self.fasthello_install_requires_dir)]
+      command=['binary', '{}:main_with_no_conflict'.format(self.hello_install_requires_dir)]
       pants_run = self.run_pants(command=command)
       self.assert_success(pants_run)
       output = subprocess.check_output(pex).decode('utf-8')
       self.assertIn('United States', output)
 
   def test_with_conflicting_transitive_deps(self):
-    command=['run', '{}:main_with_conflicting_dep'.format(self.fasthello_install_requires_dir)]
+    command=['run', '{}:main_with_conflicting_dep'.format(self.hello_install_requires_dir)]
     pants_run = self.run_pants(command=command)
     self.assert_failure(pants_run)
     self.assertIn('pycountry', pants_run.stderr_data)
     self.assertIn('fasthello', pants_run.stderr_data)
-    command=['binary', '{}:main_with_conflicting_dep'.format(self.fasthello_install_requires_dir)]
+    command=['binary', '{}:main_with_conflicting_dep'.format(self.hello_install_requires_dir)]
     pants_run = self.run_pants(command=command)
     self.assert_failure(pants_run)
     self.assertIn('pycountry', pants_run.stderr_data)
@@ -131,8 +131,8 @@ class PythonDistributionIntegrationTest(PantsRunIntegrationTest):
       command=[
         '--pants-distdir={}'.format(tmp_dir),
         'binary',
-        '{}:main_with_no_conflict'.format(self.fasthello_install_requires_dir),
-        '{}:main_with_no_pycountry'.format(self.fasthello_install_requires_dir)]
+        '{}:main_with_no_conflict'.format(self.hello_install_requires_dir),
+        '{}:main_with_no_pycountry'.format(self.hello_install_requires_dir)]
       pants_run = self.run_pants(command=command)
       self.assert_success(pants_run)
       # Check that the pex was built.
@@ -140,53 +140,12 @@ class PythonDistributionIntegrationTest(PantsRunIntegrationTest):
       self.assertTrue(os.path.isfile(pex2))
       # Check that the pex 1 runs.
       output = subprocess.check_output(pex1).decode('utf-8')
-      self._assert_native_greeting(output)
+      self._assert_greeting(output)
       # Check that the pex 2 fails due to no python_dists leaked into it.
       try:
         subprocess.check_output(pex2)
       except subprocess.CalledProcessError as e:
         self.assertNotEqual(0, e.returncode)
-
-  def test_pants_resolves_local_dists_for_current_platform_only(self):
-    # Test that pants will override pants.ini platforms config when building
-    # or running a target that depends on native (c or cpp) sources.
-    with temporary_dir() as tmp_dir:
-      pex = os.path.join(tmp_dir, 'main.pex')
-      pants_ini_config = {
-        'python-setup': {
-          # If no targets exist declaring the nonexistent platform, this should be reduced to just
-          # ['current'] in PythonNativeCode#check_build_for_current_platform_only().
-          'platforms': ['current', 'this-platform-does_not-exist'],
-        },
-      }
-      command=[
-        '--pants-distdir={}'.format(tmp_dir),
-        'run',
-        '{}:main'.format(self.fasthello_project)]
-      pants_run = self.run_pants(command=command, config=pants_ini_config)
-      self.assert_success(pants_run)
-
-      command=['binary', '{}:main'.format(self.fasthello_project)]
-      pants_run = self.run_pants(command=command, config=pants_ini_config)
-      self.assert_success(pants_run)
-      # Check that the pex was built.
-      self.assertTrue(os.path.isfile(pex))
-      # Check that the pex runs.
-      output = subprocess.check_output(pex).decode('utf-8')
-      self._assert_native_greeting(output)
-
-  def test_pants_tests_local_dists_for_current_platform_only(self):
-    with temporary_dir() as tmp_dir:
-      command=[
-        '--pants-distdir={}'.format(tmp_dir),
-        'test',
-        '{}:fasthello'.format(self.fasthello_tests)]
-      pants_run = self.run_pants(command=command, config={
-        'python-setup': {
-          'platforms': ['current'],
-        },
-      })
-      self.assert_success(pants_run)
 
   def test_python_distribution_with_setup_requires(self):
     # Validate that setup_requires dependencies are present and functional.
