2c4b197db7327f05fda88a3adda8e949ce081412
Eric Arellano <ericarellano@me.com>
2019-01-23 01:11:29 -0700

    Add back failing targets & redistribute CI
    It looks like this PR will fix a couple failing targets! (Which I wasn't expecting because they timeout locally...)
    
    Multiple targets are still failing for other reasons, which we leave off.

--- build-support/travis/generate_travis_yml.py
+++ build-support/travis/generate_travis_yml.py
@@ -8,8 +8,8 @@ import pkg_resources
 import pystache
 
 
-num_py3_integration_shards = 20
-num_py2_blacklist_integration_shards = 0
+num_py3_integration_shards = 15
+num_py2_blacklist_integration_shards = 5
 num_cron_integration_shards = 20
 
 
