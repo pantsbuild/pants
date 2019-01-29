2643b0a77010da29bd3f1e0797015420a44750df
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-01-29 11:28:21 -0800

    encode into utf-8, and don't wrap the output

--- examples/src/python/example/tensorflow_custom_op/zero_out_custom_op.py
+++ examples/src/python/example/tensorflow_custom_op/zero_out_custom_op.py
@@ -5,7 +5,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-from future.utils import binary_type
 # TODO: It would be great if we could maintain the example.tensorflow_custom_op package prefix for
 # this python_dist()!
 from wrap_lib.wrap_zero_out_op import zero_out_op_lib_path
@@ -13,4 +12,4 @@ from wrap_lib.wrap_zero_out_op import zero_out_op_lib_path
 
 # We make this a function in order to lazily load the op library.
 def zero_out_module():
-  return tf.load_op_library(binary_type(zero_out_op_lib_path.encode()))
+  return tf.load_op_library(zero_out_op_lib_path.encode('utf-8'))
