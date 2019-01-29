8e23c814b763805bc285c5804e29da1e55a98fef
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-01-29 11:55:12 -0800

    fix tensorflow py2/3 divergence

--- examples/src/python/example/tensorflow_custom_op/zero_out_custom_op.py
+++ examples/src/python/example/tensorflow_custom_op/zero_out_custom_op.py
@@ -5,6 +5,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
+from future.utils import PY3
 # TODO: It would be great if we could maintain the example.tensorflow_custom_op package prefix for
 # this python_dist()!
 from wrap_lib.wrap_zero_out_op import zero_out_op_lib_path
@@ -12,4 +13,8 @@ from wrap_lib.wrap_zero_out_op import zero_out_op_lib_path
 
 # We make this a function in order to lazily load the op library.
 def zero_out_module():
-  return tf.load_op_library(zero_out_op_lib_path.encode('utf-8'))
+  if PY3:
+    encoded_op_lib_path = zero_out_op_lib_path
+  else:
+    encoded_op_lib_path = zero_out_op_lib_path.encode()
+  return tf.load_op_library(encoded_op_lib_path)
