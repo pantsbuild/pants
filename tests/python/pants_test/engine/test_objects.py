2c68ba7a537999a2d24110d6ecd4b20854b08d26
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-02-07 10:59:22 -0800

    fix py3 compat in the other test_objects.py

--- tests/python/pants_test/engine/test_objects.py
+++ tests/python/pants_test/engine/test_objects.py
@@ -20,12 +20,14 @@ class CollectionTest(TestBase):
   def test_element_typechecking(self):
     IntColl = Collection.of(int)
     with self.assertRaisesRegexp(TypeCheckError, re.escape("""\
-field 'dependencies' was invalid: in wrapped constraint TypedCollection(Exactly(int)) matching iterable object [3, {u}'hello']: value {u}'hello' (with type 'unicode') must satisfy this type constraint: Exactly(int)."""
-                                                           .format(u='' if PY3 else 'u'))):
+field 'dependencies' was invalid: in wrapped constraint TypedCollection(Exactly(int)) matching iterable object [3, {u}'hello']: value {u}'hello' (with type '{string_type}') must satisfy this type constraint: Exactly(int)."""
+                                                           .format(u='' if PY3 else 'u',
+                                                                   string_type='str' if PY3 else 'unicode'))):
       IntColl([3, "hello"])
 
     IntOrStringColl = Collection.of(int, text_type)
     self.assertEqual([3, "hello"], [x for x in IntOrStringColl([3, "hello"])])
     with self.assertRaisesRegexp(TypeCheckError, re.escape("""\
-field 'dependencies' was invalid: in wrapped constraint TypedCollection(Exactly(int or unicode)) matching iterable object [{}]: value {} (with type 'dict') must satisfy this type constraint: Exactly(int or unicode).""")):
-      IntOrStringColl([{}])
+field 'dependencies' was invalid: in wrapped constraint TypedCollection(Exactly(int or {string_type})) matching iterable object [()]: value () (with type 'tuple') must satisfy this type constraint: Exactly(int or {string_type})."""
+                                                           .format(string_type='str' if PY3 else 'unicode'))):
+      IntOrStringColl([()])
