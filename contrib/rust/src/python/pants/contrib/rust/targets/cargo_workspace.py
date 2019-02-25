63ddb59466bf889c64cebbe953ddf1b0e1ab1430
Robert Steiner <robert.steiner@xain.io>
2019-02-25 15:11:43 +0100

    get member names and paths from manifest
    - member names and paths are automatically resolved by interpreting the workspace manifest
    - the list of member names in the BUILD file is no longer necessary

--- contrib/rust/src/python/pants/contrib/rust/targets/cargo_workspace.py
+++ contrib/rust/src/python/pants/contrib/rust/targets/cargo_workspace.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os
 
+import toml
 from pants.base.build_environment import get_buildroot
 from pants.base.payload import Payload
 from pants.base.payload_field import PrimitiveField
@@ -16,18 +17,24 @@ from pants.contrib.rust.targets.cargo_target import CargoTarget
 class CargoWorkspace(CargoTarget):
   """A class for a cargo workspace target."""
 
-  def __init__(self, address=None, manifest=None, toolchain=None, members=None, include=None,
+  def __init__(self, address=None, manifest=None, toolchain=None, include=None,
                payload=None, **kwargs):
     if manifest is not None:
       manifest = os.path.join(get_buildroot(), address.spec_path, manifest)
+    else:
+      manifest = os.path.join(get_buildroot(), address.spec_path)
 
     if toolchain is not None:
       toolchain = os.path.join(get_buildroot(), address.spec_path, toolchain)
+    else:
+      toolchain = os.path.join(get_buildroot(), address.spec_path)
 
     payload = payload or Payload()
 
-    member_paths = list(map(lambda member: os.path.join(address.spec_path, member), members))
-    payload.add_field('member_names', PrimitiveField(members))
+    member_paths = self.get_member_paths(manifest)
+    member_names = self.get_member_names(manifest, member_paths)
+
+    payload.add_field('member_names', PrimitiveField(member_names))
     payload.add_field('member_paths', PrimitiveField(member_paths))
 
     payload.add_field('include_sources', PrimitiveField(include))
@@ -46,3 +53,19 @@ class CargoWorkspace(CargoTarget):
   @property
   def include_sources(self):
     return self.payload.get_field_value('include_sources')
+
+  def get_member_paths(self, workspace_manifest):
+    workspace_toml_path = os.path.join(workspace_manifest, 'Cargo.toml')
+    workspace_toml = toml.load(workspace_toml_path)
+    member_paths = workspace_toml['workspace']['members']
+
+    return member_paths
+
+  def get_member_names(self, workspace_manifest, member_paths):
+    member_names = []
+    for member_path in member_paths:
+      member_toml_path = os.path.join(workspace_manifest, member_path, 'Cargo.toml')
+      member_toml = toml.load(member_toml_path)
+      member_names.append(member_toml['package']['name'])
+
+    return member_names
