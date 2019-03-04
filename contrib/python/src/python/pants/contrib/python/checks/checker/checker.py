933c43d210876f95b9af8b6e0757294a34b228b6
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-03-04 00:20:33 -0800

    add the implicit concatenation checker, but suppress it in this repo

--- contrib/python/src/python/pants/contrib/python/checks/checker/checker.py
+++ contrib/python/src/python/pants/contrib/python/checks/checker/checker.py
@@ -17,10 +17,8 @@ from pants.contrib.python.checks.checker.constant_logic import ConstantLogic
 from pants.contrib.python.checks.checker.except_statements import ExceptStatements
 from pants.contrib.python.checks.checker.file_excluder import FileExcluder
 from pants.contrib.python.checks.checker.future_compatibility import FutureCompatibility
-# TODO(#7307): figure out how to selectively turn on and off checkers with a pants option, since
-# this will make a lot of noise if turned on in this repo!
-# from pants.contrib.python.checks.checker.implicit_string_concatenation import \
-#   ImplicitStringConcatenation
+from pants.contrib.python.checks.checker.implicit_string_concatenation import \
+  ImplicitStringConcatenation
 from pants.contrib.python.checks.checker.import_order import ImportOrder
 from pants.contrib.python.checks.checker.indentation import Indentation
 from pants.contrib.python.checks.checker.missing_contextmanager import MissingContextManager
@@ -136,9 +134,7 @@ def plugins():
     ConstantLogic,
     ExceptStatements,
     FutureCompatibility,
-    # TODO(#7307): figure out how to selectively turn on and off checkers with a pants option, since
-    # this will make a lot of noise if turned on in this repo!
-    # ImplicitStringConcatenation,
+    ImplicitStringConcatenation,
     ImportOrder,
     Indentation,
     MissingContextManager,
