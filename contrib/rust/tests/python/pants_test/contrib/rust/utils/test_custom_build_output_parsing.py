987f5fba62471833b1b35086bc1ba4f61b3a844e
Robert Steiner <robert.steiner@xain.io>
2019-03-05 18:04:02 +0100

    fix parsing tests

--- contrib/rust/tests/python/pants_test/contrib/rust/utils/test_custom_build_output_parsing.py
+++ contrib/rust/tests/python/pants_test/contrib/rust/utils/test_custom_build_output_parsing.py
@@ -19,14 +19,11 @@ build_output = [
   'cargo:rustc-flags=-l static=samplerate',
   'cargo:rustc-flags= -l   dylib=stdc++  ',
   'cargo:rustc-flags=-l static=pfring -L /usr/local/lib',
-  'cargo:rustc-flags= -l   static=pcap  -L   /usr/local/lib  '
-]
-
-unsupported_output = [
+  'cargo:rustc-flags= -l   static=pcap  -L   /usr/local/lib  ',
   'cargo:warning=/lmdb-sys/lmdb/libraries/liblmdb/mdb.c:10033:33: warning: unused parameter [-Wunused-parameter]',
   'cargo:warning=mdb_env_get_maxkeysize(MDB_env *env)',
   'cargo:warning=                                ^',
-  'cargo:warning=1 warning generated.'
+  'cargo:warning=1 warning generated.',
 ]
 
 
@@ -90,6 +87,13 @@ class UtilsTest(unittest.TestCase):
           '--cfg',
           'memchr_runtime_sse42'
         ]
+      ],
+      'warning': [
+        '/lmdb-sys/lmdb/libraries/liblmdb/mdb.c:10033:33: warning: unused parameter [-Wunused-parameter]',
+        'mdb_env_get_maxkeysize(MDB_env *env)',
+        '                                ^',
+        '1 warning generated.'
+      ],
       ]
     }
     statements = parse_multiple_cargo_statements(output)
