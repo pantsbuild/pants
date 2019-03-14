64de417993420a099d3eb994a3526b78b475a067
Eric Arellano <ericarellano@me.com>
2019-03-14 07:13:36 -0700

    Use conditional ternary
    Good suggestion Daniel!

--- tests/python/pants_test/backend/python/interpreter_selection_utils.py
+++ tests/python/pants_test/backend/python/interpreter_selection_utils.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 from unittest import skipIf
 
-from future.utils import PY2
+from future.utils import PY3
 
 from pants.util.process_handler import subprocess
 
@@ -48,13 +48,11 @@ def python_interpreter_path(version):
   :returns: the normalized path to the interpreter binary if found; otherwise `None`
   :rtype: string
   """
-  if PY2:
-    FileNotFoundError = IOError
   try:
     command = ['python{}'.format(version), '-c', 'import sys; print(sys.executable)']
     py_path = subprocess.check_output(command).decode('utf-8').strip()
     return os.path.realpath(py_path)
-  except (subprocess.CalledProcessError, FileNotFoundError):
+  except (subprocess.CalledProcessError, FileNotFoundError if PY3 else IOError):
     return None
 
 
