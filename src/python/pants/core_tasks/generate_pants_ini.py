f85a35a5a356679a1c71bd77f383975592afc4c8
Eric Arellano <ericarellano@me.com>
2019-03-28 00:29:39 -0700

    Round 1 of responding to John's suggestions
    * Simply write directly to the file, rather than using configparser. Using configparser is a vestige of when this task originally allowed running even if pants.ini was populated, so needed to keep the original values. Now that we only generate new pants.ini, this is not necessary.
    * Import pants.version.VERSION as pants_version.
    * Use self.context.log. I'll look into making this a ConsoleTask. For now, this is an improvement.

--- src/python/pants/core_tasks/generate_pants_ini.py
+++ src/python/pants/core_tasks/generate_pants_ini.py
@@ -4,17 +4,12 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import configparser
-import logging
 import os
 import sys
 
 from pants.base.exceptions import TaskError
 from pants.task.task import Task
-from pants.version import VERSION
-
-
-logger = logging.getLogger(__name__)
+from pants.version import VERSION as pants_version
 
 
 class GeneratePantsIni(Task):
@@ -22,39 +17,25 @@ class GeneratePantsIni(Task):
 
   PANTS_INI = "pants.ini"
 
-  class CustomConfigParser(configparser.ConfigParser):
-    """We monkey-patch the write() function to allow us to write entries in the style `key: value`, 
-       rather than `key:value` or `key : value`."""
-
-    def write(self, fp):
-      # Refer to https://github.com/jaraco/configparser/blob/294a985b759bddd3505ace54ecf56da39c56a302/src/backports/configparser/__init__.py#L916
-      # for the original source.
-      delimiter = ": "
-      if self._defaults:
-        self._write_section(fp, self.default_section, self._defaults.items(), delimiter)
-      for section in self._sections:
-        self._write_section(fp, section, self._sections[section].items(), delimiter)
-
   def execute(self):
     if os.stat(self.PANTS_INI).st_size != 0:
       raise TaskError("{} is not empty! This goal is only meant for first-time "
                       "users. Please update config values by directly modifying the file.".format(self.PANTS_INI))
 
-    pants_version = VERSION
     python_version = ".".join(str(v) for v in sys.version_info[:2])
 
-    config = self.CustomConfigParser()
-    logger.info("Adding sensible defaults to the `{}` config file:".format(self.PANTS_INI))
+    self.context.log.info("Adding sensible defaults to the `{}` config file:".format(self.PANTS_INI))
+    self.context.log.info("* Pinning `pants_version` to `{}`.".format(pants_version))
+    self.context.log.info("* Pinning `pants_runtime_python_version` to `{}`.".format(python_version))
 
-    logger.info("* Pinning `pants_version` to `{}`.".format(pants_version))
-    config["GLOBAL"] = {"pants_version": pants_version}
-    if python_version is not None:
-      logger.info("* Pinning `pants_runtime_python_version` to `{}`.".format(python_version))
-      config["GLOBAL"]["pants_runtime_python_version"] = python_version
+    with open(self.PANTS_INI, "w") as f:
+      f.write("""\
+        [GLOBAL]
+        pants_version: {}
+        pants_runtime_python_version: {}
+        """.format(pants_version, python_version)
+      )
 
-    with open(self.PANTS_INI, 'w') as f:
-      config.write(f)
-
-    logger.info("{} is now set up. You may modify its values directly in the file at any time, and "
-                "the ./pants script will detect the changes for you.".format(self.PANTS_INI))
-    logger.info("You are now ready to use Pants!")
+    self.context.log.info("{} is now set up. You may modify its values directly in the file at any time, and "
+                          "the ./pants script will detect the changes for you.".format(self.PANTS_INI))
+    self.context.log.info("You are now ready to use Pants!")
