4da38000d18e928adea2578883f08833f8dac461
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-03-28 15:17:00 -0700

    remove the conditional skipIf after adding the travis yml workaround

--- examples/tests/python/example_test/tensorflow_custom_op/test_zero_out_op.py
+++ examples/tests/python/example_test/tensorflow_custom_op/test_zero_out_op.py
@@ -4,9 +4,6 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import sys
-import unittest
-
 import tensorflow as tf
 
 from example.tensorflow_custom_op.zero_out_custom_op import zero_out_module
@@ -15,7 +12,6 @@ from example.tensorflow_custom_op.zero_out_custom_op import zero_out_module
 # This code is from the guide in https://www.tensorflow.org/guide/extend/op.
 class ZeroOutTest(tf.test.TestCase):
 
-  @unittest.skipIf(sys.version_info[0:2] == (3, 7), "See https://github.com/pantsbuild/pants/issues/7417.")
   def test_zero_out(self):
     with self.cached_session():
       result = zero_out_module().zero_out([5, 4, 3, 2, 1])
