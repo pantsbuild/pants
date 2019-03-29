51f9948be266c6486219a98d8b2d3847a4af1e9a
Troy Howard <thoward37@gmail.com>
2019-03-29 13:31:28 -0700

    doc.scaladoc uses option file if args longer that MAX_ARG_STRLEN

--- src/python/pants/backend/jvm/tasks/scaladoc_gen.py
+++ src/python/pants/backend/jvm/tasks/scaladoc_gen.py
@@ -4,11 +4,15 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import os
+
 from pants.backend.jvm.subsystems.scala_platform import ScalaPlatform
 from pants.backend.jvm.tasks.jvmdoc_gen import Jvmdoc, JvmdocGen
 from pants.java.distribution.distribution import DistributionLocator
 from pants.java.executor import SubprocessExecutor
 from pants.util.memo import memoized
+
+MAX_ARG_STRLEN = 131072
 
 
 class ScaladocGen(JvmdocGen):
@@ -59,9 +63,46 @@ class ScaladocGen(JvmdocGen):
 
     args.extend(sources)
 
+    # Check the size of the ScalaDoc command arguments, and use a options file if too long.
+    #
+    # There are a couple practical limits to consider.
+    #
+    # - The length of any individual value must be less than MAX_ARG_STRLEN
+    #   which is hardcoded to 131072 bytes.
+    #
+    # - The total length of the command must be less than the OS's `getconf ARG_MAX`
+    #   value.
+    #
+    #   This value will vary by system. For exmaple, on my MacOS 10.13.6 Twitter-issued
+    #   developer laptop this value is currently 262144. Doing some research on the
+    #   internet shows a wide range of default values for this ranging from 131072 to
+    #   2097152 (2MB). Unfortunately, there doesn't seem to be an easy way to query the
+    #   system for this value. It's not in sysconfig.get_config_vars, or posix.environ,
+    #   or os.environ. We could shell out to `getconf` to get the actual value, but
+    #   anecdotal evidence suggests that there are other layers of limits beyond just
+    #   this POSIX value. POSIX requires ARG_MAX to be at least 4096 bytes, but it's
+    #   extremely unlikely to find a system with that low setting. Rather, I think a
+    #   practical middle ground would be the MAX_ARG_STRLEN fixed value.
+    #
+    #   So, given that rationale, if the arg list is longer than MAX_ARG_STRLEN
+    #   we should put it in an external file, just in case.
+
+    args_joined = ' '.join(map(str, args))
+    if len(args_joined) > MAX_ARG_STRLEN:
+      self.context.log.debug("ScalaDoc arguments too long. Using options file.")
+
+      # dump to file, overwriting any file that already exists
+      options_filepath = os.path.join(self.workdir, 'scaladoc_options.txt')
+      with open(options_filepath, 'w') as filehandle:
+        filehandle.write(args_joined)
+
+      args = ['@{}'.format(options_filepath)]
+
     java_executor = SubprocessExecutor(DistributionLocator.cached())
     runner = java_executor.runner(jvm_options=self.jvm_options,
                                   classpath=tool_classpath,
                                   main='scala.tools.nsc.ScalaDoc',
                                   args=args)
+
+    self.context.log.debug("SCALADOCS COMMAND: {}".format(runner.command))
     return runner.command
