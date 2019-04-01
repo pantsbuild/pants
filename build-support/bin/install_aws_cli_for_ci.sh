ea418b78fd31ca0705174ef42cb6430f3e10f926
Eric Arellano <ericarellano@me.com>
2019-03-31 19:47:03 -0700

    Can't use the folder /usr/local/aws for cache with Linux
    Travis runs into a permissions issue when doing so: https://travis-ci.org/pantsbuild/pants/jobs/513927894#L432.
    
    Further, the only reason we were doing this is the tutorial used this install dir.
    
    Instead, use a custom install dir. Our symlink makes it so that it doesn't really matter where we install it.

--- build-support/bin/install_aws_cli_for_ci.sh
+++ build-support/bin/install_aws_cli_for_ci.sh
@@ -9,11 +9,15 @@ set -euo pipefail
 # to use, which slows down CI jobs significantly. This is also the installation method recommended
 # by AWS, see https://docs.aws.amazon.com/cli/latest/userguide/install-bundle.html.
 
-INSTALL_DIR="/usr/local/aws"
-AWS_BIN="${INSTALL_DIR}/bin/aws"
+source build-support/common.sh
+
+if [[ -z "${AWS_CLI_ROOT}" ]]; then
+  die "Caller of the script must set the env var AWS_CLI_ROOT."
+fi
+AWS_CLI_BIN="${AWS_CLI_ROOT}/bin/aws"
 
 # We first check if AWS CLI is already installed thanks to Travis's cache.
-if [[ ! -x "${AWS_BIN}" ]]; then
+if [[ ! -x "${AWS_CLI_BIN}" ]]; then
 
   TMPDIR=$(mktemp -d)
 
@@ -21,7 +25,7 @@ if [[ ! -x "${AWS_BIN}" ]]; then
 
   curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
   unzip awscli-bundle.zip
-  sudo ./awscli-bundle/install --install-dir "${INSTALL_DIR}"
+  sudo ./awscli-bundle/install --install-dir "${AWS_CLI_ROOT}"
 
   popd
 
@@ -29,7 +33,7 @@ fi
 
 # Travis does not cache symlinks (https://docs.travis-ci.com/user/caching/), so we create
 # the symlink ourselves everytime to ensure `aws` is discoverable globally.
-sudo ln -s "${AWS_BIN}" /usr/local/bin/aws
+sudo ln -s "${AWS_CLI_BIN}" /usr/local/bin/aws
 
 # Multipart operations aren't supported for anonymous users, so we set the
 # threshold high to avoid them being used automatically by the aws cli.
