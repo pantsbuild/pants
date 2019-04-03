3974f16cbf0c32f7e24479f2a64d07399e20b6e6
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-04-03 14:11:14 -0700

    convert --skip to --strict in py thrift namespace clash check

--- tests/python/pants_test/backend/codegen/thrift/python/test_py_thrift_namespace_clash_check.py
+++ tests/python/pants_test/backend/codegen/thrift/python/test_py_thrift_namespace_clash_check.py
@@ -4,9 +4,12 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from builtins import str
+
 from pants.backend.codegen.thrift.python.py_thrift_namespace_clash_check import \
   PyThriftNamespaceClashCheck
 from pants.backend.codegen.thrift.python.python_thrift_library import PythonThriftLibrary
+from pants.util.dirutil import read_file
 from pants_test.task_test_base import DeclarativeTaskTestMixin, TaskTestBase
 
 
@@ -38,7 +41,7 @@ struct A {}
       'sources': ['bad.thrift'],
       'filemap': {
         'bad.thrift': """\
-#namespace scala org.pantsbuild.whatever
+#@namespace scala org.pantsbuild.whatever
 namespace java org.pantsbuild.whatever
 
 struct A {}
@@ -88,43 +91,54 @@ struct B {}
     }
   }
 
-  def target_dict(self):
+  def _target_dict(self):
     return self.populate_target_dict(self._target_specs)
+
+  def _run_tasks(self, target_roots):
+    self.set_options(strict=True)
+    return self.invoke_tasks(target_roots=target_roots)
 
   _exception_prelude = """\
 Clashing namespaces for python thrift library sources detected in build graph. This will silently
 overwrite previously generated python sources with generated sources from thrift files declaring the
 same python namespace. This is an upstream WONTFIX in thrift, see:
       https://issues.apache.org/jira/browse/THRIFT-515
-
-Use --test_scope-skip to avoid this check if this breaks your existing build.
 Errors:"""
 
   def test_no_py_namespace(self):
-    no_py_namespace_target = self.target_dict()['no-py-namespace']
-    with self.assertRaisesWithMessage(PyThriftNamespaceClashCheck.NamespaceParseError, """\
-no python namespace (matching the pattern '^namespace\s+py\s+([^\s]+)$') \
-found in thrift source src/py-thrift/bad.thrift from target src/py-thrift:no-py-namespace!"""):
-      self.invoke_tasks(target_roots=[no_py_namespace_target])
+    no_py_namespace_target = self._target_dict()['no-py-namespace']
+    with self.assertRaises(PyThriftNamespaceClashCheck.NamespaceExtractionError) as cm:
+      self._run_tasks(target_roots=[no_py_namespace_target])
+    self.assertEqual(str(cm.exception), """\
+Python namespaces could not be extracted from some thrift sources. Declaring a `namespace py` in
+thrift sources for python thrift library targets will soon become required.
+
+1 python library target(s) contained thrift sources not declaring a python namespace. The targets
+and/or files which need to be edited will be dumped to: {}
+"""
+                     .format(cm.exception.output_file))
+    self.assertEqual(
+      'src/py-thrift:no-py-namespace: [src/py-thrift/bad.thrift]\n',
+      read_file(cm.exception.output_file, binary_mode=False))
 
   def test_clashing_namespace_same_target(self):
-    clashing_same_target = self.target_dict()['clashing-namespace']
+    clashing_same_target = self._target_dict()['clashing-namespace']
     with self.assertRaisesWithMessage(PyThriftNamespaceClashCheck.ClashingNamespaceError, """{}
 org.pantsbuild.namespace: [(src/py-thrift:clashing-namespace, src/py-thrift/a.thrift), (src/py-thrift:clashing-namespace, src/py-thrift/b.thrift)]
 """.format(self._exception_prelude)):
-      self.invoke_tasks(target_roots=[clashing_same_target])
+      self._run_tasks(target_roots=[clashing_same_target])
 
   def test_clashing_namespace_multiple_targets(self):
-    target_dict = self.target_dict()
+    target_dict = self._target_dict()
     clashing_targets = [target_dict[k] for k in ['clashingA', 'clashingB']]
     with self.assertRaisesWithMessage(PyThriftNamespaceClashCheck.ClashingNamespaceError, """{}
 org.pantsbuild.namespace: [(src/py-thrift-clashing:clashingA, src/py-thrift-clashing/a.thrift), (src/py-thrift-clashing:clashingB, src/py-thrift-clashing/b.thrift)]
 """.format(self._exception_prelude)):
-      self.invoke_tasks(target_roots=clashing_targets)
+      self._run_tasks(target_roots=clashing_targets)
 
   def test_accepts_py_namespace_with_comments_above(self):
-    commented_thrift_source_target = self.target_dict()['with-comments-and-other-namespaces']
-    result = self.invoke_tasks(target_roots=[commented_thrift_source_target])
+    commented_thrift_source_target = self._target_dict()['with-comments-and-other-namespaces']
+    result = self._run_tasks(target_roots=[commented_thrift_source_target])
     # Check that the file was correctly mapped to the namespace parsed out of its file content.
     namespaces_by_files = result.context.products.get_data('_py_thrift_namespaces_by_files')
     self.assertEqual(
