f57721085e70912109c81588b9e691ebaa727db6
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-04-04 11:46:45 -0700

    allow python tools to specify a noop predicate and use it for conan

--- src/python/pants/backend/native/tasks/conan_prep.py
+++ src/python/pants/backend/native/tasks/conan_prep.py
@@ -5,6 +5,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from pants.backend.native.subsystems.conan import Conan
+from pants.backend.native.targets.external_native_library import ExternalNativeLibrary
 from pants.backend.python.tasks.python_tool_prep_base import PythonToolInstance, PythonToolPrepBase
 
 
@@ -15,3 +16,6 @@ class ConanInstance(PythonToolInstance):
 class ConanPrep(PythonToolPrepBase):
   tool_subsystem_cls = Conan
   tool_instance_cls = ConanInstance
+
+  def will_be_invoked(self):
+    return len(self.get_targets(lambda t: isinstance(t, ExternalNativeLibrary))) > 0
