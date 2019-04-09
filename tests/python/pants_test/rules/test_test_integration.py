4bbb39e84c491cdc9d442623013e08d9d794d086
Borja Lorente <bescobar@twitter.com>
2019-04-09 13:48:17 +0100

    Un-blacklist some tests

--- tests/python/pants_test/rules/test_test_integration.py
+++ tests/python/pants_test/rules/test_test_integration.py
@@ -4,7 +4,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from pants_test.pants_run_integration_test import PantsRunIntegrationTest, daemon_blacklist
+from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 
 class TestIntegrationTest(PantsRunIntegrationTest):
