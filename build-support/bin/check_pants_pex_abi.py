a685f09d6378410d5136fec44e3a2d6346e42b58
Benjy Weinberger <benjyw@gmail.com>
2019-04-11 11:11:10 -0700

    See if this fixes CI.

--- build-support/bin/check_pants_pex_abi.py
+++ build-support/bin/check_pants_pex_abi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # coding=utf-8
 # Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
