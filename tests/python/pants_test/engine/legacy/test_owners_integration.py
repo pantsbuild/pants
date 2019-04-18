930a23b90edd0347c194b9b38ed747c315b827b8
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-04-17 23:19:55 -0700

    remove redundant test file

--- tests/python/pants_test/engine/legacy/test_owners_integration.py
+++ tests/python/pants_test/engine/legacy/test_owners_integration.py
@@ -23,7 +23,7 @@ class ListOwnersIntegrationTest(PantsRunIntegrationTest):
                                 'list',
                                 success=True)
     self.assertIn('WARNING: No targets were matched in', pants_run.stderr_data)
-  
+
   def test_owner_list_two_target_specs(self):
     # Test that any of these combinations fail with the same error message.
     expected_error = ('Multiple target selection methods provided. Please use only one of '
@@ -45,3 +45,7 @@ class ListOwnersIntegrationTest(PantsRunIntegrationTest):
                                   'testprojects/tests/python/pants/dummies:passing_target',
                                   success=False)
     self.assertIn(expected_error, pants_run_3.stderr_data)
+
+  # def test_owner_list_repeated_directory_separator(self):
+  #   pants_run = self.do_command('--owner-of=testprojects/tests/python/pants/dummies/test_pass.py',
+  #                               'list')
