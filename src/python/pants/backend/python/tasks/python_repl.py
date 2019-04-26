428bc15c0296086d61641cb80faeb19daccceb81
Borja Lorente <bescobar@twitter.com>
2019-04-26 14:24:50 +0100

    A more local, less invasive, but more broken for repl signal handling

--- src/python/pants/backend/python/tasks/python_repl.py
+++ src/python/pants/backend/python/tasks/python_repl.py
@@ -5,16 +5,19 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os
-import signal
 
 from pex.pex_info import PexInfo
 
 from pants.backend.python.targets.python_requirement_library import PythonRequirementLibrary
 from pants.backend.python.targets.python_target import PythonTarget
 from pants.backend.python.tasks.python_execution_task_base import PythonExecutionTaskBase
+from pants.base.exception_sink import ExceptionSink, SignalHandler
 from pants.task.repl_task_mixin import ReplTaskMixin
-from pants.util.contextutil import signal_handler_as
 
+
+class PythonReplSignalHandler(SignalHandler):
+  def handle_sigint(self, signum, _frame):
+    pass
 
 class PythonRepl(ReplTaskMixin, PythonExecutionTaskBase):
   """Launch an interactive Python interpreter session."""
@@ -50,13 +53,24 @@ class PythonRepl(ReplTaskMixin, PythonExecutionTaskBase):
     pex_info.entry_point = entry_point
     return self.create_pex(pex_info)
 
+  def _run_repl(self, pex, **pex_run_kwargs):
+    env = pex_run_kwargs.pop('env', os.environ).copy()
+    pex.run(env=env, **pex_run_kwargs)
+
   # N.B. **pex_run_kwargs is used by tests only.
   def launch_repl(self, pex, **pex_run_kwargs):
-    # While the repl subprocess is synchronously spawned, we rely on process group
-    # signalling for a SIGINT to reach the repl subprocess directly - and want to
-    # do nothing in response on the parent side.
-    def ignore_control_c(signum, frame): pass
+    running_under_pantsd = self.context.options.for_global_scope().enable_pantsd
 
-    with signal_handler_as(signal.SIGINT, ignore_control_c):
-      env = pex_run_kwargs.pop('env', os.environ).copy()
-      pex.run(env=env, **pex_run_kwargs)
+    if not running_under_pantsd:
+      # While the repl subprocess is synchronously spawned, we rely on process group
+      # signalling for a SIGINT to reach the repl subprocess directly - and want to
+      # do nothing in response on the parent side.
+      with ExceptionSink.trapped_signals(PythonReplSignalHandler()):
+        self._run_repl(pex, **pex_run_kwargs)
+    else:
+      # In pantsd, this task will be running in a non-main thread,
+      # so we can't override signal handling here.
+      # That said, this means that under pantsd,
+      # Ctrl-C will simply crash the repl (and the daemon).
+      # TODO(#7623) Potential more robust (but more invasive) fix.
+      self._run_repl(pex, **pex_run_kwargs)
