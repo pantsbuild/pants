18fda9015b2625dd1ba0d0ae61a4232e86cf42e6
Eric Arellano <ericarellano@me.com>
2019-04-27 11:02:59 -0700

    fix python/clean.sh

--- build-support/python/clean.sh
+++ build-support/python/clean.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PANTS_BASE=$(dirname $0)/../..
-rm -rf ${HOME}/.pex
-rm -rf ${PANTS_BASE}/build-support/pants_dev_deps.venv
-rm -rf ${PANTS_BASE}/build-support/pants_dev_deps.py{2,3}.venv
-rm -rf ${PANTS_BASE}/build-support/pants_dev_deps.py{2,3}?.venv
-rm -rf ${PANTS_BASE}/.pants.d
-find ${PANTS_BASE} -name '*.pyc' | xargs rm -f
+PANTS_BASE=$(dirname "$0")/../..
+rm -rf "${HOME}/.pex"
+rm -rf "${PANTS_BASE}/build-support/pants_dev_deps.venv"
+rm -rf "${PANTS_BASE}/build-support/pants_dev_deps.py{2,3}.venv"
+rm -rf "${PANTS_BASE}/build-support/pants_dev_deps.py{2,3}?.venv"
+rm -rf "${PANTS_BASE}/.pants.d"
+find "${PANTS_BASE}" -name '*.pyc' -print0 | xargs -0 rm -f
