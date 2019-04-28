32a8bc6ea346ed1379ddd22d29d09bb19e587ed7
Eric Arellano <ericarellano@me.com>
2019-04-28 10:00:44 -0700

    Fix test string formatting

--- tests/python/pants_test/repo_scripts/test_git_hooks.py
+++ tests/python/pants_test/repo_scripts/test_git_hooks.py
@@ -9,6 +9,7 @@ import os
 import unittest
 from builtins import str
 from contextlib import contextmanager
+from textwrap import dedent
 
 from pants.util.contextutil import temporary_dir
 from pants.util.dirutil import safe_file_dump, touch
@@ -112,58 +113,69 @@ subdir/__init__.py
 
       # Check that a file with an empty header fails.
       safe_file_dump(new_py_path, '')
-      assert_header_check(added_files=[], expected_excerpt="""\
-subdir/file.py: missing the expected header
-""")
+      assert_header_check(
+        added_files=[],
+        expected_excerpt="subdir/file.py: missing the expected header"
+      )
 
       # Check that a file with a random header fails.
       safe_file_dump(new_py_path, 'asdf')
-      assert_header_check(added_files=[], expected_excerpt="""\
-subdir/file.py: missing the expected header
-""")
+      assert_header_check(
+        added_files=[],
+        expected_excerpt="subdir/file.py: missing the expected header"
+      )
 
       # Check that a file with a typo in the header fails
-      safe_file_dump(new_py_path, """\
-# coding=utf-8
-# Copyright {} Pants project contributors (see CONTRIBUTORS.md).
-# Licensed under the MIT License, Version 3.3 (see LICENSE).
+      safe_file_dump(new_py_path, dedent("""\
+        # coding=utf-8
+        # Copyright {} Pants project contributors (see CONTRIBUTORS.md).
+        # Licensed under the MIT License, Version 3.3 (see LICENSE).
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+        from __future__ import absolute_import, division, print_function, unicode_literals
 
-""".format(cur_year))
-      assert_header_check(added_files=[], expected_excerpt="""\
-subdir/file.py: header does not match the expected header
-""")
+        """.format(cur_year))
+      )
+      assert_header_check(
+        added_files=[],
+        expected_excerpt="subdir/file.py: header does not match the expected header"
+      )
 
       # Check that a file without a valid copyright year fails.
-      safe_file_dump(new_py_path, """\
-# coding=utf-8
-# Copyright YYYY Pants project contributors (see CONTRIBUTORS.md).
-# Licensed under the Apache License, Version 2.0 (see LICENSE).
+      safe_file_dump(new_py_path, dedent("""\
+        # coding=utf-8
+        # Copyright YYYY Pants project contributors (see CONTRIBUTORS.md).
+        # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+        from __future__ import absolute_import, division, print_function, unicode_literals
 
-""")
-      assert_header_check(added_files=[], expected_excerpt="""\
-subdir/file.py: copyright year must match '20\\d\\d' (was YYYY): current year is {}
-""".format(cur_year))
+        """)
+      )
+      assert_header_check(
+        added_files=[],
+        expected_excerpt=(
+          r"subdir/file.py: copyright year must match '20\d\d' (was YYYY): "
+          "current year is {}".format(cur_year)
+        )
+      )
 
       # Check that a newly added file must have the current year.
       last_year = str(cur_year_num - 1)
-      safe_file_dump(new_py_path, """\
-# coding=utf-8
-# Copyright {} Pants project contributors (see CONTRIBUTORS.md).
-# Licensed under the Apache License, Version 2.0 (see LICENSE).
+      safe_file_dump(new_py_path, dedent("""\
+        # coding=utf-8
+        # Copyright {} Pants project contributors (see CONTRIBUTORS.md).
+        # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+        from __future__ import absolute_import, division, print_function, unicode_literals
 
-""".format(last_year))
+        """.format(last_year))
+      )
       rel_new_py_path = os.path.relpath(new_py_path, worktree)
-      assert_header_check(added_files=[rel_new_py_path], expected_excerpt="""\
-subdir/file.py: copyright year must be {} (was {})
-""".format(cur_year, last_year))
+      assert_header_check(
+        added_files=[rel_new_py_path],
+        expected_excerpt="subdir/file.py: copyright year must be {} (was {})".format(cur_year, last_year)
+      )
 
-      # Check that a file read from stdin isn't checked against the current year if
-      # it is not passed as an arg to the script.
+      # Check that a file isn't checked against the current year if it is not passed as an
+      # arg to the script.
       # Use the same file as last time, with last year's copyright date.
       self._assert_subprocess_success(worktree, [header_check_script, 'subdir'])
