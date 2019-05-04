a14ede8c5d09f3300e370346c919d7b753965f57
Stu Hood <stuhood@gmail.com>
2019-05-03 18:34:52 -0700

    Explicitly use requests.

--- src/python/pants/backend/python/subsystems/python_repos.py
+++ src/python/pants/backend/python/subsystems/python_repos.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging
 
 from pex.fetcher import Fetcher, PyPIFetcher
-from pex.http import Context
+from pex.http import RequestsContext, requests
 
 from pants.subsystem.subsystem import Subsystem
 from pants.util.memo import memoized_method
@@ -45,6 +45,4 @@ class PythonRepos(Subsystem):
   @memoized_method
   def get_network_context(self):
     # TODO(wickman): Add retry, conn_timeout, threads, etc configuration here.
-    context = Context.get()
-    logger.debug('PEX HTTP Context is: {} (choices were {})'.format(type(context), Context._REGISTRY))
-    return context
+    return RequestsContext()
