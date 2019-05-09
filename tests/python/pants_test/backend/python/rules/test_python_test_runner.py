58ef09921ed2c6dd425f9cdb9310361c0d61891a
Eric Arellano <ericarellano@me.com>
2019-05-09 13:59:32 -0700

    Simplify test to test what we actually care about
    We don't need to test things like compatibility flag winning over global constraints - that is not what this file is focused on. Instead, the main thing we need to test is that we can convert PythonTargetAdaptors to the proper format PEX is expecting.
    
    This also works around issues with mocking the PythonSetup subsystem.

--- tests/python/pants_test/backend/python/rules/test_python_test_runner.py
+++ tests/python/pants_test/backend/python/rules/test_python_test_runner.py
@@ -14,41 +14,18 @@ from pants_test.subsystem.subsystem_util import global_subsystem_instance
 
 class TestPythonTestRunner(TestCase):
 
-  def assert_interpreter_constraints_parsed(
-    self, python_setup_constraints, target_constraints, expected
-  ):
-    python_setup = (
-      global_subsystem_instance(PythonSetup)
-      if python_setup_constraints is None else
-      global_subsystem_instance(
-        PythonSetup,
-        options={PythonSetup.options_scope: {"interpreter_constraints": python_setup_constraints}}
-      )
-    )
-    target_adaptor = PythonTargetAdaptor(compatibility=target_constraints)
-    self.assertEqual(parse_interpreter_constraints(python_setup, [target_adaptor]), expected)
-
-  # TODO: fails because it's picking up global values
-  def test_interpreter_constraints_none_used(self):
-    self.assert_interpreter_constraints_parsed(
-      python_setup_constraints=None,
-      target_constraints=None,
-      expected=[]
-    )
-
-  # TODO: fails because its using the global defaults, rather than what we try to specify.
-  def test_interpreter_constraints_global_used(self):
-    self.assert_interpreter_constraints_parsed(
-      python_setup_constraints=["CPython>=400"],
-      target_constraints=None,
-      expected=["--interpreter-constraint", "CPython>=400"]
-    )
-
-  def test_interpreter_constraints_compability_field_used(self):
-    self.assert_interpreter_constraints_parsed(
-      python_setup_constraints=None,
-      target_constraints=["CPython<=1", "CPython>=400",],
-      expected=[
-        "--interpreter-constraint", "CPython<=1", "--interpreter-constraint", "CPython>=400"
+  def test_interpreter_constraints_parsing(self):
+    python_setup = global_subsystem_instance(PythonSetup)
+    target_adaptors = [
+      # NB: This target will use the global --python-setup-interpreter-constraints.
+      PythonTargetAdaptor(compatibility=None),
+      PythonTargetAdaptor(compatibility=["CPython>=400"]),
+    ]
+    self.assertEqual(
+      parse_interpreter_constraints(python_setup, target_adaptors),
+      [
+        "--interpreter-constraint", "CPython>=2.7,<3",
+        "--interpreter-constraint", "CPython>=3.6,<4",
+        "--interpreter-constraint", "CPython>=400"
       ]
     )
