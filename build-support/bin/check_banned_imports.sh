4e96fc329e7526ee0bd6d6b2cf6ac9548a66c94d
Eric Arellano <ericarellano@me.com>
2019-05-10 10:07:31 -0700

    Revert all changes to check_banned_imports.sh
    I can't get the array working.

--- build-support/bin/check_banned_imports.sh
+++ build-support/bin/check_banned_imports.sh
@@ -1,25 +1,23 @@
 #!/bin/bash -u
 
-# NB: this assumes that there are no spaces in the filenames. For a more robust way to
-# store the result of `find` in a Bash array, see https://stackoverflow.com/a/23357277.
-PYTHON_FILES=("$(find src tests pants-plugins examples contrib -name '*.py')")
-RUST_FILES=("$(find src/rust/engine -name '*.rs')")
+PYTHON_FILES="$(find src tests pants-plugins examples contrib -name '*.py')"
+RUST_FILES="$(find src/rust/engine -name '*.rs')"
 
-bad_files="$(echo "${PYTHON_FILES}" | xargs grep -l "^import subprocess$")"
+bad_files="$(echo ${PYTHON_FILES} | xargs grep -l "^import subprocess$")"
 if [ -n "${bad_files}" ]; then
     echo >&2 "Found forbidden imports. Instead of \`import subprocess\` you should use \`from pants.util.process_handler import subprocess\`. Bad files:"
     echo >&2 "${bad_files}"
     exit 1
 fi
 
-bad_files="$(echo "${PYTHON_FILES}" | xargs grep -l "^import future.moves.collections\|^from future.moves.collections import\|^from future.moves import .*collections")"
+bad_files="$(echo ${PYTHON_FILES} | xargs grep -l "^import future.moves.collections\|^from future.moves.collections import\|^from future.moves import .*collections")"
 if [ -n "${bad_files}" ]; then
     echo >&2 "Found forbidden imports. \`future.moves.collections\` does not work as intended. Instead, you should use \`import collections\` or \`from pants.util.collections_abc_backport\`. Bad files:"
     echo >&2 "${bad_files}"
     exit 1
 fi
 
-bad_files="$(echo "${RUST_FILES}" | xargs egrep -l "^use std::sync::.*(Mutex|RwLock)")"
+bad_files="$(echo ${RUST_FILES} | xargs egrep -l "^use std::sync::.*(Mutex|RwLock)")"
 if [ -n "${bad_files}" ]; then
     echo >&2 "Found forbidden imports. Instead of \`std::sync::(Mutex|RwLock)\` you should use \`parking_lot::(Mutex|RwLock)\`. Bad files:"
     echo >&2 "${bad_files}"
