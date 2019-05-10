1fa7f3656f44b00828d6401e94bc0ff8702c9021
Eric Arellano <ericarellano@me.com>
2019-05-10 10:35:07 -0700

    Convert check_banned_imports.sh to Python

---
+++ build-support/bin/check_banned_imports.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import re
+from glob import glob
+from typing import List
+
+from common import die
+
+
+def main() -> None:
+  python_files = find_files(
+      "src", "tests", "pants-plugins", "examples", "contrib", extension=".py"
+  )
+  rust_files = find_files("src/rust/engine", extension=".rs")
+
+  check_banned_import(
+    python_files,
+    bad_import_regex=r"^import subprocess$",
+    correct_import_message="`from pants.util.process_handler import subprocess`"
+  )
+  check_banned_import(
+    python_files,
+    bad_import_regex=r"^import future.moves.collections|^from future.moves.collections import|^from future.moves import .*collections",
+    correct_import_message="`import collections` or `from pants.util.collections_abc_backport`"
+  )
+  check_banned_import(
+    rust_files,
+    bad_import_regex=r"^use std::sync::.*(Mutex|RwLock)",
+    correct_import_message="`parking_lot::(Mutex|RwLock)`"
+  )
+
+
+def find_files(*directories: str, extension: str) -> List[str]:
+  return [
+    file
+    for directory in directories
+    for file in glob(f"{directory}/**/*{extension}", recursive=True)
+  ]
+
+
+def check_banned_import(files: List[str], *, bad_import_regex: str, correct_import_message: str) -> None:
+  regex = re.compile(bad_import_regex)
+  bad_files: List[str] = []
+  for fp in files:
+    with open(fp, 'r') as f:
+      if any(re.search(regex, line) for line in f.readlines()):
+        bad_files.append(fp)
+  if bad_files:
+    bad_files_str = "\n".join(bad_files)
+    die(
+      f"Found forbidden imports matching `{bad_import_regex}`. Instead, you should use "
+      f"{correct_import_message}. Bad files:\n{bad_files_str}")
+
+
+if __name__ == "__main__":
+  main()
