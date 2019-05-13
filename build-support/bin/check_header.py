a59bd0524d6275dab0ba5841bf744d6669d1c093
Eric Arellano <ericarellano@me.com>
2019-05-13 14:32:16 -0700

    Refactor and modernize `check_header.py` to use Python 3 (#7635)
    Makes the following improvements:
    
    * Uses argparse instead of reading from `sys.argv`, which brings these benefits:
       * No longer reads from stdin to get added files. Instead, takes an optional arg `--files-added`. This makes the script more reliable and simplifies the callers of the script (i.e. the tests and `pre-commit`).
       * No longer calls the script on the file itself, which was being done because `sys.argv[0]` is the name of the script.
       * `check_header.py --help` now works.
    * Rewrites `check_header()` to not use a buffer.
        * The original motivation for a buffer was for performance, to avoid materializing the content of every checked file.
        * The downside is that buffer code is harder to read because it is far more low-level and imperative and requires keeping track of where the buffer is currently at.
        * Instead, can get same performance with better readability through a list comprehension that uses `f.readline()` 7 times.
    * Inverts several conditionals in `check_header.py` for less nesting.
    * Renames `check_header_helper.py` to `check_header.py`. This script is now the top level script, and not a helper.
    * Replaces the error message `failed to parse header at all` with `missing the expected header` and `the header does not match the expected header`.
       *  `failed to parse header at all` is not very descriptive, and suggests their was an issue with IO.
    * Adds type hints for better readability and less bugs.
    * Removes unnecessary `__future__` imports and file encoding header.
    * Use `textwrap.dedent()` where possible for better indentation.

---
+++ build-support/bin/check_header.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import argparse
+import datetime
+import os
+import re
+from textwrap import dedent
+from typing import Iterable, List
+
+from common import die
+
+
+EXPECTED_HEADER = dedent("""\
+  # coding=utf-8
+  # Copyright YYYY Pants project contributors (see CONTRIBUTORS.md).
+  # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+  from __future__ import absolute_import, division, print_function, unicode_literals
+
+  """)
+
+
+_current_year = str(datetime.datetime.now().year)
+_current_century_regex = re.compile(r'20\d\d')
+
+
+class HeaderCheckFailure(Exception):
+  """This is only used for control flow and to propagate the `.message` field."""
+
+
+def main() -> None:
+  args = create_parser().parse_args()
+  header_parse_failures = []
+  for directory in args.dirs:
+    header_parse_failures.extend(check_dir(directory, args.files_added))
+  if header_parse_failures:
+    failures = '\n  '.join(str(failure) for failure in header_parse_failures)
+    die(f"""\
+ERROR: All .py files other than __init__.py should start with the following header:
+
+{EXPECTED_HEADER}
+---
+
+The following {len(header_parse_failures)} file(s) do not conform:
+{failures}""")
+
+
+def create_parser() -> argparse.ArgumentParser:
+  parser = argparse.ArgumentParser(
+    description="Check that all .py files start with the appropriate header."
+  )
+  parser.add_argument("dirs", nargs="+",
+    help="The directories to check. Will recursively check subdirectories."
+  )
+  parser.add_argument("-a", "--files-added", nargs="*", default=[],
+    help="Any passed files will be checked for a current copyright year."
+  )
+  return parser
+
+
+def check_header(filename: str, *, is_newly_created: bool = False) -> None:
+  """Raises `HeaderCheckFailure` if the header doesn't match."""
+  try:
+    with open(filename, 'r') as f:
+      first_lines = [f.readline() for _ in range(0, 7)]
+  except IOError as e:
+    raise HeaderCheckFailure(f"{filename}: error while reading input ({e})")
+  # If a shebang line is included, remove it. Otherwise, we will have conservatively grabbed
+  # one extra line at the end for the shebang case that is no longer necessary.
+  first_lines.pop(0 if first_lines[0].startswith("#!") else - 1)
+  # Check that the first lines even exists. Note that first_lines will always have an entry
+  # for each line, even if the file is completely empty.
+  if len([line for line in first_lines if line]) < 4:
+    raise HeaderCheckFailure(f"{filename}: missing the expected header")
+  # Check copyright year. If it's a new file, it should be the current year. Else, it should
+  # be within the current century.
+  copyright_line = first_lines[1]
+  year = copyright_line[12:16]
+  if is_newly_created and year != _current_year:
+    raise HeaderCheckFailure(f'{filename}: copyright year must be {_current_year} (was {year})')
+  elif not _current_century_regex.match(year):
+    raise HeaderCheckFailure(
+      f"{filename}: copyright year must match '{_current_century_regex.pattern}' (was {year}): " +
+      f"current year is {_current_year}"
+    )
+  # Replace copyright_line with sanitized year.
+  first_lines[1] = "# Copyright YYYY" + copyright_line[16:]
+  if "".join(first_lines) != EXPECTED_HEADER:
+    raise HeaderCheckFailure(f"{filename}: header does not match the expected header")
+
+
+def check_dir(directory: str, newly_created_files: Iterable[str]) -> List[HeaderCheckFailure]:
+  header_parse_failures: List[HeaderCheckFailure] = []
+  for root, dirs, files in os.walk(directory):
+    for f in files:
+      if not f.endswith('.py') or os.path.basename(f) == '__init__.py':
+        continue
+      filename = os.path.join(root, f)
+      try:
+        check_header(filename, is_newly_created=filename in newly_created_files)
+      except HeaderCheckFailure as e:
+        header_parse_failures.append(e)
+  return header_parse_failures
+
+
+if __name__ == '__main__':
+  main()
