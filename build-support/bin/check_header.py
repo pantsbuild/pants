32c34965e18c7cb27eb458ee200d3ceb1651a741
Eric Arellano <ericarellano@me.com>
2019-05-14 05:19:53 -0700

    Refactor to make more clear what is Py3-specific vs general

--- build-support/bin/check_header.py
+++ build-support/bin/check_header.py
@@ -27,6 +27,8 @@ EXPECTED_HEADER_PY3 = dedent("""\
 
   """)
 
+EXPECTED_NUM_PY2_LINES = 6
+EXPECTED_NUM_PY3_LINES = 3
 
 _current_year = str(datetime.datetime.now().year)
 _current_century_regex = re.compile(r'20\d\d')
@@ -71,28 +73,46 @@ def create_parser() -> argparse.ArgumentParser:
 
 def check_header(filename: str, *, is_newly_created: bool = False) -> None:
   """Raises `HeaderCheckFailure` if the header doesn't match."""
-  expected_num_py2_lines = 6
-  expected_num_py3_lines = 3
+  lines = get_header_lines(filename)
+  check_header_present(filename, lines)
+  is_py3_file = all("from __future__" not in line for line in lines)
+  if is_py3_file:
+    lines = lines[:EXPECTED_NUM_PY3_LINES]
+    copyright_line_index = 0
+    expected_header = EXPECTED_HEADER_PY3
+  else:
+    copyright_line_index = 1
+    expected_header = EXPECTED_HEADER_PY2
+  check_copyright_year(
+    filename, copyright_line=lines[copyright_line_index], is_newly_created=is_newly_created
+  )
+  check_matches_header(
+    filename, lines, expected_header=expected_header, copyright_line_index=copyright_line_index
+  )
+
+
+def get_header_lines(filename: str) -> List[str]:
   try:
     with open(filename, 'r') as f:
       # We grab an extra line in case there is a shebang.
-      first_lines = [f.readline() for _ in range(0, expected_num_py2_lines + 1)]
+      lines = [f.readline() for _ in range(0, EXPECTED_NUM_PY2_LINES + 1)]
   except IOError as e:
     raise HeaderCheckFailure(f"{filename}: error while reading input ({e})")
   # If a shebang line is included, remove it. Otherwise, we will have conservatively grabbed
   # one extra line at the end for the shebang case that is no longer necessary.
-  first_lines.pop(0 if first_lines[0].startswith("#!") else - 1)
-  # Check that the first lines even exists. Note that first_lines will always have an entry
-  # for each line, even if the file is completely empty.
-  if len([line for line in first_lines if line]) < expected_num_py3_lines:
+  lines.pop(0 if lines[0].startswith("#!") else - 1)
+  return lines
+
+
+def check_header_present(filename: str, lines: List[str]) -> None:
+  num_nonempty_lines = len([line for line in lines if line])
+  if num_nonempty_lines < EXPECTED_NUM_PY3_LINES:
     raise HeaderCheckFailure(f"{filename}: missing the expected header")
-  is_py3_file = all("from __future__" not in line for line in first_lines)
-  if is_py3_file:
-    first_lines = first_lines[:expected_num_py3_lines]
-  # Check copyright year. If it's a new file, it should be the current year. Else, it should
-  # be within the current century.
-  copyright_line_index = 0 if is_py3_file else 1
-  copyright_line = first_lines[copyright_line_index]
+
+
+def check_copyright_year(filename: str, *, copyright_line: str, is_newly_created: bool) -> None:
+  """Check that copyright is current year if for a new file, else that it's within
+  the current centuury."""
   year = copyright_line[12:16]
   if is_newly_created and year != _current_year:
     raise HeaderCheckFailure(f'{filename}: copyright year must be {_current_year} (was {year})')
@@ -101,9 +121,15 @@ def check_header(filename: str, *, is_newly_created: bool = False) -> None:
       f"{filename}: copyright year must match '{_current_century_regex.pattern}' (was {year}): " +
       f"current year is {_current_year}"
     )
-  # Replace copyright_line with sanitized year.
-  first_lines[copyright_line_index] = "# Copyright YYYY" + copyright_line[16:]
-  if "".join(first_lines) not in (EXPECTED_HEADER_PY2, EXPECTED_HEADER_PY3):
+
+
+def check_matches_header(
+  filename: str, lines: List[str], *, expected_header: str, copyright_line_index: int
+) -> None:
+  copyright_line = lines[copyright_line_index]
+  sanitized_lines = lines.copy()
+  sanitized_lines[copyright_line_index] = "# Copyright YYYY" + copyright_line[16:]
+  if "".join(sanitized_lines) != expected_header:
     raise HeaderCheckFailure(f"{filename}: header does not match the expected header")
 
 
