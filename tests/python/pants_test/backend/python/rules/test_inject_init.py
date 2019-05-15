6c6caffe10ecc0d8f176807afe5eb6110068af8f
Eric Arellano <ericarellano@me.com>
2019-05-15 16:51:39 -0700

    Refactor to use self.make_snapshot()

--- tests/python/pants_test/backend/python/rules/test_inject_init.py
+++ tests/python/pants_test/backend/python/rules/test_inject_init.py
@@ -4,15 +4,9 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import os
-
-from future.utils import text_type
-
 from pants.backend.python.rules.inject_init import InitInjectedDigest, inject_init
-from pants.engine.fs import EMPTY_DIRECTORY_DIGEST, EMPTY_SNAPSHOT, PathGlobs, PathGlobsAndRoot
+from pants.engine.fs import EMPTY_DIRECTORY_DIGEST, EMPTY_SNAPSHOT
 from pants.util.collections import assert_single_element
-from pants.util.contextutil import temporary_dir
-from pants.util.dirutil import safe_file_dump
 from pants_test.test_base import TestBase
 
 
@@ -29,23 +23,22 @@ class TestInjectInit(TestBase):
     self.assertEqual(injected_digest.directory_digest, EMPTY_DIRECTORY_DIGEST)
 
   def test_noops_when_init_already_present(self):
-    with temporary_dir() as temp_dir:
-      safe_file_dump(os.path.join(temp_dir, "test", "foo.py"), makedirs=True)
-      safe_file_dump(os.path.join(temp_dir, "test", "__init__.py"), makedirs=True)
-      globs = PathGlobs(("*",), ())
-      snapshot = self.scheduler.capture_snapshots((PathGlobsAndRoot(globs, text_type(temp_dir)),))[0]
-      injected_digest = assert_single_element(
-        self.scheduler.product_request(InitInjectedDigest, [snapshot])
-      )
+    snapshot = self.make_snapshot({
+      "test/foo.py": "",
+      "test/__init__.py": ""
+    })
+    injected_digest = assert_single_element(
+      self.scheduler.product_request(InitInjectedDigest, [snapshot])
+    )
     self.assertEqual(injected_digest.directory_digest, snapshot.directory_digest)
 
   def test_adds_when_init_missing(self):
-    with temporary_dir() as temp_dir:
-      safe_file_dump(os.path.join(temp_dir, "test", "foo.py"), makedirs=True)
-      globs = PathGlobs(("*",), ())
-      snapshot = self.scheduler.capture_snapshots((PathGlobsAndRoot(globs, text_type(temp_dir)),))[0]
-      injected_digest = assert_single_element(
-        self.scheduler.product_request(InitInjectedDigest, [snapshot])
-      )
-    # TODO: how to make expected digest
-    self.assertEqual(injected_digest, EMPTY_DIRECTORY_DIGEST)
+    snapshot = self.make_snapshot({"test/foo.py": ""})
+    injected_digest = assert_single_element(
+      self.scheduler.product_request(InitInjectedDigest, [snapshot])
+    )
+    expected_digest = self.make_snapshot({
+      "test/foo.py": "",
+      "test/__init__.py": ""
+    }).directory_digest
+    self.assertEqual(injected_digest.directory_digest, expected_digest)
