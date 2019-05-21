a3a6793f959452f7f0106a0a74269c03701c63ac
John Sirois <john.sirois@gmail.com>
2019-05-21 15:58:49 -0600

    Upgrade all pytest deps to pinned latests.
    This undoes the workarounds introduced for #6282.
    
    Fixes #6282

--- src/python/pants/backend/python/subsystems/pytest.py
+++ src/python/pants/backend/python/subsystems/pytest.py
@@ -13,14 +13,13 @@ class PyTest(Subsystem):
   @classmethod
   def register_options(cls, register):
     super(PyTest, cls).register_options(register)
-    # TODO: This is currently bounded below `3.7` due to #6282.
-    register('--requirements', advanced=True, default='pytest>=3.0.7,<3.7',
+    register('--requirements', advanced=True, default='pytest==4.5.0',
              help='Requirements string for the pytest library.')
-    register('--timeout-requirements', advanced=True, default='pytest-timeout>=1.2,<1.3',
+    register('--timeout-requirements', advanced=True, default='pytest-timeout==1.3.3',
              help='Requirements string for the pytest-timeout library.')
-    register('--cov-requirements', advanced=True, default='pytest-cov>=2.4,<2.5',
+    register('--cov-requirements', advanced=True, default='pytest-cov==2.7.1',
              help='Requirements string for the pytest-cov library.')
-    register('--unittest2-requirements', advanced=True, default='unittest2>=0.6.0,<=1.9.0',
+    register('--unittest2-requirements', advanced=True, default='unittest2==1.1.0',
              help='Requirements string for the unittest2 library, which some python versions '
                   'may need.')
 
@@ -30,9 +29,7 @@ class PyTest(Subsystem):
     Make sure the requirements are satisfied in any environment used for running tests.
     """
     opts = self.get_options()
-    # TODO(6282): once we can upgrade Pytest to 4.2.1+, we can remove the more-itertools requirement
     return (
-      "more-itertools<6.0.0 ; python_version<'3'",
       opts.requirements,
       opts.timeout_requirements,
       opts.cov_requirements,
