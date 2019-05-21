a0e48dc40536266be38f5f9a545f0e40565f1ad6
Borja Lorente <blorente@users.noreply.github.com>
2019-05-22 01:33:47 +0200

    Create Pipe wrapper around pipes (#7740)
    ### Problem
    
    Dealing with raw file descriptors is confusing at best and error-prone at worst.
    
    ### Solution
    
    Define a `Pipe` abstraction that owns (read: "managest the lifetimes of") the write and read ends of a pipe, as well as provide convenience methods to query the state of the fds.
    
    ### Result
    
    It's now a bit more obvious what pipes are open.
    
    Part one of the fix for #7465

--- src/python/pants/bin/daemon_pants_runner.py
+++ src/python/pants/bin/daemon_pants_runner.py
@@ -218,8 +218,8 @@ class DaemonPantsRunner(object):
 
     # TODO https://github.com/pantsbuild/pants/issues/7653
     with maybe_handle_stdin(handle_stdin) as stdin_fd,\
-      NailgunStreamWriter.open_multi(maybe_shutdown_socket.socket, types, ttys) as ((stdout_fd, stderr_fd), writer),\
-      stdio_as(stdout_fd=stdout_fd, stderr_fd=stderr_fd, stdin_fd=stdin_fd):
+      NailgunStreamWriter.open_multi(maybe_shutdown_socket.socket, types, ttys) as ((stdout_pipe, stderr_pipe), writer),\
+      stdio_as(stdout_fd=stdout_pipe.write_fd, stderr_fd=stderr_pipe.write_fd, stdin_fd=stdin_fd):
       # N.B. This will be passed to and called by the `DaemonExiter` prior to sending an
       # exit chunk, to avoid any socket shutdown vs write races.
       stdout, stderr = sys.stdout, sys.stderr
