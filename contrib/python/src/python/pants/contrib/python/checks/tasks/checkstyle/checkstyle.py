013081e91fa38cc0978057639b3b78433b903350
Daniel Wagner-Hall <dwagnerhall@twitter.com>
2019-05-29 20:08:27 +0100

    PANTS_DEV=0 skips repo-specific checkstyle check
    This reflects behaviour in ./pants in the case of running pants from a pex

--- contrib/python/src/python/pants/contrib/python/checks/tasks/checkstyle/checkstyle.py
+++ contrib/python/src/python/pants/contrib/python/checks/tasks/checkstyle/checkstyle.py
@@ -111,7 +111,7 @@ class Checkstyle(LintTaskMixin, Task):
 
   def checker_pex(self, interpreter):
     # TODO(John Sirois): Formalize in pants.base?
-    pants_dev_mode = os.environ.get('PANTS_DEV')
+    pants_dev_mode = os.environ.get('PANTS_DEV', '0') != '0'
 
     if pants_dev_mode:
       checker_id = self.checker_target.transitive_invalidation_hash()
