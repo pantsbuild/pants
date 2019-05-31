88a367836211fb6310ff8ed7004f76e4cabaf984
Stu Hood <stuhood@gmail.com>
2019-05-31 12:23:20 -0700

    Avoid redundant work while hackily_snapshot()ing.

--- src/python/pants/binaries/binary_tool.py
+++ src/python/pants/binaries/binary_tool.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import logging
 import os
+import threading
 from builtins import str
 
 from future.utils import text_type
@@ -49,6 +50,10 @@ class BinaryToolBase(Subsystem):
 
   # Subclasses may set this to provide extra register() kwargs for the --version option.
   extra_version_option_kwargs = None
+
+  def __init__(self, *args, **kwargs):
+    super(BinaryToolBase, self).__init__(*args, **kwargs)
+    self._snapshot_lock = threading.RLock()
 
   @classmethod
   def subsystem_dependencies(cls):
@@ -179,7 +184,8 @@ class BinaryToolBase(Subsystem):
     binary_request = self._make_binary_request(version)
     return self._binary_util.select(binary_request)
 
-  def hackily_snapshot(self, context):
+  @memoized_method
+  def _hackily_snapshot_exclusive(self, context):
     bootstrapdir = self.get_options().pants_bootstrapdir
     relpath = os.path.relpath(self.select(context), bootstrapdir)
     snapshot = context._scheduler.capture_snapshots((
@@ -189,6 +195,17 @@ class BinaryToolBase(Subsystem):
       ),
     ))[0]
     return (relpath, snapshot)
+
+  def hackily_snapshot(self, context):
+    """Returns a Snapshot of this tool after downloading it.
+
+    TODO: See https://github.com/pantsbuild/pants/issues/7790, which would make this unnecessary
+    due to the engine's memoization and caching.
+    """
+    # We call a memoized method under a lock in order to avoid doing a bunch of redundant
+    # fetching and snapshotting.
+    with self._snapshot_lock:
+      return self._hackily_snapshot_exclusive(context)
 
 
 class NativeTool(BinaryToolBase):
--- tests/python/pants_test/binaries/test_binary_tool.py
+++ tests/python/pants_test/binaries/test_binary_tool.py
@@ -4,12 +4,15 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import os
 from builtins import str
 
 from pants.binaries.binary_tool import BinaryToolBase
 from pants.binaries.binary_util import (BinaryToolFetcher, BinaryToolUrlGenerator, BinaryUtil,
                                         HostPlatform)
 from pants.option.scope import GLOBAL_SCOPE
+from pants.util.contextutil import temporary_dir
+from pants.util.dirutil import safe_file_dump
 from pants_test.test_base import TestBase
 
 
@@ -144,3 +147,24 @@ class BinaryToolBaseTest(TestBase):
     self.assertIn(
       "Failed to fetch binary from https://custom-url.example.org/files/custom_urls_tool-v2.3-zzz-alternate:",
       err_msg)
+
+  def test_hackily_snapshot(self):
+    with temporary_dir() as temp_dir:
+      safe_file_dump(
+          os.path.join(temp_dir, 'bin', DefaultVersion.name, DefaultVersion.default_version, DefaultVersion.name),
+          'content!',
+        )
+      context = self.context(
+        for_subsystems=[DefaultVersion],
+        options={
+          GLOBAL_SCOPE: {
+            'binaries_baseurls': ['file:///{}'.format(temp_dir)],
+          },
+        })
+      self.maxDiff = None
+      default_version_tool = DefaultVersion.global_instance()
+      _, snapshot = default_version_tool.hackily_snapshot(context)
+      self.assertEqual(
+          '51a98706ab7458069aabe01856cb352ca97686e3edd3bf9ebd3205c2b38b2974',
+          snapshot.directory_digest.fingerprint
+        )
