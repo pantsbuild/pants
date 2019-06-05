bd47eabad6e4abdf4c45337bb17e3271af956a99
Eric Arellano <ericarellano@me.com>
2019-06-05 13:39:19 -0700

    Get tests working!

--- tests/python/pants_test/backend/python/rules/test_resolve_requirements.py
+++ tests/python/pants_test/backend/python/rules/test_resolve_requirements.py
@@ -5,6 +5,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import json
+import os.path
 import zipfile
 
 from pants.backend.python.rules.resolve_requirements import (ResolvedRequirementsPex,
@@ -13,10 +14,11 @@ from pants.backend.python.rules.resolve_requirements import (ResolvedRequirement
 from pants.backend.python.subsystems.python_native_code import (PythonNativeCode,
                                                                 create_pex_native_build_environment)
 from pants.backend.python.subsystems.python_setup import PythonSetup
-from pants.engine.fs import Snapshot
+from pants.engine.fs import DirectoryToMaterialize
 from pants.engine.rules import RootRule
 from pants.engine.selectors import Params
 from pants.util.collections import assert_single_element
+from pants.util.contextutil import temporary_dir
 from pants_test.subsystem.subsystem_util import init_subsystems
 from pants_test.test_base import TestBase
 
@@ -56,18 +58,21 @@ class TestResolveRequirements(TestBase):
         PythonNativeCode.global_instance()
       )])
     )
-    snapshot = assert_single_element(
-      self.scheduler.product_request(Snapshot, [requirements_pex.directory_digest])
-    )
-    with zipfile.ZipFile(snapshot.files[0], "r") as pex:
-      with pex.open("PEX-INFO", "r") as pex_info:
-        pex_info_content = pex_info.readline().decode("utf-8")
+    with temporary_dir() as tmp_dir:
+      self.scheduler.materialize_directories((
+        DirectoryToMaterialize(path=tmp_dir, directory_digest=requirements_pex.directory_digest),
+      ))
+      with zipfile.ZipFile(os.path.join(tmp_dir, "test.pex"), "r") as pex:
+        with pex.open("PEX-INFO", "r") as pex_info:
+          pex_info_content = pex_info.readline().decode("utf-8")
     return json.loads(pex_info_content)
 
   def test_resolves_dependencies(self):
     requirements = {"six==1.12.0", "jsonschema==2.6.0", "requests==2.22.0"}
     pex_info = self.create_pex_and_get_pex_info(requirements=requirements)
-    self.assertEqual(set(pex_info["requirements"]), requirements)
+    # NB: We do not check for transitive dependencies, which PEX-INFO will include. We only check
+    # that at least the dependencies we requested are included.
+    self.assertTrue(requirements.issubset(pex_info["requirements"]))
 
   def test_entry_point(self):
     entry_point = "pydoc"
