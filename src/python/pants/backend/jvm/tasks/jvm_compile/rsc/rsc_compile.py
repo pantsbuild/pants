57965408d6e74c32248b90a4f2f8ba9a8c1a9ae9
Stu Hood <stuhood@gmail.com>
2019-06-07 01:15:33 +0100

    Remove unused import

--- src/python/pants/backend/jvm/tasks/jvm_compile/rsc/rsc_compile.py
+++ src/python/pants/backend/jvm/tasks/jvm_compile/rsc/rsc_compile.py
@@ -23,7 +23,7 @@ from pants.base.build_environment import get_buildroot
 from pants.base.exceptions import TaskError
 from pants.base.workunit import WorkUnitLabel
 from pants.build_graph.mirrored_target_option_mixin import MirroredTargetOptionMixin
-from pants.engine.fs import (EMPTY_DIRECTORY_DIGEST, Digest, DirectoryToMaterialize, PathGlobs,
+from pants.engine.fs import (EMPTY_DIRECTORY_DIGEST, DirectoryToMaterialize, PathGlobs,
                              PathGlobsAndRoot)
 from pants.engine.isolated_process import ExecuteProcessRequest, FallibleExecuteProcessResult
 from pants.java.jar.jar_dependency import JarDependency
