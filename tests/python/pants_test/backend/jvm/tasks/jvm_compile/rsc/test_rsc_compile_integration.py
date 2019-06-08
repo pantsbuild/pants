38dafa217f98ca40626f31f3f254c5ea0925cbdc
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-06-08 14:05:33 +0100

    generate rsc compile integration tests!

--- tests/python/pants_test/backend/jvm/tasks/jvm_compile/rsc/test_rsc_compile_integration.py
+++ tests/python/pants_test/backend/jvm/tasks/jvm_compile/rsc/test_rsc_compile_integration.py
@@ -2,17 +2,27 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 import os
-from functools import wraps
 
 from pants.backend.jvm.subsystems.resolve_subsystem import JvmResolveSubsystem
 from pants.backend.jvm.tasks.jvm_compile.rsc.rsc_compile import RscCompile
 from pants.util.contextutil import temporary_dir
 from pants_test.backend.jvm.tasks.jvm_compile.base_compile_integration_test import BaseCompileIT
+from pants_test.test_base import AbstractTestGenerator
 
 
 def _for_all_supported_execution_environments(func):
-  @wraps(func)
-  def wrapper_self(*args, **kwargs):
+  func._with_run_config = True
+  return func
+
+
+class RscCompileIntegration(BaseCompileIT, AbstractTestGenerator):
+
+  @classmethod
+  def generate_tests(cls):
+    tests_with_generated_config = {
+      name: func for name, func in cls.__dict__.items() if getattr(func, '_with_run_config', False)
+    }
+
     for worker_count in [1, 2]:
       for resolver in JvmResolveSubsystem.CHOICES:
         for execution_strategy in RscCompile.ExecutionStrategy.all_variants:
@@ -49,14 +59,14 @@ def _for_all_supported_execution_environments(func):
               'hermetic': populate_necessary_hermetic_options,
             })()
 
-            func(*args, config=config, **kwargs)
-  return wrapper_self
-
-
-class RscCompileIntegration(BaseCompileIT):
+            for name, test in tests_with_generated_config.items():
+              cls.add_test(
+                'test_{}_resolver_{}_strategy_{}_worker_{}'
+                .format(name, resolver, execution_strategy.value, worker_count),
+                lambda this: test(this, config=config.copy()))
 
   @_for_all_supported_execution_environments
-  def test_basic_binary(self, config):
+  def basic_binary(self, config):
     with self.do_command_yielding_workdir(
         'compile', 'testprojects/src/scala/org/pantsbuild/testproject/mutual:bin',
         config=config) as pants_run:
@@ -72,20 +82,23 @@ class RscCompileIntegration(BaseCompileIT):
       self.assertIsFile(rsc_header_jar)
 
   @_for_all_supported_execution_environments
-  def test_executing_multi_target_binary(self, config):
+  def executing_multi_target_binary(self, config):
     pants_run = self.do_command(
       'run', 'examples/src/scala/org/pantsbuild/example/hello/exe',
       config=config)
     self.assertIn('Hello, Resource World!', pants_run.stdout_data)
 
   @_for_all_supported_execution_environments
-  def test_java_with_transitive_exported_scala_dep(self, config):
+  def java_with_transitive_exported_scala_dep(self, config):
     self.do_command(
       'compile', 'testprojects/src/scala/org/pantsbuild/testproject/javadepsonscalatransitive:java-in-different-package',
       config=config)
 
   @_for_all_supported_execution_environments
-  def test_java_sources(self, config):
+  def java_sources(self, config):
     self.do_command(
       'compile', 'testprojects/src/scala/org/pantsbuild/testproject/javasources',
       config=config)
+
+
+RscCompileIntegration.generate_tests()
