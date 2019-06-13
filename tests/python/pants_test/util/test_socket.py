f8980c5810dddced02449a10b4c05ad80af4e0a8
Eric Arellano <ericarellano@me.com>
2019-06-13 13:08:14 -0700

    Use Selectors module for more robust and efficient syscalls (#7882)
    ### Problem
    Twitter was running into the error `filedescriptor out of range in select()` when compiling a large number of targets, per https://github.com/pantsbuild/pants/issues/7880.
    
    In the process, we found that it is more efficient and robust to use the more modern syscalls of `epoll()`, `devpoll()`, and `kqueue()` than `select()`. Python 3.4 introduced the Selectors module to choose the best syscall available for us.
    
    This will close https://github.com/pantsbuild/pants/issues/7880.
    
    ### Solution
    For Python 3 code, use the new selectors library. Python 2 code stays the same as before, since the library does not exist for it and we are soon going to drop Python 2.

--- tests/python/pants_test/util/test_socket.py
+++ tests/python/pants_test/util/test_socket.py
@@ -9,11 +9,34 @@ import socket
 import unittest
 
 import mock
+from future.utils import PY3
 
-from pants.util.socket import RecvBufferedSocket
+from pants.util.socket import RecvBufferedSocket, is_readable
 
 
 PATCH_OPTS = dict(autospec=True, spec_set=True)
+
+
+class TestSocketUtils(unittest.TestCase):
+
+  @mock.patch('selectors.DefaultSelector' if PY3 else 'select.select', **PATCH_OPTS)
+  def test_is_readable(self, mock_selector):
+    mock_fileobj = mock.Mock()
+    if PY3:
+      mock_selector = mock_selector.return_value.__enter__.return_value
+      mock_selector.register = mock.Mock()
+      # NB: the return value should actually be List[Tuple[SelectorKey, Events]], but our code only
+      # cares that _some_ event happened so we choose a simpler mock here. See
+      # https://docs.python.org/3/library/selectors.html#selectors.BaseSelector.select.
+      mock_selector.select = mock.Mock(return_value=[(1, "")])
+    else:
+      mock_selector.return_value = ([1], [], [])
+    self.assertTrue(is_readable(mock_fileobj, timeout=0.1))
+    if PY3:
+      mock_selector.select = mock.Mock(return_value=[])
+    else:
+      mock_selector.return_value = ([], [], [])
+    self.assertFalse(is_readable(mock_fileobj, timeout=0.1))
 
 
 class TestRecvBufferedSocket(unittest.TestCase):
@@ -44,9 +67,18 @@ class TestRecvBufferedSocket(unittest.TestCase):
     self.server_sock.sendall(b'A' * double_chunk)
     self.assertEqual(self.buf_sock.recv(double_chunk), b'A' * double_chunk)
 
-  @mock.patch('pants.util.socket.select.select', **PATCH_OPTS)
-  def test_recv_check_calls(self, mock_select):
-    mock_select.return_value = ([1], [], [])
+  @mock.patch('selectors.DefaultSelector' if PY3 else 'select.select', **PATCH_OPTS)
+  def test_recv_check_calls(self, mock_selector):
+    if PY3:
+      mock_selector = mock_selector.return_value.__enter__.return_value
+      mock_selector.register = mock.Mock()
+      # NB: the return value should actually be List[Tuple[SelectorKey, Events]], but our code only
+      # cares that _some_ event happened so we choose a simpler mock here. See
+      # https://docs.python.org/3/library/selectors.html#selectors.BaseSelector.select.
+      mock_selector.select = mock.Mock(return_value=[(1, "")])
+    else:
+      mock_selector.return_value = ([1], [], [])
+
     self.mock_socket.recv.side_effect = [b'A' * self.chunk_size, b'B' * self.chunk_size]
 
     self.assertEqual(self.mocked_buf_sock.recv(128), b'A' * 128)
