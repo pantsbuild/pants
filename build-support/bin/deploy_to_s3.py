940b367647de667bcac1c51a188b2caa36375caa
Eric Arellano <ericarellano@me.com>
2019-06-19 17:13:26 -0700

    Simplify solution to use env vars, rather than `aws configure`

--- build-support/bin/deploy_to_s3.py
+++ build-support/bin/deploy_to_s3.py
@@ -5,7 +5,6 @@
 import os
 import shutil
 import subprocess
-from typing import Tuple
 
 from common import die
 
@@ -13,8 +12,7 @@ from common import die
 def main() -> None:
   if shutil.which("aws") is None:
     install_aws_cli()
-  access_key_id, secret_access_key = get_auth_values()
-  set_auth(access_key_id=access_key_id, secret_access_key=secret_access_key)
+  check_authentication_setup()
   deploy()
 
 
@@ -22,27 +20,11 @@ def install_aws_cli() -> None:
   subprocess.run(["./build-support/bin/install_aws_cli_for_ci.sh"], check=True)
 
 
-def get_auth_values() -> Tuple[str, str]:
-  def get_value(key: str) -> str:
-    val = os.environ.get(key, None)
-    if val is not None:
-      return val
-    die(f"Caller of the script must set the env var {key}.")
-
-  return get_value("AWS_DEPLOY_ACCESS_KEY_ID"), get_value("AWS_DEPLOY_SECRET_ACCESS_KEY")
-
-
-def set_auth(*, access_key_id: str, secret_access_key: str) -> None:
-  def set_value(key: str, value: str) -> None:
-    # NB: We intentionally catch subprocess.CalledProcessError to ensure that we don't leak the
-    # auth value upon failure, as the normal stacktrace would print the entire command invoked.
-    try:
-      subprocess.run(["aws", "configure", "set", key, value], check=True)
-    except subprocess.CalledProcessError:
-      die(f"Failed to set {key}.")
-
-  set_value("aws_access_key_id", access_key_id)
-  set_value("aws_secret_access_key", secret_access_key)
+def check_authentication_setup() -> None:
+  access_key_id = "AWS_ACCESS_KEY_ID"
+  secret_access_key = "AWS_SECRET_ACCESS_KEY"
+  if access_key_id not in os.environ or secret_access_key not in os.environ:
+    die(f"Caller of the script must set both {access_key_id} and {secret_access_key}.")
 
 
 def deploy() -> None:
