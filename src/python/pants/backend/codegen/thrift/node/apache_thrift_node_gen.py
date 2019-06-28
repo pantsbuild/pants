08933c521490f51d716a6f099aaf2726616a8394
Satwik Singh <satwiks@twitter.com>
2019-06-28 10:19:45 -0700

    adding thrift-node-gen

--- src/python/pants/backend/codegen/thrift/node/apache_thrift_node_gen.py
+++ src/python/pants/backend/codegen/thrift/node/apache_thrift_node_gen.py
@@ -1,12 +1,20 @@
 import os
 from pants.backend.codegen.thrift.lib.apache_thrift_gen_base import ApacheThriftGenBase
 from pants.contrib.node.targets.node_thrift_library import NodeThriftLibrary
+from pants.contrib.node.targets.node_module import NodeModule
 
 
 class ApacheThriftNodeGen(ApacheThriftGenBase):
 	"""Generate Nodejs source files from thrift IDL files"""
 	gentarget_type = NodeThriftLibrary
 	thrift_generator = 'nodejs'
-	default_gen_options_map = {
-		'new_style'
-	}
+
+	_COMPILER = 'thrift'
+
+
+	def synthetic_target_type(self, target):
+		return NodeModule
+
+
+	def execute_codegen(selt, target, target_workdir):
+		super().execute_codegen(target, target_workdir)
