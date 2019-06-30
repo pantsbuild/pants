299caad21ab1d1e1ee382046792ede4aecb046fe
Eric Arellano <ericarellano@me.com>
2019-06-30 10:59:28 -0700

    Remove newstr special case

--- src/python/pants/help/help_info_extracter.py
+++ src/python/pants/help/help_info_extracter.py
@@ -90,11 +90,7 @@ class HelpInfoExtracter:
       if typ == list:
         typ = kwargs.get('member_type', str)
 
-      if typ == dict:
-        metavar = '"{\'key1\':val1,\'key2\':val2,...}"'
-      else:
-        type_name = typ.__name__ if typ != newstr else 'str'  # TODO(#6071): drop special case once Py2 removed
-        metavar = '<{}>'.format(type_name)
+      metavar = f'<{typ.__name}>' if typ != dict else '"{\'key1\':val1,\'key2\':val2,...}"'
 
     return metavar
 
