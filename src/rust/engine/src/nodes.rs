c71412d951d58be867dd865d6321283e90fdd70f
Greg Shuflin <greg.shuflin@toolchain.com>
2019-07-03 16:15:31 -0700

    Avoid unwrap() call in union error message flow
    Use a guarded match expression to avoid having to unwrap a
    value we know but can't statically prove is Some

--- src/rust/engine/src/nodes.rs
+++ src/rust/engine/src/nodes.rs
@@ -863,25 +863,25 @@ impl Task {
           .edges_for_inner(&entry)
           .ok_or_else(|| throw(&format!("no edges for task {:?} exist!", entry)))
           .and_then(|edges| {
-            edges.entry_for(&dependency_key).cloned().ok_or_else(|| {
-              let is_union = get.declared_subject.map(externs::is_union).unwrap_or(false);
-              if is_union {
-                let union_ty = get.declared_subject.unwrap();
-                let value = externs::get_value_from_type_id(union_ty);
-                let description = externs::project_str(&value, "union_description");
-                throw(&format!(
-                  "Type {} is not a member of the {} @union (\"{}\")",
-                  get.subject.type_id(),
-                  union_ty,
-                  description
-                ))
-              } else {
-                throw(&format!(
+            edges
+              .entry_for(&dependency_key)
+              .cloned()
+              .ok_or_else(|| match get.declared_subject {
+                Some(ty) if externs::is_union(ty) => {
+                  let value = externs::get_value_from_type_id(ty);
+                  let description = externs::project_str(&value, "union_description");
+                  throw(&format!(
+                    "Type {} is not a member of the {} @union (\"{}\")",
+                    get.subject.type_id(),
+                    ty,
+                    description
+                  ))
+                }
+                _ => throw(&format!(
                   "{:?} did not declare a dependency on {:?}",
                   entry, dependency_key
-                ))
-              }
-            })
+                )),
+              })
           });
         // The subject of the get is a new parameter that replaces an existing param of the same
         // type.
