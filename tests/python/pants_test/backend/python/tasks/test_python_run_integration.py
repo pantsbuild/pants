96c152a3ad0bf2421c2fe6ce7295dc07f18b84d3
John Sirois <john.sirois@gmail.com>
2019-07-06 09:25:43 -0600

    Fix plugin resolution to respect interpreter ABI. (#8005)
    Previously, changing the python interpreter selected by Pants (e.g: by
    editing `[python-setup] interpreter_constraints` in `pants.ini`) did not
    invalidate an exact requirement plugin resolve leading to runtime errors
    loading requirements for the wrong ABI. This is fixed by mixing the
    interpreter ABI into the plugin resolve hash and a test is added that
    fails without this fix.
    
    Also refactor
    tests/python/pants_test/backend/python/interpreter_selection_utils.py to
    live in tests/python/pants_test since its now used outside of the python
    backend tests.

--- tests/python/pants_test/backend/python/tasks/test_python_run_integration.py
+++ tests/python/pants_test/backend/python/tasks/test_python_run_integration.py
@@ -6,9 +6,10 @@ import os
 from pex.pex_info import PexInfo
 
 from pants.util.contextutil import temporary_dir
-from pants_test.backend.python.interpreter_selection_utils import (
-  PY_3, PY_27, python_interpreter_path, skip_unless_python3_present,
-  skip_unless_python27_and_python3_present, skip_unless_python27_present)
+from pants_test.interpreter_selection_utils import (PY_3, PY_27, python_interpreter_path,
+                                                    skip_unless_python3_present,
+                                                    skip_unless_python27_and_python3_present,
+                                                    skip_unless_python27_present)
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest, ensure_daemon
 from pants_test.testutils.pexrc_util import setup_pexrc_with_pex_python_path
 
