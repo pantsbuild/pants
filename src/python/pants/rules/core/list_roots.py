c384fb612752e6b510b5f186e2ed402dfa4f48b1
Greg Shuflin <greg.shuflin@toolchain.com>
2019-07-10 16:37:55 -0700

    Define v2 rule version of ListRoots
    This commit creates a v2 rule that does the same thing as the currently
    existing roots task. It is currently named 'rootsv2' so that it doesn't
    conflict with the exisiting task, but will be renamed in the next commit
    when that task is removed.

---
+++ src/python/pants/rules/core/list_roots.py
@@ -0,0 +1,29 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.engine.console import Console
+from pants.engine.goal import Goal, LineOriented
+from pants.engine.rules import console_rule, optionable_rule
+from pants.source.source_root import SourceRootConfig
+
+
+class Roots(LineOriented, Goal):
+  """List the repo's registered source roots."""
+  name = 'rootsv2'
+
+
+@console_rule(Roots, [Console, Roots.Options, SourceRootConfig])
+def list_roots(console, options, source_root_config):
+  all_roots = source_root_config.get_source_roots().all_roots()
+  with Roots.line_oriented(options, console) as (print_stdout, print_stderr):
+    for src_root in all_roots:
+      all_langs = ','.join(sorted(src_root.langs))
+      print_stdout(f"{src_root.path}: {all_langs or '*'}")
+  yield Roots(exit_code=0)
+
+
+def rules():
+  return [
+      optionable_rule(SourceRootConfig),
+      list_roots,
+    ]
