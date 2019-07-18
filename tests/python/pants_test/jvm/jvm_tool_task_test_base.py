f42774b27b880ea9249ffdf869d49f29f9eecb38
Eric Arellano <ericarellano@me.com>
2019-07-17 21:30:01 -0700

    Restore original logic of jvm_tool_task_test_base.py

--- tests/python/pants_test/jvm/jvm_tool_task_test_base.py
+++ tests/python/pants_test/jvm/jvm_tool_task_test_base.py
@@ -2,6 +2,7 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 import os
+import shutil
 
 from pants.backend.jvm.register import build_file_aliases
 from pants.backend.jvm.subsystems.jvm_tool_mixin import JvmToolMixin
@@ -11,6 +12,7 @@ from pants.base.build_environment import get_pants_cachedir
 from pants.build_graph.build_file_aliases import BuildFileAliases
 from pants.build_graph.target import Target
 from pants.ivy.bootstrapper import Bootstrapper
+from pants.util.dirutil import safe_mkdir
 from pants_test.jvm.jvm_task_test_base import JvmTaskTestBase
 
 
@@ -62,6 +64,12 @@ class JvmToolTaskTestBase(JvmTaskTestBase):
     self.set_options_for_scope('cache.{}'.format(bootstrap_scope),
                                read_from=artifact_caches,
                                write_to=artifact_caches)
+
+    # Copy into synthetic build-root
+    shutil.copy('BUILD.tools', self.build_root)
+    build_root_third_party = os.path.join(self.build_root, '3rdparty')
+    safe_mkdir(build_root_third_party)
+    shutil.copy(os.path.join('3rdparty', 'BUILD'), build_root_third_party)
 
     Bootstrapper.reset_instance()
 
