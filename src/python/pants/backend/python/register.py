ff5e52a2abafd21f03993da62a9768b1a777bd2e
Eric Arellano <ericarellano@me.com>
2019-07-20 11:02:23 -0700

    Revert "Attempt #3: create sources pex and merge (Fails)"
    This reverts commit 83a5845ac700bac7747267dae08c93a0b43e47e4.

--- src/python/pants/backend/python/register.py
+++ src/python/pants/backend/python/register.py
@@ -5,8 +5,8 @@ from pants.backend.python.pants_requirement import PantsRequirement
 from pants.backend.python.python_artifact import PythonArtifact
 from pants.backend.python.python_requirement import PythonRequirement
 from pants.backend.python.python_requirements import PythonRequirements
-from pants.backend.python.rules import (create_requirements_pex, create_sources_pex,
-                                        download_pex_bin, inject_init, python_test_runner)
+from pants.backend.python.rules import (create_requirements_pex, download_pex_bin, inject_init,
+                                        python_test_runner)
 from pants.backend.python.subsystems.python_native_code import PythonNativeCode
 from pants.backend.python.subsystems.python_native_code import rules as python_native_code_rules
 from pants.backend.python.subsystems.subprocess_environment import SubprocessEnvironment
@@ -89,11 +89,10 @@ def register_goals():
 
 def rules():
   return (
-    create_requirements_pex.rules() +
-    create_sources_pex.rules() +
     download_pex_bin.rules() +
     inject_init.rules() +
     python_test_runner.rules() +
     python_native_code_rules() +
+    create_requirements_pex.rules() +
     subprocess_environment_rules()
   )
