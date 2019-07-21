822586e1f095576ed588ee8f89e42ba79d735f16
Eric Arellano <ericarellano@me.com>
2019-07-21 09:29:48 -0700

    Fix test_build_root.py (fails until #8063)

--- BUILD
+++ BUILD
@@ -10,3 +10,12 @@ files(
   name = '3rdparty_build',
   source = '3rdparty/BUILD',
 )
+
+# NB: Be careful when using this in tests! Some tests will use this to determine the buildroot and
+# then dynamically try to open other files relative to the buildroot, such as BUILD.tools. This
+# should not be used to dynamically import other files—those must be explicitly imported via their
+# own BUILD entries.
+files(
+  name = 'pants_script',
+  source = 'pants',
+)
--- src/python/pants/base/build_root.py
+++ src/python/pants/base/build_root.py
@@ -57,7 +57,7 @@ class BuildRoot(Singleton):
     """Manually establishes the build root for the current workspace."""
     path = os.path.realpath(root_dir)
     if not os.path.exists(path):
-      raise ValueError('Build root does not exist: {}'.format(root_dir))
+      raise ValueError(f'Build root does not exist: {root_dir}')
     self._root_dir = path
 
   def reset(self):
@@ -65,7 +65,7 @@ class BuildRoot(Singleton):
     self._root_dir = None
 
   def __str__(self):
-    return 'BuildRoot({})'.format(self._root_dir)
+    return f'BuildRoot({self._root_dir})'
 
   @contextmanager
   def temporary(self, path):
