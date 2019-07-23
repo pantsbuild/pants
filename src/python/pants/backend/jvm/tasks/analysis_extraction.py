76acff69cfdf2df7b788fc716b44ce3296311eff
Henry Fuller <hfuller@twitter.com>
2019-07-23 15:39:22 -0700

    fix jdeps regex. filter jdeps targets to those with a classpath product

--- src/python/pants/backend/jvm/tasks/analysis_extraction.py
+++ src/python/pants/backend/jvm/tasks/analysis_extraction.py
@@ -61,13 +61,23 @@ class AnalysisExtraction(NailgunTask):
   @contextmanager
   def aliased_classpaths(self, classpaths):
     """
-    Create unique names for each classpath entry as symlinks in a temporary directory.
-    returns: dict[str -> classpath entry] which maps string paths of symlinks to classpaths.
+    Create unique names for each classpath entry as symlinks in
+    a temporary directory.  returns: dict[str -> classpath entry]
+    which maps string paths of symlinks to classpaths.
+
+    ClasspathEntries generally point to a .jar of
+    the .class files generated for java_library targets.
+    These jars all have the same basename, z.jar, which
+    confuses the `jdeps` tool. Jdeps expects unique, and
+    descriptive, basenames for jars. When all basenames are
+    the same the deps collide in the jdeps output, some
+    .class files can't be found and the summary output
+    is not complete.
     """
     with temporary_dir() as tempdir:
       aliases = {}
       for i, cp in enumerate(classpaths):
-        alias = os.path.join(tempdir, f"{i}.jar" if not os.path.isdir(cp) else f"{1}")
+        alias = os.path.join(tempdir, f"{i}.jar" if not os.path.isdir(cp) else f"{i}")
         os.symlink(cp, alias)
         aliases[alias] = cp
       yield aliases
@@ -83,52 +93,51 @@ class AnalysisExtraction(NailgunTask):
     fingerprint_strategy = DependencyContext.global_instance().create_fingerprint_strategy(
         classpath_product)
 
-    targets = self.context.targets()
-    # complete classpath list
-    deps_classpaths = [
-      cp[1] for target in targets for cp in classpath_product.get_for_target(target) if cp[1]
+    # classpath fingerprint strategy only works on jvm targets.
+    targets = [target for target in self.context.targets() if classpath_product.get_for_target(target)]
+    all_classpaths = [
+      entry for target in targets for _, entry in classpath_product.get_for_target(target) if entry
     ]
-    with self.aliased_classpaths(deps_classpaths) as deps_classpath_by_alias:
-      with self.invalidated(targets,
-                            fingerprint_strategy=fingerprint_strategy,
-                            invalidate_dependents=True) as invalidation_check:
-        for vt in invalidation_check.all_vts:
-          # class paths for the target we are computing deps for
-          target_cps = [cp[1] for cp in classpath_product.get_for_target(vt.target)]
+    with self.invalidated(targets,
+                          fingerprint_strategy=fingerprint_strategy,
+                          invalidate_dependents=True) as invalidation_check:
+      for vt in invalidation_check.all_vts:
+        # class paths for the target we are computing deps for
+        target_cps = [entry for _, entry in classpath_product.get_for_target(vt.target)]
 
-          jdeps_output_json = self._jdeps_output_json(vt)
-          if not vt.valid:
-            self._run_jdeps_analysis(vt.target, target_cps, deps_classpath_by_alias, jdeps_output_json)
-          self._register_products(vt.target,
-                                  jdeps_output_json,
-                                  product_deps_by_target)
+        jdeps_output_json = self._jdeps_output_json(vt)
+        if not vt.valid:
+          self._run_jdeps_analysis(vt.target, target_cps, all_classpaths, jdeps_output_json)
+        self._register_products(vt.target,
+                                jdeps_output_json,
+                                product_deps_by_target)
 
   @memoized_property
   def _jdeps_summary_line_regex(self):
-    return re.compile(r"^\S+\s->\s(\S+)$")
+    return re.compile(r"^.+\s->\s(.+)$")
 
-  def _run_jdeps_analysis(self, target, target_cps, deps_classpath_by_alias, jdeps_output_json):
-    with open(jdeps_output_json, 'w') as f:
-      if target_cps:
-        # TODO should we find an abs path to jdeps in a better way? a jdk path?
-        cmd = [
+  def _run_jdeps_analysis(self, target, target_cps, all_classpaths, jdeps_output_json):
+    with self.aliased_classpaths(all_classpaths) as classpaths_by_alias:
+      with open(jdeps_output_json, 'w') as f:
+        if target_cps:
+          # TODO should we find an abs path to jdeps in a better way? a jdk path?
+          cmd = [
             "jdeps", "-summary",
-            '-classpath', ":".join(cp for cp in deps_classpath_by_alias.keys()),
+            '-classpath', ":".join(cp for cp in classpaths_by_alias.keys()),
           ] + target_cps
-        jdeps_output = io.StringIO(subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8'))
-        deps_classpaths = set()
-        for line in jdeps_output:
-          match = self._jdeps_summary_line_regex.fullmatch(line.strip()).group(1)
-          deps_classpaths.add(deps_classpath_by_alias.get(match, match))
+          jdeps_output = io.StringIO(subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8'))
+          deps_classpaths = set()
+          for line in jdeps_output:
+            match = self._jdeps_summary_line_regex.fullmatch(line.strip()).group(1)
+            deps_classpaths.add(classpaths_by_alias.get(match, match))
 
-      else:
-        deps_classpaths = []
-      json.dump(list(deps_classpaths), f)
+        else:
+          deps_classpaths = []
+        json.dump(list(deps_classpaths), f)
 
   def _register_products(self,
                          target,
                          jdeps_output_json,
                          product_deps_by_target):
-    if target not in product_deps_by_target:
-      with open(jdeps_output_json) as f:
-        product_deps_by_target[target] = json.load(f)
+    with open(jdeps_output_json) as f:
+      product_deps_by_target[target] = json.load(f)
