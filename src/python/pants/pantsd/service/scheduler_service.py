07ff0cbecf85502b42c848a8d8b62e83a4008d31
Borja Lorente <bescobar@twitter.com>
2019-07-23 17:51:04 +0100

    Extract all_files subscription name

--- src/python/pants/pantsd/service/scheduler_service.py
+++ src/python/pants/pantsd/service/scheduler_service.py
@@ -67,7 +67,7 @@ class SchedulerService(PantsService):
     """Service setup."""
     super().setup(services)
     # Register filesystem event handlers on an FSEventService instance.
-    self._fs_event_service.register_all_files_handler(self._enqueue_fs_event)
+    self._fs_event_service.register_all_files_handler(self._enqueue_fs_event, self._fs_event_service.PANTS_ALL_FILES_SUBSCRIPTION_NAME)
 
     # N.B. We compute the invalidating fileset eagerly at launch with an assumption that files
     # that exist at startup are the only ones that can affect the running daemon.
