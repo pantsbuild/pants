0d1f8a486eada22da42761841e982e11ba4d27eb
Satwik Singh <satwiks@twitter.com>
2019-07-24 10:50:15 -0700

    fixed indents in node-thrift-library test file

--- contrib/node/tests/python/pants_test/contrib/node/targets/test_node_thrift_library.py
+++ contrib/node/tests/python/pants_test/contrib/node/targets/test_node_thrift_library.py
@@ -10,9 +10,9 @@ class NodeThriftLibraryTest(TestBase):
 
   def test_bin_executables_dict(self):
     target1 = self.make_target(spec=':name1', target_type=NodeThriftLibrary, package_name='name1',
-                              bin_executables='./cli.js')
+                               bin_executables='./cli.js')
     target2 = self.make_target(spec=':name2', target_type=NodeThriftLibrary, package_name='name2',
-                              bin_executables={"name2": "./cli.js"})
+                               bin_executables={"name2": "./cli.js"})
     self.assertNotEqual(target1, target2)
     self.assertEqual('./cli.js', target1.payload.bin_executables)
     self.assertEqual({'name2': './cli.js'}, target2.payload.bin_executables)
