5beb466197af08870bc9dd7436b3e994d64867fa
Satwik Singh <satwiks@twitter.com>
2019-07-25 16:32:41 -0700

    added type hints

--- contrib/node/tests/python/pants_test/contrib/node/targets/test_node_thrift_library.py
+++ contrib/node/tests/python/pants_test/contrib/node/targets/test_node_thrift_library.py
@@ -3,12 +3,12 @@ from pants.contrib.node.targets.node_thrift_library import NodeThriftLibrary
 
 
 class NodeThriftLibraryTest(TestBase):
-  def test_bin_executables_string(self):
+  def test_bin_executables_string(self) -> None:
     target = self.make_target(spec=':name', target_type=NodeThriftLibrary, package_name='name',
                               bin_executables='./cli.js')
     self.assertEqual('./cli.js', target.payload.bin_executables)
 
-  def test_bin_executables_dict(self):
+  def test_bin_executables_dict(self) -> None:
     target1 = self.make_target(spec=':name1', target_type=NodeThriftLibrary, package_name='name1',
                                bin_executables='./cli.js')
     target2 = self.make_target(spec=':name2', target_type=NodeThriftLibrary, package_name='name2',
