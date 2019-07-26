44c274109eaed0a0b4a5cf795539119dbaba9dd7
Eric Arellano <ericarellano@me.com>
2019-07-26 16:20:48 -0700

    Rewrite `test_git_hooks.py` to be hermetic (#8085)
    This test failed when remoting because it relied on access to several scripts in `build-support/bin` that were not explicitly declared.
    
    This is fixed by explicitly depending on the scripts. We must jump through some hoops to then copy the scripts into the temporary workdir created by the tests.

--- tests/python/pants_test/repo_scripts/test_git_hooks.py
+++ tests/python/pants_test/repo_scripts/test_git_hooks.py
@@ -3,56 +3,59 @@
 
 import datetime
 import os
+import shutil
 import subprocess
 import unittest
 from contextlib import contextmanager
+from pathlib import Path
 from textwrap import dedent
+from typing import Optional, Sequence
 
 from pants.util.contextutil import temporary_dir
-from pants.util.dirutil import safe_file_dump, touch
-from pants_test.testutils.git_util import get_repo_root, initialize_repo
+from pants.util.dirutil import safe_file_dump, safe_mkdir_for
+from pants_test.testutils.git_util import initialize_repo
 
 
 class PreCommitHookTest(unittest.TestCase):
 
-  def setUp(self):
-    self.pants_repo_root = get_repo_root()
-
   @contextmanager
-  def _create_tiny_git_repo(self):
+  def _create_tiny_git_repo(self, *, copy_files: Optional[Sequence[Path]] = None):
     with temporary_dir() as gitdir,\
          temporary_dir() as worktree:
       # A tiny little fake git repo we will set up. initialize_repo() requires at least one file.
-      readme_file = os.path.join(worktree, 'README')
-      touch(readme_file)
+      (Path(worktree) / 'README').touch()
       # The contextmanager interface is only necessary if an explicit gitdir is not provided.
       with initialize_repo(worktree, gitdir=gitdir) as git:
+        if copy_files is not None:
+          for fp in copy_files:
+            new_fp = Path(worktree) / fp
+            safe_mkdir_for(str(new_fp))
+            shutil.copy(fp, new_fp)
         yield git, worktree, gitdir
 
   def _assert_subprocess_error(self, worktree, cmd, expected_excerpt):
-    proc = subprocess.Popen(
+    result = subprocess.run(
       cmd,
       cwd=worktree,
       stdout=subprocess.PIPE,
       stderr=subprocess.PIPE,
+      encoding="utf-8",
     )
-    (stdout_data, stderr_data) = proc.communicate()
-    stdout_data = stdout_data.decode()
-    stderr_data = stderr_data.decode()
-    self.assertNotEqual(0, proc.returncode)
-    all_output = '{}\n{}'.format(stdout_data, stderr_data)
-    self.assertIn(expected_excerpt, all_output)
+    self.assertNotEqual(0, result.returncode)
+    self.assertIn(expected_excerpt, f'{result.stdout}\n{result.stderr}')
 
   def _assert_subprocess_success(self, worktree, cmd, **kwargs):
     self.assertEqual(0, subprocess.check_call(cmd, cwd=worktree, **kwargs))
 
   def _assert_subprocess_success_with_output(self, worktree, cmd, full_expected_output):
-    output = subprocess.check_output(cmd, cwd=worktree)
-    self.assertEqual(full_expected_output, output.decode())
+    stdout = subprocess.run(
+      cmd, cwd=worktree, check=True, stdout=subprocess.PIPE, encoding="utf-8"
+    ).stdout
+    self.assertEqual(full_expected_output, stdout)
 
   def test_check_packages(self):
-    package_check_script = os.path.join(self.pants_repo_root, 'build-support/bin/check_packages.sh')
-    with self._create_tiny_git_repo() as (_, worktree, _):
+    package_check_script = 'build-support/bin/check_packages.sh'
+    with self._create_tiny_git_repo(copy_files=[Path(package_check_script)]) as (_, worktree, _):
       init_py_path = os.path.join(worktree, 'subdir/__init__.py')
 
       # Check that an invalid __init__.py errors.
@@ -77,9 +80,8 @@ subdir/__init__.py
   # more testing git functionality, but since it's not clear how this is measured, it could be
   # useful if correctly detecting copies and moves ever becomes a concern.
   def test_added_files_correctly_detected(self):
-    get_added_files_script = os.path.join(self.pants_repo_root,
-                                          'build-support/bin/get_added_files.sh')
-    with self._create_tiny_git_repo() as (git, worktree, _):
+    get_added_files_script = 'build-support/bin/get_added_files.sh'
+    with self._create_tiny_git_repo(copy_files=[Path(get_added_files_script)]) as (git, worktree, _):
       # Create a new file.
       new_file = os.path.join(worktree, 'wow.txt')
       safe_file_dump(new_file, '')
@@ -89,15 +91,15 @@ subdir/__init__.py
       self._assert_subprocess_success_with_output(
         worktree, [get_added_files_script],
         # This should be the only entry in the index, and it is a newly added file.
-        full_expected_output="{}\n".format(rel_new_file))
+        full_expected_output=f"{rel_new_file}\n")
 
   def test_check_headers(self):
-    header_check_script = os.path.join(
-      self.pants_repo_root, 'build-support/bin/check_header.py'
-    )
+    header_check_script = 'build-support/bin/check_header.py'
     cur_year_num = datetime.datetime.now().year
     cur_year = str(cur_year_num)
-    with self._create_tiny_git_repo() as (_, worktree, _):
+    with self._create_tiny_git_repo(
+      copy_files=[Path(header_check_script), "build-support/bin/common.py"]
+    ) as (_, worktree, _):
       new_py_path = os.path.join(worktree, 'subdir/file.py')
 
       def assert_header_check(added_files, expected_excerpt):
@@ -122,12 +124,11 @@ subdir/__init__.py
       )
 
       # Check that a file with a typo in the header fails
-      safe_file_dump(new_py_path, dedent("""\
-        # Copyright {} Pants project contributors (see CONTRIBUTORS.md).
+      safe_file_dump(new_py_path, dedent(f"""\
+        # Copyright {cur_year} Pants project contributors (see CONTRIBUTORS.md).
         # Licensed under the MIT License, Version 3.3 (see LICENSE).
 
-        """.format(cur_year))
-      )
+        """))
       assert_header_check(
         added_files=[],
         expected_excerpt="subdir/file.py: header does not match the expected header"
@@ -144,22 +145,22 @@ subdir/__init__.py
         added_files=[],
         expected_excerpt=(
           r"subdir/file.py: copyright year must match '20\d\d' (was YYYY): "
-          "current year is {}".format(cur_year)
+          f"current year is {cur_year}"
         )
       )
 
       # Check that a newly added file must have the current year.
       last_year = str(cur_year_num - 1)
-      safe_file_dump(new_py_path, dedent("""\
-        # Copyright {} Pants project contributors (see CONTRIBUTORS.md).
+      safe_file_dump(new_py_path, dedent(f"""\
+        # Copyright {last_year} Pants project contributors (see CONTRIBUTORS.md).
         # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-        """.format(last_year))
+        """)
       )
       rel_new_py_path = os.path.relpath(new_py_path, worktree)
       assert_header_check(
         added_files=[rel_new_py_path],
-        expected_excerpt="subdir/file.py: copyright year must be {} (was {})".format(cur_year, last_year)
+        expected_excerpt=f"subdir/file.py: copyright year must be {cur_year} (was {last_year})"
       )
 
       # Check that a file isn't checked against the current year if it is not passed as an
