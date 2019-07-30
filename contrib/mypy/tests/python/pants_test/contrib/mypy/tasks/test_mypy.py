218653175694780da7e348429d1d5dc49194297a
Mohamed Abdi <mabdi@twitter.com>
2019-07-30 10:46:55 -0700

    add year to copyright note/comment

--- contrib/mypy/tests/python/pants_test/contrib/mypy/tasks/test_mypy.py
+++ contrib/mypy/tests/python/pants_test/contrib/mypy/tasks/test_mypy.py
@@ -1,4 +1,4 @@
-# Copyright YYYY Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from pants.contrib.mypy.tasks.mypy_task import MypyTask, MypyTaskError
