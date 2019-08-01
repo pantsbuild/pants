c5bb9dea36ea9dcfdb4781a28f24b105f80a2a94
Ekaterina Tyurina <etyurina@twitter.com>
2019-08-01 15:04:46 +0100

    Add test to check return of hex_16_digit_string function

--- src/rust/engine/workunit_store/src/lib.rs
+++ src/rust/engine/workunit_store/src/lib.rs
@@ -116,6 +116,13 @@ mod tests {
     assert_eq!(16, hex_string.len());
     for ch in hex_string.chars() {
       assert!(ch.is_ascii_hexdigit())
-    }
+    };
+  }
+
+  #[test]
+  fn hex_16_digit_string_actually_uses_input_number() {
+    assert_eq!(hex_16_digit_string(0x_ffff_ffff_ffff_ffff), "ffffffffffffffff");
+    assert_eq!(hex_16_digit_string(0x_1), "0000000000000001");
+    assert_eq!(hex_16_digit_string(0x_0123_4567_89ab_cdef), "0123456789abcdef");
   }
 }
