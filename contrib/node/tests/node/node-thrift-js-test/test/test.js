d418e848600a64e20f9bd0eacf47437d852cb6a0
Satwik Singh <satwiks@twitter.com>
2019-08-01 14:24:24 -0700

    Changing dir structure for integration tests

---
+++ contrib/node/tests/node/node-thrift-js-test/test/test.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+import 'babel-polyfill'
+import assert from 'assert'
+import Name from 'employee-dep'
+
+describe('Testing Node Thrift JS generation', () => {
+  describe('Employee Test', () => {
+
+  })
+})
