5764f3b47406770c837e4c4aff95281a43e66bfd
Satwik Singh <satwiks@twitter.com>
2019-08-07 10:51:13 -0700

    Fixed js generated code test

--- contrib/node/tests/node/node-thrift-js-test/test/test.js
+++ contrib/node/tests/node/node-thrift-js-test/test/test.js
@@ -1,10 +1,14 @@
 /* eslint-env mocha */
 import 'babel-polyfill'
 import assert from 'assert'
-import Name from 'employee-dep'
+import Name from 'employee-dep' //full path contrib.node..
 
 describe('Testing Node Thrift JS generation', () => {
   describe('Employee Test', () => {
-
+    it('setting name should reflect changes', () => {
+      var test_name = Name;
+      test_name.name = "Satwik";
+      assert.ok(test_name.name === "Satwik");
+    })
   })
 })
