ac29d0ec708c14eae4582804c7bf2a678cfda65f
Satwik Singh <satwiks@twitter.com>
2019-08-12 15:48:17 -0700

    Linter fix

--- src/python/pants/backend/codegen/thrift/lib/apache_thrift_gen_base.py
+++ src/python/pants/backend/codegen/thrift/lib/apache_thrift_gen_base.py
@@ -20,6 +20,7 @@ from pants.util.memo import memoized_property
 
 
 class ApacheThriftGenBase(SimpleCodegenTask):
+
   # The name of the thrift generator to use. Subclasses must set.
   # E.g., java, py (see `thrift -help` for all available generators).
   @classproperty
