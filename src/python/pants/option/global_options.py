6695a6990131b9b6fe2065ff79cb80c0c6ae0e3e
Eric Arellano <ericarellano@me.com>
2019-08-12 17:19:09 -0700

    Recognize multiple sentinel files for determining the build root (#8105)
    ## Problem
    We cannot have implicit runtime dependencies on files in the buildroot - they must all be declared explicitly via `BUILD` entries. https://github.com/pantsbuild/pants/pull/8066 fixed several of these issues, but not all. 
    
    However, we cannot explicitly depend on the file `./pants` because it breaks V1 test execution. When running tests with V1, the folder in `.pants.d` will strip the prefix so `src/python/pants` -> `pants`. You cannot both have a directory named `pants` and a file named `pants`, so this causes most V1 tests to fail.
    
    ## Solution
    Use multiple sentinel files to establish the buildroot, such as `BUILD_ROOT`. This allows us to safely depend on a sentinel file with very few code changes.

--- src/python/pants/option/global_options.py
+++ src/python/pants/option/global_options.py
@@ -117,7 +117,7 @@ class GlobalOptionsRegistrar(SubsystemClientMixin, Optionable):
     buildroot = get_buildroot()
     default_distdir_name = 'dist'
     default_distdir = os.path.join(buildroot, default_distdir_name)
-    default_rel_distdir = '/{}/'.format(default_distdir_name)
+    default_rel_distdir = f'/{default_distdir_name}/'
 
     register('-l', '--level', choices=['trace', 'debug', 'info', 'warn'], default='info',
              recursive=True, help='Set the logging level.')
