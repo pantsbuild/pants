cd6e7ae4d654fe5516e5c114e5b07539713ea522
Alex Schmitt <codealchemy@users.noreply.github.com>
2019-08-13 11:41:48 -0700

    Allow for fetching yarn directly via official releases (#8170)
    ### Problem (and solution)
    
    Same as https://github.com/pantsbuild/pants/pull/7405
    
    [Slack thread](https://pantsbuild.slack.com/archives/C046T6T9U/p1565633047078400) that highlighted this issue.
    
    ### Result
    
    Users can now use any supported `yarn` release.
    
    Related `binaries` repo script for building yarn [`here`](https://github.com/pantsbuild/binaries/blob/master/build-yarnpkg.sh).

--- contrib/node/src/python/pants/contrib/node/subsystems/yarnpkg_distribution.py
+++ contrib/node/src/python/pants/contrib/node/subsystems/yarnpkg_distribution.py
@@ -4,9 +4,18 @@
 import logging
 
 from pants.binaries.binary_tool import NativeTool
+from pants.binaries.binary_util import BinaryToolUrlGenerator
 
 
 logger = logging.getLogger(__name__)
+
+
+class YarnReleaseUrlGenerator(BinaryToolUrlGenerator):
+
+  _DIST_URL_FMT = 'https://github.com/yarnpkg/yarn/releases/download/{version}/yarn-{version}.tar.gz'
+
+  def generate_urls(self, version, host_platform):
+    return [self._DIST_URL_FMT.format(version=version)]
 
 
 class YarnpkgDistribution(NativeTool):
@@ -16,3 +25,6 @@ class YarnpkgDistribution(NativeTool):
   name = 'yarnpkg'
   default_version = 'v1.6.0'
   archive_type = 'tgz'
+
+  def get_external_url_generator(self):
+    return YarnReleaseUrlGenerator()
