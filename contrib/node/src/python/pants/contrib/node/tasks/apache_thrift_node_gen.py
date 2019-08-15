19a9fe1883f8c8496df8cb23c72330c6f1d56618
Satwik Singh <satwiks@twitter.com>
2019-08-15 14:37:34 -0700

    Fixed synthetic dependency issue and not checking for preexisting yarn and package files

--- contrib/node/src/python/pants/contrib/node/tasks/apache_thrift_node_gen.py
+++ contrib/node/src/python/pants/contrib/node/tasks/apache_thrift_node_gen.py
@@ -35,27 +35,21 @@ class ApacheThriftNodeGen(ApacheThriftGenBase):
 
   def execute_codegen(self, target, target_workdir):
     sources_list = target.sources_relative_to_target_base()
-    if 'yarn.lock' not in sources_list.files:
-      safe_file_dump(os.path.join(target_workdir, 'yarn.lock'))
-
-    if 'package.json' not in sources_list.files:
-      dependency_list = target.dependencies
-      package_dict = {}
-      package_dict["name"] = target.name
-      package_dict["version"] = self._thrift_version
-      main_name = os.path.basename(target.sources_relative_to_buildroot()[0])
-      package_dict["main"] = re.sub(r'\.thrift', '_types.js', main_name)
-      dep_dict = {}
-      for dep in dependency_list:
-        if not isinstance(dep, NodeModule):
-          raise TaskError("One of the dependencies is not an instance of NodeModule.")
-        dep_spec = dep.address.spec_path
-        relative_path = os.path.relpath(dep_spec, target_workdir)
-        relative_path = "file:" + relative_path
-        dep_dict[dep.name] = relative_path
-      dep_dict["thrift"] = self._thrift_version
-      package_dict["dependencies"] = dep_dict
-      with open(os.path.join(target_workdir, 'package.json'), 'w') as f:
-        json.dump(package_dict, f, ensure_ascii=False, indent=2)
+    safe_file_dump(os.path.join(target_workdir, 'yarn.lock'))
+    dependency_list = target.dependencies
+    package_dict = {}
+    package_dict["name"] = target.name
+    dep_dict = {}
+    for dep in dependency_list:
+      if not isinstance(dep, NodeModule):
+        raise TaskError("One of the dependencies is not an instance of NodeModule.")
+      dep_spec = dep.address.spec_path
+      relative_path = os.path.relpath(dep_spec, target_workdir)
+      relative_path = "file:" + relative_path
+      dep_dict[dep.name] = relative_path
+    dep_dict["thrift"] = self._thrift_version
+    package_dict["dependencies"] = dep_dict
+    with open(os.path.join(target_workdir, 'package.json'), 'w') as f:
+      json.dump(package_dict, f, ensure_ascii=False, indent=2)
 
     super().execute_codegen(target, target_workdir)
