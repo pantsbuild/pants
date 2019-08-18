d0da49c142520846a7bb98a91de9b18f654a3e2b
Eric Arellano <ericarellano@me.com>
2019-08-17 18:59:35 -0700

    Replace old .travis.yml

--- build-support/travis/generate_travis_yml_main.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
-# Licensed under the Apache License, Version 2.0 (see LICENSE).
-
-from generate_travis_yml import generate_travis_yml
-
-
-if __name__ == '__main__':
-  generate_travis_yml()
