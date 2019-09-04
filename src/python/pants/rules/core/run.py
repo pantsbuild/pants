cad94be3488f1995f0db5b772e68606100a18171
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-09-03 19:25:28 -0700

    some cleanup

--- src/python/pants/rules/core/run.py
+++ src/python/pants/rules/core/run.py
@@ -11,6 +11,7 @@ from pants.engine.goal import Goal
 from pants.engine.legacy.graph import HydratedTarget
 from pants.engine.rules import console_rule, rule, union
 from pants.engine.selectors import Get
+from pants.util.collections import assert_single_element
 
 
 # TODO(#6004): use proper Logging singleton, rather than static logger.
@@ -35,23 +36,27 @@ class RunTarget:
   pass
 
 
+class RunError(Exception):
+  """???"""
+
+
 @console_rule(Run, [Console, BuildFileAddresses])
 def run(console, addresses):
-  run_results = yield [Get(RunResult, Address, address.to_address()) for address in addresses]
+  try:
+    address = assert_single_element(list(addresses))
+  except (StopIteration, ValueError):
+    raise RunError(f"the `run` goal requires exactly one top-level target! received: {addresses}")
+  result = yield Get(RunResult, Address, address.to_address())
 
-  exit_codes = []
-  for result in run_results:
-    exit_codes.append(result.status)
-    if result.status != 0:
-      console.write_stderr(f"failed with code {result.status}!\n")
-      console.write_stderr(f"stdout: {result.stdout}\n")
-      console.write_stderr(f"stderr: {result.stderr}\n")
-    else:
-      console.write_stderr("passed!")
-      console.write_stdout(result.stdout)
-      console.write_stderr(result.stderr)
+  if result.status != 0:
+    console.write_stderr(f"failed with code {result.status}!\n")
+    console.write_stderr(f"stdout: {result.stdout}\n")
+    console.write_stderr(f"stderr: {result.stderr}\n")
+  else:
+    console.write_stdout(result.stdout)
+    console.write_stderr(result.stderr)
 
-  yield Run(max(exit_codes))
+  yield Run(result.status)
 
 
 @rule(RunResult, [HydratedTarget])
