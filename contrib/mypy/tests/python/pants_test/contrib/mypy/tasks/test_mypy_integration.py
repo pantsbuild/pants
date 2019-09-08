60730df0d9dbfbd22148e30cfdfd8970285f6c66
Eric Arellano <ericarellano@me.com>
2019-09-07 21:34:44 -0700

    Port contrib/mypy

--- contrib/mypy/tests/python/pants_test/contrib/mypy/tasks/test_mypy_integration.py
+++ contrib/mypy/tests/python/pants_test/contrib/mypy/tasks/test_mypy_integration.py
@@ -1,30 +1,17 @@
 # Copyright 2017 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants_test.interpreter_selection_utils import has_python_version
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
 
 
 class MypyIntegrationTest(PantsRunIntegrationTest):
-  def test_mypy(self):
-    cmd = [
-      '--backend-packages=pants.contrib.mypy',
-      '--tag=integration_test'
-      '--lint-skip',
-      '--no-lint-mypy-skip',
-      'lint',
-      'contrib/mypy/tests/python/pants_test/contrib/mypy::',
-      '--',
-      '--follow-imports=silent'
-    ]
 
-    if any((has_python_version('3.{}'.format(v)) for v in (5, 6, 7, 8))):
-      # Python 3.5+ is available. Test that we see an error in this integration test.
-      with self.pants_results(cmd) as pants_run:
-        self.assert_success(pants_run)
-    else:
-      # Python 3.5+ was not found. Test whether mypy task fails for that reason.
-      with self.pants_results(cmd) as pants_run:
-        self.assert_failure(pants_run)
-        self.assertIn('Unable to find a Python >=3.5 interpreter (required for mypy)',
-                      pants_run.stdout_data)
+  cmdline = ['--backend-packages=pants.contrib.mypy', 'lint']
+
+  def test_valid_type_hints(self):
+    result = self.run_pants([*self.cmdline, 'contrib/mypy/examples/src/python:valid'])
+    self.assert_success(result)
+
+  def test_invalid_type_hints(self):
+    result = self.run_pants([*self.cmdline, 'contrib/mypy/examples/src/python:invalid'])
+    self.assert_failure(result)
