39e40816f5990f48ee54e2f23cf0b8a5381dfe7a
Daniel Wagner-Hall <dwagnerhall@twitter.com>
2019-09-09 23:53:22 +0100

    Use new intrinsic for making empty files
    Fixes #7718.
    
    This currently fails because of weird ambiguity errors.

--- src/python/pants/backend/python/rules/inject_init.py
+++ src/python/pants/backend/python/rules/inject_init.py
@@ -2,8 +2,8 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from pants.backend.python.subsystems.pex_build_util import identify_missing_init_files
-from pants.engine.fs import EMPTY_DIRECTORY_DIGEST, Digest, Snapshot
-from pants.engine.isolated_process import ExecuteProcessRequest, ExecuteProcessResult
+from pants.engine.fs import (EMPTY_DIRECTORY_DIGEST, Digest, FileContent, FilesContent, Snapshot,
+                             InputFilesContent)
 from pants.engine.rules import rule
 from pants.engine.selectors import Get
 from pants.util.objects import datatype
@@ -20,16 +20,12 @@ def inject_init(snapshot):
   if not missing_init_files:
     new_init_files_digest = EMPTY_DIRECTORY_DIGEST
   else:
-    # TODO(7718): add a builtin rule for FilesContent->Snapshot, so that we can avoid using touch
-    # and the absolute path and have the engine build the files for us.
-    touch_init_request = ExecuteProcessRequest(
-      argv=("/usr/bin/touch",) + missing_init_files,
-      output_files=missing_init_files,
-      description="Inject missing __init__.py files: {}".format(", ".join(missing_init_files)),
-      input_files=snapshot.directory_digest,
-    )
-    touch_init_result = yield Get(ExecuteProcessResult, ExecuteProcessRequest, touch_init_request)
-    new_init_files_digest = touch_init_result.output_directory_digest
+    new_init_files_digest = yield Get(Digest, InputFilesContent(FilesContent(tuple(
+      FileContent(
+        path=path,
+        content=b'',
+      ) for path in missing_init_files
+    ))))
   # TODO(#7710): Once this gets fixed, merge the original source digest and the new init digest
   # into one unified digest.
   yield InjectedInitDigest(directory_digest=new_init_files_digest)
