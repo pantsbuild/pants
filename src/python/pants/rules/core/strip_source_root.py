7ed040c5ba08fd03f42e57268cac385b03802465
gshuflin <greg.shuflin@toolchain.com>
2019-09-24 23:08:48 -0700

    v2 rule for stripping source roots (#8326)
    Problem
    -------
    #8185 introduced proper support stripping source roots for targets, but keeping the full path for loose files.
    
    Solution
    --------
    Now that we are porting the rest of the V2 pipeline, e.g. #8236, this functionality should be extracted out into a new rule.

---
+++ src/python/pants/rules/core/strip_source_root.py
@@ -0,0 +1,51 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.build_graph.files import Files
+from pants.engine.fs import EMPTY_SNAPSHOT, Digest, DirectoryWithPrefixToStrip, Snapshot
+from pants.engine.legacy.graph import HydratedTarget
+from pants.engine.rules import optionable_rule, rule
+from pants.engine.selectors import Get
+from pants.source.source_root import SourceRootConfig
+from pants.util.objects import datatype
+
+
+class SourceRootStrippedSources(datatype([('snapshot', Snapshot)])):
+  """Wrapper for a snapshot of targets whose source roots have been stripped."""
+
+
+@rule(SourceRootStrippedSources, [HydratedTarget, SourceRootConfig])
+def strip_source_root(hydrated_target, source_root_config):
+  """Relativize targets to their source root, e.g.
+  `src/python/pants/util/strutil.py` -> `pants/util/strutil.py ."""
+
+  target_adaptor = hydrated_target.adaptor
+  source_roots = source_root_config.get_source_roots()
+
+  # TODO: make TargetAdaptor return a 'sources' field with an empty snapshot instead of raising to
+  # simplify the hasattr() checks here!
+  if not hasattr(target_adaptor, 'sources'):
+    yield SourceRootStrippedSources(snapshot=EMPTY_SNAPSHOT)
+
+  digest = target_adaptor.sources.snapshot.directory_digest
+  source_root = source_roots.find_by_path(target_adaptor.address.spec_path)
+
+  # Loose `Files`, as opposed to `Resources` or `Target`s, have no (implied) package
+  # structure and so we do not remove their source root like we normally do, so that filesystem
+  # APIs may still access the files. See pex_build_util.py's `_create_source_dumper`.
+  if target_adaptor.type_alias == Files.alias():
+    source_root = None
+
+  resulting_digest = yield Get(
+    Digest,
+    DirectoryWithPrefixToStrip(
+      directory_digest=digest,
+      prefix=source_root.path if source_root else ""
+    )
+  )
+  resulting_snapshot = yield Get(Snapshot, Digest, resulting_digest)
+  yield SourceRootStrippedSources(snapshot=resulting_snapshot)
+
+
+def rules():
+  return [strip_source_root, optionable_rule(SourceRootConfig)]
---
+++ src/python/pants/rules/core/strip_source_roots_test.py
@@ -0,0 +1,52 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from unittest.mock import Mock
+
+from pants.build_graph.files import Files
+from pants.engine.fs import create_fs_rules
+from pants.engine.legacy.graph import HydratedTarget
+from pants.engine.rules import RootRule
+from pants.engine.selectors import Params
+from pants.rules.core.strip_source_root import SourceRootStrippedSources, strip_source_root
+from pants.source.source_root import SourceRootConfig
+from pants_test.subsystem.subsystem_util import init_subsystem
+from pants_test.test_base import TestBase
+
+
+class StripSourceRootsTests(TestBase):
+  @classmethod
+  def rules(cls):
+    return super().rules() + [
+      strip_source_root,
+      RootRule(SourceRootConfig),
+      RootRule(HydratedTarget),
+    ] + create_fs_rules()
+
+  def assert_stripped_source_file(self, *, original_path: str, expected_path: str, target_type_alias = None):
+    init_subsystem(SourceRootConfig)
+    adaptor = Mock()
+    adaptor.sources = Mock()
+    source_files = {original_path: "print('random python')"}
+    adaptor.sources.snapshot = self.make_snapshot(source_files)
+    adaptor.address = Mock()
+    adaptor.address.spec_path = original_path
+    if target_type_alias:
+      adaptor.type_alias = target_type_alias
+    target = HydratedTarget('some/target/address', adaptor, tuple())
+    output = self.scheduler.product_request(SourceRootStrippedSources, [Params(target, SourceRootConfig.global_instance())])
+    stripped_sources = output[0]
+    self.assertEqual(stripped_sources.snapshot.files, (expected_path,))
+
+  def test_source_roots_python(self):
+    self.assert_stripped_source_file(original_path='src/python/pants/util/strutil.py', expected_path='pants/util/strutil.py')
+
+  def test_source_roots_java(self):
+    self.assert_stripped_source_file(original_path='src/java/some/path/to/something.java', expected_path='some/path/to/something.java')
+
+  def test_dont_strip_source_for_files(self):
+    self.assert_stripped_source_file(
+        original_path='src/python/pants/util/strutil.py',
+        expected_path='src/python/pants/util/strutil.py',
+        target_type_alias=Files.alias()
+    )
