8da8a76ef83f0a920440fb0f12eb122dea303527
Benjy Weinberger <benjyw@gmail.com>
2019-10-03 16:44:09 -0700

    Try something.

--- build-support/bin/generate_travis_yml.py
+++ build-support/bin/generate_travis_yml.py
@@ -589,6 +589,7 @@ def rust_tests_osx() -> Dict:
       }
     },
     "before_install": [
+      'sudo /Library/Filesystems/osxfuse.fs/Contents/Resources/load_osxfuse',
       './build-support/bin/install_python_for_ci.sh "${PYENV_PY36_VERSION}"',
     ],
     "env": _osx_env_with_pyenv(python_version=PythonVersion.py36) + [
