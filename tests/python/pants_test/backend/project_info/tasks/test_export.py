53261194da67b7a0c0d65d77669515e3208650ad
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-10-04 08:42:03 -0700

    ...make python requirement export test pass

--- tests/python/pants_test/backend/project_info/tasks/test_export.py
+++ tests/python/pants_test/backend/project_info/tasks/test_export.py
@@ -66,6 +66,21 @@ class ExportTest(ConsoleTaskTestBase):
       jars=[JarDependency('org.pantsbuild', 'jar-tool', '0.0.10')]
     )
 
+    # NB: `test_has_python_requirements` will attempt to inject every possible scala compiler target
+    # spec, for versions 2.10, 2.11, 2.12, and custom, and will error out if they are not
+    # available. This isn't a problem when running pants on the command line, but in unit testing
+    # there's probably some task or subsystem that needs to be initialized to avoid this.
+    for empty_target in ['scalac', 'scala-repl', 'scala-library', 'scala-reflect', 'scalastyle']:
+      for unused_scala_version in ['2_10', '2_11']:
+        self.make_target(
+          f':{empty_target}_{unused_scala_version}',
+          Target,
+        )
+    self.make_target(
+      ':scalac-plugin-dep',
+      Target,
+    )
+
     self.make_target(
       ':jarjar',
       JarLibrary,
@@ -88,6 +103,29 @@ class ExportTest(ConsoleTaskTestBase):
       ':scalac_2_12',
       JarLibrary,
       jars=[JarDependency('org.scala-lang', 'scala-compiler', '2.12.8')]
+    )
+    self.make_target(
+      ':scala-library_2_12',
+      JarLibrary,
+      jars=[JarDependency('org.scala-lang', 'scala-library', '2.12.8')]
+    )
+    self.make_target(
+      ':scala-reflect_2_12',
+      JarLibrary,
+      jars=[JarDependency('org.scala-lang', 'scala-reflect', '2.12.8')]
+    )
+    self.make_target(
+      ':scala-repl_2_12',
+      JarLibrary,
+      jars=[JarDependency('org.scala-lang', 'scala-repl', '2.12.8')]
+    )
+    self.make_target(
+      ':scalastyle_2_12',
+      Target,
+    )
+    self.make_target(
+      ':scalastyle',
+      Target,
     )
 
 
