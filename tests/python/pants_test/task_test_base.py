5bb54c578808bd988711a11498bc751163afece3
Eric Arellano <ericarellano@me.com>
2019-10-05 17:49:35 -0700

    Fix things

--- tests/python/pants_test/task_test_base.py
+++ tests/python/pants_test/task_test_base.py
@@ -1,6 +1,8 @@
 # Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from pants.testinfra.task_test_base import ConsoleTaskTestBase as ConsoleTaskTestBase
+from pants.testinfra.task_test_base import DeclarativeTaskTestMixin as DeclarativeTaskTestMixin
 from pants.testinfra.task_test_base import TaskTestBase as TaskTestBase
 from pants.testinfra.task_test_base import ensure_cached as ensure_cached
 from pants.testinfra.task_test_base import is_exe as is_exe
--- tests/python/pants_test/test_base.py
+++ tests/python/pants_test/test_base.py
@@ -1,4 +1,5 @@
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from pants.testinfra.test_base import AbstractTestGenerator as AbstractTestGenerator
 from pants.testinfra.test_base import TestBase as TestBase
