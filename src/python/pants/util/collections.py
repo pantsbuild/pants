922c0196779ab324e7a875a8b6642c183e29ee27
Eric Arellano <ericarellano@me.com>
2019-10-11 08:50:53 -0700

    Rename TypeVars to be private so that autocomplete doesn't suggest them

--- src/python/pants/util/collections.py
+++ src/python/pants/util/collections.py
@@ -15,11 +15,11 @@ from typing import (
 )
 
 
-K = TypeVar('K')
-V = TypeVar('V')
+_K = TypeVar('_K')
+_V = TypeVar('_V')
 
 
-def factory_dict(value_factory: Callable[[K], V], *args, **kwargs) -> DefaultDict:
+def factory_dict(value_factory: Callable[[_K], _V], *args, **kwargs) -> DefaultDict:
   """A dict whose values are computed by `value_factory` when a `__getitem__` key is missing.
 
   Note that values retrieved by any other method will not be lazily computed; eg: via `get`.
@@ -55,10 +55,10 @@ def recursively_update(d: MutableMapping, d2: MutableMapping) -> None:
     d[k] = v
 
 
-T = TypeVar('T')
+_T = TypeVar('_T')
 
 
-def assert_single_element(iterable: Iterable[T]) -> T:
+def assert_single_element(iterable: Iterable[_T]) -> _T:
   """Get the single element of `iterable`, or raise an error.
 
   :raise: :class:`StopIteration` if there is no element.
@@ -75,7 +75,7 @@ def assert_single_element(iterable: Iterable[T]) -> T:
   raise ValueError(f"iterable {iterable!r} has more than one element.")
 
 
-E = TypeVar('E', bound='Enum')
+_E = TypeVar('_E', bound='Enum')
 
 
 class EnumMatchError(ValueError):
@@ -96,7 +96,7 @@ class Enum(StdLibEnum):
   def all_values(cls) -> ValuesView['Enum']:
     return cls.__members__.values()
 
-  def match(self, enum_values_to_results: Mapping[E, V]) -> V:
+  def match(self, enum_values_to_results: Mapping[_E, _V]) -> _V:
     unrecognized_values = [
       value for value in enum_values_to_results if value not in self.all_values()
     ]
@@ -111,5 +111,5 @@ class Enum(StdLibEnum):
       raise InexhaustiveMatchError(
         f"All enum values must be covered by the match. Missing: {missing_values}"
       )
-    typed_self = cast(E, self)
+    typed_self = cast(_E, self)
     return enum_values_to_results[typed_self]
