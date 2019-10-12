b702ba6fbb1ed9f792dd0555a51612c8d7a3df90
Eric Arellano <ericarellano@me.com>
2019-10-12 15:54:53 -0700

    Improve mypy.py script to take args and have less verbose error message

--- build-support/bin/mypy.py
+++ build-support/bin/mypy.py
@@ -2,26 +2,37 @@
 # Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import argparse
 import subprocess
+
+from common import die
 
 
 def main() -> None:
-  subprocess.run([
-    "./pants",
-    # We filter by tag to avoid irrelevant targets causing issues due to prerequisite goals against
-    # the lint goal.
-    "--tag=type_checked",
-    "--backend-packages=pants.contrib.mypy",
-    "lint",
-    "--lint-mypy-verbose",
-    "--lint-mypy-whitelist-tag-name=type_checked",
-    "--lint-mypy-config-file=build-support/mypy/mypy.ini",
-    "build-support::",
-    "contrib::",
-    "src/python::",
-    "tests/python::",
-    "pants-plugins::",
-  ], check=True)
+  globs = create_parser().parse_args().globs
+  try:
+    subprocess.run([
+      "./pants",
+      "--tag=type_checked",
+      "--backend-packages=pants.contrib.mypy",
+      "lint",
+      "--lint-mypy-verbose",
+      "--lint-mypy-whitelist-tag-name=type_checked",
+      "--lint-mypy-config-file=build-support/mypy/mypy.ini",
+      *globs,
+    ], check=True)
+  except subprocess.CalledProcessError:
+    die("Please fix the above errors and run again.")
+
+
+def create_parser() -> argparse.ArgumentParser:
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    "globs",
+    nargs='*',
+    default=["build-support::", "contrib::", "src/python::", "tests/python::", "pants-plugins::"]
+  )
+  return parser
 
 
 if __name__ == '__main__':
