2549d69e9682cdf24c30d245f11ee3752a1649c7
Pierre Chevalier <pchevalier@twitter.com>
2019-10-14 12:13:51 +0100

    Test python fmt: don't depend on actual code formatting

--- tests/python/pants_test/backend/python/rules/test_python_fmt_integration.py
+++ tests/python/pants_test/backend/python/rules/test_python_fmt_integration.py
@@ -1,31 +1,73 @@
 # Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from contextlib import contextmanager
 from os.path import relpath
 from pathlib import Path
 
-from pants.util.contextutil import temporary_file, temporary_file_path
+from pants.util.contextutil import temporary_dir, temporary_file_path
 from pants_test.pants_run_integration_test import PantsRunIntegrationTest
+
+
+def write_build_file(root_dir):
+  build = Path(root_dir, "BUILD")
+  build.touch
+  build.write_text("python_library()")
+
+INCONSISTENTLY_FORMATTED_HELLO: str = "def hello():x=42"
+CONSISTENTLY_FORMATTED_HELLO: str = "def hello():\n    x = 42\n"
+
+
+def write_inconsistently_formatted_file(root_dir, filename) -> Path:
+  code = Path(root_dir, filename)
+  code.touch()
+  code.write_text(INCONSISTENTLY_FORMATTED_HELLO)
+  return code
+
+
+def write_consistently_formatted_file(root_dir, filename) -> Path:
+  code = Path(root_dir, filename)
+  code.touch()
+  code.write_text(CONSISTENTLY_FORMATTED_HELLO)
+  return code
+
+
+@contextmanager
+def build_directory():
+  with temporary_dir(root_dir=".") as root_dir:
+    write_build_file(root_dir)
+    yield root_dir
 
 
 class PythonFmtIntegrationTest(PantsRunIntegrationTest):
   def test_black_one_python_source_should_leave_one_file_unchanged(self):
-    command = [
-      'fmt-v2',
-      'examples/src/python/example/hello/main:main'
+    with build_directory() as root_dir:
+      code = write_consistently_formatted_file(root_dir, "hello.py")
+      command = [
+        'fmt-v2',
+        f'{root_dir}'
       ]
-    pants_run = self.run_pants(command=command)
-    self.assert_success(pants_run)
+      pants_run = self.run_pants(command=command)
+      self.assert_success(pants_run)
+      after_formatting = code.read_text()
+      self.assertEqual(CONSISTENTLY_FORMATTED_HELLO, after_formatting)
     self.assertNotIn("reformatted", pants_run.stderr_data)
     self.assertIn("1 file left unchanged", pants_run.stderr_data)
 
   def test_black_two_python_sources_should_leave_two_files_unchanged(self):
-    command = [
-      'fmt-v2',
-      'examples/src/python/example/hello/greet:greet'
+    with build_directory() as root_dir:
+      foo = write_consistently_formatted_file(root_dir, "foo.py")
+      bar = write_consistently_formatted_file(root_dir, "bar.py")
+      command = [
+        'fmt-v2',
+        f'{root_dir}'
       ]
-    pants_run = self.run_pants(command=command)
-    self.assert_success(pants_run)
+      pants_run = self.run_pants(command=command)
+      self.assert_success(pants_run)
+      foo_after_formatting = foo.read_text()
+      self.assertEqual(CONSISTENTLY_FORMATTED_HELLO, foo_after_formatting)
+      bar_after_formatting = bar.read_text()
+      self.assertEqual(CONSISTENTLY_FORMATTED_HELLO, bar_after_formatting)
     self.assertNotIn("reformatted", pants_run.stderr_data)
     self.assertIn("2 files left unchanged", pants_run.stderr_data)
 
@@ -57,18 +99,15 @@ class PythonFmtIntegrationTest(PantsRunIntegrationTest):
     self.assertIn("1 file failed to reformat", pants_run.stderr_data)
 
   def test_black_should_format_python_code(self):
-    # Open file in the greet target as the BUILD file globs python files from there
-    with temporary_file(root_dir="./examples/src/python/example/hello/greet/", suffix=".py") as code:
-      file_name = code.name
-      code.write(b"x     = 42")
-      code.close()
+    with build_directory() as root_dir:
+      code = write_inconsistently_formatted_file(root_dir, "hello.py")
       command = [
         'fmt-v2',
-        'examples/src/python/example/hello/greet:greet'
-        ]
+        f'{root_dir}'
+      ]
       pants_run = self.run_pants(command=command)
-      formatted = Path(file_name).read_text();
-      self.assertEqual("x = 42\n", formatted)
-    self.assert_success(pants_run)
+      self.assert_success(pants_run)
+      after_formatting = code.read_text()
+      self.assertEqual(CONSISTENTLY_FORMATTED_HELLO, after_formatting)
     self.assertIn("1 file reformatted", pants_run.stderr_data)
-    self.assertIn("2 files left unchanged", pants_run.stderr_data)
+    self.assertNotIn("unchanged", pants_run.stderr_data)
