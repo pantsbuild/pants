9603ec9f3acce33408927510aa0781a62e905793
Pierre Chevalier <pchevalier@twitter.com>
2019-10-14 12:17:18 +0100

    Reformat python example for consistency with pycodestyle

--- examples/src/python/example/hello/main/main.py
+++ examples/src/python/example/hello/main/main.py
@@ -7,6 +7,6 @@ from example.hello.greet.greet import greet
 
 
 if __name__ == "__main__":
-    greetees = sys.argv[1:] or ["world"]
-    for greetee in greetees:
-        print(greet(greetee))
+  greetees = sys.argv[1:] or ["world"]
+  for greetee in greetees:
+    print(greet(greetee))
