907f3e82399c2771b6598263c609f1ca99a033df
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-10-17 23:26:15 -0500

    fix NameError in ExceptionSink.trapped_signals() if .reset_signal_handler() raises (#8482)
    Note: As noted in #8478, I didn't want to add any more flaky signal handling tests. I would love to be able to add testing for this behavior, and am very open to suggestions.
    
    ### Problem
    
    Currently, if `ExceptionSink.reset_signal_handler()` raises within `.trapped_signals()`, the variable `previous_signal_handler` is undefined, see: https://github.com/pantsbuild/pants/blob/0e993240518d344c5db9a066f14467cb81048fa7/src/python/pants/base/exception_sink.py#L374-L378 The cases where this shows up have not yet been investigated.
    
    ### Solution
    
    - Define `previous_signal_handler = None` at the top of the method.
    
    ### Result
    
    We don't add an unrelated `NameError` exception to the stacktrace if resetting the signal handler fails!

--- src/python/pants/base/exception_sink.py
+++ src/python/pants/base/exception_sink.py
@@ -371,8 +371,8 @@ class ExceptionSink:
 
     NB: This method calls signal.signal(), which will crash if not called from the main thread!
     """
+    previous_signal_handler = cls.reset_signal_handler(new_signal_handler)
     try:
-      previous_signal_handler = cls.reset_signal_handler(new_signal_handler)
       yield
     finally:
       cls.reset_signal_handler(previous_signal_handler)
