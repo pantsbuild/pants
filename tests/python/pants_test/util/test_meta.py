f0f37ef76fd90834a1b81964257a5d0003efbe7f
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-10-20 15:27:15 -0700

    add pants.util.enums dep to BUILD files

--- tests/python/pants_test/util/test_meta.py
+++ tests/python/pants_test/util/test_meta.py
@@ -1,7 +1,6 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-
 import unittest
 from abc import ABC, abstractmethod
 from dataclasses import FrozenInstanceError, dataclass
