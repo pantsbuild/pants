3b62b34f062bd1955a28e32cc7d7e856cbe7ece3
Henry Fuller <hfuller@twitter.com>
2019-11-01 21:56:56 -0700

    Rust nails client for local hermetic nailgun execution. (#8564)
    ### Problem
    
    The Python nailgun client requires us to fork, and is flaky.
    
    ### Solution
    
    Hook up the `nails` rust client directly into the nailgun command runner.
    
    ### Result
    
    Compiling hermetically with nailgun should be faster and less flaky.

--- src/rust/engine/process_execution/src/nailgun/mod.rs
+++ src/rust/engine/process_execution/src/nailgun/mod.rs
@@ -1,17 +1,20 @@
 use std::collections::btree_map::BTreeMap;
 use std::collections::btree_set::BTreeSet;
 use std::os::unix::fs::symlink;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use std::time::Duration;
 
-use futures::future::Future;
-use log::{debug, trace};
-
 use boxfuture::{try_future, BoxFuture, Boxable};
+use futures::future::Future;
+use futures::stream::Stream;
 use hashing::Digest;
+use log::{debug, trace};
+use nails::execution::{child_channel, ChildInput, ChildOutput, Command};
+use tokio::net::TcpStream;
 
-use crate::nailgun::nailgun_pool::{NailgunProcessName, Port};
+use crate::local::CapturedWorkdir;
+use crate::nailgun::nailgun_pool::NailgunProcessName;
 use crate::{
   Context, ExecuteProcessRequest, ExecuteProcessRequestMetadata, FallibleExecuteProcessResult,
   MultiPlatformExecuteProcessRequest, Platform,
@@ -30,20 +33,13 @@ use async_semaphore::AsyncSemaphore;
 pub use nailgun_pool::NailgunPool;
 use parsed_jvm_command_lines::ParsedJVMCommandLines;
 use std::fs::{read_link, remove_file};
+use std::net::SocketAddr;
 use store::Store;
 use workunit_store::WorkUnitStore;
 
 // Hardcoded constants for connecting to nailgun
 static NAILGUN_MAIN_CLASS: &str = "com.martiansoftware.nailgun.NGServer";
 static ARGS_TO_START_NAILGUN: [&str; 1] = [":0"];
-
-static NAILGUN_PORT_ENV_VAR_FOR_CLIENT: &str = "NAILGUN_PORT";
-
-// We can hardcode this because we mix it into the digest in the EPR.
-// TODO(#8480) This hardcoded path can go away
-//              when we port the fetching of the clients and servers to the rust stack,
-//              or when we switch to a different client.
-static NG_CLIENT_PATH: &str = "bin/ng/1.0.0/ng";
 
 ///
 /// Constructs the ExecuteProcessRequest that would be used
@@ -78,10 +74,8 @@ fn construct_nailgun_server_request(
 
 fn construct_nailgun_client_request(
   original_req: ExecuteProcessRequest,
-  python_distribution: String,
   client_main_class: String,
-  client_args: Vec<String>,
-  nailgun_port: Port,
+  mut client_args: Vec<String>,
 ) -> ExecuteProcessRequest {
   let ExecuteProcessRequest {
     argv: _argv,
@@ -96,25 +90,12 @@ fn construct_nailgun_client_request(
     target_platform,
     is_nailgunnable,
   } = original_req;
-  let full_client_cli = vec![
-    python_distribution,
-    NG_CLIENT_PATH.to_string(),
-    "--".to_string(),
-    client_main_class,
-  ]
-  .into_iter()
-  .chain(client_args.into_iter())
-  .collect();
-  let mut client_env = original_request_env;
-  client_env.insert(
-    NAILGUN_PORT_ENV_VAR_FOR_CLIENT.into(),
-    nailgun_port.to_string(),
-  );
+  client_args.insert(0, client_main_class);
   ExecuteProcessRequest {
-    argv: full_client_cli,
+    argv: client_args,
     input_files,
     description,
-    env: client_env,
+    env: original_request_env,
     output_files,
     output_directories,
     timeout,
@@ -133,13 +114,13 @@ fn construct_nailgun_client_request(
 /// If that flag is set, it will connect to a running nailgun server and run the command there.
 /// Otherwise, it will just delegate to the regular local runner.
 ///
+#[derive(Clone)]
 pub struct CommandRunner {
   inner: Arc<super::local::CommandRunner>,
   nailgun_pool: NailgunPool,
   async_semaphore: async_semaphore::AsyncSemaphore,
   metadata: ExecuteProcessRequestMetadata,
   workdir_base: PathBuf,
-  python_distribution_absolute_path: PathBuf,
   executor: task_executor::Executor,
 }
 
@@ -147,7 +128,6 @@ impl CommandRunner {
   pub fn new(
     runner: crate::local::CommandRunner,
     metadata: ExecuteProcessRequestMetadata,
-    python_distribution_absolute_path: PathBuf,
     workdir_base: PathBuf,
     executor: task_executor::Executor,
   ) -> Self {
@@ -157,7 +137,6 @@ impl CommandRunner {
       async_semaphore: AsyncSemaphore::new(1),
       metadata: metadata,
       workdir_base: workdir_base,
-      python_distribution_absolute_path: python_distribution_absolute_path,
       executor: executor,
     }
   }
@@ -226,10 +205,6 @@ impl CommandRunner {
         .inspect(move |_| debug!("Materialized directory {:?} before connecting to nailgun server.", &workdir_for_server2))
         .to_boxed()
   }
-
-  fn get_python_distribution_path(&self) -> String {
-    format!("{}", self.python_distribution_absolute_path.display())
-  }
 }
 
 impl super::CommandRunner for CommandRunner {
@@ -238,12 +213,7 @@ impl super::CommandRunner for CommandRunner {
     req: MultiPlatformExecuteProcessRequest,
     context: Context,
   ) -> BoxFuture<FallibleExecuteProcessResult, String> {
-    let nailgun_pool = self.nailgun_pool.clone();
-    let inner = self.inner.clone();
-    let python_distribution = self.get_python_distribution_path();
-
     let original_request = self.extract_compatible_request(&req).unwrap();
-    let original_request2 = original_request.clone();
 
     if !original_request.is_nailgunnable {
       trace!("The request is not nailgunnable! Short-circuiting to regular process execution");
@@ -251,28 +221,63 @@ impl super::CommandRunner for CommandRunner {
     }
     debug!("Running request under nailgun:\n {:#?}", &original_request);
 
+    let executor = self.executor.clone();
+    let store = self.inner.store.clone();
+    let ParsedJVMCommandLines {
+      client_main_class, ..
+    } = try_future!(ParsedJVMCommandLines::parse_command_lines(
+      &original_request.argv
+    ));
+    let nailgun_name = CommandRunner::calculate_nailgun_name(&client_main_class);
+    let workdir_for_this_nailgun = try_future!(self.get_nailgun_workdir(&nailgun_name));
+
+    self.run_and_capture_workdir(
+      original_request,
+      context,
+      store,
+      executor,
+      true,
+      &workdir_for_this_nailgun,
+    )
+  }
+
+  fn extract_compatible_request(
+    &self,
+    req: &MultiPlatformExecuteProcessRequest,
+  ) -> Option<ExecuteProcessRequest> {
+    // Request compatibility should be the same as for the local runner, so we just delegate this.
+    self.inner.extract_compatible_request(req)
+  }
+}
+
+impl CapturedWorkdir for CommandRunner {
+  fn run_in_workdir(
+    &self,
+    workdir_path: &Path,
+    req: ExecuteProcessRequest,
+    context: Context,
+  ) -> Result<Box<dyn Stream<Item = ChildOutput, Error = String> + Send>, String> {
     // Separate argument lists, to form distinct EPRs for (1) starting the nailgun server and (2) running the client in it.
     let ParsedJVMCommandLines {
       nailgun_args,
       client_args,
       client_main_class,
-    } = try_future!(ParsedJVMCommandLines::parse_command_lines(
-      &original_request.argv
-    ));
+    } = ParsedJVMCommandLines::parse_command_lines(&req.argv)?;
 
     let nailgun_name = CommandRunner::calculate_nailgun_name(&client_main_class);
     let nailgun_name2 = nailgun_name.clone();
     let nailgun_name3 = nailgun_name.clone();
+    let client_workdir = workdir_path.to_path_buf();
 
-    let jdk_home = try_future!(original_request
+    let jdk_home = req
       .jdk_home
       .clone()
-      .ok_or_else(|| "JDK home must be specified for all nailgunnable requests.".to_string()));
+      .ok_or("JDK home must be specified for all nailgunnable requests.")?;
     let nailgun_req = construct_nailgun_server_request(
       &nailgun_name,
       nailgun_args,
       jdk_home.clone(),
-      original_request.target_platform,
+      req.target_platform,
     );
     trace!("Extracted nailgun request:\n {:#?}", &nailgun_req);
 
@@ -281,21 +286,27 @@ impl super::CommandRunner for CommandRunner {
       &self.metadata,
     );
 
-    let workdir_for_this_nailgun = try_future!(self.get_nailgun_workdir(&nailgun_name));
+    let nailgun_pool = self.nailgun_pool.clone();
+    let req2 = req.clone();
+    let workdir_for_this_nailgun = self.get_nailgun_workdir(&nailgun_name)?;
     let workdir_for_this_nailgun1 = workdir_for_this_nailgun.clone();
     let executor = self.executor.clone();
     let build_id = context.build_id.clone();
     let store = self.inner.store.clone();
     let workunit_store = context.workunit_store.clone();
 
-    self
+    // Streams to read child output from
+    let (stdio_write, stdio_read) = child_channel::<ChildOutput>();
+    let (_stdin_write, stdin_read) = child_channel::<ChildInput>();
+
+    let nails_command = self
       .async_semaphore
       .with_acquired(move || {
         Self::materialize_workdir_for_server(
           store,
           workdir_for_this_nailgun.clone(),
           jdk_home,
-          original_request.input_files,
+          req.input_files,
           workunit_store,
         )
         .and_then(move |_metadata| {
@@ -315,29 +326,33 @@ impl super::CommandRunner for CommandRunner {
       .inspect(move |_| debug!("Connected to nailgun instance {}", &nailgun_name3))
       .and_then(move |nailgun_port| {
         // Run the client request in the nailgun we have active.
-        debug!("Got nailgun port {:#?}", nailgun_port);
-        let full_client_req = construct_nailgun_client_request(
-          original_request2,
-          python_distribution,
-          client_main_class,
-          client_args,
-          nailgun_port,
-        );
-        debug!("Running client request on nailgun {}", &nailgun_name2);
-        trace!("Client request: {:#?}", full_client_req);
-        inner.run(
-          MultiPlatformExecuteProcessRequest::from(full_client_req),
-          context,
-        )
-      })
-      .to_boxed()
-  }
+        debug!("Got nailgun port {} for {}", nailgun_port, nailgun_name2);
+        let client_req = construct_nailgun_client_request(req2, client_main_class, client_args);
+        let cmd = Command {
+          command: client_req.argv[0].clone(),
+          args: client_req.argv[1..].to_vec(),
+          env: client_req
+            .env
+            .iter()
+            .map(|(k, v)| (k.clone(), v.clone()))
+            .collect(),
+          working_dir: client_workdir,
+        };
+        trace!("Client request: {:#?}", client_req);
+        let addr: SocketAddr = format!("127.0.0.1:{:?}", nailgun_port).parse().unwrap();
+        debug!("Connecting to server at {}...", addr);
+        TcpStream::connect(&addr)
+          .and_then(move |stream| {
+            nails::client_handle_connection(stream, cmd, stdio_write, stdin_read)
+          })
+          .map_err(|e| format!("Error communicating with server: {}", e))
+          .map(ChildOutput::Exit)
+      });
 
-  fn extract_compatible_request(
-    &self,
-    req: &MultiPlatformExecuteProcessRequest,
-  ) -> Option<ExecuteProcessRequest> {
-    // Request compatibility should be the same as for the local runner, so we just delegate this.
-    self.inner.extract_compatible_request(req)
+    Ok(Box::new(
+      stdio_read
+        .map_err(|()| unreachable!())
+        .select(nails_command.into_stream()),
+    ))
   }
 }
