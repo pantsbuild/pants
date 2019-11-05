a03b4e483139dd1c2376c2049d5dbd586aa0aeec
Greg Shuflin <greg.shuflin@toolchain.com>
2019-11-05 13:33:08 -0800

    Add test for python run

--- src/python/pants/rules/core/run.py
+++ src/python/pants/rules/core/run.py
@@ -3,7 +3,6 @@
 
 from pathlib import Path
 
-from pants.base.build_environment import get_buildroot
 from pants.build_graph.address import Address
 from pants.engine.addressable import BuildFileAddresses
 from pants.engine.console import Console
@@ -13,6 +12,7 @@ from pants.engine.interactive_runner import InteractiveProcessRequest, Interacti
 from pants.engine.rules import console_rule
 from pants.engine.selectors import Get
 from pants.rules.core.binary import CreatedBinary
+from pants.util.contextutil import temporary_dir
 
 
 class Run(Goal):
@@ -23,32 +23,31 @@ class Run(Goal):
 @console_rule
 def run(console: Console, workspace: Workspace, runner: InteractiveRunner, addresses: BuildFileAddresses) -> Run:
   binaries = yield [Get(CreatedBinary, Address, address.to_address()) for address in addresses]
-  build_root = get_buildroot()
 
   exit_codes = []
   for (binary, bfa) in zip(binaries, addresses):
     target = bfa.to_address()
+    with temporary_dir(cleanup=False) as tmpdir:
+      dirs_to_materialize = (DirectoryToMaterialize(path=str(tmpdir), directory_digest=binary.digest),)
+      workspace.materialize_directories(dirs_to_materialize)
 
-    dirs_to_materialize = (DirectoryToMaterialize(path='dist/', directory_digest=binary.digest),)
-    workspace.materialize_directories(dirs_to_materialize)
+      console.write_stdout(f"Running target: {target}\n")
+      full_path = str(Path(tmpdir, binary.command))
+      run_request = InteractiveProcessRequest(
+        argv=[full_path],
+        run_in_workspace=True,
+      )
 
-    console.write_stdout(f"Running target: {target}\n")
-    full_path = str(Path(build_root, 'dist', binary.command))
-    run_request = InteractiveProcessRequest(
-      argv=[full_path],
-      run_in_workspace=True,
-    )
+      try:
+        result = runner.run_local_interactive_process(run_request)
+        exit_codes.append(result.process_exit_code)
+        if result.process_exit_code == 0:
+          console.write_stdout(f"{target} ran successfully.\n")
+        else:
+          console.write_stderr(f"{target} failed with code {result.process_exit_code}!\n")
 
-    try:
-      result = runner.run_local_interactive_process(run_request)
-      exit_codes.append(result.process_exit_code)
-      if result.process_exit_code == 0:
-        console.write_stdout(f"{target} ran successfully.\n")
-      else:
-        console.write_stderr(f"{target} failed with code {result.process_exit_code}!\n")
-
-    except Exception as e:
-      console.write_stderr(f"Exception when attempting to run {target} : {e}\n")
+      except Exception as e:
+        console.write_stderr(f"Exception when attempting to run {target} : {e}\n")
 
   overall_exit_code = 0 if all(code == 0 for code in exit_codes) else -1
   yield Run(overall_exit_code)
---
+++ tests/python/pants_test/rules/test_run.py
@@ -0,0 +1,56 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.build_graph.address import Address, BuildFileAddress
+from pants.engine.fs import Digest, FileContent, InputFilesContent, Workspace
+from pants.engine.interactive_runner import InteractiveRunner
+from pants.rules.core import run
+from pants.rules.core.binary import CreatedBinary
+from pants_test.console_rule_test_base import ConsoleRuleTestBase
+from pants_test.engine.util import MockConsole, run_rule
+
+
+class RunTest(ConsoleRuleTestBase):
+  goal_cls = run.Run
+
+  def create_mock_binary(self, program_text: bytes) -> CreatedBinary:
+    input_files_content = InputFilesContent((
+      FileContent(path='program.py', content=program_text, is_executable=True),
+    ))
+    digest, = self.scheduler.product_request(Digest, [input_files_content])
+    return CreatedBinary(
+      command='program.py',
+      digest=digest,
+    )
+
+  def single_target_run(self, console: MockConsole, program_text: bytes, spec: str):
+    workspace = Workspace(self.scheduler)
+    interactive_runner = InteractiveRunner(self.scheduler)
+    address = Address.parse(spec)
+    bfa =  BuildFileAddress(
+      build_file=None,
+      target_name=address.target_name,
+      rel_path='{}/BUILD'.format(address.spec_path),
+    )
+    res = run_rule(run.run, console, workspace, interactive_runner, (bfa,), {
+      (CreatedBinary, Address): lambda _: self.create_mock_binary(program_text)
+    })
+    return res
+
+  def test_normal_run(self) -> None:
+    spec = "some/addr"
+    console = MockConsole(use_colors=False)
+    program_text = b'#!/usr/bin/python\nprint("hello")'
+    res = self.single_target_run(console, program_text, spec)
+    self.assertEqual(res.exit_code, 0)
+    self.assertEquals(console.stdout.getvalue(), "Running target: some/addr:addr\nsome/addr:addr ran successfully.\n")
+    self.assertEquals(console.stderr.getvalue(), "")
+
+  def test_failed_run(self) -> None:
+    spec = "some/addr"
+    console = MockConsole(use_colors=False)
+    program_text = b'#!/usr/bin/python\nraise RuntimeError("foo")'
+    res = self.single_target_run(console, program_text, spec)
+    self.assertEqual(res.exit_code, -1)
+    self.assertEquals(console.stdout.getvalue(), "Running target: some/addr:addr\n")
+    self.assertEquals(console.stderr.getvalue(), "some/addr:addr failed with code 1!\n")
