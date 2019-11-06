7f4ef60d103cac833451b5af2944e1c83a57ba26
gshuflin <greg.shuflin@toolchain.com>
2019-11-06 11:43:55 -0800

    v2 `./pants run` (#8573)
    ## Problem
    
    The v2 engine doesn't implement the run goal yet.
    
    ## Solution
    
    Implement the run goal for python targets in the v2 engine.

--- src/python/pants/rules/core/run.py
+++ src/python/pants/rules/core/run.py
@@ -1,35 +1,58 @@
 # Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from pathlib import Path
+
+from pants.build_graph.address import Address
 from pants.engine.addressable import BuildFileAddresses
 from pants.engine.console import Console
+from pants.engine.fs import DirectoryToMaterialize, Workspace
 from pants.engine.goal import Goal
 from pants.engine.interactive_runner import InteractiveProcessRequest, InteractiveRunner
 from pants.engine.rules import console_rule
+from pants.engine.selectors import Get
+from pants.rules.core.binary import CreatedBinary
+from pants.util.contextutil import temporary_dir
 
 
 class Run(Goal):
   """Runs a runnable target."""
-  name = 'v2-run'
+  name = 'run'
 
 
+#TODO(gregs) - the `run` rule should really only accept a single target, but we don't have the infrastructure
+# yet to support a console rule that can request one and only one target (rather than BuildFileAddresses plural
+# or Specs plural). 
 @console_rule
-def run(console: Console, runner: InteractiveRunner, build_file_addresses: BuildFileAddresses) -> Run:
-  console.write_stdout("Running the `run` goal\n")
+def run(console: Console, workspace: Workspace, runner: InteractiveRunner, addresses: BuildFileAddresses) -> Run:
+  bfa = addresses.dependencies[0]
+  target = bfa.to_address()
+  binary = yield Get(CreatedBinary, Address, target)
 
-  request = InteractiveProcessRequest(
-    argv=["/usr/bin/python"],
-    env=("TEST_ENV", "TEST"),
-    run_in_workspace=False,
-  )
+  with temporary_dir(cleanup=True) as tmpdir:
+    dirs_to_materialize = (DirectoryToMaterialize(path=str(tmpdir), directory_digest=binary.digest),)
+    workspace.materialize_directories(dirs_to_materialize)
 
-  try:
-    res = runner.run_local_interactive_process(request)
-    print(f"Subprocess exited with result: {res.process_exit_code}")
-    yield Run(res.process_exit_code)
-  except Exception as e:
-    print(f"Exception when running local interactive process: {e}")
-    yield Run(-1)
+    console.write_stdout(f"Running target: {target}\n")
+    full_path = str(Path(tmpdir, binary.binary_name))
+    run_request = InteractiveProcessRequest(
+      argv=[full_path],
+      run_in_workspace=True,
+    )
+
+    try:
+      result = runner.run_local_interactive_process(run_request)
+      exit_code = result.process_exit_code
+      if result.process_exit_code == 0:
+        console.write_stdout(f"{target} ran successfully.\n")
+      else:
+        console.write_stderr(f"{target} failed with code {result.process_exit_code}!\n")
+
+    except Exception as e:
+      console.write_stderr(f"Exception when attempting to run {target} : {e}\n")
+      exit_code = -1
+
+  yield Run(exit_code)
 
 
 def rules():
