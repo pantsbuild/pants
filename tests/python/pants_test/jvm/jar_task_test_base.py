a0bcc2314c486fcaa34de9401c29890799ead1ba
Eric Arellano <ericarellano@me.com>
2019-11-06 14:54:49 -0700

    Move testinfra code from `tests/python/pants_tests` to `src/python/pants/testutil` (#8400)
    ### Problem
    We are in the process of moving our tests to live within the `src` directory, rather than `tests` (https://github.com/pantsbuild/pants/issues/7489). We should consequently have our core utility code live in `src`.
    
    Further, the current utils are spread out across several folders, which makes discovery more difficult.
    
    ### Solution
    Create a new `pants/testutil` namespace with all of the files used by the `pants.testinfra` wheel. Specifically, it has this directory structure:
    
    ```
    src/python/pants/testutil
    ├── BUILD
    ├── __init__.py
    ├── base
    │   ├── BUILD
    │   ├── __init__.py
    │   └── context_utils.py
    ├── console_rule_test_base.py
    ├── engine
    │   ├── BUILD
    │   ├── __init__.py
    │   ├── base_engine_test.py
    │   └── util.py
    ├── file_test_util.py
    ├── git_util.py
    ├── interpreter_selection_utils.py
    ├── jvm
    │   ├── BUILD
    │   ├── __init__.py
    │   ├── jar_task_test_base.py
    │   ├── jvm_task_test_base.py
    │   ├── jvm_tool_task_test_base.py
    │   └── nailgun_task_test_base.py
    ├── mock_logger.py
    ├── option
    │   ├── BUILD
    │   ├── __init__.py
    │   └── fakes.py
    ├── pants_run_integration_test.py
    ├── pexrc_util.py
    ├── process_test_util.py
    ├── subsystem
    │   ├── BUILD
    │   ├── __init__.py
    │   └── util.py
    ├── task_test_base.py
    └── test_base.py
    ```
    
    We can't yet delete the equivalent files in `pants_test` due to a public API, so we instead have those files simply re-export the implementation in `pants/testutils`.
    
    #### Does not move some util code
    There are several util files not exposed to the `pantsbuild.testinfra` wheel, like `engine/scheduler_test_base.py`. To reduce the scope of this PR, we keep those there for now. Presumably, they will be able to be moved into `pants/testinfra` without a deprecation cycle.
    
    #### Creates new wheel `pantsbuild.pants.testutil`
    We had to create a new package, rather than using `pantsbuild.pants.testinfra`, due to rules about ownership of files (https://github.com/pantsbuild/pants/pull/8400#issuecomment-538828924).
    
    In a followup, we will deprecate `pantsbuild.pants.testinfra`.
    
    ### Result
    We can now either use `pants.testutil` or the conventional `pants_test` imports. Both have the same functionality.
    
    In a followup PR, we will deprecate the `pants_test` version.

--- tests/python/pants_test/jvm/jar_task_test_base.py
+++ tests/python/pants_test/jvm/jar_task_test_base.py
@@ -1,11 +1,4 @@
 # Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants_test.jvm.nailgun_task_test_base import NailgunTaskTestBase
-
-
-class JarTaskTestBase(NailgunTaskTestBase):
-  """Prepares an ephemeral test build root that supports jar tasks.
-
-  :API: public
-  """
+from pants.testutil.jvm.jar_task_test_base import JarTaskTestBase as JarTaskTestBase  # noqa
