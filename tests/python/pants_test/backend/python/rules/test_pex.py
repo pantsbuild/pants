c0961a475a2f4a20c21b9de9c489fe40dd90860a
Eric Arellano <ericarellano@me.com>
2019-11-07 13:31:27 -0700

    Fix failing test

--- tests/python/pants_test/backend/python/rules/test_pex.py
+++ tests/python/pants_test/backend/python/rules/test_pex.py
@@ -132,4 +132,4 @@ class TestResolveRequirements(TestBase):
         constraint_set=("CPython>=2.7,<3", "CPython>=3.6,<4")
     )
     pex_info = self.create_pex_and_get_pex_info(interpreter_constraints=constraints)
-    self.assertEqual(frozenset(pex_info["interpreter_constraints"]), constraints.constraint_set)
+    self.assertEqual(set(pex_info["interpreter_constraints"]), set(constraints.constraint_set))
