0483ddf975f7e7a9c06d2bc16dd5ce8b182c0cc9
Yi Cheng <wisechengyi@gmail.com>
2019-11-09 00:32:31 -0800

    more

--- src/python/pants/backend/jvm/tasks/coursier_resolve.py
+++ src/python/pants/backend/jvm/tasks/coursier_resolve.py
@@ -162,13 +162,15 @@ class CoursierMixin(JvmResolverBase):
         if not self.get_options().report:
           # Check each individual target without context first
           # If the individuals are valid, check them as a VersionedTargetSet
-          (cached, uncached, uncached_causes) = self.check_artifact_cache([resolve_vts])
-          if not invalidation_check.invalid_vts and len(cached) == len(resolve_vts.targets):
-            # Load up from the results dir
-            success = self._load_from_results_dir(compile_classpath, vt_set_results_dir,
-                                                  coursier_cache_dir, invalidation_check, pants_jar_base_dir)
-            if success:
-              return
+          # The order of 'or' statement matters, because checking for cache is more expensive.
+          if not invalidation_check.invalid_vts and \
+            (resolve_vts.valid or len(self.check_artifact_cache([resolve_vts])[0]) == len(resolve_vts.targets)):
+            if resolve_vts.valid:
+              # Load up from the results dir
+              success = self._load_from_results_dir(compile_classpath, vt_set_results_dir,
+                coursier_cache_dir, invalidation_check, pants_jar_base_dir)
+              if success:
+                return
 
         jars_to_resolve, pinned_coords = self._compute_jars_to_resolve_and_pin(raw_jar_deps,
                                                                                artifact_set,
@@ -185,8 +187,8 @@ class CoursierMixin(JvmResolverBase):
         self._populate_results_dir(vt_set_results_dir, results)
         resolve_vts.update()
 
-        # if self.artifact_cache_writes_enabled():
-        self.update_artifact_cache([(resolve_vts, [vt_set_results_dir])])
+        if self.artifact_cache_writes_enabled():
+          self.update_artifact_cache([(resolve_vts, [vt_set_results_dir])])
 
   def _override_classifiers_for_conf(self, conf):
      # TODO Encapsulate this in the result from coursier instead of here.
