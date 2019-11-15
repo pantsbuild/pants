4bbfe07140efb0c1fb68d33740bef573e0296b33
Eric Arellano <ericarellano@me.com>
2019-11-15 12:43:51 -0800

    Rewrite test to use run_rule, which is more unit-testy

--- tests/python/pants_test/backend/python/rules/test_python_test_runner.py
+++ tests/python/pants_test/backend/python/rules/test_python_test_runner.py
@@ -1,70 +1,75 @@
 # Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.backend.python.rules.download_pex_bin import download_pex_bin
-from pants.backend.python.rules.inject_init import inject_init
-from pants.backend.python.rules.pex import CreatePex, create_pex
+from pants.backend.python.rules.inject_init import InjectedInitDigest
+from pants.backend.python.rules.pex import CreatePex, Pex
 from pants.backend.python.rules.python_test_runner import run_python_test
 from pants.backend.python.subsystems.pytest import PyTest
-from pants.backend.python.subsystems.python_native_code import (
-  PythonNativeCode,
-  create_pex_native_build_environment,
-)
 from pants.backend.python.subsystems.python_setup import PythonSetup
-from pants.backend.python.subsystems.subprocess_environment import (
-  SubprocessEnvironment,
-  create_subprocess_encoding_environment,
-)
+from pants.backend.python.subsystems.subprocess_environment import SubprocessEnvironment
+from pants.build_graph.address import Address, BuildFileAddress
+from pants.engine.fs import EMPTY_SNAPSHOT, Digest, DirectoriesToMerge
+from pants.engine.isolated_process import ExecuteProcessRequest, FallibleExecuteProcessResult
+from pants.engine.legacy.graph import BuildFileAddresses, HydratedTarget, TransitiveHydratedTargets
 from pants.engine.legacy.structs import PythonTestsAdaptor
-from pants.engine.rules import RootRule
-from pants.engine.selectors import Params
 from pants.rules.core.core_test_model import Status, TestResult
-from pants.rules.core.strip_source_root import strip_source_root
-from pants.source.source_root import SourceRootConfig
-from pants.testutil.subsystem.util import init_subsystems
+from pants.rules.core.strip_source_root import SourceRootStrippedSources
+from pants.testutil.engine.util import MockGet, run_rule
 from pants.testutil.test_base import TestBase
-from pants.util.collections import assert_single_element
 
 
 class TestPythonTestRunner(TestBase):
 
-  @classmethod
-  def rules(cls):
-    return super().rules() + [
+  def test_empty_target_succeeds(self) -> None:
+    # NB: Because this particular edge case should early return, we can avoid providing valid
+    # mocked yield gets for most of the rule's body. Future tests added to this file will need to
+    # provide valid mocks instead.
+    unimplemented_mock = lambda _: NotImplemented
+    target = PythonTestsAdaptor(address=BuildFileAddress(target_name="target", rel_path="test"))
+    result: TestResult = run_rule(
       run_python_test,
-      create_pex,
-      strip_source_root,
-      inject_init,
-      create_pex_native_build_environment,
-      create_subprocess_encoding_environment,
-      download_pex_bin,
-      RootRule(PythonTestsAdaptor),
-      RootRule(CreatePex),
-      RootRule(PyTest),
-      RootRule(PythonSetup),
-      RootRule(PythonNativeCode),
-      RootRule(SubprocessEnvironment),
-      RootRule(SourceRootConfig),
-    ]
-
-  def setUp(self):
-    super().setUp()
-    init_subsystems([
-      PythonSetup, PythonNativeCode, SubprocessEnvironment, PyTest, SourceRootConfig
-    ])
-
-  def run_pytest(self, target: PythonTestsAdaptor) -> TestResult:
-    return assert_single_element(
-      self.scheduler.product_request(TestResult, [Params(
+      rule_args=[
         target,
         PyTest.global_instance(),
         PythonSetup.global_instance(),
         SubprocessEnvironment.global_instance(),
-        PythonNativeCode.global_instance(),
-        SourceRootConfig.global_instance(),
-      )])
+      ],
+      mock_gets=[
+        MockGet(
+          product_type=TransitiveHydratedTargets,
+          subject_type=BuildFileAddresses,
+          mock=lambda _: TransitiveHydratedTargets(roots=(), closure=())
+        ),
+        MockGet(
+          product_type=SourceRootStrippedSources,
+          subject_type=Address,
+          mock=lambda _: SourceRootStrippedSources(snapshot=EMPTY_SNAPSHOT),
+        ),
+        MockGet(
+          product_type=SourceRootStrippedSources,
+          subject_type=HydratedTarget,
+          mock=unimplemented_mock,
+        ),
+        MockGet(
+          product_type=Digest,
+          subject_type=DirectoriesToMerge,
+          mock=unimplemented_mock,
+        ),
+        MockGet(
+          product_type=InjectedInitDigest,
+          subject_type=Digest,
+          mock=unimplemented_mock,
+        ),
+        MockGet(
+          product_type=Pex,
+          subject_type=CreatePex,
+          mock=unimplemented_mock,
+        ),
+        MockGet(
+          product_type=FallibleExecuteProcessResult,
+          subject_type=ExecuteProcessRequest,
+          mock=unimplemented_mock,
+        ),
+      ],
     )
-
-  def test_empty_target_succeeds(self) -> None:
-    result = self.run_pytest(target=PythonTestsAdaptor(name="test", sources=[]))
     self.assertEqual(result.status, Status.SUCCESS)
