f6208331a8eb7b663adf56ea83ee1da47d20711b
Yi Cheng <wisechengyi@gmail.com>
2019-11-16 19:47:08 -0800

    fmt

--- src/python/pants/bin/local_pants_runner.py
+++ src/python/pants/bin/local_pants_runner.py
@@ -1,5 +1,6 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 import logging
 from contextlib import contextmanager
 
