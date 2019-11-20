a8f9c5b2326c57807937ccc0341682668f25e8c3
Tansy Arron-Walker <tansy.arron@gmail.com>
2019-11-20 17:37:35 -0500

    Port dependencies task to V2 rule.

---
+++ src/python/pants/backend/project_info/rules/fast_dependencies.py
@@ -0,0 +1,53 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from typing import Set
+
+from twitter.common.collections import OrderedSet
+
+from pants.base.specs import AscendantAddresses, DescendantAddresses, SingleAddress, Specs
+from pants.build_graph.address import Address
+from pants.engine.console import Console
+from pants.engine.goal import Goal
+from pants.engine.legacy.graph import HydratedTargets, TransitiveHydratedTargets
+from pants.engine.rules import console_rule
+from pants.engine.selectors import Get
+
+
+class Dependencies(Goal):
+  name = 'fast-dependencies'
+
+  @classmethod
+  def register_options(cls, register):
+    super().register_options(register)
+    register(
+      '--transitive',
+      default=True,
+      type=bool,
+      help='Run dependencies against transitive dependencies of targets specified on the command line.',
+    )
+
+
+@console_rule
+async def dependencies(console: Console, specs: Specs, dependencies_options: Dependencies.Options) -> Dependencies:
+  addresses: Set[str] = set()
+  if dependencies_options.values.transitive:
+    transitive_targets = await Get[TransitiveHydratedTargets](Specs, specs)
+    addresses.update(hydrated_target.address.spec for hydrated_target in transitive_targets.closure)
+  else:
+    hydrated_targets = await Get[HydratedTargets](Specs, specs)
+    addresses.update(
+      dep.spec
+      for hydrated_target in hydrated_targets
+      for dep in hydrated_target.dependencies
+    )
+
+  for address in sorted(addresses):
+    console.print_stdout(address)
+
+  return Dependencies(exit_code=0)
+
+def rules():
+  return [
+    dependencies,
+  ]
---
+++ src/python/pants/backend/project_info/rules/fast_dependencies_test.py
@@ -0,0 +1,86 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import os
+from textwrap import dedent
+from typing import List, Optional
+
+import pytest
+
+from pants.backend.project_info.rules import fast_dependencies
+from pants.backend.python.targets.python_library import PythonLibrary
+from pants.build_graph.build_file_aliases import BuildFileAliases
+from pants.build_graph.target import Target
+from pants.testutil.console_rule_test_base import ConsoleRuleTestBase
+
+
+class FastDependenciesTest(ConsoleRuleTestBase):
+  goal_cls = fast_dependencies.Dependencies
+
+  @classmethod
+  def alias_groups(cls) -> BuildFileAliases:
+    return BuildFileAliases(
+      targets={
+        "python_library": PythonLibrary,
+      },
+    )
+
+  @classmethod
+  def rules(cls):
+    return super().rules() + fast_dependencies.rules()
+
+  def create_python_library(
+    self, path: str,
+    *,
+    dependencies: Optional[List[str]] = None
+  ) -> None:
+    self.create_library(
+      path=path,
+      target_type="python_library",
+      name="target",
+      sources=[],
+      dependencies=dependencies or []
+    )
+
+  def assert_dependencies(self, *, target: str, expected: List[str], transitive: bool = True) -> None:
+    args = ["--no-transitive"] if not transitive else []
+    self.assert_console_output(*expected, args=args + [target])
+
+  def test_no_dependencies(self):
+    self.create_python_library(path="some/target")
+    self.assert_dependencies(
+      target="some/target",
+      expected=[],
+      transitive=False,
+    )
+
+  def test_no_dependencies_transitive(self):
+    self.create_python_library(path="some/target")
+    self.assert_dependencies(
+      target="some/target",
+      expected=["some/target:target"],
+      transitive=True,
+    )
+
+  def test_dependencies(self):
+    self.create_python_library(path="dep/target")
+    self.create_python_library(path="some/target", dependencies=["dep/target"])
+    self.assert_dependencies(
+      target="some/target",
+      expected=["dep/target:target"],
+      transitive=False,
+    )
+
+  def test_dependencies_transitive(self):
+    self.create_python_library(path="dep/target")
+    self.create_python_library(path="some/target", dependencies=["dep/target"])
+    self.create_python_library(path="some/other/target", dependencies=["some/target"])
+    self.assert_dependencies(
+      target="some/other/target",
+      expected=[
+        "dep/target:target",
+        "some/other/target:target",
+        "some/target:target",
+      ],
+      transitive=True,
+    )
