8ab7cd1ef2d38d4938dbc5a0136ae5e64e37ab93
Eric Arellano <ericarellano@me.com>
2019-11-21 23:03:09 -0700

    Rewrite Black tests from `PantsRunIntegrationTest` to rules-style integration tests (#8684)
    Traditionally, we'd write integration tests via `PantsRunIntegrationTest`, which has the cost of starting a new Pants process and is also not very isolated/hermetic. Instead, with V2 rules, we can write integration tests using `self.scheduler.product_request()`. This is preferable because it runs faster and better isolates the rule we are testing away from other parts of Pants.
    
    For example, in https://github.com/pantsbuild/pants/pull/8683, we are going to default to Black not being turned on as a plugin within our codebase. That causes the integration tests to fail because they are no longer able to recognize the plugin. While we could work around that via setting up the Pants config within the ITs themself, the more sound solution is to use our `self.scheduler.product_request()`-based tests, which have a similar effect of integration tests but in a more controlled and reusable way.

--- src/python/pants/backend/python/rules/black_integration_test.py
+++ src/python/pants/backend/python/rules/black_integration_test.py
@@ -1,159 +1,138 @@
 # Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import os
-from contextlib import contextmanager
-from pathlib import Path
+from typing import List, Optional, Tuple
 
-from pants.testutil.pants_run_integration_test import PantsRunIntegrationTest
-from pants.util.contextutil import temporary_dir
-from pants.util.dirutil import safe_file_dump
-
-
-def write_build_file(root_dir):
-  safe_file_dump(os.path.join(root_dir, "BUILD"), "python_library()")
-
-INCONSISTENTLY_FORMATTED_HELLO: str = "def hello():x=42"
-CONSISTENTLY_FORMATTED_HELLO: str = "def hello():\n    x = 42\n"
-
-
-def write_inconsistently_formatted_file(root_dir, filename) -> Path:
-  filepath = os.path.join(root_dir, filename)
-  safe_file_dump(filepath, INCONSISTENTLY_FORMATTED_HELLO)
-  return Path(filepath)
-
-
-def write_consistently_formatted_file(root_dir, filename) -> Path:
-  filepath = os.path.join(root_dir, filename)
-  safe_file_dump(filepath, CONSISTENTLY_FORMATTED_HELLO)
-  return Path(filepath)
-
-
-@contextmanager
-def build_directory():
-  with temporary_dir(root_dir=".") as root_dir:
-    write_build_file(root_dir)
-    yield root_dir
+from pants.backend.python.rules.black import BlackSetup, fmt, lint, setup_black
+from pants.backend.python.rules.download_pex_bin import download_pex_bin
+from pants.backend.python.rules.pex import CreatePex, create_pex
+from pants.backend.python.subsystems.black import Black
+from pants.backend.python.subsystems.python_native_code import (
+  PythonNativeCode,
+  create_pex_native_build_environment,
+)
+from pants.backend.python.subsystems.python_setup import PythonSetup
+from pants.backend.python.subsystems.subprocess_environment import (
+  SubprocessEnvironment,
+  create_subprocess_encoding_environment,
+)
+from pants.backend.python.targets.formattable_python_target import FormattablePythonTarget
+from pants.build_graph.address import Address
+from pants.engine.fs import Digest, FileContent, InputFilesContent, Snapshot
+from pants.engine.legacy.structs import TargetAdaptor
+from pants.engine.rules import RootRule
+from pants.engine.selectors import Params
+from pants.rules.core.fmt import FmtResult
+from pants.rules.core.lint import LintResult
+from pants.source.wrapped_globs import EagerFilesetWithSpec
+from pants.testutil.subsystem.util import global_subsystem_instance, init_subsystems
+from pants.testutil.test_base import TestBase
+from pants.util.collections import assert_single_element
 
 
-class BlackIntegrationTest(PantsRunIntegrationTest):
-  def test_black_one_python_source_should_leave_one_file_unchanged(self):
-    with build_directory() as root_dir:
-      code = write_consistently_formatted_file(root_dir, "hello.py")
-      command = [
-        'fmt-v2',
-        f'{root_dir}'
-      ]
-      pants_run = self.run_pants(command=command)
-      self.assert_success(pants_run)
-      after_formatting = code.read_text()
-      self.assertEqual(CONSISTENTLY_FORMATTED_HELLO, after_formatting)
-    self.assertNotIn("reformatted", pants_run.stderr_data)
-    self.assertIn("1 file left unchanged", pants_run.stderr_data)
+class BlackIntegrationTest(TestBase):
 
-  def test_black_lint_given_formatted_file(self):
-    with build_directory() as root_dir:
-      code = write_consistently_formatted_file(root_dir, "hello.py")
-      command = [
-        'lint-v2',
-        f'{root_dir}'
-      ]
-      pants_run = self.run_pants(command=command)
-      self.assert_success(pants_run)
-      after_formatting = code.read_text()
-      self.assertEqual(CONSISTENTLY_FORMATTED_HELLO, after_formatting)
-    self.assertNotIn("reformatted", pants_run.stderr_data)
-    self.assertIn("1 file would be left unchanged", pants_run.stderr_data)
+  good_source = FileContent(path="test/good.py", content=b'name = "Anakin"\n')
+  bad_source = FileContent(path="test/bad.py", content=b'name=    "Anakin"\n')
+  fixed_bad_source = FileContent(path="test/bad.py", content=b'name = "Anakin"\n')
 
-  def test_black_two_python_sources_should_leave_two_files_unchanged(self):
-    with build_directory() as root_dir:
-      foo = write_consistently_formatted_file(root_dir, "foo.py")
-      bar = write_consistently_formatted_file(root_dir, "bar.py")
-      command = [
-        'fmt-v2',
-        f'{root_dir}'
-      ]
-      pants_run = self.run_pants(command=command)
-      self.assert_success(pants_run)
-      foo_after_formatting = foo.read_text()
-      self.assertEqual(CONSISTENTLY_FORMATTED_HELLO, foo_after_formatting)
-      bar_after_formatting = bar.read_text()
-      self.assertEqual(CONSISTENTLY_FORMATTED_HELLO, bar_after_formatting)
-    self.assertNotIn("reformatted", pants_run.stderr_data)
-    self.assertIn("2 files left unchanged", pants_run.stderr_data)
+  @classmethod
+  def rules(cls):
+    return (
+      *super().rules(),
+      fmt,
+      lint,
+      setup_black,
+      create_pex,
+      create_subprocess_encoding_environment,
+      create_pex_native_build_environment,
+      download_pex_bin,
+      RootRule(CreatePex),
+      RootRule(FormattablePythonTarget),
+      RootRule(Black),
+      RootRule(BlackSetup),
+      RootRule(PythonSetup),
+      RootRule(PythonNativeCode),
+      RootRule(SubprocessEnvironment),
+    )
 
-  def test_black_respects_config(self):
-    # If our config pyproject.toml is used, the compilation_failure target will be skipped.
-    # Otherwise, it will be used and Black will error out.
-    command = [
-      'fmt-v2',
-      'testprojects/src/python/unicode/compilation_failure::',
-      "--black-config=pyproject.toml",
-    ]
-    pants_run = self.run_pants(command=command)
-    self.assert_success(pants_run)
-    self.assertNotIn("reformatted", pants_run.stderr_data)
-    self.assertNotIn("unchanged", pants_run.stderr_data)
-    self.assertIn("Nothing to do", pants_run.stderr_data)
+  def setUp(self):
+    super().setUp()
+    init_subsystems([Black, PythonSetup, PythonNativeCode, SubprocessEnvironment])
 
-  def test_black_should_format_python_code(self):
-    with build_directory() as root_dir:
-      code = write_inconsistently_formatted_file(root_dir, "hello.py")
-      command = [
-        'fmt-v2',
-        f'{root_dir}'
-      ]
-      pants_run = self.run_pants(command=command)
-      self.assert_success(pants_run)
-      after_formatting = code.read_text()
-      self.assertEqual(CONSISTENTLY_FORMATTED_HELLO, after_formatting)
-    self.assertIn("1 file reformatted", pants_run.stderr_data)
-    self.assertNotIn("unchanged", pants_run.stderr_data)
+  def run_black(
+    self, source_files: List[FileContent], *, config: Optional[str] = None
+  ) -> Tuple[LintResult, FmtResult]:
+    if config is not None:
+      self.create_file(relpath="pyproject.toml", contents=config)
+    input_snapshot = assert_single_element(
+      self.scheduler.product_request(Snapshot, [InputFilesContent(source_files)])
+    )
+    target = FormattablePythonTarget(
+      TargetAdaptor(
+        sources=EagerFilesetWithSpec('test', {'globs': []}, snapshot=input_snapshot),
+        address=Address.parse("test:target"),
+      )
+    )
+    black_subsystem = global_subsystem_instance(
+      Black, options={Black.options_scope: {"config": "pyproject.toml" if config else None}}
+    )
+    black_setup = assert_single_element(
+      self.scheduler.product_request(
+        BlackSetup,
+        [Params(
+          target,
+          black_subsystem,
+          PythonNativeCode.global_instance(),
+          PythonSetup.global_instance(),
+          SubprocessEnvironment.global_instance(),
+        )]
+      )
+    )
+    fmt_and_lint_params = Params(
+      target, black_setup, PythonSetup.global_instance(), SubprocessEnvironment.global_instance()
+    )
+    lint_result: LintResult = assert_single_element(
+      self.scheduler.product_request(LintResult, [fmt_and_lint_params])
+    )
+    fmt_result: FmtResult = assert_single_element(
+      self.scheduler.product_request(FmtResult, [fmt_and_lint_params])
+    )
+    return lint_result, fmt_result
 
-  def test_black_lint_should_fail_but_not_format_python_code(self):
-    with build_directory() as root_dir:
-      code = write_inconsistently_formatted_file(root_dir, "hello.py")
-      command = [
-        'lint-v2',
-        f'{root_dir}'
-      ]
-      pants_run = self.run_pants(command=command)
-      self.assert_failure(pants_run)
-      after_formatting = code.read_text()
-      self.assertEqual(INCONSISTENTLY_FORMATTED_HELLO, after_formatting)
-    self.assertIn("1 file would be reformatted", pants_run.stderr_data)
-    self.assertNotIn("1 file reformatted", pants_run.stderr_data)
-    self.assertNotIn("unchanged", pants_run.stderr_data)
+  def get_digest(self, source_files: List[FileContent]) -> Digest:
+    return assert_single_element(
+      self.scheduler.product_request(Digest, [InputFilesContent(source_files)])
+    )
 
-  def test_black_lint_given_multiple_files(self):
-    with build_directory() as root_dir:
-      write_inconsistently_formatted_file(root_dir, "incorrect.py")
-      write_inconsistently_formatted_file(root_dir, "broken.py")
-      write_consistently_formatted_file(root_dir, "pristine.py")
-      command = [
-        'lint-v2',
-        f'{root_dir}'
-      ]
-      pants_run = self.run_pants(command=command)
-      self.assert_failure(pants_run)
-    self.assertIn("2 files would be reformatted", pants_run.stderr_data)
-    self.assertNotIn("2 files reformatted", pants_run.stderr_data)
-    self.assertIn("1 file would be left unchanged", pants_run.stderr_data)
-    self.assertNotIn("1 file left unchanged", pants_run.stderr_data)
+  def test_single_passing_source(self) -> None:
+    lint_result, fmt_result = self.run_black([self.good_source])
+    self.assertEqual(lint_result.exit_code, 0)
+    self.assertIn("1 file would be left unchanged", lint_result.stderr)
+    self.assertIn("1 file left unchanged", fmt_result.stderr)
+    self.assertEqual(fmt_result.digest, self.get_digest([self.good_source]))
 
-  def test_black_lint_given_multiple_targets(self):
-    with build_directory() as a_dir:
-      with build_directory() as another_dir:
-        write_inconsistently_formatted_file(a_dir, "incorrect.py")
-        write_inconsistently_formatted_file(another_dir, "broken.py")
-        write_inconsistently_formatted_file(another_dir, "messed_up.py")
-        command = [
-          'lint-v2',
-          f'{a_dir}',
-          f'{another_dir}'
-        ]
-        pants_run = self.run_pants(command=command)
-        self.assert_failure(pants_run)
-    self.assertIn("1 file would be reformatted", pants_run.stderr_data)
-    self.assertIn("2 files would be reformatted", pants_run.stderr_data)
-    self.assertNotIn("unchanged", pants_run.stderr_data)
+  def test_single_failing_source(self) -> None:
+    lint_result, fmt_result = self.run_black([self.bad_source])
+    self.assertEqual(lint_result.exit_code, 1)
+    self.assertIn("1 file would be reformatted", lint_result.stderr)
+    self.assertIn("1 file reformatted", fmt_result.stderr)
+    self.assertEqual(fmt_result.digest, self.get_digest([self.fixed_bad_source]))
+
+  def test_multiple_mixed_sources(self) -> None:
+    lint_result, fmt_result = self.run_black([self.good_source, self.bad_source])
+    self.assertEqual(lint_result.exit_code, 1)
+    self.assertIn("1 file would be reformatted, 1 file would be left unchanged", lint_result.stderr)
+    self.assertIn("1 file reformatted, 1 file left unchanged", fmt_result.stderr)
+    self.assertEqual(fmt_result.digest, self.get_digest([self.good_source, self.fixed_bad_source]))
+
+  def test_respects_config_file(self) -> None:
+    # Note the single quotes, which Black does not like by default.
+    source = FileContent(path="test/good.py", content=b"name = 'Anakin'\n")
+    lint_result, fmt_result = self.run_black(
+      [source], config="[tool.black]\nskip-string-normalization = 'true'\n"
+    )
+    self.assertEqual(lint_result.exit_code, 0)
+    self.assertIn("1 file would be left unchanged", lint_result.stderr)
+    self.assertIn("1 file left unchanged", fmt_result.stderr)
+    self.assertEqual(fmt_result.digest, self.get_digest([source]))
