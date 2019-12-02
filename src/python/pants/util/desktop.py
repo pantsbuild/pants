a4fd9d97f53d55770bfbfc21d577a1e8a769aa0a
Tomasz Pasternak <tpasternak@virtuslab.com>
2019-12-02 15:39:24 +0100

    Opening on Macs

--- src/python/pants/util/desktop.py
+++ src/python/pants/util/desktop.py
@@ -12,8 +12,7 @@ class OpenError(Exception):
 
 
 def _mac_open_with_idea(file_: str) -> None:
-  raise NotImplementedError
-
+  subprocess.call(['open', "-a", "/Applications/IntelliJ IDEA CE.app", file_])
 
 def _mac_open(files: Sequence[str]) -> None:
   subprocess.call(['open'] + list(files))
