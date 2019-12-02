4b5f47761061238a973d344be2d4bbd39a7c8019
Josh Soref <jsoref@users.noreply.github.com>
2019-12-02 14:31:43 -0500

    spelling: dependency

--- src/python/pants/backend/jvm/tasks/jvm_dependency_usage.py
+++ src/python/pants/backend/jvm/tasks/jvm_dependency_usage.py
@@ -329,7 +329,7 @@ class Node:
 
 
 class Edge:
-  """Record a set of used products, and a boolean indicating that a depedency edge was declared."""
+  """Record a set of used products, and a boolean indicating that a dependency edge was declared."""
 
   def __init__(self, is_declared=False, is_used=False, products_used=None):
     self.products_used = products_used or set()
