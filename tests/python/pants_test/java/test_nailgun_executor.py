7b1546e9e2b23e7f80ae515f407ac1b328d5c21f
Josh Soref <jsoref@users.noreply.github.com>
2019-12-02 16:12:45 -0500

    spelling: timeout

--- tests/python/pants_test/java/test_nailgun_executor.py
+++ tests/python/pants_test/java/test_nailgun_executor.py
@@ -82,7 +82,7 @@ class NailgunExecutorTest(TestBase):
       mock_open.return_value = stdout_read
       mock_read_file.return_value = 'err'
       # The stdout write pipe has no input and hasn't been closed, so the selector.select() should
-      # time out regardless of the timemout argument, and raise.
+      # time out regardless of the timeout argument, and raise.
       with self.assertRaisesWithMessage(NailgunExecutor.InitialNailgunConnectTimedOut, """\
 Failed to read nailgun output after 0.0001 seconds!
 Stdout:
