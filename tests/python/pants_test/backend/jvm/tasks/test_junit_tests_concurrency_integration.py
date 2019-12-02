0111430140eaaa3d9a35157d04ab1680e1ea4101
Josh Soref <jsoref@users.noreply.github.com>
2019-12-02 14:49:32 -0500

    spelling: concurrency

--- tests/python/pants_test/backend/jvm/tasks/test_junit_tests_concurrency_integration.py
+++ tests/python/pants_test/backend/jvm/tasks/test_junit_tests_concurrency_integration.py
@@ -98,7 +98,7 @@ class JunitTestsConcurrencyIntegrationTest(PantsRunIntegrationTest):
 
   @ensure_experimental
   def test_parallel_both(self):
-    """Checks the concurency='parallel_both' setting."""
+    """Checks the concurrency='parallel_both' setting."""
     with self.temporary_workdir() as workdir:
       pants_run = self.run_pants_with_workdir([
         'test',
@@ -157,7 +157,7 @@ class ExperimentalOnlyJunitTestsConcurrencyIntegrationTest(PantsRunIntegrationTe
       self.assertIn("OK (2 tests)", pants_run.stdout_data)
 
   def test_parallel_methods(self):
-    """Checks the concurency='parallel_methods' setting."""
+    """Checks the concurrency='parallel_methods' setting."""
     with self.temporary_workdir() as workdir:
       pants_run = self.run_pants_with_workdir([
         'test',
