e3153b2813afb8c107da3a2c76ad7e6623c458f5
Eric Arellano <ericarellano@me.com>
2019-12-09 14:26:12 -0700

    Remove deprecated `./pants mypy` in favor of `./pants lint.mypy` (#8773)
    This should have been removed in Pants 1.21.0.dev0.
    
    By removing the `mypy` goal scope, we are able to now reclaim `mypy` as a subsystem name.

--- contrib/mypy/src/python/pants/contrib/mypy/tasks/mypy_task.py
+++ contrib/mypy/src/python/pants/contrib/mypy/tasks/mypy_task.py
@@ -45,9 +45,6 @@ class MypyTask(LintTaskMixin, ResolveRequirementsTaskBase):
   _MYPY_COMPATIBLE_INTERPETER_CONSTRAINT = '>=3.5'
   _PYTHON_SOURCE_EXTENSION = '.py'
 
-  deprecated_options_scope = 'mypy'
-  deprecated_options_scope_removal_version = '1.21.0.dev0'
-
   @classmethod
   def prepare(cls, options, round_manager):
     super().prepare(options, round_manager)
