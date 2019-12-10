942809f12fd38e4e7b303dd37c16724013abf8bd
John Sirois <john.sirois@gmail.com>
2019-12-10 11:35:13 -0800

    Fix pantsbuild wheel publishing. (#8782)
    * Fix pantsbuild wheel publishing.
    
    The `pantsbuild.pants.testinfra` distribution has been broken since
    teh `1.23.0.dev1` release with an invalid descrition field. This has
    prevented publishing of the package as well as any packages following
    it in the release. Fixup the root problem and add a check that will
    fail PR CI in the future for similar breaks.
    
    In addition, cleanup a warning against our packages not declaring
    `long_description_content_type` brought to light by the new check.

--- tests/python/pants_test/console_rule_test_base.py
+++ tests/python/pants_test/console_rule_test_base.py
@@ -1,11 +1,8 @@
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.base.deprecated import deprecated_module
 from pants.testutil.console_rule_test_base import ConsoleRuleTestBase as ConsoleRuleTestBase  # noqa
+from pants_test.deprecated_testinfra import deprecated_testinfra_module
 
 
-deprecated_module(
-  removal_version="1.25.0.dev0",
-  hint_message="Import pants.testutil.console_rule_test_base instead."
-)
+deprecated_testinfra_module()
--- tests/python/pants_test/option/util/fakes.py
+++ tests/python/pants_test/option/util/fakes.py
@@ -1,7 +1,6 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.base.deprecated import deprecated_module
 from pants.testutil.option.fakes import _FakeOptionValues as _FakeOptionValues  # noqa
 from pants.testutil.option.fakes import (
   _options_registration_function as _options_registration_function,
@@ -10,9 +9,7 @@ from pants.testutil.option.fakes import create_options as create_options  # noqa
 from pants.testutil.option.fakes import (
   create_options_for_optionables as create_options_for_optionables,
 )  # noqa
+from pants_test.deprecated_testinfra import deprecated_testinfra_module
 
 
-deprecated_module(
-  removal_version="1.25.0.dev0",
-  hint_message="Import pants.testutil.option.fakes instead."
-)
+deprecated_testinfra_module('pants.testutil.option.fakes')
