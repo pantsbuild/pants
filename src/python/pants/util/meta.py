9bba0b305cf4e5dfbec81bfc47246fe3b6127e57
Eric Arellano <ericarellano@me.com>
2019-12-24 09:02:28 -0700

    Fix type hints for decorators in memo.py and meta.py

--- src/python/pants/util/meta.py
+++ src/python/pants/util/meta.py
@@ -6,6 +6,13 @@ from functools import wraps
 from typing import Any, Callable, Optional, Type, TypeVar, Union
 
 
+T = TypeVar("T")
+C = TypeVar("C")
+# This TypeVar is used to annotate property decorators. The decorator should take the parameter
+# func=Callable[..., P] and return P.
+P = TypeVar("P")
+
+
 class SingletonMetaclass(type):
   """When using this metaclass in your class definition, your class becomes a singleton. That is,
   every construction returns the same instance.
@@ -21,9 +28,6 @@ class SingletonMetaclass(type):
     if not hasattr(cls, 'instance'):
       cls.instance = super().__call__(*args, **kwargs)
     return cls.instance
-
-
-T = TypeVar("T")
 
 
 class ClassPropertyDescriptor:
@@ -54,7 +58,7 @@ with an @classproperty decorator."""
     return callable_field()
 
 
-def classproperty(func: Union[classmethod, staticmethod, Callable]) -> ClassPropertyDescriptor:
+def classproperty(func: Callable[..., P]) -> P:
   """Use as a decorator on a method definition to make it a class-level attribute.
 
   This decorator can be applied to a method, a classmethod, or a staticmethod. This decorator will
@@ -77,12 +81,17 @@ def classproperty(func: Union[classmethod, staticmethod, Callable]) -> ClassProp
   doc = func.__doc__
 
   if not isinstance(func, (classmethod, staticmethod)):
-    func = classmethod(func)
+    # MyPy complains about converting a Callable -> classmethod. We use a Callable in the first
+    # place because there is no typing.classmethod, i.e. a type that takes generic arguments, and
+    # we need to use TypeVars for the call sites of this decorator to work properly.
+    func = classmethod(func)  # type: ignore[assignment]
 
-  return ClassPropertyDescriptor(func, doc)
+  # If we properly annotated this function as returning a ClassPropertyDescriptor, then MyPy would
+  # no longer work correctly at call sites for this decorator.
+  return ClassPropertyDescriptor(func, doc)  # type: ignore[arg-type, return-value]
 
 
-def staticproperty(func: Union[staticmethod, Callable]) -> ClassPropertyDescriptor:
+def staticproperty(func: Callable[..., P]) -> P:
   """Use as a decorator on a method definition to make it a class-level attribute (without binding).
 
   This decorator can be applied to a method or a staticmethod. This decorator does not bind any
@@ -106,12 +115,17 @@ def staticproperty(func: Union[staticmethod, Callable]) -> ClassPropertyDescript
   doc = func.__doc__
 
   if not isinstance(func, staticmethod):
-    func = staticmethod(func)
+    # MyPy complains about converting a Callable -> staticmethod. We use a Callable in the first
+    # place because there is no typing.staticmethod, i.e. a type that takes generic arguments, and
+    # we need to use TypeVars for the call sites of this decorator to work properly.
+    func = staticmethod(func)  # type: ignore[assignment]
 
-  return ClassPropertyDescriptor(func, doc)
+  # If we properly annotated this function as returning a ClassPropertyDescriptor, then MyPy would
+  # no longer work correctly at call sites for this decorator.
+  return ClassPropertyDescriptor(func, doc)  # type: ignore[arg-type, return-value]
 
 
-def frozen_after_init(cls: Type[T]) -> Type[T]:
+def frozen_after_init(cls: Type[C]) -> Type[C]:
   """Class decorator to freeze any modifications to the object after __init__() is done.
 
   The primary use case is for @dataclasses who cannot use frozen=True due to the need for a custom
