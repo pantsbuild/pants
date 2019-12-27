4dd57fa50adacf599af2f04c1fc99e14c94511d1
Eric Arellano <ericarellano@me.com>
2019-12-27 09:22:01 -0700

    Fix isort integration test

--- tests/python/pants_test/backend/python/tasks/test_isort_run_integration.py
+++ tests/python/pants_test/backend/python/tasks/test_isort_run_integration.py
@@ -9,11 +9,12 @@ class IsortRunIntegrationTest(PantsRunIntegrationTest):
 
   @ensure_daemon
   def test_isort_no_python_sources_should_noop(self):
-    command = ['-ldebug',
-               'fmt.isort',
-               'testprojects/tests/java/org/pantsbuild/testproject/dummies/::',
-               '--',
-               '--check-only']
+    command = [
+      '-ldebug',
+      'fmt.isort',
+      'testprojects/tests/java/org/pantsbuild/testproject/dummies/::',
+      "--isort-args='--check-only'",
+    ]
     pants_run = self.run_pants(command=command)
     self.assert_success(pants_run)
-    self.assertIn(IsortRun.NOOP_MSG_HAS_TARGET_BUT_NO_SOURCE, pants_run.stderr_data)
+    assert IsortRun.NOOP_MSG_HAS_TARGET_BUT_NO_SOURCE in pants_run.stderr_data
