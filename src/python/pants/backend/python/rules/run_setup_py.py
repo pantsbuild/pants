90298324fe9ecaee81d31d383b15345dec9e5ed9
Benjy Weinberger <benjyw@gmail.com>
2019-12-30 09:10:46 -0500

    Rules to find the exported target that owns a library target, and to find all library targets owned by an exported target.

--- src/python/pants/backend/python/rules/run_setup_py.py
+++ src/python/pants/backend/python/rules/run_setup_py.py
@@ -49,10 +49,119 @@ from pants.rules.core.strip_source_root import SourceRootStrippedSources
 from pants.source.source_root import SourceRootConfig
 
 
+class NoOwnerError(Exception):
+  """Indicates an exportable target has no owning exported target."""
+
+
+class AmbiguousOwnerError(Exception):
+  """Indicates an exportable target has more than one owning exported target."""
+
+
+@dataclass(frozen=True)
+class ExportedTarget:
+  """A target that explicitly exports an artifact, using a `provides=` stanza.
+
+  The code provided by this artifact can be from this target or from any targets it owns.
+  """
+  hydrated_target: HydratedTarget
+
+
+@dataclass(frozen=True)
+class DependencyOwner:
+  """An ExportedTarget in its role as an owner of other targets.
+
+  We need this type to prevent rule ambiguities when computing the list of targets
+  owned by an ExportedTarget (which involves going from ExportedTarget -> dep -> owner (which
+  is itself an ExportedTarget) and checking if owner is this the original ExportedTarget.
+  """
+  exported_target: ExportedTarget
+
+
+@dataclass(frozen=True)
+class OwnedDependency:
+  """A target that is owned by some ExportedTarget.
+
+  Code in this target is published in the owner's artifact.
+
+  The owner of a target T is T's closest filesystem ancestor among the exported targets
+  that directly or indirectly depend on it (including T itself).
+  """
+  hydrated_target: HydratedTarget
+
+
+class OwnedDependencies(Collection[OwnedDependency]):
+  pass
+
+
 @dataclass(frozen=True)
 class SetuptoolsSetup:
   """The setuptools tool."""
   requirements_pex: Pex
+
+
+def _is_exported(target: HydratedTarget) -> bool:
+  return getattr(target.adaptor, 'provides', None) is not None
+
+
+@rule(name="Get owned targets")
+async def get_owned_dependencies(dependency_owner: DependencyOwner) -> OwnedDependencies:
+  """Find the dependencies of dependency_owner that are owned by it.
+
+  Includes dependency_owner itself.
+  """
+  tht = await Get[TransitiveHydratedTargets](
+    BuildFileAddresses([dependency_owner.exported_target.hydrated_target.address]))
+  all_tgts = list(tht.closure)
+  ownable_targets = [tgt for tgt in all_tgts
+                     if isinstance(tgt.adaptor, (PythonTargetAdaptor, ResourcesAdaptor))]
+  owners = await MultiGet(Get[ExportedTarget](OwnedDependency(ht)) for ht in ownable_targets)
+  owned_dependencies = [tgt for owner, tgt in zip(owners, ownable_targets)
+                        if owner == dependency_owner.exported_target]
+  return OwnedDependencies(OwnedDependency(t) for t in owned_dependencies)
+
+
+@rule(name="Get exporting owner for target")
+async def get_exporting_owner(owned_dependency: OwnedDependency) -> ExportedTarget:
+  """Find the exported target that owns the given target (and therefore exports it).
+
+  The owner of T (i.e., the exported target in whose artifact T's code is published) is:
+
+   1. An exported target that depends on T (or is T itself).
+   2. Is T's closest filesystem ancestor among those satisfying 1.
+
+  If there are multiple such exported targets at the same degree of ancestry, the ownership
+  is ambiguous and an error is raised. If there is no exported target that depends on T
+  and is its ancestor, then there is no owner and an error is raised.
+  """
+  hydrated_target = owned_dependency.hydrated_target
+  ancestor_addrs = AscendantAddresses(hydrated_target.address.spec_path)
+  ancestor_tgts = await Get[HydratedTargets](Specs, Specs((ancestor_addrs,)))
+  # Note that addresses sort by (spec_path, target_name), and all these targets are
+  # ancestors of the given target, i.e., their spec_paths are all prefixes. So sorting by
+  # address will effectively sort by closeness of ancestry to the given target.
+  exported_ancestor_tgts = sorted(
+    [t for t in ancestor_tgts if _is_exported(t)], key=lambda t: t.address, reverse=True)
+  exported_ancestor_iter = iter(exported_ancestor_tgts)
+  for exported_ancestor in exported_ancestor_iter:
+    tht = await Get[TransitiveHydratedTargets](BuildFileAddresses([exported_ancestor.address]))
+    if hydrated_target in tht.closure:
+      owner = exported_ancestor
+      # Find any exported siblings of owner that also depend on hydrated_target. They have the
+      # same spec_path as it, so they must immediately follow it in ancestor_iter.
+      sibling_owners = []
+      sibling = next(exported_ancestor_iter, None)
+      while sibling and sibling.address.spec_path == owner.address.spec_path:
+        tht = await Get[TransitiveHydratedTargets](BuildFileAddresses([sibling.address]))
+        if hydrated_target in tht.closure:
+          sibling_owners.append(sibling)
+        sibling = next(exported_ancestor_iter, None)
+      if sibling_owners:
+        raise AmbiguousOwnerError(
+          f'Exporting owners for {hydrated_target.address.reference()} are ambiguous. Found '
+          f'{exported_ancestor.address.reference()} and {len(sibling_owners)} others: '
+          f'{", ".join(so.address.reference() for so in sibling_owners)}')
+      return ExportedTarget(owner)
+  raise NoOwnerError(f'No exported target owner found for {hydrated_target.address.reference()}')
 
 
 @rule(name="Set up setuptools")
--- src/python/pants/backend/python/rules/run_setup_py_test.py
+++ src/python/pants/backend/python/rules/run_setup_py_test.py
@@ -54,3 +54,150 @@ class TestSetupPyBase(TestBase):
 
   def tgt(self, addr: str) -> HydratedTarget:
     return self.request_single_product(HydratedTarget, Params(Address.parse(addr)))
+
+
+class TestGetOwnedDependencies(TestSetupPyBase):
+  @classmethod
+  def rules(cls):
+    return super().rules() + [
+      get_owned_dependencies,
+      get_exporting_owner,
+      RootRule(DependencyOwner),
+    ]
+
+  def assert_owned(self, owned: Iterable[str], exported: str):
+    assert sorted(owned) == sorted(
+      od.hydrated_target.address.reference() for od in self.request_single_product(
+      OwnedDependencies, Params(DependencyOwner(ExportedTarget(self.tgt(exported))))
+    ))
+
+  def test_owned_dependencies(self):
+    self.create_file('src/python/foo/bar/baz/BUILD', textwrap.dedent("""
+      python_library(name='baz1')
+      python_library(name='baz2')
+    """))
+    self.create_file('src/python/foo/bar/BUILD', textwrap.dedent("""
+      python_library(name='bar1',
+                     dependencies=['src/python/foo/bar/baz:baz1'],
+                     provides=setup_py(name='bar1', version='1.1.1'))
+      python_library(name='bar2',
+                     dependencies=[':bar-resources', 'src/python/foo/bar/baz:baz2'])
+      resources(name='bar-resources')
+    """))
+    self.create_file('src/python/foo/BUILD', textwrap.dedent("""
+      python_library(name='foo',
+                     dependencies=['src/python/foo/bar:bar1', 'src/python/foo/bar:bar2'],
+                     provides=setup_py(name='foo', version='3.4.5'))
+    """))
+
+    self.assert_owned(['src/python/foo/bar:bar1', 'src/python/foo/bar/baz:baz1'],
+                      'src/python/foo/bar:bar1')
+    self.assert_owned(['src/python/foo', 'src/python/foo/bar:bar2',
+                       'src/python/foo/bar:bar-resources', 'src/python/foo/bar/baz:baz2'],
+                      'src/python/foo')
+
+
+class TestGetExportingOwner(TestSetupPyBase):
+  @classmethod
+  def rules(cls):
+    return super().rules() + [
+      get_exporting_owner,
+      RootRule(OwnedDependency),
+    ]
+
+  def assert_is_owner(self, owner: str, owned: str):
+    assert (owner ==
+            self.request_single_product(
+              ExportedTarget,
+              Params(OwnedDependency(self.tgt(owned)))).hydrated_target.address.reference())
+
+  def assert_error(self, owned: str, exc_cls: Type[Exception]):
+    with pytest.raises(ExecutionError) as excinfo:
+      self.request_single_product(ExportedTarget, Params(OwnedDependency(self.tgt(owned))))
+    ex = excinfo.value
+    assert len(ex.wrapped_exceptions) == 1
+    assert type(ex.wrapped_exceptions[0]) == exc_cls
+
+  def assert_no_owner(self, owned: str):
+    self.assert_error(owned, NoOwnerError)
+
+  def assert_ambiguous_owner(self, owned: str):
+    self.assert_error(owned, AmbiguousOwnerError)
+
+  def test_get_owner_simple(self):
+    self.create_file('src/python/foo/bar/baz/BUILD', textwrap.dedent("""
+      python_library(name='baz1')
+      python_library(name='baz2')
+    """))
+    self.create_file('src/python/foo/bar/BUILD', textwrap.dedent("""
+      python_library(name='bar1',
+                     dependencies=['src/python/foo/bar/baz:baz1'],
+                     provides=setup_py(name='bar1', version='1.1.1'))
+      python_library(name='bar2',
+                     dependencies=[':bar-resources', 'src/python/foo/bar/baz:baz2'])
+      resources(name='bar-resources')
+    """))
+    self.create_file('src/python/foo/BUILD', textwrap.dedent("""
+      python_library(name='foo1',
+                     dependencies=['src/python/foo/bar/baz:baz2'],
+                     provides=setup_py(name='foo1', version='0.1.2'))
+      python_library(name='foo2')
+      python_library(name='foo3',
+                     dependencies=['src/python/foo/bar:bar2'],
+                     provides=setup_py(name='foo3', version='3.4.5'))
+    """))
+
+    self.assert_is_owner('src/python/foo/bar:bar1', 'src/python/foo/bar:bar1')
+    self.assert_is_owner('src/python/foo/bar:bar1', 'src/python/foo/bar/baz:baz1')
+
+    self.assert_is_owner('src/python/foo:foo1', 'src/python/foo:foo1')
+
+    self.assert_is_owner('src/python/foo:foo3', 'src/python/foo:foo3')
+    self.assert_is_owner('src/python/foo:foo3', 'src/python/foo/bar:bar2')
+    self.assert_is_owner('src/python/foo:foo3', 'src/python/foo/bar:bar-resources')
+
+    self.assert_no_owner('src/python/foo:foo2')
+    self.assert_ambiguous_owner('src/python/foo/bar/baz:baz2')
+
+  def test_get_owner_siblings(self):
+    self.create_file('src/python/siblings/BUILD', textwrap.dedent("""
+        python_library(name='sibling1')
+        python_library(name='sibling2',
+                       dependencies=['src/python/siblings:sibling1'],
+                       provides=setup_py(name='siblings', version='2.2.2'))
+      """))
+
+    self.assert_is_owner('src/python/siblings:sibling2', 'src/python/siblings:sibling1')
+    self.assert_is_owner('src/python/siblings:sibling2', 'src/python/siblings:sibling2')
+
+  def test_get_owner_not_an_ancestor(self):
+    self.create_file('src/python/notanancestor/aaa/BUILD', textwrap.dedent("""
+        python_library(name='aaa')
+      """))
+    self.create_file('src/python/notanancestor/bbb/BUILD', textwrap.dedent("""
+        python_library(name='bbb',
+                       dependencies=['src/python/notanancestor/aaa'],
+                       provides=setup_py(name='bbb', version='11.22.33'))
+      """))
+
+    self.assert_no_owner('src/python/notanancestor/aaa')
+    self.assert_is_owner('src/python/notanancestor/bbb', 'src/python/notanancestor/bbb')
+
+  def test_get_owner_multiple_ancestor_generations(self):
+    self.create_file('src/python/aaa/bbb/ccc/BUILD', textwrap.dedent("""
+        python_library(name='ccc')
+      """))
+    self.create_file('src/python/aaa/bbb/BUILD', textwrap.dedent("""
+        python_library(name='bbb',
+                       dependencies=['src/python/aaa/bbb/ccc'],
+                       provides=setup_py(name='bbb', version='1.1.1'))
+      """))
+    self.create_file('src/python/aaa/BUILD', textwrap.dedent("""
+        python_library(name='aaa',
+                       dependencies=['src/python/aaa/bbb/ccc'],
+                       provides=setup_py(name='aaa', version='2.2.2'))
+      """))
+
+    self.assert_is_owner('src/python/aaa/bbb', 'src/python/aaa/bbb/ccc')
+    self.assert_is_owner('src/python/aaa/bbb', 'src/python/aaa/bbb')
+    self.assert_is_owner('src/python/aaa', 'src/python/aaa')
