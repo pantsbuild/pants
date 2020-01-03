5796cd5330f4b8203e3f9a9f50138d1086cced3b
codealchemy <alex@alchemist.codes>
2020-01-02 17:12:55 -0800

    Pass input files to InteractiveProcessRequest
    Instead of using workspace / tmpdir

--- src/python/pants/backend/python/rules/python_test_runner.py
+++ src/python/pants/backend/python/rules/python_test_runner.py
@@ -1,7 +1,6 @@
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pathlib import Path
 from typing import Optional
 
 from pants.backend.python.rules.inject_init import InjectedInitDigest
@@ -10,10 +9,9 @@ from pants.backend.python.rules.pex_from_target_closure import CreatePexFromTarg
 from pants.backend.python.subsystems.pytest import PyTest
 from pants.backend.python.subsystems.python_setup import PythonSetup
 from pants.backend.python.subsystems.subprocess_environment import SubprocessEncodingEnvironment
-from pants.base.build_root import BuildRoot
 from pants.build_graph.address import Address
 from pants.engine.addressable import BuildFileAddresses
-from pants.engine.fs import Digest, DirectoriesToMerge, DirectoryToMaterialize, Workspace
+from pants.engine.fs import Digest, DirectoriesToMerge
 from pants.engine.interactive_runner import InteractiveProcessRequest, InteractiveRunner
 from pants.engine.isolated_process import ExecuteProcessRequest, FallibleExecuteProcessResult
 from pants.engine.legacy.graph import HydratedTarget, TransitiveHydratedTargets
@@ -22,7 +20,6 @@ from pants.engine.rules import UnionRule, optionable_rule, rule
 from pants.engine.selectors import Get, MultiGet
 from pants.rules.core.core_test_model import TestDebugResult, TestResult, TestTarget
 from pants.rules.core.strip_source_root import SourceRootStrippedSources
-from pants.util.contextutil import temporary_dir
 
 
 def calculate_timeout_seconds(
@@ -127,9 +124,7 @@ async def debug_python_test(
   test_target: PythonTestsAdaptor,
   pytest: PyTest,
   python_setup: PythonSetup,
-  build_root: BuildRoot,
-  runner: InteractiveRunner,
-  workspace: Workspace,
+  runner: InteractiveRunner
 ) -> TestDebugResult:
 
   transitive_hydrated_targets = await Get[TransitiveHydratedTargets](
@@ -173,27 +168,18 @@ async def debug_python_test(
     ),
   )
 
-  with temporary_dir(root_dir=str(Path(build_root.path, ".pants.d")), cleanup=True) as tmpdir:
-    path_relative_to_build_root = str(Path(tmpdir).relative_to(build_root.path))
-    test_target_sources_file_names = sorted(
-      f'{path_relative_to_build_root}/{snapshot_file}'
-      for snapshot_file
-      in source_root_stripped_test_target_sources.snapshot.files
-    )
-    pex_args = (*pytest.get_args(), *test_target_sources_file_names)
+  test_target_sources_file_names = sorted(source_root_stripped_test_target_sources.snapshot.files)
+  pex_args = (*pytest.get_args(), *test_target_sources_file_names)
 
-    workspace.materialize_directory(
-      DirectoryToMaterialize(merged_input_files, path_prefix=path_relative_to_build_root)
-    )
+  run_request = InteractiveProcessRequest(
+    argv=(output_pytest_requirements_pex_filename, *pex_args),
+    run_in_workspace=False,
+    input_files=merged_input_files
+  )
 
-    run_request = InteractiveProcessRequest(
-      argv=(f'{path_relative_to_build_root}/{output_pytest_requirements_pex_filename}', *pex_args),
-      run_in_workspace=True,
-    )
+  result = runner.run_local_interactive_process(run_request)
 
-    result = runner.run_local_interactive_process(run_request)
-
-    return TestDebugResult(result.process_exit_code)
+  return TestDebugResult(result.process_exit_code)
 
 
 def rules():
