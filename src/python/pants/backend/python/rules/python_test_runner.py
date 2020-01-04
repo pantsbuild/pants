f3c9b7cf84be7a461954ba1ff6951e994a4d81f4
Greg Shuflin <greg.shuflin@toolchain.com>
2020-01-03 17:29:41 -0800

    Force color in pytest

--- src/python/pants/backend/python/rules/python_test_runner.py
+++ src/python/pants/backend/python/rules/python_test_runner.py
@@ -17,6 +17,7 @@ from pants.engine.legacy.graph import HydratedTargets, TransitiveHydratedTargets
 from pants.engine.legacy.structs import PythonTestsAdaptor
 from pants.engine.rules import UnionRule, rule, subsystem_rule
 from pants.engine.selectors import Get
+from pants.option.options_bootstrapper import OptionsBootstrapper
 from pants.rules.core.core_test_model import TestResult, TestTarget
 from pants.rules.core.strip_source_root import SourceRootStrippedSources
 
@@ -48,7 +49,8 @@ async def run_python_test(
   test_target: PythonTestsAdaptor,
   pytest: PyTest,
   python_setup: PythonSetup,
-  subprocess_encoding_environment: SubprocessEncodingEnvironment
+  subprocess_encoding_environment: SubprocessEncodingEnvironment,
+  options_bootstrapper: OptionsBootstrapper
 ) -> TestResult:
   """Runs pytest for one target."""
 
@@ -92,6 +94,11 @@ async def run_python_test(
     timeout_default=pytest.options.timeout_default,
     timeout_maximum=pytest.options.timeout_maximum,
   )
+
+  global_options = options_bootstrapper.bootstrap_options.for_global_scope()
+  colors = global_options.colors
+  env = {"PYTEST_ADDOPTS": f"--color={'yes' if colors else 'no'}"}
+
   request = resolved_requirements_pex.create_execute_request(
     python_setup=python_setup,
     subprocess_encoding_environment=subprocess_encoding_environment,
@@ -99,7 +106,8 @@ async def run_python_test(
     pex_args=(*pytest.options.args, *test_target_sources_file_names),
     input_files=merged_input_files,
     description=f'Run Pytest for {test_target.address.reference()}',
-    timeout_seconds=timeout_seconds if timeout_seconds is not None else 9999
+    timeout_seconds=timeout_seconds if timeout_seconds is not None else 9999,
+    env=env
   )
   result = await Get[FallibleExecuteProcessResult](ExecuteProcessRequest, request)
   return TestResult.from_fallible_execute_process_result(result)
