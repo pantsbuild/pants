4987d1e7af0be71f22a23cc82f809050f6441aa9
Dylan Herman <dherman@foursquare.com>
2020-01-11 16:30:45 -0600

    migrate to create_options_bootstrapper

--- src/python/pants/backend/python/lint/bandit/rules_integration_test.py
+++ src/python/pants/backend/python/lint/bandit/rules_integration_test.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from typing import List, Optional, Sequence
+from typing import List, Optional
 
 from pants.backend.python.lint.bandit.rules import BanditTarget
 from pants.backend.python.lint.bandit.rules import rules as bandit_rules
@@ -21,7 +21,8 @@ from pants.engine.rules import RootRule
 from pants.engine.selectors import Params
 from pants.rules.core.lint import LintResult
 from pants.source.wrapped_globs import EagerFilesetWithSpec
-from pants.testutil.subsystem.util import global_subsystem_instance, init_subsystems
+from pants.testutil.option.util import create_options_bootstrapper
+from pants.testutil.subsystem.util import init_subsystems
 from pants.testutil.test_base import TestBase
 
 
@@ -60,36 +61,29 @@ class BanditIntegrationTest(TestBase):
     source_files: List[FileContent],
     *,
     config: Optional[str] = None,
-    passthrough_args: Optional[Sequence[str]] = None,
-    interpreter_constraints: Optional[Sequence[str]] = None,
+    passthrough_args: Optional[str] = None,
+    interpreter_constraints: Optional[str] = None,
     skip: bool = False,
   ) -> LintResult:
-    if config is not None:
-      self.create_file(relpath="bandit.yaml", contents=config)
+    args = ["--backend-packages2=pants.backend.python.lint.bandit"]
+    if config:
+      # TODO: figure out how to get this file to exist...
+      self.create_file(relpath=".bandit", contents=config)
+      args.append("--bandit-config=.bandit")
+    if passthrough_args:
+      args.append(f"--bandit-args={passthrough_args}")
+    if skip:
+      args.append(f"--bandit-skip")
     input_snapshot = self.request_single_product(Snapshot, InputFilesContent(source_files))
     target = BanditTarget(
       PythonTargetAdaptor(
         sources=EagerFilesetWithSpec('test', {'globs': []}, snapshot=input_snapshot),
         address=Address.parse("test:target"),
-        compatibility=interpreter_constraints,
+        compatibility=[interpreter_constraints] if interpreter_constraints else None,
       )
-    )
-    bandit_subsystem = global_subsystem_instance(
-      Bandit, options={Bandit.options_scope: {
-        "config": "bandit.yaml" if config else None,
-        "args": passthrough_args or [],
-        "skip": skip,
-      }}
     )
     return self.request_single_product(
-      LintResult,
-      Params(
-        target,
-        bandit_subsystem,
-        PythonNativeCode.global_instance(),
-        PythonSetup.global_instance(),
-        SubprocessEnvironment.global_instance()
-      )
+      LintResult, Params(target, create_options_bootstrapper(args=args)),
     )
 
   def test_single_passing_source(self) -> None:
