e57d5c89642133a228d74cb07bd57a10e9d15289
Eric Arellano <ericarellano@me.com>
2020-01-21 16:42:04 -0700

    Add IPython subsystem and deprecate `--repl-py` options

---
+++ src/python/pants/backend/python/subsystems/ipython.py
@@ -0,0 +1,22 @@
+# Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from typing import List
+
+from pants.backend.python.subsystems.python_tool_base import PythonToolBase
+
+
+class IPython(PythonToolBase):
+  options_scope = 'ipython'
+  default_version = 'ipython==5.8.0'
+  default_extra_requirements: List[str] = []
+  default_entry_point = 'IPython:start_ipython'
+  default_interpreter_constraints = ["CPython>=2.7,<3", "CPython>=3.4"]
+
+  @classmethod
+  def register_options(cls, register):
+    super().register_options(register)
+    register(
+      '--enabled', type=bool, default=False, fingerprint=True,
+      help="Use IPython instead of the standard Python REPL.",
+    )
