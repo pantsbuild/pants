aeaa4c4d90124853af328ef0a1e1ef61a20390a6
Eric Arellano <ericarellano@me.com>
2020-02-11 15:24:47 -0700

    Add strip_source_root_from_snapshot

--- src/python/pants/rules/core/strip_source_roots.py
+++ src/python/pants/rules/core/strip_source_roots.py
@@ -2,13 +2,24 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from dataclasses import dataclass
+from itertools import groupby
+from pathlib import PurePath
+from typing import cast
 
 from pants.build_graph.files import Files
-from pants.engine.fs import EMPTY_SNAPSHOT, Digest, DirectoryWithPrefixToStrip, Snapshot
+from pants.engine.fs import (
+  EMPTY_SNAPSHOT,
+  Digest,
+  DirectoriesToMerge,
+  DirectoryWithPrefixToStrip,
+  PathGlobs,
+  Snapshot,
+  SnapshotSubset,
+)
 from pants.engine.legacy.graph import HydratedTarget
 from pants.engine.rules import rule, subsystem_rule
-from pants.engine.selectors import Get
-from pants.source.source_root import SourceRootConfig
+from pants.engine.selectors import Get, MultiGet
+from pants.source.source_root import NoSourceRootError, SourceRootConfig
 
 
 @dataclass(frozen=True)
@@ -17,9 +28,59 @@ class SourceRootStrippedSources:
   snapshot: Snapshot
 
 
+@dataclass(frozen=True)
+class SnapshotToStrip:
+  """A wrapper around Snapshot to remove graph ambiguity."""
+  snapshot: Snapshot
+
+
+@rule
+async def strip_source_roots_from_snapshot(
+  wrapped_snapshot: SnapshotToStrip, source_root_config: SourceRootConfig,
+) -> SourceRootStrippedSources:
+  """Removes source roots from a snapshot,
+  e.g. `src/python/pants/util/strutil.py` -> `pants/util/strutil.py`.
+  """
+  source_roots_object = source_root_config.get_source_roots()
+
+  def find_source_root(fp: str) -> str:
+    source_root = source_roots_object.safe_find_by_path(fp)
+    if source_root is not None:
+      return cast(str, source_root.path)
+    if source_root_config.options.unmatched == "fail":
+      raise NoSourceRootError(f"Could not find a source root for `{fp}`.")
+    # Otherwise, create a source root by using the parent directory.
+    return PurePath(fp).parent.as_posix()
+
+  files_grouped_by_source_root = {
+    source_root: tuple(files)
+    for source_root, files
+    in groupby(wrapped_snapshot.snapshot.files, key=find_source_root)
+  }
+  snapshot_subsets = await MultiGet(
+    Get[Snapshot](
+      SnapshotSubset(
+        directory_digest=wrapped_snapshot.snapshot.directory_digest,
+        globs=PathGlobs(files),
+      )
+    )
+    for files in files_grouped_by_source_root.values()
+  )
+  resulting_digests = await MultiGet(
+    Get[Digest](
+      DirectoryWithPrefixToStrip(directory_digest=snapshot.directory_digest, prefix=source_root)
+    )
+    for snapshot, source_root in zip(snapshot_subsets, files_grouped_by_source_root.keys())
+  )
+
+  merged_result = await Get[Digest](DirectoriesToMerge(resulting_digests))
+  resulting_snapshot = await Get[Snapshot](Digest, merged_result)
+  return SourceRootStrippedSources(snapshot=resulting_snapshot)
+
+
 @rule
 async def strip_source_roots_from_target(
-  hydrated_target: HydratedTarget, source_root_config: SourceRootConfig
+  hydrated_target: HydratedTarget,
 ) -> SourceRootStrippedSources:
   """Remove source roots from a target (depending upon the target type), e.g.
   `src/python/pants/util/strutil.py` -> `pants/util/strutil.py`.
@@ -37,26 +98,12 @@ async def strip_source_roots_from_target(
   if target_adaptor.type_alias == Files.alias():
     return SourceRootStrippedSources(snapshot=target_adaptor.sources.snapshot)
 
-  source_roots = source_root_config.get_source_roots()
-  source_root = source_roots.find_by_path(target_adaptor.address.spec_path)
-  # If we found no source root, use the target's dir.
-  # Note that when --source-unmatched is 'create' (the default) we'll never return None,
-  # but will return the target's dir. This check allows this code to work even if
-  # --source-unmatched is 'fail'.
-  source_root_path = source_root.path if source_root else target_adaptor.address.spec_path
-
-  resulting_digest = await Get[Digest](
-    DirectoryWithPrefixToStrip(
-      directory_digest=target_adaptor.sources.snapshot.directory_digest,
-      prefix=source_root_path,
-    )
-  )
-  resulting_snapshot = await Get[Snapshot](Digest, resulting_digest)
-  return SourceRootStrippedSources(snapshot=resulting_snapshot)
+  return await Get[SourceRootStrippedSources](SnapshotToStrip(target_adaptor.sources.snapshot))
 
 
 def rules():
   return [
+    strip_source_roots_from_snapshot,
     strip_source_roots_from_target,
     subsystem_rule(SourceRootConfig),
   ]
--- src/python/pants/rules/core/strip_source_roots_test.py
+++ src/python/pants/rules/core/strip_source_roots_test.py
@@ -1,15 +1,18 @@
 # Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from typing import Optional
+from functools import partial
+from typing import List, Optional, Union
 from unittest.mock import Mock
 
-from pants.build_graph.address import Address
+import pytest
+
 from pants.build_graph.files import Files
 from pants.engine.legacy.graph import HydratedTarget
 from pants.engine.rules import RootRule
+from pants.engine.scheduler import ExecutionError
 from pants.engine.selectors import Params
-from pants.rules.core.strip_source_roots import SourceRootStrippedSources
+from pants.rules.core.strip_source_roots import SnapshotToStrip, SourceRootStrippedSources
 from pants.rules.core.strip_source_roots import rules as strip_source_root_rules
 from pants.testutil.option.util import create_options_bootstrapper
 from pants.testutil.test_base import TestBase
@@ -18,38 +21,77 @@ from pants.testutil.test_base import TestBase
 class StripSourceRootsTests(TestBase):
   @classmethod
   def rules(cls):
-    return (*super().rules(), *strip_source_root_rules(), RootRule(HydratedTarget))
-
-  def assert_stripped_target(
-    self, *, original_path: str, expected_path: str, target_type_alias: Optional[str] = None,
-  ) -> None:
-    adaptor = Mock()
-    adaptor.sources = Mock()
-    adaptor.sources.snapshot = self.make_snapshot({original_path: "print('random python')"})
-    adaptor.address = Mock()
-    adaptor.address.spec_path = original_path
-    if target_type_alias:
-      adaptor.type_alias = target_type_alias
-    target = HydratedTarget(Address.parse("some/target:target"), adaptor, ())
-    stripped_sources = self.request_single_product(
-      SourceRootStrippedSources, Params(target, create_options_bootstrapper())
-    )
-    self.assertEqual(stripped_sources.snapshot.files, (expected_path,))
-
-  def test_strip_python_target(self):
-    self.assert_stripped_target(
-      original_path='src/python/pants/util/strutil.py', expected_path='pants/util/strutil.py',
+    return (
+      *super().rules(),
+      *strip_source_root_rules(),
+      RootRule(HydratedTarget),
+      RootRule(SnapshotToStrip),
     )
 
-  def test_strip_java_target(self):
-    self.assert_stripped_target(
-      original_path='src/java/some/path/to/something.java',
-      expected_path='some/path/to/something.java',
+  def get_stripped_files(
+    self, original: Union[SnapshotToStrip, HydratedTarget], *, args: Optional[List[str]] = None,
+  ) -> List[str]:
+    result = self.request_single_product(
+      SourceRootStrippedSources, Params(original, create_options_bootstrapper(args=args))
+    )
+    return sorted(result.snapshot.files)
+
+  def test_strip_snapshot(self) -> None:
+    original_paths = [
+      "src/python/project/example.py",
+      "src/java/com/project/example.java",
+      "tests/python/project_test/example.py",
+      "no-source-root/example.txt",
+    ]
+    stripped_paths = [
+      "project/example.py",
+      "com/project/example.java",
+      "project_test/example.py",
+      "example.txt",
+    ]
+
+    input_snapshot = self.make_snapshot({fp: "" for fp in original_paths})
+    get_stripped_files_for_snapshot = partial(
+      self.get_stripped_files, SnapshotToStrip(input_snapshot)
     )
 
-  def test_dont_strip_files_target(self):
-    self.assert_stripped_target(
-        original_path='src/python/pants/util/strutil.py',
-        expected_path='src/python/pants/util/strutil.py',
-        target_type_alias=Files.alias()
-    )
+    assert get_stripped_files_for_snapshot() == sorted(stripped_paths)
+
+    # Also test that we error when `--source-unmatched=fail`
+    with pytest.raises(ExecutionError) as exc:
+      get_stripped_files_for_snapshot(args=["--source-unmatched=fail"])
+    assert "NoSourceRootError: Could not find a source root for `no-source-root/example.txt`" in str(exc.value)
+
+  def test_strip_target(self) -> None:
+
+    def get_stripped_files_for_target(
+      *, source_paths: Optional[List[str]], type_alias: Optional[str] = None,
+    ) -> List[str]:
+      adaptor = Mock()
+      adaptor.type_alias = type_alias
+
+      if source_paths is None:
+        del adaptor.sources
+        return self.get_stripped_files(
+          HydratedTarget(address=Mock(), adaptor=adaptor, dependencies=()),
+        )
+
+      adaptor.sources = Mock()
+      adaptor.sources.snapshot = self.make_snapshot({fp: "" for fp in source_paths})
+
+      return self.get_stripped_files(
+        HydratedTarget(address=Mock(), adaptor=adaptor, dependencies=()),
+      )
+
+    # normal target
+    assert get_stripped_files_for_target(
+      source_paths=["src/python/project/f1.py", "src/python/project/f2.py"]
+    ) == sorted(["project/f1.py", "project/f2.py"])
+
+    # empty target
+    assert get_stripped_files_for_target(source_paths=None) == []
+
+    # files targets are not stripped
+    assert get_stripped_files_for_target(
+      source_paths=["src/python/project/f1.py"], type_alias=Files.alias(),
+    ) == ["src/python/project/f1.py"]
