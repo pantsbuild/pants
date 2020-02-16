2ced91b47150659362f511245625e2c8042cd20d
Eric Arellano <ericarellano@me.com>
2020-02-15 20:18:57 -0700

    Fix tests

--- tests/python/pants_test/backend/python/tasks/test_isort_run_integration.py
+++ tests/python/pants_test/backend/python/tasks/test_isort_run_integration.py
@@ -1,19 +1,22 @@
 # Copyright 2016 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.backend.python.tasks.isort_run import IsortRun
+from pants.backend.python.lint.isort.isort_run import IsortRun
 from pants.testutil.pants_run_integration_test import PantsRunIntegrationTest, ensure_daemon
 
 
 class IsortRunIntegrationTest(PantsRunIntegrationTest):
 
+  def hermetic(cls):
+    return True
+
   @ensure_daemon
   def test_isort_no_python_sources_should_noop(self):
     command = [
       '-ldebug',
+      "--isort-args='--check-only'",
       'fmt.isort',
       'testprojects/tests/java/org/pantsbuild/testproject/dummies/::',
-      "--isort-args='--check-only'",
     ]
     pants_run = self.run_pants(command=command)
     self.assert_success(pants_run)
