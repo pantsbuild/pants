ab8873ea5dfa49c86d9df899a4c504ae9a77f455
Eric Arellano <ericarellano@me.com>
2020-02-20 20:26:05 -0700

    Fix bad file
    I think isort and black were overwriting each other in a way that was not compatible. First black would run, then isort would overwrite Black's changes

--- examples/src/python/example/tensorflow_custom_op/zero_out_custom_op.py
+++ examples/src/python/example/tensorflow_custom_op/zero_out_custom_op.py
@@ -1,9 +1,10 @@
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import tensorflow as tf
 # TODO: It would be great if we could maintain the example.tensorflow_custom_op package prefix for
 # this python_dist()!
+
+import tensorflow as tf
 from wrap_lib.wrap_zero_out_op import zero_out_op_lib_path
 
 
