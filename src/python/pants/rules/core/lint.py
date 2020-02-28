12615a3ba19f67ef5109c381acfd6c1993625aa2
Eric Arellano <ericarellano@me.com>
2020-02-28 14:29:55 -0700

    Add `--no-lint-per-target-caching` to run V2 linters with batched targets

--- src/python/pants/rules/core/lint.py
+++ src/python/pants/rules/core/lint.py
@@ -56,6 +56,29 @@ class LintOptions(GoalSubsystem):
 
     required_union_implementations = (Linter,)
 
+    @classmethod
+    def register_options(cls, register) -> None:
+        super().register_options(register)
+        register(
+            "--per-target-caching",
+            advanced=True,
+            type=bool,
+            default=False,
+            help=(
+                "Rather than running all targets in a single batch, run each target as a "
+                "separate process. Why do this? You'll get many more cache hits. Additionally, for "
+                "Python users, if you have some targets that only work with Python 2 and some that "
+                "only work with Python 3, `--per-target-caching` will allow you to use the right "
+                "interpreter for each target. Why not do this? Linters both have substantial "
+                "startup overhead and are cheap to add one additional file to the run. On a cold "
+                "cache, it is much faster to use `--no-per-target-caching`. We only recommend "
+                "using `--per-target-caching` if you "
+                "are using a remote cache, or if you have some Python 2-only targets and "
+                "some Python 3-only targets, or if you have benchmarked that this option will be "
+                "faster than `--no-per-target-caching` for your use case."
+            ),
+        )
+
 
 class Lint(Goal):
     subsystem_cls = LintOptions
@@ -65,20 +88,37 @@ class Lint(Goal):
 async def lint(
     console: Console,
     targets_with_origins: HydratedTargetsWithOrigins,
+    options: LintOptions,
     union_membership: UnionMembership,
 ) -> Lint:
-    adaptors_with_origins = [
+    adaptors_with_origins = tuple(
         TargetAdaptorWithOrigin.create(target_with_origin.target.adaptor, target_with_origin.origin)
         for target_with_origin in targets_with_origins
-    ]
+        if target_with_origin.target.adaptor.has_sources()
+    )
 
     linters: Iterable[Type[Linter]] = union_membership.union_rules[Linter]
-    results = await MultiGet(
-        Get[LintResult](Linter, linter((adaptor_with_origin,)))
-        for adaptor_with_origin in adaptors_with_origins
-        for linter in linters
-        if adaptor_with_origin.adaptor.has_sources() and linter.is_valid_target(adaptor_with_origin)
-    )
+    if options.values.per_target_caching:
+        results = await MultiGet(
+            Get[LintResult](Linter, linter((adaptor_with_origin,)))
+            for adaptor_with_origin in adaptors_with_origins
+            for linter in linters
+            if linter.is_valid_target(adaptor_with_origin)
+        )
+    else:
+        linters_with_valid_targets = {
+            linter: tuple(
+                adaptor_with_origin
+                for adaptor_with_origin in adaptors_with_origins
+                if linter.is_valid_target(adaptor_with_origin)
+            )
+            for linter in linters
+        }
+        results = await MultiGet(
+            Get[LintResult](Linter, linter(valid_targets))
+            for linter, valid_targets in linters_with_valid_targets.items()
+            if valid_targets
+        )
 
     if not results:
         return Lint(exit_code=0)
