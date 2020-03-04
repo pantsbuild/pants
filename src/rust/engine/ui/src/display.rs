pantsbuild/pants

commit 2cdcd8ea1e0ae499028ce2b0d7264f416438305d

Handle Ctrl-C in the V2 UI (#9043)
## Problem

The v2 console UI sets the terminal to raw mode, causing it to no longer detect Ctrl-C as a sign that the user is trying to interrupt the process.

## Solution

This commit re-architects the render() method on EngineDisplay to poll for keyboard events every time it runs, and return a sigil value if it detects the key combination Ctrl-C. The scheduler's execute method passes up this potential error value to the scheduler_execute FFI function, which in turn signals that a Ctrl-C has been pressed by returning a null pointer to Python. In Python, the code checks for a null pointer and raises a Python KeyboardInterrupt if one is found.

## Result

Hitting Ctrl-C should now have the same effect as it did in V1, i.e. raising a Python KeyboardInterrupt and quitting the program after cleanup.

src/rust/engine/ui/src/display.rs
@@ -28,12 +28,14 @@
 use termion;
 
 use std::collections::{BTreeMap, VecDeque};
+use std::io::Read;
 use std::io::Write;
 use std::io::{stdout, Result, Stdout};
 
 use termion::raw::IntoRawMode;
 use termion::raw::RawTerminal;
 use termion::screen::AlternateScreen;
+use termion::{async_stdin, AsyncReader};
 use termion::{clear, color, cursor};
 use unicode_segmentation::UnicodeSegmentation;
 
@@ -55,6 +57,11 @@ enum PrintableMsgOutput {
   Stderr,
 }
 
+pub enum KeyboardCommand {
+  None,
+  CtrlC,
+}
+
 pub struct EngineDisplay {
   sigil: char,
   divider: String,
@@ -65,6 +72,7 @@ pub struct EngineDisplay {
   printable_msgs: VecDeque<PrintableMsg>,
   cursor_start: (u16, u16),
   terminal_size: (u16, u16),
+  async_stdin: AsyncReader,
 }
 
 // TODO: Prescribe a threading/polling strategy for callers - or implement a built-in one.
@@ -95,6 +103,7 @@ impl EngineDisplay {
       // as we've done here is the safest way to avoid terminal oddness.
       cursor_start: (1, 1),
       terminal_size: EngineDisplay::get_size(),
+      async_stdin: async_stdin(),
     }
   }
 
@@ -266,13 +275,46 @@ impl EngineDisplay {
   }
 
   // Paints one screen of rendering.
-  pub fn render(&mut self) {
+  pub fn render(&mut self) -> std::result::Result<KeyboardCommand, String> {
     self.set_size();
     self.clear();
     let max_log_rows = self.get_max_log_rows();
     let rendered_count = self.render_logs(max_log_rows);
     self.render_actions(rendered_count);
-    self.flush().expect("could not flush terminal!");
+    if let Err(err) = self.flush() {
+      return Err(format!("Could not flush terminal: {}", err));
+    }
+    self.handle_stdin()
+  }
+
+  fn handle_stdin(&mut self) -> std::result::Result<KeyboardCommand, String> {
+    use termion::event::{parse_event, Event, Key};
+    //This buffer must have non-zero size because termion's `read` implementation for
+    //AsyncReader will return early without doing anything if it is of length 0.
+    //(See https://docs.rs/termion/1.5.4/src/termion/async.rs.html#69 )
+    let mut buf: [u8; 32] = [0; 32];
+
+    match self.async_stdin.read(&mut buf) {
+      Ok(0) => Ok(KeyboardCommand::None),
+      Ok(_) => {
+        let initial_byte: u8 = buf[0];
+        let mut iter = buf[1..].iter().map(|byte| Ok(*byte));
+        // TODO: calling `parse_event` in this way means that we will potentially miss keyboard
+        // events - a Ctrl-C event has to be the very first event in each `render` frame, or it
+        // won't be handled. In practice, the refresh interval is 100 ms, which is fast enough
+        // on human timescales that hitting Ctrl-C while the program is running will wind up
+        // at the beginning of some frame.  Note that internally termion uses this function
+        // in the context of the `next()` function of an Iterator:
+        // https://github.com/redox-os/termion/blob/master/src/input.rs .
+        let event_or_err = parse_event(initial_byte, &mut iter);
+        match event_or_err {
+          Ok(Event::Key(Key::Ctrl('c'))) => Ok(KeyboardCommand::CtrlC),
+          Err(err) => Err(format!("EngineDisplay keyboard event error: {}", err)),
+          _ => Ok(KeyboardCommand::None),
+        }
+      }
+      Err(err) => Err(format!("EngineDisplay stdin error: {}", err)),
+    }
   }
 
   // Starts the EngineDisplay at the current cursor position.
@@ -338,7 +380,9 @@ impl EngineDisplay {
   // Initiates one last screen render, then terminates the EngineDisplay and returns the cursor
   // to a static position, then prints all buffered stdout/stderr output.
   pub fn finish(&mut self) {
-    self.render();
+    //We don't care about handling the output from render() here, since we're about to shut down
+    //the EngineDisplay anyway.
+    let _ = self.render();
     {
       self.terminal = Console::Uninitialized; //This forces the AlternateScreen to drop, restoring the original terminal state.
     }

src/python/pants/engine/scheduler.py
@@ -271,6 +271,9 @@ class Scheduler:
 
   def _run_and_return_roots(self, session, execution_request):
     raw_roots = self._native.lib.scheduler_execute(self._scheduler, session, execution_request)
+    if raw_roots == self._native.ffi.NULL:
+      raise KeyboardInterrupt
+
     remaining_runtime_exceptions_to_capture = list(self._native.consume_cffi_extern_method_runtime_exceptions())
     try:
       roots = []

src/rust/engine/engine_cffi/src/lib.rs
@@ -40,8 +40,8 @@ mod cffi_externs;
 
 use engine::externs::*;
 use engine::{
-  externs, nodes, Core, ExecutionRequest, Function, Handle, Key, Params, RootResult, Rule,
-  Scheduler, Session, Tasks, TypeId, Types, Value,
+  externs, nodes, Core, ExecutionRequest, ExecutionTermination, Function, Handle, Key, Params,
+  RootResult, Rule, Scheduler, Session, Tasks, TypeId, Types, Value,
 };
 use futures01::{future, Future};
 use hashing::{Digest, EMPTY_DIGEST};
@@ -500,9 +500,13 @@ pub extern "C" fn scheduler_execute(
   with_scheduler(scheduler_ptr, |scheduler| {
     with_execution_request(execution_request_ptr, |execution_request| {
       with_session(session_ptr, |session| {
-        Box::into_raw(RawNodes::create(
-          scheduler.execute(execution_request, session),
-        ))
+        match scheduler.execute(execution_request, session) {
+          Ok(raw_results) => Box::into_raw(RawNodes::create(raw_results)),
+          //TODO: Passing a raw null pointer to Python is a less-than-ideal way
+          //of noting an error condition. When we have a better way to send complicated
+          //error-signaling values over the FFI boundary, we should revisit this.
+          Err(ExecutionTermination::KeyboardInterrupt) => std::ptr::null(),
+        }
       })
     })
   })

src/rust/engine/src/lib.rs
@@ -44,6 +44,8 @@ mod types;
 pub use crate::context::Core;
 pub use crate::core::{Function, Key, Params, TypeId, Value};
 pub use crate::handles::Handle;
-pub use crate::scheduler::{ExecutionRequest, RootResult, Scheduler, Session};
+pub use crate::scheduler::{
+  ExecutionRequest, ExecutionTermination, RootResult, Scheduler, Session,
+};
 pub use crate::tasks::{Rule, Tasks};
 pub use crate::types::Types;

src/rust/engine/src/scheduler.rs
@@ -18,8 +18,12 @@ use indexmap::IndexMap;
 use log::{debug, info, warn};
 use logging::logger::LOGGER;
 use parking_lot::Mutex;
-use ui::EngineDisplay;
+use ui::{EngineDisplay, KeyboardCommand};
 use workunit_store::WorkUnitStore;
+
+pub enum ExecutionTermination {
+  KeyboardInterrupt,
+}
 
 ///
 /// A Session represents a related series of requests (generally: one run of the pants CLI) on an
@@ -332,7 +336,11 @@ impl Scheduler {
   ///
   /// Compute the results for roots in the given request.
   ///
-  pub fn execute(&self, request: &ExecutionRequest, session: &Session) -> Vec<RootResult> {
+  pub fn execute(
+    &self,
+    request: &ExecutionRequest,
+    session: &Session,
+  ) -> Result<Vec<RootResult>, ExecutionTermination> {
     // Bootstrap tasks for the roots, and then wait for all of them.
     debug!("Launching {} roots.", request.roots.len());
 
@@ -359,7 +367,7 @@ impl Scheduler {
     let mut tasks_to_display = IndexMap::new();
     let refresh_interval = Duration::from_millis(100);
 
-    match session.maybe_display() {
+    Ok(match session.maybe_display() {
       Some(display) => {
         {
           let mut display = display.lock();
@@ -371,12 +379,24 @@ impl Scheduler {
           if let Ok(res) = receiver.recv_timeout(refresh_interval) {
             break res;
           } else {
-            Scheduler::display_ongoing_tasks(
+            let render_result = Scheduler::display_ongoing_tasks(
               &self.core.graph,
               &roots,
               display,
               &mut tasks_to_display,
             );
+            match render_result {
+              Err(e) => warn!("{}", e),
+              Ok(KeyboardCommand::CtrlC) => {
+                info!("Exiting early in response to Ctrl-C");
+                {
+                  let mut display = display.lock();
+                  display.finish();
+                }
+                return Err(ExecutionTermination::KeyboardInterrupt);
+              }
+              Ok(KeyboardCommand::None) => (),
+            };
           }
         };
         LOGGER.deregister_engine_display(unique_handle);
@@ -391,7 +411,7 @@ impl Scheduler {
           break res;
         }
       },
-    }
+    })
   }
 
   fn display_ongoing_tasks(
@@ -399,7 +419,7 @@ impl Scheduler {
     roots: &[NodeKey],
     display: &Mutex<EngineDisplay>,
     tasks_to_display: &mut IndexMap<String, Duration>,
-  ) {
+  ) -> Result<KeyboardCommand, String> {
     // Update the graph. To do that, we iterate over heavy hitters.
 
     let worker_count = {
@@ -439,7 +459,7 @@ impl Scheduler {
     for i in tasks_to_display.len()..worker_count {
       d.update(i.to_string(), "".to_string());
     }
-    d.render();
+    d.render()
   }
 }
 

src/rust/engine/ui/Cargo.toml
@@ -13,5 +13,5 @@ path = "src/main.rs"
 
 [dependencies]
 rand = "0.5.1"
-termion = "1.5.4"
+termion = "1.5.5"
 unicode-segmentation = "1.6.0"

src/rust/engine/ui/src/main.rs
@@ -76,15 +76,15 @@ fn main() {
 
   for worker_id in worker_ids.clone() {
     display.add_worker(worker_id);
-    display.render();
+    let _ = display.render();
     thread::sleep(Duration::from_millis(63));
   }
 
-  display.render();
+  let _ = display.render();
   thread::sleep(Duration::from_secs(1));
 
   while !done {
-    display.render();
+    let _ = display.render();
     thread::sleep(Duration::from_millis(55));
 
     gen_display_work(

commit cdb30215ae266e1228240cdf3a7f677a66e70b54

Disable and re-enable async stdin when UI is disabled (#9222)
### Problem

When a user does a pants operation that involves disabling the v2 console UI (such as the `repl` goal or running `test` with the `--debug` flag), they were seeing some bugs with keyboard input. This was caused by the change in https://github.com/pantsbuild/pants/pull/9043 that introduced the termion `async_stdin` abstraction to read console stdout, which spawns a thread to poll from stdin. When we suspend the console UI, this thread was still polling from stdin, and interfering with the ability of whatever subprocess pants was running to also read from stdin, which is why were were bypassing the console UI to begin with.

### Solution

Make the `async_stdin` value on the `EngineDisplay` struct optional, and then set it to `None` on `suspend` and recreate it on `unsuspend`. This results in dropping an instance of `AsyncStdin` when pants suspends the terminal, which kills the background thread reading stdin and removes the source of interference for the subprocess. When we unsuspend, we re-create the async reader just as we previously did when we first initialized the console UI.

### Result

This fixes a currently-existing bug with erratic live keyboard input to a pants subprocess when the v2 UI is activated.

src/rust/engine/ui/src/display.rs
@@ -72,7 +72,8 @@ pub struct EngineDisplay {
   printable_msgs: VecDeque<PrintableMsg>,
   cursor_start: (u16, u16),
   terminal_size: (u16, u16),
-  async_stdin: AsyncReader,
+  async_stdin: Option<AsyncReader>,
+  suspended: bool,
 }
 
 // TODO: Prescribe a threading/polling strategy for callers - or implement a built-in one.
@@ -103,7 +104,8 @@ impl EngineDisplay {
       // as we've done here is the safest way to avoid terminal oddness.
       cursor_start: (1, 1),
       terminal_size: EngineDisplay::get_size(),
-      async_stdin: async_stdin(),
+      async_stdin: Some(async_stdin()),
+      suspended: false,
     }
   }
 
@@ -276,6 +278,9 @@ impl EngineDisplay {
 
   // Paints one screen of rendering.
   pub fn render(&mut self) -> std::result::Result<KeyboardCommand, String> {
+    if self.suspended {
+      return Ok(KeyboardCommand::None);
+    }
     self.set_size();
     self.clear();
     let max_log_rows = self.get_max_log_rows();
@@ -294,9 +299,9 @@ impl EngineDisplay {
     //(See https://docs.rs/termion/1.5.4/src/termion/async.rs.html#69 )
     let mut buf: [u8; 32] = [0; 32];
 
-    match self.async_stdin.read(&mut buf) {
-      Ok(0) => Ok(KeyboardCommand::None),
-      Ok(_) => {
+    match self.async_stdin.as_mut().map(|s| s.read(&mut buf)) {
+      Some(Ok(0)) | None => Ok(KeyboardCommand::None),
+      Some(Ok(_)) => {
         let initial_byte: u8 = buf[0];
         let mut iter = buf[1..].iter().map(|byte| Ok(*byte));
         // TODO: calling `parse_event` in this way means that we will potentially miss keyboard
@@ -313,7 +318,7 @@ impl EngineDisplay {
           _ => Ok(KeyboardCommand::None),
         }
       }
-      Err(err) => Err(format!("EngineDisplay stdin error: {}", err)),
+      Some(Err(err)) => Err(format!("EngineDisplay stdin error: {}", err)),
     }
   }
 
@@ -365,6 +370,8 @@ impl EngineDisplay {
   pub fn suspend(&mut self) {
     {
       self.terminal = Console::Uninitialized;
+      self.suspended = true;
+      self.async_stdin = None;
     }
     println!("{}", termion::cursor::Show);
   }
@@ -375,6 +382,8 @@ impl EngineDisplay {
       Ok(t) => Console::Terminal(t),
       Err(_) => Console::Pipe(stdout()),
     };
+    self.suspended = false;
+    self.async_stdin = Some(async_stdin());
   }
 
   // Initiates one last screen render, then terminates the EngineDisplay and returns the cursor