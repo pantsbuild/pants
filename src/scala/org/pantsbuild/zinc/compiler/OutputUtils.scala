3eaa03a31258c82223582f34c165db1927f20390
Pierre Chevalier <pchevalier@twitter.com>
2020-03-06 14:58:37 +0000

    Format zinc compiler with scalafmt (#9227)
    ### Problem
    I am preparing work in the scala code for the zinc compiler wrapper.
    Currently, the formatting is inconsistent and I prefer working in a formatted codebase so I don't have to mix formatting changes with semantic changes.
    
    ### Solution
    Format the zinc code with
    
    ./pants fmt src/scala/org/pantsbuild/zinc/compiler/Main.scala --scalafmt-skip=False
    Note: I'm making this its own PR to simplify reverts of the semantic PRs if needed.
    
    ### Result
    The zinc code is formatted and I can follow up with purely semantic changes.

--- src/scala/org/pantsbuild/zinc/compiler/OutputUtils.scala
+++ src/scala/org/pantsbuild/zinc/compiler/OutputUtils.scala
@@ -1,7 +1,6 @@
 /**
- * Copyright (C) 2012 Typesafe, Inc. <http://www.typesafe.com>
- */
-
+  * Copyright (C) 2012 Typesafe, Inc. <http://www.typesafe.com>
+  */
 package org.pantsbuild.zinc.compiler
 
 import java.io.File
@@ -14,21 +13,24 @@ import scala.collection.mutable
 object OutputUtils {
 
   /**
-   * Sort the contents of the `dir` in lexicographic order.
-   *
-   * @param dir File handle containing the contents to sort
-   * @return sorted set of all paths within the `dir`
-   */
+    * Sort the contents of the `dir` in lexicographic order.
+    *
+    * @param dir File handle containing the contents to sort
+    * @return sorted set of all paths within the `dir`
+    */
   def sort(dir: File): mutable.TreeSet[Path] = {
     val sorted = new mutable.TreeSet[Path]()
 
     val fileSortVisitor = new SimpleFileVisitor[Path]() {
-      override def preVisitDirectory(path: Path, attrs: BasicFileAttributes): FileVisitResult = {
+      override def preVisitDirectory(
+          path: Path,
+          attrs: BasicFileAttributes): FileVisitResult = {
         sorted.add(path)
         FileVisitResult.CONTINUE
       }
 
-      override def visitFile(path: Path, attrs: BasicFileAttributes): FileVisitResult = {
+      override def visitFile(path: Path,
+                             attrs: BasicFileAttributes): FileVisitResult = {
         sorted.add(path)
         FileVisitResult.CONTINUE
       }
@@ -39,18 +41,23 @@ object OutputUtils {
   }
 
   def relativize(base: String, path: Path): String = {
-    new File(base.toString).toURI().relativize(new File(path.toString).toURI()).getPath()
+    new File(base.toString)
+      .toURI()
+      .relativize(new File(path.toString).toURI())
+      .getPath()
   }
 
   /**
-   * Create a JAR from the file and directory paths provided.
-   *
-   * @param paths set of all paths to be added to the JAR
-   * @param outputJarPath Absolute Path to the output JAR being created
-   * @param jarEntryTime time to be set for each JAR entry
-   */
-  def createJar(
-    base: String, paths: mutable.TreeSet[Path], outputJarPath: Path, jarEntryTime: Long) {
+    * Create a JAR from the file and directory paths provided.
+    *
+    * @param paths set of all paths to be added to the JAR
+    * @param outputJarPath Absolute Path to the output JAR being created
+    * @param jarEntryTime time to be set for each JAR entry
+    */
+  def createJar(base: String,
+                paths: mutable.TreeSet[Path],
+                outputJarPath: Path,
+                jarEntryTime: Long) {
 
     val target = new JarOutputStream(Files.newOutputStream(outputJarPath))
 
@@ -64,7 +71,8 @@ object OutputUtils {
 
     def addToJar(source: Path, entryName: String) {
       if (source.toFile.isDirectory) {
-        target.putNextEntry(jarEntry(if (entryName.endsWith("/")) entryName else entryName + '/'))
+        target.putNextEntry(
+          jarEntry(if (entryName.endsWith("/")) entryName else entryName + '/'))
       } else {
         target.putNextEntry(jarEntry(entryName))
         Files.copy(source, target)
@@ -72,35 +80,38 @@ object OutputUtils {
       target.closeEntry()
     }
 
-    for (
-      path <- paths;
-      relativePath = relativize(base, path)
-      if relativePath.nonEmpty
-    ) {
+    for (path <- paths;
+         relativePath = relativize(base, path)
+         if relativePath.nonEmpty) {
       addToJar(path, relativePath)
     }
     target.close()
   }
 
   /**
-   * Jar the contents of output classes (settings.classesDirectory) and copy to settings.outputJar
-   *
-   */
-  def createClassesJar(classesDirectory: File, outputJarPath: Path, jarCreationTime: Long) = {
+    * Jar the contents of output classes (settings.classesDirectory) and copy to settings.outputJar
+    *
+    */
+  def createClassesJar(classesDirectory: File,
+                       outputJarPath: Path,
+                       jarCreationTime: Long) = {
 
     // Sort the contents of the classesDirectory for deterministic jar creation
     val sortedClasses = sort(classesDirectory)
 
-    createJar(classesDirectory.toString, sortedClasses, outputJarPath, jarCreationTime)
+    createJar(classesDirectory.toString,
+              sortedClasses,
+              outputJarPath,
+              jarCreationTime)
   }
 
   /**
-   * Determines if a file exists in a JAR provided.
-   *
-   * @param jarPath Absolute Path to the JAR being inspected
-   * @param fileName Name of the file, the existence of which is to be inspected
-   * @return
-   */
+    * Determines if a file exists in a JAR provided.
+    *
+    * @param jarPath Absolute Path to the JAR being inspected
+    * @param fileName Name of the file, the existence of which is to be inspected
+    * @return
+    */
   def existsClass(jarPath: Path, fileName: String): Boolean = {
     var jis: JarInputStream = null
     var found = false
