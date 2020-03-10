55ba5c204d2add720dea373b76f8997d6e3be9c6
Tansy Arron-Walker <tansy.arron@gmail.com>
2020-03-10 19:52:24 -0400

    Missed one.

--- contrib/scrooge/tests/python/pants_test/contrib/scrooge/tasks/test_thrift_linter.py
+++ contrib/scrooge/tests/python/pants_test/contrib/scrooge/tasks/test_thrift_linter.py
@@ -33,7 +33,10 @@ class ThriftLinterTest(TaskTestBase):
             )
 
         thrift_target = self.create_library(
-            "src/thrift/tweet", "java_thrift_library", "a", ["A.thrift"]
+            path="src/thrift/tweet",
+            target_type="java_thrift_library",
+            name="a",
+            sources=["A.thrift"],
         )
         task = self.create_task(self.context(target_roots=thrift_target))
         self._prepare_mocks(task)
@@ -66,9 +69,18 @@ class ThriftLinterTest(TaskTestBase):
                 self.context().options.for_global_scope()
             )
 
-        self.create_library("src/thrift/tweet", "java_thrift_library", "a", ["A.thrift"])
+        self.create_library(
+            path="src/thrift/tweet",
+            target_type="java_thrift_library",
+            name="a",
+            sources=["A.thrift"],
+        )
         target_b = self.create_library(
-            "src/thrift/tweet", "java_thrift_library", "b", ["B.thrift"], dependencies=[":a"]
+            path="src/thrift/tweet",
+            target_type="java_thrift_library",
+            name="b",
+            sources=["B.thrift"],
+            dependencies=[":a"],
         )
         task = self.create_task(self.context(target_roots=target_b))
         self._prepare_mocks(task)
