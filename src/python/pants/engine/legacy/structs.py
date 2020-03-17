992a4ca82863be15efa48059abb7ad2f0231c599
Benjy Weinberger <benjyw@gmail.com>
2020-03-16 19:24:47 -0700

    A newtype for the requirements.txt file dep. (#9307)
    We don't want to invalidate sources based on arbitrary
    changes to requirements.txt, but we do need to depend on
    it so we know to recompute requirements.
    
    Previously this was achieved by synthesizing a files()
    target and adding a dep on it, in the python_requirements macro.
    However when we gather sources to operate on we have to include
    all files(), meaning we were invalidating the sources even on
    a whitespace change to requirements.txt.
    
    This change adds a new, internal, target type specially for
    the synthetic requirements.txt change, so that we can maintain
    a dependency, but rules can still filter out the actual file.
    
    Context: https://github.com/pantsbuild/pants/pull/6405/

--- src/python/pants/engine/legacy/structs.py
+++ src/python/pants/engine/legacy/structs.py
@@ -280,6 +280,15 @@ class FilesAdaptor(TargetAdaptor):
     pass
 
 
+# The python_requirements macro generates python_requirement_library targets and makes them
+# depend on a _python_requirements_file() target, so that pantsd knows to invalidate correctly
+# when the requirements.txt file changes.  We don't want to use a regular files() target for
+# this, as we don't want to consider the requirements.txt a source for the purpose of building
+# pexes (e.g., we don't want whitespace changes to requirements.txt to invalidate the sources pex).
+class PythonRequirementsFileAdaptor(TargetAdaptor):
+    pass
+
+
 class RemoteSourcesAdaptor(TargetAdaptor):
     def __init__(self, dest=None, **kwargs):
         """
