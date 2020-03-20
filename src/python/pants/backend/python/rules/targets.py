9bf971d752390826b52d2220429fc07f5d30d247
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-03-20 08:40:49 -0700

    Add runtime type checking to Fields (#9353)
    ## Problem
    
    Even when we put type hints on `Field`s for what we expect their `raw_value` to be, we have no way of actually enforcing this beyond runtime type checks. For example, we expect the value for `ZipSafe` to be `Optional[bool]`, but there is nothing stopping users from doing this:
    
    ```python
    python_binary(
      zip_safe=123,
    )
    ```
    
    Without runtime type checks, this means that our type hints would now be lies and we risk getting weird `TypeError`s that are hard to debug when the value is different than what we expected.
    
    ## Solution
    
    Add runtime type checking to each of the `PrimitiveField` helpers, e.g. `BoolField` and `StringField`. This means that almost none of the actual concrete target types had to change - they get the validation automatically thanks to inheritance.
    
    ### Stop using `ClassVar` when unnecessary
    
    This also stops annotating all `Field` and `Target` subclasses with `ClassVar`, which John pointed out is redundant. Turns out, it indeed is redundant. See https://docs.python.org/3/library/dataclasses.html#class-variables for how class properties work with dataclasses. This greatly reduces boilerplate.
    
    ### Mark `PrimitiveField` subclasses with `ABCMeta`
    
    This ensures that no one ever tries to instantiate `StringField`, `BoolField`, etc. These are only superclasses for boilerplate reduction.
    
    ### Fix `Files` target registration
    
    It was missing an `alias` and its `register.py` still used `targets()`, rather than `targets2()` (see https://github.com/pantsbuild/pants/pull/9334).
    
    ## Result
    
    Invalid boolean field:
    
    > ERROR: The 'zip_safe' field in target build-support/bin:check_banned_imports must be a boolean, but was `123` with type `int`.
    
    Invalid string sequence field:
    
    > ERROR: The 'tags' field in target build-support/bin:check_banned_imports must be an iterable of strings (e.g. a list of strings), but was `{1, 2}` with type `set`.
    
    Non-python `sources`:
    
    > ERROR: The 'sources' field in target build-support/bin:check_banned_imports must only contain Python files that end in `.py`, but it had these non-Python files: ['build-support/bin/check_clippy.sh'].
    
    Binary target with > 1 source file:
    
    > ERROR: The 'sources' field in target build-support/bin:check_banned_imports must only have 0 or 1 files because it is a binary target, but it has 2 sources: ['build-support/bin/check_banned_imports.py', 'build-support/bin/check_header.py'].
    >
    > To use any additional files, put them in a `python_library` and then add that `python_library` as a `dependency`.

--- src/python/pants/backend/python/rules/targets.py
+++ src/python/pants/backend/python/rules/targets.py
@@ -2,21 +2,21 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from pathlib import PurePath
-from typing import Any, ClassVar, Optional
+from typing import Optional
 
-from pants.base.exceptions import TargetDefinitionException
 from pants.build_graph.address import Address
 from pants.engine.fs import Snapshot
 from pants.engine.objects import union
 from pants.engine.target import (
     COMMON_TARGET_FIELDS,
     BoolField,
-    ImmutableValue,
-    PrimitiveField,
+    IntField,
+    InvalidFieldException,
     Sources,
     StringField,
     StringOrStringSequenceField,
     Target,
+    UnimplementedField,
 )
 
 
@@ -25,30 +25,29 @@ class PythonSources(Sources):
     def validate_snapshot(self, snapshot: Snapshot) -> None:
         non_python_files = [fp for fp in snapshot.files if not PurePath(fp).suffix == ".py"]
         if non_python_files:
-            raise TargetDefinitionException(
-                self.address,
-                "All files in the `sources` field must be Python files (i.e., end in `.py`): "
-                f"{non_python_files}.",
+            raise InvalidFieldException(
+                f"The {repr(self.alias)} field in target {self.address} must only contain Python "
+                f"files that end in `.py`, but it had these non-Python files: {non_python_files}."
             )
 
 
 class PythonLibrarySources(PythonSources):
-    default_globs: ClassVar = ("*.py", "!test_*.py", "!*_test.py", "!conftest.py")
+    default_globs = ("*.py", "!test_*.py", "!*_test.py", "!conftest.py")
 
 
 class PythonTestsSources(PythonSources):
-    default_globs: ClassVar = ("test_*.py", "*_test.py", "conftest.py")
+    default_globs = ("test_*.py", "*_test.py", "conftest.py")
 
 
 class PythonBinarySources(PythonSources):
     def validate_snapshot(self, snapshot: Snapshot) -> None:
         super().validate_snapshot(snapshot)
         if len(snapshot.files) not in [0, 1]:
-            raise TargetDefinitionException(
-                self.address,
-                "Binary targets must have only 0 or 1 source files. Any additional files should "
-                f"be put in a `python_library` which is added to `dependencies`. This target "
-                f"has {len(snapshot.files)} sources: {snapshot.files}.",
+            raise InvalidFieldException(
+                f"The {repr(self.alias)} field in target {self.address} must only have 0 or 1 "
+                f"files because it is a binary target, but it has {len(snapshot.files)} sources: "
+                f"{sorted(snapshot.files)}.\n\nTo use any additional files, put them in a "
+                "`python_library` and then add that `python_library` as a `dependency`."
             )
 
 
@@ -60,36 +59,33 @@ class Compatibility(StringOrStringSequenceField):
     As a shortcut, you can leave off `CPython`, e.g. `>=2.7` will be expanded to `CPython>=2.7`.
     """
 
-    alias: ClassVar = "compatibility"
+    alias = "compatibility"
 
 
-# TODO: Deal with the `provides` field. This will at least allow us to correctly parse the field,
-#  rather than throwing an error when encountering it.
-class Provides(PrimitiveField):
-    alias: ClassVar = "provides"
-
-    def hydrate(self, raw_value: Optional[Any], *, address: Address) -> ImmutableValue:
-        return raw_value
+class Provides(UnimplementedField):
+    alias = "provides"
 
 
 class Coverage(StringOrStringSequenceField):
     """The module(s) whose coverage should be generated, e.g. `['pants.util']`."""
 
-    alias: ClassVar = "coverage"
+    alias = "coverage"
 
 
-class Timeout(PrimitiveField):
+class Timeout(IntField):
     """A timeout (in seconds) which covers the total runtime of all tests in this target.
 
     This only applies if `--pytest-timeouts` is set to True.
     """
 
-    alias: ClassVar = "timeout"
+    alias = "timeout"
 
     def hydrate(self, raw_value: Optional[int], *, address: Address) -> Optional[int]:
-        if raw_value is not None and raw_value <= 0:
-            raise TargetDefinitionException(
-                address, f"The `timeout` field must be > 1. Was {raw_value}."
+        hydrated_value = super().hydrate(raw_value, address=address)
+        if hydrated_value is not None and hydrated_value < 1:
+            raise InvalidFieldException(
+                f"The value for the `timeout` field in target {address} must be >= 1, but was "
+                f"{raw_value}."
             )
         return raw_value
 
@@ -101,59 +97,59 @@ class EntryPoint(StringField):
     `__main__` function.
     """
 
-    alias: ClassVar = "entry_point"
+    alias = "entry_point"
 
 
 class Platforms(StringOrStringSequenceField):
     """Extra platforms to target when building a Python binary."""
 
-    alias: ClassVar = "platforms"
+    alias = "platforms"
 
 
 class PexInheritPath(BoolField):
     """Whether to inherit the `sys.path` of the environment that the binary runs in or not."""
 
-    alias: ClassVar = "inherit_path"
-    default: ClassVar = False
+    alias = "inherit_path"
+    default = False
 
 
 class PexZipSafe(BoolField):
     """Whether or not this binary is safe to run in compacted (zip-file) form."""
 
-    alias: ClassVar = "zip_safe"
-    default: ClassVar = True
+    alias = "zip_safe"
+    default = True
 
 
 class PexAlwaysWriteCache(BoolField):
     """Whether Pex should always write the .deps cache of the Pex file to disk or not."""
 
-    alias: ClassVar = "always_write_cache"
-    default: ClassVar = False
+    alias = "always_write_cache"
+    default = False
 
 
 class PexRepositories(StringOrStringSequenceField):
     """Repositories for Pex to query for dependencies."""
 
-    alias: ClassVar = "repositories"
+    alias = "repositories"
 
 
 class PexIndices(StringOrStringSequenceField):
     """Indices for Pex to use for packages."""
 
-    alias: ClassVar = "indices"
+    alias = "indices"
 
 
 class IgnorePexErrors(BoolField):
     """Should we ignore when Pex cannot resolve dependencies?"""
 
-    alias: ClassVar = "ignore_errors"
-    default: ClassVar = False
+    alias = "ignore_errors"
+    default = False
 
 
 class PexShebang(StringField):
     """For the generated Pex, use this shebang."""
 
-    alias: ClassVar = "shebang"
+    alias = "shebang"
 
 
 # TODO: This option is weird. Its default is determined by `--python-binary-pex-emit-warnings`.
@@ -162,8 +158,8 @@ class PexShebang(StringField):
 class EmitPexWarnings(BoolField):
     """Whether or not to emit Pex warnings at runtime."""
 
-    alias: ClassVar = "emit_warnings"
-    default: ClassVar = True
+    alias = "emit_warnings"
+    default = True
 
 
 COMMON_PYTHON_FIELDS = (*COMMON_TARGET_FIELDS, Compatibility, Provides)
@@ -177,8 +173,8 @@ class PythonBinary(Target):
     http://pantsbuild.github.io/python-readme.html#how-pex-files-work.
     """
 
-    alias: ClassVar = "python_binary"
-    core_fields: ClassVar = (
+    alias = "python_binary"
+    core_fields = (
         *COMMON_PYTHON_FIELDS,
         PythonBinarySources,
         EntryPoint,
@@ -195,10 +191,10 @@ class PythonBinary(Target):
 
 
 class PythonLibrary(Target):
-    alias: ClassVar = "python_library"
-    core_fields: ClassVar = (*COMMON_PYTHON_FIELDS, PythonLibrarySources)
+    alias = "python_library"
+    core_fields = (*COMMON_PYTHON_FIELDS, PythonLibrarySources)
 
 
 class PythonTests(Target):
-    alias: ClassVar = "python_tests"
-    core_fields: ClassVar = (*COMMON_PYTHON_FIELDS, PythonTestsSources, Coverage, Timeout)
+    alias = "python_tests"
+    core_fields = (*COMMON_PYTHON_FIELDS, PythonTestsSources, Coverage, Timeout)
