f8ab773c8d95e32e7da9317417cbbcbd78ce6744
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-03-23 17:52:55 -0700

    Deprecate `pants.contrib.python.checks` in favor of MyPy and Flake8 (#9356)
    Closes https://github.com/pantsbuild/pants/issues/8897.

--- contrib/python/src/python/pants/contrib/python/checks/register.py
+++ contrib/python/src/python/pants/contrib/python/checks/register.py
@@ -1,10 +1,26 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from pants.base.deprecated import deprecated_module
 from pants.goal.task_registrar import TaskRegistrar as task
 
 from pants.contrib.python.checks.tasks.checkstyle.checkstyle import Checkstyle
 from pants.contrib.python.checks.tasks.python_eval import PythonEval
+
+deprecated_module(
+    removal_version="1.29.0.dev0",
+    hint_message=(
+        "The `pants.contrib.python.checks` will no longer be maintained as it has been superseded "
+        "by more modern and powerful linters. To prepare, remove the "
+        "`pantsbuild.pants.contrib.python.checks` plugin from your "
+        "`pants.toml` (or `pants.ini`).\n\nIf you used `lint.pythonstyle`, see "
+        "https://github.com/pantsbuild/flake8-pantsbuild#migrating-from-lintpythonstyle-to-flake8 "
+        "for a guide on how to migrate to the V2 implementation of Flake8, which offers many more "
+        "lints and allows you to easily install additional plugins.\n\nIf you used "
+        "`lint.python-eval`, we recommend using MyPy by adding `pantsbuild.pants.contrib.mypy` "
+        "to your list of `plugins`."
+    ),
+)
 
 
 def register_goals():
