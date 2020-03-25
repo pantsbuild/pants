a1364671f6108d7353e224bf1d49e7498303568c
Eric Arellano <ericarellano@me.com>
2020-03-24 20:47:48 -0700

    Also improve the error message for get_ci_bootstrapped_pants_pex.sh
    # Delete this line to force CI to run the JVM tests.
    [ci skip-jvm-tests]  # No JVM changes made.

--- build-support/bin/get_ci_bootstrapped_pants_pex.sh
+++ build-support/bin/get_ci_bootstrapped_pants_pex.sh
@@ -2,10 +2,15 @@
 
 set -euo pipefail
 
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../.. && pwd -P)"
+
 AWS_BUCKET=$1
 BOOTSTRAPPED_PEX_KEY=$2
 
 BOOTSTRAPPED_PEX_URL=s3://${AWS_BUCKET}/${BOOTSTRAPPED_PEX_KEY}
+
+# shellcheck source=build-support/common.sh
+source "${REPO_ROOT}/build-support/common.sh"
 
 # Note that in the aws cli --no-sign-request allows access to public S3 buckets without
 # credentials, as long as we specify the region.
@@ -14,7 +19,11 @@ BOOTSTRAPPED_PEX_URL=s3://${AWS_BUCKET}/${BOOTSTRAPPED_PEX_KEY}
 NUM_VERSIONS=$(aws --no-sign-request --region us-east-1 s3api list-object-versions \
   --bucket "${AWS_BUCKET}" --prefix "${BOOTSTRAPPED_PEX_KEY}" --max-items 2 \
   | jq '.Versions | length')
-[ "${NUM_VERSIONS}" == "1" ] || (echo "Error: Found ${NUM_VERSIONS} versions for ${BOOTSTRAPPED_PEX_URL}" && exit 1)
+[ "${NUM_VERSIONS}" == "1" ] || die "Multiple copies of pants.pex found at" \
+  "${BOOTSTRAPPED_PEX_URL}. This is not allowed as a security precuation. This likely happened" \
+  "from restarting the bootstrap shards in the same Travis build. Instead, initiate a new build" \
+  "by either pulling from master or pushing an empty commit (\`git commit --allow-empty\`)."
+
 
 # Now fetch the pre-bootstrapped pex, so that the ./pants wrapper script can use it
 # instead of running from sources (and re-bootstrapping).
