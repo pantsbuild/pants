90875997c006bf9652269eb14f9d2bfd0d24ed1a
Eric Arellano <ericarellano@me.com>
2020-03-24 19:44:29 -0700

    Ensure that the venv is activated
    # Delete this line to force CI to run the JVM tests.
    [ci skip-jvm-tests]  # No JVM changes made.

--- build-support/bin/native/print_engine_hash.sh
+++ build-support/bin/native/print_engine_hash.sh
@@ -6,4 +6,10 @@ REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../../.. && pwd -P)"
 # shellcheck source=build-support/bin/native/calculate_engine_hash.sh
 source "${REPO_ROOT}/build-support/bin/native/calculate_engine_hash.sh"
 
+# shellcheck source=build-support/pants_venv
+source "${REPO_ROOT}/build-support/pants_venv"
+
+export PY="${PY:-python3}"
+activate_pants_venv 1>&2  # Redirect to ensure that we don't interfere with stdout.
+
 calculate_current_hash
