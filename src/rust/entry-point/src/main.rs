a076e10ce157d8e42916d5eb371b999d1d575cc6
Greg Shuflin <greg.shuflin@toolchain.com>
2020-03-27 11:05:15 -0700

    Add nails, tokio stuff to rust-pants-runner
    [ci skip-rust-tests]  # No Rust changes made.
    
    [ci skip-jvm-tests]  # No JVM changes made.
    
    # Delete this line to force CI to run the JVM tests.
    [ci skip-jvm-tests]  # No JVM changes made.
    
    # Delete this line to force CI to run the JVM tests.
    [ci skip-jvm-tests]  # No JVM changes made.
    
    # Delete this line to force CI to run Clippy and the Rust tests.
    [ci skip-rust-tests]  # No Rust changes made.
    
    # Delete this line to force CI to run the JVM tests.
    [ci skip-jvm-tests]  # No JVM changes made.
    
    # Delete this line to force CI to run Clippy and the Rust tests.
    [ci skip-rust-tests]  # No Rust changes made.
    
    # Delete this line to force CI to run the JVM tests.
    [ci skip-jvm-tests]  # No JVM changes made.

--- src/rust/entry-point/src/main.rs
+++ src/rust/entry-point/src/main.rs
@@ -1,6 +1,8 @@
 use nails;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
+use nails::execution::{child_channel, ChildInput, ChildOutput};
+use futures::{future, Stream, StreamExt, TryFutureExt};
 
 /// This is the same as the python type `PantsDaemon.Handle`.
 #[derive(Debug)]
@@ -74,7 +76,49 @@ fn run_locally(setup: PantsCommandSetup) {
     }
 }
 
+//need to get value of --pants-subprocessdir (defualt .pids) to read directory
 fn run_with_pantsd(setup: PantsCommandSetup) {
+  use std::net::SocketAddr;
+
+  use tokio::net::TcpStream;
+  use tokio::runtime::Runtime;
+
   println!("Running with pantsd");
+  let pantsd_dir = PathBuf::from("/home/gregs/code/pants/.pids/pantsd");
+  let socket_file = pantsd_dir.join("socket_pailgun");
+  if !Path::is_file(&socket_file) {
+    println!("No pantsd running, falling back to local");
+    run_locally(setup);
+    return;
+  }
+  let port = std::fs::read_to_string(&socket_file).unwrap(); //TODO fix unwrap
+  let port = port.parse::<u16>().unwrap();
+  println!("Pantsd port: {}", port);
+  let pantsd_addr = SocketAddr::new("127.0.0.1".parse().unwrap(), port);
+
+  let mut runtime = Runtime::new().unwrap();
+  let connection = TcpStream::connect(pantsd_addr);
+
+  let exit_code = runtime
+    .block_on(future::join(connection, stdio_printer))
+    .0?;
+
+
   std::process::exit(0);
+}
+
+async fn print_stdio(
+    mut stdio_read: impl Stream<Item = ChildOutput> + Unpin,
+) -> Result<(), std::io::Error> {
+    while let Some(output) = stdio_read.next().await {
+        match output {
+            ChildOutput::Stdout(bytes) => std::io::stdout().write_all(&bytes)?,
+            ChildOutput::Stderr(bytes) => std::io::stderr().write_all(&bytes)?,
+            ChildOutput::Exit(_) => {
+                // NB: We ignore exit here and allow the main thread to handle exiting.
+                break;
+            }
+        }
+    }
+    Ok(())
 }
