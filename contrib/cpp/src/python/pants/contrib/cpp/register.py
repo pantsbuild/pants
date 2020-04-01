1e4c90b483b72b0a36bde8ea7cc16374a07ce195
Eric Arellano <ericarellano@me.com>
2020-03-31 22:44:54 -0700

    Add Target API bindings for `contrib/cpp`
    # Delete this line to force CI to run Clippy and the Rust tests.
    [ci skip-rust-tests]  # No Rust changes made.
    
    # Delete this line to force CI to run the JVM tests.
    [ci skip-jvm-tests]  # No JVM changes made.
    
    # Delete this line to force CI to run Clippy and the Rust tests.
    [ci skip-rust-tests]  # No Rust changes made.
    
    # Delete this line to force CI to run the JVM tests.
    [ci skip-jvm-tests]  # No JVM changes made.

--- contrib/cpp/src/python/pants/contrib/cpp/register.py
+++ contrib/cpp/src/python/pants/contrib/cpp/register.py
@@ -4,8 +4,9 @@
 from pants.build_graph.build_file_aliases import BuildFileAliases
 from pants.goal.task_registrar import TaskRegistrar as task
 
-from pants.contrib.cpp.targets.cpp_binary import CppBinary
-from pants.contrib.cpp.targets.cpp_library import CppLibrary
+from pants.contrib.cpp.rules.targets import CppBinary, CppLibrary
+from pants.contrib.cpp.targets.cpp_binary import CppBinary as CppBinaryV1
+from pants.contrib.cpp.targets.cpp_library import CppLibrary as CppLibraryV1
 from pants.contrib.cpp.tasks.cpp_binary_create import CppBinaryCreate
 from pants.contrib.cpp.tasks.cpp_compile import CppCompile
 from pants.contrib.cpp.tasks.cpp_library_create import CppLibraryCreate
@@ -13,7 +14,7 @@ from pants.contrib.cpp.tasks.cpp_run import CppRun
 
 
 def build_file_aliases():
-    return BuildFileAliases(targets={"cpp_library": CppLibrary, "cpp_binary": CppBinary})
+    return BuildFileAliases(targets={"cpp_library": CppLibraryV1, "cpp_binary": CppBinaryV1})
 
 
 def register_goals():
@@ -21,3 +22,7 @@ def register_goals():
     task(name="cpplib", action=CppLibraryCreate).install("binary")
     task(name="cpp", action=CppBinaryCreate).install("binary")
     task(name="cpp", action=CppRun).install("run")
+
+
+def targets2():
+    return [CppBinary, CppLibrary]
---
+++ contrib/cpp/src/python/pants/contrib/cpp/rules/targets.py
@@ -0,0 +1,33 @@
+# Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.engine.target import (
+    COMMON_TARGET_FIELDS,
+    Dependencies,
+    Sources,
+    StringSequenceField,
+    Target,
+)
+
+
+class Libraries(StringSequenceField):
+    """Libraries that this target depends on that are not pants targets.
+
+    For example, 'm' or 'rt' that are expected to be installed on the local system.
+    """
+
+    alias = "libraries"
+
+
+class CppBinary(Target):
+    """A C++ binary."""
+
+    alias = "cpp_binary"
+    core_fields = (*COMMON_TARGET_FIELDS, Dependencies, Sources, Libraries)
+
+
+class CppLibrary(Target):
+    """A statically linked C++ library."""
+
+    alias = "cpp_library"
+    core_fields = (*COMMON_TARGET_FIELDS, Dependencies, Sources)
