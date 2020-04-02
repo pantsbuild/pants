b68441593ebdc54c217c8540dd2deda078ec7417
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-04-02 12:21:30 -0700

    Add Target API bindings for `contrib/node` and `contrib/scalajs` (#9447)
    All new targets:
    ![all targets](https://user-images.githubusercontent.com/14852634/78185498-08955000-7420-11ea-97ab-8cfa5596c80d.png)
    
    ![node_bundle](https://user-images.githubusercontent.com/14852634/78185539-164ad580-7420-11ea-8914-8e4636fb9ce9.png)
    
    ![node_module](https://user-images.githubusercontent.com/14852634/78185565-25318800-7420-11ea-93c9-38759362550c.png)
    
    ![node_preinstalled_module](https://user-images.githubusercontent.com/14852634/78185614-35e1fe00-7420-11ea-9b86-08efe7176ce5.png)
    
    ![node_remote_module](https://user-images.githubusercontent.com/14852634/78185655-43978380-7420-11ea-8f90-439f5825c12c.png)
    
    ![node_test](https://user-images.githubusercontent.com/14852634/78185692-4f834580-7420-11ea-83bb-b081c74c089c.png)
    
    ![scala_js_binary](https://user-images.githubusercontent.com/14852634/78185720-5ca03480-7420-11ea-91a8-4c631c3c2484.png)
    ![scala_js_library](https://user-images.githubusercontent.com/14852634/78185765-70e43180-7420-11ea-93f5-688d0e4e87de.png)
    
    [ci skip-rust-tests]
    [ci skip-jvm-tests]

--- contrib/scalajs/src/python/pants/contrib/scalajs/register.py
+++ contrib/scalajs/src/python/pants/contrib/scalajs/register.py
@@ -5,21 +5,22 @@ from pants.build_graph.build_file_aliases import BuildFileAliases
 from pants.goal.task_registrar import TaskRegistrar as task
 
 from pants.contrib.node.tasks.node_resolve import NodeResolve
+from pants.contrib.scalajs.rules.targets import ScalaJSBinary, ScalaJSLibrary
 from pants.contrib.scalajs.subsystems.scala_js_platform import ScalaJSPlatform
-from pants.contrib.scalajs.targets.scala_js_binary import ScalaJSBinary
-from pants.contrib.scalajs.targets.scala_js_library import ScalaJSLibrary
+from pants.contrib.scalajs.targets.scala_js_binary import ScalaJSBinary as ScalaJSBinaryV1
+from pants.contrib.scalajs.targets.scala_js_library import ScalaJSLibrary as ScalaJSLibraryV1
 from pants.contrib.scalajs.tasks.scala_js_link import ScalaJSLink
 from pants.contrib.scalajs.tasks.scala_js_zinc_compile import ScalaJSZincCompile
 
 
 def build_file_aliases():
     return BuildFileAliases(
-        targets={"scala_js_binary": ScalaJSBinary, "scala_js_library": ScalaJSLibrary}
+        targets={"scala_js_binary": ScalaJSBinaryV1, "scala_js_library": ScalaJSLibraryV1}
     )
 
 
 def register_goals():
-    NodeResolve.register_resolver_for_type(ScalaJSBinary, ScalaJSPlatform)
+    NodeResolve.register_resolver_for_type(ScalaJSBinaryV1, ScalaJSPlatform)
     # NB: These task/goal assignments are pretty nuts, but are necessary in order to
     # prevent product-graph cycles between the JVM and node.js.
     #   see https://github.com/pantsbuild/pants/labels/engine
@@ -29,3 +30,7 @@ def register_goals():
 
 def global_subsystems():
     return {ScalaJSPlatform}
+
+
+def targets2():
+    return [ScalaJSBinary, ScalaJSLibrary]
