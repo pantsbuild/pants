5d7f604cdf4753365ced0890611bc8d0354d00d7
Eric Arellano <ericarellano@me.com>
2020-04-02 18:21:57 -0700

    Fix pantsd test
    # Delete this line to force CI to run Clippy and the Rust tests.
    [ci skip-rust-tests]  # No Rust changes made.
    
    # Delete this line to force CI to run the JVM tests.
    [ci skip-jvm-tests]  # No JVM changes made.

--- tests/python/pants_test/pantsd/test_pantsd_integration.py
+++ tests/python/pants_test/pantsd/test_pantsd_integration.py
@@ -427,7 +427,7 @@ class TestPantsDaemonIntegration(PantsDaemonIntegrationTestBase):
         test_build_file = os.path.join(test_path, "BUILD")
         test_src_file = os.path.join(test_path, "some_file.py")
         has_source_root_regex = r'"source_root": ".*/{}"'.format(test_path)
-        export_cmd = ["export", test_path]
+        export_cmd = ["--files-not-found-behavior=warn", "export", test_path]
 
         try:
             with self.pantsd_successful_run_context() as (pantsd_run, checker, workdir, _):
