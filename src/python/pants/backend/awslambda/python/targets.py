44fb91f440e871dec7324dcf1aa29c9d983d5ad9
Eric Arellano <ericarellano@me.com>
2020-04-07 22:59:02 -0700

    Fix V2 target definition
    * a handler entry point is not a normal entry point, i.e. it does not work with things like `./v2 binary`
    * there's no reason to only allow one single `sources` file
    
    # Delete this line to force CI to run Clippy and the Rust tests.
    [ci skip-rust-tests]  # No Rust changes made.
    
    # Delete this line to force CI to run the JVM tests.
    [ci skip-jvm-tests]  # No JVM changes made.

--- src/python/pants/backend/awslambda/python/targets.py
+++ src/python/pants/backend/awslambda/python/targets.py
@@ -1,16 +1,11 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.backend.python.rules.targets import (
-    COMMON_PYTHON_FIELDS,
-    PythonBinarySources,
-    PythonEntryPoint,
-)
-from pants.engine.target import Target
+from pants.backend.python.rules.targets import COMMON_PYTHON_FIELDS, PythonSources
+from pants.engine.target import StringField, Target
 
 
-# NB: By subclassing PythonEntryPoint, this field will work with goals like `./pants binary`.
-class PythonAwsLambdaHandler(PythonEntryPoint):
+class PythonAwsLambdaHandler(StringField):
     """Lambda handler entrypoint (module.dotted.name:handler_func)."""
 
     alias = "handler"
@@ -22,4 +17,4 @@ class PythonAWSLambda(Target):
     """A self-contained Python function suitable for uploading to AWS Lambda."""
 
     alias = "python_awslambda"
-    core_fields = (*COMMON_PYTHON_FIELDS, PythonBinarySources, PythonAwsLambdaHandler)
+    core_fields = (*COMMON_PYTHON_FIELDS, PythonSources, PythonAwsLambdaHandler)
