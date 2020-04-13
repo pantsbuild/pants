6591682a82bfea16f7beec9bd5f7b419efc94337
John Sirois <john.sirois@gmail.com>
2020-04-13 00:32:43 -0700

    Utilities to create subsystem instances.
    Introduce utility functions to create GoalSubsystem and Subsystem
    instances for testing.
    
    Fixes #9141
    [ci skip-rust-tests]
    [ci skip-jvm-tests]

--- src/python/pants/testutil/engine/util.py
+++ src/python/pants/testutil/engine/util.py
@@ -6,12 +6,25 @@ import re
 from dataclasses import dataclass
 from io import StringIO
 from types import CoroutineType, GeneratorType
-from typing import Any, Callable, List, Optional, Sequence, Tuple, Type, get_type_hints
+from typing import (
+    Any,
+    Callable,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    Type,
+    TypeVar,
+    Union,
+    cast,
+    get_type_hints,
+)
 
 from colors import blue, cyan, green, magenta, red
 
 from pants.base.file_system_project_tree import FileSystemProjectTree
 from pants.engine.addressable import addressable_sequence
+from pants.engine.goal import GoalSubsystem
 from pants.engine.native import Native
 from pants.engine.parser import SymbolTable
 from pants.engine.rules import UnionMembership
@@ -19,6 +32,9 @@ from pants.engine.scheduler import Scheduler
 from pants.engine.selectors import Get
 from pants.engine.struct import Struct
 from pants.option.global_options import DEFAULT_EXECUTION_OPTIONS
+from pants.option.option_value_container import OptionValueContainer
+from pants.option.ranked_value import Rank, RankedValue, Value
+from pants.subsystem.subsystem import Subsystem
 from pants.util.objects import SubclassesOf
 
 
@@ -29,6 +45,57 @@ class MockGet:
     product_type: Type
     subject_type: Type
     mock: Callable[[Any], Any]
+
+
+def _create_scoped_options(
+    default_rank: int, **options: Union[RankedValue, Value]
+) -> OptionValueContainer:
+    scoped_options = OptionValueContainer()
+    for key, value in options.items():
+        if not isinstance(value, RankedValue):
+            value = RankedValue(default_rank, value)
+        setattr(scoped_options, key, value)
+    return scoped_options
+
+
+GS = TypeVar("GS", bound=GoalSubsystem)
+
+
+def create_goal_subsystem(
+    goal_subsystem_type: Type[GS],
+    default_rank: Rank = RankedValue.NONE,
+    **options: Union[RankedValue, Value],
+) -> GS:
+    """Creates a new goal subsystem instance populated with the given option values.
+
+    :param goal_subsystem_type: The `GoalSubsystem` type to create.
+    :param default_rank: The rank to assign any raw option values passed.
+    :param options: The option values to populate the new goal subsystem instance with.
+    """
+    return goal_subsystem_type(
+        scope=goal_subsystem_type.name,
+        scoped_options=_create_scoped_options(default_rank, **options),
+    )
+
+
+SS = TypeVar("SS", bound=Subsystem)
+
+
+def create_subsystem(
+    subsystem_type: Type[SS],
+    default_rank: Rank = RankedValue.NONE,
+    **options: Union[RankedValue, Value],
+) -> SS:
+    """Creates a new subsystem instance populated with the given option values.
+
+    :param subsystem_type: The `Subsystem` type to create.
+    :param default_rank: The rank to assign any raw option values passed.
+    :param options: The option values to populate the new subsystem instance with.
+    """
+    options_scope = cast(str, subsystem_type.options_scope)
+    return subsystem_type(
+        scope=options_scope, scoped_options=_create_scoped_options(default_rank, **options),
+    )
 
 
 def run_rule(
