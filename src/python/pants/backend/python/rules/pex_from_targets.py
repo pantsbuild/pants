b56f2796bdd9b914b6e417a94f8f1bba3c0818b4
Benjy Weinberger <benjyw@gmail.com>
2020-04-12 20:15:28 -0700

    Lint
    [ci skip-rust-tests]
    [ci skip-jvm-tests]

--- src/python/pants/backend/python/rules/pex_from_targets.py
+++ src/python/pants/backend/python/rules/pex_from_targets.py
@@ -1,5 +1,6 @@
 # Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 import dataclasses
 from dataclasses import dataclass
 from typing import Iterable, Optional, Tuple
