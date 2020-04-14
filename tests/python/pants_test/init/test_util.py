b5f7070136faf329a99f163329c130dbce1b74b8
pliu <pliu@twitter.com>
2020-04-13 21:28:44 -0700

    Fix file header # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

--- tests/python/pants_test/init/test_util.py
+++ tests/python/pants_test/init/test_util.py
@@ -1,5 +1,6 @@
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 import os
 from contextlib import contextmanager
 
