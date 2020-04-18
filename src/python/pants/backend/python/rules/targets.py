b30188aa9233874e26d22a47e0d2f3351e5d27a7
Eric Arellano <ericarellano@me.com>
2020-04-17 22:49:53 -0700

    Revert allowing Python targets to include `.c` files in their sources # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

--- src/python/pants/backend/python/rules/targets.py
+++ src/python/pants/backend/python/rules/targets.py
@@ -36,9 +36,7 @@ from pants.python.python_setup import PythonSetup
 
 
 class PythonSources(Sources):
-    # We allow `.c` for compatibility with native wheels, e.g. `src/python/pants:pants-packaged`.
-    # This should possibly be revisited.
-    expected_file_extensions = (".py", ".c")
+    expected_file_extensions = (".py",)
 
 
 class PythonInterpreterCompatibility(StringOrStringSequenceField):
