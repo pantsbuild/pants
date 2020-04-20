1f28b3728f5c6589b39e224f631e33cb5bff7b21
Eric Arellano <ericarellano@me.com>
2020-04-20 11:08:58 -0700

    Fix running `ci.py --lint` locally # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

--- build-support/bin/ci.py
+++ build-support/bin/ci.py
@@ -446,7 +446,7 @@ def run_lint(*, oauth_token_path: Optional[str] = None) -> None:
     command_prefix = ["./pants.pex", "--tag=-nolint"]
 
     v2_command = (
-        ["--no-v1", "--v2", "lint", *targets]
+        [*command_prefix, "--no-v1", "--v2", "lint", *targets]
         if oauth_token_path is None
         else [*command_prefix, *_use_remote_execution(oauth_token_path), "lint", *targets]
     )
