ce45391d12b2649f7e8620ee932a34b38ba58efc
Henry Fuller <hrofuller@gmail.com>
2020-04-23 12:00:39 -0700

    Revert "Add link back to source control dir from physical workdir (#9531)" (#9601)
    This reverts commit 9044c8f9ed2d1ca9aef993e3916f35bb9ddb9f63.

--- src/python/pants/init/util.py
+++ src/python/pants/init/util.py
@@ -4,15 +4,12 @@
 import os
 from typing import cast
 
-from pants.base.build_environment import get_buildroot
 from pants.fs.fs import safe_filename_from_path
 from pants.goal.goal import Goal
 from pants.init.options_initializer import BuildConfigInitializer
 from pants.option.option_value_container import OptionValueContainer
 from pants.subsystem.subsystem import Subsystem
-from pants.util.dirutil import absolute_symlink, safe_mkdir, safe_rmtree, symlink_is_correct
-
-SOURCE_CONTROL_DIRS = (".git", ".hg", ".svn")
+from pants.util.dirutil import absolute_symlink, safe_mkdir, safe_rmtree
 
 
 def init_workdir(global_options: OptionValueContainer) -> str:
@@ -30,33 +27,12 @@ def init_workdir(global_options: OptionValueContainer) -> str:
     workdir_base = global_options.pants_physical_workdir_base
     workdir_dst = os.path.join(workdir_base, safe_filename_from_path(workdir_src))
 
-    def maybe_create_source_control_symlink():
-        """If the buildroot we are working in has a source control administrative directory, we need
-        to add a link to it from the physical workdir we create.
-
-        Tools which run from the physical workdir expect to be able to glean repository information
-        directly. For instance node_binary targets are built using ad-hoc build scripts which can do
-        this.
-        """
-        # Don't link anything unless this option is enabled.
-        if not global_options.pants_physical_workdir_source_control:
-            return
-        for scm_state_dir in SOURCE_CONTROL_DIRS:
-            scm_source_path = os.path.join(get_buildroot(), scm_state_dir)
-            scm_target_path = os.path.join(workdir_dst, scm_state_dir)
-            if os.path.exists(scm_source_path) and not symlink_is_correct(
-                scm_source_path, scm_target_path
-            ):
-                absolute_symlink(scm_source_path, scm_target_path)
-                break
-
     def create_symlink_to_clean_workdir():
         # Executed when no link exists. We treat this as equivalent to a request to have deleted
         # this state. Operations like `clean-all` will already have purged the destination, but in
         # cases like manual removal of the symlink, we want to treat the case as equivalent.
         safe_mkdir(workdir_dst, clean=True)
         absolute_symlink(workdir_dst, workdir_src)
-        maybe_create_source_control_symlink()
 
     if not os.path.lexists(workdir_src):
         # Does not exist.
@@ -69,11 +45,10 @@ def init_workdir(global_options: OptionValueContainer) -> str:
         else:
             # Exists and is correct: ensure that the destination exists.
             safe_mkdir(workdir_dst)
-            maybe_create_source_control_symlink()
     else:
         # Remove existing physical workdir (.pants.d dir)
         safe_rmtree(workdir_src)
-        # Create both symlink workdir (.pants.d dir) and its destination/physical workdir.
+        # Create both symlink workdir (.pants.d dir) and its destination/physical workdir
         create_symlink_to_clean_workdir()
     return workdir_src
 
