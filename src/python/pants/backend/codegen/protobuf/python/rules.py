33881009f2de607ece575c302825b22fff460484
Eric Arellano <ericarellano@me.com>
2020-04-28 20:03:15 -0700

    Improve concurrency # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

--- src/python/pants/backend/codegen/protobuf/python/rules.py
+++ src/python/pants/backend/codegen/protobuf/python/rules.py
@@ -1,6 +1,8 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from typing import Tuple, cast
+
 from pants.backend.codegen.protobuf.subsystems.protoc import Protoc
 from pants.backend.codegen.protobuf.target_types import ProtobufSources
 from pants.backend.python.target_types import PythonSources
@@ -19,7 +21,7 @@ from pants.engine.goal import Goal, GoalSubsystem
 from pants.engine.platform import Platform
 from pants.engine.process import Process, ProcessResult
 from pants.engine.rules import goal_rule, named_rule, subsystem_rule
-from pants.engine.selectors import Get
+from pants.engine.selectors import Get, MultiGet
 from pants.engine.target import (
     GeneratedSources,
     GenerateSourcesRequest,
@@ -39,13 +41,13 @@ class GeneratePythonFromProtobufRequest(GenerateSourcesRequest):
 async def generate_python_from_protobuf(
     request: GeneratePythonFromProtobufRequest, protoc: Protoc
 ) -> GeneratedSources:
-    downloaded_protoc_binary = await Get[DownloadedExternalTool](
+    download_protoc_request = Get[DownloadedExternalTool](
         ExternalToolRequest, protoc.get_request(Platform.current)
     )
 
-    # TODO(#9650): replace this with a proper intrinsic to create empty directories.
     output_dir = "_generated_files"
-    create_output_dir_result = await Get[ProcessResult](
+    # TODO(#9650): replace this with a proper intrinsic to create empty directories.
+    create_output_dir_request = Get[ProcessResult](
         Process(
             (f"/bin/mkdir", output_dir),
             description=f"Create the directory {output_dir}",
@@ -55,18 +57,39 @@ async def generate_python_from_protobuf(
     )
 
     # Protoc needs all transitive dependencies on `protobuf_libraries` to work properly. It won't
-    # actually generate those dependencies; it only needs to look at their .proto files.
+    # actually generate those dependencies; it only needs to look at their .proto files to work
+    # with imports.
     transitive_targets = await Get[TransitiveTargets](Addresses([request.protocol_target.address]))
-    all_sources = await Get[SourceFiles](
+    all_sources_request = Get[SourceFiles](
         AllSourceFilesRequest(
             (tgt.get(Sources) for tgt in transitive_targets.closure),
             for_sources_types=(ProtobufSources,),
-            # NB: By stripping the source roots, we avoid having to set the value `--proto_path`.
+            # NB: By stripping the source roots, we avoid having to set the value `--proto_path`
+            # for Protobuf imports to be discoverable.
             strip_source_roots=True,
         )
     )
-    stripped_target_sources = await Get[SourceFiles](
+    stripped_target_sources_request = Get[SourceFiles](
         AllSourceFilesRequest([request.protocol_target[ProtobufSources]], strip_source_roots=True)
+    )
+
+    # TODO(#9294): make support for a heterogeneous MultiGet more ergonomic. We have this awkward
+    #  code to improve concurrency.
+    (
+        downloaded_protoc_binary,
+        create_output_dir_result,
+        all_sources,
+        stripped_target_sources,
+    ) = cast(
+        Tuple[DownloadedExternalTool, ProcessResult, SourceFiles, SourceFiles],
+        await MultiGet(
+            [
+                download_protoc_request,
+                create_output_dir_request,
+                all_sources_request,
+                stripped_target_sources_request,
+            ]
+        ),
     )
 
     input_digest = await Get[Digest](
