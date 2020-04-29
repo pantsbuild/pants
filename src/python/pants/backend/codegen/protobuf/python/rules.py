f9980f45e56b819c92b7fa8187d7f39c4804a29f
Eric Arellano <ericarellano@me.com>
2020-04-28 19:34:39 -0700

    Get imports working! And remove _generated_files from the output # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

--- src/python/pants/backend/codegen/protobuf/python/rules.py
+++ src/python/pants/backend/codegen/protobuf/python/rules.py
@@ -8,14 +8,27 @@ from pants.backend.codegen.protobuf.target_types import ProtobufSources
 from pants.backend.python.target_types import PythonSources
 from pants.core.util_rules.determine_source_files import AllSourceFilesRequest, SourceFiles
 from pants.core.util_rules.external_tool import DownloadedExternalTool, ExternalToolRequest
+from pants.engine.addresses import Addresses
 from pants.engine.console import Console
-from pants.engine.fs import Digest, DirectoriesToMerge, FileContent, InputFilesContent, Snapshot
+from pants.engine.fs import (
+    EMPTY_DIRECTORY_DIGEST,
+    Digest,
+    DirectoriesToMerge,
+    DirectoryWithPrefixToStrip,
+    Snapshot,
+)
 from pants.engine.goal import Goal, GoalSubsystem
 from pants.engine.platform import Platform
 from pants.engine.process import Process, ProcessResult
 from pants.engine.rules import goal_rule, named_rule, subsystem_rule
 from pants.engine.selectors import Get
-from pants.engine.target import GeneratedSources, GenerateSourcesRequest, Sources, Targets
+from pants.engine.target import (
+    GeneratedSources,
+    GenerateSourcesRequest,
+    Sources,
+    Targets,
+    TransitiveTargets,
+)
 from pants.engine.unions import UnionRule
 
 
@@ -33,32 +46,61 @@ def generate_args() -> Tuple[str, ...]:
 async def generate_python_from_protobuf(
     request: GeneratePythonFromProtobufRequest, protoc: Protoc
 ) -> GeneratedSources:
-    protocol_sources = request.protocol_sources
     downloaded_protoc_binary = await Get[DownloadedExternalTool](
         ExternalToolRequest, protoc.get_request(Platform.current)
     )
-    # TODO: figure out how to create an empty directory with the engine.
-    output_dir = await Get[Digest](InputFilesContent([FileContent("_generated_files/_dummy", b"")]))
-    input_digest = await Get[Digest](
-        DirectoriesToMerge(
-            (protocol_sources.directory_digest, downloaded_protoc_binary.digest, output_dir)
+
+    # TODO(#9650): replace this with a proper intrinsic to create empty directories.
+    output_dir = "_generated_files"
+    create_output_dir_result = await Get[ProcessResult](
+        Process(
+            (f"/bin/mkdir", output_dir),
+            description=f"Create the directory {output_dir}",
+            input_files=EMPTY_DIRECTORY_DIGEST,
+            output_directories=(output_dir,),
         )
     )
+
+    # Protoc needs all transitive dependencies on `protobuf_libraries` to work properly. It won't
+    # actually generate those dependencies; it only needs to look at their .proto files.
+    transitive_targets = await Get[TransitiveTargets](Addresses([request.protocol_target.address]))
+    all_sources = await Get[SourceFiles](
+        AllSourceFilesRequest(
+            (tgt.get(Sources) for tgt in transitive_targets.closure),
+            for_sources_types=(ProtobufSources,),
+        )
+    )
+
+    input_digest = await Get[Digest](
+        DirectoriesToMerge(
+            (
+                all_sources.snapshot.directory_digest,
+                downloaded_protoc_binary.digest,
+                create_output_dir_result.output_directory_digest,
+            )
+        )
+    )
+
     result = await Get[ProcessResult](
         Process(
             (
                 downloaded_protoc_binary.exe,
                 "--python_out",
-                "_generated_files",
-                *sorted(protocol_sources.files),
+                output_dir,
+                # TODO: make this generic
+                "--proto_path",
+                "src/protobuf",
+                *sorted(request.protocol_sources.files),
             ),
             input_files=input_digest,
             description=f"Generating Python sources from {request.protocol_target.address}.",
-            output_directories=("_generated_files",),
+            output_directories=(output_dir,),
         )
     )
-    result_snapshot = await Get[Snapshot](Digest, result.output_directory_digest)
-    return GeneratedSources(result_snapshot)
+    normalized_snapshot = await Get[Snapshot](
+        DirectoryWithPrefixToStrip(result.output_directory_digest, output_dir)
+    )
+    return GeneratedSources(normalized_snapshot)
 
 
 class ProtocOptions(GoalSubsystem):
