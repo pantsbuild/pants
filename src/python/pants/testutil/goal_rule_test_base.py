e8d66e65b64870954b9d2b561e0776c82e9c53c5
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-04-30 15:01:50 -0700

    Change `GoalRuleTestBase.execute_rule` to return the captured stderr (#9664)
    In an upcoming change, `repl` will write to stderr, rather than stdout. So, we need `GoalRuleTestBase` to allow returning both the stdout and stderr.
    
    We use a dataclass to keep things organized.
    
    Technically, `GoalRuleTestBase` was marked a public API, but this doesn't make much sense because the V2 API is nowhere near stable. We end up breaking V2 tests almost every single weekly release through changes to the engine. So, this removes that premature indicator that we weren't respecting in the first place.
    
    [ci skip-rust-tests]
    [ci skip-jvm-tests]

--- src/python/pants/testutil/goal_rule_test_base.py
+++ src/python/pants/testutil/goal_rule_test_base.py
@@ -1,8 +1,11 @@
 # Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from dataclasses import dataclass
 from io import StringIO
-from typing import Any, Dict, Iterable, Optional
+from typing import Any, Dict, Iterable, Optional, Type
+
+import pytest
 
 from pants.engine.console import Console
 from pants.engine.fs import Workspace
@@ -16,39 +19,38 @@ from pants.testutil.test_base import TestBase
 from pants.util.meta import classproperty
 
 
-class GoalRuleTestBase(TestBase):
-    """A baseclass useful for testing a Goal defined as a @goal_rule.
+@dataclass(frozen=True)
+class GoalRuleResult:
+    return_code: int
+    stdout: str
+    stderr: str
 
-    :API: public
-    """
+
+class GoalRuleTestBase(TestBase):
+    """A baseclass useful for testing a Goal defined as a @goal_rule."""
 
     @classproperty
     def goal_cls(cls):
-        """Subclasses must return the Goal type to test.
-
-        :API: public
-        """
+        """Subclasses must return the Goal type to test."""
         raise NotImplementedError()
 
     def setUp(self):
         super().setUp()
-
         if not issubclass(self.goal_cls, Goal):
             raise AssertionError(f"goal_cls() must return a Goal subclass, got {self.goal_cls}")
 
     def execute_rule(
         self,
+        *,
         args: Optional[Iterable[str]] = None,
         global_args: Optional[Iterable[str]] = None,
         env: Optional[Dict[str, str]] = None,
         exit_code: int = 0,
         additional_params: Optional[Iterable[Any]] = None,
-    ) -> str:
+    ) -> GoalRuleResult:
         """Executes the @goal_rule for this test class.
 
-        :API: public
-
-        Returns the text output of the task.
+        Returns the return code, stdout, and stderr of the goal.
         """
         # Create an OptionsBootstrapper for these args/env, and a captured Console instance.
         options_bootstrapper = create_options_bootstrapper(
@@ -60,16 +62,14 @@ class GoalRuleTestBase(TestBase):
         )
         stdout, stderr = StringIO(), StringIO()
         console = Console(stdout=stdout, stderr=stderr)
-        scheduler = self.scheduler
-        workspace = Workspace(scheduler)
 
-        # Run for the target specs parsed from the args.
+        # Run for the specs parsed from the args.
         specs = SpecsCalculator.parse_specs(full_options.specs, self.build_root)
         params = Params(
             specs.provided_specs,
             console,
             options_bootstrapper,
-            workspace,
+            Workspace(self.scheduler),
             *(additional_params or []),
         )
         actual_exit_code = self.scheduler.run_goal_rule(self.goal_cls, params)
@@ -83,67 +83,59 @@ class GoalRuleTestBase(TestBase):
             exit_code == actual_exit_code
         ), f"Exited with {actual_exit_code} (expected {exit_code}):\nstdout:\n{stdout_val}\nstderr:\n{stderr_val}"
 
-        return stdout_val
+        return GoalRuleResult(actual_exit_code, stdout_val, stderr_val)
 
-    def assert_entries(self, sep, *output, **kwargs):
-        """Verifies the expected output text is flushed by the console task under test.
+    def assert_entries(self, sep: str, *output, **kwargs) -> None:
+        """Verifies the expected output text is flushed by goal rule.
 
         NB: order of entries is not tested, just presence.
-
-        :API: public
 
         sep:      the expected output separator.
         *output:  the output entries expected between the separators
         **kwargs: additional kwargs passed to execute_rule.
         """
-        # We expect each output line to be suffixed with the separator, so for , and [1,2,3] we expect:
-        # '1,2,3,' - splitting this by the separator we should get ['1', '2', '3', ''] - always an extra
-        # empty string if the separator is properly always a suffix and not applied just between
-        # entries.
-        self.assertEqual(
-            sorted(list(output) + [""]), sorted((self.execute_rule(**kwargs)).split(sep))
-        )
+        # We expect each output line to be suffixed with the separator, so for `,` and [1, 2, 3] we
+        # expect: '1,2,3,' - splitting this by the separator we should get
+        # ['1', '2', '3', ''] - always an extra empty string if the separator is properly always
+        # a suffix and not applied just between entries.
+        result = self.execute_rule(**kwargs)
+        assert sorted([*output, ""]) == sorted(result.stdout.split(sep))
 
-    def assert_console_output(self, *output, **kwargs):
-        """Verifies the expected output entries are emitted by the console task under test.
+    def assert_console_output(self, *output, **kwargs) -> None:
+        """Verifies the expected output entries are emitted by the goal rule.
 
         NB: order of entries is not tested, just presence.
-
-        :API: public
 
         *output:  the expected output entries
         **kwargs: additional kwargs passed to execute_rule.
         """
-        self.assertEqual(sorted(output), sorted(self.execute_rule(**kwargs).splitlines()))
+        result = self.execute_rule(**kwargs)
+        assert sorted(output) == sorted(result.stdout.splitlines())
 
-    def assert_console_output_contains(self, output, **kwargs):
-        """Verifies the expected output string is emitted by the console task under test.
-
-        :API: public
+    def assert_console_output_contains(self, output, **kwargs) -> None:
+        """Verifies the expected output string is emitted by the goal rule.
 
         output:  the expected output entry(ies)
         **kwargs: additional kwargs passed to execute_rule.
         """
-        self.assertIn(output, self.execute_rule(**kwargs))
+        result = self.execute_rule(**kwargs)
+        assert output in result.stdout
 
-    def assert_console_output_ordered(self, *output, **kwargs):
-        """Verifies the expected output entries are emitted by the console task under test.
+    def assert_console_output_ordered(self, *output, **kwargs) -> None:
+        """Verifies the expected output entries are emitted by the goal rule.
 
         NB: order of entries is tested.
-
-        :API: public
 
         *output:  the expected output entries in expected order
         **kwargs: additional kwargs passed to execute_rule.
         """
-        self.assertEqual(list(output), self.execute_rule(**kwargs).splitlines())
+        result = self.execute_rule(**kwargs)
+        assert list(output) == result.stdout.splitlines()
 
-    def assert_console_raises(self, exception, **kwargs):
-        """Verifies the expected exception is raised by the console task under test.
-
-        :API: public
+    def assert_console_raises(self, exception: Type[Exception], **kwargs) -> None:
+        """Verifies the expected exception is raised by the goal rule.
 
         **kwargs: additional kwargs are passed to execute_rule.
         """
-        with self.assertRaises(exception):
+        with pytest.raises(exception):
             self.execute_rule(**kwargs)
