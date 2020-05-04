d7b6bcc6e190c1429bbffd63c8af7e2069232d18
John Sirois <john.sirois@gmail.com>
2020-05-03 22:23:02 -0700

    MyPy now checks partially_type_checked targets. (#9688)
    The script was using bad `--tag` syntax. The `--tag` values are expected
    to conform to `^[+-]?[^,]+(,[^,]+)*$`. In other words, a single leading
    optional +/- operator and then a list of tag names to apply that single
    operator to. As such, previously we were passing a `+` tag list
    comprised of tag-literals `type_checked`, `+partially_type_checked` and
    `-nolint`. Of these we only ever matched the `type-checked` tag literal.
    
    Also fix up the type checking failures that this fix exposed.

--- src/python/pants/backend/python/lint/isort/rules_integration_test.py
+++ src/python/pants/backend/python/lint/isort/rules_integration_test.py
@@ -70,7 +70,7 @@ class IsortIntegrationTest(TestBase):
         lint_result = self.request_single_product(
             LintResult, Params(IsortFieldSets(field_sets), options_bootstrapper)
         )
-        input_snapshot = self.request_single_product(
+        input_sources = self.request_single_product(
             SourceFiles,
             Params(
                 AllSourceFilesRequest(field_set.sources for field_set in field_sets),
@@ -80,7 +80,7 @@ class IsortIntegrationTest(TestBase):
         fmt_result = self.request_single_product(
             FmtResult,
             Params(
-                IsortFieldSets(field_sets, prior_formatter_result=input_snapshot),
+                IsortFieldSets(field_sets, prior_formatter_result=input_sources.snapshot),
                 options_bootstrapper,
             ),
         )
