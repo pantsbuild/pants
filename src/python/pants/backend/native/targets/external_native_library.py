3a01be224aac982e92770dd01cd7d4ddca513072
Josh Soref <jsoref@users.noreply.github.com>
2020-05-17 19:05:19 -0400

    spelling: directory

--- src/python/pants/backend/native/targets/external_native_library.py
+++ src/python/pants/backend/native/targets/external_native_library.py
@@ -76,7 +76,7 @@ class ConanRequirement:
 
         Example package specification:
           "my_library/1.0.0@pants/stable"
-        Example of the direcory path that Conan downloads package data for this package to:
+        Example of the directory path that Conan downloads package data for this package to:
           "my_library/1.0.0/pants/stable"
 
         For more info on Conan package specifications, see:
