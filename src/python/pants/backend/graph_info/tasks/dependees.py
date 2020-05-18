pantsbuild/pants

commit e3d3ab338dd678b83a58b9237807dd611fca178b

Fix several typos in source code (#9800)
Spelling errors make codebases less friendly.

Generated based on #8748 using:
```sh
for a in $(hg log -T '{rev} ' -r spelling%master); do
   X="$(MODE=rs convertpatch-to -c$a)";
   ( sh -c "$X";   ) 2>&1;
done
```

Fewer spelling errors.

[ci skip-rust-tests]
[ci skip-jvm-tests]

src/python/pants/backend/graph_info/tasks/dependees.py
@@ -31,7 +31,7 @@ class ReverseDepmap(ConsoleTask):
             "--transitive",
             type=bool,
             default=False,
-            help="Whether to include only the first level of dependendees.",
+            help="Whether to include only the first level of dependencies.",
         )
 
     def __init__(self, *args, **kwargs):

commit 84fb954970c71a0dc726c8b88a58c49af0ec6a84

Fix issue semi-intentionally introduced in #9800. # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

src/python/pants/backend/graph_info/tasks/dependees.py
@@ -31,7 +31,7 @@ class ReverseDepmap(ConsoleTask):
             "--transitive",
             type=bool,
             default=False,
-            help="Whether to include only the first level of dependencies.",
+            help="Whether to include only the first level of dependees.",
         )
 
     def __init__(self, *args, **kwargs):