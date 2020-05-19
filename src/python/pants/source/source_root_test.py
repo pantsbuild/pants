b80ac0f997a26ebedcda0d8c02aaaa9082bf4071
Benjy Weinberger <benjyw@gmail.com>
2020-05-18 22:01:44 -0700

    Lint # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

--- src/python/pants/source/source_root_test.py
+++ src/python/pants/source/source_root_test.py
@@ -1,5 +1,6 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 import pytest
 
 from pants.source.source_root import NoSourceRootError, SourceRoot, SourceRoots, SourceRootTrie
