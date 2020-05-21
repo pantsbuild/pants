b4cf581c68d1f5fd4e79e97177db43e8247305b6
Eric Arellano <ericarellano@me.com>
2020-05-21 12:59:17 -0700

    Add support for dependency injection and use with V2 Protobuf [ci skip-jvm-tests] [ci skip-rust-tests]

--- src/python/pants/backend/codegen/protobuf/subsystems/protoc.py
+++ src/python/pants/backend/codegen/protobuf/subsystems/protoc.py
@@ -1,8 +1,11 @@
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from typing import List, cast
+
 from pants.core.util_rules.external_tool import ExternalTool, ExternalToolError
 from pants.engine.platform import Platform
+from pants.option.custom_types import target_option
 
 
 class Protoc(ExternalTool):
@@ -14,6 +17,22 @@ class Protoc(ExternalTool):
         "3.11.4|darwin|8c6af11e1058efe953830ecb38324c0e0fd2fb67df3891896d138c535932e7db|2482119",
         "3.11.4|linux |6d0f18cd84b918c7b3edd0203e75569e0c8caecb1367bbbe409b45e28514f5be|1591191",
     ]
+
+    @classmethod
+    def register_options(cls, register):
+        super().register_options(register)
+        register(
+            "--runtime-targets",
+            type=list,
+            member_type=target_option,
+            advanced=True,
+            help=(
+                "A list of addresses to targets for Protobuf runtime libraries. For example, a "
+                "`python_requirement_library` for the `protobuf` Python library. These targets "
+                "will be automatically injected into the `dependencies` field of every "
+                "`protobuf_library`."
+            ),
+        )
 
     def generate_url(self, plat: Platform) -> str:
         version = self.get_options().version
@@ -47,3 +66,7 @@ class Protoc(ExternalTool):
             return "protoc"
         else:
             return "bin/protoc"
+
+    @property
+    def runtime_targets(self) -> List[str]:
+        return cast(List[str], self.options.runtime_targets)
