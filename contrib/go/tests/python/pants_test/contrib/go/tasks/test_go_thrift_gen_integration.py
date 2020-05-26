57cf2774d554308276e8a4e4bbaa1b6f1f544731
Asher Foa <1268088+asherf@users.noreply.github.com>
2020-05-26 08:49:15 -0700

    remove more whitespace.

--- contrib/go/tests/python/pants_test/contrib/go/tasks/test_go_thrift_gen_integration.py
+++ contrib/go/tests/python/pants_test/contrib/go/tasks/test_go_thrift_gen_integration.py
@@ -55,9 +55,9 @@ class GoThriftGenIntegrationTest(PantsRunIntegrationTest):
                     dedent(
                         """
                         package usethrift
-            
+
                         import "thrifttest/duck"
-            
+
                         func whatevs(f duck.Feeder) string {
                           d := duck.NewDuck()
                           f.Feed(d)
