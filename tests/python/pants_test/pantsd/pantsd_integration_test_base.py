4ed9360964311fc05f6eda374eb391f5663451dc
Stu Hood <stuhood@gmail.com>
2020-06-08 13:47:44 -0700

    Update tests/python/pants_test/pantsd/pantsd_integration_test_base.py
    Co-authored-by: Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>

--- tests/python/pants_test/pantsd/pantsd_integration_test_base.py
+++ tests/python/pants_test/pantsd/pantsd_integration_test_base.py
@@ -40,7 +40,7 @@ class PantsDaemonMonitor(ProcessManager):
     def _log(self):
         print(magenta(f"PantsDaemonMonitor: pid is {self._pid} is_alive={self.is_alive()}"))
 
-    def assert_started_and_stopped(self, timeout=30):
+    def assert_started_and_stopped(self, timeout: int = 30) -> None:
         """Asserts that pantsd was alive (it wrote a pid file), but that it stops afterward."""
         self._process = None
         self._pid = self.await_pid(timeout)
