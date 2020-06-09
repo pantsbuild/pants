7f2abaf23fc0dbab45362e56db1ee9388a05a85b
Eric Arellano <ericarellano@me.com>
2020-06-09 05:48:10 -0700

    Fix header # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

--- src/python/pants/backend/python/dependency_inference/rules.py
+++ src/python/pants/backend/python/dependency_inference/rules.py
@@ -1,3 +1,6 @@
+# Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 import itertools
 from pathlib import PurePath
 
