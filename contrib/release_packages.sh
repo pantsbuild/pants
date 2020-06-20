24ba9168a063bcbde61ed0937effe47e4239109f
Eric Arellano <ericarellano@me.com>
2020-06-20 08:53:25 -0700

    Remove `pants.contrib.confluence` # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

--- contrib/release_packages.sh
+++ contrib/release_packages.sh
@@ -30,13 +30,6 @@ function pkg_node_install_test() {
       --explain test | grep "NodeTest_test_node" &> /dev/null
 }
 
-function pkg_confluence_install_test() {
-  local version=$1
-  execute_packaged_pants_with_internal_backends \
-      --plugins="['pantsbuild.pants.contrib.confluence==${version}']" \
-      --explain confluence | grep "ConfluencePublish_confluence" &> /dev/null
-}
-
 function pkg_mypy_install_test() {
   local version=$1
   execute_packaged_pants_with_internal_backends \
