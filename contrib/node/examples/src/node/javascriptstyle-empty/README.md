9923d72d3d2c8ce55167e3d670113939279263ba
Eric Arellano <ericarellano@me.com>
2020-06-20 16:59:53 -0700

    Remove `pants.contrib.node` plugin # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

--- contrib/node/examples/src/node/javascriptstyle-empty/README.md
+++
@@ -1,8 +0,0 @@
-RESERVED FOR JAVASCRIPTSTYLE UNIT TESTING
-=========================================
-
-Do not modify the contents of this directory!
-
-Please see path to the pants test target:
-
-    contrib/node/tests/python/pants_test/contrib/node/tasks:node_lint_integration
--- contrib/release_packages.sh
+++ contrib/release_packages.sh
@@ -16,13 +16,6 @@ function pkg_scrooge_install_test() {
     --explain lint | grep "thrift" &> /dev/null
 }
 
-function pkg_node_install_test() {
-  local version=$1
-  execute_packaged_pants_with_internal_backends \
-      --plugins="['pantsbuild.pants.contrib.node==${version}']" \
-      --explain test | grep "NodeTest_test_node" &> /dev/null
-}
-
 function pkg_mypy_install_test() {
   local version=$1
   execute_packaged_pants_with_internal_backends \
