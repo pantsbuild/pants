a81cb669b1a27d769d4d489f9844f6f7d224fdc4
Eric Arellano <ericarellano@me.com>
2020-06-21 10:58:05 -0700

    Remove pants.backend.docgen # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

--- build-support/bin/release.sh
+++ build-support/bin/release.sh
@@ -150,7 +150,6 @@ function execute_packaged_pants_with_internal_backends() {
     --pythonpath="['pants-plugins/src/python']" \
     --backend-packages="[\
         'pants.backend.codegen',\
-        'pants.backend.docgen',\
         'pants.backend.graph_info',\
         'pants.backend.jvm',\
         'pants.backend.native',\
