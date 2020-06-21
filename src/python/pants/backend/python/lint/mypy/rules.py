819cd54ac1a86bd7fec76c1672d304028a14f2e6
Eric Arellano <ericarellano@me.com>
2020-06-21 09:24:29 -0700

    Add v2 MyPy implementation
    [ci skip-rust-tests]
    [ci skip-jvm-tests]

---
+++ src/python/pants/backend/python/lint/mypy/rules.py
@@ -0,0 +1,127 @@
+# Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from dataclasses import dataclass
+from typing import Tuple
+
+from pants.backend.python.lint.mypy.subsystem import MyPy
+from pants.backend.python.rules import download_pex_bin, importable_python_sources, pex
+from pants.backend.python.rules.importable_python_sources import ImportablePythonSources
+from pants.backend.python.rules.pex import (
+    Pex,
+    PexInterpreterConstraints,
+    PexRequest,
+    PexRequirements,
+)
+from pants.backend.python.subsystems import python_native_code, subprocess_environment
+from pants.backend.python.subsystems.subprocess_environment import SubprocessEncodingEnvironment
+from pants.backend.python.target_types import PythonSources
+from pants.core.goals.lint import LintRequest, LintResult, LintResults
+from pants.core.util_rules import determine_source_files, strip_source_roots
+from pants.engine.addresses import Addresses
+from pants.engine.fs import AddPrefix, Digest, MergeDigests, PathGlobs, Snapshot
+from pants.engine.process import FallibleProcessResult, Process
+from pants.engine.rules import SubsystemRule, rule
+from pants.engine.selectors import Get, MultiGet
+from pants.engine.target import FieldSetWithOrigin, Targets, TransitiveTargets
+from pants.engine.unions import UnionRule
+from pants.option.global_options import GlobMatchErrorBehavior
+from pants.python.python_setup import PythonSetup
+from pants.util.strutil import pluralize
+
+
+@dataclass(frozen=True)
+class MyPyFieldSet(FieldSetWithOrigin):
+    required_fields = (PythonSources,)
+
+    sources: PythonSources
+
+
+class MyPyRequest(LintRequest):
+    field_set_type = MyPyFieldSet
+
+
+def generate_args(mypy: MyPy, *, sources_directory: str) -> Tuple[str, ...]:
+    args = []
+    if mypy.config:
+        args.append(f"--config-file={mypy.config}")
+    args.extend(mypy.args)
+    args.append(sources_directory)
+    return tuple(args)
+
+
+# TODO: Improve performance, e.g. by leveraging the MyPy cache.
+# TODO: Support plugins and type stubs.
+@rule(desc="Lint using MyPy")
+async def mypy_lint(
+    request: MyPyRequest,
+    mypy: MyPy,
+    python_setup: PythonSetup,
+    subprocess_encoding_environment: SubprocessEncodingEnvironment,
+) -> LintResults:
+    if mypy.skip:
+        return LintResults()
+
+    transitive_targets = await Get(
+        TransitiveTargets, Addresses(fs.address for fs in request.field_sets)
+    )
+
+    prepared_sources_request = Get(ImportablePythonSources, Targets(transitive_targets.closure))
+    pex_request = Get(
+        Pex,
+        PexRequest(
+            output_filename="bin/mypy.pex",
+            requirements=PexRequirements(mypy.get_requirement_specs()),
+            # TODO: figure out how to robustly handle interpreter constraints. Unlike other linters,
+            #  the version of Python used to run MyPy can be different than the version of the code.
+            interpreter_constraints=PexInterpreterConstraints(mypy.default_interpreter_constraints),
+            entry_point=mypy.get_entry_point(),
+        ),
+    )
+    config_snapshot_request = Get(
+        Snapshot,
+        PathGlobs(
+            globs=[mypy.config] if mypy.config else [],
+            glob_match_error_behavior=GlobMatchErrorBehavior.error,
+            description_of_origin="the option `--mypy-config`",
+        ),
+    )
+    prepared_sources, pex, config_snapshot = await MultiGet(
+        prepared_sources_request, pex_request, config_snapshot_request
+    )
+
+    relocated_sources_digest = await Get(
+        Digest, AddPrefix(prepared_sources.snapshot.digest, prefix="src")
+    )
+    merged_input_files = await Get(
+        Digest, MergeDigests([relocated_sources_digest, pex.digest, config_snapshot.digest])
+    )
+
+    address_references = sorted(tgt.address.spec for tgt in transitive_targets.closure)
+    process = pex.create_process(
+        python_setup=python_setup,
+        subprocess_encoding_environment=subprocess_encoding_environment,
+        pex_path=pex.output_filename,
+        pex_args=generate_args(mypy, sources_directory="src"),
+        input_digest=merged_input_files,
+        description=(
+            f"Run MyPy on {pluralize(len(address_references), 'target')}: {address_references}."
+        ),
+    )
+    result = await Get(FallibleProcessResult, Process, process)
+    return LintResults([LintResult.from_fallible_process_result(result, linter_name="MyPy")])
+
+
+def rules():
+    return [
+        mypy_lint,
+        SubsystemRule(MyPy),
+        UnionRule(LintRequest, MyPyRequest),
+        *download_pex_bin.rules(),
+        *determine_source_files.rules(),
+        *importable_python_sources.rules(),
+        *pex.rules(),
+        *python_native_code.rules(),
+        *strip_source_roots.rules(),
+        *subprocess_environment.rules(),
+    ]
---
+++ src/python/pants/backend/python/lint/mypy/subsystem.py
@@ -0,0 +1,46 @@
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from typing import Optional, Tuple, cast
+
+from pants.backend.python.subsystems.python_tool_base import PythonToolBase
+from pants.option.custom_types import file_option, shell_str
+
+
+class MyPy(PythonToolBase):
+    options_scope = "mypy"
+    default_version = "mypy==0.781"
+    default_entry_point = "mypy"
+    default_interpreter_constraints = ["CPython>=3.5"]
+
+    @classmethod
+    def register_options(cls, register):
+        super().register_options(register)
+        register(
+            "--skip", type=bool, default=False, help="Don't use MyPy when running `./pants lint`."
+        )
+        register(
+            "--args",
+            type=list,
+            member_type=shell_str,
+            help="Arguments to pass directly to mypy, e.g. "
+            '`--mypy-args="--python-version 3.7 --disallow-any-expr"`',
+        )
+        register(
+            "--config",
+            type=file_option,
+            advanced=True,
+            help="Path to `mypy.ini` or alternative MyPy config file",
+        )
+
+    @property
+    def skip(self) -> bool:
+        return cast(bool, self.options.skip)
+
+    @property
+    def args(self) -> Tuple[str, ...]:
+        return tuple(self.options.args)
+
+    @property
+    def config(self) -> Optional[str]:
+        return cast(Optional[str], self.options.config)
