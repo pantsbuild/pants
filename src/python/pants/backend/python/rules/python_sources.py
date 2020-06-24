c01c011d15b695a8643548a2fc5f358ef9bc3d48
Eric Arellano <ericarellano@me.com>
2020-06-23 19:58:03 -0700

    Fix copyright header # Delete this line to force CI to run Clippy and the Rust tests. [ci skip-rust-tests] # Delete this line to force CI to run the JVM tests. [ci skip-jvm-tests]

--- src/python/pants/backend/python/rules/python_sources.py
+++ src/python/pants/backend/python/rules/python_sources.py
@@ -1,5 +1,6 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 from dataclasses import dataclass
 from typing import Tuple
 
