b2428e7f237080c2142ef048ea2d60ff203d2b9b
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-06-29 13:06:14 -0700

    Remove v1 portions of `backend/native` and `ipex` (#10187)
    In Pants 2.0, we are deleting all v1 abstractions like `Task` and `build_graph.Target`. Unfortunately, this means that we cannot preserve all of `backend/native`. We keep the rules-based code, though.
    
    [ci skip-rust-tests]

--- testprojects/src/python/python_distribution/ctypes/src-subdir/add_three.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ADD_THREE_H__
-#define __ADD_THREE_H__
-
-int add_three(int x);
-
-#endif
--- testprojects/src/python/python_distribution/ctypes_interop/wrapped-math/wrapped_math.h
+++
@@ -1,6 +0,0 @@
-#ifndef __WRAPPED_MATH_H__
-#define __WRAPPED_MATH_H__
-
-int wrapped_function(int);
-
-#endif
