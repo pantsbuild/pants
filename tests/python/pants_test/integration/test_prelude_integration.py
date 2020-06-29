ef772cec072b7aa9d3c77321bc4fd4ff83d57f0f
Eric Arellano <ericarellano@me.com>
2020-06-29 16:01:21 -0700

    Fix test_prelude_integration.py
    # Rust tests will be skipped. Delete if not intended.
    [ci skip-rust-tests]

--- tests/python/pants_test/integration/test_prelude_integration.py
+++ tests/python/pants_test/integration/test_prelude_integration.py
@@ -1,6 +1,8 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from textwrap import dedent
+
 from pants.testutil.pants_run_integration_test import PantsRunIntegrationTest
 
 
@@ -8,15 +10,25 @@ class PreludeIntegrationTest(PantsRunIntegrationTest):
     """Tests the functionality build file preludes."""
 
     def test_build_file_prelude(self):
-        prelude = b"""def make_target():
-    python_binary(name = "main", sources = ["main.py"])
-"""
+        prelude = dedent(
+            """\
+            def make_target():
+                python_binary(name = "main", sources = ["main.py"])
+            """
+        ).encode()
         build = b"make_target()"
         python = b"print('Unique output here!!')"
 
         with self.temporary_file_content("prelude", prelude), self.temporary_file_content(
             "BUILD", build
         ), self.temporary_file_content("main.py", python):
-            run = self.run_pants(["--build-file-prelude-globs=prelude", "run", ":main"])
+            run = self.run_pants(
+                [
+                    "--build-file-prelude-globs=prelude",
+                    "--source-root-patterns=['/']",
+                    "run",
+                    ":main",
+                ]
+            )
             self.assert_success(run)
             assert "Unique output here!!" in run.stdout_data
