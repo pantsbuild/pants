169ea07a399129c94bb8b5bdc28b6e884bd0e13d
Benjy Weinberger <benjyw@gmail.com>
2020-07-02 11:15:26 -0700

    Delete the old BinaryTool mechanism.
    It's replaced by ExternalTool. This change switches
    the last remaining users of BinaryTool over to ExternalTool,
    and deletes the former.
    
    Note that the native tool classes had extra utility
    methods on them that assumed that tools were downloaded
    locally. This is not the v2 way of doing things.
    These utilities were deleted, and the native tool using code
    will need to be rewritten to accept a digest+relpath,
    in the v2 idiom.
    
    [ci skip-rust-tests]

--- src/python/pants/backend/native/subsystems/binaries/binutils.py
+++ src/python/pants/backend/native/subsystems/binaries/binutils.py
@@ -1,52 +1,18 @@
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import os
-
-from pants.backend.native.config.environment import Assembler, Linker
-from pants.binaries.binary_tool import NativeTool
-from pants.engine.rules import rule
+from pants.core.util_rules.external_tool import ExternalTool, ExternalToolError
+from pants.engine.platform import Platform
 
 
-class Binutils(NativeTool):
+class Binutils(ExternalTool):
     options_scope = "binutils"
     default_version = "2.30"
-    archive_type = "tgz"
-
-    def path_entries(self):
-        return [os.path.join(self.select(), "bin")]
-
-    def assembler(self) -> Assembler:
-        return Assembler(
-            path_entries=self.path_entries(),
-            exe_filename="as",
-            runtime_library_dirs=(),
-            extra_args=(),
-        )
-
-    def linker(self) -> Linker:
-        return Linker(
-            path_entries=self.path_entries(),
-            exe_filename="ld",
-            runtime_library_dirs=(),
-            linking_library_dirs=(),
-            extra_args=(),
-            extra_object_files=(),
-        )
-
-
-@rule
-def get_as(binutils: Binutils) -> Assembler:
-    return binutils.assembler()
-
-
-@rule
-def get_ld(binutils: Binutils) -> Linker:
-    return binutils.linker()
-
-
-def create_binutils_rules():
-    return [
-        get_as,
-        get_ld,
+    default_known_versions = [
+        "2.30|linux |7e78ee54d18fa8ad30c793e78aec24b9ddcc184a8aee5675f41d40bf6d0c89fe|40121937",
     ]
+
+    def generate_url(self, plat: Platform) -> str:
+        if plat != Platform.linux:
+            raise ExternalToolError()
+        return f"https://binaries.pantsbuild.org/bin/binutils/linux/x86_64/{self.options.version}/binutils.tar.gz"
