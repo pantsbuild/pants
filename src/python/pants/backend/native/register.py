65423af4de7ef7c2c66d42886a78737b857966a6
Benjy Weinberger <benjyw@gmail.com>
2020-07-02 14:19:42 -0700

    Delete the old BinaryTool mechanism. (#10239)
    It's replaced by ExternalTool. This change switches
    the last remaining users of BinaryTool over to ExternalTool,
    and deletes the former.
    
    Note that the native tool classes had extra utility
    methods on them that assumed that tools were downloaded
    locally. This is not the v2 way of doing things.
    These utilities were deleted, and the native tool using code
    will need to be rewritten to accept a digest+relpath,
    in the v2 idiom.
    
    [ci skip-rust-tests]

--- src/python/pants/backend/native/register.py
+++ src/python/pants/backend/native/register.py
@@ -3,9 +3,6 @@
 
 """Support for using C and C++ with Python distributions via `ctypes`."""
 
-from pants.backend.native.subsystems.binaries.binutils import create_binutils_rules
-from pants.backend.native.subsystems.binaries.gcc import create_gcc_rules
-from pants.backend.native.subsystems.binaries.llvm import create_llvm_rules
 from pants.backend.native.subsystems.native_toolchain import create_native_toolchain_rules
 from pants.backend.native.subsystems.xcode_cli_tools import create_xcode_cli_tools_rules
 
@@ -14,7 +11,4 @@ def rules():
     return (
         *create_native_toolchain_rules(),
         *create_xcode_cli_tools_rules(),
-        *create_binutils_rules(),
-        *create_gcc_rules(),
-        *create_llvm_rules(),
     )
