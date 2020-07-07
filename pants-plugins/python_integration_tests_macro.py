52e8ab26b3f2b86ec5d2924472c09fffeb5da832
Eric Arellano <ericarellano@me.com>
2020-07-07 12:26:04 -0700

    Fix integration tests
    We take the approach of forcing the target author to explicitly say whether it uses PantsRun. This is to avoid having worse caching on rules-based ITs which do not actually depend on PantsRunIntegrationTest.
    
    # Rust tests will be skipped. Delete if not intended.
    [ci skip-rust-tests]

--- pants-plugins/python_integration_tests_macro.py
+++ pants-plugins/python_integration_tests_macro.py
@@ -1,11 +1,12 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-def python_integration_tests(**kwargs):
+def python_integration_tests(*, uses_pants_run: bool, **kwargs):
     kwargs["tags"] = [*kwargs.get("tags", []), "integration"]
-    # NB: This will not be inferred via dep inference, as `:int-test` is a `target` rather than
-    # `python_library`.
-    kwargs["dependencies"] = [*kwargs.get("dependencies", []), "src/python/pants/testutil:int-test"]
+    if uses_pants_run:
+        kwargs["dependencies"] = [
+            *kwargs.get("dependencies", []), "src/python/pants/testutil:int-test"
+        ]
     if "sources" not in kwargs:
         kwargs["sources"] = ["*_integration_test.py"]
     python_tests(**kwargs)
