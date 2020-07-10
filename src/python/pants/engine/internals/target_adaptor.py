51449a197cbc3f720ad671e8a48d7441bc6519f6
Eric Arellano <ericarellano@me.com>
2020-07-09 20:38:58 -0700

    Restore __eq__, but simplified
    We leave off __hash__, which means that it will default to __hash__ = None.
    
    # Rust tests will be skipped. Delete if not intended.
    [ci skip-rust-tests]

--- src/python/pants/engine/internals/target_adaptor.py
+++ src/python/pants/engine/internals/target_adaptor.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from typing import Any
+from typing import Any, Union
 
 from typing_extensions import final
 
@@ -18,3 +18,12 @@ class TargetAdaptor:
 
     def __repr__(self) -> str:
         return f"TargetAdaptor(type_alias={self.type_alias}, name={self.name})"
+
+    def __eq__(self, other: Union[Any, "TargetAdaptor"]) -> bool:
+        if not isinstance(other, TargetAdaptor):
+            return NotImplemented
+        return (
+            self.type_alias == other.type_alias
+            and self.name == other.name
+            and self.kwargs == other.kwargs
+        )
