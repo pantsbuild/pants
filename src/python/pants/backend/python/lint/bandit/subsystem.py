377550d42043f35d1dd990efd5d5842e5c690524
Eric Arellano <ericarellano@me.com>
2020-07-10 17:23:57 -0700

    Loosen the constraint so that Py2 still works
    # Rust tests will be skipped. Delete if not intended.
    [ci skip-rust-tests]

--- src/python/pants/backend/python/lint/bandit/subsystem.py
+++ src/python/pants/backend/python/lint/bandit/subsystem.py
@@ -10,9 +10,9 @@ class Bandit(PythonToolBase):
     options_scope = "bandit"
     default_version = "bandit>=1.6.2,<1.7"
     # `setuptools<45` is for Python 2 support. `stevedore` is because the 3.0 release breaks Bandit.
-    default_extra_requirements = ["setuptools<45", "stevedore>=2.0.1,<3"]
+    default_extra_requirements = ["setuptools<45", "stevedore<3"]
     default_entry_point = "bandit"
-    default_interpreter_constraints = ["CPython>=2.7,<3", "CPython>=3.0"]
+    default_interpreter_constraints = ["CPython>=2.7,<3", "CPython>=3.5"]
 
     @classmethod
     def register_options(cls, register):
