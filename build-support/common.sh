f583d182a246c3952e9157b97ba9271ad906aed8
John Sirois <john.sirois@gmail.com>
2020-07-11 22:38:49 -0600

    Modernize virtualenv. (#10330)
    We do this by creating a virtualenv pex at the desired version and
    using that. To do so efficiently, downloading and caching of a pex
    tool is also factored up to build-support/common.py.
    
    # Rust tests will be skipped. Delete if not intended.
    [ci skip-rust-tests]

--- build-support/common.sh
+++ build-support/common.sh
@@ -68,3 +68,75 @@ function git_merge_base() {
   # This prints the tracking branch if set and otherwise falls back to local "master".
   git rev-parse --symbolic-full-name --abbrev-ref HEAD@\{upstream\} 2>/dev/null || echo 'master'
 }
+
+function safe_curl() {
+  real_curl="$(command -v curl)"
+  set +e
+  "${real_curl}" --fail -SL "$@"
+  exit_code=$?
+  set -e
+  if [[ "${exit_code}" -ne 0 ]]; then
+    echo >&2 "Curl failed with args: $*"
+    exit 1
+  fi
+}
+
+function root_dir() {
+  cd "$(dirname "${BASH_SOURCE[0]}")" && cd "$(git rev-parse --show-toplevel)" && pwd
+}
+
+function requirement() {
+  package="$1"
+  grep "^${package}[^A-Za-z0-9]" "$(root_dir)/3rdparty/python/requirements.txt" || \
+    die "Could not find requirement for ${package}"
+}
+
+# URL from which pex release binaries can be downloaded.
+: "${PEX_DOWNLOAD_PREFIX:="https://github.com/pantsbuild/pex/releases/download"}"
+
+function run_pex() {
+  (
+    PEX_VERSION="$(requirement pex | sed -e "s|pex==||")"
+    PEX_DIR="$(root_dir)/build-support/pex/${PEX_VERSION}"
+    if ! [ -x "${PEX_DIR}/pex" ]; then
+      pex_url="${PEX_DOWNLOAD_PREFIX}/v${PEX_VERSION}/pex"
+      log "Downloading pex==${PEX_VERSION} from ${pex_url}..."
+
+      pexdir="$(mktemp -d -t download_pex.XXXXX)"
+      trap 'rm -rf "${pexdir}"' EXIT
+
+      safe_curl -s "${pex_url}" > "${pexdir}/pex" && \
+      chmod +x "${pexdir}/pex" && \
+      rm -rf "${PEX_DIR}" && \
+      mkdir -p "$(dirname "${PEX_DIR}")" && \
+      mv "${pexdir}" "${PEX_DIR}"
+    fi
+
+    "${PEX_DIR}/pex" "$@"
+  )
+}
+
+
+: "${VIRTUALENV_VERSION:="20.0.26"}"
+
+function run_virtualenv() {
+  (
+    VIRTUALENV_DIR="$(root_dir)/build-support/virtualenv/${VIRTUALENV_VERSION}"
+    if ! [ -x "${VIRTUALENV_DIR}/virtualenv.pex" ]; then
+      log "Building PEX for virtualenv==${VIRTUALENV_VERSION}..."
+      pexdir="$(mktemp -d -t virtualenv_pex.XXXXX)"
+      trap 'rm -rf "${pexdir}"' EXIT
+
+      run_pex \
+        --python="${PY}" \
+        "virtualenv==${VIRTUALENV_VERSION}" \
+        -c virtualenv \
+        -o "${pexdir}/virtualenv.pex" && \
+      rm -rf "${VIRTUALENV_DIR}" && \
+      mkdir -p "$(dirname "${VIRTUALENV_DIR}")" && \
+      mv "${pexdir}" "${VIRTUALENV_DIR}"
+    fi
+
+    "${VIRTUALENV_DIR}/virtualenv.pex" "$@"
+  )
+}
