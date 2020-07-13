206ffc7f7356b0a38b62c29cfc129dcd96a45777
gshuflin <greg.shuflin@toolchain.com>
2020-07-13 12:21:51 -0700

    Update src/python/pants/help/help_printer.py
    Co-authored-by: Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>

--- src/python/pants/help/help_printer.py
+++ src/python/pants/help/help_printer.py
@@ -149,7 +149,7 @@ class HelpPrinter:
         print("  [target] accepts two special forms:")
         print("    dir:  to include all targets in the specified directory.")
         print("    dir:: to include all targets found recursively under the directory.\n")
-        print("More documentation is available on the official website:\n  http://pantsbuild.org/")
+        print("More documentation is available at https://pantsbuild.org")
 
         print(self._format_help(GLOBAL_SCOPE, advanced))
 
