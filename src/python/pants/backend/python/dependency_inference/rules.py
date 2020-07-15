63546e3a8e35b01a75d02dba7036c2d1f541f28f
Eric Arellano <ericarellano@me.com>
2020-07-14 23:09:42 -0700

    Fix dependency inference handling of dependencies on self
    [ci skip-rust-tests]

--- src/python/pants/backend/python/dependency_inference/rules.py
+++ src/python/pants/backend/python/dependency_inference/rules.py
@@ -45,7 +45,10 @@ async def infer_python_dependencies(request: InferPythonDependencies) -> Inferre
     return InferredDependencies(
         owner.address
         for owner in owner_per_import
-        if owner.address and owner.address != request.sources_field.address
+        if (
+            owner.address
+            and owner.address.maybe_convert_to_base_target() != request.sources_field.address
+        )
     )
 
 
