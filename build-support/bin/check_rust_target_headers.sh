5a9b0d76515dcdf754605a10abed81f98d943d1b
Daniel Wagner-Hall <dawagner@gmail.com>
2020-07-16 21:58:55 +0100

    Update rust to 1.45.0
    As of this release, cargo will no-op if you try to re-install a package
    you already have installed, if you request an exact version match, so
    cargo-ensure-installed is no longer necessary.

--- build-support/bin/check_rust_target_headers.sh
+++ build-support/bin/check_rust_target_headers.sh
@@ -4,7 +4,7 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 cargo="${REPO_ROOT}/build-support/bin/native/cargo"
 
-"${cargo}" ensure-installed --package cargo-ensure-prefix --version 0.1.3
+"${cargo}" install cargo-ensure-prefix --version "=0.1.3"
 
 if ! out="$("${cargo}" ensure-prefix \
   --manifest-path="${REPO_ROOT}/src/rust/engine/Cargo.toml" \
