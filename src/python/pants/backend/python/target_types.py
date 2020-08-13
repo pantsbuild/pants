3a42a2acec10833c24950d1dc49c9f9f28f70ea8
Benjy Weinberger <benjyw@gmail.com>
2020-08-13 12:54:13 -0400

    Make zip_safe default to False.
    There are too many gotchas for it to be on by default.
    Notably, PEP420 doesn't appear to work properly in a
    zipped pex. We can document that you may try to turn it
    on for a first-time binary startup performance boost.
    
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/python/target_types.py
+++ src/python/pants/backend/python/target_types.py
@@ -165,10 +165,13 @@ class PexZipSafe(BoolField):
     """Whether or not this binary is safe to run in compacted (zip-file) form.
 
     If they are not zip safe, they will be written to disk prior to execution.
+
+    We default to False, because there are various gotchas with zipped pexes. Notably,
+    PEP420 implicit namespace packages don't appear to work in that environment.
     """
 
     alias = "zip_safe"
-    default = True
+    default = False
     value: bool
 
 
