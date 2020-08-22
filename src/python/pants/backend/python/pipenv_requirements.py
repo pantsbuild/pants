ca962e5e545fbabc4cb778eac6ea6ea784b82b56
Jan Paul Erkelens <jperkelens@gmail.com>
2020-08-22 16:25:07 -0400

    Adds a `pipenv_requirements` macro (#10654)
    Part of https://github.com/pantsbuild/pants/issues/10655.
    
    This macro will read a `Pipfile.lock` and convert everything into a `python_requirement_library`. It won't actually treat this as a true lockfile, i.e. it won't feed the results into the option `--python-setup-requirements-constraints`. That will need to come in a followup. Rather, this acts like the macro `python_requirements()`, just with a Pipfile.lock.

---
+++ src/python/pants/backend/python/pipenv_requirements.py
@@ -0,0 +1,88 @@
+# Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from json import load
+from pathlib import Path
+from typing import Iterable, Mapping, Optional
+
+from pkg_resources import Requirement
+
+from pants.base.build_environment import get_buildroot
+
+
+class PipenvRequirements:
+    """Translates a Pipenv.lock file into an equivalent set `python_requirement_library` targets.
+
+    You may also use the parameter `module_mapping` to teach Pants what modules each of your
+    requirements provide. For any requirement unspecified, Pants will default to the name of the
+    requirement. This setting is important for Pants to know how to convert your import
+    statements back into your dependencies. For example:
+
+        pipenv_requirements(
+          module_mapping={
+            "ansicolors": ["colors"],
+            "setuptools": ["pkg_resources"],
+          }
+        )
+    """
+
+    def __init__(self, parse_context):
+        self._parse_context = parse_context
+
+    def __call__(
+        self,
+        requirements_relpath: str = "Pipfile.lock",
+        module_mapping: Optional[Mapping[str, Iterable[str]]] = None,
+        pipfile_target: Optional[str] = None,
+    ) -> None:
+        """
+        :param requirements_relpath: The relpath from this BUILD file to the requirements file.
+            Defaults to a `Pipfile.lock` file sibling to the BUILD file.
+        :param module_mapping: a mapping of requirement names to a list of the modules they provide.
+            For example, `{"ansicolors": ["colors"]}`. Any unspecified requirements will use the
+            requirement name as the default module, e.g. "Django" will default to
+            `modules=["django"]`.
+        :param pipfile_target: a `_python_requirements_file` target to provide for cache invalidation
+        if the requirements_relpath value is not in the current rel_path
+        """
+
+        lock_info = {}
+
+        requirements_path = Path(
+            get_buildroot(), self._parse_context.rel_path, requirements_relpath
+        )
+        with open(requirements_path, "r") as fp:
+            lock_info = load(fp)
+
+        if pipfile_target:
+            requirements_dep = pipfile_target
+        else:
+            requirements_file_target_name = requirements_relpath
+            self._parse_context.create_object(
+                "_python_requirements_file",
+                name=requirements_file_target_name,
+                sources=[requirements_relpath],
+            )
+            requirements_dep = f":{requirements_file_target_name}"
+
+        requirements = {**lock_info.get("default", {}), **lock_info.get("develop", {})}
+        for req, info in requirements.items():
+            req_str = f"{req}{info.get('version','')}"
+            if info.get("markers"):
+                req_str += f";{info['markers']}"
+
+            parsed_req = Requirement.parse(req_str)
+
+            req_module_mapping = (
+                {parsed_req.project_name: module_mapping[parsed_req.project_name]}
+                if module_mapping and parsed_req.project_name in module_mapping
+                else None
+            )
+
+            self._parse_context.create_object(
+                "python_requirement_library",
+                name=parsed_req.project_name,
+                requirements=[parsed_req],
+                dependencies=[requirements_dep],
+                module_mapping=req_module_mapping,
+            )
