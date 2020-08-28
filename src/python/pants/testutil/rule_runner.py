8ec84ba9d94ccc1cea917ddd4b39d71c7c3cee81
Eric Arellano <ericarellano@me.com>
2020-08-28 14:01:46 -0700

    Missing copyright header
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/testutil/rule_runner.py
+++ src/python/pants/testutil/rule_runner.py
@@ -1,3 +1,6 @@
+# Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 import os
 from dataclasses import dataclass
 from io import StringIO
