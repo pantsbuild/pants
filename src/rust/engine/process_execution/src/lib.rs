7b5b0fbfd6bc46007063da1db0f9a5dacc054c70
Tom Dyas <tdyas@toolchain.com>
2020-08-31 09:41:10 -0700

    fmt
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/rust/engine/process_execution/src/lib.rs
+++ src/rust/engine/process_execution/src/lib.rs
@@ -26,7 +26,7 @@
 #![allow(clippy::new_without_default, clippy::new_ret_no_self)]
 // Arc<Mutex> can be more clear than needing to grok Orderings:
 #![allow(clippy::mutex_atomic)]
-#![type_length_limit="43757804"]
+#![type_length_limit = "43757804"]
 #[macro_use]
 extern crate derivative;
 
