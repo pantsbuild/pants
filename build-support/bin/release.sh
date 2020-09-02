b1c3601d511729e6ff18e16b0484855d7e3376fb
Eric Arellano <ericarellano@me.com>
2020-09-02 14:20:13 -0700

    Properly fix `release.sh`
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]

--- build-support/bin/release.sh
+++ build-support/bin/release.sh
@@ -143,8 +143,8 @@ function execute_packaged_pants_with_internal_backends() {
   pants \
     --no-verify-config \
     --no-pantsd \
-    --pythonpath="['pants-plugins/src/python']" \
-      --backend-packages="[\
+    --pythonpath="['pants-plugins']" \
+    --backend-packages="[\
         'pants.backend.awslambda.python',\
         'pants.backend.python',\
         'internal_plugins.releases',\
