e65cbf9a93611f04267fe7afbee99a966a76744a
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-09-02 18:41:08 -0700

    Fix the `testutil` wheel's imports not working due to namespace packages (#10725)
    Imports stopped working for `testutil` in 2.0.0.dev2 because we removed the line `__import__("pkg_resources").declare_namespace(__name__)` from `src/python/pants/__init__.py`, so we were no longer using namespace packages and we had two dists with the same namespace.
    
    This PR also fixes the `setup-py` goal to error if users are manually setting `namespace_packages` and `install_requires`. Pants manages that for users. Before, we were silently overwriting values they set, which was confusing.
    
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/backend/python/goals/setup_py.py
+++ src/python/pants/backend/python/goals/setup_py.py
@@ -200,7 +200,14 @@ class SetupKwargs:
             raise ValueError(f"Missing a `version` kwarg in the `provides` field for {address}.")
 
         if not _allow_banned_keys:
-            for arg in {"data_files", "package_dir", "package_data", "packages"}:
+            for arg in {
+                "data_files",
+                "namespace_packages",
+                "package_dir",
+                "package_data",
+                "packages",
+                "install_requires",
+            }:
                 if arg in kwargs:
                     raise ValueError(
                         f"{arg} cannot be set in the `provides` field for {address}, but it was "
@@ -211,7 +218,7 @@ class SetupKwargs:
         # would require that all values are immutable, and we may have lists and dictionaries as
         # values. It's too difficult/clunky to convert those all, then to convert them back out of
         # `FrozenDict`. We don't use JSON because it does not preserve data types like `tuple`.
-        self._pickled_bytes = pickle.dumps({k: v for k, v in sorted(kwargs.items())})
+        self._pickled_bytes = pickle.dumps({k: v for k, v in sorted(kwargs.items())}, protocol=4)
 
     @memoized_property
     def kwargs(self) -> Dict[str, Any]:
@@ -524,13 +531,19 @@ async def generate_chroot(request: SetupPyChrootRequest) -> SetupPyChroot:
     target = exported_target.target
     resolved_setup_kwargs = await Get(SetupKwargs, ExportedTarget, exported_target)
     setup_kwargs = resolved_setup_kwargs.kwargs.copy()
+    # NB: We are careful to not overwrite these values, but we also don't expect them to have been
+    # set. The user must have have gone out of their way to use a `SetupKwargs` plugin, and to have
+    # specified `SetupKwargs(_allow_banned_keys=True)`.
     setup_kwargs.update(
         {
-            "package_dir": {"": CHROOT_SOURCE_ROOT},
-            "packages": sources.packages,
-            "namespace_packages": sources.namespace_packages,
-            "package_data": dict(sources.package_data),
-            "install_requires": tuple(requirements),
+            "package_dir": {"": CHROOT_SOURCE_ROOT, **setup_kwargs.get("package_dir", {})},
+            "packages": (*sources.packages, *(setup_kwargs.get("packages", []))),
+            "namespace_packages": (
+                *sources.namespace_packages,
+                *setup_kwargs.get("namespace_packages", []),
+            ),
+            "package_data": {**dict(sources.package_data), **setup_kwargs.get("package_data", {})},
+            "install_requires": (*requirements, *setup_kwargs.get("install_requires", [])),
         }
     )
     key_to_binary_spec = exported_target.provides.binaries
