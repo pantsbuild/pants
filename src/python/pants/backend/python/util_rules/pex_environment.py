6647fc8b92089a50a5884c9856540a4a28ce38a0
John Sirois <john.sirois@gmail.com>
2020-09-13 09:24:03 -0700

    Support tests for BinaryPaths.
    A BinaryPathRequest can now include a test to run against each found
    binary to validate it works and optionally fingerprint it. Eventually
    fingerprinting the binary contents should be performed automatically
    and the optional test fingerprint mixed in with it when present as
    tracked by #10769.
    
    We leverage testing found binaries in PexEnvironment to ensure we find
    only interpreter paths compatible with Pex bootstrapping. This plugs an
    existing hole not yet encountered in the wild where a Python 2.6 binary
    (for example) could be chosen and then PEX file bootstrapping fail as a
    result. We additionally fingerprint interpreters passing the version
    range test to ensure we detect interpreter upgrades and pyenv shim
    switches. Even with the automatic hashing of binaries tracked in #10769
    working, we'd still need to do this in the pyenv shim case since the
    same shim script can redirect to different interpreters depending on
    configuration external to the shim script.
    
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/python/util_rules/pex_environment.py
+++ src/python/pants/backend/python/util_rules/pex_environment.py
@@ -3,6 +3,7 @@
 
 import os
 from dataclasses import dataclass
+from textwrap import dedent
 from typing import Iterable, Mapping, Optional, Tuple, cast
 
 from pants.core.util_rules import subprocess_environment
@@ -132,7 +133,7 @@ async def find_pex_python(
     pex_runtime_env: PexRuntimeEnvironment,
     subprocess_env_vars: SubprocessEnvironmentVars,
 ) -> PexEnvironment:
-    # PEX files are compatible with bootstrapping via python2.7 or python 3.5+. The bootstrap
+    # PEX files are compatible with bootstrapping via Python 2.7 or Python 3.5+. The bootstrap
     # code will then re-exec itself if the underlying PEX user code needs a more specific python
     # interpreter. As such, we look for many Pythons usable by the PEX bootstrap code here for
     # maximum flexibility.
@@ -141,7 +142,33 @@ async def find_pex_python(
             Get(
                 BinaryPaths,
                 BinaryPathRequest(
-                    search_path=python_setup.interpreter_search_paths, binary_name=binary_name
+                    search_path=python_setup.interpreter_search_paths,
+                    binary_name=binary_name,
+                    test_args=[
+                        "-c",
+                        # N.B.: The following code snippet must be compatible with Python 2.7 and
+                        # Python 3.5+.
+                        dedent(
+                            """\
+                            import sys
+
+                            major, minor = sys.version_info[:2]
+                            if (major, minor) == (2, 7) or (major == 3 and minor >= 5):
+                                import hashlib
+                                hasher = hashlib.sha256()
+                                with open(sys.executable, "rb") as fp:
+                                    # We pick 8192 for efficiency of reads and fingerprint updates
+                                    # (writes) since it's a common OS buffer size and an even
+                                    # multiple of the hash block size.
+                                    for chunk in iter(lambda: fp.read(8192), b""):
+                                        hasher.update(chunk)
+                                sys.stdout.write(hasher.hexdigest())
+                                sys.exit(0)
+                            else:
+                                sys.exit(1)
+                            """
+                        ),
+                    ],
                 ),
             )
             for binary_name in pex_runtime_env.bootstrap_interpreter_names
@@ -151,7 +178,7 @@ async def find_pex_python(
     def first_python_binary() -> Optional[str]:
         for binary_paths in all_python_binary_paths:
             if binary_paths.first_path:
-                return binary_paths.first_path
+                return binary_paths.first_path.path
         return None
 
     return PexEnvironment(
