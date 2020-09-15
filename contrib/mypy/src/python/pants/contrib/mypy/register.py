cb4fe5eaff819e83f34825f984064e9322e72283
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2020-09-15 11:16:23 -0700

    rename mypy subsystem and add "mypy" goal

--- contrib/mypy/src/python/pants/contrib/mypy/register.py
+++ contrib/mypy/src/python/pants/contrib/mypy/register.py
@@ -6,5 +6,15 @@ from pants.goal.task_registrar import TaskRegistrar as task
 from pants.contrib.mypy.tasks.mypy_task import MypyTask
 
 
+class MypyStandalone(MypyTask):
+    @classmethod
+    def register_options(cls, register):
+        super().register_options(register)
+        register('--skip', type=bool, default=False, help='Skip running mypy.')
+        register('--transitive', type=bool, default=False,
+                 help='Whether to run mypy on transitive dependencies of the given pythohn targets.')
+
+
 def register_goals():
     task(name="mypy", action=MypyTask).install("lint")
+    task(name="mypy", action=MypyStandalone).install()
