5240f0d4540948529a7a519edf544e965ff6a0c7
Stu Hood <stuhood@gmail.com>
2020-09-15 14:30:06 -0700

    Extract helpers in the process cache tests.
    [ci skip-build-wheels]

--- src/rust/engine/process_execution/src/cache_tests.rs
+++ src/rust/engine/process_execution/src/cache_tests.rs
@@ -4,7 +4,6 @@ use crate::{
 };
 use std::io::Write;
 use std::path::PathBuf;
-use std::sync::Arc;
 
 use sharded_lmdb::{ShardedLmdb, DEFAULT_LEASE_TIME};
 use store::Store;
@@ -17,41 +16,27 @@ struct RoundtripResults {
   maybe_cached: Result<FallibleProcessResultWithPlatform, String>,
 }
 
-async fn run_roundtrip(script_exit_code: i8) -> RoundtripResults {
+fn create_local_runner() -> (Box<dyn CommandRunnerTrait>, Store, TempDir) {
   let runtime = task_executor::Executor::new(Handle::current());
-  let work_dir = TempDir::new().unwrap();
-  let named_cache_dir = TempDir::new().unwrap();
-  let store_dir = TempDir::new().unwrap();
-  let store = Store::local_only(runtime.clone(), store_dir.path()).unwrap();
-  let local = crate::local::CommandRunner::new(
+  let base_dir = TempDir::new().unwrap();
+  let named_cache_dir = base_dir.path().join("named_cache_dir");
+  let store_dir = base_dir.path().join("store_dir");
+  let store = Store::local_only(runtime.clone(), store_dir).unwrap();
+  let runner = Box::new(crate::local::CommandRunner::new(
     store.clone(),
     runtime.clone(),
-    work_dir.path().to_owned(),
-    NamedCaches::new(named_cache_dir.path().to_owned()),
+    base_dir.path().to_owned(),
+    NamedCaches::new(named_cache_dir),
     true,
-  );
+  ));
+  (runner, store, base_dir)
+}
 
-  let script_dir = TempDir::new().unwrap();
-  let script_path = script_dir.path().join("script");
-  std::fs::File::create(&script_path)
-    .and_then(|mut file| {
-      writeln!(
-        file,
-        "echo -n {} > roland && echo Hello && echo >&2 World; exit {}",
-        TestData::roland().string(),
-        script_exit_code
-      )
-    })
-    .unwrap();
-
-  let request = Process::new(vec![
-    testutil::path::find_bash(),
-    format!("{}", script_path.display()),
-  ])
-  .output_files(vec![PathBuf::from("roland")].into_iter().collect());
-
-  let local_result = local.run(request.clone().into(), Context::default()).await;
-
+fn create_cached_runner(
+  local: Box<dyn CommandRunnerTrait>,
+  store: Store,
+) -> (Box<dyn CommandRunnerTrait>, TempDir) {
+  let runtime = task_executor::Executor::new(Handle::current());
   let cache_dir = TempDir::new().unwrap();
   let max_lmdb_size = 50 * 1024 * 1024; //50 MB - I didn't pick that number but it seems reasonable.
 
@@ -69,15 +54,49 @@ async fn run_roundtrip(script_exit_code: i8) -> RoundtripResults {
     platform_properties: vec![],
   };
 
-  let caching = crate::cache::CommandRunner::new(
-    Arc::new(local),
+  let runner = Box::new(crate::cache::CommandRunner::new(
+    local.into(),
     process_execution_store,
-    store.clone(),
+    store,
     metadata,
-  );
+  ));
+
+  (runner, cache_dir)
+}
+
+fn create_script(script_exit_code: i8) -> (Process, PathBuf, TempDir) {
+  let script_dir = TempDir::new().unwrap();
+  let script_path = script_dir.path().join("script");
+  std::fs::File::create(&script_path)
+    .and_then(|mut file| {
+      writeln!(
+        file,
+        "echo -n {} > roland && echo Hello && echo >&2 World; exit {}",
+        TestData::roland().string(),
+        script_exit_code
+      )
+    })
+    .unwrap();
+
+  let process = Process::new(vec![
+    testutil::path::find_bash(),
+    format!("{}", script_path.display()),
+  ])
+  .output_files(vec![PathBuf::from("roland")].into_iter().collect());
+
+  (process, script_path, script_dir)
+}
+
+async fn run_roundtrip(script_exit_code: i8) -> RoundtripResults {
+  let (local, store, _local_runner_dir) = create_local_runner();
+  let (process, script_path, _script_dir) = create_script(script_exit_code);
+
+  let local_result = local.run(process.clone().into(), Context::default()).await;
+
+  let (caching, _cache_dir) = create_cached_runner(local, store.clone());
 
   let uncached_result = caching
-    .run(request.clone().into(), Context::default())
+    .run(process.clone().into(), Context::default())
     .await;
 
   assert_eq!(local_result, uncached_result);
@@ -86,7 +105,7 @@ async fn run_roundtrip(script_exit_code: i8) -> RoundtripResults {
   // fail due to a FileNotFound error. So, If the second run succeeds, that implies that the
   // cache was successfully used.
   std::fs::remove_file(&script_path).unwrap();
-  let maybe_cached_result = caching.run(request.into(), Context::default()).await;
+  let maybe_cached_result = caching.run(process.into(), Context::default()).await;
 
   RoundtripResults {
     uncached: uncached_result,
