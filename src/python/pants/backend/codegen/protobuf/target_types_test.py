74eda8542bb84b551a1aa8a54a4d7ea69af46329
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-09-22 12:21:21 -0700

    Fix `setup_py.with_binaries()` to automatically inject `dependencies` (#10829)
    If you have a `python_binary` target in `setup_py().with_binaries()`, it's clear that you intend to depend on the target and for its files to be included in the result.
    
    Right now, you must explicitly add it to the `dependencies` field too, otherwise you will have the right `setup.py` entry, but missing files.
    
    This uses dependency injection (not inference) to automatically inject the dependencies for a `python_distribution` target. This approach is more robust than special casing in `setup_py.py`. For example, this ensures that our invalidation will work properly in all contexts, and that `./pants dependencies` includes it.
    
    [ci skip-rust]
    [ci skip-build-wheels]

---
+++ src/python/pants/backend/codegen/protobuf/target_types_test.py
@@ -0,0 +1,51 @@
+# Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from textwrap import dedent
+
+from pants.backend.codegen.protobuf.target_types import (
+    InjectProtobufDependencies,
+    ProtobufDependencies,
+    ProtobufLibrary,
+)
+from pants.backend.codegen.protobuf.target_types import rules as target_type_rules
+from pants.core.target_types import Files
+from pants.engine.addresses import Address
+from pants.engine.target import InjectedDependencies
+from pants.option.options_bootstrapper import OptionsBootstrapper
+from pants.testutil.option_util import create_options_bootstrapper
+from pants.testutil.rule_runner import QueryRule, RuleRunner
+
+
+def test_inject_dependencies() -> None:
+    rule_runner = RuleRunner(
+        rules=[
+            *target_type_rules(),
+            QueryRule(InjectedDependencies, (InjectProtobufDependencies, OptionsBootstrapper)),
+        ],
+        target_types=[ProtobufLibrary, Files],
+    )
+    # Note that injected deps can be any target type for `--protobuf-runtime-targets`.
+    rule_runner.add_to_build_file(
+        "protos",
+        dedent(
+            """\
+            protobuf_library()
+            files(name="injected_dep", sources=[])
+            """
+        ),
+    )
+    tgt = rule_runner.get_target(Address("protos"))
+    injected = rule_runner.request(
+        InjectedDependencies,
+        [
+            InjectProtobufDependencies(tgt[ProtobufDependencies]),
+            create_options_bootstrapper(
+                args=[
+                    "--backend-packages=pants.backend.codegen.protobuf.python",
+                    "--protoc-runtime-targets=protos:injected_dep",
+                ]
+            ),
+        ],
+    )
+    assert injected == InjectedDependencies([Address("protos", target_name="injected_dep")])
