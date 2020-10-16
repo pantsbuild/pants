846443139ff26e5328b0b2b39cd886aa95ab6413
Eric Arellano <ericarellano@me.com>
2020-10-16 16:23:31 -0700

    Remove deprecated global options like `--v2` and `--backend-packages2`
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/init/options_initializer.py
+++ src/python/pants/init/options_initializer.py
@@ -57,9 +57,9 @@ class BuildConfigInitializer:
 
         # Load plugins and backends.
         return load_backends_and_plugins(
-            self._bootstrap_options.plugins + self._bootstrap_options.plugins2,
+            self._bootstrap_options.plugins,
             self._working_set,
-            self._bootstrap_options.backend_packages + self._bootstrap_options.backend_packages2,
+            self._bootstrap_options.backend_packages,
         )
 
     def setup(self) -> BuildConfiguration:
