3d07e8489e19430d581a1853140965bb698f6459
Eric Arellano <ericarellano@me.com>
2020-10-16 15:16:31 -0700

    Remove deprecated `./pants filter` options
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/project_info/filter_targets.py
+++ src/python/pants/backend/project_info/filter_targets.py
@@ -2,10 +2,8 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 import re
-from functools import partial
 from typing import Callable, Pattern
 
-from pants.base.deprecated import resolve_conflicting_options
 from pants.engine.console import Console
 from pants.engine.goal import Goal, GoalSubsystem, LineOriented
 from pants.engine.rules import collect_rules, goal_rule
@@ -17,15 +15,6 @@ from pants.engine.target import (
     UnrecognizedTargetTypeException,
 )
 from pants.util.filtering import and_filters, create_filters
-
-TARGET_REMOVAL_MSG = (
-    "`--filter-target` was removed because it is similar to `--filter-address-regex`. If you still "
-    "need this feature, please message us on Slack (https://www.pantsbuild.org/docs/community)."
-)
-ANCESTOR_REMOVAL_MSG = (
-    "`--filter-ancestor` was removed because it is not trivial to implement. If you still need "
-    "this feature, please message us on Slack (https://www.pantsbuild.org/docs/community)."
-)
 
 
 class FilterSubsystem(LineOriented, GoalSubsystem):
@@ -63,38 +52,6 @@ class FilterSubsystem(LineOriented, GoalSubsystem):
             metavar="[+-]regex1,regex2,...",
             help="Filter on targets with tags matching these regexes.",
         )
-        register(
-            "--type",
-            type=list,
-            metavar="[+-]type1,type2,...",
-            help="Filter on these target types, e.g. `resources` or `python_library`.",
-            removal_version="2.1.0.dev0",
-            removal_hint="Use `--target-type` instead of `--type`.",
-        )
-        register(
-            "--regex",
-            type=list,
-            metavar="[+-]regex1,regex2,...",
-            help="Filter on target addresses matching these regexes.",
-            removal_version="2.1.0.dev0",
-            removal_hint="Use `--address-regex` instead of `--regex`.",
-        )
-        register(
-            "--target",
-            type=list,
-            metavar="[+-]spec1,spec2,...",
-            help="Filter on these target addresses.",
-            removal_version="2.1.0.dev0",
-            removal_hint=TARGET_REMOVAL_MSG,
-        )
-        register(
-            "--ancestor",
-            type=list,
-            metavar="[+-]spec1,spec2,...",
-            help="Filter on targets that these targets depend on.",
-            removal_version="2.1.0.dev0",
-            removal_hint=ANCESTOR_REMOVAL_MSG,
-        )
 
 
 class FilterGoal(Goal):
@@ -118,10 +75,6 @@ def filter_targets(
     console: Console,
     registered_target_types: RegisteredTargetTypes,
 ) -> FilterGoal:
-    if not filter_subsystem.options.is_default("target"):
-        raise ValueError(TARGET_REMOVAL_MSG)
-    if not filter_subsystem.options.is_default("ancestor"):
-        raise ValueError(ANCESTOR_REMOVAL_MSG)
 
     def filter_target_type(target_type: str) -> TargetFilter:
         if target_type not in registered_target_types.aliases:
@@ -136,20 +89,10 @@ def filter_targets(
         regex = compile_regex(tag_regex)
         return lambda tgt: any(bool(regex.search(tag)) for tag in tgt.get(Tags).value or ())
 
-    resolve_option = partial(
-        resolve_conflicting_options,
-        old_scope="filter",
-        new_scope="filter",
-        old_container=filter_subsystem.options,
-        new_container=filter_subsystem.options,
-    )
-    target_type = resolve_option(old_option="type", new_option="target_type")
-    address_regex = resolve_option(old_option="regex", new_option="address_regex")
-
     anded_filter: TargetFilter = and_filters(
         [
-            *(create_filters(target_type, filter_target_type)),
-            *(create_filters(address_regex, filter_address_regex)),
+            *(create_filters(filter_subsystem.options.target_type, filter_target_type)),
+            *(create_filters(filter_subsystem.options.address_regex, filter_address_regex)),
             *(create_filters(filter_subsystem.options.tag_regex, filter_tag_regex)),
         ]
     )
