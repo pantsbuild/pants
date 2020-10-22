a5adb91c38e4c90d7806dc72d391eed773b3782d
Thales Menato <thales@thamenato.dev>
2020-10-21 22:55:35 -0400

    Update GrpcPythonPlugin to have url_template and url_platform_mapping
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/codegen/protobuf/python/grpc_python_plugin.py
+++ src/python/pants/backend/codegen/protobuf/python/grpc_python_plugin.py
@@ -2,8 +2,6 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from pants.core.util_rules.external_tool import ExternalTool
-from pants.engine.platform import Platform
-from pants.util.enums import match
 
 
 class GrpcPythonPlugin(ExternalTool):
@@ -15,10 +13,11 @@ class GrpcPythonPlugin(ExternalTool):
         "1.32.0|darwin|b2db586656463841aa2fd4aab34fb6bd3ef887b522d80e4f2f292146c357f533|6215304",
         "1.32.0|linux |1af99df9bf733c17a75cbe379f3f9d9ff1627d8a8035ea057c3c78575afe1687|4965728",
     ]
-
-    def generate_url(self, plat: Platform) -> str:
-        plat_str = match(plat, {Platform.darwin: "macos", Platform.linux: "linux"})
-        return (
-            f"https://binaries.pantsbuild.org/bin/grpc_python_plugin/{self.version}/"
-            f"{plat_str}/x86_64/grpc_python_plugin"
-        )
+    default_url_template = (
+        "https://binaries.pantsbuild.org/bin/grpc_python_plugin/{version}/"
+        "{platform}/x86_64/grpc_python_plugin"
+    )
+    default_url_platform_mapping = {
+        "darwin": "macos",
+        "linux": "linux",
+    }
