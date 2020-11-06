fbe4c2f56ce90ceab707f0edcdb7ea730fa78d7f
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-11-06 09:42:32 -0700

    Add `./pants py-constraints --summary` for project overview (#11099)
    This is intended to give an overview of the current status of your project, and to help with prioritization for things like Python 3. In contrast, `./pants py-constraints` without `--summary` is intended for debugging on the local terminal.
    
    We export as a CSV and expect users to import into Excel or Google Sheets to analyze the data. For example, https://docs.google.com/spreadsheets/d/1B_2WNmXuXCMbhU6h8nwhSV3YdArJZtE_qODYcI2968o/edit?usp=sharing.
    
    We always run over the entire repo, rather than looking at user specs. This simplifies handling of subtargets and differences between file args vs. target args; both types of addresses are included in the analysis.
    
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/backend/python/mixed_interpreter_constraints/py_constraints_test.py
+++ src/python/pants/backend/python/mixed_interpreter_constraints/py_constraints_test.py
@@ -9,26 +9,19 @@ from pants.backend.python.mixed_interpreter_constraints.py_constraints import Py
 from pants.backend.python.mixed_interpreter_constraints.py_constraints import (
     rules as py_constraints_rules,
 )
-from pants.backend.python.target_types import PythonLibrary
+from pants.backend.python.target_types import PythonLibrary, PythonTests
 from pants.core.target_types import Files
 from pants.testutil.rule_runner import RuleRunner
 
 
 @pytest.fixture
 def rule_runner() -> RuleRunner:
-    return RuleRunner(rules=py_constraints_rules(), target_types=[Files, PythonLibrary])
+    return RuleRunner(
+        rules=py_constraints_rules(), target_types=[Files, PythonLibrary, PythonTests]
+    )
 
 
-def test_no_matches(rule_runner: RuleRunner, caplog) -> None:
-    rule_runner.create_file("f.txt")
-    rule_runner.add_to_build_file("", "files(name='tgt', sources=['f.txt'])")
-    result = rule_runner.run_goal_rule(PyConstraintsGoal, args=["f.txt"])
-    assert result.exit_code == 0
-    assert len(caplog.records) == 1
-    assert "No Python files/targets matched" in caplog.text
-
-
-def test_render_constraints(rule_runner: RuleRunner) -> None:
+def setup_project(rule_runner: RuleRunner) -> None:
     rule_runner.add_to_build_file(
         "lib1", "python_library(sources=[], interpreter_constraints=['==2.7.*', '>=3.5'])"
     )
@@ -55,6 +48,21 @@ def test_render_constraints(rule_runner: RuleRunner) -> None:
 
     rule_runner.set_options(["--python-setup-interpreter-constraints=['>=3.6']"])
 
+
+def test_no_matches(rule_runner: RuleRunner, caplog) -> None:
+    rule_runner.create_file("f.txt")
+    rule_runner.add_to_build_file("", "files(name='tgt', sources=['f.txt'])")
+    result = rule_runner.run_goal_rule(PyConstraintsGoal, args=["f.txt"])
+    assert result.exit_code == 0
+    assert len(caplog.records) == 1
+    assert (
+        "No Python files/targets matched for the `py-constraints` goal. All target types with "
+        "Python interpreter constraints: python_library, python_tests"
+    ) in caplog.text
+
+
+def test_render_constraints(rule_runner: RuleRunner) -> None:
+    setup_project(rule_runner)
     result = rule_runner.run_goal_rule(PyConstraintsGoal, args=["app"])
     assert result.stdout == dedent(
         """\
@@ -75,3 +83,18 @@ def test_render_constraints(rule_runner: RuleRunner) -> None:
     # If we run on >1 input, we include a warning about what the "final merged constraints" mean.
     result = rule_runner.run_goal_rule(PyConstraintsGoal, args=["app", "lib1"])
     assert "Consider using a more precise query" in result.stdout
+
+
+def test_constraints_summary(rule_runner: RuleRunner) -> None:
+    setup_project(rule_runner)
+    result = rule_runner.run_goal_rule(PyConstraintsGoal, args=["--summary"])
+    assert result.stdout == dedent(
+        """\
+        Target,Constraints,Transitive Constraints,# Dependencies,# Dependees\r
+        app,CPython==3.7.*,"CPython==2.7.*,==3.7.*,>=3.6 OR CPython==3.7.*,>=3.5,>=3.6",3,0\r
+        lib1,CPython==2.7.* OR CPython>=3.5,CPython==2.7.* OR CPython>=3.5,0,1\r
+        lib2,CPython>=3.6,CPython>=3.6,2,0\r
+        lib2/a.py,CPython>=3.6,CPython>=3.6,2,3\r
+        lib2/b.py,CPython>=3.6,CPython>=3.6,2,3\r
+        """
+    )
