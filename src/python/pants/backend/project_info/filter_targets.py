28a9876c7919b07558a8843d75e2551be35b45bd
Stu Hood <stuhood@gmail.com>
2020-11-11 09:20:44 -0800

    Add support for filtering file vs BUILD targets (#11126)
    ### Problem
    
    As described in #11050: currently there is no way to filter to select only the targets declared in BUILD files, or only file targets (those generated from the files owned by BUILD targets).
    
    ### Solution
    
    Add a flag to `--filter` to enable filtering to just file, just BUILD, or both. As mentioned on the ticket, the types of cases where this filtering are likely to be necessary will probably already be using the `filter` goal: the global filter options (`--tag`, `--exclude-target-regexp`) are more specialized shorthands.
    
    ### Result
    
    Usecases like:
    ```
    # Request file targets, and then strip off the owning target name using unix tools.
    ./pants --changed-since ... filter --granularity=file | cut -d: -f1 | sort | uniq
    # Request base targets, and then ask pants to expand targets to files.
    ./pants --changed-since ... filter --granularity=BUILD | xargs ./pants filedeps
    ```
    ... are enabled. Fixes #11050.
    
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/backend/project_info/filter_targets.py
+++ src/python/pants/backend/project_info/filter_targets.py
@@ -2,6 +2,7 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 import re
+from enum import Enum
 from typing import Callable, Pattern
 
 from pants.engine.console import Console
@@ -14,13 +15,20 @@ from pants.engine.target import (
     UnexpandedTargets,
     UnrecognizedTargetTypeException,
 )
+from pants.util.enums import match
 from pants.util.filtering import and_filters, create_filters
+
+
+class TargetGranularity(Enum):
+    all_targets = "all"
+    file_targets = "file"
+    build_targets = "BUILD"
 
 
 class FilterSubsystem(LineOriented, GoalSubsystem):
     """Filter the input targets based on various criteria.
 
-    Each of the filtering options below is a comma-separated list of filtering criteria, with an
+    Most of the filtering options below are comma-separated lists of filtering criteria, with an
     implied logical OR between them, so that a target passes the filter if it matches any of the
     criteria in the list.  A '-' prefix inverts the sense of the entire comma-separated list, so
     that a target passes the filter only if it matches none of the criteria in the list.
@@ -39,6 +47,15 @@ class FilterSubsystem(LineOriented, GoalSubsystem):
             type=list,
             metavar="[+-]type1,type2,...",
             help="Filter on these target types, e.g. `resources` or `python_library`.",
+        )
+        register(
+            "--granularity",
+            type=TargetGranularity,
+            default=TargetGranularity.all_targets,
+            help=(
+                "Filter to rendering only targets declared in BUILD files, only file-level "
+                "targets, or all targets."
+            ),
         )
         register(
             "--address-regex",
@@ -88,11 +105,22 @@ def filter_targets(
         regex = compile_regex(tag_regex)
         return lambda tgt: any(bool(regex.search(tag)) for tag in tgt.get(Tags).value or ())
 
+    def filter_granularity(granularity: TargetGranularity) -> TargetFilter:
+        return match(
+            granularity,
+            {
+                TargetGranularity.all_targets: lambda _: True,
+                TargetGranularity.file_targets: lambda tgt: not tgt.address.is_base_target,
+                TargetGranularity.build_targets: lambda tgt: tgt.address.is_base_target,
+            },
+        )
+
     anded_filter: TargetFilter = and_filters(
         [
             *(create_filters(filter_subsystem.options.target_type, filter_target_type)),
             *(create_filters(filter_subsystem.options.address_regex, filter_address_regex)),
             *(create_filters(filter_subsystem.options.tag_regex, filter_tag_regex)),
+            filter_granularity(filter_subsystem.options.granularity),
         ]
     )
     addresses = sorted(target.address for target in targets if anded_filter(target))
