d3de097b30ebde6304300605b5f659b8005a0a8c
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2020-11-16 12:23:38 -0700

    Port more tests from `TestBase` to `RuleRunner` (#11183)
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/core/goals/fmt_test.py
+++ src/python/pants/core/goals/fmt_test.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from textwrap import dedent
 from typing import ClassVar, List, Optional, Type, cast
 
+import pytest
+
 from pants.core.goals.fmt import (
     EnrichedFmtResult,
     Fmt,
@@ -21,8 +23,7 @@ from pants.engine.fs import EMPTY_DIGEST, Digest, FileContent, MergeDigests, Wor
 from pants.engine.target import Sources, Target, Targets
 from pants.engine.unions import UnionMembership
 from pants.testutil.option_util import create_goal_subsystem
-from pants.testutil.rule_runner import MockConsole, MockGet, run_rule_with_mocks
-from pants.testutil.test_base import TestBase
+from pants.testutil.rule_runner import MockConsole, MockGet, RuleRunner, run_rule_with_mocks
 from pants.util.logging import LogLevel
 
 
@@ -121,152 +122,166 @@ class InvalidTargets(MockLanguageTargets):
         )
 
 
-class FmtTest(TestBase):
-    def setUp(self) -> None:
-        super().setUp()
-        self.fortran_file = FileContent("formatted.f98", b"READ INPUT TAPE 5\n")
-        self.smalltalk_file = FileContent("formatted.st", b"y := self size + super size.')\n")
-        self.fortran_digest = self.make_snapshot(
-            {self.fortran_file.path: self.fortran_file.content.decode()}
-        ).digest
-        self.merged_digest = self.make_snapshot(
-            {fc.path: fc.content.decode() for fc in (self.fortran_file, self.smalltalk_file)}
-        ).digest
+FORTRAN_FILE = FileContent("formatted.f98", b"READ INPUT TAPE 5\n")
+SMALLTALK_FILE = FileContent("formatted.st", b"y := self size + super size.')\n")
 
-    @staticmethod
-    def make_target(
-        address: Optional[Address] = None, *, target_cls: Type[Target] = FortranTarget
-    ) -> Target:
-        return target_cls({}, address=address or Address("", target_name="tests"))
 
-    def run_fmt_rule(
-        self,
-        *,
-        language_target_collection_types: List[Type[LanguageFmtTargets]],
-        targets: List[Target],
-        result_digest: Digest,
-        per_file_caching: bool,
-        include_sources: bool = True,
-    ) -> str:
-        console = MockConsole(use_colors=False)
-        union_membership = UnionMembership({LanguageFmtTargets: language_target_collection_types})
-        result: Fmt = run_rule_with_mocks(
-            fmt,
-            rule_args=[
-                console,
-                Targets(targets),
-                create_goal_subsystem(
-                    FmtSubsystem, per_file_caching=per_file_caching, per_target_caching=False
+@pytest.fixture
+def rule_runner() -> RuleRunner:
+    # While we use `run_rule_with_mocks`, rather than `RuleRunner.request()`, we still need an
+    # instance of a Scheduler.
+    return RuleRunner()
+
+
+def fortran_digest(rule_runner: RuleRunner) -> Digest:
+    return rule_runner.make_snapshot({FORTRAN_FILE.path: FORTRAN_FILE.content.decode()}).digest
+
+
+def merged_digest(rule_runner: RuleRunner) -> Digest:
+    return rule_runner.make_snapshot(
+        {fc.path: fc.content.decode() for fc in (FORTRAN_FILE, SMALLTALK_FILE)}
+    ).digest
+
+
+def make_target(
+    address: Optional[Address] = None, *, target_cls: Type[Target] = FortranTarget
+) -> Target:
+    return target_cls({}, address=address or Address("", target_name="tests"))
+
+
+def run_fmt_rule(
+    rule_runner: RuleRunner,
+    *,
+    language_target_collection_types: List[Type[LanguageFmtTargets]],
+    targets: List[Target],
+    result_digest: Digest,
+    per_file_caching: bool,
+    include_sources: bool = True,
+) -> str:
+    console = MockConsole(use_colors=False)
+    union_membership = UnionMembership({LanguageFmtTargets: language_target_collection_types})
+    result: Fmt = run_rule_with_mocks(
+        fmt,
+        rule_args=[
+            console,
+            Targets(targets),
+            create_goal_subsystem(
+                FmtSubsystem, per_file_caching=per_file_caching, per_target_caching=False
+            ),
+            Workspace(rule_runner.scheduler),
+            union_membership,
+        ],
+        mock_gets=[
+            MockGet(
+                output_type=LanguageFmtResults,
+                input_type=LanguageFmtTargets,
+                mock=lambda language_targets_collection: language_targets_collection.language_fmt_results(
+                    result_digest
                 ),
-                Workspace(self.scheduler),
-                union_membership,
-            ],
-            mock_gets=[
-                MockGet(
-                    output_type=LanguageFmtResults,
-                    input_type=LanguageFmtTargets,
-                    mock=lambda language_targets_collection: language_targets_collection.language_fmt_results(
-                        result_digest
-                    ),
-                ),
-                MockGet(
-                    output_type=TargetsWithSources,
-                    input_type=TargetsWithSourcesRequest,
-                    mock=lambda tgts: TargetsWithSources(tgts if include_sources else ()),
-                ),
-                MockGet(
-                    output_type=Digest,
-                    input_type=MergeDigests,
-                    mock=lambda _: result_digest,
-                ),
-            ],
-            union_membership=union_membership,
+            ),
+            MockGet(
+                output_type=TargetsWithSources,
+                input_type=TargetsWithSourcesRequest,
+                mock=lambda tgts: TargetsWithSources(tgts if include_sources else ()),
+            ),
+            MockGet(
+                output_type=Digest,
+                input_type=MergeDigests,
+                mock=lambda _: result_digest,
+            ),
+        ],
+        union_membership=union_membership,
+    )
+    assert result.exit_code == 0
+    assert not console.stdout.getvalue()
+    return cast(str, console.stderr.getvalue())
+
+
+def assert_workspace_modified(
+    rule_runner: RuleRunner, *, fortran_formatted: bool, smalltalk_formatted: bool
+) -> None:
+    fortran_file = Path(rule_runner.build_root, FORTRAN_FILE.path)
+    smalltalk_file = Path(rule_runner.build_root, SMALLTALK_FILE.path)
+    if fortran_formatted:
+        assert fortran_file.is_file()
+        assert fortran_file.read_text() == FORTRAN_FILE.content.decode()
+    if smalltalk_formatted:
+        assert smalltalk_file.is_file()
+        assert smalltalk_file.read_text() == SMALLTALK_FILE.content.decode()
+
+
+def test_empty_target_noops(rule_runner: RuleRunner) -> None:
+    def assert_noops(*, per_file_caching: bool) -> None:
+        stderr = run_fmt_rule(
+            rule_runner,
+            language_target_collection_types=[FortranTargets],
+            targets=[make_target()],
+            result_digest=fortran_digest(rule_runner),
+            per_file_caching=per_file_caching,
+            include_sources=False,
         )
-        assert result.exit_code == 0
-        assert not console.stdout.getvalue()
-        return cast(str, console.stderr.getvalue())
+        assert stderr.strip() == ""
+        assert_workspace_modified(rule_runner, fortran_formatted=False, smalltalk_formatted=False)
 
-    def assert_workspace_modified(
-        self, *, fortran_formatted: bool, smalltalk_formatted: bool
-    ) -> None:
-        fortran_file = Path(self.build_root, self.fortran_file.path)
-        smalltalk_file = Path(self.build_root, self.smalltalk_file.path)
-        if fortran_formatted:
-            assert fortran_file.is_file()
-            assert fortran_file.read_text() == self.fortran_file.content.decode()
-        if smalltalk_formatted:
-            assert smalltalk_file.is_file()
-            assert smalltalk_file.read_text() == self.smalltalk_file.content.decode()
+    assert_noops(per_file_caching=False)
+    assert_noops(per_file_caching=True)
 
-    def test_empty_target_noops(self) -> None:
-        def assert_noops(*, per_file_caching: bool) -> None:
-            stderr = self.run_fmt_rule(
-                language_target_collection_types=[FortranTargets],
-                targets=[self.make_target()],
-                result_digest=self.fortran_digest,
-                per_file_caching=per_file_caching,
-                include_sources=False,
-            )
-            assert stderr.strip() == ""
-            self.assert_workspace_modified(fortran_formatted=False, smalltalk_formatted=False)
 
-        assert_noops(per_file_caching=False)
-        assert_noops(per_file_caching=True)
+def test_invalid_target_noops(rule_runner: RuleRunner) -> None:
+    def assert_noops(*, per_file_caching: bool) -> None:
+        stderr = run_fmt_rule(
+            rule_runner,
+            language_target_collection_types=[InvalidTargets],
+            targets=[make_target()],
+            result_digest=fortran_digest(rule_runner),
+            per_file_caching=per_file_caching,
+        )
+        assert stderr.strip() == ""
+        assert_workspace_modified(rule_runner, fortran_formatted=False, smalltalk_formatted=False)
 
-    def test_invalid_target_noops(self) -> None:
-        def assert_noops(*, per_file_caching: bool) -> None:
-            stderr = self.run_fmt_rule(
-                language_target_collection_types=[InvalidTargets],
-                targets=[self.make_target()],
-                result_digest=self.fortran_digest,
-                per_file_caching=per_file_caching,
-            )
-            assert stderr.strip() == ""
-            self.assert_workspace_modified(fortran_formatted=False, smalltalk_formatted=False)
+    assert_noops(per_file_caching=False)
+    assert_noops(per_file_caching=True)
 
-        assert_noops(per_file_caching=False)
-        assert_noops(per_file_caching=True)
 
-    def test_summary(self) -> None:
-        """Tests that the final summary is correct.
+def test_summary(rule_runner: RuleRunner) -> None:
+    """Tests that the final summary is correct.
 
-        This checks that we:
-        * Merge multiple results for the same formatter together (when you use
-            `--per-file-caching`).
-        * Correctly distinguish between skipped, changed, and did not change.
-        """
-        fortran_addresses = [
-            Address("", target_name="f1"),
-            Address("", target_name="needs_formatting"),
-        ]
-        smalltalk_addresses = [Address("", target_name="s1"), Address("", target_name="s2")]
+    This checks that we:
+    * Merge multiple results for the same formatter together (when you use
+        `--per-file-caching`).
+    * Correctly distinguish between skipped, changed, and did not change.
+    """
+    fortran_addresses = [
+        Address("", target_name="f1"),
+        Address("", target_name="needs_formatting"),
+    ]
+    smalltalk_addresses = [Address("", target_name="s1"), Address("", target_name="s2")]
 
-        fortran_targets = [
-            self.make_target(addr, target_cls=FortranTarget) for addr in fortran_addresses
-        ]
-        smalltalk_targets = [
-            self.make_target(addr, target_cls=SmalltalkTarget) for addr in smalltalk_addresses
-        ]
+    fortran_targets = [make_target(addr, target_cls=FortranTarget) for addr in fortran_addresses]
+    smalltalk_targets = [
+        make_target(addr, target_cls=SmalltalkTarget) for addr in smalltalk_addresses
+    ]
 
-        def assert_expected(*, per_file_caching: bool) -> None:
-            stderr = self.run_fmt_rule(
-                language_target_collection_types=[FortranTargets, SmalltalkTargets],
-                targets=[*fortran_targets, *smalltalk_targets],
-                result_digest=self.merged_digest,
-                per_file_caching=per_file_caching,
-            )
-            self.assert_workspace_modified(fortran_formatted=True, smalltalk_formatted=True)
-            assert stderr == dedent(
-                """\
+    def assert_expected(*, per_file_caching: bool) -> None:
+        stderr = run_fmt_rule(
+            rule_runner,
+            language_target_collection_types=[FortranTargets, SmalltalkTargets],
+            targets=[*fortran_targets, *smalltalk_targets],
+            result_digest=merged_digest(rule_runner),
+            per_file_caching=per_file_caching,
+        )
+        assert_workspace_modified(rule_runner, fortran_formatted=True, smalltalk_formatted=True)
+        assert stderr == dedent(
+            """\
 
-                𐄂 FortranConditionallyDidChange made changes.
-                ✓ SmalltalkDidNotChange made no changes.
-                - SmalltalkSkipped skipped.
-                """
-            )
+            𐄂 FortranConditionallyDidChange made changes.
+            ✓ SmalltalkDidNotChange made no changes.
+            - SmalltalkSkipped skipped.
+            """
+        )
 
-        assert_expected(per_file_caching=False)
-        assert_expected(per_file_caching=True)
+    assert_expected(per_file_caching=False)
+    assert_expected(per_file_caching=True)
 
 
 def test_streaming_output_skip() -> None:
