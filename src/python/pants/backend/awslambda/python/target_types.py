99568eb542cc9a58f176a4d80285899ea533c894
Eric Arellano <ericarellano@me.com>
2020-11-24 13:38:22 -0700

    Fix off-by-one error for `python_awslambda` `sources` field validation
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/awslambda/python/target_types.py
+++ src/python/pants/backend/awslambda/python/target_types.py
@@ -17,7 +17,7 @@ from pants.engine.target import (
 
 
 class PythonAwsLambdaSources(PythonSources):
-    expected_num_files = range(0, 1)
+    expected_num_files = range(0, 2)
 
 
 class PythonAwsLambdaDependencies(Dependencies):
