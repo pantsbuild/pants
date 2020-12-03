615737f2db30e14d9bc752c499b87e5dc26dde7c
Eric Arellano <ericarellano@me.com>
2020-12-02 19:18:38 -0700

    Add `handler=app.py:func` shorthand to `python_awslambda`
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/awslambda/python/target_types.py
+++ src/python/pants/backend/awslambda/python/target_types.py
@@ -1,6 +1,7 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import os.path
 import re
 from dataclasses import dataclass
 from typing import Match, Optional, Tuple, cast
@@ -10,6 +11,7 @@ from pants.backend.python.dependency_inference.rules import PythonInferSubsystem
 from pants.backend.python.target_types import InterpreterConstraintsField, PythonSources
 from pants.core.goals.package import OutputPathField
 from pants.engine.addresses import Address
+from pants.engine.fs import GlobMatchErrorBehavior, PathGlobs, Paths
 from pants.engine.rules import Get, collect_rules, rule
 from pants.engine.target import (
     COMMON_TARGET_FIELDS,
@@ -23,6 +25,7 @@ from pants.engine.target import (
     WrappedTarget,
 )
 from pants.engine.unions import UnionRule
+from pants.source.source_root import SourceRoot, SourceRootRequest
 
 
 class PythonAwsLambdaSources(PythonSources):
@@ -30,11 +33,25 @@ class PythonAwsLambdaSources(PythonSources):
 
 
 class PythonAwsLambdaHandlerField(StringField, AsyncFieldMixin):
-    """AWS Lambda handler entrypoint (module.dotted.name:handler_func)."""
+    """Entry point to the AWS Lambda handler.
+
+    You can specify a full module like 'path.to.module:handler_func' or use a shorthand to specify a
+    file name, using the same syntax as the `sources` field, e.g. 'lambda.py:handler_func'.
+    """
 
     alias = "handler"
     required = True
     value: str
+
+    @classmethod
+    def compute_value(cls, raw_value: Optional[str], *, address: Address) -> str:
+        value = cast(str, super().compute_value(raw_value, address=address))
+        if ":" not in value:
+            raise InvalidFieldException(
+                f"The `{cls.alias}` field in target at {address} must end in the "
+                f"format `:my_handler_func`, but was {value}."
+            )
+        return value
 
 
 @dataclass(frozen=True)
@@ -47,11 +64,49 @@ class ResolvePythonAwsHandlerRequest:
     field: PythonAwsLambdaHandlerField
 
 
-@rule
+@rule(desc="Determining the handler for a `python_awslambda` target")
 async def resolve_python_aws_handler(
     request: ResolvePythonAwsHandlerRequest,
 ) -> ResolvedPythonAwsHandler:
-    return ResolvedPythonAwsHandler(request.field.value)
+    handler_val = request.field.value
+    field_alias = request.field.alias
+    address = request.field.address
+    path, _, func = handler_val.partition(":")
+
+    # If it's already a module, simply use that. Otherwise, convert the file name into a module
+    # path.
+    if not path.endswith(".py"):
+        return ResolvedPythonAwsHandler(handler_val)
+
+    # We don't actually need to use the engine here, but we do this to validate that the file
+    # exists and that it resolves to only one file.
+    full_glob = os.path.join(address.spec_path, path)
+    handler_paths = await Get(
+        Paths,
+        PathGlobs(
+            [full_glob],
+            glob_match_error_behavior=GlobMatchErrorBehavior.error,
+            description_of_origin=f"{address}'s `{field_alias}` field",
+        ),
+    )
+    # We will have already raised if the glob did not match, i.e. if there were no files. But
+    # we need to check if they used a file glob (`*` or `**`) that resolved to >1 file.
+    if len(handler_paths.files) != 1:
+        raise InvalidFieldException(
+            f"Multiple files matched for the `{field_alias}` {repr(handler_val)} for the target "
+            f"{address}, but only one file expected. Are you using a glob, rather than a file "
+            f"name?\n\nAll matching files: {list(handler_paths.files)}."
+        )
+    handler_path = handler_paths.files[0]
+    source_root = await Get(
+        SourceRoot,
+        SourceRootRequest,
+        SourceRootRequest.for_file(handler_path),
+    )
+    stripped_source_path = os.path.relpath(handler_path, source_root.path)
+    module_base, _ = os.path.splitext(stripped_source_path)
+    normalized_path = module_base.replace(os.path.sep, ".")
+    return ResolvedPythonAwsHandler(f"{normalized_path}:{func}")
 
 
 class PythonAwsLambdaDependencies(Dependencies):
@@ -98,8 +153,8 @@ class PythonAwsLambdaRuntime(StringField):
         value = cast(str, super().compute_value(raw_value, address=address))
         if not re.match(cls.PYTHON_RUNTIME_REGEX, value):
             raise InvalidFieldException(
-                f"runtime field in python_awslambda target at {address.spec} must "
-                f"be of the form pythonX.Y, but was {value}"
+                f"The `{cls.alias}` field in target at {address} must be of the form pythonX.Y, "
+                f"but was {value}."
             )
         return value
 
