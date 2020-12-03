72722d641db3403209053da24f94180f8218162a
Eric Arellano <ericarellano@me.com>
2020-12-03 13:23:45 -0700

    Finish docstring
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/engine/target.py
+++ src/python/pants/engine/target.py
@@ -1483,9 +1483,14 @@ class SecondaryOwnerMixin(ABC):
     Why use this? In a dependency inference world, multiple targets including the same file in the
     `sources` field causes issues due to ambiguity over which target to use. So, only one target
     should have "primary ownership" of the file. However, you may still want other targets to be
-    used when that file is included in file arguments. Secondary ownership means that both
+    used when that file is included in file arguments. For example, a `python_library` target
+    being the primary owner of the `.py` file, but a `pex_binary` still working with file
+    arguments for that file. Secondary ownership means that the target won't be used for things like
+    dependency inference and hydrating sources, but file arguments will still work.
 
-    There should be a primary owner
+    There should be a primary owner of the file(s), e.g. the `python_library` in the above example.
+    Typically, you will want to add a dependency injection rule to infer a dep on that primary
+    owner.
 
     All associated files must live in the BUILD target's directory or a subdirectory to work
     properly, like the `sources` field.
