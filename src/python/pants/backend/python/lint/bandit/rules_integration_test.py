1da46a6d9022fa1c52f46d5bc8ba6a5632d8cd25
Asher Foa <1268088+asherf@users.noreply.github.com>
2020-12-08 08:27:34 -0800

    fmt

--- src/python/pants/backend/python/lint/bandit/rules_integration_test.py
+++ src/python/pants/backend/python/lint/bandit/rules_integration_test.py
@@ -59,8 +59,7 @@ def run_bandit(
     skip: bool = False,
     additional_args: Optional[List[str]] = None,
 ) -> Sequence[LintResult]:
-    args = ["--backend-packages=pants.backend.python.lint.bandit", 
-            "--bandit-version==1.6.2"]
+    args = ["--backend-packages=pants.backend.python.lint.bandit", "--bandit-version==1.6.2"]
     if config:
         rule_runner.create_file(relpath=".bandit", contents=config)
         args.append("--bandit-config=.bandit")
