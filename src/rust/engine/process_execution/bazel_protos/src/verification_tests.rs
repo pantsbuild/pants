c5be6bb3d2f6aaefe70103b4f49c0c2f49e12728
Tom Dyas <tdyas@toolchain.com>
2020-12-14 16:47:56 -0800

    port tests for bazel_protos crate

--- src/rust/engine/process_execution/bazel_protos/src/verification_tests.rs
+++ src/rust/engine/process_execution/bazel_protos/src/verification_tests.rs
@@ -1,8 +1,7 @@
-use crate::remote_execution::{Digest, Directory, DirectoryNode, FileNode};
-use crate::verify_directory_canonical;
-
 use hashing::EMPTY_DIGEST;
-use protobuf::Message;
+
+use crate::gen::build::bazel::remote::execution::v2::{Digest, Directory, DirectoryNode, FileNode};
+use crate::verify_directory_canonical;
 
 const HASH: &str = "693d8db7b05e99c6b7a7c0616456039d89c555029026936248085193559a0b5d";
 const FILE_SIZE: i64 = 16;
@@ -16,109 +15,102 @@ const OTHER_DIRECTORY_SIZE: i64 = 0;
 fn empty_directory() {
   assert_eq!(
     Ok(()),
-    verify_directory_canonical(EMPTY_DIGEST, &Directory::new())
+    verify_directory_canonical(EMPTY_DIGEST, &Directory::default())
   );
 }
 
 #[test]
 fn canonical_directory() {
-  let mut directory = Directory::new();
-  directory.mut_files().push({
-    let mut file = FileNode::new();
-    file.set_name("roland".to_owned());
-    file.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(FILE_SIZE);
-      digest.set_hash(HASH.to_owned());
-      digest
-    });
-    file
-  });
-  directory.mut_files().push({
-    let mut file = FileNode::new();
-    file.set_name("simba".to_owned());
-    file.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(FILE_SIZE);
-      digest.set_hash(HASH.to_owned());
-      digest
-    });
-    file
-  });
-  directory.mut_directories().push({
-    let mut dir = DirectoryNode::new();
-    dir.set_name("cats".to_owned());
-    dir.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(DIRECTORY_SIZE);
-      digest.set_hash(DIRECTORY_HASH.to_owned());
-      digest
-    });
-    dir
-  });
-  directory.mut_directories().push({
-    let mut dir = DirectoryNode::new();
-    dir.set_name("dogs".to_owned());
-    dir.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(OTHER_DIRECTORY_SIZE);
-      digest.set_hash(OTHER_DIRECTORY_HASH.to_owned());
-      digest
-    });
-    dir
-  });
+  let directory = Directory {
+    files: vec![
+      FileNode {
+        name: "roland".to_owned(),
+        digest: Some(Digest {
+          hash: HASH.to_owned(),
+          size_bytes: FILE_SIZE,
+        }),
+        ..FileNode::default()
+      },
+      FileNode {
+        name: "simba".to_owned(),
+        digest: Some(Digest {
+          hash: HASH.to_owned(),
+          size_bytes: FILE_SIZE,
+        }),
+        ..FileNode::default()
+      },
+    ],
+    directories: vec![
+      DirectoryNode {
+        name: "cats".to_owned(),
+        digest: Some(Digest {
+          hash: DIRECTORY_HASH.to_owned(),
+          size_bytes: DIRECTORY_SIZE,
+        }),
+      },
+      DirectoryNode {
+        name: "dogs".to_owned(),
+        digest: Some(Digest {
+          hash: OTHER_DIRECTORY_HASH.to_owned(),
+          size_bytes: OTHER_DIRECTORY_SIZE,
+        }),
+      },
+    ],
+    ..Directory::default()
+  };
+
   assert_eq!(Ok(()), verify_directory_canonical(EMPTY_DIGEST, &directory));
 }
 
-#[test]
-fn unknown_field() {
-  let mut directory = Directory::new();
-  directory.mut_unknown_fields().add_fixed32(42, 42);
-  let error = verify_directory_canonical(EMPTY_DIGEST, &directory).expect_err("Want error");
-  assert!(
-    error.contains("unknown"),
-    format!("Bad error message: {}", error)
-  );
-}
-
-#[test]
-fn unknown_field_in_file_node() {
-  let mut directory = Directory::new();
-
-  directory.mut_files().push({
-    let mut file = FileNode::new();
-    file.set_name("roland".to_owned());
-    file.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(FILE_SIZE);
-      digest.set_hash(HASH.to_owned());
-      digest
-    });
-    file.mut_unknown_fields().add_fixed32(42, 42);
-    file
-  });
-
-  let error = verify_directory_canonical(EMPTY_DIGEST, &directory).expect_err("Want error");
-  assert!(
-    error.contains("unknown"),
-    format!("Bad error message: {}", error)
-  );
-}
+// TODO(tonic/prost): Does Prost allow access to unknown fields?
+//
+// #[test]
+// fn unknown_field() {
+//   let mut directory = Directory::new();
+//   directory.mut_unknown_fields().add_fixed32(42, 42);
+//   let error = verify_directory_canonical(EMPTY_DIGEST, &directory).expect_err("Want error");
+//   assert!(
+//     error.contains("unknown"),
+//     format!("Bad error message: {}", error)
+//   );
+// }
+//
+// #[test]
+// fn unknown_field_in_file_node() {
+//   let mut directory = Directory::new();
+//
+//   directory.mut_files().push({
+//     let mut file = FileNode::new();
+//     file.set_name("roland".to_owned());
+//     file.set_digest({
+//       let mut digest = Digest::new();
+//       digest.set_size_bytes(FILE_SIZE);
+//       digest.set_hash(HASH.to_owned());
+//       digest
+//     });
+//     file.mut_unknown_fields().add_fixed32(42, 42);
+//     file
+//   });
+//
+//   let error = verify_directory_canonical(EMPTY_DIGEST, &directory).expect_err("Want error");
+//   assert!(
+//     error.contains("unknown"),
+//     format!("Bad error message: {}", error)
+//   );
+// }
 
 #[test]
 fn empty_child_name() {
-  let mut directory = Directory::new();
-  directory.mut_directories().push({
-    let mut dir = DirectoryNode::new();
-    dir.set_name("".to_owned());
-    dir.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(DIRECTORY_SIZE);
-      digest.set_hash(DIRECTORY_HASH.to_owned());
-      digest
-    });
-    dir
-  });
+  let directory = Directory {
+    directories: vec![DirectoryNode {
+      name: "".to_owned(),
+      digest: Some(Digest {
+        hash: DIRECTORY_HASH.to_owned(),
+        size_bytes: DIRECTORY_SIZE,
+      }),
+    }],
+    ..Directory::default()
+  };
 
   let error = verify_directory_canonical(EMPTY_DIGEST, &directory).expect_err("Want error");
   assert!(
@@ -129,18 +121,16 @@ fn empty_child_name() {
 
 #[test]
 fn multiple_path_segments_in_directory() {
-  let mut directory = Directory::new();
-  directory.mut_directories().push({
-    let mut dir = DirectoryNode::new();
-    dir.set_name("pets/cats".to_owned());
-    dir.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(DIRECTORY_SIZE);
-      digest.set_hash(DIRECTORY_HASH.to_owned());
-      digest
-    });
-    dir
-  });
+  let directory = Directory {
+    directories: vec![DirectoryNode {
+      name: "pets/cats".to_owned(),
+      digest: Some(Digest {
+        hash: DIRECTORY_HASH.to_owned(),
+        size_bytes: DIRECTORY_SIZE,
+      }),
+    }],
+    ..Directory::default()
+  };
 
   let error = verify_directory_canonical(EMPTY_DIGEST, &directory).expect_err("Want error");
   assert!(
@@ -151,18 +141,17 @@ fn multiple_path_segments_in_directory() {
 
 #[test]
 fn multiple_path_segments_in_file() {
-  let mut directory = Directory::new();
-  directory.mut_files().push({
-    let mut file = FileNode::new();
-    file.set_name("cats/roland".to_owned());
-    file.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(FILE_SIZE);
-      digest.set_hash(HASH.to_owned());
-      digest
-    });
-    file
-  });
+  let directory = Directory {
+    files: vec![FileNode {
+      name: "cats/roland".to_owned(),
+      digest: Some(Digest {
+        hash: HASH.to_owned(),
+        size_bytes: FILE_SIZE,
+      }),
+      ..FileNode::default()
+    }],
+    ..Directory::default()
+  };
 
   let error = verify_directory_canonical(EMPTY_DIGEST, &directory).expect_err("Want error");
   assert!(
@@ -173,29 +162,25 @@ fn multiple_path_segments_in_file() {
 
 #[test]
 fn duplicate_path_in_directory() {
-  let mut directory = Directory::new();
-  directory.mut_directories().push({
-    let mut dir = DirectoryNode::new();
-    dir.set_name("cats".to_owned());
-    dir.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(DIRECTORY_SIZE);
-      digest.set_hash(DIRECTORY_HASH.to_owned());
-      digest
-    });
-    dir
-  });
-  directory.mut_directories().push({
-    let mut dir = DirectoryNode::new();
-    dir.set_name("cats".to_owned());
-    dir.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(DIRECTORY_SIZE);
-      digest.set_hash(DIRECTORY_HASH.to_owned());
-      digest
-    });
-    dir
-  });
+  let directory = Directory {
+    directories: vec![
+      DirectoryNode {
+        name: "cats".to_owned(),
+        digest: Some(Digest {
+          hash: DIRECTORY_HASH.to_owned(),
+          size_bytes: DIRECTORY_SIZE,
+        }),
+      },
+      DirectoryNode {
+        name: "cats".to_owned(),
+        digest: Some(Digest {
+          hash: DIRECTORY_HASH.to_owned(),
+          size_bytes: DIRECTORY_SIZE,
+        }),
+      },
+    ],
+    ..Directory::default()
+  };
 
   let error = verify_directory_canonical(EMPTY_DIGEST, &directory).expect_err("Want error");
   assert!(
@@ -206,29 +191,27 @@ fn duplicate_path_in_directory() {
 
 #[test]
 fn duplicate_path_in_file() {
-  let mut directory = Directory::new();
-  directory.mut_files().push({
-    let mut file = FileNode::new();
-    file.set_name("roland".to_owned());
-    file.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(FILE_SIZE);
-      digest.set_hash(HASH.to_owned());
-      digest
-    });
-    file
-  });
-  directory.mut_files().push({
-    let mut file = FileNode::new();
-    file.set_name("roland".to_owned());
-    file.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(FILE_SIZE);
-      digest.set_hash(HASH.to_owned());
-      digest
-    });
-    file
-  });
+  let directory = Directory {
+    files: vec![
+      FileNode {
+        name: "roland".to_owned(),
+        digest: Some(Digest {
+          hash: HASH.to_owned(),
+          size_bytes: FILE_SIZE,
+        }),
+        ..FileNode::default()
+      },
+      FileNode {
+        name: "roland".to_owned(),
+        digest: Some(Digest {
+          hash: HASH.to_owned(),
+          size_bytes: FILE_SIZE,
+        }),
+        ..FileNode::default()
+      },
+    ],
+    ..Directory::default()
+  };
 
   let error = verify_directory_canonical(EMPTY_DIGEST, &directory).expect_err("Want error");
   assert!(
@@ -239,83 +222,74 @@ fn duplicate_path_in_file() {
 
 #[test]
 fn duplicate_path_in_file_and_directory() {
-  let mut directory = Directory::new();
-  directory.mut_files().push({
-    let mut file = FileNode::new();
-    file.set_name("roland".to_owned());
-    file.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(FILE_SIZE);
-      digest.set_hash(HASH.to_owned());
-      digest
-    });
-    file
-  });
-  directory.mut_directories().push({
-    let mut dir = DirectoryNode::new();
-    dir.set_name("roland".to_owned());
-    dir.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(DIRECTORY_SIZE);
-      digest.set_hash(DIRECTORY_HASH.to_owned());
-      digest
-    });
-    dir
-  });
+  let directory = Directory {
+    files: vec![FileNode {
+      name: "roland".to_owned(),
+      digest: Some(Digest {
+        hash: HASH.to_owned(),
+        size_bytes: FILE_SIZE,
+      }),
+      ..FileNode::default()
+    }],
+    directories: vec![DirectoryNode {
+      name: "roland".to_owned(),
+      digest: Some(Digest {
+        hash: DIRECTORY_HASH.to_owned(),
+        size_bytes: DIRECTORY_SIZE,
+      }),
+    }],
+    ..Directory::default()
+  };
 
   verify_directory_canonical(EMPTY_DIGEST, &directory).expect_err("Want error");
 }
 
 #[test]
+#[ignore] // this test does nothing
 fn unsorted_path_in_directory() {
-  let mut directory = Directory::new();
-  directory.mut_directories().push({
-    let mut dir = DirectoryNode::new();
-    dir.set_name("dogs".to_owned());
-    dir.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(DIRECTORY_SIZE);
-      digest.set_hash(DIRECTORY_HASH.to_owned());
-      digest
-    });
-    dir
-  });
-  directory.mut_directories().push({
-    let mut dir = DirectoryNode::new();
-    dir.set_name("cats".to_owned());
-    dir.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(DIRECTORY_SIZE);
-      digest.set_hash(DIRECTORY_HASH.to_owned());
-      digest
-    });
-    dir
-  });
+  let directory = Directory {
+    directories: vec![
+      DirectoryNode {
+        name: "dogs".to_owned(),
+        digest: Some(Digest {
+          hash: DIRECTORY_HASH.to_owned(),
+          size_bytes: DIRECTORY_SIZE,
+        }),
+      },
+      DirectoryNode {
+        name: "cats".to_owned(),
+        digest: Some(Digest {
+          hash: DIRECTORY_HASH.to_owned(),
+          size_bytes: DIRECTORY_SIZE,
+        }),
+      },
+    ],
+    ..Directory::default()
+  };
 }
 
 #[test]
+#[ignore] // this test does nothing
 fn unsorted_path_in_file() {
-  let mut directory = Directory::new();
-  directory.mut_files().push({
-    let mut file = FileNode::new();
-    file.set_name("simba".to_owned());
-    file.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(DIRECTORY_SIZE);
-      digest.set_hash(DIRECTORY_HASH.to_owned());
-      digest
-    });
-    file
-  });
-  directory.mut_files().push({
-    let mut file = FileNode::new();
-    file.set_name("roland".to_owned());
-    file.set_digest({
-      let mut digest = Digest::new();
-      digest.set_size_bytes(FILE_SIZE);
-      digest.set_hash(HASH.to_owned());
-      digest
-    });
-    file
-  });
+  let directory = Directory {
+    files: vec![
+      FileNode {
+        name: "simba".to_owned(),
+        digest: Some(Digest {
+          hash: DIRECTORY_HASH.to_owned(), // why is this not FILE_HASH?
+          size_bytes: DIRECTORY_SIZE,
+        }),
+        ..FileNode::default()
+      },
+      FileNode {
+        name: "roland".to_owned(),
+        digest: Some(Digest {
+          hash: HASH.to_owned(), // why is this not FILE_HASH?
+          size_bytes: FILE_SIZE,
+        }),
+        ..FileNode::default()
+      },
+    ],
+    ..Directory::default()
+  };
 }
