095fa30b0ab9bff535b48717bae278e7c281ae21
Josh Reed <jriddy@gmail.com>
2020-12-19 14:02:34 -0500

    complete render raw output

--- src/python/pants/backend/project_info/peek.py
+++ src/python/pants/backend/project_info/peek.py
@@ -1,12 +1,14 @@
 # Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import os
 from enum import Enum
-from typing import Iterable, cast
+from itertools import repeat
+from typing import Iterable, TypeVar, Union, cast
 
 from pants.engine.addresses import Address, Addresses, BuildFileAddress
 from pants.engine.console import Console
-from pants.engine.fs import DigestContents, PathGlobs
+from pants.engine.fs import DigestContents, FileContent, PathGlobs
 from pants.engine.goal import Goal, GoalSubsystem
 from pants.engine.rules import Get, MultiGet, collect_rules, goal_rule
 from pants.engine.target import Target, Targets
@@ -46,6 +48,32 @@ class Peek(Goal):
     subsystem_cls = PeekSubsystem
 
 
+_T = TypeVar("_T")
+_U = TypeVar("_U")
+
+_nothing = object()
+
+
+def _intersperse(x: _T, ys: Iterable[_U]) -> Iterable[Union[_T, _U]]:
+    it = iter(ys)
+    y = next(it, _nothing)
+    if y is _nothing:
+        return
+    yield cast(_U, y)
+    for x, y in zip(repeat(x), ys):
+        yield x
+        yield y
+
+
+def _render_raw(fc: FileContent, encoding: str = "utf-8") -> str:
+    dashes = "-" * len(fc.path)
+    content = fc.content.decode(encoding)
+    parts = [dashes, fc.path, dashes, content]
+    if not content.endswith(os.linesep):
+        parts.append("")
+    return os.linesep.join(parts)
+
+
 @goal_rule
 async def peek(
     console: Console,
@@ -59,15 +87,12 @@ async def peek(
         Get(BuildFileAddress, Address, t.address) for t in targets
     )
     build_file_paths = {a.rel_path for a in build_file_addresses}
-    digests = await Get(DigestContents, PathGlobs(build_file_paths))
+    digest_contents = await Get(DigestContents, PathGlobs(build_file_paths))
 
     # TODO: programmatically determine correct encoding
-    encoding = "utf-8"
-    if len(digests) == 1:
-        content = digests[0].content.decode(encoding)
-        console.print_stdout(content)
-    else:
-        raise NotImplementedError("do it")
+    rendereds = map(_render_raw, digest_contents)
+    for output in _intersperse("\n\n", rendereds):
+        console.print_stdout(output)
 
     return Peek(exit_code=0)
 
