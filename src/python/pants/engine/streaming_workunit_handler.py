eb0497d93ba8c58f78d8d2e1448f02c8566bafda
Tom Dyas <tdyas@toolchain.com>
2021-01-04 17:21:24 -0800

    fix signature
    [ci skip-build-wheels]

--- src/python/pants/engine/streaming_workunit_handler.py
+++ src/python/pants/engine/streaming_workunit_handler.py
@@ -43,7 +43,7 @@ class StreamingWorkunitContext:
         ByteStore, if it exists, has a copy of the files fingerprinted by each Digest."""
         return self._scheduler.ensure_remote_has_recursive(digests)
 
-    def get_observation_histograms(self) -> Tuple[int, Dict[str, bytes]]:
+    def get_observation_histograms(self) -> Dict[str, Any]:
         """Invoke the internal get_observation_histograms function, which serializes histograms
         generated from Pants-internal observation metrics observed during the current run of Pants.
 
