f3410faf3f0c760c92ed9f13adf1454c2fc891b6
Greg Shuflin <greg.shuflin@toolchain.com>
2021-01-08 13:42:48 -0800

    Use self-contained fake buildroot
    [ci skip-build-wheels]

--- src/python/pants/goal/run_tracker_test.py
+++ src/python/pants/goal/run_tracker_test.py
@@ -9,17 +9,19 @@ from freezegun import freeze_time
 from pants.base.exiter import PANTS_SUCCEEDED_EXIT_CODE
 from pants.goal.run_tracker import RunTracker
 from pants.testutil.option_util import create_options_bootstrapper
+from pants.util.contextutil import environment_as, temporary_dir
 
 
 @freeze_time(datetime.datetime(2020, 1, 1, 12, 0, 0), as_kwarg="frozen_time")
 def test_run_tracker_timing_output(**kwargs) -> None:
-    open("BUILDROOT", "w")
-    run_tracker = RunTracker(create_options_bootstrapper([]).bootstrap_options)
-    run_tracker.start(run_start_time=time.time())
-    frozen_time = kwargs["frozen_time"]
-    frozen_time.tick(delta=datetime.timedelta(seconds=1))
-    run_tracker.end_run(PANTS_SUCCEEDED_EXIT_CODE)
+    with temporary_dir() as buildroot:
+        with environment_as(PANTS_BUILDROOT_OVERRIDE=buildroot):
+            run_tracker = RunTracker(create_options_bootstrapper([]).bootstrap_options)
+            run_tracker.start(run_start_time=time.time())
+            frozen_time = kwargs["frozen_time"]
+            frozen_time.tick(delta=datetime.timedelta(seconds=1))
+            run_tracker.end_run(PANTS_SUCCEEDED_EXIT_CODE)
 
-    timings = run_tracker.get_cumulative_timings()
-    assert timings[0]["label"] == "main"
-    assert timings[0]["timing"] == 1.0
+            timings = run_tracker.get_cumulative_timings()
+            assert timings[0]["label"] == "main"
+            assert timings[0]["timing"] == 1.0
