d221f77a9012ee605bd3d203aa37a2d827db70c3
Greg Shuflin <greg.shuflin@toolchain.com>
2021-01-08 11:06:23 -0800

    Use decorator for frozen time
    [ci skip-build-wheels]

--- src/python/pants/goal/run_tracker_test.py
+++ src/python/pants/goal/run_tracker_test.py
@@ -11,14 +11,15 @@ from pants.goal.run_tracker import RunTracker
 from pants.testutil.option_util import create_options_bootstrapper
 
 
-def test_run_tracker_timing_output() -> None:
-    with freeze_time(datetime.datetime(2020, 1, 1, 12, 0, 0)) as frozen_time:
-        open("BUILDROOT", "w")
-        run_tracker = RunTracker(create_options_bootstrapper([]).bootstrap_options)
-        run_tracker.start(run_start_time=time.time())
-        frozen_time.tick(delta=datetime.timedelta(seconds=1))
-        run_tracker.end_run(PANTS_SUCCEEDED_EXIT_CODE)
+@freeze_time(datetime.datetime(2020, 1, 1, 12, 0, 0), as_kwarg="frozen_time")
+def test_run_tracker_timing_output(**kwargs) -> None:
+    open("BUILDROOT", "w")
+    run_tracker = RunTracker(create_options_bootstrapper([]).bootstrap_options)
+    run_tracker.start(run_start_time=time.time())
+    frozen_time = kwargs["frozen_time"]
+    frozen_time.tick(delta=datetime.timedelta(seconds=1))
+    run_tracker.end_run(PANTS_SUCCEEDED_EXIT_CODE)
 
-        timings = run_tracker.get_cumulative_timings()
-        assert timings[0]["label"] == "main"
-        assert timings[0]["timing"] == 1.0
+    timings = run_tracker.get_cumulative_timings()
+    assert timings[0]["label"] == "main"
+    assert timings[0]["timing"] == 1.0
