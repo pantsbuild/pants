0d07a4b486dca634507d68531783faf9bb56a64e
John Sirois <john.sirois@gmail.com>
2021-01-10 18:15:03 -0800

    Document address storage in AsyncFieldMixin. (#11448)
    Without a note there is every reason to undo storage in AsyncFieldMixin
    and push the address into Field in a refactoring commit.

--- src/python/pants/engine/target.py
+++ src/python/pants/engine/target.py
@@ -227,6 +227,9 @@ class AsyncFieldMixin(Field):
         # We must temporarily unfreeze the field, but then we refreeze to continue avoiding
         # subclasses from adding arbitrary fields.
         self._unfreeze_instance()  # type: ignore[attr-defined]
+        # N.B.: We store the address here and not in the Field base class, because the memory usage
+        # of storing this value in every field was shown to be excessive / lead to performance
+        # issues.
         self.address = address
         self._freeze_instance()  # type: ignore[attr-defined]
 
