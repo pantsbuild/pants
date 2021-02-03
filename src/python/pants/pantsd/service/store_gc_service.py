2c3fec3d160918f15d17f4d46a9ae27bfad94f70
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-02-03 10:31:42 -0800

    Log target size when starting GC

--- src/python/pants/pantsd/service/store_gc_service.py
+++ src/python/pants/pantsd/service/store_gc_service.py
@@ -56,7 +56,7 @@ class StoreGCService(PantsService):
     def _maybe_garbage_collect(self):
         if time.time() < self._next_gc:
             return
-        self._logger.info("Garbage collecting store")
+        self._logger.info(f"Garbage collecting store. target_size={self._target_size_bytes}")
         self._scheduler_session.garbage_collect_store(self._target_size_bytes)
         self._logger.info("Done garbage collecting store")
         self._set_next_gc()
