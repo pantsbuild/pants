bec014ab28e26324b45a9d59b33eec6654df8d56
John Sirois <john.sirois@gmail.com>
2021-02-09 09:18:47 -0800

    Ensure Setuptools entry_point and interpreter_constraints go through a deprecation cycle before removal.
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/python/subsystems/setuptools.py
+++ src/python/pants/backend/python/subsystems/setuptools.py
@@ -10,3 +10,30 @@ class Setuptools(PythonToolRequirementsBase):
 
     default_version = "setuptools>=50.3.0,<54.0"
     default_extra_requirements = ["wheel>=0.35.1,<0.37"]
+
+    @classmethod
+    def register_options(cls, register):
+        super().register_options(register)
+        register(
+            "--entry-point",
+            type=str,
+            advanced=True,
+            help="DEPRECATED: Unused.",
+            removal_version="2.5.0.dev0",
+            removal_hint="This option was never used.",
+        )
+
+        register(
+            "--interpreter-constraints",
+            type=list,
+            advanced=True,
+            help=(
+                "DEPRECATED: Unused. Interpreter constraints for setup.py execution are now "
+                "derived from the `python_distribution` being packaged."
+            ),
+            removal_version="2.5.0.dev0",
+            removal_hint=(
+                "This is no longer used. Interpreter constraints for setup.py execution are now "
+                "derived from the `python_distribution` being packaged."
+            ),
+        )
