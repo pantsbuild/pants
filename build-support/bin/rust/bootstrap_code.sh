a5c096a869de5ea0886f69a22d71974820b6a190
Eric Arellano <ericarellano@me.com>
2021-02-18 15:14:39 -0700

    Fix recompiles causing Pants to be killed
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- build-support/bin/rust/bootstrap_code.sh
+++ build-support/bin/rust/bootstrap_code.sh
@@ -68,6 +68,9 @@ function bootstrap_native_code() {
     engine_version_calculated="$(calculate_current_hash)"
 
     # Create the native engine resource.
+    # NB: For an unknown reason, first removing native_engine.so is sometimes necessary to avoid
+    #  the Pants process from being killed when recompiling.
+    rm "${NATIVE_ENGINE_RESOURCE}"
     cp "${native_binary}" "${NATIVE_ENGINE_RESOURCE}"
 
     # Create the accompanying metadata file.
