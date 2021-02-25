7a1590f1df363a072fe4a01f01ac98600c771e1d
Eric Arellano <ericarellano@me.com>
2021-02-24 17:32:58 -0700

    Split into `--counters` and `--histograms` options
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/goal/stats_aggregator.py
+++ src/python/pants/goal/stats_aggregator.py
@@ -19,6 +19,7 @@ from pants.engine.streaming_workunit_handler import (
     WorkunitsCallbackFactoryRequest,
 )
 from pants.engine.unions import UnionRule
+from pants.option.errors import OptionsError
 from pants.option.subsystem import Subsystem
 
 logger = logging.getLogger(__name__)
@@ -31,27 +32,39 @@ class StatsAggregatorSubsystem(Subsystem):
     @classmethod
     def register_options(cls, register):
         register(
-            "--log",
-            advanced=True,
+            "--counters",
             type=bool,
             default=False,
             help=(
-                "At the end of the Pants run, log all counter metrics and histograms of "
-                "observation metrics, e.g. the number of cache hits.\n\nFor histograms to work, "
-                "you must add `hdrhistogram` to `[GLOBAL].plugins`."
+                "At the end of the Pants run, log counts of how often certain events like cache "
+                "reads and cache errors occurred."
+            ),
+        )
+        register(
+            "--histograms",
+            type=bool,
+            default=False,
+            help=(
+                "At the end of the Pants run, log histograms of observation metrics, e.g. the "
+                "time saved thanks to caching.\n\nYou must add `hdrhistogram` to the global "
+                "`--plugins` option for this to work."
             ),
         )
 
     @property
-    def log(self) -> bool:
-        return cast(bool, self.options.log)
+    def counters(self) -> bool:
+        return cast(bool, self.options.counters)
+
+    @property
+    def histograms(self) -> bool:
+        return cast(bool, self.options.histograms)
 
 
 class StatsAggregatorCallback(WorkunitsCallback):
-    def __init__(self, *, enabled: bool, has_histogram_module: bool) -> None:
+    def __init__(self, *, counter_enabled: bool, histograms_enabled: bool) -> None:
         super().__init__()
-        self.enabled = enabled
-        self.has_histogram_module = has_histogram_module
+        self.counters_enabled = counter_enabled
+        self.histograms_enabled = histograms_enabled
         self.counters: Counter = Counter()
 
     def __call__(
@@ -62,46 +75,43 @@ class StatsAggregatorCallback(WorkunitsCallback):
         finished: bool,
         context: StreamingWorkunitContext,
     ) -> None:
-        if not self.enabled:
-            return
-
-        # Aggregate counters on completed workunits.
-        for workunit in completed_workunits:
-            if "counters" in workunit:
+        if self.counters_enabled:
+            for workunit in completed_workunits:
+                if "counters" not in workunit:
+                    continue
                 for name, value in workunit["counters"].items():
                     self.counters[name] += value
 
         if not finished:
             return
 
-        # Add any counters with a count of 0.
-        for counter in context.run_tracker.counter_names:
-            if counter not in self.counters:
-                self.counters[counter] = 0
+        if self.counters_enabled:
+            # Add any counters with a count of 0.
+            for counter in context.run_tracker.counter_names:
+                if counter not in self.counters:
+                    self.counters[counter] = 0
 
-        # Log aggregated counters.
-        counter_lines = "\n".join(
-            f"  {name}: {count}" for name, count in sorted(self.counters.items())
-        )
-        logger.info(f"Counters:\n{counter_lines}")
-
-        # Retrieve all of the observation histograms.
-        if not self.has_histogram_module:
-            return
-        from hdrh.histogram import HdrHistogram
-
-        histogram_info = context.get_observation_histograms()
-        logger.info("Observation histograms:")
-        for name, encoded_histogram in histogram_info["histograms"].items():
-            # Note: The Python library for HDR Histogram will only decode compressed histograms
-            # that are further encoded with base64. See
-            # https://github.com/HdrHistogram/HdrHistogram_py/issues/29.
-            histogram = HdrHistogram.decode(base64.b64encode(encoded_histogram))
-            buffer = BytesIO()
-            histogram.output_percentile_distribution(buffer, 1)
-            logger.info(
-                f"  Histogram for `{name}`:\n{textwrap.indent(buffer.getvalue().decode(), '    ')}"
+            # Log aggregated counters.
+            counter_lines = "\n".join(
+                f"  {name}: {count}" for name, count in sorted(self.counters.items())
             )
+            logger.info(f"Counters:\n{counter_lines}")
+
+        if self.histograms_enabled:
+            from hdrh.histogram import HdrHistogram
+
+            histogram_info = context.get_observation_histograms()
+            logger.info("Observation histograms:")
+            for name, encoded_histogram in histogram_info["histograms"].items():
+                # Note: The Python library for HDR Histogram will only decode compressed histograms
+                # that are further encoded with base64. See
+                # https://github.com/HdrHistogram/HdrHistogram_py/issues/29.
+                histogram = HdrHistogram.decode(base64.b64encode(encoded_histogram))
+                buffer = BytesIO()
+                histogram.output_percentile_distribution(buffer, 1)
+                logger.info(
+                    f"  Histogram for `{name}`:\n{textwrap.indent(buffer.getvalue().decode(), '    ')}"
+                )
 
 
 class StatsAggregatorCallbackFactoryRequest:
@@ -112,23 +122,21 @@ class StatsAggregatorCallbackFactoryRequest:
 def construct_callback(
     _: StatsAggregatorCallbackFactoryRequest, subsystem: StatsAggregatorSubsystem
 ) -> WorkunitsCallbackFactory:
-    enabled = subsystem.log
-
-    has_histogram_module = False
-    if enabled:
+    if subsystem.histograms:
         try:
             import hdrh.histogram  # noqa: F401
         except ImportError:
-            logger.warning(
-                "Please run with `--plugins=hdrhistogram` if you would like histograms to be shown "
-                "at the end of the run, or permanently add `[GLOBAL].plugins = ['hdrhistogram']`. "
-                "This will cause Pants to install the `hdrhistogram` dependency from PyPI."
+            raise OptionsError(
+                "`--stats-histograms` is used, but `hdrhistogram` is not in the "
+                "global `--plugins` option.\n\nPlease run again with `--plugins=hdrhistogram`  "
+                "or add `[GLOBAL].plugins = ['hdrhistogram']` to your `pants.toml`. This will "
+                "cause Pants to install the `hdrhistogram` dependency from PyPI."
             )
-        else:
-            has_histogram_module = True
 
     return WorkunitsCallbackFactory(
-        lambda: StatsAggregatorCallback(enabled=enabled, has_histogram_module=has_histogram_module)
+        lambda: StatsAggregatorCallback(
+            counter_enabled=subsystem.counters, histograms_enabled=subsystem.histograms
+        )
     )
 
 
--- src/python/pants/goal/stats_aggregator_integration_test.py
+++ src/python/pants/goal/stats_aggregator_integration_test.py
@@ -3,34 +3,38 @@
 
 from __future__ import annotations
 
-import re
-
 from pants.testutil.pants_integration_test import run_pants, setup_tmpdir
 
 
-def test_counters_and_histograms() -> None:
+def test_counters() -> None:
+    result = run_pants(["--stats-counters", "roots"])
+    result.assert_success()
+    assert "Counters:" in result.stderr
+    assert "local_cache_requests: 0" in result.stderr
+    assert "remote_cache_requests: 0" in result.stderr
+    assert "histogram" not in result.stderr
+
+
+def test_histograms() -> None:
     # To get the cache read histogram, we must ensure the cache is populated, so we run twice.
-    with setup_tmpdir({"src/py/app.py": "print(0)", "src/py/BUILD": "python_library()"}) as tmpdir:
+    with setup_tmpdir(
+        {"src/py/app.py": "print(0)\n", "src/py/BUILD": "python_library()"}
+    ) as tmpdir:
         argv = [
             "--backend-packages=['pants.backend.python', 'pants.backend.python.lint.black']",
             "--plugins=hdrhistogram",
-            "--stats-log",
+            "--stats-histograms",
             "lint",
             f"{tmpdir}::",
         ]
         run_pants(argv).assert_success()
         result = run_pants(argv)
     result.assert_success()
-    assert "Counters:" in result.stderr
-    assert re.search(r"local_cache_requests: \d", result.stderr)
-    assert "remote_cache_requests: 0" in result.stderr
     assert "Observation histograms:" in result.stderr
     assert "Histogram for `local_store_read_blob_size`:" in result.stderr
+    assert "Counters:" not in result.stderr
 
-
-def test_warn_if_no_histograms() -> None:
-    result = run_pants(["--stats-log", "roots"])
-    result.assert_success()
-    assert "Counters:" in result.stderr
-    assert "Please run with `--plugins=hdrhistogram`" in result.stderr
-    assert "Observation histograms:" not in result.stderr
+    # The option requires `hdrhistogram` be in the global plugins.
+    result = run_pants(["--stats-histograms", "roots"])
+    result.assert_failure()
+    assert "OptionsError" in result.stderr
