6b1436da25ef145eeb7fe29266b339a9e092f962
Stu Hood <stuhood@gmail.com>
2021-03-01 15:21:29 -0800

    Ripple effects.

--- build-support/python/clean.sh
+++ build-support/python/clean.sh
@@ -2,11 +2,6 @@
 
 PANTS_BASE=$(dirname "$0")/../..
 rm -rf "${HOME}/.pex"
-rm -rf "${PANTS_BASE}/build-support/virtualenvs"
+rm -rf "${HOME}/.cache/pants/pants_dev_deps"
 rm -rf "${PANTS_BASE}/.pants.d"
 find "${PANTS_BASE}" -name '*.pyc' -print0 | xargs -0 rm -f
-
-# Legacy:
-rm -rf "${PANTS_BASE}/build-support/pants_dev_deps.venv"
-rm -rf "${PANTS_BASE}/build-support/pants_dev_deps.py{2,3}.venv"
-rm -rf "${PANTS_BASE}/build-support/pants_dev_deps.py{2,3}?.venv"
