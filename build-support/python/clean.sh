f4d4b257b0047431aebaf2006550543ea45b245d
Stu Hood <stuhood@gmail.com>
2021-03-01 17:03:07 -0800

    Continue to clear legacy entries, since they can be encountered when fetching from the cache.
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- build-support/python/clean.sh
+++ build-support/python/clean.sh
@@ -5,3 +5,9 @@ rm -rf "${HOME}/.pex"
 rm -rf "${HOME}/.cache/pants/pants_dev_deps"
 rm -rf "${PANTS_BASE}/.pants.d"
 find "${PANTS_BASE}" -name '*.pyc' -print0 | xargs -0 rm -f
+
+# Legacy:
+rm -rf "${PANTS_BASE}/build-support/virtualenvs"
+rm -rf "${PANTS_BASE}/build-support/pants_dev_deps.venv"
+rm -rf "${PANTS_BASE}/build-support/pants_dev_deps.py{2,3}.venv"
+rm -rf "${PANTS_BASE}/build-support/pants_dev_deps.py{2,3}?.venv"
