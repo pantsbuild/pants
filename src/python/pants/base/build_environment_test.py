5bbf7f9bd2ff79db2ae1e361a2653c68167ecb58
Eric Arellano <ericarellano@me.com>
2021-03-15 19:08:07 -0700

    Stop duplicating implementation of `get_pants_cachedir()` in Python and Rust
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/base/build_environment_test.py
+++ src/python/pants/base/build_environment_test.py
@@ -2,25 +2,16 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 import os
-import unittest
 
 from pants.base.build_environment import get_pants_cachedir
-from pants.engine.internals.native import Native
 from pants.util.contextutil import environment_as, temporary_file
 
 
-def test_get_pants_cachedir() -> None:
-    assert Native().default_cache_path() == get_pants_cachedir()
+def test_get_cachedir() -> None:
+    with environment_as(XDG_CACHE_HOME=""):
+        assert os.path.expanduser("~/.cache/pants") == get_pants_cachedir()
 
 
-class TestBuildEnvironment(unittest.TestCase):
-    """Test class for pants.base.build_environment."""
-
-    def test_get_cachedir(self) -> None:
-        with environment_as(XDG_CACHE_HOME=""):
-            self.assertEqual(os.path.expanduser("~/.cache/pants"), get_pants_cachedir())
-
-    def test_set_cachedir(self) -> None:
-        with temporary_file() as temp:
-            with environment_as(XDG_CACHE_HOME=temp.name):
-                self.assertEqual(os.path.join(temp.name, "pants"), get_pants_cachedir())
+def test_set_cachedir() -> None:
+    with temporary_file() as temp, environment_as(XDG_CACHE_HOME=temp.name):
+        assert os.path.join(temp.name, "pants") == get_pants_cachedir()
