53e740f961cd0118a1e44a005fc995a058b7d5dc
John Sirois <john.sirois@gmail.com>
2021-03-18 11:21:11 -0700

    Subset lockfile resolves.
    Use the new `--pex-repository` feature to resolve just the distributions
    needed from a larger constraints.txt "lockfile" resolve performantly.
    
    This eliminates use of `--requirements-pex` in `TwoStepPex` creation as
    well in favor of just asking for a full resolve of all requirements in a
    `--pex-repository` since this is roughly the same speed.
    
    Fixes #11105
    
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/backend/python/util_rules/pex_from_targets_test.py
+++ src/python/pants/backend/python/util_rules/pex_from_targets_test.py
@@ -1,14 +1,20 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import json
+import subprocess
+import sys
+from dataclasses import dataclass
+from pathlib import Path, PurePath
 from textwrap import dedent
-from typing import Optional
+from typing import List, Optional, cast
 
 import pytest
+from _pytest.tmpdir import TempPathFactory
 
 from pants.backend.python.target_types import PythonLibrary, PythonRequirementLibrary
 from pants.backend.python.util_rules import pex_from_targets
-from pants.backend.python.util_rules.pex import PexRequest, PexRequirements
+from pants.backend.python.util_rules.pex import Pex, PexRequest, PexRequirements
 from pants.backend.python.util_rules.pex_from_targets import PexFromTargetsRequest
 from pants.build_graph.address import Address
 from pants.engine.internals.scheduler import ExecutionError
@@ -27,7 +33,86 @@ def rule_runner() -> RuleRunner:
     )
 
 
-def test_constraints_validation(rule_runner: RuleRunner) -> None:
+@dataclass(frozen=True)
+class Project:
+    name: str
+    version: str
+
+
+def create_dists(workdir: Path, project: Project, *projects: Project) -> PurePath:
+    project_dirs = []
+    for proj in (project, *projects):
+        project_dir = workdir / "projects" / proj.name
+        project_dir.mkdir(parents=True)
+        project_dirs.append(project_dir)
+
+        (project_dir / "pyproject.toml").write_text(
+            dedent(
+                """\
+                [build-system]
+                requires = ["setuptools==54.1.2", "wheel==0.36.2"]
+                build-backend = "setuptools.build_meta"
+                """
+            )
+        )
+        (project_dir / "setup.cfg").write_text(
+            dedent(
+                f"""\
+                [metadata]
+                name = {proj.name}
+                version = {proj.version}
+                """
+            )
+        )
+
+    pex = workdir / "pex"
+    subprocess.run(
+        args=[sys.executable, "-m", "pex", *project_dirs, "--include-tools", "-o", pex], check=True
+    )
+
+    find_links = workdir / "find-links"
+    subprocess.run(
+        args=[
+            sys.executable,
+            "-m",
+            "pex.tools",
+            pex,
+            "repository",
+            "extract",
+            "--find-links",
+            find_links,
+        ],
+        check=True,
+    )
+    return find_links
+
+
+def requirements(rule_runner: RuleRunner, pex: Pex) -> List[str]:
+    rule_runner.scheduler.write_digest(pex.digest)
+    completed_process = subprocess.run(
+        args=[
+            sys.executable,
+            "-m",
+            "pex.tools",
+            pex.name,
+            "info",
+        ],
+        cwd=rule_runner.build_root,
+        stdout=subprocess.PIPE,
+        check=True,
+    )
+    return cast(List[str], json.loads(completed_process.stdout.decode())["requirements"])
+
+
+def test_constraints_validation(tmp_path_factory: TempPathFactory, rule_runner: RuleRunner) -> None:
+    find_links = create_dists(
+        tmp_path_factory.mktemp("sdists"),
+        Project("Foo-Bar", "1.0.0"),
+        Project("Bar", "5.5.5"),
+        Project("baz", "2.2.2"),
+        Project("QUX", "3.4.5"),
+    )
+
     rule_runner.add_to_build_file(
         "",
         dedent(
@@ -46,19 +131,10 @@ def test_constraints_validation(rule_runner: RuleRunner) -> None:
             """
             # Comment.
             --find-links=https://duckduckgo.com
-            Foo._-BAR==1.0.0  # Inline comment.
+            # TODO(John Sirois): XXX: File Pex issue and restore `.` in `Foo._-BAR` project name.
+            Foo_-BAR==1.0.0  # Inline comment.
             bar==5.5.5
             baz==2.2.2
-            qux==3.4.5
-        """
-        ),
-    )
-    rule_runner.create_file(
-        "constraints2.txt",
-        dedent(
-            """
-            foo==1.0.0
-            bar==5.5.5
             qux==3.4.5
         """
         ),
@@ -81,28 +157,34 @@ def test_constraints_validation(rule_runner: RuleRunner) -> None:
             args.append(f"--python-setup-resolve-all-constraints={resolve_all.value}")
         if constraints_file:
             args.append(f"--python-setup-requirement-constraints={constraints_file}")
-        rule_runner.set_options(args)
+        args.append("--python-repos-indexes=[]")
+        args.append(f"--python-repos-repos={find_links}")
+        rule_runner.set_options(args, env_inherit={"PATH"})
         return rule_runner.request(PexRequest, [request])
 
     pex_req1 = get_pex_request("constraints1.txt", ResolveAllConstraintsOption.NEVER)
     assert pex_req1.requirements == PexRequirements(["foo-bar>=0.1.2", "bar==5.5.5", "baz"])
+    assert pex_req1.repository_pex is None
 
     pex_req1_direct = get_pex_request(
         "constraints1.txt", ResolveAllConstraintsOption.NEVER, direct_deps_only=True
     )
     assert pex_req1_direct.requirements == PexRequirements(["baz"])
+    assert pex_req1_direct.repository_pex is None
 
     pex_req2 = get_pex_request("constraints1.txt", ResolveAllConstraintsOption.ALWAYS)
-    assert pex_req2.requirements == PexRequirements(
-        ["Foo._-BAR==1.0.0", "bar==5.5.5", "baz==2.2.2", "qux==3.4.5"]
+    assert pex_req2.requirements == PexRequirements(["foo-bar>=0.1.2", "bar==5.5.5", "baz"])
+    assert pex_req2.repository_pex is not None
+    repository_pex = pex_req2.repository_pex
+    assert ["Foo_-BAR==1.0.0", "bar==5.5.5", "baz==2.2.2", "qux==3.4.5"] == requirements(
+        rule_runner, repository_pex
     )
 
     pex_req2_direct = get_pex_request(
         "constraints1.txt", ResolveAllConstraintsOption.ALWAYS, direct_deps_only=True
     )
-    assert pex_req2_direct.requirements == PexRequirements(
-        ["Foo._-BAR==1.0.0", "bar==5.5.5", "baz==2.2.2", "qux==3.4.5"]
-    )
+    assert pex_req2_direct.requirements == PexRequirements(["baz"])
+    assert pex_req2_direct.repository_pex == repository_pex
 
     with pytest.raises(ExecutionError) as err:
         get_pex_request(None, ResolveAllConstraintsOption.ALWAYS)
