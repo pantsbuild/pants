9e4a90fe663ac7d4031f17b0582f9cc9c60bf70c
Benjy Weinberger <benjyw@gmail.com>
2021-03-19 10:22:13 -0700

    Support extra test runner output. (#11741)
    Test runners may, depending on their runtime configuration,
    output extra data that we don't know about, but the user may
    want to see.
    
    For example, pytest may be configured to run the pytest-html
    plugin, which will emit an HTML report into the process execution
    sandbox, and we won't pick it up from there.
    
    This change adds generic support for "extra output" from a test
    runner. All the user needs to do is ensure this output goes
    somewhere where the appropriate test execution rule can find it.
    Pants will dump any extra output it finds into dist/.
    
    In the specific case of pytest, we look for this output in the
    extra-output/ subdir. This means that users must ensure that output
    is directed there (e.g., using the --html=extra-output/report.html
    flag to pytest). We must document this, of course.
    
    This change also enables pytest-html in the Pants repo itself,
    as a proof of concept. To get a report:
    
    ./pants test <tgt> -- --html=extra-output/report.html
    
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/python/goals/pytest_runner.py
+++ src/python/pants/backend/python/goals/pytest_runner.py
@@ -41,7 +41,15 @@ from pants.core.goals.test import (
 )
 from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
 from pants.engine.addresses import UnparsedAddressInputs
-from pants.engine.fs import AddPrefix, Digest, DigestSubset, MergeDigests, PathGlobs, Snapshot
+from pants.engine.fs import (
+    AddPrefix,
+    Digest,
+    DigestSubset,
+    MergeDigests,
+    PathGlobs,
+    RemovePrefix,
+    Snapshot,
+)
 from pants.engine.process import (
     FallibleProcessResult,
     InteractiveProcess,
@@ -62,6 +70,12 @@ from pants.python.python_setup import PythonSetup
 from pants.util.logging import LogLevel
 
 logger = logging.getLogger()
+
+
+# If a user wants extra pytest output (e.g., plugin output) to show up in dist/
+# they must ensure that output goes under this directory. E.g.,
+# ./pants test <target> -- --html=extra-output/report.html
+_EXTRA_OUTPUT_DIR = "extra-output"
 
 
 @dataclass(frozen=True)
@@ -225,6 +239,7 @@ async def setup_pytest_for_target(
             argv=(*pytest.options.args, *coverage_args, *field_set_source_files.files),
             extra_env=extra_env,
             input_digest=input_digest,
+            output_directories=(_EXTRA_OUTPUT_DIR,),
             output_files=output_files,
             timeout_seconds=request.field_set.timeout.calculate_from_global_options(pytest),
             execution_slot_variable=pytest.options.execution_slot_var,
@@ -268,12 +283,19 @@ async def run_python_test(
             )
         else:
             logger.warning(f"Failed to generate JUnit XML data for {field_set.address}.")
+    extra_output_snapshot = await Get(
+        Snapshot, DigestSubset(result.output_digest, PathGlobs([f"{_EXTRA_OUTPUT_DIR}/**"]))
+    )
+    extra_output_snapshot = await Get(
+        Snapshot, RemovePrefix(extra_output_snapshot.digest, _EXTRA_OUTPUT_DIR)
+    )
 
     return TestResult.from_fallible_process_result(
         result,
         address=field_set.address,
         coverage_data=coverage_data,
         xml_results=xml_results_snapshot,
+        extra_output=extra_output_snapshot,
     )
 
 
