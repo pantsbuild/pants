66fded230694da93848c48fd111a4671138382c9
John Sirois <john.sirois@gmail.com>
2021-03-25 12:21:39 -0700

    Upgrade to Pex 2.1.35. (#11799) (#11801)
    This pulls in a fix for infinite loops in `--venv` mode `pex` script re-execs.
    
    Fixes #11795
    
    (cherry picked from commit 038a1a08bec3e68fabcd39e95555b58a477070a2)

--- src/python/pants/backend/python/util_rules/pex_cli.py
+++ src/python/pants/backend/python/util_rules/pex_cli.py
@@ -37,7 +37,7 @@ class PexBinary(TemplatedExternalTool):
     name = "pex"
     help = "The PEX (Python EXecutable) tool (https://github.com/pantsbuild/pex)."
 
-    default_version = "v2.1.34"
+    default_version = "v2.1.35"
     default_url_template = "https://github.com/pantsbuild/pex/releases/download/{version}/pex"
 
     @classproperty
@@ -47,8 +47,8 @@ class PexBinary(TemplatedExternalTool):
                 (
                     cls.default_version,
                     plat,
-                    "9b1a959ccb61b3deb64ffeed43a735c7115e414f4de6f96e66adc9e7fc7a757f",
-                    "3597768",
+                    "3b1c97ebd79a8b650c81f0afbdbefef17c3d1a128219a8e95b5f35c2c9dfd532",
+                    "3600775",
                 )
             )
             for plat in ["darwin", "linux"]
