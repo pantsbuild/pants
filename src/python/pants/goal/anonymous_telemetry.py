2ed69535bd828f7858a111a0e271d5f17ab25d5f
Neeraj Kashyap <nkashy1@gmail.com>
2021-03-29 11:22:00 -0700

    Added tags to anonymous telemetry
    All tags were already present as data in the message body. No new
    information is being reported.
    
    Just added this information as tags for more efficient aggregation +
    querying on Bugout.
    
    Tags added to repos in this commit:
    1. `repo:<repo_id>`
    2. `user:<user_id>` (if defined in report body; else the tag is
    `user:UNKNOWN`)
    3. `machine:<machine_id>` (if defined in report body; else the
    tag is `machine:UNKNOWN`)
    4. `duration:<duration_seconds>` (if defined in report body; else
    the tag is `duration:0`)

--- src/python/pants/goal/anonymous_telemetry.py
+++ src/python/pants/goal/anonymous_telemetry.py
@@ -162,8 +162,12 @@ class AnonymousTelemetryCallback(WorkunitsCallback):
                     system_tags
                     + [
                         f"pants_version:{telemetry_data.get('pants_version')}",
+                        f"repo:{repo_id}",
+                        f"user:{telemetry_data.get('user_id', 'UNKNOWN')}",
+                        f"machine:{telemetry_data.get('machine_id', 'UNKNOWN')}",
+                        f"duration:{telemetry_data.get('duration', '0')}",
                     ]
-                    + [f"goal:{goal}" for goal in telemetry_data.get("goals", [])]
+                    + [f"goal:{goal}" for goal in telemetry_data.get("standard_goals", [])]
                 )
 
                 report = Report(
