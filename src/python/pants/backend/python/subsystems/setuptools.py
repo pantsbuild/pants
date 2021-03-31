2944734d76cfc6ef74a5f212b3fabea2092f7562
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2021-03-31 10:27:26 -0700

    Remove deprecated `[setuptools].entry_point` and `[setuptools].interpreter_constraints` (#11822)
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/backend/python/subsystems/setuptools.py
+++ src/python/pants/backend/python/subsystems/setuptools.py
@@ -1,8 +1,6 @@
 # Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from typing import Tuple
-
 from pants.backend.python.subsystems.python_tool_base import PythonToolRequirementsBase
 
 
@@ -12,34 +10,3 @@ class Setuptools(PythonToolRequirementsBase):
 
     default_version = "setuptools>=50.3.0,<54.0"
     default_extra_requirements = ["wheel>=0.35.1,<0.37"]
-
-    @classmethod
-    def register_options(cls, register):
-        super().register_options(register)
-        register(
-            "--entry-point",
-            type=str,
-            advanced=True,
-            help="DEPRECATED: Unused.",
-            removal_version="2.5.0.dev0",
-            removal_hint="This option was never used.",
-        )
-
-        register(
-            "--interpreter-constraints",
-            type=list,
-            advanced=True,
-            help=(
-                "DEPRECATED: Python interpreter constraints to use when selecting an interpreter "
-                "to package `python_distribution` targets using setup.py."
-            ),
-            removal_version="2.5.0.dev0",
-            removal_hint=(
-                "Interpreter constraints for setup.py execution are now derived from the "
-                "`python_distribution` being packaged so this option is not needed."
-            ),
-        )
-
-    @property
-    def interpreter_constraints(self) -> Tuple[str, ...]:
-        return tuple(self.options.interpreter_constraints)
