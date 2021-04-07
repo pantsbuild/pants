1a8feedd01526a278b39212d745e884303793685
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-04-06 20:18:44 -0700

    [internal] Load toolchain auth token in all GH events except PR
    instead of checking for push event, make sure this is not a PR (where the GH secret is not available), in other cases (cron/schedule, push, release, etc...) the secret should be available so we should tr
    y to initalize it.

--- build-support/bin/generate_github_workflows.py
+++ build-support/bin/generate_github_workflows.py
@@ -97,7 +97,7 @@ def setup_toolchain_auth() -> Sequence[Step]:
     return [
         {
             "name": "Setup toolchain auth",
-            "if": "github.event_name == 'push'",
+            "if": "github.event_name != 'pull_request'",
             "run": dedent(
                 """\
                 echo TOOLCHAIN_AUTH_TOKEN="${{ secrets.TOOLCHAIN_AUTH_TOKEN }}" >> $GITHUB_ENV
