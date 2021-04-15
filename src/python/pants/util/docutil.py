a59474068025142cde0a538348533a576f0f2e17
Benjy Weinberger <benjyw@gmail.com>
2021-04-15 16:30:54 -0700

    Finesse some docgen markdown<->html issues.
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/util/docutil.py
+++ src/python/pants/util/docutil.py
@@ -15,7 +15,11 @@ def terminal_width(*, fallback: int = 96, padding: int = 2) -> int:
 def bracketed_docs_url(slug: str) -> str:
     """Link to the Pants docs using the current version of Pants.
 
-    Returned URL is surrounded by square brackets, to prevent linkifiers from considering any
+    Returned URL is surrounded by parentheses, to prevent linkifiers from considering any
     adjacent punctuation (such as a period at the end of a sentence) as part of the URL.
+
+    Note that this function used to use square brackets, hence the name, but it turns out
+    that those prevent any linkification at all from happening on readme.com, so we switched
+    to parens, but didn't update the name to prevent churn.
     """
-    return f"[https://www.pantsbuild.org/v{MAJOR_MINOR}/docs/{slug}]"
+    return f"(https://www.pantsbuild.org/v{MAJOR_MINOR}/docs/{slug})"
