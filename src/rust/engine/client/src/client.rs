4c99899be67c796025360ca9ce510a7730491cfc
John Sirois <john.sirois@gmail.com>
2021-04-21 19:23:56 -0600

    Add best effort tty settings save & restore.
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/rust/engine/client/src/client.rs
+++ src/rust/engine/client/src/client.rs
@@ -1,9 +1,12 @@
 // Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-use nailgun::NailgunClientError;
 use std::os::unix::io::AsRawFd;
 use std::time::SystemTime;
+
+use log::debug;
+
+use nailgun::NailgunClientError;
 
 pub struct ConnectionSettings {
   pub port: u16,
@@ -41,12 +44,22 @@ pub async fn execute_command(
     connection_settings.timeout_limit.to_string(),
   ));
 
-  if connection_settings.dynamic_ui {
-    for raw_fd in &[
-      std::io::stdin().as_raw_fd(),
-      std::io::stdout().as_raw_fd(),
-      std::io::stderr().as_raw_fd(),
-    ] {
+  let raw_io_fds = [
+    std::io::stdin().as_raw_fd(),
+    std::io::stdout().as_raw_fd(),
+    std::io::stderr().as_raw_fd(),
+  ];
+  let mut tty_settings = Vec::with_capacity(raw_io_fds.len());
+  for raw_fd in &raw_io_fds {
+    match termios::Termios::from_fd(*raw_fd) {
+      Ok(termios) => tty_settings.push((raw_fd, termios)),
+      Err(err) => debug!(
+        "Failed to save terminal attributes for file descriptor {fd}: {err}",
+        fd = raw_fd,
+        err = err
+      ),
+    }
+    if connection_settings.dynamic_ui {
       if let Ok(path) = nix::unistd::ttyname(*raw_fd) {
         env.push((
           format!("NAILGUN_TTY_PATH_{fd}", fd = raw_fd),
@@ -63,23 +76,31 @@ pub async fn execute_command(
 
   let args = argv.iter().skip(1).cloned().collect();
 
-  nailgun::client_execute(connection_settings.port, command, args, env)
-    .await
-    .map_err(|error| match error {
-      NailgunClientError::PreConnect(err) => format!(
-        "Problem connecting to pantsd at {port}: {err}",
-        port = connection_settings.port,
+  let nailgun_result = nailgun::client_execute(connection_settings.port, command, args, env).await;
+  for (raw_fd, termios) in tty_settings {
+    if let Err(err) = termios::tcsetattr(*raw_fd, termios::TCSADRAIN, &termios) {
+      debug!(
+        "Failed to restore terminal attributes for file descriptor {fd}: {err}",
+        fd = raw_fd,
         err = err
-      ),
-      NailgunClientError::PostConnect(err) => format!(
-        "Problem communicating with pantsd at {port}: {err}",
-        port = connection_settings.port,
-        err = err
-      ),
-      NailgunClientError::BrokenPipe => format!(
-        "Broken pipe communicating with pantsd at {port}.",
-        port = connection_settings.port
-      ),
-      NailgunClientError::KeyboardInterrupt => "User interrupt.".to_owned(),
-    })
+      );
+    }
+  }
+  nailgun_result.map_err(|error| match error {
+    NailgunClientError::PreConnect(err) => format!(
+      "Problem connecting to pantsd at {port}: {err}",
+      port = connection_settings.port,
+      err = err
+    ),
+    NailgunClientError::PostConnect(err) => format!(
+      "Problem communicating with pantsd at {port}: {err}",
+      port = connection_settings.port,
+      err = err
+    ),
+    NailgunClientError::BrokenPipe => format!(
+      "Broken pipe communicating with pantsd at {port}.",
+      port = connection_settings.port
+    ),
+    NailgunClientError::KeyboardInterrupt => "User interrupt.".to_owned(),
+  })
 }
