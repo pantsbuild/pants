860908b2828031f2cb0a03e05cc252df8d3aaf1a
Asher Foa <asher@asherfoa.com>
2021-04-29 13:53:49 -0700

    Upgrade lambdex from 0.1.3 to 0.1.4 to remove setuptools dependency (#11972)
    https://github.com/wickman/lambdex/issues/6
    https://github.com/pantsbuild/lambdex/pull/8

--- src/python/pants/backend/awslambda/python/lambdex.py
+++ src/python/pants/backend/awslambda/python/lambdex.py
@@ -9,10 +9,7 @@ class Lambdex(PythonToolBase):
     options_scope = "lambdex"
     help = "A tool for turning .pex files into AWS Lambdas (https://github.com/wickman/lambdex)."
 
-    default_version = "lambdex==0.1.3"
-    # TODO(John Sirois): Remove when we can upgrade to a version of lambdex with
-    # https://github.com/wickman/lambdex/issues/6 fixed.
-    default_extra_requirements = ["setuptools>=50.3.0,<50.4"]
+    default_version = "lambdex==0.1.4"
     register_interpreter_constraints = True
-    default_interpreter_constraints = ["CPython>=3.5"]
+    default_interpreter_constraints = ["CPython>=3.6"]
     default_main = ConsoleScript("lambdex")
