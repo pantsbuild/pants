a9d08f4463c0e0f3c30f151fd2d69e12a202c5a2
Eric Arellano <ericarellano@me.com>
2021-05-03 12:17:51 -0700

    Fix import
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/engine/target_test.py
+++ src/python/pants/engine/target_test.py
@@ -24,7 +24,6 @@ from pants.engine.target import (
     InvalidFieldTypeException,
     RequiredFieldMissingException,
     ScalarField,
-    SentinelField,
     SequenceField,
     Sources,
     StringField,
