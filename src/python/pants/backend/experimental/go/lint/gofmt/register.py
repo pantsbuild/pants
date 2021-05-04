8b44a9dd660c9264e22145ba21eaefefb9fc3369
Eric Arellano <ericarellano@me.com>
2021-05-03 17:04:00 -0700

    Fix imports
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/go/lint/gofmt/register.py
+++ src/python/pants/backend/experimental/go/lint/gofmt/register.py
@@ -2,8 +2,8 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from pants.backend.go.lint import fmt
-from pants.backend.go.lint.gofmt.rules import rules as gofmt_rules
 from pants.backend.go.lint.gofmt import skip_field
+from pants.backend.go.lint.gofmt.rules import rules as gofmt_rules
 
 
 def rules():
