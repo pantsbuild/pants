f2ea08cab04a3718c544a766cda0a01e84444fd5
Eric Arellano <ericarellano@me.com>
2021-05-24 09:52:25 -0700

    Fix Flake8 not liking stub files

--- src/python/pants/engine/internals/native_engine_pyo3.pyi
+++ src/python/pants/engine/internals/native_engine_pyo3.pyi
@@ -1,6 +1,10 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+# TODO: black and flake8 disagree about the content of this file:
+#   see https://github.com/psf/black/issues/1548
+# flake8: noqa: E302
+
 class PyExecutor:
     def __init__(self, core_threads: int, max_threads: int) -> None: ...
 
