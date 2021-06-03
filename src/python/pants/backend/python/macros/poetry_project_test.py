bab4c50c0e6f99e92257f49920ebf98e22e9be83
Liam Wilson <lswilson0709@gmail.com>
2021-06-03 05:19:07 -0700

    Fixed header for test file
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/python/macros/poetry_project_test.py
+++ src/python/pants/backend/python/macros/poetry_project_test.py
@@ -1,3 +1,6 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 from pkg_resources import Requirement
 
 from pants.backend.python.macros.poetry_project import (
