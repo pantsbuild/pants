100e8e16c4d4f70d6f9554d2e399e592a134b68e
Liam Wilson <lswilson0709@gmail.com>
2021-06-25 13:31:00 -0700

    Add `poetry_requirements` macro for Poetry support (#12174)
    Closes https://github.com/pantsbuild/pants/issues/10655.
    
    [ci skip-rust]

--- src/python/pants/option/global_options.py
+++ src/python/pants/option/global_options.py
@@ -1516,6 +1516,8 @@ class GlobalOptions(Subsystem):
                 # macros should be adapted to allow this dependency to be automatically detected.
                 "requirements.txt",
                 "3rdparty/**/requirements.txt",
+                "pyproject.toml",
+                "3rdparty/**/pyproject.toml",
                 *bootstrap_options.pantsd_invalidation_globs,
             )
         )
