12b92ff4c294bd8f93e1a0bbffbd9d33781cf638
Stu Hood <stuhood@gmail.com>
2021-06-29 09:50:38 -0700

    [internal] Fix inconsistent Rust file prefix on main and align pre-commit checks with CI (#12254)
    An inconsistent Rust file prefix made it on master, causing unrelated commit hook runs to fail. Fix the prefix, and adjust CI to run the same three commands as the commit hooks. Also, simplify `check_rust_formatting.sh` to recommend directly running `./cargo fmt --all` (which has seemingly worked since we moved to `prost`...?).

--- build-support/bin/check_rust_formatting.sh
+++ build-support/bin/check_rust_formatting.sh
@@ -2,72 +2,12 @@
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
-function usage() {
-  echo "Checks formatting of rust files, optionally fixing mis-formatted files."
-  echo
-  echo "Usage: $0 (-h|-f)"
-  echo " -h    print out this help message"
-  echo " -f    instead of erroring on files with bad formatting, fix those files"
+BASE_COMMAND="${REPO_ROOT}/cargo fmt --all --"
 
-  if [[ -n "$*" ]]; then
-    echo
-    echo "$@"
-  fi
-}
-
-check="true"
-
-while getopts "hf" opt; do
-  case ${opt} in
-    h)
-      usage
-      exit 0
-      ;;
-    f)
-      check="false"
-      ;;
-    *)
-      usage "Unrecognized arguments."
-      exit 1
-      ;;
-  esac
-done
-
-cmd=(
-  "${REPO_ROOT}/cargo" fmt --all --
-)
-if [[ "${check}" == "true" ]]; then
-  cmd=("${cmd[@]}" "--check")
-fi
-
-# WONTFIX: fixing the array expansion is too difficult to be worth it. See https://github.com/koalaman/shellcheck/wiki/SC2207.
-# shellcheck disable=SC2207
-bad_files=(
-  $(
-    # Ensure generated code is present since `cargo fmt` needs to do enough parsing to follow use's
-    # and these will land in generated code.
-    echo >&2 "Ensuring generated code is present for downstream formatting checks..."
-    "${REPO_ROOT}/cargo" check -p bazel_protos
-
-    "${cmd[@]}" |
-      awk '$0 ~ /^Diff in/ {print $3}' |
-      sort -u
-    exit "${PIPESTATUS[0]}"
-  )
-)
+$BASE_COMMAND --check
 exit_code=$?
 
 if [[ ${exit_code} -ne 0 ]]; then
-  if [[ "${check}" == "true" ]]; then
-    echo >&2 "The following Rust files were incorrectly formatted, run \`$0 -f\` to reformat them:"
-    for bad_file in ${bad_files[*]}; do
-      echo >&2 "${bad_file}"
-    done
-  else
-    cat << EOF >&2
-An error occurred while checking the formatting of Rust files:
-EOF
-    cd "${NATIVE_ROOT}" && "${cmd[@]}" > /dev/null
-  fi
+  echo >&2 "Rust files incorrectly formatted, run \`${BASE_COMMAND}\` to reformat them."
   exit 1
 fi
