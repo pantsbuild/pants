5cb565bed438c362b26c4c5c90f0b49e26f9fe37
Patrick Lawson <patrick.a.lawson@gmail.com>
2021-07-06 14:30:52 -0400

    More CI debugging
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/java/compile/javac_binary_test.py
+++ src/python/pants/backend/java/compile/javac_binary_test.py
@@ -48,6 +48,9 @@ def test_java_binary_versions(rule_runner: RuleRunner) -> None:
     # default version is 1.11
     assert "javac 11.0" in run_javac_version(rule_runner)
 
+    print(run_javac_version(rule_runner))
+    assert "force failure" in run_javac_version(rule_runner)
+
     rule_runner.set_options(["--javac-jdk=adopt:1.8"])
     assert "javac 1.8" in run_javac_version(rule_runner)
 
