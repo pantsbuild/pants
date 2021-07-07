7999d7f3c2c11c7d78a97ce3b473781fa037696c
Eric Arellano <ericarellano@me.com>
2021-07-07 08:58:49 -0700

    Fix bad merge
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/base/parse_context.py
+++ src/python/pants/base/parse_context.py
@@ -24,8 +24,6 @@ class ParseContext:
     ) -> None:
         """Create a ParseContext.
 
-        :param type_aliases: A dictionary of BUILD file symbols.
-        :param rel_path_oracle: An oracle than can be queried for the current BUILD file path.
         :param build_root: The absolute path to the build root.
         :param type_aliases: A dictionary of BUILD file symbols.
         :param rel_path_oracle: An oracle than can be queried for the current BUILD file path.
