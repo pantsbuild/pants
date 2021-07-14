750c7163c20c899b486083319f2ed6c5a995beaa
Eric Arellano <ericarellano@me.com>
2021-07-14 10:59:15 -0700

    [Internal] Add `git_url()` helper to `docutil.py`
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/util/docutil.py
+++ src/python/pants/util/docutil.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 import shutil
 
-from pants.version import MAJOR_MINOR
+from pants.version import MAJOR_MINOR, PANTS_SEMVER
 
 
 # NB: This is not memoized because that would cause Pants to not pick up terminal resizing when
@@ -16,3 +16,13 @@ def terminal_width(*, fallback: int = 96, padding: int = 2) -> int:
 
 def doc_url(slug: str) -> str:
     return f"https://www.pantsbuild.org/v{MAJOR_MINOR}/docs/{slug}"
+
+
+def git_url(fp: str) -> str:
+    """Link to code in pantsbuild/pants.
+
+    Note that links will not be stable for dev releases because `main` floats. Only release
+    candidates and stable releases have stable links.
+    """
+    branch = "main" if PANTS_SEMVER.is_devrelease else f"{MAJOR_MINOR}.x"
+    return f"https://github.com/pantsbuild/pants/blob/{branch}/{fp}"
