9acc58cee2a91ba3216d507ee1f7b3fce19d43c3
Andreas Stenius <andreas.stenius@svenskaspel.se>
2021-07-16 13:57:22 +0200

    Docker target types.
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/docker/target_types.py
+++ src/python/pants/backend/docker/target_types.py
@@ -1,15 +1,42 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.engine.target import COMMON_TARGET_FIELDS, Dependencies, Sources, Target
+from pants.engine.target import COMMON_TARGET_FIELDS, Dependencies, Sources, StringField, Target
 
 
 class DockerImageSources(Sources):
-    expected_num_files = 1
     default = ("Dockerfile",)
+    expected_num_files = 1
+    help = "Name of the Dockerfile to use when building the Docker image."
+
+
+class DockerImageVersion(StringField):
+    alias = "version"
+    default = "latest"
+    help = "Image tag to apply to built images."
+
+
+class DockerDependencies(Dependencies):
+    pass
+
+
+class DockerBuildRoot(StringField):
+    alias = "build_root"
+    default = "."
+    help = (
+        "Root directory for Docker build context. Default is to use the directory of the `BUILD` "
+        "file. Use '/' to use the project root, thus putting any resource from the entire project "
+        "within scope."
+    )
 
 
 class DockerImage(Target):
     alias = "docker_image"
-    core_fields = (*COMMON_TARGET_FIELDS, Dependencies, DockerImageSources)
+    core_fields = (
+        *COMMON_TARGET_FIELDS,
+        DockerBuildRoot,
+        DockerDependencies,
+        DockerImageSources,
+        DockerImageVersion,
+    )
     help = "A Docker image."
