ac6db67f69fa84b91b52f972ff95445f928e0225
Josh Reed <jriddy@gmail.com>
2021-07-17 09:55:58 -0400

    expand tests to cover several json output scenarios
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/project_info/peek_test.py
+++ src/python/pants/backend/project_info/peek_test.py
@@ -7,7 +7,8 @@ import pytest
 
 from pants.backend.project_info import peek
 from pants.backend.project_info.peek import Peek
-from pants.core.target_types import Files
+from pants.core.target_types import ArchiveTarget, Files
+from pants.engine.addresses import Address
 from pants.testutil.rule_runner import RuleRunner
 
 
@@ -18,16 +19,86 @@ from pants.testutil.rule_runner import RuleRunner
             [],
             False,
             "[]\n",
-            # dedent(
-            #     """\
-            #     {
-            #       "targets": [],
-            #       "excludeDefaults": false
-            #     }
-            #     """
-            # ),
             id="null-case",
-        )
+        ),
+        pytest.param(
+            [Files({"sources": []}, Address("example", target_name="files_target"))],
+            True,
+            dedent(
+                """\
+                [
+                  {
+                    "address": "example:files_target",
+                    "target_type": "files",
+                    "sources": []
+                  }
+                ]
+                """
+            ),
+            id="single-files-target/exclude-defaults",
+        ),
+        pytest.param(
+            [Files({"sources": []}, Address("example", target_name="files_target"))],
+            False,
+            dedent(
+                """\
+                [
+                  {
+                    "address": "example:files_target",
+                    "target_type": "files",
+                    "dependencies": null,
+                    "description": null,
+                    "sources": [],
+                    "tags": null
+                  }
+                ]
+                """
+            ),
+            id="single-files-target/include-defaults",
+        ),
+        pytest.param(
+            [
+                Files(
+                    {"sources": ["*.txt"], "tags": ["zippable"]},
+                    Address("example", target_name="files_target"),
+                ),
+                ArchiveTarget(
+                    {
+                        "output_path": "my-archive.zip",
+                        "format": "zip",
+                        "files": ["example:files_target"],
+                    },
+                    Address("example", target_name="archive_target"),
+                ),
+            ],
+            True,
+            dedent(
+                """\
+                [
+                  {
+                    "address": "example:files_target",
+                    "target_type": "files",
+                    "sources": [
+                      "*.txt"
+                    ],
+                    "tags": [
+                      "zippable"
+                    ]
+                  },
+                  {
+                    "address": "example:archive_target",
+                    "target_type": "archive",
+                    "files": [
+                      "example:files_target"
+                    ],
+                    "format": "zip",
+                    "output_path": "my-archive.zip"
+                  }
+                ]
+                """
+            ),
+            id="single-files-target/exclude-defaults",
+        ),
     ],
 )
 def test_render_targets_as_json(targets, exclude_defaults, expected_output):
@@ -42,7 +113,7 @@ def rule_runner() -> RuleRunner:
 
 def test_raw_output_single_build_file(rule_runner: RuleRunner) -> None:
     rule_runner.add_to_build_file("project", "# A comment\nfiles(sources=[])")
-    result = rule_runner.run_goal_rule(Peek, args=[ "--output=raw", "project"])
+    result = rule_runner.run_goal_rule(Peek, args=["--output=raw", "project"])
     expected_output = dedent(
         """\
         -------------
@@ -58,7 +129,7 @@ def test_raw_output_single_build_file(rule_runner: RuleRunner) -> None:
 def test_raw_output_two_build_files(rule_runner: RuleRunner) -> None:
     rule_runner.add_to_build_file("project1", "# A comment\nfiles(sources=[])")
     rule_runner.add_to_build_file("project2", "# Another comment\nfiles(sources=[])")
-    result = rule_runner.run_goal_rule(Peek, args=[ "--output=raw", "project1", "project2"])
+    result = rule_runner.run_goal_rule(Peek, args=["--output=raw", "project1", "project2"])
     expected_output = dedent(
         """\
         --------------
