aa98b86f0a9ac7f38a2982a71fe9e94574ee0cde
Tom Dyas <tdyas@toolchain.com>
2021-07-20 14:19:42 -0400

    code review updates
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/go/register.py
+++ src/python/pants/backend/experimental/go/register.py
@@ -1,12 +1,12 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.backend.go import build, distribution, import_analysis, target_type_rules
-from pants.backend.go.target_types import GoBinary, GoPackage
+from pants.backend.go import build, distribution, import_analysis, target_type_rules, target_types
+from pants.backend.go.target_types import GoBinary, GoExternalModule, GoModule, GoPackage
 
 
 def target_types():
-    return [GoBinary, GoPackage]
+    return [GoBinary, GoPackage, GoModule, GoExternalModule]
 
 
 def rules():
@@ -14,5 +14,6 @@ def rules():
         *build.rules(),
         *distribution.rules(),
         *import_analysis.rules(),
+        *target_types.rules(),
         *target_type_rules.rules(),
     ]
--- src/python/pants/backend/go/target_type_rules.py
+++ src/python/pants/backend/go/target_type_rules.py
@@ -20,20 +20,26 @@ class InjectGoModuleDependency(InjectDependenciesRequest):
 
 @rule
 async def inject_go_module_dependency(request: InjectGoModuleDependency) -> InjectedDependencies:
-    original_tgt = await Get(WrappedTarget, Address, request.dependencies_field.address)
-    spec_path = original_tgt.target.address.spec_path
+    # Obtain unexpanded targets and ensure file targets are filtered out. Unlike Python, file targets do not
+    # make sense semantically for Go source since Go builds entire packages at a time. The filtering is
+    # accomplished by requesting `UnexpandedTargets` and also filtering on `is_file_target`.
+    spec_path = request.dependencies_field.address.spec_path
     candidate_targets = await Get(
         UnexpandedTargets,
         AddressSpecs([AscendantAddresses(spec_path), SiblingAddresses(spec_path)]),
     )
     go_module_targets = [
-        (tgt.address.spec_path, tgt)
+        tgt
         for tgt in candidate_targets
         if tgt.has_field(GoModuleSources) and not tgt.address.is_file_target
     ]
-    sorted_go_module_targets = sorted(go_module_targets, key=lambda x: x[0], reverse=True)
+
+    # Sort by address.spec_path in descending order so the nearest go_module target is sorted first.
+    sorted_go_module_targets = sorted(
+        go_module_targets, key=lambda tgt: tgt.address.spec_path, reverse=True
+    )
     if sorted_go_module_targets:
-        nearest_go_module_target = sorted_go_module_targets[0][1]
+        nearest_go_module_target = sorted_go_module_targets[0]
         return InjectedDependencies([nearest_go_module_target.address])
     else:
         # TODO: Consider eventually requiring all go_package's to associate with a go_module.
