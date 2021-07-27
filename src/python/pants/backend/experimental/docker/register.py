e6e525ce5f1f3da8e449d08f4af1f80fc67334c7
Andreas Stenius <andreas.stenius@svenskaspel.se>
2021-07-27 13:23:07 +0200

    no docker rules, yet.
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/docker/register.py
+++ src/python/pants/backend/experimental/docker/register.py
@@ -1,16 +1,12 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.backend.docker import rules as docker_rules
 from pants.backend.docker import tailor
 from pants.backend.docker.target_types import DockerImage
 
 
 def rules():
-    return (
-        *docker_rules.rules(),
-        *tailor.rules(),
-    )
+    return (*tailor.rules(),)
 
 
 def target_types():
