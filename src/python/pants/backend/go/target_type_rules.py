6be0571163b54330cf1bffa408cfbc78b08dc54d
Tom Dyas <tdyas@toolchain.com>
2021-08-03 10:33:11 -0500

    Go: dependency inference for packages (#12484)
    Add dependency inference for `go_package` targets as follows:
    - Add a dependency on any other imported first-party `go_package` that is part of the same Go module.
    - Add a dependency on the applicable `go_external_module` for any package imported from a third-party module
    
    This PR intentionally does not solve handling dependencies on first-party code owned by a different `go_module` than the current package's owning `go_module`. It also punts on a better refactoring of the dependency inference rules until after we figure out how to handle vendored code.
    
    Also:
    - Renames `InjectGoModuleDependency` to `InjectGoPackageDependenciesRequest`.

--- src/python/pants/backend/go/target_type_rules.py
+++ src/python/pants/backend/go/target_type_rules.py
@@ -1,19 +1,41 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+import logging
+
+from pants.backend.go import import_analysis, pkg
+from pants.backend.go.import_analysis import ResolvedImportPathsForGoLangDistribution
 from pants.backend.go.module import FindNearestGoModuleRequest, ResolvedOwningGoModule
-from pants.backend.go.target_types import GoPackageDependencies
-from pants.engine.internals.selectors import Get
+from pants.backend.go.pkg import ResolvedGoPackage, ResolveGoPackageRequest
+from pants.backend.go.target_types import (
+    GoExternalModule,
+    GoImportPath,
+    GoPackageDependencies,
+    GoPackageSources,
+)
+from pants.base.specs import AddressSpecs, MaybeEmptyDescendantAddresses, MaybeEmptySiblingAddresses
+from pants.engine.internals.selectors import Get, MultiGet
 from pants.engine.rules import collect_rules, rule
-from pants.engine.target import InjectDependenciesRequest, InjectedDependencies
+from pants.engine.target import (
+    InferDependenciesRequest,
+    InferredDependencies,
+    InjectDependenciesRequest,
+    InjectedDependencies,
+    UnexpandedTargets,
+)
 from pants.engine.unions import UnionRule
 
+logger = logging.getLogger(__name__)
 
-class InjectGoModuleDependency(InjectDependenciesRequest):
+
+# Inject a dependency between a go_package and its owning go_module.
+class InjectGoPackageDependenciesRequest(InjectDependenciesRequest):
     inject_for = GoPackageDependencies
 
 
 @rule
-async def inject_go_module_dependency(request: InjectGoModuleDependency) -> InjectedDependencies:
+async def inject_go_package_dependencies(
+    request: InjectGoPackageDependenciesRequest,
+) -> InjectedDependencies:
     owning_go_module_result = await Get(
         ResolvedOwningGoModule,
         FindNearestGoModuleRequest(request.dependencies_field.address.spec_path),
@@ -24,8 +46,105 @@ async def inject_go_module_dependency(request: InjectGoModuleDependency) -> Inje
         return InjectedDependencies()
 
 
+class InferGoPackageDependenciesRequest(InferDependenciesRequest):
+    infer_from = GoPackageSources
+
+
+# TODO: Refactor this rule so as much as possible is memoized by invoking other rules. Consider
+# for example `FirstPartyPythonModuleMapping` and `ThirdPartyPythonModuleMapping`.
+@rule
+async def infer_go_dependencies(
+    request: InferGoPackageDependenciesRequest,
+    goroot_imports: ResolvedImportPathsForGoLangDistribution,
+) -> InferredDependencies:
+    this_go_package = await Get(
+        ResolvedGoPackage, ResolveGoPackageRequest(request.sources_field.address)
+    )
+
+    # Obtain all go_package targets under this package's go_module.
+    assert this_go_package.module_address is not None
+    spec_path = this_go_package.module_address.spec_path
+    address_specs = [
+        MaybeEmptySiblingAddresses(spec_path),
+        MaybeEmptyDescendantAddresses(spec_path),
+    ]
+    candidate_targets = await Get(UnexpandedTargets, AddressSpecs(address_specs))
+    go_package_targets = [
+        tgt
+        for tgt in candidate_targets
+        if tgt.has_field(GoPackageSources)
+        and not tgt.address.is_file_target
+        and tgt.address != this_go_package.address
+    ]
+
+    # Find all go_external_modules in the repo and map their import paths to their address.
+    candidate_go_external_module_targets = await Get(
+        UnexpandedTargets, AddressSpecs([MaybeEmptyDescendantAddresses("")])
+    )
+    go_external_module_targets = [
+        tgt for tgt in candidate_go_external_module_targets if isinstance(tgt, GoExternalModule)
+    ]
+    third_party_import_path_to_address = {
+        tgt[GoImportPath].value: tgt.address for tgt in go_external_module_targets
+    }
+
+    # Resolve all of the packages found.
+    first_party_import_path_to_address = {}
+    first_party_go_packages = await MultiGet(
+        Get(ResolvedGoPackage, ResolveGoPackageRequest(tgt.address)) for tgt in go_package_targets
+    )
+    for first_party_go_package in first_party_go_packages:
+        # Skip packages that are not part of this package's module.
+        # TODO: This requires that all first-party code in the monorepo be part of the same go_module. Will need
+        # figure out how multiple modules in a monorepo can interact.
+        if first_party_go_package.module_address != this_go_package.module_address:
+            continue
+
+        first_party_import_path_to_address[
+            first_party_go_package.import_path
+        ] = first_party_go_package.address
+
+    # Loop through all of the imports of this package and add dependencies on other packages and
+    # external modules.
+    inferred_dependencies = []
+    for import_path in this_go_package.imports + this_go_package.test_imports:
+        # Check whether the import path comes from the standard library.
+        if import_path in goroot_imports.import_path_mapping:
+            continue
+
+        # Infer first-party dependencies to other packages in same go_module.
+        if import_path in first_party_import_path_to_address:
+            inferred_dependencies.append(first_party_import_path_to_address[import_path])
+            continue
+
+        # Infer third-party dependencies on go_external_module targets.
+        found_module_import = False
+        for module_import_path, address in third_party_import_path_to_address.items():
+            # TODO: This check assumes that the import path for the external module is a prefix of any package
+            # from the external module and that the import path will never overlap with another external module's
+            # import path. This may be the case, but bears confirmation.
+            #
+            # TODO: Also cleanup mandatory go_module ownership, so module_import_path is not Optional[str].
+            if module_import_path and import_path.startswith(module_import_path):
+                inferred_dependencies.append(address)
+                found_module_import = True
+
+        if found_module_import:
+            continue
+
+        logger.debug(
+            f"Unable to infer dependency for import path '{import_path}' "
+            f"in go_package at address '{this_go_package.address}'."
+        )
+
+    return InferredDependencies(inferred_dependencies, sibling_dependencies_inferrable=False)
+
+
 def rules():
     return (
         *collect_rules(),
-        UnionRule(InjectDependenciesRequest, InjectGoModuleDependency),
+        *pkg.rules(),
+        *import_analysis.rules(),
+        UnionRule(InjectDependenciesRequest, InjectGoPackageDependenciesRequest),
+        UnionRule(InferDependenciesRequest, InferGoPackageDependenciesRequest),
     )
--- src/python/pants/backend/go/target_type_rules_test.py
+++ src/python/pants/backend/go/target_type_rules_test.py
@@ -1,15 +1,31 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+import textwrap
+
 import pytest
 
-from pants.backend.go import module, target_type_rules
-from pants.backend.go.target_types import GoModule, GoModuleSources, GoPackage
+from pants.backend.go import module, pkg, target_type_rules
+from pants.backend.go.target_type_rules import InferGoPackageDependenciesRequest
+from pants.backend.go.target_types import (
+    GoExternalModule,
+    GoModule,
+    GoModuleSources,
+    GoPackage,
+    GoPackageSources,
+)
 from pants.build_graph.address import Address
 from pants.core.util_rules import external_tool, source_files
 from pants.engine.addresses import Addresses
 from pants.engine.rules import QueryRule
-from pants.engine.target import Dependencies, DependenciesRequest, Target, UnexpandedTargets
+from pants.engine.target import (
+    Dependencies,
+    DependenciesRequest,
+    InferredDependencies,
+    Target,
+    UnexpandedTargets,
+)
 from pants.testutil.rule_runner import RuleRunner
+from pants.util.ordered_set import FrozenOrderedSet
 
 
 @pytest.fixture
@@ -19,11 +35,13 @@ def rule_runner() -> RuleRunner:
             *external_tool.rules(),
             *source_files.rules(),
             *module.rules(),
+            *pkg.rules(),
             *target_type_rules.rules(),
             QueryRule(Addresses, (DependenciesRequest,)),
             QueryRule(UnexpandedTargets, (Addresses,)),
+            QueryRule(InferredDependencies, (InferGoPackageDependenciesRequest,)),
         ],
-        target_types=[GoPackage, GoModule],
+        target_types=[GoPackage, GoModule, GoExternalModule],
     )
 
 
@@ -35,21 +53,91 @@ def assert_go_module_address(rule_runner: RuleRunner, target: Target, expected_a
     assert go_module_targets[0].address == expected_address
 
 
-def test_go_module_dependency_injection(rule_runner: RuleRunner) -> None:
-    rule_runner.add_to_build_file("foo", "go_module()")
+def test_go_package_dependency_injection(rule_runner: RuleRunner) -> None:
     rule_runner.write_files(
         {
-            "foo/pkg/foo.go": "package pkg\n",
+            "foo/BUILD": "go_module()\n",
             "foo/go.mod": "module foo\n",
             "foo/go.sum": "",
+            "foo/pkg/BUILD": "go_package()\n",
+            "foo/pkg/foo.go": "package pkg\n",
+            "foo/bar/BUILD": "go_module(name='mod')\ngo_package(name='pkg')\n",
+            "foo/bar/go.mod": "module bar\n",
             "foo/bar/src.go": "package bar\n",
         }
     )
-    rule_runner.add_to_build_file("foo/pkg", "go_package()")
-    rule_runner.add_to_build_file("foo/bar", "go_module(name='mod')\ngo_package(name='pkg')\n")
 
     target = rule_runner.get_target(Address("foo/pkg", target_name="pkg"))
     assert_go_module_address(rule_runner, target, Address("foo"))
 
     target = rule_runner.get_target(Address("foo/bar", target_name="pkg"))
     assert_go_module_address(rule_runner, target, Address("foo/bar", target_name="mod"))
+
+
+def test_go_package_dependency_inference(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(
+        (
+            {
+                "foo/BUILD": textwrap.dedent(
+                    """\
+                    go_module()
+                    go_external_module(
+                      name="github.com_google_go-cmp_0.4.0",
+                      path="github.com/google/go-cmp/cmp",
+                      version="v0.4.0",
+                      import_path="github.com/google/go-cmp",
+                    )
+                """
+                ),
+                "foo/go.mod": textwrap.dedent(
+                    """\
+                module go.example.com/foo
+                go 1.16"""
+                ),
+                "foo/go.sum": textwrap.dedent(
+                    """\
+                github.com/google/go-cmp v0.4.0/go.mod h1:v8dTdLbMG2kIc/vJvl+f65V22dbkXbowE6jgT/gNBxE=
+                golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 h1:E7g+9GITq07hpfrRu66IVDexMakfv52eLZ2CXBWiKr4=
+                golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
+                """
+                ),
+                "foo/pkg/BUILD": "go_package()\n",
+                "foo/pkg/foo.go": textwrap.dedent(
+                    """\
+            package pkg
+            import "github.com/google/go-cmp/cmp"
+            func grok(left, right string) bool {
+                return cmp.Equal(left, right)
+            }
+            """
+                ),
+                "foo/cmd/BUILD": "go_package()\n",
+                "foo/cmd/main.go": textwrap.dedent(
+                    """\
+                package main
+                import (
+                    "fmt"
+                    "go.example.com/foo/pkg"
+                )
+                func main() {
+                    fmt.Printf("%s\n", pkg.Grok())
+                }"""
+                ),
+            }
+        )
+    )
+    target1 = rule_runner.get_target(Address("foo/cmd"))
+    inferred_deps_1 = rule_runner.request(
+        InferredDependencies, [InferGoPackageDependenciesRequest(target1[GoPackageSources])]
+    )
+    assert inferred_deps_1.dependencies == FrozenOrderedSet([Address("foo/pkg")])
+    assert not inferred_deps_1.sibling_dependencies_inferrable
+
+    target2 = rule_runner.get_target(Address("foo/pkg"))
+    inferred_deps_2 = rule_runner.request(
+        InferredDependencies, [InferGoPackageDependenciesRequest(target2[GoPackageSources])]
+    )
+    assert inferred_deps_2.dependencies == FrozenOrderedSet(
+        [Address("foo", target_name="github.com_google_go-cmp_0.4.0")]
+    )
+    assert not inferred_deps_2.sibling_dependencies_inferrable
