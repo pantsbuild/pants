95ccba836ecd55896a696042fed8bb12ae23870a
Tom Dyas <tdyas@toolchain.com>
2021-08-03 16:55:12 -0400

    SDK invoke rules
    [ci skip-build-wheels]

---
+++ src/python/pants/backend/go/sdk.py
@@ -0,0 +1,87 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+import textwrap
+from dataclasses import dataclass
+from typing import Optional, Tuple
+
+from pants.backend.go.distribution import GoLangDistribution
+from pants.core.util_rules.external_tool import DownloadedExternalTool, ExternalToolRequest
+from pants.engine.fs import CreateDigest, Digest, FileContent, MergeDigests
+from pants.engine.internals.selectors import Get
+from pants.engine.platform import Platform
+from pants.engine.process import BashBinary, Process, ProcessResult
+from pants.engine.rules import collect_rules, rule
+from pants.util.logging import LogLevel
+
+
+@dataclass(frozen=True)
+class InvokeGoSdkRequest:
+    digest: Digest
+    command: Tuple[str, ...]
+    description: str
+    working_dir: Optional[str] = None
+    output_files: Tuple[str, ...] = ()
+    output_directories: Tuple[str, ...] = ()
+
+
+@dataclass(frozen=True)
+class InvokeGoSdkResult:
+    result: ProcessResult
+
+
+@rule
+async def invoke_go_sdk(
+    request: InvokeGoSdkRequest,
+    goroot: GoLangDistribution,
+    bash: BashBinary,
+) -> InvokeGoSdkResult:
+    downloaded_goroot = await Get(
+        DownloadedExternalTool,
+        ExternalToolRequest,
+        goroot.get_request(Platform.current),
+    )
+
+    working_dir_cmd = f"cd '{request.working_dir}'" if request.working_dir else ""
+
+    script_digest = await Get(
+        Digest,
+        CreateDigest(
+            [
+                FileContent(
+                    "__pants__.sh",
+                    textwrap.dedent(
+                        f"""\
+                export GOROOT="$(/bin/pwd)/go"
+                export GOPATH="$(/bin/pwd)/gopath"
+                export GOCACHE="$(/bin/pwd)/cache"
+                /bin/mkdir -p "$GOPATH" "$GOCACHE"
+                {working_dir_cmd}
+                exec {' '.join(request.command)}
+                """
+                    ).encode("utf-8"),
+                )
+            ]
+        ),
+    )
+
+    input_root_digest = await Get(
+        Digest,
+        MergeDigests([downloaded_goroot.digest, script_digest, request.digest]),
+    )
+
+    process = Process(
+        argv=[bash.path, "__pants__.sh"],
+        input_digest=input_root_digest,
+        description=request.description,
+        output_files=request.output_files,
+        output_directories=request.output_directories,
+        level=LogLevel.DEBUG,
+    )
+
+    result = await Get(ProcessResult, Process, process)
+
+    return InvokeGoSdkResult(result=result)
+
+
+def rules():
+    return collect_rules()
