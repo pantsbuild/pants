ca71ead2a72c013f966148e3e3185abd77b3555f
Eric Arellano <ericarellano@me.com>
2021-08-16 19:39:24 -0700

    Fix imports
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/python/lint/pylint/rules_integration_test.py
+++ src/python/pants/backend/python/lint/pylint/rules_integration_test.py
@@ -7,10 +7,10 @@ from textwrap import dedent
 
 import pytest
 
+from pants.backend.python.lint.pylint import subsystem
 from pants.backend.python.lint.pylint.rules import PylintRequest
 from pants.backend.python.lint.pylint.rules import rules as pylint_rules
 from pants.backend.python.lint.pylint.subsystem import PylintFieldSet
-from pants.backend.python.lint.pylint import subsystem
 from pants.backend.python.target_types import PythonLibrary, PythonRequirementLibrary
 from pants.core.goals.lint import LintResult, LintResults
 from pants.core.util_rules import config_files
