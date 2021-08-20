af9842a4d1750091747ce5bafe84155dc321d15b
Eric Arellano <ericarellano@me.com>
2021-08-19 17:47:16 -0700

    Add `from __future__ import annotations`
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/python/lockfile_metadata_test.py
+++ src/python/pants/backend/experimental/python/lockfile_metadata_test.py
@@ -1,6 +1,8 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import annotations
+
 import pytest
 
 from pants.backend.experimental.python.lockfile_metadata import (
