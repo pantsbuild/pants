36cc5d54a3792c9fa5e9fdb41d14db17df6ce4ca
Eric Arellano <ericarellano@me.com>
2021-08-26 11:13:52 -0700

    Fix missing wildcard in coverage constraints
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/python/goals/coverage_py_integration_test.py
+++ src/python/pants/backend/python/goals/coverage_py_integration_test.py
@@ -140,7 +140,7 @@ def run_coverage(tmpdir: str, *extra_args: str) -> PantsResult:
 def test_coverage(major_minor_interpreter: str) -> None:
     with setup_tmpdir(SOURCES) as tmpdir:
         result = run_coverage(
-            tmpdir, f"--coverage-py-interpreter-constraints=['=={major_minor_interpreter}']"
+            tmpdir, f"--coverage-py-interpreter-constraints=['=={major_minor_interpreter}.*']"
         )
     assert (
         dedent(
