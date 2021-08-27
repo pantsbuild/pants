0d9470914d794d43a5f79948e9a97abf36cbaba3
Eric Arellano <ericarellano@me.com>
2021-08-27 11:36:16 -0700

    [wip] Stop pinning `stevedore<3` by default for Bandit
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/python/lint/bandit/subsystem.py
+++ src/python/pants/backend/python/lint/bandit/subsystem.py
@@ -48,8 +48,7 @@ class Bandit(PythonToolBase):
     default_version = "bandit>=1.7.0,<1.8"
     default_extra_requirements = [
         "setuptools<45; python_full_version == '2.7.*'",
-        "setuptools; python_version > '2.7'",
-        "stevedore<3",  # stevedore 3.0 breaks Bandit.
+        "setuptools; python_version >= '3.5'",
     ]
     default_main = ConsoleScript("bandit")
 
