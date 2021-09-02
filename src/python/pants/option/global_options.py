c7bb8c40495f99759f07b160fe3259e3e708cc84
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2021-09-01 17:03:00 -0700

    Include `.gitignore` in default `pantsd_invalidation_globs` (#12711) (#12730)
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/option/global_options.py
+++ src/python/pants/option/global_options.py
@@ -1525,6 +1525,7 @@ class GlobalOptions(Subsystem):
             (
                 "!*.pyc",
                 "!__pycache__/",
+                ".gitignore",
                 # TODO: This is a bandaid for https://github.com/pantsbuild/pants/issues/7022:
                 # macros should be adapted to allow this dependency to be automatically detected.
                 "requirements.txt",
