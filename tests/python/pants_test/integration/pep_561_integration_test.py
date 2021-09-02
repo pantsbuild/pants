900ba015b9517f9418b4fa9b2cb925a7f7092e2f
Andreas Stenius <andreas.stenius@svenskaspel.se>
2021-09-02 22:08:05 +0200

    add failing test for PEP-561 compliance test.
    Signed-off-by: Andreas Stenius <andreas.stenius@svenskaspel.se>
    
    [ci skip-rust]
    
    [ci skip-build-wheels]

---
+++ tests/python/pants_test/integration/pep_561_integration_test.py
@@ -0,0 +1,83 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import os
+from glob import glob
+from textwrap import dedent
+
+from pants.testutil.pants_integration_test import PantsResult, run_pants, setup_tmpdir
+
+
+def typecheck_file(filename: str) -> PantsResult:
+    return run_pants(
+        [
+            "--backend-packages=["
+            "'pants.backend.python',"
+            "'pants.backend.python.typecheck.mypy'"
+            "]",
+            "typecheck",
+            filename,
+        ],
+        # match the setup_py_commands --python-tag of src/python/pants/testutil:testutil_wheel
+        config={"python-setup": {"interpreter_constraints": ["CPython>=3.7<=3.9"]}},
+    )
+
+
+def test_typecheck_works() -> None:
+    project = {
+        "proj1/BUILD": "python_library()",
+        "proj1/ok.py": dedent(
+            """\
+            def dummy() -> int:
+                return 42
+            """
+        ),
+        "proj1/err.py": dedent(
+            """\
+            def dummy() -> int:
+                return "not an int"
+            """
+        ),
+    }
+
+    with setup_tmpdir(project) as tmpdir:
+        ok_result = typecheck_file(f"{tmpdir}/proj1/ok.py")
+        err_result = typecheck_file(f"{tmpdir}/proj1/err.py")
+
+    ok_result.assert_success()
+    err_result.assert_failure()
+
+
+def test_type_stubs() -> None:
+    wheel = glob(f"{os.getcwd()}/pantsbuild.pants.testutil-*.whl")[0]
+    project = {
+        "proj1/BUILD": dedent(
+            f"""\
+            python_requirement_library(
+                name="pants",
+                requirements=["Pants @ file://{wheel}"],
+            )
+            python_library()
+            """
+        ),
+        "proj1/ok.py": dedent(
+            """\
+            from pants.testutil.option_util import create_subsystem
+            """
+        ),
+        "proj1/err.py": dedent(
+            """\
+            from pants.testutil.option_util import create_subsystem
+
+            def dummy() -> None:
+                create_subsystem()
+            """
+        ),
+    }
+
+    with setup_tmpdir(project) as tmpdir:
+        ok_result = typecheck_file(f"{tmpdir}/proj1/ok.py")
+        err_result = typecheck_file(f"{tmpdir}/proj1/err.py")
+
+    ok_result.assert_success()
+    err_result.assert_failure()
