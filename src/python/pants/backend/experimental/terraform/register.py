3cdd8987b9f1eaf4340134f200120bcb69efca70
Tom Dyas <tdyas@toolchain.com>
2021-09-17 14:12:35 -0400

    [internal] terraform: add dependency inference for local modules (#12916)
    Add dependency inference between `terraform_module` targets that are included via the Terraform `module` block with a local source path.
    
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/terraform/register.py
+++ src/python/pants/backend/experimental/terraform/register.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
-from pants.backend.terraform import tailor, target_gen, tool
+from pants.backend.python.util_rules.pex import rules as pex_rules
+from pants.backend.terraform import dependency_inference, tailor, target_gen, tool
 from pants.backend.terraform.lint import fmt
 from pants.backend.terraform.target_types import TerraformModule, TerraformModules
 from pants.backend.terraform.target_types import rules as target_types_rules
@@ -14,9 +15,11 @@ def target_types():
 def rules():
     return [
         *collect_rules(),
+        *dependency_inference.rules(),
         *tailor.rules(),
         *target_gen.rules(),
         *target_types_rules(),
         *tool.rules(),
         *fmt.rules(),
+        *pex_rules(),
     ]
