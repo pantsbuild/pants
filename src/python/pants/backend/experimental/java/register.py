10a7f2e70ff5217842764694a307cc5d97220280
Tom Dyas <tdyas@toolchain.com>
2021-09-21 13:58:33 -0400

    add util_rules to plugin registration
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/java/register.py
+++ src/python/pants/backend/experimental/java/register.py
@@ -2,11 +2,12 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from pants.backend.java import tailor
+from pants.backend.java import util_rules as java_util_rules
 from pants.backend.java.compile import javac, javac_binary
 from pants.backend.java.target_types import JavaLibrary, JunitTests
 from pants.backend.java.target_types import rules as target_types_rules
 from pants.backend.java.test import junit
-from pants.jvm import util_rules
+from pants.jvm import util_rules as jvm_util_rules
 from pants.jvm.goals import coursier
 from pants.jvm.resolve import coursier_fetch, coursier_setup
 from pants.jvm.target_types import JvmDependencyLockfile
@@ -25,6 +26,7 @@ def rules():
         *coursier_fetch.rules(),
         *coursier_setup.rules(),
         *tailor.rules(),
-        *util_rules.rules(),
+        *jvm_util_rules.rules(),
+        *java_util_rules.rules(),
         *target_types_rules(),
     ]
