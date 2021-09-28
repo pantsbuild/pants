b5ef4ba1aee388c7d336096576f8c5887c70be3f
Stu Hood <stuhood@gmail.com>
2021-09-28 10:00:07 -0700

    Since #12585, `--colors` has not needed to be a bootstrap option. Move it to global_options to avoid invalidating the Scheduler when piping output.
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/option/global_options.py
+++ src/python/pants/option/global_options.py
@@ -558,17 +558,6 @@ class GlobalOptions(Subsystem):
         )
 
         register(
-            "--colors",
-            type=bool,
-            default=sys.stdout.isatty(),
-            help=(
-                "Whether Pants should use colors in output or not. This may also impact whether "
-                "some tools Pants runs use color.\n\nWhen unset, this value defaults based on "
-                "whether the output destination supports color."
-            ),
-        )
-
-        register(
             "--ignore-warnings",
             type=list,
             member_type=str,
@@ -1303,6 +1292,16 @@ class GlobalOptions(Subsystem):
         # global-scope options, for convenience.
         cls.register_bootstrap_options(register)
 
+        register(
+            "--colors",
+            type=bool,
+            default=sys.stdout.isatty(),
+            help=(
+                "Whether Pants should use colors in output or not. This may also impact whether "
+                "some tools Pants runs use color.\n\nWhen unset, this value defaults based on "
+                "whether the output destination supports color."
+            ),
+        )
         register(
             "--dynamic-ui",
             type=bool,
