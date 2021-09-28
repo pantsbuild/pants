cbc9ee9f2d8893ac3c1ce459d5815126bd24feec
Christopher Neugebauer <chrisjrn@toolchain.com>
2021-09-28 08:12:10 -0700

    Complicates org.pantsbuild.example by making it depend on things
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- testprojects/src/java/org/pantsbuild/example/ExampleLib.java
+++ testprojects/src/java/org/pantsbuild/example/ExampleLib.java
@@ -1,7 +1,29 @@
-package org.pantsbuild.example.lib;
+package org.pantsbuild.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ExampleLib {
-    public static String hello() {
-        return "Hello!";
+    private String template = "{\"contents\": \"Hello, World!\"}";
+
+    public String getGreeting() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            SerializedThing thing = mapper.readValue(template, SerializedThing.class);
+            return thing.contents;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public static void main(String[] args) {
+        System.out.println(new ExampleLib().getGreeting());
+    }
+}
+
+
+// TODO: Replicate in public class, and then in a public class in another package.
+class SerializedThing {
+    
+    public String contents;
+
 }
