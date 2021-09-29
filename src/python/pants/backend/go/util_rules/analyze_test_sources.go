59f043015d69d388670382fdae75ae94f82c4668
Tom Dyas <tdyas@toolchain.com>
2021-09-29 19:27:45 -0400

    draft support for analyzer
    [ci skip-rust]
    
    [ci skip-build-wheels]

---
+++ src/python/pants/backend/go/util_rules/analyze_test_sources.go
@@ -0,0 +1,168 @@
+/* Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+ * Licensed under the Apache License, Version 2.0 (see LICENSE).
+ */
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
+	"strings"
+)
+
+
+//
+// Parse Go sources and extract various metadata about the tests contained therein.
+// Based in part on:
+//   * rules_go (https://github.com/bazelbuild/rules_go/blob/master/go/tools/builders/generate_test_main.go)
+//   * `go` tool (https://github.com/golang/go/blob/master/src/cmd/go/internal/load/test.go)
+//
+// As explained by the rules_go source:
+//
+// A Go test comprises three packages:
+//
+// 1. An internal test package, compiled from the sources of the library being
+//    tested and any _test.go files with the same package name.
+// 2. An external test package, compiled from _test.go files with a package
+//    name ending with "_test".
+// 3. A generated main package that imports both packages and initializes the
+//    test framework with a list of tests, benchmarks, examples, and fuzz
+//    targets read from source files.
+//
+
+type TestCase struct {
+	Package string
+	Name string
+}
+
+// TestSourcesMetadata contains metadata about tests/benchmarks extracted from the parsed sources.
+// TODO: "Examples" and "fuzz targets" (Go 1.18+).
+type TestSourcesMetadata struct {
+	// Names of all functions in the test sources that heuristically look like test functions.
+	Tests []TestCase
+
+	// Names of all functions in the test sources that heuristically look like benchmark functions.
+	Benchmarks []TestCase
+
+	// True if the sources already contain a `TestMain` function (which is the entry point for test binaries).
+	HasTestMain bool
+}
+
+func processFile(fileSet *token.FileSet, filename string) (*TestSourcesMetadata, error) {
+	p, err := parser.ParseFile(fileSet, filename, nil, parser.ParseComments)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse: %s", err)
+	}
+
+	var metadata TestSourcesMetadata
+
+	pkgName := p.Name.Name
+
+	for _, decl := range p.Decls {
+		fn, ok := decl.(*ast.FuncDecl)
+		if !ok {
+			continue
+		}
+		if fn.Recv != nil {
+			continue
+		}
+
+		// If the source has a `TestMain` function, then record that fact so that Pants knows to use the
+		// user-supplied `TestMain` instead of generating a `TestMain`.
+		if fn.Name.Name == "TestMain" {
+			metadata.HasTestMain = true
+			continue
+		}
+
+		// The following test/benchmark heuristic is based on the code in Bazel's rules_go.
+		// https://github.com/bazelbuild/rules_go/blob/master/go/tools/builders/generate_test_main.go#L308-L367
+
+		// Here we check the signature of the Test* function. To be considered a test:
+
+		// 1. The function should have a single argument.
+		if len(fn.Type.Params.List) != 1 {
+			continue
+		}
+
+		// 2. The function should return nothing.
+		if fn.Type.Results != nil {
+			continue
+		}
+
+		// 3. The only parameter should have a type identified as
+		//    *<something>.T
+		starExpr, ok := fn.Type.Params.List[0].Type.(*ast.StarExpr)
+		if !ok {
+			continue
+		}
+		selExpr, ok := starExpr.X.(*ast.SelectorExpr)
+		if !ok {
+			continue
+		}
+
+		// We do not discriminate on the referenced type of the
+		// parameter being *testing.T. Instead, we assert that it
+		// should be *<something>.T. This is because the import
+		// could have been aliased as a different identifier.
+
+		if strings.HasPrefix(fn.Name.Name, "Test") {
+			if selExpr.Sel.Name != "T" {
+				continue
+			}
+			metadata.Tests = append(metadata.Tests, TestCase{
+				Name: fn.Name.Name,
+				Package: pkgName,
+			})
+		}
+		if strings.HasPrefix(fn.Name.Name, "Benchmark") {
+			if selExpr.Sel.Name != "B" {
+				continue
+			}
+			metadata.Benchmarks = append(metadata.Benchmarks, TestCase{
+				Name: fn.Name.Name,
+				Package: pkgName,
+			})
+		}
+	}
+
+	return &metadata, nil
+}
+
+func main() {
+	var allMetadata TestSourcesMetadata
+
+	fileSet := token.NewFileSet()
+	for _, arg := range os.Args {
+		fileMetadata, err := processFile(fileSet, arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s", arg, err)
+			os.Exit(1)
+		}
+
+		allMetadata.Tests = append(allMetadata.Tests, fileMetadata.Tests...)
+		allMetadata.Benchmarks = append(allMetadata.Benchmarks, fileMetadata.Benchmarks...)
+		allMetadata.HasTestMain = allMetadata.HasTestMain || fileMetadata.HasTestMain
+	}
+
+	output, err := json.Marshal(&allMetadata)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to marshall JSON output: %s", err)
+		os.Exit(1)
+	}
+
+	amtWritten := 0
+	for amtWritten < len(output) {
+		n, err := os.Stdout.Write(output[amtWritten:])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to write output: %s", err)
+			os.Exit(1)
+		}
+		amtWritten += n
+	}
+
+	os.Exit(0)
+}
---
+++ src/python/pants/backend/go/util_rules/test_analysis.py
@@ -0,0 +1,127 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+from __future__ import annotations
+
+import json
+import pkgutil
+from dataclasses import dataclass
+
+from pants.backend.go.util_rules.compile import CompiledGoSources, CompileGoSourcesRequest
+from pants.backend.go.util_rules.import_analysis import GatheredImports, GatherImportsRequest
+from pants.backend.go.util_rules.link import LinkedGoBinary, LinkGoBinaryRequest
+from pants.engine.fs import CreateDigest, Digest, FileContent, MergeDigests
+from pants.engine.internals.selectors import Get, MultiGet
+from pants.engine.process import Process, ProcessResult
+from pants.engine.rules import collect_rules, rule
+from pants.util.ordered_set import FrozenOrderedSet
+
+
+@dataclass(frozen=True)
+class AnalyzeTestSourcesRequest:
+    digest: Digest
+    paths: FrozenOrderedSet[str]
+
+
+@dataclass(frozen=True)
+class TestCase:
+    name: str
+    package: str
+
+    @classmethod
+    def from_json_dict(cls, data: dict) -> TestCase:
+        return cls(name=data["name"], package=data["package"])
+
+
+@dataclass(frozen=True)
+class AnalyzedTestSources:
+    tests: FrozenOrderedSet[TestCase]
+    benchmarks: FrozenOrderedSet[TestCase]
+    has_test_main: bool
+
+    @classmethod
+    def from_json_dict(cls, data: dict) -> AnalyzedTestSources:
+        return cls(
+            tests=FrozenOrderedSet([TestCase.from_json_dict(d) for d in data["tests"]]),
+            benchmarks=FrozenOrderedSet([TestCase.from_json_dict(d) for d in data["benchmarks"]]),
+            has_test_main=data["has_test_main"],
+        )
+
+
+@dataclass(frozen=True)
+class AnalyzerSetup:
+    digest: Digest
+    path: str
+
+
+@rule
+async def setup_analyzer() -> AnalyzerSetup:
+    source_entry_content = pkgutil.get_data(
+        "pants.backend.go.util_rules", "analyze_test_sources.go"
+    )
+    if not source_entry_content:
+        raise ValueError("Unable to find resouce for `analyze_test_sources.go`.")
+
+    source_entry = FileContent(
+        "analyze_test_sources.go",
+        source_entry_content,
+    )
+
+    source_digest, imports = await MultiGet(
+        Get(Digest, CreateDigest([source_entry])),
+        Get(
+            GatheredImports, GatherImportsRequest(packages=FrozenOrderedSet(), include_stdlib=True)
+        ),
+    )
+
+    input_digest = await Get(Digest, MergeDigests([source_digest, imports.digest]))
+
+    compiled_analyzer = await Get(
+        CompiledGoSources,
+        CompileGoSourcesRequest(
+            digest=input_digest,
+            sources=(source_entry.path,),
+            import_path="main",
+            description="Compile Go test sources analyzer.",
+            import_config_path="./importcfg",
+        ),
+    )
+
+    link_input_digest = await Get(
+        Digest, MergeDigests([compiled_analyzer.output_digest, imports.digest])
+    )
+
+    analyzer = await Get(
+        LinkedGoBinary,
+        LinkGoBinaryRequest(
+            input_digest=link_input_digest,
+            archives=("__pkg__.a",),
+            import_config_path="./importcfg",
+            output_filename="./analyzer",
+            description="Link Go test sources analyzer.",
+        ),
+    )
+
+    return AnalyzerSetup(digest=analyzer.output_digest, path="./analyzer")
+
+
+@rule
+async def analyze_test_sources(
+    request: AnalyzeTestSourcesRequest, analyzer: AnalyzerSetup
+) -> AnalyzedTestSources:
+    input_digest = await Get(Digest, MergeDigests([request.digest, analyzer.digest]))
+
+    result = await Get(
+        ProcessResult,
+        Process(
+            argv=(analyzer.path, *request.paths),
+            input_digest=input_digest,
+            description="Analyze Go test sources.",
+        ),
+    )
+
+    metadata = json.loads(result.stdout)
+    return AnalyzedTestSources.from_json_dict(metadata)
+
+
+def rules():
+    return collect_rules()
