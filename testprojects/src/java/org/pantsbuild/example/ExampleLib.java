3686f0207cec85c04c912a3a826e8c6af6dde90f
Christopher Neugebauer <chrisjrn@toolchain.com>
2021-09-30 09:26:18 -0700

    Partially revert java ExampleLib
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- testprojects/src/java/org/pantsbuild/example/ExampleLib.java
+++ testprojects/src/java/org/pantsbuild/example/ExampleLib.java
@@ -1,29 +1,7 @@
-package org.pantsbuild.example;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
+package org.pantsbuild.example.lib;
 
 public class ExampleLib {
-    private String template = "{\"contents\": \"Hello, World!\"}";
-
-    public String getGreeting() {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            SerializedThing thing = mapper.readValue(template, SerializedThing.class);
-            return thing.contents;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public static String hello() {
+        return "Hello!";
     }
-
-    public static void main(String[] args) {
-        System.out.println(new ExampleLib().getGreeting());
-    }
-}
-
-
-// TODO: Replicate in public class, and then in a public class in another package.
-class SerializedThing {
-    
-    public String contents;
-
 }
