6c6886f0eabbfc0dc29e139196ebace24e1985b1
Stu Hood <stuhood@gmail.com>
2021-09-30 12:25:48 -0700

    Enable JVM tests by default, but leave the decorator.
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/jvm/testutil.py
+++ src/python/pants/jvm/testutil.py
@@ -1,12 +1,12 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import ast
 import os
 
 import pytest
 
 
 def maybe_skip_jdk_test(func):
-    return pytest.mark.skipif("PANTS_RUN_JDK_TESTS" not in os.environ, reason="Skip JDK tests")(
-        func
-    )
+    run_jdk_tests = bool(ast.literal_eval(os.environ.get("PANTS_RUN_JDK_TESTS", "True")))
+    return pytest.mark.skipif(not run_jdk_tests, reason="Skip JDK tests")(func)
