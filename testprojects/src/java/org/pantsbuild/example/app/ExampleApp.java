7a7ba6300107491f492440e21b38eaa8fbd60d2f
Christopher Neugebauer <chrisjrn@toolchain.com>
2021-09-30 10:11:26 -0700

    Adds `ExampleApp` class
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

---
+++ testprojects/src/java/org/pantsbuild/example/app/ExampleApp.java
@@ -0,0 +1,8 @@
+package org.pantsbuild.example.app;
+
+public class ExampleApp {
+    public static void main(String[] args) {
+        // TODO: Use dependencies
+        System.out.println("Hello, World");
+    }
+}
