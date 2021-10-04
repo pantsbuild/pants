74c5130c4ae67bce6f33a0b8bd48f020c21e3cac
Christopher Neugebauer <chrisjrn@toolchain.com>
2021-10-04 11:02:20 -0700

    add dependency inference
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- testprojects/src/java/org/pantsbuild/example/app/ExampleApp.java
+++ testprojects/src/java/org/pantsbuild/example/app/ExampleApp.java
@@ -1,9 +1,9 @@
 package org.pantsbuild.example.app;
 
-//import org.pantsbuild.example.lib.ExampleLib;
+import org.pantsbuild.example.lib.ExampleLib;
 
 public class ExampleApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!" /* ExampleLib.hello() /**/);
+        System.out.println(ExampleLib.hello());
     }
 }
