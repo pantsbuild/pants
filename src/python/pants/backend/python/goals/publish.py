1d23bcc80ae28f6f56f65609a43f0cbf9cbfb07d
Andreas Stenius <andreas.stenius@svenskaspel.se>
2021-10-05 09:35:37 +0200

    New `publish` goal (#13057)
    This adds the core functionality to support publishing artifacts/assets/distributions to repositories/registries etc.
    
    To be usable, there is a publish plugin using twine for the experimental python backend, adding a `pypi_repositories` field to `python_distribution` targets.
    
    Example BUILD file usage for using Twine:
    ```python
    python_distribution(
        ...
        pypi_repositories=[
            "https://pypi-registry.example.com",  # Use regular Twine env vars to configure credentials.
            "@pypi",  # Configure using a `.pypirc` config file, or with a `_PYPI` suffix on the environment variable names.
            "@private-repo",  # Like the previous, but with a `_PRIVATE_REPO` suffix. Will use the `[private-repo]` section of the pypirc file, if any.
        ]
    )
    ```
    
    Configuration is done with `.pypirc` files and/or environment variables. The `keyring` tool may also work (untested).
    When using repositories by name, the regular Twine env vars, such as `TWINE_USERNAME`, `TWINE_REPOSITORY_URL` etc take the repository name as suffix (uppercased, and any `-` replaced by `_`. e.g. `TWINE_USERNAME_MY_REPO`), to support publishing to multiple repositories with a single invocation of Pants.
    
    
    Co-authored-by: Chris Targett <chris@xlevus.net>

---
+++ src/python/pants/backend/python/goals/publish.py
@@ -0,0 +1,196 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+import logging
+from dataclasses import dataclass
+
+from pants.backend.python.subsystems.twine import TwineSubsystem
+from pants.backend.python.target_types import PythonDistribution
+from pants.backend.python.util_rules.pex import PexRequest, VenvPex, VenvPexProcess
+from pants.core.goals.publish import (
+    PublishFieldSet,
+    PublishPackages,
+    PublishProcesses,
+    PublishRequest,
+)
+from pants.core.util_rules.config_files import ConfigFiles, ConfigFilesRequest
+from pants.engine.environment import Environment, EnvironmentRequest
+from pants.engine.fs import Digest, MergeDigests
+from pants.engine.process import InteractiveProcess, Process
+from pants.engine.rules import Get, MultiGet, collect_rules, rule
+from pants.engine.target import BoolField, StringSequenceField
+
+logger = logging.getLogger(__name__)
+
+
+class PyPiRepositories(StringSequenceField):
+    alias = "pypi_repositories"
+    help = "List of PyPi repositories to publish the target package to."
+
+    # Twine uploads to 'pypi' by default, but we don't set default to ["@pypi"] here to make it
+    # explicit in the BUILD file when a package is meant for public distribution.
+
+
+class SkipTwineUploadField(BoolField):
+    alias = "skip_twine"
+    default = False
+    help = "If true, don't publish this target's packages using Twine."
+
+
+class PublishToPyPiRequest(PublishRequest):
+    pass
+
+
+@dataclass(frozen=True)
+class PublishToPyPiFieldSet(PublishFieldSet):
+    publish_request_type = PublishToPyPiRequest
+    required_fields = (PyPiRepositories,)
+
+    repositories: PyPiRepositories
+    skip_twine: SkipTwineUploadField
+
+    # I'd rather opt out early here, so we don't build unnecessarily, however the error feedback is
+    # misleading and not very helpful in that case.
+    #
+    # @classmethod
+    # def opt_out(cls, tgt: Target) -> bool:
+    #     return not tgt[PyPiRepositories].value
+
+
+def twine_upload_args(
+    twine_subsystem: TwineSubsystem, config_files: ConfigFiles, repo: str, dists: tuple[str, ...]
+) -> tuple[str, ...]:
+    args = ["upload", "--non-interactive"]
+
+    if config_files.snapshot.files:
+        args.append(f"--config-file={config_files.snapshot.files[0]}")
+
+    args.extend(twine_subsystem.args)
+
+    if repo.startswith("@"):
+        # Named repository from the config file.
+        args.append(f"--repository={repo[1:]}")
+    else:
+        args.append(f"--repository-url={repo}")
+
+    args.extend(dists)
+    return tuple(args)
+
+
+def twine_env_suffix(repo: str) -> str:
+    return f"_{repo[1:]}".replace("-", "_").upper() if repo.startswith("@") else ""
+
+
+def twine_env_request(repo: str) -> EnvironmentRequest:
+    suffix = twine_env_suffix(repo)
+    req = EnvironmentRequest(
+        [
+            f"{var}{suffix}"
+            for var in [
+                "TWINE_USERNAME",
+                "TWINE_PASSWORD",
+                "TWINE_REPOSITORY_URL",
+                # "TWINE_CERT",  # Does the --cert arg to pex take care of this for us?
+            ]
+        ]
+    )
+    return req
+
+
+def twine_env(env: Environment, repo: str) -> Environment:
+    suffix = twine_env_suffix(repo)
+    if not suffix:
+        return env
+
+    return Environment({key.rsplit(suffix, maxsplit=1)[0]: value for key, value in env.items()})
+
+
+@rule
+async def twine_upload(
+    request: PublishToPyPiRequest, twine_subsystem: TwineSubsystem
+) -> PublishProcesses:
+    dists = tuple(
+        artifact.relpath
+        for pkg in request.packages
+        for artifact in pkg.artifacts
+        if artifact.relpath
+    )
+
+    if twine_subsystem.skip or not dists:
+        return PublishProcesses()
+
+    # Too verbose to provide feedback as to why some packages were skipped?
+    skip = None
+    if request.field_set.skip_twine.value:
+        skip = f"(by `{request.field_set.skip_twine.alias}` on {request.field_set.address})"
+    elif not request.field_set.repositories.value:
+        # I'd rather have used the opt_out mechanism on the field set, but that gives no hint as to
+        # why the target was not applicable..
+        skip = f"(no `{request.field_set.repositories.alias}` specified for {request.field_set.address})"
+
+    if skip:
+        return PublishProcesses(
+            [
+                PublishPackages(
+                    names=dists,
+                    description=skip,
+                ),
+            ]
+        )
+
+    twine_pex, packages_digest, config_files = await MultiGet(
+        Get(
+            VenvPex,
+            PexRequest(
+                output_filename="twine.pex",
+                internal_only=True,
+                requirements=twine_subsystem.pex_requirements(),
+                interpreter_constraints=twine_subsystem.interpreter_constraints,
+                main=twine_subsystem.main,
+            ),
+        ),
+        Get(Digest, MergeDigests(pkg.digest for pkg in request.packages)),
+        Get(ConfigFiles, ConfigFilesRequest, twine_subsystem.config_request()),
+    )
+
+    input_digest = await Get(Digest, MergeDigests((packages_digest, config_files.snapshot.digest)))
+    pex_proc_requests = []
+    twine_envs = await MultiGet(
+        Get(Environment, EnvironmentRequest, twine_env_request(repo))
+        for repo in request.field_set.repositories.value
+    )
+
+    for repo, env in zip(request.field_set.repositories.value, twine_envs):
+        pex_proc_requests.append(
+            VenvPexProcess(
+                twine_pex,
+                argv=twine_upload_args(twine_subsystem, config_files, repo, dists),
+                input_digest=input_digest,
+                extra_env=twine_env(env, repo),
+                description=repo,
+            )
+        )
+
+    processes = await MultiGet(
+        Get(Process, VenvPexProcess, request) for request in pex_proc_requests
+    )
+
+    return PublishProcesses(
+        PublishPackages(
+            names=dists,
+            process=InteractiveProcess.from_process(process),
+            description=process.description,
+        )
+        for process in processes
+    )
+
+
+def rules():
+    return (
+        *collect_rules(),
+        *PublishToPyPiFieldSet.rules(),
+        PythonDistribution.register_plugin_field(PyPiRepositories),
+        PythonDistribution.register_plugin_field(SkipTwineUploadField),
+    )
---
+++ src/python/pants/core/goals/publish.py
@@ -0,0 +1,272 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+"""Goal for publishing packaged targets to any repository or registry etc.
+
+Plugins implement the publish protocol that provides this goal with the processes to run in order to
+publish the artifacts.
+
+The publish protocol consists of defining two union members and one rule, returning the processes to
+run. See the doc for the corresponding classses in this module for details on the classes to define.
+
+Example rule:
+
+    @rule
+    async def publish_example(request: PublishToMyRepoRequest, ...) -> PublishProcesses:
+      # Create `InteractiveProcess` instances as required by the `request`.
+      return PublishProcesses(...)
+"""
+
+
+from __future__ import annotations
+
+import logging
+from abc import ABCMeta
+from dataclasses import dataclass
+from itertools import chain
+from typing import ClassVar, Generic, Iterable, Type, TypeVar
+
+from typing_extensions import final
+
+from pants.core.goals.package import BuiltPackage, PackageFieldSet
+from pants.engine.collection import Collection
+from pants.engine.console import Console
+from pants.engine.goal import Goal, GoalSubsystem
+from pants.engine.process import InteractiveProcess, InteractiveRunner
+from pants.engine.rules import Get, MultiGet, collect_rules, goal_rule, rule
+from pants.engine.target import (
+    FieldSet,
+    NoApplicableTargetsBehavior,
+    TargetRootsToFieldSets,
+    TargetRootsToFieldSetsRequest,
+)
+from pants.engine.unions import UnionRule, union
+
+logger = logging.getLogger(__name__)
+
+
+_F = TypeVar("_F", bound=FieldSet)
+
+
+@union
+@dataclass(frozen=True)
+class PublishRequest(Generic[_F]):
+    """Implement a union member subclass of this union class along with a PublishFieldSet subclass
+    that appoints that member subclass in order to receive publish requests for targets compatible
+    with the field set.
+
+    The `packages` hold all artifacts produced for a given target to be published.
+
+    Example:
+
+        PublishToMyRepoRequest(PublishRequest):
+          pass
+
+        PublishToMyRepoFieldSet(PublishFieldSet):
+          publish_request_type = PublishToMyRepoRequest
+
+          # Standard FieldSet semantics from here on:
+          required_fields = (MyRepositories,)
+          ...
+    """
+
+    field_set: _F
+    packages: tuple[BuiltPackage, ...]
+
+
+_T = TypeVar("_T", bound=PublishRequest)
+
+
+@union
+@dataclass(frozen=True)
+class PublishFieldSet(Generic[_T], FieldSet, metaclass=ABCMeta):
+    """FieldSet for PublishRequest.
+
+    Union members may list any fields required to fullfill the instantiation of the
+    `PublishProcesses` result of the publish rule.
+    """
+
+    # Subclasses must provide this, to a union member (subclass) of `PublishRequest`.
+    publish_request_type: ClassVar[Type[_T]]
+
+    @final
+    def _request(self, packages: tuple[BuiltPackage, ...]) -> _T:
+        """Internal helper for the core publish goal."""
+        return self.publish_request_type(field_set=self, packages=packages)
+
+    @final
+    @classmethod
+    def rules(cls) -> tuple[UnionRule, ...]:
+        """Helper method for registering the union members."""
+        return (
+            UnionRule(PublishFieldSet, cls),
+            UnionRule(PublishRequest, cls.publish_request_type),
+        )
+
+
+@dataclass(frozen=True)
+class PublishPackages:
+    """Processes to run in order to publish the named artifacts.
+
+    The `names` should list all artifacts being published by the `process` command.
+
+    The `process` may be `None`, indicating that it will not be published. This will be logged as
+    `skipped`. If the process returns a non zero exit code, it will be logged as `failed`.
+
+    The `description` may be a reason explaining why the publish was skipped, or identifying which
+    repository the artifacts are published to.
+    """
+
+    names: tuple[str, ...]
+    process: InteractiveProcess | None = None
+    description: str | None = None
+
+
+class PublishProcesses(Collection[PublishPackages]):
+    """Collection of what processes to run for all built packages.
+
+    This is returned from implementing rules in response to a PublishRequest.
+
+    Depending on the capabilities of the publishing tool, the work may be partitioned based on
+    number of artifacts and/or repositories to publish to.
+    """
+
+
+@dataclass(frozen=True)
+class PublishProcessesRequest:
+    """Internal request taking all field sets for a target and turning it into a `PublishProcesses`
+    collection (via registered publish plugins)."""
+
+    package_field_sets: tuple[PackageFieldSet, ...]
+    publish_field_sets: tuple[PublishFieldSet, ...]
+
+
+class PublishSubsystem(GoalSubsystem):
+    name = "publish"
+    help = "Publish deliverables (assets, distributions, images, etc)."
+
+    required_union_implementations = (
+        PackageFieldSet,
+        PublishFieldSet,
+    )
+
+
+class Publish(Goal):
+    subsystem_cls = PublishSubsystem
+
+
+@goal_rule
+async def run_publish(console: Console, interactive_runner: InteractiveRunner) -> Publish:
+    target_roots_to_package_field_sets, target_roots_to_publish_field_sets = await MultiGet(
+        Get(
+            TargetRootsToFieldSets,
+            TargetRootsToFieldSetsRequest(
+                field_set,
+                goal_description="the `publish` goal",
+                no_applicable_targets_behavior=NoApplicableTargetsBehavior.warn,
+            ),
+        )
+        for field_set in [PackageFieldSet, PublishFieldSet]
+    )
+
+    # Only keep field sets that both package someething, and have something to publish.
+    targets = set(target_roots_to_package_field_sets.targets).intersection(
+        set(target_roots_to_publish_field_sets.targets)
+    )
+
+    if not targets:
+        return Publish(exit_code=0)
+
+    # Build all packages and request the processes to run for each field set.
+    processes = await MultiGet(
+        Get(
+            PublishProcesses,
+            PublishProcessesRequest(
+                target_roots_to_package_field_sets.mapping[tgt],
+                target_roots_to_publish_field_sets.mapping[tgt],
+            ),
+        )
+        for tgt in targets
+    )
+
+    # Run all processes interactively.
+    exit_code, results = run_publish_processes(
+        console, interactive_runner, chain.from_iterable(processes)
+    )
+
+    console.print_stderr("")
+    if not results:
+        sigil = console.sigil_skipped()
+        console.print_stderr(f"{sigil} Nothing published.")
+
+    # We collect all results to the end, so all output from the interactive processes are done,
+    # before printing the results.
+    for line in results:
+        console.print_stderr(line)
+
+    return Publish(exit_code)
+
+
+def run_publish_processes(
+    console: Console,
+    interactive_runner: InteractiveRunner,
+    publish_processes: Iterable[PublishPackages],
+) -> tuple[int, list[str]]:
+    exit_code = 0
+    output = []
+    for pub in publish_processes:
+        if not pub.process:
+            sigil = console.sigil_skipped()
+            status = "skipped"
+            if pub.description:
+                status += f" {pub.description}"
+            for name in pub.names:
+                output.append(f"{sigil} {name} {status}.")
+            continue
+
+        res = interactive_runner.run(pub.process)
+        if res.exit_code == 0:
+            sigil = console.sigil_succeeded()
+            status = "published"
+            prep = "to"
+        else:
+            sigil = console.sigil_failed()
+            status = "failed"
+            prep = "for"
+            exit_code = res.exit_code
+
+        if pub.description:
+            status += f" {prep} {pub.description}"
+
+        for name in pub.names:
+            output.append(f"{sigil} {name} {status}.")
+    return exit_code, output
+
+
+@rule
+async def package_for_publish(request: PublishProcessesRequest) -> PublishProcesses:
+    packages = await MultiGet(
+        Get(BuiltPackage, PackageFieldSet, field_set) for field_set in request.package_field_sets
+    )
+
+    for pkg in packages:
+        for artifact in pkg.artifacts:
+            if artifact.relpath:
+                logger.info(f"Packaged {artifact.relpath}")
+            elif artifact.extra_log_lines:
+                logger.info(str(artifact.extra_log_lines[0]))
+
+    publish = await MultiGet(
+        Get(
+            PublishProcesses,
+            PublishRequest,
+            field_set._request(packages),
+        )
+        for field_set in request.publish_field_sets
+    )
+
+    # Merge all PublishProcesses into one.
+    return PublishProcesses(chain.from_iterable(publish))
+
+
+def rules():
+    return collect_rules()
