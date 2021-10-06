4093824b2bf135b208caee15195e162efec507c0
Christopher Neugebauer <chrisjrn@toolchain.com>
2021-10-06 09:20:49 -0700

    A more complex version of java example-app, demonstrating dependency inference
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- testprojects/src/java/org/pantsbuild/example/lib/ExampleLib.java
+++ testprojects/src/java/org/pantsbuild/example/lib/ExampleLib.java
@@ -1,7 +1,26 @@
 package org.pantsbuild.example.lib;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ExampleLib {
-    public static String hello() {
-        return "Hello, World!";
+
+    private String template = "{\"contents\": \"Hello, World!\"}";
+
+    public String getGreeting() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            SerializedThing thing = mapper.readValue(template, SerializedThing.class);
+            return thing.contents;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public static String hello() {
+        return new ExampleLib().getGreeting();
+    }
+}
+
+class SerializedThing {
+    public String contents;
 }
