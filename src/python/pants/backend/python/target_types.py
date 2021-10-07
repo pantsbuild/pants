6efc4e5a2bbbee7b25a7c22688cbd6e096a773e6
Eric Arellano <ericarellano@me.com>
2021-10-07 16:37:01 -0700

    Clarify `help` that a `python_requirement` target is meant to be for a single 3rdparty project

--- src/python/pants/backend/python/target_types.py
+++ src/python/pants/backend/python/target_types.py
@@ -735,8 +735,12 @@ class PythonRequirementsField(_RequirementSequenceField):
     alias = "requirements"
     required = True
     help = (
-        "A sequence of pip-style requirement strings, e.g. `['foo==1.8', "
-        "\"bar<=3 ; python_version<'3'\"]`."
+        "A pip-style requirement string, e.g. `[\"Django==3.2.8\"]`.\n\n"
+        "You can specify multiple requirements for the same project in order to use environment "
+        "markers, such as `[\"foo>=1.2,<1.3 ; python_version>'3.6'\", \"foo==0.9 ; "
+        "python_version<'3'\"]`.\n\n"
+        "If the requirement depends on some other requirement to work, such as needing "
+        "`setuptools` to be built, use the `dependencies` field instead."
     )
 
 
