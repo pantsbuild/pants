79f71d57c766830d6530316fe060a090758c044f
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2021-10-08 08:53:35 -0700

    [internal] Remove `go/custom_goals.py` (#13166)
    `go-build` will soon be replaced by a `check` implementation. 
    
    `go-resolve` should not exist because we decided in https://github.com/pantsbuild/pants/issues/13050 that third-party dependency management should be done independently of Pants for the first version of Go support.
    
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/go/register.py
+++ src/python/pants/backend/experimental/go/register.py
@@ -2,7 +2,7 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from pants.backend.go import target_type_rules
-from pants.backend.go.goals import custom_goals, package_binary, tailor
+from pants.backend.go.goals import package_binary, tailor
 from pants.backend.go.lint import fmt
 from pants.backend.go.lint.gofmt import skip_field as gofmt_skip_field
 from pants.backend.go.lint.gofmt.rules import rules as gofmt_rules
@@ -46,7 +46,6 @@ def rules():
         *tests_analysis.rules(),
         *tailor.rules(),
         *target_type_rules.rules(),
-        *custom_goals.rules(),
         *package_binary.rules(),
         # Gofmt
         *fmt.rules(),
