61f81029f80cac4cf97e44143f83c78e38c59421
Eric Arellano <ericarellano@me.com>
2021-10-07 23:02:40 -0700

    Add missing __future__
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/project_info/filter_targets.py
+++ src/python/pants/backend/project_info/filter_targets.py
@@ -1,6 +1,8 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import annotations
+
 import re
 from enum import Enum
 from typing import Callable, Pattern
