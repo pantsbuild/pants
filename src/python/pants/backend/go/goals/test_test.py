bebf251efb147ce3b1bdb0beaeb37334a99d4f6f
Tom Dyas <tdyas@toolchain.com>
2021-10-10 21:40:04 -0400

    more tests
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/go/goals/test_test.py
+++ src/python/pants/backend/go/goals/test_test.py
@@ -99,6 +99,7 @@ def test_internal_test_fails(rule_runner: RuleRunner) -> None:
     tgt = rule_runner.get_target(Address("foo", generated_name="./"))
     result = rule_runner.request(TestResult, [GoTestFieldSet.create(tgt)])
     assert result.exit_code == 1
+    assert "FAIL: TestAdd" in result.stdout
 
 
 def test_internal_benchmark_passes(rule_runner: RuleRunner) -> None:
@@ -222,3 +223,148 @@ def test_external_test_success(rule_runner: RuleRunner) -> None:
     result = rule_runner.request(TestResult, [GoTestFieldSet.create(tgt)])
     assert result.exit_code == 0
     assert result.stdout == "PASS\n"
+
+
+def test_external_test_fails(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(
+        {
+            "foo/BUILD": "go_mod()",
+            "foo/go.mod": "module foo",
+            "foo/add.go": textwrap.dedent(
+                """
+                package foo
+                func Add(x, y int) int {
+                  return x + y
+                }
+                """
+            ),
+            "foo/add_test.go": textwrap.dedent(
+                """
+                package foo_test
+                import (
+                  _ "foo"
+                  "testing"
+                )
+                func TestAdd(t *testing.T) {
+                  t.Fail()
+                }
+                """
+            ),
+        }
+    )
+    tgt = rule_runner.get_target(Address("foo", generated_name="./"))
+    result = rule_runner.request(TestResult, [GoTestFieldSet.create(tgt)])
+    assert result.exit_code == 1
+    assert "FAIL: TestAdd" in result.stdout
+
+
+def test_external_benchmark_passes(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(
+        {
+            "foo/BUILD": "go_mod()",
+            "foo/go.mod": "module foo",
+            "foo/fib.go": textwrap.dedent(
+                """
+                package foo
+                func Fib(n int) int {
+                  if n < 2 {
+                    return n
+                  }
+                  return Fib(n-1) + Fib(n-2)
+                }
+                """
+            ),
+            "foo/fib_test.go": textwrap.dedent(
+                """
+                package foo_test
+                import (
+                  "foo"
+                  "testing"
+                )
+                func BenchmarkAdd(b *testing.B) {
+                  for n := 0; n < b.N; n++ {
+                    foo.Fib(10)
+                  }
+                }
+                """
+            ),
+        }
+    )
+    tgt = rule_runner.get_target(Address("foo", generated_name="./"))
+    result = rule_runner.request(TestResult, [GoTestFieldSet.create(tgt)])
+    assert result.exit_code == 0
+    assert result.stdout == "PASS\n"
+
+
+def test_external_example_passes(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(
+        {
+            "foo/BUILD": "go_mod()",
+            "foo/go.mod": "module foo",
+            "foo/print.go": textwrap.dedent(
+                """
+                package foo
+                import "fmt"
+                func MyPrint(msg string) {
+                  fmt.Println(msg)
+                }
+                """
+            ),
+            "foo/print_test.go": textwrap.dedent(
+                """
+                package foo_test
+                import (
+                  "foo"
+                )
+                func ExamplePrint() {
+                  foo.MyPrint("foo")
+                  // Output: foo
+                }
+                """
+            ),
+        }
+    )
+    tgt = rule_runner.get_target(Address("foo", generated_name="./"))
+    result = rule_runner.request(TestResult, [GoTestFieldSet.create(tgt)])
+    assert result.exit_code == 0
+    assert result.stdout == "PASS\n"
+
+
+def test_external_test_with_test_main(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(
+        {
+            "foo/BUILD": "go_mod()",
+            "foo/go.mod": "module foo",
+            "foo/add.go": textwrap.dedent(
+                """
+                package foo
+                func Add(x, y int) int {
+                  return x + y
+                }
+                """
+            ),
+            "foo/add_test.go": textwrap.dedent(
+                """
+                package foo_test
+                import (
+                  "foo"
+                  "fmt"
+                  "testing"
+                )
+                func TestAdd(t *testing.T) {
+                  if foo.Add(2, 3) != 5 {
+                    t.Fail()
+                  }
+                }
+                func TestMain(m *testing.M) {
+                  fmt.Println("foo_test.TestMain called")
+                  m.Run()
+                }
+                """
+            ),
+        }
+    )
+    tgt = rule_runner.get_target(Address("foo", generated_name="./"))
+    result = rule_runner.request(TestResult, [GoTestFieldSet.create(tgt)])
+    assert result.exit_code == 0
+    assert "foo_test.TestMain called" in result.stdout
