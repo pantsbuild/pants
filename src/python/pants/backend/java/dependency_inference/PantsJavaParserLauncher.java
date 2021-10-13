74628a61e761bf7f438950aea5b9998b2a2b05ee
Tom Dyas <tdyas@toolchain.com>
2021-10-13 13:30:56 -0400

    parse out consumed unqualified types
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/java/dependency_inference/PantsJavaParserLauncher.java
+++ src/python/pants/backend/java/dependency_inference/PantsJavaParserLauncher.java
@@ -1,12 +1,21 @@
 package org.pantsbuild.javaparser;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.javaparser.ast.body.TypeDeclaration;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.expr.Name;
-import com.github.javaparser.ast.ImportDeclaration;
-import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.ImportDeclaration;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.PackageDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.Name;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.Type;
+import com.github.javaparser.ast.type.WildcardType;
+
 import java.io.File;
 import java.util.AbstractCollection;
 import java.util.ArrayList;
@@ -14,6 +23,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -34,19 +44,66 @@ class Import {
 }
 
 class CompilationUnitAnalysis {
-    CompilationUnitAnalysis(String declaredPackage, ArrayList<Import> imports, ArrayList<String> topLevelTypes) {
+    CompilationUnitAnalysis(
+        String declaredPackage,
+        ArrayList<Import> imports,
+        ArrayList<String> topLevelTypes,
+        ArrayList<String> consumedUnqualifiedTypes
+    ) {
         this.declaredPackage = declaredPackage;
         this.imports = imports;
         this.topLevelTypes = topLevelTypes;
+        this.consumedUnqualifiedTypes = consumedUnqualifiedTypes;
     }
 
     public final String declaredPackage;
     public final ArrayList<Import> imports;
     public final ArrayList<String> topLevelTypes;
+    public final ArrayList<String> consumedUnqualifiedTypes;
 }
 
 
 public class PantsJavaParserLauncher {
+    // Unwrap a `Type` and return the identifiers representing the "consumed" types.
+    private static List<String> unwrapIdentifiersForType(Type type) {
+        if (type.isArrayType()) {
+            return unwrapIdentifiersForType(type.asArrayType().getComponentType());
+        } else if (type.isWildcardType()) {
+            WildcardType wildcardType = type.asWildcardType();
+            ArrayList<String> result = new ArrayList<>();
+            if (wildcardType.getExtendedType().isPresent()) {
+                result.addAll(unwrapIdentifiersForType(wildcardType.getExtendedType().get()));
+            }
+            if (wildcardType.getSuperType().isPresent()) {
+                result.addAll(unwrapIdentifiersForType(wildcardType.getSuperType().get()));
+            }
+            return result;
+        } else if (type.isClassOrInterfaceType()) {
+            ArrayList<String> result = new ArrayList<>();
+            ClassOrInterfaceType classType = type.asClassOrInterfaceType();
+            Optional<NodeList<Type>> typeArguments = classType.getTypeArguments();
+            if (typeArguments.isPresent()) {
+                for (Type argumentType : typeArguments.get()) {
+                    result.addAll(unwrapIdentifiersForType(argumentType));
+                }
+            }
+            result.add(classType.getNameWithScope());
+            return result;
+        } else if (type.isIntersectionType()) {
+            ArrayList<String> result = new ArrayList<>();
+            for (Type elementType : type.asIntersectionType().getElements()) {
+                result.addAll(unwrapIdentifiersForType(elementType));
+            }
+            return result;
+        }
+
+        // Not handled:
+        // - PrimitiveType
+        // - VarType (Java `var` keyword to be inferred by the compiler.
+
+        return new ArrayList<>();
+    }
+
     public static void main(String[] args) throws Exception {
         String analysisOutputPath = args[0];
         String sourceToAnalyze = args[1];
@@ -76,7 +133,34 @@ public class PantsJavaParserLauncher {
                 .map(Optional::get)
                 .collect(Collectors.toList()));
 
-        CompilationUnitAnalysis analysis = new CompilationUnitAnalysis(declaredPackage, imports, topLevelTypes);
+        HashSet<Type> candidateConsumedTypes = new HashSet<>();
+        cu.walk(new Consumer<Node>() {
+            @Override
+            public void accept(Node node) {
+                if (node instanceof VariableDeclarator) {
+                    VariableDeclarator varDecl = (VariableDeclarator) node;
+                    candidateConsumedTypes.add(varDecl.getType());
+                }
+                if (node instanceof MethodDeclaration) {
+                    MethodDeclaration methodDecl = (MethodDeclaration) node;
+                    candidateConsumedTypes.add(methodDecl.getType());
+                    for (Parameter param : methodDecl.getParameters()) {
+                        candidateConsumedTypes.add(param.getType());
+                    }
+                    System.out.println("Method type: " + methodDecl.getType());
+                }
+            }
+        });
+
+        HashSet<String> identifiers = new HashSet<>();
+        for (Type type : candidateConsumedTypes) {
+            List<String> identifiersForType = unwrapIdentifiersForType(type);
+            identifiers.addAll(identifiersForType);
+        }
+
+        ArrayList<String> consumedUnqualifiedTypes = new ArrayList<>(identifiers);
+
+        CompilationUnitAnalysis analysis = new CompilationUnitAnalysis(declaredPackage, imports, topLevelTypes, consumedUnqualifiedTypes);
         ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(new File(analysisOutputPath), analysis);
     }
