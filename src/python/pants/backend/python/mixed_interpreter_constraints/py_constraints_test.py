0c3dfa4de0a689592ca966a47d3d5022c8bad831
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2021-10-13 09:51:28 -0700

    `files` and `resources` targets now generate `file` and `resource` targets (#13235)
    Now users can explicitly create a `file` and `resource` target. `files` and `resources` do that automatically.
    
    ### Impacts `./pants peek`
    
    The `target_type` now says `file` and `resources`. There's no way around that, afaict.
    
    ### Impacts `./pants filter --target-type`
    
    `--target-type=files` and `--target-type=resources` no longer matches the generated targets, which is breaking. I don't see how to fix this? For `python_source`, we could have it so that the deprecated name `python_library` matches both `python_sources` and `python_soruce`. But that isn't possible with `files` because there is no deprecated name, and it's wrong for `--target-type=files` to match `file`.
    
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/backend/python/mixed_interpreter_constraints/py_constraints_test.py
+++ src/python/pants/backend/python/mixed_interpreter_constraints/py_constraints_test.py
@@ -61,7 +61,7 @@ def run_goal(rule_runner: RuleRunner, args: list[str]) -> GoalRuleResult:
 
 
 def test_no_matches(rule_runner: RuleRunner, caplog) -> None:
-    rule_runner.write_files({"f.txt": "", "BUILD": "files(name='tgt', source='f.txt')"})
+    rule_runner.write_files({"f.txt": "", "BUILD": "file(name='tgt', source='f.txt')"})
     result = run_goal(rule_runner, ["f.txt"])
     assert result.exit_code == 0
     assert len(caplog.records) == 1
