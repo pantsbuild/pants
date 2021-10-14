cd140111ff3c7f60f58d088714760ea24f0bee43
Eric Arellano <ericarellano@me.com>
2021-10-13 19:41:30 -0700

    Fix missing annotations import
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/engine/internals/build_files.py
+++ src/python/pants/engine/internals/build_files.py
@@ -1,6 +1,8 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import annotations
+
 import os.path
 from dataclasses import dataclass
 from typing import Any, Dict
