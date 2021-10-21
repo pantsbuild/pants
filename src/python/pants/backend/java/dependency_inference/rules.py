17a6ae3e02522fc24431927e6612920b219bc869
Stu Hood <stuhood@gmail.com>
2021-10-21 09:03:02 -0700

    Add ambiguity warnings and resolution.
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/java/dependency_inference/rules.py
+++ src/python/pants/backend/java/dependency_inference/rules.py
@@ -1,19 +1,14 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import itertools
 import logging
 from typing import cast
 
 from pants.backend.java.dependency_inference import import_parser, java_parser, package_mapper
-from pants.backend.java.dependency_inference.import_parser import (
-    ParsedJavaImports,
-    ParseJavaImportsRequest,
-)
 from pants.backend.java.dependency_inference.package_mapper import FirstPartyJavaPackageMapping
 from pants.backend.java.dependency_inference.types import JavaSourceDependencyAnalysis
 from pants.backend.java.target_types import JavaSourceField
-from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
+from pants.core.util_rules.source_files import SourceFilesRequest
 from pants.core.util_rules.source_files import rules as source_files_rules
 from pants.engine.addresses import Address
 from pants.engine.rules import Get, MultiGet, collect_rules, rule
@@ -27,6 +22,7 @@ from pants.engine.target import (
 )
 from pants.engine.unions import UnionRule
 from pants.option.subsystem import Subsystem
+from pants.util.ordered_set import OrderedSet
 
 logger = logging.getLogger(__name__)
 
@@ -42,73 +38,71 @@ class JavaInferSubsystem(Subsystem):
             "--imports",
             default=True,
             type=bool,
-            help=(
-                "Infer a target's imported dependencies by parsing import statements from sources."
-            ),
+            help=("Infer a target's dependencies by parsing import statements from sources."),
+        )
+        register(
+            "--consumed-types",
+            default=True,
+            type=bool,
+            help=("Infer a target's dependencies by parsing consumed types from sources."),
         )
 
     @property
     def imports(self) -> bool:
         return cast(bool, self.options.imports)
 
+    @property
+    def consumed_types(self) -> bool:
+        return cast(bool, self.options.consumed_types)
 
-class InferJavaImportDependencies(InferDependenciesRequest):
+
+class InferJavaSourceDependencies(InferDependenciesRequest):
     infer_from = JavaSourceField
 
 
 @rule(desc="Inferring Java dependencies by analyzing imports")
 async def infer_java_dependencies_via_imports(
-    request: InferJavaImportDependencies,
+    request: InferJavaSourceDependencies,
     java_infer_subsystem: JavaInferSubsystem,
     first_party_dep_map: FirstPartyJavaPackageMapping,
 ) -> InferredDependencies:
-    if not java_infer_subsystem.imports:
+    if not java_infer_subsystem.imports and not java_infer_subsystem.consumed_types:
         return InferredDependencies([])
 
-    wrapped_tgt = await Get(WrappedTarget, Address, request.sources_field.address)
-    explicitly_provided_deps, detected_imports = await MultiGet(
+    address = request.sources_field.address
+    wrapped_tgt = await Get(WrappedTarget, Address, address)
+    explicitly_provided_deps, analysis = await MultiGet(
         Get(ExplicitlyProvidedDependencies, DependenciesRequest(wrapped_tgt.target[Dependencies])),
-        Get(
-            ParsedJavaImports,
-            ParseJavaImportsRequest(
-                request.sources_field,
-            ),
-        ),
+        Get(JavaSourceDependencyAnalysis, SourceFilesRequest([request.sources_field])),
     )
-    relevant_imports = detected_imports  # TODO: Remove stdlib
+
+    types: OrderedSet[str] = OrderedSet()
+    if java_infer_subsystem.imports:
+        types.update(imp.name for imp in analysis.imports)
+    if java_infer_subsystem.consumed_types:
+        package = analysis.declared_package
+        types.update(
+            f"{package}.{consumed_type}" for consumed_type in analysis.consumed_unqualified_types
+        )
 
     dep_map = first_party_dep_map.package_rooted_dependency_map
 
-    candidate_symbols = list(relevant_imports)
-    return InferredDependencies(
-        dependencies=itertools.chain.from_iterable(
-            dep_map.addresses_for_type(imp) for imp in candidate_symbols
-        ),
-    )
+    dependencies: OrderedSet[Address] = OrderedSet()
+    for typ in types:
+        matches = dep_map.addresses_for_type(typ)
+        if not matches:
+            continue
+        explicitly_provided_deps.maybe_warn_of_ambiguous_dependency_inference(
+            matches,
+            address,
+            import_reference="type",
+            context=f"The target {address} imports `{typ}`",
+        )
+        maybe_disambiguated = explicitly_provided_deps.disambiguated(matches)
+        if maybe_disambiguated:
+            dependencies.add(maybe_disambiguated)
 
-
-class InferJavaConsumedTypesDependencies(InferDependenciesRequest):
-    infer_from = JavaSourceField
-
-
-@rule(desc="Inferring Java dependencies by analyzing consumed and top-level types")
-async def infer_java_dependencies_via_consumed_types(
-    request: InferJavaConsumedTypesDependencies,
-    first_party_dep_map: FirstPartyJavaPackageMapping,
-) -> InferredDependencies:
-    source_files = await Get(SourceFiles, SourceFilesRequest([request.sources_field]))
-    analysis = await Get(JavaSourceDependencyAnalysis, SourceFiles, source_files)
-
-    package = analysis.declared_package
-    dep_map = first_party_dep_map.package_rooted_dependency_map
-    candidate_consumed_types = [
-        f"{package}.{consumed_type}" for consumed_type in analysis.consumed_unqualified_types
-    ]
-    return InferredDependencies(
-        dependencies=itertools.chain.from_iterable(
-            dep_map.addresses_for_type(imp) for imp in candidate_consumed_types
-        ),
-    )
+    return InferredDependencies(dependencies)
 
 
 def rules():
@@ -118,6 +112,5 @@ def rules():
         *import_parser.rules(),
         *package_mapper.rules(),
         *source_files_rules(),
-        UnionRule(InferDependenciesRequest, InferJavaImportDependencies),
-        UnionRule(InferDependenciesRequest, InferJavaConsumedTypesDependencies),
+        UnionRule(InferDependenciesRequest, InferJavaSourceDependencies),
     ]
