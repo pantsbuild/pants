507a355bbee6c06151a197547eb1a64f39d5a9ea
Christopher Neugebauer <chrisjrn@toolchain.com>
2021-10-26 13:51:28 -0700

    SIGH
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/jvm/goals/coursier_integration_test.py
+++ src/python/pants/jvm/goals/coursier_integration_test.py
@@ -27,8 +27,7 @@ from pants.jvm.resolve.coursier_setup import rules as coursier_setup_rules
 from pants.jvm.target_types import JvmArtifact, JvmDependencyLockfile
 from pants.jvm.testutil import maybe_skip_jdk_test
 from pants.jvm.util_rules import rules as util_rules
-from pants.testutil.rule_runner import PYTHON_BOOTSTRAP_ENV, RuleRunner
-from pants.testutil.rule_runner import logging
+from pants.testutil.rule_runner import PYTHON_BOOTSTRAP_ENV, RuleRunner, logging
 
 HAMCREST_COORD = Coordinate(
     group="org.hamcrest",
