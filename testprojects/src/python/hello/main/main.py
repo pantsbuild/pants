8fcad591eca7d6ee5fa2296cee9f61891cd22248
Benjy Weinberger <benjyw@gmail.com>
2021-11-01 08:13:08 -0700

    Fix tests
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- testprojects/src/python/hello/main/main.py
+++ testprojects/src/python/hello/main/main.py
@@ -1,6 +1,7 @@
 # Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import os
 import sys
 
 from hello.greet.greet import greet
@@ -9,3 +10,4 @@ if __name__ == "__main__":
     greetees = sys.argv[1:] or ["world"]
     for greetee in greetees:
         print(greet(greetee))
+    print(f"XXXXXX {os.getcwd()}")
