31c59b24787b91031167d5b24cc333f7a66c943c
Benjy Weinberger <benjyw@gmail.com>
2021-11-02 22:46:41 -0400

    An export goal for exporting data to be read by IDEs. (#13415)
    This change provides:
    
    A generic extensible mechanism for backends to add exportable data.
    
    An implementation in the python backend that exports a venv with
    all the target set's 3rdparty deps.
    
    Follow-up changes can do things like export codegenned sources, or export
    source roots into formats that PyCharm and VSCode can read.
    
    [ci skip-rust]
    
    [ci skip-build-wheels]

---
+++ src/python/pants/backend/python/goals/export.py
@@ -0,0 +1,83 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+import os
+from dataclasses import dataclass
+
+from pants.backend.python.subsystems.setup import PythonSetup
+from pants.backend.python.util_rules.interpreter_constraints import InterpreterConstraints
+from pants.backend.python.util_rules.pex import VenvPex, VenvPexProcess
+from pants.backend.python.util_rules.pex_environment import PexEnvironment
+from pants.backend.python.util_rules.pex_from_targets import PexFromTargetsRequest
+from pants.core.goals.export import ExportableData, ExportableDataRequest, ExportError, Symlink
+from pants.engine.internals.selectors import Get
+from pants.engine.process import ProcessResult
+from pants.engine.rules import collect_rules, rule
+from pants.engine.unions import UnionRule
+
+
+@dataclass(frozen=True)
+class ExportedVenvRequest(ExportableDataRequest):
+    pass
+
+
+@rule
+async def export_venv(
+    request: ExportedVenvRequest, python_setup: PythonSetup, pex_env: PexEnvironment
+) -> ExportableData:
+    # Pick a single interpreter for the venv.
+    interpreter_constraints = InterpreterConstraints.create_from_targets(
+        request.targets, python_setup
+    )
+    if not interpreter_constraints:
+        # If there were no targets that defined any constraints, fall back to the global ones.
+        interpreter_constraints = InterpreterConstraints(python_setup.interpreter_constraints)
+    min_interpreter = interpreter_constraints.snap_to_minimum(python_setup.interpreter_universe)
+    if not min_interpreter:
+        raise ExportError(
+            "The following interpreter constraints were computed for all the targets for which "
+            f"export was requested: {interpreter_constraints}. There is no python interpreter "
+            "compatible with these constraints. Please restrict the target set to one that shares "
+            "a compatible interpreter."
+        )
+
+    venv_pex = await Get(
+        VenvPex,
+        PexFromTargetsRequest,
+        PexFromTargetsRequest.for_requirements(
+            (tgt.address for tgt in request.targets),
+            internal_only=True,
+            hardcoded_interpreter_constraints=min_interpreter,
+        ),
+    )
+
+    complete_pex_env = pex_env.in_workspace()
+    venv_abspath = os.path.join(complete_pex_env.pex_root, venv_pex.venv_rel_dir)
+
+    # Run the venv_pex to get the full python version (including patch #), so we
+    # can use it in the symlink name.
+    res = await Get(
+        ProcessResult,
+        VenvPexProcess(
+            venv_pex=venv_pex,
+            description="Create virtualenv",
+            argv=["-c", "import sys; print('.'.join(str(x) for x in sys.version_info[0:3]))"],
+            input_digest=venv_pex.digest,
+        ),
+    )
+    py_version = res.stdout.strip().decode()
+
+    return ExportableData(
+        f"virtualenv for {min_interpreter}",
+        os.path.join("python", "virtualenv"),
+        symlinks=[Symlink(venv_abspath, py_version)],
+    )
+
+
+def rules():
+    return [
+        *collect_rules(),
+        UnionRule(ExportableDataRequest, ExportedVenvRequest),
+    ]
---
+++ src/python/pants/core/goals/export_test.py
@@ -0,0 +1,114 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+import os
+from pathlib import Path
+from typing import List, Tuple
+
+from pants.base.build_root import BuildRoot
+from pants.core.goals.export import (
+    Export,
+    ExportableData,
+    ExportableDataRequest,
+    ExportSubsystem,
+    Symlink,
+    export,
+)
+from pants.core.util_rules.distdir import DistDir
+from pants.engine.addresses import Address
+from pants.engine.fs import AddPrefix, CreateDigest, Digest, FileContent, MergeDigests, Workspace
+from pants.engine.rules import QueryRule
+from pants.engine.target import Target, Targets
+from pants.engine.unions import UnionMembership, UnionRule
+from pants.testutil.option_util import create_goal_subsystem, create_options_bootstrapper
+from pants.testutil.rule_runner import MockGet, RuleRunner, mock_console, run_rule_with_mocks
+
+
+class MockTarget(Target):
+    alias = "target"
+    core_fields = ()
+
+
+def make_target(path: str, target_name: str) -> Target:
+    return MockTarget({}, Address(path, target_name=target_name))
+
+
+class MockExportableDataRequest(ExportableDataRequest):
+    pass
+
+
+def mock_export(
+    edr: ExportableDataRequest, digest: Digest, symlinks: tuple[Symlink, ...]
+) -> ExportableData:
+    return ExportableData(
+        description=f"mock export for {','.join(t.address.spec for t in edr.targets)}",
+        reldir="mock",
+        digest=digest,
+        symlinks=symlinks,
+    )
+
+
+def run_export_rule(rule_runner: RuleRunner, targets: List[Target]) -> Tuple[int, str]:
+    union_membership = UnionMembership({ExportableDataRequest: [MockExportableDataRequest]})
+    with open(os.path.join(rule_runner.build_root, "somefile"), "wb") as fp:
+        fp.write(b"SOMEFILE")
+    with mock_console(create_options_bootstrapper()) as (console, stdio_reader):
+        digest = rule_runner.request(Digest, [CreateDigest([FileContent("foo/bar", b"BAR")])])
+        result: Export = run_rule_with_mocks(
+            export,
+            rule_args=[
+                console,
+                Targets(targets),
+                create_goal_subsystem(ExportSubsystem),
+                Workspace(rule_runner.scheduler, _enforce_effects=False),
+                union_membership,
+                BuildRoot(),
+                DistDir(relpath=Path("dist")),
+            ],
+            mock_gets=[
+                MockGet(
+                    output_type=ExportableData,
+                    input_type=ExportableDataRequest,
+                    mock=lambda edr: mock_export(
+                        edr, digest, (Symlink("somefile", "link_to_somefile"),)
+                    ),
+                ),
+                MockGet(
+                    output_type=Digest,
+                    input_type=MergeDigests,
+                    mock=lambda md: rule_runner.request(Digest, [md]),
+                ),
+                MockGet(
+                    output_type=Digest,
+                    input_type=AddPrefix,
+                    mock=lambda ap: rule_runner.request(Digest, [ap]),
+                ),
+            ],
+            union_membership=union_membership,
+        )
+        return result.exit_code, stdio_reader.get_stdout()
+
+
+def test_run_export_rule() -> None:
+    rule_runner = RuleRunner(
+        rules=[
+            UnionRule(ExportableDataRequest, MockExportableDataRequest),
+            QueryRule(Digest, [CreateDigest]),
+        ],
+        target_types=[MockTarget],
+    )
+    exit_code, stdout = run_export_rule(rule_runner, [make_target("foo/bar", "baz")])
+    assert exit_code == 0
+    assert "Wrote mock export for foo/bar:baz to dist/export/mock" in stdout
+    expected_dist_path = os.path.join(rule_runner.build_root, "dist/export/mock/foo/bar")
+    assert os.path.isfile(expected_dist_path)
+    with open(expected_dist_path, "rb") as fp:
+        assert fp.read() == b"BAR"
+
+    symlink = "dist/export/mock/link_to_somefile"
+    assert os.path.islink(symlink)
+    assert os.readlink(symlink) == os.path.join(rule_runner.build_root, "somefile")
+    with open(symlink, "rb") as fp:
+        assert fp.read() == b"SOMEFILE"
