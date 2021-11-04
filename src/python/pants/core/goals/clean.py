f9309afb84fec64d2732d6e78c18078864c28194
Joshua Cannon <joshdcannon@gmail.com>
2021-11-04 10:31:46 -0500

    end the first pass

--- src/python/pants/core/goals/clean.py
+++ src/python/pants/core/goals/clean.py
@@ -38,10 +38,51 @@ from pants.option.global_options import (
 )
 from pants.init.logging import pants_log_path
 
+# N.B. We use a list so that named-caches can go last, as it is quite large and for dry-run
+# it's a nicer UX if it's last.
+_NAMED_CACHE_FLAGS_INFO = [
+    (
+        "process-execution-temp-dirs",
+        "Whether to keep the process execution temporary directories. These are normally cleaned up"
+        " automatically, however they are leaked if --no-process-execution-local-cleanup.",
+    ),
+    (
+        "lmdb-store",
+        "Whether to keep the local file store, which stores the results of subprocesses run by Pants.",
+    ),
+    (
+        "run-tracker",
+        "Whether to clean directories persisted after using `./pants run`.",
+    ),
+    (
+        "repl-temp-dirs",
+        "Whether to clean directories persisted after using `./pants repl`.",
+    ),
+    (
+        "dist-dir",
+        "Whether to clean the distributable files directory. "
+    ),
+    (
+        "bootstrap-dir",
+        "Whether to clean directories used by pants for bootstrapping itself."
+    ),
+    (
+        "named-caches",
+        "Whether to clean the named global caches."
+    ),
+]
+
+def _dir_info(*paths: Path):
+    total_size = num_files = 0
+    for path in paths:
+        for dirpath, _, filenames in os.walk(path):
+            total_size += sum(os.path.getsize(os.path.join(dirpath, f)) for f in filenames)
+            num_files += len(filenames)
+    return total_size, num_files
+
 class CleanSubsystem(GoalSubsystem):
     name = "clean"
-    # @TODO: Note about how it can only clean disk based on current values
-    help = "Clean pants caches and temp dirs"
+    help = "Clean pants caches and temp dirs (based on current settings)"
 
     @classmethod
     def register_options(cls, register) -> None:
@@ -52,153 +93,90 @@ class CleanSubsystem(GoalSubsystem):
             type=bool,
             default=True,
             help=(
-                "Whether to just report what would be cleaned. A `False` value will actually clean"
-                " the files on disk. Further control of what gets cleaned is provided through the"
-                " advanced options, and should be conbined with `--no-dry-run`."
+                "Whether to just report what would be cleaned. @TODO: more"
+            ),
+        )
+        register(
+            "--all",
+            advanced=False,
+            type=bool,
+            default=False,  # @TODO: True?
+            help=(
+                "Clear all caches. Can be combined with the advanced options for further control."
             ),
         )
 
-        # Add "all" option
-
-        # @TODO: Fill these out
         # @TODO: Is there a way to collect these programmatically?
-        for named_path, help in (
-            # Leaked when --no-process-execution-local-cleanup is used
-            ("process-execution-temp-dirs", "@TODO"),
-            # @TODO: Big cache
-            ("lmdb-store", "@TODO"),
-            # @TODO: Big cache
-            ("named-caches", "@TODO"),
-            # From ./pants run
-            ("run-tracker", "@TODO"),
-            # From ./pants repl
-            ("repl-temp-dirs", "@TODO"),
-            ("dist-dir", "@TODO"),
-            ("bootstrap-dir", "@TODO"),
-            # @TODO: pants-subprocessdir?
-        ):
+        for flag_slug, help in _NAMED_CACHE_FLAGS_INFO:
             register(
-                f"--keep-{named_path}",
+                f"--keep-{flag_slug}",
                 advanced=True,
                 type=bool,
-                default=False,
+                default=True,
                 help=help,
             )
-
 
 class Clean(Goal):
     subsystem_cls = CleanSubsystem
 
-def _dir_info(*paths: Path):
-    total_size = num_files = 0
-    for path in paths:
-        for dirpath, _, filenames in os.walk(path):
-            total_size += sum(os.path.getsize(os.path.join(dirpath, f)) for f in filenames)
-            num_files += len(filenames)
-    return total_size, num_files
-
-def _print_dry_run_info(
-    console: Console,
-    flag: str,
-    human_path: str,
-    paths: List[Path],
-):
-    # @TODO: handle empty (0 bytes or 0 files)?
-    console.print_stdout(f"Would clean {console.green(human_path)}:")
-    total_size, num_files = _dir_info(*paths)
-    console.print_stdout(f"  Controlled by {console.blue(f'--[no-]{flag}')}")
-    console.print_stdout(f"  {len(paths)} dirs, spanning {humanize.intword(num_files)} files totalling {humanize.naturalsize(total_size)}")
-
-def _maybe_clean(
-    console: Console,
-    dry_run: bool,
-    flag: str,
-    path: Union[str, Path],
-    glob: str = "*",
-):
-    path = Path(path)
-
-    if dry_run:
-        _print_dry_run_info(console, flag, path / glob, list(path.glob(glob)))
-    else:
-        pass  # @TODO: Cleaning!
-    # (safe_delete, safe_delete) or rm_rf
 
 @goal_rule
 async def clean(
     clean_subsystem: CleanSubsystem,
     console: Console,
-    workspace: Workspace,
-    union_membership: UnionMembership,
     real_opts: _Options,
     dist_dir: DistDir,
-    # specs: Specs,
 ) -> Clean:
+    def _maybe_clean(
+        keep_flag_slug: str,
+        keep_flag_value: bool,
+        path: Union[str, Path],
+        glob: str = "*",
+    ):
+        path = Path(path)
+        paths = list(path.glob(glob))
+        should_clean = (
+            (clean_subsystem.options.all and not keep_flag_value)
+            or (clean_subsystem.options.all and not not keep_flag_value)
+        )
+
+        if clean_subsystem.options.dry_run:
+            # @TODO: handle empty (0 bytes or 0 files) specially?
+            total_size, num_files = _dir_info(*paths)
+            maybe_negative = "" if should_clean else console.red("not ")
+
+            console.print_stdout(f"Would {maybe_negative}clean {console.green(path / glob)}:")
+            # @TODO: Print the relevant flag that gave us this directory?
+            console.print_stdout(f"  Controlled by {console.blue(f'--[no-]keep-{keep_flag_slug}')}")
+            console.print_stdout(f"  {len(paths)} dirs, spanning {humanize.intword(num_files)} files totalling {humanize.naturalsize(total_size)}")
+        elif should_clean:
+            # @TODO: Should we multi-process this?
+            console.print_stdout(f"Cleaning {console.green(path / glob)}")
+            for path in paths:
+                rm_rf(str(path))
+
 
     global_options = real_opts.options.for_global_scope()
     bootstrap_options = real_opts.options.bootstrap_option_values()
-    pants_cache_dir = get_pants_cachedir()  # Needed for setup
+    pants_cache_dir = get_pants_cachedir()
 
-    if not clean_subsystem.options.keep_process_execution_temp_dirs:
+    for flag_slug, _ in _NAMED_CACHE_FLAGS_INFO:
         _maybe_clean(
-            console,
-            clean_subsystem.options.dry_run,
-            "keep-process-execution-temp-dirs",
-            Path(tempfile.gettempdir()),
-            "pants-pe*",
+            flag_slug,
+            getattr(clean_subsystem.options, f"keep_{flag_slug.replace('-', '_')}"),
+            *{
+                "process-execution-temp-dirs": (Path(tempfile.gettempdir()), "pants-pe*"),
+                "lmdb-store": (LocalStoreOptions.from_options(global_options).store_dir,),
+                "run-tracker": (Path(global_options.pants_workdir) / "run-tracker",),
+                "repl-temp-dirs": (Path(global_options.pants_workdir), "repl*"),
+                "dist-dir": (dist_dir.relpath,),
+                "bootstrap-dir": (Path(pants_cache_dir) / "setup",),
+                "named-caches": (bootstrap_options.named_caches_dir,),
+            }[flag_slug]
         )
 
-    if not clean_subsystem.options.keep_lmdb_store:
-        _maybe_clean(
-            console,
-            clean_subsystem.options.dry_run,
-            "keep-lmdb-store",
-            LocalStoreOptions.from_options(global_options).store_dir,
-        )
-
-    if not clean_subsystem.options.keep_run_tracker:
-        _maybe_clean(
-            console,
-            clean_subsystem.options.dry_run,
-            "keep-run-tracker",
-            Path(global_options.pants_workdir) / "run-tracker",
-        )
-
-    if not clean_subsystem.options.keep_repl_temp_dirs:
-        _maybe_clean(
-            console,
-            clean_subsystem.options.dry_run,
-            "keep-repl-temp-dirs",
-            Path(global_options.pants_workdir),
-            "repl*",
-        )
-
-    if not clean_subsystem.options.keep_dist_dir:
-        _maybe_clean(
-            console,
-            clean_subsystem.options.dry_run,
-            "keep-dist-dir",
-            dist_dir.relpath,
-        )
-
-    if not clean_subsystem.options.keep_bootstrap_dir:
-        _maybe_clean(
-            console,
-            clean_subsystem.options.dry_run,
-            "keep-bootstrap-dir",
-            Path(pants_cache_dir) / "setup",
-        )
-
-    # N.B. This one is last because it can get quite large
-    if not clean_subsystem.options.keep_named_caches:
-        _maybe_clean(
-            console,
-            clean_subsystem.options.dry_run,
-            "keep-named-caches",
-            bootstrap_options.named_caches_dir,
-        )
-
-    console.print_stdout(f"Specify {console.blue('--no-dry-run')} to actually clean the disk")
+    if clean_subsystem.options.dry_run:
+        console.print_stdout(f"Specify {console.blue('--no-dry-run')} to actually do the cleaning")
 
     return Clean(exit_code=0)
 
