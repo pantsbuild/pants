0b35e45604298ff0c2a71521875a99ae4c9161a0
Stu Hood <stuhood@gmail.com>
2021-11-05 12:45:27 -0700

    Add scala to the JVM smoketest directory, and rename.
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- testprojects/src/java/org/pantsbuild/example/app/ExampleApp.java
+++
@@ -1,9 +0,0 @@
-package org.pantsbuild.example.app;
-
-import org.pantsbuild.example.lib.ExampleLib;
-
-public class ExampleApp {
-    public static void main(String[] args) {
-        System.out.println(ExampleLib.hello());
-    }
-}
