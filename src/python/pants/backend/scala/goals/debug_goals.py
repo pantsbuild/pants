0923889a617fac359232b2f3d4caadd4c8dcb78d
Tom Dyas <tdyas@toolchain.com>
2021-11-09 11:48:18 -0500

    add file header
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/scala/goals/debug_goals.py
+++ src/python/pants/backend/scala/goals/debug_goals.py
@@ -1,3 +1,6 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 import json
 
 from pants.backend.experimental.scala.register import rules as scala_rules
