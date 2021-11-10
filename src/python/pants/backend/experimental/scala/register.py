2e36207ebcf3fe5590e9e6343ab8399ee3c372cd
Tom Dyas <tdyas@toolchain.com>
2021-11-10 13:57:23 -0500

    scala: repl support
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/scala/register.py
+++ src/python/pants/backend/experimental/scala/register.py
@@ -10,13 +10,14 @@ from pants.backend.java.target_types import (  # TODO: All of these should move 
 from pants.backend.java.test import junit  # TODO: Should move to the JVM package.
 from pants.backend.scala.compile import scalac
 from pants.backend.scala.dependency_inference import rules as dep_inf_rules
-from pants.backend.scala.goals import check, tailor
 from pants.backend.scala.target_types import (
     ScalaJunitTestsGeneratorTarget,
     ScalaJunitTestTarget,
     ScalaSourcesGeneratorTarget,
     ScalaSourceTarget,
 )
+from pants.backend.scala.goals import check, repl, tailor
+from pants.backend.scala.target_types import ScalaSourcesGeneratorTarget, ScalaSourceTarget
 from pants.backend.scala.target_types import rules as target_types_rules
 from pants.jvm import classpath, jdk_rules
 from pants.jvm import util_rules as jvm_util_rules
@@ -44,6 +45,7 @@ def rules():
         *scalac.rules(),
         *check.rules(),
         *tailor.rules(),
+        *repl.rules(),
         *classpath.rules(),
         *junit.rules(),
         *deploy_jar.rules(),
---
+++ testprojects/src/scala/Adder.scala
@@ -0,0 +1,6 @@
+package org.pantsbuild.scala
+
+object Adder {
+  def add(x: Integer, y: Integer): Integer = x + y
+}
+
