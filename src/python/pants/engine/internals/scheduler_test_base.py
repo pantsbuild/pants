b5f4bbaef511da24aefa754ba415826479d8ee6a
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-11-18 16:44:13 -0800

    fix mkdir

--- src/python/pants/engine/internals/scheduler_test_base.py
+++ src/python/pants/engine/internals/scheduler_test_base.py
@@ -29,7 +29,7 @@ class SchedulerTestBase:
         """Creates a SchedulerSession for a Scheduler with the given Rules installed."""
 
         build_root = tmp_path / "build_root"
-        build_root.mkdir(parents=True)
+        build_root.mkdir(parents=True, exist_ok=True)
 
         local_execution_root_dir = os.path.realpath(safe_mkdtemp())
         named_caches_dir = os.path.realpath(safe_mkdtemp())
