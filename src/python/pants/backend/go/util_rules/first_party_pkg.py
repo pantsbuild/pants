7be685f322ccb7961fde0136c0701277f0e72b24
Eric Arellano <ericarellano@me.com>
2021-11-23 10:45:21 -0700

    Fix first_party_pkg.py
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/go/util_rules/first_party_pkg.py
+++ src/python/pants/backend/go/util_rules/first_party_pkg.py
@@ -104,12 +104,12 @@ class FirstPartyPkgInfoRequest(EngineAwareParameter):
 async def compute_first_party_package_import_path(
     request: FirstPartyPkgImportPathRequest,
 ) -> FirstPartyPkgImportPath:
-    go_mod_address = await Get(OwningGoMod, OwningGoModRequest(request.address))
+    owning_go_mod = await Get(OwningGoMod, OwningGoModRequest(request.address))
 
     # The generated_name will have been set to `./{subpath}`.
     subpath = request.address.generated_name[2:]  # type: ignore[index]
 
-    go_mod_info = await Get(GoModInfo, GoModInfoRequest(go_mod_address))
+    go_mod_info = await Get(GoModInfo, GoModInfoRequest(owning_go_mod.address))
     import_path = f"{go_mod_info.import_path}/{subpath}" if subpath else go_mod_info.import_path
     return FirstPartyPkgImportPath(import_path, subpath)
 
@@ -124,11 +124,11 @@ class PackageAnalyzerSetup:
 async def compute_first_party_package_info(
     request: FirstPartyPkgInfoRequest, analyzer: PackageAnalyzerSetup
 ) -> FallibleFirstPartyPkgInfo:
-    go_mod_address = await Get(OwningGoMod, OwningGoModRequest(request.address))
+    owning_go_mod = await Get(OwningGoMod, OwningGoModRequest(request.address))
     wrapped_target, import_path_info, go_mod_info = await MultiGet(
         Get(WrappedTarget, Address, request.address),
         Get(FirstPartyPkgImportPath, FirstPartyPkgImportPathRequest(request.address)),
-        Get(GoModInfo, GoModInfoRequest(go_mod_address)),
+        Get(GoModInfo, GoModInfoRequest(owning_go_mod.address)),
     )
 
     pkg_sources = await Get(
