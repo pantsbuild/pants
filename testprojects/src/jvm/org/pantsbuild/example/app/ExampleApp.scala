a27e44e55a29e641da9d66393ad52ebb8ab9e184
Christopher Neugebauer <chrisjrn@toolchain.com>
2021-11-23 13:06:13 -0800

    [DO NOT MERGE] demo of `provides` on testproject
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- testprojects/src/jvm/org/pantsbuild/example/app/ExampleApp.scala
+++ testprojects/src/jvm/org/pantsbuild/example/app/ExampleApp.scala
@@ -1,9 +1,11 @@
 package org.pantsbuild.example.app;
 
 import org.pantsbuild.example.lib.ExampleLib
+import com.google.common.truth.BadAndDangerous
 
 class ExampleApp {
     def main(args: Array[String]): Unit = {
-        println(ExampleLib.hello())
+        println(BadAndDangerous.hello())
+        com.google.common.truth.Truth.assertThat(new Object())
     }
 }
---
+++ testprojects/src/jvm/org/pantsbuild/example/lib/BadAndDangerous.java
@@ -0,0 +1,7 @@
+package com.google.common.truth;
+
+public class BadAndDangerous {
+    public static String hello() {
+        return "Hello, World!";
+    }
+}
