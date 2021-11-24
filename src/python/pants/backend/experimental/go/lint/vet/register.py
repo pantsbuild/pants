07c02b91ab07fa068e74a07840110dcca2ece463
Tom Dyas <tdyas@toolchain.com>
2021-11-24 15:27:26 -0500

    fix backend load test
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/go/lint/vet/register.py
+++ src/python/pants/backend/experimental/go/lint/vet/register.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from pants.backend.experimental.go.register import rules as go_rules
 from pants.backend.go.lint.vet import skip_field
 from pants.backend.go.lint.vet.rules import rules as go_vet_rules
 
@@ -9,4 +10,5 @@ def rules():
     return [
         *go_vet_rules(),
         *skip_field.rules(),
+        *go_rules(),
     ]
