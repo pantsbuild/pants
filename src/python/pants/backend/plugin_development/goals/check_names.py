8fcf104e742482e56e7982a05b51e798ec4cca76
Andreas Stenius <andreas.stenius@svenskaspel.se>
2021-12-04 00:12:51 +0100

    Plugin development tool to check names, such as Field class names.
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

---
+++ src/python/pants/backend/plugin_development/goals/check_names.py
@@ -0,0 +1,76 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+"""A Plugin development tool, to help check that naming conventions are followed in all loaded
+backends.
+
+Usage:
+
+    $ ./pants --backend-packages=pants.backend.plugin_development check-names
+
+Implemented checks:
+
+  * Field classes should have a `Field` suffix on the class name. (`Mixin` and `Base` are also
+    accepted, but not advertised.)
+"""
+
+from __future__ import annotations
+
+from typing import Iterator
+
+from pants.engine.console import Console
+from pants.engine.goal import Goal, GoalSubsystem
+from pants.engine.rules import collect_rules, goal_rule
+from pants.engine.target import Field
+
+
+class CheckNamesSubsystem(GoalSubsystem):
+    name = "check-names"
+    help = "Check names of things, that they follow Pants naming standards."
+
+
+class CheckNames(Goal):
+    subsystem_cls = CheckNamesSubsystem
+
+
+@goal_rule
+async def check_names(console: Console, subsystem: CheckNamesSubsystem) -> CheckNames:
+    console.print_stdout("Checking field class names...")
+    exit_code = check_field_names(console)
+
+    if exit_code == 0:
+        sigil = console.sigil_succeeded()
+        status = "all checks passed"
+    else:
+        sigil = console.sigil_failed()
+        status = "some checks failed"
+
+    console.print_stdout(f"\n{sigil} {status}.")
+    return CheckNames(exit_code)
+
+
+def get_subclasses_name_does_not_end_with(
+    base_class: type, *endswith: str
+) -> Iterator[tuple[str, str]]:
+    for cls in base_class.__subclasses__():
+        for suffix in endswith:
+            if cls.__name__.endswith(suffix):
+                break
+        else:
+            yield cls.__name__, cls.__module__
+        yield from get_subclasses_name_does_not_end_with(cls, *endswith)
+
+
+def check_field_names(console: Console) -> int:
+    exit_code = 0
+    for class_name, module in get_subclasses_name_does_not_end_with(
+        Field, "Field", "Base", "Mixin"
+    ):
+        console.print_stdout(f"Rename field class {module}.{class_name} => {class_name}Field")
+        exit_code = 1
+
+    return exit_code
+
+
+def rules():
+    return collect_rules()
--- src/python/pants/backend/plugin_development/register.py
+++ src/python/pants/backend/plugin_development/register.py
@@ -2,11 +2,15 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from pants.backend.plugin_development import pants_requirements
+from pants.backend.plugin_development.goals import check_names
 from pants.backend.plugin_development.pants_requirements import PantsRequirementsTargetGenerator
 
 
 def rules():
-    return pants_requirements.rules()
+    return (
+        *check_names.rules(),
+        *pants_requirements.rules(),
+    )
 
 
 def target_types():
