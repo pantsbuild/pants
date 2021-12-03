c17587c7b42e7e794ed83adaf3c8fc7f4cf0bc5b
Christopher Neugebauer <chrisjrn@toolchain.com>
2021-12-03 11:50:57 -0800

    Revert examplelib.java
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- testprojects/src/jvm/org/pantsbuild/example/lib/ExampleLib.java
+++ testprojects/src/jvm/org/pantsbuild/example/lib/ExampleLib.java
@@ -1,10 +1,7 @@
 package org.pantsbuild.example.lib;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class ExampleLib { 
+public class ExampleLib {
     public static String hello() {
-        ObjectMapper o = new ObjectMapper();
-        return "Hello, World and frederick";
+        return "Hello, World!";
     }
 }
