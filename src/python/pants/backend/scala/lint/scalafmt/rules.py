180bab01bb4b67ffd20f15b4e45bddcb00c73376
Tom Dyas <tdyas@toolchain.com>
2021-12-05 18:18:58 -0500

    scala: add scalafmt tool
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

---
+++ src/python/pants/backend/scala/lint/scalafmt/rules.py
@@ -0,0 +1,175 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+import textwrap
+from dataclasses import dataclass
+from typing import Iterable
+
+from pants.backend.scala.lint.scala_fmt import ScalaFmtRequest
+from pants.backend.scala.lint.scalafmt.skip_field import SkipScalafmtField
+from pants.backend.scala.lint.scalafmt.subsystem import ScalafmtSubsystem
+from pants.backend.scala.target_types import ScalaSourceField
+from pants.core.goals.fmt import FmtResult
+from pants.core.goals.lint import LintRequest, LintResult, LintResults
+from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
+from pants.engine.fs import CreateDigest, Digest, FileContent, MergeDigests
+from pants.engine.internals.selectors import Get, MultiGet
+from pants.engine.process import BashBinary, FallibleProcessResult, Process, ProcessResult
+from pants.engine.rules import collect_rules, rule
+from pants.engine.target import FieldSet, Target
+from pants.engine.unions import UnionRule
+from pants.jvm.jdk_rules import JdkSetup
+from pants.jvm.resolve.coursier_fetch import MaterializedClasspath, MaterializedClasspathRequest
+from pants.jvm.resolve.jvm_tool import JvmToolLockfileRequest, JvmToolLockfileSentinel
+from pants.testutil.rule_runner import logging
+from pants.util.logging import LogLevel
+from pants.util.strutil import pluralize
+
+
+@dataclass(frozen=True)
+class ScalafmtFieldSet(FieldSet):
+    required_fields = (ScalaSourceField,)
+
+    source: ScalaSourceField
+
+    @classmethod
+    def opt_out(cls, tgt: Target) -> bool:
+        return tgt.get(SkipScalafmtField).value
+
+
+class ScalafmtRequest(ScalaFmtRequest, LintRequest):
+    field_set_type = ScalafmtFieldSet
+
+
+class ScalafmtToolLockfileSentinel(JvmToolLockfileSentinel):
+    options_scope = ScalafmtSubsystem.options_scope
+
+
+@dataclass(frozen=True)
+class SetupRequest:
+    request: ScalafmtRequest
+    check_only: bool
+
+
+@dataclass(frozen=True)
+class Setup:
+    process: Process
+    original_digest: Digest
+
+
+@logging
+@rule(level=LogLevel.DEBUG)
+async def setup_google_java_format(
+    setup_request: SetupRequest,
+    tool: ScalafmtSubsystem,
+    jdk_setup: JdkSetup,
+    bash: BashBinary,
+) -> Setup:
+    source_files, tool_classpath, conf_digest = await MultiGet(
+        Get(
+            SourceFiles,
+            SourceFilesRequest(field_set.source for field_set in setup_request.request.field_sets),
+        ),
+        Get(
+            MaterializedClasspath,
+            MaterializedClasspathRequest(
+                prefix="__toolcp",
+                lockfiles=(tool.resolved_lockfile(),),
+            ),
+        ),
+        # TODO: Figure out how to scan for the correct configuration file to use while also dealing with
+        #  the seeming requirement that `version` must be set in the configuration file.
+        Get(
+            Digest,
+            CreateDigest(
+                [
+                    FileContent(
+                        path=".scalafmt.conf",
+                        content=textwrap.dedent(
+                            f"""\
+                version = "{tool.version}"
+                runner.dialect = scala213
+                """
+                        ).encode(),
+                    )
+                ]
+            ),
+        ),
+    )
+
+    source_files_snapshot = (
+        source_files.snapshot
+        if setup_request.request.prior_formatter_result is None
+        else setup_request.request.prior_formatter_result
+    )
+
+    input_digest = await Get(
+        Digest,
+        MergeDigests(
+            [source_files_snapshot.digest, conf_digest, tool_classpath.digest, jdk_setup.digest]
+        ),
+    )
+
+    def add_check_args() -> Iterable[str]:
+        if setup_request.check_only:
+            yield "--list"
+
+    args = [
+        *jdk_setup.args(bash, tool_classpath.classpath_entries()),
+        "org.scalafmt.cli.Cli",
+        "--config=.scalafmt.conf",
+        "--non-interactive",
+        *add_check_args(),
+    ]
+    args.extend(source_files.files)
+
+    process = Process(
+        argv=args,
+        input_digest=input_digest,
+        output_files=source_files_snapshot.files,
+        append_only_caches=jdk_setup.append_only_caches,
+        env=jdk_setup.env,
+        description=f"Run `scalafmt` against {pluralize(len(setup_request.request.field_sets), 'file')}.",
+        level=LogLevel.DEBUG,
+    )
+
+    return Setup(process, original_digest=source_files_snapshot.digest)
+
+
+@rule(desc="Format with scalafmt", level=LogLevel.DEBUG)
+async def scalafmt_fmt(field_sets: ScalafmtRequest, tool: ScalafmtSubsystem) -> FmtResult:
+    if tool.skip:
+        return FmtResult.skip(formatter_name="scalafmt")
+    setup = await Get(Setup, SetupRequest(field_sets, check_only=False))
+    result = await Get(ProcessResult, Process, setup.process)
+    return FmtResult.from_process_result(
+        result,
+        original_digest=setup.original_digest,
+        formatter_name="scalafmt",
+        strip_chroot_path=True,
+    )
+
+
+@rule(desc="Lint with scalafmt", level=LogLevel.DEBUG)
+async def scalafmt_lint(field_sets: ScalafmtRequest, tool: ScalafmtSubsystem) -> LintResults:
+    if tool.skip:
+        return LintResults([], linter_name="scalafmt")
+    setup = await Get(Setup, SetupRequest(field_sets, check_only=True))
+    result = await Get(FallibleProcessResult, Process, setup.process)
+    lint_result = LintResult.from_fallible_process_result(result)
+    return LintResults([lint_result], linter_name="scalafmt")
+
+
+@rule
+async def generate_scalafmt_lockfile_request(
+    _: ScalafmtToolLockfileSentinel,
+    tool: ScalafmtSubsystem,
+) -> JvmToolLockfileRequest:
+    return JvmToolLockfileRequest.from_tool(tool)
+
+
+def rules():
+    return [
+        *collect_rules(),
+        UnionRule(ScalaFmtRequest, ScalafmtRequest),
+        UnionRule(JvmToolLockfileSentinel, ScalafmtToolLockfileSentinel),
+    ]
---
+++ src/python/pants/backend/scala/lint/scalafmt/rules_integration_test.py
@@ -0,0 +1,153 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+from __future__ import annotations
+
+import pytest
+
+from pants.backend.scala import target_types
+from pants.backend.scala.compile.scalac import rules as scalac_rules
+from pants.backend.scala.lint import scala_fmt
+from pants.backend.scala.lint.scalafmt import skip_field
+from pants.backend.scala.lint.scalafmt.rules import ScalafmtFieldSet, ScalafmtRequest
+from pants.backend.scala.lint.scalafmt.rules import rules as scalafmt_rules
+from pants.backend.scala.target_types import ScalaSourcesGeneratorTarget, ScalaSourceTarget
+from pants.build_graph.address import Address
+from pants.core.goals.fmt import FmtResult
+from pants.core.goals.lint import LintResult, LintResults
+from pants.core.util_rules import config_files, source_files
+from pants.core.util_rules.external_tool import rules as external_tool_rules
+from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
+from pants.engine.fs import CreateDigest, Digest, FileContent
+from pants.engine.rules import QueryRule
+from pants.engine.target import Target
+from pants.jvm import classpath
+from pants.jvm.jdk_rules import rules as jdk_rules
+from pants.jvm.resolve.coursier_fetch import rules as coursier_fetch_rules
+from pants.jvm.resolve.coursier_setup import rules as coursier_setup_rules
+from pants.jvm.util_rules import rules as util_rules
+from pants.testutil.rule_runner import PYTHON_BOOTSTRAP_ENV, RuleRunner
+
+NAMED_RESOLVE_OPTIONS = '--jvm-resolves={"test": "coursier_resolve.lockfile"}'
+DEFAULT_RESOLVE_OPTION = "--jvm-default-resolve=test"
+
+
+@pytest.fixture
+def rule_runner() -> RuleRunner:
+    rule_runner = RuleRunner(
+        rules=[
+            *config_files.rules(),
+            *classpath.rules(),
+            *coursier_fetch_rules(),
+            *coursier_setup_rules(),
+            *external_tool_rules(),
+            *source_files.rules(),
+            *scalac_rules(),
+            *util_rules(),
+            *jdk_rules(),
+            *target_types.rules(),
+            *scala_fmt.rules(),
+            *scalafmt_rules(),
+            *skip_field.rules(),
+            QueryRule(LintResults, (ScalafmtRequest,)),
+            QueryRule(FmtResult, (ScalafmtRequest,)),
+            QueryRule(SourceFiles, (SourceFilesRequest,)),
+        ],
+        target_types=[ScalaSourceTarget, ScalaSourcesGeneratorTarget],
+    )
+    rule_runner.set_options(
+        [
+            NAMED_RESOLVE_OPTIONS,
+            DEFAULT_RESOLVE_OPTION,
+        ],
+        env_inherit=PYTHON_BOOTSTRAP_ENV,
+    )
+    return rule_runner
+
+
+GOOD_FILE = """\
+package org.pantsbuild.example
+
+object Foo {
+  val Foo = 3
+}
+"""
+
+BAD_FILE = """\
+package org.pantsbuild.example
+
+object Bar {
+val Foo = 3
+}
+"""
+
+FIXED_BAD_FILE = """\
+package org.pantsbuild.example
+
+object Bar {
+  val Foo = 3
+}
+"""
+
+
+def run_scalafmt(
+    rule_runner: RuleRunner, targets: list[Target]
+) -> tuple[tuple[LintResult, ...], FmtResult]:
+    field_sets = [ScalafmtFieldSet.create(tgt) for tgt in targets]
+    lint_results = rule_runner.request(LintResults, [ScalafmtRequest(field_sets)])
+    input_sources = rule_runner.request(
+        SourceFiles,
+        [
+            SourceFilesRequest(field_set.source for field_set in field_sets),
+        ],
+    )
+    fmt_result = rule_runner.request(
+        FmtResult,
+        [
+            ScalafmtRequest(field_sets, prior_formatter_result=input_sources.snapshot),
+        ],
+    )
+    return lint_results.results, fmt_result
+
+
+def get_digest(rule_runner: RuleRunner, source_files: dict[str, str]) -> Digest:
+    files = [FileContent(path, content.encode()) for path, content in source_files.items()]
+    return rule_runner.request(Digest, [CreateDigest(files)])
+
+
+def test_passing(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files({"Foo.scala": GOOD_FILE, "BUILD": "scala_sources(name='t')"})
+    tgt = rule_runner.get_target(Address("", target_name="t", relative_file_path="Foo.scala"))
+    lint_results, fmt_result = run_scalafmt(rule_runner, [tgt])
+    assert len(lint_results) == 1
+    assert lint_results[0].exit_code == 0
+    assert fmt_result.output == get_digest(rule_runner, {"Foo.scala": GOOD_FILE})
+    assert fmt_result.did_change is False
+
+
+def test_failing(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files({"Bar.scala": BAD_FILE, "BUILD": "scala_sources(name='t')"})
+    tgt = rule_runner.get_target(Address("", target_name="t", relative_file_path="Bar.scala"))
+    lint_results, fmt_result = run_scalafmt(rule_runner, [tgt])
+    assert len(lint_results) == 1
+    assert lint_results[0].exit_code == 1
+    assert "Bar.scala\n" == lint_results[0].stdout
+    assert fmt_result.output == get_digest(rule_runner, {"Bar.scala": FIXED_BAD_FILE})
+    assert fmt_result.did_change is True
+
+
+def test_multiple_targets(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(
+        {"Foo.scala": GOOD_FILE, "Bar.scala": BAD_FILE, "BUILD": "scala_sources(name='t')"}
+    )
+    tgts = [
+        rule_runner.get_target(Address("", target_name="t", relative_file_path="Foo.scala")),
+        rule_runner.get_target(Address("", target_name="t", relative_file_path="Bar.scala")),
+    ]
+    lint_results, fmt_result = run_scalafmt(rule_runner, tgts)
+    assert len(lint_results) == 1
+    assert lint_results[0].exit_code == 1
+    assert "Bar.scala\n" == lint_results[0].stdout
+    assert fmt_result.output == get_digest(
+        rule_runner, {"Foo.scala": GOOD_FILE, "Bar.scala": FIXED_BAD_FILE}
+    )
+    assert fmt_result.did_change is True
