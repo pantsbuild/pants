a0a65c06c285bd11cdd3f506309279014950d289
Tom Dyas <tdyas@toolchain.com>
2021-12-05 21:25:23 -0500

    fmt
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/java/lint/google_java_format/register.py
+++ src/python/pants/backend/experimental/java/lint/google_java_format/register.py
@@ -1,7 +1,8 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.backend.java.lint.google_java_format import rules as fmt_rules, skip_field
+from pants.backend.java.lint.google_java_format import rules as fmt_rules
+from pants.backend.java.lint.google_java_format import skip_field
 
 
 def rules():
