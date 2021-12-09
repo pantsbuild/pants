ff8c62a585d8827b48440713c362e86534b412c0
Eric Arellano <ericarellano@me.com>
2021-12-08 18:31:54 -0700

    Add missing future annotatoins
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/python/util_rules/ancestor_files_test.py
+++ src/python/pants/backend/python/util_rules/ancestor_files_test.py
@@ -1,6 +1,8 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import annotations
+
 import pytest
 
 from pants.backend.python.util_rules import ancestor_files
