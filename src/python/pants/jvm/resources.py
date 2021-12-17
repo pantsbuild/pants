489642ed8ff711fb3486fb9610e0f20ec735fcff
Christopher Neugebauer <chrisjrn@toolchain.com>
2021-12-17 12:59:25 +1100

    Pull in dependencies when computing resource classpaths
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/jvm/resources.py
+++ src/python/pants/jvm/resources.py
@@ -1,14 +1,17 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
-
+from itertools import chain
+import logging
 from pants.core.target_types import ResourcesFieldSet, ResourcesGeneratorFieldSet
 from pants.core.util_rules.archive import ZipBinary
 from pants.core.util_rules.source_files import SourceFilesRequest
 from pants.core.util_rules.stripped_source_files import StrippedSourceFiles
+from pants.engine.fs import Digest, MergeDigests
+from pants.engine.internals.selectors import MultiGet
 from pants.engine.process import Process, ProcessResult
 from pants.engine.rules import Get, collect_rules, rule
 from pants.engine.target import SourcesField
-from pants.engine.unions import UnionRule
+from pants.engine.unions import UnionMembership, UnionRule
 from pants.jvm.compile import (
     ClasspathEntry,
     ClasspathEntryRequest,
@@ -16,6 +19,7 @@ from pants.jvm.compile import (
     FallibleClasspathEntry,
 )
 
+logger = logging.getLogger(__name__)
 
 class JvmResourcesRequest(ClasspathEntryRequest):
     field_sets = (
@@ -24,14 +28,37 @@ class JvmResourcesRequest(ClasspathEntryRequest):
     )
 
 
-_PANTS_RESOURCES_PARTIAL_JAR_FILENAME = "pants-resources.jar"
-
-
 @rule(desc="Fetch with coursier")
 async def assemble_resources_jar(
     zip: ZipBinary,
+    union_membership: UnionMembership,
     request: JvmResourcesRequest,
 ) -> FallibleClasspathEntry:
+
+    # Request the component's direct dependency classpath, and additionally any prerequisite.
+    classpath_entry_requests = [
+        *((request.prerequisite,) if request.prerequisite else ()),
+        *(
+            ClasspathEntryRequest.for_targets(
+                union_membership, component=coarsened_dep, resolve=request.resolve
+            )
+            for coarsened_dep in request.component.dependencies
+        ),
+    ]
+    direct_dependency_classpath_entries = FallibleClasspathEntry.if_all_succeeded(
+        await MultiGet(
+            Get(FallibleClasspathEntry, ClasspathEntryRequest, cpe)
+            for cpe in classpath_entry_requests
+        )
+    )
+
+    if direct_dependency_classpath_entries is None:
+        return FallibleClasspathEntry(
+            description=str(request.component),
+            result=CompileResult.DEPENDENCY_FAILED,
+            output=None,
+            exit_code=1,
+        )
 
     source_files = await Get(
         StrippedSourceFiles,
@@ -50,14 +77,24 @@ async def assemble_resources_jar(
                 output_filename,
                 *source_files.snapshot.files,
             ],
-            description="Build partial JAR containing resources files file",
+            description="Build partial JAR containing resources files",
             input_digest=resources_jar_input_digest,
             output_files=output_files,
         ),
     )
 
     cpe = ClasspathEntry(resources_jar_result.output_digest, output_files, [])
-    return FallibleClasspathEntry("Resources JAR assembly", CompileResult.SUCCEEDED, cpe, 0)
+
+    merged_cpe_digest = await Get(
+        Digest,
+        MergeDigests(chain((cpe.digest,), (i.digest for i in direct_dependency_classpath_entries))),
+    )
+
+    merged_cpe = ClasspathEntry.merge(
+        digest=merged_cpe_digest, entries=[cpe, *direct_dependency_classpath_entries]
+    )
+
+    return FallibleClasspathEntry(output_filename, CompileResult.SUCCEEDED, merged_cpe, 0)
 
 
 def rules():
