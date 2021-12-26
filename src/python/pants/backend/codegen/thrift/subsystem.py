69ac3ed52830aa861394e43a3c1bfc3cbf9b9f91
Tom Dyas <tdyas@toolchain.com>
2021-12-26 07:47:12 +0100

    add target types for thrift sources

---
+++ src/python/pants/backend/codegen/thrift/subsystem.py
@@ -0,0 +1,27 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+from typing import cast
+
+from pants.option.subsystem import Subsystem
+
+
+class ThriftSubsystem(Subsystem):
+    options_scope = "thrift"
+    help = "Thrift IDL compiler (https://thrift.apache.org/)."
+
+    @classmethod
+    def register_options(cls, register):
+        super().register_options(register)
+        register(
+            "--dependency-inference",
+            type=bool,
+            # TODO: Implement dependency inference at a later point in this PR.
+            default=True,
+            help=(
+                "Infer Thrift dependencies on other Thrift files by analyzing import " "statements."
+            ),
+        )
+
+    @property
+    def dependency_inference(self) -> bool:
+        return cast(bool, self.options.dependency_inference)
