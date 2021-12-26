3043b08d86d2b0124d65a967c94d84c071568cbe
Tom Dyas <tdyas@toolchain.com>
2021-12-26 22:14:45 +0000

    add backends for Thrift

---
+++ src/python/pants/backend/experimental/codegen/thrift/apache/java/register.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+from pants.backend.codegen.thrift.target_types import ThriftSourceTarget, ThriftSourcesGeneratorTarget
+from pants.backend.codegen.thrift.apache.java.rules import rules as apache_thrift_java_rules
+from pants.backend.codegen.thrift.apache.rules import rules as apache_thrift_rules
+from pants.backend.codegen.thrift.rules import rules as thrift_rules
+
+
+def target_types():
+    return [ThriftSourcesGeneratorTarget, ThriftSourceTarget]
+
+
+def rules():
+    return [
+        *thrift_rules(),
+        *apache_thrift_rules(),
+        *apache_thrift_java_rules(),
+    ]
