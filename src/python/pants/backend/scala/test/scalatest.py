ea3fe763489c7810ce0d56ed6d8a316939f7640a
Stu Hood <stuhood@gmail.com>
2021-12-27 17:12:56 -0500

    Support remote debugging in scalatest and junit. (cherrypick of #13958 and #13966) (#14000)
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/backend/scala/test/scalatest.py
+++ src/python/pants/backend/scala/test/scalatest.py
@@ -9,13 +9,21 @@ from pants.backend.scala.target_types import ScalatestTestSourceField
 from pants.core.goals.test import TestDebugRequest, TestFieldSet, TestResult, TestSubsystem
 from pants.engine.addresses import Addresses
 from pants.engine.fs import Digest, DigestSubset, MergeDigests, PathGlobs, RemovePrefix, Snapshot
-from pants.engine.process import BashBinary, FallibleProcessResult, Process
+from pants.engine.process import (
+    BashBinary,
+    FallibleProcessResult,
+    InteractiveProcess,
+    InteractiveProcessRequest,
+    Process,
+    ProcessCacheScope,
+)
 from pants.engine.rules import Get, MultiGet, collect_rules, rule
 from pants.engine.unions import UnionRule
 from pants.jvm.classpath import Classpath
 from pants.jvm.jdk_rules import JdkSetup
 from pants.jvm.resolve.coursier_fetch import MaterializedClasspath, MaterializedClasspathRequest
 from pants.jvm.resolve.jvm_tool import JvmToolLockfileRequest, JvmToolLockfileSentinel
+from pants.jvm.subsystems import JvmSubsystem
 from pants.util.logging import LogLevel
 
 logger = logging.getLogger(__name__)
@@ -32,16 +40,29 @@ class ScalatestToolLockfileSentinel(JvmToolLockfileSentinel):
     resolve_name = Scalatest.options_scope
 
 
-@rule(desc="Run Scalatest", level=LogLevel.DEBUG)
-async def run_scalatest_test(
+@dataclass(frozen=True)
+class TestSetupRequest:
+    field_set: ScalatestTestFieldSet
+    is_debug: bool
+
+
+@dataclass(frozen=True)
+class TestSetup:
+    process: Process
+    reports_dir_prefix: str
+
+
+@rule(level=LogLevel.DEBUG)
+async def setup_scalatest_for_target(
+    request: TestSetupRequest,
     bash: BashBinary,
     jdk_setup: JdkSetup,
+    jvm: JvmSubsystem,
     scalatest: Scalatest,
     test_subsystem: TestSubsystem,
-    field_set: ScalatestTestFieldSet,
-) -> TestResult:
+) -> TestSetup:
     classpath, scalatest_classpath = await MultiGet(
-        Get(Classpath, Addresses([field_set.address])),
+        Get(Classpath, Addresses([request.field_set.address])),
         Get(
             MaterializedClasspath,
             MaterializedClasspathRequest(lockfiles=(scalatest.resolved_lockfile(),)),
@@ -57,41 +78,60 @@ async def run_scalatest_test(
     }
 
     reports_dir_prefix = "__reports_dir"
-    reports_dir = f"{reports_dir_prefix}/{field_set.address.path_safe_spec}"
+    reports_dir = f"{reports_dir_prefix}/{request.field_set.address.path_safe_spec}"
 
     # Classfiles produced by the root `scalatest_test` targets are the only ones which should run.
     user_classpath_arg = ":".join(classpath.root_args())
 
-    process_result = await Get(
-        FallibleProcessResult,
-        Process(
-            argv=[
-                *jdk_setup.args(
-                    bash,
-                    [
-                        *classpath.args(),
-                        *scalatest_classpath.classpath_entries(toolcp_relpath),
-                    ],
-                ),
-                "org.scalatest.tools.Runner",
-                # TODO: We currently give the entire user classpath to the JVM for startup (which
-                # mixes it with the user classpath), and then only specify the roots to run here.
-                #   see https://github.com/pantsbuild/pants/issues/13871
-                *(("-R", user_classpath_arg) if user_classpath_arg else ()),
-                "-o",
-                "-u",
-                reports_dir,
-                *scalatest.options.args,
-            ],
-            input_digest=merged_classpath_digest,
-            immutable_input_digests=immutable_input_digests,
-            output_directories=(reports_dir,),
-            append_only_caches=jdk_setup.append_only_caches,
-            env=jdk_setup.env,
-            description=f"Run Scalatest runner for {field_set.address}",
-            level=LogLevel.DEBUG,
-        ),
+    # Cache test runs only if they are successful, or not at all if `--test-force`.
+    cache_scope = (
+        ProcessCacheScope.PER_SESSION if test_subsystem.force else ProcessCacheScope.SUCCESSFUL
     )
+
+    extra_jvm_args: list[str] = []
+    if request.is_debug:
+        extra_jvm_args.extend(jvm.debug_args)
+
+    process = Process(
+        argv=[
+            *jdk_setup.args(
+                bash,
+                [
+                    *classpath.args(),
+                    *scalatest_classpath.classpath_entries(toolcp_relpath),
+                ],
+            ),
+            *extra_jvm_args,
+            "org.scalatest.tools.Runner",
+            # TODO: We currently give the entire user classpath to the JVM for startup (which
+            # mixes it with the user classpath), and then only specify the roots to run here.
+            #   see https://github.com/pantsbuild/pants/issues/13871
+            *(("-R", user_classpath_arg) if user_classpath_arg else ()),
+            "-o",
+            "-u",
+            reports_dir,
+            *scalatest.options.args,
+        ],
+        input_digest=merged_classpath_digest,
+        immutable_input_digests=immutable_input_digests,
+        output_directories=(reports_dir,),
+        append_only_caches=jdk_setup.append_only_caches,
+        env=jdk_setup.env,
+        description=f"Run Scalatest runner for {request.field_set.address}",
+        level=LogLevel.DEBUG,
+        cache_scope=cache_scope,
+    )
+    return TestSetup(process=process, reports_dir_prefix=reports_dir_prefix)
+
+
+@rule(desc="Run Scalatest", level=LogLevel.DEBUG)
+async def run_scalatest_test(
+    test_subsystem: TestSubsystem,
+    field_set: ScalatestTestFieldSet,
+) -> TestResult:
+    test_setup = await Get(TestSetup, TestSetupRequest(field_set, is_debug=False))
+    process_result = await Get(FallibleProcessResult, Process, test_setup.process)
+    reports_dir_prefix = test_setup.reports_dir_prefix
 
     xml_result_subset = await Get(
         Digest, DigestSubset(process_result.output_digest, PathGlobs([f"{reports_dir_prefix}/**"]))
@@ -106,10 +146,16 @@ async def run_scalatest_test(
     )
 
 
-# Required by standard test rules. Do nothing for now.
 @rule(level=LogLevel.DEBUG)
-async def setup_scalatest_debug_request(_field_set: ScalatestTestFieldSet) -> TestDebugRequest:
-    raise NotImplementedError("TestDebugResult is not implemented for Scalatest (yet?).")
+async def setup_scalatest_debug_request(field_set: ScalatestTestFieldSet) -> TestDebugRequest:
+    setup = await Get(TestSetup, TestSetupRequest(field_set, is_debug=True))
+    interactive_process = await Get(
+        InteractiveProcess,
+        InteractiveProcessRequest(
+            setup.process, forward_signals_to_process=False, restartable=True
+        ),
+    )
+    return TestDebugRequest(interactive_process)
 
 
 @rule
--- src/python/pants/engine/process.py
+++ src/python/pants/engine/process.py
@@ -13,7 +13,15 @@ from typing import Iterable, Mapping
 
 from pants.engine.collection import DeduplicatedCollection
 from pants.engine.engine_aware import EngineAwareReturnType, SideEffecting
-from pants.engine.fs import EMPTY_DIGEST, CreateDigest, Digest, FileContent, FileDigest
+from pants.engine.fs import (
+    EMPTY_DIGEST,
+    AddPrefix,
+    CreateDigest,
+    Digest,
+    FileContent,
+    FileDigest,
+    MergeDigests,
+)
 from pants.engine.internals.selectors import MultiGet
 from pants.engine.internals.session import RunId
 from pants.engine.platform import Platform
@@ -349,6 +357,14 @@ class InteractiveProcess(SideEffecting):
         forward_signals_to_process: bool = True,
         restartable: bool = False,
     ) -> InteractiveProcess:
+        # TODO: Remove this check once https://github.com/pantsbuild/pants/issues/13852 is
+        #  implemented and the immutable_input_digests are propagated into the InteractiveProcess.
+        if process.immutable_input_digests:
+            raise ValueError(
+                "Process has immutable_input_digests, so it cannot be converted to an "
+                "InteractiveProcess by calling from_process().  Use an async "
+                "InteractiveProcessRequest instead."
+            )
         return InteractiveProcess(
             argv=process.argv,
             env=process.env,
@@ -357,6 +373,39 @@ class InteractiveProcess(SideEffecting):
             restartable=restartable,
             append_only_caches=process.append_only_caches,
         )
+
+
+@dataclass(frozen=True)
+class InteractiveProcessRequest:
+    process: Process
+    forward_signals_to_process: bool = True
+    restartable: bool = False
+
+
+@rule
+async def interactive_process_from_process(req: InteractiveProcessRequest) -> InteractiveProcess:
+    # TODO: Temporary workaround until https://github.com/pantsbuild/pants/issues/13852
+    #  is implemented. Once that is implemented we can get rid of this rule, and the
+    #  InteractiveProcessRequest type, and use InteractiveProcess.from_process directly.
+
+    if req.process.immutable_input_digests:
+        prefixed_immutable_input_digests = await MultiGet(
+            Get(Digest, AddPrefix(digest, prefix))
+            for prefix, digest in req.process.immutable_input_digests.items()
+        )
+        full_input_digest = await Get(
+            Digest, MergeDigests([req.process.input_digest, *prefixed_immutable_input_digests])
+        )
+    else:
+        full_input_digest = req.process.input_digest
+    return InteractiveProcess(
+        argv=req.process.argv,
+        env=req.process.env,
+        input_digest=full_input_digest,
+        forward_signals_to_process=req.forward_signals_to_process,
+        restartable=req.restartable,
+        append_only_caches=req.process.append_only_caches,
+    )
 
 
 @frozen_after_init
