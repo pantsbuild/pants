1307431efd952609d0e77475bf16011eb85326f2
Andreas Stenius <andreas.stenius@svenskaspel.se>
2022-01-10 22:58:14 +0100

    [Docker] Fix bug when a putative target address did not exist. (#14125) (#14131)

--- src/python/pants/backend/docker/util_rules/dependencies.py
+++ src/python/pants/backend/docker/util_rules/dependencies.py
@@ -3,6 +3,7 @@
 
 from pants.backend.docker.subsystems.dockerfile_parser import DockerfileInfo, DockerfileInfoRequest
 from pants.backend.docker.target_types import DockerDependenciesField
+from pants.base.specs import AddressSpecs, MaybeEmptySiblingAddresses
 from pants.core.goals.package import PackageFieldSet
 from pants.engine.addresses import Addresses, UnparsedAddressInputs
 from pants.engine.rules import Get, collect_rules, rule
@@ -22,17 +23,28 @@ class InjectDockerDependencies(InjectDependenciesRequest):
 
 @rule
 async def inject_docker_dependencies(request: InjectDockerDependencies) -> InjectedDependencies:
-    """Inspects COPY instructions in the Dockerfile for references to known targets."""
+    """Inspects COPY instructions in the Dockerfile for references to known packagable targets."""
     dockerfile_info = await Get(
         DockerfileInfo, DockerfileInfoRequest(request.dependencies_field.address)
     )
-    targets = await Get(
-        Targets,
+
+    # Parse all putative target addresses.
+    putative_addresses = await Get(
+        Addresses,
         UnparsedAddressInputs(
             dockerfile_info.putative_target_addresses,
             owning_address=None,
         ),
     )
+
+    # Get the target for those addresses that are known.
+    directories = {address.spec_path for address in putative_addresses}
+    all_addresses = await Get(Addresses, AddressSpecs(map(MaybeEmptySiblingAddresses, directories)))
+    targets = await Get(
+        Targets, Addresses((address for address in putative_addresses if address in all_addresses))
+    )
+
+    # Only keep those targets that we can "package".
     package = await Get(FieldSetsPerTarget, FieldSetsPerTargetRequest(PackageFieldSet, targets))
     referenced_targets = (
         field_sets[0].address for field_sets in package.collection if len(field_sets) > 0
