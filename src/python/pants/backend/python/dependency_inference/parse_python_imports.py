21ef0f1106314c87d7b00f1dd1c1a39951692ae0
Joshua Cannon <joshdcannon@gmail.com>
2022-01-14 19:54:38 -0600

    newline

--- src/python/pants/backend/python/dependency_inference/parse_python_imports.py
+++ src/python/pants/backend/python/dependency_inference/parse_python_imports.py
@@ -1,7 +1,6 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-
 import json
 import pkgutil
 from dataclasses import dataclass
