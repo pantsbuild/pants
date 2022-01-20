9c8e78e9081e77f7d0212fe04d861982dba962ca
Eric Arellano <ericarellano@me.com>
2022-01-19 17:50:07 -0700

    Small cleanup
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/jvm/resolve/lockfile_metadata.py
+++ src/python/pants/jvm/resolve/lockfile_metadata.py
@@ -3,7 +3,6 @@
 
 from __future__ import annotations
 
-import logging
 from dataclasses import dataclass
 from enum import Enum
 from typing import Any, Iterable, cast
@@ -17,8 +16,6 @@ from pants.core.util_rules.lockfile_metadata import (
 )
 from pants.jvm.resolve.common import ArtifactRequirement
 from pants.util.ordered_set import FrozenOrderedSet
-
-logger = logging.getLogger(__name__)
 
 _jvm_lockfile_metadata = lockfile_metadata_registrar(LockfileScope.JVM)
 
@@ -56,15 +53,6 @@ class JVMLockfileMetadata(LockfileMetadata):
                 LockfileScope.JVM, lockfile, lockfile_path, resolve_name
             ),
         )
-
-    def _header_dict(self) -> dict[Any, Any]:
-        """Produce a dictionary to be serialized into the lockfile header.
-
-        Subclasses should call `super` and update the resulting dictionary.
-        """
-
-        d = super()._header_dict()
-        return d
 
     def is_valid_for(
         self,
@@ -111,12 +99,12 @@ class JVMLockfileMetadataV1(JVMLockfileMetadata):
         return JVMLockfileMetadataV1(requirements)
 
     def _header_dict(self) -> dict[Any, Any]:
-        out = super()._header_dict()
-
-        out["generated_with_requirements"] = (
-            sorted(self.requirements) if self.requirements is not None else None
-        )
-        return out
+        return {
+            **super()._header_dict(),
+            "generated_with_requirements": (
+                sorted(self.requirements) if self.requirements is not None else None
+            ),
+        }
 
     def is_valid_for(
         self,
