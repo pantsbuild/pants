97737a37ba68e97abd550428533f51df8d7eb264
Eric Arellano <ericarellano@me.com>
2022-01-24 10:31:56 -0700

    Final rule graph fix...
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/codegen/avro/java/register.py
+++ src/python/pants/backend/experimental/codegen/avro/java/register.py
@@ -1,11 +1,13 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 from pants.backend.codegen.avro.java.rules import rules as avro_java_rules
 from pants.backend.codegen.avro.rules import rules as avro_rules
 from pants.backend.codegen.avro.target_types import AvroSourcesGeneratorTarget, AvroSourceTarget
 from pants.core.util_rules import config_files, source_files, stripped_source_files
 from pants.jvm import classpath, compile, jdk_rules
 from pants.jvm.resolve import coursier_fetch, jvm_tool
+from pants.jvm.goals import lockfile
 from pants.jvm.util_rules import rules as util_rules
 
 
@@ -27,4 +29,5 @@ def rules():
         *jdk_rules.rules(),
         *stripped_source_files.rules(),
         *compile.rules(),
+        *lockfile.rules(),
     ]
