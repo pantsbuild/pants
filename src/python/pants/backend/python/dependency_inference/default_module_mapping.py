93b6a8bd4d7bcdb72dec265ca41c56bd67cd4457
Joshua Cannon <joshdcannon@gmail.com>
2022-01-27 20:27:58 +0000

    Update src/python/pants/backend/python/dependency_inference/default_module_mapping.py
    Co-authored-by: Benjy Weinberger <benjy@toolchain.com>

--- src/python/pants/backend/python/dependency_inference/default_module_mapping.py
+++ src/python/pants/backend/python/dependency_inference/default_module_mapping.py
@@ -81,7 +81,7 @@ DEFAULT_MODULE_MAPPING = {
     "python-jose": ("jose",),
     "python-pptx": ("pptx",),
     "pyyaml": ("yaml",),
-    "pymongo": ("bson", "gridfs", "requests"),
+    "pymongo": ("bson", "gridfs", "pymongo"),
     "pymupdf": ("fitz",),
     "pytest-runner": ("ptr",),
     "scikit-image": ("skimage",),
