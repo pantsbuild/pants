a099547b113513343868c1eedce700bb14460c11
Andreas Stenius <andreas.stenius@svenskaspel.se>
2022-01-29 22:33:42 +0100

    Graduate the Dockerfile linter Hadolint from experimental status.
    [ci skip-rust]

--- src/python/pants/backend/experimental/docker/lint/hadolint/register.py
+++ src/python/pants/backend/experimental/docker/lint/hadolint/register.py
@@ -1,14 +1,16 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.backend.docker.lint.hadolint import skip_field
-from pants.backend.docker.lint.hadolint.rules import rules as hadolint_rules
-from pants.backend.docker.rules import rules as docker_rules
+from pants.backend.docker.lint.hadolint import register
+from pants.base.deprecated import deprecated
 
 
+@deprecated(
+    "2.11.0.dev0",
+    (
+        "The `pants.backend.experimental.docker.lint.hadolint` backend has graduated. Use "
+        "`pants.backend.docker.lint.hadolint` instead."
+    ),
+)
 def rules():
-    return (
-        *docker_rules(),
-        *hadolint_rules(),
-        *skip_field.rules(),
-    )
+    return register.rules()
