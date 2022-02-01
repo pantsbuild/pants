2d437811668d7b357dc83c882674e093ab657e67
Andreas Stenius <andreas.stenius@svenskaspel.se>
2022-02-01 10:19:59 +0100

    Setup Material UI. Swap to using yarn rather than npm for package management.
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/javascript/explorer/src/index.tsx
+++ src/javascript/explorer/src/index.tsx
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
