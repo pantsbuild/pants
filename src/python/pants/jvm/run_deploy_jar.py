59a6a9279dc2af6c96bae5aa458739c6d272ff91
Christopher Neugebauer <chrisjrn@toolchain.com>
2022-02-02 09:08:25 -0800

    Make `run_deploy_jar` actually use the deploy jar
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/jvm/run_deploy_jar.py
+++ src/python/pants/jvm/run_deploy_jar.py
@@ -5,18 +5,15 @@ import logging
 import shlex
 import textwrap
 
+from pants.core.goals.package import BuiltPackage
 from pants.core.goals.run import RunFieldSet, RunRequest
-from pants.engine.addresses import Addresses
 from pants.engine.fs import CreateDigest, Digest, FileContent, MergeDigests
 from pants.engine.internals.native_engine import AddPrefix
 from pants.engine.process import BashBinary, Process
 from pants.engine.rules import Get, MultiGet, collect_rules, rule
-from pants.engine.target import CoarsenedTargets
 from pants.engine.unions import UnionRule
-from pants.jvm.compile import ClasspathEntry
 from pants.jvm.jdk_rules import JvmProcess
-from pants.jvm.package.deploy_jar import DeployJarClasspathEntryRequest, DeployJarFieldSet
-from pants.jvm.resolve.key import CoursierResolveKey
+from pants.jvm.package.deploy_jar import DeployJarFieldSet
 from pants.util.logging import LogLevel
 
 logger = logging.getLogger(__name__)
@@ -27,34 +24,22 @@ async def create_deploy_jar_run_request(
     field_set: DeployJarFieldSet,
     bash: BashBinary,
 ) -> RunRequest:
-    """transitive_targets = await MultiGet(
-        Get(TransitiveTargets, TransitiveTargetsRequest([field_set.address])),
-    )"""
-
-    coarsened_targets = await Get(CoarsenedTargets, Addresses([field_set.address]))
-    resolve = await Get(CoursierResolveKey, CoarsenedTargets, coarsened_targets)
-
-    item = await Get(
-        ClasspathEntry,
-        DeployJarClasspathEntryRequest(component=coarsened_targets[0], resolve=resolve),
-    )
-
-    assert (
-        len(item.dependencies) == 1
-    )  # Deploy JAR requests should only produce a single deploy JAR
 
     main_class = field_set.main_class.value
     assert main_class is not None
 
-    out_jar: ClasspathEntry = next(iter(item.dependencies))
+    package = await Get(BuiltPackage, DeployJarFieldSet, field_set)
+    assert len(package.artifacts) == 1
+    jar_path = package.artifacts[0].relpath
+    assert jar_path is not None
 
     proc = await Get(
         Process,
         JvmProcess(
-            classpath_entries=out_jar.filenames,
+            classpath_entries=[jar_path],
             argv=(main_class,),
-            input_digest=out_jar.digest,
-            description=f"Run {out_jar.filenames[0]}",
+            input_digest=package.digest,
+            description=f"Run {main_class}.main(String[])",
             use_nailgun=False,
         ),
     )
@@ -92,7 +77,12 @@ async def create_deploy_jar_run_request(
     args = [bash.path, f"{{chroot}}/{relapath}"]
     logger.warning("%s", f"{args=}")
 
-    return RunRequest(digest=request_digest, args=args, extra_env=proc.env)
+    return RunRequest(
+        digest=request_digest,
+        args=args,
+        extra_env=proc.env,
+        append_only_caches=proc.append_only_caches,
+    )
 
 
 def rules():
