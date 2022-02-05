ee296d2883bdbf1cd4dadfc0c10d0adc91e06d58
Joshua Cannon <joshdcannon@gmail.com>
2022-02-04 19:51:21 -0600

    go

--- src/python/pants/backend/go/lint/gofmt/subsystem.py
+++ src/python/pants/backend/go/lint/gofmt/subsystem.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from pants.option.option_types import BoolOption
 from pants.option.subsystem import Subsystem
 
 
@@ -8,15 +9,7 @@ class GofmtSubsystem(Subsystem):
     options_scope = "gofmt"
     help = "Gofmt-specific options."
 
-    @classmethod
-    def register_options(cls, register):
-        super().register_options(register)
-        register(
-            "--skip",
-            type=bool,
-            default=False,
-            help=(
-                f"Don't use gofmt when running `{register.bootstrap.pants_bin_name} fmt` and "
-                f"`{register.bootstrap.pants_bin_name} lint`."
-            ),
-        )
+    skip = BoolOption(
+        "--skip",
+        help=("Don't use gofmt when running `./pants fmt` and `./pants lint`."),
+    )
--- src/python/pants/backend/go/lint/vet/subsystem.py
+++ src/python/pants/backend/go/lint/vet/subsystem.py
@@ -1,8 +1,8 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from typing import cast
 
+from pants.option.option_types import BoolOption
 from pants.option.subsystem import Subsystem
 
 
@@ -10,16 +10,7 @@ class GoVetSubsystem(Subsystem):
     options_scope = "go-vet"
     help = "`go vet`-specific options."
 
-    @classmethod
-    def register_options(cls, register):
-        super().register_options(register)
-        register(
-            "--skip",
-            type=bool,
-            default=False,
-            help=f"Don't use `go vet` when running `{register.bootstrap.pants_bin_name} lint`.",
-        )
-
-    @property
-    def skip(self) -> bool:
-        return cast(bool, self.options.skip)
+    skip = BoolOption(
+        "--skip",
+        help=("Don't use gofmt when running `./pants fmt` and `./pants lint`."),
+    )
