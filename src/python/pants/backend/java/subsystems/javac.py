fdffba71e66f4aa4b456fa09111cb54f027d78e0
Joshua Cannon <joshdcannon@gmail.com>
2022-02-04 19:55:31 -0600

    java

--- src/python/pants/backend/java/subsystems/javac.py
+++ src/python/pants/backend/java/subsystems/javac.py
@@ -4,9 +4,8 @@
 from __future__ import annotations
 
 import logging
-from typing import cast
 
-from pants.option.custom_types import shell_str
+from pants.option.option_types import ArgsListOption
 from pants.option.subsystem import Subsystem
 
 logger = logging.getLogger(__name__)
@@ -16,20 +15,6 @@ class JavacSubsystem(Subsystem):
     options_scope = "javac"
     help = "The javac Java source compiler."
 
-    @classmethod
-    def register_options(cls, register):
-        super().register_options(register)
-        register(
-            "--args",
-            type=list,
-            member_type=shell_str,
-            default=[],
-            help=(
-                "Global `javac` compiler flags, e.g. "
-                f"`--{cls.options_scope}-args='-g -deprecation'`."
-            ),
-        )
-
-    @property
-    def args(self) -> list[str]:
-        return cast("list[str]", self.options.args)
+    args = ArgsListOption(
+        help=(f"Global `javac` compiler flags, e.g. `--{options_scope}-args='-g -deprecation'`."),
+    )
