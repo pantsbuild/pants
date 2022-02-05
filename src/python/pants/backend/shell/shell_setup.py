214290b15428eb415a8db6956a82208fa4a3a935
Joshua Cannon <joshdcannon@gmail.com>
2022-02-05 06:55:03 -0600

    shell+terraform

--- src/python/pants/backend/shell/shell_setup.py
+++ src/python/pants/backend/shell/shell_setup.py
@@ -4,9 +4,9 @@
 from __future__ import annotations
 
 import os
-from typing import cast
 
 from pants.engine.environment import Environment
+from pants.option.option_types import BoolOption, StrListOption
 from pants.option.subsystem import Subsystem
 from pants.util.memo import memoized_method
 from pants.util.ordered_set import OrderedSet
@@ -16,35 +16,29 @@ class ShellSetup(Subsystem):
     options_scope = "shell-setup"
     help = "Options for Pants's Shell support."
 
-    @classmethod
-    def register_options(cls, register):
-        super().register_options(register)
-        register(
+    _executable_search_path = (
+        StrListOption(
             "--executable-search-paths",
-            advanced=True,
-            type=list,
             default=["<PATH>"],
-            metavar="<binary-paths>",
             help=(
                 "The PATH value that will be used to find shells and to run certain processes "
                 "like the shunit2 test runner.\n\n"
                 'The special string "<PATH>" will expand to the contents of the PATH env var.'
             ),
         )
-        register(
-            "--dependency-inference",
-            advanced=True,
-            type=bool,
-            default=True,
-            help=(
-                "Infer Shell dependencies on other Shell files by analyzing `source` statements."
-            ),
-        )
+        .advanced()
+        .metavar("<binary-paths>")
+    )
+    dependency_inference = BoolOption(
+        "--dependency-inference",
+        default=True,
+        help=("Infer Shell dependencies on other Shell files by analyzing `source` statements."),
+    ).advanced()
 
     @memoized_method
     def executable_search_path(self, env: Environment) -> tuple[str, ...]:
         def iter_path_entries():
-            for entry in self.options.executable_search_paths:
+            for entry in self._executable_search_path:
                 if entry == "<PATH>":
                     path = env.get("PATH")
                     if path:
@@ -53,7 +47,3 @@ class ShellSetup(Subsystem):
                     yield entry
 
         return tuple(OrderedSet(iter_path_entries()))
-
-    @property
-    def dependency_inference(self) -> bool:
-        return cast(bool, self.options.dependency_inference)
