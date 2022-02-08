32cbfa6646bf7d6c9b546e9941cea20c82dc6722
Eric Arellano <ericarellano@me.com>
2022-02-08 09:12:58 -0700

    Remove deprecated `generate-user-lockfile` and `validate` goals and `sourcefile-validation` options scope
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/python/register.py
+++ src/python/pants/backend/experimental/python/register.py
@@ -1,17 +1,10 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.backend.experimental.python import user_lockfiles
-from pants.backend.python.goals import lockfile, publish
+from pants.backend.python.goals import publish
 from pants.backend.python.subsystems import twine
 from pants.backend.python.util_rules import pex
 
 
 def rules():
-    return (
-        *lockfile.rules(),
-        *pex.rules(),
-        *publish.rules(),
-        *twine.rules(),
-        *user_lockfiles.rules(),
-    )
+    return (*pex.rules(), *publish.rules(), *twine.rules())
