c6ad2b0165c82cda2a0bbafdba33877ee5f0c6ec
Joshua Cannon <joshdcannon@gmail.com>
2022-02-09 15:38:07 -0600

    parametrize

--- tests/python/pants_test/integration/bin_name_integration_test.py
+++ tests/python/pants_test/integration/bin_name_integration_test.py
@@ -1,17 +1,16 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import pytest
+
 from pants.testutil.pants_integration_test import run_pants
 
 
-def test_pants_bin_name() -> None:
-    def ask_for_help(use_pantsd: bool) -> str:
-        pants_run = run_pants(
-            ["--pants-bin-name='./pantsV2'", "help", "target"],
-            use_pantsd=use_pantsd,
-        )
-        pants_run.assert_success()
-        return pants_run.stdout
-
-    assert "Use `'./pantsV2' list --documented ::`" in ask_for_help(False)
-    assert "Use `'./pantsV2' list --documented ::`" in ask_for_help(True)
+@pytest.mark.parametrize("use_pantsd", [True, False])
+def test_pants_bin_name(use_pantsd) -> None:
+    pants_run = run_pants(
+        ["--pants-bin-name='./pantsV2'", "help", "target"],
+        use_pantsd=use_pantsd,
+    )
+    pants_run.assert_success()
+    assert "Use `'./pantsV2' list --documented ::`" in pants_run.stdout
