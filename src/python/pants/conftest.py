b45ed786e24117ab3af26083535f079ed01102c7
Joshua Cannon <joshdcannon@gmail.com>
2022-02-09 15:32:16 -0600

    Fix how we test

--- src/python/pants/conftest.py
+++ src/python/pants/conftest.py
@@ -1,7 +1,6 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import os
 from pathlib import Path
 
 # The top-level `pants` module must be a namespace package, because we build two dists from it
@@ -29,10 +28,3 @@ from pathlib import Path
 # source root (src/python). We only need `pants` to be a namespace package in the dists we create.
 
 Path("src/python/pants/__init__.py").touch()
-
-
-def pytest_sessionstart(session) -> None:
-    # This is needed for `pants.util.docutil.pants_bin`, and needs to be defined very early in
-    # pytests' lifecycle as `pants_bin` is used at import time to define static help strings.
-    # (E.g. can't use an autouse, session-scoped fixture)
-    os.environ["PANTS_BIN_NAME"] = "./pants"
