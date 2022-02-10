ecd75a7d7caea5ce946d1af7befbd1664948ffa5
Tom Dyas <tdyas@toolchain.com>
2022-02-10 18:26:52 -0500

    fix test
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/scala/resolve/lockfile_test.py
+++ src/python/pants/backend/scala/resolve/lockfile_test.py
@@ -5,17 +5,24 @@ from __future__ import annotations
 
 import pytest
 
+from pants.backend.scala import target_types
 from pants.backend.scala.dependency_inference import rules as scala_dep_inf_rules
 from pants.backend.scala.resolve.lockfile import rules as scala_lockfile_rules
-from pants.core.goals.generate_lockfiles import GenerateLockfileResult
+from pants.backend.scala.target_types import ScalaSourcesGeneratorTarget, ScalaSourceTarget
+from pants.core.goals.generate_lockfiles import GenerateLockfileResult, UserGenerateLockfiles
 from pants.core.util_rules import archive, source_files
 from pants.core.util_rules.external_tool import rules as external_tool_rules
 from pants.engine.fs import DigestContents, FileDigest
-from pants.engine.internals import graph
+from pants.engine.internals import build_files, graph
 from pants.jvm import jdk_rules
 from pants.jvm.goals import lockfile
-from pants.jvm.goals.lockfile import RequestedJVMserResolveNames
-from pants.jvm.resolve.common import ArtifactRequirements, Coordinate, Coordinates
+from pants.jvm.goals.lockfile import GenerateJvmLockfile, RequestedJVMserResolveNames
+from pants.jvm.resolve.common import (
+    ArtifactRequirement,
+    ArtifactRequirements,
+    Coordinate,
+    Coordinates,
+)
 from pants.jvm.resolve.coursier_fetch import CoursierLockfileEntry, CoursierResolvedLockfile
 from pants.jvm.resolve.coursier_fetch import rules as coursier_fetch_rules
 from pants.jvm.resolve.coursier_setup import rules as coursier_setup_rules
@@ -43,12 +50,20 @@ def rule_runner() -> RuleRunner:
             *util_rules(),
             *archive.rules(),
             *graph.rules(),
-            # QueryRule(UserGenerateLockfiles, [RequestedJVMserResolveNames]),
-            QueryRule(GenerateLockfileResult, [RequestedJVMserResolveNames]),
+            *build_files.rules(),
+            *target_types.rules(),
+            QueryRule(UserGenerateLockfiles, (RequestedJVMserResolveNames,)),
+            QueryRule(GenerateLockfileResult, (GenerateJvmLockfile,)),
         ],
-        target_types=[JvmArtifactTarget],
+        target_types=[JvmArtifactTarget, ScalaSourceTarget, ScalaSourcesGeneratorTarget],
     )
-    rule_runner.set_options(['--scala-version-for-resolve={"foo":"2.13.8"}'], env_inherit={"PATH"})
+    rule_runner.set_options(
+        [
+            '--scala-version-for-resolve={"foo":"2.13.8"}',
+            '--jvm-resolves={"foo": "foo/foo.lock"}',
+        ],
+        env_inherit={"PATH"},
+    )
     return rule_runner
 
 
@@ -56,16 +71,22 @@ def rule_runner() -> RuleRunner:
 def test_scala_library_added_to_resolve(rule_runner: RuleRunner) -> None:
     rule_runner.write_files(
         {
-            "foo/BUILD": "scala_sources(resolve='foo')",
+            "foo/BUILD": "scala_sources(compatible_resolves=['foo'])",
             "foo/Foo.scala": "package foo",
         }
     )
-    artifacts = ArtifactRequirements()
+
     result = rule_runner.request(
-        GenerateLockfileResult,
+        UserGenerateLockfiles,
         [RequestedJVMserResolveNames(["foo"])],
     )
-    digest_contents = rule_runner.request(DigestContents, [result.digest])
+    assert len(result) == 1
+    user_gen_lockfile = result[0]
+    assert isinstance(user_gen_lockfile, GenerateJvmLockfile)
+
+    lockfile_result = rule_runner.request(GenerateLockfileResult, [user_gen_lockfile])
+
+    digest_contents = rule_runner.request(DigestContents, [lockfile_result.digest])
     assert len(digest_contents) == 1
 
     expected = CoursierResolvedLockfile(
@@ -76,15 +97,27 @@ def test_scala_library_added_to_resolve(rule_runner: RuleRunner) -> None:
                     artifact="scala-library",
                     version="2.13.8",
                 ),
-                file_name="",
+                file_name="org.scala-lang_scala-library_2.13.8.jar",
                 direct_dependencies=Coordinates([]),
                 dependencies=Coordinates([]),
                 file_digest=FileDigest(
-                    fingerprint="66fdef91e9739348df7a096aa384a5685f4e875584cce89386a7a47251c4d8e9",
-                    serialized_bytes_length=45024,
+                    fingerprint="a0882b82514190c2bac7d1a459872a75f005fc0f3e88b2bc0390367146e35db7",
+                    serialized_bytes_length=6003601,
                 ),
             ),
         ),
-        metadata=JVMLockfileMetadata.new(artifacts),
+        metadata=JVMLockfileMetadata.new(
+            ArtifactRequirements(
+                [
+                    ArtifactRequirement(
+                        coordinate=Coordinate(
+                            group="org.scala-lang",
+                            artifact="scala-library",
+                            version="2.13.8",
+                        )
+                    ),
+                ]
+            )
+        ),
     )
     assert CoursierResolvedLockfile.from_serialized(digest_contents[0].content) == expected
