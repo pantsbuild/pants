ed5ea478b504f123b371f54a8fe5a5aea0dd08ce
Joshua Cannon <joshdcannon@gmail.com>
2022-02-10 14:22:57 -0600

    Add DictOption

--- src/python/pants/option/option_types.py
+++ src/python/pants/option/option_types.py
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
 
 _PropType = TypeVar("_PropType")
 _EnumT = TypeVar("_EnumT", bound=Enum)
-_ValueType = TypeVar("_ValueType")
+_ValueT = TypeVar("_ValueT")
 # NB: We don't provide constraints, as our `XListOption` types act like a set of contraints
 _ListMemberType = TypeVar("_ListMemberType")
 
@@ -286,6 +286,42 @@ class EnumOption(_OptionBase[_PropType], Generic[_PropType]):
         return super().__new__(
             cls, *flag_names, default=default, option_type=option_type, help=help
         )
+
+class DictOption(_OptionBase['dict[str, _ValueT]'], Generic[_ValueT]):
+    """A dictionary option mapping strings to client-provided `_ValueT`.
+
+    If you provide a `default` parameter, the `_ValueT` type parameter will be inferred from the
+    type of the values in the default. Otherwise, you'll need to provide `_ValueT` if you want a
+    non-`Any` type.
+
+    E.g.
+        # Explicit
+        DictOption[str](...)  # property type is `dict[str, str]`
+        DictOption[Any](..., default=dict(key="val"))  # property type is `dict[str, Any]`
+        # Implicit
+        DictOption(...)  # property type is `dict[str, Any]`
+        DictOption(..., default={"key": "val"})  # property type is `dict[str, str]`
+        DictOption(..., default=dict(key="val"))  # property type is `dict[str, str]`
+        DictOption(..., default=dict(key=1))  # property type is `dict[str, int]`
+        DictOption(..., default=dict(key1=1, key2="str"))  # property type is `dict[str, Any]`
+
+    NOTE: The property returns a mutable object. Care should be used to not mutate the object.
+    NOTE: Dictionary values are simply returned as parsed, and are not guaranteed to be of the
+    `_ValueT` specified.
+    """
+
+    option_type: Any = dict
+
+    def __new__(cls, *flag_names, default: dict[str, _ValueT] | None = None, help):
+        return super().__new__(
+            cls,
+            *flag_names,
+            default=default or {},
+            help=help,
+        )
+
+    def _convert_(self, val: Any) -> dict[str, _ValueT]:
+        return cast('dict[_ValueT]', val)
 
 
 class TargetOption(StrOption):
--- src/python/pants/option/option_types_test.py
+++ src/python/pants/option/option_types_test.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 from enum import Enum
 from types import SimpleNamespace
+from typing import Any
 from unittest.mock import Mock, call
 
 import pytest
@@ -14,6 +15,7 @@ from pants.option.option_types import (
     ArgsListOption,
     BoolListOption,
     BoolOption,
+    DictOption,
     DirListOption,
     DirOption,
     EnumListOption,
@@ -51,6 +53,7 @@ def test_option_typeclasses() -> None:
             self.options.float_opt = 1.0
             self.options.bool_opt = False
             self.options.enum_opt = MyEnum.Val1
+            self.options.dict_opt = {"a key": "a val"}
             self.options.target_opt = ""
             self.options.dir_opt = "."
             self.options.file_opt = "."
@@ -100,6 +103,7 @@ def test_option_typeclasses() -> None:
         float_prop = FloatOption("--float-opt", default=0.0, help="")
         bool_prop = BoolOption("--bool-opt", default=True, help="")
         enum_prop = EnumOption("--enum-opt", default=MyEnum.Val1, help="")
+        dict_prop = DictOption("--dict-opt", default={"key": "val"}, help="")
         target_prop = TargetOption("--target-opt", default="a str", help="")
         dir_prop = DirOption("--dir-opt", default="a str", help="")
         file_prop = FileOption("--file-opt", default="a str", help="")
@@ -159,6 +163,7 @@ def test_option_typeclasses() -> None:
         call("--float-opt", type=float, default=0.0, help=""),
         call("--bool-opt", type=bool, default=True, help=""),
         call("--enum-opt", type=MyEnum, default=MyEnum.Val1, help=""),
+        call("--dict-opt", type=dict, default={"key": "val"}, help=""),
         call("--target-opt", type=target_option, default="a str", help=""),
         call("--dir-opt", type=dir_option, default="a str", help=""),
         call("--file-opt", type=file_option, default="a str", help=""),
@@ -225,6 +230,7 @@ def test_option_typeclasses() -> None:
     assert my_subsystem.float_prop == 1.0
     assert not my_subsystem.bool_prop
     assert my_subsystem.enum_prop == MyEnum.Val1
+    assert my_subsystem.dict_prop == {"a key": "a val"}
     assert my_subsystem.target_prop == ""
     assert my_subsystem.dir_prop == "."
     assert my_subsystem.file_prop == "."
@@ -278,6 +284,7 @@ def test_option_typeclasses() -> None:
     var_float: float = my_subsystem.float_prop  # noqa: F841
     var_bool: bool = my_subsystem.bool_prop  # noqa: F841
     var_enum: MyEnum = my_subsystem.enum_prop  # noqa: F841
+    var_dict: dict[str, str] = my_subsystem.dict_prop  # noqa: F841
 
     var_target: str = my_subsystem.target_prop  # noqa: F841
     var_dir: str = my_subsystem.dir_prop  # noqa: F841
@@ -371,3 +378,27 @@ def test_subsystem_option_ordering() -> None:
         call("--b", type=str, default=None, help=""),
         call("--a", type=str, default=None, help=""),
     ]
+
+def test_dict_option_valuetype() -> None:
+    class MySubsystem(Subsystem):
+        def __init__(self):
+            self.options = SimpleNamespace()
+            self.options.opt = None
+
+        d1 = DictOption[str]("--opt", help="")
+        d2 = DictOption[Any]("--opt", default=dict(key="val"), help="")
+        # mypy correctly complains about needing a type annotation
+        d3 = DictOption("--opt", help="")  # type: ignore[var-annotated]
+        d4 = DictOption("--opt", default={"key": "val"}, help="")
+        d5 = DictOption("--opt", default=dict(key="val"), help="")
+        d6 = DictOption("--opt", default=dict(key=1), help="")
+        d7 = DictOption("--opt", default=dict(key1=1, key2="str"), help="")
+
+    my_subsystem = MySubsystem()
+    d1: dict[str, str] = my_subsystem.d1  # noqa: F841
+    d2: dict[str, Any] = my_subsystem.d2  # noqa: F841
+    d3: dict[str, Any] = my_subsystem.d3  # noqa: F841
+    d4: dict[str, str] = my_subsystem.d4  # noqa: F841
+    d5: dict[str, str] = my_subsystem.d5  # noqa: F841
+    d6: dict[str, int] = my_subsystem.d6  # noqa: F841
+    d7: dict[str, Any] = my_subsystem.d7  # noqa: F841
