294591ffbcbd4565e3d6107e163f967ce5daa4ea
Joshua Cannon <joshdcannon@gmail.com>
2022-02-10 19:49:54 -0600

    More Options V2!
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/scala/subsystems/scala.py
+++ src/python/pants/backend/scala/subsystems/scala.py
@@ -4,9 +4,8 @@
 from __future__ import annotations
 
 import logging
-from typing import cast
 
-from pants.option.option_types import StrOption
+from pants.option.option_types import DictOption, StrOption
 from pants.option.subsystem import Subsystem
 
 DEFAULT_SCALA_VERSION = "2.13.6"
@@ -29,27 +28,18 @@ class ScalaSubsystem(Subsystem):
         ),
     ).deprecated(removal_version="2.11.0.dev0", hint="Use `[scala].version_for_resolve` instead.")
 
-    @classmethod
-    def register_options(cls, register):
-        super().register_options(register)
-
-        register(
-            "--version-for-resolve",
-            type=dict,
-            help=(
-                "A dictionary mapping the name of a resolve to the Scala version to use for all Scala "
-                "targets consuming that resolve.\n\n"
-                'All Scala-compiled jars on a resolve\'s classpath must be "compatible" with one another and '
-                "with all Scala-compiled first-party sources from `scala_sources` (and other Scala target types) "
-                "using that resolve. The option sets the Scala version that will be used to compile all "
-                "first-party sources using the resolve. This ensures that the compatibility property is "
-                "maintained for a resolve. To support multiple Scala versions, use multiple resolves."
-            ),
-        )
-
-    @property
-    def _version_for_resolve(self) -> dict[str, str]:
-        return cast("dict[str, str]", self.options.version_for_resolve)
+    _version_for_resolve = DictOption[str](
+        "--version-for-resolve",
+        help=(
+            "A dictionary mapping the name of a resolve to the Scala version to use for all Scala "
+            "targets consuming that resolve.\n\n"
+            'All Scala-compiled jars on a resolve\'s classpath must be "compatible" with one another and '
+            "with all Scala-compiled first-party sources from `scala_sources` (and other Scala target types) "
+            "using that resolve. The option sets the Scala version that will be used to compile all "
+            "first-party sources using the resolve. This ensures that the compatibility property is "
+            "maintained for a resolve. To support multiple Scala versions, use multiple resolves."
+        ),
+    )
 
     def version_for_resolve(self, resolve: str) -> str:
         version = self._version_for_resolve.get(resolve)
--- src/python/pants/jvm/subsystems.py
+++ src/python/pants/jvm/subsystems.py
@@ -3,10 +3,9 @@
 
 from __future__ import annotations
 
-from typing import cast
-
 from pants.engine.target import InvalidFieldException, Target
 from pants.jvm.target_types import JvmCompatibleResolvesField, JvmResolveField
+from pants.option.option_types import DictOption, StrListOption, StrOption
 from pants.option.subsystem import Subsystem
 
 
@@ -14,65 +13,40 @@ class JvmSubsystem(Subsystem):
     options_scope = "jvm"
     help = "Options for general JVM functionality."
 
-    @classmethod
-    def register_options(cls, register):
-        super().register_options(register)
-        register(
-            "--jdk",
-            default="adopt:1.11",
-            advanced=True,
-            help=(
-                "The JDK to use.\n\n"
-                " This string will be passed directly to Coursier's `--jvm` parameter."
-                " Run `cs java --available` to see a list of available JVM versions on your platform.\n\n"
-                " If the string 'system' is passed, Coursier's `--system-jvm` option will be used"
-                " instead, but note that this can lead to inconsistent behavior since the JVM version"
-                " will be whatever happens to be found first on the system's PATH."
-            ),
-        )
-        register(
-            "--resolves",
-            type=dict,
-            default={"jvm-default": "3rdparty/jvm/default.lock"},
-            # TODO: expand help message
-            help="A dictionary mapping resolve names to the path of their lockfile.",
-        )
-        register(
-            "--default-resolve",
-            type=str,
-            default="jvm-default",
-            help=(
-                "The default value used for the `resolve` and `compatible_resolves` fields.\n\n"
-                "The name must be defined as a resolve in `[jvm].resolves`."
-            ),
-        )
-        register(
-            "--debug-args",
-            type=list,
-            member_type=str,
-            default=[],
-            help=(
-                "Extra JVM arguments to use when running tests in debug mode.\n\n"
-                "For example, if you want to attach a remote debugger, use something like "
-                "['-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005']"
-            ),
-        )
-
-    @property
-    def jdk(self) -> str:
-        return cast(str, self.options.jdk)
-
-    @property
-    def resolves(self) -> dict[str, str]:
-        return cast("dict[str, str]", dict(self.options.resolves))
-
-    @property
-    def default_resolve(self) -> str:
-        return cast(str, self.options.default_resolve)
-
-    @property
-    def debug_args(self) -> tuple[str, ...]:
-        return cast("tuple[str, ...]", tuple(self.options.debug_args))
+    jdk = StrOption(
+        "--jdk",
+        default="adopt:1.11",
+        help=(
+            "The JDK to use.\n\n"
+            " This string will be passed directly to Coursier's `--jvm` parameter."
+            " Run `cs java --available` to see a list of available JVM versions on your platform.\n\n"
+            " If the string 'system' is passed, Coursier's `--system-jvm` option will be used"
+            " instead, but note that this can lead to inconsistent behavior since the JVM version"
+            " will be whatever happens to be found first on the system's PATH."
+        ),
+    ).advanced()
+    resolves = DictOption(
+        "--resolves",
+        default={"jvm-default": "3rdparty/jvm/default.lock"},
+        # TODO: expand help message
+        help="A dictionary mapping resolve names to the path of their lockfile.",
+    )
+    default_resolve = StrOption(
+        "--default-resolve",
+        default="jvm-default",
+        help=(
+            "The default value used for the `resolve` and `compatible_resolves` fields.\n\n"
+            "The name must be defined as a resolve in `[jvm].resolves`."
+        ),
+    )
+    debug_args = StrListOption(
+        "--debug-args",
+        help=(
+            "Extra JVM arguments to use when running tests in debug mode.\n\n"
+            "For example, if you want to attach a remote debugger, use something like "
+            "['-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005']"
+        ),
+    )
 
     def is_jvm_target(self, target: Target) -> bool:
         """Returns `True` only if the given target is a valid JVM source or destination target."""
