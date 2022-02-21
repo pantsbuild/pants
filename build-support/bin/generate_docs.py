70ee4f6746bba020db17db4cff15080602ab5cc8
Andreas Stenius <andreas.stenius@svenskaspel.se>
2022-02-21 21:34:37 +0100

    tweaks.
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]

--- build-support/bin/generate_docs.py
+++ build-support/bin/generate_docs.py
@@ -214,6 +214,7 @@ def run_pants_help_all() -> dict[str, Any]:
         "pants.backend.experimental.python.lint.pyupgrade",
         "pants.backend.experimental.scala",
         "pants.backend.experimental.scala.lint.scalafmt",
+        "pants.backend.experimental.shell",
         "pants.backend.google_cloud_function.python",
         "pants.backend.plugin_development",
         "pants.backend.python",
