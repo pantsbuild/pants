4ec790179caaa1bea34ed1c23233452522b04806
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2022-02-22 21:30:42 -0700

    Fix PyOxidizer depending on `python_distribution` with `sdist=True` (#14394)
    This also contains several changes to make the backend more idiomatic.
    
    Some planned follow-ups:
    
    - Figure out the story for multiple `python_distribution`s, especially if you depend on one that depends on another one.
    - Implement https://github.com/pantsbuild/pants/issues/14565
    
    [ci skip-rust]
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/python/packaging/pyoxidizer/subsystem.py
+++ src/python/pants/backend/python/packaging/pyoxidizer/subsystem.py
@@ -1,17 +1,18 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from typing import Tuple
-
 from pants.backend.python.subsystems.python_tool_base import PythonToolBase
 from pants.backend.python.target_types import ConsoleScript
-from pants.engine.rules import collect_rules
-from pants.option.custom_types import shell_str
+from pants.option.option_types import ArgsListOption
 
 
 class PyOxidizer(PythonToolBase):
     options_scope = "pyoxidizer"
-    help = """The PyOxidizer utility for packaging Python code in a Rust binary (https://pyoxidizer.readthedocs.io/en/stable/pyoxidizer.html)."""
+    help = (
+        "The PyOxidizer utility for packaging Python code in a Rust binary "
+        "(https://pyoxidizer.readthedocs.io/en/stable/pyoxidizer.html).\n\n"
+        "Used with the `pyoxidizer_binary` target."
+    )
 
     default_version = "pyoxidizer==0.18.0"
     default_main = ConsoleScript("pyoxidizer")
@@ -19,23 +20,9 @@ class PyOxidizer(PythonToolBase):
     register_interpreter_constraints = True
     default_interpreter_constraints = ["CPython>=3.8"]
 
-    @classmethod
-    def register_options(cls, register):
-        super().register_options(register)
-        register(
-            "--args",
-            type=list,
-            member_type=shell_str,
-            help=(
-                "Arguments to pass directly to PyOxidizer, e.g. "
-                f'`--{cls.options_scope}-args="--release"`'
-            ),
-        )
-
-    @property
-    def args(self) -> Tuple[str, ...]:
-        return tuple(self.options.args)
-
-
-def rules():
-    return (*collect_rules(),)
+    args = ArgsListOption(
+        help=(
+            "Arguments to pass directly to PyOxidizer, e.g. "
+            f'`--{options_scope}-args="--release"`'
+        ),
+    )
