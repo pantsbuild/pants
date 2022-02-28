7d3eeb0dc3ea8d6b2d56c2319cc91d2ba236916c
Tom Dyas <tdyas@toolchain.com>
2022-02-28 14:36:23 -0500

    checkpoint
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/bsp/util_rules/compile.py
+++ src/python/pants/bsp/util_rules/compile.py
@@ -1,11 +1,12 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+from dataclasses import dataclass
 
 import time
 
 from pants.bsp.context import BSPContext
 from pants.bsp.protocol import BSPHandlerMapping
-from pants.bsp.spec.base import StatusCode, TaskId
+from pants.bsp.spec.base import StatusCode, TaskId, BuildTargetIdentifier
 from pants.bsp.spec.compile import CompileParams, CompileReport, CompileResult, CompileTask
 
 # -----------------------------------------------------------------------------------------------
@@ -13,8 +14,25 @@ from pants.bsp.spec.compile import CompileParams, CompileReport, CompileResult, 
 # See https://build-server-protocol.github.io/docs/specification.html#compile-request
 # -----------------------------------------------------------------------------------------------
 from pants.bsp.spec.task import TaskFinishParams, TaskStartParams
+from pants.engine.internals.native_engine import Digest
 from pants.engine.rules import collect_rules, rule
-from pants.engine.unions import UnionRule
+from pants.engine.target import FieldSet
+from pants.engine.unions import UnionRule, union
+
+
+@union
+@dataclass(frozen=True)
+class BSPCompileFieldSet(FieldSet):
+    """FieldSet used to hook BSP compilation."""
+    bsp_target_id: BuildTargetIdentifier
+
+
+@dataclass(frozen=True)
+class BSPCompileResult:
+    """Result of compilation of a target capable of target compilation."""
+
+    bsp_target_id: BuildTargetIdentifier
+    output_digest: Digest
 
 
 class CompileRequestHandlerMapping(BSPHandlerMapping):
