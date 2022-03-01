4cefb2b6af0f18d57206d8f9d26f96ce5a59ae5a
Antonio Alonso Dominguez <alonso.domin@pm.me>
2022-03-01 17:35:25 +0100

    Fix linting issues in register.py

--- src/python/pants/backend/experimental/helm/register.py
+++ src/python/pants/backend/experimental/helm/register.py
@@ -1,10 +1,14 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from pants.backend.helm.goals import lint
 from pants.backend.helm.target_types import HelmChartTarget
+from pants.backend.helm.util_rules import chart, sources, tool
+
 
 def target_types():
-  return [HelmChartTarget]
+    return [HelmChartTarget]
+
 
 def rules():
-  return []
+    return [*chart.rules(), *lint.rules(), *sources.rules(), *tool.rules()]
