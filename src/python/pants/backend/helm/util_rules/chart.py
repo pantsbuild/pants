48aea2eaa435c0bdc44bc08ca110aff1d25fc708
Antonio Alonso Dominguez <alonso.domin@pm.me>
2022-03-01 10:47:58 +0100

    Implement lint goal

--- src/python/pants/backend/helm/util_rules/chart.py
+++ src/python/pants/backend/helm/util_rules/chart.py
@@ -50,6 +50,8 @@ class HelmChart:
     snapshot: Snapshot
     artifact: ResolvedHelmArtifact | None = None
 
+    lint_strict: bool | None
+
     @property
     def path(self) -> str:
         return self.metadata.name
