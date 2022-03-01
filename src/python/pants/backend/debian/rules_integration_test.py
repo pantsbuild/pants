15e7dd7104b72317ffce34009c1acb1d9c2a6942
Alexey Tereshenkov <alexey.tereshenkov@oxbotica.com>
2022-03-01 15:35:51 +0000

    Add an integration test creating a Debian package

--- src/python/pants/backend/debian/rules_integration_test.py
+++ src/python/pants/backend/debian/rules_integration_test.py
@@ -1,15 +1,20 @@
-# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+import os
+import subprocess
 from textwrap import dedent
 
 import pytest
 
-from pants.backend.debian.rules import package_debian_package
+from pants.backend.debian.rules import rules as debian_rules, DebianPackageFieldSet
 from pants.backend.debian.target_types import DebianPackage
 from pants.backend.python import target_types_rules
+from pants.build_graph.address import Address
+from pants.core.goals.package import BuiltPackage
 from pants.core.util_rules import source_files
 from pants.core.util_rules.source_files import SourceFilesRequest, SourceFiles
 from pants.engine.rules import QueryRule
+from pants.engine.target import Target
 from pants.testutil.rule_runner import RuleRunner
 
 
@@ -17,27 +22,36 @@ from pants.testutil.rule_runner import RuleRunner
 def rule_runner() -> RuleRunner:
     return RuleRunner(
         rules=[
-            *package_debian_package(),
+            *debian_rules(),
             *source_files.rules(),
             *target_types_rules.rules(),
             QueryRule(SourceFiles, (SourceFilesRequest,)),
+            QueryRule(BuiltPackage, (DebianPackageFieldSet,)),
         ],
         target_types=[DebianPackage],
     )
 
 
-def create_debian_package(rule_runner: RuleRunner) -> None:
+def build_package(rule_runner: RuleRunner, binary_target: Target) -> BuiltPackage:
+    field_set = DebianPackageFieldSet.create(binary_target)
+    result = rule_runner.request(BuiltPackage, [field_set])
+    rule_runner.write_digest(result.digest)
+    return result
+
+
+def test_create_debian_package(rule_runner: RuleRunner) -> None:
     rule_runner.write_files(
-        {"project/BUILD": dedent(
+        {"BUILD": dedent(
             """
             debian_package(
-                name='my-package',
-                description='Useful tools installed as Debian package.',                                
-                sources=['project/files/**/*'],
+                name='sample-debian-package',
+                description='Useful tools installed as a Debian package.',
+                packages=[],                                
+                sources=['project/**/*'],
             )
             """
         ),
-         "project/files/DEBIAN/control": dedent(
+         "project/DEBIAN/control": dedent(
             """
             Package: sample-debian-package
             Version: 1.0
@@ -50,7 +64,7 @@ def create_debian_package(rule_runner: RuleRunner) -> None:
             Description: A sample Debian package built with Pants.
             """
         ),
-         "project/files/platform.conf": dedent(
+         "project/opt/company/platform.conf": dedent(
             """
             Some configuration.
             """
@@ -58,5 +72,14 @@ def create_debian_package(rule_runner: RuleRunner) -> None:
         }
     )
 
-    # TODO(alexey): create a Debian package
+    binary_tgt = rule_runner.get_target(Address("", target_name="sample-debian-package"))
+    built_package = build_package(rule_runner, binary_tgt)
+    assert len(built_package.artifacts) == 1
+    assert built_package.artifacts[0].relpath == "sample-debian-package.deb"
+
+    # list the contents of a Debian package to see that a file was included
+    result = subprocess.run(["dpkg", "-c", os.path.join(rule_runner.build_root, "sample-debian-package.deb")],
+                            stdout=subprocess.PIPE)
+    assert result.returncode == 0
+    assert b"opt/company/platform.conf" in result.stdout
     return
