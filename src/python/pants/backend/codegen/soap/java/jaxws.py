b361c7eb7b3afe7c9e3b986271d4c62975c84cd2
Antonio Alonso Dominguez <alonso.domin@pm.me>
2022-03-02 14:59:22 +0100

    Update file headers

--- src/python/pants/backend/codegen/soap/java/jaxws.py
+++ src/python/pants/backend/codegen/soap/java/jaxws.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from pants.jvm.resolve.jvm_tool import JvmToolBase
