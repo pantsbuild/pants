ac4dd1e70536b56d18290540a30e34f13983ba07
Antonio Alonso Dominguez <alonso.domin@pm.me>
2022-03-03 09:50:03 +0100

    Improve `sources` tests by using target generators plus additional test cases

--- src/python/pants/backend/helm/util_rules/sources_test.py
+++ src/python/pants/backend/helm/util_rules/sources_test.py
@@ -17,7 +17,7 @@ from pants.backend.helm.testutil import (
 from pants.backend.helm.util_rules import sources
 from pants.backend.helm.util_rules.sources import HelmChartSourceFiles, HelmChartSourceFilesRequest
 from pants.build_graph.address import Address
-from pants.core.target_types import FileTarget, ResourceTarget
+from pants.core.target_types import FilesGeneratorTarget, ResourcesGeneratorTarget
 from pants.core.util_rules import stripped_source_files
 from pants.engine.rules import QueryRule
 from pants.testutil.rule_runner import RuleRunner
@@ -26,7 +26,7 @@ from pants.testutil.rule_runner import RuleRunner
 @pytest.fixture
 def rule_runner() -> RuleRunner:
     return RuleRunner(
-        target_types=[HelmChartTarget, ResourceTarget, FileTarget],
+        target_types=[HelmChartTarget, ResourcesGeneratorTarget, FilesGeneratorTarget],
         rules=[
             *sources.rules(),
             *stripped_source_files.rules(),
@@ -35,33 +35,111 @@ def rule_runner() -> RuleRunner:
     )
 
 
-def test_collects_resource_sources_dependencies(rule_runner: RuleRunner) -> None:
+def test_source_templates_are_always_included(rule_runner: RuleRunner) -> None:
     rule_runner.write_files(
         {
             "BUILD": dedent(
                 """\
-                helm_chart(name='mychart', dependencies=[':xml'])
-                resource(name="xml", source='xml_file.xml')
+                helm_chart(name='mychart', dependencies=[':resources', ':files'])
+                resources(name="resources", sources=['*.xml'])
+                files(name="files", sources=['*.txt'])
                 """
             ),
             "Chart.yaml": HELM_CHART_FILE,
             "values.yaml": HELM_VALUES_FILE,
             "templates/_helpers.tpl": HELM_TEMPLATE_HELPERS_FILE,
             "templates/service.yaml": K8S_SERVICE_FILE,
-            "xml_file.xml": "",
+            "resource.xml": "",
+            "file.txt": "",
         }
     )
 
     address = Address("", target_name="mychart")
     tgt = rule_runner.get_target(address)
     source_files = rule_runner.request(
-        HelmChartSourceFiles, [HelmChartSourceFilesRequest.create(tgt)]
+        HelmChartSourceFiles,
+        [
+            HelmChartSourceFilesRequest.create(
+                tgt, include_metadata=False, include_resources=False, include_files=False
+            )
+        ],
     )
 
-    assert "xml_file.xml" in source_files.snapshot.files
+    assert "Chart.yaml" not in source_files.snapshot.files
+    assert "values.yaml" in source_files.snapshot.files
+    assert "templates/_helpers.tpl" in source_files.snapshot.files
+    assert "templates/service.yaml" in source_files.snapshot.files
+    assert "included.xml" not in source_files.snapshot.files
+    assert "ignored.txt" not in source_files.snapshot.files
 
 
-def test_skip_collecting_metadata_file(rule_runner: RuleRunner) -> None:
+def test_resource_sources_dependencies(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(
+        {
+            "BUILD": dedent(
+                """\
+                helm_chart(name='mychart', dependencies=[':resources', ':files'])
+                resources(name="resources", sources=['*.xml'])
+                files(name="files", sources=['*.txt'])
+                """
+            ),
+            "Chart.yaml": HELM_CHART_FILE,
+            "values.yaml": HELM_VALUES_FILE,
+            "templates/_helpers.tpl": HELM_TEMPLATE_HELPERS_FILE,
+            "templates/service.yaml": K8S_SERVICE_FILE,
+            "included.xml": "",
+            "ignored.txt": "",
+        }
+    )
+
+    address = Address("", target_name="mychart")
+    tgt = rule_runner.get_target(address)
+    source_files = rule_runner.request(
+        HelmChartSourceFiles,
+        [
+            HelmChartSourceFilesRequest.create(
+                tgt, include_metadata=False, include_resources=True, include_files=False
+            )
+        ],
+    )
+
+    assert "included.xml" in source_files.snapshot.files
+
+
+def test_collects_files_sources_dependencies(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(
+        {
+            "BUILD": dedent(
+                """\
+                helm_chart(name='mychart', dependencies=[':resources', ':files'])
+                resources(name="resources", sources=['*.xml'])
+                files(name="files", sources=['*.txt'])
+                """
+            ),
+            "Chart.yaml": HELM_CHART_FILE,
+            "values.yaml": HELM_VALUES_FILE,
+            "templates/_helpers.tpl": HELM_TEMPLATE_HELPERS_FILE,
+            "templates/service.yaml": K8S_SERVICE_FILE,
+            "resource.xml": "",
+            "file.txt": "",
+        }
+    )
+
+    address = Address("", target_name="mychart")
+    tgt = rule_runner.get_target(address)
+    source_files = rule_runner.request(
+        HelmChartSourceFiles,
+        [
+            HelmChartSourceFilesRequest.create(
+                tgt, include_metadata=False, include_resources=False, include_files=True
+            )
+        ],
+    )
+
+    assert "file.txt" in source_files.snapshot.files
+
+
+def test_include_metadata_file(rule_runner: RuleRunner) -> None:
     rule_runner.write_files(
         {
             "BUILD": """helm_chart(name='mychart')""",
@@ -75,7 +153,12 @@ def test_skip_collecting_metadata_file(rule_runner: RuleRunner) -> None:
     address = Address("", target_name="mychart")
     tgt = rule_runner.get_target(address)
     source_files = rule_runner.request(
-        HelmChartSourceFiles, [HelmChartSourceFilesRequest.create(tgt, include_metadata=False)]
+        HelmChartSourceFiles,
+        [
+            HelmChartSourceFilesRequest.create(
+                tgt, include_metadata=True, include_files=False, include_resources=False
+            )
+        ],
     )
 
-    assert "Chart.yaml" not in source_files.snapshot.files
+    assert "Chart.yaml" in source_files.snapshot.files
