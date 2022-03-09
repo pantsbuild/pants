ecf6d72f1c56db33b3da4cb5a4ef010bf774260e
Antonio Alonso Dominguez <alonso.domin@pm.me>
2022-03-09 12:42:55 +0100

    Initial implementation of unittest plugin support

--- src/python/pants/backend/helm/target_types.py
+++ src/python/pants/backend/helm/target_types.py
@@ -259,7 +259,6 @@ def all_helm_unittest_test_targets(all_targets: AllTargets) -> AllHelmUnitTestTe
 # `helm_unittest_tests` target generator
 # -----------------------------------------------------------------------------------------------
 
-
 class HelmUnitTestGeneratingSourcesField(MultipleSourcesField):
     default = ("*_test.yaml",)
     expected_file_extensions = (
