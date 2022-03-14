7078c9a7b2a4ec24da60399a011fd5b30d61be6e
Antonio Alonso Dominguez <alonso.domin@pm.me>
2022-03-14 13:29:27 +0100

    Implement dependency inference for helm unittests

--- src/python/pants/backend/helm/target_types.py
+++ src/python/pants/backend/helm/target_types.py
@@ -261,6 +261,7 @@ def all_helm_unittest_test_targets(all_targets: AllTargets) -> AllHelmUnitTestTe
 # `helm_unittest_tests` target generator
 # -----------------------------------------------------------------------------------------------
 
+
 class HelmUnitTestGeneratingSourcesField(MultipleSourcesField):
     default = ("*_test.yaml",)
     expected_file_extensions = (
