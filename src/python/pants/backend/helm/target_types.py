e046668c20f323662249d52ceda9b25df2db2bf6
A. Alonso Dominguez <2269440+alonsodomin@users.noreply.github.com>
2022-03-15 18:53:57 +0100

    Basic support for Helm charts (#14658)
    This is a follow up from the massive #14487 PR and the discussion on attempting to split it to make it much easier to review.
    
    The changes in here bring the foundational structures used in the previous PR to be able to handle Helm charts _as a single compilation unit_. To prove that this works fine in combination with the Helm commands, the `lint` goal is also implemented.
    
    Test coverage has been improved in comparison to the same code in the previous PR, but the approach with the `helm_chart` target is the same: Instead of defining fine grained targets, the `helm_chart` target is quite coarse as it includes different types of files as its _sources_: the `Chart.yaml` file, the local `values.yaml` file (which brings the defaults), the `templates/*.tpl` files (go partial templates) and the `templates/*.yaml` files (the kuberentes manifest templates using Go template syntax).
    
    A different approach to this, and maybe one that is worth exploring, would be to have the following targets:
    
    `chart/templates/BUILD`:
    ```python
    helm_partial_templates(name="partials")  # Uses default sources="*.tpl"
    helm_kube_tempaltes(name="kube") # Uses default sources="*.yaml"
    ```
    
    `chart/BUILD`:
    ```python
     # Has two source fields:
    #  - chart="Chart.yaml"
    # - defaults="values.yaml"
    helm_chart(dependencies=["templates:partials", "templates:kube"])
    ```
    
    What I like a lot with this approach is that each target is very specific and can be configured very easily. What I mostly don't like is the explosion of targets if produces and having to set the `helm_chart` dependencies expicitly - although this could be mitigated with dependency injection/inference ...
    
    Let me know what you think of current approach VS the one mentioned here.

---
+++ src/python/pants/backend/helm/target_types.py
@@ -0,0 +1,104 @@
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+import os
+from dataclasses import dataclass
+from textwrap import dedent
+
+from pants.core.goals.package import OutputPathField
+from pants.engine.target import (
+    COMMON_TARGET_FIELDS,
+    BoolField,
+    Dependencies,
+    FieldSet,
+    MultipleSourcesField,
+    SingleSourceField,
+    Target,
+    TriBoolField,
+)
+from pants.util.docutil import bin_name
+
+# -----------------------------------------------------------------------------------------------
+# Generic commonly used fields
+# -----------------------------------------------------------------------------------------------
+
+
+class HelmSkipPushField(BoolField):
+    alias = "skip_push"
+    default = False
+    help = (
+        "If set to true, do not push this helm chart "
+        f"to registries when running `{bin_name()} publish`."
+    )
+
+
+# -----------------------------------------------------------------------------------------------
+# `helm_chart` target
+# -----------------------------------------------------------------------------------------------
+
+
+class HelmChartMetaSourceField(SingleSourceField):
+    alias = "chart"
+    default = "Chart.yaml"
+    expected_file_extensions = (
+        ".yaml",
+        ".yml",
+    )
+    required = False
+    help = "The chart definition file"
+
+
+class HelmChartSourcesField(MultipleSourcesField):
+    default = ("values.yaml", "templates/*.yaml", "templates/*.tpl")
+    expected_file_extensions = (".yaml", ".yml", ".tpl")
+
+
+class HelmChartDependenciesField(Dependencies):
+    pass
+
+
+class HelmChartOutputPathField(OutputPathField):
+    help = dedent(
+        """\
+        The destination folder where the final packaged chart will be located.\n
+        The final package name will still follow Helm convention, this output path will only affect the destination folder where can be found.
+        """
+    )
+
+    def value_or_default(self, *, file_ending: str | None) -> str:
+        if self.value:
+            return self.value
+        return os.path.join(self.address.spec_path.replace(os.sep, "."))
+
+
+class HelmChartLintStrictField(TriBoolField):
+    alias = "lint_strict"
+    help = "If set to true, enables strict linting of this Helm chart"
+
+
+class HelmChartTarget(Target):
+    alias = "helm_chart"
+    core_fields = (
+        *COMMON_TARGET_FIELDS,
+        HelmChartMetaSourceField,
+        HelmChartSourcesField,
+        HelmChartDependenciesField,
+        HelmChartOutputPathField,
+        HelmChartLintStrictField,
+        HelmSkipPushField,
+    )
+    help = "A Helm chart"
+
+
+@dataclass(frozen=True)
+class HelmChartFieldSet(FieldSet):
+    required_fields = (
+        HelmChartMetaSourceField,
+        HelmChartSourcesField,
+    )
+
+    chart: HelmChartMetaSourceField
+    sources: HelmChartSourcesField
+    dependencies: HelmChartDependenciesField
