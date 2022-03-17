38fa73cd60158385d99c54c32a7cbf483641324c
Eric Arellano <ericarellano@me.com>
2022-03-16 21:30:58 -0700

    Add `cwd()` symbol to BUILD files
    [ci skip-build-wheels]
    
    [ci skip-rust]

--- src/python/pants/engine/internals/parser.py
+++ src/python/pants/engine/internals/parser.py
@@ -107,7 +107,13 @@ class Parser:
                 parse_state.add(target_adaptor)
                 return target_adaptor
 
-        symbols: dict[str, Any] = dict(object_aliases.objects)
+        class Cwd:
+            """Lets you call `cwd()` in a BUILD file to get the relative path."""
+
+            def __call__(self) -> str:
+                return parse_state.rel_path()
+
+        symbols: dict[str, Any] = {**object_aliases.objects, "cwd": Cwd()}
         symbols.update(
             (alias, Registrar(alias))
             for alias in target_type_aliases
