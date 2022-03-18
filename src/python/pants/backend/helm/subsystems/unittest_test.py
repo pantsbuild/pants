4fcb6680eb1fbde8f76fc1a506da85a0d10ec9d9
Antonio Alonso Dominguez <alonso.domin@pm.me>
2022-03-18 12:15:39 +0100

    Document the procedure used to test Helm plugins are properly installed

--- src/python/pants/backend/helm/subsystems/unittest_test.py
+++ src/python/pants/backend/helm/subsystems/unittest_test.py
@@ -8,6 +8,7 @@ import re
 import pytest
 
 from pants.backend.helm.subsystems import unittest
+from pants.backend.helm.subsystems.unittest import HelmUnitTestSubsystem
 from pants.backend.helm.util_rules import tool
 from pants.backend.helm.util_rules.tool import HelmProcess
 from pants.core.util_rules import external_tool
@@ -39,7 +40,19 @@ def test_install_plugin(rule_runner: RuleRunner) -> None:
     )
 
     process_result = rule_runner.request(ProcessResult, [plugin_ls_process])
-    plugin_table = process_result.stdout.decode().splitlines()[1:]
-    loaded_plugins = [re.split(r"\t+", line.rstrip())[0] for line in plugin_table]
 
-    assert loaded_plugins == ["unittest"]
+    # The result of the `helm plugin ls` command is a table with a header like
+    #    NAME           VERSION DESCRIPTION
+    #    plugin_name    0.1.0   Some plugin description
+    #
+    # So to build the test expectation we parse that output and keeping
+    # the plugin's name and version to be used in the comparison
+    plugin_table_rows = process_result.stdout.decode().splitlines()[1:]
+    loaded_plugins = [
+        (columns[0].strip(), columns[1].strip())
+        for columns in (re.split(r"\t+", line.rstrip()) for line in plugin_table_rows)
+    ]
+
+    assert loaded_plugins == [
+        (HelmUnitTestSubsystem.plugin_name, HelmUnitTestSubsystem.default_version)
+    ]
