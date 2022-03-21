a12bbe2fed2bcc43f887b50d0f182f3d7f7bc5e9
Tom Dyas <tdyas@toolchain.com>
2022-03-21 18:17:58 -0400

    [internal] BSP: get third-party deps working for Scala (#14860)
    Get third-party dependencies working for Scala code. IntelliJ does not call the `buildTarget/dependencyModules` BSP endpoint, while VSCode Metals does call it. IntelliJ only looks at the `classpath` attribute returned by the `buildTarget/scalacOptions` endpoint.

--- src/python/pants/backend/scala/bsp/rules.py
+++ src/python/pants/backend/scala/bsp/rules.py
@@ -20,30 +20,54 @@ from pants.base.build_root import BuildRoot
 from pants.base.specs import AddressSpecs
 from pants.bsp.protocol import BSPHandlerMapping
 from pants.bsp.spec.base import BuildTarget, BuildTargetIdentifier, StatusCode
+from pants.bsp.spec.targets import DependencyModule
 from pants.bsp.util_rules.compile import BSPCompileFieldSet, BSPCompileResult
 from pants.bsp.util_rules.lifecycle import BSPLanguageSupport
 from pants.bsp.util_rules.targets import (
     BSPBuildTargetsMetadataRequest,
     BSPBuildTargetsMetadataResult,
     BSPBuildTargetsNew,
+    BSPDependencyModulesRequest,
+    BSPDependencyModulesResult,
 )
 from pants.engine.addresses import Addresses
-from pants.engine.fs import EMPTY_DIGEST, AddPrefix, CreateDigest, Digest, DigestEntries
+from pants.engine.fs import (
+    EMPTY_DIGEST,
+    AddPrefix,
+    CreateDigest,
+    Digest,
+    DigestEntries,
+    FileEntry,
+    Workspace,
+)
 from pants.engine.internals.native_engine import Snapshot
 from pants.engine.internals.selectors import Get, MultiGet
-from pants.engine.rules import collect_rules, rule
-from pants.engine.target import CoarsenedTargets, FieldSet, Target, Targets
+from pants.engine.rules import _uncacheable_rule, collect_rules, rule
+from pants.engine.target import (
+    CoarsenedTargets,
+    FieldSet,
+    Target,
+    Targets,
+    TransitiveTargets,
+    TransitiveTargetsRequest,
+)
 from pants.engine.unions import UnionRule
+from pants.jvm.bsp.spec import MavenDependencyModule, MavenDependencyModuleArtifact
 from pants.jvm.compile import (
     ClasspathEntryRequest,
     ClasspathEntryRequestFactory,
     FallibleClasspathEntry,
 )
-from pants.jvm.resolve.common import ArtifactRequirements, Coordinate
-from pants.jvm.resolve.coursier_fetch import ToolClasspath, ToolClasspathRequest
+from pants.jvm.resolve.common import ArtifactRequirement, ArtifactRequirements, Coordinate
+from pants.jvm.resolve.coursier_fetch import (
+    CoursierLockfileEntry,
+    CoursierResolvedLockfile,
+    ToolClasspath,
+    ToolClasspathRequest,
+)
 from pants.jvm.resolve.key import CoursierResolveKey
 from pants.jvm.subsystems import JvmSubsystem
-from pants.jvm.target_types import JvmResolveField
+from pants.jvm.target_types import JvmArtifactFieldSet, JvmResolveField
 
 LANGUAGE_ID = "scala"
 
@@ -179,11 +203,12 @@ class HandleScalacOptionsResult:
     item: ScalacOptionsItem
 
 
-@rule
+@_uncacheable_rule
 async def handle_bsp_scalac_options_request(
     request: HandleScalacOptionsRequest,
     build_root: BuildRoot,
     bsp_build_targets: BSPBuildTargetsNew,
+    workspace: Workspace,
 ) -> HandleScalacOptionsResult:
     bsp_target_name = request.bsp_target_id.uri[len("pants:") :]
     if bsp_target_name not in bsp_build_targets.targets_mapping:
@@ -195,12 +220,31 @@ async def handle_bsp_scalac_options_request(
     )
     coarsened_targets = await Get(CoarsenedTargets, Addresses(tgt.address for tgt in targets))
     resolve = await Get(CoursierResolveKey, CoarsenedTargets, coarsened_targets)
+    lockfile = await Get(CoursierResolvedLockfile, CoursierResolveKey, resolve)
+
+    resolve_digest = await Get(
+        Digest,
+        CreateDigest([FileEntry(entry.file_name, entry.file_digest) for entry in lockfile.entries]),
+    )
+
+    resolve_digest = await Get(
+        Digest, AddPrefix(resolve_digest, f"jvm/resolves/{resolve.name}/lib")
+    )
+
+    workspace.write_digest(resolve_digest, path_prefix=".pants.d/bsp")
+
+    classpath = [
+        build_root.pathlib_path.joinpath(
+            f".pants.d/bsp/jvm/resolves/{resolve.name}/lib/{entry.file_name}"
+        ).as_uri()
+        for entry in lockfile.entries
+    ]
 
     return HandleScalacOptionsResult(
         ScalacOptionsItem(
             target=request.bsp_target_id,
             options=(),
-            classpath=(),
+            classpath=tuple(classpath),
             class_directory=build_root.pathlib_path.joinpath(
                 f".pants.d/bsp/jvm/resolves/{resolve.name}/classes"
             ).as_uri(),
@@ -214,6 +258,102 @@ async def bsp_scalac_options_request(request: ScalacOptionsParams) -> ScalacOpti
         Get(HandleScalacOptionsResult, HandleScalacOptionsRequest(btgt)) for btgt in request.targets
     )
     return ScalacOptionsResult(items=tuple(result.item for result in results))
+
+
+# -----------------------------------------------------------------------------------------------
+# Dependency Modules
+# -----------------------------------------------------------------------------------------------
+
+
+@dataclass(frozen=True)
+class ScalaBSPDependencyModulesRequest(BSPDependencyModulesRequest):
+    field_set_type = ScalaMetadataFieldSet
+
+
+def get_entry_for_coord(
+    lockfile: CoursierResolvedLockfile, coord: Coordinate
+) -> CoursierLockfileEntry | None:
+    for entry in lockfile.entries:
+        if entry.coord == coord:
+            return entry
+    return None
+
+
+@rule
+async def scala_bsp_dependency_modules(
+    request: ScalaBSPDependencyModulesRequest,
+    build_root: BuildRoot,
+) -> BSPDependencyModulesResult:
+    coarsened_targets = await Get(
+        CoarsenedTargets, Addresses([fs.address for fs in request.field_sets])
+    )
+    resolve = await Get(CoursierResolveKey, CoarsenedTargets, coarsened_targets)
+    lockfile = await Get(CoursierResolvedLockfile, CoursierResolveKey, resolve)
+
+    # TODO: Can this use ClasspathEntryRequest?
+    transitive_targets = await Get(
+        TransitiveTargets,
+        TransitiveTargetsRequest(
+            roots=[
+                tgt.address
+                for coarsened_target in coarsened_targets
+                for tgt in coarsened_target.members
+            ]
+        ),
+    )
+
+    artifact_requirements = [
+        ArtifactRequirement.from_jvm_artifact_target(tgt)
+        for tgt in transitive_targets.closure
+        if JvmArtifactFieldSet.is_applicable(tgt)
+    ]
+
+    applicable_lockfile_entries: set[CoursierLockfileEntry] = set()
+    for artifact_requirement in artifact_requirements:
+        entry = get_entry_for_coord(lockfile, artifact_requirement.coordinate)
+        if not entry:
+            _logger.warning(
+                f"No lockfile entry for {artifact_requirement.coordinate} in resolve {resolve.name}."
+            )
+            continue
+        applicable_lockfile_entries.add(entry)
+
+    resolve_digest = await Get(
+        Digest,
+        CreateDigest(
+            [FileEntry(entry.file_name, entry.file_digest) for entry in applicable_lockfile_entries]
+        ),
+    )
+
+    resolve_digest = await Get(
+        Digest, AddPrefix(resolve_digest, f"jvm/resolves/{resolve.name}/lib")
+    )
+
+    modules = [
+        DependencyModule(
+            name=f"{entry.coord.group}:{entry.coord.artifact}",
+            version=entry.coord.version,
+            data=MavenDependencyModule(
+                organization=entry.coord.group,
+                name=entry.coord.artifact,
+                version=entry.coord.version,
+                scope=None,
+                artifacts=(
+                    MavenDependencyModuleArtifact(
+                        uri=build_root.pathlib_path.joinpath(
+                            f".pants.d/bsp/jvm/resolves/{resolve.name}/lib/{entry.file_name}"
+                        ).as_uri()
+                    ),
+                ),
+            ),
+        )
+        for entry in applicable_lockfile_entries
+    ]
+
+    return BSPDependencyModulesResult(
+        modules=tuple(modules),
+        digest=resolve_digest,
+    )
 
 
 # -----------------------------------------------------------------------------------------------
@@ -267,4 +407,5 @@ def rules():
         UnionRule(BSPBuildTargetsMetadataRequest, ScalaBSPBuildTargetsMetadataRequest),
         UnionRule(BSPHandlerMapping, ScalacOptionsHandlerMapping),
         UnionRule(BSPCompileFieldSet, ScalaBSPCompileFieldSet),
+        UnionRule(BSPDependencyModulesRequest, ScalaBSPDependencyModulesRequest),
     )
