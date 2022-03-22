33689a437858997b45f3b5b10fc36be93f0729d2
Tom Dyas <tdyas@toolchain.com>
2022-03-21 23:19:11 -0400

    tailor
    [ci skip-rust]
    
    [ci skip-build-wheels]

---
+++ src/python/pants/backend/kotlin/goals/tailor_test.py
@@ -0,0 +1,57 @@
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import pytest
+
+from pants.backend.kotlin.goals import tailor
+from pants.backend.kotlin.goals.tailor import PutativeKotlinTargetsRequest
+from pants.backend.kotlin.target_types import KotlinSourcesGeneratorTarget
+from pants.core.goals.tailor import (
+    AllOwnedSources,
+    PutativeTarget,
+    PutativeTargets,
+    PutativeTargetsSearchPaths,
+)
+from pants.engine.rules import QueryRule
+from pants.testutil.rule_runner import RuleRunner
+
+
+@pytest.fixture
+def rule_runner() -> RuleRunner:
+    return RuleRunner(
+        rules=[
+            *tailor.rules(),
+            QueryRule(PutativeTargets, (PutativeKotlinTargetsRequest, AllOwnedSources)),
+        ],
+        target_types=[KotlinSourcesGeneratorTarget],
+    )
+
+
+def test_find_putative_targets(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(
+        {
+            "src/kotlin/owned/BUILD": "kotlin_sources()\n",
+            "src/kotlin/owned/OwnedFile.kt": "package owned",
+            "src/kotlin/unowned/UnownedFile.kt": "package unowned\n",
+        }
+    )
+    putative_targets = rule_runner.request(
+        PutativeTargets,
+        [
+            PutativeKotlinTargetsRequest(PutativeTargetsSearchPaths(("",))),
+            AllOwnedSources(["src/kotlin/owned/OwnedFile.kt"]),
+        ],
+    )
+    assert (
+        PutativeTargets(
+            [
+                PutativeTarget.for_target_type(
+                    KotlinSourcesGeneratorTarget,
+                    "src/kotlin/unowned",
+                    "unowned",
+                    ["UnownedFile.kt"],
+                ),
+            ]
+        )
+        == putative_targets
+    )
