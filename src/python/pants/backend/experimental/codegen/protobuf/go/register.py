4cd9727c1505790887c75257b7dcc732fb712755
Tom Dyas <tdyas@toolchain.com>
2022-03-22 15:57:05 -0400

    fmt
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/codegen/protobuf/go/register.py
+++ src/python/pants/backend/experimental/codegen/protobuf/go/register.py
@@ -1,5 +1,6 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+from pants.backend.codegen import export_codegen_goal
 from pants.backend.codegen.protobuf import protobuf_dependency_inference, tailor
 from pants.backend.codegen.protobuf import target_types as protobuf_target_types
 from pants.backend.codegen.protobuf.go import rules as go_protobuf_rules
@@ -7,7 +8,6 @@ from pants.backend.codegen.protobuf.target_types import (
     ProtobufSourcesGeneratorTarget,
     ProtobufSourceTarget,
 )
-from pants.backend.codegen import export_codegen_goal
 
 
 def target_types():
