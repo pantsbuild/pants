3141eea5d2e835b8747bb81927d9074b041e97fb
Joshua Cannon <joshdcannon@gmail.com>
2022-03-24 12:20:00 -0500

    fix with .get
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/core/goals/lint.py
+++ src/python/pants/core/goals/lint.py
@@ -193,9 +193,9 @@ async def lint(
     dist_dir: DistDir,
 ) -> Lint:
     lint_target_request_types = cast(
-        "Iterable[type[LintTargetsRequest]]", union_membership[LintTargetsRequest]
+        "Iterable[type[LintTargetsRequest]]", union_membership.get(LintTargetsRequest)
     )
-    fmt_target_request_types = cast("Iterable[type[FmtRequest]]", union_membership[FmtRequest])
+    fmt_target_request_types = cast("Iterable[type[FmtRequest]]", union_membership.get(FmtRequest))
     target_request_types = [*lint_target_request_types, *fmt_target_request_types]
     file_request_types = union_membership[LintFilesRequest]
     specified_names = determine_specified_tool_names(
