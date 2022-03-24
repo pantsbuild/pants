280e5562e673db8661cd1971b320610ebf023e61
Stu Hood <stuhood@gmail.com>
2022-03-23 17:04:12 -0700

    Unify glob-matching implementations to fix malformed snapshot created by subsetting (cherrypick of #14889) (#14896)
    #14858 reported that an invalid `Snapshot` was being created, and reproducing in debug mode triggered `debug_assertions` related to the validity of `remexec::Directory`s created by the subset matching code.
    
    Although porting the existing subset-matching code to `DigestTrie` _and_ fixing the bug would be one option, we've wanted to remove duplication between the "apply globs to a `Snapshot`" and "apply globs to the filesystem" codepaths for a long time (see #9967), and fixing the bug by unifying the codepaths kills two birds with one stone.
    
    This change ports to implementing subset matching using `Vfs::expand_globs`, followed by the creation of a new `Snapshot` from the matches. This is definitely not as optimized as the direct subset matching was (`./cargo bench -p store -- subset` reports a change from ~20ms to ~100ms for 10k files), but it opens the door to unified optimization of _both_ our glob-expansion code and in-memory glob matching in parallel: see #14890.
    
    Fixes #9967, fixes #14858, fixes #12462, and fixes #13112.
    
    [ci skip-build-wheels]

--- src/rust/engine/fs/store/src/lib.rs
+++ src/rust/engine/fs/store/src/lib.rs
@@ -33,7 +33,7 @@ mod snapshot_ops;
 mod snapshot_ops_tests;
 #[cfg(test)]
 mod snapshot_tests;
-pub use crate::snapshot_ops::{SnapshotOps, SnapshotOpsError, SubsetParams};
+pub use crate::snapshot_ops::{SnapshotOps, SubsetParams};
 
 use std::collections::{BTreeMap, HashMap, HashSet};
 use std::fmt::Debug;
@@ -385,7 +385,11 @@ impl Store {
     let mut directories = Vec::new();
     tree.walk(&mut |_, entry| match entry {
       directory::Entry::Directory(d) => {
-        directories.push((Some(d.digest()), d.as_remexec_directory().to_bytes()))
+        let directory = d.as_remexec_directory();
+        if cfg!(debug_assertions) {
+          protos::verify_directory_canonical(d.digest(), &directory).unwrap();
+        }
+        directories.push((Some(d.digest()), directory.to_bytes()))
       }
       directory::Entry::File(_) => (),
     });
@@ -410,14 +414,18 @@ impl Store {
     initial_lease: bool,
   ) -> Result<Digest, String> {
     let local = self.local.clone();
-    local
+    let digest = local
       .store_bytes(
         EntryType::Directory,
         None,
         directory.to_bytes(),
         initial_lease,
       )
-      .await
+      .await?;
+    if cfg!(debug_assertions) {
+      protos::verify_directory_canonical(digest, directory)?;
+    }
+    Ok(digest)
   }
 
   ///
@@ -512,7 +520,7 @@ impl Store {
             )
           })?;
           if cfg!(debug_assertions) {
-            protos::verify_directory_canonical(digest, &directory).unwrap();
+            protos::verify_directory_canonical(digest, &directory)?;
           }
           Ok(directory)
         },
