073f12ead4d19613767511739ab3792839277081
Jonas Stendahl <jonas.stendahl@gmail.com>
2022-03-25 21:16:58 +0100

    Add Protobuf formatting using buf format (#14907)
    This PR adds support for Protobuf formatting using the newly released `buf format`. I took the libery of bumping the default version of `buf`since `buf format` requires Buf v1.2.0 and above. Right now there's no check to make sure the user is actually on >v1.2.0, but that shouldn't be too hard to add if needed.

--- src/python/pants/backend/codegen/protobuf/lint/buf/subsystem.py
+++ src/python/pants/backend/codegen/protobuf/lint/buf/subsystem.py
@@ -9,16 +9,16 @@ from pants.option.option_types import ArgsListOption, SkipOption
 
 
 class BufSubsystem(TemplatedExternalTool):
-    options_scope = "buf-lint"
+    options_scope = "buf"
     name = "Buf"
-    help = "A linter for Protocol Buffers (https://github.com/bufbuild/buf)."
+    help = "A linter and formatter for Protocol Buffers (https://github.com/bufbuild/buf)."
 
-    default_version = "v1.0.0"
+    default_version = "v1.3.0"
     default_known_versions = [
-        "v1.0.0|linux_arm64 |c4b095268fe0fc8de2ad76c7b4677ccd75f25623d5b1f971082a6b7f43ff1eb0|13378006",
-        "v1.0.0|linux_x86_64|5f0ff97576cde9e43ec86959046169f18ec5bcc08e31d82dcc948d057212f7bf|14511545",
-        "v1.0.0|macos_arm64 |e922c277487d941c4b056cac6c1b4c6e5004e8f3dda65ae2d72d8b10da193297|15147463",
-        "v1.0.0|macos_x86_64|8963e1ab7685aac59b8805cc7d752b06a572b1c747a6342a9e73b94ccdf89ddb|15187858",
+        "v1.3.0|linux_arm64 |fbfd53c501451b36900247734bfa4cbe86ae05d0f51bc298de8711d5ee374ee5|13940828",
+        "v1.3.0|linux_x86_64|e29c4283b1cd68ada41fa493171c41d7605750d258fcd6ecdf692a63fae95213|15267162",
+        "v1.3.0|macos_arm64 |147985d7f2816a545792e38b26178ff4027bf16cd3712f6e387a4e3692a16deb|15391890",
+        "v1.3.0|macos_x86_64|3b6bd2e5a5dd758178aee01fb067261baf5d31bfebe93336915bfdf7b21928c4|15955291",
     ]
     default_url_template = (
         "https://github.com/bufbuild/buf/releases/download/{version}/buf-{platform}.tar.gz"
@@ -30,8 +30,10 @@ class BufSubsystem(TemplatedExternalTool):
         "linux_x86_64": "Linux-x86_64",
     }
 
-    skip = SkipOption("lint")
-    args = ArgsListOption(example="--error-format json")
+    skip_format = SkipOption("fmt", "lint", flag_name="--format-skip")
+    skip_lint = SkipOption("lint", flag_name="--lint-skip")
+    format_args = ArgsListOption(example="--error-format json", flag_name="--format-args")
+    lint_args = ArgsListOption(example="--error-format json", flag_name="--lint-args")
 
     def generate_exe(self, plat: Platform) -> str:
         return "./buf/bin/buf"
