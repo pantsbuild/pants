abc0a61f12227444590ba433dc9cd837c0734e97
Joshua Cannon <joshdcannon@gmail.com>
2022-03-28 12:53:06 -0500

    protobuf

--- src/python/pants/backend/codegen/protobuf/lint/buf/format_rules.py
+++ src/python/pants/backend/codegen/protobuf/lint/buf/format_rules.py
@@ -9,7 +9,6 @@ from pants.backend.codegen.protobuf.target_types import (
     ProtobufSourceField,
 )
 from pants.core.goals.fmt import FmtRequest, FmtResult
-from pants.core.goals.lint import LintResult, LintResults, LintTargetsRequest
 from pants.core.util_rules.external_tool import DownloadedExternalTool, ExternalToolRequest
 from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
 from pants.core.util_rules.system_binaries import (
@@ -21,7 +20,7 @@ from pants.core.util_rules.system_binaries import (
 from pants.engine.fs import Digest, MergeDigests
 from pants.engine.internals.native_engine import Snapshot
 from pants.engine.platform import Platform
-from pants.engine.process import FallibleProcessResult, Process, ProcessResult
+from pants.engine.process import Process, ProcessResult
 from pants.engine.rules import Get, MultiGet, collect_rules, rule
 from pants.engine.target import FieldSet, Target
 from pants.engine.unions import UnionRule
@@ -41,15 +40,9 @@ class BufFieldSet(FieldSet):
         return tgt.get(SkipBufFormatField).value
 
 
-class BufFormatRequest(LintTargetsRequest, FmtRequest):
+class BufFormatRequest(FmtRequest):
     field_set_type = BufFieldSet
     name = "buf-format"
-
-
-@dataclass(frozen=True)
-class SetupRequest:
-    request: BufFormatRequest
-    check_only: bool
 
 
 @dataclass(frozen=True)
@@ -59,7 +52,7 @@ class Setup:
 
 
 @rule(level=LogLevel.DEBUG)
-async def setup_buf_format(setup_request: SetupRequest, buf: BufSubsystem) -> Setup:
+async def setup_buf_format(request: BufFormatRequest, buf: BufSubsystem) -> Setup:
     diff_binary = await Get(DiffBinary, DiffBinaryRequest())
     download_buf_get = Get(
         DownloadedExternalTool, ExternalToolRequest, buf.get_request(Platform.current)
@@ -75,7 +68,7 @@ async def setup_buf_format(setup_request: SetupRequest, buf: BufSubsystem) -> Se
     )
     source_files_get = Get(
         SourceFiles,
-        SourceFilesRequest(field_set.sources for field_set in setup_request.request.field_sets),
+        SourceFilesRequest(field_set.sources for field_set in request.field_sets),
     )
     downloaded_buf, binary_shims, source_files = await MultiGet(
         download_buf_get, binary_shims_get, source_files_get
@@ -83,8 +76,8 @@ async def setup_buf_format(setup_request: SetupRequest, buf: BufSubsystem) -> Se
 
     source_files_snapshot = (
         source_files.snapshot
-        if setup_request.request.prior_formatter_result is None
-        else setup_request.request.prior_formatter_result
+        if request.prior_formatter_result is None
+        else request.prior_formatter_result
     )
 
     input_digest = await Get(
@@ -95,9 +88,7 @@ async def setup_buf_format(setup_request: SetupRequest, buf: BufSubsystem) -> Se
     argv = [
         downloaded_buf.exe,
         "format",
-        # If linting, use `-d` to error with a diff and `--exit-code` to exit with a non-zero exit code if
-        # the file is not already formatted. Else, write the change with `-w`.
-        *(["-d", "--exit-code"] if setup_request.check_only else ["-w"]),
+        "-w",
         *buf.format_args,
         "--path",
         ",".join(source_files_snapshot.files),
@@ -106,7 +97,7 @@ async def setup_buf_format(setup_request: SetupRequest, buf: BufSubsystem) -> Se
         argv=argv,
         input_digest=input_digest,
         output_files=source_files_snapshot.files,
-        description=f"Run buf format on {pluralize(len(setup_request.request.field_sets), 'file')}.",
+        description=f"Run buf format on {pluralize(len(request.field_sets), 'file')}.",
         level=LogLevel.DEBUG,
         env={
             "PATH": binary_shims.bin_directory,
@@ -119,7 +110,7 @@ async def setup_buf_format(setup_request: SetupRequest, buf: BufSubsystem) -> Se
 async def run_buf_format(request: BufFormatRequest, buf: BufSubsystem) -> FmtResult:
     if buf.skip_format:
         return FmtResult.skip(formatter_name=request.name)
-    setup = await Get(Setup, SetupRequest(request, check_only=False))
+    setup = await Get(Setup, BufFormatRequest, request)
     result = await Get(ProcessResult, Process, setup.process)
     output_snapshot = await Get(Snapshot, Digest, result.output_digest)
     return FmtResult(
@@ -131,18 +122,8 @@ async def run_buf_format(request: BufFormatRequest, buf: BufSubsystem) -> FmtRes
     )
 
 
-@rule(desc="Lint with buf format", level=LogLevel.DEBUG)
-async def run_buf_lint(request: BufFormatRequest, buf: BufSubsystem) -> LintResults:
-    if buf.skip_format:
-        return LintResults([], linter_name=request.name)
-    setup = await Get(Setup, SetupRequest(request, check_only=True))
-    result = await Get(FallibleProcessResult, Process, setup.process)
-    return LintResults([LintResult.from_fallible_process_result(result)], linter_name=request.name)
-
-
 def rules():
     return [
         *collect_rules(),
         UnionRule(FmtRequest, BufFormatRequest),
-        UnionRule(LintTargetsRequest, BufFormatRequest),
     ]
