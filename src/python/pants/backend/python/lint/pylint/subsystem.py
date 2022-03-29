08de481bf639fb896dbfa6bda0b07647e3425047
Joshua Cannon <joshdcannon@gmail.com>
2022-03-29 15:13:49 -0500

    Update src/python/pants/backend/python/lint/pylint/subsystem.py
    Co-authored-by: Stu Hood <stuhood@gmail.com>

--- src/python/pants/backend/python/lint/pylint/subsystem.py
+++ src/python/pants/backend/python/lint/pylint/subsystem.py
@@ -141,7 +141,7 @@ class Pylint(PythonToolBase):
             specified=self.config,
             specified_option_name=f"[{self.options_scope}].config",
             discovery=self.config_discovery,
-            check_existence=[".pylinrc", *(os.path.join(d, "pylintrc") for d in ("", *dirs))],
+            check_existence=[".pylintrc", *(os.path.join(d, "pylintrc") for d in ("", *dirs))],
             check_content={"pyproject.toml": b"[tool.pylint.", "setup.cfg": b"[pylint."},
         )
 
