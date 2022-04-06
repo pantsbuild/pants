6b2209d860b842db2683f8bf8956d8eaed513c0d
Stu Hood <stuhood@gmail.com>
2022-04-06 16:44:45 -0700

    Fix default layout and use of PEX-repositories for legacy lockfiles (#15014)
    As reported in #14980 and #14991: 1) the default layout of non-internal PEXes changed to `PACKED`, and 2) the PEX-repository subsetting optimization was disabled for manual or poetry lockfiles.
    
    This change fixes those issues, and adds a test of the `pex_from_targets` codepath which decides which resolution strategy to use. But it also fairly significantly refactors the `PexRequest` consumption code to improve typesafety. In particular:
    * Replaces the `is_all_constraints_resolve` special case with explicitly specified `PexLayout`s for those `PexRequests` (which would have been harder at the time when the special case was originally added, since `PexRequest.layout` didn't exist yet).
    * Moves lockfile header parsing and PEX-native-detection into a `@rule` which produces a `LoadedLockfile`.
    * Moves "subsetting of a lockfile or a repository PEX" to the `PexRequirements.from_subset` field, which takes a `LoadedLockfile` and asserts (not worth introducing another type yet probably) that the lockfile is PEX-native.
    * Move the requirements which used to be attached to the `Lockfile`/`LockfileContents` off onto an `PexRequest.requirements = EntireLockfile` case, to assert that they are optional, and only used for metadata validation (rather than subsetting).
    
    Fixes #14980 and fixes #14991.

--- src/python/pants/backend/python/util_rules/pex_requirements.py
+++ src/python/pants/backend/python/util_rules/pex_requirements.py
@@ -4,7 +4,7 @@
 from __future__ import annotations
 
 import logging
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, Iterable, Iterator
 
 from pants.backend.python.pip_requirement import PipRequirement
@@ -17,7 +17,15 @@ from pants.backend.python.util_rules.lockfile_metadata import (
     PythonLockfileMetadataV2,
 )
 from pants.core.util_rules.lockfile_metadata import InvalidLockfileError, LockfileMetadataValidation
-from pants.engine.fs import FileContent
+from pants.engine.fs import (
+    CreateDigest,
+    Digest,
+    DigestContents,
+    FileContent,
+    GlobMatchErrorBehavior,
+    PathGlobs,
+)
+from pants.engine.rules import Get, collect_rules, rule
 from pants.util.docutil import bin_name, doc_url
 from pants.util.meta import frozen_after_init
 from pants.util.ordered_set import FrozenOrderedSet
@@ -34,7 +42,6 @@ class Lockfile:
     file_path: str
     file_path_description_of_origin: str
     resolve_name: str
-    req_strings: FrozenOrderedSet[str]
     lockfile_hex_digest: str | None = None
 
 
@@ -42,7 +49,6 @@ class Lockfile:
 class LockfileContent:
     file_content: FileContent
     resolve_name: str
-    req_strings: FrozenOrderedSet[str]
     lockfile_hex_digest: str | None = None
 
 
@@ -62,36 +68,179 @@ class ToolCustomLockfile(Lockfile, _ToolLockfileMixin):
     pass
 
 
+@dataclass(frozen=True)
+class LoadedLockfile:
+    """A lockfile after loading and header stripping.
+
+    Validation is deferred until consumption time, because each consumed subset (in the case of a
+    PEX-native lockfile) can be individually validated.
+    """
+
+    # The digest of the loaded lockfile (which may not be identical to the input).
+    lockfile_digest: Digest
+    # The path of the loaded lockfile within the Digest.
+    lockfile_path: str
+    # The loaded metadata for this lockfile, if any.
+    metadata: PythonLockfileMetadata | None = field(hash=False)
+    # An estimate of the number of requirements in this lockfile, to be used as a heuristic for
+    # available parallelism.
+    requirement_estimate: int
+    # True if the loaded lockfile is in PEX's native format.
+    is_pex_native: bool
+    # The original file or file content (which may not have identical content to the output
+    # `lockfile_digest).
+    original_lockfile: Lockfile | LockfileContent
+
+
+@dataclass(frozen=True)
+class LoadedLockfileRequest:
+    """A request to load and validate the content of the given lockfile."""
+
+    lockfile: Lockfile | LockfileContent
+
+
+def _strip_comments_from_pex_json_lockfile(lockfile_bytes: bytes) -> bytes:
+    """Pex does not like the header Pants adds to lockfiles, as it violates JSON.
+
+    Note that we only strip lines starting with `//`, which is all that Pants will ever add. If
+    users add their own comments, things will fail.
+    """
+    return b"\n".join(
+        line for line in lockfile_bytes.splitlines() if not line.lstrip().startswith(b"//")
+    )
+
+
+def is_probably_pex_json_lockfile(lockfile_bytes: bytes) -> bool:
+    for line in lockfile_bytes.splitlines():
+        if line and not line.startswith(b"//"):
+            # Note that pip/Pex complain if a requirements.txt style starts with `{`.
+            return line.lstrip().startswith(b"{")
+    return False
+
+
+def _pex_lockfile_requirement_count(lockfile_bytes: bytes) -> int:
+    # TODO: this is a very naive heuristic that will overcount, and also relies on Pants
+    #  setting `--indent` when generating lockfiles. More robust would be parsing the JSON
+    #  and getting the len(locked_resolves.locked_requirements.project_name), but we risk
+    #  if Pex ever changes its lockfile format.
+
+    num_lines = len(lockfile_bytes.splitlines())
+    # These are very naive estimates, and they bias towards overcounting. For example, requirements
+    # often are 20+ lines.
+    num_lines_for_options = 10
+    lines_per_req = 10
+    return max((num_lines - num_lines_for_options) // lines_per_req, 2)
+
+
+@rule
+async def load_lockfile(
+    request: LoadedLockfileRequest,
+    python_setup: PythonSetup,
+) -> LoadedLockfile:
+    lockfile = request.lockfile
+    if isinstance(lockfile, Lockfile):
+        synthetic_lock = False
+        lockfile_path = lockfile.file_path
+        lockfile_digest = await Get(
+            Digest,
+            PathGlobs(
+                [lockfile_path],
+                glob_match_error_behavior=GlobMatchErrorBehavior.error,
+                description_of_origin=lockfile.file_path_description_of_origin,
+            ),
+        )
+        _digest_contents = await Get(DigestContents, Digest, lockfile_digest)
+        lock_bytes = _digest_contents[0].content
+    else:
+        synthetic_lock = True
+        _fc = lockfile.file_content
+        lockfile_path, lock_bytes = (_fc.path, _fc.content)
+        lockfile_digest = await Get(Digest, CreateDigest([_fc]))
+
+    is_pex_native = is_probably_pex_json_lockfile(lock_bytes)
+    if is_pex_native:
+        header_delimiter = "//"
+        lockfile_digest = await Get(
+            Digest,
+            CreateDigest(
+                [FileContent(lockfile_path, _strip_comments_from_pex_json_lockfile(lock_bytes))]
+            ),
+        )
+        requirement_estimate = _pex_lockfile_requirement_count(lock_bytes)
+    else:
+        header_delimiter = "#"
+        # Note: this is a very naive heuristic. It will overcount because entries often
+        # have >1 line due to `--hash`.
+        requirement_estimate = len(lock_bytes.decode().splitlines())
+
+    metadata: PythonLockfileMetadata | None = None
+    if should_validate_metadata(lockfile, python_setup):
+        metadata = PythonLockfileMetadata.from_lockfile(
+            lock_bytes,
+            **(dict() if synthetic_lock else dict(lockfile_path=lockfile_path)),
+            resolve_name=lockfile.resolve_name,
+            delimeter=header_delimiter,
+        )
+
+    return LoadedLockfile(
+        lockfile_digest,
+        lockfile_path,
+        metadata,
+        requirement_estimate,
+        is_pex_native,
+        original_lockfile=lockfile,
+    )
+
+
+@dataclass(frozen=True)
+class EntireLockfile:
+    """A request to resolve the entire contents of a lockfile.
+
+    This resolution mode is used in a few cases:
+    1. for poetry or hand-written lockfiles (which do not support being natively subsetted the
+       way that a PEX lockfile can be), in order to build a repository-PEX to subset separately.
+    2. for tool lockfiles, which (regardless of format), need to resolve the entire lockfile
+       content anyway.
+    """
+
+    lockfile: Lockfile | LockfileContent
+    # If available, the current complete set of requirement strings that influence this lockfile.
+    # Used for metadata validation.
+    complete_req_strings: tuple[str, ...] | None = None
+
+
 @frozen_after_init
 @dataclass(unsafe_hash=True)
 class PexRequirements:
+    """A request to resolve a series of requirements (optionally from a "superset" resolve)."""
+
     req_strings: FrozenOrderedSet[str]
     constraints_strings: FrozenOrderedSet[str]
-    # TODO: The constraints.txt resolve for `resolve_all_constraints` will be removed as part of
-    # #12314, but in the meantime, it "acts like" a lockfile, but isn't actually typed as a Lockfile
-    # because the constraints are modified in memory first. This flag marks a `PexRequirements`
-    # resolve as being a request for the entire constraints file.
-    is_all_constraints_resolve: bool
-    repository_pex: Pex | None
+    # If these requirements should be resolved as a subset of either a repository PEX, or a
+    # PEX-native lockfile, the superset to use. # NB: Use of a lockfile here asserts that the
+    # lockfile is PEX-native, because legacy lockfiles do not support subset resolves.
+    from_superset: Pex | LoadedLockfile | None
 
     def __init__(
         self,
         req_strings: Iterable[str] = (),
         *,
         constraints_strings: Iterable[str] = (),
-        is_all_constraints_resolve: bool = False,
-        repository_pex: Pex | None = None,
+        from_superset: Pex | LoadedLockfile | None = None,
     ) -> None:
         """
         :param req_strings: The requirement strings to resolve.
         :param constraints_strings: Constraints strings to apply during the resolve.
-        :param repository_pex: An optional PEX to resolve requirements from via the Pex CLI
-            `--pex-repository` option.
+        :param from_superset: An optional superset PEX or lockfile to resolve the req strings from.
         """
         self.req_strings = FrozenOrderedSet(sorted(req_strings))
         self.constraints_strings = FrozenOrderedSet(sorted(constraints_strings))
-        self.is_all_constraints_resolve = is_all_constraints_resolve
-        self.repository_pex = repository_pex
+        if isinstance(from_superset, LoadedLockfile) and not from_superset.is_pex_native:
+            raise ValueError(
+                f"The lockfile {from_superset.original_lockfile} was not in PEX's "
+                "native format, and so cannot be directly used as a superset."
+            )
+        self.from_superset = from_superset
 
     @classmethod
     def create_from_requirement_fields(
@@ -120,10 +269,13 @@ def validate_metadata(
     metadata: PythonLockfileMetadata,
     interpreter_constraints: InterpreterConstraints,
     lockfile: Lockfile | LockfileContent,
+    consumed_req_strings: Iterable[str],
     python_setup: PythonSetup,
 ) -> None:
+    """Given interpreter constraints and requirements to be consumed, validate lockfile metadata."""
+
     # TODO(#12314): Improve the exception if invalid strings
-    user_requirements = {PipRequirement.parse(i) for i in lockfile.req_strings}
+    user_requirements = {PipRequirement.parse(i) for i in consumed_req_strings}
     validation = metadata.is_valid_for(
         is_tool=isinstance(lockfile, (ToolCustomLockfile, ToolDefaultLockfile)),
         expected_invalidation_digest=lockfile.lockfile_hex_digest,
@@ -279,3 +431,7 @@ def _invalid_user_lockfile_error(
     yield f"run `{bin_name()} generate-lockfiles --resolve={lockfile.resolve_name}`." if isinstance(
         lockfile, Lockfile
     ) else f"Update your plugin generating this object: {lockfile}"
+
+
+def rules():
+    return collect_rules()
