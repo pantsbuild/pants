317f611d9cf7bbf2df089986156bb4f8f1ef821a
Tom Dyas <tdyas@toolchain.com>
2022-04-10 22:27:30 -0400

    test include parsing
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/cc/dependency_inference/rules_integration_test.py
+++ src/python/pants/backend/cc/dependency_inference/rules_integration_test.py
@@ -1,10 +1,16 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+from __future__ import annotations
+
 from textwrap import dedent
 
 import pytest
 
-from pants.backend.cc.dependency_inference.rules import InferCCDependenciesRequest
+from pants.backend.cc.dependency_inference.rules import (
+    CCIncludeDirective,
+    InferCCDependenciesRequest,
+    parse_includes,
+)
 from pants.backend.cc.dependency_inference.rules import rules as cc_dep_inf_rules
 from pants.backend.cc.target_types import CCSourceField, CCSourcesGeneratorTarget, CCSourceTarget
 from pants.backend.cc.target_types import rules as target_type_rules
@@ -12,6 +18,38 @@ from pants.build_graph.address import Address
 from pants.engine.target import InferredDependencies
 from pants.testutil.rule_runner import RuleRunner
 from pants.util.strutil import softwrap
+
+
+@pytest.mark.parametrize(
+    "file_content,expected",
+    [
+        ('#include "foo.h"', {CCIncludeDirective("foo.h", False)}),
+        ("#include <foo.h>", {CCIncludeDirective("foo.h", True)}),
+        ('  #  include "foo.h"', {CCIncludeDirective("foo.h", False)}),
+        ("  #  include <foo.h>", {CCIncludeDirective("foo.h", True)}),
+        ('\t#\tinclude "foo.h"', {CCIncludeDirective("foo.h", False)}),
+        ("\t#\tinclude <foo.h>", {CCIncludeDirective("foo.h", True)}),
+        # More complex file names.
+        ('#include "path/to_dir/f.h"', {CCIncludeDirective("path/to_dir/f.h", False)}),
+        ('#include "path/to dir/f.h"', {CCIncludeDirective("path/to dir/f.h", False)}),
+        ('#include "path\\to-dir\\f.h"', {CCIncludeDirective("path\\to-dir\\f.h", False)}),
+        ("#include <âčĘï.h>", {CCIncludeDirective("âčĘï.h", True)}),
+        ('#include "123.h"', {CCIncludeDirective("123.h", False)}),
+        ("#include <123.h>", {CCIncludeDirective("123.h", True)}),
+        (
+            dedent(
+                """\
+                    #include "dir/foo.h"
+                    some random proto code;
+                    #include <ábč.h>
+                    """
+            ),
+            {CCIncludeDirective("dir/foo.h", False), CCIncludeDirective("ábč.h", True)},
+        ),
+    ],
+)
+def test_parse_proto_imports(file_content: str, expected: set[CCIncludeDirective]) -> None:
+    assert set(parse_includes(file_content)) == expected
 
 
 @pytest.fixture
