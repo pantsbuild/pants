fae3e1bb173fc4c1115fecc8fb2870fb3aae91f7
Tom Dyas <tdyas@toolchain.com>
2022-04-11 21:56:45 -0400

    get version working w/o package targets - rule graph fun!
    [ci skip-rust]

--- src/python/pants/backend/experimental/kotlin/register.py
+++ src/python/pants/backend/experimental/kotlin/register.py
@@ -5,19 +5,16 @@ from pants.backend.kotlin.goals import check, tailor
 from pants.backend.kotlin.target_types import KotlinSourcesGeneratorTarget, KotlinSourceTarget
 from pants.backend.kotlin.target_types import rules as target_types_rules
 from pants.core.util_rules import source_files, system_binaries
-from pants.jvm import classpath, jdk_rules, resources, run_deploy_jar
+from pants.jvm import classpath, jdk_rules, resources
 from pants.jvm import util_rules as jvm_util_rules
-from pants.jvm.package.war import rules as war_rules
-from pants.jvm.resolve import coursier_fetch, coursier_setup, jvm_tool
-from pants.jvm.target_types import DeployJarTarget, JvmArtifactTarget, JvmWarTarget
 from pants.jvm.goals import lockfile
+from pants.jvm.resolve import coursier_fetch, coursier_setup, jvm_tool
+from pants.jvm.target_types import JvmArtifactTarget
 
 
 def target_types():
     return [
-        DeployJarTarget,
         JvmArtifactTarget,
-        JvmWarTarget,
         KotlinSourceTarget,
         KotlinSourcesGeneratorTarget,
     ]
@@ -37,8 +34,6 @@ def rules():
         *target_types_rules(),
         *jvm_tool.rules(),
         *resources.rules(),
-        *run_deploy_jar.rules(),
-        *war_rules(),
         *system_binaries.rules(),
         *source_files.rules(),
     ]
