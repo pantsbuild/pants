acb701723fa03938ddb48ec7caa64e66f1bbb8d6
Tom Dyas <tdyas@toolchain.com>
2022-04-11 21:59:02 -0400

    add back packaging targets
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/kotlin/register.py
+++ src/python/pants/backend/experimental/kotlin/register.py
@@ -5,11 +5,12 @@ from pants.backend.kotlin.goals import check, tailor
 from pants.backend.kotlin.target_types import KotlinSourcesGeneratorTarget, KotlinSourceTarget
 from pants.backend.kotlin.target_types import rules as target_types_rules
 from pants.core.util_rules import source_files, system_binaries
-from pants.jvm import classpath, jdk_rules, resources
+from pants.jvm import classpath, jdk_rules, resources, run_deploy_jar
 from pants.jvm import util_rules as jvm_util_rules
 from pants.jvm.goals import lockfile
+from pants.jvm.package import deploy_jar, war
 from pants.jvm.resolve import coursier_fetch, coursier_setup, jvm_tool
-from pants.jvm.target_types import JvmArtifactTarget
+from pants.jvm.target_types import DeployJarTarget, JvmArtifactTarget, JvmWarTarget
 
 
 def target_types():
@@ -17,6 +18,8 @@ def target_types():
         JvmArtifactTarget,
         KotlinSourceTarget,
         KotlinSourcesGeneratorTarget,
+        DeployJarTarget,
+        JvmWarTarget,
     ]
 
 
@@ -36,4 +39,7 @@ def rules():
         *resources.rules(),
         *system_binaries.rules(),
         *source_files.rules(),
+        *deploy_jar.rules(),
+        *run_deploy_jar.rules(),
+        *war.rules(),
     ]
