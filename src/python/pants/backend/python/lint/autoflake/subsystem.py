54abdc5aea3d4dac7131d81a8f080f92237903a7
Daniel Moran <danxmoran@gmail.com>
2022-04-13 13:18:34 -0400

    Add autoflake export hook, but leave it disabled for now.
    Enabling/exposing the hook causes a panic from the rule graph.
    
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/python/lint/autoflake/subsystem.py
+++ src/python/pants/backend/python/lint/autoflake/subsystem.py
@@ -4,6 +4,7 @@
 from __future__ import annotations
 
 from pants.backend.python.goals import lockfile
+from pants.backend.python.goals.export import ExportPythonTool, ExportPythonToolSentinel
 from pants.backend.python.goals.lockfile import GeneratePythonLockfile
 from pants.backend.python.subsystems.python_tool_base import PythonToolBase
 from pants.backend.python.subsystems.setup import PythonSetup
@@ -48,9 +49,23 @@ async def setup_autoflake_lockfile(
     )
 
 
+class AutoflakeExportSentinel(ExportPythonToolSentinel):
+    pass
+
+
+@rule
+def autoflake_export(_: AutoflakeExportSentinel, autoflake: Autoflake) -> ExportPythonTool:
+    return ExportPythonTool(
+        resolve_name=autoflake.options_scope, pex_request=autoflake.to_pex_request()
+    )
+
+
 def rules():
     return (
         *collect_rules(),
         *lockfile.rules(),
         UnionRule(GenerateToolLockfileSentinel, AutoflakeLockfileSentinel),
+        # NOTE: If I un-comment this line to register the rule, `./pants` starts to panic
+        # from a loop in the rule graph.
+        # UnionRule(ExportPythonToolSentinel, AutoflakeExportSentinel),
     )
