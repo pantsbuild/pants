f8b672c4478408ef226f24769c191e9f594cdd87
John Sirois <john.sirois@gmail.com>
2022-04-20 09:52:52 -0700

    Upgrade Pex to 2.1.82. (#15200)
    This fixes a corner-case for distributions built with setuptools that
    have funky project names.
    
    See the changelog here:
      https://github.com/pantsbuild/pex/releases/tag/v2.1.82

--- src/python/pants/backend/python/util_rules/pex_cli.py
+++ src/python/pants/backend/python/util_rules/pex_cli.py
@@ -39,9 +39,9 @@ class PexCli(TemplatedExternalTool):
     name = "pex"
     help = "The PEX (Python EXecutable) tool (https://github.com/pantsbuild/pex)."
 
-    default_version = "v2.1.80"
+    default_version = "v2.1.82"
     default_url_template = "https://github.com/pantsbuild/pex/releases/download/{version}/pex"
-    version_constraints = ">=2.1.80,<3.0"
+    version_constraints = ">=2.1.82,<3.0"
 
     @classproperty
     def default_known_versions(cls):
@@ -50,8 +50,8 @@ class PexCli(TemplatedExternalTool):
                 (
                     cls.default_version,
                     plat,
-                    "5485e1e770ef93e6450eee8d2d7ab495e192e5033c1f37d2b20aa306684196be",
-                    "3741488",
+                    "f39219166e8e47ebf2386079bd66ba3ae337d6d70edf6465a774968a75988825",
+                    "3741517",
                 )
             )
             for plat in ["macos_arm64", "macos_x86_64", "linux_x86_64"]
