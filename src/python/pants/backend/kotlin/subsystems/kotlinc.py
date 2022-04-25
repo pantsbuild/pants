73dc1c0bef70dd781a18bf241449ef922a0c90ca
Tom Dyas <tdyas@toolchain.com>
2022-04-25 16:56:20 -0400

    [internal] kotlin: add support for `kotlinc` compiler plugins (#15184)
    Add support for `kotlinc` compiler plugins via a `kotlinc_plugin` target type and related rules.

--- src/python/pants/backend/kotlin/subsystems/kotlinc.py
+++ src/python/pants/backend/kotlin/subsystems/kotlinc.py
@@ -1,7 +1,10 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
-from pants.option.option_types import ArgsListOption
+from __future__ import annotations
+
+from pants.option.option_types import ArgsListOption, DictOption
 from pants.option.subsystem import Subsystem
+from pants.util.strutil import softwrap
 
 
 class KotlincSubsystem(Subsystem):
@@ -13,3 +16,22 @@ class KotlincSubsystem(Subsystem):
         example="-Werror",
         extra_help="See https://kotlinlang.org/docs/compiler-reference.html for supported arguments.",
     )
+
+    # TODO: see if we can use an actual list mechanism? If not, this seems like an OK option
+    default_plugins = DictOption[str](
+        "--plugins-for-resolve",
+        help=softwrap(
+            """
+            A dictionary, whose keys are the names of each JVM resolve that requires default
+            `kotlinc` plugins, and the value is a comma-separated string consisting of kotlinc plugin
+            names. Each specified plugin must have a corresponding `kotlinc_plugin` target that specifies
+            that name in either its `plugin_name` field or is the same as its target name.
+            """
+        ),
+    )
+
+    def parsed_default_plugins(self) -> dict[str, list[str]]:
+        return {
+            key: [i.strip() for i in value.split(",")]
+            for key, value in self.default_plugins.items()
+        }
