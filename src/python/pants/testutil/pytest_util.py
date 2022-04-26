34dc248d6ebb6ecd41a26ec5d9f7b505db4ed6db
Andreas Stenius <andreas.stenius@svenskaspel.se>
2022-04-26 21:05:27 +0200

    Do not lowercase Docker image tags. (Cherry pick of #15254) (#15259)

--- src/python/pants/testutil/pytest_util.py
+++ src/python/pants/testutil/pytest_util.py
@@ -31,11 +31,17 @@ def no_exception():
     When declaring parametrized tests, the test function can take a exceptions
     expectation as input, and always use a with-block for the code under test.
 
-        @pytest.mark.parametrize('answer, expect_raises', [
-            (42, no_exception()),
-            (12, pytest.raises(WrongAnswer)),
-        ])
-        def test_search_for_the_meaning_of_life_universe_and_everything(answer, expect_raises):
+        @pytest.mark.parametrize(
+            "answer, expect_raises",
+            [
+                (42, no_exception()),
+                (12, pytest.raises(WrongAnswer)),
+            ]
+        )
+        def test_search_for_the_meaning_of_life_universe_and_everything(
+            answer: int,
+            expect_raises: typing.ContextManager,
+        ):
             with expect_raises:
                 computer.validate_result(answer)
     """
