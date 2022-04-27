21f8a514c2a5ab9d1450714101490bcab106abb5
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2022-04-27 03:57:49 -0700

    Revert "[internal] add mark and fixture for lockfile generation (#15190)" (#15267)
    This causes the build wheels job to fail, which blocks the release: https://github.com/pantsbuild/pants/runs/6181314347?check_suite_focus=true#step:10:1878
    
    This reverts commit 0147029f7d6a1537e39220e02d04326b4cb234ae.
    
    [ci skip-rust]

--- src/python/pants/backend/scala/conftest.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
-# Licensed under the Apache License, Version 2.0 (see LICENSE).
-from pants.testutil.lockfile_fixture import JvmLockfilePlugin
-
-
-def pytest_configure(config):
-    config.pluginmanager.register(JvmLockfilePlugin())
