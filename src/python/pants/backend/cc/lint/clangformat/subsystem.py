96d81104007989eaae0da6344bf9de5c062edb07
SJ <suresh@vicarasolutions.com>
2022-05-10 22:37:40 -0400

    See issue #15377: Pulled pants-plugins clang-format plugin - Updated to 2.13.x - Removed explicit LintTargetRequests - Replaced `cpp` targets with `cc`
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

---
+++ src/python/pants/backend/cc/lint/clangformat/subsystem.py
@@ -0,0 +1,39 @@
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import os
+from typing import Iterable
+
+from pants.backend.python.subsystems.python_tool_base import PythonToolBase
+from pants.backend.python.target_types import ConsoleScript
+from pants.core.util_rules.config_files import ConfigFilesRequest
+from pants.util.strutil import softwrap
+
+
+class ClangFormat(PythonToolBase):
+    options_scope = "clang-format"
+    help = softwrap(
+        """
+        The clang-format utility for formatting C/C++ (and others) code
+        (https://clang.llvm.org/docs/ClangFormat.html).
+        """
+    )
+
+    default_version = "clang-format==14.0.3"
+    default_main = ConsoleScript("clang-format")
+
+    register_interpreter_constraints = True
+    default_interpreter_constraints = ["CPython>=3.7"]
+
+    def config_request(self, dirs: Iterable[str]) -> ConfigFilesRequest:
+        """clang-format will use the closest configuration file to the file currently being
+        formatted, so add all of them."""
+        config_files = (
+            ".clang-format",
+            "_clang-format",
+        )
+        check_existence = [os.path.join(d, file) for file in config_files for d in ("", *dirs)]
+        return ConfigFilesRequest(
+            discovery=True,
+            check_existence=check_existence,
+        )
---
+++ src/python/pants/backend/experimental/cc/lint/clangformat/register.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+"""A formatter for C/C++ (and several other languages).
+
+See https://clang.llvm.org/docs/ClangFormat.html for details.
+"""
+
+from __future__ import annotations
+
+from typing import Iterable
+
+from pants.backend.cc.lint.clangformat import rules as clangformat_rules
+from pants.engine.rules import Rule
+from pants.engine.unions import UnionRule
+
+
+def rules() -> Iterable[Rule | UnionRule]:
+    return clangformat_rules.rules()
