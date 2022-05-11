45e5fda8d81bf0fc4937cf79071c0895a0fed4db
Stu Hood <stuhood@gmail.com>
2022-05-11 16:35:00 -0700

    Render compile task progress.
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/bsp/util_rules/compile.py
+++ src/python/pants/bsp/util_rules/compile.py
@@ -64,12 +64,16 @@ async def compile_bsp_target(
                 field_set = field_set_type.create(target)
                 field_sets_by_request_type[compile_request_type].add(field_set)
 
-    task_id = TaskId(id=uuid.uuid4().hex)
+    task_id = TaskId(
+        id=uuid.uuid4().hex, parents=((request.origin_id,) if request.origin_id else None)
+    )
+    message = f"Compilation of {request.bsp_target.bsp_target_id.uri}"
 
     bsp_context.notify_client(
         TaskStartParams(
             task_id=task_id,
             event_time=int(time.time() * 1000),
+            message=message,
             data=CompileTask(target=request.bsp_target.bsp_target_id),
         )
     )
@@ -78,7 +82,9 @@ async def compile_bsp_target(
         Get(
             BSPCompileResult,
             BSPCompileRequest,
-            compile_request_type(bsp_target=request.bsp_target, field_sets=tuple(field_sets)),
+            compile_request_type(
+                bsp_target=request.bsp_target, field_sets=tuple(field_sets), task_id=task_id
+            ),
         )
         for compile_request_type, field_sets in field_sets_by_request_type.items()
     )
@@ -91,6 +97,7 @@ async def compile_bsp_target(
         TaskFinishParams(
             task_id=task_id,
             event_time=int(time.time() * 1000),
+            message=message,
             status=status,
             data=CompileReport(
                 target=request.bsp_target.bsp_target_id,
