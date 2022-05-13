64591f0a56b8a79f06d978917d091ec86e8f9aa9
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2022-05-13 12:19:47 -0500

    Rewrite `changed_integration_test.py` (#15438)
    I had a tough time understanding the original test and extending it, e.g. for https://github.com/pantsbuild/pants/pull/15396.
    
    This avoids using a `testproject`, which I've found brittle to maintain and hard to read because information is so spread out.
    
    [ci skip-rust]
    [ci skip-build-wheels]

--- testprojects/src/python/python_targets/test_direct_dependee.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
-# Licensed under the Apache License, Version 2.0 (see LICENSE).
-
-from python_targets import test_library
-
-
-def say_test():
-    test_library.say_test()
