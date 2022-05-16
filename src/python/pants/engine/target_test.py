ebe355cee039bef71282e36ad2d2bd2b93a3d2b9
Joshua Cannon <joshdcannon@gmail.com>
2022-05-16 09:38:36 -0500

    fix test
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/engine/target_test.py
+++ src/python/pants/engine/target_test.py
@@ -1,8 +1,6 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import annotations
-
 import string
 from collections import namedtuple
 from dataclasses import dataclass
