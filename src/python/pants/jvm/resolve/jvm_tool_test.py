pantsbuild/pants

commit c1a6dd1ad90b255c00defeb1e85e35e5fe90a504

[internal] jvm: split lockfile access into read and write paths (#15242)
Split `GenerateJvmLockfileFromTool.lockfile_dest` into read and write fields (`read_lockfile_dest` and `write_lockfile_dest`) to support tool lockfiles that are not exposed to users via `JvmToolBase`.

When using `JvmToolBase`, both fields will be the same, but for tool lockfiles that should not be exposed to users (for example, as done for the Scala parser in https://github.com/pantsbuild/pants/pull/15222), `read_lockfile_dest` can be set to `DEFAULT_TOOL_LOCKFILE` to enable reading from the resource, while still setting `write_lockfile_dest` to the path to use when re-generating that resource.

[ci skip-rust]

[ci skip-build-wheels]

src/python/pants/jvm/resolve/jvm_tool.py
@@ -159,7 +159,8 @@ class GenerateJvmLockfileFromTool:
     artifact_option_name: str
     lockfile_option_name: str
     resolve_name: str
-    lockfile_dest: str
+    read_lockfile_dest: str  # Path to lockfile when reading, or DEFAULT_TOOL_LOCKFILE to read from resource.
+    write_lockfile_dest: str  # Path to lockfile when generating the lockfile.
     default_lockfile_resource: tuple[str, str]
 
     @classmethod
@@ -169,7 +170,8 @@ class GenerateJvmLockfileFromTool:
             artifact_option_name=f"[{tool.options_scope}].artifacts",
             lockfile_option_name=f"[{tool.options_scope}].lockfile",
             resolve_name=tool.options_scope,
-            lockfile_dest=tool.lockfile,
+            read_lockfile_dest=tool.lockfile,
+            write_lockfile_dest=tool.lockfile,
             default_lockfile_resource=tool.default_lockfile_resource,
         )
 
@@ -183,7 +185,9 @@ async def setup_lockfile_request_from_tool(
         GatherJvmCoordinatesRequest(request.artifact_inputs, request.artifact_option_name),
     )
     return GenerateJvmLockfile(
-        artifacts=artifacts, resolve_name=request.resolve_name, lockfile_dest=request.lockfile_dest
+        artifacts=artifacts,
+        resolve_name=request.resolve_name,
+        lockfile_dest=request.write_lockfile_dest,
     )
 
 

src/python/pants/jvm/resolve/coursier_fetch.py
@@ -725,16 +725,16 @@ async def materialize_classpath_for_tool(request: ToolClasspathRequest) -> ToolC
         lockfile_req = request.lockfile
         assert lockfile_req is not None
         regen_command = f"`{GenerateLockfilesSubsystem.name} --resolve={lockfile_req.resolve_name}`"
-        if lockfile_req.lockfile_dest == DEFAULT_TOOL_LOCKFILE:
+        if lockfile_req.read_lockfile_dest == DEFAULT_TOOL_LOCKFILE:
             lockfile_bytes = importlib.resources.read_binary(
                 *lockfile_req.default_lockfile_resource
             )
             resolution = CoursierResolvedLockfile.from_serialized(lockfile_bytes)
         else:
-            lockfile_snapshot = await Get(Snapshot, PathGlobs([lockfile_req.lockfile_dest]))
+            lockfile_snapshot = await Get(Snapshot, PathGlobs([lockfile_req.read_lockfile_dest]))
             if not lockfile_snapshot.files:
                 raise ValueError(
-                    f"No lockfile found at {lockfile_req.lockfile_dest}, which is configured "
+                    f"No lockfile found at {lockfile_req.read_lockfile_dest}, which is configured "
                     f"by the option {lockfile_req.lockfile_option_name}."
                     f"Run {regen_command} to generate it."
                 )
@@ -743,7 +743,7 @@ async def materialize_classpath_for_tool(request: ToolClasspathRequest) -> ToolC
                 CoursierResolvedLockfile,
                 CoursierResolveKey(
                     name=lockfile_req.resolve_name,
-                    path=lockfile_req.lockfile_dest,
+                    path=lockfile_req.read_lockfile_dest,
                     digest=lockfile_snapshot.digest,
                 ),
             )
@@ -759,7 +759,7 @@ async def materialize_classpath_for_tool(request: ToolClasspathRequest) -> ToolC
             lockfile_inputs, LockfileContext.TOOL
         ):
             raise ValueError(
-                f"The lockfile {lockfile_req.lockfile_dest} (configured by the option "
+                f"The lockfile {lockfile_req.read_lockfile_dest} (configured by the option "
                 f"{lockfile_req.lockfile_option_name}) was generated with different requirements "
                 f"than are currently set via {lockfile_req.artifact_option_name}. Run "
                 f"{regen_command} to regenerate the lockfile."

commit 9762509ed08c48e72af81bf0698e4667681acac5

don't generate default tool lockfiles (#15488)
Do not generate tool lockfiles if the tool is using the default lockfile. The bug was probably introduced by me in #15242.

Closes https://github.com/pantsbuild/pants/issues/15460.

[ci skip-rust]

src/python/pants/jvm/resolve/jvm_tool.py
@@ -187,7 +187,9 @@ async def setup_lockfile_request_from_tool(
     return GenerateJvmLockfile(
         artifacts=artifacts,
         resolve_name=request.resolve_name,
-        lockfile_dest=request.write_lockfile_dest,
+        lockfile_dest=request.write_lockfile_dest
+        if request.read_lockfile_dest != DEFAULT_TOOL_LOCKFILE
+        else DEFAULT_TOOL_LOCKFILE,
     )
 
 

src/python/pants/jvm/resolve/jvm_tool_test.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 import textwrap
 
-from pants.core.goals.generate_lockfiles import GenerateToolLockfileSentinel
+from pants.core.goals.generate_lockfiles import DEFAULT_TOOL_LOCKFILE, GenerateToolLockfileSentinel
 from pants.core.util_rules import config_files, source_files
 from pants.core.util_rules.external_tool import rules as external_tool_rules
 from pants.engine.fs import Digest, DigestContents
@@ -20,6 +20,7 @@ from pants.jvm.resolve.jvm_tool import GenerateJvmLockfileFromTool, JvmToolBase
 from pants.jvm.target_types import JvmArtifactTarget
 from pants.jvm.util_rules import rules as util_rules
 from pants.testutil.rule_runner import PYTHON_BOOTSTRAP_ENV, QueryRule, RuleRunner
+from pants.util.ordered_set import FrozenOrderedSet
 
 
 class MockJvmTool(JvmToolBase):
@@ -36,11 +37,34 @@ class MockJvmToolLockfileSentinel(GenerateToolLockfileSentinel):
     resolve_name = MockJvmTool.options_scope
 
 
+class MockInternalToolLockfileSentinel(GenerateToolLockfileSentinel):
+    resolve_name = "mock-internal-tool"
+
+
 @rule
 def generate_test_tool_lockfile_request(
     _: MockJvmToolLockfileSentinel, tool: MockJvmTool
 ) -> GenerateJvmLockfileFromTool:
     return GenerateJvmLockfileFromTool.create(tool)
+
+
+@rule
+def generate_internal_test_tool_lockfile_request(
+    _: MockInternalToolLockfileSentinel,
+) -> GenerateJvmLockfileFromTool:
+    return GenerateJvmLockfileFromTool(
+        artifact_inputs=FrozenOrderedSet(
+            {
+                "com.google.code.gson:gson:2.9.0",
+            }
+        ),
+        artifact_option_name="n/a",
+        lockfile_option_name="n/a",
+        resolve_name=MockInternalToolLockfileSentinel.resolve_name,
+        read_lockfile_dest=DEFAULT_TOOL_LOCKFILE,
+        write_lockfile_dest="mock-write-lockfile.lock",
+        default_lockfile_resource=("pants.backend.jvm.resolve", "mock-internal-tool.lock"),
+    )
 
 
 def test_jvm_tool_base_extracts_correct_coordinates() -> None:
@@ -55,8 +79,10 @@ def test_jvm_tool_base_extracts_correct_coordinates() -> None:
             *jvm_tool.rules(),
             *lockfile_rules(),
             generate_test_tool_lockfile_request,
+            generate_internal_test_tool_lockfile_request,
             SubsystemRule(MockJvmTool),
             QueryRule(GenerateJvmLockfile, (MockJvmToolLockfileSentinel,)),
+            QueryRule(GenerateJvmLockfile, (MockInternalToolLockfileSentinel,)),
             QueryRule(DigestContents, (Digest,)),
         ],
         target_types=[JvmArtifactTarget],
@@ -89,3 +115,9 @@ def test_jvm_tool_base_extracts_correct_coordinates() -> None:
         Coordinate(group="junit", artifact="junit", version="4.13.2"),
         Coordinate(group="org.hamcrest", artifact="hamcrest-core", version="1.3"),
     ]
+
+    # Ensure that an internal-only tool will not have a lockfile generated.
+    default_lockfile_result = rule_runner.request(
+        GenerateJvmLockfile, [MockInternalToolLockfileSentinel()]
+    )
+    assert default_lockfile_result.lockfile_dest == DEFAULT_TOOL_LOCKFILE