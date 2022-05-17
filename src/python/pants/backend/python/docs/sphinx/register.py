c38bc23346392a03e60b4c9a557a94c7ee4e0993
Eric Arellano <ericarellano@me.com>
2022-05-17 15:56:48 -0500

    Change target type to `sphinx_project`
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/python/docs/sphinx/register.py
+++ src/python/pants/backend/python/docs/sphinx/register.py
@@ -2,16 +2,12 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 from pants.backend.python.docs.sphinx import sphinx_subsystem
-from pants.backend.python.docs.sphinx.target_types import (
-    SphinxSourcesGeneratorTarget,
-    SphinxSourceTarget,
-)
-from pants.backend.python.docs.sphinx.target_types import rules as target_type_rules
+from pants.backend.python.docs.sphinx.target_types import SphinxProjectTarget
 
 
 def rules():
-    return [*sphinx_subsystem.rules(), *target_type_rules()]
+    return [*sphinx_subsystem.rules()]
 
 
 def target_types():
-    return [SphinxSourceTarget, SphinxSourcesGeneratorTarget]
+    return [SphinxProjectTarget]
--- src/python/pants/backend/python/docs/sphinx/target_types.py
+++ src/python/pants/backend/python/docs/sphinx/target_types.py
@@ -1,94 +1,62 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pants.engine.rules import collect_rules, rule
+from __future__ import annotations
+
+from pathlib import PurePath
+from typing import Sequence
+
 from pants.engine.target import (
     COMMON_TARGET_FIELDS,
-    Dependencies,
+    InvalidFieldException,
     MultipleSourcesField,
-    OverridesField,
-    SingleSourceField,
     Target,
-    TargetFilesGenerator,
-    TargetFilesGeneratorSettings,
-    TargetFilesGeneratorSettingsRequest,
-    generate_file_based_overrides_field_help_message,
 )
-from pants.engine.unions import UnionRule
-
-# -----------------------------------------------------------------------------------------------
-# `sphinx_source` target
-# -----------------------------------------------------------------------------------------------
+from pants.util.strutil import softwrap
 
 
-class SphinxSourceField(SingleSourceField):
+class SphinxProjectSourcesField(MultipleSourcesField):
     # TODO: support markdown
-    expected_file_extensions = (".rst",)
+    default = ("conf.py", "**/*.rst")
+    expected_file_extensions = (".py", ".rst")
+
+    def validate_resolved_files(self, files: Sequence[str]) -> None:
+        super().validate_resolved_files(files)
+        py_files = [f for f in files if PurePath(f).suffix == ".py"]
+        if not py_files:
+            raise InvalidFieldException(
+                softwrap(
+                    f"""
+                    The {repr(self.alias)} field in target {self.address} must have exactly one
+                    `.py` file named `conf.py`, but no `.py` files found.
+                    """
+                )
+            )
+        if len(py_files) > 1:
+            raise InvalidFieldException(
+                softwrap(
+                    f"""
+                    The {repr(self.alias)} field in target {self.address} must have exactly one
+                    `.py` file named `conf.py`, but it had multiple `.py` files: {sorted(py_files)}
+                    """
+                )
+            )
+        # TODO: check if it's conf.py in the root of the dir
+        if py_files[0] == "TODO":
+            raise InvalidFieldException(
+                softwrap(
+                    """
+                    
+                    """
+                )
+            )
 
 
-class SphinxSourceTarget(Target):
-    alias = "sphinx_source"
+class SphinxProjectTarget(Target):
+    alias = "sphinx_project"
     core_fields = (
         *COMMON_TARGET_FIELDS,
-        Dependencies,
-        SphinxSourceField,
+        SphinxProjectSourcesField,
+        # TODO: Add `dependencies` when that makes sense. Likely we just want deps on Python targets.
     )
-    help = "A single Sphinx file used to generate docs."
-
-
-# -----------------------------------------------------------------------------------------------
-# `sphinx_sources` target generator
-# -----------------------------------------------------------------------------------------------
-
-
-class GeneratorSettingsRequest(TargetFilesGeneratorSettingsRequest):
-    pass
-
-
-@rule
-def generator_settings(_: GeneratorSettingsRequest) -> TargetFilesGeneratorSettings:
-    return TargetFilesGeneratorSettings(
-        # TODO: add dep inference and disable if set.
-        add_dependencies_on_all_siblings=True
-    )
-
-
-class SphinxSourcesGeneratingSourcesField(MultipleSourcesField):
-    # TODO: support markdown
-    default = ("*.rst",)
-    expected_file_extensions = (".rst",)
-
-
-class SphinxSourcesOverridesField(OverridesField):
-    help = generate_file_based_overrides_field_help_message(
-        SphinxSourceTarget.alias,
-        # TODO: make this example helpful
-        (
-            "overrides={\n"
-            '  "foo.rst": {"grpc": True},\n'
-            '  "bar.rst": {"description": "our main guide"},\n'
-            '  ("foo.rst", "bar.rst"): {"tags": ["overridden"]},\n'
-            "}"
-        ),
-    )
-
-
-class SphinxSourcesGeneratorTarget(TargetFilesGenerator):
-    alias = "sphinx_sources"
-    core_fields = (
-        *COMMON_TARGET_FIELDS,
-        SphinxSourcesGeneratingSourcesField,
-        SphinxSourcesOverridesField,
-    )
-    generated_target_cls = SphinxSourceTarget
-    copied_fields = COMMON_TARGET_FIELDS
-    moved_fields = (Dependencies,)
-    settings_request_cls = GeneratorSettingsRequest
-    help = "Generate a `sphinx_source` target for each file in the `sources` field."
-
-
-def rules():
-    return [
-        *collect_rules(),
-        UnionRule(TargetFilesGeneratorSettingsRequest, GeneratorSettingsRequest),
-    ]
+    help = "A website generated via Sphinx."
