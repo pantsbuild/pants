7f8017cef444d2f82c3ce0f04dc6e8a9796019e6
Eric Arellano <ericarellano@me.com>
2022-05-20 08:07:14 -0500

    Stop using custom workdir for changed_integration_test.py
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/vcs/changed_integration_test.py
+++ src/python/pants/vcs/changed_integration_test.py
@@ -10,31 +10,31 @@ from typing import Iterator
 
 import pytest
 
-from pants.base.build_environment import get_buildroot
 from pants.testutil.pants_integration_test import run_pants
-from pants.util.contextutil import environment_as, temporary_dir
+from pants.util.contextutil import environment_as
 from pants.vcs.changed import DependeesOption
 
 
 def _run_git(command: list[str]) -> None:
-    subprocess.run(
-        ["git", *command], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-    )
+    subprocess.run(["git", *command], check=True)
 
 
-@pytest.fixture
-def repo() -> Iterator[str]:
-    with temporary_dir(root_dir=get_buildroot()) as worktree, environment_as(
-        GIT_CONFIG_GLOBAL="/dev/null"
-    ):
+@pytest.fixture(autouse=True)
+def init_repo() -> Iterator[None]:
+    with environment_as(GIT_CONFIG_GLOBAL="/dev/null"):
+        if Path(".git").exists():
+            reset_edits()
+            yield
+            return
+
         _run_git(["init"])
         _run_git(["config", "user.email", "you@example.com"])
         _run_git(["config", "user.name", "Your Name"])
 
         project = {
             ".gitignore": dedent(
-                f"""\
-                {worktree}
+                """\
+                .pants.d
                 .pids
                 """
             ),
@@ -72,8 +72,8 @@ def repo() -> Iterator[str]:
             create_file(fp, content)
 
         _run_git(["add", "."])
-        _run_git(["commit", "-m", "blah"])
-        yield worktree
+        _run_git(["commit", "-m", "init repo"])
+        yield
 
 
 def create_file(fp: str, content: str) -> None:
@@ -96,7 +96,6 @@ def reset_edits() -> None:
 
 
 def assert_list_stdout(
-    workdir: str,
     expected: list[str],
     dependees: DependeesOption = DependeesOption.NONE,
     *,
@@ -109,7 +108,6 @@ def assert_list_stdout(
             f"--changed-dependees={dependees.value}",
             "list",
         ],
-        workdir=workdir,
         # We must set `hermetic=False` for some reason.
         hermetic=False,
     )
@@ -117,35 +115,32 @@ def assert_list_stdout(
     assert sorted(result.stdout.strip().splitlines()) == sorted(expected)
 
 
-def test_no_changes(repo: str) -> None:
-    assert_list_stdout(repo, [])
+def test_no_changes() -> None:
+    assert_list_stdout([])
 
 
-def test_change_no_deps(repo: str) -> None:
+def test_change_no_deps() -> None:
     append_to_file("standalone.sh", "# foo")
     for dependees in DependeesOption:
-        assert_list_stdout(repo, ["//:standalone"], dependees=dependees)
+        assert_list_stdout(["//:standalone"], dependees=dependees)
 
 
-def test_change_transitive_dep(repo: str) -> None:
+def test_change_transitive_dep() -> None:
     append_to_file("transitive.sh", "# foo")
-    assert_list_stdout(repo, ["//transitive.sh:lib"])
+    assert_list_stdout(["//transitive.sh:lib"])
+    assert_list_stdout(["//dep.sh:lib", "//transitive.sh:lib"], dependees=DependeesOption.DIRECT)
     assert_list_stdout(
-        repo, ["//dep.sh:lib", "//transitive.sh:lib"], dependees=DependeesOption.DIRECT
-    )
-    assert_list_stdout(
-        repo,
         ["//app.sh:lib", "//dep.sh:lib", "//transitive.sh:lib"],
         dependees=DependeesOption.TRANSITIVE,
     )
 
 
-def test_unowned_file(repo: str) -> None:
+def test_unowned_file() -> None:
     create_file("dir/some_file.ext", "")
-    assert_list_stdout(repo, [])
+    assert_list_stdout([])
 
 
-def test_delete_generated_target(repo: str) -> None:
+def test_delete_generated_target() -> None:
     """If a generated target is deleted, we claim the target generator was modified.
 
     It's unlikely these are actually the semantics we want...See:
@@ -154,26 +149,26 @@ def test_delete_generated_target(repo: str) -> None:
     """
     delete_file("transitive.sh")
     for dependees in DependeesOption:
-        assert_list_stdout(repo, ["//:lib"], dependees=dependees)
+        assert_list_stdout(["//:lib"], dependees=dependees)
 
     # Make sure that our fix for https://github.com/pantsbuild/pants/issues/15544 does not break
     # this test when using `--tag`.
     for dependees in (DependeesOption.NONE, DependeesOption.TRANSITIVE):
-        assert_list_stdout(repo, ["//:lib"], dependees=dependees, extra_args=["--tag=a"])
+        assert_list_stdout(["//:lib"], dependees=dependees, extra_args=["--tag=a"])
 
     # If we also edit a sibling generated target, we should still (for now at least) include the
     # target generator.
     append_to_file("app.sh", "# foo")
     for dependees in DependeesOption:
-        assert_list_stdout(repo, ["//:lib", "//app.sh:lib"], dependees=dependees)
+        assert_list_stdout(["//:lib", "//app.sh:lib"], dependees=dependees)
 
 
-def test_delete_atom_target(repo: str) -> None:
+def test_delete_atom_target() -> None:
     delete_file("standalone.sh")
-    assert_list_stdout(repo, ["//:standalone"])
+    assert_list_stdout(["//:standalone"])
 
 
-def test_change_build_file(repo: str) -> None:
+def test_change_build_file() -> None:
     """Every target in a BUILD file is changed when it's edited.
 
     This is because we don't know what the target was like before-hand, so we're overly
@@ -181,25 +176,23 @@ def test_change_build_file(repo: str) -> None:
     """
     append_to_file("BUILD", "# foo")
     # Note that the target generator `//:lib` does not show up.
-    assert_list_stdout(
-        repo, ["//app.sh:lib", "//dep.sh:lib", "//transitive.sh:lib", "//:standalone"]
-    )
+    assert_list_stdout(["//app.sh:lib", "//dep.sh:lib", "//transitive.sh:lib", "//:standalone"])
 
 
-def test_different_build_file_changed(repo: str) -> None:
+def test_different_build_file_changed() -> None:
     """Only invalidate if the BUILD file where a target is defined has changed, even if the changed
     BUILD file is in the same directory."""
     create_file("BUILD.other", "")
-    assert_list_stdout(repo, [])
+    assert_list_stdout([])
 
 
-def test_tag_filtering(repo: str) -> None:
+def test_tag_filtering() -> None:
     append_to_file("dep.sh", "# foo")
     append_to_file("standalone.sh", "# foo")
-    assert_list_stdout(repo, ["//dep.sh:lib"], extra_args=["--tag=+b"])
-    assert_list_stdout(repo, ["//:standalone"], extra_args=["--tag=-b"])
+    assert_list_stdout(["//dep.sh:lib"], extra_args=["--tag=+b"])
+    assert_list_stdout(["//:standalone"], extra_args=["--tag=-b"])
     assert_list_stdout(
-        repo, ["//dep.sh:lib"], dependees=DependeesOption.TRANSITIVE, extra_args=["--tag=+b"]
+        ["//dep.sh:lib"], dependees=DependeesOption.TRANSITIVE, extra_args=["--tag=+b"]
     )
 
     # Regression test for https://github.com/pantsbuild/pants/issues/14977: make sure a generated
@@ -207,7 +200,6 @@ def test_tag_filtering(repo: str) -> None:
     reset_edits()
     append_to_file("transitive.sh", "# foo")
     assert_list_stdout(
-        repo,
         ["//app.sh:lib", "//transitive.sh:lib"],
         dependees=DependeesOption.TRANSITIVE,
         extra_args=["--tag=-b"],
@@ -220,7 +212,7 @@ def test_tag_filtering(repo: str) -> None:
     # find the dependees of `dep.sh`, like `app.sh`, and only then apply the filter.
     reset_edits()
     append_to_file("dep.sh", "# foo")
-    assert_list_stdout(repo, [], dependees=DependeesOption.NONE, extra_args=["--tag=a"])
+    assert_list_stdout([], dependees=DependeesOption.NONE, extra_args=["--tag=a"])
     assert_list_stdout(
-        repo, ["//app.sh:lib"], dependees=DependeesOption.TRANSITIVE, extra_args=["--tag=a"]
+        ["//app.sh:lib"], dependees=DependeesOption.TRANSITIVE, extra_args=["--tag=a"]
     )
