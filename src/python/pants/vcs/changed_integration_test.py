pantsbuild/pants

commit 4b1a6293a8948e1bbb19443463661da88e49fc20

Don't error when `dir:` and `dir::` have no targets, as long as the directories exist (#15570)
This is necessary so that `./pants count-loc ::` works in a repository without any BUILD files.

We still, however, want to error when doing `./pants count-loc does-not-exist::`. That can be turned off via `--unmatched-cli-globs`. To pull this off, we create a second set of `PathGlobs` solely for validation. Closes https://github.com/pantsbuild/pants/issues/15558.

[ci skip-rust]
[ci skip-build-wheels]

src/python/pants/engine/internals/graph.py
@@ -334,22 +334,14 @@ async def resolve_targets(
 
 @rule(desc="Find all targets in the project", level=LogLevel.DEBUG)
 async def find_all_targets(_: AllTargetsRequest) -> AllTargets:
-    tgts = await Get(
-        Targets,
-        SpecsWithoutFileOwners(
-            recursive_globs=(RecursiveGlobSpec("", error_if_no_target_matches=False),)
-        ),
-    )
+    tgts = await Get(Targets, SpecsWithoutFileOwners(recursive_globs=(RecursiveGlobSpec(""),)))
     return AllTargets(tgts)
 
 
 @rule(desc="Find all targets in the project", level=LogLevel.DEBUG)
 async def find_all_unexpanded_targets(_: AllTargetsRequest) -> AllUnexpandedTargets:
     tgts = await Get(
-        UnexpandedTargets,
-        SpecsWithoutFileOwners(
-            recursive_globs=(RecursiveGlobSpec("", error_if_no_target_matches=False),)
-        ),
+        UnexpandedTargets, SpecsWithoutFileOwners(recursive_globs=(RecursiveGlobSpec(""),))
     )
     return AllUnexpandedTargets(tgts)
 

src/python/pants/backend/go/target_type_rules.py
@@ -276,9 +276,7 @@ async def determine_main_pkg_for_go_binary(
             )
         return GoBinaryMainPackage(wrapped_specified_tgt.target.address)
 
-    candidate_targets = await Get(
-        Targets, Specs(dir_globs=(DirGlobSpec(addr.spec_path, error_if_no_target_matches=False),))
-    )
+    candidate_targets = await Get(Targets, Specs(dir_globs=(DirGlobSpec(addr.spec_path),)))
     relevant_pkg_targets = [
         tgt
         for tgt in candidate_targets

src/python/pants/backend/terraform/dependency_inference.py
@@ -13,6 +13,7 @@ from pants.backend.python.subsystems.setup import PythonSetup
 from pants.backend.python.target_types import EntryPoint
 from pants.backend.python.util_rules.pex import PexRequest, VenvPex, VenvPexProcess
 from pants.backend.terraform.target_types import TerraformModuleSourcesField
+from pants.base.glob_match_error_behavior import GlobMatchErrorBehavior
 from pants.base.specs import DirGlobSpec, Specs
 from pants.core.goals.generate_lockfiles import GenerateToolLockfileSentinel
 from pants.core.goals.tailor import group_by_dir
@@ -140,9 +141,8 @@ async def infer_terraform_module_dependencies(
     candidate_targets = await Get(
         Targets,
         Specs(
-            dir_globs=tuple(
-                DirGlobSpec(path, error_if_no_target_matches=False) for path in candidate_spec_paths
-            )
+            dir_globs=tuple(DirGlobSpec(path) for path in candidate_spec_paths),
+            unmatched_glob_behavior=GlobMatchErrorBehavior.ignore,
         ),
     )
     # TODO: Need to either implement the standard ambiguous dependency logic or ban >1 terraform_module

src/python/pants/base/specs.py
@@ -127,7 +127,6 @@ class DirGlobSpec(Spec):
     """
 
     directory: str
-    error_if_no_target_matches: bool = True
 
     def __str__(self) -> str:
         return f"{self.directory}:"
@@ -150,7 +149,6 @@ class RecursiveGlobSpec(Spec):
     """
 
     directory: str
-    error_if_no_target_matches: bool = True
 
     def __str__(self) -> str:
         return f"{self.directory}::"
@@ -173,7 +171,6 @@ class AncestorGlobSpec(Spec):
     """
 
     directory: str
-    error_if_no_target_matches: bool = False
 
     def __str__(self) -> str:
         return f"{self.directory}^"
@@ -192,6 +189,7 @@ class Specs:
     recursive_globs: tuple[RecursiveGlobSpec, ...] = ()
     ancestor_globs: tuple[AncestorGlobSpec, ...] = ()
 
+    unmatched_glob_behavior: GlobMatchErrorBehavior = GlobMatchErrorBehavior.error
     filter_by_global_options: bool = False
     from_change_detection: bool = False
 
@@ -201,6 +199,7 @@ class Specs:
         specs: Iterable[Spec],
         *,
         convert_dir_literal_to_address_literal: bool,
+        unmatched_glob_behavior: GlobMatchErrorBehavior = GlobMatchErrorBehavior.error,
         filter_by_global_options: bool = False,
         from_change_detection: bool = False,
     ) -> Specs:
@@ -244,6 +243,7 @@ class Specs:
             tuple(dir_globs),
             tuple(recursive_globs),
             tuple(ancestor_globs),
+            unmatched_glob_behavior=unmatched_glob_behavior,
             filter_by_global_options=filter_by_global_options,
             from_change_detection=from_change_detection,
         )
@@ -293,6 +293,7 @@ class SpecsWithoutFileOwners:
     recursive_globs: tuple[RecursiveGlobSpec, ...] = ()
     ancestor_globs: tuple[AncestorGlobSpec, ...] = ()
 
+    unmatched_glob_behavior: GlobMatchErrorBehavior = GlobMatchErrorBehavior.error
     filter_by_global_options: bool = False
 
     @classmethod
@@ -302,6 +303,7 @@ class SpecsWithoutFileOwners:
             specs.dir_globs,
             specs.recursive_globs,
             specs.ancestor_globs,
+            unmatched_glob_behavior=specs.unmatched_glob_behavior,
             filter_by_global_options=specs.filter_by_global_options,
         )
 
@@ -310,25 +312,49 @@ class SpecsWithoutFileOwners:
         yield from self.recursive_globs
         yield from self.ancestor_globs
 
-    def to_build_file_path_globs(
+    def to_build_file_path_globs_tuple(
         self, *, build_patterns: Iterable[str], build_ignore_patterns: Iterable[str]
-    ) -> PathGlobs:
-        includes: set[str] = set()
+    ) -> tuple[PathGlobs, PathGlobs]:
+        """Returns `PathGlobs` for the actual BUILD files, along with another solely used to
+        validate that the directories exist.
+
+        The second `PathGlobs` is necessary so that we can error on directories that don't actually
+        exist, yet we don't error if the directory simply has no targets. See
+        https://github.com/pantsbuild/pants/issues/15558.
+        """
+        build_includes: set[str] = set()
+        validation_includes: set[str] = set()
         for spec in (*self.dir_globs, *self.ancestor_globs):
             spec = cast("DirGlobSpec | AncestorGlobSpec", spec)
-            includes.update(
+            validation_includes.add(os.path.join(spec.directory, "*"))
+            build_includes.update(
                 os.path.join(f, pattern)
                 for pattern in build_patterns
                 for f in recursive_dirname(spec.directory)
             )
+
         for recursive_spec in self.recursive_globs:
+            validation_includes.add(os.path.join(recursive_spec.directory, "**"))
             for pattern in build_patterns:
-                includes.update(
+                build_includes.update(
                     os.path.join(f, pattern) for f in recursive_dirname(recursive_spec.directory)
                 )
-                includes.add(os.path.join(recursive_spec.directory, "**", pattern))
+                build_includes.add(os.path.join(recursive_spec.directory, "**", pattern))
+
         ignores = (f"!{p}" for p in build_ignore_patterns)
-        return PathGlobs((*includes, *ignores))
+        build_path_globs = PathGlobs((*build_includes, *ignores))
+        validation_path_globs = (
+            PathGlobs(())
+            if self.unmatched_glob_behavior == GlobMatchErrorBehavior.ignore
+            else PathGlobs(
+                (*validation_includes, *ignores),
+                glob_match_error_behavior=self.unmatched_glob_behavior,
+                # We validate that _every_ glob is valid.
+                conjunction=GlobExpansionConjunction.all_match,
+                description_of_origin="CLI arguments",
+            )
+        )
+        return build_path_globs, validation_path_globs
 
 
 @dataclass(frozen=True)
@@ -342,6 +368,7 @@ class SpecsWithOnlyFileOwners:
     file_literals: tuple[FileLiteralSpec, ...] = ()
     file_globs: tuple[FileGlobSpec, ...] = ()
 
+    unmatched_glob_behavior: GlobMatchErrorBehavior = GlobMatchErrorBehavior.error
     filter_by_global_options: bool = False
     from_change_detection: bool = False
 
@@ -350,6 +377,7 @@ class SpecsWithOnlyFileOwners:
         return SpecsWithOnlyFileOwners(
             specs.file_literals,
             specs.file_globs,
+            unmatched_glob_behavior=specs.unmatched_glob_behavior,
             filter_by_global_options=specs.filter_by_global_options,
             from_change_detection=specs.from_change_detection,
         )
@@ -357,33 +385,31 @@ class SpecsWithOnlyFileOwners:
     def _generate_path_globs(
         self,
         specs: Iterable[FileLiteralSpec | FileGlobSpec],
-        glob_match_error_behavior: GlobMatchErrorBehavior,
     ) -> PathGlobs:
-        if self.from_change_detection:
-            glob_match_error_behavior = GlobMatchErrorBehavior.ignore
+        unmatched_glob_behavior = (
+            GlobMatchErrorBehavior.ignore
+            if self.from_change_detection
+            else self.unmatched_glob_behavior
+        )
         return PathGlobs(
             globs=(s.to_glob() for s in specs),
-            glob_match_error_behavior=glob_match_error_behavior,
+            glob_match_error_behavior=unmatched_glob_behavior,
             # We validate that _every_ glob is valid.
             conjunction=GlobExpansionConjunction.all_match,
             description_of_origin=(
                 None
-                if glob_match_error_behavior == GlobMatchErrorBehavior.ignore
-                else "file/directory arguments"
+                if unmatched_glob_behavior == GlobMatchErrorBehavior.ignore
+                else "CLI arguments"
             ),
         )
 
-    def path_globs_for_spec(
-        self,
-        spec: FileLiteralSpec | FileGlobSpec,
-        glob_match_error_behavior: GlobMatchErrorBehavior,
-    ) -> PathGlobs:
+    def path_globs_for_spec(self, spec: FileLiteralSpec | FileGlobSpec) -> PathGlobs:
         """Generate PathGlobs for the specific spec."""
-        return self._generate_path_globs([spec], glob_match_error_behavior)
+        return self._generate_path_globs([spec])
 
-    def to_path_globs(self, glob_match_error_behavior: GlobMatchErrorBehavior) -> PathGlobs:
+    def to_path_globs(self) -> PathGlobs:
         """Generate a single PathGlobs for the instance."""
-        return self._generate_path_globs(self.all_specs(), glob_match_error_behavior)
+        return self._generate_path_globs(self.all_specs())
 
     def all_specs(self) -> Iterator[FileLiteralSpec | FileGlobSpec]:
         yield from self.file_literals

src/python/pants/base/specs_parser.py
@@ -8,6 +8,7 @@ from pathlib import Path, PurePath
 from typing import Iterable
 
 from pants.base.build_environment import get_buildroot
+from pants.base.glob_match_error_behavior import GlobMatchErrorBehavior
 from pants.base.specs import (
     AddressLiteralSpec,
     DirGlobSpec,
@@ -101,9 +102,15 @@ class SpecsParser:
             return FileLiteralSpec(spec_path)
         return DirLiteralSpec(spec_path)
 
-    def parse_specs(self, specs: Iterable[str]) -> Specs:
+    def parse_specs(
+        self,
+        specs: Iterable[str],
+        *,
+        unmatched_glob_behavior: GlobMatchErrorBehavior = GlobMatchErrorBehavior.error,
+    ) -> Specs:
         return Specs.create(
             (self.parse_spec(spec) for spec in specs),
             convert_dir_literal_to_address_literal=True,
+            unmatched_glob_behavior=unmatched_glob_behavior,
             filter_by_global_options=True,
         )

src/python/pants/base/specs_test.py
@@ -11,9 +11,17 @@ from pants.base.specs import (
 )
 
 
-def assert_build_file_globs(specs: SpecsWithoutFileOwners, expected: set[str]) -> None:
-    result = specs.to_build_file_path_globs(build_patterns=["BUILD"], build_ignore_patterns=[])
-    assert set(result.globs) == expected
+def assert_build_file_globs(
+    specs: SpecsWithoutFileOwners,
+    *,
+    expected_build_globs: set[str],
+    expected_validation_globs: set[str],
+) -> None:
+    build_path_globs, validation_path_globs = specs.to_build_file_path_globs_tuple(
+        build_patterns=["BUILD"], build_ignore_patterns=[]
+    )
+    assert set(build_path_globs.globs) == expected_build_globs
+    assert set(validation_path_globs.globs) == expected_validation_globs
 
 
 def test_dir_glob() -> None:
@@ -24,13 +32,19 @@ def test_dir_glob() -> None:
     assert spec.matches_target("dir/subdir/nested") is False
     assert spec.matches_target("another/subdir") is False
     assert_build_file_globs(
-        SpecsWithoutFileOwners(dir_globs=(spec,)), {"BUILD", "dir/BUILD", "dir/subdir/BUILD"}
+        SpecsWithoutFileOwners(dir_globs=(spec,)),
+        expected_build_globs={"BUILD", "dir/BUILD", "dir/subdir/BUILD"},
+        expected_validation_globs={"dir/subdir/*"},
     )
 
     spec = DirGlobSpec("")
     assert spec.matches_target("") is True
     assert spec.matches_target("dir") is False
-    assert_build_file_globs(SpecsWithoutFileOwners(dir_globs=(spec,)), {"BUILD"})
+    assert_build_file_globs(
+        SpecsWithoutFileOwners(dir_globs=(spec,)),
+        expected_build_globs={"BUILD"},
+        expected_validation_globs={"*"},
+    )
 
 
 def test_recursive_glob() -> None:
@@ -43,14 +57,19 @@ def test_recursive_glob() -> None:
     assert spec.matches_target("another/subdir") is False
     assert_build_file_globs(
         SpecsWithoutFileOwners(recursive_globs=(spec,)),
-        {"BUILD", "dir/BUILD", "dir/subdir/BUILD", "dir/subdir/**/BUILD"},
+        expected_build_globs={"BUILD", "dir/BUILD", "dir/subdir/BUILD", "dir/subdir/**/BUILD"},
+        expected_validation_globs={"dir/subdir/**"},
     )
 
     spec = RecursiveGlobSpec("")
     assert spec.matches_target("") is True
     assert spec.matches_target("dir") is True
     assert spec.matches_target("another_dir") is True
-    assert_build_file_globs(SpecsWithoutFileOwners(recursive_globs=(spec,)), {"BUILD", "**/BUILD"})
+    assert_build_file_globs(
+        SpecsWithoutFileOwners(recursive_globs=(spec,)),
+        expected_build_globs={"BUILD", "**/BUILD"},
+        expected_validation_globs={"**"},
+    )
 
 
 def test_ancestor_glob() -> None:
@@ -61,10 +80,16 @@ def test_ancestor_glob() -> None:
     assert spec.matches_target("dir/subdir/nested") is False
     assert spec.matches_target("another/subdir") is False
     assert_build_file_globs(
-        SpecsWithoutFileOwners(ancestor_globs=(spec,)), {"BUILD", "dir/BUILD", "dir/subdir/BUILD"}
+        SpecsWithoutFileOwners(ancestor_globs=(spec,)),
+        expected_build_globs={"BUILD", "dir/BUILD", "dir/subdir/BUILD"},
+        expected_validation_globs={"dir/subdir/*"},
     )
 
     spec = AncestorGlobSpec("")
     assert spec.matches_target("") is True
     assert spec.matches_target("dir") is False
-    assert_build_file_globs(SpecsWithoutFileOwners(ancestor_globs=(spec,)), {"BUILD"})
+    assert_build_file_globs(
+        SpecsWithoutFileOwners(ancestor_globs=(spec,)),
+        expected_build_globs={"BUILD"},
+        expected_validation_globs={"*"},
+    )

src/python/pants/engine/internals/specs_rules.py
@@ -11,7 +11,6 @@ from collections import defaultdict
 from pathlib import PurePath
 from typing import Iterable
 
-from pants.base.exceptions import ResolveError
 from pants.base.specs import (
     AddressLiteralSpec,
     FileLiteralSpec,
@@ -47,8 +46,8 @@ from pants.engine.target import (
     WrappedTarget,
 )
 from pants.engine.unions import UnionMembership
-from pants.option.global_options import GlobalOptions, OwnersNotFoundBehavior, UnmatchedCliGlobs
-from pants.util.docutil import bin_name, doc_url
+from pants.option.global_options import GlobalOptions, OwnersNotFoundBehavior
+from pants.util.docutil import bin_name
 from pants.util.logging import LogLevel
 from pants.util.ordered_set import FrozenOrderedSet, OrderedSet
 from pants.util.strutil import bullet_list
@@ -119,14 +118,15 @@ async def addresses_from_specs_without_file_owners(
         return Addresses(matched_addresses)
 
     # Resolve all globs.
-    build_file_paths = await Get(
-        Paths,
-        PathGlobs,
-        specs.to_build_file_path_globs(
-            build_patterns=build_file_options.patterns,
-            build_ignore_patterns=build_file_options.ignores,
-        ),
+    build_file_globs, validation_globs = specs.to_build_file_path_globs_tuple(
+        build_patterns=build_file_options.patterns,
+        build_ignore_patterns=build_file_options.ignores,
     )
+    build_file_paths, _ = await MultiGet(
+        Get(Paths, PathGlobs, build_file_globs),
+        Get(Paths, PathGlobs, validation_globs),
+    )
+
     dirnames = {os.path.dirname(f) for f in build_file_paths.files}
     address_families = await MultiGet(Get(AddressFamily, AddressFamilyDir(d)) for d in dirnames)
     base_addresses = Addresses(
@@ -155,23 +155,6 @@ async def addresses_from_specs_without_file_owners(
                 if filtering_disabled or specs_filter.matches(tgt)
             )
 
-    unmatched_globs = [
-        glob
-        for glob in specs.glob_specs()
-        if glob not in matched_globs and glob.error_if_no_target_matches
-    ]
-    if unmatched_globs:
-        glob_description = (
-            f"the glob `{unmatched_globs[0]}`"
-            if len(unmatched_globs) == 1
-            else f"these globs: {sorted(str(glob) for glob in unmatched_globs)}"
-        )
-        raise ResolveError(
-            f"No targets found for {glob_description}\n\n"
-            f"Do targets exist in those directories? Maybe run `{bin_name()} tailor` to generate "
-            f"BUILD files? See {doc_url('targets')} about targets and BUILD files."
-        )
-
     return Addresses(sorted(matched_addresses))
 
 
@@ -181,29 +164,17 @@ async def addresses_from_specs_without_file_owners(
 
 
 @rule
-def extract_unmatched_cli_globs(global_options: GlobalOptions) -> UnmatchedCliGlobs:
-    return global_options.unmatched_cli_globs
-
-
-@rule
 def extract_owners_not_found_behavior(global_options: GlobalOptions) -> OwnersNotFoundBehavior:
     return global_options.owners_not_found_behavior
 
 
 @rule
 async def addresses_from_specs_with_only_file_owners(
-    specs: SpecsWithOnlyFileOwners,
-    unmatched_cli_globs: UnmatchedCliGlobs,
-    owners_not_found_behavior: OwnersNotFoundBehavior,
+    specs: SpecsWithOnlyFileOwners, owners_not_found_behavior: OwnersNotFoundBehavior
 ) -> Addresses:
     """Find the owner(s) for each spec."""
     paths_per_include = await MultiGet(
-        Get(
-            Paths,
-            PathGlobs,
-            specs.path_globs_for_spec(spec, unmatched_cli_globs.to_glob_match_error_behavior()),
-        )
-        for spec in specs.all_specs()
+        Get(Paths, PathGlobs, specs.path_globs_for_spec(spec)) for spec in specs.all_specs()
     )
     owners_per_include = await MultiGet(
         Get(
@@ -262,9 +233,7 @@ def setup_specs_filter(global_options: GlobalOptions) -> SpecsFilter:
 
 
 @rule(desc="Find all sources from input specs", level=LogLevel.DEBUG)
-async def resolve_specs_snapshot(
-    specs: Specs, unmatched_cli_globs: UnmatchedCliGlobs
-) -> SpecsSnapshot:
+async def resolve_specs_snapshot(specs: Specs) -> SpecsSnapshot:
     """Resolve all files matching the given specs.
 
     All matched targets will use their `sources` field. Certain specs like FileLiteralSpec will
@@ -305,7 +274,7 @@ async def resolve_specs_snapshot(
         target_less_digest = await Get(
             Digest,
             PathGlobs,
-            with_files_owners.to_path_globs(unmatched_cli_globs.to_glob_match_error_behavior()),
+            with_files_owners.to_path_globs(),
         )
         digests.append(target_less_digest)
 

src/python/pants/engine/internals/specs_rules_test.py
@@ -10,6 +10,7 @@ from typing import Iterable, Type
 import pytest
 
 from pants.base.exceptions import ResolveError
+from pants.base.glob_match_error_behavior import GlobMatchErrorBehavior
 from pants.base.specs import (
     AddressLiteralSpec,
     AncestorGlobSpec,
@@ -132,9 +133,15 @@ def rule_runner() -> RuleRunner:
 def resolve_specs_without_file_owners(
     rule_runner: RuleRunner,
     specs: Iterable[Spec],
+    ignore_nonexistent: bool = False,
 ) -> list[Address]:
     specs_obj = Specs.create(
-        specs, filter_by_global_options=True, convert_dir_literal_to_address_literal=True
+        specs,
+        filter_by_global_options=True,
+        convert_dir_literal_to_address_literal=True,
+        unmatched_glob_behavior=(
+            GlobMatchErrorBehavior.ignore if ignore_nonexistent else GlobMatchErrorBehavior.error
+        ),
     )
     result = rule_runner.request(Addresses, [SpecsWithoutFileOwners.from_specs(specs_obj)])
     return sorted(result)
@@ -353,59 +360,46 @@ def test_specs_without_file_owners_do_not_exist(rule_runner: RuleRunner) -> None
         {"real/f.txt": "", "real/BUILD": "target(sources=['f.txt'])", "empty/BUILD": "# empty"}
     )
 
-    def assert_resolve_error(specs: Iterable[Spec], *, expected: str) -> None:
+    def assert_resolve_error(spec: Spec, *, expected: str) -> None:
         with engine_error(contains=expected):
-            resolve_specs_without_file_owners(rule_runner, specs)
+            resolve_specs_without_file_owners(rule_runner, [spec])
 
-    # Literal addresses require for the relevant BUILD file to exist and for the target to be
-    # resolved.
+    def assert_does_not_error(spec: Spec, *, ignore_nonexistent: bool = False) -> None:
+        assert not resolve_specs_without_file_owners(
+            rule_runner, [spec], ignore_nonexistent=ignore_nonexistent
+        )
+
+    # Literal addresses require for the target to beresolved.
     assert_resolve_error(
-        [AddressLiteralSpec("fake", "tgt")], expected="'fake' does not exist on disk"
+        AddressLiteralSpec("fake", "tgt"), expected="'fake' does not exist on disk"
     )
     assert_resolve_error(
-        [AddressLiteralSpec("fake/f.txt", "tgt")],
+        AddressLiteralSpec("fake/f.txt", "tgt"),
         expected="'fake/f.txt' does not exist on disk",
     )
     did_you_mean = ResolveError.did_you_mean(
         bad_name="fake_tgt", known_names=["real"], namespace="real"
     )
-    assert_resolve_error([AddressLiteralSpec("real", "fake_tgt")], expected=str(did_you_mean))
-    assert_resolve_error([AddressLiteralSpec("real/f.txt", "fake_tgt")], expected=str(did_you_mean))
+    assert_resolve_error(AddressLiteralSpec("real", "fake_tgt"), expected=str(did_you_mean))
+    assert_resolve_error(AddressLiteralSpec("real/f.txt", "fake_tgt"), expected=str(did_you_mean))
 
     assert_resolve_error(
-        [DirGlobSpec("fake")],
-        expected="No targets found for the glob `fake:`",
+        DirGlobSpec("fake"), expected='Unmatched glob from CLI arguments: "fake/*"'
     )
-    assert_resolve_error([DirGlobSpec("empty")], expected="No targets found for the glob `empty:`")
-    assert not resolve_specs_without_file_owners(
-        rule_runner, [DirGlobSpec("fake", error_if_no_target_matches=False)]
-    )
-    assert not resolve_specs_without_file_owners(
-        rule_runner, [DirGlobSpec("empty", error_if_no_target_matches=False)]
-    )
+    assert_does_not_error(DirGlobSpec("empty"))
+    assert_does_not_error(DirGlobSpec("fake"), ignore_nonexistent=True)
 
     assert_resolve_error(
-        [RecursiveGlobSpec("fake"), RecursiveGlobSpec("empty")],
-        expected="No targets found for these globs: ['empty::', 'fake::']",
+        RecursiveGlobSpec("fake"), expected='Unmatched glob from CLI arguments: "fake/**"'
     )
-    assert not resolve_specs_without_file_owners(
-        rule_runner,
-        [
-            RecursiveGlobSpec("fake", error_if_no_target_matches=False),
-            RecursiveGlobSpec("empty", error_if_no_target_matches=False),
-        ],
-    )
+    assert_does_not_error(RecursiveGlobSpec("empty"))
+    assert_does_not_error(RecursiveGlobSpec("fake"), ignore_nonexistent=True)
 
-    assert not resolve_specs_without_file_owners(
-        rule_runner, [AncestorGlobSpec("fake"), AncestorGlobSpec("empty")]
-    )
     assert_resolve_error(
-        [
-            AncestorGlobSpec("fake", error_if_no_target_matches=True),
-            AncestorGlobSpec("empty", error_if_no_target_matches=True),
-        ],
-        expected="No targets found for these globs: ['empty^', 'fake^']",
+        AncestorGlobSpec("fake"), expected='Unmatched glob from CLI arguments: "fake/*"'
     )
+    assert_does_not_error(AncestorGlobSpec("empty"))
+    assert_does_not_error(AncestorGlobSpec("fake"), ignore_nonexistent=True)
 
 
 def test_specs_without_file_owners_generated_target_does_not_belong_to_generator(
@@ -549,11 +543,15 @@ def test_specs_without_file_owners_parametrize(
 
 
 def resolve_specs_with_only_file_owners(
-    rule_runner: RuleRunner,
-    specs: Iterable[Spec],
+    rule_runner: RuleRunner, specs: Iterable[Spec], ignore_nonexistent: bool = False
 ) -> list[Address]:
     specs_obj = Specs.create(
-        specs, filter_by_global_options=True, convert_dir_literal_to_address_literal=True
+        specs,
+        filter_by_global_options=True,
+        convert_dir_literal_to_address_literal=True,
+        unmatched_glob_behavior=(
+            GlobMatchErrorBehavior.ignore if ignore_nonexistent else GlobMatchErrorBehavior.error
+        ),
     )
     result = rule_runner.request(Addresses, [SpecsWithOnlyFileOwners.from_specs(specs_obj)])
     return sorted(result)
@@ -618,11 +616,10 @@ def test_specs_with_only_file_owners_glob(rule_runner: RuleRunner) -> None:
 
 def test_specs_with_only_file_owners_nonexistent_file(rule_runner: RuleRunner) -> None:
     spec = FileLiteralSpec("demo/fake.txt")
-    with engine_error(contains='Unmatched glob from file/directory arguments: "demo/fake.txt"'):
+    with engine_error(contains='Unmatched glob from CLI arguments: "demo/fake.txt"'):
         resolve_specs_with_only_file_owners(rule_runner, [spec])
 
-    rule_runner.set_options(["--unmatched-cli-globs=ignore"])
-    assert not resolve_specs_with_only_file_owners(rule_runner, [spec])
+    assert not resolve_specs_with_only_file_owners(rule_runner, [spec], ignore_nonexistent=True)
 
 
 def test_specs_with_only_file_owners_no_owner(rule_runner: RuleRunner) -> None:

commit a7a243b7310d1975723c03b0883a9e59c7c396e2

Fix `--changed-since` with directories unknown to Pants (#15714)
Fixes a regression from https://github.com/pantsbuild/pants/pull/15570. When you had a changed file in a directory Pants does not find because it was deleted or it's in `--pants-ignore`, we would error.

Note that we still error when you explicitly run on the directory, like `./pants list does-not-exist`, per `--unmatched-cli-globs`. This only impacts `--changed-since`.

[ci skip-rust]

src/python/pants/engine/internals/graph.py
@@ -22,7 +22,7 @@ from pants.engine.addresses import (
     UnparsedAddressInputs,
 )
 from pants.engine.collection import Collection
-from pants.engine.fs import EMPTY_SNAPSHOT, PathGlobs, Paths, Snapshot
+from pants.engine.fs import EMPTY_SNAPSHOT, GlobMatchErrorBehavior, PathGlobs, Paths, Snapshot
 from pants.engine.internals import native_engine
 from pants.engine.internals.parametrize import Parametrize, _TargetParametrization
 from pants.engine.internals.parametrize import (  # noqa: F401
@@ -685,13 +685,17 @@ async def find_owners(owners_request: OwnersRequest) -> Owners:
         live_get = Get(
             FilteredTargets,
             RawSpecsWithoutFileOwners(
-                ancestor_globs=live_candidate_specs, filter_by_global_options=True
+                ancestor_globs=live_candidate_specs,
+                filter_by_global_options=True,
+                unmatched_glob_behavior=GlobMatchErrorBehavior.ignore,
             ),
         )
         deleted_get = Get(
             UnexpandedTargets,
             RawSpecsWithoutFileOwners(
-                ancestor_globs=deleted_candidate_specs, filter_by_global_options=True
+                ancestor_globs=deleted_candidate_specs,
+                filter_by_global_options=True,
+                unmatched_glob_behavior=GlobMatchErrorBehavior.ignore,
             ),
         )
     else:

src/python/pants/vcs/changed_integration_test.py
@@ -100,6 +100,7 @@ def _run_pants_goal(
         [
             *(extra_args or ()),
             "--changed-since=HEAD",
+            "--print-stacktrace",
             f"--changed-dependees={dependees.value}",
             goal,
         ],
@@ -280,3 +281,11 @@ def test_tag_filtering(repo: str) -> None:
 
     assert_list_stdout(repo, ["//app.sh:lib"], DependeesOption.TRANSITIVE, extra_args=["--tag=a"])
     assert_count_loc(repo, DependeesOption.TRANSITIVE, expected_num_files=1, extra_args=["--tag=a"])
+
+
+def test_pants_ignored_file(repo: str) -> None:
+    """Regression test for
+    https://github.com/pantsbuild/pants/issues/15655#issuecomment-1140081185."""
+    create_file(".ignored/f.txt", "")
+    assert_list_stdout(repo, [], DependeesOption.NONE)
+    assert_count_loc(repo, DependeesOption.NONE, expected_num_files=0)