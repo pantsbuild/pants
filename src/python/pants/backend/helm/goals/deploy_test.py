691b38e8f25b93157dfb1200ff1914da936713a5
Antonio Alonso Dominguez <alonso.domin@pm.me>
2022-06-16 16:51:35 +0200

    Move --create-namespace argument to renderer module

--- src/python/pants/backend/helm/goals/deploy_test.py
+++ src/python/pants/backend/helm/goals/deploy_test.py
@@ -98,6 +98,7 @@ def test_run_helm_deploy(rule_runner: RuleRunner) -> None:
         '"Foo deployment"',
         "--namespace",
         "uat",
+        "--create-namespace",
         "--skip-crds",
         "--no-hooks",
         "--values",
