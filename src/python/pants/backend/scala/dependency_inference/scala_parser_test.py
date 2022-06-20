6c096ad14beaf1d38f851072143977bbd968461d
Doron Somech <somdoron@gmail.com>
2022-06-20 20:32:29 +0300

    Scala: mark an object that extends another type as recursive (#15865)
    Scala Parser cannot figure out the provided types of an object that extends another type. This PR solves this by marking the object as recursive.
    
    The following example won't compile with Pants at the moment:
    
    File A:
    ```
    object A {
      def a(x: Int): Int = ???
    }
    ```
    
    File B:
    ```
    import A
    
    object B extends A {
      
    }
    ```
    File Main:
    ```
    import B.a
    
    def main() = println(a(5))
    ```
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]

--- src/python/pants/backend/scala/dependency_inference/scala_parser_test.py
+++ src/python/pants/backend/scala/dependency_inference/scala_parser_test.py
@@ -9,6 +9,7 @@ from pants.backend.scala.dependency_inference import scala_parser
 from pants.backend.scala.dependency_inference.scala_parser import (
     AnalyzeScalaSourceRequest,
     ScalaImport,
+    ScalaProvidedSymbol,
     ScalaSourceDependencyAnalysis,
 )
 from pants.backend.scala.target_types import ScalaSourceField, ScalaSourceTarget
@@ -145,7 +146,7 @@ def test_parser_simple(rule_runner: RuleRunner) -> None:
         ),
     )
 
-    assert sorted(analysis.provided_symbols) == [
+    assert sorted(symbol.name for symbol in analysis.provided_symbols) == [
         "org.pantsbuild.example.ASubClass",
         "org.pantsbuild.example.ASubTrait",
         "org.pantsbuild.example.ApplyQualifier",
@@ -179,7 +180,7 @@ def test_parser_simple(rule_runner: RuleRunner) -> None:
         "org.pantsbuild.example.OuterTrait.NestedVar",
     ]
 
-    assert sorted(analysis.provided_symbols_encoded) == [
+    assert sorted(symbol.name for symbol in analysis.provided_symbols_encoded) == [
         "org.pantsbuild.example.ASubClass",
         "org.pantsbuild.example.ASubTrait",
         "org.pantsbuild.example.ApplyQualifier",
@@ -441,7 +442,10 @@ def test_package_object(rule_runner: RuleRunner) -> None:
             """
         ),
     )
-    assert sorted(analysis.provided_symbols) == ["foo.bar", "foo.bar.Hello"]
+    assert sorted(symbol.name for symbol in analysis.provided_symbols) == [
+        "foo.bar",
+        "foo.bar.Hello",
+    ]
 
 
 def test_source3(rule_runner: RuleRunner) -> None:
@@ -523,4 +527,29 @@ def test_type_arguments(rule_runner: RuleRunner) -> None:
         "foo.AnotherType",
         "foo.B",
         "foo.SomeType",
+    ]
+
+
+def test_recursive_objects(rule_runner: RuleRunner) -> None:
+    analysis = _analyze(
+        rule_runner,
+        textwrap.dedent(
+            """
+            package foo
+
+            object Bar {
+                def a = ???
+            }
+
+            object Foo extends Bar {
+                def b = ???
+            }
+            """
+        ),
+    )
+
+    assert sorted(analysis.provided_symbols, key=lambda s: s.name) == [
+        ScalaProvidedSymbol("foo.Bar", False),
+        ScalaProvidedSymbol("foo.Bar.a", False),
+        ScalaProvidedSymbol("foo.Foo", True),
     ]
