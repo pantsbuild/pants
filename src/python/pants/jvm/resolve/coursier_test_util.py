250ea566216b131cb043fbd312a55b52a145b38a
Tom Dyas <tdyas@toolchain.com>
2022-06-22 17:56:29 -0400

    remove TestCoursierWrapper
    [ci skip-build-wheels]
    
    [ci skip-rust]

--- src/python/pants/jvm/resolve/coursier_test_util.py
+++ src/python/pants/jvm/resolve/coursier_test_util.py
@@ -1,33 +1,23 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from dataclasses import dataclass
-from typing import Iterable
 
-from pants.jvm.resolve.common import ArtifactRequirement
-from pants.jvm.resolve.coursier_fetch import CoursierLockfileEntry, CoursierResolvedLockfile
+from pants.jvm.resolve.coursier_fetch import CoursierResolvedLockfile
 from pants.jvm.resolve.lockfile_metadata import JVMLockfileMetadata
-from pants.util.docutil import bin_name
 
 
-@dataclass
+def _make_empty_lockfile() -> bytes:
+    lockfile = CoursierResolvedLockfile(entries=())
+    metadata = JVMLockfileMetadata.new([])
+    return metadata.add_header_to_lockfile(
+        lockfile.to_serialized(),
+        regenerate_command="N/A - empty lockfile for test",
+        delimeter="#",
+    )
+
+
+EMPTY_JVM_LOCKFILE = _make_empty_lockfile()
+
+
 class TestCoursierWrapper:
-    """Utility class to make it easier to create a serialized Coursier lockfile with a correct
-    metadata header in tests."""
-
-    lockfile: CoursierResolvedLockfile
-
-    @classmethod
-    def new(cls, entries: Iterable[CoursierLockfileEntry]):
-        return cls(CoursierResolvedLockfile(entries=tuple(entries)))
-
-    def serialize(self, requirements: Iterable[ArtifactRequirement] = ()) -> str:
-        return (
-            JVMLockfileMetadata.new(requirements)
-            .add_header_to_lockfile(
-                self.lockfile.to_serialized(),
-                regenerate_command=f"{bin_name()} generate-lockfiles",
-                delimeter="#",
-            )
-            .decode()
-        )
+    pass
