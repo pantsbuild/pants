f7217987d9e703a06e3b5646722ee49e67ec0af9
Stu Hood <stuhood@gmail.com>
2022-06-23 14:08:57 -0700

    Add option for max wait before terminating interactive process (Cherry-pick of #15767) (#15908)
    [ci skip-build-wheels]
    
    Co-authored-by: Joshua Cannon <joshdcannon@gmail.com>

--- src/rust/engine/src/externs/interface.rs
+++ src/rust/engine/src/externs/interface.rs
@@ -250,6 +250,7 @@ impl PyExecutionStrategyOptions {
     remote_cache_write: bool,
     child_default_memory: usize,
     child_max_memory: usize,
+    graceful_shutdown_timeout: usize,
   ) -> Self {
     Self(ExecutionStrategyOptions {
       local_parallelism,
@@ -261,6 +262,7 @@ impl PyExecutionStrategyOptions {
       remote_cache_write,
       child_default_memory,
       child_max_memory,
+      graceful_shutdown_timeout: Duration::from_secs(graceful_shutdown_timeout.try_into().unwrap()),
     })
   }
 }
