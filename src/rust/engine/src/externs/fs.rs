48a6a532c6abecf2eade518ba6b0a84627a1997e
Tom Dyas <tdyas@toolchain.com>
2022-06-29 14:21:25 -0400

    Backtrack through calls from `@rules` to synchronous engine methods (Cherry-pick of #15979) (#16001)
    As reported in #15954, backtracking doesn't currently work when a synchronous method like `Workspace.write_digests` is the source of a `MissingDigest` error. This was due to a TODO left behind in #15761, where we did not propagate the `Failure` type "through" `@rule` bodies.
    
    To fix this, we add a conversion from `Failure` to `PrErr` which wraps in a well known exception type, and then look for that type when converting back from `PyErr` to `Failure`.
    
    Fixes #15954.
    
    [ci skip-build-wheels]
    
    Co-authored-by: Stu Hood <stuhood@gmail.com>

--- src/rust/engine/src/externs/fs.rs
+++ src/rust/engine/src/externs/fs.rs
@@ -19,6 +19,8 @@ use fs::{
 use hashing::{Digest, Fingerprint, EMPTY_DIGEST};
 use store::Snapshot;
 
+use crate::Failure;
+
 pub(crate) fn register(m: &PyModule) -> PyResult<()> {
   m.add_class::<PyDigest>()?;
   m.add_class::<PyFileDigest>()?;
@@ -36,11 +38,16 @@ pub(crate) fn register(m: &PyModule) -> PyResult<()> {
   Ok(())
 }
 
-// TODO: This method is a marker, but in a followup PR we will need to propagate a particular
-// Exception type out of `@rule` bodies and back into `Failure::MissingDigest`, to allow for retry
-// via #11331.
-pub fn todo_possible_store_missing_digest(e: store::StoreError) -> PyErr {
-  PyException::new_err(e.to_string())
+///
+/// A marker indicating that a `StoreError` is being converted into a python exception, since retry
+/// via #11331 needs to preserve `Failure` information across Python boundaries.
+///
+/// TODO: Any use of `PyErr::from(Failure::from(StoreError))` would trigger this same conversion,
+/// so this method can eventually be replaced with direct conversion via `?`.
+///
+pub fn possible_store_missing_digest(e: store::StoreError) -> PyErr {
+  let failure: Failure = e.into();
+  failure.into()
 }
 
 #[pyclass(name = "Digest")]
