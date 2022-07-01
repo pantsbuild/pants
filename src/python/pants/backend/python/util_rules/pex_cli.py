94e7e6c2247af244c105ee87f868acc8f2baa6a2
John Sirois <john.sirois@gmail.com>
2022-07-01 15:17:46 -0600

    Upgrade Pex to 2.1.94.
    There are numerous changes, most in 2.1.93, but the two changes of
    most interest to Pants immediately are support for local projects in
    locks and a fix for custom index authentication when fetching artifacts
    recorded in a lock.
    
    The Pex changelogs are here:
    + https://github.com/pantsbuild/pex/releases/tag/v2.1.91
    + https://github.com/pantsbuild/pex/releases/tag/v2.1.92
    + https://github.com/pantsbuild/pex/releases/tag/v2.1.93
    + https://github.com/pantsbuild/pex/releases/tag/v2.1.94
    
    Supplants #15911
    
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/backend/python/util_rules/pex_cli.py
+++ src/python/pants/backend/python/util_rules/pex_cli.py
@@ -39,9 +39,9 @@ class PexCli(TemplatedExternalTool):
     name = "pex"
     help = "The PEX (Python EXecutable) tool (https://github.com/pantsbuild/pex)."
 
-    default_version = "v2.1.90"
+    default_version = "v2.1.94"
     default_url_template = "https://github.com/pantsbuild/pex/releases/download/{version}/pex"
-    version_constraints = ">=2.1.90,<3.0"
+    version_constraints = ">=2.1.94,<3.0"
 
     @classproperty
     def default_known_versions(cls):
@@ -50,8 +50,8 @@ class PexCli(TemplatedExternalTool):
                 (
                     cls.default_version,
                     plat,
-                    "2781255baf77c2a8fdc85c5e830f7191a6048fd91d2e20b5c7a20e5a0b7beb66",
-                    "3755345",
+                    "775c22b97e057d595a8b334fab89f5a07ac9c552a36912504f62577f53a2d2d2",
+                    "3802178",
                 )
             )
             for plat in ["macos_arm64", "macos_x86_64", "linux_x86_64", "linux_arm64"]
