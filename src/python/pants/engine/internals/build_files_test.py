pantsbuild/pants

commit bc3661f97169d20aa26869fcc783e8fe635ecee2

Add `__defaults__` BUILD file symbol (#15836)
Provide default field values for a set of targets in a particular subtree of a project using the `__defaults__` symbol, using the same syntax as for the `overrides` field.

Example:

```python
# BUILD file
__defaults__(
  {
    pyhon_sources: dict(sources=["*.py"]),
    (python_tests, python_testutils): dict(tags=["some-tag"]),
  },
  all=dict(description="Copyright (c) 2022, Company Ltd."),
)

...
```

The `__defaults__` declaration should go at the top of the BUILD file, in order to apply to the targets in the same file. All BUILD files in subdirectories inherit the same defaults by default, but can be overridden/extended with their own `__defaults__`.

Closes #13767.
Discussion: #15809.

src/python/pants/engine/internals/build_files.py
@@ -17,10 +17,13 @@ from pants.build_graph.address import (
 )
 from pants.engine.engine_aware import EngineAwareParameter
 from pants.engine.fs import DigestContents, GlobMatchErrorBehavior, PathGlobs, Paths
+from pants.engine.internals.defaults import BuildFileDefaults, BuildFileDefaultsParserState
 from pants.engine.internals.mapper import AddressFamily, AddressMap
 from pants.engine.internals.parser import BuildFilePreludeSymbols, Parser, error_on_imports
 from pants.engine.internals.target_adaptor import TargetAdaptor, TargetAdaptorRequest
 from pants.engine.rules import Get, collect_rules, rule
+from pants.engine.target import RegisteredTargetTypes
+from pants.engine.unions import UnionMembership
 from pants.option.global_options import GlobalOptions
 from pants.util.frozendict import FrozenDict
 from pants.util.strutil import softwrap
@@ -117,13 +120,28 @@ class AddressFamilyDir(EngineAwareParameter):
         return self.path
 
 
+@dataclass(frozen=True)
+class OptionalAddressFamily:
+    path: str
+    address_family: AddressFamily | None = None
+
+
+@rule
+async def ensure_address_family(request: OptionalAddressFamily) -> AddressFamily:
+    if request.address_family is None:
+        raise ResolveError(f"Directory '{request.path}' does not contain any BUILD files.")
+    return request.address_family
+
+
 @rule(desc="Search for addresses in BUILD files")
 async def parse_address_family(
     parser: Parser,
     build_file_options: BuildFileOptions,
     prelude_symbols: BuildFilePreludeSymbols,
     directory: AddressFamilyDir,
-) -> AddressFamily:
+    registered_target_types: RegisteredTargetTypes,
+    union_membership: UnionMembership,
+) -> OptionalAddressFamily:
     """Given an AddressMapper and a directory, return an AddressFamily.
 
     The AddressFamily may be empty, but it will not be None.
@@ -138,13 +156,30 @@ async def parse_address_family(
         ),
     )
     if not digest_contents:
-        raise ResolveError(f"Directory '{directory.path}' does not contain any BUILD files.")
+        return OptionalAddressFamily(directory.path)
 
+    defaults = BuildFileDefaults({})
+    parent_dir = os.path.dirname(directory.path)
+    if parent_dir != directory.path:
+        maybe_parent = await Get(OptionalAddressFamily, AddressFamilyDir(parent_dir))
+        if maybe_parent.address_family is not None:
+            defaults = maybe_parent.address_family.defaults
+
+    defaults_parser_state = BuildFileDefaultsParserState.create(
+        directory.path, defaults, registered_target_types, union_membership
+    )
     address_maps = [
-        AddressMap.parse(fc.path, fc.content.decode(), parser, prelude_symbols)
+        AddressMap.parse(
+            fc.path, fc.content.decode(), parser, prelude_symbols, defaults_parser_state
+        )
         for fc in digest_contents
     ]
-    return AddressFamily.create(directory.path, address_maps)
+    return OptionalAddressFamily(
+        directory.path,
+        AddressFamily.create(
+            directory.path, address_maps, defaults_parser_state.get_frozen_defaults()
+        ),
+    )
 
 
 @rule

src/python/pants/engine/internals/build_files_test.py
@@ -16,13 +16,22 @@ from pants.engine.fs import DigestContents, FileContent, PathGlobs
 from pants.engine.internals.build_files import (
     AddressFamilyDir,
     BuildFileOptions,
+    OptionalAddressFamily,
     evaluate_preludes,
     parse_address_family,
 )
 from pants.engine.internals.parser import BuildFilePreludeSymbols, Parser
 from pants.engine.internals.scheduler import ExecutionError
 from pants.engine.internals.target_adaptor import TargetAdaptor, TargetAdaptorRequest
-from pants.engine.target import Dependencies, MultipleSourcesField, StringField, Tags, Target
+from pants.engine.target import (
+    Dependencies,
+    MultipleSourcesField,
+    RegisteredTargetTypes,
+    StringField,
+    Tags,
+    Target,
+)
+from pants.engine.unions import UnionMembership
 from pants.testutil.rule_runner import (
     MockGet,
     QueryRule,
@@ -35,13 +44,15 @@ from pants.util.frozendict import FrozenDict
 
 def test_parse_address_family_empty() -> None:
     """Test that parsing an empty BUILD file results in an empty AddressFamily."""
-    af = run_rule_with_mocks(
+    optional_af = run_rule_with_mocks(
         parse_address_family,
         rule_args=[
             Parser(build_root="", target_type_aliases=[], object_aliases=BuildFileAliases()),
             BuildFileOptions(("BUILD",)),
             BuildFilePreludeSymbols(FrozenDict()),
             AddressFamilyDir("/dev/null"),
+            RegisteredTargetTypes({}),
+            UnionMembership({}),
         ],
         mock_gets=[
             MockGet(
@@ -49,8 +60,17 @@ def test_parse_address_family_empty() -> None:
                 input_type=PathGlobs,
                 mock=lambda _: DigestContents([FileContent(path="/dev/null/BUILD", content=b"")]),
             ),
+            MockGet(
+                output_type=OptionalAddressFamily,
+                input_type=AddressFamilyDir,
+                mock=lambda _: OptionalAddressFamily("/dev"),
+            ),
         ],
     )
+    assert optional_af.path == "/dev/null"
+    assert optional_af.address_family is not None
+    af = optional_af.address_family
+    assert af.namespace == "/dev/null"
     assert len(af.name_to_target_adaptors) == 0
 
 
@@ -210,6 +230,39 @@ def test_target_adaptor_parsed_correctly(target_adaptor_rule_runner: RuleRunner)
     )
     assert target_adaptor.name == "t2"
     assert target_adaptor.type_alias == "mock_tgt"
+
+
+def test_target_adaptor_defaults_applied(target_adaptor_rule_runner: RuleRunner) -> None:
+    target_adaptor_rule_runner.write_files(
+        {
+            "helloworld/dir/BUILD": dedent(
+                """\
+                __defaults__({mock_tgt: dict(resolve="mock")}, all=dict(tags=["24"]))
+                mock_tgt(tags=["42"])
+                mock_tgt(name='t2')
+                """
+            )
+        }
+    )
+    target_adaptor = target_adaptor_rule_runner.request(
+        TargetAdaptor,
+        [TargetAdaptorRequest(Address("helloworld/dir"), description_of_origin="tests")],
+    )
+    assert target_adaptor.name is None
+    assert target_adaptor.kwargs["resolve"] == "mock"
+    assert target_adaptor.kwargs["tags"] == ["42"]
+
+    target_adaptor = target_adaptor_rule_runner.request(
+        TargetAdaptor,
+        [
+            TargetAdaptorRequest(
+                Address("helloworld/dir", target_name="t2"), description_of_origin="tests"
+            )
+        ],
+    )
+    assert target_adaptor.name == "t2"
+    assert target_adaptor.kwargs["resolve"] == "mock"
+    assert target_adaptor.kwargs["tags"] == ["24"]
 
 
 def test_target_adaptor_not_found(target_adaptor_rule_runner: RuleRunner) -> None:

src/python/pants/engine/internals/defaults.py
@@ -1,0 +1,160 @@
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+"""The `BuildFileDefaultsParserState.set_defaults` is used by the pants.engine.internals.Parser,
+exposed as the `__defaults__` BUILD file symbol.
+
+When parsing a BUILD (from the rule `pants.engine.internals.build_files.parse_address_family`) the
+defaults from the closest parent BUILD file is passed as input to the parser, and the new defaults
+resulting after the BUILD file have been parsed is returned in the `AddressFamily`.
+
+These defaults are then applied when creating the `TargetAdaptor` targets by the `Registrar` in the
+parser.
+"""
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any, Iterable, Mapping, Tuple, Union
+
+from pants.engine.addresses import Address
+from pants.engine.target import ImmutableValue, InvalidFieldException, RegisteredTargetTypes
+from pants.engine.unions import UnionMembership
+from pants.util.frozendict import FrozenDict
+
+SetDefaultsValueT = Mapping[str, Any]
+SetDefaultsKeyT = Union[str, Tuple[str, ...]]
+SetDefaultsT = Mapping[SetDefaultsKeyT, SetDefaultsValueT]
+
+
+class BuildFileDefaults(FrozenDict[str, FrozenDict[str, ImmutableValue]]):
+    """Map target types to default field values."""
+
+
+@dataclass
+class BuildFileDefaultsParserState:
+    address: Address
+    defaults: dict[str, Mapping[str, Any]]
+    registered_target_types: RegisteredTargetTypes
+    union_membership: UnionMembership
+
+    @classmethod
+    def create(
+        cls,
+        path: str,
+        defaults: BuildFileDefaults,
+        registered_target_types: RegisteredTargetTypes,
+        union_membership: UnionMembership,
+    ) -> BuildFileDefaultsParserState:
+        return cls(
+            address=Address(path, generated_name="__defaults__"),
+            defaults=dict(defaults),
+            registered_target_types=registered_target_types,
+            union_membership=union_membership,
+        )
+
+    def get_frozen_defaults(self) -> BuildFileDefaults:
+        types = self.registered_target_types.aliases_to_types
+        return BuildFileDefaults(
+            {
+                target_alias: FrozenDict(
+                    {
+                        field_type.alias: field_type.compute_value(
+                            raw_value=default, address=self.address
+                        )
+                        for field_alias, default in fields.items()
+                        for field_type in types[target_alias].class_field_types(
+                            self.union_membership
+                        )
+                        if field_alias in (field_type.alias, field_type.deprecated_alias)
+                    }
+                )
+                for target_alias, fields in self.defaults.items()
+            }
+        )
+
+    def get(self, target_alias: str) -> Mapping[str, Any]:
+        # Used by `pants.engine.internals.parser.Parser._generate_symbols.Registrar.__call__`
+        return self.defaults.get(target_alias, {})
+
+    def set_defaults(
+        self,
+        *args: SetDefaultsT,
+        all: SetDefaultsValueT | None = None,
+        extend: bool = False,
+        **kwargs,
+    ) -> None:
+        defaults: dict[str, dict[str, Any]] = (
+            {} if not extend else {k: dict(v) for k, v in self.defaults.items()}
+        )
+
+        if all is not None:
+            self._process_defaults(
+                defaults,
+                {tuple(self.registered_target_types.aliases): all},
+                ignore_unknown_fields=True,
+            )
+
+        for arg in args:
+            self._process_defaults(defaults, arg)
+
+        # Update with new defaults, dropping targets without any default values.
+        for tgt, default in defaults.items():
+            if not default:
+                self.defaults.pop(tgt, None)
+            else:
+                self.defaults[tgt] = default
+
+    def _process_defaults(
+        self,
+        defaults: dict[str, dict[str, Any]],
+        targets_defaults: SetDefaultsT,
+        ignore_unknown_fields: bool = False,
+    ):
+        if not isinstance(targets_defaults, dict):
+            raise ValueError(
+                f"Expected dictionary mapping targets to default field values for {self.address} "
+                f"but got: {type(targets_defaults).__name__}."
+            )
+
+        types = self.registered_target_types.aliases_to_types
+        for target, default in targets_defaults.items():
+            if not isinstance(default, dict):
+                raise ValueError(
+                    f"Invalid default field values in {self.address} for target type {target}, "
+                    f"must be an `dict` but was {default!r} with type `{type(default).__name__}`."
+                )
+
+            targets: Iterable[str]
+            targets = target if isinstance(target, tuple) else (target,)
+            for target_alias in map(str, targets):
+                if target_alias in types:
+                    target_type = types[target_alias]
+                else:
+                    raise ValueError(f"Unrecognized target type {target_alias} in {self.address}.")
+
+                # Copy default dict if we may mutate it.
+                raw_values = dict(default) if ignore_unknown_fields else default
+
+                # Validate that field exists on target
+                valid_field_aliases = set(
+                    target_type._get_field_aliases_to_field_types(
+                        target_type.class_field_types(self.union_membership)
+                    ).keys()
+                )
+
+                for field_alias in default.keys():
+                    if field_alias not in valid_field_aliases:
+                        if ignore_unknown_fields:
+                            del raw_values[field_alias]
+                        else:
+                            raise InvalidFieldException(
+                                f"Unrecognized field `{field_alias}` for target {target_type.alias}. "
+                                f"Valid fields are: {', '.join(sorted(valid_field_aliases))}.",
+                            )
+
+                # TODO: moved fields for TargetGenerators ?  See: `Target._calculate_field_values()`.
+
+                # TODO: support parametrization ? --needs special care due to Parametrize object not
+                # being hashable, and thus not acceptable in a FrozenDict instance.
+
+                # Merge all provided defaults for this call.
+                defaults.setdefault(target_type.alias, {}).update(raw_values)

src/python/pants/engine/internals/defaults_test.py
@@ -1,0 +1,207 @@
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+from collections import namedtuple
+
+import pytest
+
+from pants.core.target_types import GenericTarget
+from pants.engine.internals.defaults import BuildFileDefaults, BuildFileDefaultsParserState
+from pants.engine.target import InvalidFieldException, RegisteredTargetTypes
+from pants.engine.unions import UnionMembership
+from pants.testutil.pytest_util import no_exception
+from pants.util.frozendict import FrozenDict
+
+
+class Test1Target(GenericTarget):
+    alias = "test_type_1"
+
+
+class Test2Target(GenericTarget):
+    alias = "test_type_2"
+
+
+@pytest.fixture
+def registered_target_types() -> RegisteredTargetTypes:
+    return RegisteredTargetTypes(
+        {tgt.alias: tgt for tgt in (GenericTarget, Test1Target, Test2Target)}
+    )
+
+
+@pytest.fixture
+def union_membership() -> UnionMembership:
+    return UnionMembership({})
+
+
+def test_assumptions(
+    registered_target_types: RegisteredTargetTypes, union_membership: UnionMembership
+) -> None:
+    defaults = BuildFileDefaultsParserState.create(
+        "", BuildFileDefaults({}), registered_target_types, union_membership
+    )
+    defaults.set_defaults({"target": dict(tags=["foo", "bar"])})
+
+    frozen = defaults.get_frozen_defaults()
+    assert frozen == BuildFileDefaults(
+        {
+            "target": FrozenDict({"tags": ("foo", "bar")}),
+        }
+    )
+
+
+Scenario = namedtuple(
+    "Scenario",
+    "path, args, kwargs, expected_defaults, expected_error, parent_defaults",
+    defaults=("", (), {}, {}, None, {}),
+)
+
+
+@pytest.mark.parametrize(
+    "scenario",
+    [
+        pytest.param(
+            Scenario(
+                path="src/proj/a",
+                kwargs=dict(all=dict(tags=["tagged-2"])),
+                parent_defaults={
+                    "test_type_1": {
+                        "tags": ("tagged-1",),
+                    },
+                },
+                expected_defaults={
+                    "target": {
+                        "tags": ("tagged-2",),
+                    },
+                    "test_type_1": {
+                        "tags": ("tagged-2",),
+                    },
+                    "test_type_2": {
+                        "tags": ("tagged-2",),
+                    },
+                },
+            ),
+            id="simple inherit",
+        ),
+        pytest.param(
+            Scenario(
+                path="src",
+                args=({Test2Target.alias: dict(description="only desc default")},),
+                kwargs=dict(extend=False),
+                parent_defaults={
+                    "target": {"tags": ("root-tag",)},
+                    "test_type_1": {"tags": ("root-tag",)},
+                    "test_type_2": {
+                        "tags": ("root-tag",),
+                        "description": "extend default with desc",
+                    },
+                },
+                expected_defaults={
+                    "target": {"tags": ("root-tag",)},
+                    "test_type_1": {"tags": ("root-tag",)},
+                    "test_type_2": {"description": "only desc default"},
+                },
+            ),
+            id="extend test",
+        ),
+        pytest.param(
+            Scenario(
+                args=(["bad type"],),
+                expected_error=pytest.raises(
+                    ValueError,
+                    match=(
+                        r"Expected dictionary mapping targets to default field values for "
+                        r"//#__defaults__ but got: list\."
+                    ),
+                ),
+            ),
+            id="invalid defaults args",
+        ),
+        pytest.param(
+            Scenario(
+                args=({"test_type_1": ()},),
+                expected_error=pytest.raises(
+                    ValueError,
+                    match=(
+                        r"Invalid default field values in //#__defaults__ for target type "
+                        r"test_type_1, must be an `dict` but was \(\) with type `tuple`\."
+                    ),
+                ),
+            ),
+            id="invalid default field values",
+        ),
+        pytest.param(
+            Scenario(
+                args=({"unknown_target": {}},),
+                expected_error=pytest.raises(
+                    ValueError,
+                    match=r"Unrecognized target type unknown_target in //#__defaults__\.",
+                ),
+            ),
+            id="unknown target",
+        ),
+        pytest.param(
+            Scenario(
+                args=({Test1Target.alias: {"does-not-exist": ()}},),
+                expected_error=pytest.raises(
+                    InvalidFieldException,
+                    match=(
+                        r"Unrecognized field `does-not-exist` for target test_type_1\. "
+                        r"Valid fields are: dependencies, description, tags\."
+                    ),
+                ),
+            ),
+            id="invalid field",
+        ),
+        pytest.param(
+            Scenario(
+                path="src/proj/a",
+                args=({"test_type_1": {"tags": "foo-bar"}},),
+                expected_error=pytest.raises(
+                    InvalidFieldException,
+                    match=(
+                        r"The 'tags' field in target src/proj/a#__defaults__ must be an "
+                        r"iterable of strings \(e\.g\. a list of strings\), but was "
+                        r"`'foo-bar'` with type `str`\."
+                    ),
+                ),
+            ),
+            id="invalid field value",
+        ),
+        pytest.param(
+            Scenario(
+                kwargs=dict(all=dict(foo_bar="ignored")),
+                expected_defaults={},
+            ),
+            id="unknown fields ignored for `all` targets",
+        ),
+        pytest.param(
+            Scenario(
+                args=({Test1Target.alias: {}},),
+                parent_defaults={"test_type_1": {"tags": ("foo-bar",)}},
+                expected_defaults={},
+            ),
+            id="reset default",
+        ),
+    ],
+)
+def test_set_defaults(
+    scenario: Scenario,
+    registered_target_types: RegisteredTargetTypes,
+    union_membership: UnionMembership,
+) -> None:
+    with (scenario.expected_error or no_exception()):
+        defaults = BuildFileDefaultsParserState.create(
+            scenario.path,
+            BuildFileDefaults(
+                {tgt: FrozenDict(val) for tgt, val in scenario.parent_defaults.items()}
+            ),
+            registered_target_types,
+            union_membership,
+        )
+        defaults.set_defaults(*scenario.args, **scenario.kwargs)
+        actual_defaults = {
+            tgt: dict(field_values) for tgt, field_values in defaults.get_frozen_defaults().items()
+        }
+        assert scenario.expected_defaults == actual_defaults

src/python/pants/engine/internals/mapper.py
@@ -11,6 +11,7 @@ from typing import Iterable, Mapping
 from pants.backend.project_info.filter_targets import FilterSubsystem
 from pants.base.exceptions import MappingError
 from pants.build_graph.address import Address, BuildFileAddress
+from pants.engine.internals.defaults import BuildFileDefaults, BuildFileDefaultsParserState
 from pants.engine.internals.parser import BuildFilePreludeSymbols, Parser
 from pants.engine.internals.target_adaptor import TargetAdaptor
 from pants.engine.target import RegisteredTargetTypes, Tags, Target
@@ -36,6 +37,7 @@ class AddressMap:
         build_file_content: str,
         parser: Parser,
         extra_symbols: BuildFilePreludeSymbols,
+        defaults: BuildFileDefaultsParserState,
     ) -> AddressMap:
         """Parses a source for targets.
 
@@ -43,7 +45,7 @@ class AddressMap:
         the same namespace but from a separate source are left as unresolved pointers.
         """
         try:
-            target_adaptors = parser.parse(filepath, build_file_content, extra_symbols)
+            target_adaptors = parser.parse(filepath, build_file_content, extra_symbols, defaults)
         except Exception as e:
             raise MappingError(f"Failed to parse ./{filepath}:\n{e}")
         name_to_target_adaptors: dict[str, TargetAdaptor] = {}
@@ -76,14 +78,21 @@ class AddressFamily:
 
     :param namespace: The namespace path of this address family.
     :param name_to_target_adaptors: A dict mapping from name to the target adaptor.
+    :param defaults: The default target field values, per target type, applicable for this address family.
     """
 
     # The directory from which the adaptors were parsed.
     namespace: str
     name_to_target_adaptors: dict[str, tuple[str, TargetAdaptor]]
+    defaults: BuildFileDefaults
 
     @classmethod
-    def create(cls, spec_path: str, address_maps: Iterable[AddressMap]) -> AddressFamily:
+    def create(
+        cls,
+        spec_path: str,
+        address_maps: Iterable[AddressMap],
+        defaults: BuildFileDefaults = BuildFileDefaults({}),
+    ) -> AddressFamily:
         """Creates an address family from the given set of address maps.
 
         :param spec_path: The directory prefix shared by all address_maps.
@@ -113,6 +122,7 @@ class AddressFamily:
         return AddressFamily(
             namespace=spec_path,
             name_to_target_adaptors=dict(sorted(name_to_target_adaptors.items())),
+            defaults=defaults,
         )
 
     @memoized_property
@@ -144,7 +154,7 @@ class AddressFamily:
         return target_adaptor
 
     def __hash__(self):
-        return hash(self.namespace)
+        return hash((self.namespace, self.defaults))
 
     def __repr__(self) -> str:
         return (

commit 212271f568c298fc57fbf85e64d50fda6c0bb4dc

Fix issue with `__defaults__` not propagating over gaps in the tree of BUILD files. (#16089)
Fix bug introduced in #15836 

As reported by @ced-f on Slack:
> I have a top level `django/BUILD` file which contains:
> ```python
> __defaults__({
>     (python_tests): dict(dependencies=["django/django_core/settings.py"])
> })
> ```
> And a BUILD file under `django/sub/tests/classes/BUILD` which just defines:
> ```python
> python_tests(
>     name="tests",
> )
> ```
> The dependencies goal in the subdirectory does not reference `django/django_core/settings.py`
> If I extend the contents of `django/sub/tests/classes/BUILD` the dependency `django/django_core/settings.py` gets shown correctly:
> ```python
> __defaults__({
>     (python_tests): dict(dependencies=["django/django_core/settings.py"])
> })
> python_tests(
>     name="tests",
> )
> ```

src/python/pants/engine/internals/build_files.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 import os.path
 from dataclasses import dataclass
+from pathlib import PurePath
 from typing import Any
 
 from pants.build_graph.address import (
@@ -21,7 +22,7 @@ from pants.engine.internals.defaults import BuildFileDefaults, BuildFileDefaults
 from pants.engine.internals.mapper import AddressFamily, AddressMap
 from pants.engine.internals.parser import BuildFilePreludeSymbols, Parser, error_on_imports
 from pants.engine.internals.target_adaptor import TargetAdaptor, TargetAdaptorRequest
-from pants.engine.rules import Get, collect_rules, rule
+from pants.engine.rules import Get, MultiGet, collect_rules, rule
 from pants.engine.target import RegisteredTargetTypes
 from pants.engine.unions import UnionMembership
 from pants.option.global_options import GlobalOptions
@@ -159,11 +160,16 @@ async def parse_address_family(
         return OptionalAddressFamily(directory.path)
 
     defaults = BuildFileDefaults({})
-    parent_dir = os.path.dirname(directory.path)
-    if parent_dir != directory.path:
-        maybe_parent = await Get(OptionalAddressFamily, AddressFamilyDir(parent_dir))
-        if maybe_parent.address_family is not None:
-            defaults = maybe_parent.address_family.defaults
+    parent_dirs = tuple(PurePath(directory.path).parents)
+    if parent_dirs:
+        maybe_parents = await MultiGet(
+            Get(OptionalAddressFamily, AddressFamilyDir(str(parent_dir)))
+            for parent_dir in parent_dirs
+        )
+        for maybe_parent in maybe_parents:
+            if maybe_parent.address_family is not None:
+                defaults = maybe_parent.address_family.defaults
+                break
 
     defaults_parser_state = BuildFileDefaultsParserState.create(
         directory.path, defaults, registered_target_types, union_membership

src/python/pants/engine/internals/build_files_test.py
@@ -270,7 +270,33 @@ def test_target_adaptor_defaults_applied(target_adaptor_rule_runner: RuleRunner)
     )
     assert target_adaptor.name == "t2"
     assert target_adaptor.kwargs["resolve"] == "mock"
+
+    # The defaults are not frozen until after the BUILD file have been fully parsed, so this is a
+    # list rather than a tuple at this time.
     assert target_adaptor.kwargs["tags"] == ["24"]
+
+
+def test_inherit_defaults(target_adaptor_rule_runner: RuleRunner) -> None:
+    target_adaptor_rule_runner.write_files(
+        {
+            "BUILD": """__defaults__(all=dict(tags=["root"]))""",
+            "helloworld/dir/BUILD": dedent(
+                """\
+                __defaults__({mock_tgt: dict(resolve="mock")}, extend=True)
+                mock_tgt()
+                """
+            ),
+        }
+    )
+    target_adaptor = target_adaptor_rule_runner.request(
+        TargetAdaptor,
+        [TargetAdaptorRequest(Address("helloworld/dir"), description_of_origin="tests")],
+    )
+    assert target_adaptor.name is None
+    assert target_adaptor.kwargs["resolve"] == "mock"
+
+    # The defaults originates from a parent BUILD file, and as such has been frozen.
+    assert target_adaptor.kwargs["tags"] == ("root",)
 
 
 def test_target_adaptor_not_found(target_adaptor_rule_runner: RuleRunner) -> None: