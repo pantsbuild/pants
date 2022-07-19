757a2664cb82fac6693f13609ce14911d8fd88e3
Stu Hood <stuhood@gmail.com>
2022-07-19 11:04:10 -0700

    Upgrade Pex to 2.1.99. (Cherry-pick of #16184) (#16225)
    This pulls in a concurrency fix for downloading lock file artifacts with
    an empty PEX_ROOT.
    
    See the changelog here:
      https://github.com/pantsbuild/pex/releases/tag/v2.1.99
    
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/backend/python/util_rules/pex_cli.py
+++ src/python/pants/backend/python/util_rules/pex_cli.py
@@ -39,9 +39,9 @@ class PexCli(TemplatedExternalTool):
     name = "pex"
     help = "The PEX (Python EXecutable) tool (https://github.com/pantsbuild/pex)."
 
-    default_version = "v2.1.98"
+    default_version = "v2.1.99"
     default_url_template = "https://github.com/pantsbuild/pex/releases/download/{version}/pex"
-    version_constraints = ">=2.1.98,<3.0"
+    version_constraints = ">=2.1.99,<3.0"
 
     @classproperty
     def default_known_versions(cls):
@@ -50,8 +50,8 @@ class PexCli(TemplatedExternalTool):
                 (
                     cls.default_version,
                     plat,
-                    "b3db597492c3d1250036749bec8330a0e8786ec7a4bd6f0c9f8ff1675c90c0b4",
-                    "3811372",
+                    "7e00a1d81a43fb913085182b2eba2f3b61822dd99fe1ddd0931aa824959a759f",
+                    "3811337",
                 )
             )
             for plat in ["macos_arm64", "macos_x86_64", "linux_x86_64", "linux_arm64"]
