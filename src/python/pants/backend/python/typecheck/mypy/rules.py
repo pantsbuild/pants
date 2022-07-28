16076201d1595ae6d48c176fc3330e9f817830c9
Joshua Cannon <joshdcannon@gmail.com>
2022-07-28 11:44:35 -0500

    Update rules.py

--- src/python/pants/backend/python/typecheck/mypy/rules.py
+++ src/python/pants/backend/python/typecheck/mypy/rules.py
@@ -291,7 +291,7 @@ async def mypy_typecheck_partition(
                             # for different versions) and uses a one-process-at-a-time daemon by default,
                             # multuple MyPy processes operating on a single db cache should be rare.
 
-                            {mkdir.path} -p {run_cache_dir}/{py_version} > /dev/null 2>&1
+                            {mkdir.path} -p {run_cache_dir}/{py_version} > /dev/null 2>&1 || true
                             {cp.path} {named_cache_dir}/{py_version}/cache.db {run_cache_dir}/{py_version}/cache.db > /dev/null 2>&1 || true
                             {' '.join((shell_quote(arg) for arg in argv))}
                             EXIT_CODE=$?
