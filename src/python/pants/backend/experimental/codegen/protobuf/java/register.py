1f3d756bf0cb5c50bd9bf8183b392a6bec896e5c
Tom Dyas <tdyas@toolchain.com>
2022-07-29 15:45:50 -0400

    lint
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/experimental/codegen/protobuf/java/register.py
+++ src/python/pants/backend/experimental/codegen/protobuf/java/register.py
@@ -2,8 +2,8 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 from pants.backend.codegen import export_codegen_goal
 from pants.backend.codegen.protobuf import protobuf_dependency_inference
-from pants.backend.codegen.protobuf import target_types as protobuf_target_types
 from pants.backend.codegen.protobuf import tailor as protobuf_tailor
+from pants.backend.codegen.protobuf import target_types as protobuf_target_types
 from pants.backend.codegen.protobuf.java import rules as java_protobuf_rules
 from pants.backend.codegen.protobuf.target_types import (
     ProtobufSourcesGeneratorTarget,
