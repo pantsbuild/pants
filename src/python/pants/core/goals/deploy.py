191f9c467ca9ee0518da6871bc019be877b02ce6
Antonio Alonso Dominguez <alonso.domin@pm.me>
2022-08-02 16:23:20 +0200

    Perform all the publishing ahead of starting deploying

--- src/python/pants/core/goals/deploy.py
+++ src/python/pants/core/goals/deploy.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 import logging
 from abc import ABCMeta
 from dataclasses import dataclass
+from itertools import chain
 from typing import Iterable
 
 from pants.core.goals.package import PackageFieldSet
@@ -167,22 +168,18 @@ async def run_deploy(console: Console, deploy_subsystem: DeploySubsystem) -> Dep
         for field_set in target_roots_to_deploy_field_sets.field_sets
     )
 
+    publish_targets = set(
+        chain.from_iterable([deploy.publish_dependencies for deploy in deploy_processes])
+    )
+    publish_processes = await _find_publish_processes(publish_targets)
+
     exit_code: int = 0
     results: list[str] = []
-    published_targets: set[Target] = set()
-    for deploy in deploy_processes:
-        # If any of the deployment processes has common publish dependencies, we run the publish step only
-        # once for those common dependencies
-        publish_dependency_targets = [
-            tgt for tgt in deploy.publish_dependencies if tgt not in published_targets
-        ]
-        publish_processes = await _find_publish_processes(publish_dependency_targets)
 
-        if publish_processes:
-            logger.info(f"Publishing dependencies for {deploy.name}.")
+    if publish_processes:
+        logger.info("Publishing dependencies...")
 
         # Publish all deployment dependencies first.
-        publish_exit_code = 0
         for publish in publish_processes:
             ec, statuses = await _invoke_process(
                 console,
@@ -191,36 +188,24 @@ async def run_deploy(console: Console, deploy_subsystem: DeploySubsystem) -> Dep
                 description=publish.description,
                 success_status="published",
             )
-            publish_exit_code = ec if ec != 0 else publish_exit_code
+            exit_code = ec if ec != 0 else exit_code
             results.extend(statuses)
 
-        if publish_exit_code == 0:
-            published_targets.update(publish_dependency_targets)
-        else:
-            # If we could not publish all dependencies of a deployment process we skip the deployment step and continue.
-            logger.warning(
-                f"Failed publishing deployment dependencies for {deploy.name}. Skipping deployment."
+    # Only proceed to deploy of all dependencies have been successfully published
+    if exit_code == 0 and deploy_processes:
+        logger.info("Deploying targets...")
+
+        for deploy in deploy_processes:
+            # Invoke the deployment.
+            ec, statuses = await _invoke_process(
+                console,
+                deploy.process,
+                names=[deploy.name],
+                success_status="deployed",
+                description=deploy.description,
             )
-
-            sigil = console.sigil_failed()
-            status = "failed"
-            if deploy.description:
-                status += f" for {deploy.description}"
-            results.append(f"{sigil} {deploy.name} {status}")
-
-            exit_code = publish_exit_code
-            continue
-
-        # Invoke the deployment.
-        ec, statuses = await _invoke_process(
-            console,
-            deploy.process,
-            names=[deploy.name],
-            success_status="deployed",
-            description=deploy.description,
-        )
-        exit_code = ec if ec != 0 else exit_code
-        results.extend(statuses)
+            exit_code = ec if ec != 0 else exit_code
+            results.extend(statuses)
 
     console.print_stderr("")
     if not results:
