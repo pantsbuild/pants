4e13b4a5c604803608fece5dfef8242836d93b8f
Tom Dyas <tdyas@toolchain.com>
2022-08-02 23:10:22 -0400

    java/protobuf: add support for grpc-java codegen (#16350)
    Add support for generating gRPC stubs for Protobuf and Java using https://github.com/grpc/grpc-java.
    
    Note: The gRPC Java plugin is a native binary which is distributed through Maven Central as a non-jar artifact.
    
    Fixes https://github.com/pantsbuild/pants/issues/15691.
    
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/codegen/protobuf/java/dependency_inference.py
+++ src/python/pants/backend/codegen/protobuf/java/dependency_inference.py
@@ -1,9 +1,14 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+from __future__ import annotations
+
 from dataclasses import dataclass
 from typing import FrozenSet
 
-from pants.backend.codegen.protobuf.target_types import ProtobufDependenciesField
+from pants.backend.codegen.protobuf.target_types import (
+    ProtobufDependenciesField,
+    ProtobufGrpcToggleField,
+)
 from pants.build_graph.address import Address
 from pants.engine.internals.selectors import Get
 from pants.engine.rules import collect_rules, rule
@@ -23,18 +28,20 @@ _PROTOBUF_JAVA_RUNTIME_ARTIFACT = "protobuf-java"
 
 
 @dataclass(frozen=True)
-class ProtobufJavaRuntimeDependencyInferenceFiedSet(FieldSet):
+class ProtobufJavaRuntimeDependencyInferenceFieldSet(FieldSet):
     required_fields = (
         ProtobufDependenciesField,
         JvmResolveField,
+        ProtobufGrpcToggleField,
     )
 
     dependencies: ProtobufDependenciesField
     resolve: JvmResolveField
+    grpc: ProtobufGrpcToggleField
 
 
 class InferProtobufJavaRuntimeDependencyRequest(InferDependenciesRequest):
-    infer_from = ProtobufJavaRuntimeDependencyInferenceFiedSet
+    infer_from = ProtobufJavaRuntimeDependencyInferenceFieldSet
 
 
 @dataclass(frozen=True)
@@ -69,6 +76,54 @@ async def resolve_protobuf_java_runtime_for_resolve(
     return ProtobufJavaRuntimeForResolve(addresses)
 
 
+@dataclass(frozen=True)
+class ProtobufJavaGrpcRuntimeForResolveRequest:
+    resolve_name: str
+
+
+@dataclass(frozen=True)
+class ProtobufJavaGrpcRuntimeForResolve:
+    addresses: FrozenSet[Address]
+
+
+@rule
+async def resolve_protobuf_java_grpc_runtime_for_resolve(
+    jvm_artifact_targets: AllJvmArtifactTargets,
+    jvm: JvmSubsystem,
+    request: ProtobufJavaGrpcRuntimeForResolveRequest,
+) -> ProtobufJavaGrpcRuntimeForResolve:
+    addresses = find_jvm_artifacts_or_raise(
+        required_coordinates=[
+            # For non-Android uses:
+            # TODO: Maybe support Android jars? See https://github.com/grpc/grpc-java#download for
+            # the differences in required jars.
+            UnversionedCoordinate(
+                group="io.grpc",
+                artifact="grpc-netty-shaded",
+            ),
+            UnversionedCoordinate(
+                group="io.grpc",
+                artifact="grpc-protobuf",
+            ),
+            UnversionedCoordinate(
+                group="io.grpc",
+                artifact="grpc-stub",
+            ),
+            # TODO: This is only required for JDK 9+ according to https://github.com/grpc/grpc-java#download.
+            UnversionedCoordinate(
+                group="org.apache.tomcat",
+                artifact="annotations-api",
+            ),
+        ],
+        resolve=request.resolve_name,
+        jvm_artifact_targets=jvm_artifact_targets,
+        jvm=jvm,
+        subsystem="the Protobuf Java gRPC runtime",
+        target_type="protobuf_sources",
+    )
+    return ProtobufJavaGrpcRuntimeForResolve(addresses)
+
+
 @rule
 async def infer_protobuf_java_runtime_dependency(
     request: InferProtobufJavaRuntimeDependencyRequest,
@@ -79,8 +134,15 @@ async def infer_protobuf_java_runtime_dependency(
     protobuf_java_runtime_target_info = await Get(
         ProtobufJavaRuntimeForResolve, ProtobufJavaRuntimeForResolveRequest(resolve)
     )
+    addresses: set[Address] = set(protobuf_java_runtime_target_info.addresses)
 
-    return InferredDependencies(protobuf_java_runtime_target_info.addresses)
+    if request.field_set.grpc.value:
+        grpc_runtime_info = await Get(
+            ProtobufJavaGrpcRuntimeForResolve, ProtobufJavaGrpcRuntimeForResolveRequest(resolve)
+        )
+        addresses.update(grpc_runtime_info.addresses)
+
+    return InferredDependencies(frozenset(addresses))
 
 
 def rules():
