a914acc9930fb8ae2f96dc6f7719414da627e38d
Christopher Neugebauer <chrisjrn@toolchain.com>
2022-08-04 10:34:55 -0700

    lint
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/version.py
+++ src/python/pants/version.py
@@ -3,11 +3,10 @@
 
 import os
 
-# Generate a inferrable dependency on the `pants._version` package and its associated resources.
-import pants._version
-
 from packaging.version import Version
 
+# Generate a inferrable dependency on the `pants._version` package and its associated resources.
+import pants._version
 from pants.util.resources import read_resource
 
 # Set this env var to override the version pants reports. Useful for testing.
@@ -18,8 +17,8 @@ VERSION: str = (
     os.environ.get(_PANTS_VERSION_OVERRIDE)
     or
     # NB: We expect VERSION to always have an entry and want a runtime failure if this is false.
-    # `pants._version` is a non-namespace package that can be loaded by `importlib.resources` in 
-    # pre-3.11 versions of Python. Once we can read `pants.VERSION` using the resource loader, 
+    # `pants._version` is a non-namespace package that can be loaded by `importlib.resources` in
+    # pre-3.11 versions of Python. Once we can read `pants.VERSION` using the resource loader,
     # we can remove the `pants._version` package. See #16379.
     read_resource(pants._version.__name__, "VERSION").decode().strip()
 )
