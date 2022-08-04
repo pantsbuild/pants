c633384b0ae65c64b833133ad8351d590c41ac78
Christopher Neugebauer <chrisjrn@toolchain.com>
2022-08-04 11:47:47 -0700

    Tidy up `ox.py`
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/ox.py
+++ src/python/pants/ox.py
@@ -3,15 +3,20 @@
 
 import sys
 
-# True if this is being run as an oxidized binary.
-is_oxidized = False
+# Provide the `is_oxidized` symbol, to allow for workarounds in Pants code where we use things
+# that don't work under PyOxidizer's custom importer. `oxidized_importer` is only accessible
+# in Pants under PyOxidizer, so an import failure will occur if we're not oxidized.
+try:
+    import oxidized_importer  # type: ignore # pants: no-infer-dep # noqa: F401
+
+    is_oxidized = True
+except ModuleNotFoundError:
+    is_oxidized = False
 
 
-# Patch for PyOxidizer
-if not sys.argv[0]:
-    # A giant pile of pants consumer code copies around `sys.argv`, which is modified in an
+if is_oxidized and not sys.argv[0]:
+    # A not insignificant amount of Pants code relies on `sys.argv[0]`, which is modified in an
     # invalid way by python's `pymain_run_module` support. For our purposes, the executable
     # distribution is the correct `argv[0]`.
     # See https://github.com/indygreg/PyOxidizer/issues/307
     sys.argv[0] = sys.executable
-    is_oxidized = True
