e7be1a85a225b094d95a6ae322601c503311b944
Christopher Neugebauer <chrisjrn@toolchain.com>
2022-08-11 14:22:46 -0700

    Approaches the `read_resource` api with an approach that doesn't require a symlink (see #13679)
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    (cherry picked from commit 1e77a5246572f881ce088a35927ce6a209588817)

---
+++ src/python/pants/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
