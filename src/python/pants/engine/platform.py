9247199d98242c91d4fdb20f6588251cc45afa6f
Stu Hood <stuhood@gmail.com>
2022-08-29 11:30:17 -0700

    Deprecate `Platform.current`.
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/engine/platform.py
+++ src/python/pants/engine/platform.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 from enum import Enum
 from typing import Iterable
 
+from pants.base.deprecated import deprecated
 from pants.engine.rules import Rule, collect_rules, rule
 from pants.util.memo import memoized_classproperty
 from pants.util.osutil import get_normalized_arch_name, get_normalized_os_name
@@ -21,9 +22,24 @@ class Platform(Enum):
     def is_macos(self) -> bool:
         return self in [Platform.macos_arm64, Platform.macos_x86_64]
 
-    # TODO: try to turn all of these accesses into v2 dependency injections!
     @memoized_classproperty
+    @deprecated(
+        "2.16.0.dev1",
+        (
+            "The `Platform` to use is dependent on a `@rule`'s position in the `@rule` graph. "
+            "Request the `Platform` to use as a `@rule` argument to get the appropriate `Platform`."
+        ),
+    )
     def current(cls) -> Platform:
+        return cls.create_for_localhost()
+
+    @classmethod
+    def create_for_localhost(cls) -> Platform:
+        """Creates a Platform instance for localhost.
+
+        This method should never be accessed directly by `@rules`: instead, to get the currently
+        active `Platform`, they should request a `Platform` as a positional argument.
+        """
         return Platform(f"{get_normalized_os_name()}_{get_normalized_arch_name()}")
 
 
@@ -31,7 +47,7 @@ class Platform(Enum):
 # which means replacing this singleton rule with a RootRule populated by an option.
 @rule
 def current_platform() -> Platform:
-    return Platform.current
+    return Platform.create_for_localhost()
 
 
 def rules() -> Iterable[Rule]:
