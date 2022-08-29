a7907210dd03e95df05e37a9a95d3fd3c020ab12
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2022-08-29 14:23:51 -0500

    Bootstrap scheduler ignores invalid BUILD file symbols when parsing (#16661)
    Part of the environment redesign from https://github.com/pantsbuild/pants/issues/7735.
    
    We decided (for now, subject to change) to model each environment's metadata via targets. This creates a chicken-and-egg problem with Pants's bootstrapping with `PluginResolver`. The `PluginResolver` runs a `VenvPex`, and thus needs to access several config files like `[GLOBAL].ca_certs_path` and `[python-bootstrap]`. To do that, we will need to load what `local_environment` targets the user has. That works fine, except that we will not yet have loaded most backends and any plugins, so it's very likely BUILD files will fail to parse due to unrecognized symbols.
    
    The solution is to simply ignore those symbols at the bootstrap stage. As shown by this PR's tests, any problematic symbols will be skipped; the valid targets still work. After the bootstrap stage, those BUILD files will be re-parsed and we will properly error on unrecognized symbols.
    
    The alternative approach I considered is creating a convention that environments should only be defined in specific BUILD files, without other targets. However, that is arbitrary to limit our users, and it would be hard to pull off because our code is based on `AddressFamilyDir`, which looks at all BUILD files in that directory, not only one of them.
    
    This does not yet properly handle `__defaults__`: https://github.com/pantsbuild/pants/issues/16669
    
    [ci skip-rust]
    [ci skip-build-wheels]

--- src/python/pants/engine/internals/mapper_test.py
+++ src/python/pants/engine/internals/mapper_test.py
@@ -8,6 +8,7 @@ from textwrap import dedent
 import pytest
 
 from pants.backend.project_info.filter_targets import FilterSubsystem, TargetGranularity
+from pants.base.exceptions import MappingError
 from pants.build_graph.address import Address
 from pants.build_graph.build_file_aliases import BuildFileAliases
 from pants.engine.internals.defaults import BuildFileDefaults, BuildFileDefaultsParserState
@@ -18,7 +19,7 @@ from pants.engine.internals.mapper import (
     DuplicateNameError,
     SpecsFilter,
 )
-from pants.engine.internals.parser import BuildFilePreludeSymbols, Parser
+from pants.engine.internals.parser import BuildFilePreludeSymbols, Parser, _unrecognized_symbol_func
 from pants.engine.internals.target_adaptor import TargetAdaptor
 from pants.engine.target import RegisteredTargetTypes, Tags, Target
 from pants.engine.unions import UnionMembership
@@ -26,9 +27,14 @@ from pants.testutil.option_util import create_goal_subsystem
 from pants.util.frozendict import FrozenDict
 
 
-def parse_address_map(build_file: str) -> AddressMap:
+def parse_address_map(build_file: str, *, ignore_unrecognized_symbols: bool = False) -> AddressMap:
     path = "/dev/null"
-    parser = Parser(build_root="", target_type_aliases=["thing"], object_aliases=BuildFileAliases())
+    parser = Parser(
+        build_root="",
+        target_type_aliases=["thing"],
+        object_aliases=BuildFileAliases(),
+        ignore_unrecognized_symbols=ignore_unrecognized_symbols,
+    )
     address_map = AddressMap.parse(
         path,
         build_file,
@@ -62,6 +68,32 @@ def test_address_map_parse() -> None:
         "one": TargetAdaptor(type_alias="thing", name="one", age=42),
         "two": TargetAdaptor(type_alias="thing", name="two", age=37),
     } == address_map.name_to_target_adaptor
+
+
+def test_address_map_unrecognized_symbol() -> None:
+    build_file = dedent(
+        """
+        thing(name="one")
+        thing(name="bad", age=fake)
+        thing(name="two")
+        another_fake()
+        yet_another()
+        thing(name="three")
+        and_finally(age=fakey)
+        """
+    )
+    address_map = parse_address_map(build_file, ignore_unrecognized_symbols=True)
+    assert {
+        "one": TargetAdaptor(type_alias="thing", name="one"),
+        "bad": TargetAdaptor(type_alias="thing", name="bad", age=_unrecognized_symbol_func),
+        "two": TargetAdaptor(type_alias="thing", name="two"),
+        "three": TargetAdaptor(
+            type_alias="thing",
+            name="three",
+        ),
+    } == address_map.name_to_target_adaptor
+    with pytest.raises(MappingError):
+        parse_address_map(build_file, ignore_unrecognized_symbols=False)
 
 
 def test_address_map_duplicate_names() -> None:
