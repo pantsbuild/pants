8ebe984ad6998fc02deba15a4b337992f1851b20
Christopher Neugebauer <chrisjrn@toolchain.com>
2022-09-14 13:15:44 -0700

    Removes plugin field scrubbing code and replaces with address checking
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/core/util_rules/environments_test.py
+++ src/python/pants/core/util_rules/environments_test.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from textwrap import dedent
-from typing import Any, cast
 
 import pytest
 
@@ -17,7 +16,6 @@ from pants.core.util_rules.environments import (
     AmbiguousEnvironmentError,
     ChosenLocalEnvironmentName,
     DockerEnvironmentTarget,
-    DockerImageField,
     EnvironmentField,
     EnvironmentName,
     EnvironmentNameRequest,
@@ -28,7 +26,6 @@ from pants.core.util_rules.environments import (
 )
 from pants.engine.target import FieldSet, OptionalSingleSourceField, Target
 from pants.testutil.rule_runner import QueryRule, RuleRunner, engine_error
-from pants.util.frozendict import FrozenDict
 
 
 @pytest.fixture
@@ -43,35 +40,6 @@ def rule_runner() -> RuleRunner:
         target_types=[LocalEnvironmentTarget, DockerEnvironmentTarget],
         singleton_environment=None,
     )
-
-
-class ScrubPluginFields:
-    """Helper functions to scrub the plugin field values from test return values.
-
-    Installing subsystems with environment-sensisitve options adds plugin fields that are visible
-    even when testing, which do not appear when manually constructing a target instance. These
-    methods will delete the plugin field instances from targets, so that youc an test for equality
-    with manually constructed targets.
-    """
-
-    @classmethod
-    def all_environment_targets(cls, tgts: AllEnvironmentTargets) -> None:
-        for target in tgts.values():
-            cls.target(target)
-
-    @classmethod
-    def environment_target(cls, env_tgt: EnvironmentTarget) -> EnvironmentTarget:
-        if env_tgt.val is not None:
-            cls.target(env_tgt.val)
-        return env_tgt
-
-    @classmethod
-    def target(cls, target: Target) -> None:
-        plugin_fields = set(target.plugin_fields)
-        with cast(Any, target)._unfrozen():
-            target.field_values = FrozenDict(
-                (i, j) for (i, j) in target.field_values.items() if i not in plugin_fields
-            )
 
 
 def test_all_environments(rule_runner: RuleRunner) -> None:
@@ -91,17 +59,11 @@ def test_all_environments(rule_runner: RuleRunner) -> None:
         ["--environments-preview-names={'e1': '//:e1', 'e2': '//:e2', 'docker': '//:docker'}"]
     )
     result = rule_runner.request(AllEnvironmentTargets, [])
-    ScrubPluginFields.all_environment_targets(result)
-
-    assert result == AllEnvironmentTargets(
-        {
-            "e1": LocalEnvironmentTarget({}, Address("", target_name="e1")),
-            "e2": LocalEnvironmentTarget({}, Address("", target_name="e2")),
-            "docker": DockerEnvironmentTarget(
-                {DockerImageField.alias: "centos6:latest"}, Address("", target_name="docker")
-            ),
-        }
-    )
+    assert {i: j.address for (i, j) in result.items()} == {
+        "e1": Address("", target_name="e1"),
+        "e2": Address("", target_name="e2"),
+        "docker": Address("", target_name="docker"),
+    }
 
 
 def test_choose_local_environment(rule_runner: RuleRunner) -> None:
@@ -120,15 +82,13 @@ def test_choose_local_environment(rule_runner: RuleRunner) -> None:
 
     def get_env() -> EnvironmentTarget:
         name = rule_runner.request(ChosenLocalEnvironmentName, [])
-        return ScrubPluginFields.environment_target(
-            rule_runner.request(EnvironmentTarget, [EnvironmentName(name.val)])
-        )
+        return rule_runner.request(EnvironmentTarget, [EnvironmentName(name.val)])
 
     # If `--names` is not set, do not choose an environment.
     assert get_env().val is None
 
     rule_runner.set_options(["--environments-preview-names={'e': '//:e1'}"])
-    assert get_env().val == LocalEnvironmentTarget({}, Address("", target_name="e1"))
+    assert get_env().val.address == Address("", target_name="e1")  # type: ignore[union-attr]
 
     # Error if `--names` set, but no compatible platforms
     rule_runner.set_options(["--environments-preview-names={'e': '//:not-compatible'}"])
@@ -185,9 +145,7 @@ def test_resolve_environment_tgt(rule_runner: RuleRunner) -> None:
     )
 
     def get_tgt(v: str | None) -> EnvironmentTarget:
-        return ScrubPluginFields.environment_target(
-            rule_runner.request(EnvironmentTarget, [EnvironmentName(v)])
-        )
+        return rule_runner.request(EnvironmentTarget, [EnvironmentName(v)])
 
     assert get_tgt(None).val is None
 
@@ -195,7 +153,7 @@ def test_resolve_environment_tgt(rule_runner: RuleRunner) -> None:
     with engine_error(AssertionError):
         get_tgt("bad")
 
-    assert get_tgt("env").val == LocalEnvironmentTarget({}, Address("", target_name="env"))
+    assert get_tgt("env").val.address == Address("", target_name="env")  # type: ignore[union-attr]
     with engine_error(ResolveError):
         get_tgt("bad-address")
 
