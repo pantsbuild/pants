43acdbecc0a843d03783c6b7c199bbf6b5c74a44
Joshua Cannon <joshdcannon@gmail.com>
2022-09-27 13:30:36 -0500

    fix test
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- tests/python/pants_test/init/test_options_initializer.py
+++ tests/python/pants_test/init/test_options_initializer.py
@@ -5,7 +5,6 @@ import unittest
 
 from pants.engine.env_vars import CompleteEnvironmentVars
 from pants.engine.internals.scheduler import ExecutionError
-from pants.engine.unions import UnionMembership
 from pants.init.options_initializer import OptionsInitializer
 from pants.option.options_bootstrapper import OptionsBootstrapper
 
@@ -20,15 +19,8 @@ class OptionsInitializerTest(unittest.TestCase):
 
         env = CompleteEnvironmentVars({})
         initializer = OptionsInitializer(options_bootstrapper)
-        build_config = initializer.build_config(options_bootstrapper, env)
         with self.assertRaises(ExecutionError):
-            initializer.options(
-                options_bootstrapper,
-                env,
-                build_config,
-                union_membership=UnionMembership({}),
-                raise_=True,
-            )
+            initializer.build_config(options_bootstrapper, env)
 
     def test_global_options_validation(self) -> None:
         # Specify an invalid combination of options.
@@ -39,11 +31,8 @@ class OptionsInitializerTest(unittest.TestCase):
         )
         env = CompleteEnvironmentVars({})
         initializer = OptionsInitializer(ob)
-        build_config = initializer.build_config(ob, env)
         with self.assertRaises(ExecutionError) as exc:
-            initializer.options(
-                ob, env, build_config, union_membership=UnionMembership({}), raise_=True
-            )
+            initializer.build_config(ob, env)
         self.assertIn(
             "The `--no-watch-filesystem` option may not be set if `--pantsd` or `--loop` is set.",
             str(exc.exception),
