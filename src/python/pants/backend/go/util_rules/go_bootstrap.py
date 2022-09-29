4640a6c5e1e2c94295a3ff52610a62de99c62f93
Christopher Neugebauer <chrisjrn@toolchain.com>
2022-09-29 14:13:21 -0700

    Make the go bootstrap more amenable to caching when asdf is not used
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/go/util_rules/go_bootstrap.py
+++ src/python/pants/backend/go/util_rules/go_bootstrap.py
@@ -5,74 +5,84 @@ from __future__ import annotations
 import logging
 import os
 from dataclasses import dataclass
+from typing import Iterable
 
 from pants.backend.go.subsystems.golang import GolangSubsystem
 from pants.core.util_rules import asdf
 from pants.core.util_rules.asdf import AsdfToolPathsRequest, AsdfToolPathsResult
 from pants.engine.env_vars import EnvironmentVars, EnvironmentVarsRequest
 from pants.engine.internals.selectors import Get
-from pants.engine.rules import collect_rules, rule
+from pants.engine.rules import collect_rules, rule, rule_helper
 
 logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
 class GoBootstrap:
-    raw_go_search_paths: tuple[str, ...]
-    asdf_standard_tool_paths: tuple[str, ...]
-    asdf_local_tool_paths: tuple[str, ...]
-    _path: str | None
+    go_search_paths: tuple[str, ...]
 
-    @property
-    def go_search_paths(self) -> tuple[str, ...]:
-        special_strings = {
-            "<PATH>": lambda: self.environment_paths,
-            "<ASDF>": lambda: self.asdf_standard_tool_paths,
-            "<ASDF_LOCAL>": lambda: self.asdf_local_tool_paths,
-        }
-        expanded: list[str] = []
-        for s in self.raw_go_search_paths:
-            if s in special_strings:
-                special_paths = special_strings[s]()
-                expanded.extend(special_paths)
-            else:
-                expanded.append(s)
-        return tuple(expanded)
 
-    @property
-    def environment_paths(self) -> list[str]:
-        """Returns a list of paths specified by the PATH env var."""
-        if self._path:
-            return self._path.split(os.pathsep)
-        return []
+@rule_helper
+async def _go_search_paths(
+    golang_subsystem: GolangSubsystem, paths: Iterable[str]
+) -> tuple[str, ...]:
+
+    resolve_standard, resolve_local = "<ASDF>" in paths, "<ASDF_LOCAL>" in paths
+
+    if resolve_standard or resolve_local:
+        # AsdfToolPathsResult is uncacheable, so only request it if absolutely necessary.
+        asdf_result = await Get(
+            AsdfToolPathsResult,
+            AsdfToolPathsRequest(
+                tool_name=golang_subsystem.asdf_tool_name,
+                tool_description="Go distribution",
+                resolve_standard=resolve_standard,
+                resolve_local=resolve_local,
+                paths_option_name="[golang].go_search_paths",
+                extra_env_var_names=(),
+                bin_relpath=golang_subsystem.asdf_bin_relpath,
+            ),
+        )
+        asdf_standard_tool_paths = asdf_result.standard_tool_paths
+        asdf_local_tool_paths = asdf_result.local_tool_paths
+    else:
+        asdf_standard_tool_paths = ()
+        asdf_local_tool_paths = ()
+
+    special_strings = {
+        "<ASDF>": lambda: asdf_standard_tool_paths,
+        "<ASDF_LOCAL>": lambda: asdf_local_tool_paths,
+    }
+
+    expanded: list[str] = []
+    for s in paths:
+        if s == "<PATH>":
+            expanded.extend(await _environment_paths())
+        elif s in special_strings:
+            special_paths = special_strings[s]()
+            expanded.extend(special_paths)
+        else:
+            expanded.append(s)
+    return tuple(expanded)
+
+
+@rule_helper
+async def _environment_paths() -> list[str]:
+    """Returns a list of paths specified by the PATH env var."""
+    env = await Get(EnvironmentVars, EnvironmentVarsRequest(("PATH",)))
+    path = env.get("PATH")
+    if path:
+        return path.split(os.pathsep)
+    return []
 
 
 @rule
 async def resolve_go_bootstrap(
     golang_subsystem: GolangSubsystem, golang_env_aware: GolangSubsystem.EnvironmentAware
 ) -> GoBootstrap:
-    raw_go_search_paths = golang_env_aware.raw_go_search_paths
-    result = await Get(
-        AsdfToolPathsResult,
-        AsdfToolPathsRequest(
-            tool_name=golang_subsystem.asdf_tool_name,
-            tool_description="Go distribution",
-            resolve_standard="<ASDF>" in raw_go_search_paths,
-            resolve_local="<ASDF_LOCAL>" in raw_go_search_paths,
-            paths_option_name="[golang].go_search_paths",
-            extra_env_var_names=(),
-            bin_relpath=golang_subsystem.asdf_bin_relpath,
-        ),
-    )
+    paths = await _go_search_paths(golang_subsystem, golang_env_aware.raw_go_search_paths)
 
-    env = await Get(EnvironmentVars, EnvironmentVarsRequest(("PATH",)))
-
-    return GoBootstrap(
-        raw_go_search_paths=raw_go_search_paths,
-        asdf_standard_tool_paths=result.standard_tool_paths,
-        asdf_local_tool_paths=result.local_tool_paths,
-        _path=env.get("PATH", None),
-    )
+    return GoBootstrap(go_search_paths=paths)
 
 
 def compatible_go_version(*, compiler_version: str, target_version: str) -> bool:
