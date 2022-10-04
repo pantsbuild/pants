ee0ae10661e8efcc94629cb7ba2331d19ae8a52b
Tom Dyas <tdyas@toolchain.com>
2022-10-03 21:26:13 -0400

    avoid for loop and use MultiGet
    [ci skip-rust]
    
    [ci skip-build-wheels]

--- src/python/pants/backend/go/goals/tailor.py
+++ src/python/pants/backend/go/goals/tailor.py
@@ -45,7 +45,7 @@ def has_package_main(content: bytes) -> bool:
     return _package_main_re.search(content) is not None
 
 
-def has_go_mod_ancestor(dirname: str, all_go_mod_dirs: set[str]) -> bool:
+def has_go_mod_ancestor(dirname: str, all_go_mod_dirs: frozenset[str]) -> bool:
     """We shouldn't add package targets if there is no `go.mod`, as it will cause an error."""
     return any(dirname.startswith(go_mod_dir) for go_mod_dir in all_go_mod_dirs)
 
@@ -100,44 +100,77 @@ async def _find_cgo_sources(
     return [*wildcard_globs, *sorted(files_to_add)], sorted(triggering_files)
 
 
+@dataclass(frozen=True)
+class FindPutativeGoPackageTargetRequest:
+    dir_path: str
+    files: tuple[str, ...]
+    all_go_mod_dirs: frozenset[str]
+
+
+@dataclass(frozen=True)
+class FindPutativeGoPackageTargetResult:
+    putative_target: PutativeTarget | None
+
+
+@rule
+async def find_putative_go_package_target(
+    request: FindPutativeGoPackageTargetRequest,
+    all_owned_sources: AllOwnedSources,
+) -> FindPutativeGoPackageTargetResult:
+    # Ignore paths that have `testdata` or `vendor` in them.
+    # From `go help packages`: Note, however, that a directory named vendor that itself
+    # contains code is not a vendored package: cmd/vendor would be a command named vendor.
+    dirname_parts = PurePath(request.dir_path).parts
+    if "testdata" in dirname_parts or "vendor" in dirname_parts[0:-1]:
+        return FindPutativeGoPackageTargetResult(None)
+    if not has_go_mod_ancestor(request.dir_path, request.all_go_mod_dirs):
+        return FindPutativeGoPackageTargetResult(None)
+
+    cgo_sources, triggering_cgo_files = await _find_cgo_sources(request.dir_path, all_owned_sources)
+    kwargs = {}
+    if cgo_sources:
+        kwargs = {"sources": ("*.go", *cgo_sources)}
+
+    return FindPutativeGoPackageTargetResult(
+        PutativeTarget.for_target_type(
+            GoPackageTarget,
+            path=request.dir_path,
+            name=None,
+            kwargs=kwargs,
+            triggering_sources=[*request.files, *triggering_cgo_files],
+        )
+    )
+
+
 @rule_helper
 async def _find_go_package_targets(
-    request: PutativeGoTargetsRequest, all_go_mod_dirs: set[str], all_owned_sources: AllOwnedSources
+    request: PutativeGoTargetsRequest,
+    all_go_mod_dirs: frozenset[str],
+    all_owned_sources: AllOwnedSources,
 ) -> list[PutativeTarget]:
-    putative_targets = []
     all_go_files = await Get(Paths, PathGlobs, request.path_globs("*.go"))
     unowned_go_files = set(all_go_files.files) - set(all_owned_sources)
-    for dirname, filenames in group_by_dir(unowned_go_files).items():
-        # Ignore paths that have `testdata` or `vendor` in them.
-        # From `go help packages`: Note, however, that a directory named vendor that itself
-        # contains code is not a vendored package: cmd/vendor would be a command named vendor.
-        dirname_parts = PurePath(dirname).parts
-        if "testdata" in dirname_parts or "vendor" in dirname_parts[0:-1]:
-            continue
-        if not has_go_mod_ancestor(dirname, all_go_mod_dirs):
-            continue
-
-        cgo_sources, triggering_cgo_files = await _find_cgo_sources(dirname, all_owned_sources)
-        kwargs = {}
-        if cgo_sources:
-            kwargs = {"sources": ("*.go", *cgo_sources)}
-
-        putative_targets.append(
-            PutativeTarget.for_target_type(
-                GoPackageTarget,
-                path=dirname,
-                name=None,
-                kwargs=kwargs,
-                triggering_sources=[*filenames, *triggering_cgo_files],
-            )
+    candidate_putative_targets = await MultiGet(
+        Get(
+            FindPutativeGoPackageTargetResult,
+            FindPutativeGoPackageTargetRequest(
+                dir_path=dirname,
+                files=tuple(filenames),
+                all_go_mod_dirs=all_go_mod_dirs,
+            ),
         )
-
-    return putative_targets
+        for dirname, filenames in group_by_dir(unowned_go_files).items()
+    )
+    return [
+        ptgt.putative_target
+        for ptgt in candidate_putative_targets
+        if ptgt.putative_target is not None
+    ]
 
 
 @rule_helper
 async def _find_go_binary_targets(
-    request: PutativeGoTargetsRequest, all_go_mod_dirs: set[str]
+    request: PutativeGoTargetsRequest, all_go_mod_dirs: frozenset[str]
 ) -> list[PutativeTarget]:
     all_go_files_digest_contents = await Get(DigestContents, PathGlobs, request.path_globs("*.go"))
 
@@ -185,7 +218,7 @@ async def find_putative_go_targets(
     putative_targets = []
     _all_go_mod_paths = await Get(Paths, PathGlobs, request.path_globs("go.mod"))
     all_go_mod_files = set(_all_go_mod_paths.files)
-    all_go_mod_dirs = {os.path.dirname(fp) for fp in all_go_mod_files}
+    all_go_mod_dirs = frozenset(os.path.dirname(fp) for fp in all_go_mod_files)
 
     if golang_subsystem.tailor_go_mod_targets:
         putative_targets.extend(await _find_go_mod_targets(all_go_mod_files, all_owned_sources))
