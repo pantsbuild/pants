6f728d9ddfa277534bed5cc4d3f20685db887d16
Jonas Stendahl <jonas@stendahl.me>
2022-10-07 01:51:38 +0200

    Move to experimental
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- build-support/bin/generate_docs.py
+++ build-support/bin/generate_docs.py
@@ -251,6 +251,7 @@ def run_pants_help_all() -> dict[str, Any]:
         "pants.backend.experimental.kotlin",
         "pants.backend.experimental.kotlin.lint.ktlint",
         "pants.backend.experimental.openapi",
+        "pants.backend.experimental.openapi.lint.spectral",
         "pants.backend.experimental.python",
         "pants.backend.experimental.python.lint.add_trailing_comma",
         "pants.backend.experimental.python.lint.autoflake",
