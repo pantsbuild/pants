faeaa25af23f26c55a8860c00aa01715440c8f20
Eric Arellano <ericarellano@me.com>
2022-10-07 11:29:04 -0500

    Fix platform_rules.py too
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/engine/internals/platform_rules.py
+++ src/python/pants/engine/internals/platform_rules.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 from pants.core.util_rules.environments import (
     DockerImageField,
     DockerPlatformField,
+    EnvironmentsSubsystem,
     EnvironmentTarget,
     RemotePlatformField,
 )
@@ -19,7 +20,11 @@ from pants.util.logging import LogLevel
 
 
 @rule
-def current_platform(env_tgt: EnvironmentTarget, global_options: GlobalOptions) -> Platform:
+def current_platform(
+    env_tgt: EnvironmentTarget,
+    global_options: GlobalOptions,
+    environments_subsystem: EnvironmentsSubsystem,
+) -> Platform:
     if env_tgt.val:
         if env_tgt.val.has_field(DockerPlatformField):
             return Platform(env_tgt.val[DockerPlatformField].normalized_value)
@@ -28,18 +33,20 @@ def current_platform(env_tgt: EnvironmentTarget, global_options: GlobalOptions) 
         # Else, it's a local environment.
         return Platform.create_for_localhost()
 
-    # Else, the environments mechanism is not used. For now, at least, we continue to support
-    # enabling remote execution globally for every build via the `--remote-execution` option.
-    return (
-        Platform.linux_x86_64
-        if global_options.remote_execution
-        else Platform.create_for_localhost()
-    )
+    # If the environments mechanism is not used, fall back to legacy behavior of
+    # `--remote-execution` being a global toggle.
+    if not environments_subsystem.names and global_options.remote_execution:
+        return Platform.linux_x86_64
+
+    return Platform.create_for_localhost()
 
 
 @rule
 async def complete_environment_vars(
-    session_values: SessionValues, env_tgt: EnvironmentTarget, global_options: GlobalOptions
+    session_values: SessionValues,
+    env_tgt: EnvironmentTarget,
+    global_options: GlobalOptions,
+    environments_subsystem: EnvironmentsSubsystem,
 ) -> CompleteEnvironmentVars:
     # If a local environment is used, we simply use SessionValues. Otherwise, we need to run `env`
     # and parse the output.
@@ -57,7 +64,9 @@ async def complete_environment_vars(
             # Else, it's a local environment.
             return session_values[CompleteEnvironmentVars]
     else:
-        if global_options.remote_execution:
+        # If the environments mechanism is not used, fall back to legacy behavior of
+        # `--remote-execution` being a global toggle.
+        if not environments_subsystem.names and global_options.remote_execution:
             description_of_env_source = "the remote execution environment"
         else:
             return session_values[CompleteEnvironmentVars]
