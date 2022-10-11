fc9433063e1aabd0228b915490f74e2ed9b359bf
Joshua Cannon <joshdcannon@gmail.com>
2022-10-11 15:01:12 -0500

    sweet!
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/build_files/fmt/black/register.py
+++ src/python/pants/backend/build_files/fmt/black/register.py
@@ -1,37 +1,17 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import os
-
+from pants.backend.build_files.fmt.base import FmtBuildFilesRequest
 from pants.backend.python.lint.black import subsystem as black_subsystem
 from pants.backend.python.lint.black.rules import _run_black
 from pants.backend.python.lint.black.subsystem import Black
-from pants.core.goals.fmt import FmtFilesRequest, FmtResult, Partitions
-from pants.engine.internals.build_files import BuildFileOptions
+from pants.core.goals.fmt import FmtResult
 from pants.engine.rules import collect_rules, rule
-from pants.source.filespec import FilespecMatcher
 from pants.util.logging import LogLevel
 
 
-class BlackRequest(FmtFilesRequest):
+class BlackRequest(FmtBuildFilesRequest):
     tool_subsystem = Black
-
-
-@rule
-async def partition_build_files(
-    request: BlackRequest.PartitionRequest,
-    black: Black,
-    build_file_options: BuildFileOptions,
-) -> Partitions:
-    if black.skip:
-        return Partitions()
-
-    specified_build_files = FilespecMatcher(
-        includes=[os.path.join("**", p) for p in build_file_options.patterns],
-        excludes=build_file_options.ignores,
-    ).matches(request.files)
-
-    return Partitions.single_partition(specified_build_files)
 
 
 @rule(desc="Format with Black", level=LogLevel.DEBUG)
