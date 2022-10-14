b1e8acbb09a9d6f2caf52cb14d6e4d23af8b7a65
Stu Hood <stuhood@gmail.com>
2022-10-14 16:47:25 -0700

    Increase verbosity to DEBUG level.
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- build-support/reapi-sample-server/entrypoint.sh
+++ build-support/reapi-sample-server/entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -xeuo pipefail
 
-bgd server start --verbose buildgrid/data/config/default.yml &
+bgd server start -vvv buildgrid/data/config/default.yml &
 
-bgd bot --verbose --remote http://localhost:50051 --remote-cas http://localhost:50051 host-tools
+bgd bot -vvv --remote http://localhost:50051 --remote-cas http://localhost:50051 host-tools
