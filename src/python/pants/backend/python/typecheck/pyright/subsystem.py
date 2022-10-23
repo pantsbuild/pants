cc0a15d445c1b31ccc458f8b803aad60161adea7
SJ <suresh@vicarasolutions.com>
2022-10-22 21:36:09 -0400

    Attempting to handle interpreter/resolve partitioning by ripping off mypy
    # Rust tests and lints will be skipped. Delete if not intended.
    [ci skip-rust]
    
    # Building wheels and fs_util will be skipped. Delete if not intended.
    [ci skip-build-wheels]

--- src/python/pants/backend/python/typecheck/pyright/subsystem.py
+++ src/python/pants/backend/python/typecheck/pyright/subsystem.py
@@ -3,7 +3,8 @@
 
 from __future__ import annotations
 
-from pants.option.option_types import ArgsListOption, SkipOption
+from pants.backend.python.util_rules.interpreter_constraints import InterpreterConstraints
+from pants.option.option_types import ArgsListOption, SkipOption, StrListOption
 from pants.option.subsystem import Subsystem
 from pants.util.strutil import softwrap
 
@@ -22,3 +23,17 @@ class Pyright(Subsystem):
 
     skip = SkipOption("check")
     args = ArgsListOption(example="--version")
+
+    _interpreter_constraints = StrListOption(
+        advanced=True,
+        default=["CPython>=3.7,<4"],
+        help="Python interpreter constraints for Pyright (which is, itself, a NodeJS tool).",
+    )
+
+    @property
+    def interpreter_constraints(self) -> InterpreterConstraints:
+        """The interpreter constraints to use when installing and running the tool.
+
+        This assumes you have set the class property `register_interpreter_constraints = True`.
+        """
+        return InterpreterConstraints(self._interpreter_constraints)
