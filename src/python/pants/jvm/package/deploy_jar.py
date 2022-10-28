7ec49bd35633c23d2f4d80d00ca14553e0ed25cd
Antonio Alonso Dominguez <alonso.domin@pm.me>
2022-10-28 13:22:20 +0200

    Upgrade JAR stripper dependencies

--- src/python/pants/jvm/package/deploy_jar.py
+++ src/python/pants/jvm/package/deploy_jar.py
@@ -12,7 +12,6 @@ from pants.core.goals.package import (
     PackageFieldSet,
 )
 from pants.core.goals.run import RunFieldSet
-from pants.core.util_rules.system_binaries import BashBinary, ZipBinary
 from pants.engine.addresses import Addresses
 from pants.engine.fs import EMPTY_DIGEST, AddPrefix, Digest, MergeDigests
 from pants.engine.rules import Get, collect_rules, rule
@@ -39,11 +38,6 @@ from pants.jvm.target_types import (
 )
 
 logger = logging.getLogger(__name__)
-
-
-_JAVA_MANIFEST_FILENAME = "META-INF/MANIFEST.MF"
-_PANTS_MANIFEST_PARTIAL_JAR_FILENAME = "pants_manifest_only.notajar"
-_PANTS_CAT_AND_REPAIR_ZIP_FILENAME = "_cat_and_repair_zip_files.sh"
 
 
 @dataclass(frozen=True)
@@ -98,18 +92,14 @@ async def deploy_jar_classpath(
 
 @rule
 async def package_deploy_jar(
-    bash: BashBinary,
-    zip: ZipBinary,
     jvm: JvmSubsystem,
     field_set: DeployJarFieldSet,
 ) -> BuiltPackage:
     """
     Constructs a deploy ("fat") JAR file by
     1. Resolving/compiling a Classpath for the `root_address` target,
-    2. Producing a ZIP file containing _only_ the JAR manifest file for the `main_class`
-    3. Creating a deploy jar with a broken ZIP index by concatenating all dependency JARs together,
-       followed by the thin JAR we created
-    4. Using the unix `zip` utility's repair function to fix the broken fat jar
+    2. Creating a deploy jar with a valid ZIP index and deduplicated entries
+    3. Strip the jar from all metadata that may cause it to be non-reproducible (https://reproducible-builds.org)
     """
 
     if field_set.main_class.value is None:
@@ -121,6 +111,10 @@ async def package_deploy_jar(
 
     classpath = await Get(Classpath, Addresses([field_set.address]))
     classpath_digest = await Get(Digest, MergeDigests(classpath.digests()))
+
+    #
+    # 2. Use Pants' JAR tool to build a runnable fat JAR
+    #
 
     output_filename = PurePath(field_set.output_path.value_or_default(file_ending="jar"))
     jar_digest = await Get(
@@ -137,6 +131,10 @@ async def package_deploy_jar(
         ),
     )
 
+    #
+    # 3. Strip the JAR from  all non-reproducible metadata if requested so
+    #
+
     if jvm.reproducible_jars:
         jar_digest = await Get(
             Digest,
@@ -146,101 +144,8 @@ async def package_deploy_jar(
             ),
         )
 
-    #
-    # 2. Produce JAR manifest, and output to a ZIP file that can be included with the JARs
-    #
-
-    # main_class = field_set.main_class.value
-
-    # manifest_content = FileContent(
-    #     _JAVA_MANIFEST_FILENAME,
-    #     # NB: we're joining strings with newlines, because the JAR manifest format
-    #     # needs precise indentation, and _cannot_ start with a blank line. `dedent` seriously
-    #     # messes up those requirements.
-    #     "\n".join(
-    #         [
-    #             "Manifest-Version: 1.0",
-    #             f"Main-Class: {main_class}",
-    #             "",  # THIS BLANK LINE WILL BREAK EVERYTHING IF DELETED. DON'T DELETE IT.
-    #         ]
-    #     ).encode("utf-8"),
-    # )
-
-    # manifest_jar_input_digest = await Get(Digest, CreateDigest([manifest_content]))
-    # manifest_jar_result = await Get(
-    #     ProcessResult,
-    #     Process(
-    #         argv=[
-    #             zip.path,
-    #             _PANTS_MANIFEST_PARTIAL_JAR_FILENAME,
-    #             _JAVA_MANIFEST_FILENAME,
-    #         ],
-    #         description="Build partial JAR containing manifest file",
-    #         input_digest=manifest_jar_input_digest,
-    #         output_files=[_PANTS_MANIFEST_PARTIAL_JAR_FILENAME],
-    #     ),
-    # )
-
-    # manifest_jar = manifest_jar_result.output_digest
-    # if jvm.reproducible_jars:
-    #     manifest_jar = await Get(
-    #         Digest,
-    #         StripJarRequest(
-    #             digest=manifest_jar,
-    #             filenames=(_PANTS_MANIFEST_PARTIAL_JAR_FILENAME,),
-    #         ),
-    #     )
-
-    #
-    # 3/4. Create broken deploy JAR, then repair it with `zip -FF`
-    #
-
-    # NB. Concatenating multiple ZIP files produces a zip file that is _mostly_ safe to
-    # be distributed (it can be fixed with `-FF`), so that's how we construct our fat JAR
-    # without exploding the files to disk.
-    #
-    # `ZIP` files are extracted top-to-bottom and archives can have duplicate names
-    # (e.g. `META-INF/MANIFEST.MF`). In the case of a `JAR` file, the JVM will understand the
-    # last file with that file name to be the actual one. Therefore, our thin JAR needs to be
-    # appear at the end of the file for (in particular) our manifest to take precedence.
-    # If there are duplicate classnames at a given package address fat JARs, then
-    # behaviour will be non-deterministic. Sorry!  --chrisjrn
-
-    # output_filename = PurePath(field_set.output_path.value_or_default(file_ending="jar"))
-    # input_filenames = " ".join(shlex.quote(i) for i in classpath.args())
-    # _PANTS_BROKEN_DEPLOY_JAR = "pants_broken_deploy_jar.notajar"
-    # cat_and_repair_script = FileContent(
-    #     _PANTS_CAT_AND_REPAIR_ZIP_FILENAME,
-    #     # Using POSIX location/arg format for `cat`. If this gets more complicated, refactor.
-    #     textwrap.dedent(
-    #         f"""
-    #         set -e
-    #         /bin/cat {input_filenames} {_PANTS_MANIFEST_PARTIAL_JAR_FILENAME} > {_PANTS_BROKEN_DEPLOY_JAR}
-    #         {zip.path} -FF {_PANTS_BROKEN_DEPLOY_JAR} --out {output_filename.name}
-    #         """
-    #     ).encode("utf-8"),
-    # )
-
-    # cat_and_repair_script_digest = await Get(Digest, CreateDigest([cat_and_repair_script]))
-    # broken_deploy_jar_inputs_digest = await Get(
-    #     Digest,
-    #     MergeDigests([*classpath.digests(), cat_and_repair_script_digest, manifest_jar]),
-    # )
-
-    # cat_and_repair = await Get(
-    #     ProcessResult,
-    #     Process(
-    #         argv=[bash.path, _PANTS_CAT_AND_REPAIR_ZIP_FILENAME],
-    #         input_digest=broken_deploy_jar_inputs_digest,
-    #         output_files=[output_filename.name],
-    #         description="Assemble combined JAR file",
-    #     ),
-    # )
-
     renamed_output_digest = await Get(Digest, AddPrefix(jar_digest, str(output_filename.parent)))
-
     artifact = BuiltPackageArtifact(relpath=str(output_filename))
-
     return BuiltPackage(digest=renamed_output_digest, artifacts=(artifact,))
 
 
