pantsbuild/pants

commit 91d68f1949e73db7529c13c5b8ba754b82994876

Refactor plumbing for `test` goal to support batched tests. (#17134)
To support batched test execution, this updates the API of the core `test` goal to use the generic partitioning APIs currently used by `lint`/`fmt`/`fix`.

I've left a handful of TODOs in place for user-facing API changes that will follow from this change, because I wanted to leave them separate from the plugin API change.

src/python/pants/core/goals/test.py
@@ -9,8 +9,9 @@ from abc import ABC, ABCMeta
 from dataclasses import dataclass
 from enum import Enum
 from pathlib import PurePath
-from typing import Any, ClassVar, Optional, TypeVar, cast
+from typing import Any, ClassVar, Iterable, Optional, TypeVar, cast
 
+from pants.core.goals.multi_tool_goal_helper import SkippableSubsystem
 from pants.core.goals.package import BuiltPackage, PackageFieldSet
 from pants.core.subsystems.debug_adapter import DebugAdapterSubsystem
 from pants.core.util_rules.distdir import DistDir
@@ -18,6 +19,14 @@ from pants.core.util_rules.environments import (
     ChosenLocalEnvironmentName,
     EnvironmentName,
     EnvironmentNameRequest,
+)
+from pants.core.util_rules.partitions import (
+    PartitionElementT,
+    PartitionerType,
+    PartitionMetadataT,
+    Partitions,
+    _BatchBase,
+    _PartitionFieldSetsRequestBase,
 )
 from pants.engine.addresses import Address, UnparsedAddressInputs
 from pants.engine.collection import Collection
@@ -50,10 +59,12 @@ from pants.engine.target import (
     ValidNumbers,
     parse_shard_spec,
 )
-from pants.engine.unions import UnionMembership, union
+from pants.engine.unions import UnionMembership, UnionRule, distinct_union_type_per_subclass, union
 from pants.option.option_types import BoolOption, EnumOption, IntOption, StrListOption, StrOption
+from pants.util.collections import partition_sequentially
 from pants.util.docutil import bin_name
 from pants.util.logging import LogLevel
+from pants.util.meta import classproperty
 from pants.util.strutil import softwrap
 
 logger = logging.getLogger(__name__)
@@ -66,9 +77,10 @@ class TestResult(EngineAwareReturnType):
     stdout_digest: FileDigest
     stderr: str
     stderr_digest: FileDigest
-    address: Address
+    addresses: tuple[Address, ...]
     output_setting: ShowOutput
     result_metadata: ProcessResultMetadata | None
+    partition_description: str | None = None
 
     coverage_data: CoverageData | None = None
     # TODO: Rename this to `reports`. There is no guarantee that every language will produce
@@ -80,22 +92,39 @@ class TestResult(EngineAwareReturnType):
     # Prevent this class from being detected by pytest as a test class.
     __test__ = False
 
-    @classmethod
-    def skip(cls, address: Address, output_setting: ShowOutput) -> TestResult:
-        return cls(
+    @staticmethod
+    def skip(address: Address, output_setting: ShowOutput) -> TestResult:
+        return TestResult(
             exit_code=None,
             stdout="",
             stderr="",
             stdout_digest=EMPTY_FILE_DIGEST,
             stderr_digest=EMPTY_FILE_DIGEST,
-            address=address,
+            addresses=(address,),
             output_setting=output_setting,
             result_metadata=None,
         )
 
-    @classmethod
+    @staticmethod
+    def skip_batch(
+        batch: TestRequest.Batch[_TestFieldSetT, Any], output_setting: ShowOutput
+    ) -> TestResult:
+        return TestResult(
+            exit_code=None,
+            stdout="",
+            stderr="",
+            stdout_digest=EMPTY_FILE_DIGEST,
+            stderr_digest=EMPTY_FILE_DIGEST,
+            addresses=tuple(field_set.address for field_set in batch.elements),
+            output_setting=output_setting,
+            result_metadata=None,
+            partition_description=(
+                batch.partition_metadata.description if batch.partition_metadata else None
+            ),
+        )
+
+    @staticmethod
     def from_fallible_process_result(
-        cls,
         process_result: FallibleProcessResult,
         address: Address,
         output_setting: ShowOutput,
@@ -104,13 +133,13 @@ class TestResult(EngineAwareReturnType):
         xml_results: Snapshot | None = None,
         extra_output: Snapshot | None = None,
     ) -> TestResult:
-        return cls(
+        return TestResult(
             exit_code=process_result.exit_code,
             stdout=process_result.stdout.decode(),
             stdout_digest=process_result.stdout_digest,
             stderr=process_result.stderr.decode(),
             stderr_digest=process_result.stderr_digest,
-            address=address,
+            addresses=(address,),
             output_setting=output_setting,
             result_metadata=process_result.metadata,
             coverage_data=coverage_data,
@@ -118,9 +147,50 @@ class TestResult(EngineAwareReturnType):
             extra_output=extra_output,
         )
 
+    @staticmethod
+    def from_batched_fallible_process_result(
+        process_result: FallibleProcessResult,
+        batch: TestRequest.Batch[_TestFieldSetT, Any],
+        output_setting: ShowOutput,
+        *,
+        coverage_data: CoverageData | None = None,
+        xml_results: Snapshot | None = None,
+        extra_output: Snapshot | None = None,
+    ) -> TestResult:
+        return TestResult(
+            exit_code=process_result.exit_code,
+            stdout=process_result.stdout.decode(),
+            stdout_digest=process_result.stdout_digest,
+            stderr=process_result.stderr.decode(),
+            stderr_digest=process_result.stderr_digest,
+            addresses=tuple(field_set.address for field_set in batch.elements),
+            output_setting=output_setting,
+            result_metadata=process_result.metadata,
+            coverage_data=coverage_data,
+            xml_results=xml_results,
+            extra_output=extra_output,
+            partition_description=(
+                batch.partition_metadata.description if batch.partition_metadata else None
+            ),
+        )
+
     @property
     def skipped(self) -> bool:
         return self.exit_code is None or self.result_metadata is None
+
+    @property
+    def description(self) -> str:
+        if len(self.addresses) == 1:
+            return self.addresses[0].spec
+
+        return f"{self.addresses[0].spec} and {len(self.addresses)-1} other files"
+
+    @property
+    def path_safe_description(self) -> str:
+        if len(self.addresses) == 1:
+            return self.addresses[0].path_safe_spec
+
+        return f"{self.addresses[0].path_safe_spec}+{len(self.addresses)-1}"
 
     def __lt__(self, other: Any) -> bool:
         """We sort first by status (skipped vs failed vs succeeded), then alphanumerically within
@@ -128,7 +198,7 @@ class TestResult(EngineAwareReturnType):
         if not isinstance(other, TestResult):
             return NotImplemented
         if self.exit_code == other.exit_code:
-            return self.address.spec < other.address.spec
+            return self.description < other.description
         if self.skipped or self.exit_code is None:
             return True
         if other.skipped or other.exit_code is None:
@@ -154,6 +224,8 @@ class TestResult(EngineAwareReturnType):
             return "skipped."
         status = "succeeded" if self.exit_code == 0 else f"failed (exit code {self.exit_code})"
         message = f"{status}."
+        if self.partition_description:
+            message += f"\nPartition: {self.partition_description}"
         if self.output_setting == ShowOutput.NONE or (
             self.output_setting == ShowOutput.FAILED and self.exit_code == 0
         ):
@@ -168,7 +240,7 @@ class TestResult(EngineAwareReturnType):
         return f"{message}{output}"
 
     def metadata(self) -> dict[str, Any]:
-        return {"address": self.address.spec}
+        return {"addresses": [address.spec for address in self.addresses]}
 
     def cacheable(self) -> bool:
         """Is marked uncacheable to ensure that it always renders."""
@@ -206,6 +278,73 @@ class TestFieldSet(FieldSet, metaclass=ABCMeta):
     sources: SourcesField
 
     __test__ = False
+
+
+_TestFieldSetT = TypeVar("_TestFieldSetT", bound=TestFieldSet)
+
+
+@union
+class TestRequest:
+    """Base class for plugin types wanting to be run as part of `test`.
+
+    Plugins should define a new type which subclasses this type, and set the
+    appropriate class variables.
+    E.g.
+        class DryCleaningRequest(TestRequest):
+            tool_subsystem = DryCleaningSubsystem
+            field_set_type = DryCleaningFieldSet
+
+    Then register the rules which tell Pants about your plugin.
+    E.g.
+        def rules():
+            return [
+                *collect_rules(),
+                *DryCleaningRequest.rules(),
+            ]
+    """
+
+    tool_subsystem: ClassVar[type[SkippableSubsystem]]
+    field_set_type: ClassVar[type[TestFieldSet]]
+    partitioner_type: ClassVar[PartitionerType] = PartitionerType.DEFAULT_ONE_PARTITION_PER_INPUT
+
+    __test__ = False
+
+    @classproperty
+    def tool_name(cls) -> str:
+        return cls.tool_subsystem.options_scope
+
+    @distinct_union_type_per_subclass(in_scope_types=[EnvironmentName])
+    class PartitionRequest(_PartitionFieldSetsRequestBase[_TestFieldSetT]):
+        pass
+
+    @distinct_union_type_per_subclass(in_scope_types=[EnvironmentName])
+    class Batch(_BatchBase[PartitionElementT, PartitionMetadataT]):
+        @property
+        def single_element(self) -> PartitionElementT:
+            """Return the single element of this batch.
+
+            NOTE: Accessing this property will raise a `TypeError` if this `Batch` contains
+            >1 elements. It is only safe to be used by test runners utilizing the "default"
+            one-input-per-partition partitioner type.
+            """
+
+            if len(self.elements) != 1:
+                description = ""
+                if self.partition_metadata:
+                    description = f" from partition '{self.partition_metadata.description}'"
+                raise TypeError(
+                    f"Expected a single element in batch{description}, but found {len(self.elements)}"
+                )
+
+            return self.elements[0]
+
+    @classmethod
+    def rules(cls) -> Iterable:
+        yield from cls.partitioner_type.default_rules(cls, by_file=False)
+
+        yield UnionRule(TestRequest, cls)
+        yield UnionRule(TestRequest.PartitionRequest, cls.PartitionRequest)
+        yield UnionRule(TestRequest.Batch, cls.Batch)
 
 
 class CoverageData(ABC):
@@ -487,8 +626,51 @@ class TestExtraEnvVarsField(StringSequenceField, metaclass=ABCMeta):
 
 
 @rule_helper
+async def _get_test_batches(
+    core_request_types: Iterable[type[TestRequest]],
+    targets_to_field_sets: TargetRootsToFieldSets,
+    local_environment_name: ChosenLocalEnvironmentName,
+) -> list[TestRequest.Batch]:
+    def partitions_get(request_type: type[TestRequest]) -> Get[Partitions]:
+        partition_type = cast(TestRequest, request_type)
+        field_set_type = partition_type.field_set_type
+        applicable_field_sets: list[TestFieldSet] = []
+        for target, field_sets in targets_to_field_sets.mapping.items():
+            if field_set_type.is_applicable(target):
+                applicable_field_sets.extend(field_sets)
+
+        partition_request = partition_type.PartitionRequest(tuple(applicable_field_sets))
+        return Get(
+            Partitions,
+            {
+                partition_request: TestRequest.PartitionRequest,
+                local_environment_name.val: EnvironmentName,
+            },
+        )
+
+    all_partitions = await MultiGet(
+        partitions_get(request_type) for request_type in core_request_types
+    )
+
+    return [
+        request_type.Batch(
+            cast(TestRequest, request_type).tool_name, tuple(batch), partition.metadata
+        )
+        for request_type, partitions in zip(core_request_types, all_partitions)
+        for partition in partitions
+        for batch in partition_sequentially(
+            # TODO: Expose max batch size as a subsystem parameter.
+            partition.elements,
+            key=lambda x: str(x),
+            size_target=128,
+            size_max=128,
+        )
+    ]
+
+
+@rule_helper
 async def _run_debug_tests(
-    targets_to_valid_field_sets: TargetRootsToFieldSets,
+    batches: Iterable[TestRequest.Batch],
     test_subsystem: TestSubsystem,
     debug_adapter: DebugAdapterSubsystem,
     local_environment_name: ChosenLocalEnvironmentName,
@@ -499,15 +681,15 @@ async def _run_debug_tests(
         (
             Get(
                 TestDebugRequest,
-                {field_set: TestFieldSet, local_environment_name.val: EnvironmentName},
+                {batch: TestRequest.Batch, local_environment_name.val: EnvironmentName},
             )
             if not test_subsystem.debug_adapter
             else Get(
                 TestDebugAdapterRequest,
-                {field_set: TestFieldSet, local_environment_name.val: EnvironmentName},
+                {batch: TestRequest.Batch, local_environment_name.val: EnvironmentName},
             )
         )
-        for field_set in targets_to_valid_field_sets.field_sets
+        for batch in batches
     )
     exit_code = 0
     for debug_request in debug_requests:
@@ -531,6 +713,29 @@ async def _run_debug_tests(
         if debug_result.exit_code != 0:
             exit_code = debug_result.exit_code
     return Test(exit_code)
+
+
+@rule(desc="Determine environment for partition", level=LogLevel.DEBUG)
+async def get_batch_environment(batch: TestRequest.Batch) -> EnvironmentName:
+    environment_names_per_element = await MultiGet(
+        Get(
+            EnvironmentName,
+            EnvironmentNameRequest,
+            EnvironmentNameRequest.from_field_set(field_set),
+        )
+        for field_set in batch.elements
+    )
+    unique_environments = len({name.val for name in environment_names_per_element})
+    if unique_environments != 1:
+        batch_description = "Test batch"
+        if batch.partition_metadata:
+            batch_description = f"{batch_description} '{batch.partition_metadata.description}'"
+
+        raise AssertionError(
+            # TODO: Print conflicting field sets in env.
+            f"{batch_description} contains elements from {unique_environments} environments; exactly 1 environment is expected"
+        )
+    return environment_names_per_element[0]
 
 
 @goal_rule
@@ -566,27 +771,28 @@ async def run_tests(
         ),
     )
 
+    request_types = union_membership.get(TestRequest)
+    test_batches = await _get_test_batches(
+        request_types,
+        targets_to_valid_field_sets,
+        local_environment_name,
+    )
+
     if test_subsystem.debug or test_subsystem.debug_adapter:
         return await _run_debug_tests(
-            targets_to_valid_field_sets,
-            test_subsystem,
-            debug_adapter,
-            local_environment_name,
+            test_batches, test_subsystem, debug_adapter, local_environment_name
         )
 
-    environment_names_per_field_set = await MultiGet(
+    environment_names = await MultiGet(
         Get(
-            EnvironmentName,
-            EnvironmentNameRequest,
-            EnvironmentNameRequest.from_field_set(field_set),
+            EnvironmentName, {batch: TestRequest.Batch, local_environment_name.val: EnvironmentName}
         )
-        for field_set in targets_to_valid_field_sets.field_sets
+        for batch in test_batches
     )
+
     results = await MultiGet(
-        Get(TestResult, {field_set: TestFieldSet, environment_name: EnvironmentName})
-        for field_set, environment_name in zip(
-            targets_to_valid_field_sets.field_sets, environment_names_per_field_set
-        )
+        Get(TestResult, {batch: TestRequest.Batch, environment_name: EnvironmentName})
+        for batch, environment_name in zip(test_batches, environment_names)
     )
 
     # Print summary.
@@ -604,7 +810,7 @@ async def run_tests(
         if result.extra_output and result.extra_output.files:
             workspace.write_digest(
                 result.extra_output.digest,
-                path_prefix=str(distdir.relpath / "test" / result.address.path_safe_spec),
+                path_prefix=str(distdir.relpath / "test" / result.path_safe_description),
             )
 
     if test_subsystem.report:
@@ -706,7 +912,7 @@ def _format_test_summary(result: TestResult, run_id: RunId, console: Console) ->
         elapsed_print = f"in {elapsed_secs:.2f}s"
 
     suffix = f" {elapsed_print}{source_print}"
-    return f"{sigil} {result.address} {status}{suffix}."
+    return f"{sigil} {result.description} {status}{suffix}."
 
 
 @dataclass(frozen=True)

src/python/pants/base/specs_integration_test.py
@@ -80,8 +80,8 @@ def test_address_literal() -> None:
         assert run(["list", *list_specs]).stdout.splitlines() == list_specs
 
         test_result = run(["test", f"{tmpdir}/py:tests"]).stderr
-        assert f"{tmpdir}/py/utils/strutil_test.py:../tests - succeeded." in test_result
-        assert f"{tmpdir}/py/base/common_test.py:../tests - succeeded." in test_result
+        assert f"{tmpdir}/py/utils/strutil_test.py:../tests succeeded" in test_result
+        assert f"{tmpdir}/py/base/common_test.py:../tests succeeded" in test_result
         assert f"{tmpdir}/py:tests" not in test_result
 
 
@@ -109,14 +109,14 @@ def test_sibling_addresses() -> None:
         # Even though no `python_test` targets are explicitly defined in `util/`, a generated
         # target is resident there.
         test_result = run(["test", f"{tmpdir}/py/utils:"]).stderr
-        assert f"{tmpdir}/py/utils/strutil_test.py:../tests - succeeded." in test_result
+        assert f"{tmpdir}/py/utils/strutil_test.py:../tests succeeded" in test_result
         assert f"{tmpdir}/py/base/common_test.py:../tests" not in test_result
         assert f"{tmpdir}/py:tests" not in test_result
 
         # Even though no `_test.py` files live in this dir, we match the `python_tests` target
         # and replace it with its generated targets.
         test_result = run(["test", f"{tmpdir}/py:"]).stderr
-        assert f"{tmpdir}/py/utils/strutil_test.py:../tests - succeeded." in test_result
+        assert f"{tmpdir}/py/utils/strutil_test.py:../tests succeeded" in test_result
         assert f"{tmpdir}/py/base/common_test.py:../tests" in test_result
         assert f"{tmpdir}/py:tests" not in test_result
 
@@ -137,7 +137,7 @@ def test_descendent_addresses() -> None:
         ]
 
         test_result = run(["test", f"{tmpdir}/py::"]).stderr
-        assert f"{tmpdir}/py/utils/strutil_test.py:../tests - succeeded." in test_result
+        assert f"{tmpdir}/py/utils/strutil_test.py:../tests succeeded" in test_result
         assert f"{tmpdir}/py/base/common_test.py:../tests" in test_result
         assert f"{tmpdir}/py:tests" not in test_result
 

src/python/pants/backend/go/goals/test.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 import os
 from dataclasses import dataclass
-from typing import Sequence
+from typing import Any, Sequence
 
 from pants.backend.go.subsystems.gotest import GoTestSubsystem
 from pants.backend.go.target_types import (
@@ -42,6 +42,7 @@ from pants.core.goals.test import (
     TestDebugRequest,
     TestExtraEnv,
     TestFieldSet,
+    TestRequest,
     TestResult,
     TestSubsystem,
 )
@@ -104,6 +105,12 @@ class GoTestFieldSet(TestFieldSet):
         return tgt.get(SkipGoTestsField).value
 
 
+class GoTestRequest(TestRequest):
+    # TODO: Remove the type-ignore after adding a `skip` option to the subsystem.
+    tool_subsystem = GoTestSubsystem  # type: ignore[assignment]
+    field_set_type = GoTestFieldSet
+
+
 def transform_test_args(args: Sequence[str], timeout_field_value: int | None) -> tuple[str, ...]:
     result = []
     i = 0
@@ -161,12 +168,14 @@ def transform_test_args(args: Sequence[str], timeout_field_value: int | None) ->
 
 @rule(desc="Test with Go", level=LogLevel.DEBUG)
 async def run_go_tests(
-    field_set: GoTestFieldSet,
+    batch: GoTestRequest.Batch[GoTestFieldSet, Any],
     test_subsystem: TestSubsystem,
     go_test_subsystem: GoTestSubsystem,
     test_extra_env: TestExtraEnv,
     goroot: GoRoot,
 ) -> TestResult:
+    field_set = batch.single_element
+
     maybe_pkg_analysis, maybe_pkg_digest, dependencies = await MultiGet(
         Get(FallibleFirstPartyPkgAnalysis, FirstPartyPkgAnalysisRequest(field_set.address)),
         Get(FallibleFirstPartyPkgDigest, FirstPartyPkgDigestRequest(field_set.address)),
@@ -180,7 +189,7 @@ async def run_go_tests(
             stderr=stderr or "",
             stdout_digest=EMPTY_FILE_DIGEST,
             stderr_digest=EMPTY_FILE_DIGEST,
-            address=field_set.address,
+            addresses=(field_set.address,),
             output_setting=test_subsystem.output,
             result_metadata=None,
         )
@@ -418,13 +427,13 @@ async def run_go_tests(
 
 
 @rule
-async def generate_go_tests_debug_request(field_set: GoTestFieldSet) -> TestDebugRequest:
+async def generate_go_tests_debug_request(_: GoTestRequest.Batch) -> TestDebugRequest:
     raise NotImplementedError("This is a stub.")
 
 
 @rule
 async def generate_go_tests_debug_adapter_request(
-    field_set: GoTestFieldSet,
+    _: GoTestRequest.Batch,
 ) -> TestDebugAdapterRequest:
     raise NotImplementedError("This is a stub.")
 
@@ -433,4 +442,5 @@ def rules():
     return [
         *collect_rules(),
         UnionRule(TestFieldSet, GoTestFieldSet),
+        *GoTestRequest.rules(),
     ]

src/python/pants/backend/codegen/protobuf/go/rules_integration_test.py
@@ -21,7 +21,7 @@ from pants.backend.codegen.protobuf.target_types import (
 from pants.backend.codegen.protobuf.target_types import rules as protobuf_target_types_rules
 from pants.backend.go import target_type_rules
 from pants.backend.go.goals import test
-from pants.backend.go.goals.test import GoTestFieldSet
+from pants.backend.go.goals.test import GoTestFieldSet, GoTestRequest
 from pants.backend.go.target_types import GoModTarget, GoPackageTarget
 from pants.backend.go.util_rules import (
     assembly,
@@ -74,7 +74,7 @@ def rule_runner() -> RuleRunner:
             QueryRule(HydratedSources, [HydrateSourcesRequest]),
             QueryRule(GeneratedSources, [GenerateGoFromProtobufRequest]),
             QueryRule(DigestContents, (Digest,)),
-            QueryRule(TestResult, (GoTestFieldSet,)),
+            QueryRule(TestResult, (GoTestRequest.Batch,)),
         ],
         target_types=[
             GoModTarget,
@@ -289,7 +289,9 @@ def test_generates_go(rule_runner: RuleRunner) -> None:
         env_inherit=PYTHON_BOOTSTRAP_ENV,
     )
     tgt = rule_runner.get_target(Address("src/go/people", target_name="pkg"))
-    result = rule_runner.request(TestResult, [GoTestFieldSet.create(tgt)])
+    result = rule_runner.request(
+        TestResult, [GoTestRequest.Batch("", (GoTestFieldSet.create(tgt),), None)]
+    )
     assert result.exit_code == 0
     assert "PASS: TestProtoGen" in result.stdout
 

commit 1964cc4441e2eef3fe01dae3c5f1d7ab48e55a56

Restore debug hints for `test` execution (Cherry-pick of #17406) (#17407)
Closes #17386

91d68f1949e73db7529c13c5b8ba754b82994876 broke streaming output for `test`. This commit should fix the problem by having the new partitioning API's types subclass `EngineAwareParameter`.

For now I've only implemented `debug_hint` for the `TestRequest.Batch` type, because it's the direct replacement for the old `TestFieldSet`-driven API.

src/python/pants/core/goals/test.py
@@ -21,7 +21,6 @@ from pants.core.util_rules.environments import (
     EnvironmentNameRequest,
 )
 from pants.core.util_rules.partitions import (
-    PartitionElementT,
     PartitionerType,
     PartitionMetadataT,
     Partitions,
@@ -311,12 +310,13 @@ class TestRequest:
 
     @distinct_union_type_per_subclass(in_scope_types=[EnvironmentName])
     class PartitionRequest(_PartitionFieldSetsRequestBase[_TestFieldSetT]):
-        pass
+        def metadata(self) -> dict[str, Any]:
+            return {"addresses": [field_set.address.spec for field_set in self.field_sets]}
 
     @distinct_union_type_per_subclass(in_scope_types=[EnvironmentName])
-    class Batch(_BatchBase[PartitionElementT, PartitionMetadataT]):
+    class Batch(_BatchBase[_TestFieldSetT, PartitionMetadataT]):
         @property
-        def single_element(self) -> PartitionElementT:
+        def single_element(self) -> _TestFieldSetT:
             """Return the single element of this batch.
 
             NOTE: Accessing this property will raise a `TypeError` if this `Batch` contains
@@ -333,6 +333,18 @@ class TestRequest:
                 )
 
             return self.elements[0]
+
+        def debug_hint(self) -> str:
+            if len(self.elements) == 1:
+                return self.elements[0].address.spec
+
+            return f"{self.elements[0].address.spec} and {len(self.elements)-1} other files"
+
+        def metadata(self) -> dict[str, Any]:
+            return {
+                "addresses": [field_set.address.spec for field_set in self.elements],
+                "partition_description": self.partition_metadata.description,
+            }
 
     @classmethod
     def rules(cls) -> Iterable:

src/python/pants/core/util_rules/partitions.py
@@ -14,6 +14,7 @@ from typing_extensions import Protocol
 
 from pants.core.goals.multi_tool_goal_helper import SkippableSubsystem
 from pants.engine.collection import Collection
+from pants.engine.engine_aware import EngineAwareParameter
 from pants.engine.internals.selectors import Get, MultiGet
 from pants.engine.rules import collect_rules, rule
 from pants.engine.target import (
@@ -154,7 +155,7 @@ class Partitions(Collection[Partition[PartitionElementT, PartitionMetadataT]]):
 @frozen_after_init
 @dataclass(unsafe_hash=True)
 @runtime_ignore_subscripts
-class _BatchBase(Generic[PartitionElementT, PartitionMetadataT]):
+class _BatchBase(Generic[PartitionElementT, PartitionMetadataT], EngineAwareParameter):
     """Base class for a collection of elements that should all be processed together.
 
     For example, a collection of strings pointing to files that should be linted in one process, or
@@ -168,7 +169,7 @@ class _BatchBase(Generic[PartitionElementT, PartitionMetadataT]):
 
 @dataclass(frozen=True)
 @runtime_ignore_subscripts
-class _PartitionFieldSetsRequestBase(Generic[_FieldSetT]):
+class _PartitionFieldSetsRequestBase(Generic[_FieldSetT], EngineAwareParameter):
     """Returns a unique type per calling type.
 
     This serves us 2 purposes:
@@ -180,7 +181,7 @@ class _PartitionFieldSetsRequestBase(Generic[_FieldSetT]):
 
 
 @dataclass(frozen=True)
-class _PartitionFilesRequestBase:
+class _PartitionFilesRequestBase(EngineAwareParameter):
     """Returns a unique type per calling type.
 
     This serves us 2 purposes:

src/python/pants/base/specs_integration_test.py
@@ -80,8 +80,8 @@ def test_address_literal() -> None:
         assert run(["list", *list_specs]).stdout.splitlines() == list_specs
 
         test_result = run(["test", f"{tmpdir}/py:tests"]).stderr
-        assert f"{tmpdir}/py/utils/strutil_test.py:../tests succeeded" in test_result
-        assert f"{tmpdir}/py/base/common_test.py:../tests succeeded" in test_result
+        assert f"{tmpdir}/py/utils/strutil_test.py:../tests - succeeded." in test_result
+        assert f"{tmpdir}/py/base/common_test.py:../tests - succeeded." in test_result
         assert f"{tmpdir}/py:tests" not in test_result
 
 
@@ -109,14 +109,14 @@ def test_sibling_addresses() -> None:
         # Even though no `python_test` targets are explicitly defined in `util/`, a generated
         # target is resident there.
         test_result = run(["test", f"{tmpdir}/py/utils:"]).stderr
-        assert f"{tmpdir}/py/utils/strutil_test.py:../tests succeeded" in test_result
+        assert f"{tmpdir}/py/utils/strutil_test.py:../tests - succeeded." in test_result
         assert f"{tmpdir}/py/base/common_test.py:../tests" not in test_result
         assert f"{tmpdir}/py:tests" not in test_result
 
         # Even though no `_test.py` files live in this dir, we match the `python_tests` target
         # and replace it with its generated targets.
         test_result = run(["test", f"{tmpdir}/py:"]).stderr
-        assert f"{tmpdir}/py/utils/strutil_test.py:../tests succeeded" in test_result
+        assert f"{tmpdir}/py/utils/strutil_test.py:../tests - succeeded." in test_result
         assert f"{tmpdir}/py/base/common_test.py:../tests" in test_result
         assert f"{tmpdir}/py:tests" not in test_result
 
@@ -137,7 +137,7 @@ def test_descendent_addresses() -> None:
         ]
 
         test_result = run(["test", f"{tmpdir}/py::"]).stderr
-        assert f"{tmpdir}/py/utils/strutil_test.py:../tests succeeded" in test_result
+        assert f"{tmpdir}/py/utils/strutil_test.py:../tests - succeeded." in test_result
         assert f"{tmpdir}/py/base/common_test.py:../tests" in test_result
         assert f"{tmpdir}/py:tests" not in test_result
 