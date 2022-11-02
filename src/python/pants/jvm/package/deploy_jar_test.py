4050ad01a5d1624823548fa5ba6cff960415064e
A. Alonso Dominguez <2269440+alonsodomin@users.noreply.github.com>
2022-11-02 19:49:59 +0000

    Handle duplicate JAR entries in `deploy_jar` (#17419)
    Rescues the jar-tool binary used in Pants v1 to generate JAR files with a duplicate policy

--- src/python/pants/jvm/package/deploy_jar_test.py
+++ src/python/pants/jvm/package/deploy_jar_test.py
@@ -17,6 +17,7 @@ from pants.core.util_rules.system_binaries import BashBinary, UnzipBinary
 from pants.engine.process import Process, ProcessResult
 from pants.jvm import jdk_rules
 from pants.jvm.classpath import rules as classpath_rules
+from pants.jvm.jar_tool import jar_tool
 from pants.jvm.jdk_rules import InternalJdk, JvmProcess
 from pants.jvm.package.deploy_jar import DeployJarFieldSet
 from pants.jvm.package.deploy_jar import rules as deploy_jar_rules
@@ -24,7 +25,7 @@ from pants.jvm.resolve import jvm_tool
 from pants.jvm.resolve.coursier_fetch import CoursierResolvedLockfile
 from pants.jvm.resolve.coursier_test_util import EMPTY_JVM_LOCKFILE
 from pants.jvm.strip_jar import strip_jar
-from pants.jvm.target_types import DeployJarTarget, JvmArtifactTarget
+from pants.jvm.target_types import DeployJarTarget, JarDuplicateRule, JvmArtifactTarget
 from pants.jvm.testutil import maybe_skip_jdk_test
 from pants.jvm.util_rules import rules as util_rules
 from pants.testutil.rule_runner import PYTHON_BOOTSTRAP_ENV, QueryRule, RuleRunner
@@ -38,6 +39,7 @@ def rule_runner() -> RuleRunner:
             *classpath_rules(),
             *jvm_tool.rules(),
             *strip_jar.rules(),
+            *jar_tool.rules(),
             *deploy_jar_rules(),
             *javac_rules(),
             *jdk_rules.rules(),
@@ -56,6 +58,7 @@ def rule_runner() -> RuleRunner:
             JvmArtifactTarget,
             DeployJarTarget,
         ],
+        objects={JarDuplicateRule.alias: JarDuplicateRule},
     )
     rule_runner.set_options(args=[], env_inherit=PYTHON_BOOTSTRAP_ENV)
     return rule_runner
@@ -290,6 +293,49 @@ def test_deploy_jar_coursier_deps(rule_runner: RuleRunner) -> None:
                         dependencies=[
                             ":example",
                         ],
+                    )
+
+                    java_sources(
+                        name="example",
+                        sources=["**/*.java", ],
+                        dependencies=[
+                            ":com.fasterxml.jackson.core_jackson-databind",
+                        ],
+                    )
+
+                    jvm_artifact(
+                        name = "com.fasterxml.jackson.core_jackson-databind",
+                        group = "com.fasterxml.jackson.core",
+                        artifact = "jackson-databind",
+                        version = "2.12.5",
+                    )
+                """
+            ),
+            "3rdparty/jvm/default.lock": COURSIER_LOCKFILE_SOURCE,
+            "Example.java": JAVA_MAIN_SOURCE,
+            "lib/ExampleLib.java": JAVA_JSON_MANGLING_LIB_SOURCE,
+        }
+    )
+
+    _deploy_jar_test(rule_runner, "example_app_deploy_jar")
+
+
+@maybe_skip_jdk_test
+def test_deploy_jar_coursier_deps_duplicate_policy(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(
+        {
+            "BUILD": dedent(
+                """\
+                    deploy_jar(
+                        name="example_app_deploy_jar",
+                        main="org.pantsbuild.example.Example",
+                        output_path="dave.jar",
+                        dependencies=[
+                            ":example",
+                        ],
+                        duplicate_policy=[
+                            duplicate_rule(pattern="^org/pantsbuild/example/lib", action="replace")
+                        ]
                     )
 
                     java_sources(
