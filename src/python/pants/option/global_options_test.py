c493b3963b19e8a3aa99122bc5f082b895d80d79
Asher Foa <1268088+asherf@users.noreply.github.com>
2022-11-08 16:47:19 -0500

    update test

--- src/python/pants/option/global_options_test.py
+++ src/python/pants/option/global_options_test.py
@@ -3,9 +3,8 @@
 
 from __future__ import annotations
 
-import sys
 from pathlib import Path
-from textwrap import dedent
+from typing import Callable
 
 import pytest
 
@@ -14,9 +13,37 @@ from pants.engine.env_vars import CompleteEnvironmentVars
 from pants.engine.internals.scheduler import ExecutionError
 from pants.engine.unions import UnionMembership
 from pants.init.options_initializer import OptionsInitializer
-from pants.option.global_options import DynamicRemoteOptions, GlobalOptions
+from pants.option.global_options import (
+    AuthPluginResult,
+    AuthPluginState,
+    DynamicRemoteOptions,
+    GlobalOptions,
+)
 from pants.option.options_bootstrapper import OptionsBootstrapper
 from pants.testutil.option_util import create_options_bootstrapper
+
+
+def get_remote_auth_plugin_func(expected_state: AuthPluginState):
+    def auth_func(initial_execution_headers, initial_store_headers, options, **kwargs):
+
+        return AuthPluginResult(
+            plugin_name="pants-unit-tests",
+            state=expected_state,
+            execution_headers={
+                **{k: "baz" for k in initial_execution_headers},
+                "exec": "xyz",
+            },
+            store_headers={
+                **{k: "baz" for k in initial_store_headers},
+                "store": "abc",
+                "store_url": options.for_global_scope().remote_store_address,
+            },
+            instance_name="custom_instance",
+            store_address="grpc://custom_store",
+            execution_address="grpc://custom_exec",
+        )
+
+    return auth_func
 
 
 def create_dynamic_remote_options(
@@ -24,7 +51,7 @@ def create_dynamic_remote_options(
     initial_headers: dict[str, str] | None = None,
     address: str | None = "grpc://fake.url:10",
     token_path: str | None = None,
-    plugin: str | None = None,
+    remote_auth_plugin_func: Callable | None = None,
 ) -> DynamicRemoteOptions:
     if initial_headers is None:
         initial_headers = {}
@@ -38,15 +65,13 @@ def create_dynamic_remote_options(
     ]
     if token_path:
         args.append(f"--remote-oauth-bearer-token-path={token_path}")
-    if plugin:
-        args.append(f"--remote-auth-plugin={plugin}")
     ob = create_options_bootstrapper(args)
     env = CompleteEnvironmentVars({})
     oi = OptionsInitializer(ob)
     _build_config = oi.build_config(ob, env)
     options = oi.options(ob, env, _build_config, union_membership=UnionMembership({}), raise_=False)
     return DynamicRemoteOptions.from_options(
-        options, env, remote_auth_plugin_func=_build_config.remote_auth_plugin_func
+        options, env, remote_auth_plugin_func=remote_auth_plugin_func
     )[0]
 
 
@@ -63,43 +88,10 @@ def test_dynamic_remote_options_oauth_bearer_token_path(tmp_path: Path) -> None:
 
 
 def test_dynamic_remote_options_auth_plugin(tmp_path: Path) -> None:
-    def compute_options(state: str, tempdir: Path) -> DynamicRemoteOptions:
-        # NB: For an unknown reason, if we use the same file name for multiple runs, the plugin
-        # result gets memoized. So, we use a distinct file name.
-        plugin_path = tempdir / f"auth_plugin_{state}.py"
-        plugin_path.touch()
-        plugin_path.write_text(
-            dedent(
-                f"""\
-                from pants.option.global_options import AuthPluginState, AuthPluginResult
-
-                def auth_func(initial_execution_headers, initial_store_headers, options, **kwargs):
-                    return AuthPluginResult(
-                        state=AuthPluginState.{state},
-                        execution_headers={{
-                            **{{k: "baz" for k in initial_execution_headers}},
-                            "exec": "xyz",
-                        }},
-                        store_headers={{
-                            **{{k: "baz" for k in initial_store_headers}},
-                            "store": "abc",
-                            "store_url": options.for_global_scope().remote_store_address,
-                        }},
-                        instance_name="custom_instance",
-                        store_address="grpc://custom_store",
-                        execution_address="grpc://custom_exec",
-                    )
-                """
-            )
-        )
-        sys.path.append(tempdir.as_posix())
-        result = create_dynamic_remote_options(
-            initial_headers={"foo": "bar"}, plugin=f"auth_plugin_{state}:auth_func"
-        )
-        sys.path.pop()
-        return result
-
-    opts = compute_options("OK", tmp_path)
+    opts = create_dynamic_remote_options(
+        initial_headers={"foo": "bar"},
+        remote_auth_plugin_func=get_remote_auth_plugin_func(AuthPluginState.OK),
+    )
     assert opts.store_headers == {
         "store": "abc",
         "foo": "baz",
@@ -114,7 +106,10 @@ def test_dynamic_remote_options_auth_plugin(tmp_path: Path) -> None:
     assert opts.store_address == "http://custom_store"
     assert opts.execution_address == "http://custom_exec"
 
-    opts = compute_options("UNAVAILABLE", tmp_path)
+    opts = create_dynamic_remote_options(
+        initial_headers={"foo": "bar"},
+        remote_auth_plugin_func=get_remote_auth_plugin_func(AuthPluginState.UNAVAILABLE),
+    )
     assert opts.cache_read is False
     assert opts.cache_write is False
     assert opts.execution is False
