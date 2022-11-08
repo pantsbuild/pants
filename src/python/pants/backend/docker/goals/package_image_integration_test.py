b597ed3b502cdbc09a028f304b6c2c5f6b02489c
Daniel Moran <danxmoran@gmail.com>
2022-11-08 10:00:58 -0500

    Flip the default for `pull` on `docker_image` to `False` (Cherry-pick of #17459) (#17460)
    Flip the default for `pull` on `docker_image` to `False` (#17459)
    
    Closes #17458
    
    57746408819a19862123600447ea28cee8021064 added the `pull` field to `docker_image`, with a default of `True`. Changing the default behavior in that way caused multi-stage builds with "transient" base images to break, because Docker can't pull the base images that only exist locally. This commit fixes the problem by flipping the default to `False`.
    
    IMO a default of `False` makes sense regardless of the bug it fixes, because it aligns with the default behavior of `docker build`. Users who want to always pull by default can use `__defaults__` to achieve the same effect.
    
    I've added a simple integration test to guard against regressions here in the future.

--- src/python/pants/backend/docker/goals/package_image_integration_test.py
+++ src/python/pants/backend/docker/goals/package_image_integration_test.py
@@ -3,6 +3,8 @@
 
 from __future__ import annotations
 
+from textwrap import dedent
+
 import pytest
 
 from pants.backend.docker.goals.package_image import DockerFieldSet
@@ -46,12 +48,39 @@ def run_docker(
     return result
 
 
-def test_docker_build(rule_runner) -> None:
+def test_docker_build(rule_runner: RuleRunner) -> None:
     """This test requires a running docker daemon."""
     rule_runner.write_files(
         {
             "src/BUILD": "docker_image(name='test-image', image_tags=['1.0'])",
             "src/Dockerfile": "FROM python:3.8",
+        }
+    )
+    target = rule_runner.get_target(Address("src", target_name="test-image"))
+    result = run_docker(rule_runner, target)
+    assert len(result.artifacts) == 1
+    assert len(result.artifacts[0].extra_log_lines) == 2
+    assert "Built docker image: test-image:1.0" == result.artifacts[0].extra_log_lines[0]
+    assert "Docker image ID:" in result.artifacts[0].extra_log_lines[1]
+    assert "<unknown>" not in result.artifacts[0].extra_log_lines[1]
+
+
+def test_docker_build_multi_layer(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(
+        {
+            "src/BUILD": dedent(
+                """\
+                docker_image(name='test-image', image_tags=['1.0'])
+                docker_image(name='base', source='Dockerfile.base')
+                """
+            ),
+            "src/Dockerfile": dedent(
+                """\
+                ARG BASE=src:base
+                FROM $BASE
+                """
+            ),
+            "src/Dockerfile.base": "FROM python:3.8",
         }
     )
     target = rule_runner.get_target(Address("src", target_name="test-image"))
