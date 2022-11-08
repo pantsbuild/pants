0189342784a483dfa0a8aac53c325669babc33f2
Andreas Stenius <andreas.stenius@imanage.com>
2022-11-08 18:51:23 -0500

    Ensure lockfile target exists before injecting a dependency to it. (#17365)
    * Ensure lockfile target exists before injecting a dependency to it.
    
    * add feature toggle for python lockfiles targets.
    
    * Refactor requirement generators to use a common rule helper.
    
    * Warn when lockfile targets are being shadowed.
    
    * make ignored arg optional

--- src/python/pants/backend/python/macros/python_requirements_test.py
+++ src/python/pants/backend/python/macros/python_requirements_test.py
@@ -7,6 +7,7 @@ from textwrap import dedent
 
 import pytest
 
+from pants.backend.python.goals import lockfile
 from pants.backend.python.macros import python_requirements
 from pants.backend.python.macros.python_requirements import PythonRequirementsTargetGenerator
 from pants.backend.python.target_types import PythonRequirementTarget
@@ -21,6 +22,7 @@ from pants.testutil.rule_runner import QueryRule, RuleRunner, engine_error
 def rule_runner() -> RuleRunner:
     return RuleRunner(
         rules=[
+            *lockfile.rules(),
             *python_requirements.rules(),
             QueryRule(_TargetParametrizations, [_TargetParametrizationsRequest]),
         ],
