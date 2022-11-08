869b08b7a834556070215dd654474ccb2b589c98
Benjy Weinberger <benjyw@gmail.com>
2022-11-08 15:20:10 -0800

    Add Ponder to list of Pants users.

--- build-support/bin/generate_user_list.py
+++ build-support/bin/generate_user_list.py
@@ -87,6 +87,7 @@ _orgs = (
         "https://www.peopledatalabs.com/",
         "https://files.readme.io/8c4f5cd-peopledatalabs-small.png",
     ),
+    Org("Ponder", "https://ponder.io/", "https://files.readme.io/fd34269-ponder.png"),
     Org(
         "Rippling",
         "https://www.rippling.com/",
