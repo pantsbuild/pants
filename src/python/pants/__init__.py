ace547413e29809bf75dd5921e60efee5e18b3b7
Joshua <joshdcannon@gmail.com>
2022-11-17 10:53:55 -0600

    Make it explicit

---
+++ src/python/pants/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+# NB: Mark this as an explicit namespace packages
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
