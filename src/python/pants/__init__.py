da89e229db81cb3c6fefe5ef155968411a35d4f9
Joshua <joshdcannon@gmail.com>
2022-11-17 10:55:28 -0600

    more comment

--- src/python/pants/__init__.py
+++ src/python/pants/__init__.py
@@ -1,5 +1,6 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-# NB: Mark this as an explicit namespace packages
+# NB: Mark this as an explicit namespace packages, so that `pants.testutil`
+# can be loaded, if installed.
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
