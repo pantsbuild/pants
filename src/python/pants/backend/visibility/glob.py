ee2602476804f1114aad75b2e2cb0ccee60661cc
Andreas Stenius <andreas.stenius@imanage.com>
2022-11-23 12:30:16 -0500

    Add TargetGlob selector class.

--- src/python/pants/backend/visibility/glob.py
+++ src/python/pants/backend/visibility/glob.py
@@ -4,9 +4,12 @@ from __future__ import annotations
 
 import os.path
 import re
+from collections.abc import Mapping, Sequence
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import Pattern
+from typing import Any, Pattern
+
+from pants.util.strutil import softwrap
 
 
 class PathGlobAnchorMode(Enum):
@@ -29,14 +32,24 @@ class PathGlob:
     anchor_mode: PathGlobAnchorMode = field(compare=False)
     glob: Pattern = field(compare=False)
 
+    def __str__(self) -> str:
+        if self.anchor_mode is PathGlobAnchorMode.INVOKED_PATH and self.raw:
+            return f"./{self.raw}"
+        elif self.anchor_mode is PathGlobAnchorMode.DECLARED_PATH:
+            return self.raw
+        else:
+            return f"{self.anchor_mode.value}{self.raw}"
+
     @classmethod
     def parse(cls, pattern: str, base: str) -> PathGlob:
+        if not isinstance(pattern, str):
+            raise ValueError(f"invalid path glob, expected string but got: {pattern!r}")
         anchor_mode = PathGlobAnchorMode.parse(pattern)
         glob = os.path.normpath(pattern).lstrip("./")
         if anchor_mode is PathGlobAnchorMode.DECLARED_PATH:
             glob = os.path.join(base, glob)
         return cls(
-            raw=pattern,
+            raw=glob,
             anchor_mode=anchor_mode,
             glob=cls._parse_pattern(glob),
         )
@@ -74,3 +87,104 @@ class PathGlob:
                 )
             )
         )
+
+
+@dataclass(frozen=True)
+class TargetGlob:
+    type_: str | None
+    path: PathGlob | None
+    tags: tuple[str, ...] | None
+
+    def __post_init__(self) -> None:
+        if not isinstance(self.type_, (str, type(None))):
+            raise ValueError(f"invalid target type, expected glob but got: {self.type_!r}")
+        if not isinstance(self.path, (PathGlob, type(None))):
+            raise ValueError(f"invalid target path, expected glob but got: {self.path!r}")
+        if not isinstance(self.tags, (tuple, type(None))):
+            raise ValueError(
+                f"invalid target tags, expected sequence of values but got: {self.tags!r}"
+            )
+
+    def __str__(self) -> str:
+        # Note: when there are more selection criteria used than is supported as a single text
+        # value, switch over to a dict based representation.
+        tags = (
+            f"({', '.join(str(tag) if ',' not in tag else repr(tag) for tag in self.tags)})"
+            if self.tags is not None
+            else ""
+        )
+        path = f"[{self.path}]" if self.path is not None else ""
+        return f"{self.type_ or ''}{tags}{path}" or "*"
+
+    @classmethod
+    def parse(cls, spec: str | Mapping[str, Any], relpath: str) -> TargetGlob:
+        if isinstance(spec, str):
+            spec_dict = cls._parse_string(spec)
+            from pprint import pprint
+
+            pprint(spec_dict)
+        elif isinstance(spec, Mapping):
+            spec_dict = spec
+        else:
+            raise ValueError(f"invalid target spec, expected string or dict but got: {spec!r}")
+
+        return cls(
+            type_=spec_dict.get("type"),
+            path=PathGlob.parse(spec_dict["path"], relpath)
+            if spec_dict.get("path") is not None
+            else None,
+            tags=cls._parse_tags(spec_dict.get("tags")),
+        )
+
+    @staticmethod
+    def _parse_string(spec: str) -> Mapping[str, Any]:
+        match = re.match(
+            r"""
+            (?P<type>[^([]*)
+            (?:
+              \(
+                (?P<tags>[^)]*)
+              \)
+            )?
+            (?:
+              \[
+                (?P<path>[^]]*)
+              \]
+            )?
+            $
+            """,
+            spec,
+            re.VERBOSE,
+        )
+        if not match:
+            raise ValueError(
+                f"invalid target spec string with optional parts of 'target-glob(tag-value, ...)[path-glob]' "
+                f"but got: {spec!r}"
+            )
+        print(match, match.groups())
+        return match.groupdict()
+
+    @staticmethod
+    def _parse_tags(tags: str | Sequence[str] | None) -> tuple[Any, ...] | None:
+        if tags is None:
+            return None
+        if isinstance(tags, str):
+            if "'" in tags or '"' in tags:
+                raise ValueError(
+                    softwrap(
+                        f"""
+                        Quotes not supported for tags rule selector in string form, quotes only
+                        needed for tag values with commas in them otherwise simply remove the
+                        quotes. For embedded commas, use the dictionary syntax:
+
+                          {{"tags": [{tags!r}, ...], ...}}
+                        """
+                    )
+                )
+            tags = tags.split(",")
+        if not isinstance(tags, Sequence):
+            raise ValueError(
+                f"invalid tags, expected a tag or a list of tags but got: {type(tags).__name__}"
+            )
+        tags = tuple(str(tag).strip() for tag in tags)
+        return tags
--- src/python/pants/backend/visibility/glob_test.py
+++ src/python/pants/backend/visibility/glob_test.py
@@ -2,26 +2,33 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 from __future__ import annotations
 
+from typing import Any, Mapping
+
 import pytest
 
-from pants.backend.visibility.glob import PathGlob
+from pants.backend.visibility.glob import PathGlob, TargetGlob
 
 
 @pytest.mark.parametrize(
-    "pattern, base, anchor_mode, glob",
+    "pattern, base, anchor_mode, raw, glob, text",
     [
-        ("foo", "base", "", "foo$"),
-        (".", "base", ".", "$"),
-        ("./foo", "base", ".", "foo$"),
-        ("/foo", "base", "/", "base/foo$"),
-        ("//foo", "base", "//", "foo$"),
+        ("foo", "base", "", "foo", "foo$", "foo"),
+        (".", "base", ".", "", "$", "."),
+        ("./foo", "base", ".", "foo", "foo$", "./foo"),
+        ("/foo", "base", "/", "base/foo", "base/foo$", "base/foo"),
+        ("//foo", "base", "//", "foo", "foo$", "//foo"),
+        ("foo/**/bar", "base", "", "foo/**/bar", "foo(/.*)?/bar$", "foo/**/bar"),
+        ("foo/../bar", "base", "", "bar", "bar$", "bar"),
     ],
 )
-def test_parse_pattern(pattern: str, base: str, anchor_mode: str, glob: str) -> None:
+def test_parse_pattern(
+    pattern: str, base: str, anchor_mode: str, raw: str, glob: str, text: str
+) -> None:
     parsed = PathGlob.parse(pattern, base)
-    assert pattern == parsed.raw
+    assert raw == parsed.raw
     assert anchor_mode == parsed.anchor_mode.value
     assert glob == parsed.glob.pattern
+    assert text == str(parsed)
 
 
 @pytest.mark.parametrize(
@@ -103,3 +110,26 @@ def test_match(glob: PathGlob, tests: tuple[tuple[str, str, bool], ...]) -> None
     for path, base, expected in tests:
         print(path, base)
         assert expected == glob.match(path, base)
+
+
+@pytest.mark.parametrize(
+    "target_spec, expected",
+    [
+        ({}, "*"),
+        ("", "*"),
+        (dict(type="resources"), "resources"),
+        (dict(type="file", path="glob/*/this.ext"), "file[glob/*/this.ext]"),
+        (dict(path="glob/*/this.ext"), "[glob/*/this.ext]"),
+        (dict(tags=["tagged"]), "(tagged)"),
+        (dict(tags=["tag-a", "tag-b , b", "c"]), "(tag-a, 'tag-b , b', c)"),
+        (dict(type="file*", tags=["foo", "bar"], path="baz.txt"), "file*(foo, bar)[baz.txt]"),
+        ("resources", "resources"),
+        ("file[glob/*/this.ext]", "file[glob/*/this.ext]"),
+        ("[glob/*/this.ext]", "[glob/*/this.ext]"),
+        ("(tag-a)", "(tag-a)"),
+        ("(tag-a  ,  tag-b)", "(tag-a, tag-b)"),
+        ("file*(foo, bar)[baz.txt]", "file*(foo, bar)[baz.txt]"),
+    ],
+)
+def test_target_glob_parse_spec(target_spec: str | Mapping[str, Any], expected: str) -> None:
+    assert expected == str(TargetGlob.parse(target_spec, "base"))
