9e51969bf7fca3c3672066a7304d1318bbd9e50c
Andreas Stenius <andreas.stenius@imanage.com>
2022-11-23 08:48:30 -0500

    cleanup and fix some changed log msgs.

--- src/python/pants/backend/visibility/rule_types_test.py
+++ src/python/pants/backend/visibility/rule_types_test.py
@@ -3,7 +3,6 @@
 
 from __future__ import annotations
 
-import itertools
 import logging
 import re
 from pathlib import PurePath
@@ -586,8 +585,8 @@ def test_dependency_rules(rule_runner: RuleRunner, caplog) -> None:
         expect_logged=[
             (
                 logging.DEBUG,
-                "WARN: dependency on the `target` target src/a/a2:joker from a target at src/a by "
-                "dependent rule '?*' declared in src/a/a2/BUILD",
+                "WARN: type:target name:joker path:'src/a' relpath:'src/a/a2' address:src/a:a "
+                "rule:'?*' src/a/a2/BUILD: ?*",
             ),
         ],
         exclusively=False,
@@ -608,8 +607,8 @@ def test_dependency_rules(rule_runner: RuleRunner, caplog) -> None:
         expect_logged=[
             (
                 logging.DEBUG,
-                "DENY: dependency on the `resources` target src/a:internal from a target at src/b "
-                "by dependent rule '!*' declared in src/a/BUILD",
+                "DENY: type:resources name:internal path:'src/b' relpath:'src/a' "
+                "address:src/b:b rule:'!*' src/a/BUILD: ., !*",
             ),
         ],
         exclusively=False,
@@ -627,10 +626,8 @@ def test_dependency_rules(rule_runner: RuleRunner, caplog) -> None:
         expect_logged=[
             (
                 logging.DEBUG,
-                # This message is different, as it fired from a dependency rule, rather than a
-                # dependent rule as in the other cases.
-                "DENY: the `resources` target src/a:internal dependency to a target at src/b by "
-                "dependency rule '!*' declared in src/a/BUILD",
+                "DENY: type:resources name:internal path:'src/b' relpath:'src/a' address:src/b:b "
+                "rule:'!*' src/a/BUILD: ., !*",
             ),
         ],
         exclusively=False,
@@ -835,79 +832,13 @@ def test_file_specific_rules(rule_runner: RuleRunner) -> None:
     )
 
 
-@pytest.fixture
-def setup_testbed(rule_runner: RuleRunner) -> bool:
-    """Comprehensive tree layout to test various rule selection paths."""
-    files = tuple(
-        itertools.chain.from_iterable(
-            (
-                f"{dir}file1{ext}",
-                f"{dir}file2{ext}",
-                f"{dir}origin/file1{ext}",
-                f"{dir}origin/file2{ext}",
-                f"{dir}dependency/file1{ext}",
-                f"{dir}dependency/file2{ext}",
-            )
-            for cwd, ext in (
-                ("", ".txt"),
-                ("a/", ".txt"),
-                ("a/b/", ".txt"),
-                ("anchor-mode/", ".top"),
-                ("anchor-mode/proj-root/", ".root"),
-                ("anchor-mode/declared/", ".dec"),
-                ("anchor-mode/invoked/", ".inv"),
-                ("anchor-mode/floating/", ".float"),
-                ("anchor-mode/proj-root/a/", ".root"),
-                ("anchor-mode/declared/a/", ".dec"),
-                ("anchor-mode/invoked/a/", ".inv"),
-                ("anchor-mode/floating/a/", ".float"),
-                ("anchor-mode/proj-root/a/b/", ".root"),
-                ("anchor-mode/declared/a/b/", ".dec"),
-                ("anchor-mode/invoked/a/b/", ".inv"),
-                ("anchor-mode/floating/a/b/", ".float"),
-            )
-        )
-    )
+def test_relpath_for_file_targets(rule_runner: RuleRunner) -> None:
+    # Testing purpose:
+    #
+    # When a file is owned by a target declared in a parent directory, make sure the correct BUILD
+    # file is consulted for the rules set to apply, and with the correct relpath for the matching.
     rule_runner.write_files(
         {
-            "BUILD": "files(name='txt', sources=['**/*.txt'])",
-            "a/b/origin/BUILD": "files(name='txt', sources=['**/*.txt-b'])",
-            "a/b/dependency/BUILD": "files(name='txt', sources=['**/*.txt-b'])",
-            "anchor-mode/BUILD": "files(name='top', sources=['**/*.top'])",
-            "anchor-mode/proj-root/a/b/dependency/BUILD": "files(name='txt', sources=['**/*.root-b'])",
-            "anchor-mode/proj-root/a/b/origin/BUILD": "files(name='txt', sources=['**/*.root-b'])",
-            "anchor-mode/declared/a/b/dependency/BUILD": "files(name='txt', sources=['**/*.dec-b'])",
-            "anchor-mode/declared/a/b/origin/BUILD": "files(name='txt', sources=['**/*.dec-b'])",
-            "anchor-mode/invoked/a/b/dependency/BUILD": "files(name='txt', sources=['**/*.inv-b'])",
-            "anchor-mode/invoked/a/b/origin/BUILD": "files(name='txt', sources=['**/*.inv-b'])",
-            "anchor-mode/floating/a/b/dependency/BUILD": "files(name='txt', sources=['**/*.float-b'])",
-            "anchor-mode/floating/a/b/origin/BUILD": "files(name='txt', sources=['**/*.float-b'])",
-            "anchor-mode/proj-root/BUILD": dedent(
-                """
-                files(name="root", sources=["**/*.root"])
-
-                __dependencies_rules__(
-                  ("*", "//dependency/*", "!*"),
-                )
-
-                __dependents_rules__(
-                  ("*", "//origin/*", "!*"),
-                )
-                """
-            ),
-            "anchor-mode/declared/BUILD": dedent(
-                """
-                files(name="dec", sources=["**/*.dec"])
-
-                __dependencies_rules__(
-                  ("*", "/dependency/*", "!*"),
-                )
-
-                __dependents_rules__(
-                  ("*", "/origin/*", "!*"),
-                )
-                """
-            ),
             "anchor-mode/invoked/BUILD": dedent(
                 """
                 files(name="inv", sources=["**/*.inv"])
@@ -921,30 +852,11 @@ def setup_testbed(rule_runner: RuleRunner) -> bool:
                 )
                 """
             ),
-            "anchor-mode/floating/BUILD": dedent(
-                """
-                files(name="float", sources=["**/*.float"])
-
-                __dependencies_rules__(
-                  ("*", "dependency/*", "!*"),
-                )
-
-                __dependents_rules__(
-                  ("*", "origin/*", "!*"),
-                )
-                """
-            ),
-            **{path: "" for path in files},
+            "anchor-mode/invoked/origin/file1.inv:../inv": "",
+            "anchor-mode/invoked/dependency/file2.inv:../inv": "",
         },
     )
-    return True
 
-
-def test_relpath_for_file_targets(rule_runner: RuleRunner, setup_testbed: bool) -> None:
-    # Testing purpose:
-    #
-    # When a file is owned by a target declared in a parent directory, make sure the correct BUILD
-    # file is consulted for the rules set to apply, and with the correct relpath for the matching.
     assert_dependency_rules(
         rule_runner,
         "anchor-mode/invoked/origin/file1.inv:../inv",
