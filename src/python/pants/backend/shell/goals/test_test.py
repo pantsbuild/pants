8af71721935decb84482ec77e20ff17a09a93c9f
Tom Dyas <tom.dyas@gmail.com>
2022-11-26 09:49:12 -0500

    explicit exit code check

--- src/python/pants/backend/shell/goals/test_test.py
+++ src/python/pants/backend/shell/goals/test_test.py
@@ -101,5 +101,5 @@ def test_shell_command_as_test(rule_runner: RuleRunner) -> None:
 
     fail_target = rule_runner.get_target(Address("", target_name="fail"))
     fail_result = run_test(fail_target)
-    assert fail_result.exit_code != 0
+    assert fail_result.exit_code == 1
     assert fail_result.stdout == "does not contain 'xyzzy'\n"
