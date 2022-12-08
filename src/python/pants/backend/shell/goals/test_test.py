4d832920123e50bb2ef1a8b63ca896ad6ebd91dc
Christopher Neugebauer <chrisjrn@toolchain.com>
2022-12-08 11:03:54 -0800

    Revert "touch python files too"
    This reverts commit eac9611bccae463a6cf80d54ff7de4e5797083c5.

--- src/python/pants/backend/shell/goals/test_test.py
+++ src/python/pants/backend/shell/goals/test_test.py
@@ -103,4 +103,3 @@ def test_shell_command_as_test(rule_runner: RuleRunner) -> None:
     fail_result = run_test(fail_target)
     assert fail_result.exit_code == 1
     assert fail_result.stdout == "does not contain 'xyzzy'\n"
-
