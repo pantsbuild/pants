afbafa97922f299b9d6db388266d8c966455a9cc
Tom Dyas <tom.dyas@gmail.com>
2022-12-15 23:50:52 +0100

    go: detect when `go.sum` needs additional entries filled in (#17811)
    As described in https://github.com/pantsbuild/pants/issues/17794, the Go backend will error for inconsistent `go.sum` entries when a module's checksum is different than what is in `go.sum`, but was tolerating _missing_ `go.sum` entries which means that the "source of truth" in `go.sum` was not being updated as it would be if the user were using `go` directly.
    
    Fix this issue by detecting when `go mod download` updates `go.sum` and require the user to update `go.sum` accordingly.
    
    Note: At some point in the future, Pants should manage `go.sum` entries more directly, but that functionality is not needed at the moment to detect and fix this issue.

--- src/python/pants/backend/go/util_rules/third_party_pkg_test.py
+++ src/python/pants/backend/go/util_rules/third_party_pkg_test.py
@@ -4,6 +4,7 @@
 from __future__ import annotations
 
 import os.path
+import re
 from textwrap import dedent
 
 import pytest
@@ -28,7 +29,9 @@ from pants.backend.go.util_rules.third_party_pkg import (
     ThirdPartyPkgAnalysis,
     ThirdPartyPkgAnalysisRequest,
 )
+from pants.build_graph.address import Address
 from pants.engine.fs import Digest, Snapshot
+from pants.engine.internals.scheduler import ExecutionError
 from pants.engine.process import ProcessExecutionFailure
 from pants.engine.rules import QueryRule
 from pants.testutil.rule_runner import RuleRunner, engine_error
@@ -93,7 +96,14 @@ def test_download_and_analyze_all_packages(rule_runner: RuleRunner) -> None:
     input_digest = rule_runner.make_snapshot({"go.mod": GO_MOD, "go.sum": GO_SUM}).digest
     all_packages = rule_runner.request(
         AllThirdPartyPackages,
-        [AllThirdPartyPackagesRequest(input_digest, "go.mod", build_opts=GoBuildOptions())],
+        [
+            AllThirdPartyPackagesRequest(
+                Address("fake_addr_for_test", target_name="mod"),
+                input_digest,
+                "go.mod",
+                build_opts=GoBuildOptions(),
+            )
+        ],
     )
     assert set(all_packages.import_paths_to_pkg_info.keys()) == {
         "golang.org/x/text/encoding/japanese",
@@ -266,7 +276,14 @@ def test_invalid_go_sum(rule_runner: RuleRunner) -> None:
     with engine_error(ProcessExecutionFailure, contains="SECURITY ERROR"):
         rule_runner.request(
             AllThirdPartyPackages,
-            [AllThirdPartyPackagesRequest(digest, "go.mod", build_opts=GoBuildOptions())],
+            [
+                AllThirdPartyPackagesRequest(
+                    Address("fake_addr_for_test", target_name="mod"),
+                    digest,
+                    "go.mod",
+                    build_opts=GoBuildOptions(),
+                )
+            ],
         )
 
 
@@ -293,7 +310,14 @@ def test_missing_go_sum(rule_runner: RuleRunner) -> None:
     with engine_error(contains="github.com/google/uuid@v1.3.0: missing go.sum entry"):
         rule_runner.request(
             AllThirdPartyPackages,
-            [AllThirdPartyPackagesRequest(digest, "go.mod", build_opts=GoBuildOptions())],
+            [
+                AllThirdPartyPackagesRequest(
+                    Address("fake_addr_for_test", target_name="mod"),
+                    digest,
+                    "go.mod",
+                    build_opts=GoBuildOptions(),
+                )
+            ],
         )
 
 
@@ -323,7 +347,14 @@ def test_stale_go_mod(rule_runner: RuleRunner) -> None:
     with engine_error(ProcessExecutionFailure, contains="updates to go.mod needed"):
         rule_runner.request(
             AllThirdPartyPackages,
-            [AllThirdPartyPackagesRequest(digest, "go.mod", build_opts=GoBuildOptions())],
+            [
+                AllThirdPartyPackagesRequest(
+                    Address("fake_addr_for_test", target_name="mod"),
+                    digest,
+                    "go.mod",
+                    build_opts=GoBuildOptions(),
+                )
+            ],
         )
 
 
@@ -336,7 +367,11 @@ def test_pkg_missing(rule_runner: RuleRunner) -> None:
             ThirdPartyPkgAnalysis,
             [
                 ThirdPartyPkgAnalysisRequest(
-                    "another_project.org/foo", digest, "go.mod", build_opts=GoBuildOptions()
+                    "another_project.org/foo",
+                    Address("fake_addr_for_test", target_name="mod"),
+                    digest,
+                    "go.mod",
+                    build_opts=GoBuildOptions(),
                 )
             ],
         )
@@ -361,7 +396,14 @@ def test_module_with_no_packages(rule_runner) -> None:
     )
     all_packages = rule_runner.request(
         AllThirdPartyPackages,
-        [AllThirdPartyPackagesRequest(digest, "go.mod", build_opts=GoBuildOptions())],
+        [
+            AllThirdPartyPackagesRequest(
+                Address("fake_addr_for_test", target_name="mod"),
+                digest,
+                "go.mod",
+                build_opts=GoBuildOptions(),
+            )
+        ],
     )
     assert not all_packages.import_paths_to_pkg_info
 
@@ -484,7 +526,11 @@ def test_determine_pkg_info_module_with_replace_directive(rule_runner: RuleRunne
         ThirdPartyPkgAnalysis,
         [
             ThirdPartyPkgAnalysisRequest(
-                "github.com/hashicorp/consul/api", digest, "go.mod", build_opts=GoBuildOptions()
+                "github.com/hashicorp/consul/api",
+                Address("fake_addr_for_test", target_name="mod"),
+                digest,
+                "go.mod",
+                build_opts=GoBuildOptions(),
             )
         ],
     )
@@ -516,7 +562,11 @@ def test_ambiguous_package(rule_runner: RuleRunner) -> None:
         ThirdPartyPkgAnalysis,
         [
             ThirdPartyPkgAnalysisRequest(
-                "github.com/ugorji/go/codec", digest, "go.mod", build_opts=GoBuildOptions()
+                "github.com/ugorji/go/codec",
+                Address("fake_addr_for_test", target_name="mod"),
+                digest,
+                "go.mod",
+                build_opts=GoBuildOptions(),
             )
         ],
     )
@@ -526,3 +576,34 @@ def test_ambiguous_package(rule_runner: RuleRunner) -> None:
         == "gopath/pkg/mod/github.com/ugorji/go/codec@v0.0.0-20181204163529-d75b2dcb6bc8"
     )
     assert "encode.go" in pkg_info.go_files
+
+
+def test_go_sum_with_missing_entries_triggers_error(rule_runner: RuleRunner) -> None:
+    digest = set_up_go_mod(
+        rule_runner,
+        dedent(
+            """\
+            module example.com/third-party-module
+            go 1.16
+            require github.com/google/uuid v1.3.0
+            """
+        ),
+        "",
+    )
+    msg = (
+        "For `go_mod` target `fake_addr_for_test:mod`, the go.sum file is incomplete because "
+        "it was updated while processing third-party dependency `github.com/google/uuid`."
+    )
+    with pytest.raises(ExecutionError, match=re.escape(msg)):
+        _ = rule_runner.request(
+            ThirdPartyPkgAnalysis,
+            [
+                ThirdPartyPkgAnalysisRequest(
+                    "github.com/ugorji/go/codec",
+                    Address("fake_addr_for_test", target_name="mod"),
+                    digest,
+                    "go.mod",
+                    build_opts=GoBuildOptions(),
+                )
+            ],
+        )
