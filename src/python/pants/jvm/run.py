fcd41ca9a8e1c2d1bc89daaf20441b438768b057
Christopher Neugebauer <chrisjrn@toolchain.com>
2022-12-19 09:47:09 -0800

    Factors out `_find_main`

--- src/python/pants/jvm/run.py
+++ src/python/pants/jvm/run.py
@@ -1,17 +1,20 @@
 # Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import annotations
+
 import logging
-from typing import Iterable
+import re
+from typing import Iterable, Optional, Tuple
 
 from pants.core.goals.package import BuiltPackage
 from pants.core.goals.run import RunRequest
 from pants.core.util_rules.system_binaries import UnzipBinary
 from pants.core.util_rules.system_binaries import rules as system_binaries_rules
 from pants.engine.addresses import Addresses
-from pants.engine.internals.native_engine import Digest, MergeDigests
+from pants.engine.internals.native_engine import Digest, MergeDigests, Snapshot
 from pants.engine.process import Process, ProcessResult
-from pants.engine.rules import Get, collect_rules, rule
+from pants.engine.rules import Get, collect_rules, rule, rule_helper
 from pants.engine.target import CoarsenedTargets
 from pants.jvm.classpath import Classpath
 from pants.jvm.classpath import rules as classpath_rules
@@ -55,6 +58,104 @@ async def create_deploy_jar_run_request(
     return _post_process_jvm_process(proc, jdk)
 
 
+@rule_helper
+async def _find_main(
+    unzip: UnzipBinary, jdk: JdkEnvironment, input_digest: Digest, jarfile: str
+) -> str:
+    # Find the `main` method, first by inspecting the manifest, and then by inspecting the index
+    # of the included classes.
+
+    main_from_manifest = await _find_main_by_manifest(unzip, input_digest, jarfile)
+
+    if main_from_manifest:
+        return main_from_manifest
+
+    mains_from_javap = await _find_main_by_javap(unzip, jdk, input_digest, jarfile)
+
+    if len(mains_from_javap) == 0:
+        raise Exception(
+            f"Could not find a `public static void main(String[])` method in `{jarfile}`"
+        )
+    if len(mains_from_javap) > 1:
+        raise Exception(f"Found multiple classes that provide `main` methods in `{jarfile}`.")
+
+    return mains_from_javap[0]
+
+
+@rule_helper
+async def _find_main_by_manifest(
+    unzip: UnzipBinary, input_digest: Digest, jarfile: str
+) -> Optional[str]:
+    # jvm only allows `-cp` or `-jar` to be specified, and `-jar` takes precedence. So, even for a
+    # JAR with a valid `Main-Class` in the manifest, we need to peek inside the manifest and
+    # extract `main` ourself.
+    manifest = await Get(
+        ProcessResult,
+        Process(
+            description="Get manifest destails from classpath",
+            argv=(unzip.path, "-p", jarfile, "META-INF/MANIFEST.MF"),
+            input_digest=input_digest,
+        ),
+    )
+
+    main_class_lines = [
+        r.strip()
+        for _, is_main, r in (
+            i.partition("Main-Class:") for i in manifest.stdout.decode().splitlines()
+        )
+        if is_main
+    ]
+
+    return main_class_lines[0] if main_class_lines else None
+
+
+@rule_helper
+async def _find_main_by_javap(
+    unzip: UnzipBinary, jdk: JdkEnvironment, input_digest: Digest, jarfile: str
+) -> Tuple[str, ...]:
+    # Finds the `main` class by inspecting all of the classes inside the specified JAR
+    # to find one with a JVM main method.
+
+    first_jar_contents = await Get(
+        ProcessResult,
+        Process(
+            description=f"Get class files from `{jarfile}` to find `main`",
+            argv=(unzip.path, jarfile, "*.class", "-d", "zip_output"),
+            input_digest=input_digest,
+            output_directories=("zip_output",),
+        ),
+    )
+
+    outputs = await Get(Snapshot, Digest, first_jar_contents.output_digest)
+
+    class_index = await Get(
+        ProcessResult,
+        JvmProcess(
+            jdk=jdk,
+            classpath_entries=[f"{jdk.java_home}/lib/tools.jar"],
+            argv=[
+                "com.sun.tools.javap.Main",
+                *("-cp", jarfile),
+                *outputs.files,
+            ],
+            input_digest=first_jar_contents.output_digest,
+            description=f"Index class files in `{jarfile}` to find `main`",
+            level=LogLevel.DEBUG,
+        ),
+    )
+
+    output = class_index.stdout.decode()
+    p = re.compile(r"^public class (.*?) .*?{(.*?)}$", flags=re.MULTILINE | re.DOTALL)
+    classes: list[tuple[str, str]] = re.findall(p, output)
+    mains = tuple(
+        classname
+        for classname, definition in classes
+        if "public static void main(java.lang.String[])" in definition
+    )
+
+    return mains
+
+
 @rule(level=LogLevel.DEBUG)
 async def create_jvm_artifact_run_request(
     field_set: JvmArtifactFieldSet,
@@ -70,29 +171,8 @@ async def create_jvm_artifact_run_request(
 
     input_digest = await Get(Digest, MergeDigests(classpath.digests()))
 
-    # Assume that the first entry is the artifact specified in `Addresses`?
-
-    # jvm only allows `-cp` or `-jar` to be specified, and `-jar` takes precedence. So, we need
-    # peek inside the JAR for the thing we want to run, and extract its `Main-Class` line from the
-    # manifest.
-    manifest = await Get(
-        ProcessResult,
-        Process(
-            description="Get manifest destails from classpath",
-            argv=(unzip.path, "-p", classpath_entries[0], "META-INF/MANIFEST.MF"),
-            input_digest=input_digest,
-        ),
-    )
-
-    main_class_line = [
-        r.strip()
-        for _, is_main, r in (
-            i.partition("Main-Class:") for i in manifest.stdout.decode().splitlines()
-        )
-        if is_main
-    ]
-    assert main_class_line
-    main_class = main_class_line[0]
+    # Assume that the first entry in `classpath_entries` is the artifact specified in `Addresses`?
+    main_class = await _find_main(unzip, jdk, input_digest, classpath_entries[0])
 
     proc = await Get(
         Process,
