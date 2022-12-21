ae62e725f6629bf0af13ffee9c37971b459280ea
Christopher Neugebauer <chrisjrn@toolchain.com>
2022-12-21 11:19:51 -0800

    placation

--- src/python/pants/backend/python/goals/run_python_requirement.py
+++ src/python/pants/backend/python/goals/run_python_requirement.py
@@ -1,5 +1,8 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
 import logging
 import os
 from collections import defaultdict
