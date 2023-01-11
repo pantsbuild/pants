b8f714f2f5ca479aad906582cff7efe19fde927c
Benjy Weinberger <benjyw@gmail.com>
2023-01-11 13:41:49 -0500

    Build Linux wheels in custom images.
    These are the standard manylinux images, with the aws cli installed.
    
    We need the awscli to publish the wheels to S3, and our on-the-fly
    installation doesn't work for aarch64 because we don't run as
    root in the image.

--- build-support/bin/generate_github_workflows.py
+++ build-support/bin/generate_github_workflows.py
@@ -701,12 +701,14 @@ def build_wheels_job(platform: Platform, python_versions: list[str]) -> Jobs:
     # the code, install rustup and expose Pythons.
     # TODO: Apply rust caching here.
     if platform == Platform.LINUX_X86_64:
-        container = {"image": "quay.io/pypa/manylinux2014_x86_64:latest"}
+        container = {
+            "image": "registry.hub.docker.com/pantsbuild/wheel_build_x86_64:v1-568cfc69e"
+        }
     elif platform == Platform.LINUX_ARM64:
         # Unfortunately Equinix do not support the CentOS 7 image on the hardware we've been
         # generously given by the Runs on ARM program. Se we have to build in this image.
         container = {
-            "image": "quay.io/pypa/manylinux2014_aarch64:latest",
+            "image": "registry.hub.docker.com/pantsbuild/wheel_build_aarch64:v1-568cfc69e",
             # The uid/gid for the gha user and group we set up on the self-hosted runner.
             # Necessary to avoid https://github.com/actions/runner/issues/434.
             # Alternatively we could run absolutely everything in a container,
