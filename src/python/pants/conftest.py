e4b6408beae41002c12288edc8679379bfeeb8cf
Joshua <joshdcannon@gmail.com>
2023-01-25 11:42:12 -0600

    there we go :smile:

--- src/python/pants/conftest.py
+++ src/python/pants/conftest.py
@@ -57,10 +57,16 @@ def _check_frozen_dataclass_attributes() -> None:
 
                 def new__init__(self, *args, **kwargs):
                     old__init__(self, *args, **kwargs)
-                    assert sorted(field.name for field in dataclasses.fields(self)) == sorted(
-                        attrname for attrname in dir(self) if not attrname.startswith("__")
-                    )
-                    setattr(dataclass_cls, "__init__", new__init__)
+                    expected = sorted(field.name for field in dataclasses.fields(self))
+                    if hasattr(self, "__dict__"):
+                        actual = sorted(self.__dict__)
+                        assert expected == actual
+                    else:
+                        for attrname in self.__slots__:
+                            # Only way to validate it was initialized is to trigger the descriptor
+                            getattr(self, attrname)
+
+                setattr(dataclass_cls, "__init__", new__init__)
 
             return dataclass_cls
 
