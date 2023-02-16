4399477ac212def5aa4721dc6daddfc06aea223c
Daniel Goldman <danielgoldman4@gmail.com>
2023-02-15 22:18:22 -0500

    fix: actually use LockfileRules in test

--- src/python/pants/backend/python/util_rules/lockfile_test.py
+++ src/python/pants/backend/python/util_rules/lockfile_test.py
@@ -9,6 +9,7 @@ from pants.backend.python.dependency_inference.rules import import_rules
 from pants.backend.python.goals import lockfile
 from pants.backend.python.subsystems.python_tool_base import PythonToolBase
 from pants.backend.python.target_types import ConsoleScript
+from pants.backend.python.util_rules.lockfile import LockfileRules
 from pants.core.goals import generate_lockfiles
 from pants.core.goals.generate_lockfiles import GenerateLockfilesGoal, GenerateToolLockfileSentinel
 from pants.engine.target import Dependencies, SingleSourceField, Target
@@ -46,6 +47,7 @@ class FakeTool(PythonToolBase):
     default_lockfile_path = "cowsay.lock"
     default_lockfile_resource = ("", "")
     default_lockfile_url = " "
+    lockfile_rules_type = LockfileRules.SIMPLE
 
 
 class MockSourceField(SingleSourceField):
