d3e34654e778f67a2def89119f51121f1db6f915
Joshua <joshdcannon@gmail.com>
2023-02-16 10:32:41 -0600

    conftest

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import glob
+import os
+import shutil
 
 import pytest
 
@@ -32,3 +35,19 @@ def pytest_addoption(parser):
     parser.addoption(
         "--noskip", action="store_true", default=False, help="Treat skipped tests as errors"
     )
+
+
+def pytest_runtestloop():
+    # These files are so big they exceed our "immutable symlink" hueristic. However, our integration
+    # tests run Pants in the sandbox, and Pants doesn't allow globbing absolute symlinks.
+    # Therefore we re-materialize them from their symlink.
+    for fileglob in [
+        "*.pex/.bootstrap",
+        "pantsbuild.pants*.whl",
+        "src/python/pants/engine/internals/native_engine.so",
+    ]:
+        for filepath in glob.iglob(fileglob):
+            if os.path.islink(filepath):
+                actual = os.path.realpath(filepath)
+                os.unlink(filepath)
+                shutil.copyfile(actual, filepath)
