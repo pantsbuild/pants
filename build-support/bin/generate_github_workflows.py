d15e878301c161b0c4f6876693bd8f886590155c
Tom Dyas <tom.dyas@gmail.com>
2023-02-17 19:53:45 +0100

    go 1.20

--- build-support/bin/generate_github_workflows.py
+++ build-support/bin/generate_github_workflows.py
@@ -238,7 +238,7 @@ def install_go() -> Step:
     return {
         "name": "Install Go",
         "uses": "actions/setup-go@v3",
-        "with": {"go-version": "1.19.5"},
+        "with": {"go-version": "1.20.1"},
     }
 
 
