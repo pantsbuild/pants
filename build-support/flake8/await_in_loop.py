5644db807d0b5412c035620cfc8c493e73f81c73
Huon Wilson <huon@exoflare.io>
2023-02-19 14:56:26 +1100

    Implement flake8 rule disallowing `await` inside loops

---
+++ build-support/flake8/await_in_loop.py
@@ -0,0 +1,123 @@
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+"""Disallow 'await' in a loop."""
+from __future__ import annotations
+
+import ast
+from contextlib import contextmanager
+from pathlib import PurePath
+from typing import Iterator
+
+
+def check_for_await_in_loop(tree: ast.AST, filename: str) -> Iterator[tuple[int, int, str, None]]:
+    path = PurePath(filename)
+    if (
+        not filename.startswith("src/python")
+        or path.stem.startswith("test_")
+        or path.stem.endswith("_test")
+    ):
+        return
+
+    violations: list[tuple[int, int, str, None]] = []
+
+    class Visitor(ast.NodeVisitor):
+        def __init__(self):
+            # this isn't entirely correct: function/class definitions within a loop might have
+            # `await`s in them, but aren't _necessarily_ a problem (see example below).
+            #
+            # tasks = []
+            # for i in range(10):
+            #     async def foo(i=i):
+            #         await bar(i)
+            #     tasks.append(foo())
+            # asyncio.gather(tasks)
+            self._in_loop = False
+
+        @contextmanager
+        def in_loop(self) -> Iterator[None]:
+            old = self._in_loop
+            self._in_loop = True
+            try:
+                yield
+            finally:
+                self._in_loop = old
+
+        def traverse(self, node: ast.Ast | list[ast.Ast]):
+            if isinstance(node, list):
+                for x in node:
+                    self.visit(x)
+            else:
+                self.visit(x)
+
+        def visit_for(self, node: ast.For | ast.AsyncFor):
+            """Example::
+
+            [async] for MULTIPLE in await ONCE:
+                await MULTIPLE
+            else:
+                await ONCE
+            """
+            self.visit(node.iter)
+            self.traverse(node.orelse)
+
+            with self.in_loop():
+                self.visit(node.target)
+                self.traverse(node.body)
+
+        visit_For = visit_AsyncFor = visit_for
+
+        def visit_While(self, node: ast.While):
+            """Example:
+
+            while await MULTIPLE:
+                await MULTIPLE
+            """
+            with self.in_loop():
+                self.generic_visit(node)
+
+        def visit_comp(self, node: ast.DictComp | ast.ListComp | ast.SetComp | ast.GeneratorExp):
+            """Example::
+
+            [
+                await MULTIPLE
+                [async] for MULTIPLE in await ONCE
+                if MULTIPLE
+                for MULTIPLE in await MULTIPLE
+            ]
+            """
+            first_comp = node.generators[0]
+            self.visit(first_comp.iter)
+
+            with self.in_loop():
+                self.visit(first_comp.target)
+                for expr in first_comp.ifs:
+                    self.visit(expr)
+
+                for other_comp in node.generators[1:]:
+                    self.visit(other_comp)
+
+                if isinstance(node, ast.DictComp):
+                    self.visit(node.key)
+                    self.visit(node.value)
+                else:
+                    self.visit(node.elt)
+
+        visit_ListComp = visit_GeneratorExp = visit_SetComp = visit_DictComp = visit_comp
+
+        def visit_Await(self, node: ast.Await):
+            if self._in_loop:
+                violations.append(
+                    (
+                        node.lineno,
+                        node.col_offset,
+                        "PNT30 `await` in a loop may be a performance hazard: prefer concurrent requests via MultiGet, or add `# noqa: PNT30 - <explanation>` if this is required",
+                        None,
+                    )
+                )
+
+    Visitor().visit(tree)
+    yield from violations
+
+
+setattr(check_for_await_in_loop, "name", __name__)
+setattr(check_for_await_in_loop, "version", "0.0.0")
