6bcd56853a823c3d51a735e6c0977076076df68a
Tom Solberg <me@sbg.dev>
2023-02-21 23:38:46 +0100

    Enable running third-party Go `main` packages (#17449)
    Minimal fix for #17427.
    
    This allows `go_third_party_package` to be treated as a potential main, analyzed and executed. I tried to stick to the shape of the code as much as possible so nothing really interesting to say about it. Simply a few branches based on whether the target has a `GoImportPath` instead of a `GoPackageSourcesField` field.
    
    I'm not happy about the test-case using my repo, but didn't find a way of "mocking" a third-party package. If a better method exists, please let me know. Otherwise I can keep the repo around for the foreseeable future, or we can transfer it to the pants org as a test util.

--- src/python/pants/backend/go/goals/package_binary.py
+++ src/python/pants/backend/go/goals/package_binary.py
@@ -6,7 +6,12 @@ from __future__ import annotations
 from dataclasses import dataclass
 from pathlib import PurePath
 
-from pants.backend.go.target_types import GoBinaryMainPackageField, GoBinaryTarget, GoPackageTarget
+from pants.backend.go.target_types import (
+    GoBinaryMainPackageField,
+    GoBinaryTarget,
+    GoPackageTarget,
+    GoThirdPartyPackageTarget,
+)
 from pants.backend.go.util_rules.binary import GoBinaryMainPackage, GoBinaryMainPackageRequest
 from pants.backend.go.util_rules.build_opts import GoBuildOptions, GoBuildOptionsFromTargetRequest
 from pants.backend.go.util_rules.build_pkg import BuiltGoPackage
@@ -15,7 +20,12 @@ from pants.backend.go.util_rules.first_party_pkg import (
     FallibleFirstPartyPkgAnalysis,
     FirstPartyPkgAnalysisRequest,
 )
+from pants.backend.go.util_rules.go_mod import GoModInfo, GoModInfoRequest
 from pants.backend.go.util_rules.link import LinkedGoBinary, LinkGoBinaryRequest
+from pants.backend.go.util_rules.third_party_pkg import (
+    ThirdPartyPkgAnalysis,
+    ThirdPartyPkgAnalysisRequest,
+)
 from pants.core.goals.package import (
     BuiltPackage,
     BuiltPackageArtifact,
@@ -48,26 +58,50 @@ async def package_go_binary(field_set: GoBinaryFieldSet) -> BuiltPackage:
         Get(GoBinaryMainPackage, GoBinaryMainPackageRequest(field_set.main)),
         Get(GoBuildOptions, GoBuildOptionsFromTargetRequest(field_set.address)),
     )
-    main_pkg_analysis = await Get(
-        FallibleFirstPartyPkgAnalysis,
-        FirstPartyPkgAnalysisRequest(main_pkg.address, build_opts=build_opts),
-    )
-    analysis = main_pkg_analysis.analysis
-    if not analysis:
-        raise ValueError(
-            f"Unable to analyze main package `{main_pkg.address}` for go_binary target {field_set.address}: {main_pkg_analysis.stderr}"
+
+    if main_pkg.is_third_party:
+        assert isinstance(main_pkg.import_path, str)
+
+        go_mod_address = main_pkg.address.maybe_convert_to_target_generator()
+        go_mod_info = await Get(GoModInfo, GoModInfoRequest(go_mod_address))
+
+        analysis = await Get(
+            ThirdPartyPkgAnalysis,
+            ThirdPartyPkgAnalysisRequest(
+                main_pkg.import_path,
+                go_mod_address,
+                go_mod_info.digest,
+                go_mod_info.mod_path,
+                build_opts=build_opts,
+            ),
         )
-    if analysis.name != "main":
+
+        package_name = analysis.name
+    else:
+        main_pkg_analysis = await Get(
+            FallibleFirstPartyPkgAnalysis,
+            FirstPartyPkgAnalysisRequest(main_pkg.address, build_opts=build_opts),
+        )
+        if not main_pkg_analysis.analysis:
+            raise ValueError(
+                f"Unable to analyze main package `{main_pkg.address}` for go_binary target {field_set.address}: {main_pkg_analysis.stderr}"
+            )
+
+        package_name = main_pkg_analysis.analysis.name
+
+    if package_name != "main":
         raise ValueError(
-            f"{GoPackageTarget.alias} target `{main_pkg.address}` is used as the main package for "
-            f"{GoBinaryTarget.address} target `{field_set.address}` but uses package name `{analysis.name}` "
-            "instead of `main`. Go requires that main packages actually use `main` as the package name."
+            f"{GoThirdPartyPackageTarget.alias if main_pkg.is_third_party else GoPackageTarget.alias} "
+            f"target `{main_pkg.address}` is used as the main package for {GoBinaryTarget.alias} target "
+            f"`{field_set.address}` but uses package name `{analysis.name}` instead of `main`. Go "
+            "requires that main packages actually use `main` as the package name."
         )
 
     built_package = await Get(
         BuiltGoPackage,
         BuildGoPackageTargetRequest(main_pkg.address, is_main=True, build_opts=build_opts),
     )
+
     main_pkg_a_file_path = built_package.import_paths_to_pkg_a_files["main"]
 
     output_filename = PurePath(field_set.output_path.value_or_default(file_ending=None))
