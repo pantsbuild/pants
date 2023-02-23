f2f9d8b5019ccb1c5555c31b25abfaa53ed2ec46
Tobias Nilsson <tobias.nilsson@amramedical.com>
2023-02-23 21:28:06 +0100

    chore: Update preambles/config.yaml with .js patterns

--- src/python/pants/backend/javascript/import_parser/script.cjs
+++ src/python/pants/backend/javascript/import_parser/script.cjs
@@ -1,3 +1,5 @@
+// Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
 const parser = require("@babel/parser");
 const traverse = require("@babel/traverse").default;
 const fs = require("fs").promises;
