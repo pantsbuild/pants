0c46448cf025621e7339e07d4a854294294f9f9e
Daniel Goldman <danielgoldman4@gmail.com>
2023-02-25 15:32:07 -0500

    remove outdated doc reference

--- src/python/pants/backend/python/subsystems/python_tool_base.py
+++ src/python/pants/backend/python/subsystems/python_tool_base.py
@@ -52,7 +52,7 @@ class PythonToolRequirementsBase(Subsystem):
     # If this tool does not mix with user requirements you should set this to True.
     #
     # You also need to subclass `GeneratePythonToolLockfileSentinel` and create a rule that goes
-    # from it -> GeneratePythonLockfile by calling `GeneratePythonLockfile.from_python_tool()`.
+    # from it -> GeneratePythonLockfile by calling `to_lockfile_request`.
     # Register the UnionRule.
     register_lockfile: ClassVar[bool] = False
     default_lockfile_resource: ClassVar[tuple[str, str] | None] = None
