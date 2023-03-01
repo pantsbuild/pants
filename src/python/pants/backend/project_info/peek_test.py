21cc565589c3e55adcd73bea69538ece443bef6f
Huon Wilson <huon@exoflare.io>
2023-03-02 09:14:51 +1100

    from __future__ import annotations  for older pythons

--- src/python/pants/backend/project_info/peek_test.py
+++ src/python/pants/backend/project_info/peek_test.py
@@ -1,6 +1,8 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import annotations
+
 import dataclasses
 from textwrap import dedent
 
