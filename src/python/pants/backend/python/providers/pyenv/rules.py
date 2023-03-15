3f626713c1d996aa6f2239da4c61cd82fd94afdd
Joshua Cannon <joshdcannon@gmail.com>
2023-03-15 13:07:05 -0500

    Move the `pants-pyenv-install` target to a sub-plugin (#18499)
    This was leftover from the prior PR, but split the magic into an opt-in
    plugin. I also added tests for it, yay.
    
    Expected usage (which will be documented) is:
    `ENVVAR1=blah pants --concurrent
    --backend-packages=pants.backend.python.providers.experimental.pyenv.custom_install
    run :pants-pyenv-install -- 3.9`

--- src/python/pants/backend/python/providers/pyenv/rules.py
+++ src/python/pants/backend/python/providers/pyenv/rules.py
@@ -1,16 +1,13 @@
 # Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import dataclasses
-from dataclasses import dataclass
 from textwrap import dedent  # noqa: PNT20
 
-from pants.backend.python.providers.pyenv.target_types import PyenvInstallSentinelField
 from pants.backend.python.subsystems.setup import PythonSetup
 from pants.backend.python.util_rules.pex import PythonProvider
 from pants.backend.python.util_rules.pex import rules as pex_rules
 from pants.backend.python.util_rules.pex_environment import PythonExecutable
-from pants.core.goals.run import RunFieldSet, RunInSandboxBehavior, RunRequest
+from pants.core.goals.run import RunRequest
 from pants.core.util_rules.external_tool import (
     DownloadedExternalTool,
     ExternalToolRequest,
@@ -22,8 +19,6 @@ from pants.engine.env_vars import EnvironmentVars, EnvironmentVarsRequest
 from pants.engine.fs import CreateDigest, FileContent
 from pants.engine.internals.native_engine import Digest, MergeDigests
 from pants.engine.internals.selectors import Get, MultiGet
-from pants.engine.internals.synthetic_targets import SyntheticAddressMaps, SyntheticTargetsRequest
-from pants.engine.internals.target_adaptor import TargetAdaptor
 from pants.engine.platform import Platform
 from pants.engine.process import Process, ProcessCacheScope, ProcessResult
 from pants.engine.rules import collect_rules, rule
@@ -262,77 +257,10 @@ async def get_python(
     )
 
 
-@dataclass(frozen=True)
-class SyntheticPyenvTargetsRequest(SyntheticTargetsRequest):
-    path: str = SyntheticTargetsRequest.SINGLE_REQUEST_FOR_ALL_TARGETS
-
-
-@rule
-async def make_synthetic_targets(request: SyntheticPyenvTargetsRequest) -> SyntheticAddressMaps:
-    return SyntheticAddressMaps.for_targets_request(
-        request, [("BUILD.pyenv", (TargetAdaptor("_pyenv_install", "pants-pyenv-install"),))]
-    )
-
-
-@dataclass(frozen=True)
-class RunPyenvInstallFieldSet(RunFieldSet):
-    run_in_sandbox_behavior = RunInSandboxBehavior.NOT_SUPPORTED
-    required_fields = (PyenvInstallSentinelField,)
-
-    _sentinel: PyenvInstallSentinelField
-
-
-@rule
-async def run_pyenv_install(
-    _: RunPyenvInstallFieldSet,
-    platform: Platform,
-    pyenv_subsystem: PyenvPythonProviderSubsystem,
-) -> RunRequest:
-    run_request, pyenv = await MultiGet(
-        Get(RunRequest, PyenvInstallInfoRequest()),
-        Get(DownloadedExternalTool, ExternalToolRequest, pyenv_subsystem.get_request(platform)),
-    )
-
-    wrapper_script_digest = await Get(
-        Digest,
-        CreateDigest(
-            [
-                FileContent(
-                    "run_install_python_shim.sh",
-                    dedent(
-                        f"""\
-                        #!/usr/bin/env bash
-                        set -e
-                        cd "$CHROOT"
-                        SPECIFIC_VERSION=$("{pyenv.exe}" latest --known $1)
-                        {" ".join(run_request.args)} $SPECIFIC_VERSION
-                        """
-                    ).encode(),
-                    is_executable=True,
-                )
-            ]
-        ),
-    )
-    digest = await Get(Digest, MergeDigests([run_request.digest, wrapper_script_digest]))
-    return dataclasses.replace(
-        run_request,
-        args=("{chroot}/run_install_python_shim.sh",),
-        digest=digest,
-        extra_env=FrozenDict(
-            {
-                "CHROOT": "{chroot}",
-                **run_request.extra_env,
-            }
-        ),
-    )
-
-
 def rules():
     return (
         *collect_rules(),
         *pex_rules(),
         *external_tools_rules(),
-        *RunPyenvInstallFieldSet.rules(),
         UnionRule(PythonProvider, PyenvPythonProvider),
-        UnionRule(SyntheticTargetsRequest, SyntheticPyenvTargetsRequest),
     )
