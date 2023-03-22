f363d85a1e2de1ba76326ff54dba4e344735b0aa
Tobias Nilsson <tobias.nilsson@amramedical.com>
2023-03-22 21:32:45 +0100

    chore: Add annotations import in more places

--- src/python/pants/backend/javascript/goals/tailor_test.py
+++ src/python/pants/backend/javascript/goals/tailor_test.py
@@ -1,5 +1,6 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+from __future__ import annotations
 
 import pytest
 
