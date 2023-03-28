00f37af5a863e120a3249a3c4af937ed48c7e9ce
Benjy Weinberger <benjyw@gmail.com>
2023-03-28 10:19:25 -0700

    Fix `./pants` -> `pants` in some docs and help strings. (cherrypick of #18596) (#18607)

--- src/python/pants/engine/internals/specs_rules_test.py
+++ src/python/pants/engine/internals/specs_rules_test.py
@@ -960,7 +960,7 @@ def test_no_applicable_targets_exception() -> None:
         goal_description="the `foo` goal",
     )
     remedy = (
-        "Please specify relevant file and/or target arguments. Run `./pants "
+        "Please specify relevant file and/or target arguments. Run `pants "
         "--filter-target-type=tgt1 list ::` to find all applicable targets in your project."
     )
     assert (
@@ -989,9 +989,9 @@ def test_no_applicable_targets_exception() -> None:
         goal_description="the `foo` goal",
     )
     remedy = (
-        "Please specify relevant file and/or target arguments. Run `./pants "
+        "Please specify relevant file and/or target arguments. Run `pants "
         "--filter-target-type=tgt1,tgt2 list ::` to find all applicable targets in your project, "
-        "or run `./pants --filter-target-type=tgt1,tgt2 filedeps ::` to find all "
+        "or run `pants --filter-target-type=tgt1,tgt2 filedeps ::` to find all "
         "applicable files."
     )
     assert (
