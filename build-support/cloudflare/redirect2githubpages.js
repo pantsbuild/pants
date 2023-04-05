fae404008900aeab955583355d865aede5bc88c9
Benjy Weinberger <benjyw@gmail.com>
2023-04-05 14:52:54 -0700

    Add lint/fmt support (and run fmt)

--- build-support/cloudflare/redirect2githubpages.js
+++ build-support/cloudflare/redirect2githubpages.js
@@ -19,92 +19,96 @@
 // GA4_API_SECRET must be set under Settings > Variables > Environment Variables for the worker.
 // The secret is obtained under "Measurement Protocol API secrets" on the stream details page
 // in Google Analytics.
-const apiSecret = GA4_API_SECRET
+const apiSecret = GA4_API_SECRET;
 
 // The measurement id is obtained from the stream details page in Google Analytics.
-const measurementId = "G-Z7HQ5KDHDP"
+const measurementId = "G-Z7HQ5KDHDP";
 
 const ga4URL = `https://www.google-analytics.com/mp/collect?measurement_id=${measurementId}&api_secret=${apiSecret}`;
 
 function sendToGA4(headers, host, path) {
-    const clientId = Math.random().toString(16).substring(2)
-    const clientIP = headers.get("CF-Connecting-IP") || ""
+  const clientId = Math.random().toString(16).substring(2);
+  const clientIP = headers.get("CF-Connecting-IP") || "";
 
-    // GA drops hits with non-browser user agents on the floor,
-    // including curl, which is what we expect people to mostly use.
-    // So we detect if the userAgent isn't a browser,and adjust if so.
-    let userAgent = headers.get("User-Agent") || ""
-    if (!userAgent.toLowerCase().startsWith("mozilla") &&
-        !userAgent.toLowerCase().startsWith("opera")) {
-        userAgent = ""  // GA accepts an empty user agent, go figure.
-    }
+  // GA drops hits with non-browser user agents on the floor,
+  // including curl, which is what we expect people to mostly use.
+  // So we detect if the userAgent isn't a browser,and adjust if so.
+  let userAgent = headers.get("User-Agent") || "";
+  if (
+    !userAgent.toLowerCase().startsWith("mozilla") &&
+    !userAgent.toLowerCase().startsWith("opera")
+  ) {
+    userAgent = ""; // GA accepts an empty user agent, go figure.
+  }
 
-    const events = [
-        {
-            "name": "page_view",
-            "params": {
-                "page_host": host,
-                "page_path": path,
-                "ip": clientIP,
-                "user_agent": userAgent
-            }
-        }
-    ]
+  const events = [
+    {
+      name: "page_view",
+      params: {
+        page_host: host,
+        page_path: path,
+        ip: clientIP,
+        user_agent: userAgent,
+      },
+    },
+  ];
 
-    const data = {
-        "client_id": clientId,
-        "events": events,
-    }
+  const data = {
+    client_id: clientId,
+    events: events,
+  };
 
-    const payload = JSON.stringify(data);
-    const gaHeaders = new Headers();
-    gaHeaders.append("Content-Type", "application/json");
-    return fetch(ga4URL, {"method": "POST", "headers": gaHeaders, "body": payload})
+  const payload = JSON.stringify(data);
+  const gaHeaders = new Headers();
+  gaHeaders.append("Content-Type", "application/json");
+  return fetch(ga4URL, { method: "POST", headers: gaHeaders, body: payload });
 }
 
 // UA is being phased out in mid-2023, so we'll delete this once we have GA4 set up properly.
 function sendToUA(headers, host, path) {
-    const url = "https://www.google-analytics.com/collect"
-    const uuid = Math.random().toString(16).substring(2)
-    const clientIP = headers.get("CF-Connecting-IP") || ""
+  const url = "https://www.google-analytics.com/collect";
+  const uuid = Math.random().toString(16).substring(2);
+  const clientIP = headers.get("CF-Connecting-IP") || "";
 
-    // GA drops hits with non-browser user agents on the floor,
-    // including curl, which is what we expect people to mostly use.
-    // So we detect if the userAgent isn't a browser,and adjust if so.
-    userAgent = headers.get("User-Agent") || ""
-    if (!userAgent.toLowerCase().startsWith("mozilla") &&
-        !userAgent.toLowerCase().startsWith("opera")) {
-        userAgent = ""  // GA accepts an empty user agent, go figure.
-    }
+  // GA drops hits with non-browser user agents on the floor,
+  // including curl, which is what we expect people to mostly use.
+  // So we detect if the userAgent isn't a browser,and adjust if so.
+  userAgent = headers.get("User-Agent") || "";
+  if (
+    !userAgent.toLowerCase().startsWith("mozilla") &&
+    !userAgent.toLowerCase().startsWith("opera")
+  ) {
+    userAgent = ""; // GA accepts an empty user agent, go figure.
+  }
 
-    data = {
-        "v": "1",
-        "tid": "UA-78111411-2",
-        "cid": uuid,
-        "t": "pageview",
-        "dh": host,
-        "dp": path,
-        "uip": clientIP,
-        "ua": userAgent,
-    }
+  data = {
+    v: "1",
+    tid: "UA-78111411-2",
+    cid: uuid,
+    t: "pageview",
+    dh: host,
+    dp: path,
+    uip: clientIP,
+    ua: userAgent,
+  };
 
-    const payload = new URLSearchParams(data).toString();
-    return fetch(url, {"method": "POST", "body": payload})
+  const payload = new URLSearchParams(data).toString();
+  return fetch(url, { method: "POST", body: payload });
 }
 
 async function handleRequest(event) {
-    const url = new URL(event.request.url)
-    const { pathname, search } = url
-    const destinationURL = "https://pantsbuild.github.io" + pathname + search
-    event.waitUntil(
-        Promise.all([
-            sendToGA4(event.request.headers, url.host, pathname),
-            sendToUA(event.request.headers, url.host, pathname)
-        ])
-    )
-    return Response.redirect(destinationURL, 302)
+  const url = new URL(event.request.url);
+  const { pathname, search } = url;
+  const destinationURL = "https://pantsbuild.github.io" + pathname + search;
+  event.waitUntil(
+    Promise.all([
+      sendToGA4(event.request.headers, url.host, pathname),
+      sendToUA(event.request.headers, url.host, pathname),
+    ])
+  );
+  return Response.redirect(destinationURL, 302);
 }
 
-addEventListener("fetch", async event => {
-    event.respondWith(handleRequest(event))
-})
+addEventListener("fetch", async (event) => {
+  event.respondWith(handleRequest(event));
+});
