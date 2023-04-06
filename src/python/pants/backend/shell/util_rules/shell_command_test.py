41f1254d977e40942736c1e12dc1d4ad2eedff1a
Christopher Neugebauer <chrisjrn@toolchain.com>
2023-04-06 13:33:23 -0700

    Makes `adhoc_tool` actually respect the `extra_env_vars` field; adds tests (#18692)
    Fixes #18691 .

--- src/python/pants/backend/shell/util_rules/shell_command_test.py
+++ src/python/pants/backend/shell/util_rules/shell_command_test.py
@@ -803,3 +803,29 @@ def test_missing_tool_called(
         )
 
     assert "requires the names of any external commands" in caplog.text
+
+
+def test_env_vars(rule_runner: RuleRunner) -> None:
+    envvar_value = "clang"
+    rule_runner.write_files(
+        {
+            "src/BUILD": dedent(
+                f"""\
+                shell_command(
+                  name="envvars",
+                  tools=[],
+                  command='echo $ENVVAR > out.log',
+                  output_files=["out.log"],
+                  extra_env_vars=["ENVVAR={envvar_value}"],
+                  root_output_directory=".",
+                )
+                """
+            ),
+        }
+    )
+
+    assert_shell_command_result(
+        rule_runner,
+        Address("src", target_name="envvars"),
+        expected_contents={"out.log": f"{envvar_value}\n"},
+    )
