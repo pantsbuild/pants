9ec6211ecce7e423aad0704b286cb2d435e3522a
Asher Foa <1268088+asherf@users.noreply.github.com>
2023-04-06 11:46:56 -0400

    revert mypy change

--- src/python/pants/backend/python/typecheck/mypy/subsystem.py
+++ src/python/pants/backend/python/typecheck/mypy/subsystem.py
@@ -93,9 +93,9 @@ class MyPy(PythonToolBase):
     name = "MyPy"
     help = "The MyPy Python type checker (http://mypy-lang.org/)."
 
-    default_version = "mypy>=1.1.1,<2"
+    default_version = "mypy==1.1.1"
     default_main = ConsoleScript("mypy")
-    default_requirements = [default_version]
+    default_requirements = ["mypy>=0.961,<2"]
 
     # See `mypy/rules.py`. We only use these default constraints in some situations.
     register_interpreter_constraints = True
