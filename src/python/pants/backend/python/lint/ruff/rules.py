1b60f0f234f48bee3aa7a5cc12ab8f4748b07420
Joshua <joshdcannon@gmail.com>
2023-04-10 13:13:45 -0500

    ok here we go

--- src/python/pants/backend/python/lint/ruff/rules.py
+++ src/python/pants/backend/python/lint/ruff/rules.py
@@ -9,13 +9,16 @@ from pants.backend.python.lint.ruff.subsystem import Ruff, RuffFieldSet
 from pants.backend.python.util_rules import pex
 from pants.backend.python.util_rules.pex import PexRequest, VenvPex, VenvPexProcess
 from pants.core.goals.fix import FixResult, FixTargetsRequest
-from pants.core.goals.lint import LintRequest, LintTargetsRequest
+from pants.core.goals.lint import LintRequest, LintResult, LintTargetsRequest
 from pants.core.util_rules.config_files import ConfigFiles, ConfigFilesRequest
 from pants.core.util_rules.partitions import PartitionerType
+from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
 from pants.engine.fs import Digest, MergeDigests
+from pants.engine.internals.native_engine import Snapshot
 from pants.engine.process import FallibleProcessResult
 from pants.engine.rules import Get, MultiGet, collect_rules, rule
 from pants.util.logging import LogLevel
+from pants.util.meta import classproperty
 from pants.util.strutil import pluralize
 
 
@@ -24,6 +27,10 @@ class RuffFixRequest(FixTargetsRequest):
     tool_subsystem = Ruff
     partitioner_type = PartitionerType.DEFAULT_SINGLE_PARTITION
 
+    @classproperty
+    def tool_name(cls) -> str:
+        return "ruff --fix"
+
 
 class RuffLintRequest(LintTargetsRequest):
     field_set_type = RuffFieldSet
@@ -31,9 +38,9 @@ class RuffLintRequest(LintTargetsRequest):
     partitioner_type = PartitionerType.DEFAULT_SINGLE_PARTITION
 
 
-@dataclass(frzoen=True)
+@dataclass(frozen=True)
 class _RunRuffRequest:
-    batch: LintRequest.Batch
+    snapshot: Snapshot
     is_fix: bool
 
 
@@ -45,14 +52,14 @@ async def run_ruff(
     ruff_pex_get = Get(VenvPex, PexRequest, ruff.to_pex_request())
 
     config_files_get = Get(
-        ConfigFiles, ConfigFilesRequest, ruff.config_request(request.batch.snapshot.dirs)
+        ConfigFiles, ConfigFilesRequest, ruff.config_request(request.snapshot.dirs)
     )
 
     ruff_pex, config_files = await MultiGet(ruff_pex_get, config_files_get)
 
     input_digest = await Get(
         Digest,
-        MergeDigests((request.batch.snapshot.digest, config_files.snapshot.digest)),
+        MergeDigests((request.snapshot.digest, config_files.snapshot.digest)),
     )
 
     conf_args = [f"--config={ruff.config}"] if ruff.config else []
@@ -62,10 +69,10 @@ async def run_ruff(
         FallibleProcessResult,
         VenvPexProcess(
             ruff_pex,
-            argv=(*maybe_fix_args, *conf_args, *ruff.args, *request.batch.files),
+            argv=(*maybe_fix_args, *conf_args, *ruff.args, *request.snapshot.files),
             input_digest=input_digest,
-            output_files=request.batch.files,
-            description=f"Run ruff {' '.join(maybe_fix_args)} on {pluralize(len(request.batch.elements), 'file')}.",
+            output_files=request.snapshot.files,
+            description=f"Run ruff {' '.join(maybe_fix_args)} on {pluralize(len(request.snapshot.files), 'file')}.",
             level=LogLevel.DEBUG,
         ),
     )
@@ -74,19 +81,23 @@ async def run_ruff(
 
 @rule(desc="Fix with ruff", level=LogLevel.DEBUG)
 async def ruff_fix(request: RuffFixRequest.Batch, ruff: Ruff) -> FixResult:
-    result = await Get(FallibleProcessResult, _RunRuffRequest(batch=request, is_fix=True))
+    result = await Get(FallibleProcessResult, _RunRuffRequest(snapshot=request.snapshot, is_fix=True))
     return await FixResult.create(request, result, strip_chroot_path=True)
 
 
 @rule(desc="Lint with ruff", level=LogLevel.DEBUG)
-async def ruff_lint(request: RuffFixRequest.Batch) -> FixResult:
-    result = await Get(FallibleProcessResult, _RunRuffRequest(batch=request, is_fix=False))
-    return await FixResult.create(request, result, strip_chroot_path=True)
+async def ruff_lint(request: RuffLintRequest.Batch) -> LintResult:
+    source_files = await Get(
+        SourceFiles, SourceFilesRequest(field_set.source for field_set in request.elements)
+    )
+    result = await Get(FallibleProcessResult, _RunRuffRequest(snapshot=source_files.snapshot, is_fix=False))
+    return LintResult.create(request, result, strip_chroot_path=True)
 
 
 def rules():
     return [
         *collect_rules(),
         *RuffFixRequest.rules(),
+        *RuffLintRequest.rules(),
         *pex.rules(),
     ]
