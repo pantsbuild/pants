f71db2539faf696c7ec3fac72b2ba4723c4d939b
Huon Wilson <huon@exoflare.io>
2023-04-21 08:53:45 +1000

    a bunch more tests, xfail --force test

--- src/python/pants/backend/tools/semgrep/rules_integration_test.py
+++ src/python/pants/backend/tools/semgrep/rules_integration_test.py
@@ -31,7 +31,7 @@ DIR = "src"
 SEMGREP_ERROR_FAILURE_RETURN_CODE = 1
 
 GOOD_FILE = "nothing_bad"
-BAD_FILE = "bad_pattern"
+BAD_FILE = "bad_pattern\nalso_bad"
 RULES = dedent(
     """\
     rules:
@@ -48,6 +48,40 @@ RULES = dedent(
          - '*.yml'
     """
 )
+RULES2 = dedent(
+    """\
+    rules:
+    - id: find-another-bad-pattern
+      patterns:
+        - pattern: also_bad
+      message: >-
+        second bad found!
+      languages: [generic]
+      severity: ERROR
+      paths:
+        # 'generic' means this finds itself
+        exclude:
+         - '*.yml'
+    """
+)
+
+SINGLE_FILE_BUILD = dedent(
+    """\
+    file(name="f", source="file.txt")
+    semgrep_rule_sources(name="s")
+    """
+)
+
+BAD_FILE_LAYOUT = {
+    f"{DIR}/file.txt": BAD_FILE,
+    f"{DIR}/.semgrep.yml": RULES,
+    f"{DIR}/BUILD": SINGLE_FILE_BUILD,
+}
+GOOD_FILE_LAYOUT = {
+    f"{DIR}/file.txt": GOOD_FILE,
+    f"{DIR}/.semgrep.yml": RULES,
+    f"{DIR}/BUILD": SINGLE_FILE_BUILD,
+}
 
 
 @pytest.fixture
@@ -92,12 +126,14 @@ def run_semgrep(
 def assert_success(
     rule_runner: RuleRunner, target: Target, *, extra_args: Sequence[str] = ()
 ) -> None:
-    result = run_semgrep(rule_runner, [target], extra_args=extra_args)
+    results = run_semgrep(rule_runner, [target], extra_args=extra_args)
 
-    assert len(result) == 1
-    assert "Ran 1 rule on 1 file: 0 findings" in result[0].stderr
-    assert result[0].exit_code == 0
-    assert result[0].report == EMPTY_DIGEST
+    assert len(results) == 1
+    result = results[0]
+    assert result.stdout == ""
+    assert "Ran 1 rule on 1 file: 0 findings" in result.stderr
+    assert result.exit_code == 0
+    assert result.report == EMPTY_DIGEST
 
 
 @pytest.mark.parametrize(
@@ -105,18 +141,7 @@ def assert_success(
     all_major_minor_python_versions(Semgrep.default_interpreter_constraints),
 )
 def test_passing(rule_runner: RuleRunner, major_minor_interpreter: str) -> None:
-    rule_runner.write_files(
-        {
-            f"{DIR}/file.txt": GOOD_FILE,
-            f"{DIR}/.semgrep.yml": RULES,
-            f"{DIR}/BUILD": dedent(
-                """\
-                file(name="f", source="file.txt")
-                semgrep_rule_sources(name="s")
-                """
-            ),
-        }
-    )
+    rule_runner.write_files(GOOD_FILE_LAYOUT)
     tgt = rule_runner.get_target(Address(DIR, target_name="f"))
     assert_success(
         rule_runner,
@@ -126,25 +151,225 @@ def test_passing(rule_runner: RuleRunner, major_minor_interpreter: str) -> None:
 
 
 def test_failing(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(BAD_FILE_LAYOUT)
+    tgt = rule_runner.get_target(Address(DIR, target_name="f"))
+
+    results = run_semgrep(rule_runner, [tgt])
+    assert len(results) == 1
+    result = results[0]
+    assert "find-bad-pattern" in result.stdout
+    assert "Ran 1 rule on 1 file: 1 finding" in result.stderr
+    assert result.exit_code == SEMGREP_ERROR_FAILURE_RETURN_CODE
+    assert result.report == EMPTY_DIGEST
+
+
+def test_multiple_targets(rule_runner: RuleRunner) -> None:
     rule_runner.write_files(
         {
-            f"{DIR}/file.txt": BAD_FILE,
+            f"{DIR}/good.txt": GOOD_FILE,
+            f"{DIR}/bad.txt": BAD_FILE,
             f"{DIR}/.semgrep.yml": RULES,
             f"{DIR}/BUILD": dedent(
                 """\
-                file(name="f", source="file.txt")
+                file(name="g", source="good.txt")
+                file(name="b", source="bad.txt")
                 semgrep_rule_sources(name="s")
                 """
             ),
         }
     )
-    tgt = rule_runner.get_target(Address(DIR, target_name="f"))
 
-    result = run_semgrep(
+    tgts = [rule_runner.get_target(Address(DIR, target_name=name)) for name in ["g", "b"]]
+
+    results = run_semgrep(
+        rule_runner,
+        tgts,
+    )
+    assert len(results) == 1
+    result = results[0]
+    assert "find-bad-pattern" in result.stdout
+    assert "Ran 1 rule on 2 files: 1 finding" in result.stderr
+    assert result.exit_code == SEMGREP_ERROR_FAILURE_RETURN_CODE
+    assert result.report == EMPTY_DIGEST
+
+
+@pytest.mark.parametrize(
+    "files",
+    [
+        pytest.param(
+            {
+                **BAD_FILE_LAYOUT,
+                ".semgrep.yml": RULES2,
+                "BUILD": """semgrep_rule_sources(name="s")""",
+            },
+            id="via nesting",
+        ),
+        pytest.param(
+            {
+                f"{DIR}/bad.txt": BAD_FILE,
+                f"{DIR}/BUILD": """file(name="f", source="bad.txt")""",
+                ".semgrep/one.yml": RULES,
+                ".semgrep/two.yml": RULES2,
+                "BUILD": """semgrep_rule_sources(name="s")""",
+            },
+            id="via .semgrep directory",
+        ),
+    ],
+)
+def test_multiple_configs(rule_runner: RuleRunner, files: dict[str, str]) -> None:
+    rule_runner.write_files(files)
+
+    tgt = rule_runner.get_target(Address(DIR, target_name="f"))
+    results = run_semgrep(rule_runner, [tgt])
+
+    assert len(results) == 1
+    result = results[0]
+    assert "find-bad-pattern" in result.stdout
+    assert "find-another-bad-pattern" in result.stdout
+    assert "Ran 2 rules on 1 file: 2 findings" in result.stderr
+    assert result.exit_code == SEMGREP_ERROR_FAILURE_RETURN_CODE
+    assert result.report == EMPTY_DIGEST
+
+
+def test_semgrepignore(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files({**BAD_FILE_LAYOUT, ".semgrepignore": "file.txt"})
+
+    tgt = rule_runner.get_target(Address(DIR, target_name="f"))
+    results = run_semgrep(rule_runner, [tgt])
+
+    assert len(results) == 1
+    result = results[0]
+    assert result.stdout == ""
+    assert "Ran 1 rule on 0 files: 0 findings" in result.stderr
+    assert result.exit_code == 0
+    assert result.report == EMPTY_DIGEST
+
+
+def test_semgrepignore_nested_ignored(rule_runner: RuleRunner, caplog) -> None:
+    rule_runner.write_files({**BAD_FILE_LAYOUT, f"{DIR}/.semgrepignore": "file.txt"})
+
+    tgt = rule_runner.get_target(Address(DIR, target_name="f"))
+    results = run_semgrep(rule_runner, [tgt])
+
+    # a nested semgrep ignore file is completely unused...
+    assert len(results) == 1
+    result = results[0]
+    assert "find-bad-pattern" in result.stdout
+    assert "Ran 1 rule on 1 file: 1 finding" in result.stderr
+    assert result.exit_code == SEMGREP_ERROR_FAILURE_RETURN_CODE
+    assert result.report == EMPTY_DIGEST
+
+    # ...but there's a pants warning about it...
+    assert "Semgrep does not obey .semgrepignore outside the working directory" in caplog.text
+    assert f"{DIR}/.semgrepignore" in caplog.text
+
+    caplog.clear()
+
+    # ... that can be silenced
+    results = run_semgrep(
         rule_runner,
         [tgt],
+        extra_args=["--semgrep-acknowledge-nested-semgrepignore-files-are-not-used"],
     )
-    assert len(result) == 1
-    assert "Ran 1 rule on 1 file: 1 finding" in result[0].stderr
-    assert result[0].exit_code == SEMGREP_ERROR_FAILURE_RETURN_CODE
-    assert result[0].report == EMPTY_DIGEST
+    assert not caplog.records
+
+
+def test_partition_by_config(rule_runner: RuleRunner) -> None:
+    file_dirs = []
+
+    def file___(dir: str) -> dict[str, str]:
+        file_dirs.append(dir)
+        return {
+            f"{dir}/file.txt": GOOD_FILE,
+            f"{dir}/BUILD": """file(name="f", source="file.txt")""",
+        }
+
+    def semgrep(dir: str) -> dict[str, str]:
+        return {f"{dir}/.semgrep.yml": RULES, f"{dir}/BUILD": """semgrep_rule_sources(name="s")"""}
+
+    rule_runner.write_files(
+        {
+            # 'y'/'n' indicates whether that level has semgrep config
+            **semgrep("y"),
+            **file___("y/dir1"),
+            **file___("y/dir2"),
+            **file___("y/n/dir1"),
+            **file___("y/n/dir2"),
+            **semgrep("y/y"),
+            **file___("y/y/dir1"),
+            **file___("y/y/dir2"),
+            **file___("n"),
+        }
+    )
+
+    field_sets = tuple(
+        SemgrepFieldSet.create(rule_runner.get_target(Address(dir, target_name="f")))
+        for dir in file_dirs
+    )
+
+    partitions = rule_runner.request(
+        Partitions[SemgrepFieldSet, PartitionMetadata],
+        [SemgrepRequest.PartitionRequest(field_sets)],
+    )
+
+    sorted_partitions = sorted(
+        (
+            sorted(field_set.address.spec for field_set in partition.elements),
+            sorted(f.address.filename for f in partition.metadata.config_files),
+        )
+        for partition in partitions
+    )
+
+    assert sorted_partitions == [
+        (
+            ["y/dir1:f", "y/dir2:f", "y/n/dir1:f", "y/n/dir2:f"],
+            ["y/.semgrep.yml"],
+        ),
+        (
+            ["y/y/dir1:f", "y/y/dir2:f"],
+            ["y/.semgrep.yml", "y/y/.semgrep.yml"],
+        ),
+        # n: doesn't appear in any partition
+    ]
+
+
+def test_skip(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(BAD_FILE_LAYOUT)
+    tgt = rule_runner.get_target(Address(DIR, target_name="f"))
+
+    results = run_semgrep(rule_runner, [tgt], extra_args=["--semgrep-skip"])
+    assert not results
+
+
+@pytest.mark.xfail
+def test_force(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(GOOD_FILE_LAYOUT)
+    tgt = rule_runner.get_target(Address(DIR, target_name="f"))
+
+    # Should not receive a memoized result if force=True.
+    results1 = run_semgrep(rule_runner, [tgt], extra_args=["--semgrep-force"])
+    results2 = run_semgrep(rule_runner, [tgt], extra_args=["--semgrep-force"])
+
+    assert len(results1) == len(results2) == 1
+    assert results1[0].exit_code == results2[0].exit_code == 0
+    assert results1[0] is not results2[0]
+
+    # But should if force=False.
+    results1 = run_semgrep(rule_runner, [tgt])
+    results2 = run_semgrep(rule_runner, [tgt])
+    assert len(results1) == len(results2) == 1
+    assert results1[0].exit_code == results2[0].exit_code == 0
+    assert results1[0] is results2[0]
+
+
+def test_extra_args(rule_runner: RuleRunner) -> None:
+    rule_runner.write_files(BAD_FILE_LAYOUT)
+    tgt = rule_runner.get_target(Address(DIR, target_name="f"))
+
+    results = run_semgrep(rule_runner, [tgt], extra_args=["--semgrep-args=--quiet"])
+    assert len(results) == 1
+    result = results[0]
+    assert "find-bad-pattern" in result.stdout
+    assert result.stderr == ""
+    assert result.exit_code == SEMGREP_ERROR_FAILURE_RETURN_CODE
+    assert result.report == EMPTY_DIGEST
