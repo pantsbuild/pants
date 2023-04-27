97b2682c6690b8bea7bc1606ae6aebce76c46fb0
Jacob Floyd <cognifloyd@gmail.com>
2023-04-27 15:03:38 -0500

    Add stub files to hold the packager-specific nFPM metadata fields

---
+++ src/python/pants/backend/nfpm/archlinux_fields.py
@@ -0,0 +1,8 @@
+# Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+# These fields are used by the `nfpm_archlinux_package` target
+# Some fields will be duplicated by other package types which
+# allows the help string to be packager-specific.
