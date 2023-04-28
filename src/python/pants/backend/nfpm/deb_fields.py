100cb638e7ec46c6e498a477b93c068a204075b6
Jacob Floyd <cognifloyd@gmail.com>
2023-04-27 21:44:37 -0500

    Add nFPM fields for deb 'section' and 'priority'

--- src/python/pants/backend/nfpm/deb_fields.py
+++ src/python/pants/backend/nfpm/deb_fields.py
@@ -3,6 +3,8 @@
 
 from __future__ import annotations
 
+from enum import Enum
+
 from pants.engine.target import StringField
 from pants.util.strutil import help_text
 
@@ -28,3 +30,47 @@ class NfpmDebMaintainerField(StringField):
         """
     )
     # TODO: Add validation for the "Name <email@domain>" format
+
+
+class NfpmDebSectionField(StringField):
+    alias = "section"
+    help = help_text(
+        """
+        Which section, or application area, this package is part of.
+
+        For example, you could classify your application under the "education"
+        section, or under a language section like "python", "rust", or "ruby".
+
+        See: https://www.debian.org/doc/debian-policy/ch-archive.html#sections
+
+        Valid sections are listed here:
+        https://www.debian.org/doc/debian-policy/ch-archive.html#s-subsections
+        """
+    )
+
+
+class DebPriority(Enum):
+    # based on https://www.debian.org/doc/debian-policy/ch-archive.html#priorities
+    required = "required"
+    important = "important"
+    standard = "standard"
+    optional = "optional"
+    extra = "extra"  # Debian notes that this is deprecated and equivalent to optional.
+
+
+class NfpmDebPriorityField(StringField):
+    alias = "priority"
+    valid_choices = DebPriority
+    default = DebPriority.optional.value
+    help = help_text(
+        """
+        Indicates how important the package is for OS functions.
+
+        Most packages should just stick with the default priority: "optional".
+
+        See: https://www.debian.org/doc/debian-policy/ch-archive.html#s-f-priority
+
+        Valid priorities are listed here:
+        https://www.debian.org/doc/debian-policy/ch-archive.html#priorities
+        """
+    )
