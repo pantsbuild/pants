fd4bc9c754e37260adb49110c8bd1609f73c32b8
Lasse Alm <lassealm2@gmail.com>
2023-04-29 11:45:05 +0200

    updated copyright date

--- src/python/pants/backend/experimental/rust/register.py
+++ src/python/pants/backend/experimental/rust/register.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 from pants.backend.rust.goals import tailor
 from pants.backend.rust.lint.rustfmt.rules import rules as rustfmt_rules
--- src/python/pants/backend/rust/lint/rustfmt/skip_field.py
+++ src/python/pants/backend/rust/lint/rustfmt/skip_field.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 from pants.backend.rust.target_types import RustCrateTarget
 from pants.engine.target import BoolField
