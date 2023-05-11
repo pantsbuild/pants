77afe548ef5778b429aff67a9bcbb554696ed609
Joshua <joshdcannon@gmail.com>
2023-05-10 20:53:36 -0500

    cherries

--- build-support/bin/cherry_pick.sh
+++ build-support/bin/cherry_pick.sh
@@ -8,6 +8,7 @@ set -e
 
 function fail {
   printf '%s\n' "$1" >&2
+  echo "Don't forget to remove the 'needs-cherrypick' label after making the PR!"
   exit "${2-1}"
 }
 
@@ -25,14 +26,24 @@ if [[ -z $PR_NUM ]]; then
   read -r PR_NUM
 fi
 
-MILESTONE=$2
-if [[ -z $MILESTONE ]]; then
-  MILESTONE=$(gh pr view "$PR_NUM" --json milestone --jq '.milestone.title')
-  if [[ -z $MILESTONE ]]; then
+TARGET_MILESTONE=$2
+if [[ -z $TARGET_MILESTONE ]]; then
+  TARGET_MILESTONE=$(gh pr view "$PR_NUM" --json milestone --jq '.milestone.title')
+  if [[ -z $TARGET_MILESTONE ]]; then
     echo "No milestone on PR. What's the milestone? (E.g. 2.10.x)"
-    read -r MILESTONE
+    read -r TARGET_MILESTONE
   fi
 fi
+
+MILESTONES=$(gh api graphql -F owner=':owner' -F name=':repo' -f query='
+  query ListMilestones($name: String!, $owner: String!) {
+    repository(owner: $owner, name: $name) {
+      milestones(last: 10) {
+        nodes {title}
+      }
+    }
+  }' --jq .data.repository.milestones.nodes.[].title | awk "/$TARGET_MILESTONE/{p=1}p" -)
+
 
 COMMIT=$(gh pr view "$PR_NUM" --json mergeCommit --jq '.mergeCommit.oid')
 TITLE=$(gh pr view "$PR_NUM" --json title --jq '.title')
@@ -44,22 +55,26 @@ if [[ -z $CATEGORY_LABEL ]]; then
   read -r CATEGORY_LABEL
 fi
 REVIEWERS=$(gh pr view "$PR_NUM" --json reviews --jq '.reviews.[].author.login' | sort | uniq)
-BODY_FILE=$(mktemp "/tmp/github.cherrypick.$PR_NUM.$MILESTONE.XXXXXX")
-PR_CREATE_CMD=(gh pr create --base "$MILESTONE" --title "$TITLE (Cherry-pick of #$PR_NUM)" --label "$CATEGORY_LABEL" --body-file "$BODY_FILE")
-while IFS= read -r REVIEWER; do PR_CREATE_CMD+=(--reviewer "$REVIEWER"); done <<< "$REVIEWERS"
-BRANCH_NAME="cherry-pick-$PR_NUM-to-$MILESTONE"
 
-if [[ -z $COMMIT ]]; then
-  fail "Wasn't able to retrieve merge commit for $PR_NUM."
-fi
+for MILESTONE in $MILESTONES
+do
+  BODY_FILE=$(mktemp "/tmp/github.cherrypick.$PR_NUM.$MILESTONE.XXXXXX")
+  PR_CREATE_CMD=(gh pr create --base "$MILESTONE" --title "$TITLE (Cherry-pick of #$PR_NUM)" --label "$CATEGORY_LABEL" --body-file "$BODY_FILE")
+  while IFS= read -r REVIEWER; do PR_CREATE_CMD+=(--reviewer "$REVIEWER"); done <<< "$REVIEWERS"
+  BRANCH_NAME="cherry-pick-$PR_NUM-to-$MILESTONE"
 
-gh pr view "$PR_NUM" --json body --jq '.body' > "$BODY_FILE"
-git fetch https://github.com/pantsbuild/pants "$MILESTONE"
-git checkout -b "$BRANCH_NAME" FETCH_HEAD
-git cherry-pick "$COMMIT" ||
-  fail "\nPlease fix the above conflicts, commit, and then run:\n  ${PR_CREATE_CMD[*]}"
+  if [[ -z $COMMIT ]]; then
+    fail "Wasn't able to retrieve merge commit for $PR_NUM."
+  fi
 
-"${PR_CREATE_CMD[@]}"
-rm "$BODY_FILE"
+  gh pr view "$PR_NUM" --json body --jq '.body' > "$BODY_FILE"
+  git fetch https://github.com/pantsbuild/pants "$MILESTONE"
+  git checkout -b "$BRANCH_NAME" FETCH_HEAD
+  git cherry-pick "$COMMIT" ||
+    fail "\nPlease fix the above conflicts, commit, and then run:\n  ${PR_CREATE_CMD[*]}"
 
-echo "Don't forget to remove the 'needs-cherrypick' label from PR #$PR_NUM!"
+  "${PR_CREATE_CMD[@]}"
+  rm "$BODY_FILE"
+done
+
+gh pr edit "$PR_NUM" --remove-label "need-cherrypick"
