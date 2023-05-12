426eecd29e0d21ba9f9ba75006fb2972cbb44650
Tobias Nilsson <tobias.jens.nilsson@gmail.com>
2023-05-12 21:16:00 +0200

    doc: Motivate functions existence

--- src/rust/engine/dep_inference/src/util.rs
+++ src/rust/engine/dep_inference/src/util.rs
@@ -1,8 +1,13 @@
+// Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
 use std::path::{Component, Path, PathBuf};
 
-/// Creates a [`std::path::PathBuf`], normalizing `.` and `..`.
-/// Returns `None` when the normalized path is a parent directory
-/// of `path`s base.
+/// Creates a [`PathBuf`], normalizing `.` and `..`.
+///
+/// Returns [`None`] when the normalization walks out of
+/// the [`Path`]s base.
+/// This is different to [`NormalizePath`](https://docs.rs/normalize-path/latest/normalize_path/trait.NormalizePath.html),
+/// which returns the file name in this case.
 pub fn normalize_path(path: &Path) -> Option<PathBuf> {
   let mut components = path.components().peekable();
   let mut ret = if let Some(c @ Component::Prefix(..)) = components.peek().cloned() {
