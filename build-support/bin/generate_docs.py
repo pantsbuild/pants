eea1fdda6496869034688a2472e61631056caa87
Joshua Cannon <joshdcannon@gmail.com>
2023-05-15 16:30:15 -0500

    Add a workunit logging plugin (#18984)
    This got surfaced in Slack and I realized I had one lying around.
    
    Idea is to unconditionally enable it (like I have in this repo) and then
    selectively use `pants --workunit-logger-enabled ...`
    
    ---------
    
    Co-authored-by: Huon Wilson <wilson.huon@gmail.com>

--- build-support/bin/generate_docs.py
+++ build-support/bin/generate_docs.py
@@ -263,6 +263,7 @@ def run_pants_help_all() -> dict[str, Any]:
         "pants.backend.experimental.scala",
         "pants.backend.experimental.scala.lint.scalafmt",
         "pants.backend.experimental.terraform",
+        "pants.backend.experimental.tools.workunit_logger",
         "pants.backend.experimental.tools.yamllint",
         "pants.backend.google_cloud_function.python",
         "pants.backend.plugin_development",
