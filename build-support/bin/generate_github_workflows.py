03e194d29c562094c19beeb0ca54bba6dcfaa92c
Stu Hood <stuhood@gmail.com>
2023-05-19 13:27:02 -0700

    Additional release job publishing fixes (#19058)
    The publishing job needs additional fixes, which unfortunately can't
    (all) be accomplished using workflow edits. This change needs
    cherry-picking to `2.17.x` in order to make a second attempt at
    `2.17.0a1`.

--- build-support/bin/generate_github_workflows.py
+++ build-support/bin/generate_github_workflows.py
@@ -423,25 +423,26 @@ class Helper:
             },
         ]
 
-    def setup_primary_python(self) -> Sequence[Step]:
+    def setup_primary_python(self, version: str | None = None) -> Sequence[Step]:
+        version = version or gha_expr("matrix.python-version")
         ret = []
         # We pre-install Pythons on our self-hosted platforms.
         # We must set them up on Github-hosted platforms.
         if self.platform in GITHUB_HOSTED:
             ret.append(
                 {
-                    "name": f"Set up Python {gha_expr('matrix.python-version')}",
+                    "name": f"Set up Python {version}",
                     "uses": "actions/setup-python@v4",
-                    "with": {"python-version": f"{gha_expr('matrix.python-version')}"},
+                    "with": {"python-version": version},
                 }
             )
         ret.append(
             {
-                "name": f"Tell Pants to use Python {gha_expr('matrix.python-version')}",
+                "name": f"Tell Pants to use Python {version}",
                 "run": dedent(
                     f"""\
-                    echo "PY=python{gha_expr('matrix.python-version')}" >> $GITHUB_ENV
-                    echo "PANTS_PYTHON_INTERPRETER_CONSTRAINTS=['=={gha_expr('matrix.python-version')}.*']" >> $GITHUB_ENV
+                    echo "PY=python{version}" >> $GITHUB_ENV
+                    echo "PANTS_PYTHON_INTERPRETER_CONSTRAINTS=['=={version}.*']" >> $GITHUB_ENV
                     """
                 ),
             }
@@ -962,6 +963,8 @@ def release_jobs_and_inputs() -> tuple[Jobs, dict[str, Any]]:
     """Builds and releases a git ref to S3, and (if the ref is a release tag) to PyPI."""
     inputs, env = workflow_dispatch_inputs([WorkflowInput("REF", "string")])
 
+    pypi_release_dir = "dest/pypi_release"
+    helper = Helper(Platform.LINUX_X86_64)
     wheels_jobs = build_wheels_jobs(
         needs=["determine_ref"], for_deploy_ref=gha_expr("needs.determine_ref.outputs.build-ref")
     )
@@ -1007,12 +1010,17 @@ def release_jobs_and_inputs() -> tuple[Jobs, dict[str, Any]]:
                     "uses": "actions/checkout@v3",
                     "with": {"ref": f"{gha_expr('needs.determine_ref.outputs.build-ref')}"},
                 },
+                setup_toolchain_auth(),
+                *helper.setup_primary_python(PYTHON39_VERSION),
+                *helper.expose_all_pythons(),
                 {
                     "name": "Fetch and stabilize wheels",
-                    "run": "./build-support/bin/release.sh fetch-and-stabilize",
+                    "run": f"./build-support/bin/release.sh fetch-and-stabilize --dest={pypi_release_dir}",
                     "env": {
                         # This step does not actually build anything: only download wheels from S3.
                         "MODE": "debug",
+                        # We're not building anything, but without specifying a version, we get 3.10.
+                        "USE_PY39": "true",
                     },
                 },
                 {
@@ -1038,6 +1046,8 @@ def release_jobs_and_inputs() -> tuple[Jobs, dict[str, Any]]:
                     "uses": "pypa/gh-action-pypi-publish@release/v1",
                     "with": {
                         "password": gha_expr("secrets.PANTSBUILD_PYPI_API_TOKEN"),
+                        "packages-dir": pypi_release_dir,
+                        "skip-existing": True,
                     },
                 },
                 deploy_to_s3(
