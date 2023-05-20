2bc078b0252931ccd5140432a04f3328a0d32e3f
Huon Wilson <huon@exoflare.io>
2023-05-20 12:07:25 +1000

    Implement layout="zip" for Lambda/GCF, skipping lambdex (#19022)
    This fixes #18879 by allowing the `python_awslambda` and
    `python_google_cloud_function` FaaS artefacts to be generated in
    "simple" format, using the `pex3 venv create --layout=flat-zipped`
    functionality recently added in PEX 2.1.135
    (https://github.com/pantsbuild/pex/releases/tag/v2.1.135). This format
    is just: put everything at the top-level, e.g. the zip contains
    `cowsay/__init__.py` etc., rather than `.deps/cowsay-....whl` (plus the
    dynamic PEX initialisation).
    
    This shifts the dynamic dependency computation/extraction/layout from
    run-time to build-time, relying on the FaaS environment to be generally
    consistent. It shouldn't change what actually happens after
    initialisation. This can:
    
    - reduce cold-starts noticeably: for instance, some of our lambdas spend
    1s doing PEX/Lambdex start up.
    - reduce package size somewhat (the PEX `.bootstrap/` folder seems to be
    about 2MB uncompressed, ~1MB compressed).
    - increase build times.
     
    For instance, for one Python 3.9 Lambda in our codebase:
    
    | metric                        | before   | after            |
    |-------------------------------|----------|------------------|
    | init time on cold start       | 2.3-2.5s | 1.3-1.4s (-1s)   |
    | compressed size               | 24.6MB   | 23.8MB (-0.8MB)  |
    | uncompressed size             | 117.8MB  | 115.8MB (-2.0MB) |
    | PEX-construction build time   | ~5s      | ~5s              |
    | PEX-postprocessing build time | 0.14s    | 4.8s             |
    
    (The PEX-postprocessing time metric is specifically the time to run the
    `Setting up handler` (lambdex) or `Build python_awslambda` (`pex3 venv
    create`) process, computed by running `pants --keep-sandboxes=always
    package ...` for each layout, and then `hyperfine -r3 -w1
    path/to/first/__run.sh path/to/second/__run.sh`. This _doesn't_ include
    the time to construct the input PEX, which is the same for both.)
    
    This functionality is driven by adding a new `layout` field. It defaults
    to `lambdex` (retaining the current code paths), but also supports
    `zip`, which keys into the functionality above. I've tried to keep the
    non-lambdex implementation generally separate to the lambdex one, rather
    than reusing all of the code that happens to be common currently,
    because it'd make sense to deprecate/remove the lambdex functionality
    and thus I feel it's best for this new functionality to be mostly a
    fresh start.
    
    This PR's commits can be reviewed independently. It comes in three
    phases:
    
    1. Add the `pex_venv.py` util rules for running `pex3 venv create ...`.
    Currently this only supports a limited subset of the functionality
    there, but can presumably be expanded freely as required. (First commit)
    2. Do some minor refactoring. (Commits labelled "refactor: ...")
    3. Draw the rest of the owl. (The others.)
    
    I _think_ this is an acceptable MVP for this functionality, but there's
    various bits of follow-up:
    
    - deprecate `layout="lambdex"` (in favour of `layout="zip"` and/or
    normal `pex_binary`) (#19032)
    - add a warning about `files` being loaded into these packages, which
    has been temporarily lost (#19027)
    - adjust documentation
    - other improvements like #18195 and #18880 
    - improve performance, e.g. potentially `pex3 venv create ...` could use
    the lock file and sources to directly compute the appropriate files,
    without having to materialise a normal pex first

--- src/python/pants/backend/awslambda/python/rules.py
+++ src/python/pants/backend/awslambda/python/rules.py
@@ -12,8 +12,14 @@ from pants.backend.awslambda.python.target_types import (
     PythonAwsLambdaIncludeRequirements,
     PythonAwsLambdaRuntime,
 )
-from pants.backend.python.util_rules import pex_from_targets
-from pants.backend.python.util_rules.faas import BuildLambdexRequest, PythonFaaSCompletePlatforms
+from pants.backend.python.util_rules.faas import (
+    BuildLambdexRequest,
+    BuildPythonFaaSRequest,
+    PythonFaaSCompletePlatforms,
+    PythonFaaSLayout,
+    PythonFaaSLayoutField,
+)
+from pants.backend.python.util_rules.faas import rules as faas_rules
 from pants.core.goals.package import BuiltPackage, OutputPathField, PackageFieldSet
 from pants.core.util_rules.environments import EnvironmentField
 from pants.engine.rules import Get, collect_rules, rule
@@ -33,27 +39,49 @@ class PythonAwsLambdaFieldSet(PackageFieldSet):
     complete_platforms: PythonFaaSCompletePlatforms
     output_path: OutputPathField
     environment: EnvironmentField
+    layout: PythonFaaSLayoutField
 
 
 @rule(desc="Create Python AWS Lambda", level=LogLevel.DEBUG)
 async def package_python_awslambda(
     field_set: PythonAwsLambdaFieldSet,
 ) -> BuiltPackage:
+    layout = PythonFaaSLayout(field_set.layout.value)
+
+    if layout is PythonFaaSLayout.LAMBDEX:
+        return await Get(
+            BuiltPackage,
+            BuildLambdexRequest(
+                address=field_set.address,
+                target_name=PythonAWSLambda.alias,
+                complete_platforms=field_set.complete_platforms,
+                runtime=field_set.runtime,
+                handler=field_set.handler,
+                output_path=field_set.output_path,
+                include_requirements=field_set.include_requirements.value,
+                script_handler=None,
+                script_module=None,
+                # The AWS-facing handler function is always lambdex_handler.handler, which is the
+                # wrapper injected by lambdex that manages invocation of the actual handler.
+                handler_log_message="lambdex_handler.handler",
+            ),
+        )
+
     return await Get(
         BuiltPackage,
-        BuildLambdexRequest(
+        BuildPythonFaaSRequest(
             address=field_set.address,
             target_name=PythonAWSLambda.alias,
             complete_platforms=field_set.complete_platforms,
             runtime=field_set.runtime,
             handler=field_set.handler,
+            layout=layout,
             output_path=field_set.output_path,
             include_requirements=field_set.include_requirements.value,
-            script_handler=None,
-            script_module=None,
-            # The AWS-facing handler function is always lambdex_handler.handler, which is the
-            # wrapper injected by lambdex that manages invocation of the actual handler.
-            handler_log_message="lambdex_handler.handler",
+            # This doesn't matter (just needs to be fixed), but is the default name used by the AWS
+            # console when creating a Python lambda, so is as good as any
+            # https://docs.aws.amazon.com/lambda/latest/dg/python-handler.html
+            reexported_handler_module="lambda_function",
         ),
     )
 
@@ -62,5 +90,5 @@ def rules():
     return [
         *collect_rules(),
         UnionRule(PackageFieldSet, PythonAwsLambdaFieldSet),
-        *pex_from_targets.rules(),
+        *faas_rules(),
     ]
