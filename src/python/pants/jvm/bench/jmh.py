d8ad9b2ca5369729506da7d35fb6968d7acb56c7
Antonio Alonso Dominguez <alonso.domin@pm.me>
2023-05-25 17:50:07 +0200

    Add bytecode generation rule

--- src/python/pants/jvm/bench/jmh.py
+++ src/python/pants/jvm/bench/jmh.py
@@ -20,8 +20,17 @@ from pants.core.target_types import FileSourceField
 from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
 from pants.engine.addresses import Address, Addresses
 from pants.engine.env_vars import EnvironmentVars, EnvironmentVarsRequest
-from pants.engine.fs import Digest, DigestSubset, MergeDigests, PathGlobs, RemovePrefix, Snapshot
-from pants.engine.process import FallibleProcessResult, ProcessCacheScope
+from pants.engine.fs import (
+    CreateDigest,
+    Digest,
+    DigestSubset,
+    Directory,
+    MergeDigests,
+    PathGlobs,
+    RemovePrefix,
+    Snapshot,
+)
+from pants.engine.process import FallibleProcessResult, ProcessCacheScope, ProcessResult
 from pants.engine.rules import Get, MultiGet, collect_rules, rule
 from pants.engine.target import SourcesField, TransitiveTargets, TransitiveTargetsRequest
 from pants.engine.unions import UnionRule
@@ -111,13 +120,15 @@ async def setup_jmh_for_target(
         ),
     )
 
-    # jmh_generated = await Get(
-    #     GeneratedJmhSources,
-    #     GenerateJmhSourcesRequest(
-    #         request.field_set.address, jdk, jmh_classpath, classpath, files.snapshot.digest
-    #     ),
-    # )
-    input_digest = await Get(Digest, MergeDigests([*classpath.digests(), files.snapshot.digest]))
+    jmh_generated = await Get(
+        GeneratedJmhSources,
+        GenerateJmhSourcesRequest(
+            request.field_set.address, jdk, jmh_classpath, classpath, files.snapshot.digest
+        ),
+    )
+    input_digest = await Get(
+        Digest, MergeDigests([*classpath.digests(), files.snapshot.digest, jmh_generated.digest])
+    )
 
     toolcp_relpath = "__toolcp"
     extra_immutable_input_digests = {
@@ -193,52 +204,52 @@ async def run_jmh_benchmark(
     )
 
 
-# @rule(level=LogLevel.DEBUG)
-# async def generate_jmh_sources(request: GenerateJmhSourcesRequest, jmh: Jmh) -> GeneratedJmhSources:
-#     input_prefix = "__input"
-#     input_digest = await Get(
-#         Digest, MergeDigests([*request.classpath.digests(), request.extra_files])
-#     )
+@rule(level=LogLevel.DEBUG)
+async def generate_jmh_sources(request: GenerateJmhSourcesRequest, jmh: Jmh) -> GeneratedJmhSources:
+    input_prefix = "__input"
+    input_digest = await Get(
+        Digest, MergeDigests([*request.classpath.digests(), request.extra_files])
+    )
 
-#     toolcp_relpath = "__toolcp"
-#     extra_immutable_input_digests = {
-#         toolcp_relpath: request.jmh_classpath.digest,
-#         input_prefix: input_digest,
-#     }
+    toolcp_relpath = "__toolcp"
+    extra_immutable_input_digests = {
+        toolcp_relpath: request.jmh_classpath.digest,
+        input_prefix: input_digest,
+    }
 
-#     sources_output_dir = os.path.join("__gen", "sources")
-#     files_output_dir = os.path.join("__gen", "files")
-#     empty_dirs_digest = await Get(
-#         Digest, CreateDigest([Directory(sources_output_dir), Directory(files_output_dir)])
-#     )
+    sources_output_dir = os.path.join("__gen", "sources")
+    files_output_dir = os.path.join("__gen", "files")
+    empty_dirs_digest = await Get(
+        Digest, CreateDigest([Directory(sources_output_dir), Directory(files_output_dir)])
+    )
 
-#     process_result = await Get(
-#         ProcessResult,
-#         JvmProcess(
-#             jdk=request.jdk,
-#             classpath_entries=[
-#                 *request.classpath.args(),
-#                 *request.jmh_classpath.classpath_entries(toolcp_relpath),
-#             ],
-#             argv=[
-#                 "org.openjdk.jmh.generators.bytecode.JmhBytecodeGenerator",
-#                 input_prefix,
-#                 sources_output_dir,
-#                 files_output_dir,
-#                 jmh.generator_type.value,
-#             ],
-#             input_digest=empty_dirs_digest,
-#             extra_jvm_options=jmh.jvm_options,
-#             extra_immutable_input_digests=extra_immutable_input_digests,
-#             output_directories=(sources_output_dir, files_output_dir),
-#             description=f"Generate JMH sources for {request.address}",
-#             level=LogLevel.DEBUG,
-#             use_nailgun=False,
-#         ),
-#     )
+    process_result = await Get(
+        ProcessResult,
+        JvmProcess(
+            jdk=request.jdk,
+            classpath_entries=[
+                *request.classpath.args(),
+                *request.jmh_classpath.classpath_entries(toolcp_relpath),
+            ],
+            argv=[
+                "org.openjdk.jmh.generators.bytecode.JmhBytecodeGenerator",
+                input_prefix,
+                sources_output_dir,
+                files_output_dir,
+                jmh.generator_type.value,
+            ],
+            input_digest=empty_dirs_digest,
+            extra_jvm_options=jmh.jvm_options,
+            extra_immutable_input_digests=extra_immutable_input_digests,
+            output_directories=(sources_output_dir, files_output_dir),
+            description=f"Generate JMH sources for {request.address}",
+            level=LogLevel.DEBUG,
+            use_nailgun=False,
+        ),
+    )
 
-#     generated_digest = await Get(Digest, RemovePrefix(process_result.output_digest, "__gen"))
-#     return GeneratedJmhSources(generated_digest)
+    generated_digest = await Get(Digest, RemovePrefix(process_result.output_digest, "__gen"))
+    return GeneratedJmhSources(generated_digest)
 
 
 @rule
