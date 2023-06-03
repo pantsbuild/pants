86ad0ef3e675053a1800d0bcb4c1dc986be00c19
Tom Dyas <tom.dyas@gmail.com>
2023-06-03 02:20:53 +0200

    store stdout/stderr output from tests as bytes (#19184)
    ## Problem
    
    Pants expects test output to be in UTF-8 even though this assumption is not a valid to make given that test output comes from process invocations. As described in https://github.com/pantsbuild/pants/issues/19121, this causes a `UnicodeDecodeError` under the conditions mentioned there.
    
    ## Solution
    
    Instead of storing a `str`, just store the binary output for stdout and stderr from the underlying `Process` to avoid string encoding issues as much as possible. When creating a message for output, only then decode the stings as UTF-8 and replace invalid characters.
    
    The `stdout` and `stderr` attributes are renamed to `stdout_bytes` and `stderr_bytes`, respectively, Introduce `stdout` and `stderr` properties so that external plugins get a deprecation warning.
    
    Fixes https://github.com/pantsbuild/pants/issues/19121.

--- src/python/pants/core/goals/test.py
+++ src/python/pants/core/goals/test.py
@@ -11,6 +11,7 @@ from enum import Enum
 from pathlib import PurePath
 from typing import Any, ClassVar, Iterable, Optional, Sequence, TypeVar, cast
 
+from pants.base.deprecated import deprecated
 from pants.core.goals.multi_tool_goal_helper import SkippableSubsystem
 from pants.core.goals.package import BuiltPackage, EnvironmentAwarePackageRequest, PackageFieldSet
 from pants.core.subsystems.debug_adapter import DebugAdapterSubsystem
@@ -64,7 +65,7 @@ from pants.option.option_types import BoolOption, EnumOption, IntOption, StrList
 from pants.util.collections import partition_sequentially
 from pants.util.docutil import bin_name
 from pants.util.logging import LogLevel
-from pants.util.memo import memoized
+from pants.util.memo import memoized, memoized_property
 from pants.util.meta import classproperty
 from pants.util.strutil import help_text, softwrap
 
@@ -76,9 +77,9 @@ class TestResult(EngineAwareReturnType):
     # A None exit_code indicates a backend that performs its own test discovery/selection
     # (rather than delegating that to the underlying test tool), and discovered no tests.
     exit_code: int | None
-    stdout: str
+    stdout_bytes: bytes
     stdout_digest: FileDigest
-    stderr: str
+    stderr_bytes: bytes
     stderr_digest: FileDigest
     addresses: tuple[Address, ...]
     output_setting: ShowOutput
@@ -105,8 +106,8 @@ class TestResult(EngineAwareReturnType):
         """Used when we do test discovery ourselves, and we didn't find any."""
         return TestResult(
             exit_code=None,
-            stdout="",
-            stderr="",
+            stdout_bytes=b"",
+            stderr_bytes=b"",
             stdout_digest=EMPTY_FILE_DIGEST,
             stderr_digest=EMPTY_FILE_DIGEST,
             addresses=(address,),
@@ -121,8 +122,8 @@ class TestResult(EngineAwareReturnType):
         """Used when we do test discovery ourselves, and we didn't find any."""
         return TestResult(
             exit_code=None,
-            stdout="",
-            stderr="",
+            stdout_bytes=b"",
+            stderr_bytes=b"",
             stdout_digest=EMPTY_FILE_DIGEST,
             stderr_digest=EMPTY_FILE_DIGEST,
             addresses=tuple(field_set.address for field_set in batch.elements),
@@ -144,9 +145,9 @@ class TestResult(EngineAwareReturnType):
     ) -> TestResult:
         return TestResult(
             exit_code=process_result.exit_code,
-            stdout=process_result.stdout.decode(),
+            stdout_bytes=process_result.stdout,
             stdout_digest=process_result.stdout_digest,
-            stderr=process_result.stderr.decode(),
+            stderr_bytes=process_result.stderr,
             stderr_digest=process_result.stderr_digest,
             addresses=(address,),
             output_setting=output_setting,
@@ -170,9 +171,9 @@ class TestResult(EngineAwareReturnType):
     ) -> TestResult:
         return TestResult(
             exit_code=process_result.exit_code,
-            stdout=process_result.stdout.decode(),
+            stdout_bytes=process_result.stdout,
             stdout_digest=process_result.stdout_digest,
-            stderr=process_result.stderr.decode(),
+            stderr_bytes=process_result.stderr,
             stderr_digest=process_result.stderr_digest,
             addresses=tuple(field_set.address for field_set in batch.elements),
             output_setting=output_setting,
@@ -183,6 +184,20 @@ class TestResult(EngineAwareReturnType):
             log_extra_output=log_extra_output,
             partition_description=batch.partition_metadata.description,
         )
+
+    @memoized_property
+    @deprecated(
+        removal_version="2.19.0.dev0", hint="Use `TestResult.stdout_bytes` instead of `stdout`."
+    )
+    def stdout(self) -> str:
+        return self.stdout_bytes.decode(errors="replace")
+
+    @memoized_property
+    @deprecated(
+        removal_version="2.19.0.dev0", hint="Use `TestResult.stderr_bytes` instead of `stderr`."
+    )
+    def stderr(self) -> str:
+        return self.stderr_bytes.decode(errors="replace")
 
     @property
     def description(self) -> str:
@@ -236,10 +251,10 @@ class TestResult(EngineAwareReturnType):
         ):
             return message
         output = ""
-        if self.stdout:
-            output += f"\n{self.stdout}"
-        if self.stderr:
-            output += f"\n{self.stderr}"
+        if self.stdout_bytes:
+            output += f"\n{self.stdout_bytes.decode(errors='replace')}"
+        if self.stderr_bytes:
+            output += f"\n{self.stderr_bytes.decode(errors='replace')}"
         if output:
             output = f"{output.rstrip()}\n\n"
         return f"{message}{output}"
