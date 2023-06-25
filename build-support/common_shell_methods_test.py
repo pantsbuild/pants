8f04462d57df00d5e096bdc9edb0dc76b86bc60c
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-06-25 02:59:00 -0400

    make create_test_script factory fixture

--- build-support/common_shell_methods_test.py
+++ build-support/common_shell_methods_test.py
@@ -8,27 +8,44 @@ import tempfile
 from collections.abc import Iterator
 from pathlib import Path
 from textwrap import dedent
+from typing import Callable
 
 import pytest
 
 
 @pytest.fixture
-def script_dir() -> Iterator[Path]:
-    common_script_path = Path(__file__).parent / 'common.sh'
+def common_script_dir() -> Iterator[Path]:
+    # common.sh is located in the same directory as this file due to a resources() dependency.
+    common_script_path = Path(__file__).parent / "common.sh"
     with tempfile.TemporaryDirectory() as td:
         dir_path = Path(td)
-        shutil.copyfile(common_script_path, dir_path / 'common.sh')
+        shutil.copyfile(common_script_path, dir_path / "common.sh")
         yield dir_path
 
-def test_log(script_dir: Path):
-    test_script_path = script_dir / 'log.sh'
-    test_script_path.write_text(dedent(f"""\
-    #!/usr/bin/env bash
 
-    source {script_dir}/common.sh
+@pytest.fixture
+def create_test_script(common_script_dir: Path) -> Callable[[str, str], Path]:
+    def _make_test_script(script_name: str, script_contents: str) -> Path:
+        test_script_path = common_script_dir / script_name
+        test_script_path.write_text(
+            dedent(
+                f"""\
+        #!/usr/bin/env bash
 
-    log hey
-    """))
-    test_script_path.chmod(test_script_path.stat().st_mode | stat.S_IXUSR)
+        source {common_script_dir}/common.sh
 
-    assert subprocess.run([test_script_path], capture_output=True, check=True).stderr == b'hey\n'
+        {script_contents}
+        """
+            )
+        )
+        test_script_path.chmod(test_script_path.stat().st_mode | stat.S_IXUSR)
+
+        return test_script_path
+
+    return _make_test_script
+
+
+def test_log(create_test_script):
+    test_script_path = create_test_script("log.sh", "log hey")
+
+    assert subprocess.run([test_script_path], capture_output=True, check=True).stderr == b"hey\n"
