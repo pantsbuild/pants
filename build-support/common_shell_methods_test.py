49f2b4baa9e2ff656f2c58c19a467426d2608977
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-06-25 02:49:44 -0400

    add types to test script

--- build-support/common_shell_methods_test.py
+++ build-support/common_shell_methods_test.py
@@ -5,6 +5,7 @@ import shutil
 import stat
 import subprocess
 import tempfile
+from collections.abc import Iterator
 from pathlib import Path
 from textwrap import dedent
 
@@ -12,14 +13,14 @@ import pytest
 
 
 @pytest.fixture
-def script_dir():
+def script_dir() -> Iterator[Path]:
     common_script_path = Path(__file__).parent / 'common.sh'
     with tempfile.TemporaryDirectory() as td:
         dir_path = Path(td)
         shutil.copyfile(common_script_path, dir_path / 'common.sh')
         yield dir_path
 
-def test_log(script_dir):
+def test_log(script_dir: Path):
     test_script_path = script_dir / 'log.sh'
     test_script_path.write_text(dedent(f"""\
     #!/usr/bin/env bash
