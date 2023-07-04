a56c9ab4d45e4d27c4abc3ec1cecb087e92a40cc
Idan Attias <idanat@google.com>
2023-07-04 15:14:47 +0300

    fix: requirement was tuple instead of str

--- src/python/pants/init/options_initializer.py
+++ src/python/pants/init/options_initializer.py
@@ -94,7 +94,7 @@ def _collect_backends_requirements(backends: List[str]) -> List[str]:
         )
         if requirements_txt_file_path.exists():
             content = requirements_txt_file_path.read_text()
-            backend_package_requirements = load_requirements(content)
+            backend_package_requirements = [r[0] for r in load_requirements(content)]
             requirements.extend(backend_package_requirements)
 
     return requirements
