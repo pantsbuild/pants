2dfd8da30dd2b0e610b004a0ad2550485e67b8df
Idan Attias <idanat@google.com>
2023-07-05 10:49:28 +0300

    update in-repo plugins docs

--- src/python/pants/init/plugin_resolver.py
+++ src/python/pants/init/plugin_resolver.py
@@ -7,7 +7,7 @@ import logging
 import site
 import sys
 from dataclasses import dataclass
-from typing import List, Optional, cast
+from typing import Optional, cast, Iterable
 
 from pkg_resources import Requirement, WorkingSet
 from pkg_resources import working_set as global_working_set
