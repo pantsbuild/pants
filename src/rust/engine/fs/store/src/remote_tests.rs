0778777961fdf62216c8b16e12bd3747b489cc56
Huon Wilson <huon@exoflare.io>
2023-07-07 09:06:54 +1000

    Remove redundant tests

--- src/rust/engine/fs/store/src/remote_tests.rs
+++ src/rust/engine/fs/store/src/remote_tests.rs
@@ -7,12 +7,12 @@ use bytes::Bytes;
 use grpc_util::tls;
 use hashing::Digest;
 use mock::StubCAS;
-use testutil::data::{TestData, TestDirectory};
+use testutil::data::TestData;
 use tokio::io::{AsyncReadExt, AsyncSeekExt};
 use workunit_store::WorkunitStore;
 
 use crate::remote::{ByteStore, RemoteOptions};
-use crate::tests::{big_file_bytes, big_file_fingerprint, new_cas};
+use crate::tests::new_cas;
 use crate::MEGABYTES;
 
 #[tokio::test]
@@ -71,32 +71,6 @@ async fn missing_file() {
 }
 
 #[tokio::test]
-async fn load_directory() {
-  let cas = new_cas(10);
-  let testdir = TestDirectory::containing_roland();
-
-  assert_eq!(
-    load_directory_proto_bytes(&new_byte_store(&cas), testdir.digest()).await,
-    Ok(Some(testdir.bytes()))
-  );
-}
-
-#[tokio::test]
-async fn missing_directory() {
-  let _ = WorkunitStore::setup_for_tests();
-  let cas = StubCAS::empty();
-
-  assert_eq!(
-    load_directory_proto_bytes(
-      &new_byte_store(&cas),
-      TestDirectory::containing_roland().digest()
-    )
-    .await,
-    Ok(None)
-  );
-}
-
-#[tokio::test]
 async fn load_file_grpc_error() {
   let _ = WorkunitStore::setup_for_tests();
   let cas = StubCAS::cas_always_errors();
@@ -107,67 +81,6 @@ async fn load_file_grpc_error() {
   assert!(
     error.contains("StubCAS is configured to always fail"),
     "Bad error message, got: {error}"
-  )
-}
-
-#[tokio::test]
-async fn load_directory_grpc_error() {
-  let _ = WorkunitStore::setup_for_tests();
-  let cas = StubCAS::cas_always_errors();
-
-  let error = load_directory_proto_bytes(
-    &new_byte_store(&cas),
-    TestDirectory::containing_roland().digest(),
-  )
-  .await
-  .expect_err("Want error");
-  assert!(
-    error.contains("StubCAS is configured to always fail"),
-    "Bad error message, got: {error}"
-  )
-}
-
-#[tokio::test]
-async fn fetch_less_than_one_chunk() {
-  let testdata = TestData::roland();
-  let cas = new_cas(testdata.bytes().len() + 1);
-
-  assert_eq!(
-    load_file_bytes(&new_byte_store(&cas), testdata.digest()).await,
-    Ok(Some(testdata.bytes()))
-  )
-}
-
-#[tokio::test]
-async fn fetch_exactly_one_chunk() {
-  let testdata = TestData::roland();
-  let cas = new_cas(testdata.bytes().len());
-
-  assert_eq!(
-    load_file_bytes(&new_byte_store(&cas), testdata.digest()).await,
-    Ok(Some(testdata.bytes()))
-  )
-}
-
-#[tokio::test]
-async fn fetch_multiple_chunks_exact() {
-  let testdata = TestData::roland();
-  let cas = new_cas(1);
-
-  assert_eq!(
-    load_file_bytes(&new_byte_store(&cas), testdata.digest()).await,
-    Ok(Some(testdata.bytes()))
-  )
-}
-
-#[tokio::test]
-async fn fetch_multiple_chunks_nonfactor() {
-  let testdata = TestData::roland();
-  let cas = new_cas(9);
-
-  assert_eq!(
-    load_file_bytes(&new_byte_store(&cas), testdata.digest()).await,
-    Ok(Some(testdata.bytes()))
   )
 }
 
@@ -204,62 +117,6 @@ fn remote_options(
 }
 
 #[tokio::test]
-async fn write_file_multiple_chunks() {
-  let _ = WorkunitStore::setup_for_tests();
-  let cas = StubCAS::empty();
-
-  let store = ByteStore::new(
-    None,
-    remote_options(
-      cas.address(),
-      10 * 1024,
-      0, // disable batch API, force streaming API
-    ),
-  )
-  .unwrap();
-
-  let all_the_henries = big_file_bytes();
-
-  let fingerprint = big_file_fingerprint();
-
-  assert_eq!(store.store_bytes(all_the_henries.clone()).await, Ok(()));
-
-  let blobs = cas.blobs.lock();
-  assert_eq!(blobs.get(&fingerprint), Some(&all_the_henries));
-
-  let write_message_sizes = cas.write_message_sizes.lock();
-  assert_eq!(
-    write_message_sizes.len(),
-    98,
-    "Wrong number of chunks uploaded"
-  );
-  for size in write_message_sizes.iter() {
-    assert!(
-      size <= &(10 * 1024),
-      "Size {} should have been <= {}",
-      size,
-      10 * 1024
-    );
-  }
-}
-
-#[tokio::test]
-async fn write_empty_file() {
-  let _ = WorkunitStore::setup_for_tests();
-  let empty_file = TestData::empty();
-  let cas = StubCAS::empty();
-
-  let store = new_byte_store(&cas);
-  assert_eq!(store.store_bytes(empty_file.bytes()).await, Ok(()));
-
-  let blobs = cas.blobs.lock();
-  assert_eq!(
-    blobs.get(&empty_file.fingerprint()),
-    Some(&empty_file.bytes())
-  );
-}
-
-#[tokio::test]
 async fn write_file_errors() {
   let _ = WorkunitStore::setup_for_tests();
   let cas = StubCAS::cas_always_errors();
@@ -271,28 +128,6 @@ async fn write_file_errors() {
     .expect_err("Want error");
   assert!(
     error.contains("StubCAS is configured to always fail"),
-    "Bad error message, got: {error}"
-  );
-}
-
-#[tokio::test]
-async fn write_connection_error() {
-  let _ = WorkunitStore::setup_for_tests();
-  let store = ByteStore::new(
-    None,
-    remote_options(
-      "http://doesnotexist.example".to_owned(),
-      10 * 1024 * 1024,
-      super::tests::STORE_BATCH_API_SIZE_LIMIT,
-    ),
-  )
-  .unwrap();
-  let error = store
-    .store_bytes(TestData::roland().bytes())
-    .await
-    .expect_err("Want error");
-  assert!(
-    error.contains("Unavailable: \"error trying to connect: dns error"),
     "Bad error message, got: {error}"
   );
 }
@@ -357,17 +192,6 @@ fn new_byte_store(cas: &StubCAS) -> ByteStore {
   .unwrap()
 }
 
-pub async fn load_file_bytes(store: &ByteStore, digest: Digest) -> Result<Option<Bytes>, String> {
-  load_bytes(store, digest).await
-}
-
-pub async fn load_directory_proto_bytes(
-  store: &ByteStore,
-  digest: Digest,
-) -> Result<Option<Bytes>, String> {
-  load_bytes(store, digest).await
-}
-
-async fn load_bytes(store: &ByteStore, digest: Digest) -> Result<Option<Bytes>, String> {
+async fn load_file_bytes(store: &ByteStore, digest: Digest) -> Result<Option<Bytes>, String> {
   store.load_bytes(digest).await
 }
