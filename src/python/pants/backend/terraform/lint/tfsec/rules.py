31cc6cf48733c302d031f8d0139d9a54a1be99e2
Daniel Goldman <danielgoldman4@gmail.com>
2023-07-09 22:23:54 -0400

    add config discovery

--- src/python/pants/backend/terraform/lint/tfsec/rules.py
+++ src/python/pants/backend/terraform/lint/tfsec/rules.py
@@ -1,7 +1,8 @@
 # Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
-from pants.backend.terraform.lint.tfsec.tfsec import TfSec, TfSecRequest
+from pants.backend.terraform.lint.tfsec.tfsec import TFSec, TfSecRequest
 from pants.core.goals.lint import LintResult
+from pants.core.util_rules.config_files import ConfigFiles, ConfigFilesRequest
 from pants.core.util_rules.external_tool import DownloadedExternalTool, ExternalToolRequest
 from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
 from pants.engine.fs import Digest, MergeDigests
@@ -12,11 +13,12 @@ from pants.util.logging import LogLevel
 
 
 @rule
-async def run_tfsec(request: TfSecRequest.Batch, tfsec: TfSec, platform: Platform) -> LintResult:
-    get_tfsec = Get(DownloadedExternalTool, ExternalToolRequest, tfsec.get_request(platform))
-    sources_request = Get(SourceFiles, SourceFilesRequest(fs.sources for fs in request.elements))
-
-    downloaded_tfsec, sources = await MultiGet(get_tfsec, sources_request)
+async def run_tfsec(request: TfSecRequest.Batch, tfsec: TFSec, platform: Platform) -> LintResult:
+    downloaded_tfsec, sources, config_file = await MultiGet(
+        Get(DownloadedExternalTool, ExternalToolRequest, tfsec.get_request(platform)),
+        Get(SourceFiles, SourceFilesRequest(fs.sources for fs in request.elements)),
+        Get(ConfigFiles, ConfigFilesRequest, tfsec.config_request()),
+    )
 
     input_digest = await Get(
         Digest,
@@ -24,15 +26,21 @@ async def run_tfsec(request: TfSecRequest.Batch, tfsec: TfSec, platform: Platfor
             (
                 downloaded_tfsec.digest,
                 sources.snapshot.digest,
+                config_file.snapshot.digest,
             )
         ),
     )
+
+    computed_args = []
+    if tfsec.config:
+        computed_args = [f"--config-file={tfsec.config}"]
 
     process_result = await Get(
         FallibleProcessResult,
         Process(
             argv=[
                 downloaded_tfsec.exe,
+                *computed_args,
                 *tfsec.args,
             ],
             input_digest=input_digest,
