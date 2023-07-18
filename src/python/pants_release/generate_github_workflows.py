9f9a270d9f5d8092144f2146e0ac2f9bd35bf296
Joshua <joshdcannon@gmail.com>
2023-07-18 14:49:51 -0500

    slashstar

--- src/python/pants_release/generate_github_workflows.py
+++ src/python/pants_release/generate_github_workflows.py
@@ -1201,7 +1201,7 @@ def release_jobs_and_inputs() -> tuple[Jobs, dict[str, Any]]:
                     },
                     "run": dedent(
                         f"""\
-                        gh release upload {gha_expr("needs.release_info.outputs.build-ref") } {pypi_release_dir}
+                        gh release upload {gha_expr("needs.release_info.outputs.build-ref") } {pypi_release_dir}/*
                         gh release edit {gha_expr("needs.release_info.outputs.build-ref") } --draft=false
                         """
                     ),
