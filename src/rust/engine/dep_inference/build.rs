7ac75c2cf0afb5556670d7ac2ae9f6ad385ea025
Josh Cannon <joshdcannon@gmail.com>
2023-09-08 16:21:29 -0500

    `with contextlib.suppress(ImportError)` weakens imports (Cherry-pick of #19293) (#19789)
    fixes #17691 and fixes #19751
    
    also:
    - fixes the case of nested weakenings
    - bumps tree-sitter-python to an unreleased version (last release was a
    year ago)
    - rebuilds the generation of tree-sitter node types to support multiple
    symbols for a name
    
    ---------
    
    Co-authored-by: Daniel Goldman <danielgoldman4@gmail.com>

--- src/rust/engine/dep_inference/build.rs
+++ src/rust/engine/dep_inference/build.rs
@@ -25,10 +25,18 @@
 // Arc<Mutex> can be more clear than needing to grok Orderings:
 #![allow(clippy::mutex_atomic)]
 
+use std::collections::HashMap;
 use std::env;
 use std::{collections::HashSet, io::Write, path::Path};
 
-fn gen_constants_file(out_dir: &Path) {
+/// The tree-sitter interfaces don't have nice constants that allow us to reference their magic numbers by name.
+/// We generate those constants here.
+/// Tree-sitter grammars don't have to give symbols unique names (in `ts_symbol_names`),
+/// and there can be multiple symbols mapped to the same name.
+/// For example, they might map both `block` and `_match_block` to "block" because one of those in internal
+/// For most names, there will only be 1 symbol; for those, we create a const u16 for convenience.
+/// For the names with multiple symbols, we generate a const array (hashmaps would have been nice, but I couldn't figure out how to make them const)
+fn gen_constants_file(language: &tree_sitter::Language, out_dir: &Path) {
   let mut file = std::fs::File::create(out_dir.join("constants.rs")).unwrap();
 
   file
@@ -45,22 +53,43 @@ impl KindID {
     )
     .unwrap();
 
-  let python_lang = tree_sitter_python::language();
-  let mut kinds_seen = HashSet::new();
+  let mut kinds_to_ids: HashMap<String, HashSet<u16>> = HashMap::new();
 
-  for id in (0_u16..(python_lang.node_kind_count() as u16)).chain(
-    [python_lang.id_for_node_kind("ERROR", true)]
-      .iter()
-      .cloned(),
-  ) {
-    if python_lang.node_kind_is_named(id) {
-      let kind = python_lang.node_kind_for_id(id).unwrap().to_uppercase();
-      if kinds_seen.insert(kind.clone()) {
-        file
-          .write_all(format!("  pub const {kind}: u16 = {id};\n").as_bytes())
-          .unwrap();
-      }
+  // Collect the mapping of name->symbols
+  for id in (0_u16..(language.node_kind_count() as u16))
+    .chain([language.id_for_node_kind("ERROR", true)].iter().cloned())
+  {
+    if language.node_kind_is_named(id) {
+      let kind = language.node_kind_for_id(id).unwrap().to_uppercase();
+      kinds_to_ids
+        .entry(kind)
+        .or_insert_with(HashSet::new)
+        .insert(id);
     }
+  }
+
+  // Codegen for each name->symbol mapping
+  for (kind, ids) in kinds_to_ids {
+    let text = match ids.len() {
+      1 => {
+        let single = ids.iter().next().unwrap();
+        format!("  pub const {kind}: u16 = {single};\n")
+      }
+      _ => {
+        let items: String = ids
+          .iter()
+          .map(|id| id.to_string())
+          .collect::<Vec<String>>()
+          .join(", ");
+        format!(
+          "  pub const {}: [u16; {}]  = [{}];\n",
+          kind,
+          ids.len(),
+          items
+        )
+      }
+    };
+    file.write_all(text.as_bytes()).unwrap();
   }
 
   file.write_all(b"}\n").unwrap();
@@ -132,7 +161,7 @@ pub trait Visitor {
 fn main() {
   let out_dir = env::var_os("OUT_DIR").unwrap();
   let out_dir = Path::new(&out_dir);
-  gen_constants_file(out_dir);
+  gen_constants_file(&tree_sitter_python::language(), out_dir);
   gen_visitor_file(out_dir);
   println!("cargo:rerun-if-env-changed=build.rs");
 }
