df243a30f47c2a1d784f98889cf356d6819214b0
Marius Räsener <marius.raesener@fincite.de>
2023-09-12 11:28:52 +0200

    add force-exclude to ruff args

--- src/python/pants/backend/python/lint/ruff/rules.py
+++ src/python/pants/backend/python/lint/ruff/rules.py
@@ -64,7 +64,7 @@ async def run_ruff(
     )
 
     conf_args = [f"--config={ruff.config}"] if ruff.config else []
-    initial_args = ("--fix",) if request.is_fix else ()
+    initial_args = ("--force-exclude","--fix",) if request.is_fix else ("--force-exclude",)
 
     result = await Get(
         FallibleProcessResult,
