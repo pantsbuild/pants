2c7d99841e536652a1409bb4b130e3491f721af8
Stu Hood <stuhood@gmail.com>
2023-09-15 09:43:16 -0700

    Add fingerprint mismatch test.

--- src/rust/engine/client/src/client_tests.rs
+++ src/rust/engine/client/src/client_tests.rs
@@ -1,8 +1,10 @@
 // Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+use std::collections::HashMap;
 use std::time::SystemTime;
 
+use options::{Args, Env, OptionParser};
 use pantsd::pantsd_testing::launch_pantsd;
 
 use crate::execute_command;
@@ -21,4 +23,29 @@ async fn test_client() {
   .await
   .unwrap();
   assert_eq!(0, exit_code)
+}
+
+#[tokio::test]
+async fn test_client_fingerprint_mismatch() {
+  // Launch `pantsd` with one fingerprint.
+  let (build_root, _options_parser, tmpdir) = launch_pantsd();
+
+  // Then connect with a different set of options (but with a matching `pants_subprocessdir`, so
+  // that we find the relevant `.pids` directory).
+  let options_parser = OptionParser::new(
+    Env::new(HashMap::new()),
+    Args::new(vec![format!(
+      "--pants-subprocessdir={}",
+      tmpdir.path().display()
+    )]),
+  )
+  .unwrap();
+  let error = pantsd::find_pantsd(&build_root, &options_parser)
+    .err()
+    .unwrap();
+
+  assert!(
+    error.contains("Fingerprint mismatched:"),
+    "Error was: {error}"
+  )
 }
