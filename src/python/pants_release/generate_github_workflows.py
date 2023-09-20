3f494e2e9d4c68bbce790b7c331bc496c9521aad
Joshua <joshdcannon@gmail.com>
2023-09-20 13:51:40 -0500

    OK wanna play?

--- src/python/pants_release/generate_github_workflows.py
+++ src/python/pants_release/generate_github_workflows.py
@@ -442,8 +442,8 @@ class Helper:
         return [
             install_protoc(),  # for `prost` crate
             {
-                "name": "Ensure default rustup profile",
-                "run": "rustup set profile default",
+                "name": "joshs",
+                "run": "cat  ~/.rustup/settings.toml && rustup set profile default && cat  ~/.rustup/settings.toml"
             },
             {
                 "name": "Cache Rust toolchain",
