bea06022ec6aa7941ba3ec1930517226b0e91301
Stu Hood <stuhood@gmail.com>
2023-09-21 17:29:26 -0700

    Update tests for removal of `python36` from Github Actions hostedtoolcache (#19901)
    Github Actions recently removed `python3.6` from `/opt/hostedtoolcache`,
    which means that our [`pantsbuild/actions/expose-pythons`
    action](https://github.com/pantsbuild/actions/blob/main/expose-pythons/action.yml)
    does not expose it.
    
    This causes tests which require Python 3.6 to fail
    ([example](https://github.com/pantsbuild/pants/actions/runs/6265433640/job/17021049356?pr=19894))
    on `main` (and in branches).

--- src/python/pants/testutil/python_interpreter_selection.py
+++ src/python/pants/testutil/python_interpreter_selection.py
@@ -110,9 +110,9 @@ def skip_unless_python27_and_python36_present(func):
     return skip_unless_all_pythons_present(PY_27, PY_36)(func)
 
 
-def skip_unless_python36_and_python37_present(func):
-    """A test skip decorator that only runs a test method if python3.6 and python3.7 are present."""
-    return skip_unless_all_pythons_present(PY_36, PY_37)(func)
+def skip_unless_python38_and_python39_present(func):
+    """A test skip decorator that only runs a test method if python3.8 and python3.9 are present."""
+    return skip_unless_all_pythons_present(PY_38, PY_39)(func)
 
 
 def all_major_minor_python_versions(
