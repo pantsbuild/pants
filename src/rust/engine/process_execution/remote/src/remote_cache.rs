984bc759a712748454fc929de2b8b39eaf71f023
Huon Wilson <huon@exoflare.io>
2023-09-27 15:03:45 +1000

    TODO -> a note

--- src/rust/engine/process_execution/remote/src/remote_cache.rs
+++ src/rust/engine/process_execution/remote/src/remote_cache.rs
@@ -106,8 +106,10 @@ async fn choose_provider(
       remote_options,
     )?))
   } else if let Some(url) = address.strip_prefix("github-actions-cache+") {
-    // TODO: this is relying on python validating that it was set as
-    // `github-actions-cache+https://...`
+    // This is relying on python validating that it was set as `github-actions-cache+https://...` so
+    // incorrect values could easily slip through here and cause downstream confusion. We're
+    // intending to change the approach (https://github.com/pantsbuild/pants/issues/19902) so this
+    // is tolerable for now.
     Ok(Arc::new(base_opendal::Provider::github_actions_cache(
       url,
       "action-cache".to_owned(),
