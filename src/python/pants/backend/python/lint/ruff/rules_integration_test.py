068c8fa44c9325d1b96f3fc29368966470907da8
Krishnan Chandra <krishnan.chandra@gmail.com>
2023-10-24 19:23:11 -0400

    Add format results to test

--- src/python/pants/backend/python/lint/ruff/rules_integration_test.py
+++ src/python/pants/backend/python/lint/ruff/rules_integration_test.py
@@ -4,12 +4,13 @@
 from __future__ import annotations
 
 from pathlib import Path
+from pants.core.goals.fmt import FmtResult
 
 import pytest
 
 from pants.backend.python import target_types_rules
 from pants.backend.python.lint.ruff import skip_field
-from pants.backend.python.lint.ruff.rules import RuffFixRequest, RuffLintRequest
+from pants.backend.python.lint.ruff.rules import RuffFixRequest, RuffFormatRequest, RuffLintRequest
 from pants.backend.python.lint.ruff.rules import rules as ruff_rules
 from pants.backend.python.lint.ruff.subsystem import RuffFieldSet
 from pants.backend.python.lint.ruff.subsystem import rules as ruff_subsystem_rules
@@ -39,6 +40,7 @@ def rule_runner() -> RuleRunner:
             *target_types_rules.rules(),
             QueryRule(FixResult, [RuffFixRequest.Batch]),
             QueryRule(LintResult, [RuffLintRequest.Batch]),
+            QueryRule(FmtResult, [RuffFormatRequest.Batch]),
             QueryRule(SourceFiles, (SourceFilesRequest,)),
         ],
         target_types=[PythonSourcesGeneratorTarget],
@@ -50,7 +52,7 @@ def run_ruff(
     targets: list[Target],
     *,
     extra_args: list[str] | None = None,
-) -> tuple[FixResult, LintResult]:
+) -> tuple[FixResult, LintResult, FmtResult]:
     args = ["--backend-packages=pants.backend.python.lint.ruff", *(extra_args or ())]
     rule_runner.set_options(args, env_inherit={"PATH", "PYENV_ROOT", "HOME"})
 
@@ -79,8 +81,19 @@ def run_ruff(
             ),
         ],
     )
+    fmt_result = rule_runner.request(
+        FmtResult,
+        [
+            RuffFormatRequest.Batch(
+                "",
+                input_sources.snapshot.files,
+                partition_metadata=None,
+                snapshot=input_sources.snapshot,
+            )
+        ]
+    )
 
-    return fix_result, lint_result
+    return fix_result, lint_result, fmt_result
 
 
 @pytest.mark.platform_specific_behavior
@@ -91,7 +104,7 @@ def run_ruff(
 def test_passing(rule_runner: RuleRunner, major_minor_interpreter: str) -> None:
     rule_runner.write_files({"f.py": GOOD_FILE, "BUILD": "python_sources(name='t')"})
     tgt = rule_runner.get_target(Address("", target_name="t", relative_file_path="f.py"))
-    fix_result, lint_result = run_ruff(
+    fix_result, lint_result, fmt_result = run_ruff(
         rule_runner,
         [tgt],
         extra_args=[f"--python-interpreter-constraints=['=={major_minor_interpreter}.*']"],
@@ -106,7 +119,7 @@ def test_passing(rule_runner: RuleRunner, major_minor_interpreter: str) -> None:
 def test_failing(rule_runner: RuleRunner) -> None:
     rule_runner.write_files({"f.py": BAD_FILE, "BUILD": "python_sources(name='t')"})
     tgt = rule_runner.get_target(Address("", target_name="t", relative_file_path="f.py"))
-    fix_result, lint_result = run_ruff(rule_runner, [tgt])
+    fix_result, lint_result, fmt_result = run_ruff(rule_runner, [tgt])
     assert lint_result.exit_code == 1
     assert fix_result.stdout == "Found 1 error (1 fixed, 0 remaining).\n"
     assert fix_result.stderr == ""
@@ -126,7 +139,7 @@ def test_multiple_targets(rule_runner: RuleRunner) -> None:
         rule_runner.get_target(Address("", target_name="t", relative_file_path="good.py")),
         rule_runner.get_target(Address("", target_name="t", relative_file_path="bad.py")),
     ]
-    fix_result, lint_result = run_ruff(rule_runner, tgts)
+    fix_result, lint_result, fmt_result = run_ruff(rule_runner, tgts)
     assert lint_result.exit_code == 1
     assert fix_result.output == rule_runner.make_snapshot(
         {"good.py": GOOD_FILE, "bad.py": GOOD_FILE}
@@ -163,6 +176,6 @@ def test_config_file(
     rel_file_path = file_path.relative_to(*file_path.parts[:1]) if spec_path else file_path
     addr = Address(spec_path, relative_file_path=str(rel_file_path))
     tgt = rule_runner.get_target(addr)
-    fix_result, lint_result = run_ruff(rule_runner, [tgt], extra_args=extra_args)
+    fix_result, lint_result, fmt_result = run_ruff(rule_runner, [tgt], extra_args=extra_args)
     assert lint_result.exit_code == bool(should_change)
     assert fix_result.did_change is should_change
