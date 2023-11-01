291993a9217be315ab80fdede4b2e45785a70a5d
Gautham Nair <gautham.p.nair@gmail.com>
2023-11-01 06:22:43 -0400

    separate tool hydration from batch processing

--- src/python/pants/backend/code_quality_tool/lib.py
+++ src/python/pants/backend/code_quality_tool/lib.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 from dataclasses import dataclass
-from typing import ClassVar, Type
+from typing import ClassVar, Type, Mapping, Iterable
 
 from pants.core.goals.fix import FixResult
 from pants.core.goals.fmt import FmtFilesRequest
@@ -154,6 +154,179 @@ class FmtGoal(SupportedGoal):
         return request.files
 
 
+@dataclass(frozen=True)
+class HydratedCodeQualityToolRequest:
+    address: str
+
+
+@dataclass(frozen=True)
+class HydratedCodeQualityTool:
+    environment_name: EnvironmentName
+    run_field_set: RunFieldSet
+    runnable_target: Target
+    run_request: RunInSandboxRequest
+    merged_extras: ExtraSandboxContents
+    dependencies_digest: Digest
+    extra_env: Mapping[str, str]
+    digest: Digest
+    cmd_args: tuple[str, ...]
+
+
+@dataclass(frozen=True)
+class CodeQualityToolBatch:
+    tool: HydratedCodeQualityTool
+    sources_snapshot: Snapshot
+    output_files: tuple[str, ...]
+
+@rule
+async def process_files(batch: CodeQualityToolBatch) -> FallibleProcessResult:
+    tool = batch.tool
+
+    input_digest = await Get(
+        Digest, MergeDigests((tool.digest, batch.sources_snapshot.digest))
+    )
+
+    cmd_args = tool.cmd_args
+
+    append_only_caches = {
+        **tool.merged_extras.append_only_caches,
+    }
+
+    Process(
+        argv=tuple(tool.run_request.args + cmd_args + batch.sources_snapshot.files),
+        description="Running byotool",
+        input_digest=input_digest,
+        append_only_caches=append_only_caches,
+        immutable_input_digests=FrozenDict.frozen(tool.merged_extras.immutable_input_digests),
+        env=FrozenDict(tool.extra_env),
+        output_files=batch.output_files,
+    )
+
+    result = await Get(
+        FallibleProcessResult,
+        Process(
+            argv=tuple(tool.run_request.args + cmd_args + batch.sources_snapshot.files),
+            description="Running byotool",
+            input_digest=input_digest,
+            append_only_caches=append_only_caches,
+            immutable_input_digests=FrozenDict.frozen(tool.merged_extras.immutable_input_digests),
+            env=FrozenDict(tool.extra_env),
+            output_files=batch.output_files,
+        ))
+    return result
+
+
+@rule
+async def hydrate_code_quality_tool(request: HydratedCodeQualityToolRequest) -> HydratedCodeQualityTool:
+    linter_address_str = request.address
+    linter_address = await Get(
+        Address,
+        AddressInput,
+        AddressInput.parse(linter_address_str, description_of_origin=f"ByoTool linter target"),
+    )
+
+    addresses = Addresses((linter_address,))
+    addresses.expect_single()
+
+    linter_targets = await Get(Targets, Addresses, addresses)
+
+    linter = linter_targets[0]
+
+    runnable_address_str = linter[CodeQualityToolRunnableField].value
+    runnable_address = await Get(
+        Address,
+        AddressInput,
+        AddressInput.parse(
+            runnable_address_str,
+            # Need to add a relative_to=addresses
+            description_of_origin=f"ByoTool runnable target",
+        ),
+    )
+
+    addresses = Addresses((runnable_address,))
+    addresses.expect_single()
+
+    runnable_targets = await Get(Targets, Addresses, addresses)
+
+    target = runnable_targets[0]
+
+
+    field_sets = await Get(
+        FieldSetsPerTarget, FieldSetsPerTargetRequest(RunFieldSet, runnable_targets)
+    )
+
+    environment_name = await Get(
+        EnvironmentName, EnvironmentNameRequest, EnvironmentNameRequest.from_target(target)
+    )
+
+    run_field_set: RunFieldSet = field_sets.field_sets[0]
+
+    run_request = await Get(
+        RunInSandboxRequest, {environment_name: EnvironmentName, run_field_set: RunFieldSet}
+    )
+
+    execution_environment = await Get(
+        ResolvedExecutionDependencies,
+        ResolveExecutionDependenciesRequest(
+            target.address,
+            execution_dependencies=linter[CodeQualityToolExecutionDependenciesField].value,
+            runnable_dependencies=None,
+        ),
+    )
+
+    dependencies_digest = execution_environment.digest
+    runnable_dependencies = execution_environment.runnable_dependencies
+
+    extra_env: dict[str, str] = dict(run_request.extra_env or {})
+    extra_path = extra_env.pop("PATH", None)
+
+    extra_sandbox_contents = []
+
+    extra_sandbox_contents.append(
+        ExtraSandboxContents(
+            EMPTY_DIGEST,
+            extra_path,
+            run_request.immutable_input_digests or FrozenDict(),
+            run_request.append_only_caches or FrozenDict(),
+            run_request.extra_env or FrozenDict(),
+        )
+    )
+
+    if runnable_dependencies:
+        extra_sandbox_contents.append(
+            ExtraSandboxContents(
+                EMPTY_DIGEST,
+                f"{{chroot}}/{runnable_dependencies.path_component}",
+                runnable_dependencies.immutable_input_digests,
+                runnable_dependencies.append_only_caches,
+                runnable_dependencies.extra_env,
+            )
+        )
+
+    merged_extras = await Get(
+        ExtraSandboxContents, MergeExtraSandboxContents(tuple(extra_sandbox_contents))
+    )
+    extra_env = dict(merged_extras.extra_env)
+    if merged_extras.path:
+        extra_env["PATH"] = merged_extras.path
+
+    digest = await Get(Digest, MergeDigests((dependencies_digest, run_request.digest)))
+
+    cmd_args = linter[CodeQualityToolArgumentsField].value or ()
+
+    return HydratedCodeQualityTool(
+        run_field_set=run_field_set,
+        environment_name=environment_name,
+        runnable_target=target,
+        run_request=run_request,
+        merged_extras=merged_extras,
+        dependencies_digest=dependencies_digest,
+        extra_env=FrozenDict(extra_env),
+        digest=digest,
+        cmd_args=tuple(cmd_args),
+    )
+
+
 @dataclass
 class CodeQualityToolRuleBuilder:
     goal: str
@@ -233,108 +406,17 @@ class CodeQualityToolRuleBuilder:
             addresses = Addresses((linter_address,))
             addresses.expect_single()
 
-            linter_targets = await Get(Targets, Addresses, addresses)
+            code_quality_tool = await Get(HydratedCodeQualityTool,
+                      HydratedCodeQualityToolRequest(address=subsystem.linter))
 
-            linter = linter_targets[0]
+            proc_result = await Get(
+                FallibleProcessResult,
+                CodeQualityToolBatch(
+                    tool=code_quality_tool,
+                    sources_snapshot=sources_snapshot,
+                    output_files=goal.output_files_to_read(request),
+                ))
 
-            runnable_address_str = linter[CodeQualityToolRunnableField].value
-            runnable_address = await Get(
-                Address,
-                AddressInput,
-                AddressInput.parse(
-                    runnable_address_str,
-                    # Need to add a relative_to=addresses
-                    description_of_origin=f"ByoTool runnable target",
-                ),
-            )
-
-            addresses = Addresses((runnable_address,))
-            addresses.expect_single()
-
-            runnable_targets = await Get(Targets, Addresses, addresses)
-
-            target = runnable_targets[0]
-
-            environment_name = await Get(
-                EnvironmentName, EnvironmentNameRequest, EnvironmentNameRequest.from_target(target)
-            )
-
-            field_sets = await Get(
-                FieldSetsPerTarget, FieldSetsPerTargetRequest(RunFieldSet, runnable_targets)
-            )
-
-            run_field_set: RunFieldSet = field_sets.field_sets[0]
-
-            run_request = await Get(
-                RunInSandboxRequest, {environment_name: EnvironmentName, run_field_set: RunFieldSet}
-            )
-
-            execution_environment = await Get(
-                ResolvedExecutionDependencies,
-                ResolveExecutionDependenciesRequest(
-                    target.address,
-                    execution_dependencies=linter[CodeQualityToolExecutionDependenciesField].value,
-                    runnable_dependencies=None,
-                ),
-            )
-
-            dependencies_digest = execution_environment.digest
-            runnable_dependencies = execution_environment.runnable_dependencies
-
-            extra_env: dict[str, str] = dict(run_request.extra_env or {})
-            extra_path = extra_env.pop("PATH", None)
-
-            extra_sandbox_contents = []
-
-            extra_sandbox_contents.append(
-                ExtraSandboxContents(
-                    EMPTY_DIGEST,
-                    extra_path,
-                    run_request.immutable_input_digests or FrozenDict(),
-                    run_request.append_only_caches or FrozenDict(),
-                    run_request.extra_env or FrozenDict(),
-                )
-            )
-
-            if runnable_dependencies:
-                extra_sandbox_contents.append(
-                    ExtraSandboxContents(
-                        EMPTY_DIGEST,
-                        f"{{chroot}}/{runnable_dependencies.path_component}",
-                        runnable_dependencies.immutable_input_digests,
-                        runnable_dependencies.append_only_caches,
-                        runnable_dependencies.extra_env,
-                    )
-                )
-
-            merged_extras = await Get(
-                ExtraSandboxContents, MergeExtraSandboxContents(tuple(extra_sandbox_contents))
-            )
-            extra_env = dict(merged_extras.extra_env)
-            if merged_extras.path:
-                extra_env["PATH"] = merged_extras.path
-
-            input_digest = await Get(
-                Digest, MergeDigests((dependencies_digest, run_request.digest, sources_snapshot.digest))
-            )
-
-            cmd_args = linter[CodeQualityToolArgumentsField].value or ()
-
-            append_only_caches = {
-                **merged_extras.append_only_caches,
-            }
-
-            proc = Process(
-                argv=tuple(run_request.args + cmd_args + sources_snapshot.files),
-                description="Running byotool",
-                input_digest=input_digest,
-                append_only_caches=append_only_caches,
-                immutable_input_digests=FrozenDict.frozen(merged_extras.immutable_input_digests),
-                env=FrozenDict(extra_env),
-                output_files=goal.output_files_to_read(request),
-            )
-
-            proc_result = await Get(FallibleProcessResult, Process, proc)
             output = await Get(Snapshot, Digest, proc_result.output_digest)
 
             return goal.create_result(request, process_result=proc_result, output=output)
@@ -342,6 +424,8 @@ class CodeQualityToolRuleBuilder:
         namespace = dict(locals())
 
         return [
+            process_files,
+            hydrate_code_quality_tool,
             *collect_rules(namespace),
             *ByoToolRequest.rules(),
         ]
