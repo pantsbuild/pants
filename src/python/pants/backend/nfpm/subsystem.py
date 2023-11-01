aae85337f5d8d9a9c365a1d130bd791b1d7a9b41
Jacob Floyd <cognifloyd@gmail.com>
2023-11-01 02:12:06 -0500

    Update nFPM to v2.34.0

--- src/python/pants/backend/nfpm/subsystem.py
+++ src/python/pants/backend/nfpm/subsystem.py
@@ -17,29 +17,29 @@ class NfpmSubsystem(TemplatedExternalTool):
     help = "The nFPM deb, rpm, and apk packager (https://nfpm.goreleaser.com)."
 
     # The version WITHOUT the 'v' prefix which we add as needed in the url_template.
-    default_version = "2.28.0"
+    default_version = "2.34.0"  # released 28 Oct 2023
     default_known_versions = [
-        # checksums from https://github.com/goreleaser/nfpm/releases/download/v2.28.0/checksums.txt
-        "2.28.0|linux_arm64 |0754838455f61adfff821be0404965f6844d42e883f227c6c973f404d422e890|4594814",
-        "2.28.0|linux_x86_64|c948faaed97ca16ac2a4e24c42119606130ee0b371011a203d7a1c34ff422ea0|5050638",
-        "2.28.0|macos_arm64 |9fdfd9871df2877388bbd71e505ee9524470d7fd23d7074ea8cc7ea0b42b88b7|4986427",
-        "2.28.0|macos_x86_64|3bcc807a05ba3a0dbf62409064cd165256b9dcae2d317aa44b6c321142818a3e|5251388",
-        # "2.28.0|win_arm64 |76cacd133c5d1ac9adafc8a85d5e40605287fe0dadee00c0cca106bab52a8951|4717144",
-        # "2.28.0|win_x86_64|30de182bf1b7348b2c7fbf0554d0aa95aa569ee1bea81dc3e848af4217b7175e|5189195",
+        # checksums from https://github.com/goreleaser/nfpm/releases/download/v2.34.0/checksums.txt
+        "2.34.0|linux_arm64 |9b4fe0e650bbbc9d27456290b2eeb0719472519593d98bf1a4ba152022a4e872|4659916",
+        "2.34.0|linux_x86_64|1c97da72c055e3ddedf80bcfac155ccb008a99a55e59b3561c16ec4c6ce7e2c7|5067985",
+        "2.34.0|macos_arm64 |75985dc7660b9de2462d2aa6027c40c2cb116248b9340f70a83e9441488369ab|5041781",
+        "2.34.0|macos_x86_64|c5a7bd5fba2c9ba0b84a515439ecc5e61278c11643f99d577988bfe542f504be|5267411",
+        "2.34.0|win_arm64   |9586a2a772b82c1c603df4f14de8434d48adcdba80ced2d83adda31f04ea1745|4757387",
+        "2.34.0|win_x86_64  |72d1a774271bbfab3fa72b90fa5579c40a35d2536ca6782c86964064df9191d3|5252379",
     ]
 
     default_url_template = (
-        "https://github.com/goreleaser/nfpm/releases/download/v{version}/nfpm_{version}_{platform}.tar.gz"
-        # Windows uses .zip instead of .tar.gz
+        "https://github.com/goreleaser/nfpm/releases/download/v{version}/nfpm_{version}_{platform}"
     )
 
     default_url_platform_mapping = {
-        "macos_arm64": "Darwin_arm64",
-        "macos_x86_64": "Darwin_x86_64",
-        "linux_arm64": "Linux_arm64",
-        "linux_x86_64": "Linux_x86_64",
-        # "win_arm64": "Windows_arm64",
-        # "win_x86_64": "Windows_x86_64",
+        # Platform includes the extension because Windows uses .zip instead of .tar.gz
+        "macos_arm64": "Darwin_arm64.tar.gz",
+        "macos_x86_64": "Darwin_x86_64.tar.gz",
+        "linux_arm64": "Linux_arm64.tar.gz",
+        "linux_x86_64": "Linux_x86_64.tar.gz",
+        "win_arm64": "Windows_arm64.zip",
+        "win_x86_64": "Windows_x86_64.zip",
     }
 
     # all args controlled via target options
