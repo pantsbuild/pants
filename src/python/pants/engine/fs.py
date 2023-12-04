215cedb90db83edf3876a9c6081ec46ca44af93f
Krishnan Chandra <1229365+krishnan-chandra@users.noreply.github.com>
2023-12-04 16:09:29 -0500

    Validate that the file content has type bytes (#20261)
    Fixes #20024.

--- src/python/pants/engine/fs.py
+++ src/python/pants/engine/fs.py
@@ -53,6 +53,12 @@ class FileContent:
     content: bytes
     is_executable: bool = False
 
+    def __post_init__(self):
+        if not isinstance(self.content, bytes):
+            raise TypeError(
+                f"Expected 'content' to be bytes, but got {type(self.content).__name__}"
+            )
+
     def __repr__(self) -> str:
         return (
             f"FileContent(path={self.path}, content=(len:{len(self.content)}), "
