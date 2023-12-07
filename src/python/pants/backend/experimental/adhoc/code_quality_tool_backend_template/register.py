1d1033ce0f62b95448f1f5c1df479a236ea78537
Gautham Nair <gautham.p.nair@gmail.com>
2023-12-07 13:20:40 -0500

    accommodate package alias

--- src/python/pants/backend/experimental/adhoc/code_quality_tool_backend_template/register.py
+++ src/python/pants/backend/experimental/adhoc/code_quality_tool_backend_template/register.py
@@ -1,11 +1,11 @@
 from pants.backend.adhoc.code_quality_tool import CodeQualityToolRuleBuilder
 
 
-def rules(backend_package: str, goal: str, target: str, name: str):
+def rules(backend_package_alias: str, goal: str, target: str, name: str):
     cfg = CodeQualityToolRuleBuilder(
         goal=goal,
         target=target,
         name=name,
-        scope=backend_package,
+        scope=backend_package_alias,
     )
     return cfg.rules()
