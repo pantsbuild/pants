08ab145b75f55530b7e6dbdc843c073a61ba5355
Huon Wilson <huon@exoflare.io>
2023-12-11 15:20:53 +1100

    Allow passing arbitrary args to PEX invocation when building FaaS artifacts (#20237)
    This adds an `pex3_venv_create_extra_args` field to all FaaS targets
    (`python_aws_lambda_function`, `python_aws_lambda_layer`,
    `python_google_cloud_function`). This allows adding arbitrary extra
    arguments that are passed to the `pex3 venv create --layout=flat-zipped
    ...` invocation that is used to create the final zip file.
    
    Most acutely, this is driven by making it possible to pass the
    `--collisions-ok` flag. This allows work around dependencies that are
    packaged with files outside a namespaced directories, e.g. commonly a
    LICENCE or README file, or `tests/` directory, since they'll have
    different content. A command like `pip install ...` will happily install
    them and have one of the files "win" arbitrarily, while PEX is more
    correct and flags that it doesn't know what to do in that circumstance.
    
    Fixes #20224
    
    This is marked for cherry picking back to 2.18 because it can block
    adoption of the new layout, and the old (lambdex) layout is deprecated
    and using it is noisy in 2.18.

--- src/python/pants/backend/google_cloud_function/python/rules.py
+++ src/python/pants/backend/google_cloud_function/python/rules.py
@@ -12,7 +12,11 @@ from pants.backend.google_cloud_function.python.target_types import (
     PythonGoogleCloudFunctionRuntime,
     PythonGoogleCloudFunctionType,
 )
-from pants.backend.python.util_rules.faas import BuildPythonFaaSRequest, PythonFaaSCompletePlatforms
+from pants.backend.python.util_rules.faas import (
+    BuildPythonFaaSRequest,
+    PythonFaaSCompletePlatforms,
+    PythonFaaSPex3VenvCreateExtraArgsField,
+)
 from pants.backend.python.util_rules.faas import rules as faas_rules
 from pants.core.goals.package import BuiltPackage, OutputPathField, PackageFieldSet
 from pants.core.util_rules.environments import EnvironmentField
@@ -30,6 +34,7 @@ class PythonGoogleCloudFunctionFieldSet(PackageFieldSet):
     handler: PythonGoogleCloudFunctionHandlerField
     runtime: PythonGoogleCloudFunctionRuntime
     complete_platforms: PythonFaaSCompletePlatforms
+    pex3_venv_create_extra_args: PythonFaaSPex3VenvCreateExtraArgsField
     type: PythonGoogleCloudFunctionType
     output_path: OutputPathField
     environment: EnvironmentField
@@ -47,6 +52,7 @@ async def package_python_google_cloud_function(
             complete_platforms=field_set.complete_platforms,
             runtime=field_set.runtime,
             handler=field_set.handler,
+            pex3_venv_create_extra_args=field_set.pex3_venv_create_extra_args,
             output_path=field_set.output_path,
             include_requirements=True,
             include_sources=True,
--- src/python/pants/backend/google_cloud_function/python/target_types.py
+++ src/python/pants/backend/google_cloud_function/python/target_types.py
@@ -10,6 +10,7 @@ from pants.backend.python.util_rules.faas import (
     PythonFaaSCompletePlatforms,
     PythonFaaSDependencies,
     PythonFaaSHandlerField,
+    PythonFaaSPex3VenvCreateExtraArgsField,
     PythonFaaSRuntimeField,
 )
 from pants.backend.python.util_rules.faas import rules as faas_rules
@@ -116,6 +117,7 @@ class PythonGoogleCloudFunction(Target):
         PythonGoogleCloudFunctionRuntime,
         PythonFaaSCompletePlatforms,
         PythonGoogleCloudFunctionType,
+        PythonFaaSPex3VenvCreateExtraArgsField,
         PythonResolveField,
         EnvironmentField,
     )
