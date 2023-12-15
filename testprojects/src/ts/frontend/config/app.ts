7d6f92ad87eea99c4d6b79849c2d976b0d164bb5
Alexey Tereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2023-12-15 00:07:13 +0000

    typescript: extend test project with new files

--- testprojects/src/ts/frontend/config/app.ts
+++ testprojects/src/ts/frontend/config/app.ts
@@ -1,6 +1,13 @@
 // import from file `services/browser.ts`
 import { type BrowserService } from 'services/browser';
 
+// import everything from deployment
+import * from "deployment"
+
 // import from 3rd party packages, ignored
 import { type foo, type bar } from 'redux'
 import * as Sentry from '@sentry/react';
+
+// local file import
+import { dispatcher } from './dispatcher';
+import { receiver } from './receiver';
