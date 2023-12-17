328fb1fb703e39c1499f1768acbf86008eaa903e
Worker Pants (Pantsbuild GitHub Automation Bot) <133242086+WorkerPants@users.noreply.github.com>
2023-12-17 08:26:19 -0800

    Fix: run `terraform validate` on modules again (Cherry-pick of #20230) (#20298)
    Only running `terraform validate` on deployments is unexpected for
    users. See https://github.com/pantsbuild/pants/issues/20222 . Running
    `terraform validate` on all modules might lead to errors with Terraform,
    but those are predictable and understandable by users.
    This MR:
    - Runs `terraform validate` on `terraform_modules` again
    - Adds the `skip_terraform_validate` field for `terraform_modules` which
    cannot be `validate`d by Terraform
    - Adds some documentation for the Terraform backend
    
    It leaves open the possibility of validating both
    `terraform_deployment`s and `terraform_module`s. Hopefully we can work
    out something like the Helm backend's handling of kubeconform.
    
    Co-authored-by: Daniel Goldman <merkavabuilder@gmail.com>

--- src/python/pants/backend/terraform/goals/check.py
+++ src/python/pants/backend/terraform/goals/check.py
@@ -1,12 +1,23 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+from dataclasses import dataclass
+from typing import Union
+
 from pants.backend.terraform.dependencies import TerraformInitRequest, TerraformInitResponse
-from pants.backend.terraform.target_types import TerraformDeploymentFieldSet
+from pants.backend.terraform.target_types import (
+    TerraformBackendConfigField,
+    TerraformDeploymentFieldSet,
+    TerraformDeploymentTarget,
+    TerraformFieldSet,
+    TerraformModuleTarget,
+    TerraformRootModuleField,
+)
 from pants.backend.terraform.tool import TerraformProcess
 from pants.core.goals.check import CheckRequest, CheckResult, CheckResults
 from pants.engine.internals.selectors import Get, MultiGet
 from pants.engine.process import FallibleProcessResult
 from pants.engine.rules import collect_rules, rule
+from pants.engine.target import BoolField, Target
 from pants.engine.unions import UnionRule
 from pants.option.option_types import SkipOption
 from pants.option.subsystem import Subsystem
@@ -21,9 +32,39 @@ class TerraformValidateSubsystem(Subsystem):
     skip = SkipOption("check")
 
 
+class SkipTerraformValidateField(BoolField):
+    alias = "skip_terraform_validate"
+    default = False
+    help = "If true, don't run `terraform validate` on this target's code. If this target is a module, `terraform validate might still be run on a `terraform_deployment that references this module."
+
+
+@dataclass(frozen=True)
+class TerraformValidateFieldSet(TerraformFieldSet):
+    @classmethod
+    def opt_out(cls, tgt: Target) -> bool:
+        return tgt.get(SkipTerraformValidateField).value
+
+
 class TerraformCheckRequest(CheckRequest):
-    field_set_type = TerraformDeploymentFieldSet
+    field_set_type = TerraformValidateFieldSet
     tool_name = TerraformValidateSubsystem.options_scope
+
+
+def terraform_fieldset_to_init_request(
+    terraform_fieldset: Union[TerraformDeploymentFieldSet, TerraformFieldSet]
+) -> TerraformInitRequest:
+    if isinstance(terraform_fieldset, TerraformDeploymentFieldSet):
+        deployment = terraform_fieldset
+        return TerraformInitRequest(
+            deployment.root_module, deployment.backend_config, deployment.dependencies
+        )
+    if isinstance(terraform_fieldset, TerraformFieldSet):
+        module = terraform_fieldset
+        return TerraformInitRequest(
+            TerraformRootModuleField(module.address.spec, module.address),
+            TerraformBackendConfigField(None, module.address),
+            module.dependencies,
+        )
 
 
 @rule
@@ -36,9 +77,8 @@ async def terraform_check(
     initialised_terraforms = await MultiGet(
         Get(
             TerraformInitResponse,
-            TerraformInitRequest(
-                deployment.root_module, deployment.backend_config, deployment.dependencies
-            ),
+            TerraformInitRequest,
+            terraform_fieldset_to_init_request(deployment),
         )
         for deployment in request.field_sets
     )
@@ -71,5 +111,7 @@ async def terraform_check(
 def rules():
     return (
         *collect_rules(),
+        TerraformDeploymentTarget.register_plugin_field(SkipTerraformValidateField),
+        TerraformModuleTarget.register_plugin_field(SkipTerraformValidateField),
         UnionRule(CheckRequest, TerraformCheckRequest),
     )
