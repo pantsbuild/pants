9f6547b94386cdf34f96bbe228498da36e149d6c
Joshua <joshdcannon@gmail.com>
2023-12-20 13:09:56 -0600

    read only

--- src/rust/engine/process_execution/docker/src/docker.rs
+++ src/rust/engine/process_execution/docker/src/docker.rs
@@ -818,6 +818,9 @@ impl<'a> ContainerCache<'a> {
             .await
             .map_err(|e| format!("Failed to create named cache volume for {image_name}: {e}"))?;
 
+        for mount in bind_mounts.iter_mut() {
+            mount.push_str(":ro");
+        }
         bind_mounts.extend(vec![
             format!("{work_dir_base}:{SANDBOX_BASE_PATH_IN_CONTAINER}"),
             format!("{named_cache_volume_name}:{NAMED_CACHES_BASE_PATH_IN_CONTAINER}",),
