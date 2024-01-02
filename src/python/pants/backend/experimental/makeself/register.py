2af95a827ff52ee79281b58b914b13b06e03236a
Borodin Gregory <grihabor@gmail.com>
2024-01-02 13:18:51 +0100

    Fix register.py

--- src/python/pants/backend/experimental/makeself/register.py
+++ src/python/pants/backend/experimental/makeself/register.py
@@ -1,10 +1,9 @@
 # Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+from pants.backend.makeself import makeself
+from pants.backend.makeself.goals import package, run
+from pants.backend.makeself.target_types import MakeselfArchiveTarget
 from pants.core.util_rules import system_binaries
-
-from . import makeself
-from .goals import package, run
-from .target_types import MakeselfArchiveTarget
 
 
 def target_types():
