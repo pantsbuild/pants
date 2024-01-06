1d7e912f3aa923b5d089d1201189e695ac1e5ab1
Borodin Gregory <grihabor@gmail.com>
2024-01-06 14:54:53 +0100

    Add module doc comment

--- src/python/pants/backend/experimental/makeself/register.py
+++ src/python/pants/backend/experimental/makeself/register.py
@@ -1,5 +1,9 @@
 # Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+"""Make self-extractable archives on Unix
+
+See https://github.com/megastep/makeself.
+"""
 from pants.backend.makeself import makeself
 from pants.backend.makeself.goals import package, run
 from pants.backend.makeself.target_types import MakeselfArchiveTarget
