430a8f96c9974cca71e4fef66d2735115d3039e9
Krishnan Chandra <krishnan.chandra@gmail.com>
2024-01-15 10:32:58 -0500

    Add tests

--- src/python/pants/backend/build_files/fmt/ruff/integration_test.py
+++ src/python/pants/backend/build_files/fmt/ruff/integration_test.py
@@ -5,11 +5,11 @@ from __future__ import annotations
 
 import pytest
 
-from pants.backend.build_files.fmt.black.register import BlackRequest
-from pants.backend.build_files.fmt.black.register import rules as black_build_rules
-from pants.backend.python.lint.black.rules import rules as black_fmt_rules
-from pants.backend.python.lint.black.subsystem import Black
-from pants.backend.python.lint.black.subsystem import rules as black_subsystem_rules
+from pants.backend.build_files.fmt.ruff.register import RuffRequest
+from pants.backend.build_files.fmt.ruff.register import rules as ruff_build_rules
+from pants.backend.python.lint.ruff.rules import rules as ruff_fmt_rules
+from pants.backend.python.lint.ruff.subsystem import Ruff
+from pants.backend.python.lint.ruff.subsystem import rules as ruff_subsystem_rules
 from pants.backend.python.target_types import PythonSourcesGeneratorTarget
 from pants.core.goals.fmt import FmtResult
 from pants.core.util_rules import config_files
@@ -23,19 +23,19 @@ from pants.testutil.rule_runner import QueryRule, RuleRunner
 def rule_runner() -> RuleRunner:
     return RuleRunner(
         rules=[
-            *black_build_rules(),
-            *black_fmt_rules(),
-            *black_subsystem_rules(),
+            *ruff_build_rules(),
+            *ruff_fmt_rules(),
+            *ruff_subsystem_rules(),
             *config_files.rules(),
-            QueryRule(FmtResult, (BlackRequest.Batch,)),
+            QueryRule(FmtResult, (RuffRequest.Batch,)),
         ],
         target_types=[PythonSourcesGeneratorTarget],
     )
 
 
-def run_black(rule_runner: RuleRunner, *, extra_args: list[str] | None = None) -> FmtResult:
+def run_ruff(rule_runner: RuleRunner, *, extra_args: list[str] | None = None) -> FmtResult:
     rule_runner.set_options(
-        ["--backend-packages=pants.backend.build_files.fmt.black", *(extra_args or ())],
+        ["--backend-packages=pants.backend.build_files.fmt.ruff", *(extra_args or ())],
         # We propagate LANG and LC_ALL to satisfy click, which black depends upon. Without this we
         # see something like the following in CI:
         #
@@ -49,13 +49,13 @@ def run_black(rule_runner: RuleRunner, *, extra_args: list[str] | None = None) -
         #     export LC_ALL=C.UTF-8
         #     export LANG=C.UTF-8
         #
-        env_inherit={"PATH", "PYENV_ROOT", "HOME", "LANG", "LC_ALL"},
+        env_inherit={"PATH", "PYENV_ROOT", "HOME"},
     )
     snapshot = rule_runner.request(Snapshot, [PathGlobs(["**/BUILD"])])
     fmt_result = rule_runner.request(
         FmtResult,
         [
-            BlackRequest.Batch("", snapshot.files, partition_metadata=None, snapshot=snapshot),
+            RuffRequest.Batch("", snapshot.files, partition_metadata=None, snapshot=snapshot),
         ],
     )
     return fmt_result
@@ -64,16 +64,16 @@ def run_black(rule_runner: RuleRunner, *, extra_args: list[str] | None = None) -
 @pytest.mark.platform_specific_behavior
 @pytest.mark.parametrize(
     "major_minor_interpreter",
-    all_major_minor_python_versions(Black.default_interpreter_constraints),
+    all_major_minor_python_versions(Ruff.default_interpreter_constraints),
 )
 def test_passing(rule_runner: RuleRunner, major_minor_interpreter: str) -> None:
     rule_runner.write_files({"BUILD": 'python_sources(name="t")\n'})
     interpreter_constraint = (
         ">=3.6.2,<3.7" if major_minor_interpreter == "3.6" else f"=={major_minor_interpreter}.*"
     )
-    fmt_result = run_black(
+    fmt_result = run_ruff(
         rule_runner,
-        extra_args=[f"--black-interpreter-constraints=['{interpreter_constraint}']"],
+        extra_args=[f"--ruff-interpreter-constraints=['{interpreter_constraint}']"],
     )
     assert "1 file left unchanged" in fmt_result.stderr
     assert fmt_result.output == rule_runner.make_snapshot({"BUILD": 'python_sources(name="t")\n'})
@@ -82,7 +82,7 @@ def test_passing(rule_runner: RuleRunner, major_minor_interpreter: str) -> None:
 
 def test_failing(rule_runner: RuleRunner) -> None:
     rule_runner.write_files({"BUILD": "python_sources(name='t')\n"})
-    fmt_result = run_black(rule_runner)
+    fmt_result = run_ruff(rule_runner)
     assert "1 file reformatted" in fmt_result.stderr
     assert fmt_result.output == rule_runner.make_snapshot({"BUILD": 'python_sources(name="t")\n'})
     assert fmt_result.did_change is True
@@ -95,7 +95,7 @@ def test_multiple_files(rule_runner: RuleRunner) -> None:
             "bad/BUILD": "python_sources(name='t')\n",
         }
     )
-    fmt_result = run_black(rule_runner)
+    fmt_result = run_ruff(rule_runner)
     assert "1 file reformatted, 1 file left unchanged" in fmt_result.stderr
     assert fmt_result.output == rule_runner.make_snapshot(
         {"good/BUILD": 'python_sources(name="t")\n', "bad/BUILD": 'python_sources(name="t")\n'}
@@ -105,16 +105,16 @@ def test_multiple_files(rule_runner: RuleRunner) -> None:
 
 @pytest.mark.parametrize(
     "config_path,extra_args",
-    (["pyproject.toml", []], ["custom_config.toml", ["--black-config=custom_config.toml"]]),
+    (["pyproject.toml", []], ["custom_config.toml", ["--ruff-config=custom_config.toml"]]),
 )
 def test_config_file(rule_runner: RuleRunner, config_path: str, extra_args: list[str]) -> None:
     rule_runner.write_files(
         {
             "BUILD": "python_sources(name='t')\n",
-            config_path: "[tool.black]\nskip-string-normalization = 'true'\n",
+            config_path: "[tool.ruff]\nskip-string-normalization = 'true'\n",
         }
     )
-    fmt_result = run_black(rule_runner, extra_args=extra_args)
+    fmt_result = run_ruff(rule_runner, extra_args=extra_args)
     assert "1 file left unchanged" in fmt_result.stderr
     assert fmt_result.output == rule_runner.make_snapshot({"BUILD": "python_sources(name='t')\n"})
     assert fmt_result.did_change is False
@@ -122,7 +122,8 @@ def test_config_file(rule_runner: RuleRunner, config_path: str, extra_args: list
 
 def test_passthrough_args(rule_runner: RuleRunner) -> None:
     rule_runner.write_files({"BUILD": "python_sources(name='t')\n"})
-    fmt_result = run_black(rule_runner, extra_args=["--black-args='--skip-string-normalization'"])
+    # Pass through the diff argument, which should cause no files to be changed
+    fmt_result = run_ruff(rule_runner, extra_args=["--diff"])
     assert "1 file left unchanged" in fmt_result.stderr
     assert fmt_result.output == rule_runner.make_snapshot({"BUILD": "python_sources(name='t')\n"})
     assert fmt_result.did_change is False
