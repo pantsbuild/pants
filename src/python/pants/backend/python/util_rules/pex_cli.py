407d55946814fa4f564fe7b86c820377c0fd325c
Chris Burroughs <chris.burroughs@gmail.com>
2024-01-16 11:13:55 -0500

    upgrade PEX to 2.1.159
    Changelogs:
     * https://github.com/pantsbuild/pex/releases/tag/v2.1.157
     * https://github.com/pantsbuild/pex/releases/tag/v2.1.158
     * https://github.com/pantsbuild/pex/releases/tag/v2.1.159
    
    ```
    Lockfile diff: 3rdparty/python/user_reqs.lock [python-default]
    
    ==                    Upgraded dependencies                     ==
    
      attrs                          23.1.0       -->   23.2.0
      pex                            2.1.156      -->   2.1.159
    ```
    
    Enables repl tab completion for #20389
    Fixes a bug with `pex3 lock update` of relevance for #15704

--- src/python/pants/backend/python/util_rules/pex_cli.py
+++ src/python/pants/backend/python/util_rules/pex_cli.py
@@ -35,7 +35,7 @@ class PexCli(TemplatedExternalTool):
     name = "pex"
     help = "The PEX (Python EXecutable) tool (https://github.com/pantsbuild/pex)."
 
-    default_version = "v2.1.156"
+    default_version = "v2.1.159"
     default_url_template = "https://github.com/pantsbuild/pex/releases/download/{version}/pex"
     version_constraints = ">=2.1.135,<3.0"
 
@@ -46,8 +46,8 @@ class PexCli(TemplatedExternalTool):
                 (
                     cls.default_version,
                     plat,
-                    "b606aefcc5da71b16fd483ecf2114703332fb85e279825afdae45c05c975974b",
-                    "3666831",
+                    "83c3090938b4d276703864c34ba50bcb3616db0663c54b56dd0521a668d9555f",
+                    "3671772",
                 )
             )
             for plat in ["macos_arm64", "macos_x86_64", "linux_x86_64", "linux_arm64"]
