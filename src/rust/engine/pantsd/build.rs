39fb59e5f77ed53453f86c2dfd5961e18895d900
Joshua <joshdcannon@gmail.com>
2024-01-19 20:12:31 -0600

    thatll do it

--- src/rust/engine/pantsd/build.rs
+++
@@ -1,9 +0,0 @@
-use std::process::Command;
-fn main() {
-    let output = Command::new("git")
-        .args(["describe", "--tags"])
-        .output()
-        .unwrap();
-    let git_tag = String::from_utf8(output.stdout).unwrap();
-    println!("cargo:rustc-env=GIT_TAG={}", git_tag);
-}
