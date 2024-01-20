5af7b1f0409905131571feb3e082824238da6fd3
Alexey Tereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2024-01-20 09:17:43 +0000

    Split code into separate functions

--- src/python/pants/backend/project_info/dependencies.py
+++ src/python/pants/backend/project_info/dependencies.py
@@ -2,7 +2,6 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 import itertools
 import json
-from dataclasses import dataclass
 from enum import Enum
 
 from pants.engine.addresses import Addresses
@@ -13,7 +12,6 @@ from pants.engine.target import AlwaysTraverseDeps
 from pants.engine.target import Dependencies as DependenciesField
 from pants.engine.target import (
     DependenciesRequest,
-    Target,
     Targets,
     TransitiveTargets,
     TransitiveTargetsRequest,
@@ -56,104 +54,115 @@ class Dependencies(Goal):
     environment_behavior = Goal.EnvironmentBehavior.LOCAL_ONLY
 
 
-@dataclass
-class TargetToDependencies:
-    target: Target
-    dependencies: Targets
+async def list_dependencies_as_json(
+    addresses: Addresses, dependencies_subsystem: DependenciesSubsystem, console: Console
+) -> None:
+    """Get dependencies for given addresses and list them in the console in JSON.
+
+    Note that `--closed` option is ignored as it doesn't make sense to duplicate source address in
+    the list of its dependencies.
+    """
+    # NB: We must preserve target generators for the roots, i.e. not replace with their
+    # generated targets.
+    target_roots = await Get(UnexpandedTargets, Addresses, addresses)
+    # NB: When determining dependencies, though, we replace target generators with their
+    # generated targets.
+    if dependencies_subsystem.transitive:
+        transitive_targets_group = await MultiGet(
+            Get(
+                TransitiveTargets,
+                TransitiveTargetsRequest(
+                    (address,), should_traverse_deps_predicate=AlwaysTraverseDeps()
+                ),
+            )
+            for address in addresses
+        )
+
+        iterated_targets = []
+        for transitive_targets in transitive_targets_group:
+            iterated_targets.append(
+                sorted([str(tgt.address) for tgt in transitive_targets.dependencies])
+            )
+
+    else:
+        dependencies_per_target_root = await MultiGet(
+            Get(
+                Targets,
+                DependenciesRequest(
+                    tgt.get(DependenciesField),
+                    should_traverse_deps_predicate=AlwaysTraverseDeps(),
+                ),
+            )
+            for tgt in target_roots
+        )
+
+        iterated_targets = []
+        for targets in dependencies_per_target_root:
+            iterated_targets.append(sorted([str(tgt.address) for tgt in targets]))
+
+    # the assumption is that when iterating the targets and sending dependency requests
+    # for them, the lists of dependencies are returned in the very same order
+    mapping = dict(zip([str(tgt.address) for tgt in target_roots], iterated_targets))
+    output = json.dumps(mapping, indent=4)
+    console.print_stdout(output)
 
 
-@dataclass
-class TargetsToDirectDependencies:
-    mapping: Addresses
-    dependencies: list[str]
+async def list_dependencies_as_merged(
+    addresses: Addresses, dependencies_subsystem: DependenciesSubsystem, console: Console
+) -> None:
+    """Get dependencies for given addresses and list them in the console as a single list."""
+    if dependencies_subsystem.transitive:
+        transitive_targets = await Get(
+            TransitiveTargets,
+            TransitiveTargetsRequest(
+                addresses, should_traverse_deps_predicate=AlwaysTraverseDeps()
+            ),
+        )
+        targets = Targets(transitive_targets.dependencies)
+    else:
+        # NB: We must preserve target generators for the roots, i.e. not replace with their
+        # generated targets.
+        target_roots = await Get(UnexpandedTargets, Addresses, addresses)
+        # NB: When determining dependencies, though, we replace target generators with their
+        # generated targets.
+        dependencies_per_target_root = await MultiGet(
+            Get(
+                Targets,
+                DependenciesRequest(
+                    tgt.get(DependenciesField),
+                    should_traverse_deps_predicate=AlwaysTraverseDeps(),
+                ),
+            )
+            for tgt in target_roots
+        )
+        targets = Targets(itertools.chain.from_iterable(dependencies_per_target_root))
+
+    address_strings = {addr.spec for addr in addresses} if dependencies_subsystem.closed else set()
+    for tgt in targets:
+        address_strings.add(tgt.address.spec)
+
+    with dependencies_subsystem.line_oriented(console) as print_stdout:
+        for address in sorted(address_strings):
+            print_stdout(address)
 
 
 @goal_rule
 async def dependencies(
     console: Console, addresses: Addresses, dependencies_subsystem: DependenciesSubsystem
 ) -> Dependencies:
-    # NB: We must preserve target generators for the roots, i.e. not replace with their
-    # generated targets.
-    target_roots = await Get(UnexpandedTargets, Addresses, addresses)
-    # NB: When determining dependencies, though, we replace target generators with their
-    # generated targets.
-
-    if dependencies_subsystem.format == DependenciesOutputFormat.json:
-        if dependencies_subsystem.transitive:
-            transitive_targets_group = await MultiGet(
-                Get(
-                    TransitiveTargets,
-                    TransitiveTargetsRequest(
-                        (address,), should_traverse_deps_predicate=AlwaysTraverseDeps()
-                    ),
-                )
-                for address in addresses
-            )
-
-            iterated_targets = []
-            for transitive_targets in transitive_targets_group:
-                iterated_targets.append(
-                    sorted([str(tgt.address) for tgt in transitive_targets.dependencies])
-                )
-
-        else:  # dependencies_subsystem.direct
-            dependencies_per_target_root = await MultiGet(
-                Get(
-                    Targets,
-                    DependenciesRequest(
-                        tgt.get(DependenciesField),
-                        should_traverse_deps_predicate=AlwaysTraverseDeps(),
-                    ),
-                )
-                for tgt in target_roots
-            )
-
-            iterated_targets = []
-            for targets in dependencies_per_target_root:
-                iterated_targets.append(sorted([str(tgt.address) for tgt in targets]))
-
-        # the assumption is that when iterating the targets and sending dependency requests
-        # for them, the lists of dependencies are returned in the very same order
-        mapping = dict(zip([str(tgt.address) for tgt in target_roots], iterated_targets))
-        output = json.dumps(mapping, indent=4)
-        console.print_stdout(output)
-
     if dependencies_subsystem.format == DependenciesOutputFormat.merged:
-        if dependencies_subsystem.transitive:
-            transitive_targets = await Get(
-                TransitiveTargets,
-                TransitiveTargetsRequest(
-                    addresses, should_traverse_deps_predicate=AlwaysTraverseDeps()
-                ),
-            )
-            targets = Targets(transitive_targets.dependencies)
-        else:
-            # NB: We must preserve target generators for the roots, i.e. not replace with their
-            # generated targets.
-            target_roots = await Get(UnexpandedTargets, Addresses, addresses)
-            # NB: When determining dependencies, though, we replace target generators with their
-            # generated targets.
-            dependencies_per_target_root = await MultiGet(
-                Get(
-                    Targets,
-                    DependenciesRequest(
-                        tgt.get(DependenciesField),
-                        should_traverse_deps_predicate=AlwaysTraverseDeps(),
-                    ),
-                )
-                for tgt in target_roots
-            )
-            targets = Targets(itertools.chain.from_iterable(dependencies_per_target_root))
-
-        address_strings = (
-            {addr.spec for addr in addresses} if dependencies_subsystem.closed else set()
+        await list_dependencies_as_merged(
+            addresses=addresses,
+            dependencies_subsystem=dependencies_subsystem,
+            console=console,
         )
-        for tgt in targets:
-            address_strings.add(tgt.address.spec)
 
-        with dependencies_subsystem.line_oriented(console) as print_stdout:
-            for address in sorted(address_strings):
-                print_stdout(address)
+    elif dependencies_subsystem.format == DependenciesOutputFormat.json:
+        await list_dependencies_as_json(
+            addresses=addresses,
+            dependencies_subsystem=dependencies_subsystem,
+            console=console,
+        )
 
     return Dependencies(exit_code=0)
 
