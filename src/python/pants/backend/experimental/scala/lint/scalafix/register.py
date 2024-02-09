dae5df45ce3e5f46f7fd0cdc90977dbcf1477e50
Antonio Alonso Dominguez <alonso.domin@pm.me>
2024-02-09 11:38:19 +0100

    Add BUILD file

--- src/python/pants/backend/experimental/scala/lint/scalafix/register.py
+++ src/python/pants/backend/experimental/scala/lint/scalafix/register.py
@@ -1,5 +1,6 @@
 # Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 from pants.backend.experimental.scala.register import rules as all_scala_rules
 from pants.backend.scala.lint.scalafix import extra_fields
 from pants.backend.scala.lint.scalafix import rules as scalafix_rules
