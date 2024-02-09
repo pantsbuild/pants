b305eeb8e30c8168766c80800f1271f3a689100e
Krishnan Chandra <krishnan.chandra@gmail.com>
2024-02-08 20:46:13 -0500

    Add warn_or_error message

--- src/python/pants/backend/experimental/python/lint/ruff/register.py
+++ src/python/pants/backend/experimental/python/lint/ruff/register.py
@@ -8,7 +8,14 @@ See https://www.pantsbuild.org/docs/python-linters-and-formatters and https://do
 
 from pants.backend.python.lint.ruff import rules as ruff_rules
 from pants.backend.python.lint.ruff import skip_field, subsystem
+from pants.base.deprecated import warn_or_error
 
 
 def rules():
+    warn_or_error(
+        "2.23.0.dev0",
+        "Using the experimental Ruff backend",
+        "The ruff backend has moved to `pants.backend.python.lint.ruff`",
+        start_version="2.20.0",
+    )
     return (*ruff_rules.rules(), *skip_field.rules(), *subsystem.rules())
