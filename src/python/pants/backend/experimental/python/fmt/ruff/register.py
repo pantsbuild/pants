230916da8bf9b4bfbfd127a9707de23cba22514d
Krishnan Chandra <krishnan.chandra@gmail.com>
2024-02-13 23:43:30 -0500

    Add experimental.python.fmt.ruff backend

---
+++ src/python/pants/backend/experimental/python/fmt/ruff/register.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+"""Linter & formatter for Python.
+
+See https://www.pantsbuild.org/docs/python-linters-and-formatters and https://docs.astral.sh/ruff/
+"""
+
+from pants.backend.python.lint.ruff import fmt_rules as ruff_fmt_rules
+from pants.backend.python.lint.ruff import skip_field, subsystem
+
+
+def rules():
+    return (*ruff_fmt_rules.rules(), *skip_field.rules(), *subsystem.rules())
