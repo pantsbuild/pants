b3e5631a0a0f331a9001d9276849ffa1e2ff58ac
Krishnan Chandra <krishnan.chandra@gmail.com>
2024-02-15 09:34:52 -0500

    Use dev7 which is the latest release

--- src/python/pants/backend/experimental/python/lint/ruff/register.py
+++ src/python/pants/backend/experimental/python/lint/ruff/register.py
@@ -15,6 +15,6 @@ def rules():
         "2.23.0.dev0",
         "The `pants.backend.experimental.python.lint.ruff` backend",
         hint="Use `pants.backend.experimental.python.lint.ruff.check` instead.",
-        start_version="2.20.0.dev0",
+        start_version="2.20.0.dev7",
     )
     return ruff_check_rules.rules()
