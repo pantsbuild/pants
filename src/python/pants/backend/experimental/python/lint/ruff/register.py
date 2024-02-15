05badd1869fe43ad689a5c8e851280edff648f90
Krishnan Chandra <krishnan.chandra@gmail.com>
2024-02-15 08:24:23 -0500

    Add start and end versions

--- src/python/pants/backend/experimental/python/lint/ruff/register.py
+++ src/python/pants/backend/experimental/python/lint/ruff/register.py
@@ -12,8 +12,9 @@ from pants.base.deprecated import warn_or_error
 
 def rules():
     warn_or_error(
-        "2.22.0",
+        "2.22.0.dev0",
         "using the `pants.backend.experimental.python.lint.ruff` backend",
         hint="Use `pants.backend.experimental.python.lint.ruff.check` instead.",
+        start_version="2.20.0",
     )
     return ruff_check_rules.rules()
