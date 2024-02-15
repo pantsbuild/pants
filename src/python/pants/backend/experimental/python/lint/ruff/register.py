a2e289707ffd798113bfcf40bece83d834df1c8f
Krishnan Chandra <krishnan.chandra@gmail.com>
2024-02-15 08:17:07 -0500

    Re-register check rules at root

---
+++ src/python/pants/backend/experimental/python/lint/ruff/register.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+"""Linter & formatter for Python.
+
+See https://www.pantsbuild.org/docs/python-linters-and-formatters and https://docs.astral.sh/ruff/
+"""
+
+from pants.backend.experimental.python.lint.ruff.check import register as ruff_check_rules
+
+
+def rules():
+    return ruff_check_rules.rules()
