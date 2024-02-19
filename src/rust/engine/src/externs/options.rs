c08cc8c2246996349d9d5414e85416bb1bb40789
Benjy Weinberger <benjyw@gmail.com>
2024-02-19 09:07:27 -0800

    WIP

--- src/rust/engine/src/externs/options.rs
+++ src/rust/engine/src/externs/options.rs
@@ -7,7 +7,7 @@ use pyo3::exceptions::{PyException, PyValueError};
 use pyo3::types::{PyDict, PyList, PyTuple};
 
 //use options::{Args, Config, Env, OptionId, OptionParser, Scope, Source, Val};
-use options::{Args, Env, Val, OptionId, OptionParser, OptionValue, Scope};
+use options::{Args, Env, Val, OptionId, OptionParser, OptionValue, Scope, ListOptionValue};
 
 use std::collections::HashMap;
 
@@ -36,12 +36,11 @@ fn val_to_py_object(py: Python, val: Val) -> PyResult<PyObject> {
         Val::Dict(dict) => {
             let pydict = PyDict::new(py);
             for (k, v) in dict {
-                pydict.set_item(k, val_to_py_object(py, v)?)?;
+                pydict.set_item(k.into_py(py), val_to_py_object(py, v)?)?;
             }
             pydict.into_py(py)
         }
     };
-
     Ok(res)
 }
 
@@ -80,10 +79,18 @@ struct PyOptionParser(OptionParser);
 impl PyOptionParser {
     fn get_scalar<T: ToOwned + ?Sized>(
         &self, option_id: &PyOptionId, default: &T,
-        getter: fn(&OptionParser, &OptionId, &T) -> Result<OptionValue<T::Owned>, String>,) -> PyResult<(T::Owned, String)> {
+        getter: fn(&OptionParser, &OptionId, &T) -> Result<OptionValue<T::Owned>, String>,) -> PyResult<T::Owned> {
         let opt_val = getter(&self.0, &option_id.0, default)
             .map_err(PyException::new_err)?;
-        Ok((opt_val.value, format!("{:?}", opt_val.source)))
+        Ok(opt_val.value)
+    }
+
+    fn get_list<T: ToOwned + ?Sized>(
+        &self, option_id: &PyOptionId, default: &Vec<T::Owned>,
+        getter: fn(&OptionParser, &OptionId, &Vec<T::Owned>) -> Result<ListOptionValue<T::Owned>, String>,) -> PyResult<Vec<T::Owned>> {
+        let opt_val = getter(&self.0, &option_id.0, default)
+            .map_err(PyException::new_err)?;
+        Ok(opt_val.value)
     }
 }
 
@@ -97,38 +104,58 @@ impl PyOptionParser {
             .map(|kv_pair| kv_pair.extract::<(String, String)>())
             .collect::<Result<HashMap<_, _>, _>>()?;
 
-        let option_parser = OptionParser::new(Args::new(args), Env::new(env), Some(configs), allow_pantsrc)
+        let option_parser = OptionParser::new(Args::new(args), Env::new(env), Some(configs), allow_pantsrc, false, None)
             .map_err(PyValueError::new_err)?;
         Ok(Self(option_parser))
     }
 
-    fn get_bool(&self, option_id: &PyOptionId, default: bool) -> PyResult<(bool, String)> {
+    fn get_bool(&self, option_id: &PyOptionId, default: bool) -> PyResult<bool> {
         self.get_scalar(option_id, &default, |op, oid, def| op.parse_bool(oid, *def))
     }
 
-    fn get_int(&self, option_id: &PyOptionId, default: i64) -> PyResult<(i64, String)> {
+    fn get_int(&self, option_id: &PyOptionId, default: i64) -> PyResult<i64> {
         self.get_scalar(option_id, &default, |op, oid, def| op.parse_int(oid, *def))
     }
 
-    fn get_float(&self, option_id: &PyOptionId, default: f64) -> PyResult<(f64, String)> {
+    fn get_float(&self, option_id: &PyOptionId, default: f64) -> PyResult<f64> {
         self.get_scalar(option_id, &default, |op, oid, def| op.parse_float(oid, *def))
     }
 
-    fn get_string(
-        &self,
-        option_id: &PyOptionId,
-        default: &str,
-    ) -> PyResult<(String, String)> {
+    fn get_string(&self, option_id: &PyOptionId, default: &str) -> PyResult<String> {
         self.get_scalar(option_id, default, |op, oid, def| op.parse_string(oid, def))
     }
 
-    // fn get_bool_list(
-    //     &self,
-    //     option_id: &PyOptionId,
-    //     default: Vec<bool>,
-    // ) -> PyResult<(Vec<bool>, String)> {
-    //     self.get_scalar(option_id, &default, |op, oid, def| op.parse_bool_list(oid, def))
-    // }
+    fn get_bool_list(
+        &self,
+        option_id: &PyOptionId,
+        default: Vec<bool>,
+    ) -> PyResult<Vec<bool>> {
+        self.get_list::<bool>(option_id, &default, |op, oid, def| op.parse_bool_list(oid, def))
+    }
+
+    fn get_int_list(
+        &self,
+        option_id: &PyOptionId,
+        default: Vec<i64>,
+    ) -> PyResult<Vec<i64>> {
+        self.get_list::<i64>(option_id, &default, |op, oid, def| op.parse_int_list(oid, def))
+    }
+
+    fn get_float_list(
+        &self,
+        option_id: &PyOptionId,
+        default: Vec<f64>,
+    ) -> PyResult<Vec<f64>> {
+        self.get_list::<f64>(option_id, &default, |op, oid, def| op.parse_float_list(oid, def))
+    }
+
+    fn get_string_list(
+        &self,
+        option_id: &PyOptionId,
+        default: Vec<String>,
+    ) -> PyResult<Vec<String>> {
+        self.get_list::<String>(option_id, &default, |op, oid, def| op.parse_string_list(oid, def))
+    }
 
     // fn get_string_list(
     //     &self,
