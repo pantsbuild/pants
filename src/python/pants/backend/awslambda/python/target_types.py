6b6bd60806b939ace963c3058bcc2d5ecbf71a45
Huon Wilson <huon@exoflare.io>
2024-02-27 09:39:23 +1100

    Revert python_awslambda extension

--- src/python/pants/backend/awslambda/python/target_types.py
+++ src/python/pants/backend/awslambda/python/target_types.py
@@ -180,7 +180,7 @@ class PythonAWSLambda(_AWSLambdaBaseTarget):
     alias = "python_aws_lambda_function"
 
     deprecated_alias = "python_awslambda"
-    deprecated_alias_removal_version = "2.23.0.dev0"
+    deprecated_alias_removal_version = "2.21.0.dev0"
 
     core_fields = (
         *_AWSLambdaBaseTarget.core_fields,
