932ec702e11bf73e6744523e0e77af722da272b3
sj <suresh@vicarasolutions.com>
2024-03-15 01:53:21 -0400

    Updated zsh and bash files to short-circuit for filesystem completions (huge speedup)

--- src/python/pants/goal/pants-completion.bash
+++ src/python/pants/goal/pants-completion.bash
@@ -1,19 +1,19 @@
 # bash completion support for Pants
 
-function _pants_completions()
-{
+function _pants_completions() {
     local current_word
     current_word=${COMP_WORDS[COMP_CWORD]}
 
-    # Call the complete goal with all of the arguments that we've received so far.
-    COMPREPLY=( $(pants complete -- "${COMP_WORDS[@]}") )
-
-    # If pants complete doesn't provide any completions, fall back to file system completions.
-    if [ ${#COMPREPLY[@]} -eq 0 ]; then
-        COMPREPLY=( $(compgen -f -- "$current_word") )
+    # Check if we're completing a relative (.), absolute (/), or homedir (~) path. If so, fallback to readline (default) completion.
+    # This short-circuits the call to the complete goal, which can take a couple hundred milliseconds to complete
+    if [[ $current_word =~ "^(\.|/|~\/)" ]]; then
+        COMPREPLY=()
+    else 
+        # Call the pants complete goal with all of the arguments that we've received so far.
+        COMPREPLY=( $(pants complete -- "${COMP_WORDS[@]}") )
     fi
 
     return 0
 }
 
-complete -F _pants_completions pants
+complete -o default -F _pants_completions pants
--- src/python/pants/goal/pants-completion.zsh
+++ src/python/pants/goal/pants-completion.zsh
@@ -2,5 +2,17 @@
 
 # zsh completion support for Pants
 function _pants_completions() {
-  compadd $(pants complete -- "${words[@]}")
+    local current_word
+    current_word=${words[CURRENT]}
+
+    # Check if we're completing a relative (.), absolute (/), or homedir (~) path. If so, fallback to readline (default) completion.
+    # This short-circuits the call to the complete goal, which can take a couple hundred milliseconds to complete
+    if [[ $current_word =~ "^(\.|/|~\/)" ]]; then
+        _files
+    else
+        # Call the pants complete goal with all of the arguments that we've received so far.
+        compadd $(pants complete -- "${words[@]}")
+    fi
+
+    return 0
 }
