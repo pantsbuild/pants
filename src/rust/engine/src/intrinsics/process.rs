091cbc95f6b0a5269ba0aaa27332ecb9aacd2f40
Tom Dyas <tom.dyas@gmail.com>
2024-03-17 10:19:17 +0100

    use same year as base file

--- src/rust/engine/src/intrinsics/process.rs
+++ src/rust/engine/src/intrinsics/process.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
+// Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 use std::time::Duration;
