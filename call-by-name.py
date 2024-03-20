d060b66bf84001084a73fee99a1f8d4fc07c2bd7
sj <suresh@vicarasolutions.com>
2024-03-20 00:22:40 -0400

    See issue #20572: Saving this tangled mess of code before I try to YOLO it on the repo

--- call-by-name.py
+++ call-by-name.py
@@ -1,8 +1,13 @@
+# Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
 # On a Mac Mini M2 Pro, parsing the AST of all non-test python files (1184ish) and running in-place source-code replacements on 157 files takes < 1 second
+
+from __future__ import annotations
 
 import ast
 from dataclasses import dataclass
 import fileinput
+import json
 from pathlib import Path
 import tokenize
 
@@ -12,6 +17,113 @@ class Replacement:
     col_range: tuple[int, int]
     current_source: str
     new_source: str
+    new_imports: list[str]
+
+
+with open("migrations.json", "r") as f:
+    data = json.load(f)
+
+def removed_module_prefix(s: str) -> str:
+    return s.split(".")[-1]
+
+def split_module_and_func(s: str) -> tuple[str, str]:
+    """Split the module and function name from a string of the form `module.function`"""
+    parts = s.split(".")
+    return parts[-1], ".".join(parts[:-1])
+
+# The goal is to make a lookup using the output type, and inputs as the key, and the rule_dep as the value
+lookup: dict[tuple[str, ...], str] = {}
+for item in data:
+    for get in item["gets"]:
+        output_type = removed_module_prefix(get["output_type"])
+        input_types = [removed_module_prefix(input_type) for input_type in get["input_types"]]
+        key = (output_type, *tuple(input_types))
+        # key = (get["output_type"].split(".")[-1], *tuple(get["input_types"]))
+        value = get["rule_dep"]
+        if key in lookup:
+            assert f"Duplicate key found in lookup table! {key}"
+        lookup[key] = value
+
+# import pprint  
+# pprint.pprint(lookup)
+
+def map_no_args_get_to_new_syntax(get: ast.Call) -> str:
+    """Get(<OutputType>) -> the_rule_to_call(**implicitly())"""
+    # e.g. Call(func=Name(id='Get', ctx=Load()), args=[Name(id='Browser', ctx=Load())], keywords=[])
+    assert len(get.args) == 1, f"Expected 1 arg, got {len(get.args)}"
+    assert isinstance(get.args[0], ast.Name), f"Expected Name, got {get.args[0]}"
+    key = (get.args[0].id,)
+    new_function = lookup[key]
+    return f"{new_function}(**implicitly())"
+    
+def map_long_form_get_to_new_syntax(get: ast.Call) -> tuple[str,list[str]]:
+    """Get(<OutputType>, <InputType>, input) -> the_rule_to_call(**implicitly(input))"""
+    # Call(func=Name(id='Get', ctx=Load()), 
+    #   args=[
+    #       Name(id='Browser', ctx=Load()), 
+    #       Call(func=Name(id='BrowserRequest', ctx=Load()), args=[
+    #           Call(func=Attribute(value=Name(id='request', ctx=Load()), attr='browser_request', ctx=Load()), args=[], keywords=[])], keywords=[])], keywords=[])
+    assert len(get.args) == 3, f"Expected 3 arg, got {len(get.args)}"
+    assert isinstance(get.args[0], ast.Name), f"Expected Name, got {get.args[0]}"
+    assert isinstance(get.args[1], ast.Name), f"Expected Name, got {get.args[1]}"
+    assert isinstance(get.args[2], ast.Call), f"Expected Call, got {get.args[2]}"
+    key = (get.args[0].id, get.args[1].id)
+    new_function, module = split_module_and_func(lookup[key])
+    input_args = ast.unparse(get.args[2])
+
+    return f"{new_function}(**implicitly({input_args}))", [f"from {module} import {new_function}"]
+
+
+def map_get_to_new_syntax(get: ast.Call) -> Replacement:
+    """There are 4 forms the old Get() syntax can take, so we can account for each one of them individually"""
+    new_source: str | None = None
+    imports: list[str] = []
+    if len(get.args) == 1:
+         new_source = map_no_args_get_to_new_syntax(get)
+    elif len(get.args) == 3:
+        new_source, imports = map_long_form_get_to_new_syntax(get)
+    else:
+        raise NotImplementedError(f"get: {get} not implemented")
+    
+    return Replacement(
+        line_range=(get.lineno, get.end_lineno),
+        col_range=(get.col_offset, get.end_col_offset),
+        current_source=ast.unparse(get),
+        new_source=new_source,
+        new_imports=["from pants.engine.rules import implicitly", *imports]
+    )
+
+# From selectors.py: Get's can be created 4 ways, so need to handle each of them
+
+# Call(func=Name(id='Get', ctx=Load()), args=[Name(id='Browser', ctx=Load()), Dict(keys=[Call(func=Attribute(value=Name(id='request', ctx=Load()), attr='browser_request', ctx=Load()), args=[], keywords=[])], values=[Name(id='BrowserRequest', ctx=Load())])], keywords=[])
+    # + No arguments:
+    #     
+    # Is of the form:
+    #   
+    # And Becomes:
+    #    the_rule_to_call(**implicitly())
+
+    # + Long form:
+    #     Get(<OutputType>, <InputType>, input)
+    # Is of the form:
+    #   Call(func=Name(id='Get', ctx=Load()), args=[Name(id='Browser', ctx=Load()), Name(id='BrowserRequest', ctx=Load()), Call(func=Attribute(value=Name(id='request', ctx=Load()), attr='browser_request', ctx=Load()), args=[], keywords=[])], keywords=[])
+    # Becomes:
+    #     the_rule_to_call(input)
+
+    # + Short form
+    #     Get(<OutputType>, <InputType>(<constructor args for input>))
+    # Is of the form:
+    #   Call(func=Name(id='Get', ctx=Load()), args=[Name(id='Browser', ctx=Load()), Call(func=Name(id='BrowserRequest', ctx=Load()), args=[Call(func=Attribute(value=Name(id='request', ctx=Load()), attr='browser_request', ctx=Load()), args=[], keywords=[])], keywords=[])], keywords=[])
+    # Becomes:
+    #     the_rule_to_call(input)
+
+    # + Dict form
+    #     Get(<OutputType>, {input1: <Input1Type>, ..inputN: <InputNType>})
+    # Is of the form:
+    #   Call(func=Name(id='Get', ctx=Load()), args=[Name(id='Browser', ctx=Load()), Dict(keys=[Call(func=Attribute(value=Name(id='request', ctx=Load()), attr='browser_request', ctx=Load()), args=[], keywords=[])], values=[Name(id='BrowserRequest', ctx=Load())])], keywords=[])
+    # Becomes:
+    #     the_rule_to_call(input1=input1, ..inputN=inputN)
+
 
 class CallByNameVisitor(ast.NodeVisitor):
    
@@ -36,14 +148,10 @@ class CallByNameVisitor(ast.NodeVisitor):
         # In the body, look for `await Get`, and replace it with a call-by-name equivalent
         for child in node.body:
             if call := self._maybe_replaceable_call(child):
-                self.replacements.append(
-                    Replacement(
-                        line_range=(call.lineno, call.end_lineno),
-                        col_range=(call.col_offset, call.end_col_offset),
-                        current_source=ast.unparse(call),
-                        new_source="call_by_some_name(TODO TODO TODO)"
-                    )
-                )
+                replacement = map_get_to_new_syntax(call)
+                self.replacements.append(replacement)
+                print(replacement)
+
 
     def _should_visit_node(self, decorator_list: list[ast.expr]) -> bool:
         """Only interested in async functions with the @rule decorator"""
@@ -66,7 +174,7 @@ class CallByNameVisitor(ast.NodeVisitor):
 def create_replacements_for_file(file: Path) -> list[Replacement]:
     visitor = CallByNameVisitor()
     with open(file, "rb") as f:
-        print(f"Processing {file}")
+        # print(f"Processing {file}")
         try:
             tree = ast.parse(f.read(), filename=file, type_comments=True)        
             visitor.visit(tree)            
@@ -74,7 +182,7 @@ def create_replacements_for_file(file: Path) -> list[Replacement]:
             print(f"SyntaxError in {file}: {e}")
         except tokenize.TokenError as e:
             print(f"TokenError in {file}: {e}")
-        print("\n")
+        # print("\n")
     return visitor.replacements
 
 def perform_replacements_on_file(file: Path, replacements: list[Replacement]):
@@ -88,12 +196,15 @@ def perform_replacements_on_file(file: Path, replacements: list[Replacement]):
             for replacement in replacements:
                 if line_number == replacement.line_range[0]:
                     # On the first line of the range, emit the new source code where the old code started
-                    print(line[:replacement.col_range[0]], end="")
-                    print(replacement.new_source)
-                    modified = True
+                    # print(line[:replacement.col_range[0]], end="")
+                    # print(replacement.new_source)
+                    # modified = True
+                    modified = False
                 elif line_number in range(replacement.line_range[0], replacement.line_range[1] + 1):
                     # If there are other lines in the range, just skip them
-                    modified = True
+                    # modified = True
+                    # TODO: Handle comments
+                    modified = False
                     continue
 
             # For any lines that were not involved with replacements, emit them verbatim
@@ -105,6 +216,12 @@ def perform_replacements_on_file(file: Path, replacements: list[Replacement]):
 files = Path().rglob("*.py")
 for file in files:
     if "_test.py" in file.name:
+        continue
+
+    if not "graphql" in file.parent.name:
+        continue
+
+    if not "rules" in file.name:
         continue
 
     if replacements := create_replacements_for_file(file):
--- src/python/pants/goal/migrate_call_by_name.py
+++ src/python/pants/goal/migrate_call_by_name.py
@@ -3,6 +3,8 @@
 
 from __future__ import annotations
 
+from functools import partial
+import json
 import logging
 
 from pants.base.exiter import ExitCode
@@ -29,9 +31,40 @@ class MigrateCallByNameBuiltinGoal(BuiltinGoal):
         union_membership: UnionMembership,
     ) -> ExitCode:
         # Emit all `@rules` which use non-union Gets.
-        for rule, dependencies in graph_session.scheduler_session.rule_graph_rule_gets().items():
-            print(f"{rule}")
-            for output_type, input_types, rule_dep in dependencies:
-                print(f"  Get({output_type}, {input_types}) -> {rule_dep}")
+        
+        # Create a list of items. Each item is an object using the `rule` module+name as the key.
+        # The value is a list of objects.
+        # Each object has the output_type as a string, the input_types as a list of strings, and the rule_dep as a string.
 
+        items: list[dict[str, list[dict[str, str]]]] = []
+
+        for rule, deps in graph_session.scheduler_session.rule_graph_rule_gets().items():
+            if isinstance(rule, partial):
+                continue
+            # if "get_graphql_uvicorn_setup" in rule.__name__:
+            #     print(f"R: {rule} -- {deps}")
+            key = rule.__module__ + "." + rule.__name__
+            item = { "function": key, "gets": [] }
+
+            # print(f"F: {rule} -- {rule.__name__} -- {rule.__module__} -- {rule.__qualname__}")
+
+            for output_type, input_types, rule_dep in deps:
+                if isinstance(rule_dep, partial):
+                    continue
+                
+                item["gets"].append(
+                    {
+                        "output_type": output_type.__module__ + "." + output_type.__name__,
+                        "input_types": [input_type.__module__ + "." + input_type.__name__ for input_type in input_types],
+                        "rule_dep": rule_dep.__module__ + "." + rule_dep.__name__,
+                    }
+                )
+                # print(f"    O: {output_type} -- {output_type.__name__} -- {output_type.__module__} -- {output_type.__qualname__}")
+                # for input_type in input_types:
+                #     print(f"    I: {input_type} -- {input_type.__name__} -- {input_type.__module__} -- {input_type.__qualname__}")
+                # print(f"    R: {rule_dep} -- {rule_dep.__name__} -- {rule_dep.__module__} -- {rule_dep.__qualname__}")
+
+            items.append(item)
+        
+        print(json.dumps(items, indent=4))
         return 0
