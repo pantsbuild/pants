4ff3d1559fdfd39cc1b62b21da974f484f077f18
sj <suresh@vicarasolutions.com>
2024-03-23 23:17:12 -0400

    Debugging why the integration test isn't kicking off the migrations

--- src/python/pants/goal/migrate_call_by_name_integration_test.py
+++ src/python/pants/goal/migrate_call_by_name_integration_test.py
@@ -3,76 +3,79 @@
 
 from __future__ import annotations
 
-from pathlib import Path
 from textwrap import dedent
 
-from pants.backend.python.util_rules.pex import PexRequest
 from pants.testutil.pants_integration_test import run_pants, setup_tmpdir
-
-# @rule
-# async def get_browser(request: BrowserRequest, open_binary: OpenBinary) -> Browser:
-#     return Browser(open_binary, request.protocol, request.server)
-
-
-# def rules():
-#     return (
-#         *collect_rules(),
-#         QueryRule(ProcessResult, (Process, EnvironmentName)),
-#     )
 
 FILE_1 = dedent(
     """\
-    from pants.engine.rules import Get, rule
+    from pants.backend.python.lint.black.subsystem import Black
+    from pants.backend.python.util_rules.pex import PexRequest, VenvPex
+    from pants.engine.rules import collect_rules, Get, rule
 
     @rule
-    async def setup_foo(subsystem: FooSubsystem) -> Foo:
-        pex = await Get(VenvPex, PexRequest, subsystem.to_pex_request())
+    async def setup_black(black: Black) -> int:
+        pex = await Get(VenvPex, PexRequest, black.to_pex_request())
 
     @rule(desc="Ensure multi-line calls are migrated")
-    async def setup_bar(bar: BarSubsystem) -> Bar:
+    async def setup_black_multiline(black: Black) -> int:
         pex = await Get(
-            VenvPex, 
-            PexRequest, 
-            bar.to_pex_request()
+            VenvPex,
+            PexRequest,
+            black.to_pex_request()
         )
 
     @rule(desc="Ensure calls with embedded comments are ignored")
-    async def setup_baz(baz: BazSubsystem) -> Baz:
+    async def setup_black_embedded_comments(black: Black) -> int:
         pex = await Get(
-            VenvPex, 
-            PexRequest, 
+            VenvPex,
+            PexRequest,
             # Some comment that the AST parse wipes out, so we can't migrate this call safely
-            bar.to_pex_request()
+            black.to_pex_request()
         )
+
+    def rules():
+        return collect_rules()
     """
 )
 
-FILE_2 = dedent(
-    """\
-    from pants.engine.rules import Get, rule
+# FILE_2 = dedent(
+#     """\
+#     from pants.engine.rules import collect_rules, Get, rule
 
-    @rule(desc="Ensure assignment name is not shadowed by new syntax")
-    async def setup_foo(subsystem: FooSubsystem) -> Foo:
-        create_venv_pex = await Get(VenvPex, PexRequest, subsystem.to_pex_request())
-    """
-)
+#     @rule(desc="Ensure assignment name is not shadowed by new syntax")
+#     async def setup_foo(subsystem: FooSubsystem) -> Foo:
+#         create_venv_pex = await Get(VenvPex, PexRequest, subsystem.to_pex_request())
+
+#     def rules():
+#         return collect_rules()
+#     """
+# )
 
 REGISTER_FILE = dedent(
     """\
-    from src.py.app import rules as app_rules
+    from migrateme.app import rules as app_rules
 
     def rules():
         return app_rules()
     """
 )
-                
+
+
 def test_migrate_call_by_name_syntax():
     with setup_tmpdir(
-        {"src/py/register.py": REGISTER_FILE,  "src/py/app.py": FILE_1, "src/py/BUILD": "python_sources()"}
+        {
+            "src/migrateme/__init__.py": "",
+            "src/migrateme/BUILD": "python_sources()",
+            "src/migrateme/register.py": REGISTER_FILE,
+            "src/migrateme/app.py": FILE_1,
+        }
     ) as tmpdir:
         argv = [
-            "--backend-packages=['src/py']",
+            f"--pythonpath=['{tmpdir}/src']",
+            "--backend-packages=['pants.backend.python', 'pants.backend.python.lint.black', 'migrateme']",
             "migrate-call-by-name",
+            "--json",
             f"{tmpdir}::",
         ]
         run_pants(argv).assert_success()
@@ -80,7 +83,6 @@ def test_migrate_call_by_name_syntax():
         print(result.stdout)
         print(result.stderr)
         # print content of FILE_1's changes
-        with open(Path(tmpdir, "src/py/app.py")) as f:
-            print(f.read())
+        # with open(Path(tmpdir, "src/migrateme/app.py")) as f:
+        #     print(f.read())
         assert False
-    
