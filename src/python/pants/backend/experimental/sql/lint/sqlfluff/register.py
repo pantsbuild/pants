57b3b526a232139ca05f70f29dd1748a841ceb69
Borodin Gregory <grihabor@gmail.com>
2024-04-29 11:41:07 +0200

    pants fix src/python/pants/backend/experimental/sql/::

--- src/python/pants/backend/experimental/sql/lint/sqlfluff/register.py
+++ src/python/pants/backend/experimental/sql/lint/sqlfluff/register.py
@@ -1,3 +1,5 @@
+# Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
 from pants.backend.sql.lint.sqlfluff import rules as sqlfluff_rules
 from pants.backend.sql.lint.sqlfluff import skip_field, subsystem
 
