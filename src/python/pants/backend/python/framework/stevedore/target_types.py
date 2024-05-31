213f78cc85e9a812b334b41f68f09a2dac2707fc
Andreas Stenius <andreas.stenius@svenskaspel.se>
2024-05-31 14:14:42 +0200

    Fix help text for `stevedore_namespace` BUILD file symbol.
    Closes #20977
    
    ```
    ❯ pants --backend-packages=pants.backend.experimental.python.framework.stevedore stevedore_namespace --help
    `stevedore_namespace` BUILD file symbol
    ---------------------------------------
    
    Tag a namespace in entry_points as a stevedore namespace.
    
    This is required for the entry_point to be visible to dep inference
    based on the `stevedore_namespaces` field.
    
    For example:
        python_distribution(
            ...
            entry_points={{
                stevedore_namespace("a.b.c"): {{
                    "plugin_name": "some.entry:point",
                }},
            }},
        )
    ```

--- src/python/pants/backend/python/framework/stevedore/target_types.py
+++ src/python/pants/backend/python/framework/stevedore/target_types.py
@@ -11,13 +11,13 @@ from pants.util.strutil import help_text
 
 
 class StevedoreNamespace(str):
-    f"""Tag a namespace in entry_points as a stevedore namespace.
+    """Tag a namespace in entry_points as a stevedore namespace.
 
     This is required for the entry_point to be visible to dep inference
     based on the `stevedore_namespaces` field.
 
     For example:
-        {PythonDistribution.alias}(
+        python_distribution(
             ...
             entry_points={{
                 stevedore_namespace("a.b.c"): {{
