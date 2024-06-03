e7825bfe79fdf4cf344cdef2814ff13a22696824
Brandon Novick <brandon.novick@klaviyo.com>
2024-06-03 11:33:56 -0500

    Inline hashes

--- src/python/pants/backend/tools/trufflehog/subsystem.py
+++ src/python/pants/backend/tools/trufflehog/subsystem.py
@@ -19,15 +19,11 @@ class Trufflehog(ExternalTool):
     default_version = "v3.34.0"
     help = "Trufflehog secrets scanning"
     skip = SkipOption("lint")
-    macos_arm_sha = "19e10e34e95d797cbb924b342b873caa9e71296f9bead28b390d96981f47fbb0"
-    macos_amd_sha = "6a0a425be18ef1b3c0bf5ff88a895e01c1de70892ee48026b9a9ed89ad0398d4"
-    linux_arm_sha = "6aeb5a91dbd981a5446312946072b470dc3a706711c85966394f62717cd71111"
-    linux_amd_sha = "2fda581fb26ed5c866045a4532ce73511f873b70f20eaaae01a7279c3b1c2993"
     default_known_versions = [
-        f"{default_version}|macos_arm64|{macos_arm_sha}|26743363",
-        f"{default_version}|macos_x86_64|{macos_amd_sha}|27466237",
-        f"{default_version}|linux_arm64|{linux_arm_sha}|26021125",
-        f"{default_version}|linux_x86_64|{linux_amd_sha}|27805118",
+        f"{default_version}|macos_arm64|19e10e34e95d797cbb924b342b873caa9e71296f9bead28b390d96981f47fbb0|26743363",
+        f"{default_version}|macos_x86_64|6a0a425be18ef1b3c0bf5ff88a895e01c1de70892ee48026b9a9ed89ad0398d4|27466237",
+        f"{default_version}|linux_arm64|6aeb5a91dbd981a5446312946072b470dc3a706711c85966394f62717cd71111|26021125",
+        f"{default_version}|linux_x86_64|2fda581fb26ed5c866045a4532ce73511f873b70f20eaaae01a7279c3b1c2993|27805118",
     ]
 
     exclude = StrListOption(
