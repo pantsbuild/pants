fdddfaa19dfdecd4e61a74b68c352abc43f22bd9
Andreas Stenius <git@astekk.se>
2024-06-10 10:54:34 +0200

    Update src/python/pants/help/help_info_extracter.py
    Co-authored-by: Daniel Goldman <danielgoldman4@gmail.com>

--- src/python/pants/help/help_info_extracter.py
+++ src/python/pants/help/help_info_extracter.py
@@ -680,6 +680,11 @@ class HelpInfoExtracter:
 
     @staticmethod
     def get_provider(providers: tuple[str, ...] | None, hint: str | None = None) -> str:
+        """
+        Get the best match for the provider.
+        
+        To take advantage of provider names being modules, `hint` should be something like the a subsystem's `__module__` or a rule's `canonical_name`
+        """
         if not providers:
             return ""
         if not hint:
