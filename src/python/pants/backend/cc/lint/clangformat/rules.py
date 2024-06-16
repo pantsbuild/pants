d137bd60d6f2d524e2080f7bec3a3b3e3ca4dda4
sj <suresh@vicarasolutions.com>
2024-06-16 19:55:10 -0400

    Using implicitly to convert from PexProcess to appropriate type

--- src/python/pants/backend/cc/lint/clangformat/rules.py
+++ src/python/pants/backend/cc/lint/clangformat/rules.py
@@ -9,15 +9,18 @@ from typing import Iterable
 
 from pants.backend.cc.lint.clangformat.subsystem import ClangFormat
 from pants.backend.cc.target_types import CCSourceField
-from pants.backend.python.util_rules.pex import create_pex
+from pants.backend.python.util_rules.pex import PexProcess, create_pex
 from pants.core.goals.fmt import FmtResult, FmtTargetsRequest
 from pants.core.util_rules.config_files import find_config_file
 from pants.core.util_rules.partitions import PartitionerType
 from pants.engine.fs import MergeDigests
 from pants.engine.intrinsics import merge_digests_request_to_digest
-from pants.engine.rules import Rule, collect_rules, concurrently, rule
+from pants.engine.process import fallible_to_exec_result_or_raise
+from pants.engine.rules import Rule, collect_rules, concurrently, implicitly, rule
 from pants.engine.target import FieldSet
+from pants.engine.unions import UnionRule
 from pants.util.logging import LogLevel
+from pants.util.strutil import pluralize
 
 logger = logging.getLogger(__name__)
 
@@ -57,12 +60,23 @@ async def clangformat_fmt(request: ClangFormatRequest.Batch, clangformat: ClangF
         *clangformat.args,  # User-added arguments
         *request.files,
     )
-    # result = await fallible_to_exec_result_or_raise(
-    #     PexProcess(clangformat_pex, argv=argv, input_digest=input_digest, output_files=request.files, description=f"Run clang-format on {pluralize(len(request.files), 'file')}.", level=LogLevel.DEBUG), **implicitly())
-    # return await FmtResult.create(request, result)
+    result = await fallible_to_exec_result_or_raise(
+        **implicitly(
+            PexProcess(
+                clangformat_pex,
+                argv=argv,
+                input_digest=input_digest,
+                output_files=request.files,
+                description=f"Run clang-format on {pluralize(len(request.files), 'file')}.",
+                level=LogLevel.DEBUG,
+            )
+        ),
+    )
+
+    return await FmtResult.create(request, result)
 
 
-def rules() -> Iterable[Rule]:
+def rules() -> Iterable[Rule | UnionRule]:
     return (
         *collect_rules(),
         *ClangFormatRequest.rules(),
