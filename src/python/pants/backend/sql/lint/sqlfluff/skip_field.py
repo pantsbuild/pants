147ece22f5c8ed9d011373209d7a9c230f3c6e68
sj <suresh@vicarasolutions.com>
2024-07-02 21:20:00 -0400

    Converting list to tuple

--- src/python/pants/backend/sql/lint/sqlfluff/skip_field.py
+++ src/python/pants/backend/sql/lint/sqlfluff/skip_field.py
@@ -1,7 +1,10 @@
 # Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from typing import Iterable
 from pants.backend.sql.target_types import SqlSourcesGeneratorTarget, SqlSourceTarget
 from pants.engine.target import BoolField
+from pants.engine.unions import UnionRule
 
 
 class SkipSqlfluffField(BoolField):
@@ -10,8 +13,8 @@ class SkipSqlfluffField(BoolField):
     help = "If true, don't run sqlfluff on this target's code."
 
 
-def rules():
-    return [
+def rules() -> Iterable[UnionRule]:
+    return (
         SqlSourcesGeneratorTarget.register_plugin_field(SkipSqlfluffField),
         SqlSourceTarget.register_plugin_field(SkipSqlfluffField),
-    ]
+    )
