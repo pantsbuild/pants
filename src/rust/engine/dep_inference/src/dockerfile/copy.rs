e1cd4fb4cbfb3380860607cfd00866c61e205091
Tobias Nilsson <tobias.jens.nilsson@gmail.com>
2024-07-10 00:40:51 +0200

    feat: Implement a dockerfile parser in rust
    Based off of fork of tree-sitter-dockerfile until
    https://github.com/camdencheek/tree-sitter-dockerfile/pull/58
    is merged.

---
+++ src/rust/engine/dep_inference/src/dockerfile/copy.rs
@@ -0,0 +1,65 @@
+// Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+use crate::code;
+use crate::dockerfile::{ChildBehavior, Visitor};
+use tree_sitter::Node;
+
+#[derive(Debug, Eq, PartialEq)]
+pub(crate) enum CopiedFile<'a> {
+    Path(&'a str),
+    Arg(&'a str),
+}
+
+pub(crate) struct CopyFileCollector<'a> {
+    pub files: Vec<CopiedFile<'a>>,
+    is_from: bool,
+    code: &'a str,
+}
+
+impl<'a> CopyFileCollector<'a> {
+    pub fn new(code: &'a str) -> Self {
+        Self {
+            code,
+            is_from: false,
+            files: Vec::default(),
+        }
+    }
+}
+
+impl Visitor for CopyFileCollector<'_> {
+    fn visit_variable(&mut self, node: Node) -> ChildBehavior {
+        if self.is_from {
+            ChildBehavior::Ignore
+        } else if let Some(CopiedFile::Path(_)) = self.files.last() {
+            self.replace_last_path_with_encountered_variable_name(node)
+        } else {
+            ChildBehavior::Visit
+        }
+    }
+
+    fn visit_path(&mut self, node: Node) -> ChildBehavior {
+        if self.is_from {
+            return ChildBehavior::Ignore;
+        }
+        self.files.push(CopiedFile::Path(code::at(self.code, node)));
+        ChildBehavior::Visit
+    }
+
+    fn visit_param(&mut self, node: Node) -> ChildBehavior {
+        if code::at(self.code, node).contains("--from") {
+            self.is_from = true;
+            ChildBehavior::Ignore
+        } else {
+            ChildBehavior::Visit
+        }
+    }
+}
+
+impl CopyFileCollector<'_> {
+    /// Because a variable "ARG" is always wrapped in a path "$ARG".
+    fn replace_last_path_with_encountered_variable_name(&mut self, node: Node) -> ChildBehavior {
+        self.files.pop();
+        self.files.push(CopiedFile::Arg(code::at(self.code, node)));
+        ChildBehavior::Ignore
+    }
+}
