b84aa5ace057b6d14a1de6478c03bab234d1ad85
Tobias Nilsson <tobias.jens.nilsson@gmail.com>
2024-07-11 18:58:12 +0200

    feat: Enable usage of new native parser

--- src/python/pants/backend/docker/subsystems/dockerfile_parser.py
+++ src/python/pants/backend/docker/subsystems/dockerfile_parser.py
@@ -15,6 +15,8 @@ from pants.backend.python.util_rules import pex
 from pants.backend.python.util_rules.pex import PexRequest, VenvPex, VenvPexProcess
 from pants.engine.addresses import Address
 from pants.engine.fs import CreateDigest, Digest, FileContent
+from pants.engine.internals.native_engine import NativeDependenciesRequest
+from pants.engine.intrinsics import parse_dockerfile_info
 from pants.engine.process import Process, ProcessResult
 from pants.engine.rules import Get, collect_rules, rule
 from pants.engine.target import (
@@ -24,8 +26,11 @@ from pants.engine.target import (
     WrappedTarget,
     WrappedTargetRequest,
 )
+from pants.option.option_types import BoolOption
+from pants.util.docutil import bin_name
 from pants.util.logging import LogLevel
 from pants.util.resources import read_resource
+from pants.util.strutil import softwrap
 
 _DOCKERFILE_SANDBOX_TOOL = "dockerfile_wrapper_script.py"
 _DOCKERFILE_PACKAGE = "pants.backend.docker.subsystems"
@@ -40,6 +45,23 @@ class DockerfileParser(PythonToolRequirementsBase):
     register_interpreter_constraints = True
 
     default_lockfile_resource = (_DOCKERFILE_PACKAGE, "dockerfile.lock")
+
+    use_rust_parser = BoolOption(
+        default=False,
+        help=softwrap(
+            f"""
+            Use the new Rust-based, multithreaded, in-process dependency parser.
+
+            If you think the new behaviour is causing problems, it is recommended that you run
+            `{bin_name()} peek :: > before.json` and then
+            `{bin_name()} --dockerfile-parser-use-rust-parser=False peek :: > after.json` and compare the
+            two results.
+
+            If you think there is a bug, please file an issue:
+            https://github.com/pantsbuild/pants/issues/new/choose.
+            """
+        ),
+    )
 
 
 @dataclass(frozen=True)
@@ -119,8 +141,59 @@ class DockerfileInfoRequest:
     address: Address
 
 
+async def _natively_parse_dockerfile(address: Address, digest: Digest) -> DockerfileInfo:
+    result = await parse_dockerfile_info(NativeDependenciesRequest(digest))
+    return DockerfileInfo(
+        address=address,
+        digest=digest,
+        source=result.source,
+        build_args=DockerBuildArgs.from_strings(*result.build_args, duplicates_must_match=True),
+        copy_source_paths=tuple(result.copy_source_paths),
+        copy_build_args=DockerBuildArgs.from_strings(
+            *result.copy_build_args, duplicates_must_match=True
+        ),
+        from_image_build_args=DockerBuildArgs.from_strings(
+            *result.from_image_build_args, duplicates_must_match=True
+        ),
+        version_tags=tuple(result.version_tags),
+    )
+
+
+async def _legacy_parse_dockerfile(
+    address: Address, digest: Digest, dockerfiles: tuple[str, ...]
+) -> DockerfileInfo:
+    result = await Get(ProcessResult, DockerfileParseRequest(digest, dockerfiles))
+
+    try:
+        raw_output = result.stdout.decode("utf-8")
+        outputs = json.loads(raw_output)
+        assert len(outputs) == len(dockerfiles)
+    except Exception as e:
+        raise DockerfileInfoError(
+            f"Unexpected failure to parse Dockerfiles: {', '.join(dockerfiles)}, "
+            f"for the {address} target: {e}\nDockerfile parser output:\n{raw_output}"
+        ) from e
+    info = outputs[0]
+    return DockerfileInfo(
+        address=address,
+        digest=digest,
+        source=info["source"],
+        build_args=DockerBuildArgs.from_strings(*info["build_args"], duplicates_must_match=True),
+        copy_source_paths=tuple(info["copy_source_paths"]),
+        copy_build_args=DockerBuildArgs.from_strings(
+            *info["copy_build_args"], duplicates_must_match=True
+        ),
+        from_image_build_args=DockerBuildArgs.from_strings(
+            *info["from_image_build_args"], duplicates_must_match=True
+        ),
+        version_tags=tuple(info["version_tags"]),
+    )
+
+
 @rule
-async def parse_dockerfile(request: DockerfileInfoRequest) -> DockerfileInfo:
+async def parse_dockerfile(
+    request: DockerfileInfoRequest, dockerfile_parser: DockerfileParser
+) -> DockerfileInfo:
     wrapped_target = await Get(
         WrappedTarget, WrappedTargetRequest(request.address, description_of_origin="<infallible>")
     )
@@ -139,46 +212,16 @@ async def parse_dockerfile(request: DockerfileInfoRequest) -> DockerfileInfo:
         f"Internal error: Expected a single source file to Dockerfile parse request {request}, "
         f"got: {dockerfiles}."
     )
-
-    result = await Get(
-        ProcessResult,
-        DockerfileParseRequest(
-            sources.snapshot.digest,
-            dockerfiles,
-        ),
-    )
-
     try:
-        raw_output = result.stdout.decode("utf-8")
-        outputs = json.loads(raw_output)
-        assert len(outputs) == len(dockerfiles)
-    except Exception as e:
-        raise DockerfileInfoError(
-            f"Unexpected failure to parse Dockerfiles: {', '.join(dockerfiles)}, "
-            f"for the {request.address} target: {e}\nDockerfile parser output:\n{raw_output}"
-        ) from e
-
-    info = outputs[0]
-    try:
-        return DockerfileInfo(
-            address=request.address,
-            digest=sources.snapshot.digest,
-            source=info["source"],
-            build_args=DockerBuildArgs.from_strings(
-                *info["build_args"], duplicates_must_match=True
-            ),
-            copy_source_paths=tuple(info["copy_source_paths"]),
-            copy_build_args=DockerBuildArgs.from_strings(
-                *info["copy_build_args"], duplicates_must_match=True
-            ),
-            from_image_build_args=DockerBuildArgs.from_strings(
-                *info["from_image_build_args"], duplicates_must_match=True
-            ),
-            version_tags=tuple(info["version_tags"]),
-        )
+        if dockerfile_parser.use_rust_parser:
+            return await _natively_parse_dockerfile(target.address, sources.snapshot.digest)
+        else:
+            return await _legacy_parse_dockerfile(
+                target.address, sources.snapshot.digest, dockerfiles
+            )
     except ValueError as e:
         raise DockerfileInfoError(
-            f"Error while parsing {info['source']} for the {request.address} target: {e}"
+            f"Error while parsing {dockerfiles[0]} for the {request.address} target: {e}"
         ) from e
 
 
