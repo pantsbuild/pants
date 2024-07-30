39a1e6ad9c666ea2a2154b56c6bd45396b2bb959
Tom Dyas <tom.dyas@gmail.com>
2024-07-30 10:50:49 -0400

    fix watch API typos

--- src/rust/engine/src/nodes/mod.rs
+++ src/rust/engine/src/nodes/mod.rs
@@ -370,27 +370,29 @@ impl NodeKey {
 
     async fn maybe_watch(&self, context: &Context) -> NodeResult<()> {
         if let Some(path) = self.fs_path_to_watch() {
-            let use_local_system_watcher =
-                path.is_absolute() && !path.starts_with(&context.core.build_root);
-            if use_local_system_watcher {
-                if let Some(watcher) = context.core.local_system_watcher.as_ref() {
-                    watcher
-                        .watch(path.to_path_buf())
-                        .map_err(|e| Context::mk_error(&e))
-                        .await
-                } else {
-                    Ok(())
-                }
+            let (watcher, abs_path) =
+                match path.is_absolute() && !path.starts_with(&context.core.build_root) {
+                    true => (
+                        context.core.local_system_watcher.as_ref(),
+                        path.to_path_buf(),
+                    ),
+                    false => {
+                        let abs_path = if path.starts_with(&context.core.build_root) {
+                            path.to_path_buf()
+                        } else {
+                            context.core.build_root.join(path)
+                        };
+                        (context.core.buildroot_watcher.as_ref(), abs_path)
+                    }
+                };
+
+            if let Some(watcher) = watcher {
+                watcher
+                    .watch(abs_path)
+                    .map_err(|e| Context::mk_error(&e))
+                    .await
             } else {
-                if let Some(watcher) = context.core.buildroot_watcher.as_ref() {
-                    let abs_path = context.core.build_root.join(path);
-                    watcher
-                        .watch(abs_path)
-                        .map_err(|e| Context::mk_error(&e))
-                        .await
-                } else {
-                    Ok(())
-                }
+                Ok(())
             }
         } else {
             Ok(())
