53b82ab27039a037406ece6999b7964de8ac833c
Daniel Goldman <danielgoldman4@gmail.com>
2024-07-31 23:35:57 -0400

    add test that we passed platforms to `providers lock`

--- src/python/pants/backend/terraform/testutil.py
+++ src/python/pants/backend/terraform/testutil.py
@@ -135,6 +135,7 @@ provider "registry.terraform.io/hashicorp/null" {
   version     = "3.2.2"
   constraints = "~> 3.2.0, <= 3.2.2"
   hashes = [
+    "h1:Gef5VGfobY5uokA5nV/zFvWeMNR2Pmq79DH94QnNZPM=",
     "h1:zT1ZbegaAYHwQa+QwIFugArWikRJI9dqohj8xb0GY88=",
     "zh:3248aae6a2198f3ec8394218d05bd5e42be59f43a3a7c0b71c66ec0df08b69e7",
     "zh:32b1aaa1c3013d33c245493f4a65465eab9436b454d250102729321a44c8ab9a",
