dda934b8cc67fae348135b721c50d4f10fe126c0
Jacob Floyd <cognifloyd@gmail.com>
2024-08-08 20:33:37 -0500

    nfpm: add missing 'from future import __annotations__'

--- src/python/pants/backend/nfpm/dependency_inference.py
+++ src/python/pants/backend/nfpm/dependency_inference.py
@@ -1,5 +1,8 @@
 # Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 from pants.backend.nfpm.fields.scripts import NfpmPackageScriptsField
--- src/python/pants/backend/nfpm/target_types_rules.py
+++ src/python/pants/backend/nfpm/target_types_rules.py
@@ -1,6 +1,8 @@
 # Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import annotations
+
 from pants.backend.nfpm.fields.contents import (
     NfpmContentDirDstField,
     NfpmContentDirsField,
