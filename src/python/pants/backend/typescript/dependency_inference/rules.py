86bd9208dfb761f89a256d5e35e6bcf995d118b3
Alexey Tereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2024-08-09 17:22:31 +0200

    Refactor to support merged changes adding TS config support

--- src/python/pants/backend/typescript/dependency_inference/rules.py
+++ src/python/pants/backend/typescript/dependency_inference/rules.py
@@ -14,23 +14,27 @@ from pants.backend.javascript.dependency_inference.rules import (
     RequestNodePackagesCandidateMap,
 )
 from pants.backend.javascript.package_json import (
-    OwningNodePackage,
     OwningNodePackageRequest,
+    PackageJsonImports,
     PackageJsonSourceField,
+    find_owning_package,
+    subpath_imports_for_source,
 )
 from pants.backend.javascript.subsystems.nodejs_infer import NodeJSInfer
+from pants.backend.typescript import tsconfig
 from pants.backend.typescript.target_types import (
     TS_FILE_EXTENSIONS,
     TypeScriptDependenciesField,
     TypeScriptSourceField,
 )
+from pants.backend.typescript.tsconfig import ParentTSConfigRequest, TSConfig, find_parent_ts_config
 from pants.build_graph.address import Address
 from pants.engine.addresses import Addresses
 from pants.engine.internals.graph import Owners, OwnersRequest
 from pants.engine.internals.native_dep_inference import NativeParsedJavascriptDependencies
 from pants.engine.internals.native_engine import InferenceMetadata, NativeDependenciesRequest
-from pants.engine.internals.selectors import Get, MultiGet
-from pants.engine.rules import Rule, collect_rules, rule
+from pants.engine.internals.selectors import Get, MultiGet, concurrently
+from pants.engine.rules import Rule, collect_rules, implicitly, rule
 from pants.engine.target import (
     FieldSet,
     HydratedSources,
@@ -62,12 +66,37 @@ class InferTypeScriptDependenciesRequest(InferDependenciesRequest):
     infer_from = TypeScriptSourceInferenceFieldSet
 
 
-async def _prepare_inference_metadata(address: Address) -> InferenceMetadata:
-    owning_pkg = await Get(OwningNodePackage, OwningNodePackageRequest(address))
+def _create_inference_metadata(
+    imports: PackageJsonImports, config: TSConfig | None
+) -> InferenceMetadata:
+    return InferenceMetadata.javascript(
+        imports.root_dir,
+        dict(imports.imports),
+        config.resolution_root_dir if config else None,
+        dict(config.paths or {}) if config else {},
+    )
+
+
+async def _prepare_inference_metadata(address: Address, file_path: str) -> InferenceMetadata:
+    owning_pkg, maybe_config = await concurrently(
+        find_owning_package(OwningNodePackageRequest(address)),
+        find_parent_ts_config(ParentTSConfigRequest(file_path, "tsconfig.json"), **implicitly()),
+    )
+
     if not owning_pkg.target:
-        return InferenceMetadata.javascript(address.spec_path, {})
-    return await Get(
-        InferenceMetadata, PackageJsonSourceField, owning_pkg.target[PackageJsonSourceField]
+        return InferenceMetadata.javascript(
+            (
+                os.path.dirname(maybe_config.ts_config.path)
+                if maybe_config.ts_config
+                else address.spec_path
+            ),
+            {},
+            maybe_config.ts_config.resolution_root_dir if maybe_config.ts_config else None,
+            dict(maybe_config.ts_config.paths or {}) if maybe_config.ts_config else {},
+        )
+    return _create_inference_metadata(
+        await subpath_imports_for_source(owning_pkg.target[PackageJsonSourceField]),
+        maybe_config.ts_config,
     )
 
 
@@ -98,7 +127,7 @@ async def infer_typescript_source_dependencies(
     sources = await Get(
         HydratedSources, HydrateSourcesRequest(source, for_sources_types=[TypeScriptSourceField])
     )
-    metadata = await _prepare_inference_metadata(request.field_set.address)
+    metadata = await _prepare_inference_metadata(request.field_set.address, source.file_path)
 
     import_strings = await Get(
         NativeParsedJavascriptDependencies,
@@ -136,6 +165,7 @@ def rules() -> Iterable[Rule | UnionRule]:
     return [
         *collect_rules(),
         *package_json.rules(),
+        *tsconfig.rules(),
         UnionRule(InferDependenciesRequest, InferNodePackageDependenciesRequest),
         UnionRule(InferDependenciesRequest, InferTypeScriptDependenciesRequest),
     ]
