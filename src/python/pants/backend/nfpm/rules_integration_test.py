d237b36dbded1bc35cceb9f878a8577bbfb1224a
Jacob Floyd <cognifloyd@gmail.com>
2024-08-13 17:46:34 -0500

    Revert "nfpm: drop tests that cannot run w/o nfpm_*_package targets"
    This reverts commit 3e1a88d4da86da66b5b097b20c509b4823e5cd9b.

---
+++ src/python/pants/backend/nfpm/rules_integration_test.py
@@ -0,0 +1,222 @@
+# Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+from textwrap import dedent
+from typing import Any, ContextManager, cast
+
+import pytest
+from _pytest.mark import ParameterSet
+
+from pants.backend.nfpm.dependency_inference import rules as nfpm_dependency_inference_rules
+from pants.backend.nfpm.field_sets import NFPM_PACKAGE_FIELD_SET_TYPES, NfpmPackageFieldSet
+from pants.backend.nfpm.rules import rules as nfpm_rules
+from pants.backend.nfpm.subsystem import rules as nfpm_subsystem_rules
+from pants.backend.nfpm.target_types import target_types as nfpm_target_types
+from pants.backend.nfpm.target_types_rules import rules as nfpm_target_types_rules
+from pants.core.goals.package import BuiltPackage
+from pants.core.target_types import FilesGeneratorTarget, FileTarget
+from pants.core.target_types import rules as core_target_type_rules
+from pants.engine.internals.native_engine import Address
+from pants.engine.internals.scheduler import ExecutionError
+from pants.engine.target import Target
+from pants.testutil.pytest_util import no_exception
+from pants.testutil.rule_runner import QueryRule, RuleRunner
+
+
+@pytest.fixture
+def rule_runner() -> RuleRunner:
+    rule_runner = RuleRunner(
+        target_types=[
+            FileTarget,
+            FilesGeneratorTarget,
+            *nfpm_target_types(),
+        ],
+        rules=[
+            *core_target_type_rules(),
+            *nfpm_subsystem_rules(),
+            *nfpm_target_types_rules(),
+            *nfpm_dependency_inference_rules(),
+            *nfpm_rules(),
+            *(
+                QueryRule(BuiltPackage, [field_set_type])
+                for field_set_type in NFPM_PACKAGE_FIELD_SET_TYPES
+            ),
+        ],
+    )
+    return rule_runner
+
+
+def build_package(
+    rule_runner: RuleRunner,
+    binary_target: Target,
+    field_set_type: type[NfpmPackageFieldSet],
+) -> BuiltPackage:
+    field_set = field_set_type.create(binary_target)
+    result = rule_runner.request(BuiltPackage, [field_set])
+    rule_runner.write_digest(result.digest)
+    return result
+
+
+def _assert_one_built_artifact(
+    pkg_name: str, built_package: BuiltPackage, field_set_type: type[NfpmPackageFieldSet]
+) -> None:
+    assert len(built_package.artifacts) == 1
+    artifact = built_package.artifacts[0]
+    relpath = artifact.relpath or ""
+    assert relpath.endswith(field_set_type.extension)
+    assert relpath.startswith(f"{pkg_name}/{pkg_name}")
+
+
+_pkg_name = "pkg"
+_pkg_version = "3.2.1"
+
+_test_cases: tuple[ParameterSet, ...] = ()  # TODO: add packagers
+
+
+@pytest.mark.skip("no nfpm_*_package targets available yet")
+@pytest.mark.parametrize("field_set_type,extra_metadata,valid_target", _test_cases)
+def test_generate_package_without_contents(
+    rule_runner: RuleRunner,
+    field_set_type: type[NfpmPackageFieldSet],
+    extra_metadata: dict[str, Any],
+    valid_target: bool,
+) -> None:
+    packager = field_set_type.packager
+    # do not use scripts for this test.
+    extra_metadata = {key: value for key, value in extra_metadata.items() if key != "scripts"}
+    rule_runner.write_files(
+        {
+            "BUILD": dedent(
+                f"""
+                nfpm_{packager}_package(
+                    name="{_pkg_name}",
+                    package_name="{_pkg_name}",
+                    version="{_pkg_version}",
+                    **{extra_metadata}
+                )
+                """
+            ),
+        }
+    )
+
+    # noinspection DuplicatedCode
+    with cast(ContextManager, no_exception()) if valid_target else pytest.raises(ExecutionError):
+        binary_tgt = rule_runner.get_target(Address("", target_name="pkg"))
+    if not valid_target:
+        return
+
+    built_package = build_package(rule_runner, binary_tgt, field_set_type)
+    _assert_one_built_artifact(_pkg_name, built_package, field_set_type)
+
+
+@pytest.mark.skip("no nfpm_*_package targets available yet")
+@pytest.mark.parametrize("field_set_type,extra_metadata,valid_target", _test_cases)
+def test_generate_package_with_contents(
+    rule_runner: RuleRunner,
+    field_set_type: type[NfpmPackageFieldSet],
+    extra_metadata: dict[str, Any],
+    valid_target: bool,
+) -> None:
+    packager = field_set_type.packager
+    scripts = extra_metadata.get("scripts", {})
+    rule_runner.write_files(
+        {
+            "BUILD": dedent(
+                f"""
+                nfpm_{packager}_package(
+                    name="{_pkg_name}",
+                    package_name="{_pkg_name}",
+                    version="{_pkg_version}",
+                    dependencies=[
+                        "contents:files",
+                        "contents:file",
+                        "contents:symlinks",
+                        "contents:symlink",
+                        "contents:dirs",
+                        "contents:dir",
+                    ],
+                    **{extra_metadata}
+                )
+                if {bool(scripts)}:
+                    files(
+                        name="scripts",
+                        sources={list(scripts.values())},
+                    )
+                """
+            ),
+            **{path: "" for path in scripts.values()},
+            "contents/BUILD": dedent(
+                f"""
+                file(
+                    name="sandbox_file",
+                    source="sandbox-file.txt",
+                )
+                nfpm_content_files(
+                    name="files",
+                    files=[
+                        ("sandbox-file.txt", "/usr/share/{_pkg_name}/{_pkg_name}.{_pkg_version}/installed-file.txt"),
+                        ("sandbox-file.txt", "/etc/{_pkg_name}/installed-file.txt"),
+                    ],
+                    dependencies=[":sandbox_file"],
+                    overrides={{
+                        "/etc/{_pkg_name}/installed-file.txt": dict(
+                            content_type="config",
+                            file_mode="rw-------",  # same as 0o600 and "600"
+                            file_group="root",
+                        ),
+                    }},
+                    content_type="",
+                    file_owner="root",
+                    file_group="{_pkg_name}",
+                    file_mode="644",  # same as 0o644 and "rw-r--r--"
+                )
+                nfpm_content_file(
+                    name="file",
+                    source="some-executable",
+                    dst="/usr/bin/some-executable",
+                    file_mode=0o755,  # same as "755" and "rwxr-xr-x"
+                )
+                nfpm_content_symlinks(
+                    name="symlinks",
+                    symlinks=(
+                        ("some-executable", "/usr/bin/new-relative-symlinked-exe"),
+                        ("/usr/bin/some-executable", "/usr/bin/new-absolute-symlinked-exe"),
+                    ),
+                    overrides={{
+                        "/usr/bin/new-relative-symlinked-exe": dict(file_group="special-group"),
+                    }},
+                )
+                nfpm_content_symlink(
+                    name="symlink",
+                    src="/usr/bin/some-executable",
+                    dst="/usr/sbin/sbin-executable",
+                )
+                nfpm_content_dirs(
+                    name="dirs",
+                    dirs=["/usr/share/{_pkg_name}"],
+                    overrides={{
+                        "/usr/share/{_pkg_name}": dict(file_group="special-group"),
+                    }},
+                )
+                nfpm_content_dir(
+                    name="dir",
+                    dst="/etc/{_pkg_name}",
+                    file_mode=0o700,
+                )
+                """
+            ),
+            "contents/sandbox-file.txt": "",
+            "contents/some-executable": "",
+        }
+    )
+
+    # noinspection DuplicatedCode
+    with cast(ContextManager, no_exception()) if valid_target else pytest.raises(ExecutionError):
+        binary_tgt = rule_runner.get_target(Address("", target_name=_pkg_name))
+    if not valid_target:
+        return
+
+    built_package = build_package(rule_runner, binary_tgt, field_set_type)
+    _assert_one_built_artifact(_pkg_name, built_package, field_set_type)
--- src/python/pants/backend/nfpm/util_rules/sandbox_test.py
+++ src/python/pants/backend/nfpm/util_rules/sandbox_test.py
@@ -3,14 +3,45 @@
 
 from __future__ import annotations
 
+import dataclasses
+from textwrap import dedent
+from typing import ContextManager, cast
+
 import pytest
 
+from pants.backend.nfpm.dependency_inference import rules as nfpm_dependency_inference_rules
 from pants.backend.nfpm.field_sets import NfpmPackageFieldSet
-from pants.backend.nfpm.target_types import NfpmContentDir, NfpmContentFile, NfpmContentSymlink
-from pants.backend.nfpm.util_rules.sandbox import _DepCategory
-from pants.core.target_types import ArchiveTarget, FileTarget, GenericTarget, ResourceTarget
+from pants.backend.nfpm.target_types import (
+    NfpmContentDir,
+    NfpmContentFile,
+    NfpmContentFiles,
+    NfpmContentSymlink,
+)
+from pants.backend.nfpm.target_types_rules import rules as nfpm_target_types_rules
+from pants.backend.nfpm.util_rules.sandbox import (
+    NfpmContentSandbox,
+    NfpmContentSandboxRequest,
+    _DepCategory,
+)
+from pants.backend.nfpm.util_rules.sandbox import rules as nfpm_sandbox_rules
+from pants.core.target_types import (
+    ArchiveTarget,
+    FilesGeneratorTarget,
+    FileSourceField,
+    FileTarget,
+    GenericTarget,
+    ResourceTarget,
+)
+from pants.core.target_types import rules as core_target_type_rules
 from pants.engine.addresses import Address
-from pants.engine.target import Target
+from pants.engine.fs import CreateDigest, DigestContents, DigestEntries
+from pants.engine.internals.native_engine import EMPTY_DIGEST, Digest, Snapshot
+from pants.engine.internals.selectors import Get
+from pants.engine.rules import QueryRule, rule
+from pants.engine.target import GeneratedSources, GenerateSourcesRequest, SingleSourceField, Target
+from pants.engine.unions import UnionRule
+from pants.testutil.pytest_util import no_exception
+from pants.testutil.rule_runner import RuleRunner
 
 # _NfpmSortedDeps.sort(...)
 
@@ -47,3 +78,183 @@ def test_dep_category_for_target(
 ):
     category = _DepCategory.for_target(tgt, field_set_type)
     assert category == expected
+
+
+class MockCodegenSourceField(SingleSourceField):
+    pass
+
+
+class MockCodegenTarget(Target):
+    alias = "codegen_target"
+    core_fields = (MockCodegenSourceField,)
+    help = "n/a"
+
+
+class MockCodegenGenerateSourcesRequest(GenerateSourcesRequest):
+    input = MockCodegenSourceField
+    output = FileSourceField
+
+
+@rule
+async def do_codegen(request: MockCodegenGenerateSourcesRequest) -> GeneratedSources:
+    # Generate a file with the same contents as each input file.
+    input_files = await Get(DigestContents, Digest, request.protocol_sources.digest)
+    generated_files = [
+        dataclasses.replace(input_file, path=input_file.path + ".generated")
+        for input_file in input_files
+    ]
+    result = await Get(Snapshot, CreateDigest(generated_files))
+    return GeneratedSources(result)
+
+
+@pytest.fixture
+def rule_runner() -> RuleRunner:
+    rule_runner = RuleRunner(
+        target_types=[
+            ArchiveTarget,
+            FileTarget,
+            FilesGeneratorTarget,
+            NfpmContentFile,
+            NfpmContentFiles,
+            MockCodegenTarget,
+        ],
+        rules=[
+            *core_target_type_rules(),
+            *nfpm_target_types_rules(),
+            *nfpm_dependency_inference_rules(),
+            *nfpm_sandbox_rules(),
+            QueryRule(NfpmContentSandbox, (NfpmContentSandboxRequest,)),
+            QueryRule(DigestEntries, (Digest,)),
+            do_codegen,
+            UnionRule(GenerateSourcesRequest, MockCodegenGenerateSourcesRequest),
+        ],
+    )
+    rule_runner.set_options([], env_inherit={"PATH", "PYENV_ROOT", "HOME"})
+    return rule_runner
+
+
+@pytest.mark.skip("no nfpm_*_package targets available yet")
+@pytest.mark.parametrize(
+    "packager,field_set_type,dependencies,scripts,expected",
+    (),  # TODO: add packagers
+)
+def test_populate_nfpm_content_sandbox(
+    rule_runner: RuleRunner,
+    packager: str,
+    field_set_type: type[NfpmPackageFieldSet],
+    dependencies: list[str],
+    scripts: dict[str, str],
+    expected: set[str] | ContextManager,
+):
+    rule_runner.write_files(
+        {
+            "BUILD": dedent(
+                f"""
+                nfpm_{packager}_package(
+                    name="{_pkg_name}",
+                    package_name="{_pkg_name}",
+                    version="{_pkg_version}",
+                    dependencies={repr(dependencies)},
+                    scripts={repr(scripts)},
+                )
+                """
+            ),
+            "codegen/BUILD": dedent(
+                """
+                codegen_target(
+                    source="./foobar.codegen",
+                )
+                nfpm_content_file(
+                    name="generated",
+                    src="foobar.codegen.generated",
+                    dst="/usr/lib/foobar.codegen.generated",
+                    dependencies=["./foobar.codegen"],
+                )
+                """
+            ),
+            "codegen/foobar.codegen": "",
+            "package/BUILD": dedent(
+                """
+                file(
+                    name="file",
+                    source="archive-contents.txt",
+                )
+                archive(
+                    name="archive",
+                    format="tar",
+                    files=[":file"],
+                )
+                nfpm_content_file(
+                    name="package",
+                    src="archive.tar",
+                    dst="/opt/foo/archive.tar",
+                    dependencies=[":archive"],
+                )
+                """
+            ),
+            "package/archive-contents.txt": "",
+            "contents/BUILD": dedent(
+                f"""
+                file(
+                    name="unrelated_file",
+                    source="should.not.be.in.digest.txt",
+                )
+                file(
+                    name="sandbox_file",
+                    source="sandbox-file.txt",
+                )
+                nfpm_content_files(
+                    name="files",
+                    files=[
+                        ("sandbox-file.txt", "/usr/share/{_pkg_name}/{_pkg_name}.{_pkg_version}/installed-file.txt"),
+                        ("sandbox-file.txt", "/etc/{_pkg_name}/installed-file.txt"),
+                    ],
+                    dependencies=[":sandbox_file"],
+                )
+                nfpm_content_file(
+                    name="file",
+                    source="some-executable",
+                    dst="/usr/bin/some-executable",
+                )
+                """
+            ),
+            "contents/sandbox-file.txt": "",
+            "contents/some-executable": "",
+            "scripts/BUILD": dedent(
+                """
+                files(
+                    name="scripts",
+                    sources=["*", "!BUILD"],
+                )
+                """
+            ),
+            **{
+                path: ""
+                for path in [
+                    "scripts/postinstall.sh",
+                ]
+            },
+        }
+    )
+
+    target = rule_runner.get_target(Address("", target_name=_pkg_name))
+
+    with cast(ContextManager, no_exception()) if isinstance(expected, set) else expected:
+        result = rule_runner.request(
+            NfpmContentSandbox,
+            [
+                NfpmContentSandboxRequest(field_set=field_set_type.create(target)),
+            ],
+        )
+
+    if not isinstance(expected, set):
+        # error was raised, nothing else to check
+        return
+
+    if not expected:
+        assert result.digest == EMPTY_DIGEST
+        return
+
+    digest_entries = rule_runner.request(DigestEntries, (result.digest,))
+    paths = {entry.path for entry in digest_entries}
+    assert paths == expected
