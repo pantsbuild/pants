4f17ebfbc10226758f5f734495e7689de5f37cde
SJ <suresh@vicarasolutions.com>
2024-08-31 08:19:59 -0400

    [Call-by-name] JVM linters (#21368)
    Auto-migrated the JVM linters to call-by-name

--- src/python/pants/backend/java/lint/google_java_format/rules.py
+++ src/python/pants/backend/java/lint/google_java_format/rules.py
@@ -9,14 +9,13 @@ from pants.backend.java.target_types import JavaSourceField
 from pants.core.goals.fmt import FmtResult, FmtTargetsRequest
 from pants.core.goals.resolves import ExportableTool
 from pants.core.util_rules.partitions import PartitionerType
-from pants.engine.internals.selectors import Get
-from pants.engine.process import ProcessResult
-from pants.engine.rules import collect_rules, rule
+from pants.engine.process import fallible_to_exec_result_or_raise
+from pants.engine.rules import collect_rules, implicitly, rule
 from pants.engine.target import FieldSet, Target
 from pants.engine.unions import UnionRule
 from pants.jvm.jdk_rules import InternalJdk, JvmProcess
 from pants.jvm.resolve import jvm_tool
-from pants.jvm.resolve.coursier_fetch import ToolClasspath, ToolClasspathRequest
+from pants.jvm.resolve.coursier_fetch import ToolClasspathRequest, materialize_classpath_for_tool
 from pants.jvm.resolve.jvm_tool import GenerateJvmLockfileFromTool
 from pants.util.logging import LogLevel
 from pants.util.strutil import pluralize
@@ -48,7 +47,9 @@ async def google_java_format_fmt(
     jdk: InternalJdk,
 ) -> FmtResult:
     lockfile_request = GenerateJvmLockfileFromTool.create(tool)
-    tool_classpath = await Get(ToolClasspath, ToolClasspathRequest(lockfile=lockfile_request))
+    tool_classpath = await materialize_classpath_for_tool(
+        ToolClasspathRequest(lockfile=lockfile_request)
+    )
 
     toolcp_relpath = "__toolcp"
     extra_immutable_input_digests = {
@@ -73,20 +74,21 @@ async def google_java_format_fmt(
         *request.files,
     ]
 
-    result = await Get(
-        ProcessResult,
-        JvmProcess(
-            jdk=jdk,
-            argv=args,
-            classpath_entries=tool_classpath.classpath_entries(toolcp_relpath),
-            input_digest=request.snapshot.digest,
-            extra_jvm_options=tool.jvm_options,
-            extra_immutable_input_digests=extra_immutable_input_digests,
-            extra_nailgun_keys=extra_immutable_input_digests,
-            output_files=request.files,
-            description=f"Run Google Java Format on {pluralize(len(request.files), 'file')}.",
-            level=LogLevel.DEBUG,
-        ),
+    result = await fallible_to_exec_result_or_raise(
+        **implicitly(
+            JvmProcess(
+                jdk=jdk,
+                argv=args,
+                classpath_entries=tool_classpath.classpath_entries(toolcp_relpath),
+                input_digest=request.snapshot.digest,
+                extra_jvm_options=tool.jvm_options,
+                extra_immutable_input_digests=extra_immutable_input_digests,
+                extra_nailgun_keys=extra_immutable_input_digests,
+                output_files=request.files,
+                description=f"Run Google Java Format on {pluralize(len(request.files), 'file')}.",
+                level=LogLevel.DEBUG,
+            ),
+        )
     )
     return await FmtResult.create(request, result)
 
