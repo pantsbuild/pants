be16de0881e57d2395e03015560e942615669c97
Chris Burroughs <chris.burroughs@gmail.com>
2024-09-10 15:34:19 -0400

    Revert "mypy fix redux"
    This reverts commit 11286a15f1b3e0067fd12f8a4d0df1be1f2e7422.

--- src/python/pants/backend/python/typecheck/mypy/subsystem.py
+++ src/python/pants/backend/python/typecheck/mypy/subsystem.py
@@ -73,7 +73,7 @@ class MyPy(PythonToolBase):
     # Temporary upper bounds until one or both of:
     # https://github.com/pantsbuild/pants/pull/21158
     # https://github.com/pantsbuild/pants/pull/21387
-    default_requirements = ["mypy[python2]>=0.961,<1.5"]
+    default_requirements = ["mypy>=0.961,<1.5"]
 
     # See `mypy/rules.py`. We only use these default constraints in some situations.
     register_interpreter_constraints = True
