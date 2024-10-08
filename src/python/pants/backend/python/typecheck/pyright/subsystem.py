dfdb833e068e13aaf41ff83f874e0fcb862a721d
Krishnan Chandra <krishnan.chandra@gmail.com>
2024-10-08 12:53:30 -0400

    Upgrade default Pyright version to 1.1.383

--- src/python/pants/backend/python/typecheck/pyright/subsystem.py
+++ src/python/pants/backend/python/typecheck/pyright/subsystem.py
@@ -20,7 +20,7 @@ class Pyright(NodeJSToolBase):
         """
     )
 
-    default_version = "pyright@1.1.365"
+    default_version = "pyright@1.1.383"
 
     skip = SkipOption("check")
     args = ArgsListOption(example="--version")
