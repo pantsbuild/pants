071c9e456fb4b79552f20d2bdaf30563a7044a78
Tom Dyas <tom.dyas@gmail.com>
2024-10-08 19:22:30 -0400

    simplfy the Cargo.toml

--- src/rust/engine/src/externs/interface.rs
+++ src/rust/engine/src/externs/interface.rs
@@ -504,7 +504,7 @@ struct PyExecutionRequest(RefCell<ExecutionRequest>);
 #[pymethods]
 impl PyExecutionRequest {
     #[new]
-    #[pyo3(signature = (poll, poll_delay_in_ms=None, timeout_in_ms=None))]
+    #[pyo3(signature = (*, poll, poll_delay_in_ms, timeout_in_ms))]
     fn __new__(poll: bool, poll_delay_in_ms: Option<u64>, timeout_in_ms: Option<u64>) -> Self {
         let request = ExecutionRequest {
             poll,
@@ -702,7 +702,21 @@ fn hash_prefix_zero_bits(item: &str) -> u32 {
 /// affect the created Scheduler.
 ///
 #[pyfunction]
-#[pyo3(signature = (py_executor, py_tasks, types_ptr, build_root, local_execution_root_dir, named_caches_dir, ignore_patterns, use_gitignore, watch_filesystem, remoting_options, local_store_options, exec_strategy_opts, ca_certs_path=None))]
+#[pyo3(signature = (
+    py_executor,
+    py_tasks,
+    types_ptr,
+    build_root,
+    local_execution_root_dir,
+    named_caches_dir,
+    ignore_patterns,
+    use_gitignore,
+    watch_filesystem,
+    remoting_options,
+    local_store_options,
+    exec_strategy_opts,
+    ca_certs_path
+))]
 fn scheduler_create(
     py_executor: &Bound<'_, externs::scheduler::PyExecutor>,
     py_tasks: &Bound<'_, PyTasks>,
@@ -1925,7 +1939,7 @@ fn stdio_thread_set_destination(stdio_destination: &Bound<'_, PyStdioDestination
 
 // TODO: Needs to be thread-local / associated with the Console.
 #[pyfunction]
-#[pyo3(signature = (log_path=None))]
+#[pyo3(signature = (log_path))]
 fn set_per_run_log_path(py: Python, log_path: Option<PathBuf>) {
     py.allow_threads(|| {
         PANTS_LOGGER.set_per_run_logs(log_path);
