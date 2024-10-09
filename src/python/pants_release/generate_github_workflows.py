7515ce2cf81fb38bc595dba3167d36f4a5b1ee7b
Benjy Weinberger <benjyw@gmail.com>
2024-10-09 21:01:08 +0200

    Upgrade rust cache action. (#21511)

--- src/python/pants_release/generate_github_workflows.py
+++ src/python/pants_release/generate_github_workflows.py
@@ -38,7 +38,7 @@ def action(name: str, node16_compat: bool = False) -> str:
             "download-artifact": "actions/download-artifact@v4",
             "expose-pythons": "pantsbuild/actions/expose-pythons@v9",
             "github-action-required-labels": "mheap/github-action-required-labels@v4.0.0",
-            "rust-cache": "benjyw/rust-cache@461b9f8eee66b575bce78977bf649b8b7a8d53f1",
+            "rust-cache": "benjyw/rust-cache@61b5b2e17a28350779e9a535e353da2f8b00e832",
             "setup-go": "actions/setup-go@v5",
             "setup-java": "actions/setup-java@v4",
             "setup-node": "actions/setup-node@v4",
