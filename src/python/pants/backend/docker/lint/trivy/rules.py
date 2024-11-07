8495fcca8af7296bd450e376a0e31131acc1d8b0
Daniel Goldman <danielgoldman4@gmail.com>
2024-11-07 00:08:24 -0500

    first pass at running Trivy on Docker targets

---
+++ src/python/pants/backend/docker/lint/trivy/rules.py
@@ -0,0 +1,74 @@
+# Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+from dataclasses import dataclass
+from typing import Any
+
+from pants.backend.docker.subsystems.docker_options import DockerOptions
+from pants.backend.docker.target_types import DockerImageSourceField
+from pants.backend.terraform.lint.trivy.rules import SkipTrivyField
+from pants.backend.tools.trivy.rules import run_trivy, RunTrivyRequest
+from pants.backend.tools.trivy.subsystem import Trivy
+from pants.core.goals.lint import LintTargetsRequest, LintResult
+from pants.core.goals.package import EnvironmentAwarePackageRequest, PackageFieldSet, BuiltPackage
+from pants.core.goals.test import BuiltPackageDependencies, BuildPackageDependenciesRequest
+from pants.core.util_rules.partitions import PartitionerType
+from pants.core.util_rules.source_files import determine_source_files, SourceFilesRequest
+from pants.engine.addresses import Addresses
+from pants.engine.internals.native_engine import Digest, EMPTY_DIGEST, Address
+from pants.engine.internals.selectors import Get, MultiGet
+from pants.engine.process import FallibleProcessResult, Process
+from pants.engine.rules import rule, collect_rules
+from pants.engine.target import FieldSet, Target, Targets, FieldSetsPerTarget, FieldSetsPerTargetRequest
+from pants.util.logging import LogLevel
+
+
+@dataclass(frozen=True)
+class TrivyDockerFieldSet(FieldSet):
+    required_fields = (DockerImageSourceField,)
+
+    source: DockerImageSourceField
+
+    @classmethod
+    def opt_out(cls, tgt: Target) -> bool:
+        return False
+        return tgt.get(SkipTrivyField).value
+
+
+class TrivyDockerRequest(LintTargetsRequest):
+    field_set_type = TrivyDockerFieldSet
+    tool_subsystem = Trivy
+    partitioner_type = PartitionerType.DEFAULT_ONE_PARTITION_PER_INPUT
+
+
+@rule(desc="Lint Docker image with Trivy", level=LogLevel.DEBUG)
+async def run_trivy_docker(
+    request: TrivyDockerRequest.Batch[TrivyDockerRequest, Any],
+) -> LintResult:
+
+    assert len(request.elements) == 1, "not single element in partition"  # "Do we need to?"
+    addrs = tuple(e.address for e in request.elements)
+
+    tgts = await Get(Targets, Addresses(addrs))
+
+    field_sets_per_tgt = await Get(
+        FieldSetsPerTarget, FieldSetsPerTargetRequest(PackageFieldSet, tgts)
+    )
+    [field_set] = field_sets_per_tgt.field_sets
+
+    package = await Get(BuiltPackage, EnvironmentAwarePackageRequest(field_set))
+    r = await run_trivy(RunTrivyRequest(
+        command="image",
+        scanners=(),
+        target=package.artifacts[0].image_id,
+        input_digest=EMPTY_DIGEST,
+        description=f"Run Trivy on docker image {','.join(package.artifacts[0].tags)}"
+    ))
+
+    return LintResult.create(request, r)
+
+
+def rules():
+    return (
+        *collect_rules(),
+        *TrivyDockerRequest.rules()
+    )
---
+++ src/python/pants/backend/tools/trivy/rules.py
@@ -0,0 +1,87 @@
+# Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from pants.backend.tools.trivy.subsystem import Trivy
+from pants.core.goals.lint import LintResult
+from pants.core.goals.resolves import ExportableTool
+from pants.core.util_rules.external_tool import DownloadedExternalTool, ExternalToolRequest, download_external_tool
+from pants.engine.env_vars import EnvironmentVars, EnvironmentVarsRequest
+from pants.engine.internals.native_engine import Digest
+from pants.engine.internals.selectors import Get
+from pants.engine.intrinsics import execute_process
+from pants.engine.platform import Platform
+from pants.engine.process import Process, FallibleProcessResult
+from pants.engine.rules import rule, collect_rules
+from pants.engine.unions import UnionRule
+from pants.util.logging import LogLevel
+
+
+@dataclass(frozen=True)
+class RunTrivyRequest:
+    # trivy fields
+    command: str
+    scanners: tuple[str, ...]
+    target: str
+    # pants fields
+    input_digest: Digest
+    description: str
+
+
+# TODO: caching https://aquasecurity.github.io/trivy/v0.17.2/examples/cache/
+# TODO: capture output file?
+# TODO: config files
+# TODO: report format
+# TODO: exit code options
+
+@rule
+async def run_trivy(
+        request: RunTrivyRequest,
+        trivy: Trivy,
+        platform: Platform,
+) -> FallibleProcessResult:
+    """Run Trivy"""
+    argv = ["__trivy/trivy", "-d", "--format=table", "--exit-code=1"]
+
+    argv.extend(["--db-repository", "ghcr.io/aquasecurity/trivy-db,public.ecr.aws/aquasecurity/trivy-db"])
+
+    argv.append(request.command)
+
+    if request.scanners:
+        argv.append("--scanners")
+        argv.append(",".join(request.scanners))
+
+    argv.append(request.target)
+
+    argv.extend(trivy.args)
+
+    download_trivy = await download_external_tool(trivy.get_request(platform))
+
+    env = await Get(EnvironmentVars, EnvironmentVarsRequest(trivy.extra_env_vars))
+
+
+    immutable_input_digests = {
+        "__trivy": download_trivy.digest
+    }
+
+    result = await execute_process(
+        Process(
+        argv=tuple(argv),
+        input_digest=request.input_digest,
+        immutable_input_digests=immutable_input_digests,
+        env=env,
+        description=request.description,
+        level=LogLevel.DEBUG,
+    ))
+    return result
+
+
+def rules():
+    from pants.core.util_rules import external_tool
+    return (
+        *collect_rules(),
+        *external_tool.rules(),
+        UnionRule(ExportableTool, Trivy),
+    )
