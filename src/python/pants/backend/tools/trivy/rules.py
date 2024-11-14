c70341ab855e438904762b6682753a4c148decb0
Daniel Goldman <danielgoldman4@gmail.com>
2024-11-13 22:41:32 -0500

    load config files

--- src/python/pants/backend/tools/trivy/rules.py
+++ src/python/pants/backend/tools/trivy/rules.py
@@ -7,11 +7,12 @@ from dataclasses import dataclass
 from pants.backend.tools.trivy.subsystem import Trivy
 from pants.core.goals.resolves import ExportableTool
 from pants.core.util_rules import external_tool
+from pants.core.util_rules.config_files import find_config_file
 from pants.core.util_rules.external_tool import download_external_tool
 from pants.engine.env_vars import EnvironmentVars, EnvironmentVarsRequest
-from pants.engine.internals.native_engine import Digest
+from pants.engine.internals.native_engine import Digest, MergeDigests
 from pants.engine.internals.selectors import Get
-from pants.engine.intrinsics import execute_process
+from pants.engine.intrinsics import execute_process, merge_digests
 from pants.engine.platform import Platform
 from pants.engine.process import FallibleProcessResult, Process
 from pants.engine.rules import collect_rules, rule
@@ -31,7 +32,6 @@ class RunTrivyRequest:
 
 
 # TODO: capture output file?
-# TODO: config files
 # TODO: report format
 # TODO: exit code options
 
@@ -43,7 +43,7 @@ async def run_trivy(
     platform: Platform,
 ) -> FallibleProcessResult:
     """Run Trivy."""
-    argv = ["__trivy/trivy", "-d", "--format=table", "--exit-code=1"]
+    argv = ["__trivy/trivy", "-d", "--exit-code=1"]
 
     # workaround for Trivy DB being overloaded on pulls
     argv.extend(
@@ -51,6 +51,10 @@ async def run_trivy(
     )
 
     argv.extend(["--cache-dir", trivy.cache_dir])
+
+    config_file = await find_config_file(trivy.config_request())
+    if trivy.config:
+        argv.extend(["--config", trivy.config])
 
     argv.append(request.command)
 
@@ -70,10 +74,14 @@ async def run_trivy(
 
     immutable_input_digests = {"__trivy": download_trivy.digest}
 
+    input_digest = await merge_digests(
+        MergeDigests((request.input_digest, config_file.snapshot.digest))
+    )
+
     result = await execute_process(
         Process(
             argv=tuple(argv),
-            input_digest=request.input_digest,
+            input_digest=input_digest,
             immutable_input_digests=immutable_input_digests,
             append_only_caches=trivy.append_only_caches,
             env=env,
