f428d3743403b9d2b6791508167065b4773db4ff
Tom Dyas <tom.dyas@gmail.com>
2024-11-20 12:26:19 -0500

    generate_urls.py improvements

--- src/python/pants/backend/python/providers/python_build_standalone/scripts/generate_urls.py
+++ src/python/pants/backend/python/providers/python_build_standalone/scripts/generate_urls.py
@@ -2,19 +2,19 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 from __future__ import annotations
 
+import argparse
 import hashlib
 import itertools
 import json
 import os
+import re
 from pathlib import Path
 
 import github
 import requests
 from github.GitReleaseAsset import GitReleaseAsset
 
-from pants.base.build_environment import get_buildroot
-
-VERSIONS_PATH = get_buildroot() / Path(
+VERSIONS_PATH = Path(
     "src/python/pants/backend/python/providers/python_build_standalone/versions_info.json"
 )
 
@@ -22,6 +22,8 @@ VERSIONS_PATH = get_buildroot() / Path(
 def _github():
     # generate with `gh auth token`
     token = os.environ.get("GH_TOKEN")
+    if token is None:
+        print("WARNING: No GitHub token configured in GH_TOKEN. Lower rate limits will apply!")
     return github.Github(auth=github.Auth.Token(token) if token else None)
 
 
@@ -37,19 +39,37 @@ def _compute_sha256(url):
 
 
 def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--scrape-all-releases", dest="scrape_all_releases", action="store_true")
+    parser.add_argument(
+        "--scrape-release", metavar="RELEASE", dest="scrape_releases", action="append"
+    )
+    options = parser.parse_args()
+
+    print("Starting to scrape GitHub PBS releases.")
+    if not VERSIONS_PATH.parent.exists():
+        raise Exception("This helper script must be run from the root of the Pants repository.")
+
     versions_info = json.loads(VERSIONS_PATH.read_text())
     scraped_releases = set(versions_info["scraped_releases"])
 
     github = _github()
     pbs_repo = github.get_repo("indygreg/python-build-standalone")
+    print("Downloading PBS release metadata.")
     releases = pbs_repo.get_releases()
+    print("Downloaded PBS release metadata.")
 
     asset_map: dict[str, GitReleaseAsset] = {}
     sha256_map: dict[str, str] = {}
     for release in releases.reversed:
         tag_name = release.tag_name
 
-        if tag_name not in scraped_releases:
+        if (
+            tag_name not in scraped_releases
+            or options.scrape_all_releases
+            or tag_name in options.scrape_releases
+        ):
+            print(f"Scraping release tag `{tag_name}`.")
             scraped_releases.add(release.tag_name)
             assets = release.get_assets()
             for asset in assets:
@@ -72,10 +92,18 @@ def main() -> None:
                 else:
                     asset_map[asset.name] = asset
 
+    print("Finished scraping releases.")
+
     versions_info["scraped_releases"] = sorted(scraped_releases)
     pythons_dict = versions_info["pythons"]
+    asset_matcher = re.compile(r"^([a-zA-Z0-9]+)-([0-9.]+)\+([0-9.]+)-")
+
     for asset in asset_map.values():
-        python_version = asset.name.split("+")[0].split("-")[1]
+        matched_versions = asset_matcher.match(asset.name)
+        if not matched_versions:
+            continue
+
+        python_version, pbs_release_tag = matched_versions.groups()[1:3]
         if python_version not in pythons_dict:
             pythons_dict[python_version] = {}
 
