42753dead662f4bcccca278c22e9d44f22eeeab8
Daniel Goldman <danielgoldman4@gmail.com>
2024-11-27 15:52:58 -0500

    trivy can lint helm charts too

---
+++ src/python/pants/backend/experimental/helm/lint/trivy/register.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+from pants.backend.experimental.helm.register import rules as helm_rules
+from pants.backend.tools.trivy.rules import rules as trivy_rules
+from pants.backend.helm.lint.trivy.rules import rules as trivy_helm_rules
+
+
+def rules():
+    return (
+        *helm_rules(),
+        *trivy_rules(),
+        *trivy_helm_rules(),
+    )
--- src/python/pants/backend/helm/lint/trivy/register.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
-# Licensed under the Apache License, Version 2.0 (see LICENSE).
-from pants.backend.helm.lint.trivy.rules import rules as trivy_rules
-
-
-def rules():
-    return trivy_rules()
