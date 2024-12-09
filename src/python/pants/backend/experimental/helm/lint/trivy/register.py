0e9e878cd22b3a226adc9b3282014766c41dd382
Daniel Goldman <danielgoldman4@gmail.com>
2024-12-08 23:26:03 -0500

    rebuild trivy validation to use actual subcommand for parsing output

--- src/python/pants/backend/experimental/helm/lint/trivy/register.py
+++ src/python/pants/backend/experimental/helm/lint/trivy/register.py
@@ -1,8 +1,8 @@
 # Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 from pants.backend.experimental.helm.register import rules as helm_rules
-from pants.backend.tools.trivy.rules import rules as trivy_rules
 from pants.backend.helm.lint.trivy.rules import rules as trivy_helm_rules
+from pants.backend.tools.trivy.rules import rules as trivy_rules
 
 
 def rules():
