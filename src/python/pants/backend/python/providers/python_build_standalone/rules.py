345eacdd986db6a5e9082d46e6bbce94fcfe5baf
Tom Dyas <tom@shoalshoftware.com>
2024-12-09 16:24:22 -0500

    empty string - mypy

--- src/python/pants/backend/python/providers/python_build_standalone/rules.py
+++ src/python/pants/backend/python/providers/python_build_standalone/rules.py
@@ -169,10 +169,10 @@ class PBSPythonProviderSubsystem(Subsystem):
             if pyversion not in all_pythons:
                 all_pythons[pyversion] = {}
 
-            # Note: Tag is set `None` which means this version will always match the release constraint.
+            # Note: Tag is set "" which means this version will always match the release constraint.
             # TODO: Infer the release tag from the URL.
             all_pythons[pyversion][platform] = PBSPythonInfo(
-                url=url, sha256=sha256, size=int(filesize), tag=None
+                url=url, sha256=sha256, size=int(filesize), tag=""
             )
 
         return all_pythons
