f3bbf0958b11280e683372a67aad0c49bbcaa248
CJ Steiner <clintonsteiner@gmail.com>
2024-12-17 20:07:30 -0600

    ci: update python to use a matrix and have correct ordering
    * Introduced in
      https://github.com/pantsbuild/pants/commit/e18484d94ae835ddd7407bfba67c04c472785fb4#diff-8f5723a712281f34ef9e6ab0a15737801bd4c72972b4d95d83fb8e95f4eb7b6aR18
    * Previously only using a single python version here
    * Also, kept 3.7 and 3.8 in to be less controversial but those are both
      EOL and could be dropped off if we decide to keep other versions in

--- src/python/pants_release/generate_github_workflows.py
+++ src/python/pants_release/generate_github_workflows.py
@@ -73,7 +73,7 @@ CARGO_AUDIT_IGNORED_ADVISORY_IDS = (
 # We don't specify a patch version so that we get the latest, which comes pre-installed:
 #  https://github.com/actions/setup-python#available-versions-of-python
 # NOTE: The last entry becomes the default
-_BASE_PYTHON_VERSIONS = ["3.7", "3.8", "3.9", "3.10", "3.12", "3.13", "3.11"]
+_BASE_PYTHON_VERSIONS = ["3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13"]
 
 PYTHON_VERSIONS_PER_PLATFORM = {
     Platform.LINUX_X86_64: _BASE_PYTHON_VERSIONS,
