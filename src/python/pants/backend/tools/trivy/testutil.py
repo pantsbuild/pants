e2de6de60f05b17f055b89d17302ef65cafc1dbe
Daniel Goldman <danielgoldman4@gmail.com>
2024-12-18 22:31:28 -0500

    improve assertions

--- src/python/pants/backend/tools/trivy/testutil.py
+++ src/python/pants/backend/tools/trivy/testutil.py
@@ -2,20 +2,37 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 import json
 
+from pants.core.goals.lint import LintResult
+
 trivy_config = """
 format: json
 """
 
 
 def assert_trivy_output(
-    result, expected_exit_code: int, target: str, scanner_type: str, expected_error_count: int
+    result: LintResult,
+    expected_exit_code: int,
+    target: str,
+    scanner_type: str,
+    expected_error_count: int,
 ):
-    assert result.exit_code == expected_exit_code
-    report = json.loads(result.stdout)
+    if result.exit_code != expected_exit_code:
+        raise AssertionError(
+            f"Trivy process had incorrect exit code, expected={expected_exit_code}, actual={result.exit_code}, stdout={result.stdout}, stderr={result.stderr}"
+        )
+
+    try:
+        report = json.loads(result.stdout)
+    except json.decoder.JSONDecodeError as e:
+        raise AssertionError(
+            f"Trivy output could not be parsed as JSON, stdout={result.stdout=}, stderr={result.stderr}"
+        ) from e
+
     findings_by_target = {res["Target"]: res for res in report["Results"]}
     assert (
         target in findings_by_target
     ), f"Did not find expected file in results, target={target} files={list(findings_by_target.keys())}"
+
     if scanner_type == "config":
         found_count = findings_by_target[target]["MisconfSummary"]["Failures"]
         assert (
@@ -26,3 +43,15 @@ def assert_trivy_output(
         assert (
             found_count == expected_error_count
         ), f"Did not find expected vulnerabilities found={found_count} expected={expected_error_count}"
+
+
+def assert_trivy_success(result: LintResult):
+    if result.exit_code != 0:
+        raise AssertionError(f"Trivy process was not successful, stdout={result.stdout}")
+
+    try:
+        json.loads(result.stdout)
+    except json.decoder.JSONDecodeError as e:
+        raise AssertionError(
+            f"Trivy output could not be parsed as JSON, stdout={result.stdout}, stderr={result.stderr}"
+        ) from e
