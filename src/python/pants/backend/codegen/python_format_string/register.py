422a551283f2177ca99d7af255f977cc29847619
Borodin Gregory <grihabor@gmail.com>
2025-01-12 10:42:50 +0100

    Copy internal python_format_string backend

---
+++ src/python/pants/backend/codegen/python_format_string/register.py
@@ -0,0 +1,9 @@
+from experimental.codegen.python_format_string import target_types as python_format_string_target_types
+
+
+def target_types():
+    return python_format_string_target_types.target_types()
+
+
+def rules():
+    pass
---
+++ src/python/pants/backend/codegen/python_format_string/target_types.py
@@ -0,0 +1,64 @@
+import logging
+from typing import Optional
+
+from pants.core.goals.package import OutputPathField
+from pants.engine.target import (
+    COMMON_TARGET_FIELDS,
+    DictStringToStringField,
+    MultipleSourcesField,
+    SingleSourceField,
+    Target,
+    TargetFilesGenerator,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class PythonFormatStringSourceField(SingleSourceField):
+    pass
+
+
+class PythonFormatStringValuesField(DictStringToStringField):
+    alias = "values"
+    required = True
+
+
+class PythonFormatStringOutputPathField(OutputPathField):
+    def value_or_default(self, *, file_ending: Optional[str]) -> str:
+        if self.address.is_generated_target:
+            if self.address.is_parametrized:
+                return f"{self.address.filename}.{file_ending}{self.address.parameters_repr}"
+            return f"{self.address.filename}.{file_ending}"
+        return super().value_or_default(file_ending=file_ending)
+
+
+class PythonFormatStringTarget(Target):
+    alias = "python_format_string"
+    core_fields = (
+        *COMMON_TARGET_FIELDS,
+        PythonFormatStringSourceField,
+        PythonFormatStringValuesField,
+        PythonFormatStringOutputPathField,
+    )
+
+
+class PythonFormatStringsSourcesField(MultipleSourcesField):
+    pass
+
+
+class PythonFormatStringTargetGenerator(TargetFilesGenerator):
+    alias = "python_format_strings"
+    generated_target_cls = PythonFormatStringTarget
+    core_fields = (
+        *COMMON_TARGET_FIELDS,
+        PythonFormatStringsSourcesField,
+    )
+    copied_fields = COMMON_TARGET_FIELDS
+    moved_fields = (PythonFormatStringValuesField,)
+
+
+def target_types():
+    return (
+        PythonFormatStringTarget,
+        PythonFormatStringTargetGenerator,
+    )
