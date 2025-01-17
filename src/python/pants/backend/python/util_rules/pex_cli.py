31cc9965678ee754815d54c61eba41c6fb20fc3f
Tom Dyas <tom@shoalsoftware.com>
2025-01-16 22:26:23 -0500

    pex: upgrade to v2.29.0

--- src/python/pants/backend/python/util_rules/pex_cli.py
+++ src/python/pants/backend/python/util_rules/pex_cli.py
@@ -42,7 +42,7 @@ class PexCli(TemplatedExternalTool):
     name = "pex"
     help = "The PEX (Python EXecutable) tool (https://github.com/pex-tool/pex)."
 
-    default_version = "v2.27.1"
+    default_version = "v2.29.0"
     default_url_template = "https://github.com/pex-tool/pex/releases/download/{version}/pex"
     version_constraints = ">=2.13.0,<3.0"
 
@@ -65,8 +65,8 @@ class PexCli(TemplatedExternalTool):
                 (
                     cls.default_version,
                     plat,
-                    "013a824e5af50f9687f765a43e8ffe94b4faa4fe795d017333c687bf943a4a68",
-                    "4369121",
+                    "8307cb6f5ce09f82e4f5e7858428237cb6440fc91fcb723dc1e09cb2d57e2c2f",
+                    "4370181",
                 )
             )
             for plat in ["macos_arm64", "macos_x86_64", "linux_x86_64", "linux_arm64"]
