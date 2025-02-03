7821be78ccd83a9720f93eb67212da2b08128ac1
Benjy Weinberger <benjyw@gmail.com>
2025-02-02 20:31:49 -0800

    A test to prove that @fromfile invalidation works now. (#21901)
    See https://github.com/pantsbuild/pants/issues/10360

--- src/python/pants/init/options_initializer.py
+++ src/python/pants/init/options_initializer.py
@@ -135,7 +135,7 @@ class OptionsInitializer:
     TODO: We would eventually like to use the bootstrap Scheduler to construct the
     OptionsBootstrapper as well, but for now we do the opposite thing, and the Scheduler is
     used only to resolve plugins.
-      see: https://github.com/pantsbuild/pants/issues/10360
+      see https://github.com/pantsbuild/pants/pull/11568
     """
 
     def __init__(
