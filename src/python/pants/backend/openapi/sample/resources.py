3d9e927eb80175b6875c681bc69571d3887e4e36
Tom Dyas <tom@shoalsoftware.com>
2025-03-05 12:02:17 -0500

    remove deprecated usage

--- src/python/pants/backend/openapi/sample/resources.py
+++ src/python/pants/backend/openapi/sample/resources.py
@@ -1,8 +1,8 @@
 # Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-import importlib.resources as pkg_resources
+import importlib.resources
 
 from pants.backend.openapi import sample
 
-PETSTORE_SAMPLE_SPEC = pkg_resources.read_text(sample.__name__, "petstore_spec.yaml")
+PETSTORE_SAMPLE_SPEC = importlib.resources.files(sample.__name__).joinpath("petstore_spec.yaml").read_text()
