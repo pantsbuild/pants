1ffb9175147708dfa78285d9070bf9983a840948
Borodin Gregory <grihabor@gmail.com>
2025-03-09 21:29:00 +0100

    Upgrade external tools

--- src/python/pants/backend/cue/subsystem.py
+++ src/python/pants/backend/cue/subsystem.py
@@ -25,14 +25,18 @@ class Cue(TemplatedExternalTool):
         Homepage: https://cuelang.org/
         """
     )
-    default_version = "v0.4.3"
+    default_version = "0.12.0"
     default_known_versions = [
-        "v0.4.3|macos_x86_64|1161254cf38b928b87a7ac1552dc2e12e6c5da298f9ce370d80e5518ddb6513d|6240316",
-        "v0.4.3|macos_arm64 |3d84b85a7288f94301a4726dcf95b2d92c8ff796c4d45c4733fbdcc04ceaf21d|5996085",
-        "v0.4.3|linux_x86_64|5e7ecb614b5926acfc36eb1258800391ab7c6e6e026fa7cacbfe92006bac895c|6037013",
-        "v0.4.3|linux_arm64 |a8c3f4140d18c324cc69f5de4df0566e529e1636cff340095a42475799bf3fed|5548404",
+        "0.12.0|macos_x86_64|8474e522a978ecadef49b06d706ff276cd07629b1aa107b88adfc1284d3f93cc|8902127",
+        "0.12.0|macos_arm64|7055a6423f753c8ea763699d48d78d341e8543397399daee281c66ecdc9ec5a5|8400578",
+        "0.12.0|linux_x86_64|e55cd5abd98a592c110f87a7da9ef15bc72515200aecfe1bed04bf86311f5ba1|8731479",
+        "0.12.0|linux_arm64|488012bb0e5c080e2a9694ef8765403dd1075a4ec373dda618efa2d37b47f14f|8067539",
+        "0.4.3|macos_x86_64|1161254cf38b928b87a7ac1552dc2e12e6c5da298f9ce370d80e5518ddb6513d|6240316",
+        "0.4.3|macos_arm64|3d84b85a7288f94301a4726dcf95b2d92c8ff796c4d45c4733fbdcc04ceaf21d|5996085",
+        "0.4.3|linux_x86_64|5e7ecb614b5926acfc36eb1258800391ab7c6e6e026fa7cacbfe92006bac895c|6037013",
+        "0.4.3|linux_arm64|a8c3f4140d18c324cc69f5de4df0566e529e1636cff340095a42475799bf3fed|5548404"
     ]
-    default_url_template = "https://github.com/cue-lang/cue/releases/download/{version}/cue_{version}_{platform}.tar.gz"
+    default_url_template = "https://github.com/cue-lang/cue/releases/download/v{version}/cue_v{version}_{platform}.tar.gz"
     default_url_platform_mapping = {
         "macos_arm64": "darwin_arm64",
         "macos_x86_64": "darwin_amd64",
