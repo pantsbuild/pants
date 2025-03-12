24e92e4bb2d4fb804f2a14ab94cd0e6ca4a0b4e9
Benjy Weinberger <benjyw@gmail.com>
2025-03-11 20:57:34 -0700

    Refactor children out into a separate crate. (#22076)
    So it can be easily reused without introducing
    possibly circular deps on process_execution.

--- src/rust/engine/process_execution/children/src/lib.rs
+++ src/rust/engine/process_execution/children/src/lib.rs
@@ -1,5 +1,6 @@
 // Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 use std::ops::{Deref, DerefMut};
 use std::{thread, time};
 
