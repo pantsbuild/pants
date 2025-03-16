475f318791d74ef74b90a351e397c00211dbe69a
Benjy Weinberger <benjyw@gmail.com>
2025-03-16 16:58:31 -0700

    Improve CI change classifier.

--- build-support/bin/classify_changed_files_test.py
+++ build-support/bin/classify_changed_files_test.py
@@ -2,28 +2,31 @@
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 import pytest
-from classify_changed_files import Affected, classify
+from classify_changed_files import ChangeLabel, classify
 
 
 @pytest.mark.parametrize(
     ["changed_files", "expected"],
     (
-        [["docs/path/to/some/doc", "docs/path/to/some/other/doc"], {Affected.docs}],
+        [
+            ["docs/path/to/some/doc", "docs/path/to/some/other/doc"],
+            {ChangeLabel.docs, ChangeLabel.no_code},
+        ],
         [
             ["README.md", "path/to/some/dir/README.md"],
-            {Affected.docs},
+            {ChangeLabel.docs, ChangeLabel.no_code},
         ],
         [
             ["docs/notes/2.16.x.md"],
-            {Affected.docs, Affected.notes},
+            {ChangeLabel.docs, ChangeLabel.notes, ChangeLabel.no_code},
         ],
-        [["src/rust/engine/path/to/file.rs"], {Affected.rust}],
-        [["src/python/pants/VERSION"], {Affected.release}],
-        [["src/python/pants_release/generate_github_workflows.py"], {Affected.ci_config}],
-        [["src/python/pants/whatever.py"], {Affected.other}],
+        [["src/rust/engine/path/to/file.rs"], {ChangeLabel.rust}],
+        [["src/python/pants/VERSION"], {ChangeLabel.release}],
+        [["src/python/pants_release/generate_github_workflows.py"], {ChangeLabel.ci_config}],
+        [["src/python/pants/whatever.py"], {ChangeLabel.other}],
         [
             ["docs/path/to/some/doc", "src/rust/engine/rust-toolchain"],
-            {Affected.docs, Affected.rust},
+            {ChangeLabel.docs, ChangeLabel.rust},
         ],
         [
             [
@@ -31,7 +34,14 @@ from classify_changed_files import Affected, classify
                 "src/rust/engine/rust-toolchain",
                 "src/python/pants/whatever.py",
             ],
-            {Affected.docs, Affected.rust, Affected.other},
+            {ChangeLabel.docs, ChangeLabel.rust, ChangeLabel.other},
+        ],
+        [
+            [
+                ".devcontainer/Dockerfile",
+                "docs/path/to/some/doc",
+            ],
+            {ChangeLabel.dev_utils, ChangeLabel.docs, ChangeLabel.no_code},
         ],
     ),
 )
