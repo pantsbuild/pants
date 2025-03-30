cc1ab8d7a2bfa5555d94da77ac86eb3bf4b10d03
0xG0nz0 <null>
2025-03-30 16:29:54 +0000

    Fixed formatting in skip_utils.py

--- src/python/pants/testutil/skip_utils.py
+++ src/python/pants/testutil/skip_utils.py
@@ -8,6 +8,7 @@ import pytest
 
 from pants.engine.platform import Platform
 
+
 @lru_cache
 def skip_if_command_errors(*args: str):
     def empty_decorator(func):
@@ -20,14 +21,18 @@ def skip_if_command_errors(*args: str):
 
     return empty_decorator
 
+
 def skip_if_linux_arm64(func):
     """Skip the test if running on linux-arm64."""
     if Platform.create_for_localhost() == Platform.linux_arm64:
         return pytest.mark.skip(reason="Test cannot run on Linux ARM64. Skipping.")(func)
     else:
+
         def empty_decorator(func):
             return func
+
         return empty_decorator(func)
+
 
 requires_go = skip_if_command_errors("go", "version")
 requires_thrift = skip_if_command_errors("thrift", "-version")
