69162786fe4985f0e466af769a7b91a0ba7434dd
Kacper Krylowicz <kacper.krylowicz@greenteg.com>
2025-05-01 16:55:33 +0200

    Create a helper for summarizing diffs between two DynamicRemoteOptions instances

--- src/python/pants/option/options_diff.py
+++ src/python/pants/option/options_diff.py
@@ -1,11 +1,16 @@
 # Copyright 2025 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from dataclasses import fields
+from enum import Enum
 from typing import Any
+
+from pants.option.global_options import DynamicRemoteOptions
 
 
 def summarize_options_map_diff(old: dict[str, Any], new: dict[str, Any]) -> str:
     """Compare two options map (as produced by `OptionsFingerprinter.options_map_for_scope`), and
-    return a one-liner like:
+    return a one-liner summarizing all differences, e.g.:
 
     `key1: 'old' -> "new'; list_key: ['x', 'y'] -> ['x']`
     """
@@ -16,6 +21,28 @@ def summarize_options_map_diff(old: dict[str, Any], new: dict[str, Any]) -> str:
 
         if o == n:
             continue
+
+        diffs.append(f"{key}: {o!r} -> {n!r}")
+
+    return "; ".join(diffs)
+
+
+def summarize_dynamic_options_diff(old: DynamicRemoteOptions, new: DynamicRemoteOptions) -> str:
+    """Compare two `DynamicRemoteOptions` and return a one-liner summarizing all differences, e.g.:
+
+    `provider: 'reapi' -> 'experimental-file'; cache_read: False -> True`
+    """
+    diffs = []
+    for field in fields(DynamicRemoteOptions):
+        key = field.name
+        o = getattr(old, key)
+        n = getattr(new, key)
+
+        if o == n:
+            continue
+
+        o = o.value if isinstance(o, Enum) else o
+        n = n.value if isinstance(n, Enum) else n
 
         diffs.append(f"{key}: {o!r} -> {n!r}")
 
--- src/python/pants/option/options_diff_test.py
+++ src/python/pants/option/options_diff_test.py
@@ -1,10 +1,13 @@
 # Copyright 2025 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+from dataclasses import replace
 from typing import Any
 
 import pytest
 
-from pants.option.options_diff import summarize_options_map_diff
+from pants.option.global_options import DynamicRemoteOptions, RemoteProvider
+from pants.option.options_diff import summarize_dynamic_options_diff, summarize_options_map_diff
+from pants.testutil.option_util import create_dynamic_remote_options
 
 
 @pytest.mark.parametrize(
@@ -41,4 +44,50 @@ def test_summarize_options_map_diff(
     old: dict[str, Any], new: dict[str, Any], expected_diff: str
 ) -> None:
     diff = summarize_options_map_diff(old, new)
+    assert diff == expected_diff
+
+
+@pytest.mark.parametrize(
+    "old, new, expected_diff",
+    [
+        [
+            create_dynamic_remote_options(),
+            create_dynamic_remote_options(),
+            "",
+        ],
+        [
+            create_dynamic_remote_options(),
+            replace(create_dynamic_remote_options(), execution=True),
+            "execution: False -> True",
+        ],
+        [
+            create_dynamic_remote_options(),
+            replace(create_dynamic_remote_options(), provider=RemoteProvider.experimental_file),
+            "provider: 'reapi' -> 'experimental-file'",
+        ],
+        [
+            create_dynamic_remote_options(),
+            replace(create_dynamic_remote_options(), store_headers={"auth": "token"}),
+            "store_headers: {} -> {'auth': 'token'}",
+        ],
+        [
+            replace(create_dynamic_remote_options(), execution=True, execution_headers={"x": "1"}),
+            replace(create_dynamic_remote_options(), execution=False, execution_headers={}),
+            "execution: True -> False; execution_headers: {'x': '1'} -> {}",
+        ],
+        [
+            create_dynamic_remote_options(),
+            replace(
+                create_dynamic_remote_options(),
+                instance_name="test",
+                parallelism=32,
+            ),
+            "instance_name: 'main' -> 'test'; parallelism: 128 -> 32",
+        ],
+    ],
+)
+def test_summarize_dynamic_options_diff(
+    old: DynamicRemoteOptions, new: DynamicRemoteOptions, expected_diff: str
+) -> None:
+    diff = summarize_dynamic_options_diff(old, new)
     assert diff == expected_diff
