782a4a76508894c00b3f507a068f839449af5c92
Daniel Goldman <danielgoldman4@gmail.com>
2025-05-05 00:21:08 -0400

    add used TF rules
    turns out we don't need a lot of them

--- src/python/pants/backend/experimental/terraform/lint/trivy/register.py
+++ src/python/pants/backend/experimental/terraform/lint/trivy/register.py
@@ -1,12 +1,13 @@
 # Copyright 2024 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+from pants.backend.terraform.dependencies import rules as terraform_rules
 from pants.backend.terraform.lint.trivy.rules import rules as trivy_terraform_rules
 from pants.backend.tools.trivy.rules import rules as trivy_rules
 
 
 def rules():
     return (
-        # TODO: Register Terraform rules
+        *terraform_rules(),
         *trivy_rules(),
         *trivy_terraform_rules(),
     )
