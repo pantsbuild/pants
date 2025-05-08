2f116606f356866f2efb425295106a80a9282149
Kacper Krylowicz <kacper.krylowicz@greenteg.com>
2025-05-08 02:12:05 +0200

    Include a summary of which initialization options changed in Pantsd daemon restart logs (#22232)
    With this change, the pantsd scheduler logs will now include a summary
    of which options changed during restart:
    
    - Instead of fingerprinting bootstrap options directly, build a map of
    each fingerprintable options to its current value and track it across
    restarts.
    - Generate a one-line diff comparing the old and new set of bootstrap
    and remote options
    - Those diffs are then logged whenever the scheduler restarts
    
    Closes #22224

--- src/python/pants/option/global_options_test.py
+++ src/python/pants/option/global_options_test.py
@@ -11,11 +11,8 @@ from typing import ContextManager
 import pytest
 
 from pants.base.build_environment import get_buildroot
-from pants.engine.env_vars import CompleteEnvironmentVars
 from pants.engine.internals.native_engine import PyRemotingOptions
 from pants.engine.internals.scheduler import ExecutionError
-from pants.engine.unions import UnionMembership
-from pants.init.options_initializer import OptionsInitializer
 from pants.option.errors import OptionsError
 from pants.option.global_options import (
     DynamicRemoteOptions,
@@ -24,42 +21,10 @@ from pants.option.global_options import (
     RemoteProvider,
 )
 from pants.option.options_bootstrapper import OptionsBootstrapper
-from pants.testutil import rule_runner
-from pants.testutil.option_util import create_options_bootstrapper
+from pants.testutil.option_util import create_dynamic_remote_options
 from pants.testutil.pytest_util import no_exception
 from pants.util.dirutil import safe_mkdir_for
 from pants.version import VERSION
-
-
-def create_dynamic_remote_options(
-    *,
-    initial_headers: dict[str, str] | None = None,
-    address: str | None = "grpc://fake.url:10",
-    token_path: str | None = None,
-    plugin: str | None = None,
-) -> DynamicRemoteOptions:
-    if initial_headers is None:
-        initial_headers = {}
-    args = [
-        "--remote-cache-read",
-        f"--remote-execution-address={address}",
-        f"--remote-store-address={address}",
-        f"--remote-store-headers={initial_headers}",
-        f"--remote-execution-headers={initial_headers}",
-        "--remote-instance-name=main",
-    ]
-    if token_path:
-        args.append(f"--remote-oauth-bearer-token=@{token_path}")
-    if plugin:
-        args.append(f"--backend-packages={plugin}")
-    ob = create_options_bootstrapper(args)
-    env = CompleteEnvironmentVars({})
-    oi = OptionsInitializer(ob, rule_runner.EXECUTOR)
-    _build_config = oi.build_config(ob, env)
-    options = oi.options(ob, env, _build_config, union_membership=UnionMembership({}), raise_=False)
-    return DynamicRemoteOptions.from_options(
-        options, env, remote_auth_plugin_func=_build_config.remote_auth_plugin_func
-    )[0]
 
 
 def test_dynamic_remote_options_oauth_bearer_token_with_path(tmp_path: Path) -> None:
