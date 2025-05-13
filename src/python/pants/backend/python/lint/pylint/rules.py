6d1f32bef28af8a49f60a2ccf4e2fe1539b5b328
SJ <suresh@vicarasolutions.com>
2025-05-13 18:50:19 -0400

    [Call-by-name] Migrated `pants.backend.python.lint.pylint` (#22260)

--- src/python/pants/backend/python/lint/pylint/rules.py
+++ src/python/pants/backend/python/lint/pylint/rules.py
@@ -21,23 +21,25 @@ from pants.backend.python.util_rules.partition import (
 from pants.backend.python.util_rules.pex import (
     Pex,
     PexRequest,
-    PexResolveInfo,
-    VenvPex,
     VenvPexProcess,
     VenvPexRequest,
+    create_pex,
+    create_venv_pex,
+    determine_pex_resolve_info,
 )
 from pants.backend.python.util_rules.pex_environment import PexEnvironment
 from pants.backend.python.util_rules.pex_from_targets import RequirementsPexRequest
 from pants.backend.python.util_rules.python_sources import (
-    PythonSourceFiles,
     PythonSourceFilesRequest,
+    prepare_python_sources,
 )
 from pants.core.goals.lint import REPORT_DIR, LintResult, LintTargetsRequest, Partitions
-from pants.core.util_rules.config_files import ConfigFiles, ConfigFilesRequest
+from pants.core.util_rules.config_files import find_config_file
 from pants.core.util_rules.partitions import Partition
-from pants.engine.fs import CreateDigest, Digest, Directory, MergeDigests, RemovePrefix
-from pants.engine.process import FallibleProcessResult
-from pants.engine.rules import Get, MultiGet, collect_rules, rule
+from pants.engine.fs import CreateDigest, Directory, MergeDigests, RemovePrefix
+from pants.engine.internals.graph import coarsened_targets as coarsened_targets_get
+from pants.engine.intrinsics import create_digest, execute_process, merge_digests, remove_prefix
+from pants.engine.rules import collect_rules, concurrently, implicitly, rule
 from pants.engine.target import CoarsenedTargets, CoarsenedTargetsRequest
 from pants.util.logging import LogLevel
 from pants.util.strutil import pluralize
@@ -89,9 +91,9 @@ async def partition_pylint(
         _partition_by_interpreter_constraints_and_resolve(request.field_sets, python_setup)
     )
 
-    coarsened_targets = await Get(
-        CoarsenedTargets,
+    coarsened_targets = await coarsened_targets_get(
         CoarsenedTargetsRequest(field_set.address for field_set in request.field_sets),
+        **implicitly(),
     )
     coarsened_targets_by_address = coarsened_targets.by_address()
 
@@ -132,53 +134,50 @@ async def run_pylint(
     )
     coarsened_closure = tuple(coarsened_targets.closure())
 
-    requirements_pex_get = Get(
-        Pex,
-        RequirementsPexRequest(
-            (target.address for target in coarsened_closure),
-            # NB: These constraints must be identical to the other PEXes. Otherwise, we risk using
-            # a different version for the requirements than the other two PEXes, which can result
-            # in a PEX runtime error about missing dependencies.
-            hardcoded_interpreter_constraints=request.partition_metadata.interpreter_constraints,
-        ),
+    requirements_pex_get = create_pex(
+        **implicitly(
+            RequirementsPexRequest(
+                (target.address for target in coarsened_closure),
+                # NB: These constraints must be identical to the other PEXes. Otherwise, we risk using
+                # a different version for the requirements than the other two PEXes, which can result
+                # in a PEX runtime error about missing dependencies.
+                hardcoded_interpreter_constraints=request.partition_metadata.interpreter_constraints,
+            )
+        )
     )
 
-    pylint_pex_get = Get(
-        Pex,
-        PexRequest,
+    pylint_pex_get = create_pex(
         pylint.to_pex_request(
             interpreter_constraints=request.partition_metadata.interpreter_constraints,
             extra_requirements=first_party_plugins.requirement_strings,
-        ),
+        )
     )
 
-    sources_get = Get(
-        PythonSourceFiles,
-        PythonSourceFilesRequest(coarsened_closure),
+    sources_get = prepare_python_sources(
+        PythonSourceFilesRequest(coarsened_closure), **implicitly()
     )
     # Ensure that the empty report dir exists.
-    report_directory_digest_get = Get(Digest, CreateDigest([Directory(REPORT_DIR)]))
+    report_directory_digest_get = create_digest(CreateDigest([Directory(REPORT_DIR)]))
 
     (
         pylint_pex,
         requirements_pex,
         sources,
         report_directory,
-    ) = await MultiGet(
+    ) = await concurrently(
         pylint_pex_get,
         requirements_pex_get,
         sources_get,
         report_directory_digest_get,
     )
 
-    pylint_pex_info = await Get(PexResolveInfo, Pex, pylint_pex)
+    pylint_pex_info = await determine_pex_resolve_info(**implicitly({pylint_pex: Pex}))
     astroid_info = pylint_pex_info.find("astroid")
     # Astroid is a transitive dependency of pylint and should always be available in the pex.
     assert astroid_info
 
-    pylint_runner_pex, config_files = await MultiGet(
-        Get(
-            VenvPex,
+    pylint_runner_pex, config_files = await concurrently(
+        create_venv_pex(
             VenvPexRequest(
                 PexRequest(
                     output_filename="pylint_runner.pex",
@@ -192,20 +191,16 @@ async def run_pylint(
                 # https://github.com/PyCQA/pylint/issues/1470
                 site_packages_copies=(astroid_info.version < packaging.version.Version("2.9.1")),
             ),
+            **implicitly(),
         ),
-        Get(
-            ConfigFiles,
-            ConfigFilesRequest,
-            pylint.config_request(sources.source_files.snapshot.dirs),
-        ),
+        find_config_file(pylint.config_request(sources.source_files.snapshot.dirs)),
     )
 
     pythonpath = list(sources.source_roots)
     if first_party_plugins:
         pythonpath.append(first_party_plugins.PREFIX)
 
-    input_digest = await Get(
-        Digest,
+    input_digest = await merge_digests(
         MergeDigests(
             (
                 config_files.snapshot.digest,
@@ -213,29 +208,30 @@ async def run_pylint(
                 sources.source_files.snapshot.digest,
                 report_directory,
             )
-        ),
+        )
     )
 
-    result = await Get(
-        FallibleProcessResult,
-        VenvPexProcess(
-            pylint_runner_pex,
-            argv=generate_argv(request.elements, pylint),
-            input_digest=input_digest,
-            output_directories=(REPORT_DIR,),
-            extra_env={"PEX_EXTRA_SYS_PATH": ":".join(pythonpath)},
-            concurrency_available=len(request.elements),
-            description=f"Run Pylint on {pluralize(len(request.elements), 'target')}.",
-            level=LogLevel.DEBUG,
-        ),
+    result = await execute_process(
+        **implicitly(
+            VenvPexProcess(
+                pylint_runner_pex,
+                argv=generate_argv(request.elements, pylint),
+                input_digest=input_digest,
+                output_directories=(REPORT_DIR,),
+                extra_env={"PEX_EXTRA_SYS_PATH": ":".join(pythonpath)},
+                concurrency_available=len(request.elements),
+                description=f"Run Pylint on {pluralize(len(request.elements), 'target')}.",
+                level=LogLevel.DEBUG,
+            )
+        )
     )
-    report = await Get(Digest, RemovePrefix(result.output_digest, REPORT_DIR))
+    report = await remove_prefix(RemovePrefix(result.output_digest, REPORT_DIR))
     return LintResult.create(request, result, report=report)
 
 
 def rules():
-    return [
+    return (
         *collect_rules(),
         *PylintRequest.rules(),
         *pex_from_targets.rules(),
-    ]
+    )
