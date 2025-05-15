1aef4bf8e15ac46a48820442a82df4bdca11aec6
Tom Dyas <tom@shoalsoftware.com>
2025-05-15 13:36:52 -0400

    migrate

--- src/python/pants/jvm/strip_jar/strip_jar.py
+++ src/python/pants/jvm/strip_jar/strip_jar.py
@@ -7,11 +7,12 @@ from dataclasses import dataclass
 from pants.core.goals.resolves import ExportableTool
 from pants.engine.fs import AddPrefix, CreateDigest, Digest, Directory, FileContent
 from pants.engine.internals.native_engine import MergeDigests, RemovePrefix
-from pants.engine.process import FallibleProcessResult, ProcessCacheScope, ProcessResult
-from pants.engine.rules import Get, MultiGet, collect_rules, rule
+from pants.engine.intrinsics import add_prefix, create_digest, merge_digests, remove_prefix
+from pants.engine.process import FallibleProcessResult, ProcessCacheScope, execute_process_or_raise
+from pants.engine.rules import collect_rules, concurrently, implicitly, rule
 from pants.engine.unions import UnionRule
 from pants.jvm.jdk_rules import InternalJdk, JvmProcess
-from pants.jvm.resolve.coursier_fetch import ToolClasspath, ToolClasspathRequest
+from pants.jvm.resolve.coursier_fetch import ToolClasspathRequest, materialize_classpath_for_tool
 from pants.jvm.resolve.jvm_tool import GenerateJvmLockfileFromTool, JvmToolBase
 from pants.util.logging import LogLevel
 
@@ -60,12 +61,11 @@ async def strip_jar(
     toolcp_relpath = "__toolcp"
     processorcp_relpath = "__processorcp"
 
-    tool_classpath, prefixed_jars_digest = await MultiGet(
-        Get(
-            ToolClasspath,
-            ToolClasspathRequest(lockfile=GenerateJvmLockfileFromTool.create(tool)),
+    tool_classpath, prefixed_jars_digest = await concurrently(
+        materialize_classpath_for_tool(
+            ToolClasspathRequest(lockfile=GenerateJvmLockfileFromTool.create(tool))
         ),
-        Get(Digest, AddPrefix(request.digest, input_path)),
+        add_prefix(AddPrefix(request.digest, input_path)),
     )
 
     extra_immutable_input_digests = {
@@ -73,26 +73,27 @@ async def strip_jar(
         processorcp_relpath: processor_classfiles.digest,
     }
 
-    process_result = await Get(
-        ProcessResult,
-        JvmProcess(
-            jdk=jdk,
-            classpath_entries=[
-                *tool_classpath.classpath_entries(toolcp_relpath),
-                processorcp_relpath,
-            ],
-            argv=["org.pantsbuild.stripjar.StripJar", input_path, _OUTPUT_PATH, *filenames],
-            input_digest=prefixed_jars_digest,
-            extra_immutable_input_digests=extra_immutable_input_digests,
-            output_directories=(_OUTPUT_PATH,),
-            extra_nailgun_keys=extra_immutable_input_digests,
-            description=f"Stripping jar {filenames[0]}",
-            level=LogLevel.DEBUG,
-            cache_scope=ProcessCacheScope.LOCAL_SUCCESSFUL,
-        ),
+    process_result = await execute_process_or_raise(
+        **implicitly(
+            JvmProcess(
+                jdk=jdk,
+                classpath_entries=[
+                    *tool_classpath.classpath_entries(toolcp_relpath),
+                    processorcp_relpath,
+                ],
+                argv=["org.pantsbuild.stripjar.StripJar", input_path, _OUTPUT_PATH, *filenames],
+                input_digest=prefixed_jars_digest,
+                extra_immutable_input_digests=extra_immutable_input_digests,
+                output_directories=(_OUTPUT_PATH,),
+                extra_nailgun_keys=extra_immutable_input_digests,
+                description=f"Stripping jar {filenames[0]}",
+                level=LogLevel.DEBUG,
+                cache_scope=ProcessCacheScope.LOCAL_SUCCESSFUL,
+            )
+        )
     )
 
-    return await Get(Digest, RemovePrefix(process_result.output_digest, _OUTPUT_PATH))
+    return await remove_prefix(RemovePrefix(process_result.output_digest, _OUTPUT_PATH))
 
 
 def _load_strip_jar_source() -> bytes:
@@ -104,15 +105,13 @@ def _load_strip_jar_source() -> bytes:
 @rule
 async def build_processors(jdk: InternalJdk, tool: StripJarTool) -> StripJarCompiledClassfiles:
     dest_dir = "classfiles"
-    materialized_classpath, source_digest = await MultiGet(
-        Get(
-            ToolClasspath,
+    materialized_classpath, source_digest = await concurrently(
+        materialize_classpath_for_tool(
             ToolClasspathRequest(
                 prefix="__toolcp", lockfile=GenerateJvmLockfileFromTool.create(tool)
-            ),
+            )
         ),
-        Get(
-            Digest,
+        create_digest(
             CreateDigest(
                 [
                     FileContent(
@@ -121,43 +120,43 @@ async def build_processors(jdk: InternalJdk, tool: StripJarTool) -> StripJarComp
                     ),
                     Directory(dest_dir),
                 ]
-            ),
+            )
         ),
     )
 
-    merged_digest = await Get(
-        Digest,
+    merged_digest = await merge_digests(
         MergeDigests(
             (
                 materialized_classpath.digest,
                 source_digest,
             )
-        ),
+        )
     )
 
-    process_result = await Get(
-        ProcessResult,
-        JvmProcess(
-            jdk=jdk,
-            classpath_entries=[f"{jdk.java_home}/lib/tools.jar"],
-            argv=[
-                "com.sun.tools.javac.Main",
-                "-cp",
-                ":".join(materialized_classpath.classpath_entries()),
-                "-d",
-                dest_dir,
-                _STRIP_JAR_BASENAME,
-            ],
-            input_digest=merged_digest,
-            output_directories=(dest_dir,),
-            description=f"Compile {_STRIP_JAR_BASENAME} with javac",
-            level=LogLevel.DEBUG,
-            # NB: We do not use nailgun for this process, since it is launched exactly once.
-            use_nailgun=False,
-        ),
+    process_result = await execute_process_or_raise(
+        **implicitly(
+            JvmProcess(
+                jdk=jdk,
+                classpath_entries=[f"{jdk.java_home}/lib/tools.jar"],
+                argv=[
+                    "com.sun.tools.javac.Main",
+                    "-cp",
+                    ":".join(materialized_classpath.classpath_entries()),
+                    "-d",
+                    dest_dir,
+                    _STRIP_JAR_BASENAME,
+                ],
+                input_digest=merged_digest,
+                output_directories=(dest_dir,),
+                description=f"Compile {_STRIP_JAR_BASENAME} with javac",
+                level=LogLevel.DEBUG,
+                # NB: We do not use nailgun for this process, since it is launched exactly once.
+                use_nailgun=False,
+            )
+        )
     )
-    stripped_classfiles_digest = await Get(
-        Digest, RemovePrefix(process_result.output_digest, dest_dir)
+    stripped_classfiles_digest = await remove_prefix(
+        RemovePrefix(process_result.output_digest, dest_dir)
     )
     return StripJarCompiledClassfiles(digest=stripped_classfiles_digest)
 
