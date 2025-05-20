dee969675c29cb84843997c86e127034459f09a1
Tom Dyas <tom@shoalsoftware.com>
2025-05-20 13:43:32 -0400

    migrate remaining files in k8s backend

--- src/python/pants/backend/k8s/goals/deploy.py
+++ src/python/pants/backend/k8s/goals/deploy.py
@@ -15,21 +15,17 @@ from pants.backend.k8s.target_types import (
     K8sSourceField,
 )
 from pants.core.goals.deploy import DeployFieldSet, DeployProcess
-from pants.core.util_rules.external_tool import DownloadedExternalTool, ExternalToolRequest
-from pants.engine.addresses import UnparsedAddressInputs
-from pants.engine.env_vars import EnvironmentVars, EnvironmentVarsRequest
-from pants.engine.fs import MergeDigests, Snapshot
+from pants.core.util_rules.external_tool import download_external_tool
+from pants.engine.env_vars import EnvironmentVarsRequest
+from pants.engine.fs import MergeDigests
+from pants.engine.internals.graph import hydrate_sources, resolve_targets
 from pants.engine.internals.native_engine import Digest
+from pants.engine.internals.platform_rules import environment_vars_subset
+from pants.engine.intrinsics import digest_to_snapshot
 from pants.engine.platform import Platform
 from pants.engine.process import InteractiveProcess, Process, ProcessCacheScope
-from pants.engine.rules import Get, MultiGet, collect_rules, rule
-from pants.engine.target import (
-    DependenciesRequest,
-    HydratedSources,
-    HydrateSourcesRequest,
-    SourcesField,
-    Targets,
-)
+from pants.engine.rules import collect_rules, concurrently, implicitly, rule
+from pants.engine.target import DependenciesRequest, HydrateSourcesRequest, SourcesField
 from pants.engine.unions import UnionRule
 from pants.util.frozendict import FrozenDict
 from pants.util.logging import LogLevel
@@ -77,41 +73,39 @@ async def run_k8s_deploy(
             f"Context `{context}` is not listed in `[{K8sSubsystem.options_scope}].available_contexts`"
         )
 
-    dependencies = await Get(
-        Targets, UnparsedAddressInputs, field_set.sources.to_unparsed_address_inputs()
+    dependencies = await resolve_targets(
+        **implicitly(field_set.sources.to_unparsed_address_inputs())
     )
-    file_sources = await MultiGet(
-        Get(
-            HydratedSources,
+    file_sources = await concurrently(
+        hydrate_sources(
             HydrateSourcesRequest(
                 t.get(SourcesField),
                 for_sources_types=(K8sSourceField,),
                 enable_codegen=True,
             ),
+            **implicitly(),
         )
         for t in dependencies
     )
-    snapshot, target_dependencies = await MultiGet(
-        Get(
-            Snapshot,
-            MergeDigests((*(sources.snapshot.digest for sources in file_sources),)),
+    snapshot, target_dependencies = await concurrently(
+        digest_to_snapshot(
+            **implicitly(MergeDigests((*(sources.snapshot.digest for sources in file_sources),)))
         ),
-        Get(Targets, DependenciesRequest(field_set.dependencies)),
+        resolve_targets(**implicitly(DependenciesRequest(field_set.dependencies))),
     )
 
     publish_targets = [
         tgt for tgt in target_dependencies if DockerPackageFieldSet.is_applicable(tgt)
     ]
 
-    env = await Get(
-        EnvironmentVars,
-        EnvironmentVarsRequest,
-        EnvironmentVarsRequest(requested=kubectl.extra_env_vars),
+    env = await environment_vars_subset(
+        **implicitly(
+            {EnvironmentVarsRequest(requested=kubectl.extra_env_vars): EnvironmentVarsRequest}
+        )
     )
 
     process = InteractiveProcess.from_process(
-        await Get(
-            Process,
+        await kubectl_apply_process(
             KubectlApply(
                 snapshot.files,
                 platform=platform,
@@ -119,6 +113,7 @@ async def run_k8s_deploy(
                 env=env,
                 context=context,
             ),
+            **implicitly(),
         )
     )
 
@@ -147,9 +142,7 @@ async def kubectl_apply_process(
     for path in request.paths:
         argv += ("-f", path)
 
-    kubectl_tool = await Get(
-        DownloadedExternalTool, ExternalToolRequest, kubectl.get_request(platform)
-    )
+    kubectl_tool = await download_external_tool(kubectl.get_request(platform))
 
     immutable_input_digests = {
         tool_relpath: kubectl_tool.digest,
