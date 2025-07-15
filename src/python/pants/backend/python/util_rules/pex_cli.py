40b6b8d5e54399bbee61b54fe54c5bfaa1b71bf6
Tom Dyas <tom@shoalsoftware.com>
2025-07-14 21:21:22 -0400

    upgrade to pex v2.45.2 (#22490)
    Upgrade Pex to
    [v2.45.2](https://github.com/pex-tool/pex/releases/tag/v2.45.2).
    
    ```
    Lockfile diff: 3rdparty/python/user_reqs.lock [python-default]
    
    ==                    Upgraded dependencies                     ==
    
      asgiref                        3.8.1        -->   3.9.1
      certifi                        2025.4.26    -->   2025.7.14
      cryptography                   45.0.3       -->   45.0.5
      pex                            2.40.1       -->   2.45.2
      pygments                       2.19.1       -->   2.19.2
      python-dotenv                  1.1.0        -->   1.1.1
      requests                       2.32.3       -->   2.32.4
      typing-extensions              4.14.0       -->   4.14.1
    ```

--- src/python/pants/backend/python/util_rules/pex_cli.py
+++ src/python/pants/backend/python/util_rules/pex_cli.py
@@ -42,7 +42,7 @@ class PexCli(TemplatedExternalTool):
     name = "pex"
     help = "The PEX (Python EXecutable) tool (https://github.com/pex-tool/pex)."
 
-    default_version = "v2.40.1"
+    default_version = "v2.45.2"
     default_url_template = "https://github.com/pex-tool/pex/releases/download/{version}/pex"
     version_constraints = ">=2.13.0,<3.0"
 
@@ -59,10 +59,10 @@ class PexCli(TemplatedExternalTool):
     )
 
     default_known_versions = [
-        "v2.40.1|macos_x86_64|2153ed70e82424e673a92b5e6e8ff92e13abca7475ad37ed5123391233a3d554|4831780",
-        "v2.40.1|macos_arm64|2153ed70e82424e673a92b5e6e8ff92e13abca7475ad37ed5123391233a3d554|4831780",
-        "v2.40.1|linux_x86_64|2153ed70e82424e673a92b5e6e8ff92e13abca7475ad37ed5123391233a3d554|4831780",
-        "v2.40.1|linux_arm64|2153ed70e82424e673a92b5e6e8ff92e13abca7475ad37ed5123391233a3d554|4831780",
+        "v2.45.2|macos_x86_64|570a3d5ca306a39aa3a180bd4cf3e2661b7c74b0579422b34659246daf122384|4833957",
+        "v2.45.2|macos_arm64|570a3d5ca306a39aa3a180bd4cf3e2661b7c74b0579422b34659246daf122384|4833957",
+        "v2.45.2|linux_x86_64|570a3d5ca306a39aa3a180bd4cf3e2661b7c74b0579422b34659246daf122384|4833957",
+        "v2.45.2|linux_arm64|570a3d5ca306a39aa3a180bd4cf3e2661b7c74b0579422b34659246daf122384|4833957",
     ]
 
 
