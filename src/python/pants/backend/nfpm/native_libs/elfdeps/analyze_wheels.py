74bcb180fe4594aa08738107ae36666d5f83a8e5
Jacob Floyd <cognifloyd@gmail.com>
2025-08-04 16:43:17 -0500

    nfpm.native_libs.elfdeps: Add analyze_wheels

---
+++ src/python/pants/backend/nfpm/native_libs/elfdeps/analyze_wheels.py
@@ -0,0 +1,79 @@
+# Copyright 2025 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+import json
+import sys
+import zipfile
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Generator
+
+# elfdeps 0.2.0 added analyze_zipfile
+from elfdeps import ELFAnalyzeSettings, ELFInfo, SOInfo, analyze_zipfile
+
+
+@dataclass(frozen=True)
+class WheelsELFInfo:
+    provides: tuple[SOInfo, ...]
+    requires: tuple[SOInfo, ...]
+
+    def to_dict(self) -> dict[str, str | list[str]]:
+        # so_info: SOInfo(soname: str, version: str, marker: str)
+        # marker is one of "(64bit)" or ""
+        # str(so_info) = f"{soname}({version}){marker}"
+        return {
+            "provides": [str(so_info) for so_info in sorted(self.provides)],
+            "requires": [str(so_info) for so_info in sorted(self.requires)],
+        }
+
+
+def analyze_wheel(wheel_path: Path, settings: ELFAnalyzeSettings) -> Generator[ELFInfo]:
+    print(".", end="", file=sys.stderr)  # a progress indicator
+    with zipfile.ZipFile(wheel_path, mode="r") as wheel:
+        yield from analyze_zipfile(wheel, settings=settings)
+
+
+def analyze_wheels_repo(wheel_repo: Path) -> WheelsELFInfo:
+    settings = ELFAnalyzeSettings(unique=True)
+
+    print(f"Analyzing wheels in {wheel_repo}", file=sys.stderr)
+    elf_infos: list[ELFInfo] = [
+        elf_info
+        for wheel in wheel_repo.iterdir()
+        for elf_info in analyze_wheel(wheel, settings)
+    ]
+    print(
+        f"Done analyzing wheels in {wheel_repo}.", file=sys.stderr
+    )  # end progress indicators
+
+    provides: set[SOInfo] = set()
+    requires: set[SOInfo] = set()
+    for elf_info in elf_infos:
+        provides.update(elf_info.provides)  # elf_info.provides: list[SOInfo]
+        requires.update(elf_info.requires)  # elf_info.requires: list[SOInfo]
+
+    return WheelsELFInfo(tuple(provides), tuple(requires))
+
+
+def main() -> int:
+    if len(sys.argv) != 2:
+        raise RuntimeError(
+            f"{sys.argv[0]} expects one positional arg, the wheel_repo path"
+        )
+    wheel_repo = Path(sys.argv[1])
+    if not wheel_repo.resolve().is_dir():
+        raise NotADirectoryError(
+            f"{wheel_repo} is not a directory (or a symlink to a directory)!"
+        )
+
+    wheels_elf_info = analyze_wheels_repo(wheel_repo=wheel_repo)
+
+    print(json.dumps(wheels_elf_info.to_dict(), indent=None, separators=(",", ":")))
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ src/python/pants/backend/nfpm/native_libs/elfdeps/rules.py
@@ -0,0 +1,92 @@
+# Copyright 2025 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+import json
+from dataclasses import dataclass
+from pathlib import PurePath
+from typing import Iterable
+
+from pants.backend.nfpm.native_libs.elfdeps.subsystem import rules as subsystem_rules
+from pants.backend.nfpm.native_libs.elfdeps.subsystem import (
+    setup_elfdeps_analyze_wheels_tool,
+)
+from pants.backend.python.util_rules.pex import Pex, PexProcess, VenvPexProcess
+from pants.backend.python.util_rules.pex_cli import PexPEX
+from pants.engine.process import ProcessResult, fallible_to_exec_result_or_raise
+from pants.engine.rules import Rule, collect_rules, concurrently, implicitly, rule
+from pants.util.logging import LogLevel
+
+
+@dataclass(frozen=True)
+class RequestPexELFInfo:
+    target_pex: Pex
+
+
+@dataclass(frozen=True)
+class PexELFInfo:
+    provides: tuple[str, ...]
+    requires: tuple[str, ...]
+
+
+@rule(
+    desc="Analyze ELF (native lib) dependencies of wheels in a PEX",
+    level=LogLevel.DEBUG,
+)
+async def elfdeps_analyze_pex_wheels(
+    request: RequestPexELFInfo, pex_pex: PexPEX
+) -> PexELFInfo:
+    wheel_repo_dir = str(PurePath(request.target_pex.name).with_suffix(".wheel_repo"))
+
+    extracted_wheels, elfdeps_analyze_wheels_tool = await concurrently(
+        fallible_to_exec_result_or_raise(
+            **implicitly(
+                PexProcess(
+                    pex=Pex(
+                        digest=pex_pex.digest,
+                        name=pex_pex.exe,
+                        python=request.target_pex.python,
+                    ),
+                    argv=[
+                        request.target_pex.name,
+                        "repository",
+                        "extract",
+                        "--dest-dir",
+                        wheel_repo_dir,
+                    ],
+                    input_digest=request.target_pex.digest,
+                    output_directories=(wheel_repo_dir,),
+                    extra_env={"PEX_MODULE": "pex.tools"},
+                    description=f"Extract wheels from {request.target_pex.name}",
+                    level=LogLevel.DEBUG,
+                )
+            )
+        ),
+        setup_elfdeps_analyze_wheels_tool(**implicitly()),
+    )
+
+    result: ProcessResult = await fallible_to_exec_result_or_raise(
+        **implicitly(
+            VenvPexProcess(
+                elfdeps_analyze_wheels_tool.pex,
+                argv=(wheel_repo_dir,),
+                input_digest=extracted_wheels.output_digest,
+                description=f"Calculate ELF provides+requires for wheels in pex {request.target_pex.name}",
+                level=LogLevel.DEBUG,
+            )
+        )
+    )
+
+    pex_elf_info = json.loads(result.stdout)
+    return PexELFInfo(
+        provides=tuple(pex_elf_info["provides"]),
+        requires=tuple(pex_elf_info["requires"]),
+    )
+
+
+def rules() -> Iterable[Rule]:
+    return (
+        *subsystem_rules(),
+        *collect_rules(),
+    )
