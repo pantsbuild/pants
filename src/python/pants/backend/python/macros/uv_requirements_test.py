489ee31f37fe0ad8c48500addd0b60993c557ae3
Benjy Weinberger <benjyw@gmail.com>
2025-09-16 20:11:11 -0700

    Fix tests

--- src/python/pants/backend/python/macros/uv_requirements_test.py
+++ src/python/pants/backend/python/macros/uv_requirements_test.py
@@ -11,6 +11,7 @@ from pants.backend.python.goals import lockfile
 from pants.backend.python.macros import uv_requirements
 from pants.backend.python.macros.uv_requirements import UvRequirementsTargetGenerator
 from pants.backend.python.target_types import PythonRequirementTarget
+from pants.backend.python.util_rules import pex
 from pants.core.target_types import TargetGeneratorSourcesHelperTarget
 from pants.engine.addresses import Address
 from pants.engine.internals.graph import _TargetParametrizations, _TargetParametrizationsRequest
@@ -23,6 +24,7 @@ def rule_runner() -> RuleRunner:
     return RuleRunner(
         rules=[
             *lockfile.rules(),
+            *pex.rules(),
             *uv_requirements.rules(),
             QueryRule(_TargetParametrizations, [_TargetParametrizationsRequest]),
         ],
