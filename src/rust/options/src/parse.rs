abd6d9424a1dd7b05a16172c0e5a53b31199a694
Tom Dyas <tom@shoalsoftware.com>
2025-09-21 00:17:47 -0400

    the ## token is reserved in 2024 Edition

--- src/rust/options/src/parse.rs
+++ src/rust/options/src/parse.rs
@@ -66,7 +66,7 @@ peg::parser! {
         // NB: ##method(X) is an undocumented peg feature expression that calls input.method(pos, X)
         // (see https://github.com/kevinmehall/rust-peg/issues/283).
         rule quoted_character(quote_char: &'static str) -> char
-            = !(##parse_string_literal(quote_char) / "\\x") c:$([_]) { c.chars().next().unwrap() }
+            = !(#{|input, pos| input.parse_string_literal(pos, quote_char)} / "\\x") c:$([_]) { c.chars().next().unwrap() }
 
         // Python string literal escape sequences.
         // See https://docs.python.org/3/reference/lexical_analysis.html#escape-sequences.
