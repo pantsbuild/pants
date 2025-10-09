283a6c242ed2b892e28fac4f416ba9c38a25a293
Jacob Floyd <cognifloyd@gmail.com>
2025-10-09 15:04:30 -0500

    nfpm.native_libs: rpm_depends_from_pex rules - basic integration tests

--- src/python/pants/backend/nfpm/native_libs/rules.py
+++ src/python/pants/backend/nfpm/native_libs/rules.py
@@ -4,11 +4,31 @@
 from __future__ import annotations
 
 from collections.abc import Iterable
+from dataclasses import dataclass
 
+from pants.backend.python.util_rules.pex import Pex
 from pants.engine.rules import Rule, collect_rules
 from pants.engine.unions import UnionRule
 
+from .elfdeps.rules import RequestPexELFInfo, elfdeps_analyze_pex
 from .elfdeps.rules import rules as elfdeps_rules
+
+
+@dataclass(frozen=True)
+class RpmDependsFromPexRequest:
+    target_pex: Pex
+
+
+@dataclass(frozen=True)
+class RpmDependsInfo:
+    provides: tuple[str, ...]
+    requires: tuple[str, ...]
+
+
+@rule
+async def rpm_depends_from_pex(request: RpmDependsFromPexRequest) -> RpmDependsInfo:
+    pex_elf_info = await elfdeps_analyze_pex(RequestPexELFInfo(request.target_pex), **implicitly())
+    return RpmDependsInfo(provides=pex_elf_info.provides, requires=pex_elf_info.requires)
 
 
 def rules() -> Iterable[Rule | UnionRule]:
