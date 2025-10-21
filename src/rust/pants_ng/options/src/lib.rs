4aa66fa08ea6d0cb910a9ad732623fa96dc315cd
Benjy Weinberger <benjyw@gmail.com>
2025-10-20 21:01:46 -0400

    A new command-line parser.
    This supports the command/subcommand paradigm, rather than
    the "list of goals" paradigm of the existing cli parser.
    
    This is experimental, not wired up to anything yet, and intended
    to support a "next-generation Pants" (pants_ng for short).
    
    It is not intended to support all current Pants 2 use-cases, but
    is a much simpler design based on our experience with the
    complexities of the current cli parser.
    
    In particular, this new cli parser does not need to know about
    any registered options (unlike the old one, which uses them to
    tell the difference between specs and goals). This parser instead
    requires specs to contain a filepath separator.
    
    This cli parser currently supports one cmd (and optionally one
    subcommand) per invocation. If we want to support multiple
    cmd/subcommand pairs in a single invocation in the future we
    can do so by using a delimiter that isn't interpreted by the shell.
    
    See https://github.com/pantsbuild/pants/discussions/22692 for context.

---
+++ src/rust/pants_ng/options/src/lib.rs
@@ -0,0 +1,6 @@
+// Copyright 2025 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+pub mod pants_invocation;
+#[cfg(test)]
+mod pants_invocation_tests;
---
+++ src/rust/pants_ng/options/src/pants_invocation.rs
@@ -0,0 +1,215 @@
+// Copyright 2025 Pants project contributors (see CONTRIBUTORS.md).
+// Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+use std::env;
+use std::iter::Peekable;
+use std::path;
+use std::sync::LazyLock;
+
+use regex::Regex;
+
+static NAME_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^[a-z|A-Z]\w*$").unwrap());
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Args {
+    pub(crate) arg_strings: Vec<String>,
+}
+
+impl Args {
+    // Create an Args instance with the provided args, which must *not* include the
+    // argv[0] process name.
+    pub fn new<I: IntoIterator<Item = String>>(arg_strs: I) -> Self {
+        Self {
+            arg_strings: arg_strs.into_iter().collect(),
+        }
+    }
+
+    pub fn argv() -> Self {
+        let mut args = env::args().collect::<Vec<_>>().into_iter();
+        args.next(); // Consume the process name (argv[0]).
+        // TODO: In Pants's own integration tests we may invoke Pants in a subprocess via
+        //  `python -m pants` or `python path/to/__main__.py` or similar. So
+        //  skipping argv[0] may not be sufficient to get just the set of args to split.
+        //  In practice our tests pass despite these extra args being interpreted as specs
+        //  or goals, but that is skating on thin ice.
+        Self::new(args.collect::<Vec<_>>())
+    }
+}
+
+// Represents a single cli flag used to set an option value, e.g., `--foo`, or `--bar=baz`.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Flag {
+    pub key: String, // The flag name, without the `--` prefix, up to the `=` if any.
+    pub value: Option<String>, // The value after the `=`, if any.
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct SubCommand {
+    pub name: String,
+    pub flags: Vec<Flag>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Command {
+    pub name: String,
+    pub flags: Vec<Flag>,
+    pub subcommand: Option<SubCommand>,
+}
+
+// The details of a Pants invocation command.
+//
+// An invocation has the form:
+//
+// pants --global-flags \
+//   cmd1 --cmd1-flags (subcmd1 --subcmd1-flags)? \
+//   @ cmd2 --cmd2-flags (subcmd2 --subcmd2-flags)? \
+//   ... \
+//   @ cmdN --cmdN-flags (subcmdN --subcmdN-flags)? \
+//   path/to/spec1 path/to/spec2 ... path/to/specM \
+//   (-- passthru-args)?
+//
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct PantsInvocation {
+    pub global_flags: Vec<Flag>,
+    pub commands: Vec<Command>,
+    pub specs: Vec<String>, // Non-flag arguments, typically file paths or globs.
+    pub passthru: Option<Vec<String>>, // Any remaining args specified after a -- separator.
+}
+
+impl PantsInvocation {
+    pub fn empty() -> Self {
+        Self {
+            global_flags: vec![],
+            commands: vec![],
+            specs: vec![],
+            passthru: None,
+        }
+    }
+
+    pub fn from_args(args: Args) -> Result<Self, String> {
+        #[inline]
+        fn is_flag(s: &str) -> bool {
+            s.starts_with("--") && s.len() > 2
+        }
+
+        #[inline]
+        fn is_name(s: &str) -> bool {
+            NAME_RE.is_match(s)
+        }
+
+        #[inline]
+        fn is_spec(s: &str) -> bool {
+            s.contains(path::MAIN_SEPARATOR)
+        }
+
+        fn consume_flags<I>(iter: &mut Peekable<I>) -> Vec<Flag>
+        where
+            I: Iterator<Item = String>,
+        {
+            let mut ret = vec![];
+            while let Some(s) = iter.peek()
+                && is_flag(s)
+            {
+                let mut components = s.splitn(2, '=');
+                ret.push(Flag {
+                    key: components.next().unwrap()[2..].to_string(),
+                    value: components.next().map(str::to_string),
+                });
+                iter.next();
+            }
+            ret
+        }
+
+        fn consume_cmd<I>(iter: &mut Peekable<I>) -> Option<Command>
+        where
+            I: Iterator<Item = String>,
+        {
+            if let Some(s) = iter.peek()
+                && is_name(s)
+            {
+                let name = iter.next().unwrap();
+                let flags = consume_flags(iter);
+                let subcommand = if let Some(s) = iter.peek()
+                    && is_name(s)
+                {
+                    Some(SubCommand {
+                        name: iter.next().unwrap(),
+                        flags: consume_flags(iter),
+                    })
+                } else {
+                    None
+                };
+
+                Some(Command {
+                    name,
+                    flags,
+                    subcommand,
+                })
+            } else {
+                None
+            }
+        }
+
+        fn consume_specs<I>(iter: &mut Peekable<I>) -> Vec<String>
+        where
+            I: Iterator<Item = String>,
+        {
+            let mut ret = vec![];
+            while let Some(s) = iter.peek()
+                && is_spec(s)
+            {
+                ret.push(iter.next().unwrap());
+            }
+            ret
+        }
+
+        let mut unconsumed_args = args.arg_strings.into_iter().peekable();
+
+        let global_flags = consume_flags(&mut unconsumed_args);
+
+        let mut commands = vec![];
+        let mut cmd_opt = consume_cmd(&mut unconsumed_args);
+        while cmd_opt.is_some()
+            && let Some(s) = unconsumed_args.peek()
+            && s == "@"
+        {
+            unconsumed_args.next();
+            commands.push(cmd_opt.unwrap());
+            cmd_opt = consume_cmd(&mut unconsumed_args);
+        }
+        if let Some(cmd) = cmd_opt {
+            commands.push(cmd);
+        }
+
+        let specs = consume_specs(&mut unconsumed_args);
+
+        if commands.is_empty() && !specs.is_empty() {
+            return Err(format!(
+                "Path specs must come after commands, but found `{}` before any commands",
+                specs.join(" ")
+            ));
+        }
+
+        let mut passthru = None;
+        if let Some(s) = unconsumed_args.next() {
+            if s == "--" {
+                let mut remainder = vec![];
+                for s in unconsumed_args.by_ref() {
+                    remainder.push(s);
+                }
+                passthru = Some(remainder);
+            } else if specs.is_empty() {
+                return Err(format!("Invalid command name `{s}`"));
+            } else {
+                return Err(format!("Extraneous argument `{s}`"));
+            }
+        }
+
+        Ok(PantsInvocation {
+            global_flags,
+            commands,
+            specs,
+            passthru,
+        })
+    }
+}
