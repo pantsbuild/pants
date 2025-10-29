9ea5b59545aa9a116ffb117401ad83d8a16a1d8d
sj <suresh@vicarasolutions.com>
2025-10-29 14:52:39 -0400

    Make preamble happy

--- src/rust/dep_inference/src/python/tests.rs
+++ src/rust/dep_inference/src/python/tests.rs
@@ -1,7 +1,8 @@
-use itertools::Itertools;
-
 // Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
 // Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+use itertools::Itertools;
+
 use crate::python::{ImportCollector, get_dependencies};
 use std::collections::{HashMap, HashSet};
 use std::path::PathBuf;
