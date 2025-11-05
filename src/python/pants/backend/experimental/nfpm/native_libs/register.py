cd7cf1993d6633ca66960d5d0c8cb2bb9c3673f5
Jacob Floyd <cognifloyd@gmail.com>
2025-11-05 13:33:00 -0600

    nfpm.native_libs: cleanup register.py

--- src/python/pants/backend/experimental/nfpm/native_libs/register.py
+++ src/python/pants/backend/experimental/nfpm/native_libs/register.py
@@ -1,17 +1,18 @@
-# Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
+# Copyright 2025 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-"""Analyze contents of nfpm-generated system packages to auto-generate deps from native lib deps.
-"""
+"""Analyze contents of nfpm-generated system packages to auto-generate deps from native lib deps."""
+
+from __future__ import annotations
+
+from collections.abc import Iterable
 
 from pants.backend.nfpm.native_libs.rules import rules as native_libs_rules
+from pants.engine.rules import Rule
+from pants.engine.unions import UnionRule
 
 
-# def target_types():
-#     return native_libs_target_types()
-
-
-def rules():
+def rules() -> Iterable[Rule | UnionRule]:
     return [
         *native_libs_rules(),
     ]
