d6850790a9a36aeced319a52bef9dce2f600fd86
Benjy Weinberger <benjyw@gmail.com>
2025-12-02 13:48:17 -0800

    Upgrade Pex to 2.73.1.

--- src/python/pants/backend/python/util_rules/pex_cli.py
+++ src/python/pants/backend/python/util_rules/pex_cli.py
@@ -37,9 +37,9 @@ from pants.util.strutil import softwrap
 logger = logging.getLogger(__name__)
 
 
-_PEX_VERSION = "v2.69.2"
-_PEX_BINARY_HASH = "ba521d8665bf58f759ce7bf0ebb534a1f1e5006044be2e42e7a298ad4fbd2ec5"
-_PEX_BINARY_SIZE = 4937401
+_PEX_VERSION = "v2.73.1"
+_PEX_BINARY_HASH = "e6907e079a3f7c917dc88b41d892f732d4b8dbe388abfefc064d19c4a9f3c7e8"
+_PEX_BINARY_SIZE = 4939987
 
 
 class PexCli(TemplatedExternalTool):
