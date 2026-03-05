f0030f5e782a25edb3786498aba4dbd4f3d8453a
Chris Burroughs <chris.burroughs@gmail.com>
2026-03-05 14:30:07 -0500

    plumb through pex --compress flag to pex_binary targets (#22983)
    closes #20227

--- src/python/pants/backend/python/target_types.py
+++ src/python/pants/backend/python/target_types.py
@@ -518,6 +518,19 @@ class PexCompletePlatformsField(SpecialCasedDependencies):
     )
 
 
+class PexCompressField(BoolField):
+    alias = "compress"
+    default = True
+    help = help_text(
+        """
+        Whether to compress zip entries when creating either a
+        zipapp PEX file or a packed PEX's bootstrap and
+        dependency zip files. Does nothing for loose layout
+        PEXes.
+        """
+    )
+
+
 class PexInheritPathField(StringField):
     alias = "inherit_path"
     valid_choices = ("false", "fallback", "prefer")
@@ -1026,6 +1039,7 @@ _PEX_BINARY_COMMON_FIELDS = (
     PexBinaryDependenciesField,
     PexCheckField,
     PexCompletePlatformsField,
+    PexCompressField,
     PexInheritPathField,
     PexStripEnvField,
     PexIgnoreErrorsField,
