05246bc65129dbb22ffba227def59ca167b09ce1
Chris Burroughs <chris.burroughs@gmail.com>
2026-04-15 09:53:00 -0400

    update the default Pex version to v2.92.2
    Release Notes:
     * https://github.com/pex-tool/pex/releases/tag/v2.92.2

--- src/python/pants/backend/python/util_rules/pex_cli.py
+++ src/python/pants/backend/python/util_rules/pex_cli.py
@@ -37,9 +37,9 @@ from pants.util.strutil import softwrap
 logger = logging.getLogger(__name__)
 
 
-_PEX_VERSION = "v2.92.1"
-_PEX_BINARY_HASH = "116da6936a81563ee2f1bafb221666b77617caec9ec82e38269d5e31784b932d"
-_PEX_BINARY_SIZE = 5081477
+_PEX_VERSION = "v2.92.2"
+_PEX_BINARY_HASH = "5ccaf151161debe5381d767c7631b39d5516c7106e725c45dbae24aa0e5fe569"
+_PEX_BINARY_SIZE = 5081502
 
 
 class PexCli(TemplatedExternalTool):
