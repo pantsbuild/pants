270fd16c024657a72bc0e40975dae662082da8fe
Chris Burroughs <chris.burroughs@gmail.com>
2026-06-24 14:02:17 -0400

    comment

--- build-support/bin/generate_builtin_lockfiles.py
+++ build-support/bin/generate_builtin_lockfiles.py
@@ -268,6 +268,7 @@ def generate_python_tool_lockfiles(
             "--python-resolves-to-constraints-file={}",
             "--python-resolves-to-no-binary={}",
             "--python-resolves-to-only-binary={}",
+            # Aligned with Pant's internal value in pants.toml
             "--python-resolves-to-uploaded-prior-to={'__default__': 'P2D'}",
             "--python-separate-lockfile-metadata-file",
             "--pex-emit-warnings=true",
